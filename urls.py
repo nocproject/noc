62aef2ccef922cc34c7618333509d5722f15ee60
Dmitry Volodin <dv@noc.effortel.ru>
2008-07-24 00:32:49 +0400

    * Migrate IP4vAddresses * Common infrastructure imported from old project

--- urls.py
+++ urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls.defaults import *
 
+handler404="noc.main.views.handler404"
+
 urlpatterns = patterns('',
     # For debugging purposes only. Overriden by lighttpd/apache
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'static'}),
-    # Example:
-    # (r'^noc/', include('noc.foo.urls')),
+     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'static'}),
+    # 
+     (r'^$',      include('noc.main.urls')),
      (r'^admin/', include('django.contrib.admin.urls')),
      (r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name':'template/login.html'}),
+     (r'^main/',  include('noc.main.urls')),
      (r"^ip/", include("noc.ip.urls")),
 )
