0838606050cdfa1a97e84d349ceadc72c8f570c6
Dmitry Volodin <dv@noc.effortel.ru>
2008-07-28 11:21:12 +0400

    Site-specific settings are in site_settings.py now

--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
 # Django settings for noc project.
+# Do not modify this file directly
+# Edit site_settings.py instead
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -9,10 +11,10 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'postgresql_psycopg2'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
-DATABASE_NAME = 'noc'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'dima'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'dima'         # Not used with sqlite3.
+DATABASE_ENGINE = ''           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
+DATABASE_NAME = ''             # Or path to database file if using sqlite3.
+DATABASE_USER = ''             # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
@@ -88,3 +90,6 @@ INSTALLED_APPS = (
     "noc.dns",
     "noc.peer",
 )
+
+# Override defaults using site-specific settings
+from site_settings import *
