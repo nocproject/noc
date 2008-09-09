477362ecdbbbcd1a1d501159fbb067da0d39f523
Dmitry Volodin <dv@noc.effortel.ru>
2008-09-09 17:42:08 +0400

    remove views.index and obsolete templates

--- peer/urls.py
+++ peer/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls.defaults import *
 from django.contrib.auth.decorators import login_required
-from noc.peer.views import index,as_rpsl,as_dot,as_set_rpsl
+from noc.peer.views import as_rpsl,as_set_rpsl,as_dot
 
 urlpatterns = patterns ( "",
-    (r"^$",                                      login_required(index)),
     (r"^AS/(?P<asn>\d+)/rpsl/",                  login_required(as_rpsl)),
     (r"^AS/(?P<asn>\d+)/dot/",                  login_required(as_dot)),
     (r"^AS-SET/(?P<as_set>AS-[A-Z0-9\-]+)/rpsl/", login_required(as_set_rpsl)),
--- peer/views.py
+++ peer/views.py
@@ -2,18 +2,13 @@ from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect,HttpResponseForbidden
 
 from noc.peer.models import AS,ASSet
-from noc.lib.render import render
+from noc.lib.render import render,render_plain_text
 from noc.lib.validators import is_asn,is_as_set
 
-def index(request):
-    ases=AS.objects.all()
-    as_sets=ASSet.objects.all()
-    return render(request,"peer/index.html",{"ases":ases,"as_sets":as_sets})
-    
 def as_rpsl(request,asn):
     assert is_asn(asn)
     asn=get_object_or_404(AS,asn=int(asn))
-    return render(request,"peer/as_rpsl.html",{"asn":asn})
+    return render_plain_text(asn.rpsl)
     
 def as_dot(request,asn):
     assert is_asn(asn)
@@ -23,4 +18,4 @@ def as_dot(request,asn):
 def as_set_rpsl(request,as_set):
     assert is_as_set(as_set)
     as_set=get_object_or_404(ASSet,name=as_set)
-    return render(request,"peer/as_set_rpsl.html",{"as_set":as_set})
+    return render_plain_text(as_set.rpsl)
