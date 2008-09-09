ddb6b6d68d6488aa1c0a59a48d07bc6a01d583c4
Dmitry Volodin <dv@noc.effortel.ru>
2008-09-09 09:21:25 +0400

    admin templates are moved to template/ include template/ into TEMPLATE_PATH

--- settings.py
+++ settings.py
@@ -74,6 +74,7 @@ TEMPLATE_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     ".",
+    "template"
 )
 
 CACHE_BACKEND = 'locmem:///'
