bb3c6a2ac67a1d0980594dda88853b3340c7f2a4
Dmitry Volodin <dv@noc.effortel.ru>
2008-09-09 17:21:43 +0400

    render_plain_text shortcut

--- lib/render.py
+++ lib/render.py
@@ -1,5 +1,10 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from django.http import HttpResponse
 
 def render(request,template,dict={}):
     return render_to_response(template,dict,context_instance=RequestContext(request))
+
+def render_plain_text(text):
+    return HttpResponse(text,mimetype="text/plain")
+    
