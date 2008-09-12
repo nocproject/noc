4ddb8c2c4cd463a0869727ad17e632577e469e9f
Dmitry Volodin <dv@noc.effortel.ru>
2008-09-12 15:10:47 +0400

    More logic in SocketHandler. Preparing to move SocketHandler into lib/

--- scripts/lgd.py
+++ scripts/lgd.py
@@ -40,7 +40,7 @@ class Supervisor(object):
             
     def start_lookup(self,id,peering_point_id,query_type_id,query):
         self.cursor.execute("BEGIN")
-        self.cursor.execute("SELECT pp.lg_rcmd,c.command "\
+        self.cursor.execute("SELECT ppt.name,pp.lg_rcmd,c.command "\
             +"FROM peer_lgquerycommand c JOIN peer_peeringpointtype ppt ON (c.peering_point_type_id=ppt.id) "\
             +"JOIN peer_peeringpoint pp ON (pp.type_id=ppt.id) "\
             +"WHERE pp.id=%d AND c.query_type_id=%d"%(peering_point_id,query_type_id))
@@ -48,18 +48,20 @@ class Supervisor(object):
         if len(r)==0:
             self.query_error("Query type is not supported")
         else:
-            rcmd,command=r[0]
+            ppt,rcmd,command=r[0]
             if "%(query)s" in command:
                 command=command%{"query":query}
-            self.rcmd(id,rcmd,command)
+            self.rcmd(ppt,id,rcmd,command)
         self.cursor.execute("COMMIT")
         
     def query_error(self,id,msg):
+        logging.debug("Query error: query_id=%d %s"%(id,msg))
         self.cursor.execute("BEGIN")
         self.cursor.execute("UPDATE peer_lgquery SET status='f',out='%s' WHERE id=%d"%(msg.replace("'","''"),id))
         self.cursor.execute("END")
         
-    def rcmd(self,id,rcmd,command):
+    def rcmd(self,peeting_point_type,id,rcmd,command):
+        logging.debug("RCMD: %s"%command)
         match=rx_url.match(rcmd)
         if not match:
             self.query_error(id,"Invalid RCMD")
@@ -71,9 +73,10 @@ class Supervisor(object):
         if scheme not in ACCESS_SCHEME:
             self.query_error("Unsupported access scheme: %s"%scheme)
             return
-        self.children[id]=ACCESS_SCHEME[scheme](self,id,host,user,password,command)
+        self.children[id]=ACCESS_SCHEME[scheme](self,STREAM_PARSERS[peeting_point_type],id,host,user,password,command)
         
     def append_output(self,id,out,status="p"):
+        logging.debug("Writing output: id=%d status=%s out=%s"%(id,status,out.replace("\n","\\n")))
         self.cursor.execute("BEGIN")
         self.cursor.execute("UPDATE peer_lgquery SET status='%s',out=out||'%s' WHERE id=%d"%(status.replace("'","''"),
             out.replace("'","''"),id))
@@ -111,13 +114,35 @@ class QueryChecker(asyncore.dispatcher):
 
     def handle_connect(self):
         pass
+        
+##
+## Stream Parsers
+##
+class StreamParser(object):
+    pattern_username="[Uu]sername:"
+    pattern_password="[Pp]assword:"
+    pattern_prompt=r"^\S*[>#]"
+    startup_commands=[]
+    logout_commands=["exit"]
+    
+class CiscoParser(StreamParser): pass
+
+class JuniperParser(StreamParser):
+    pattern_prompt="^({master}\n)?\S*>"
+
+STREAM_PARSERS={
+    "Cisco"   : CiscoParser,
+    "Juniper" : JuniperParser,
+}
+
 ##
 ##
 ##
 class SocketHandler(asyncore.dispatcher):
-    def __init__(self,supervisor,id,host,user,password,command):
+    def __init__(self,supervisor,parser,id,host,user,password,command):
         asyncore.dispatcher.__init__(self)
         self.supervisor=supervisor
+        self.parser=parser
         self.id=id
         self.host=host
         self.user=user
@@ -125,33 +150,58 @@ class SocketHandler(asyncore.dispatcher):
         self.command=command
         self.out_buffer=""
         self.in_buffer=""
-        self.result=""
+        self.to_write_result=False
         self.expect=None
         self.prepare_socket()
-        
+        self.startup_commands=self.parser.startup_commands[:]
+        self.logout_commands=self.parser.logout_commands[:]
+        self.set_expect(
+            [
+                (self.parser.pattern_username,self.x_username),
+                (self.parser.pattern_password,self.x_password),
+            ])
     def prepare_socket(self):
-        pass
+        raise Exception,"No socket created"
         
     def set_expect(self,v=None):
         if v:
             r=[]
             for rx,handler in v:
-                r.append((re.compile(rx),handler))
+                r.append((re.compile(rx,re.DOTALL|re.MULTILINE),handler))
             self.expect=r
         else:
             self.expect=None
             
+    def set_result_output(self,v):
+        if v:
+            logging.debug("Starting to write result")
+        else:
+            logging.debug("Stopping to write result")
+        self.to_write_result=v
+            
     def write(self,txt):
+        logging.debug("Writing: %s"%txt.replace("\n","\\n"))
         self.out_buffer+=txt
+        
+    def write_result(self,r):
+        if self.to_write_result:
+            logging.debug("Writting result: %s"%r.replace("\n","\\n"))
+            self.supervisor.append_output(self.id,r)
     
     def handle_read(self):
-        self.in_buffer+=self.recv(8192)
+        d=self.recv(8192)
+        d=d.replace("\r","")
+        logging.debug("Received: %s"%d.replace("\n","\\n"))
+        self.in_buffer+=d
         if self.expect:
             for rx,handler in self.expect:
                 match=rx.search(self.in_buffer)
                 if match:
+                    self.write_result(self.in_buffer[:match.start(0)])
+                    self.in_buffer=self.in_buffer[match.end(0):]
+                    logging.debug("FSM transition: %s"%(str(handler)))
                     self.set_expect(handler(match))
-                    self.in_buffer=self.in_buffer[match.endpos:]
+                    break
         
     def handle_write(self):
         sent=self.send(self.out_buffer)
@@ -164,61 +214,60 @@ class SocketHandler(asyncore.dispatcher):
         pass
         
     def handle_close(self):
+        logging.debug("Handle close")
         self.close()
-        self.supervisor.append_output(self.id,self.result,status="c")
+        self.write_result(self.in_buffer)
+        self.supervisor.append_output(self.id,"",status="c")
+        
+    def x_username(self,match):
+        self.write(self.user+"\n")
+        return [(self.parser.pattern_password,self.x_password)]
 
+    def x_password(self,match):
+        self.write(self.password+"\n")
+        if len(self.startup_commands):
+            return [(self.parser.pattern_prompt,self.x_startup_prompt)]
+        else:
+            return [(self.parser.pattern_prompt,self.x_prompt)]
+            
+    def x_startup_prompt(self,match):
+        sc=self.startup_commands.pop(0)
+        self.write(sc+"\n")
+        if len(self.startup_commands):
+            return [(self.parser.pattern_prompt,self.x_startup_prompt)]
+        else:
+            return [(self.parser.pattern_prompt,self.x_prompt)]
+    
+    def x_logout_prompt(self,match):
+        self.set_result_output(False)
+        lc=self.logout_commands.pop(0)
+        self.write(lc+"\n")
+        return [(self.parser.pattern_prompt,self.x_logout_prompt)]
+
+    def x_prompt(self,match):
+        self.write(self.command+"\n")
+        self.set_result_output(True)
+        return [(self.parser.pattern_prompt,self.x_logout_prompt)]
 ##
 ##
 ##
 class TelnetHandler(SocketHandler):
     def prepare_socket(self):
+        logging.debug("TelnetHandler connecting %s"%self.host)
         self.create_socket(socket.AF_INET,socket.SOCK_STREAM)
         self.connect((self.host,23))
-        self.set_expect([("Username:",self.x_username)])
-        
-    # Expect handlers
-    def x_username(self,match):
-        self.write(self.user+"\n")
-        return [("Password:",self.x_password)]
-        
-    def x_password(self,match):
-        self.write(self.password+"\n")
-        return [("[>#]",self.x_prompt)]
-        
-    def x_prompt(self,match):
-        self.write(self.command+"\n")
-        return [("[>#]",self.x_end_prompt)]
-        
-    def x_end_prompt(self,match):
-        self.result+=self.in_buffer
-        self.write("exit"+"\n")
-        return []
 #
 #
 #
 class SSHHandler(SocketHandler):
     def prepare_socket(self):
+        logging.debug("SSHHandler connecting %s"%self.host)
         pid,fd=os.forkpty()
         if pid==0:
-            os.execv("/usr/bin/ssh",["/usr/bin/ssh","-l",self.user,self.host,self.command])
+            os.execv("/usr/bin/ssh",["/usr/bin/ssh","-l",self.user,self.host])
         else:
             self.set_socket(asyncore.file_wrapper(fd))
-            self.set_expect([("password:",self.x_password)])
-            
-    def x_password(self,match):
-        self.write(self.password+"\n")
-        return [("[>#]",self.x_prompt)]
-        
-    def x_prompt(self,match):
-        self.write(self.command+"\n")
-        return [("[>#]",self.x_end_prompt)]
-        
-    def x_end_prompt(self,match):
-        self.result+=self.in_buffer
-        print "<<<<",self.result,">>>>"
-        self.write("exit"+"\n")
-        return []
-            
+
 ACCESS_SCHEME={
     "telnet" : TelnetHandler,
     "ssh"    : SSHHandler,
