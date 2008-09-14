1005add59c4731208cadc207c4a66f76a50c5b7b
Dmitry Volodin <dv@noc.effortel.ru>
2008-09-14 19:54:11 +0400

    * is_differ and rewrite_when_differ functions. * safe_rewrite not creates all necessary directories

--- lib/fileutils.py
+++ lib/fileutils.py
@@ -1,10 +1,12 @@
 ##
 ##
 ##
-import os,tempfile
+import os,tempfile,sha
 
 def safe_rewrite(path,text):
     d=os.path.dirname(path)
+    if not os.path.exists(d):
+        os.makedirs(d)
     b=os.path.basename(path)
     h,p=tempfile.mkstemp(suffix=".tmp",prefix=b,dir=d)
     f=os.fdopen(h,"w")
@@ -14,3 +16,24 @@ def safe_rewrite(path,text):
         os.unlink(path)
     os.link(p,path)
     os.unlink(p)
+##
+## Check file content is differ from string
+##
+def is_differ(path,content):
+    if os.path.isfile(path):
+        f=open(path)
+        cs1=sha.sha(f.read()).digest()
+        f.close()
+        cs2=sha.sha(content).digest()
+        return cs1!=cs2
+    else:
+        return True
+##
+## Rewrites file when content is differ
+## Returns boolean signalling wherher file was rewritten
+##
+def rewrite_when_differ(path,content):
+    d=is_differ(path,content)
+    if d:
+        safe_rewrite(path,content)
+    return d
