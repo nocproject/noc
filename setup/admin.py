06a48a60ce007c5dccc5db3c0a39b1ff8616b818
Dmitry Volodin <dv@noc.effortel.ru>
2008-09-21 01:37:19 +0400

    Display setting's value in admin list

--- setup/admin.py
+++ setup/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from noc.setup.models import Settings
 
-class SettingsAdmin(admin.ModelAdmin): pass
+class SettingsAdmin(admin.ModelAdmin):
+    list_display=["key","value","default"]
+    search_fields=["key","value"]
 
 admin.site.register(Settings,SettingsAdmin)
