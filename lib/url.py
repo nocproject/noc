e644254f9e414dd7ba0587f22f5dcabf8df8d7dc
Dmitry Volodin <dv@noc.effortel.ru>
2008-09-21 01:36:24 +0400

    URL class

---
+++ lib/url.py
@@ -0,0 +1,21 @@
+##
+## URL Processing functions
+##
+import re
+
+rx_url=re.compile(r"^(?P<scheme>[^:]+)://(?P<user>[^:]+):(?P<password>[^@]+)@(?P<host>[^/]+)(?:(?P<port>\d+))?(?P<path>.*)$")
+
+class InvalidURLException(Exception): pass
+
+class URL(object):
+    def __init__(self,url):
+        self.url=url
+        match=rx_url.match(self.url)
+        if not match:
+            raise InvalidURLException
+        self.scheme=match.group("scheme")
+        self.user=match.group("user")
+        self.password=match.group("password")
+        self.host=match.group("host")
+        self.port=match.group("port")
+        self.path=match.group("path")
