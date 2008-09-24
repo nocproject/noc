ba0a073b24f579ea9c0de79daf6fea0e24485091
Dmitry Volodin <dv@noc.effortel.ru>
2008-09-24 20:08:25 +0400

    PeeringPoint.provision_rcmd

--- peer/migrations/0009_filter_name.py
+++ peer/migrations/0009_filter_name.py
@@ -7,7 +7,11 @@ class Migration:
     def forwards(self):
         db.add_column("peer_peer","import_filter_name",models.CharField("Import Filter Name",max_length=64,blank=True,null=True))
         db.add_column("peer_peer","export_filter_name",models.CharField("Export Filter Name",max_length=64,blank=True,null=True))
+        db.add_column("peer_peeringpoint","provision_rcmd",models.CharField("Provisioning URL",max_length=128,blank=True,null=True))
+        
     
     def backwards(self):
+        db.delete_column("peer_peeringpoint","provision_rcmd")
         db.delete_column("peer_peer","import_filter_name")
         db.delete_column("peer_peer","export_filter_name")
+        
