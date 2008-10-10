a9318d8408609e3d37f8b6eaa979335b4791f518
Dmitry Volodin <dv@noc.effortel.ru>
2008-10-10 22:18:37 +0400

    LG Hilight column

--- sa/reports/supported_equipment.py
+++ sa/reports/supported_equipment.py
@@ -1,4 +1,4 @@
-from noc.main.report import Column
+from noc.main.report import Column,BooleanColumn
 import noc.main.report
 from noc.sa.profiles import profile_registry
 
@@ -6,9 +6,9 @@ class Report(noc.main.report.Report):
     name="sa.supported_equipment"
     title="Supported Equipment"
     requires_cursor=False
-    columns=[Column("Vendor"),Column("OS")]
+    columns=[Column("Vendor"),Column("OS"),BooleanColumn("LG Hilight")]
     
     def get_queryset(self):
-        r=[x[0] for x in profile_registry.choices]
-        r.sort()
-        return [x.split(".") for x in r]
+        r=[x for x in profile_registry.classes.items()]
+        r.sort(lambda x,y:cmp(x[0],y[0]))
+        return [x.split(".")+[c.pattern_lg_as_path_list is not None or c.pattern_lg_best_path is not None] for x,c in r]
