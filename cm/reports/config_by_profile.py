d3eca4444acb991933b6c65125cdab211d81ab30
Dmitry Volodin <dv@noc.effortel.ru>
2008-10-20 14:23:37 +0400

    Config by Profile report

---
+++ cm/reports/config_by_profile.py
@@ -0,0 +1,11 @@
+from noc.main.report import Column,BooleanColumn
+import noc.main.report
+
+class Report(noc.main.report.Report):
+    name="cm.config_by_profile"
+    title="Configs by profile"
+    requires_cursor=True
+    columns=[Column("Profile"),Column("Qty")]
+    
+    def get_queryset(self):
+        return self.execute("SELECT profile_name,COUNT(*) FROM cm_object WHERE handler_class_name='config' GROUP BY 1 ORDER BY 2 DESC")
