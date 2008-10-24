8ef97c197510daebadf87fd5ff807fe09ef24916
Dmitry Volodin <dv@noc.effortel.ru>
2008-10-24 14:50:50 +0400

    more_pattern fixed

--- sa/profiles/Alcatel/AOS.py
+++ sa/profiles/Alcatel/AOS.py
@@ -8,6 +8,6 @@ import noc.sa.profiles
 class Profile(noc.sa.profiles.Profile):
     name="Alcatel.AOS"
     pattern_username="User Name:"
-    pattern_more="^More: "
+    pattern_more="^More: .*?$"
     command_more=" "
     command_pull_config=["show running-config"]
