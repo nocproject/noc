b9b5799ea1baf1d1bb475489ec32703e5d65c576
Dmitry Volodin <dv@noc.effortel.ru>
2008-10-26 19:24:32 +0300

    diff preview

--- cm/urls.py
+++ cm/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls.defaults import *
 from django.contrib.auth.decorators import login_required
-from noc.cm.views import view
+from noc.cm.views import view,diff
 
 urlpatterns = patterns ( "",
     (r"^view/(?P<object_id>\d+)/$", login_required(view)),
     (r"^view/(?P<object_id>\d+)/(?P<revision>\d+)/$", login_required(view)),
     (r"^view/(?P<object_id>\d+)/text/$", login_required(view), {"format":"text"}),
     (r"^view/(?P<object_id>\d+)/(?P<revision>\d+)/text/$", login_required(view), {"format":"text"}),
-    
+    (r"^view/(?P<object_id>\d+)/diff/", login_required(diff)),
 )
--- cm/views.py
+++ cm/views.py
@@ -2,7 +2,7 @@ from noc.cm.models import Object
 from django.shortcuts import get_object_or_404
 from noc.lib.render import render,render_plain_text
 import os
-from django.http import HttpResponseNotFound
+from django.http import HttpResponseNotFound,HttpResponseRedirect
 
 def view(request,object_id,revision=None,format="html"):
     o=get_object_or_404(Object,id=int(object_id))
@@ -23,3 +23,13 @@ def view(request,object_id,revision=None,format="html"):
         return render_plain_text(content)
     else:
         return HttpResponseNotFound("Invalid format: %s"%format)
+
+def diff(request,object_id):
+    o=get_object_or_404(Object,id=int(object_id))
+    if request.POST:
+        r1=o.find_revision(request.POST["r1"])
+        r2=o.find_revision(request.POST["r2"])
+        diff=o.diff(r1,r2)
+        return render(request,"cm/diff.html",{"o":o,"diff":diff,"r1":r1,"r2":r2})
+    else:
+        return HttpResponseRedirect("/cm/view/%d/"%o.id)
