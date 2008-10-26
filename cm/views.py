6c61b858bbf8a28d6d7e6585b23a39d52d3d757f
Dmitry Volodin <dv@noc.effortel.ru>
2008-10-26 18:53:50 +0300

    Revision preview

--- cm/views.py
+++ cm/views.py
@@ -4,7 +4,7 @@ from noc.lib.render import render,render_plain_text
 import os
 from django.http import HttpResponseNotFound
 
-def view(request,object_id,revision=None):
+def view(request,object_id,revision=None,format="html"):
     o=get_object_or_404(Object,id=int(object_id))
     if revision:
         r=None
@@ -16,8 +16,10 @@ def view(request,object_id,revision=None):
             return HttpResponseNotFound("Revision not found")
     else:
         r=o.revisions[0]
-    return render(request,"cm/view.html",{"o":o,"r":r})
-    
-def text(request,object_id):
-    o=get_object_or_404(Object,id=int(object_id))
-    return render_plain_text(o.data)
+    content=o.get_revision(r)
+    if format=="html":
+        return render(request,"cm/view.html",{"o":o,"r":r,"content":content})
+    elif format=="text":
+        return render_plain_text(content)
+    else:
+        return HttpResponseNotFound("Invalid format: %s"%format)
