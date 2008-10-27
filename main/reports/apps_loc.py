336c99e5d55d4c14ed4c724d6afcfde607edfb7d
Dmitry Volodin <dv@noc.effortel.ru>
2008-10-27 07:35:43 +0300

    RST and Other LOCs

--- main/reports/apps_loc.py
+++ main/reports/apps_loc.py
@@ -6,7 +6,11 @@ class Report(noc.main.report.Report):
     name="main.apps_loc"
     title="Lines of code"
     requires_cursor=False
-    columns=[Column("App"),Column(".py lines",align="RIGHT"),Column(".html lines",align="RIGHT")]
+    columns=[Column("App"),
+            Column(".py lines",align="RIGHT"),
+            Column(".html lines",align="RIGHT"),
+            Column(".rst lines",align="RIGHT"),
+            Column("Other lines",align="RIGHT")]
     
     def get_queryset(self):
         def loc(f):
@@ -18,11 +22,19 @@ class Report(noc.main.report.Report):
         for dirname in [d for d in os.listdir(".") if os.path.isdir(d) and not d.startswith(".")]:
             py_loc=0
             html_loc=0
+            rst_loc=0
+            other_loc=0
             for dirpath,dirnames,filenames in os.walk(dirname):
-                for f in [f for f in filenames if f.endswith(".py")]:
-                    py_loc+=loc(os.path.join(dirpath,f))
-                for f in [f for f in filenames if f.endswith(".html")]:
-                    html_loc+=loc(os.path.join(dirpath,f))
-            r.append((dirname,py_loc,html_loc))
+                for f in [f for f in filenames if not (f.endswith(".pyc") or f.endswith(".pyo"))]:
+                    lines=loc(os.path.join(dirpath,f))
+                    if f.endswith(".py"):
+                        py_loc+=lines
+                    elif f.endswith(".html"):
+                        html_loc+=lines
+                    elif f.endswith(".rst"):
+                        rst_loc+=lines
+                    else:
+                        other_loc+=lines
+            r.append((dirname,py_loc,html_loc,rst_loc,other_loc))
         return r
             
