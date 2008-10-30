ccf13a046006e5b29297bcdeadcbaaaff2de5d41
Dmitry Volodin <dv@noc.effortel.ru>
2008-10-31 00:31:21 +0300

    cm.last_changes report

---
+++ cm/reports/last_changes.py
@@ -0,0 +1,26 @@
+from noc.main.report import Column
+import noc.main.report
+from noc.cm.models import Object
+from django import forms
+import datetime
+
+class ReportForm(forms.Form):
+    repo=forms.ChoiceField(label="Type",choices=[("config","config"),("dns","dns"),("prefix-list","prefix-list")])
+    days=forms.IntegerField(label="In Days",min_value=1)
+
+class Report(noc.main.report.Report):
+    name="cm.last_changes"
+    title="Latest Changes"
+    form_class=ReportForm
+    columns=[Column("Object"),Column("Last Changed")]
+    
+    def get_queryset(self):
+        oc=Object.get_object_class(self.form.cleaned_data["repo"])
+        baseline=datetime.datetime.now()-datetime.timedelta(days=self.form.cleaned_data["days"])
+        r=[]
+        for o in oc.objects.all():
+            lm=o.last_modified
+            if lm and lm>baseline:
+                r+=[[o,lm]]
+        r.sort(lambda x,y:-cmp(x[1],y[1]))
+        return r
