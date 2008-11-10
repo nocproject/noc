63eb6802aeaabc2d53c9dae8ed14644b5175ad79
Dmitry Volodin <dv@noc.effortel.ru>
2008-11-10 17:23:49 +0300

    Juniper.ScreenOS is working now

--- sa/profiles/Juniper/ScreenOS.py
+++ sa/profiles/Juniper/ScreenOS.py
@@ -8,7 +8,7 @@ from noc.sa.protocols.sae_pb2 import TELNET,SSH
 class Profile(noc.sa.profiles.Profile):
     name="Juniper.ScreenOS"
     supported_schemes=[TELNET,SSH]
-    pattern_prompt="^\S*-> "
+    pattern_prompt=r"^\s*\S*-> "
     pattern_more=r"^--- more ---"
     command_more=" "
     #pattern_lg_as_path_list=r"(?<=AS path: )(\d+(?: \d+)*)"
