5a256d007e0c53174dd340974e8002432600d6fb
Dmitry Volodin <dv@noc.effortel.ru>
2008-11-10 14:50:47 +0300

    Protei.{MAK,ITG,MTU}

---
+++ sa/profiles/Protei/ITG.py
@@ -0,0 +1,14 @@
+##
+## Vendor: Protei
+## OS:     MAK
+##
+import noc.sa.profiles
+from noc.sa.protocols.sae_pb2 import TELNET,SSH
+
+class Profile(noc.sa.profiles.Profile):
+    name="Protei.MAK"
+    supported_schemes=[TELNET,SSH]
+    command_submit="\r"
+    pattern_prompt="(^\S+\$|ITG>)"
+    command_pull_config=["cd /usr/protei/CLI/Client","./clip","show-recursive",""]
+    config_skip_head=10
