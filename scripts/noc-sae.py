84702ff2db9fa37ffde656e01587347678cfb24b
Dmitry Volodin <dv@noc.effortel.ru>
2008-11-15 19:14:29 +0300

    became_daemon moved to sysutils

--- scripts/noc-sae.py
+++ scripts/noc-sae.py
@@ -13,37 +13,6 @@ def usage():
     print "\t-L\t\t- Listen at <ip>:<port>"
     print "\t-l<logfile>\t- Write log to <logfile>"
     print "\t-p<pidfile>\t- Write pid to <pidfile>"
-    
-def become_daemon(dirname,pidfile=None,umask=022):
-    logging.debug("Change directory to '%s'"%dirname)
-    os.chdir(dirname)
-    try:
-        if os.fork():
-            sys.exit(0)
-    except OSError,e:
-        sys.stderr.write("Fork failed")
-        sys.exit(1)
-    os.setsid()
-    os.umask(umask)
-    try:
-        pid=os.fork()
-    except OSError,e:
-        sys.stderr.write("Fork failed")
-        os._exit(1)
-    if pid:
-        if pidfile:
-            f=open(pidfile,"w")
-            f.write(str(pid))
-            f.close()
-        os._exit(0)
-    i=open("/dev/null","r")
-    o=open("/dev/null","a+")
-    e=open("/dev/null","a+")
-    os.dup2(i.fileno(), sys.stdin.fileno())
-    os.dup2(o.fileno(), sys.stdout.fileno())
-    os.dup2(e.fileno(), sys.stderr.fileno())
-    sys.stdout=o
-    sys.stderr=e
 #
 def main():
     log_level=logging.INFO
@@ -74,6 +43,7 @@ def main():
         logging.basicConfig(level=log_level,format='%(asctime)s %(levelname)s %(message)s')
     logging.info("Starting SAE")
     if not run_foreground:
+        from noc.lib.sysutils import become_daemon
         dirname=os.path.join(os.path.dirname(sys.argv[0]),"..")
         become_daemon(dirname,pidfile)
     os.environ['DJANGO_SETTINGS_MODULE']="noc.settings"
