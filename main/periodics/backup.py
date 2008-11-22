350143de0d917738ce0b879e96f843c206bc4613
Dmitry Volodin <dv@noc.effortel.ru>
2008-11-23 01:04:54 +0300

    Remove broken backup when pg_dump failed. Backup no longer use /bin/sh

--- main/periodics/backup.py
+++ main/periodics/backup.py
@@ -17,21 +17,29 @@ class Task(noc.sa.periodic.Task):
         from noc.setup.models import Settings
         from django.conf import settings
         
-        args=[Settings.get("shell.pg_dump"),"-Fc"]
-        if settings.DATABASE_USER:
-            args+=["-U '%s'"%settings.DATABASE_USER]
-        #if settings.DATABASE_PASSWORD:
-        #    args+=["-W"]
-        if settings.DATABASE_HOST:
-            args+=["-h '%s'"%settings.DATABASE_HOST]
-        if settings.DATABASE_PORT:
-            args+=["-p '%s'"%settings.DATABASE_PORT]
-        args+=["'%s'"%settings.DATABASE_NAME]
         now=datetime.datetime.now()
+        cmd=[Settings.get("shell.pg_dump"),"-Fc"]
         out="noc-%04d-%02d-%02d-%02d-%02d.dump"%(now.year,now.month,now.day,now.hour,now.minute)
         out=os.path.join(Settings.get('main.backup_dir'),out)
+        cmd+=["-f",out]
+        if settings.DATABASE_USER:
+            cmd+=["-U",settings.DATABASE_USER]
+        #if settings.DATABASE_PASSWORD:
+        #    cmd+=["-W"]
+        if settings.DATABASE_HOST:
+            cmd+=["-h",settings.DATABASE_HOST]
+        if settings.DATABASE_PORT:
+            cmd+=["-p",str(settings.DATABASE_PORT)]
+        cmd+=[settings.DATABASE_NAME]
+
         logging.debug("main.backup: dumping into %s"%out)
-        cmd=" ".join(args)+" > "+"'%s'"%out
-        p=subprocess.Popen(cmd,shell=True)
+        p=subprocess.Popen(cmd)
         pid,status=os.waitpid(p.pid,0)
+        if status!=0:
+            logging.error("main.backup: dump failed. Removing broken dump '%s'"%out)
+            # Remove broken dump
+            try:
+                os.unlink(out)
+            except:
+                pass
         return status==0
