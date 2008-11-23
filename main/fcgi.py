7924c11a9a36b1123ac60db51d370d399438d418
Dmitry Volodin <dv@noc.effortel.ru>
2008-11-24 00:57:12 +0300

    noc-fcgi uses noc.lib.daemon framework now

---
+++ main/fcgi.py
@@ -0,0 +1,13 @@
+from noc.lib.daemon import Daemon
+from django.core.servers.fastcgi import runfastcgi
+
+class FCGI(Daemon):
+    daemon_name="noc-fcgi"
+    def run(self):
+        runfastcgi(["method=threaded", 
+                    "daemonize=false",
+                    "socket=%s"%self.config.get("fcgi","socket"),
+                    "minspare=%d"%self.config.getint("fcgi","minspare"),
+                    "maxspare=%d"%self.config.getint("fcgi","maxspare"),
+                    "maxrequests=%s"%self.config.getint("fcgi","maxrequests"),
+                    "maxchildren=%s"%self.config.getint("fcgi","maxchildren")])
--- scripts/noc-fcgi.py
+++ scripts/noc-fcgi.py
@@ -1,56 +1,20 @@
 #!/usr/bin/env python
-from django.core.servers.fastcgi import runfastcgi
-import getopt,sys,os
+#
+# Service Activation Engine
+#
+import os,sys,getopt
 
-SOCKET="/tmp/noc.fcgi"
-MINSPARE=2
-MAXSPARE=3
-MAXREQUESTS=100
-MAXCHILDREN=20
-PIDFILE="/var/run/noc-fcgi.pid"
-PREFIX="/var/www/noc"
-
-def run():
-    sys.path.insert(0,PREFIX)
-    prev=os.sep+os.sep.join([x for x in PREFIX.split(os.sep) if x!=""][:-1])+os.sep
-    sys.path.insert(0,prev)
+if __name__ == '__main__':
+    d=os.path.dirname(sys.argv[0])
+    sys.path.insert(0,os.path.join(d,"..",".."))
+    sys.path.insert(0,os.path.join(d,".."))
+    sys.path.insert(0,d)
+    try:
+        import settings
+    except ImportError:
+        sys.stderr.write("Error: Can't find file 'settings.py'. (If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n")
+        sys.exit(1)
     os.environ['DJANGO_SETTINGS_MODULE']="noc.settings"
-    runfastcgi(["method=threaded", 
-                "daemonize=true",
-                "socket=%s"%SOCKET,
-                "minspare=%d"%MINSPARE,
-                "maxspare=%d"%MAXSPARE,
-                "maxrequests=%s"%MAXREQUESTS,
-                "maxchildren=%s"%MAXCHILDREN,
-                "pidfile=%s"%PIDFILE,
-                "workdir=%s"%PREFIX])
-                
-def usage():
-    print "USAGE:"
-    print "%s [-s <socket_path>] [-S<minspare>:<maxspare>] [-C<maxchildren>] [-R<maxrequests>] [-p<pidfile>] [-r<prefix>]"%sys.argv[0]
-    print "\t-s<socket_path>\t- UNIX socket for FastCGI backend"
-    print "\t-S<min_spare>:<max_spare>\t- Minimum and maximum of spare threads available"
-    print "\t-C<maxchildren>\t- Maximum of threads available"
-    print "\t-R<maxrequests>\t- Restart children after number of requests"
-    print "\t-p<pidfile>\t- Create pid file"
-    print "\t-r<prefix>\t - Root directory"
-    
-if __name__=="__main__":
-    optlist,optarg=getopt.getopt(sys.argv[1:],"s:S:C:R:p:r:")
-    for k,v in optlist:
-        if k=="-s":
-            SOCKET=v
-        elif k=="-S":
-            MINSPARE,MAXSPARE=[int(x) for x in v.split(":")]
-        elif k=="-C":
-            MAXCHILDREN=int(v)
-        elif k=="-R":
-            MAXREQUESTS=int(v)
-        elif k=="-p":
-            PIDFILE=v
-        elif k=="-r":
-            PREFIX=v
-        else:
-            usage()
-            sys.exit(1)
-    run()
+    from noc.main.fcgi import FCGI
+    fcgi=FCGI()
+    fcgi.process_command()
