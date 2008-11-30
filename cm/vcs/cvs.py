55da7296ffb2a58f588f22fa655e7cfcf78a2b77
Dmitry Volodin <dv@noc.effortel.ru>
2008-11-30 23:49:33 +0300

    os.system is finally removed

--- cm/vcs/cvs.py
+++ cm/vcs/cvs.py
@@ -12,7 +12,3 @@ class VCS(noc.cm.vcs.VCS):
     def check_repository(self):
         if not os.path.exists(os.path.join(self.repo,"CVSROOT")):
             self.cmd("init",check=False)
-    def cmd(self,cmd,check=True):
-        if check:
-            self.check_repository()
-        os.system("cd %s && %s -d %s %s"%(self.repo,Settings.get("cm.vcs_path"),self.repo,cmd))
