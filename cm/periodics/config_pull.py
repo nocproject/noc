67b5fb388b4efcf75f37f476b751dc0331e4d18a
Dmitry Volodin <dv@noc.effortel.ru>
2008-12-03 01:22:07 +0300

    Do not pull configs with empty pull_every field

--- cm/periodics/config_pull.py
+++ cm/periodics/config_pull.py
@@ -8,7 +8,7 @@ class Task(noc.sa.periodic.Task):
     
     def execute(self):
         from noc.cm.models import Config
-        for o in Config.objects.filter(Q(next_pull__lt=datetime.datetime.now())|Q(next_pull__isnull=True)):
+        for o in Config.objects.filter((Q(next_pull__lt=datetime.datetime.now())|Q(next_pull__isnull=True))&Q(pull_every__isnull=False)):
             logging.debug("Pulling %s",str(o))
             o.pull(self.sae)
         return True
