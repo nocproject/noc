83c1f16c1fc8a81ad931ba1dd40f4fde7eca8336
Dmitry Volodin <dv@noc.effortel.ru>
2008-12-03 09:00:11 +0300

    Order pulls by next_pull

--- cm/periodics/config_pull.py
+++ cm/periodics/config_pull.py
@@ -8,7 +8,7 @@ class Task(noc.sa.periodic.Task):
     
     def execute(self):
         from noc.cm.models import Config
-        for o in Config.objects.filter((Q(next_pull__lt=datetime.datetime.now())|Q(next_pull__isnull=True))&Q(pull_every__isnull=False)):
+        for o in Config.objects.filter((Q(next_pull__lt=datetime.datetime.now())|Q(next_pull__isnull=True))&Q(pull_every__isnull=False)).order_by("next_pull"):
             logging.debug("Pulling %s",str(o))
             o.pull(self.sae)
         return True
