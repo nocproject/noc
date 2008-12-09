89d883819562a7db6017232eb8014c56cdf53c7d
Dmitry Volodin <dv@noc.effortel.ru>
2008-12-09 09:06:41 +0300

    "terminal length 0" before "show configuration"

--- sa/profiles/Juniper/JUNOSe.py
+++ sa/profiles/Juniper/JUNOSe.py
@@ -15,6 +15,6 @@ class Profile(noc.sa.profiles.Profile):
     command_more=" "
     #pattern_lg_as_path_list=r"(?<=AS path: )(\d+(?: \d+)*)"
     #pattern_lg_best_path=r"^(\s+[+*].+?\s+Router ID: \S+)"
-    command_pull_config=["show configuration"]
+    command_pull_config=["terminal length 0","show configuration"]
     config_volatile=["^! Configuration script being generated on.*?^"]
     
