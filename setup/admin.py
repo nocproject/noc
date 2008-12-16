c75409ed207cf5b7dbf1b23c319b268a8ed5724e
Dmitry Volodin <dv@noc.effortel.ru>
2008-12-16 11:35:30 +0300

    setup module deprecated and removed
     * setup module removed
     * noc.settings.config used instead
     * All setup-related migrations are deprecated and removed

--- setup/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from noc.setup.models import Settings
-
-class SettingsAdmin(admin.ModelAdmin):
-    list_display=["key","value","default"]
-    search_fields=["key","value"]
-
-admin.site.register(Settings,SettingsAdmin)
