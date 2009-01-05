ddc85d0ac24456120d4f1a6ecb7b82a3b01a1c2e
Dmitry Volodin <dv@noc.effortel.ru>
2009-01-06 01:17:50 +0300

    Removed due to case mess

--- sa/profiles/ZyXEL/ZyNOS/get_config.py
+++
@@ -1,7 +0,0 @@
-import noc.sa.script
-
-class Script(noc.sa.script.Script):
-    name="Zyxel.ZyNOS.get_config"
-    def execute(self):
-        config=self.cli("show running-config")
-        return self.cleaned_config(config)
