969826f8a5fb161330c43e31d6e9f486b02d6975
Dmitry Volodin <dv@noc.effortel.ru>
2009-01-06 23:55:30 +0300

    Cisco.IOS.ping script

---
+++ sa/profiles/Cisco/IOS/ping.py
@@ -0,0 +1,26 @@
+import noc.sa.script
+import re
+
+rx_result=re.compile(r"^Success rate is \d+ percent \((?P<success>\d+)/(?P<count>\d+)\)(, round-trip min/avg/max = (?P<min>\d+)/(?P<avg>\d+)/(?P<max>\d+) ms)?",re.MULTILINE|re.DOTALL)
+
+class Script(noc.sa.script.Script):
+    name="Cisco.IOS.ping"
+    def execute(self,address,count=None,source_address=None,size=None,df=None):
+        cmd="ping ip %s"%address
+        if count:
+            cmd+=" count %d"%int(count)
+        if source_address:
+            cmd+=" source %s"%source_address
+        if size:
+            cmd+=" size %d"%int(size)
+        if df:
+            cmd+=" df-bit"
+        pr=self.cli(cmd)
+        match=rx_result.search(pr)
+        return {
+                "success": match.group("success"),
+                "count"  : match.group("count"),
+                "min"    : match.group("min"),
+                "avg"    : match.group("avg"),
+                "max"    : match.group("max"),
+            }
