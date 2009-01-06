5f260f95c038814948688fb7e06498038594b18c
Dmitry Volodin <dv@noc.effortel.ru>
2009-01-06 16:48:56 +0300

    HTTP scheme support

--- sa/profiles/Audiocodes/Mediant2000/get_config.py
+++ sa/profiles/Audiocodes/Mediant2000/get_config.py
@@ -3,6 +3,11 @@ import noc.sa.script
 class Script(noc.sa.script.Script):
     name="Audiocodes.Mediant2000.get_config"
     def execute(self):
-        self.cli("conf")
-        config=self.cli("cf get")
+        if self.access_profile.scheme in [self.TELNET,self.SSH]:
+            self.cli("conf")
+            config=self.cli("cf get")
+        elif self.access_profile.scheme==self.HTTP:
+            config=self.http.get("/FS/BOARD.ini")
+        else:
+            raise Exception("Unsupported access scheme")
         return self.cleaned_config(config)
