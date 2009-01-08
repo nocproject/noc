766e1b03fad69c28cab364d087d0db880d486316
Dmitry Volodin <dv@noc.effortel.ru>
2009-01-09 01:00:34 +0300

    Cisco.IOS.get_vlans and Cisoc.IOS.has_vlan scripts

---
+++ sa/profiles/Cisco/IOS/get_vlans.py
@@ -0,0 +1,19 @@
+import noc.sa.script
+from noc.sa.interfaces import IGetVlans
+import re
+
+rx_vlan_line=re.compile(r"^(?P<vlan_id>\d{1,4})\s+(?P<name>\S+)\s")
+
+class Script(noc.sa.script.Script):
+    name="Cisco.IOS.get_vlans"
+    implements=[IGetVlans]
+    def execute(self):
+        self.cli("terminal length 0")
+        vlans=self.cli("show vlan brief")
+        r=[]
+        for l in vlans.split("\n"):
+            match=rx_vlan_line.match(l.strip())
+            if match:
+                r.append({"vlan_id": int(match.group("vlan_id")),
+                          "name"   : match.group("name")})
+        return r
