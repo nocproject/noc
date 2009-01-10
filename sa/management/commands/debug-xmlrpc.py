104869f030101f25b927d16b3f53a9d4e5328b4c
Dmitry Volodin <dv@noc.effortel.ru>
2009-01-10 22:49:11 +0300

    manage.py "debug-xmlrpc" command.

---
+++ sa/management/commands/debug-xmlrpc.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from xmlrpclib import ServerProxy, Error
+import ConfigParser
+
+defaults_config_path="etc/%(daemon_name)s.defaults"
+
+class Command(BaseCommand):
+    help="Debug SAE XML-RPC server"
+    def handle(self, *args, **options):
+        config=ConfigParser.SafeConfigParser()
+        for p in ["etc/noc-sae.defaults","etc/noc-sae.conf"]:
+            config.read(p)
+        server=ServerProxy("http://%s:%d"%(config.get("xmlrpc","listen"),config.getint("xmlrpc","port")))
+        proxy=getattr(server,args[0])
+        print proxy(*args[1:])
+        
