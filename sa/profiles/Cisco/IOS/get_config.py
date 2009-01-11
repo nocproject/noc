f8f7bfc2f6cc575d876f22e3890b51e17b7dc1e3
Dmitry Volodin <dv@noc.effortel.ru>
2009-01-11 13:24:53 +0300

    Strip first 3 lines of config

--- sa/profiles/Cisco/IOS/get_config.py
+++ sa/profiles/Cisco/IOS/get_config.py
@@ -7,4 +7,5 @@ class Script(noc.sa.script.Script):
     def execute(self):
         self.cli("terminal length 0")
         config=self.cli("show running-config")
+        config=self.strip_first_lines(config,3)
         return self.cleaned_config(config)
