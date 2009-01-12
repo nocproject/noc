4664f93d2cf652190b360e9b53a996c043d4438f
Dmitry Volodin <dv@noc.effortel.ru>
2009-01-12 11:47:39 +0300

    Protei.MediaGateway.get_version script

---
+++ sa/profiles/Protei/MediaGateway/get_version.py
@@ -0,0 +1,17 @@
+import noc.sa.script
+from noc.sa.interfaces import IGetVersion
+import re
+
+rx_ver=re.compile(r"ProductCode (?P<version>\S+) build.*Hardware type: (?P<platform>\S+)",re.MULTILINE|re.DOTALL)
+
+class Script(noc.sa.script.Script):
+    name="Protei.MediaGateway.get_version"
+    implements=[IGetVersion]
+    def execute(self):
+        v=self.cli("_version full")
+        match=rx_ver.search(v)
+        return {
+            "vendor"    : "Protei",
+            "platform"  : match.group("platform"),
+            "version"   : match.group("version"),
+        }
