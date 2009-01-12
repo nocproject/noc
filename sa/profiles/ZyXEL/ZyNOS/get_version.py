7483a32a155333fdceff84ece5843edf1ad11b0f
Maksim Petukhov <maximus@noc.effortel.ru>
2009-01-12 16:13:48 +0300

    ZyXEL.ZyNOS.get_version

---
+++ sa/profiles/ZyXEL/ZyNOS/get_version.py
@@ -0,0 +1,17 @@
+import noc.sa.script
+from noc.sa.interfaces import IGetVersion
+import re
+
+rx=re.compile(r"ZyNOS F/W Version\s+(?P<version>\S+).+Product Model\s+(?P<platform>\S+)",re.MULTILINE|re.DOTALL)
+
+class Script(noc.sa.script.Script):
+    name="Zyxel.ZyNOS.get_version"
+    implements=[IGetVersion]
+    def execute(self):
+        v=self.cli("show system-information")
+        match=rx.search(v.replace(":",""))
+        return {
+            "vendor"    : "Zyxel",
+            "platform"  : match.group("platform"),
+            "version"   : match.group("version"),
+        }
