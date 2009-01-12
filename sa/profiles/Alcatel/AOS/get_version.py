e71923a25c5e51650e2617b4527d8385d3c47786
Maksim Petukhov <maximus@noc.effortel.ru>
2009-01-12 16:10:19 +0300

    Alcatel.AOS.get_versionn

---
+++ sa/profiles/Alcatel/AOS/get_version.py
@@ -0,0 +1,20 @@
+import noc.sa.script
+from noc.sa.interfaces import IGetVersion
+import re
+
+rx_sys=re.compile(r"System Description:\s+(?P<platform>.+?)$",re.MULTILINE|re.DOTALL)
+rx_ver=re.compile(r"SW version\s+(?P<version>\S+)",re.MULTILINE|re.DOTALL)
+
+class Script(noc.sa.script.Script):
+    name="Alcatel.AOS.get_version"
+    implements=[IGetVersion]
+    def execute(self):
+        v=self.cli("show system")
+        match_sys=rx_sys.search(v)
+        v=self.cli("show version")
+        match_ver=rx_ver.search(v)
+        return {
+            "vendor"    : "Alcatel",
+            "platform"  : match_sys.group("platform"),
+            "version"   : match_ver.group("version"),
+        }
