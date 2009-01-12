5785612179aeb18406fb70f71cf3aef35d7b3174
Maksim Petukhov <maximus@noc.effortel.ru>
2009-01-12 16:13:23 +0300

    Juniper.ScreenOS.get_version

---
+++ sa/profiles/Juniper/ScreenOS/get_version.py
@@ -0,0 +1,17 @@
+import noc.sa.script
+from noc.sa.interfaces import IGetVersion
+import re
+
+rx=re.compile(r"Product Name:\s+(?P<platform>\S+)$.+Software Version:\s+(?P<version>.+?)\s*\,",re.MULTILINE|re.DOTALL)
+
+class Script(noc.sa.script.Script):
+    name="Juniper.ScreenOS.get_version"
+    implements=[IGetVersion]
+    def execute(self):
+        v=self.cli("get system")
+        match=rx.search(v)
+        return {
+            "vendor"    : "Juniper",
+            "platform"  : match.group("platform"),
+            "version"   : match.group("version"),
+        }
