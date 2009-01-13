b58aeec8650cff5fb8ee56c1f159dea410f636f5
Dmitry Volodin <dv@noc.effortel.ru>
2009-01-13 08:52:46 +0300

    Set MAC-address format to "cisco"

--- sa/profiles/Cisco/IOS/__init__.py
+++ sa/profiles/Cisco/IOS/__init__.py
@@ -15,6 +15,7 @@ class Profile(noc.sa.profiles.Profile):
     pattern_lg_as_path_list=r"^(\s+\d+(?: \d+)*),"
     pattern_lg_best_path=r"(<A HREF.+?>.+?best)"
     requires_netmask_conversion=True
+    convert_mac=noc.sa.profiles.Profile.convert_mac_to_cisco
     config_volatile=["^ntp clock-period .*?^"]
     oid_trap_config_changed="1.3.6.1.4.1.9.9.43.2"
     syslog_config_changed="%SYS-5-CONFIG_I: Configured from"
