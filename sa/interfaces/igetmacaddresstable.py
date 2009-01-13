ce3d2794f4cbb0a0ad1f90e068e4b3345eef480f
Dmitry Volodin <dv@noc.effortel.ru>
2009-01-13 08:54:28 +0300

    IGetMACAddressTable now accepts optional interface, vlan and mac parameters

--- sa/interfaces/igetmacaddresstable.py
+++ sa/interfaces/igetmacaddresstable.py
@@ -1,6 +1,9 @@
 from base import *
 
 class IGetMACAddressTable(Interface):
+    interface=REStringParameter(r"^\S+$",required=False)
+    vlan=VLANIDParameter(required=False)
+    mac=MACAddressParameter(required=False)
     returns=ListOfParameter(element=DictParameter(attrs={"vlan_id"    : VLANIDParameter(),
                                                          "mac"        : MACAddressParameter(),
                                                          "interfaces" : StringListParameter(),
