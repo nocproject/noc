3f4b7ab5e99c5a7950edc25d5293281527e44f1a
Dmitry Volodin <dv@noc.effortel.ru>
2009-01-16 12:00:51 +0300

    Cisco.IOS.has_vlan -> Generic.has_vlan

---
+++ sa/profiles/Generic/has_vlan.py
@@ -0,0 +1,13 @@
+import noc.sa.script
+from noc.sa.interfaces import IHasVlan, IGetVlans
+import re
+
+class Script(noc.sa.script.Script):
+    name="Generic.has_vlan"
+    implements=[IHasVlan]
+    requires=[("get_vlans",IGetVlans)]
+    def execute(self,vlan_id):
+        for v in self.scripts.get_vlans():
+            if v["vlan_id"]==vlan_id:
+                return True
+        return False
