ebe9ee63afcaf8abc84fd41f1c5c364f645633ec
Dmitry Volodin <dv@noc.effortel.ru>
2009-01-19 08:48:20 +0300

    Huawei.VRP.get_vlans

---
+++ sa/profiles/Huawei/VRP/get_vlans.py
@@ -0,0 +1,11 @@
+import noc.sa.script
+from noc.sa.interfaces import IGetVlans
+
+class Script(noc.sa.script.Script):
+    name="Huawei.VRP.get_vlans"
+    implements=[IGetVlans]
+    def execute(self):
+        self.cli("undo terminal logging")
+        vlans=self.cli("display vlan").split("\n")
+        vlans=vlans[1].replace("(default)","")
+        return [{"vlan_id":int(x)} for x in self.expand_rangelist(vlans)]
