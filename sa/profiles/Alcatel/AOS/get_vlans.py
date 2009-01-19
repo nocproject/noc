47e46e6e22676e5688fd06fdee3a27cd0acd08fa
Dmitry Volodin <dv@noc.effortel.ru>
2009-01-19 08:24:48 +0300

    Alcatel.AOS.get_vlans

---
+++ sa/profiles/Alcatel/AOS/get_vlans.py
@@ -0,0 +1,22 @@
+import noc.sa.script
+from noc.sa.interfaces import IGetVlans
+
+class Script(noc.sa.script.Script):
+    name="Alcatel.AOS.get_vlans"
+    implements=[IGetVlans]
+    def execute(self):
+        vlans=self.cli("show vlan")
+        r=[]
+        in_body=False
+        for l in vlans.split("\n"):
+            l=l.strip()
+            if not in_body:
+                if l.startswith("----"):
+                    in_body=True
+                continue
+            try:
+                vlan_id,name,ports,vlan_type,auth=l.split()
+            except:
+                continue
+            r.append({"vlan_id":int(vlan_id),"name":name})
+        return r
