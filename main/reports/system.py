44e4f1520c061171be4249455a72a554d80c592a
Dmitry Volodin <dv@noc.effortel.ru>
2009-01-19 08:58:02 +0300

    main.system report

---
+++ main/reports/system.py
@@ -0,0 +1,17 @@
+from noc.main.report import Column
+import noc.main.report
+import os,sys
+import django
+
+class Report(noc.main.report.Report):
+    name="main.system"
+    title="System version"
+    requires_cursor=False
+    columns=[Column("Component"),Column("Version")]
+    def get_queryset(self):
+        return [
+            ["OS"," ".join(os.uname())],
+            ["Python",sys.version],
+            ["Django","%d.%d %s"%django.VERSION],
+            # NOC Version, hg tip
+        ]
