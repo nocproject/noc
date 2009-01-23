858ea73a1dad298fc8aef8230840060be27ef89e
Dmitry Volodin <dv@noc.effortel.ru>
2009-01-23 12:59:19 +0300

    Juniper.JUNOS.get_version uses SNMP when possible

--- sa/profiles/Juniper/JUNOS/get_version.py
+++ sa/profiles/Juniper/JUNOS/get_version.py
@@ -3,12 +3,23 @@ from noc.sa.interfaces import IGetVersion
 import re
 
 rx_ver=re.compile(r"Model:\s+(?P<platform>\S+).+JUNOS Base OS Software Suite \[(?P<version>[^\]]+)\]",re.MULTILINE|re.DOTALL)
+rx_snmp_ver=re.compile(r"Juniper Networks, Inc.\s+(?P<platform>\S+).+?JUNOS\s+(?P<version>\S+)")
 
 class Script(noc.sa.script.Script):
     name="Juniper.JUNOS.get_version"
     implements=[IGetVersion]
     def execute(self):
-        self.cli("set cli screen-length 0")
+        if self.snmp and self.access_profile.snmp_ro:
+            try:
+                v=self.snmp.get("1.3.6.1.2.1.1.1.0") # sysDescr.0
+                match=rx_snmp_ver.search(v)
+                return {
+                    "vendor"    : "Juniper",
+                    "platform"  : match.group("platform"),
+                    "version"   : match.group("version"),
+                }
+            except self.snmp.TimeOutError:
+                pass
         v=self.cli("show version")
         match=rx_ver.search(v)
         return {
