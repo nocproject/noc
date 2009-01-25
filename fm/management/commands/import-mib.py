7a00e8ac597bc4b6b29b628bce9789cbe90b6478
Dmitry Volodin <dv@noc.effortel.ru>
2009-01-25 22:05:17 +0300

    import-mib executed in transaction context now

--- fm/management/commands/import-mib.py
+++ fm/management/commands/import-mib.py
@@ -1,9 +1,14 @@
 from django.core.management.base import BaseCommand
-from noc.fm.models import MIB,MIBData
+from django.db import transaction
+from noc.fm.models import MIB
 
 class Command(BaseCommand):
     help="Import MIB into database"
     def handle(self, *args, **options):
+        transaction.enter_transaction_management()
         for a in args:
             MIB.load(a)
+        transaction.commit()
+        transaction.leave_transaction_management()
+
         
