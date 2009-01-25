1f212c6db6f0acc709d0818767f082cf1bb0c1dd
Dmitry Volodin <dv@noc.effortel.ru>
2009-01-25 22:28:29 +0300

    fm.mibs report

---
+++ fm/reports/mibs.py
@@ -0,0 +1,19 @@
+from noc.main.report import Column,BooleanColumn
+import noc.main.report
+
+class Report(noc.main.report.Report):
+    name="fm.mibs"
+    title="Loaded MIBs"
+    requires_cursor=True
+    columns=[
+        Column("MIB"),
+        Column("Last Updated"),
+        Column("Uploaded"),
+        Column("Count")]
+    
+    def get_queryset(self):
+        return self.execute("""
+            SELECT m.name,m.last_updated,m.uploaded,COUNT(*)
+            FROM fm_mib m JOIN fm_mibdata d ON (d.mib_id=m.id)
+            GROUP BY 1,2,3
+            ORDER BY 1 DESC""")
