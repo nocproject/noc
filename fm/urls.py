529a2a71383ee800f0e0f383d4ef5bfc1fd3e556
Dmitry Volodin <dv@noc.effortel.ru>
2009-01-27 15:00:46 +0300

    Event re-classification and automatical rule creation

--- fm/urls.py
+++ fm/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls.defaults import *
 from django.contrib.auth.decorators import login_required
-from noc.fm.views import index,event
+from noc.fm.views import index,event,reclassify,create_rule
 
 urlpatterns = patterns ( "",
+    (r"^(?P<event_id>\d+)/reclassify/$", login_required(reclassify)),
+    (r"^(?P<event_id>\d+)/create_rule/$", login_required(create_rule)),
     (r"^(?P<event_id>\d+)/$", login_required(event)),
     (r"^$",                   login_required(index)),
 )
--- fm/views.py
+++ fm/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404
 from noc.lib.render import render
-from noc.fm.models import Event,EventData
+from noc.fm.models import Event,EventData,EventClassificationRule,EventClassificationRE
+from django.http import HttpResponseRedirect,HttpResponseForbidden
+import random
 
 def index(request):
     events=Event.objects.order_by("-id")
@@ -9,3 +11,20 @@ def index(request):
 def event(request,event_id):
     event=get_object_or_404(Event,id=int(event_id))
     return render(request,"fm/event.html",{"e":event})
+
+def reclassify(request,event_id):
+    event=get_object_or_404(Event,id=int(event_id))
+    event.subject=None
+    event.save()
+    return HttpResponseRedirect("/fm/%d/"%event.id)
+
+def create_rule(request,event_id):
+    def re_q(s):
+        return s.replace("\\","\\\\").replace(".","\\.").replace("+","\\+").replace("*","\\*")
+    event=get_object_or_404(Event,id=int(event_id))
+    rule=EventClassificationRule(event_class=event.event_class,name="Rule #%d:%d"%(event.id,random.randint(0,100000)),preference=1000)
+    rule.save()
+    for d in event.eventdata_set.filter(is_enriched=False):
+        r=EventClassificationRE(rule=rule,left_re=re_q(d.key),right_re=re_q(d.value))
+        r.save()
+    return HttpResponseRedirect("/admin/fm/eventclassificationrule/%d/"%rule.id)
