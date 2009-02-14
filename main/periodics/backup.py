0da9381f10d4a1d3aa9b18e346f1d5a44efada10
Dmitry Volodin <dvolodin7@google.com>
2009-02-14 21:59:59 +0300

    Use tar and gzip paths from etc/noc.conf. Pipe tar and gzip together to reduce disk space usage

--- main/periodics/backup.py
+++ main/periodics/backup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
-## Backup database to main.backupdir
+## Backup database and repo to main.backupdir
 ##----------------------------------------------------------------------
 ## Copyright (C) 2007-2009 The NOC Project
 ## See LICENSE for details
@@ -24,11 +24,12 @@ class Task(noc.sa.periodic.Task):
             except:
                 pass
         from django.conf import settings
-        
+        # Build backup path
         now=datetime.datetime.now()
-        cmd=[config.get("path","pg_dump"),"-Fc"]
         out="noc-%04d-%02d-%02d-%02d-%02d.dump"%(now.year,now.month,now.day,now.hour,now.minute)
         out=os.path.join(config.get("path","backup_dir"),out)
+        # Build pg_dump command and options
+        cmd=[config.get("path","pg_dump"),"-Fc"]
         cmd+=["-f",out]
         if settings.DATABASE_USER:
             cmd+=["-U",settings.DATABASE_USER]
@@ -39,31 +40,26 @@ class Task(noc.sa.periodic.Task):
         if settings.DATABASE_PORT:
             cmd+=["-p",str(settings.DATABASE_PORT)]
         cmd+=[settings.DATABASE_NAME]
-
+        # Launch pg_dump
         logging.info("main.backup: dumping database into %s"%out)
         retcode=subprocess.call(cmd)
         if retcode!=0:
             logging.error("main.backup: dump failed. Removing broken dump '%s'"%out)
             safe_unlink(out)
             return False
+        #
+        # Back up repo
+        #
         repo_root=config.get("cm","repo")
-        repo_out="noc-%04d-%02d-%02d-%02d-%02d.tar"%(now.year,now.month,now.day,now.hour,now.minute)
+        repo_out="noc-%04d-%02d-%02d-%02d-%02d.tar.gz"%(now.year,now.month,now.day,now.hour,now.minute)
         repo_out=os.path.join(config.get("path","backup_dir"),repo_out)
-        cmd=["/usr/bin/tar","cf",repo_out]+[f for f in os.listdir(repo_root) if not f.startswith(".")]
-        # Dumping repo
         logging.info("main.backup: dumping repo into %s"%repo_out)
-        retcode=subprocess.call(cmd,cwd=repo_root)
-        if retcode!=0:
-            logging.error("main.backup: repo dump failed. Removing broken dumps")
-            safe_unlink(out)
-            safe_unlink(repo_out)
-            return False
-        cmd=["/usr/bin/gzip",repo_out]
-        logging.info("main.backup: gzipping repo dump")
-        retcode=subprocess.call(cmd)
-        if retcode!=0:
-            logging.error("main.backup: repo dump gzip failed. Removing broken dumps")
-            safe_unlink(out)
-            safe_unlink(repo_out)
-            return False
+        tar_cmd=[config.get("path","tar"),"cf","-"]+[f for f in os.listdir(repo_root) if not f.startswith(".")]
+        gzip_cmd=[config.get("path","gzip")]
+        f=open(repo_out,"w")
+        p1=subprocess.Popen(tar_cmd,cwd=repo_root,stdout=subprocess.PIPE)
+        p2=subprocess.Popen(gzip_cmd,stdin=p1.stdout,stdout=f)
+        f.close()
         return True
+        #logging.error("main.backup: repo dump failed. Removing broken dumps")
+        #safe_unlink(repo_out)
