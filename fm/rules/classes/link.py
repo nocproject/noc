791cd6385049f73c23f271d87f8c85b5b5b676a3
Dmitry Volodin <dvolodin7@google.com>
2009-02-16 02:36:59 +0300

    LinkUp and LinkDown

---
+++ fm/rules/classes/link.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Small DSL for event class description
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from noc.fm.rules.classes import EventClass,Var
+##
+## Link UP
+##
+class LinkUp(EventClass):
+    name="Link Up"
+    category="NETWORK"
+    priority="MAJOR"
+    subject_template="Link Up: {{interface}}"
+    body_template="""Link Up: {{interface}}"""
+    class Vars:
+        interface=Var(required=True)
+##
+## Link Down
+##
+class LinkDown(EventClass):
+    name="Link Down"
+    category="NETWORK"
+    priority="MAJOR"
+    subject_template="Link Down: {{interface}}"
+    body_template="""Link Down: {{interface}}"""
+    class Vars:
+        interface=Var(required=True)
+
