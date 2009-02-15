a5ff69cccb571e57972cc23cfe8880f24a0a3ac4
Dmitry Volodin <dvolodin7@google.com>
2009-02-16 00:52:46 +0300

    fm.unclassified_trap_oids report

---
+++ fm/reports/unclassified_trap_oids.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from noc.main.report import Column,BooleanColumn
+import noc.main.report
+
+class Report(noc.main.report.Report):
+    name="fm.unclassified_trap_oids"
+    title="Unclassified Trap OIDs"
+    requires_cursor=True
+    columns=[
+        Column("OID"),
+        Column("Count")]
+    
+    def get_queryset(self):
+        return self.execute("""
+            SELECT ed.value,COUNT(*)
+            FROM fm_eventdata ed JOIN fm_event e ON (e.id=ed.event_id)
+            WHERE ed.key='1.3.6.1.6.3.1.1.4.1.0'
+                AND e.event_class_id IN (
+                    SELECT id FROM fm_eventclass
+                    WHERE name IN ('DEFAULT','SNMP Trap')
+                    )
+            GROUP BY 1
+            ORDER BY 2 DESC""")
+
