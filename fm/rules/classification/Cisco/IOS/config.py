23670a7d8a38d8d31575028a1847a49331c427cf
Dmitry Volodin <dvolodin7@google.com>
2009-02-17 10:29:55 +0300

    Drop all configuration events except for running-config alterations

--- fm/rules/classification/Cisco/IOS/config.py
+++ fm/rules/classification/Cisco/IOS/config.py
@@ -9,6 +9,8 @@
 """
 from noc.fm.rules.classification import ClassificationRule
 from noc.fm.rules.classes.config import *
+from noc.fm.rules.classes.default import DROP
+
 ##
 ## Cisco.IOS Config Changed SNMP
 ##
@@ -19,7 +21,23 @@ class Cisco_IOS_Config_Changed_SNMP_Rule(ClassificationRule):
     required_mibs=["CISCO-CONFIG-MAN-MIB"]
     patterns=[
         (r"^profile$",r"^Cisco\.IOS$"),
-        (r"^1\.3\.6\.1\.6\.3\.1\.1\.4\.1\.0$",r"^1\.3\.6\.1\.4\.1\.9\.9\.43\.2\.0\.1$"),
+        (r"^1\.3\.6\.1\.6\.3\.1\.1\.4\.1\.0$",r"^1\.3\.6\.1\.4\.1\.9\.9\.43\.2\.0\.1$"), # snmpTrapOID.0 == ciscoConfigManEvent
+        (r"^1.3.6.1.4.1.9.9.43.1.1.6.1.5.\d+",r"3"), # ccmHistoryEventConfigDestination == running
+        (r"^source$",r"^SNMP Trap$"),
+    ]
+##
+## Cisco.IOS Config Event
+## Drop all events except saving to running-config
+##
+class Cisco_IOS_Config_Event_Drop_SNMP_Rule(ClassificationRule):
+    name="Cisco.IOS Config Event Drop SNMP"
+    event_class=DROP
+    preference=90000
+    drop_event=True
+    required_mibs=["CISCO-CONFIG-MAN-MIB"]
+    patterns=[
+        (r"^profile$",r"^Cisco\.IOS$"),
+        (r"^1\.3\.6\.1\.6\.3\.1\.1\.4\.1\.0$",r"^1\.3\.6\.1\.4\.1\.9\.9\.43\.2\.0\.1$"), # snmpTrapOID.0 == ciscoConfigManEvent
         (r"^source$",r"^SNMP Trap$"),
     ]
 ##
