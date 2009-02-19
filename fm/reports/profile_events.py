71c5c65e0582c503147a1c45763c6828caec4475
Dmitry Volodin <dvolodin7@google.com>
2009-02-19 09:52:12 +0300

    fm.profile_events report

---
+++ fm/reports/profile_events.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from noc.main.report import Column,BooleanColumn
+import noc.main.report
+
+class Report(noc.main.report.Report):
+    name="fm.profile_events"
+    title="Profile Summary"
+    requires_cursor=True
+    columns=[
+        Column("Profile"),
+        Column("Events",align="RIGHT")]
+    
+    def get_queryset(self):
+        return self.execute("""
+            SELECT mo.profile_name,COUNT(*)
+            FROM fm_event e JOIN sa_managedobject mo ON (e.managed_object_id=mo.id)
+            GROUP BY 1
+            ORDER BY 2 DESC""")
+
