5950ee6044751721b185c489ac83b2e7deba3523
Dmitry Volodin <dvolodin7@google.com>
2009-02-19 09:55:28 +0300

    fm.managed_object_events report

---
+++ fm/reports/managed_object_events.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from noc.main.report import Column,BooleanColumn
+import noc.main.report
+
+class Report(noc.main.report.Report):
+    name="fm.managed_object_events"
+    title="Managed Object Summary"
+    requires_cursor=True
+    columns=[
+        Column("Managed Object"),
+        Column("Events",align="RIGHT")]
+    
+    def get_queryset(self):
+        return self.execute("""
+            SELECT mo.name,COUNT(*)
+            FROM fm_event e JOIN sa_managedobject mo ON (e.managed_object_id=mo.id)
+            GROUP BY 1
+            ORDER BY 2 DESC""")
+
