642ee6a8a4d55acb79dc67fcf8fbe85dcfbfb455
Dmitry Volodin <dvolodin7@google.com>
2009-02-21 03:05:34 +0300

    AutoCompleteTextInput and lookup handler

---
+++ lib/widgets.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Form widgets
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from django import forms
+from django.http import HttpResponse
+import itertools
+
+##
+## Autocomplete widget
+##
+class AutoCompleteTextInput(forms.TextInput):
+    def __init__(self,lookup_url,*args,**kwargs):
+        super(AutoCompleteTextInput,self).__init__(*args,**kwargs)
+        self.lookup_url=lookup_url
+    def render(self,name,value=None,attrs=None):
+        return "%s<script type=\"text/javascript\">$(\"#%s\").autocomplete(\"%s\",{minChars:3,mustMatch:1});</script>"%(
+            super(AutoCompleteTextInput,self).render(name,value,attrs),attrs["id"],self.lookup_url
+        )
+##
+## Autocomplete lookup function:
+## 
+def lookup(request,func):
+    result=[]
+    if request.GET and "q" in request.GET:
+        q=request.GET["q"]
+        if len(q)>2: # Ignore requests shorter than 3 letters
+            result=list(itertools.islice(func(q),15))
+    return HttpResponse("\n".join(result), mimetype='text/plain')
