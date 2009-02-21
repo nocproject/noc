b4c454c3d61f7e2d82290e97dce56277ba9a6342
Dmitry Volodin <dvolodin7@google.com>
2009-02-21 13:49:55 +0300

    "BGP Established", "BGP FSM State Change" and "BGP Backward Transition" event classes

--- fm/rules/classes/bgp.py
+++ fm/rules/classes/bgp.py
@@ -9,6 +9,42 @@
 """
 from noc.fm.rules.classes import EventClass,Var
 ##
+##
+##
+class BGPEstablished(EventClass):
+    name = "BGP Established"
+    category = "NETWORK"
+    priority = "NORMAL"
+    class Vars:
+        neighbor_ip=Var(required=True)
+##
+## BGP Backward Transition
+##
+class BGPFSMStateChange(EventClass):
+    name = "BGP FSM State Change"
+    category = "NETWORK"
+    priority = "MAJOR"
+    subject_template="BGP Peer {{neighbor_ip}} state changed from {{from_state}} to {{to_state}}"
+    body_template="""BGP Peer {{neighbor_ip}} changed state from {{from_state}} to {{to_state}}
+    """
+    class Vars:
+        neighbor_ip=Var(required=True)
+        from_state=Var(required=True)
+        to_state=Var(required=True)
+##
+## BGP Backward Transition
+##
+class BGPBackwardTransition(EventClass):
+    name = "BGP Backward Transition"
+    category = "NETWORK"
+    priority = "MAJOR"
+    subject_template="BGP Peer backward transition: {{neighbor_ip}}"
+    body_template="""BGP Peer {{neighbor_ip}} performs backward transition to state {{state}}
+    """
+    class Vars:
+        neighbor_ip=Var(required=True)
+        state=Var(required=True)
+##
 ## BGP Max Prefixes Warning
 ##
 class BGPMaxPrefixesWarning(EventClass):
