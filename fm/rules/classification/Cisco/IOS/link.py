8155d812bf9f91005a928f7f20fba68dcadbc6c4
Dmitry Volodin <dvolodin7@google.com>
2009-02-21 16:27:04 +0300

    Cisco-like style of linkUp/linkDown traps rules are moved to separate module

--- fm/rules/classification/Cisco/IOS/link.py
+++ fm/rules/classification/Cisco/IOS/link.py
@@ -11,6 +11,19 @@ from noc.fm.rules.classification import ClassificationRule
 from noc.fm.rules.classes.link import *
 from noc.fm.rules.classes.default import DROP
 ##
+## Cisco.IOS Link Up SNMP
+##
+class Cisco_IOS_Link_Up_SNMP(ClassificationRule):
+    name="Cisco.IOS Link Up SNMP"
+    event_class=LinkUp
+    preference=2000
+    required_mibs=["IF-MIB"]
+    patterns=[
+        (r"^source$",r"^SNMP Trap$"),
+        (r"^1\.3\.6\.1\.6\.3\.1\.1\.4\.1\.0$",r"^1\.3\.6\.1\.6\.3\.1\.1\.5\.4$"), # linkUp
+        (r"^1\.3\.6\.1\.2\.1\.2\.2\.1\.2\.\d+$",r"(?P<interface>.*)"),            # ifDescr
+    ]
+##
 ## Cisco.IOS Link Up Syslog SNMP
 ##
 class Cisco_IOS_Link_Up_Syslog_SNMP_Rule(ClassificationRule):
@@ -37,6 +50,19 @@ class Cisco_IOS_Link_Up_SYSLOG_Rule(ClassificationRule):
         (r"^profile$",r"^Cisco\.IOS$"),
         (r"^source$",r"^syslog$"),
         (r"^message$",r"%LINK-3-UPDOWN: Interface (?P<interface>.+), changed state to up$"),
+    ]
+##
+## Cisco.IOS Link Down SNMP
+##
+class Cisco_IOS_Link_Down_SNMP_Rule(ClassificationRule):
+    name="Cisco.IOS Link Down SNMP"
+    event_class=LinkDown
+    preference=2000
+    required_mibs=["IF-MIB"]
+    patterns=[
+        (r"^source$",r"^SNMP Trap$"),
+        (r"^1\.3\.6\.1\.6\.3\.1\.1\.4\.1\.0$",r"^1\.3\.6\.1\.6\.3\.1\.1\.5\.3$"), # linkDown
+        (r"^1\.3\.6\.1\.2\.1\.2\.2\.1\.2\.\d+$",r"(?P<interface>.*)"),            # ifDescr
     ]
 ##
 ## Cisco.IOS Link Down Syslog SNMP
