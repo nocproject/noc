a4b1330154e5ca9c42e1ab5c2f322ddcd9a5cc7f
Dmitry Volodin <dvolodin7@google.com>
2009-02-21 16:25:35 +0300

    Rules renamed to common format. Look for interface name in ifName rather than in ifDescr

--- fm/rules/classification/link.py
+++ fm/rules/classification/link.py
@@ -13,26 +13,26 @@ from noc.fm.rules.classes.link import *
 ##
 ## SNMP Link Down
 ##
-class SNMP_Link_Down_Rule(ClassificationRule):
-    name="SNMP Link Down"
+class Link_Down_SNMP_Rule(ClassificationRule):
+    name="Link Down SNMP"
     event_class=LinkDown
     preference=1000
     required_mibs=["IF-MIB"]
     patterns=[
         (r"^source$",r"^SNMP Trap$"),
-        (r"^1\.3\.6\.1\.6\.3\.1\.1\.4\.1\.0$",r"^1\.3\.6\.1\.6\.3\.1\.1\.5\.3$"),
-        (r"^1\.3\.6\.1\.2\.1\.2\.2\.1\.2\.\d+$",r"(?P<interface>.*)"),
+        (r"^1\.3\.6\.1\.6\.3\.1\.1\.4\.1\.0$",r"^1\.3\.6\.1\.6\.3\.1\.1\.5\.3$"), # linkDown
+        (r"^1\.3\.6\.1\.2\.1\.31\.1\.1\.1\.1\.\d+$",r"(?P<interface>.*)"),        # ifName
     ]
 ##
 ## SNMP Link Up
 ##
-class SNMP_Link_Up_Rule(ClassificationRule):
-    name="SNMP Link Up"
+class Link_Up_SNMP_Rule(ClassificationRule):
+    name="Link Up SNMP"
     event_class=LinkUp
     preference=1000
     required_mibs=["IF-MIB"]
     patterns=[
         (r"^source$",r"^SNMP Trap$"),
-        (r"^1\.3\.6\.1\.6\.3\.1\.1\.4\.1\.0$",r"^1\.3\.6\.1\.6\.3\.1\.1\.5\.4$"),
-        (r"^1\.3\.6\.1\.2\.1\.2\.2\.1\.2\.\d+$",r"(?P<interface>.*)"),
+        (r"^1\.3\.6\.1\.6\.3\.1\.1\.4\.1\.0$",r"^1\.3\.6\.1\.6\.3\.1\.1\.5\.4$"), # linkUp
+        (r"^1\.3\.6\.1\.2\.1\.31\.1\.1\.1\.1\.\d+$",r"(?P<interface>.*)"),        # ifName
     ]
