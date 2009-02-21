83186f77b7deefd2760f7560745073a6fb9ddb38
Dmitry Volodin <dvolodin7@google.com>
2009-02-21 23:20:02 +0300

    "Cisco.IOS Phone Call SNMP" event classification rule

---
+++ fm/rules/classification/Cisco/IOS/phone.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Power-over-Ethernet classification rules
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from noc.fm.rules.classification import ClassificationRule
+from noc.fm.rules.classes.phone import *
+##
+## Cisco.IOS Phone Call SNMP
+##
+class Cisco_IOS_Phone_Call_SNMP_Rule(ClassificationRule):
+    name="Cisco.IOS Phone Call SNMP"
+    event_class=PhoneCall
+    preference=1000
+    patterns=[
+        (r"^source$",r"^SNMP Trap$"),
+        (r"^profile$",r"^Cisco\.IOS$"),
+        (r"^1\.3\.6\.1\.6\.3\.1\.1\.4\.1\.0$",r"^1\.3\.6\.1\.2\.1\.10\.21\.2$"),
+        (r"^1\.3\.6\.1\.4\.1\.9\.10\.25\.1\.4\.3\.1\.5\.\d+$",r"^(?P<called_number>.+)$"),
+        (r"^1\.3\.6\.1\.4\.1\.9\.10\.25\.1\.4\.3\.1\.10\.\d+$",r"^(?P<connect_time>\d+)$"),
+        (r"^1\.3\.6\.1\.4\.1\.9\.10\.25\.1\.4\.3\.1\.8\.\d+$",r"(?P<call_clearing>\S+)"),
+        (r"^1\.3\.6\.1\.4\.1\.9\.10\.25\.1\.4\.3\.1\.3\.\d+$",r"^(?P<calling_number>\S+)$"),
+        (r"^1\.3\.6\.1\.4\.1\.9\.10\.25\.1\.4\.3\.1\.11\.\d+$",r"^(?P<disconnect_time>\d+)$"),
+    ]
