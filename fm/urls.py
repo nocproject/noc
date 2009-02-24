5c314e6da0898370ec40537f60f9a141bfed8cff
Dmitry Volodin <dvolodin7@google.com>
2009-02-24 10:07:50 +0300

    "Refresh rules" button in Event Classification Rule admin list

--- fm/urls.py
+++ fm/urls.py
@@ -10,7 +10,7 @@
 from django.conf.urls.defaults import *
 from django.contrib.auth.decorators import login_required
 from noc.fm.views import index,event,reclassify,create_rule,event_list_css,view_rules,\
-    py_event_class,py_event_classification_rule,upload_mib,lookup_managed_object,lookup_events
+    py_event_class,py_event_classification_rule,upload_mib,lookup_managed_object,lookup_events,reload_classifier_config
 
 urlpatterns = patterns ( "",
     (r"^(?P<event_id>\d+)/reclassify/$", login_required(reclassify)),
@@ -24,4 +24,5 @@ urlpatterns = patterns ( "",
     (r"^$",                   login_required(index)),
     (r"^lookup/managed_object/", lookup_managed_object),
     (r"^lookup/events/",      lookup_events),
+    (r"^reload_classifier_config", login_required(reload_classifier_config))
 )
