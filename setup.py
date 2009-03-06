2d7ba87f952b801d79868b1bad14c14b7bffc309
Dmitry Volodin <dvolodin7@google.com>
2009-03-06 18:26:21 +0300

    distutils setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,98 @@
 #!/usr/bin/env python
-##
-##
-##
+##----------------------------------------------------------------------
+## Distutils setup.py
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+from __future__ import with_statement
+from distutils.core import setup
+import distutils.command.sdist
+from distutils.command.install import INSTALL_SCHEMES
+import subprocess,os,sys
 
 ##
-## Use setuptools instead of docutils
+## Prefix to where noc to be installed
 ##
-import ez_setup
-ez_setup.use_setuptools()
-from setuptools import setup, find_packages
+PREFIX="/opt/noc"
 
-setup(name='noc',
-      version='0.1',
-      description='Network Operation Center\'s OSS',
-      author='Dmitry Volodin',
-      author_email='dv@noc.effortel.ru',
-      url='<nothing yet>',
-      zip_safe=False,
-      packages=find_packages(),
-      install_requires=[
-        "psycopg2",
-        "Django >= 1.0",
+##
+## Read actual noc version from VERSION file
+##
+def get_version():
+    with open("VERSION") as f:
+        return f.read().strip()
+##
+## Build distribution manifest
+##
+MANIFEST=[]
+def get_manifest():
+    global MANIFEST
+    if not MANIFEST:
+        if not os.path.exists("MANIFEST"):
+            proc=subprocess.Popen(["hg","locate"],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+            stdout,stderr=proc.communicate()
+            with open("MANIFEST","w") as f:
+                f.write(stdout+"MANIFEST\n")
+        with open("MANIFEST") as f:
+            MANIFEST=[n for n in f.read().splitlines() if not n.startswith(".hg")]
+    return MANIFEST
+##
+## Return list of packages
+##
+def get_packages():
+    return [""]+[f[:-12].replace(os.sep,".") for f in get_manifest() if f.endswith("__init__.py") and f!="__init__.py"]
+##
+## Return data files
+##
+def get_data():
+    data={}
+    for df in get_manifest():
+        if df.endswith(".py"):
+            continue
+        d,f=os.path.split(df)
+        if d not in data:
+            data[d]=[df]
+        else:
+            data[d].append(df)
+    return data.items()
+##
+## Get file list for sdist from MANIFEST
+##
+class noc_sdist(distutils.command.sdist.sdist):
+    def get_file_list(self):
+        self.filelist.files=get_manifest()
+##
+## Monkeypatch distutils to install noc to the desired location
+##
+for scheme in INSTALL_SCHEMES.values():
+    scheme['purelib']=PREFIX
+    scheme['data']=PREFIX
+##
+## Pass control to the setuptools
+##
+setup(name="noc",
+      version      = get_version(),
+      description  = "Network Operation Center's OSS",
+      author       = "Dmitry Volodin",
+      author_email = "dvolodin7@google.com",
+      url          = "http://nocproject.org/",
+      license      = "BSD",
+      long_description = """NOC Project is an Operation Support System (OSS) for telecom companies,
+service providers, and enterprise Network Operation Centers (NOC).
+Areas covered by NOC include fault management, service activation/provisioning, multi-VRF address space management,
+configuration management, DNS provisioning, peering management, RPSL and BGP filter generation, and reporting.""",
+      cmdclass     = {"sdist":noc_sdist},
+      packages     = get_packages(),
+      data_files   = get_data(),
+      provides=["noc"],
+      requires=[
+        "psycopg2 (>= 2.0.5)",
+        "Django (>= 1.0.2)",
         #"south"
-        "flup >= 1.0",
-        "Sphinx >= 0.4",
-      ],
-      dependency_links = [
-        ""
+        "flup (>= 1.0)",
+        "pysnmp (>= 4.1.7)",
+        "pyasn1 (>= 0.0.6)",
+        "pyke (>= 0.7)",
       ],
      )
