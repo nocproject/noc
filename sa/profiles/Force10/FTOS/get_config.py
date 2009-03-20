8b7422afdc52854af0dad89ffe17ef0419cee9e0
Dmitry Volodin <dvolodin7@google.com>
2009-03-20 18:48:15 +0300

    Force10.FTOS profile and Force10.FTOS.get_config script

---
+++ sa/profiles/Force10/FTOS/get_config.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Force10.FTOS.get_config
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+import noc.sa.script
+from noc.sa.interfaces import IGetConfig
+
+class Script(noc.sa.script.Script):
+    name="Force10.FTOS.get_config"
+    implements=[IGetConfig]
+    def execute(self):
+        self.cli("terminal length 0")
+        config=self.cli("show running-config")
+        config=self.strip_first_lines(config,1)
+        return self.cleaned_config(config)
