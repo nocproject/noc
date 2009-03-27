c85167d877ccbf16c7b8fbc985854149de5b0410
Dmitry Volodin <dvolodin7@google.com>
2009-03-27 10:00:50 +0300

    Reimplemented using parse_table

--- sa/profiles/Force10/FTOS/get_vlans.py
+++ sa/profiles/Force10/FTOS/get_vlans.py
@@ -9,33 +9,12 @@
 """
 import noc.sa.script
 from noc.sa.interfaces import IGetVlans
+from noc.lib.text import parse_table
 import re
-
-rx_header_line=re.compile(r"^([\-]+)(\s+)([\-]+)\s.*$")
 
 class Script(noc.sa.script.Script):
     name="Force10.FTOS.get_vlans"
     implements=[IGetVlans]
     def execute(self):
         self.cli("terminal length 0")
-        vlans=self.cli("show vlan brief")
-        r=[]
-        fl=0
-        sl=0
-        ssl=0
-        for l in vlans.split("\n"):
-            l=l.strip()
-            if not l:
-                continue
-            if not fl:
-                if l.startswith("-"):
-                    match=rx_header_line.match(l)
-                    if match:
-                        fl=len(match.group(1))
-                        ssl=len(match.group(2))
-                        sl=len(match.group(3))
-                        continue
-            else:
-                r.append({"vlan_id": int(l[:fl].strip()),
-                          "name"   : l[fl+ssl:fl+ssl+sl].strip()})
-        return r
+        return [{"vlan_id":x[0],"name":x[1]} for x in parse_table(self.cli("show vlan brief"))]
