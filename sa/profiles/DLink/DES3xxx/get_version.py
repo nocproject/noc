64ec44ec9dcfc240060136c99c9d6f7ce4d8c1dc
Dmitry Volodin <dvolodin7@google.com>
2009-04-01 15:14:27 +0400

    Script name fixed

--- sa/profiles/DLink/DES3xxx/get_version.py
+++ sa/profiles/DLink/DES3xxx/get_version.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
-## DLink.DGS3xxx.get_version
+## DLink.DES3xxx.get_version
 ##----------------------------------------------------------------------
 ## Copyright (C) 2007-2009 The NOC Project
 ## See LICENSE for details
@@ -14,7 +14,7 @@ import re
 rx_ver=re.compile(r"Device Type\s+:\s+(?P<platform>\S+).+Firmware Version\s+:\s+(?P<version>\S+)",re.MULTILINE|re.DOTALL)
 
 class Script(noc.sa.script.Script):
-    name="DLink.DGS3xxx.get_version"
+    name="DLink.DES3xxx.get_version"
     implements=[IGetConfig]
     def execute(self):
         self.cli("disable clipaging")
