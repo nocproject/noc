428e34a59f26818fb97d2dca04329e3cdf624d69
Dmitry Volodin <dvolodin7@google.com>
2009-04-01 14:07:06 +0400

    DES3028/DES3010G compatibility

--- sa/profiles/DLink/DES35xx/__init__.py
+++ sa/profiles/DLink/DES35xx/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
 ## Vendor: D-Link
-## OS:     DES-35xx
+## OS:     DES-3xxx
 ## Compatible:
 ##----------------------------------------------------------------------
 ## Copyright (C) 2007-2009 The NOC Project
@@ -15,7 +15,9 @@ from noc.sa.protocols.sae_pb2 import TELNET,SSH
 class Profile(noc.sa.profiles.Profile):
     name="DLink.DES35xx"
     supported_schemes=[TELNET,SSH]
-    pattern_more="^CTRL\+C.+?a All"
+    pattern_username="([Uu]ser[Nn]ame|[Ll]ogin):"
+    pattern_password="[Pp]ass[Ww]ord:"
+    pattern_more="CTRL\+C.+?a All"
     pattern_prompt=r"^\S+?#"
     command_more="a"
     config_volatile=["^%.*?$"]
