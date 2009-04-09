54956b11872391b05b2b6bb1f0edfe90cf4c4c2f
Dmitry Volodin <dvolodin7@google.com>
2009-04-10 01:04:43 +0400

    Language model

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,23 @@
 # -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
+## Administrative interface for Main application
+##----------------------------------------------------------------------
 ## Copyright (C) 2007-2009 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 """
 """
 from django.contrib import admin
+from noc.main.models import *
+
+##
+## Admin for Language
+##
+class LanguageAdmin(admin.ModelAdmin):
+    list_display=["name","native_name","is_active"]
+    search_fields=["name","native_name"]
+    list_filter=["is_active"]
+##
+## Register administrative interfaces
+##
+admin.site.register(Language, LanguageAdmin)
--- main/models.py
+++ main/models.py
@@ -12,6 +12,20 @@ from noc.main.menu import Menu
 import noc.main.search # Set up signal handlers
 
 ##
+## Languages
+##
+class Language(models.Model):
+    class Meta:
+        verbose_name="Language"
+        verbose_name_plural="Languages"
+        ordering=("name",)
+    name=models.CharField("Name",max_length=32,unique=True)
+    native_name=models.CharField("Native Name",max_length=32)
+    is_active=models.BooleanField("Is Active",default=False)
+    def __unicode__(self):
+        return self.name
+    
+##
 ## Application Menu
 ##
 class AppMenu(Menu):
@@ -20,7 +34,8 @@ class AppMenu(Menu):
     items=[
         ("Setup", [
             ("Users",  "/admin/auth/user/",  "auth.change_user"),
-            ("Groups", "/admin/auth/group/", "auth.change_group")
+            ("Groups", "/admin/auth/group/", "auth.change_group"),
+            ("Languages","/admin/main/language/", "main.change_language"),
         ]),
         ("Documentation", [
             ("Administrator's Guide", "/static/doc/en/ag/html/index.html"),
