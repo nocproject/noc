55b83c7379f6fe1c46c1c07d8cdc31270d68ae9e
Dmitry Volodin <dvolodin7@google.com>
2009-04-20 12:50:45 +0400

    [main]/date_format, time_format, datetime_format settings

--- settings.py
+++ settings.py
@@ -31,6 +31,10 @@ DATABASE_PORT     = config.get("database","port")
 
 TIME_ZONE = config.get("main","timezone")
 LANGUAGE_CODE = config.get("main","language_code")
+# Set up date and time formats
+DATE_FORMAT     = config.get("main","date_format")
+TIME_FORMAT     = config.get("main","time_format")
+DATETIME_FORMAT = config.get("main","datetime_format")
 
 SITE_ID = 1
 
