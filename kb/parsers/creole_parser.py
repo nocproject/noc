457dbec0ef2dd92c2fd4bf6309634f5eabbd2e39
Dmitry Volodin <dvolodin7@google.com>
2009-04-21 13:01:34 +0400

    Inline images support

--- kb/parsers/creole_parser.py
+++ kb/parsers/creole_parser.py
@@ -21,8 +21,13 @@ class Parser(noc.kb.parsers.Parser):
                 return link
             else:
                 return cls.convert_link(kb_entry,link[link.index(">")+1:-4])
+        def custom_image_emit(node):
+            target=cls.convert_attach(kb_entry,node.content)
+            text=html_emitter.get_text(node)
+            return u'<img src="%s" alt="%s" />' % (html_emitter.attr_escape(target),html_emitter.attr_escape(text))
         import creole
         parser=creole.Parser(unicode(kb_entry.body))
         html_emitter=creole.HtmlEmitter(parser.parse())
         html_emitter.link_emit=custom_link_emit
+        html_emitter.image_emit=custom_image_emit
         return html_emitter.emit()
