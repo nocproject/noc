7a21fccedaa3d913c4015ced329fba3cf2ca525c
Dmitry Volodin <dvolodin7@google.com>
2009-04-27 12:31:31 +0400

    Decode macro arguments to hash

--- kb/parsers/macros/__init__.py
+++ kb/parsers/macros/__init__.py
@@ -6,7 +6,7 @@
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 from noc.lib.registry import Registry
-
+import types,re
 ##
 ## Macro Registry
 ##
@@ -26,11 +26,35 @@ class MacroBase(type):
         macro_registry.register(m.name,m)
         return m
 ##
+## Args regular expression
+##
+rx_args=re.compile(r"\s*(?P<attr>\S+)\s*=\s*(?P<quote>['\"])(?P<value>.*?)(?P=quote)")
+##
 ## Macro Base
 ##
 class Macro(object):
     __metaclass__=MacroBase
     name=None
+    ##
+    ## Converts a string of html-like attributes to hash
+    ##
+    @classmethod
+    def parse_args(cls,args):
+        if type(args)==types.DictType:
+            return args
+        return dict([(m[0],m[2]) for m in rx_args.findall(args)])
+    ##
+    ## Decodes args and calls handle method
+    ##
     @classmethod
     def expand(cls,args,text):
-        return u"Not implemented macro"
+        return cls.handle(cls.parse_args(args),text)
+    ##
+    ## Specific macro handler to be overriden in child classes
+    ## Accepts a hash of args and text and returns formatted HTML
+    ## to be included in output
+    ##
+    @classmethod
+    def handle(cls,args,text):
+        raise NotImplementedError
+
