a51835f6fbaa3459e2364a2df546746b06d50ab8
Dmitry Volodin <dvolodin7@google.com>
2009-05-07 10:49:18 +0400

    InfiNet.WANFlexX profile

---
+++ sa/profiles/InfiNet/WANFlexX/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Vendor: InfiNet
+## OS:     WANFlexX
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+import noc.sa.profiles
+from noc.sa.protocols.sae_pb2 import TELNET
+
+class Profile(noc.sa.profiles.Profile):
+    name="InfiNet.WANFlexX"
+    supported_schemes=[TELNET]
+    pattern_more="^-- more --"
+    pattern_prompt=r"^\S+?#\d+>"
+    command_submit="\r"
+    command_more=" "
---
+++ share/doc/en/ug/profiles/InfiNet.WANFlexX.rst
@@ -0,0 +1,12 @@
+InfiNet.WANFlexX
+================
+
+====== =============================================
+Vendor `InfiNet <http://www.infinetwireless.com/>`_
+OS     WANFlexX
+====== =============================================
+
+Tested Equipment
+----------------
+.. supported:: InfiNet.WANFlexX
+
