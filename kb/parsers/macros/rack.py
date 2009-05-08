ff849e0a8734fb4896c0f5280026db9206dc4e0d
Dmitry Volodin <dvolodin7@google.com>
2009-05-08 12:44:53 +0400

    Racks in rackset rendered in single row now

--- kb/parsers/macros/rack.py
+++ kb/parsers/macros/rack.py
@@ -15,10 +15,97 @@ class RackSet(object):
         self.id=id
         self.racks=[]
     ##
+    ## Return a list of allocations for a rack
+    ## allocation is a tuple of: top position, height, is empty space, title
+    ##
+    def compile_allocations(self,rack):
+        allocations=sorted(rack.allocations,lambda x,y: -cmp(x.position,y.position))
+        sp=[]
+        if len(allocations)==0:
+            sp+=[(rack.height,rack.height,True,None)]
+        else:
+            a=allocations.pop(0)
+            empty_top=rack.height-a.position-a.height+1
+            if empty_top:
+                sp+=[(rack.height,empty_top,True,None)]
+            sp+=[(a.position+a.height-1,a.height,False,a.id)]
+            while allocations:
+                last_a=a
+                a=allocations.pop(0)
+                empty_top=last_a.position-a.height-a.position
+                if empty_top:
+                    sp+=[(last_a.position-1,empty_top,True,None)]
+                sp+=[(a.height+a.position-1,a.height,False,a.id)]
+            if a.position>1:
+                sp+=[(a.position-1,a.position-1,True,None)]
+        return sp
+    ##
     ## Render RackSet contents to HTML
     ##
     def render_html(self):
-        return "<div>%s</div>"%"\n".join([r.render_html() for r in self.racks])
+        if len(self.racks)==0: # Empty rackset
+            return u""
+        #return "<div>%s</div>"%"\n".join([r.render_html() for r in self.racks])
+        self.height=max([r.height for r in self.racks]) # RackSet height is a maximal height of the racks
+        # Fill RackSet matrix
+        # RSM is a hash: row -> column -> {"colspan","rowspan","style","text"}
+        rsm={}
+        for i in range(1,self.height+1):
+            rsm[i]={}
+            for j in range(len(self.racks)):
+                rsm[i][j*2]=None
+                rsm[i][j*2+1]={"class":"ruler","text":str(i)}
+        # Remove the tops of the racks if necessary
+        for j in range(len(self.racks)):
+            rh=self.racks[j].height
+            if rh<self.height:
+                for i in range(self.height,rh,-1):
+                    rsm[i][j*2]=None
+                    rsm[i][j*2+1]=None
+                rsm[self.height][j*2]={"colspan":2,"rowspan":self.height-rh,"class":"emptytop"}
+        # Place allocations
+        for j in range(len(self.racks)):
+            for a in self.compile_allocations(self.racks[j]):
+                position,height,is_empty,title=a
+                if is_empty:
+                    style="empty"
+                else:
+                    style="occupied"
+                if height==1:
+                    rsm[position][j*2]={"class":style,"text":title}
+                else:
+                    rsm[position][j*2]={"class":style,"text":title,"rowspan":height}
+                    for i in range(position-1,position-height,-1):
+                        rsm[i][j*2]=None
+        # Render the matrix
+        out=["<table class='rackset'>"]
+        if self.id:
+            out+=["<caption>%s</caption>"%self.id]
+        for i in range(self.height,0,-1):
+            out+=["<tr>"]
+            for j in range(len(self.racks)*2):
+                v=rsm[i][j]
+                if v:
+                    td="<td "
+                    for attr in ["colspan","rowspan","class"]:
+                        if attr in v:
+                            td+="%s='%s' "%(attr,v[attr])
+                    td+=">"
+                    if "text" in v and v["text"]:
+                        td+=v["text"]
+                    td+="</td>"
+                    out+=[td]
+            out+=["</tr>"]
+        # Render rack titles
+        out+=["<tr>"]
+        for r in self.racks:
+            if r.id:
+                out+=["<td colspan='2' class='racklabel'>%s</td>"%r.id]
+            else:
+                out+=["<td colspan='2' class='racklabel'></td>"]
+        out+=["</tr>"]
+        out+=["</table>"]
+        return u"\n".join(out)
 ##
 ## Rack Representation
 ##
