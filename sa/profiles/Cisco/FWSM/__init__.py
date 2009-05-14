19f6911fab527417faf61d829d642e9eda0f14a3
Dmitry Volodin <dvolodin7@google.com>
2009-05-14 17:58:48 +0400

    Cisco.FWSM profile

---
+++ sa/profiles/Cisco/FWSM/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Vendor: Cisco
+## OS:     ASA
+## Compatible: 7.0
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+import noc.sa.profiles
+from noc.sa.protocols.sae_pb2 import TELNET,SSH
+
+class Profile(noc.sa.profiles.Profile):
+    name="Cisco.FWSM"
+    supported_schemes=[TELNET,SSH]
+    pattern_more="^<--- More --->"
+    pattern_unpriveleged_prompt=r"^\S+?>"
+    command_super="enable"
+    pattern_prompt=r"^\S+?#"
+    command_more=" "
