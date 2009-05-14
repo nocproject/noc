d5f3fcd2945f8a5aa157542cbfa7d33d41dac467
Dmitry Volodin <dvolodin7@google.com>
2009-05-14 18:03:31 +0400

    Cisco.FWSM.get_version

---
+++ sa/profiles/Cisco/FWSM/get_version.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Cisco.FWSM.get_version
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+import noc.sa.script
+from noc.sa.interfaces import IGetVersion
+import re
+
+rx_ver=re.compile(r'FWSM Firewall Version (?P<version>\S+).*Hardware:\s+(?P<platform>\S+),')
+
+class Script(noc.sa.script.Script):
+    name="Cisco.FWSM.get_version"
+    implements=[IGetVersion]
+    def execute(self):
+        self.cli("terminal pager 0")
+        v=self.cli("show version")
+        match=rx_ver.search(v)
+        return {
+            "vendor"    : "Cisco",
+            "platform"  : match.group("platform"),
+            "version"   : match.group("version"),
+        }
