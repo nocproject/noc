939ca91be2a9a9f994349aea045f237cb3f35e77
Dmitry Volodin <dvolodin7@google.com>
2009-05-15 15:56:15 +0400

    "Force10.FTOS Link Up SNMP" and "Force10.FTOS Link Down SNMP"

--- fm/rules/classification/Force10/FTOS/link.py
+++ fm/rules/classification/Force10/FTOS/link.py
@@ -33,3 +33,29 @@ class Force10_FTOS_Link_Down_SYSLOG_Rule(ClassificationRule):
         (r"^message$",r"%IFMGR-5-OSTATE_DN: Changed interface state to down: (?P<interface>.+)$"),
         (r"^source$",r"^syslog$"),
     ]
+##
+## Force10.FTOS Link Up SNMP
+##
+class Force10_FTOS_Link_Up_SNMP_Rule(ClassificationRule):
+    name="Force10.FTOS Link Up SNMP"
+    event_class=LinkUp
+    preference=1000
+    patterns=[
+        (r"^profile$",r"^Force10\.FTOS$"),
+        (r"^1\.3\.6\.1\.6\.3\.1\.1\.4\.1\.0$",r"^1\.3\.6\.1\.6\.3\.1\.1\.5\.4$"),
+        (r"^1\.3\.6\.1\.4\.1\.6027\.3\.1\.1\.4\.1\.2$",r"^OSTATE_UP: Changed interface state to up: (?P<interface>.+)$"),
+        (r"^source$",r"^SNMP Trap$"),
+    ]
+##
+## Force10.FTOS Link Down SNMP
+##
+class Force10_FTOS_Link_Down_SNMP_Rule(ClassificationRule):
+    name="Force10.FTOS Link Down SNMP"
+    event_class=LinkDown
+    preference=1000
+    patterns=[
+        (r"^profile$",r"^Force10\.FTOS$"),
+        (r"^1\.3\.6\.1\.6\.3\.1\.1\.4\.1\.0$",r"^1\.3\.6\.1\.6\.3\.1\.1\.5\.3$"),
+        (r"^1\.3\.6\.1\.4\.1\.6027\.3\.1\.1\.4\.1\.2$",r"^OSTATE_DN: Changed interface state to down: (?P<interface>.+)$"),
+        (r"^source$",r"^SNMP Trap$"),
+    ]
