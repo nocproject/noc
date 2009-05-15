69d4bdb4dcfccf67bc59f515e2a8e85e7328eb81
Dmitry Volodin <dvolodin7@google.com>
2009-05-15 14:43:02 +0400

    ACL Permit class

--- fm/rules/classes/security.py
+++ fm/rules/classes/security.py
@@ -30,3 +30,26 @@ Direction: {{src_ip}}:{{src_port}} -> {{dst_ip}}:{{dst_port}}"""
         dst_ip=Var(required=True,repeat=True)
         dst_port=Var(required=True,repeat=True)
         proto=Var(required=False,repeat=False)
+##
+## ACL Permit
+##
+class ACLPermit(EventClass):
+    name     = "ACL Permit"
+    category = "SECURITY"
+    priority = "NORMAL"
+    subject_template="Packet permitted by ACL: {{src_ip}}:{{src_port}} -> {{dst_ip}}:{{dst_port}}"
+    body_template="""ACL Name: {{acl_name}}
+Action: PERMIT
+Packets count: {{count}}
+Direction: {{src_ip}}:{{src_port}} -> {{dst_ip}}:{{dst_port}}"""
+    repeat_suppression=True
+    repeat_suppression_interval=3600
+    trigger=None
+    class Vars:
+        acl_name=Var(required=False,repeat=False)
+        count=Var(required=False,repeat=False)
+        src_port=Var(required=True,repeat=False)
+        src_ip=Var(required=True,repeat=True)
+        dst_ip=Var(required=True,repeat=True)
+        dst_port=Var(required=True,repeat=True)
+        proto=Var(required=False,repeat=False)
