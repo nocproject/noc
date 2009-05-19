aea6ec1f63d3d8ca628062b8b262f326103b5056
Dmitry Volodin <dvolodin7@google.com>
2009-05-19 14:39:24 +0400

    Config syntax highlighting

---
+++ sa/profiles/ZTE/ZXDSL531/highlight.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## ZTE.ZXDSL highlight lexers
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+from pygments.lexers import XmlLexer
+from pygments.token import *
+##
+## ZTE ZXDSL531 Configuration lexer
+##
+class ConfigLexer(XmlLexer):
+    pass
