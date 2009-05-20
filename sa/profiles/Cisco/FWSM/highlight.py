75a3b92e83d1235203161af7c1a442f37eb4b17c
Dmitry Volodin <dvolodin7@google.com>
2009-05-20 12:14:12 +0400

    Improved highlighting

--- sa/profiles/Cisco/FWSM/highlight.py
+++ sa/profiles/Cisco/FWSM/highlight.py
@@ -5,7 +5,7 @@
 ## Copyright (C) 2007-2009 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-from pygments.lexer import RegexLexer
+from pygments.lexer import RegexLexer,bygroups
 from pygments.token import *
 ##
 ## Cisco FWSM configuration lexer
@@ -15,7 +15,12 @@ class ConfigLexer(RegexLexer):
     tokens={
         "root" : [
             (r"^!.*", Comment),
+            (r"(password)(\s+\S+\s+)(encrypted)", bygroups(Keyword,String.Double,Keyword)),
+            (r"^(interface|router\s+\S+)(.*?)$", bygroups(Keyword,Name.Attribute)),
+            (r"^(group-policy)(\s+\S+\s+)(\S+)", bygroups(Keyword,Name.Attribute,Keyword)),
+            (r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(/\d{1,2})?", Number), # IPv4 Address/Prefix
             (r"^(?:no\s+)?\S+", Keyword),
-            (r".*\n", Text),
+            (r"\d+", Number),
+            (r".", Text),
         ]
     }
