eedb75077ca6d23fce0340ca9f3a77db49906a78
Dmitry Volodin <dvolodin7@google.com>
2009-06-04 15:36:34 +0400

    "ISDN Q.931 Call Clearings" Reference Book

---
+++ main/refbooks/refbooks/q931.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Built-in Q.931 refbooks
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+from noc.main.refbooks.refbooks import RefBook,Field
+
+##
+## IEEE OUI Refbook
+##
+class IEEEOUI(RefBook):
+    name="Q.931 Call Clearings"
+    description="Q.931 ISDN Call Clearings"
+    downloader="CSV"
+    download_url="http://update.nocproject.org/db/q931_call_clearing.csv"
+    refresh_interval=90
+    fields=[
+        Field(name="DEC"),
+        Field(name="HEX"),
+        Field(name="Description",search_method="substring"),
+        ]
