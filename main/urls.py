67a2db70829c8d3029cc9906dfb1fa03eacaff08
Dmitry Volodin <dvolodin7@google.com>
2009-06-05 15:16:14 +0400

    Databrowse integration

--- main/urls.py
+++ main/urls.py
@@ -11,10 +11,13 @@ from django.conf.urls.defaults import *
 from django.contrib.auth.decorators import login_required
 from noc.main.views import index,logout,report,report_index,success,failure,menu,search,config_index,config_view,\
     refbook_index,refbook_view
+from django.contrib import databrowse
+from noc.lib.decorators import superuser_required
 
 urlpatterns = patterns ( None,
         (r"^$",        login_required(index)),
         (r"^logout/$", login_required(logout)),
+        (r"^databrowse/(.*)", superuser_required(databrowse.site.root)),
         (r"^report/(?P<report>[a-z0-9\-_.]+)/$", login_required(report)),
         (r"^report/$", login_required(report_index)),
         (r"success/$", login_required(success)),
