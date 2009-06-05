c08a1e2e0e3e58a94fa111a0be26b8c9ffd1b2f4
Dmitry Volodin <dvolodin7@google.com>
2009-06-05 13:04:40 +0400

    "E.164 Country Prefixes" Refbook

---
+++ main/refbooks/refbooks/e164.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## E.164 Country Codes
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+from noc.main.refbooks.refbooks import RefBook,Field
+
+##
+## IEEE OUI Refbook
+##
+class E164(RefBook):
+    name="E.164 Country Prefixes"
+    description="E.164 Country Prefixes"
+    downloader="CSV"
+    download_url="http://update.nocproject.org/db/e164.csv"
+    refresh_interval=90
+    fields=[
+        Field(name="Prefix"),
+        Field(name="Country",search_method="subscring"),
+        ]
