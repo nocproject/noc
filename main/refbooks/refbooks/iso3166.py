44b8d1fa1560fafc492456329c40ddad48c943bd
Dmitry Volodin <dvolodin7@google.com>
2009-06-06 10:45:28 +0400

    "ISO3166 Country Codes" Reference Book

---
+++ main/refbooks/refbooks/iso3166.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## ISO 3166 Country Codes
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+from noc.main.refbooks.refbooks import RefBook,Field
+
+##
+## IEEE OUI Refbook
+##
+class E164(RefBook):
+    name="ISO 3166 Country Codes"
+    description="ISO 3166 Country Codes"
+    downloader="CSV"
+    download_url="http://update.nocproject.org/db/iso3166_1.csv"
+    refresh_interval=90
+    fields=[
+        Field(name="Country",search_method="substring"),
+        Field(name="Code",search_method="substring"),
+        ]
