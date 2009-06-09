52f7dd9959af5eb4ca9c560c6daef4f834cf65b0
Dmitry Volodin <dvolodin7@google.com>
2009-06-09 15:50:55 +0400

    paginator tag

---
+++ main/templatetags/paginator.py
@@ -0,0 +1,31 @@
+# http://code.djangoproject.com/wiki/PaginatorTag
+from django import template
+
+register = template.Library()
+
+def paginator(context, adjacent_pages=2):
+    """
+    To be used in conjunction with the object_list generic view.
+
+    Adds pagination context variables for use in displaying first, adjacent and
+    last page links in addition to those created by the object_list generic
+    view.
+    """
+    page_numbers = [n for n in \
+                    range(context["page"] - adjacent_pages, context["page"] + adjacent_pages + 1) \
+                    if n > 0 and n <= context["pages"]]
+    return {
+        "hits": context["hits"],
+        "results_per_page": context["results_per_page"],
+        "page": context["page"],
+        "pages": context["pages"],
+        "page_numbers": page_numbers,
+        "next": context["next"],
+        "previous": context["previous"],
+        "has_next": context["has_next"],
+        "has_previous": context["has_previous"],
+        "show_first": 1 not in page_numbers,
+        "show_last": context["pages"] not in page_numbers,
+    }
+
+register.inclusion_tag("main/templatetags/paginator.html", takes_context=True)(paginator)
