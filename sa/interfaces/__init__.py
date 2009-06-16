4d6a2eee450497a5fe6661c0b83368a77bb80412
Dmitry Volodin <dvolodin7@google.com>
2009-06-16 15:45:08 +0400

    ICommands interface

--- sa/interfaces/__init__.py
+++ sa/interfaces/__init__.py
@@ -14,4 +14,4 @@ from noc.sa.interfaces.igetmacaddresstable import IGetMACAddressTable
 from noc.sa.interfaces.igetdot11associations import IGetDot11Associations
 from noc.sa.interfaces.iping import IPing
 from noc.sa.interfaces.igetarp import IGetARP
-
+from noc.sa.interfaces.icommands import ICommands
