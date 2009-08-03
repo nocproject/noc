f87e44fbbeac8b2e378a6c9282c335c5e25fc34f
Dmitry Volodin <dvolodin7@google.com>
2009-08-03 18:36:21 +0400

    Repeat protection in DHCPD Address Conflict

--- fm/rules/classes/dhcp.py
+++ fm/rules/classes/dhcp.py
@@ -17,8 +17,8 @@ class DHCPDAddressConflict(EventClass):
     priority = "WARNING"
     subject_template="DHCPD detects address conflict: {{ip}}"
     body_template="""DHCPD detects address conflict: {{ip}}"""
-    repeat_suppression=False
-    repeat_suppression_interval=3600
+    repeat_suppression=True
+    repeat_suppression_interval=600
     trigger=None
     class Vars:
         ip=Var(required=True,repeat=False)
