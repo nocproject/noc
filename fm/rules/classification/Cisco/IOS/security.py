baa188c6d6dbc3d4bdb0f7d861a531092dd3f3d1
Dmitry Volodin <dvolodin7@google.com>
2009-08-13 00:41:39 +0400

    "Cisco.IOS Login Success SYSLOG", "Cisco.IOS Login Failed SYSLOG" and "Cisco.IOS SSH Other SYSLOG" classification rules

--- fm/rules/classification/Cisco/IOS/security.py
+++ fm/rules/classification/Cisco/IOS/security.py
@@ -18,7 +18,7 @@ class Cisco_IOS_ACL_Reject_SYSLOG_Rule(ClassificationRule):
     preference=1000
     patterns=[
         (r"^profile$",r"^Cisco\.IOS$"),
-        (r"^message$",r"%SEC-6-IPACCESSLOGP: list (?P<acl_name>.+?) denied (?P<proto>\S+) (?P<src_ip>\S+)\((?P<src_port>\d+)\) \(.*\) -> (?P<dst_ip>\S+)\((?P<dst_port>\d+)\), (?P<count>\d+) packets?$"),
+        (r"^message$",r"%SEC-6-IPACCESSLOGP: list (?P<acl_name>.+?) denied (?P<proto>\S+) (?P<src_ip>\S+)\((?P<src_port>\d+)\) (?:\(.*\) )?-> (?P<dst_ip>\S+)\((?P<dst_port>\d+)\), (?P<count>\d+) packets?$"),
         (r"^source$",r"^syslog$"),
     ]
 ##
@@ -44,5 +44,57 @@ class Cisco_IOS_IPACCESSLOGRL_SYSLOG_Rule(ClassificationRule):
     patterns=[
         (r"^profile$",r"^Cisco\.IOS$"),
         (r"^message$",r"%SEC-6-IPACCESSLOGRL:"),
+        (r"^source$",r"^syslog$"),
+    ]
+##
+## Cisco.IOS Login Success SYSLOG
+##
+class Cisco_IOS_Login_Success_SYSLOG_Rule(ClassificationRule):
+    name="Cisco.IOS Login Success SYSLOG"
+    event_class=LoginSuccess
+    preference=1000
+    patterns=[
+        (r"^profile$",r"^Cisco\.IOS$"),
+        (r"^message$",r"%SEC_LOGIN-5-LOGIN_SUCCESS: Login Success \[user: (?P<user>.*?)\] \[Source: (?P<ip>\S+)\] \[localport: \d+\]"),
+        (r"^source$",r"^syslog$"),
+    ]
+##
+## Cisco.IOS Login Failed SYSLOG SNMP
+##
+class Cisco_IOS_Login_Failed_SYSLOG_SNMP_Rule(ClassificationRule):
+    name="Cisco.IOS Login Failed SYSLOG SNMP"
+    event_class=LoginFailed
+    preference=1000
+    patterns=[
+        (r"^profile$",r"^Cisco\.IOS$"),
+        (r"^1\.3\.6\.1\.4\.1\.9\.9\.41\.1\.2\.3\.1\.2\.\d+$",r"^SEC_LOGIN$"),
+        (r"^1\.3\.6\.1\.6\.3\.1\.1\.4\.1\.0$",r"^1\.3\.6\.1\.4\.1\.9\.9\.41\.2\.0\.1$"),
+        (r"^source$",r"^SNMP Trap$"),
+        (r"^1\.3\.6\.1\.4\.1\.9\.9\.41\.1\.2\.3\.1\.5\.\d+$",r"^Login failed \[user: (?P<user>.*?)\] \[Source: (?P<ip>\S+)\] \[localport: 22\] \[Reason: Login Authentication Failed\]"),
+        (r"^1\.3\.6\.1\.4\.1\.9\.9\.41\.1\.2\.3\.1\.4\.\d+$",r"^LOGIN_FAILED$"),
+    ]
+##
+## Cisco.IOS Login Failed SYSLOG
+##
+class Cisco_IOS_Login_Failed_SYSLOG_Rule(ClassificationRule):
+    name="Cisco.IOS Login Failed SYSLOG"
+    event_class=LoginFailed
+    preference=1000
+    patterns=[
+        (r"^profile$",r"^Cisco\.IOS$"),
+        (r"^message$",r"%SEC_LOGIN-4-LOGIN_FAILED: Login failed \[user: (?P<user>.*?)\] \[Source: (?P<ip>\S+)\] \[localport: \d+\] \[Reason: Login Authentication Failed\]"),
+        (r"^source$",r"^syslog$"),
+    ]
+##
+## Cisco.IOS SSH Other SYSLOG
+##
+class Cisco_IOS_SSH_Other_SYSLOG_Rule(ClassificationRule):
+    name="Cisco.IOS SSH Other SYSLOG"
+    event_class=DROP
+    preference=1000
+    action=DROP_EVENT
+    patterns=[
+        (r"^profile$",r"^Cisco\.IOS$"),
+        (r"^message$",r"%SSH-5-SSH2_(?:USERAUTH|SESSION|CLOSE)"),
         (r"^source$",r"^syslog$"),
     ]
