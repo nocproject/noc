46620f00080e7d1e08d3cc5037be55abe56ed6d5
Dmitry Volodin <dvolodin7@google.com>
2009-08-21 09:24:32 +0400

    Sortable tables for CSV parser

--- kb/parsers/csv_parser.py
+++ kb/parsers/csv_parser.py
@@ -16,8 +16,13 @@ class Parser(noc.kb.parsers.Parser):
     @classmethod
     def to_html(cls,kb_entry):
         reader=csv.reader([l.encode("utf-8") for l in kb_entry.body.splitlines()])
-        r=["<TABLE BORDER='1'>"]
+        r=[u"<TABLE BORDER='1' ID='csvtable' class='tablesorter'>","<thead>"]
+        r+=[u"<TR>"]+[u"<TH>%s</TH>"%unicode(c,"utf-8") for c in reader.next()]+[u"</TR>"]
+        r+=[u"</thead>",u"<tbody>"]
         for row in reader:
             r+=[u"<TR>"]+[u"<TD>%s</TD>"%unicode(c,"utf-8") for c in row]+[u"</TR>"]
-        r+=[u"</TABLE>"]
+        r+=[u"</tbody></TABLE>"]
+        r+=[u"<script type='text/javascript'>",
+            u"$(document).ready(function() {$('#csvtable').tablesorter();});",
+            u"</script>"]
         return "".join(r)
