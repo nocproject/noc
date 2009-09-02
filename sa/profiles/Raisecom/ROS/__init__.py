c5b6e0b30ed7ddbd88a8be2f92fc432c3456e9e9
Dmitry Volodin <dvolodin7@gmail.com>
2009-09-02 15:44:11 +0400

    Raisecom.ROS profile

---
+++ sa/profiles/Raisecom/ROS/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Vendor: Raisecom
+## OS:     ROS
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+import noc.sa.profiles
+from noc.sa.protocols.sae_pb2 import TELNET,SSH
+
+class Profile(noc.sa.profiles.Profile):
+    name="Raisecom.ROS"
+    supported_schemes=[TELNET,SSH]
+    pattern_more="^ --More--"
+    pattern_unpriveleged_prompt=r"^\S+?>"
+    command_super="enable"
+    pattern_prompt=r"^\S+?#"
+    command_more=" "
---
+++ share/doc/en/ug/profiles/Raisecom.ROS.rst
@@ -0,0 +1,14 @@
+Raisecom.ROS
+============
+
+====== =========================================
+Vendor `Raisecom <http://www.naraisecom.com/>`_
+OS     ROS
+====== =========================================
+
+Raisecom.ROS profile supports Raisecom's family of ethernet switches.
+
+Tested Equipment
+----------------
+.. supported:: Raisecom.ROS
+
