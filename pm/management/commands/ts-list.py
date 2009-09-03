cf4045c36189c3037b97818df1c24e93249c59fe
Dmitry Volodin <dvolodin7@gmail.com>
2009-09-03 11:30:59 +0400

    ts-list can filter by regular expression

--- pm/management/commands/ts-list.py
+++ pm/management/commands/ts-list.py
@@ -9,11 +9,20 @@
 """
 from django.core.management.base import BaseCommand
 from noc.pm.models import TimeSeries
+import re
 ##
 ## build-manifest command handler
 ##
 class Command(BaseCommand):
     help="List Available Time Series"
     def handle(self, *args, **options):
+        rx=[re.compile(o) for o in args]
         for ts in TimeSeries.objects.order_by("name"):
-            print ts.name
+            ts_name=ts.name
+            if rx:
+                for r in rx:
+                    if r.search(ts_name):
+                        print ts_name
+                        break
+            else:
+                print ts_name
