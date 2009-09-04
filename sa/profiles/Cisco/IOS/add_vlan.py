74002ad61a8d3e047c3e48ebf4d260fb6fcab5c2
Dmitry Volodin <dvolodin7@gmail.com>
2009-09-04 17:10:39 +0400

    Cisco.IOS.add_vlan script

---
+++ sa/profiles/Cisco/IOS/add_vlan.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Cisco.IOS.add_vlan
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+import noc.sa.script
+from noc.sa.interfaces import IAddVlan
+
+class Script(noc.sa.script.Script):
+    name="Cisco.IOS.add_vlan"
+    implements=[IAddVlan]
+    def execute(self,vlan_id,name):
+        if self.scripts.has_vlan(vlan_id=vlan_id):
+            return False
+        with self.configure():
+            self.cli("vlan %d"%vlan_id)
+            self.cli("name \"%s\""%name)
+            self.cli("exit")
+        self.save_config()
+        return True
