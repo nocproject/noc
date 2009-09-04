2b96601e902ce07cf3db84eb3cf4534b21304519
Dmitry Volodin <dvolodin7@gmail.com>
2009-09-05 00:48:22 +0400

    Force10.FTOS.add_vlan

---
+++ sa/profiles/Force10/FTOS/add_vlan.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Force10.FTOS.add_vlan
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+import noc.sa.script
+from noc.sa.interfaces import IAddVlan
+
+class Script(noc.sa.script.Script):
+    name="Force10.FTOS.add_vlan"
+    implements=[IAddVlan]
+    def execute(self,vlan_id,name,tagged_ports):
+        with self.configure():
+            self.cli("interface vlan %d"%vlan_id)
+            self.cli("name %s"%name)
+            for tp in tagged_ports:
+                self.cli("tagged %s"%tp)
+            self.cli("no shutdown")
+            self.cli("exit")
+        self.save_config()
+        return True
