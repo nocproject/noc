97d9be7ce63ba72da41b26aaa5b3848d6105ccba
Dmitry Volodin <dvolodin7@gmail.com>
2009-09-05 00:48:34 +0400

    Force10.FTOS.remove_vlan

---
+++ sa/profiles/Force10/FTOS/remove_vlan.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Force10.FTOS.remove_vlan
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+import noc.sa.script
+from noc.sa.interfaces import IRemoveVlan
+
+class Script(noc.sa.script.Script):
+    name="Force10.FTOS.remove_vlan"
+    implements=[IRemoveVlan]
+    def execute(self,vlan_id):
+        if not self.scripts.has_vlan(vlan_id=vlan_id):
+            return False
+        with self.configure():
+            self.cli("no interface vlan %d"%vlan_id)
+        self.save_config()
+        return True
