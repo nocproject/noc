fcc6701ea5c0ff83af82b2eec5104d0837a98a1b
Dmitry Volodin <dvolodin7@gmail.com>
2009-09-04 17:05:57 +0400

    ISyncVlans interface

---
+++ sa/interfaces/isyncvlans.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from base import *
+
+class ISyncVlans(Interface):
+    vlans=ListOfParameter(element=DictParameter(attrs={"vlan_id":VLANIDParameter(),
+                                                       "name":StringParameter(required=False)}))
+    returns=DictParameter(attrs={"created" : ListOfParameter(element=VLANIDParameter()),
+                                 "removed" : ListOfParameter(element=VLANIDParameter())})
