8dbfa2ba65b2ccdbede6c92b302a83e9a607d087
Dmitry Volodin <dvolodin7@gmail.com>
2009-09-28 09:19:57 +0400

    Limit transaction size

--- fm/periodics/archive.py
+++ fm/periodics/archive.py
@@ -15,10 +15,20 @@ class Task(noc.sa.periodic.Task):
     description=""
     
     def execute(self):
+        from django.db import connection
+        cursor=connection.cursor()
+                
         from noc.fm.models import Event,EventArchivationRule
         
+        cursor.execute("BEGIN")
         for rule in EventArchivationRule.objects.all():
             ts=datetime.datetime.now()-datetime.timedelta(seconds=rule.ttl*{"s":1,"m":60,"h":3600,"d":86400}[rule.ttl_measure])
-            Event.objects.filter(status="C",timestamp__lte=ts,event_class=rule.event_class).delete()
+            while True:
+                cursor.execute("SELECT COUNT(delete_event(id)) FROM fm_event WHERE id IN (SELECT id FROM fm_event WHERE status=%s AND timestamp<=%s AND event_class_id=%s LIMIT 100)",
+                    ["C",ts,rule.event_class.id])
+                c=cursor.fetchall()[0][0]
+                cursor.execute("COMMIT")
+                if c==0:
+                    break
         return True
 
