301b27aa52749d1f34bce03044805cffbad2aa53
Dmitry Volodin <dvolodin7@gmail.com>
2009-10-01 00:18:42 +0400

    Sun iLOM3 profile

---
+++ sa/profiles/Sun/iLOM3/get_config.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Sun.iLOM3.get_config
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from __future__ import with_statement
+import noc.sa.script
+from noc.sa.interfaces import IGetConfig
+
+class Script(noc.sa.script.Script):
+    name="Sun.iLOM3.get_config"
+    implements=[IGetConfig]
+    def execute(self):
+        self.cli("cd /SP/config")
+        with self.servers.http() as http:
+            url=http.get_url(self.access_profile.address)
+            self.cli("dump -destination %s"%url)
+            config=http.get_data()
+        self.cli("cd /")
+        return self.cleaned_config(config)
