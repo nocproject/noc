5996271aa6e95ebd3e1e2f6038359422e00f0ba0
Dmitry Volodin <dvolodin7@gmail.com>
2009-10-01 08:17:35 +0400

    Syntax highlighting for Sun.iLOM3

---
+++ sa/profiles/Sun/iLOM3/highlight.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Sun.iLOM3 highlight lexers
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+from pygments.lexers import XmlLexer
+from pygments.token import *
+##
+## Sun iLOM3 Configuration lexer
+##
+class ConfigLexer(XmlLexer):
+    name="Sun.iLOM3"
