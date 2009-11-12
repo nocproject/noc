a2ae9dbe0038aea9bce9d3e5d24d09decaac2713
Dmitry Volodin <dvolodin7@gmail.com>
2009-11-12 11:39:05 +0300

    Generic.configure script

---
+++ sa/profiles/Generic/configure.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Generic.configure
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+import noc.sa.script
+from noc.sa.interfaces import ICommands
+##
+## Enter a configuration mode and execute a list of CLI commands. return a list of results
+##
+class Script(noc.sa.script.Script):
+    name="Generic.commands"
+    implements=[ICommands]
+    requires=[]
+    def execute(self,commands):
+        with self.configure():
+            r=[self.cli(c) for c in commands]
+        self.save_config()
+        return r
+
