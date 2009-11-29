f348af17e9f5578b0bd5a3fa5a15d5f5077ae3cb
Dmitry Volodin <dvolodin7@gmail.com>
2009-11-29 11:11:16 +0300

    cm.dns_pull and cm.dns_push cannot be lauched concurrently

--- cm/periodics/dns_push.py
+++ cm/periodics/dns_push.py
@@ -10,7 +10,7 @@ import noc.sa.periodic
 class Task(noc.sa.periodic.Task):
     name="cm.dns_push"
     description=""
-    
+    wait_for=["cm.dns_pull"]
     def execute(self):
         from noc.cm.models import DNS
         DNS.global_push()
