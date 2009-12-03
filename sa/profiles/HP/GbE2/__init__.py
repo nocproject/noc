0bd6a7153cd5360c38374577c0b02fa1abef5a48
Dmitry Volodin <dvolodin7@gmail.com>
2009-12-03 17:49:37 +0300

    Ignore comments with configuration capture time

--- sa/profiles/HP/GbE2/__init__.py
+++ sa/profiles/HP/GbE2/__init__.py
@@ -19,3 +19,4 @@ class Profile(noc.sa.profiles.Profile):
     command_more=" "
     command_leave_config="apply"
     command_save_config="save\ny\n"
+    config_volatile=["^/\* Configuration dump taken"]
