65d5b22cc48efbc83706ab719f5a400594d10527
Dmitry Volodin <dvolodin7@gmail.com>
2009-12-28 17:15:53 +0300

    Add missed ()

--- sa/interfaces/isyncprefixlists.py
+++ sa/interfaces/isyncprefixlists.py
@@ -10,7 +10,7 @@
 from base import *
 
 class ISyncPrefixLists(Interface):
-    changed_prefix_lists=ListOfParameter(element=DictParameter(attrs={"name":StringParameter,
-                                                                      "prefix_list":ListOfParameter(element=IPv4PrefixParameter),
-                                                                      "strict": BooleanParameter}))
-    returns=ListOfParameter(element=DictParameter(attrs={"name":StringParameter,"status":BooleanParameter}))
+    changed_prefix_lists=ListOfParameter(element=DictParameter(attrs={"name":StringParameter(),
+                                                                      "prefix_list":ListOfParameter(element=IPv4PrefixParameter()),
+                                                                      "strict": BooleanParameter()}))
+    returns=ListOfParameter(element=DictParameter(attrs={"name":StringParameter(),"status":BooleanParameter()}))
