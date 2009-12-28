be337cb1f840cc87539ead4c4a98014fbdf0bb58
Dmitry Volodin <dvolodin7@gmail.com>
2009-12-28 15:52:10 +0300

    ISyncPrefixLists interface

---
+++ sa/interfaces/isyncprefixlists.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## ISyncPrefixLists interface
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from base import *
+
+class ISyncPrefixLists(Interface):
+    changed_prefix_lists=ListOfParameter(element=DictParameter(attrs={"name":StringParameter,
+                                                                      "prefix_list":ListOfParameter(element=IPv4PrefixParameter),
+                                                                      "strict": BooleanParameter}))
+    returns=ListOfParameter(element=DictParameter(attrs={"name":StringParameter,"status":BooleanParameter}))
