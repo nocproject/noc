90692cae6f932377dbaefdadddc3144f6e9e6e86
Dmitry Volodin <dvolodin7@gmail.com>
2009-12-29 08:31:54 +0300

    peer.prefix_list_provisioning periodic task

---
+++ peer/periodics/prefix_list_provisioning.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## peer.prefix_list_provisioning
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+import noc.sa.periodic
+import logging
+TIMEOUT=180
+CHECK_TIMEOUT=TIMEOUT/10
+
+class Task(noc.sa.periodic.Task):
+    name="peer.prefix_list_provisioning"
+    description=""
+    wait_for=["cm.prefix_list_pull"]
+    def execute(self):
+        from noc.peer.models import PrefixListCache
+        from noc.sa.models import ManagedObject,ReduceTask
+        #
+        prefix_lists={} # PeeringPoint -> [prefix_lists]
+        # For all out-of-dated prefix lists
+        for pc in PrefixListCache.objects.filter(peering_point__enable_prefix_list_provisioning=True):
+            if pc.pushed is not None and pc.pushed>pc.changed:
+                continue
+            if pc.peering_point not in prefix_lists:
+                prefix_lists[pc.peering_point]=[pc]
+            else:
+                prefix_lists[pc.peering_point]+=[pc]
+        # Run Map/Reduce tasks
+        tasks=[]
+        for peering_point in prefix_lists:
+            # Try to find managed object
+            try:
+                pp=ManagedObject.objects.get(name=peering_point.hostname)
+            except ManagedObject.DoesNotExist:
+                logging.warning("No managed object for peering point '%s' found"%peering_point.name)
+                continue
+            task=ReduceTask.create_task(
+                object_selector=[pp],
+                reduce_script="PrefixListProvisioningReport",
+                reduce_script_params=None,
+                map_script="sync_prefix_lists",
+                map_script_params=[{"name":pl.name,"strict":pl.strict,"prefix_list":pl.data} for pl in prefix_lists[peering_point]],
+                timeout=TIMEOUT)
+            tasks+=[task]
+        # Wait for tasks completion
+        ReduceTask.wait_for_task(tasks)
+        return True
