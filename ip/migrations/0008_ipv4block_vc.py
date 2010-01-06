c145733e80dd4b171416f80ce735d72cbd25d0e0
Dmitry Volodin <dvolodin7@gmail.com>
2010-01-06 22:35:11 +0300

    Execute 0008_ipv4block_vc only after vc module initialized

--- ip/migrations/0008_ipv4block_vc.py
+++ ip/migrations/0008_ipv4block_vc.py
@@ -5,6 +5,9 @@ from django.db import models
 from noc.ip.models import *
 
 class Migration:
+    depends_on=(
+        ("vc","0001_initial"),
+    )
     def forwards(self):
         VC = db.mock_model(model_name='VC', db_table='vc_vc', db_tablespace='', pk_field_name='id', pk_field_type=models.AutoField)
         db.add_column("ip_ipv4block","vc",models.ForeignKey(VC,verbose_name="VC",null=True,blank=True))
