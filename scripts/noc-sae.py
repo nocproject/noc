3be88d0fa12a63cfda9bc70ad59a79ecb5d74661
Dmitry Volodin <dvolodin7@gmail.com>
2010-01-06 18:56:29 +0300

    Include contrib/lib into PYTHONPATH

--- scripts/noc-sae.py
+++ scripts/noc-sae.py
@@ -6,9 +6,11 @@ import os,sys,getopt
 
 if __name__ == '__main__':
     d=os.path.dirname(sys.argv[0])
+    sys.path.insert(0,os.path.join(d,"..","contrib","lib"))
     sys.path.insert(0,os.path.join(d,"..",".."))
     sys.path.insert(0,os.path.join(d,".."))
     sys.path.insert(0,d)
+    print sys.path
     try:
         import settings
     except ImportError:
