d7d21bfddc9af1ea37a9406cbe0e6d16c0ec7f50
Dmitry Volodin <dvolodin7@gmail.com>
2010-01-08 01:12:39 +0300

    Peer: mark planned/active/shutdown admin actions

--- peer/admin.py
+++ peer/admin.py
@@ -52,6 +52,39 @@ class PeerAdmin(admin.ModelAdmin):
     list_display=["peering_point","local_asn","remote_asn","status","admin_import_filter","admin_export_filter","local_ip","remote_ip","admin_tt_url","description","communities"]
     search_fields=["remote_asn","description"]
     list_filter=["peering_point","status"]
+    actions=["mark_as_planned","mark_as_active","mark_as_shutdown"]
+    ##
+    ##
+    ##
+    def set_peer_status(self,request,queryset,status,message):
+        count=0
+        for p in queryset:
+            p.status=status
+            p.save()
+            count+=1
+        if count==1:
+            self.message_user(request,"1 peer marked as %s"%message)
+        else:
+            self.message_user(request,"%d peers marked as %s"%(count,message))
+    ##
+    ## Mark selected peers as planned
+    ##
+    def mark_as_planned(self,request,queryset):
+        return self.set_peer_status(request,queryset,"P","planned")
+    mark_as_planned.short_description="Mark as planned"
+    ##
+    ## Mark selected peers as planned
+    ##
+    def mark_as_active(self,request,queryset):
+        return self.set_peer_status(request,queryset,"A","active")
+    mark_as_active.short_description="Mark as active"
+    ##
+    ## Mark selected peers as planned
+    ##
+    def mark_as_shutdown(self,request,queryset):
+        return self.set_peer_status(request,queryset,"S","shutdown")
+    mark_as_shutdown.short_description="Mark as shutdown"
+
 
 admin.site.register(RIR,RIRAdmin)
 admin.site.register(Person,PersonAdmin)
