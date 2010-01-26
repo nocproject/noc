7c643ba72b8a95dd81cb88dd800778b4ace762f3
Dmitry Volodin <dvolodin7@gmail.com>
2010-01-26 13:13:45 +0300

    Pull only enabled and configuration managed objects

--- cm/periodics/config_pull.py
+++ cm/periodics/config_pull.py
@@ -16,7 +16,8 @@ class Task(noc.sa.periodic.Task):
     def execute(self):
         from noc.cm.models import Config
         for o in Config.objects.filter((Q(next_pull__lt=datetime.datetime.now())|Q(next_pull__isnull=True))&Q(pull_every__isnull=False)).order_by("next_pull"):
-            logging.debug("Pulling %s",str(o))
-            o.pull(self.sae)
+            if o.managed_object.is_managed and o.managed_object.is_configuration_managed:
+                logging.debug("Pulling %s",str(o))
+                o.pull(self.sae)
         return True
 
