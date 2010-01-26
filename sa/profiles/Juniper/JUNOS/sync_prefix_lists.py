93bcfa97416af7774777f3f269a95a669e0aa20d
Dmitry Volodin <dvolodin7@gmail.com>
2010-01-26 15:20:48 +0300

    Apply only prefix-list delta

--- sa/profiles/Juniper/JUNOS/sync_prefix_lists.py
+++ sa/profiles/Juniper/JUNOS/sync_prefix_lists.py
@@ -10,24 +10,41 @@
 from __future__ import with_statement
 import noc.sa.script
 from noc.sa.interfaces import ISyncPrefixLists
-import re
+import re,difflib
+
+rx_pl=re.compile(r"^set policy-options policy-statement \S+ term pass from route-filter (\S+) (\S+)$")
 
 class Script(noc.sa.script.Script):
     name="Juniper.JUNOS.sync_prefix_lists"
     implements=[ISyncPrefixLists]
     TIMEOUT=1800
     def execute(self,changed_prefix_lists):
+        actions=[]
         result=[]
-        with self.configure():
-            for l in changed_prefix_lists:
-                name=l["name"]
-                # Generate prefix list
-                pl=self.profile.generate_prefix_list(name,l["prefix_list"],l["strict"])
-                # Install new prefix list
-                self.cli("top")
-                self.cli("delete policy-options policy-statement %s"%name)
-                self.cli("edit policy-options policy-statement %s"%name)
-                self.cli("load merge relative terminal")
-                r=self.cli(pl+"\x04",bulk_lines=10)
-                result+=[{"name":name,"status":"load complete" in r}]
+        for l in changed_prefix_lists:
+            name=l["name"]
+            suffix="exact" if l["strict"] else "orlonger"
+            # Retrieve prefix list
+            pl=self.cli("show configuration policy-options policy-statement %s | display set | no-more"%name)
+            applied_pl=[]
+            for ln in pl.splitlines():
+                match=rx_pl.match(ln)
+                if match:
+                    applied_pl+=["%s %s"%(match.group(1),match.group(2))]
+            # Compare prefix-lists and make the changeset
+            new_pl=["%s %s"%(x,suffix) for x in l["prefix_list"]]
+            for d in difflib.ndiff(sorted(applied_pl),sorted(new_pl)):
+                code=d[:2]
+                rest=d[2:]
+                if code=="- ": # To delete
+                    actions+=["delete policy-options policy-statement %s term pass from route-filter %s"%(name,rest)]
+                elif code=="+ ": # To create
+                    actions+=["set policy-options policy-statement %s term pass from route-filter %s"%(name,rest)]
+            #
+            result+=[{"name":name,"status":True}]
+        # Apply changeset
+        if actions:
+            with self.configure():
+                for cmd in actions:
+                    self.cli(cmd)
         return result
