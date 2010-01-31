af967945565a5f9f8bb3f221d68033b3f51e4c00
Dmitry Volodin <dvolodin7@gmail.com>
2010-02-01 00:27:45 +0300

    Refactored

--- sa/servers/http.py
+++ sa/servers/http.py
@@ -16,11 +16,10 @@ class HTTPServerSocket(AcceptedTCPSocket):
     def __init__(self,factory,socket,server_hub):
         super(HTTPServerSocket,self).__init__(factory,socket)
         self.server_hub=server_hub
-        self.context=None
         self.request=""
+        self.data=None
         self.path=None
-        self.host=""
-        self.data_size=0
+        self.address=None
     ##
     ## <!> STUB
     ##
@@ -28,63 +27,39 @@ class HTTPServerSocket(AcceptedTCPSocket):
     def check_access(self,address):
         return True
     ##
-    def feed(self,data):
-        self.data_size-=len(data)
-        self.context.feed(data)
     ##
-    ## Parse incoming data
     ##
     def on_read(self,data):
-        if self.context is None:
-            # Parse request
+        if self.data is None:
             self.request+=data
-            for s in ["\n\n","\r\n\r\n"]:
-                if s in self.request:
-                    req,data=self.request.split(s,1)
-                    l0,lo=req.split("\n",1)
-                    self.debug(l0)
-                    r=l0.split(" ")
-                    if len(r)!=3 or r[0] not in ["GET","PUT"]:
-                        self.error("Invalid HTTP request")
-                        self.close()
-                        return
-                    method=r[0]
-                    address,port=self.socket.getpeername()
-                    self.path=urllib.unquote(r[1])
-                    if method=="PUT":
-                        try:
-                            self.context=self.server_hub.get_context("http",address,self.path)
-                        except KeyError:
-                            self.error("Permission denied: %s %s"%(address,self.path))
-                            self.close()
-                            return
-                        for l in lo.split("\n"):
-                            l=l.strip()
-                            k,v=[x.strip() for x in l.split(":",1)]
-                            k=k.lower()
-                            if k=="content-length":
-                                self.data_size=int(v)
-                            elif k=="host":
-                                self.host=v
-                    elif method=="GET":
-                        if url in self.dl_data:
-                            self.send_response(200,"OK",self.dl_data[url],content_type="application/octet-stream")
-                        else:
-                            self.send_response(404,"Not Found")
-                    break
-        else: # Process PUT
-            # Populate data
-            self.feed(data)
-            if self.data_size<=0:
-                # Return response on all data received
-                self.send_response(201,"Created")
+            if "\r\n\r\n" in self.request:
+                self.request,self.data=self.request.split("\r\n\r\n",1)
+                headers=self.request.splitlines()
+                method,self.path,proto=headers[0]
+                self.address,port=self.socket.getpeername()
+                getattr(self,"handle_%s"%method)(path)
+    ##
+    ##
+    ##
+    def handle_GET(self,path):
+        context=self.server_hub.get_context("http",address,self.path)
+        try:
+            self.send_response(200,"OK",context.get_url_data(path))
+        except KeyError:
+            self.send_response(404,"Not Found")
+    ##
+    ## <!> STUB
+    ##
+    @classmethod
+    def check_access(self,address):
+        return True
     
     def send_response(code,message,data="",content_type=""):
         headers=["HTTP/1.1 %d %s"%(code,message),"Content-Length: %d"%len(data)]
         if content_type:
             headers+=["Content-Type: %s"%content_type]
         self.write("\r\n".join(headers)+"\r\n\r\n"+data)
-        self.close()
+        self.close(flush=True)
 
 class HTTPServer(ListenTCPSocket):
     def __init__(self,factory,address,server_hub):
