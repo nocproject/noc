913947ddf3ad40cd007e6a9c6dcbe678af7f84c7
Dmitry Volodin <dvolodin7@gmail.com>
2010-02-25 17:33:15 +0300

    Process Eggs from contrib/lib correctly

--- scripts/noc-notifier.py
+++ scripts/noc-notifier.py
@@ -6,14 +6,18 @@
 ## Copyright (C) 2007-2009 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-import os,sys,getopt
+import os,sys,site
 
 if __name__ == '__main__':
+    # Adjust paths
     d=os.path.dirname(sys.argv[0])
-    sys.path.insert(0,os.path.join(d,"..","contrib","lib"))
+    contrib=os.path.join(d,"..","contrib","lib")
+    sys.path.insert(0,contrib)
     sys.path.insert(0,os.path.join(d,"..",".."))
     sys.path.insert(0,os.path.join(d,".."))
     sys.path.insert(0,d)
+    # Install eggs from contrib/lib
+    site.addsitedir(contrib)
     try:
         import settings
     except ImportError:
