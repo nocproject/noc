b08cb29814064f6b0559c3fce25a1de893edd6d3
Dmitry Volodin <dvolodin7@gmail.com>
2010-03-31 14:25:04 +0400

    Bugfix

--- sa/profiles/Juniper/JUNOS/sync_prefix_lists.py
+++ sa/profiles/Juniper/JUNOS/sync_prefix_lists.py
@@ -26,6 +26,7 @@ class Script(noc.sa.script.Script):
             if len(l["prefix_list"])==0:
                 result+=[{"name":name,"status":False}]
                 self.error("Refusing to apply empty policy-option %s"%name)
+                continue
             suffix="exact" if l["strict"] else "orlonger"
             # Retrieve prefix list
             pl=self.cli("show configuration policy-options policy-statement %s | display set | no-more"%name)
