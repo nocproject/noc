d567f72be2f0a7799a9e6dc77782f76f6b550b58
Dmitry Volodin <dvolodin7@gmail.com>
2010-04-21 12:52:54 +0400

    New application framework

--- urls.py
+++ urls.py
@@ -7,29 +7,13 @@
 ##----------------------------------------------------------------------
 """
 """
-from django.conf.urls.defaults import *
-from django.contrib import admin
-admin.autodiscover()
+from noc.lib.app import site,patterns
+
+urlpatterns=patterns("",
+    ('^$',                     'django.views.generic.simple.redirect_to', {'url' : '/main/index/'}),
+    # For debugging purposes only. Overriden by HTTP server directives
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'static'}),
+    (r'^doc/(?P<path>.*)$',    'django.views.static.serve', {'document_root': 'share/doc/users_guide/html/'}),
+)+site.urls
 
 handler404="noc.main.views.handler404"
-
-urlpatterns = patterns('',
-    # For debugging purposes only. Overriden by lighttpd/apache
-     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'static'}),
-     (r'^doc/(?P<path>.*)$',    'django.views.static.serve', {'document_root': 'share/doc/users_guide/html/'}),
-    # 
-     (r'^$',      include('noc.main.urls')),
-     (r"^admin/(?P<app_label>[^/]+)/(?P<model_name>[^/]+)/csv/export/", "noc.main.views.admin_list_csv_export"),
-     (r'^admin/', include(admin.site.urls)),
-     (r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name':'template/login.html'}),
-     (r'^main/',  include('noc.main.urls')),
-     (r'^sa/',  include('noc.sa.urls')),
-     (r'^fm/', include('noc.fm.urls')),
-     (r'^cm/',  include('noc.cm.urls')),
-     (r"^ip/", include("noc.ip.urls")),
-     (r"^vc/", include("noc.vc.urls")),
-     (r"^peer/", include("noc.peer.urls")),
-     (r"^dns/", include("noc.dns.urls")),
-     (r"^kb/", include("noc.kb.urls")),
-     (r"^pm/", include("noc.pm.urls")),
-)
