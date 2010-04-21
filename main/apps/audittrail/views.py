942ca6c0fa620227e53d76b135645a79f7f5efdd
Dmitry Volodin <dvolodin7@gmail.com>
2010-04-21 12:58:48 +0400

    main.audittrail

---
+++ main/apps/audittrail/views.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## AuditTrail Manager
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2010 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+from django.contrib import admin
+from noc.lib.app import ModelApplication
+from noc.main.models import AuditTrail
+##
+## AuditTrail admin
+##
+class AuditTrailAdmin(admin.ModelAdmin):
+    list_display=["user","timestamp","model","db_table","operation","subject"]
+    list_filter=["user"]
+##
+## AuditTrail application
+##
+class AuditTrailApplication(ModelApplication):
+    model=AuditTrail
+    model_admin=AuditTrailAdmin
+    menu="Audit Trail"
