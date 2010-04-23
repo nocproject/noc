b663b8d4de0f2ea96d24d496b3eeb5fc1539a0f1
Dmitry Volodin <dvolodin7@gmail.com>
2010-04-23 15:30:05 +0400

    peer.prefixlistbuilder application

---
+++ peer/apps/prefixlistbuilder/views.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## <<DESCRIPTION>>
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2010 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+from django import forms
+from noc.lib.app import Application
+from noc.peer.resolver import resolve_as_set_prefixes
+from noc.peer.models import PeeringPoint
+
+##
+## Prefix list builder form
+##
+class PrefixListBuilderForm(forms.Form):
+    peering_point= forms.ModelChoiceField(queryset=PeeringPoint.objects.all())
+    name=forms.CharField()
+    as_set=forms.CharField()
+    
+    def clean_as_set(self):
+        as_set=self.cleaned_data["as_set"]
+        if not as_set.startswith("AS"):
+            raise forms.ValidationError("Invalid AS or as-set")
+        return as_set
+##
+## Prefix List Builder Application
+##
+class PrefixListBuilderAppplication(Application):
+    ##
+    ## Resolve prefix list
+    ##
+    def view_builder(self,request):
+        pl=""
+        if request.POST:
+            form=PrefixListBuilderForm(request.POST)
+            if form.is_valid():
+                prefixes=resolve_as_set_prefixes(form.cleaned_data["as_set"],True)
+                prefixes=sorted(prefixes)
+                pl=form.cleaned_data["peering_point"].profile.generate_prefix_list(form.cleaned_data["name"],prefixes,False)
+        else:
+            form=PrefixListBuilderForm()
+        return self.render(request,"builder.html",{"form":form,"prefix_list":pl})
+    view_builder.url=r"^$"
+    view_builder.access=Application.has_perm("peer.change_peer")
+    view_builder.menu="Prefix List Builder"
