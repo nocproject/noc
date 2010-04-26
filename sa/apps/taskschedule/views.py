db87db2b3bb1bd6d496976e9d321ae381e2c0815
Dmitry Volodin <dvolodin7@gmail.com>
2010-04-26 18:26:09 +0400

    sa.taskschedule application

---
+++ sa/apps/taskschedule/views.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## TaskSchedule Manager
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2010 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+from django.contrib import admin
+from noc.lib.app import ModelApplication
+from noc.sa.models import TaskSchedule
+import datetime
+##
+## TaskSchedule admin
+##
+class TaskScheduleAdmin(admin.ModelAdmin):
+    list_display=["periodic_name","is_enabled","run_every","next_run"]
+    search_fields=["periodic_name"]
+    actions=["run_now"]
+    ##
+    ## Reschedule selected tasks
+    ##
+    def run_now(self,request,queryset):
+        updated=0
+        now=datetime.datetime.now()
+        for t in queryset:
+            t.next_run=now
+            t.save()
+            updated+=1
+        if updated==1:
+            message="1 task rescheduled"
+        else:
+            message="%d tasks rescheduled"%updated
+        self.message_user(request,message)
+    run_now.short_description="Run selected tasks now"
+##
+## TaskSchedule application
+##
+class TaskScheduleApplication(ModelApplication):
+    model=TaskSchedule
+    model_admin=TaskScheduleAdmin
+    menu="Task Schedules"
