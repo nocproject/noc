3da2fa284e1bf4084a272f571c48ecffe913cb7e
Dmitry Volodin <dvolodin7@gmail.com>
2010-04-26 14:12:02 +0400

    Zone uploading and previewer

--- dns/apps/dnszone/views.py
+++ dns/apps/dnszone/views.py
@@ -6,9 +6,13 @@
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 from django import forms
+from django.shortcuts import get_object_or_404
 from django.contrib import admin
 from noc.lib.app import ModelApplication
-from noc.dns.models import DNSZone,DNSZoneRecord
+from noc.dns.models import DNSZone,DNSZoneRecord,DNSServer,DNSZoneProfile
+from noc.sa.models import TaskSchedule
+from noc.lib.validators import is_fqdn
+import datetime
 ##
 ## Validation form for RR inlines
 ##
@@ -49,9 +53,63 @@ class DNSZoneAdmin(admin.ModelAdmin):
         return self.app.render_plain_text("\n\n".join([o.rpsl for o in queryset]))
     rpsl_for_selected.short_description="RPSL for selected objects"
 ##
+## Zone upload form
+##
+class ZonesUploadForm(forms.Form):
+    profile=forms.ModelChoiceField(queryset=DNSZoneProfile.objects)
+    file=forms.FileField()
+##
 ## DNSZone application
 ##
 class DNSZoneApplication(ModelApplication):
     model=DNSZone
     model_admin=DNSZoneAdmin
     menu="Zones"
+    ##
+    ## Render zone content
+    ##
+    def view_zone(self,request,zone_id,ns_id):
+        z=get_object_or_404(DNSZone,id=int(zone_id))
+        ns=get_object_or_404(DNSServer,id=int(ns_id))
+        return self.render_plain_text(z.zonedata(ns))
+    view_zone.url=r"^(?P<zone_id>\d+)/ns/(?P<ns_id>\d+)/$"
+    view_zone.access=ModelApplication.has_perm("dns.change_dnszone")
+    ##
+    ## Render tools page
+    ##
+    def view_tools(self,request):
+        return self.render(request,"tools.html",{"zones_upload_form":ZonesUploadForm()})
+    view_tools.url=r"^tools/$"
+    view_tools.name="tools"
+    view_tools.access=ModelApplication.has_perm("dns.change_dnszone")
+    ##
+    ## Upload zones
+    ##
+    def view_upload(self,request):
+        if request.method=="POST":
+            form = ZonesUploadForm(request.POST, request.FILES)
+            if form.is_valid():
+                profile=form.cleaned_data["profile"]
+                n=0
+                # Create missed records
+                for l in request.FILES["file"].read().split("\n"):
+                    l=l.strip().lower()
+                    if not l or not is_fqdn(l):
+                        continue
+                    try:
+                        DNSZone.objects.get(name=l)
+                        continue # Skip existing zones
+                    except DNSZone.DoesNotExist:
+                        pass
+                    DNSZone(name=l,is_auto_generated=False,profile=profile).save()
+                    n+=1
+                if n==0:
+                    self.message_user(request,"No new zones found")
+                else:
+                    # Trigger dns.update_domain_expiration to update paid_till
+                    TaskSchedule.reschedule("dns.update_domain_expiration",minutes=10)
+                    self.message_user(request,"%d new zones are imported"%n)
+                return self.response_redirect(self.base_url)
+        return self.response_redirect(self.base_url+"tools/")
+    view_upload.url="^tools/upload/$"
+    view_upload.access=ModelApplication.has_perm("dns.change_dnszone")
