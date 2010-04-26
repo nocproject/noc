7edfcc7d04ee88b9c82d20ee679e8d8884b7016d
Dmitry Volodin <dvolodin7@gmail.com>
2010-04-26 18:25:14 +0400

    sa.activator application

---
+++ sa/apps/activator/views.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Activator Manager
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2010 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+from django.contrib import admin
+from noc.lib.app import ModelApplication
+from noc.sa.models import Activator
+##
+## Activator admin
+##
+class ActivatorAdmin(admin.ModelAdmin):
+    list_display=["name","ip","is_active"]
+##
+## Activator application
+##
+class ActivatorApplication(ModelApplication):
+    model=Activator
+    model_admin=ActivatorAdmin
+    menu="Setup | Activators"
