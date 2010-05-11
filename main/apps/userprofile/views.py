923bd1399c8fc2d3cb2a2e14488e9ae7b49067d0
Dmitry Volodin <dvolodin7@gmail.com>
2010-05-11 18:01:10 +0400

    Delete "History" link

---
+++ main/apps/userprofile/views.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## UserProfile Manager
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2010 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+from django.contrib import admin
+from noc.lib.app import ModelApplication
+from noc.main.models import UserProfile,UserProfileContact
+
+
+##
+## UserProfile Contact Inline
+##
+class UserProfileContactAdmin(admin.TabularInline):
+    extra=5
+    model=UserProfileContact
+##
+## User profile admin
+##
+class UserProfileAdmin(admin.ModelAdmin):
+    inlines=[UserProfileContactAdmin]
+    fieldsets=(
+        (None, {
+            "fields" : ("preferred_language",),
+        }),
+    )
+##
+## UserProfile application
+##
+class UserProfileApplication(ModelApplication):
+    model=UserProfile
+    model_admin=UserProfileAdmin
+    ##
+    ## Edit profile
+    ##
+    def view_change(self,request,extra_content=None):
+        def response_change(*args):
+            self.message_user(request,"User Profile changed successfully")
+            return self.response_redirect("")
+        user=request.user
+        # Create profile if not exists yet
+        try:
+            profile=user.get_profile()
+        except:
+            profile=UserProfile(user=user)
+            profile.save()
+        self.admin.response_change=response_change
+        return self.admin.change_view(request,str(profile.id),extra_content)
+    view_change.url=r"^$"
+    view_change.url_name="change"
+    view_change.access=ModelApplication.permit_logged
