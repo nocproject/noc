886006c411ff30609d8b377b35bf3648293f8bf6
Dmitry Volodin <dvolodin7@gmail.com>
2010-05-11 18:01:10 +0400

    Delete "History" link

---
+++ main/apps/userprofile/tests/test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## userprofile Test
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+from noc.lib.test import ModelApplicationTestCase
+
+class userprofileTestCase(ModelApplicationTestCase):
+    pass
