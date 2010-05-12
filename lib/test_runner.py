9b05dd588d12f8ba556b7ad4e836dfbeab3ccd04
Dmitry Volodin <dvolodin7@gmail.com>
2010-05-12 15:56:45 +0400

    Application testing

--- lib/test_runner.py
+++ lib/test_runner.py
@@ -41,8 +41,8 @@ def get_test_suite(path):
 ## Build module list for Coverage
 ## and application test suite
 ##
-def get_tests():
-    modules=[]
+def get_tests(test_labels):
+    modules=["urls.py","settings.py"]
     suite=[]
     # Add additional test libraries
     for d in ["","lib"]:
@@ -57,13 +57,14 @@ def get_tests():
             suite+=list(get_test_suite(td))
         #
         for root,dirs,files in os.walk(d):
-            if "migrations" in root or "tests" in root: # Skip migrations and tests
-                continue
-            modules+=[m for m in [os.path.join(root,f) for f in files if f.endswith(".py")] if os.path.getsize(m)>1]
             parts=root.split(os.path.sep)
+            if "migrations" in parts: # Skip migrations
+                continue
+            if "tests" not in parts:
+                modules+=[m for m in [os.path.join(root,f) for f in files if f.endswith(".py")] if os.path.getsize(m)>1]
             # Add all tests from applications's tests/
             if len(parts)==4 and parts[1]=="apps" and parts[3]=="tests":
-                suit+=list(get_test_suite(root))
+                suite+=list(get_test_suite(root))
             # Add all applications tests.py
             if len(parts)==3 and parts[1]=="apps" and "tests.py" in files:
                 mn=".".join(["noc"]+parts+["tests"])
@@ -73,16 +74,25 @@ def get_tests():
         if "tests" in root:
             continue
         modules+=[m for m in [os.path.join(root,f) for f in files if f.endswith(".py")] if os.path.getsize(m)>1]
-    # Build test suite
-    ts=[]
-    for m in suite:
-        ts+=unittest.defaultTestLoader.loadTestsFromModule(m)
-    return modules,ts
+    return modules,suite
 ##
 ## "manage.py test" runner
 ##
 def run_tests(test_labels,verbosity=1,interactive=True,extra_tests=[]):
-    modules,extra_tests=get_tests()
+    def match_test(m):
+        if not test_labels:
+            return True
+        for l in test_labels:
+            if m.startswith(l):
+                return True
+        return False
+    # Mark testing environment
+    settings.NOC_TEST=True
+    # Scan for tests
+    print "Scanning for tests ...",
+    modules,suite=get_tests(test_labels)
+    print "... %d test cases found"%(len(modules)+len(suite))
+    print "Preparing test cases ..."
     coverage=Coverage()
     coverage.exclude(r"^\s*$")                # Exclude empty lines
     coverage.exclude(r"^\s*#.*$")             # Exclude comment blocks
@@ -91,19 +101,29 @@ def run_tests(test_labels,verbosity=1,interactive=True,extra_tests=[]):
     coverage.start()
     # Add docstrings and module load tests
     # Run inside Coverage context
+    extra_tests=[]
     doctestOutputChecker = OutputChecker()
     for m in modules:
         mn="noc."+m.replace(os.path.sep,".")[:-3]
         if mn.endswith("__init__"):
             mn=mn[:-9]
-        # Module load
-        extra_tests+=[ImportTestCase(mn)]
-        # Docstrings
-        try:
-            extra_tests+=doctest.DocTestSuite(mn,checker=doctestOutputChecker,runner=DocTestRunner)
-        except ValueError:
-            pass # No doctests in module
-    test_results=simple.run_tests(test_labels,verbosity,interactive,extra_tests)
+        if match_test(mn):
+            # Module load
+            extra_tests+=[ImportTestCase(mn)]
+            # Docstrings
+            try:
+                extra_tests+=[doctest.DocTestSuite(mn,checker=doctestOutputChecker,runner=DocTestRunner)]
+            except ValueError:
+                pass # No doctests in module
+    for m in suite:
+        if not match_test(m):
+            continue
+        mo=__import__(m,{},{},"*")
+        extra_tests+=[unittest.defaultTestLoader.loadTestsFromModule(mo)]
+    if verbosity>1:
+        print "Extra tests: ",extra_tests
+    print "Testing ..."
+    test_results=simple.run_tests([],verbosity,interactive,extra_tests)
     coverage.stop()
     # Coverage HTML Report
     print "Writing Coverage report to %s"%settings.COVERAGE_REPORT_PATH
