2e4b171c6f52f09ca59062636da3e632c85f2f2a
Dmitry Volodin <dvolodin7@gmail.com>
2010-05-12 15:33:45 +0400

    main.calculator application

---
+++ main/apps/calculator/views.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Calculator application
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2010 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+from noc.lib.app import Application
+from noc.main.apps.calculator.calculators import calculator_registry
+##
+## Register all calculators
+print "ZZZZZ"
+calculator_registry.register_all()
+print calculator_registry.choices
+##
+## Calculator application
+##
+class CalculatorAppplication(Application):
+    ##
+    ## Calculator index
+    ##
+    def view_index(self,request):
+        r=[(cn,c.title) for cn,c in calculator_registry.classes.items()]
+        r=sorted(r,lambda x,y: cmp(x[1],y[1]))
+        return self.render(request,"index.html",{"calculators":r})
+    view_index.url=r"^$"
+    view_index.menu="Calculators"
+    view_index.access=Application.permit
+    ##
+    ## Calculator view
+    ##
+    def view_calculate(self,request,calculator):
+        try:
+            c=calculator_registry[calculator](self)
+        except KeyError:
+            return self.response_not_found("No calculator found")
+        return c.render(request)
+    view_calculate.url=r"^(?P<calculator>\S+)/$"
+    view_calculate.access=Application.permit
