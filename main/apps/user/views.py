cf9617e94e0bb0a113592a10a0819436e247d058
Dmitry Volodin <dvolodin7@gmail.com>
2010-05-16 18:57:41 +0400

    Change user password forms

--- main/apps/user/views.py
+++ main/apps/user/views.py
@@ -8,6 +8,7 @@
 from noc.lib.app import ModelApplication
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
+from django.shortcuts import get_object_or_404
 ##
 ##
 ##
@@ -19,6 +20,17 @@ class UserApplication(ModelApplication):
     ## Change password view
     ##
     def view_change_password(self,request,object_id):
-        return self.admin.user_change_password(request,object_id)
+        if not self.admin.has_change_permission(request):
+            return self.response_fobidden("Permission denied")
+        user=get_object_or_404(self.model,pk=object_id)
+        if request.POST:
+            form=self.admin.change_password_form(user,request.POST)
+            if form.is_valid():
+                new_user=form.save()
+                self.message_user(request,"Password changed")
+                return self.response_redirect("main:user:change",object_id)
+        else:
+            form=self.admin.change_password_form(user)
+        return self.render(request,"change_password.html",{"form":form,"original":user})
     view_change_password.url=r"^(\d+)/password/$"
     view_change_password.access=ModelApplication.permit_change
