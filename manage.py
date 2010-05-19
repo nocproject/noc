b6094e510466b8fbf0729a46afdc5bc170a0ef98
Dmitry Volodin <dvolodin7@gmail.com>
2010-05-19 12:04:24 +0400

    Do not initialize applications in "manage.py test" mode

--- manage.py
+++ manage.py
@@ -18,8 +18,9 @@ except ImportError:
 from django.core.management import execute_manager
 
 if __name__ == "__main__":
-    # Initialize models
-    os.environ["DJANGO_SETTINGS_MODULE"]="noc.settings"
-    import noc.urls
+    if sys.argv[1] not in ["test"]:
+        # Initialize models
+        os.environ["DJANGO_SETTINGS_MODULE"]="noc.settings"
+        import noc.urls
     # Execute command
     execute_manager(settings)
