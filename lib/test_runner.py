da0110835a4e32a1b0a67cf416d607b40fb75e2d
Dmitry Volodin <dvolodin7@gmail.com>
2010-05-21 00:56:40 +0400

    Code cleanup

--- lib/test_runner.py
+++ lib/test_runner.py
@@ -22,6 +22,25 @@ class ImportTestCase(unittest.TestCase):
     def runTest(self):
         __import__(self.module,{},{},"*")
 ##
+## Convert path to module name
+##
+def path_to_mod(path):
+    if path.endswith(".py"):
+        path=path[:-3]
+    if path.endswith("__init__"):
+        path=path[:-8]
+    return "noc."+path.replace(os.path.sep,".")
+##
+## Check module matches test_labels
+##
+def match_test(test_labels,m):
+    if not test_labels:
+        return True
+    for l in test_labels:
+        if m.startswith(l):
+            return True
+    return False
+##
 ## Generator returning NOC's modules
 ##
 def get_modules():
@@ -35,9 +54,11 @@ def get_modules():
 ## Return test modules from below path
 ##
 def get_test_suite(path):
+    r=[]
     for root,dirs,files in os.walk(path):
         for f in [f for f in files if f.endswith(".py")]:
-            yield ".".join(["noc"]+root.split(os.path.sep)+[f[:-3]])
+            r+=[path_to_mod(os.path.join(root,f))]
+    return r
 ##
 ## Build module list for Coverage
 ## and application test suite
@@ -49,44 +70,35 @@ def get_tests(test_labels):
     for d in ["","lib"]:
         p=os.path.join(d,"tests")
         if os.path.isdir(p):
-            suite+=list(get_test_suite(p))
+            suite+=get_test_suite(p)
     # Scan modules for tests
     for d in get_modules():
         # Add module's tests/
         td=os.path.join(d,"tests")
         if os.path.isdir(td):
-            suite+=list(get_test_suite(td))
-        #
+            suite+=get_test_suite(td)
+        # Walk for application's tests
         for root,dirs,files in os.walk(d):
             parts=root.split(os.path.sep)
-            if "migrations" in parts: # Skip migrations
+            if len(parts)<=2 or parts[1]!="apps":
                 continue
             if "tests" not in parts:
-                modules+=[m for m in [os.path.join(root,f) for f in files if f.endswith(".py")] if os.path.getsize(m)>1]
+                # Add modules for import and docstring testings
+                modules+=[path_to_mod(p) for p in [os.path.join(root,f) for f in files if f.endswith(".py")] if os.path.getsize(p)>1]
             # Add all tests from applications's tests/
-            if len(parts)==4 and parts[1]=="apps" and parts[3]=="tests":
-                suite+=list(get_test_suite(root))
+            elif len(parts)==4 and parts[3]=="tests":
+                suite+=get_test_suite(root)
             # Add all applications tests.py
-            if len(parts)==3 and parts[1]=="apps" and "tests.py" in files:
-                mn=".".join(["noc"]+parts+["tests"])
-                suite+=[mn]
-    ## Scan lib/ for tests
-    for root,dirs,files in os.walk("lib"):
-        if "tests" in root:
-            continue
-        modules+=[m for m in [os.path.join(root,f) for f in files if f.endswith(".py")] if os.path.getsize(m)>1]
+            elif len(parts)==3 and "tests.py" in files:
+                suite+=[".".join(["noc"]+parts+["tests"])]
+    # Filter out
+    modules=[m for m in modules if match_test(test_labels,m)]
+    suite=[m for m in suite if match_test(test_labels,m)]
     return modules,suite
 ##
 ## "manage.py test" runner
 ##
 def run_tests(test_labels,verbosity=1,interactive=True,extra_tests=[],coverage=True,reuse_db=False):
-    def match_test(m):
-        if not test_labels:
-            return True
-        for l in test_labels:
-            if m.startswith(l):
-                return True
-        return False
     # Mark testing environment
     settings.NOC_TEST=True
     suite=unittest.TestSuite()
@@ -106,21 +118,18 @@ def run_tests(test_labels,verbosity=1,interactive=True,extra_tests=[],coverage=T
     # Run inside Coverage context
     doctestOutputChecker = OutputChecker()
     for m in modules:
-        mn="noc."+m.replace(os.path.sep,".")[:-3]
-        if mn.endswith("__init__"):
-            mn=mn[:-9]
-        if match_test(mn):
-            # Module load
-            suite.addTest(ImportTestCase(mn))
-            # Docstrings
-            try:
-                suite.addTest(doctest.DocTestSuite(mn,checker=doctestOutputChecker,runner=DocTestRunner))
-            except ValueError:
-                pass # No doctests in module
+        # Module load
+        suite.addTest(ImportTestCase(mn))
+        # Docstrings
+        try:
+            suite.addTest(doctest.DocTestSuite(mn,checker=doctestOutputChecker,runner=DocTestRunner))
+        except ValueError:
+            pass # No doctests in module
     for m in tsuite:
-        if not match_test(m):
-            continue
         mo=__import__(m,{},{},"*")
+        import types
+        for name in dir(mo):
+            obj=getattr(mo,name)
         suite.addTest(unittest.defaultTestLoader.loadTestsFromModule(mo))
     for test in extra_tests:
         suite.addTest(test)
