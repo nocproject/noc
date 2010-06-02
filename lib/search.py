da325fc2c9b6b299887b6e1aae9535af5aa65866
Dmitry Volodin <dvolodin7@gmail.com>
2010-06-02 10:03:42 +0400

    Remove search method registration from library to prevent import loops

--- lib/search.py
+++ lib/search.py
@@ -7,35 +7,9 @@
 ##----------------------------------------------------------------------
 """
 """
-from django.db.models.signals import class_prepared
-from django.conf import settings
+from noc.lib.app import site
+from noc.main.models import search_methods
 import types
-##
-## Prevent recursive import
-##
-if settings.IS_WEB:
-    from noc.lib.app import site
-##
-## A hash of Model.search classmethods.
-## Populated by "class_prepared" signal listener
-## Model.search is a generator taking parameters (user,query,limit)
-## And yielding a SearchResults (ordered by relevancy)
-##
-search_methods={}
-
-##
-## Register new search handler if model has .search classmethod
-##
-def on_new_model(sender,**kwargs):
-    if hasattr(sender,"search"):
-        search_methods[getattr(sender,"search")]=None
-
-##
-## Attach to the 'class_prepared' signal
-## and on_new_model on every new model
-##
-class_prepared.connect(on_new_model)
-
 ##
 ## Wrapper for search results
 ##
