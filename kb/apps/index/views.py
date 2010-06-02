3787ae6da2aee929706185b289dbf03d123bfa92
Dmitry Volodin <dvolodin7@gmail.com>
2010-06-02 09:52:45 +0400

    Use NOC permissions

--- kb/apps/index/views.py
+++ kb/apps/index/views.py
@@ -6,7 +6,7 @@
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 from django.shortcuts import get_object_or_404
-from noc.lib.app import Application
+from noc.lib.app import Application,PermitLogged
 from noc.kb.models import *
 ##
 ## Knowledge Base Index
@@ -34,7 +34,7 @@ class IndexAppplication(Application):
     view_index.url=r"^$"
     view_index.url_name="index"
     view_index.menu="Knowledge Base"
-    view_index.access=Application.permit_logged
+    view_index.access=PermitLogged()
     ##
     ## Render named tab
     ##
@@ -51,7 +51,7 @@ class IndexAppplication(Application):
         return tabs[tab](request)
     view_tab.url=r"^(?P<tab>[^/]+)/$"
     view_tab.url_name="tab"
-    view_tab.access=Application.permit_logged
+    view_tab.access=PermitLogged()
     ##
     ## Bookmarks page
     ##
@@ -60,7 +60,7 @@ class IndexAppplication(Application):
             [b.kb_entry for b in KBGlobalBookmark.objects.all()]\
             +[b.kb_entry for b in KBUserBookmark.objects.filter(user=request.user)],"bookmarks")
     view_index_bookmarks.url=r"^bookmarks/$"
-    view_index_bookmarks.access=Application.permit_logged
+    view_index_bookmarks.access=PermitLogged()
     ##
     ## Categories page
     ##
@@ -68,28 +68,28 @@ class IndexAppplication(Application):
         return self.render_index(request,KBCategory.objects.order_by("name"),"categories")
     view_index_categories.url=r"^categories/$"
     view_index_categories.url_name="categories"
-    view_index_categories.access=Application.permit_logged
+    view_index_categories.access=PermitLogged()
     ##
     ## Last Modified page
     ##
     def view_index_latest(self,request):
         return self.render_index(request,KBEntry.last_modified(self.TOP_ENTRIES),"latest")
     view_index_latest.url=r"^latest/$"
-    view_index_latest.access=Application.permit_logged
+    view_index_latest.access=PermitLogged()
     ##
     ## Popular page
     ##
     def view_index_popular(self,request):
         return self.render_index(request,KBEntry.most_popular(self.TOP_ENTRIES),"popular")
     view_index_popular.url=r"^popular/$"
-    view_index_popular.access=Application.permit_logged
+    view_index_popular.access=PermitLogged()
     ##
     ## All page
     ##
     def view_index_all(self,request):
         return self.render_index(request,KBEntry.objects.order_by("-id"),"popular")
     view_index_all.url=r"^all/$"
-    view_index_all.access=Application.permit_logged
+    view_index_all.access=PermitLogged()
     ##
     ## Category Index
     ##
@@ -98,4 +98,4 @@ class IndexAppplication(Application):
         return self.render(request,"index_category.html",{"category":category,"entries":category.kbentry_set.order_by("id")})
     view_index_category.url=r"^categories/(?P<category_id>\d+)/$"
     view_index_category.url_name="category"
-    view_index_category.access=Application.permit_logged
+    view_index_category.access=PermitLogged()
