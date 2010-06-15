4804d626d893ce429b5cfa991d3e78c7364baa77
Dmitry Volodin <dvolodin7@gmail.com>
2010-06-15 16:55:43 +0400

    Brocade.FabricOS profile

---
+++ sa/profiles/Brocade/FabricOS/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Vendor: Brocade
+## OS:     FabricOS
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2010 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+import noc.sa.profiles
+from noc.sa.protocols.sae_pb2 import TELNET,SSH
+
+class Profile(noc.sa.profiles.Profile):
+    name="Brocade.FabricOS"
+    supported_schemes=[TELNET,SSH]
+    pattern_prompt=r"\S+:\S+>"
---
+++ sa/profiles/Brocade/FabricOS/get_version.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Brocade.FabricOS.get_version
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2010 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+import noc.sa.script
+from noc.sa.interfaces import IGetVersion
+import re
+
+rx_version=re.compile(r"Fabric OS:\s+(?P<version>\S+)",re.MULTILINE)
+rx_platform=re.compile(r"^Part Num:\s+(?P<platform>\S+)",re.MULTILINE)
+
+class Script(noc.sa.script.Script):
+    name="Brocade.FabricOS.get_version"
+    implements=[IGetVersion]
+    def execute(self):
+        v=self.cli("version")
+        vm=rx_version.search(v)
+        v=self.cli("chassisshow")
+        print v
+        cm=rx_platform.search(v)
+        return {
+            "vendor"    : "Brocade",
+            "platform"  : cm.group("platform"),
+            "version"   : vm.group("version"),
+        }
