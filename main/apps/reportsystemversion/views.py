7aac7c3afae718d8ae73c00cfea54b4a85859e61
Dmitry Volodin <dvolodin7@gmail.com>
2010-07-09 07:56:38 +0400

    Display contrib/ software versions

--- main/apps/reportsystemversion/views.py
+++ main/apps/reportsystemversion/views.py
@@ -5,9 +5,10 @@
 ## Copyright (C) 2007-2010 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
+from __future__ import with_statement
 from noc.lib.app.simplereport import SimpleReport
 from noc.lib.version import get_version
-import os,sys
+import os,sys,csv
 import django
 ##
 ##
@@ -15,12 +16,18 @@ import django
 class ReportSystemVersion(SimpleReport):
     title="System Version"
     def get_data(self,**kwargs):
+        versions=[
+            ["NOC"    ,    get_version()],
+            ["OS"     ,    " ".join(os.uname())],
+            ["Python" ,    sys.version],
+            ["PostgreSQL", self.execute("SELECT VERSION()")[0][0]],
+        ]
+        cv_path=os.path.join("contrib","lib","VERSION.csv")
+        if os.path.exists(cv_path):
+            with open(cv_path) as f:
+                r=csv.reader(f)
+                for row in r:
+                    versions+=[row]
         return {
-            "data" : [
-                ["NOC"    ,    get_version()],
-                ["OS"     ,    " ".join(os.uname())],
-                ["Python" ,    sys.version],
-                ["Django" ,    "%s"%str(django.VERSION)],
-                ["PostgreSQL", self.execute("SELECT VERSION()")[0][0]],
-            ]
+            "data" : versions
         }
