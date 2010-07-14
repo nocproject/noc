7acdc7d5fc0ebc84b74d1208b1e852ec68b5795f
Dmitry Volodin <dvolodin7@gmail.com>
2010-07-14 08:55:00 +0400

    dns.supported_servers migrated to a new application framework

---
+++ dns/apps/reportsupportedns/tests/test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## reportsupportedns Test
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+from noc.lib.test import ReportApplicationTestCase
+
+class reportsupportednsTestCase(ReportApplicationTestCase):
+    pass
