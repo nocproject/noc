07ceb41ebee14414fd8121406eb6cb777b02b8f8
Dmitry Volodin <dvolodin7@gmail.com>
2010-07-14 16:33:43 +0400

    dns.duplicated_fqdns migrated to a new application framework

---
+++ dns/apps/reportduplicatedfqdns/views.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Duplicated FQDNs Report
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2010 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+from noc.lib.app.simplereport import SimpleReport
+from noc.ip.models import VRF
+##
+##
+##
+class Reportreportduplicatedfqdns(SimpleReport):
+    title="Duplicated FQDNs"
+    def get_data(self,**kwargs):
+        vrf_id=VRF.get_global().id
+        return self.from_query(title=self.title,
+            columns=["FQDN","N","Addresses"],
+            query="""
+            SELECT fqdn,COUNT(*),
+                array_to_string(ARRAY(SELECT ip FROM ip_ipv4address WHERE fqdn=a.fqdn AND vrf_id=%s ORDER BY ip),', ')
+            FROM ip_ipv4address a
+            WHERE vrf_id=%s
+            GROUP BY 1
+            HAVING COUNT(*)>1
+            ORDER BY 2 DESC""",
+            params=[vrf_id,vrf_id])
