fc02d0de2df35887202ea08b5d3cb81892e7058b
Dmitry Volodin <dvolodin7@gmail.com>
2010-07-14 17:50:42 +0400

    dns.missed_p2p_address migrated to a new application framework

---
+++ dns/apps/reportmissedp2p/tests/test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## reportmissedp2p Test
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+from noc.lib.test import ReportApplicationTestCase
+
+class reportmissedp2pTestCase(ReportApplicationTestCase):
+    pass
