46b9460aa24cd97fc4f6645de1cba721ce263c33
Dmitry Volodin <dvolodin7@gmail.com>
2010-07-14 14:58:49 +0400

    Extreme.XOS profile

---
+++ sa/profiles/Extreme/XOS/get_version.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Extreme.XOS.get_version
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+import noc.sa.script
+from noc.sa.interfaces import IGetVersion
+import re
+
+rx_platform=re.compile(r"Card type:\s+(?P<platform>\S+)",re.MULTILINE|re.DOTALL)
+rx_version=re.compile(r"EXOS version:\s+(?P<version>\S+)",re.MULTILINE|re.DOTALL)
+
+class Script(noc.sa.script.Script):
+    name="Extreme.XOS.get_version"
+    implements=[IGetVersion]
+    def execute(self):
+        v=self.cli("debug hal show version")
+        platform=rx_platform.search(v).group("platform")
+        version=rx_version.search(v).group("version")
+        return {
+            "vendor"    : "Extreme",
+            "platform"  : platform,
+            "version"   : version,
+        }
---
+++ sa/profiles/Extreme/XOS/get_vlans.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Extreme.XOS.get_vlans
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2010 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+import noc.sa.script
+from noc.sa.interfaces import IGetVlans
+import re
+
+rx_vlan_line=re.compile(r"^(?P<name>\S+)\s+(?P<vlan_id>\d{1,4})\s")
+
+class Script(noc.sa.script.Script):
+    name="Extreme.XOS.get_vlans"
+    implements=[IGetVlans]
+    def execute(self):
+        vlans=self.cli("show vlan")
+        r=[]
+        for l in vlans.split("\n"):
+            match=rx_vlan_line.match(l.strip())
+            if match:
+                name=match.group("name")
+                vlan_id=int(match.group("vlan_id"))
+                r.append({
+                    "vlan_id": vlan_id,
+                    "name"   : name
+                    })
+        return r
