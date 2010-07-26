24c8c6f1fea2743d866be3fbb979b768a45fa2f4
Dmitry Volodin <dvolodin7@gmail.com>
2010-07-27 01:22:44 +0400

    Cisco.CatOS profile

---
+++ sa/profiles/Cisco/CatOS/get_version.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Cisco.CatOS.get_version
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2010 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+import noc.sa.script
+from noc.sa.interfaces import IGetVersion
+import re
+
+rx_platform=re.compile(r"^Hardware\s+Version:\s+(?:\d|\.)+\s+Model:\s+(?P<platform>\S+)\s+Serial\s+#:\s+\S+$",re.MULTILINE|re.DOTALL)
+rx_version=re.compile(r"^\S+\s+Software,\s+Version\s+\S+:\s+(?P<version>\S+)",re.MULTILINE|re.DOTALL)
+
+class Script(noc.sa.script.Script):
+    name="Cisco.CatOS.get_version"
+    implements=[IGetVersion]
+    def execute(self):
+        v=self.cli("show version")
+        platform=rx_platform.search(v).group("platform")
+        version=rx_version.search(v).group("version")
+        return {
+            "vendor"    : "Cisco",
+            "platform"  : platform,
+            "version"   : version,
+        }
---
+++ share/doc/en/ug/profiles/Cisco.CatOS.rst
@@ -0,0 +1,13 @@
+Cisco.CatOS
+===========
+
+====== ========================================
+Vendor `Cisco Systems <http://www.cisco.com/>`_
+OS     CatOS
+====== ========================================
+
+Cisco.CatOS profile supports CatOS-based Cisco Catalyst switches
+
+Tested Equipment
+----------------
+.. supported:: Cisco.CatOS
