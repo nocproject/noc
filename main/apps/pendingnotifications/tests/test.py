8ef589c5220f4b65def32019383c0caedb330259
Dmitry Volodin <dvolodin7@gmail.com>
2010-07-26 10:24:56 +0400

    main.pendingnotifications application

---
+++ main/apps/pendingnotifications/tests/test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## pendingnotifications Test
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+from noc.lib.test import ModelApplicationTestCase
+
+class pendingnotificationsTestCase(ModelApplicationTestCase):
+    pass
---
+++ main/apps/pendingnotifications/views.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Pending Notifications
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2010 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+from django.contrib import admin
+from noc.lib.app import ModelApplication
+from noc.main.models import Notification
+##
+## Notification admin
+##
+class NotificationAdmin(admin.ModelAdmin):
+    display_list=["timestamp","notification_method","notification_params","subject","next_try"]
+##
+## Notification application
+##
+class NotificationApplication(ModelApplication):
+    model=Notification
+    model_admin=NotificationAdmin
+    menu="Pending Notifications"
