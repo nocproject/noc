8cdc1e21b166e745c2b8d7e0a2d4be11525061dd
Dmitry Volodin <dvolodin7@gmail.com>
2010-07-31 09:20:05 +0400

    EdgeCore.ES35xx profile improvements

--- sa/profiles/EdgeCore/ES35xx/__init__.py
+++ sa/profiles/EdgeCore/ES35xx/__init__.py
@@ -2,20 +2,22 @@
 ##----------------------------------------------------------------------
 ## Vendor: EdgeCore
 ## OS:     ES35xx
-## Compatible with: ES3526S
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2009 The NOC Project
+## Copyright (C) 2007-2010 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 """
 """
 import noc.sa.profiles
 from noc.sa.protocols.sae_pb2 import TELNET,SSH
+import re
 
 class Profile(noc.sa.profiles.Profile):
     name="EdgeCore.ES35xx"
     supported_schemes=[TELNET,SSH]
+    pattern_unpriveleged_prompt=r"^\S+?>"
+    pattern_prompt=r"^\S+?#"
     pattern_more="^---More---.*?$"
     command_more=" "
-    rogue_chars=["\r","\x08"]
+    rogue_chars=["\r",re.compile(r" *\x08+")]
 
