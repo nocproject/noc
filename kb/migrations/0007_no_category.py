ee08612175007c9fe1c17a3c23f1604f11474d22
Dmitry Volodin <dvolodin7@gmail.com>
2010-08-02 20:00:51 +0400

    Truncate categories to 50 chars

--- kb/migrations/0007_no_category.py
+++ kb/migrations/0007_no_category.py
@@ -10,6 +10,7 @@ class Migration:
         # Migrate tags
         ctype_id=db.execute("SELECT id FROM django_content_type WHERE model='kbentry'")[0][0]
         for category,entry_id in db.execute("SELECT c.name,e.kbentry_id FROM kb_kbentry_categories e JOIN kb_kbcategory c ON (e.kbcategory_id=c.id)"):
+            category=category[:49]
             if db.execute("SELECT COUNT(*) FROM tagging_tag WHERE name=%s",[category])[0][0]==0:
                 db.execute("INSERT INTO tagging_tag(name) VALUES(%s)",[category])
             tag_id=db.execute("SELECT id FROM tagging_tag WHERE name=%s",[category])[0][0]
