de84b34e65060eca597db7824ce10e080eb50eba
Dmitry Volodin <dvolodin7@gmail.com>
2010-08-10 14:11:03 +0400

    IEvent interface

---
+++ sa/interfaces/ievent.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## IEvent interface
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2010 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from base import *
+
+class IEvent(Interface):
+    task=InstanceOfParameter("Event")
