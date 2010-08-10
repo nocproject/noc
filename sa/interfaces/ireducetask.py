1fef432964ef3f72ca541585e6a070c5d0a21095
Dmitry Volodin <dvolodin7@gmail.com>
2010-08-10 11:56:59 +0400

    IReduceTask interface

---
+++ sa/interfaces/ireducetask.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## IReduceTask interface
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2010 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from base import *
+
+class IReduceTask(Interface):
+    task=InstanceOfParameter("ReduceTask")
