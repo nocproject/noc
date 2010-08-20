dd680b3a1e53549e7f5a6a447998c86e0be264b2
Dmitry Volodin <dvolodin7@gmail.com>
2010-08-20 11:48:56 +0400

    Compatibility with CBS31X0

--- sa/profiles/Cisco/IOS/get_mac_address_table.py
+++ sa/profiles/Cisco/IOS/get_mac_address_table.py
@@ -9,7 +9,7 @@ import noc.sa.script
 from noc.sa.interfaces import IGetMACAddressTable
 import re
 
-rx_line=re.compile(r"^(?:\*\s+)?(?P<vlan_id>\d+)\s+(?P<mac>\S+)\s+(?P<type>\S+)\s+\S+\s+(?:\S+\s+)?(?P<interfaces>.*)$")
+rx_line=re.compile(r"^(?:\*\s+)?(?P<vlan_id>\d+)\s+(?P<mac>\S+)\s+(?P<type>\S+)\s+(?:\S+\s+){0,2}(?P<interfaces>.*)$")
 
 class Script(noc.sa.script.Script):
     name="Cisco.IOS.get_mac_address_table"
@@ -24,13 +24,13 @@ class Script(noc.sa.script.Script):
             cmd+=" vlan %s"%vlan
         vlans=self.cli(cmd)
         r=[]
-        for l in vlans.split("\n"):
+        for l in vlans.splitlines():
             match=rx_line.match(l.strip())
             if match:
                 r.append({
                     "vlan_id"   : match.group("vlan_id"),
                     "mac"       : match.group("mac"),
                     "interfaces": [match.group("interfaces")],
-                    "type"      : {"dynamic":"D","static":"S"}[match.group("type")],
+                    "type"      : {"dynamic":"D","static":"S"}[match.group("type").lower()],
                 })
         return r
