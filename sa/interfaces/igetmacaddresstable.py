cd9c2dc499b0f16049dcc9b4ef98287f713f710d
Dmitry Volodin <dvolodin7@gmail.com>
2010-08-26 17:02:00 +0400

    Use InterfaceNameParameter in interface names

--- sa/interfaces/igetmacaddresstable.py
+++ sa/interfaces/igetmacaddresstable.py
@@ -8,12 +8,12 @@
 from base import *
 
 class IGetMACAddressTable(Interface):
-    interface=REStringParameter(r"^\S+$",required=False)
+    interface=InterfaceNameParameter(required=False)
     vlan=VLANIDParameter(required=False)
     mac=MACAddressParameter(required=False)
     returns=ListOfParameter(element=DictParameter(attrs={"vlan_id"    : VLANIDParameter(),
                                                          "mac"        : MACAddressParameter(),
-                                                         "interfaces" : StringListParameter(),
+                                                         "interfaces" : ListOfParameter(element=InterfaceNameParameter()),
                                                          "type"       : StringParameter(), # choices=["D","S"]
                                                          }))
     
