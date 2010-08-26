b96d4c12a423391d54c7105372fd424eadc4a4ec
Dmitry Volodin <dvolodin7@gmail.com>
2010-08-26 16:59:03 +0400

    xml_to_table() and list_to_ranges()

--- lib/text.py
+++ lib/text.py
@@ -22,6 +22,12 @@ import re
 rx_header_start=re.compile(r"^\s*[-=]+\s+[-=]+")
 rx_col=re.compile(r"^(\s*)([\-]+|[=]+)")
 def parse_table(s):
+    """
+    >>> parse_table("First Second Third\\n----- ------ -----\\na     b       c\\nddd   eee     fff\\n")
+    [['a', 'b', 'c'], ['ddd', 'eee', 'fff']]
+    >>> parse_table("First Second Third\\n----- ------ -----\\na             c\\nddd   eee     fff\\n")
+    [['a', '', 'c'], ['ddd', 'eee', 'fff']]
+    """
     columns=None
     r=[]
     columns=[]
@@ -51,3 +57,64 @@ def strip_html_tags(s):
     for k,v in [("&nbsp;"," "),("&lt;","<"),("&gt;",">"),("&amp;","&")]:
         t=t.replace(k,v)
     return t
+
+##
+## Convert XML to list of elements
+##
+def xml_to_table(s,root,row):
+    """
+    >>> xml_to_table('<?xml version="1.0" encoding="UTF-8" ?><response><action><row><a>1</a><b>2</b></row><row><a>3</a><b>4</b></row></action></response>','action','row')
+    [{'a': '1', 'b': '2'}, {'a': '3', 'b': '4'}]
+    """
+    # Detect root element
+    match=re.search(r"<%s>(.*)</%s>"%(root,root),s,re.DOTALL|re.IGNORECASE)
+    if not match:
+        return []
+    s=match.group(1)
+    row_re=re.compile(r"<%s>(.*?)</%s>"%(row,row),re.DOTALL|re.IGNORECASE)
+    item_re=re.compile(r"<([^\]+])>(.*?)</\1>",re.DOTALL|re.IGNORECASE)
+    r=[]
+    for m in [x for x in row_re.split(s) if x]:
+        data=item_re.findall(m)
+        if data:
+            r+=[dict(data)]
+    return r
+##
+## Convert list of values to string of ranges
+##
+def list_to_ranges(s):
+    """
+    >>> list_to_ranges([])
+    ''
+    >>> list_to_ranges([1])
+    '1'
+    >>> list_to_ranges([1,2])
+    '1-2'
+    >>> list_to_ranges([1,2,3])
+    '1-3'
+    >>> list_to_ranges([1,2,3,5])
+    '1-3,5'
+    >>> list_to_ranges([1,2,3,5,6,7])
+    '1-3,5-7'
+    >>> list_to_ranges(range(1,4001))
+    '1-4000'
+    """
+    def f():
+        if last_start==last_end:
+            return str(last_start)
+        else:
+            return "%d-%d"%(last_start,last_end)
+    last_start=None
+    last_end=None
+    r=[]
+    for i in sorted(s):
+        if last_end is not None and i==last_end+1:
+            last_end+=1
+        else:
+            if last_start is not None:
+                r+=[f()]
+            last_start=i
+            last_end=i
+    if last_start is not None:
+        r+=[f()]
+    return ",".join(r)
