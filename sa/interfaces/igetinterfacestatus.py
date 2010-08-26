582a36e79d93cf12a194300fad6d792b34aceeb8
Dmitry Volodin <dvolodin7@gmail.com>
2010-08-26 17:01:27 +0400

    IGetInterfaceStatus

---
+++ sa/interfaces/igetinterfacestatus.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## IGetInterfaceStatus
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from base import *
+##
+## Returns a list of interfaces status (up/down),
+## includint port-channels and SVIs
+##
+class IGetInterfaceStatus(Interface):
+    interface=InterfaceNameParameter(required=False)
+    returns=ListOfParameter(element=DictParameter(attrs={
+        "interface" : InterfaceNameParameter(),
+        "status"    : BooleanParameter(),
+    }))
