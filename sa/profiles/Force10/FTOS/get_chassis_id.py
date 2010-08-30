9895b8cfc64f0a37e722b779c4d16c489effaa5f
Dmitry Volodin <dvolodin7@gmail.com>
2010-08-30 13:00:12 +0400

    Compatibility with S-series

--- sa/profiles/Force10/FTOS/get_chassis_id.py
+++ sa/profiles/Force10/FTOS/get_chassis_id.py
@@ -11,12 +11,20 @@ import noc.sa.script
 from noc.sa.interfaces import IGetChassisID
 import re
 
-rx_id=re.compile(r"Chassis MAC\s+:\s*(?P<id>\S+)",re.IGNORECASE|re.MULTILINE)
+rx_chassis_id=re.compile(r"Chassis MAC\s+:\s*(?P<id>\S+)",re.IGNORECASE|re.MULTILINE)
+rx_system_id=re.compile(r"Stack MAC\s+:\s*(?P<id>\S+)",re.IGNORECASE|re.MULTILINE)
 
 class Script(noc.sa.script.Script):
     name="Force10.FTOS.get_chassis_id"
     implements=[IGetChassisID]
     def execute(self):
-        v=self.cli("show chassis brief")
-        match=rx_id.search(v)
+        platform=self.scripts.get_version()["platform"]
+        if platform[0]=="S":
+            # S-series
+            v=self.cli("show system brief")
+            match=rx_system_id.search(v)
+        else:
+            # E/C-series
+            v=self.cli("show chassis brief")
+            match=rx_chassis_id.search(v)
         return match.group("id")
