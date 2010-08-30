977a58acc473be31d3a6ecfb653025d96dc41c55
Dmitry Volodin <dvolodin7@gmail.com>
2010-08-30 13:00:55 +0400

    Get LLDP topology data

--- sa/profiles/Generic/get_topology_data.py
+++ sa/profiles/Generic/get_topology_data.py
@@ -16,16 +16,26 @@ class Script(noc.sa.script.Script):
     name="Generic.get_topology_data"
     implements=[IGetTopologyData]
     requires=[]
-    def execute(self,get_mac=False):
+    def execute(self,get_mac=False,get_lldp=False):
         print get_mac
         data={
-            "has_mac": False,
-            "mac"    : {},
+            "has_mac" : False,
+            "mac"     : [],
+            "has_lldp": False,
+            "lldp_neighbors" : []
         }
         if get_mac and self.scripts.has_script("get_mac_address_table"):
             mac_data=self.scripts.get_mac_address_table()
             if mac_data:
                 data["has_mac"]=True
                 data["mac"]=mac_data
+        if get_lldp:
+            if self.scripts.has_script("get_chassis_id"):
+                data["chassis_id"]=self.scripts.get_chassis_id()
+            if self.scripts.has_script("get_lldp_neighbors"):
+                lldp_neighbors=self.scripts.get_lldp_neighbors()
+                if lldp_neighbors:
+                    data["has_lldp"]=True
+                    data["lldp_neighbors"]=lldp_neighbors
         return data
 
