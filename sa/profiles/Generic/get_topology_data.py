a17a289dbeb182355b844ecf1c3a94a462c5dde0
Dmitry Volodin <dvolodin7@gmail.com>
2010-08-31 08:39:31 +0400

    HP.ProCurve.get_portchannel

--- sa/profiles/Generic/get_topology_data.py
+++ sa/profiles/Generic/get_topology_data.py
@@ -22,13 +22,16 @@ class Script(noc.sa.script.Script):
             "has_mac" : False,
             "mac"     : [],
             "has_lldp": False,
-            "lldp_neighbors" : []
+            "lldp_neighbors" : [],
+            "portchannels"   : [],
         }
+        # get mac addresses
         if get_mac and self.scripts.has_script("get_mac_address_table"):
             mac_data=self.scripts.get_mac_address_table()
             if mac_data:
                 data["has_mac"]=True
                 data["mac"]=mac_data
+        # get lldp neighbors
         if get_lldp:
             if self.scripts.has_script("get_chassis_id"):
                 data["chassis_id"]=self.scripts.get_chassis_id()
@@ -37,5 +40,8 @@ class Script(noc.sa.script.Script):
                 if lldp_neighbors:
                     data["has_lldp"]=True
                     data["lldp_neighbors"]=lldp_neighbors
+        # get portchannels
+        if self.scripts.has_script("get_portchannel"):
+            data["portchannels"]=self.scripts.get_portchannel()
         return data
 
