a32e2294b98d21bc1d3245a1dc03d47fbf0b6d74
Dmitry Volodin <dvolodin7@gmail.com>
2010-09-04 13:13:07 +0400

    Do not load modules starting with "."

--- sa/interfaces/__init__.py
+++ sa/interfaces/__init__.py
@@ -9,7 +9,7 @@ from noc.sa.interfaces.base import *
 import os,inspect,sys
 # Interface autoloader
 im=sys.modules["noc.sa.interfaces"]
-for f in [f for f in os.listdir(__path__[0]) if f.endswith(".py") and f not in ["__init__.py","base.py"]]:
+for f in [f for f in os.listdir(__path__[0]) if not f.startswith(".") and f.endswith(".py") and f not in ["__init__.py","base.py"]]:
     m=__import__("noc.sa.interfaces."+f[:-3],{},{},"*")
     for cn in dir(m):
         c=getattr(m,cn)
