462b4bbf3b98d8f7ec1643a479f9d000d938e6c9
Dmitry Volodin <dvolodin7@gmail.com>
2010-09-09 17:03:38 +0400

    Generic.Host profile

---
+++ sa/profiles/Generic/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Generic.Host
+## Dummb profile to allow managed object creating
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+import noc.sa.profiles
+from noc.sa.protocols.sae_pb2 import TELNET,SSH
+
+class Profile(noc.sa.profiles.Profile):
+    name="Generic.Host"
+    supported_schemes=[TELNET,SSH]
