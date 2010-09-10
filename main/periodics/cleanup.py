e3636f953ddb4bffcbd0b234c139105019dea89d
Dmitry Volodin <dvolodin7@gmail.com>
2010-09-10 18:33:13 +0400

    main.cleanup_session is renamed to main.cleanup. Hanging tags cleanup

--- main/periodics/cleanup.py
+++ main/periodics/cleanup.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2009 The NOC Project
+## Obsolete data cleanup and system maintainance
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2010 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 """
@@ -9,14 +11,22 @@ import noc.sa.periodic
 from noc.settings import config
 import os,subprocess,datetime
 import logging
+from tagging.models import TaggedItem
 
 class Task(noc.sa.periodic.Task):
-    name="main.cleanup_sessions"
-    description=""
+    name="main.cleanup"
+    description="Obsolete data cleanup and system maintainance"
     
     def execute(self):
         from django.db import connection
         cursor=connection.cursor()
+        # Delete expired sessions
         cursor.execute("DELETE FROM django_session WHERE expire_date<'now'")
+        cursor.execute("COMMIT")
+        # Find and delete hangling tags
+        for i in TaggedItem.objects.all():
+            if i.object is None:
+                logging.info("Delete hanging tag reference: %s"%str(i))
+                i.delete()
         cursor.execute("COMMIT")
         return True
