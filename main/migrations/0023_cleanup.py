840f43d05e75798b94208cad5a6c68ca03dca680
Dmitry Volodin <dvolodin7@gmail.com>
2010-09-10 18:33:13 +0400

    main.cleanup_session is renamed to main.cleanup. Hanging tags cleanup

---
+++ main/migrations/0023_cleanup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from south.db import db
+from noc.main.models import *
+
+class Migration:
+    
+    def forwards(self):
+        db.execute("UPDATE sa_taskschedule SET periodic_name='main.cleanup' WHERE periodic_name='main.cleanup_sessions'")
+
+    def backwards(self):
+        db.execute("UPDATE sa_taskschedule SET periodic_name='main.cleanup_sessions' WHERE periodic_name='main.cleanup'")
