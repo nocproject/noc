508916e8c7c5871966003377c16b5b51a3a9b3e1
Dmitry Volodin <dvolodin7@gmail.com>
2010-09-13 10:57:57 +0400

    Updated for better compatibility

--- fm/rules/classification/DLink/DES3xxx/lbd.py
+++ fm/rules/classification/DLink/DES3xxx/lbd.py
@@ -8,27 +8,26 @@
 from noc.fm.rules.classes.lbd import *
 from noc.fm.rules.classification import ClassificationRule,Expression,CLOSE_EVENT,DROP_EVENT
 ##
-## DLink.DES3xxx Loop Recovered SYSLOG
+## DLink.DES3xxx LBD Loop Recovered SYSLOG
 ##
-class DLink_DES3xxx_Loop_Recovered_SYSLOG_Rule(ClassificationRule):
+class DLink_DES3xxx_LBD_Loop_Recovered_SYSLOG_Rule(ClassificationRule):
     name="DLink.DES3xxx LBD Loop Recovered SYSLOG" 
     event_class=LBDLoopRecovered
     preference=1000
     patterns=[
-        (r"^source$" , r"^syslog$"),
-        (r"^profile$", r"^DLink\.D[EG]S3xxx$"),
-        (r"^message$", r"Port (?P<interface>\d+) [VID ]*(?P<vlan>\d+)\s*LBD recovered"),
+        (r"^source$",r"^syslog$"),
+        (r"^profile$",r"^DLink\.D[EG]S3xxx$"),
+        (r"^message$",r"Port\s(?P<interface>\d+)\s+(?:VID\s+(?P<vlan>\d+)\s+)?LBD\s(?:port\s+)?recovered"),
     ]
-
 ##
-## DLink.DES3xxx Loop Detected SYSLOG
+## DLink.DES3xxx LBD Loop Detected SYSLOG
 ##
-class DLink_DES3xxx_Loop_Detected_SYSLOG_Rule(ClassificationRule):
+class DLink_DES3xxx_LBD_Loop_Detected_SYSLOG_Rule(ClassificationRule):
     name="DLink.DES3xxx LBD Loop Detected SYSLOG" 
     event_class=LBDLoopDetected
     preference=1000
     patterns=[
-        (r"^source$" , r"^syslog$"),
-        (r"^profile$", r"^DLink\.D[EG]S3xxx$"),
-        (r"^message$", r"Port (?P<interface>\d+) [VID ]*(?P<vlan>\d+)\s*LBD loop occurred"),
+        (r"^message$",r"Port\s(?P<interface>\d+)\s+(?:VID\s+(?P<vlan>\d+)\s+)?LBD\sloop\soccurred"),
+        (r"^source$",r"^syslog$"),
+        (r"^profile$",r"^DLink\.D[EG]S3xxx$"),
     ]
