17136e01231e459c43896478b3fa12b00b16764e
Dmitry Volodin <dvolodin7@gmail.com>
2010-09-16 13:04:27 +0400

    Use profile's pager disabling capabilities

--- sa/profiles/Juniper/JUNOS/get_config.py
+++ sa/profiles/Juniper/JUNOS/get_config.py
@@ -12,6 +12,5 @@ class Script(noc.sa.script.Script):
     name="Juniper.JUNOS.get_config"
     implements=[IGetConfig]
     def execute(self):
-        self.cli("set cli screen-length 0")
         config=self.cli("show configuration")
         return self.cleaned_config(config)
