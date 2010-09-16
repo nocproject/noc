55490631cea0dcaf316e23540e55577b2bd7a36f
Dmitry Volodin <dvolodin7@gmail.com>
2010-09-16 12:54:54 +0400

    HP.GbE2.get_arp and HP.ProCurve.get_arp

---
+++ sa/profiles/HP/GbE2/get_arp.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## HP.GbE2.get_arp
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+import noc.sa.script
+from noc.sa.interfaces import IGetARP
+import re
+
+rx_line=re.compile(r"^\s*(?P<ip>\d+\.\d+\.\d+\.\d+).+?(?P<mac>[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2})\s+\d+\s+(?P<interface>\S+)")
+
+class Script(noc.sa.script.Script):
+    name="HP.GbE2.get_arp"
+    implements=[IGetARP]
+    def execute(self):
+        r=self.cli("/i/l3/arp/dump",list_re=rx_line)
+        self.cli("/")
+        return r
---
+++ sa/profiles/HP/ProCurve/get_arp.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## HP.ProCurve.get_arp
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+import noc.sa.script
+from noc.sa.interfaces import IGetARP
+import re
+
+rx_line=re.compile(r"^\s*(?P<ip>\d+\.\d+\.\d+\.\d+)\s+(?P<mac>[0-9a-f]{6}-[0-9a-f]{6})\s+(?:dynamic|static)\s+(?P<interface>\S+)")
+
+class Script(noc.sa.script.Script):
+    name="HP.ProCurve.get_arp"
+    implements=[IGetARP]
+    def execute(self):
+        return self.cli("show arp",list_re=rx_line)
