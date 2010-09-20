59e6df065bcfadb7cb56ab2865d7e3f976bd5a13
Dmitry Volodin <dvolodin7@gmail.com>
2010-09-20 13:08:34 +0400

    DLink.{DES,DGS}3xxx.{add,remove}_vlan

---
+++ sa/profiles/DLink/DGS3xxx/remove_vlan.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## DLink.DGS3xxx.remove_vlan
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2010 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from __future__ import with_statement
+import noc.sa.script
+from noc.sa.interfaces import IRemoveVlan
+
+class Script(noc.sa.script.Script):
+    name="DLink.DGS3xxx.remove_vlan"
+    implements=[IRemoveVlan]
+    def execute(self,vlan_id):
+        for v in  self.scripts.get_vlans():
+            if v["vlan_id"]==vlan_id:
+                with self.configure():
+                    self.cli("delete vlan %s"%v["name"])
+                self.save_config()
+                return True
+        return False
