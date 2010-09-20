dce70628558a5bb73e363ad4d41760e4dfe92afd
Dmitry Volodin <dvolodin7@gmail.com>
2010-09-20 13:08:34 +0400

    DLink.{DES,DGS}3xxx.{add,remove}_vlan

---
+++ sa/profiles/DLink/DGS3xxx/add_vlan.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## DLink.DGS3xxx.add_vlan
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2010 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from __future__ import with_statement
+import noc.sa.script
+from noc.sa.interfaces import IAddVlan
+
+class Script(noc.sa.script.Script):
+    name="DLink.DGS3xxx.add_vlan"
+    implements=[IAddVlan]
+    def execute(self,vlan_id,name,tagged_ports):
+        with self.configure():
+            self.cli("create vlan %s tag %d"%(name,vlan_id))
+            if tagged_ports:
+                for port in tagged_ports:
+                    self.cli("config vlan %s add tagged %s"%(name,port))
+        self.save_config()
+        return True
