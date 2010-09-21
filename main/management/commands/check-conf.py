f7b056b9a07bc0d1d6db3066fe05ded9592ca42d
Dmitry Volodin <dvolodin7@gmail.com>
2010-09-21 18:05:05 +0400

    Check config file permissions

--- main/management/commands/check-conf.py
+++ main/management/commands/check-conf.py
@@ -14,8 +14,13 @@ import os,stat
 class Command(BaseCommand):
     help="Check configuration files"
     def handle(self, *args, **options):
-        # Check executables
         failed=[]
+        # Check config permissions
+        for c in ["noc.conf","noc-sae.conf","noc-activator.conf","noc-classifier.conf","noc-correlator.conf","noc-notifier.conf"]:
+            path=os.path.join("etc",c)
+            if os.path.isfile(path) and not os.access(path,os.R_OK):
+                failed+=["%s: permission denied"%path]
+        # Check executables
         for f in ["telnet","ssh","pg_dump","tar","gzip","smidump","smilint"]:
             path=config.get("path",f)
             if not os.path.isfile(path):
@@ -23,4 +28,4 @@ class Command(BaseCommand):
             elif os.stat(path)[stat.ST_MODE]&stat.S_IXUSR==0:
                 failed+=["%s: %s is not executeble"%(f,path)]
         if failed:
-            raise CommandError("\n".join(failed))
+            raise CommandError("\n\t"+"\n\t".join(failed))
