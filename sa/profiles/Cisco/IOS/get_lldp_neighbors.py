ab5a56dd8e49819d7989276d25d196817b16a33b
Dmitry Volodin <dvolodin7@gmail.com>
2010-09-21 16:10:37 +0400

    Detect and properly handle macAddress(3) and networkAddress(4) port id subtypes

--- sa/profiles/Cisco/IOS/get_lldp_neighbors.py
+++ sa/profiles/Cisco/IOS/get_lldp_neighbors.py
@@ -9,13 +9,15 @@
 """
 import noc.sa.script
 from noc.sa.interfaces import IGetLLDPNeighbors
+from noc.sa.interfaces.base import MACAddressParameter
+from noc.lib.validators import is_int,is_ipv4
 import re
 
 rx_summary_split=re.compile(r"^Device ID.+?\n",re.MULTILINE|re.IGNORECASE)
 rx_s_line=re.compile(r"^\S+\s+(?P<local_if>\S+)\s+\d+\s+(?P<capability>\S*)\s+(?P<remote_if>.+)$")
 rx_chassis_id=re.compile(r"^Chassis id:\s*(?P<id>\S+)",re.MULTILINE|re.IGNORECASE)
 rx_system=re.compile(r"^System Name:\s*(?P<name>\S+)",re.MULTILINE|re.IGNORECASE)
-
+rx_mac=re.compile(r"^[0-9a-f]{4}\.[0-9a-f]{4}\.[0-9a-f]{4}$")
 
 class Script(noc.sa.script.Script):
     name="Cisco.IOS.get_lldp_neighbors"
@@ -34,7 +36,20 @@ class Script(noc.sa.script.Script):
             local_if=match.group("local_if")
             i={"local_interface":local_if, "neighbors": []}
             # Build neighbor data
-            n={"remote_port":match.group("remote_if"),"remote_port_subtype":5,"remote_chassis_id_subtype":4}
+            remote_port=match.group("remote_if")
+            remote_port_subtype=5
+            if rx_mac.match(remote_port):
+                # Actually macAddress(3)
+                # Convert MAC to common form
+                remote_port=MACAddressParameter().clean(remote_port)
+                remote_port_subtype=3
+            elif is_ipv4(remote_port):
+                # Actually networkAddress(4)
+                remote_port_subtype=4
+            elif is_int(remote_port):
+                # Actually local(7)
+                remote_port_subtype=7
+            n={"remote_port":remote_port,"remote_port_subtype":remote_port_subtype,"remote_chassis_id_subtype":4}
             # Get capability
             cap=0
             for c in match.group("capability").split(","):
