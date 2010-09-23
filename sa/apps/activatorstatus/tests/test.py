3d108ba2b8cd0266387f294b348090aa5fd2c1f7
Dmitry Volodin <dvolodin7@gmail.com>
2010-09-23 14:04:04 +0400

    sa.activatorstatus application

---
+++ sa/apps/activatorstatus/tests/test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## activatorstatus Test
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+from noc.lib.test import ApplicationTestCase
+
+class activatorstatusTestCase(ApplicationTestCase):
+    pass
