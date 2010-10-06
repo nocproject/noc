1f3d1af72f956fd4f217f771c55a7cdfa92c5338
Dmitry Volodin <dvolodin7@gmail.com>
2010-10-06 12:01:20 +0400

    Zyxel.ZyNOS.get_chassis_id

---
+++ sa/profiles/Zyxel/ZyNOS/get_chassis_id.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Zyxel.ZyNOS.get_chassis_id
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2010 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+import noc.sa.script
+from noc.sa.interfaces import IGetChassisID
+import re
+
+rx_chassis_id=re.compile(r"Ethernet Address\s+:\s*(?P<id>\S+)",re.IGNORECASE|re.MULTILINE)
+
+class Script(noc.sa.script.Script):
+    name="Zyxel.ZyNOS.get_chassis_id"
+    implements=[IGetChassisID]
+    def execute(self):
+        v=self.cli("show system-information")
+        match=rx_chassis_id.search(v)
+        return match.group("id")
