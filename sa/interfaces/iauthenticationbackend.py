36aadd4fb80c2805222b05c621bed2605875c9da
Dmitry Volodin <dvolodin7@gmail.com>
2010-10-07 01:09:30 +0400

    IAuthenticationBackend interface

---
+++ sa/interfaces/iauthenticationbackend.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## IAuthenticationBackend interface
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2010 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from base import *
+
+class IAuthenticationBackend(Interface):
+    returns=InstanceOfParameter("User",required=False)
