4afeadd577ac9b11bcc9d3cd31958185a99264b9
Dmitry Volodin <dvolodin7@gmail.com>
2010-10-18 16:21:33 +0400

    IGetCDPNeighbors

---
+++ sa/interfaces/igetcdpneighbors.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## IGetCDPNeighbors
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from base import *
+##
+class IGetCDPNeighbors(Interface):
+    returns=DictParameter(attrs={
+        # Local device id
+        "device_id" : StringParameter(),
+        "neighbors" : ListOfParameter(element=DictParameter(attrs={
+            # Remote device id
+            "device_id"       : StringParameter(),
+            # Local interface
+            "local_interface" : InterfaceNameParameter(),
+            # Remote interface
+            "remote_interface": StringParameter(),
+        }))
+    })
