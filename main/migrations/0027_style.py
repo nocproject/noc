f0a5d075048505c5d817aa767cb9b65f2c483d2c
Dmitry Volodin <dvolodin7@gmail.com>
2010-11-01 10:56:28 +0300

    main_style table

---
+++ main/migrations/0027_style.py
@@ -0,0 +1,25 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+from noc.main.models import *
+
+class Migration(SchemaMigration):
+
+    def forwards(self):
+        # Model 'Color'
+        db.create_table('main_style', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('name', models.CharField("Name",max_length=64,unique=True)),
+            ('font_color', ColorField("Font Color",default=0x000000)),
+            ('background_color', ColorField("Background Color",default=0xffffff)),
+            ('bold', models.BooleanField('Bold', default=False)),
+            ('italic', models.BooleanField('Italic', default=False)),
+            ('underlined', models.BooleanField('Underlined', default=False)),
+            ('is_active', models.BooleanField("Is Active",default=True)),
+            ('description', models.TextField("Description",null=True,blank=True)),
+        ))
+
+    def backwards(self):
+        db.delete_table("main_style")
