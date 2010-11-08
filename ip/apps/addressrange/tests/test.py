3fb6b3ea10447daf8e93e56d41b9bac4b9fb4c08
Dmitry Volodin <dvolodin7@gmail.com>
2010-11-08 10:44:02 +0300

    ip.addressrange application replaces ip.ipv4addressrange

--- ip/apps/addressrange/tests/test.py
+++ ip/apps/addressrange/tests/test.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
-## ipv4addressrange Test
+## addressrange Test
 ##----------------------------------------------------------------------
 ## Copyright (C) 2007-2009 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 from noc.lib.test import ModelApplicationTestCase
-from django.utils import simplejson as json
 
-class ipv4addressrangeTestCase(ModelApplicationTestCase):
+class addressrangeTestCase(ModelApplicationTestCase):
     pass
