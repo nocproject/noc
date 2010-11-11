3a3d61f555bad04f7c47eff2c058ca68f23a4419
Dmitry Volodin <dvolodin7@gmail.com>
2010-11-11 15:19:50 +0300

    Vyatta.Vyatta profile

---
+++ sa/profiles/Vyatta/Vyatta/get_version.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Vyatta.Vyatta.get_version
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2010 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+import noc.sa.script
+from noc.sa.interfaces import IGetVersion
+import re
+
+rx_ver=re.compile(r"^Version:\s+(?P<version>\S+)",re.MULTILINE)
+
+class Script(noc.sa.script.Script):
+    name="Vyatta.Vyatta.get_version"
+    implements=[IGetVersion]
+    def execute(self):
+        v=self.cli("show version")
+        match=rx_ver.search(v)
+        return {
+            "vendor"    : "Vyatta",
+            "platform"  : "VC",
+            "version"   : match.group("version"),
+        }
---
+++ share/docs/en/nocbook/profiles/Vyatta/Vyatta/index.rst
@@ -0,0 +1,15 @@
+.. _Vyatta.Vyatta:
+
+Vyatta.Vyatta
+=============
+
+====== ========================================
+Vendor `Vyatta <http://www.vyatta.com/>`_
+OS     Vyatta
+====== ========================================
+
+Vyatta.Vyatta profile supports Vyatta-based family of hardware and software routers
+
+Tested Equipment
+----------------
+.. supported:: Vyatta.Vyatta
