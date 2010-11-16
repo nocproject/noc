9d8f6280e676775cd42b5c61e485b8e1025656eb
Dmitry Volodin <dvolodin7@gmail.com>
2010-11-17 00:21:28 +0300

    Add main:calculator:view permission

--- main/apps/calculator/views.py
+++ main/apps/calculator/views.py
@@ -5,7 +5,7 @@
 ## Copyright (C) 2007-2010 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-from noc.lib.app import Application,Permit
+from noc.lib.app import Application, HasPerm, view
 from noc.main.apps.calculator.calculators import calculator_registry
 ##
 ## Register all calculators
@@ -18,23 +18,25 @@ class CalculatorAppplication(Application):
     ##
     ## Calculator index
     ##
+    @view(  url=r"^$",
+            url_name="index",
+            menu="Calculators",
+            access=HasPerm("view"))
     def view_index(self,request):
         r=[(cn,c.title) for cn,c in calculator_registry.classes.items()]
         r=sorted(r,lambda x,y: cmp(x[1],y[1]))
         return self.render(request,"index.html",{"calculators":r})
-    view_index.url=r"^$"
-    view_index.url_name="index"
-    view_index.menu="Calculators"
-    view_index.access=Permit()
+    
     ##
     ## Calculator view
     ##
+    @view(  url=r"^(?P<calculator>\S+)/$",
+            url_name="calculate",
+            access=HasPerm("view"))
     def view_calculate(self,request,calculator):
         try:
             c=calculator_registry[calculator](self)
         except KeyError:
             return self.response_not_found("No calculator found")
         return c.render(request)
-    view_calculate.url=r"^(?P<calculator>\S+)/$"
-    view_calculate.url_name="calculate"
-    view_calculate.access=Permit()
+    
