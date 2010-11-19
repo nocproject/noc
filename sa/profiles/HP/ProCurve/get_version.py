0c0dae49a3d88cc4b2c2b0541372bbdb565ceeda
Dmitry Volodin <dvolodin7@gmail.com>
2010-11-19 18:26:30 +0300

    Use walkMIB instead of MOTD analysys

--- sa/profiles/HP/ProCurve/get_version.py
+++ sa/profiles/HP/ProCurve/get_version.py
@@ -11,35 +11,23 @@ import noc.sa.script
 from noc.sa.interfaces import IGetVersion
 import re
 
-rx_ver=re.compile(r"^Image stamp:\s+\S+\n[^\n]+?\n\s*(?P<version>\S+)\n\s+\S+\nBoot Image:\s+\S+\n$",re.MULTILINE|re.DOTALL)
-rx_snmp_ver=re.compile(r"^ProCurve\s+\S+\s+(?P<platform>\S+).*?,\s+revision\s+(?P<version>\S+),.+$")
-rx_motd=re.compile(r"ProCurve\s+\S+\s+(?P<platform>\S+)",re.IGNORECASE|re.MULTILINE)
+rx_ver=re.compile(r"ProCurve\s+\S+\s+(Switch\s+)?(?P<platform>\S+).*?,\s*revision\s+(?P<version>\S+),",re.IGNORECASE)
 
 class Script(noc.sa.script.Script):
     name="HP.ProCurve.get_version"
     implements=[IGetVersion]
     def execute(self):
+        v=None
         if self.snmp and self.access_profile.snmp_ro:
             try:
                 v=self.snmp.get("1.3.6.1.2.1.1.1.0") # sysDescr.0
-                match=rx_snmp_ver.search(v)
-                return {
-                    "vendor"    : "HP",
-                    "platform"  : match.group("platform"),
-                    "version"   : match.group("version"),
-                }
             except self.snmp.TimeOutError:
                 pass
-        # ProCurve does not returns platform via CLI,
-        # While still displays it in message of the day
-        # Try to fetch platform
-        match=rx_motd.search(self.motd)
-        platform=match.group("platform") if match else "ProCurve"
-        # Get version
-        v=self.cli("show version")
-        match=rx_ver.search(v)
+        if not v:
+            v=self.cli("walkMIB sysDescr")
+        match=rx_ver.search(v.strip())
         return {
             "vendor"    : "HP",
-            "platform"  : platform,
+            "platform"  : match.group("platform"),
             "version"   : match.group("version"),
         }
