4922542d608d58af2f39479055cfa6dfa1bbb961
Dmitry Volodin <dvolodin7@gmail.com>
2010-11-21 20:54:04 +0300

    CDP neighbors information added

--- sa/interfaces/igettopologydata.py
+++ sa/interfaces/igettopologydata.py
@@ -11,6 +11,7 @@ from base import *
 from igetmacaddresstable import IGetMACAddressTable
 from igetarp import IGetARP
 from igetlldpneighbors import IGetLLDPNeighbors
+from igetcdpneighbors import IGetCDPNeighbors
 from igetspanningtree import IGetSpanningTree
 from igetportchannel import IGetPortchannel
 
@@ -18,6 +19,7 @@ class IGetTopologyData(Interface):
     get_mac=BooleanParameter(required=False,default=False)  # Get MAC address table
     get_arp=BooleanParameter(required=False,default=False)  # Get ARP cache
     get_lldp=BooleanParameter(required=False,default=False) # Get LLDP neighbor information
+    get_cdp=BooleanParameter(required=False,default=False)  # Get CDP neighbor information
     get_stp=BooleanParameter(required=False,default=False)  # Get STP information
     
     returns=DictParameter(attrs={
@@ -25,23 +27,28 @@ class IGetTopologyData(Interface):
         "has_mac"    : BooleanParameter(required=False,default=False),
         # Mac address table.
         # Returned only if get_mac is True
-        "mac"        : IGetMACAddressTable.returns,
+        "mac"        : NoneParameter() | IGetMACAddressTable.returns,
         # Set to true if "arp" is not empy
         "has_arp"    : BooleanParameter(required=False,default=False),
         # ARP Cache
         # Returned only if get_arp is True
-        "arp"        : IGetARP.returns,
+        "arp"        : NoneParameter() | IGetARP.returns,
         # Set to true if "lldp_neighbors" is not empty
         "has_lldp"   : BooleanParameter(required=False,default=False),
         # Chassis ID, returned only if get_lldp is set
         "chassis_id" : MACAddressParameter(required=False),
         # LLDP neighbors
         # Returned only if get_lldp is set
-        "lldp_neighbors" : IGetLLDPNeighbors.returns,
+        "lldp_neighbors" : NoneParameter() | IGetLLDPNeighbors.returns,
+        # Set to true if "has_cdp" is not empty
+        "has_cdp"        : BooleanParameter(required=False,default=False),
+        # CDP neighbors
+        # Returned only if get_cdp is set
+        "cdp_neighbors"  : NoneParameter() | IGetCDPNeighbors.returns,
         # Set to true if "stp" is not empty
         "has_stp"    : BooleanParameter(required=False,default=False),
         # STP Protocol data
-        "stp"        : IGetSpanningTree.returns | NoneParameter(),
+        "stp"        : NoneParameter() | IGetSpanningTree.returns,
         # Portchannels
         "portchannels"   : IGetPortchannel.returns,
     })
