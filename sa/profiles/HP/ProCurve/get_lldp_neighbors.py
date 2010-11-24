9b57bb2f03cc8ec36a5532d92789d40fa9a3251b
Dmitry Volodin <dvolodin7@gmail.com>
2010-11-24 08:41:49 +0300

    Treat empty remote capabilities properly

--- sa/profiles/HP/ProCurve/get_lldp_neighbors.py
+++ sa/profiles/HP/ProCurve/get_lldp_neighbors.py
@@ -7,35 +7,41 @@
 ##----------------------------------------------------------------------
 """
 """
-import noc.sa.script
-from noc.sa.interfaces import IGetLLDPNeighbors
+## Python modules
 import re
+## NOC modules
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetLLDPNeighbors
+from noc.lib.text import parse_table
 
-rx_localport=re.compile(r"^\s*(.+?)\s*\|\s*local\s+(\d+)\s+.+?$",re.MULTILINE|re.DOTALL)
-rx_split=re.compile(r"^\s*----.+?\n",re.MULTILINE|re.DOTALL)
-rx_line=re.compile(r"^\s*(?P<port>\S+)\s*|",re.MULTILINE|re.DOTALL)
-rx_chassis_id=re.compile(r"^\s*ChassisId\s*:\s*(.{17})",re.MULTILINE|re.DOTALL|re.IGNORECASE)
-rx_port_id=re.compile(r"^\s*PortId\s*:\s*(.+?)\s*$",re.MULTILINE|re.DOTALL|re.IGNORECASE)
-rx_sys_name=re.compile(r"^\s*SysName\s*:\s*(.+?)\s*$",re.MULTILINE|re.DOTALL|re.IGNORECASE)
-rx_cap=re.compile(r"^\s*System Capabilities Enabled\s*:\s*(.*?)\s*$",re.MULTILINE|re.DOTALL|re.IGNORECASE)
-
-class Script(noc.sa.script.Script):
+##
+## HP.ProCurve.get_lldp_neighbors
+##
+class Script(NOCScript):
     name="HP.ProCurve.get_lldp_neighbors"
     implements=[IGetLLDPNeighbors]
+    
+    rx_localport=re.compile(r"^\s*(\S+)\s*\|\s*local\s+(\d+)\s+.+?$",re.MULTILINE)
+    rx_split=re.compile(r"^\s*----.+?\n",re.MULTILINE|re.DOTALL)
+    rx_line=re.compile(r"^\s*(?P<port>\S+)\s*|",re.MULTILINE|re.DOTALL)
+    rx_chassis_id=re.compile(r"^\s*ChassisId\s*:\s*(.{17})",re.MULTILINE|re.DOTALL|re.IGNORECASE)
+    rx_port_id=re.compile(r"^\s*PortId\s*:\s*(.+?)\s*$",re.MULTILINE|re.DOTALL|re.IGNORECASE)
+    rx_sys_name=re.compile(r"^\s*SysName\s*:\s*(.+?)\s*$",re.MULTILINE|re.DOTALL|re.IGNORECASE)
+    rx_cap=re.compile(r"^\s*System Capabilities Enabled\s*:\s*(.*?)\s*$",re.IGNORECASE)
     def execute(self):
         r=[]
         # HP.ProCurve advertises local(7) port sub-type, so local_interface_id parameter required
         # Collect data
         local_port_ids={} # name -> id
-        for port,local_id in rx_localport.findall(self.cli("show lldp info local-device")):
+        for port,local_id in self.rx_localport.findall(self.cli("show lldp info local-device")):
             local_port_ids[port]=int(local_id)
         # Get neighbors
         v=self.cli("show lldp info remote-device")
-        for l in rx_split.split(v)[1].splitlines():
+        for l in self.rx_split.split(v)[1].splitlines():
             l=l.strip()
             if not l:
                 continue
-            match=rx_line.search(l)
+            match=self.rx_line.search(l)
             if not match:
                 continue
             local_interface=match.group("port")
@@ -45,24 +51,24 @@ class Script(noc.sa.script.Script):
                 i["local_interface_id"]=local_port_ids[local_interface]
             v=self.cli("show lldp info remote-device %s"%local_interface)
             # Get chassis id
-            match=rx_chassis_id.search(v)
+            match=self.rx_chassis_id.search(v)
             if not match:
                 continue
             remote_chassis_id=match.group(1).replace(" ","")
             remote_chassis_id="%s-%s"%(remote_chassis_id[:6],remote_chassis_id[6:]) # Convert to HP-style mac
             n={"remote_chassis_id":remote_chassis_id,"remote_port_subtype":5,"remote_chassis_id_subtype":4}
             # Get remote port
-            match=rx_port_id.search(v)
+            match=self.rx_port_id.search(v)
             if not match:
                 continue
             n["remote_port"]=match.group(1)
             # Get remote system name
-            match=rx_sys_name.search(v)
+            match=self.rx_sys_name.search(v)
             if match:
                 n["remote_system_name"]=match.group(1)
             # Get capabilities
             caps=0
-            match=rx_cap.search(v)
+            match=self.rx_cap.search(v)
             if match:
                 for c in match.group(1).split(", "):
                     caps|={
@@ -79,3 +85,4 @@ class Script(noc.sa.script.Script):
             i["neighbors"]+=[n]
             r+=[i]
         return r
+    
