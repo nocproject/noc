e60deb76ca25c4591fcb90040a117f3ce4ab8764
Dmitry Volodin <dvolodin7@gmail.com>
2010-11-24 07:44:50 +0300

    cmp_version

--- sa/profiles/Force10/FTOS/__init__.py
+++ sa/profiles/Force10/FTOS/__init__.py
@@ -34,6 +34,33 @@ class Profile(noc.sa.profiles.Profile):
         p+="\n".join(["    permit %s%s"%(x,suffix) for x in pl])
         p+="\nexit\n"
         return p
+    ##
+    ## Compare versions.
+    ## Versions are in format
+    ## N1.N2[.N3[.N4[L]]]
+    ##
+    @classmethod
+    def cmp_version(cls,v1,v2):
+        if "a"<=v1[-1]<="z":
+            n1=[int(x) for x in v1[:-1].split(".")]+[v1[-1]]
+        else:
+            n1=[int(x) for x in v1.split(".")]
+        if "a"<=v2[-1]<="z":
+            n2=[int(x) for x in v2[:-1].split(".")]+[v2[-1]]
+        else:
+            n2=[int(x) for x in v2.split(".")]
+        l1=len(n1)
+        l2=len(n2)
+        if l1>l2:
+            n2+=[None]*(l1-l2)
+        elif l1<l2:
+            n1+=[None]*(l2-l1)
+        for c1,c2 in zip(n1,n2):
+            r=cmp(c1,c2)
+            if r!=0:
+                return r
+        return 0
+    
 
 ##
 ## Platform matching helpers
