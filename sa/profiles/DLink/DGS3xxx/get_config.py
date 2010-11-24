278a0c88d70d8d463324aed2dfe902c2a2e2728f
Dmitry Volodin <dvolodin7@gmail.com>
2010-11-24 14:13:22 +0300

    Raise exception on DGS-3024 and DGS-3048

--- sa/profiles/DLink/DGS3xxx/get_config.py
+++ sa/profiles/DLink/DGS3xxx/get_config.py
@@ -14,18 +14,25 @@ class Script(NOCScript):
     name="DLink.DGS3xxx.get_config"
     implements=[IGetConfig]
     ##
-    ## DGS-3612, DGS-3627, DGS-3650
+    ## DGS-3612, DGS-3612G, DGS-3627, DGS-3627G, DGS-3650
     ##
     @NOCScript.match(platform__regex=r"(3612|3627|3650)")
     def execute_config_active(self):
         return self.cleaned_config(self.cli("show config active"))
     
     ##
-    ## DGS-3100
+    ## DGS-3100-24, DGS-3100-24P, DGS-3100-24TG, DGS-3100-49, DGS-3100-48P
     ##
     @NOCScript.match(platform__contains="3100")
     def execute_configuration_running(self):
         return self.cleaned_config(self.cli("show configuration running"))
+    
+    ##
+    ## DGS-3024, DGS-3048
+    ##
+    @NOCScript.match(platform__regex=r"(3024|3048)")
+    def execute_not_supported(self):
+        raise self.NotSupportedError()
     
     ##
     ## Other
