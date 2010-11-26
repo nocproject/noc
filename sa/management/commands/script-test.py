007801e22ead5e2658865513606997b0da14ae6e
Dmitry Volodin <dvolodin7@gmail.com>
2010-11-26 07:45:10 +0300

    -u option (Show scripts without proper unittests)

--- sa/management/commands/script-test.py
+++ sa/management/commands/script-test.py
@@ -10,9 +10,14 @@
 from django.core.management.base import BaseCommand
 from noc.lib.test import ScriptTestCase
 import os,types,sys
+from optparse import OptionParser, make_option
 
 class Command(BaseCommand):
     help="Dump script tests data"
+    option_list=BaseCommand.option_list+(
+        make_option("-u","--untested",dest="untested",action="store_true"),
+    )
+    
     def get_tests(self,filter):
         for dirpath,dirnames,files in os.walk(os.path.join("sa","profiles")):
             parts=dirpath.split(os.sep)
@@ -25,6 +30,18 @@ class Command(BaseCommand):
                         if type(o)==types.TypeType and issubclass(o,ScriptTestCase) and o!=ScriptTestCase:
                             if len([x for x,y in zip(o.script.split("."),filter) if x==y or y is None])==3:
                                 yield o
+    
+    def get_scripts(self,filter):
+        for dirpath,dirnames,files in os.walk(os.path.join("sa","profiles")):
+            parts=dirpath.split(os.sep)
+            if parts[-1]!="tests":
+                for f in [f for f in files if f.endswith(".py") and not f.startswith(".") and f!="__init__.py"]:
+                    fp=os.path.join(dirpath,f)
+                    m=__import__("noc."+fp[:-2].replace(os.sep,"."),{},{},"*")
+                    if hasattr(m,"Script"):
+                        name=m.Script.name
+                        if len([x for x,y in zip(name.split("."),filter) if x==y or y is None])==3:
+                            yield name
     
     def display(self,tests):
         r={} # Profile -> script -> platform -> version
@@ -61,4 +78,17 @@ class Command(BaseCommand):
             if l<3:
                 filter+=[None]*(3-l)
             filter=[(x if x!="*" else None) for x in filter]
-        self.display(self.get_tests(filter))
+        if options["untested"]:
+            scripts=set(self.get_scripts(filter))
+            tested=set([t.script for t in self.get_tests(filter)])
+            lp=None
+            for s in sorted(scripts-tested):
+                v,o,s=s.split(".")
+                p="%s.%s"%(v,o)
+                if p!=lp:
+                    print p
+                    print "="*len(p)
+                    lp=p
+                print "    "+s
+        else:
+            self.display(self.get_tests(filter))
