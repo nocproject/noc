b9cd0530be2debd769e2eb9fe1568ddc5c464709
Dmitry Volodin <dvolodin7@gmail.com>
2010-11-30 14:15:33 +0300

    EdgeCore.ES.get_vlans

---
+++ sa/profiles/EdgeCore/ES/get_vlans.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## EdgeCore.ES.get_vlans
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2010 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+## Python modules
+import re
+## NOC Modules
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetVlans
+##
+## EdgeCore.ES.get_vlans
+##
+class Script(NOCScript):
+    name="EdgeCore.ES.get_vlans"
+    cache=True
+    implements=[IGetVlans]
+    ##
+    ## ES4626 = Cisco Style
+    ##
+    rx_vlan_line_4626=re.compile(r"^\s*(?P<vlan_id>\d{1,4})\s+(?P<name>\S+)\s+", re.IGNORECASE|re.MULTILINE)
+    @NOCScript.match(platform__regex="4626|4612")
+    def execute_4626(self):
+        vlans=self.cli("show vlan")
+        return [{"vlan_id": int(match.group("vlan_id")) ,"name": match.group("name")} for match in self.rx_vlan_line_4626.finditer(vlans)]
+    
+    ##
+    ## Other
+    ##
+    rx_vlan_line_3526=re.compile(r"^VLAN ID:\s+?(?P<vlan_id>\d{1,4})\n.*?Name:\s+(?P<name>\S*?)\n", re.IGNORECASE|re.DOTALL|re.MULTILINE)
+    @NOCScript.match()
+    def execute_3526(self):
+        vlans=self.cli("show vlan")
+        return [{"vlan_id": int(match.group("vlan_id")), "name": match.group("name")} for match in self.rx_vlan_line_3526.finditer(vlans)]
+    
