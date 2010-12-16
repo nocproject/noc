19181da6f77a3bc8b77a3d6275e95579d70f216a
Dmitry Volodin <dvolodin7@gmail.com>
2010-12-16 23:15:17 +0300

    OS.FreeBSD profile

---
+++ sa/profiles/OS/FreeBSD/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Vendor: FreeBSD
+## OS:     FreeBSD
+## Compatible:
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2010 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+import noc.sa.profiles
+from noc.sa.protocols.sae_pb2 import TELNET,SSH
+
+class Profile(noc.sa.profiles.Profile):
+    name="OS.FreeBSD"
+    supported_schemes=[TELNET,SSH]
+    command_super="/usr/bin/su"
+    pattern_unpriveleged_prompt=r"^\S*?%"
+    pattern_prompt=r"^\S*?#"
