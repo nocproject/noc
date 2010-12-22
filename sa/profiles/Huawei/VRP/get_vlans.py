81bea2513c2287b40aaa63d99ee690ae6b0f4f88
Shilov Ilya <shilov_i@pntl.ru>
2010-12-22 17:40:40 +0300

    Huawei.VRP.get_vlans fixed and new beef

--- sa/profiles/Huawei/VRP/get_vlans.py
+++ sa/profiles/Huawei/VRP/get_vlans.py
@@ -7,16 +7,33 @@
 ##----------------------------------------------------------------------
 """
 """
+## Python modules
+import re
+## NOC Modules
 from noc.sa.script import Script as NOCScript
 from noc.sa.interfaces import IGetVlans
 
 class Script(NOCScript):
     name="Huawei.VRP.get_vlans"
     implements=[IGetVlans]
-    def execute(self):
-        self.cli("undo terminal logging")
-        v=self.cli("display vlan")
-        vlans=", ".join(v.splitlines()[1:])
-        vlans=vlans.replace("(default)","")
-        return [{"vlan_id":int(x)} for x in self.expand_rangelist(vlans)]
+#    def execute(self):
+#        self.cli("undo terminal logging")
+#        v=self.cli("display vlan")
+#        vlans=", ".join(v.splitlines()[1:])
+#        vlans=vlans.replace("(default)","")
+#        return [{"vlan_id":int(x)} for x in self.expand_rangelist(vlans)]
+
+    rx_vlan_line_vrp5=re.compile(r"^(?P<vlan_id>\d{1,4})\s*?.*?", re.IGNORECASE|re.DOTALL|re.MULTILINE)
+    @NOCScript.match(version__startswith="5.")
+    def execute_vrp5(self):
+        vlans=self.cli("display vlan")
+        return [{"vlan_id": int(match.group("vlan_id")), "name": "VLAN" + match.group("vlan_id")} for match in self.rx_vlan_line_vrp5.finditer(vlans)]
+
+    rx_vlan_line_vrp3=re.compile(r"^\sVLAN ID:\s+?(?P<vlan_id>\d{1,4})\n.*?Name:\s+(?P<name>.*?)\n.*?(\n\n|$)", re.IGNORECASE|re.DOTALL|re.MULTILINE)
+    @NOCScript.match()
+    def execute_vrp3(self):
+        vlans=self.cli("display vlan all")
+        return [{"vlan_id": int(match.group("vlan_id")), "name": match.group("name")} for match in self.rx_vlan_line_vrp3.finditer(vlans)]
+#	return [{"vlan_id": int(match.group("vlan_id")), "name": "VLAN" + match.group("vlan_id")} for match in self.rx_vlan_line_vrp3.finditer(vlans)]
+
     
