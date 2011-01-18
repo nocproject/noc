be57fd8be1712ead60aa12f8db5c6c0db170c960
Dmitry Volodin <dvolodin7@gmail.com>
2011-01-18 10:02:29 +0300

    ManagedObject.config_filter_rule and .config_validation_rule

---
+++ sa/interfaces/iconfigfilter.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## IConfigFilter
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from base import *
+##
+## IConfigFilter interface
+##
+class IConfigFilter(Interface):
+    managed_object=InstanceOfParameter("ManagedObject")
+    config=StringParameter()
+    returns=StringParameter()
---
+++ sa/interfaces/iconfigvalidator.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## IConfigValidator
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from base import *
+##
+## IConfigValidator interface
+##
+class IConfigValidator(Interface):
+    managed_object=InstanceOfParameter("ManagedObject")
+    config=StringParameter()
+    returns=StringListParameter()
