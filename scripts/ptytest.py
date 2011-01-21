621eca1ac3f80ca972b80a58d28ffe216de59f27
Dmitry Volodin <dvolodin7@gmail.com>
2011-01-21 15:35:44 +0300

    ./scripts/ptytest.py

---
+++ scripts/ptytest.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Test for available processes and PTYs
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+import pty, time
+
+def test():
+    n=0
+    while True:
+        try:
+            pid,f=pty.fork()
+        except OSError:
+            print "%d PTYs available"%n
+            return
+        if pid==0:
+            time.sleep(15)
+            return
+        else:
+            n+=1
+    
+
+if __name__=="__main__":
+    test()
