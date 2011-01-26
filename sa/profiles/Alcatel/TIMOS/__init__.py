378b6501b54167c7258489e2b9a03301fd721b7f
Dmitry Volodin <dvolodin7@gmail.com>
2011-01-26 13:35:19 +0300

    Alcatel.TIMOS profile

---
+++ sa/profiles/Alcatel/TIMOS/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Vendor: Alcatel
+## OS:     TIMOS
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+import noc.sa.profiles
+from noc.sa.protocols.sae_pb2 import TELNET,SSH
+
+class Profile(noc.sa.profiles.Profile):
+    name="Alcatel.TIMOS"
+    supported_schemes=[TELNET,SSH]
+    pattern_username="[Ll]ogin: "
+    pattern_password="[Pp]assword: "
+    command_disable_pager="environment no more"
+    pattern_prompt=r"^\S+?#"
+    config_volatile=[r"^# Finished.*$",r"^# Generated.*$"]
+    pattern_more=r"^Press any key to continue.*$"
+    command_more=" "
---
+++ sa/profiles/Alcatel/TIMOS/get_version.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Alcatel.TIMOS.get_version
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+## Python modules
+import re
+## NOC modules
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetVersion
+##
+## Alcatel.TIMOS.get_version
+##
+class Script(NOCScript):
+    name="Alcatel.TIMOS.get_version"
+    cache=True
+    implements=[IGetVersion]
+    
+    rx_sys = re.compile(r"System Type\s+:\s+(?P<platform>.+?)$", re.MULTILINE | re.DOTALL)
+    rx_ver = re.compile(r"System Version\s+:\s+(?P<version>.+?)$", re.MULTILINE | re.DOTALL)
+    def execute(self):
+        v=self.cli("show system information")
+        match_sys=self.re_search(self.rx_sys, v)
+        match_ver=self.re_search(self.rx_ver, v)
+        return {
+            "vendor"    : "Alcatel",
+            "platform"  : match_sys.group("platform"),
+            "version"   : match_ver.group("version"),
+        }
+
