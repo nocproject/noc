1ab083c889f0d78292649a36d3f45e56c84aa742
Dmitry Volodin <dvolodin7@gmail.com>
2011-01-26 13:35:19 +0300

    Alcatel.TIMOS profile

---
+++ sa/profiles/Alcatel/TIMOS/get_config.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Alcatel.TIMOS.get_config
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+## NOC modules
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetConfig
+##
+## Alcatel.TIMOS.get_config
+##
+class Script(NOCScript):
+    name="Alcatel.TIMOS.get_config"
+    implements=[IGetConfig]
+    def execute(self):
+        config=self.cli("admin display")
+        config=self.strip_first_lines(config,6)
+        return self.cleaned_config(config)
