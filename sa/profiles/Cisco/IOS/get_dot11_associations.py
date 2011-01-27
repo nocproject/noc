a10a48cce2ef30b5afe667055127d9d19594f0ff
Dmitry Volodin <dvolodin7@gmail.com>
2011-01-27 12:16:05 +0300

    Converted to a new script API

--- sa/profiles/Cisco/IOS/get_dot11_associations.py
+++ sa/profiles/Cisco/IOS/get_dot11_associations.py
@@ -1,24 +1,32 @@
 # -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2009 The NOC Project
+## Cisco.IOS.get_dot11_associations
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 """
 """
-import noc.sa.script
-from noc.sa.interfaces import IGetDot11Associations
+## Python modules
 import re
-
-rx_assoc_line=re.compile(r"^(?P<mac>[0-9a-f]{4}\.[0-9a-f]{4}\.[0-9a-f]{4}) (?P<ip>[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}).*$")
-
-class Script(noc.sa.script.Script):
+## NOC modules
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetDot11Associations
+##
+## Cisco.IOS.get_dot11_associations
+##
+class Script(NOCScript):
     name="Cisco.IOS.get_dot11_associations"
     implements=[IGetDot11Associations]
+    
+    rx_assoc_line=re.compile(r"^(?P<mac>[0-9a-f]{4}\.[0-9a-f]{4}\.[0-9a-f]{4}) (?P<ip>[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}).*$", re.MULTILINE)
     def execute(self):
-        assoc=self.cli("show dot11 associations")
+        try:
+            assoc=self.cli("show dot11 associations")
+        except self.CLISyntaxError:
+            raise self.NotSupportedError()
         r=[]
-        for l in assoc.split("\n"):
-            match=rx_assoc_line.match(l.strip())
-            if match:
-                r.append({"mac":match.group("mac"),"ip":match.group("ip")})
+        for match in self.rx_assoc_line.findall(assoc):
+            r+=[{"mac":match.group("mac"),"ip":match.group("ip")}]
         return r
+    
