1b0635733afff53ea9a44013f7948a528435c8d3
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2011-01-27 16:54:24 +0200

    Use new API

--- sa/profiles/DLink/DES2108/add_vlan.py
+++ sa/profiles/DLink/DES2108/add_vlan.py
@@ -2,16 +2,16 @@
 ##----------------------------------------------------------------------
 ## DLink.DES2108.add_vlan
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2010 The NOC Project
+## Copyright (C) 2007-2011 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 """
 """
 from __future__ import with_statement
-import noc.sa.script
+from noc.sa.script import Script as NOCScript
 from noc.sa.interfaces import IAddVlan
 
-class Script(noc.sa.script.Script):
+class Script(NOCScript):
     name="DLink.DES2108.add_vlan"
     implements=[IAddVlan]
     def execute(self,vlan_id,name,tagged_ports):
