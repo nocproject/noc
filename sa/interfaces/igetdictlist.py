acb01c7a9c359cb3f5ca0b297fdafffef594dee2
Dmitry Volodin <dvolodin7@gmail.com>
2011-01-31 15:58:12 +0300

    IGetDictList interface

---
+++ sa/interfaces/igetdictlist.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## IGetDictList interface
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from base import *
+
+class IGetDictList(Interface):
+    returns=DictListParameter()
