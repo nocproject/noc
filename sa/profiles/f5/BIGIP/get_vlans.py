3f4715b7b5bffabc7b460d49e17b6209ef33686c
Dmitry Volodin <dvolodin7@gmail.com>
2011-01-31 14:45:29 +0300

    f5.BIGIP.get_vlans script

---
+++ sa/profiles/f5/BIGIP/get_vlans.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## f5.BIGIP.get_vlans
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+## Python modules
+import re
+## NOC modules
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetVlans
+##
+## f5.BIGIP.get_vlans
+##
+class Script(NOCScript):
+    name="f5.BIGIP.get_vlans"
+    cache=True
+    implements=[IGetVlans]
+    
+    rx_vlan=re.compile(r"^VLAN\s+(?P<name>.+?)(?:\s+\([^)]+\))?\s+tag\s+(?P<tag>\d+)", re.MULTILINE)
+    def execute(self):
+        r=[]
+        for match in self.rx_vlan.finditer(self.cli("vlan show")):
+            r+=[{
+                "name":    match.group("name"),
+                "vlan_id": match.group("tag")
+            }]
+        return r
+    
