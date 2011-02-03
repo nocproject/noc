f48229b3d2e1b373898a83d04e2a568b4301116b
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2011-02-03 13:33:56 +0200

    Add OS.FreeBSD.ping

---
+++ sa/profiles/OS/FreeBSD/ping.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## OS.FreeBSD.ping
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IPing
+import re
+
+class Script(NOCScript):
+    name="OS.FreeBSD.ping"
+    implements=[IPing]
+    rx_result=re.compile(r"^\s*(?P<count>\d+) packets transmitted, (?P<success>\d+) packets received, \d+\.\d+% packet loss",re.MULTILINE|re.DOTALL|re.IGNORECASE)
+    def execute(self,address,count=None,source_address=None,size=None,df=None):
+        cmd="/sbin/ping -q"
+        if count:
+            cmd+=" -c %d"%int(count)
+        else:
+            cmd+=" -c 5"
+        if source_address:
+            cmd+=" -S %s"%source_address
+        if size:
+            cmd+=" -s %d"%int(size)
+        if df:
+            cmd+=" -D"
+        cmd+=" %s"%address
+        match=self.rx_result.search(self.cli(cmd))
+        return {
+            "success": match.group("success"),
+            "count"  : match.group("count"),
+        }
