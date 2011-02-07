fc37f0972847d78dceb5fb2804ab54ca144498a3
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2011-02-07 16:56:11 +0200

    Add DLink.DxS_Cisco_CLI profile

---
+++ sa/profiles/DLink/DxS_Cisco_CLI/get_arp.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## DLink.DxS_Cisco_CLI.get_arp
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetARP
+import re
+
+class Script(NOCScript):
+    name="DLink.DxS_Cisco_CLI.get_arp"
+    implements=[IGetARP]
+    rx_line=re.compile(r"^Internet\s+(?P<ip>\S+)\s+(\d+|\-\-)\s+(?P<mac>\S+)\s+arpa\s+(?P<interface>.+)\s*$",re.MULTILINE)
+    def execute(self):
+        s=self.cli("show arp")
+        r=[]
+        for match in self.rx_line.finditer(self.cli("show arp")):
+            r+=[{
+                "ip"       : match.group("ip"),
+                "mac"      : match.group("mac"),
+                "interface": match.group("interface").strip(),
+                }]
+        return r
---
+++ sa/profiles/DLink/DxS_Cisco_CLI/get_version.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## DLink.DxS_Cisco_CLI.get_version
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetVersion
+import re
+
+class Script(NOCScript):
+    name="DLink.DxS_Cisco_CLI.get_version"
+    cache=True
+    implements=[IGetVersion]
+    rx_ver=re.compile(r"System description\s+:\s+(?P<platform>\S+).+System software version\s+:\s+v(?P<version>\S+), Release\(\d+\)",re.MULTILINE|re.DOTALL)
+    def execute(self):
+        match=self.re_search(self.rx_ver, self.cli("show version"))
+        return {
+            "vendor"    : "DLink",
+            "platform"  : match.group("platform"),
+            "version"   : match.group("version"),
+        }
