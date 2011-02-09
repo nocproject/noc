084aa0806c6ed97dc0d44aa54d80bd79148fcd34
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2011-02-09 17:07:26 +0200

    Add some scripts and beefs to DLink.DxS_Cisco_CLI

---
+++ sa/profiles/DLink/DxS_Cisco_CLI/add_vlan.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## DLink.DxS_Cisco_CLI.add_vlan
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from __future__ import with_statement
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IAddVlan
+
+class Script(NOCScript):
+    name="DLink.DxS_Cisco_CLI.add_vlan"
+    implements=[IAddVlan]
+    def execute(self,vlan_id,name,tagged_ports):
+        with self.configure():
+            self.cli("vlan %d"%vlan_id)
+            self.cli("name %s"%name)
+            self.cli("exit")
+        self.save_config()
+        return True
---
+++ sa/profiles/DLink/DxS_Cisco_CLI/get_vlans.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## DLink.DxS_Cisco_CLI.get_vlans
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetVlans
+import re
+
+class Script(NOCScript):
+    name="DLink.DxS_Cisco_CLI.get_vlans"
+    implements=[IGetVlans]
+    rx_vlan_line=re.compile(r"^(?P<vlan_id>\s{1,3}\d{1,4})\s+(?P<name>\S+)\s", re.MULTILINE)
+    def execute(self):
+        r=[]
+        for match in self.rx_vlan_line.finditer(self.cli("show vlan")):
+            r+=[{
+                "vlan_id" : int(match.group("vlan_id")),
+                "name"    : match.group("name"),
+                }]
+        return r
