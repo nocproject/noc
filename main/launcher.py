d491ecc468d1cb26a5d78b9145d2f9bd90e9aaa0
Dmitry Volodin <dvolodin7@gmail.com>
2011-02-09 09:37:54 +0300

    noc-launcher can start multiple daemon instances

--- main/launcher.py
+++ main/launcher.py
@@ -17,15 +17,18 @@ HEARTBEAT_TIMEOUT=10
 ## Daemon wrapper
 ##
 class DaemonData(object):
-    def __init__(self,name,is_superuser,enabled,user,uid,group,gid):
-        logging.debug("Reading config for %s"%name)
+    def __init__(self,name,is_superuser,enabled,user,uid,group,gid,instance_id,config_path):
+        logging.debug("Reading config for %s[#%s]: %s"%(name, instance_id, config_path))
+        self.instance_id=instance_id
+        self.name=name
+        self.logname="%s[#%s]"%(self.name, self.instance_id)
+        self.config_path=config_path
         self.config=ConfigParser.SafeConfigParser()
         self.config.read("etc/%s.defaults"%name)
-        self.config.read("etc/%s.conf"%name)
-        self.name=name
+        self.config.read(config_path)
         self.enabled=enabled
         self.pid=None
-        self.pidfile=self.config.get("main","pidfile")
+        self.pidfile=self.config.get("main","pidfile").replace("{{instance}}", self.instance_id)
         self.is_superuser=is_superuser
         self.user=user
         self.uid=uid
@@ -40,15 +43,15 @@ class DaemonData(object):
     ## Launch daemon
     ##
     def launch(self):
-        logging.info("Launching %s"%self.name)
+        logging.info("Launching %s"%(self.logname))
         try:
             pid=os.fork()
         except OSError, e:
-            logging.error("%s: Fork failed: %s(%s)"%(self.name,e.strerror,e.errno))
+            logging.error("%s: Fork failed: %s(%s)"%(self.logname,e.strerror,e.errno))
             return
         if pid:
             self.pid=pid
-            logging.info("Daemon %s started as PID %d"%(self.name,self.pid))
+            logging.info("Daemon %s started as PID %d"%(self.logname, self.pid))
             self.next_heartbeat_check=time.time()+HEARTBEAT_TIMEOUT
         else:
             # Run child
@@ -64,21 +67,21 @@ class DaemonData(object):
                     # Adjust HOME and USER environment variables
                     os.environ["USER"]=self.user
                     os.environ["HOME"]=pwd.getpwuid(self.uid).pw_dir
-                os.execv(sys.executable,[sys.executable,"./scripts/%s.py"%self.name,"launch"])
+                os.execv(sys.executable,[sys.executable,"./scripts/%s.py"%self.name,"launch","-c",self.config_path,"-i",self.instance_id])
             except OSError, e:
-                logging.error("%s: OS Error: %s(%s)"%(self.name,e.strerror,e.errno))
+                logging.error("%s: OS Error: %s(%s)"%(self.logname,e.strerror,e.errno))
                 sys.exit(1)
     ##
     ## Kill daemon
     ##
     def kill(self):
         if not self.pid:
-            logging.info("%s: No PID to kill"%self.name)
+            logging.info("%s: No PID to kill"%self.logname)
         try:
-            logging.info("%s: killing"%self.name)
+            logging.info("%s: killing"%self.logname)
             os.kill(self.pid,signal.SIGTERM)
         except:
-            logging.error("%s: Unable to kill daemon"%self.name)
+            logging.error("%s: Unable to kill daemon"%self.logname)
     ##
     ##
     ##
@@ -88,7 +91,7 @@ class DaemonData(object):
         t=time.time()
         if t<self.next_heartbeat_check:
             return
-        logging.debug("Checking heartbeat from %s"%self.name)
+        logging.debug("Checking heartbeat from %s"%self.logname)
         self.next_heartbeat_check=t+HEARTBEAT_TIMEOUT
         try:
             mt=os.stat(self.pidfile)[stat.ST_MTIME]
@@ -96,7 +99,7 @@ class DaemonData(object):
             logging.error("Unable to stat pidfile: %s"%self.pidfile)
             return
         if t-mt>=HEARTBEAT_TIMEOUT:
-            logging.info("%s: Heartbeat lost. Restarting"%self.name)
+            logging.info("%s: Heartbeat lost. Restarting"%self.logname)
             self.kill()
 
 class Launcher(Daemon):
@@ -106,7 +109,7 @@ class Launcher(Daemon):
         self.daemons=[]
         gids={}
         uids={}
-        self.is_superuser=os.getuid()==0
+        self.is_superuser=os.getuid()==0 # @todo: rewrite
         self.crashinfo_uid=None
         self.crashinfo_dir=None
         for n in ["fcgi","sae","activator","classifier","correlator","notifier","probe"]:
@@ -145,16 +148,25 @@ class Launcher(Daemon):
             if not self.is_superuser and gids:
                 logging.error("Need to be superuser to change GID")
                 sys.exit(1)
+            # Check for configs and daemon instances
+            opts=self.config.options(dn)
+            if "config" in opts:
+                configs=[("0", self.config.get(dn, "config"))]
+            else:
+                configs=[(c[7:], self.config.get(dn, c)) for c in opts if c.startswith("config.")]
             # Initialize daemon data
-            self.daemons+=[
-                DaemonData(dn,
-                    is_superuser = self.is_superuser,
-                    enabled = is_enabled,
-                    user    = user_name,
-                    uid     = uid,
-                    group   = group_name,
-                    gid     = gid)
-                    ]
+            for instance_id, config in configs:
+                self.daemons+=[
+                    DaemonData(dn,
+                        is_superuser = self.is_superuser,
+                        enabled = is_enabled,
+                        user    = user_name,
+                        uid     = uid,
+                        group   = group_name,
+                        gid     = gid,
+                        instance_id = instance_id,
+                        config_path = config)
+                        ]
             # Set crashinfo uid
             if self.is_superuser and dn=="noc-sae" and is_enabled:
                 self.crashinfo_uid=uid
@@ -194,7 +206,7 @@ class Launcher(Daemon):
                         pid=0
                         status=0
                     if pid==d.pid:
-                        logging.info("%s daemon is terminated with status %d"%(d.name,d.pid))
+                        logging.info("%s daemon is terminated with status %d"%(d.logname,d.pid))
                         d.pid=None
             time.sleep(1)
             t=time.time()
@@ -221,7 +233,7 @@ class Launcher(Daemon):
         for d in self.daemons:
             if d.enabled and d.pid:
                 try:
-                    logging.info("Stopping daemon: %s (PID %d)"%(d.name,d.pid))
+                    logging.info("Stopping daemon: %s (PID %d)"%(d.logname,d.pid))
                     os.kill(d.pid,signal.SIGKILL)
                     d.pid=None
                 except OSError:
