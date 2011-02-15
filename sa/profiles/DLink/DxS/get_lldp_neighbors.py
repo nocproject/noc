39731dfc3fd275885f3d9aaa51e815798a847fe7
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2011-02-15 09:27:10 +0200

    Typo fixed

--- sa/profiles/DLink/DxS/get_lldp_neighbors.py
+++ sa/profiles/DLink/DxS/get_lldp_neighbors.py
@@ -130,10 +130,10 @@ class Script(NOCScript):
                     print "\n\n\n\n\nremote_port_id\n\n\n\n\n"
                     continue
                 n["remote_port"] = match.group("port").strip()
-                # On DES-3028/52 Series "Port ID" may be typed as
+                # On DES-3028/52 Series "Port ID" may be typed like
                 # "RMON Port 28 on Unit 1"
                 if n["remote_port_subtype"] == 7 \
-                and n["remote_port"].lower().startswith("RMON Port"):
+                and n["remote_port"].lower().startswith("rmon port"):
                     match=self.rx_remote_port_id2.search(n["remote_port"])
                     if not match:
                         # Debug string
