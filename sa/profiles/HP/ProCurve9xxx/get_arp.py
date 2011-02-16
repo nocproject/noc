d4d4c597f9f8a4d268cf23aff17adaabf745733b
roshin <none@none>
2011-02-16 17:37:06 +0300

    Fixed get_fdp_neighbors, get_mac_address_table, get_version. Added get_arp,get_fqdn,get_spanning_tree. Added beef tests.

---
+++ sa/profiles/HP/ProCurve9xxx/get_arp.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## HP.ProCurve9xxx.get_arp
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+import noc.sa.script
+from noc.sa.interfaces import IGetARP
+import re
+
+rx_line=re.compile(r"^\d+\s+(?P<ip>\S+)\s+(?P<mac>\S+)\s+(?P<type>\S+)\s+\d+\s+(?P<interface>\S+)")
+
+
+class Script(noc.sa.script.Script):
+    name="HP.ProCurve9xxx.get_arp"
+    implements=[IGetARP]
+    def execute(self):
+        s=self.cli("show arp")
+        r=[]
+        for l in s.split("\n"):
+            match=rx_line.match(l.strip())
+            if not match:
+                continue
+            type=match.group("type")
+            mac=match.group("mac")
+            if mac.lower()=="incomplete" or type.lower()=="pending":
+                r.append({"ip":match.group("ip"),"mac":None,"interface":None})
+            else:
+                r.append({"ip":match.group("ip"),"mac":match.group("mac"),"interface":match.group("interface")})
+        return r
