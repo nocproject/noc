1897887d7c2ab2e3267c4398d35c95bce682d882
Dmitry Volodin <dvolodin7@gmail.com>
2011-02-20 00:33:46 +0300

    Decrease memory usage

--- sa/apps/reportmrtask/views.py
+++ sa/apps/reportmrtask/views.py
@@ -7,21 +7,25 @@
 ##----------------------------------------------------------------------
 ## Python modules
 import datetime
+# Django modules
+from django.utils.dateformat import DateFormat
 ## NOC modules
 from noc.lib.app.simplereport import *
 from noc.sa.models import ReduceTask
+from noc import settings
 ##
 ## Map/Reduce task report
 ##
 class ReportMRTask(SimpleReport):
     title="Active Map/Reduce tasks"
     def get_data(self,**kwargs):
+        dt=lambda f: DateFormat(f).format(settings.DATETIME_FORMAT)
         r=[]
         now=datetime.datetime.now()
         for rt in ReduceTask.objects.all().order_by("-stop_time"):
-            r+=[SectionRow("#%d. %s"%(rt.id,"[Complete]" if rt.stop_time else "[Running]"))]
-            for mt in rt.maptask_set.all():
-                r+=[(mt.managed_object.name, mt.map_script, mt.status, mt.script_result)]
+            r+=[SectionRow("#%d. %s. %s - %s"%(rt.id, "[Complete]" if rt.stop_time else "[Running]", dt(rt.start_time), dt(rt.stop_time)))]
+            for mt in rt.maptask_set.all().only("managed_object__name", "map_script", "status"):
+                r+=[(mt.managed_object.name, mt.map_script, mt.get_status_display())]
         return self.from_dataset(title=self.title,
-            columns=["Object","Script","Status", TableColumn("Result", format="pprint")],data=r)
+            columns=["Object","Script","Status"], data=r)
     
