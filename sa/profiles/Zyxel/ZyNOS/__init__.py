6438749cdcb4f084b699f47dcdb9c99b4c3bb254
Dmitry Volodin <dvolodin7@gmail.com>
2011-03-24 14:06:49 +0300

    Zyxel.ZyNOS.add_vlan, Zyxel.ZyNOS.remove_vlan

--- sa/profiles/Zyxel/ZyNOS/__init__.py
+++ sa/profiles/Zyxel/ZyNOS/__init__.py
@@ -2,9 +2,8 @@
 ##----------------------------------------------------------------------
 ## Vendor: Zyxel
 ## OS:     ZyNOS
-## Compatible: 3124
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2009 The NOC Project
+## Copyright (C) 2007-2011 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 """
@@ -17,6 +16,12 @@ class Profile(noc.sa.profiles.Profile):
     supported_schemes=[TELNET,SSH]
     pattern_username="User name:"
     pattern_unpriveleged_prompt=r"^\S+?>"
-    command_super="enable"
     pattern_prompt=r"^\S+?#"
+    pattern_more=r"^-- more --.*?$" 
+    command_super="enable"
     command_more=" "
+    command_enter_config="configure" 
+    command_leave_config="exit" 
+    command_exit="exit" 
+    command_save_config="write memory" 
+    pattern_syntax_error="Invalid command"
---
+++ sa/profiles/Zyxel/ZyNOS/remove_vlan.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Zyxel.ZyNOS.remove_vlan
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from __future__ import with_statement
+## NOC modules
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IRemoveVlan
+##
+## Zyxel.ZyNOS.remove_vlan
+##
+class Script(NOCScript):
+    name="Zyxel.ZyNOS.remove_vlan"
+    implements=[IRemoveVlan]
+    def execute(self,vlan_id):
+        for v in  self.scripts.get_vlans():
+            if v["vlan_id"]==vlan_id:
+                with self.configure():
+                    self.cli("no vlan %s"%v["vlan_id"])
+                self.save_config()
+                return True
+        return False
+    
