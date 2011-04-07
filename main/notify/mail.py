90848022915708dfb835cf83f93191011db240a1
Dmitry Volodin <dvolodin7@gmail.com>
2011-04-07 09:21:11 +0400

    sendmail support

--- main/notify/mail.py
+++ main/notify/mail.py
@@ -2,22 +2,30 @@
 ##----------------------------------------------------------------------
 ##  SMTP Mail plugin
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2009 The NOC Project
+## Copyright (C) 2007-2011 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-from noc.main.notify import Notify as NotifyBase
-from noc.lib.validators import is_email
-import smtplib,socket
+
+## Python modules
+import smtplib
+import socket
+import subprocess
+import shlex
 from email.mime.text import MIMEText
 from email.header import Header
 import email.utils
+## NOC modules
+from noc.main.notify import Notify as NotifyBase
+from noc.lib.validators import is_email
 ##
 ##
 ##
 class Notify(NotifyBase):
-    name="mail"
+    name = "mail"
 
-    def send_message(self,params,subject,body,link=None):
+    def send_message(self, params, subject, body, link=None):
+        print "-------------------->>>>"
+        use_sendmail = bool(self.config.get(self.name, "command").strip())
         # Check params
         if not is_email(params):
             self.error("Invalid email: %s"%params)
@@ -31,38 +39,53 @@ class Notify(NotifyBase):
         message["To"]=params
         message["Date"]=email.utils.formatdate()
         message["Subject"]=Header(subject,"utf-8")
-        self.debug(message.as_string())
-        # Connect to SMTP server
-        smtp=smtplib.SMTP()
-        self.debug("Connecting %s"%self.config.get(self.name,"smtp_server"))
-        try:
-            smtp.connect(self.config.get(self.name,"smtp_server"),self.config.getint(self.name,"smtp_port"))
-        except socket.error,why:
-            self.error("SMTP error: %s"%str(why))
-            return False
-        smtp.ehlo(self.config.get(self.name,"helo_hostname"))
-        # Enforce TLS when required
-        if self.config.getboolean(self.name,"use_tls"):
-            smtp.starttls()
-            smtp.ehlo(self.config.get(self.name,"helo_hostname"))
-        # Authenticate when necessary
-        smtp_user=self.config.get(self.name,"smtp_user")
-        smtp_password=self.config.get(self.name,"smtp_password")
-        if smtp_user and smtp_password:
-            self.debug("Authenticating as %s"%smtp_user)
+        msg = message.as_string()
+        self.debug(msg)
+        if use_sendmail:
+            # Spool via sendmail
+            cmd = self.config.get(self.name, "command", "/usr/sbin/sendmail -t -i")
+            c = shlex.split(cmd)
+            self.debug("Spooling to %s" % c[0])
             try:
-                smtp.login(smtp_user,smtp_password)
-            except smtplib.SMTPAuthenticationError,why:
-                self.error("SMTP Authentication error: %s"%str(why))
+                p = subprocess.Popen(cmd, stdin=subprocess.PIPE)
+            except OSError, why:
+                self.error("Failed to run %s: %s" % (c[0], why))
                 return False
-        # Send mail
-        try:
-            smtp.sendmail(from_address,[params],message.as_string())
-        except smtplib.SMTPSenderRefused,why:
-            self.error("Sender refused: %s"%str(why))
-            return False
-        except smtplib.SMTPServerDisconnected, why:
-            self.error("Server disconnected: %s"%str(why))
-            return False
+            p.stdin.write(msg)
+            p.stdin.close()
+        else:
+            # Spool via SMTP
+            # Connect to SMTP server
+            smtp=smtplib.SMTP()
+            self.debug("Connecting %s"%self.config.get(self.name,"smtp_server"))
+            try:
+                smtp.connect(self.config.get(self.name,"smtp_server"),self.config.getint(self.name,"smtp_port"))
+            except socket.error,why:
+                self.error("SMTP error: %s"%str(why))
+                return False
+            smtp.ehlo(self.config.get(self.name,"helo_hostname"))
+            # Enforce TLS when required
+            if self.config.getboolean(self.name,"use_tls"):
+                smtp.starttls()
+                smtp.ehlo(self.config.get(self.name,"helo_hostname"))
+            # Authenticate when necessary
+            smtp_user=self.config.get(self.name,"smtp_user")
+            smtp_password=self.config.get(self.name,"smtp_password")
+            if smtp_user and smtp_password:
+                self.debug("Authenticating as %s"%smtp_user)
+                try:
+                    smtp.login(smtp_user,smtp_password)
+                except smtplib.SMTPAuthenticationError,why:
+                    self.error("SMTP Authentication error: %s"%str(why))
+                    return False
+            # Send mail
+            try:
+                smtp.sendmail(from_address,[params],msg)
+            except smtplib.SMTPSenderRefused,why:
+                self.error("Sender refused: %s"%str(why))
+                return False
+            except smtplib.SMTPServerDisconnected, why:
+                self.error("Server disconnected: %s"%str(why))
+                return False
         #
         return True
