e60fcf5d681489da2699db6484700adca80315cf
Dmitry Volodin <dvolodin7@gmail.com>
2011-04-19 09:30:23 +0400

    IGetSNMPGet interface and Generic.get_snmp_get script

---
+++ sa/interfaces/igetsnmpget.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## IGetSNMPGet
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+## NOC modules
+from base import *
+
+class IGetSNMPGet(Interface):
+    oid = OIDParameter()
+    returns = NoneParameter() | StringParameter()
+    template = "interfaces/igetsnmpget.html"
---
+++ sa/profiles/Generic/get_snmp_get.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Generic.get_snmp_get
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+## NOC modules
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetSNMPGet
+
+class Script(NOCScript):
+    name="Generic.get_snmp_get"
+    implements=[IGetSNMPGet]
+    requires=[]
+    
+    def execute(self, oid, community_suffix=None):
+        try:
+            return self.snmp.get(oid, community_suffix)
+        except self.snmp.TimeOutError:
+            return None
+    
