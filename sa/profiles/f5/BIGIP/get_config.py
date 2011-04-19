ca10e3b98d71cbec07d56d724c5ca8f1c2306630
Dmitry Volodin <dvolodin7@gmail.com>
2011-04-19 13:11:38 +0400

    Use "export"

--- sa/profiles/f5/BIGIP/get_config.py
+++ sa/profiles/f5/BIGIP/get_config.py
@@ -13,6 +13,8 @@ from noc.sa.interfaces import IGetConfig
 class Script(noc.sa.script.Script):
     name="f5.BIGIP.get_config"
     implements=[IGetConfig]
+    TIMEOUT = 300
+    CLI_TIMEOUT = 60
     def execute(self):
-        config=self.cli("list all")
+        config=self.cli("export")
         return self.cleaned_config(config)
