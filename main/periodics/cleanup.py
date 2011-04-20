1456429c2191aa09766c108b80de334b1b9d71cd
Dmitry Volodin <dvolodin7@gmail.com>
2011-04-20 13:03:13 +0400

    Wipe out forgotten map/reduce tasks

--- main/periodics/cleanup.py
+++ main/periodics/cleanup.py
@@ -29,4 +29,18 @@ class Task(noc.lib.periodic.Task):
                 logging.info("Delete hanging tag reference: %s"%str(i))
                 i.delete()
         cursor.execute("COMMIT")
+        # Delete forgotten map/reduce tasks
+        cursor.execute("""
+            DELETE FROM sa_maptask
+            WHERE task_id IN (
+                SELECT id
+                FROM sa_reducetask
+                WHERE stop_time < ('now'::timestamp - '1day'::interval)
+            )
+        """)
+        cursor.execute("""
+            DELETE FROM sa_reducetask
+            WHERE stop_time < ('now'::timestamp - '1day'::interval)
+        """)
+        cursor.execute("COMMIT")
         return True
