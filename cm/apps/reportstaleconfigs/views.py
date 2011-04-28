b98520032642b51d4104792550871abae1dd2225
Dmitry Volodin <dvolodin7@gmail.com>
2011-04-28 10:06:53 +0400

    Use Config.is_stale

--- cm/apps/reportstaleconfigs/views.py
+++ cm/apps/reportstaleconfigs/views.py
@@ -2,35 +2,21 @@
 ##----------------------------------------------------------------------
 ## Stale Configs Report
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2010 The NOC Project
+## Copyright (C) 2007-2011 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-from noc.lib.app.simplereport import SimpleReport,TableColumn
-## Interval in days to consider config stale
-STALE_INTERVAL=2
-##
-##
-##
-class Reportreportstaleconfigs(SimpleReport):
-    title="Stale Configs (for %d days)"%STALE_INTERVAL
-    def get_data(self,**kwargs):
-        return self.from_query(title=self.title,
-            columns=["Config",TableColumn("Last Pull",format="datetime")],
-            query="""
-                SELECT repo_path,last_pull
-                FROM cm_config
-                WHERE
-                        (
-                            last_pull IS NULL
-                        OR  last_pull<('now'::timestamp-'%d days'::interval)
-                        )
-                    AND managed_object_id IN (
-                        SELECT  id
-                        FROM    sa_managedobject
-                        WHERE
-                                    is_managed
-                                AND is_configuration_managed
-                        )
-                ORDER BY 2
-            """%STALE_INTERVAL,
-            enumerate=True)
+
+## NOC modules
+from noc.lib.app.simplereport import SimpleReport, TableColumn
+from noc.cm.models import Config
+
+
+class ReportStaleConfig(SimpleReport):
+    title = "Stale Configs"
+
+    def get_data(self, **kwargs):
+        return self.from_dataset(title=self.title,
+            columns=["Config", TableColumn("Last Pull", format="datetime")],
+            data=[(c.repo_path, c.last_pull) for c
+                  in Config.objects.all()
+                  if c.is_stale])
