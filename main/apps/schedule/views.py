a1c14dff2920d675926042dd46feeb6225e04909
Dmitry Volodin <dvolodin7@gmail.com>
2011-04-28 08:50:42 +0400

    pyRule periodic tasks

--- main/apps/schedule/views.py
+++ main/apps/schedule/views.py
@@ -10,51 +10,72 @@
 import datetime
 ## Django modules
 from django.contrib import admin
+from django import forms
 ## NOC modules
 from noc.lib.app import ModelApplication
-from noc.main.models import Schedule
+from noc.main.models import Schedule, PyRule
 from noc.lib.periodic import periodic_registry
-##
-## Schedule admin
-##
+
+
+class ScheduleAdminForm(forms.ModelForm):
+    class Meta:
+        model = Schedule
+
+    periodic_name = forms.ChoiceField(label="Periodic Task", choices=())
+
+    def __init__(self, *args, **kwargs):
+        super(ScheduleAdminForm, self).__init__(*args, **kwargs)
+        c = [("Builtin", periodic_registry.choices)]
+        prc = list(PyRule.objects.filter(interface="IPeriodicTask").order_by("name"))
+        if prc:
+            c += [("pyRule", [("pyrule:%s" % x, x) for x in prc])]
+        self.fields["periodic_name"].choices = c
+
+
 class ScheduleAdmin(admin.ModelAdmin):
+    """
+    Schedule admin
+    """
+    form = ScheduleAdminForm
     list_display = ["periodic_name", "is_enabled", "time_pattern",
                     "run_every", "timeout", "last_run", "last_status"]
     search_fields = ["periodic_name"]
     actions = ["run_now"]
-    list_editable = ["is_enabled","run_every", "timeout"]
+    list_editable = ["is_enabled", "run_every", "timeout"]
     list_filter = ["is_enabled"]
-    ##
-    ## Reschedule selected tasks
-    ##
+
     def run_now(self, request, queryset):
+        """
+        Reschedule selected tasks
+        """
         updated = 0
         now = datetime.datetime.now()
         for t in queryset:
             t.last_run = now - datetime.timedelta(seconds=t.run_every)
             t.save()
             updated += 1
-        if updated==1:
+        if updated == 1:
             message = "1 task has been rescheduled"
         else:
             message = "%d tasks have been rescheduled" % updated
         self.message_user(request, message)
-    run_now.short_description="Run selected tasks now"
-    ##
-    ## Handle timeout
-    ##
+    run_now.short_description = "Run selected tasks now"
+
     def save_model(self, request, obj, form, change):
-        if obj.periodic_name:
+        """
+        Handle timeout
+        """
+        if obj.periodic_name and not obj.periodic_name.startswith("pyrule:"):
             t = periodic_registry[obj.periodic_name].default_timeout
             if not t:
                 obj.timeout = None
         return super(ScheduleAdmin, self).save_model(request, obj, form, change)
-    
 
-##
-## Schedule application
-##
+
 class ScheduleApplication(ModelApplication):
-    model=Schedule
-    model_admin=ScheduleAdmin
-    menu="Setup | Schedules"
+    """
+    Schedule application
+    """
+    model = Schedule
+    model_admin = ScheduleAdmin
+    menu = "Setup | Schedules"
