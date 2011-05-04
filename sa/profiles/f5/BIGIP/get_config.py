48611d3d079f2ef6ec6338c5a4b6013226cd8837
Dmitry Volodin <dvolodin7@gmail.com>
2011-05-04 09:34:49 +0400

    "with self.tmsh()" construction

--- sa/profiles/f5/BIGIP/get_config.py
+++ sa/profiles/f5/BIGIP/get_config.py
@@ -13,14 +13,14 @@ from noc.sa.interfaces import IGetConfig
 
 
 class Script(NOCScript):
-    name="f5.BIGIP.get_config"
-    implements=[IGetConfig]
+    name = "f5.BIGIP.get_config"
+    implements = [IGetConfig]
     TIMEOUT = 300
     CLI_TIMEOUT = 60
+
     def execute(self):
-        self.cli("tmsh")  # Enter tmsh
-        self.cli("modify /cli preference pager disabled")  # Disable pager
-        config = self.cli("list")  # Get config
-        self.cli("quit")  # Leave tmsh
+        print self.tmsh
+        with self.tmsh():
+            config = self.cli("list")  # Get config
         config = self.strip_first_lines(config, 1)
         return self.cleaned_config(config)
