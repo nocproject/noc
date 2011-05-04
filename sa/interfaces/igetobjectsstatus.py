dcfd9b356bb7581611dd537ae3f9ae85258df6ef
Dmitry Volodin <dvolodin7@gmail.com>
2011-05-04 16:11:59 +0400

    IGetObjectsStatus interface and NOC.SAE.get_object_status script

---
+++ sa/interfaces/igetobjectsstatus.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## IGetObjectsStatus interface
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from base import *
+
+class IGetObjectsStatus(Interface):
+    objects = ListOfParameter(element=IntParameter(), required=False)
+    returns = ListOfParameter(element=DictParameter(attrs={
+        "object_id"  : IntParameter(),
+        "status": BooleanParameter() | NoneParameter(),  # Up/Down/Unknown
+    }))
