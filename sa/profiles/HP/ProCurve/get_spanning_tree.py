e842d77f3e3901709566109f7e9b528d2b3d8f9a
Dmitry Volodin <dvolodin7@gmail.com>
2011-05-24 16:29:56 +0400

    Support for RSTP mode

--- sa/profiles/HP/ProCurve/get_spanning_tree.py
+++ sa/profiles/HP/ProCurve/get_spanning_tree.py
@@ -9,6 +9,7 @@
 """
 import noc.sa.script
 from noc.sa.interfaces import IGetSpanningTree
+#from noc.sa.interfaces.base import MACAddressParameter
 from noc.lib.text import list_to_ranges
 import re
 
@@ -97,6 +98,8 @@ class Script(noc.sa.script.Script):
                         vlans[instance_id]+=v[instance_id]
                     except KeyError:
                         vlans[instance_id]=v[instance_id]
+
+        if not vlans.has_key(0): vlans[0] = ''
         # Convert bitmask to vlan list
         rest_vlans=set(range(1,4096))
         for instance_id in vlans:
@@ -163,6 +166,7 @@ class Script(noc.sa.script.Script):
                 "1" : "disabled",
                 "?" : "alternate",
                 "5" : "backup",
+                "6" : "disabled",
                 "2" : "root",
                 "3" : "designated",
                 "_" : "master",
@@ -205,6 +209,82 @@ class Script(noc.sa.script.Script):
         r["instances"]=sorted(instances.values(),lambda x,y:cmp(x["id"],y["id"]))
         return r
     ##
+    def process_rstp(self):
+        r={
+            "mode"      : "RSTP",
+            
+        }
+        # Get bridge id
+        bridge_id=self.mib_get("dot1dBaseBridgeAddress").replace(" ","")
+        bridge_id="%s-%s"%(bridge_id[:6],bridge_id[6:])
+        
+        # Create instances
+        instance={
+                   'id' : 0,
+                   'vlans'   : "1-4095",
+                   'bridge_id' : bridge_id
+                   } 
+        
+        for l in self.cli('sh spanning-tree instance ist').splitlines():
+            if l.find('Switch Priority') > 0:
+                instance['bridge_priority'] = int(l.split(':')[1].strip())
+            if l.find('Regional Root MAC Address') >0:
+                instance['root_id'] = l.split(':')[1].strip()
+            if l.find('Regional Root Priority') > 0:
+                instance['root_priority'] = int(l.split(':')[1].strip())
+
+            
+
+        ports={} # port_id -> {"interface","port_id","edge","point_to_point"}
+        ifname=self.mib_walk("ifName")
+        v=self.mib_walk("dot1dBasePortIfIndex")
+        for port_id in v:
+            ports[port_id]={
+                "interface" : ifname[int(v[port_id])],
+                "port_id"   : port_id,
+        }
+        # Edge port status
+        v=self.mib_walk("hpicfBridgeRstpOperEdgePort")
+        for port_id in v:
+            ports[port_id]["edge"]= v[port_id]=="1"
+        # point_to_point status
+        v=self.mib_walk("hpicfBridgeRstpOperPointToPointMac")
+        for port_id in v:
+            ports[port_id]["point_to_point"]= v[port_id]=="1"
+            
+        for bl in self.cli('sh spanning-tree instance ist detail').split('\n\n'):
+            iface={}
+            for l in bl.splitlines():
+                if l.find('Port') == 2:
+                    iface['interface'] = l.split(':')[1].strip()
+                if l.find('Role') >0: 
+                    iface['role'] = l.split(':')[1].strip().lower()
+                if l.find('State') >0:
+                    iface['state'] = l.split(':')[1].strip().lower()
+                if l.find('Priority') == 2:
+                    pri = l.split(':')[1].strip()
+                    iface['priority'] = int(pri)
+                if l.find('Designated Bridge ID') > 0:
+                    iface['designated_bridge_priority'] = l.split(':')[1].lstrip()
+                    iface['designated_bridge_id'] = l.split(':')[2].rstrip()
+                if l.find('Designated Port ID') > 0:
+                    iface['designated_port_id'] = l.split(':')[2].rstrip()
+      
+            if not iface:
+                    continue
+                
+            for p in ports:
+                    if iface['interface'] == ports[p]['interface']:
+                        iface['port_id'] = '%s.%s' % (pri,ports[p]['port_id'])
+                        iface['edge'] = ports[p]['edge']
+                        iface['point_to_point'] = ports[p]['point_to_point']
+            try:
+                instance['interfaces'].append(iface)
+            except KeyError:
+                instance['interfaces'] = [iface]
+
+        r["instances"]=[instance]
+        return r
     def execute(self):
         stp_version=self.mib_get("hpicfBridgeRstpForceVersion")
         if stp_version=="1":
@@ -212,7 +292,7 @@ class Script(noc.sa.script.Script):
             pass
         elif stp_version=="2":
             # RSTP
-            pass
+            return self.process_rstp()
         elif stp_version=="3":
             # MSTP
             return self.process_mstp()
