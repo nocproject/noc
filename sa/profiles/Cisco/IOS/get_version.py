602f742a9afa611f0943fcd122770054fa01b266
Dmitry Volodin <dvolodin7@gmail.com>
2011-05-31 11:21:24 +0400

    Use caching

--- sa/profiles/Cisco/IOS/get_version.py
+++ sa/profiles/Cisco/IOS/get_version.py
@@ -1,26 +1,32 @@
 # -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2009 The NOC Project
+## Cisco.IOS.get_version
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 """
 """
-import noc.sa.script
-from noc.sa.interfaces import IGetVersion
+## Python modules
 import re
+## NOC modules
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetVersion
 
-rx_ver=re.compile(r"^(?:Cisco IOS Software,.*?|IOS \(tm\)) (?P<platform>.+?) Software \((?P<image>[^)]+)\), Version (?P<version>[^,]+),",re.MULTILINE|re.DOTALL)
-rx_snmp_ver=re.compile(r"(?P<platform>\S+) Software \((?P<image>[^)]+)\), Version (?P<version>[^,]+),")
 
-class Script(noc.sa.script.Script):
-    name="Cisco.IOS.get_version"
-    cache=True
-    implements=[IGetVersion]
+class Script(NOCScript):
+    name = "Cisco.IOS.get_version"
+    cache = True
+    implements = [IGetVersion]
+
+    rx_ver = re.compile(r"^(?:Cisco IOS Software,.*?|IOS \(tm\)) (?P<platform>.+?) Software \((?P<image>[^)]+)\), Version (?P<version>[^,]+),", re.MULTILINE | re.DOTALL)
+    rx_snmp_ver = re.compile(r"(?P<platform>\S+) Software \((?P<image>[^)]+)\), Version (?P<version>[^,]+),")
+
     def execute(self):
         if self.snmp and self.access_profile.snmp_ro:
             try:
-                v=self.snmp.get("1.3.6.1.2.1.1.1.0") # sysDescr.0
-                match=rx_snmp_ver.search(v)
+                v = self.snmp.get("1.3.6.1.2.1.1.1.0", cached=True)  # sysDescr.0
+                match = self.re_search(self.rx_snmp_ver, v)
                 return {
                     "vendor"     : "Cisco",
                     "platform"   : match.group("platform"),
@@ -31,8 +37,8 @@ class Script(noc.sa.script.Script):
                 }
             except self.snmp.TimeOutError:
                 pass
-        v=self.cli("show version")
-        match=rx_ver.search(v)
+        v = self.cli("show version", cached=True)
+        match = self.re_search(self.rx_ver, v)
         return {
             "vendor"    : "Cisco",
             "platform"  : match.group("platform"),
