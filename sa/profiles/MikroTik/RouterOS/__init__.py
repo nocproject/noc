5bf9c2f3dc2b55e9d7ee0b2f8a8ca1bc7ac5521e
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2011-06-03 16:26:58 +0300

    Improve get_version and PEP8 formatting

--- sa/profiles/MikroTik/RouterOS/__init__.py
+++ sa/profiles/MikroTik/RouterOS/__init__.py
@@ -2,26 +2,27 @@
 ##----------------------------------------------------------------------
 ## Vendor: MikroTik
 ## OS:     RouterOS
-## Compatible: 2.8, 2.9
+## Compatible: 3.14 and above
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2009 The NOC Project
+## Copyright (C) 2007-2011 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 """
 """
 import noc.sa.profiles
-from noc.sa.protocols.sae_pb2 import TELNET,SSH
+from noc.sa.protocols.sae_pb2 import TELNET, SSH
 
 class Profile(noc.sa.profiles.Profile):
-    name="MikroTik.RouterOS"
-    supported_schemes=[TELNET,SSH]
-    command_submit="\r"
-    pattern_prompt=r"\[(?P<prompt>.+?@.+?)\] > "
-    pattern_more=[
+    name = "MikroTik.RouterOS"
+    supported_schemes = [TELNET,SSH]
+    command_submit = "\r"
+    pattern_prompt = r"\[(?P<prompt>.+?@.+?)\] > "
+    pattern_more = [
         ("Please press \"Enter\" to continue!", "\n"),
     ]
-    config_volatile=[r"^#.*?$", r"^\s?"]
-
+    config_volatile = [r"^#.*?$", r"^\s?"]
+    # Starting from v3.14 we can specify console options during login process.
     def setup_script(self, script):
-        if script.parent is None and not script.access_profile.user.endswith("+ct"):
+        if script.parent is None \
+        and not script.access_profile.user.endswith("+ct"):
             script.access_profile.user += "+ct"
