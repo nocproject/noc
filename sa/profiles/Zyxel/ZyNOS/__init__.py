ec99329d9ce0e6e718bf2de3e294bc8cf73f08f0
Dmitry Volodin <dvolodin7@gmail.com>
2011-06-03 10:07:58 +0400

    convert_interface_name(), improved error matching and PEP8 formatting

--- sa/profiles/Zyxel/ZyNOS/__init__.py
+++ sa/profiles/Zyxel/ZyNOS/__init__.py
@@ -8,20 +8,39 @@
 ##----------------------------------------------------------------------
 """
 """
+## Python modules
+import re
+## NOC modules
 import noc.sa.profiles
-from noc.sa.protocols.sae_pb2 import TELNET,SSH
+from noc.sa.protocols.sae_pb2 import TELNET, SSH
+
 
 class Profile(noc.sa.profiles.Profile):
-    name="Zyxel.ZyNOS"
-    supported_schemes=[TELNET,SSH]
-    pattern_username="User name:"
-    pattern_unpriveleged_prompt=r"^\S+?>"
-    pattern_prompt=r"^\S+?#"
-    pattern_more=r"^-- more --.*?$" 
-    command_super="enable"
-    command_more=" "
-    command_enter_config="configure" 
-    command_leave_config="exit" 
-    command_exit="exit" 
-    command_save_config="write memory" 
-    pattern_syntax_error="Invalid command"
+    name = "Zyxel.ZyNOS"
+    supported_schemes = [TELNET, SSH]
+    pattern_username = "User name:"
+    pattern_unpriveleged_prompt = r"^\S+?>"
+    pattern_prompt = r"^\S+?#"
+    pattern_more = r"^-- more --.*?$"
+    command_super = "enable"
+    command_more = " "
+    command_enter_config = "configure"
+    command_leave_config = "exit"
+    command_exit = "exit"
+    command_save_config = "write memory"
+    pattern_syntax_error = "Invalid (command|input)"
+
+    rx_ifname = re.compile(r"^swp(?P<number>\d+)$")
+
+    def convert_interface_name(self, s):
+        """
+        >>> Profile().convert_interface_name("1")
+        '1'
+        >>> Profile().convert_interface_name("swp00")
+        '1'
+        """
+        match = self.rx_ifname.match(s)
+        if match:
+            return "%d" % (int(match.group("number")) + 1)
+        else:
+            return s
