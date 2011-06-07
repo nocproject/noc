aa0211a00bd1f47e6fad1f3555898edc37c7fbe8
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2011-06-07 10:24:48 +0300

    Add MikroTik.RouterOS.get_license

---
+++ sa/profiles/MikroTik/RouterOS/get_license.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## MikroTik.RouterOS.get_license
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+## Python modules
+import re
+## NOC modiles
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetLicense
+
+class Script(NOCScript):
+    name = "MikroTik.RouterOS.get_license"
+    cache = True
+    implements = [IGetLicense]
+
+    rx_lic = re.compile(r"^\s*software-id: (?P<sid>\S+).+upgradable-to: (?P<upto>\S+).+nlevel: (?P<nlevel>\d+).+features:.*(?P<features>\.*)$",re.MULTILINE|re.DOTALL)
+    def execute(self):
+        match = self.re_search(self.rx_lic, self.cli("system license print"))
+        r = {
+            "software-id"   : match.group("sid"),
+            "upgradable-to" : match.group("upto"),
+            "nlevel"        : int(match.group("nlevel")),
+        }
+        features = match.group("features").strip()
+        if features:
+            r.update({"features" : features})
+        return r
