b059e31198cf9df1cf50a3fec243954eea0068ca
Dmitry Volodin <dvolodin7@gmail.com>
2011-06-15 10:20:29 +0400

    Alarm Manager

---
+++ fm/apps/alarm/tests/test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## event Test
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+from noc.lib.test import ApplicationTestCase
+from django.utils import simplejson as json
+
+class eventTestCase(ApplicationTestCase):
+    pass
