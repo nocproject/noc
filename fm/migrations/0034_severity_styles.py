d3b5e8e23e673ca5c8ea292116266f7ede38bbec
Dmitry Volodin <dvolodin7@gmail.com>
2011-06-15 10:14:44 +0400

    Create styles for alarm severities

---
+++ fm/migrations/0034_severity_styles.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2009 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+import re
+from south.db import db
+from noc.fm.models import *
+import pymongo
+import noc.settings
+
+
+class Migration:    
+    def forwards(self):
+        connection = pymongo.Connection()
+        nosql_db = pymongo.database.Database(connection, noc.settings.NOSQL_DATABASE_NAME)
+        # Create styles for alarm severities
+        for name, font, background, description in [
+            ("FM INFO", 12255232, 14480371, "Alarm severity INFO"),
+            ("FM WARNING", 12255232, 16777181, "Alarm severity WARNING"),
+            ("FM MINOR", 12255232, 16777147, "Alarm severity MINOR"),
+            ("FM MAJOR", 12255232, 16772829, "Alarm severity MAJOR"),
+            ("FM CRITICAL", 12255232, 16768460, "Alarm severity CRITICAL"),
+        ]:
+            if db.execute("SELECT COUNT(*) FROM main_style WHERE name = %s", [name])[0][0] == 0:
+                db.execute("INSERT INTO main_style(name, font_color, background_color, description) VALUES (%s, %s, %s, %s)", [name, font, background, description])
+    
+    
+    def backwards(self):
+        db.execute("DELETE FROM main_style WHERE name LIKE 'FM %%'")
