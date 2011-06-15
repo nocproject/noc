14d236ca2eaf43639959d31a97912217c7821950
Dmitry Volodin <dvolodin7@gmail.com>
2011-06-15 10:19:58 +0400

    Event Manager

--- fm/apps/event/views.py
+++ fm/apps/event/views.py
@@ -1,150 +1,200 @@
 # -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
-## <<DESCRIPTION>>
+## FM event application
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2010 The NOC Project
+## Copyright (C) 2007-2011 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
+
+## Django modules
 from django import forms
-from django.shortcuts import get_object_or_404
-from django.forms.widgets import HiddenInput
-from noc.lib.widgets import AutoCompleteTextInput,lookup
-from noc.lib.app import Application,HasPerm
+from django.forms.widgets import HiddenInput, DateTimeInput
+from django.utils.dateformat import DateFormat
+from django.http import Http404
+## NOC modules
+from noc.lib.widgets import AutoCompleteTextInput, lookup, TreePopupField
+from noc.lib.app import Application, HasPerm, view
 from noc.fm.models import *
 from noc.sa.models import ManagedObject
-from django.utils.dateformat import DateFormat
-##
-## Event Manager
-##
+
+
 class EventAppplication(Application):
-    title="Events"
+    """
+    Event manager
+    """
+    title = "Events"
     ## Amount of events per page
-    PAGE_SIZE=20
-    ##
-    ## Event search form
-    ##
+    PAGE_SIZE = 50  # @todo: move to application config
+
+    def get_event_or_404(self, event_id):
+        """
+        Return event or raise 404
+        """
+        e = get_event(event_id)
+        if e:
+            return e
+        raise Http404("Event not found: %s" % event_id)
+
     class EventSearchForm(Application.Form):
-        page=forms.IntegerField(required=False,min_value=0,widget=HiddenInput)
-        from_time=forms.DateTimeField(required=False,input_formats=["%d.%m.%Y %H:%M:%S"])
-        to_time=forms.DateTimeField(required=False,input_formats=["%d.%m.%Y %H:%M:%S"])
-        managed_object=forms.CharField(required=False,widget=AutoCompleteTextInput("sa:managedobject:lookup"))
-        event_category=forms.ModelChoiceField(required=False,queryset=EventCategory.objects.all())
-        event_class=forms.ModelChoiceField(required=False,queryset=EventClass.objects.all())
-        status=forms.ChoiceField(required=False,choices=[("","---------")]+EVENT_STATUS_CHOICES)
-        event_priority=forms.ModelChoiceField(required=False,queryset=EventPriority.objects.all())
-        subject=forms.CharField(required=False)
-    ##
-    ## Display event list
-    ##
-    def view_index(self,request):
-        initial={"status":"A"}
-        try:
-            initial["event_priority"]=EventPriority.objects.get(name="WARNING").id
-        except EventPriority.DoesNotExist:
-            pass
-        form=self.EventSearchForm(initial=initial)
-        return self.render(request,"index.html",{"form":form})
-    view_index.url=r"^$"
-    view_index.access=HasPerm("view")
-    view_index.menu="Events"
-    ##
-    ## Display event colorification CSS
-    ## <!> use cache
-    def view_css(self,request):
-        text="\n\n".join([p.css_style for p in EventPriority.objects.all()])
-        return self.render_plain_text(text,mimetype="text/css")
-    view_css.url=r"css/$"
-    view_css.url_name="css"
-    view_css.access=HasPerm("view")
-    ##
-    ## Display event
-    ##
-    def view_event(self,request,event_id):
-        event=get_object_or_404(Event,id=int(event_id))
-        return self.render(request,"event.html",{"e":event})
-    view_event.url=r"^(?P<event_id>\d+)/$"
-    view_event.url_name="event"
-    view_event.access=HasPerm("view")
-    ##
-    ## Mark event as open
-    ##
-    def view_open(self,request,event_id):
-        event=get_object_or_404(Event,id=int(event_id))
-        event.open_event("Event opened by '%s'"%request.user.username)
-        return self.response_redirect_to_referrer(request,self.base_url+"%d/"%event.id)
-    view_open.url=r"^(?P<event_id>\d+)/open/$"
-    view_open.url_name="open"
-    view_open.access=HasPerm("change")
-    ##
-    ## Mark event as closed
-    ##
-    def view_close(self,request,event_id):
-        event=get_object_or_404(Event,id=int(event_id))
-        event.close_event("Event closed by '%s'"%request.user.username)
-        return self.response_redirect_to_referrer(request,self.base_url+"%d/"%event.id)
-    view_close.url=r"^(?P<event_id>\d+)/close/$"
-    view_close.url_name="close"
-    view_close.access=HasPerm("close")
-    ##
-    ## Begin event reclassification
-    ##
-    def view_reclassify(self,request,event_id):
-        event=get_object_or_404(Event,id=int(event_id))
-        event.reclassify_event("Reclassification requested by '%s'"%request.user.username)
-        return self.response_redirect_to_referrer(request,self.base_url+"%d/"%event.id)
-    view_reclassify.url=r"^(?P<event_id>\d+)/reclassify/$"
-    view_reclassify.url_name="reclassify"
-    view_reclassify.access=HasPerm("reclassify")
-    ##
-    ## Retrieve JSON list of events
-    ## Returns:
-    ## { 
-    ##    count:
-    ##    page:
-    ##    pages:
-    ##    events: [list of events]
-    ##
-    def view_events(self,request):
-        datetime_format=self.config.get("main","datetime_format")
-        events=Event.objects
-        page=0
-        if request.GET:
-            form=self.EventSearchForm(request.GET)
+        """
+        Event filter form
+        """
+        page = forms.IntegerField(required=False, min_value=0, widget=HiddenInput)
+        from_time = forms.DateTimeField(label="From time",
+                                        required=False,
+                                        input_formats=["%d.%m.%Y %H:%M:%S"],
+                                        widget=DateTimeInput(format="%d.%m.%Y %H:%M:%S"))
+        to_time = forms.DateTimeField(label="To time",
+                                      required=False,
+                                      input_formats=["%d.%m.%Y %H:%M:%S"])
+        managed_object = forms.CharField(label="Managed Object",
+                                         required=False,
+                                         widget=AutoCompleteTextInput("sa:managedobject:lookup"))
+        status = forms.ChoiceField(label="Status",
+                                   required=False,
+                                   choices=[("N", "New"),
+                                            ("F", "Failed"),
+                                            ("A", "Active"),
+                                            ("S", "Archived")])
+        event_class = TreePopupField(label="Event Class",
+                                     required=False,
+                                     document=EventClass,
+                                     title="Select Event Class",
+                                     lookup="/fm/eventclass/popup/")
+
+    @view(url=r"^$", url_name="index", menu="Events", access=HasPerm("view"))
+    def view_index(self, request):
+        """
+        Display event list and search form
+        """
+        initial = {"status": "A"}
+        form = self.EventSearchForm(initial=initial)
+        return self.render(request, "index.html", form=form)
+
+    @view(url="^(?P<event_id>[0-9a-f]{24})/$", url_name="event",
+          access=HasPerm("view"))
+    def view_event(self, request, event_id):
+        """
+        Display event
+        """
+        event = self.get_event_or_404(event_id)
+        u_lang = request.session["django_language"]
+        if event.status in ("A", "S"):
+            subject = event.get_translated_subject(u_lang)
+            body = event.get_translated_body(u_lang)
+            symptoms = event.get_translated_symptoms(u_lang)
+            probable_causes = event.get_translated_probable_causes(u_lang)
+            recommended_actions = event.get_translated_recommended_actions(u_lang)
+        else:
+            subject = ""
+            body = ""
+            symptoms = ""
+            probable_causes = ""
+            recommended_actions = ""
+        return self.render(request, "event.html", e=event,
+                           subject=subject,
+                           body=body,
+                           symptoms=symptoms,
+                           probable_causes=probable_causes,
+                           recommended_actions=recommended_actions)
+
+    @view(url="^(?P<event_id>[0-9a-f]{24})/reclassify/$", url_name="reclassify",
+          access=HasPerm("change"))
+    def view_reclassify(self, request, event_id):
+        """
+        Mark event as new
+        """
+        event = self.get_event_or_404(event_id)
+        if event.status not in ("A", "C", "F"):
+            return self.response_forbidden("Invalid event state for requested action")
+        event.mark_as_new("Event reclassification has been requested by user %s" % request.user.username)
+        self.message_user(request, "Event has been reclassified")
+        return self.response_redirect_to_referrer(request)
+
+    class MessageForm(Application.Form):
+        message = forms.CharField()
+
+    @view(url="^(?P<event_id>[0-9a-f]{24})/message/$", url_name="message",
+          access=HasPerm("change"))
+    def view_message(self, request, event_id):
+        """
+        Submit new message to event
+        """
+        event = self.get_event_or_404(event_id)
+        if request.POST:
+            form = self.MessageForm(request.POST)
             if form.is_valid():
+                event.log_message("%s: %s" % (request.user.username,
+                                              form.cleaned_data["message"]))
+                self.message_user(request, "Message posted")
+                return self.response_redirect_to_referrer(request)
+        self.message_user(request, "Message posting failed")
+        return self.response_redirect_to_referrer(request)
+
+    @view(url=r"^events/$", url_name="events", access=HasPerm("view"))
+    def view_events(self, request):
+        """
+        Return JSON list of selected events.
+        Returned structure is dict of:
+            count:
+            page:
+            pages:
+            events: [list_of_events]
+        """
+        datetime_format = self.config.get("main", "datetime_format")
+        page = 0
+        # Process request
+        if request.GET:
+            form = self.EventSearchForm(request.GET)
+            if form.is_valid():
+                status = form.cleaned_data["status"]
+                # Select event collection
+                events = {
+                    "N": NewEvent,
+                    "A": ActiveEvent,
+                    "F": FailedEvent,
+                    "S": ArchivedEvent
+                }[status].objects
                 ## Apply additional restriction
                 if form.cleaned_data["page"]:
-                    page=form.cleaned_data["page"]-1
+                    page = form.cleaned_data["page"] - 1
                 if form.cleaned_data["from_time"]:
-                    events=events.filter(timestamp__gte=form.cleaned_data["from_time"])
+                    events = events.filter(timestamp__gte=form.cleaned_data["from_time"])
                 if form.cleaned_data["to_time"]:
-                    events=events.filter(timestamp__lte=form.cleaned_data["to_time"])
+                    events = events.filter(timestamp__lte=form.cleaned_data["to_time"])
                 if form.cleaned_data["managed_object"]:
                     try:
-                        mo=ManagedObject.objects.get(name=form.cleaned_data["managed_object"])
-                        events=events.filter(managed_object=mo)
+                        mo = ManagedObject.objects.get(name=form.cleaned_data["managed_object"])
+                        events = events.filter(managed_object=mo.id)
                     except ManagedObject.DoesNotExist:
                         pass
-                if form.cleaned_data["event_class"]:
-                    events=events.filter(event_class=form.cleaned_data["event_class"])
-                if form.cleaned_data["status"]:
-                    events=events.filter(status=form.cleaned_data["status"])
-                if form.cleaned_data["event_priority"]:
-                    events=events.filter(event_priority__priority__gte=form.cleaned_data["event_priority"].priority)
-                if form.cleaned_data["event_category"]:
-                    events=events.filter(event_category=form.cleaned_data["event_category"])
-                if form.cleaned_data["subject"]:
-                    events=events.filter(subject__icontains=form.cleaned_data["subject"])
-        # Return list of events
-        count=events.count()
+                if status in ("A", "C") and "event_class" in form.cleaned_data and form.cleaned_data["event_class"]:
+                    events = events.filter(event_class=form.cleaned_data["event_class"].id)
+            else:
+                return self.render_json({"error": str(form.errors)})
+        lr = self.PAGE_SIZE * page
+        rr = self.PAGE_SIZE * (page + 1)
+        data = []
+        count = events.count()
+        u_lang = request.session["django_language"]
+        for e in events.order_by("-timestamp")[lr:rr]:
+            if e.status in ("A", "S"):
+                subject = e.get_translated_subject(u_lang)
+                event_class = e.event_class.name
+            else:
+                subject = ""
+                event_class = ""
+            data += [[
+                str(e.id),
+                e.managed_object.name,
+                DateFormat(e.timestamp).format(datetime_format),
+                e.status,
+                event_class,
+                subject
+            ]]
         return self.render_json({
             "count" : count,
             "page"  : page,
-            "pages" : count/self.PAGE_SIZE+(1 if count%self.PAGE_SIZE else 0),
-            "events": [[e.event_priority.css_style_name,e.id,e.managed_object.name,DateFormat(e.timestamp).format(datetime_format),
-                        e.status,
-                        e.event_category.name,e.event_class.name,e.event_priority.name,e.subject]
-                        for e in events.order_by("-timestamp")[self.PAGE_SIZE*page:self.PAGE_SIZE*(page+1)]]
-        })
-    view_events.url=r"^events/$"
-    view_events.url_name="events"
-    view_events.access=HasPerm("view")
+            "pages" : count / self.PAGE_SIZE + (1 if count % self.PAGE_SIZE else 0),
+            "events": data
+            })
