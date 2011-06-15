09fb4d2680e90f7951f0ddc30003b44254985b5b
Dmitry Volodin <dvolodin7@gmail.com>
2011-06-15 10:23:41 +0400

    Remove postgresql's transactional stuff

--- fm/management/commands/import-mib.py
+++ fm/management/commands/import-mib.py
@@ -1,22 +1,24 @@
 # -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
+## Load MIBs
+##----------------------------------------------------------------------
 ## Copyright (C) 2007-2009 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 """
 """
-from django.core.management.base import BaseCommand,CommandError
-from django.db import transaction
-from noc.fm.models import MIB,MIBRequiredException
+## Django modules
+from django.core.management.base import BaseCommand, CommandError
+## NOC modules
+from noc.fm.models import MIB, MIBRequiredException
+
 
 class Command(BaseCommand):
-    help="Import MIB into database"
+    help = "Import MIBs into database"
+
     def handle(self, *args, **options):
-        transaction.enter_transaction_management()
         for a in args:
             try:
                 MIB.load(a)
             except MIBRequiredException, why:
                 raise CommandError(why)
-        transaction.commit()
-        transaction.leave_transaction_management()
