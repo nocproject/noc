a4508c528f2db0d0823bb4613a64e912bf3d852b
Dmitry Volodin <dvolodin7@gmail.com>
2011-07-19 01:37:16 +0400

    "Refresh Config" event trigger

--- main/pyrules/refresh_config.py
+++ main/pyrules/refresh_config.py
@@ -2,27 +2,28 @@
 ##----------------------------------------------------------------------
 ## Refresh config
 ##----------------------------------------------------------------------
-## INTERFACE: IEvent
+## INTERFACE: IEventTrigger
 ##----------------------------------------------------------------------
 ## DESCRIPTION:
 ## Reschedule device configuration to fetch
+## Used in event trigger for Config | Config Changed event class
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2010 The NOC Project
+## Copyright (C) 2007-2011 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 import datetime
 
-DELAY=10 # Minutes to delay config fetch
+DELAY = 10  # Minutes to delay config fetch
+
 
 @pyrule
 def refresh_config(event):
-    next_pull=event.timestamp+datetime.timedelta(minutes=DELAY)
-    if next_pull<datetime.datetime.now(): # next_pull points to the past
+    next_pull = event.timestamp + datetime.timedelta(minutes=DELAY)
+    if next_pull < datetime.datetime.now():  # next_pull points to the past
         return
     try:
-        config=event.managed_object.config
+        config = event.managed_object.config
     except:
-        return # No config found
-    config.next_pull=next_pull
+        return  # No config found
+    config.next_pull = next_pull
     config.save()
-
