8df87c434b752a0a5d66178b36cfe3d3fa594d29
Dmitry Volodin <dvolodin7@gmail.com>
2011-07-19 12:05:23 +0400

    Bugfix

--- fm/migrations/0007_event_repeat.py
+++ fm/migrations/0007_event_repeat.py
@@ -11,7 +11,9 @@ from noc.fm.models import *
 class Migration:
     
     def forwards(self):
-        
+        # Mock Models
+        Event = db.mock_model(model_name='Event', db_table='fm_event', db_tablespace='', pk_field_name='id', pk_field_type=models.AutoField)
+
         # Model 'EventRepeat'
         db.create_table('fm_eventrepeat', (
             ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
