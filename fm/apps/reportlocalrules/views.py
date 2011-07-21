955403c4662982c13f9924ee856d75c8c986315f
Dmitry Volodin <dvolodin7@gmail.com>
2011-07-21 11:49:40 +0400

    Local Classification Rules report

---
+++ fm/apps/reportlocalrules/views.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Local Classification Rules Report
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+from noc.lib.app.reportapplication import ReportApplication
+from noc.fm.models import EventClassificationRule
+from noc.lib.text import indent
+
+class ReportClassificationRules(ReportApplication):
+    title = "Local Classification Rules (JSON)"
+    
+    def report_html(self):
+        r = ["["]
+        r += [",\n".join([indent(r.to_json())
+                          for r in EventClassificationRule.objects.filter(is_builtin=False)])]
+        r += ["]",""]
+        return "<pre>" + "\n".join(r) + "</pre>"
