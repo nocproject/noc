1da538d903c6044ec65c6b7f64ff3d6030ee911d
Dmitry Volodin <dvolodin7@gmail.com>
2011-07-24 23:30:41 +0400

    Count canned beef as tests

--- main/apps/reportloc/views.py
+++ main/apps/reportloc/views.py
@@ -2,34 +2,50 @@
 ##----------------------------------------------------------------------
 ## Lines of Code  Report
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2010 The NOC Project
+## Copyright (C) 2007-2011 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
+
+## Python modules
 from __future__ import with_statement
-from noc.lib.app.simplereport import SimpleReport,TableColumn,SectionRow
+import os
+import glob
+## NOC modules
+from noc.lib.app.simplereport import SimpleReport, TableColumn, SectionRow
 from noc import settings
-import os,glob
-##
-##
-##
-class Reportreportloc(SimpleReport):
-    title="Lines Of Code"
+
+
+class ReportLOC(SimpleReport):
+    title = "Lines Of Code"
+
     def get_data(self,**kwargs):
         def lines(path):
+            """
+            Count lines in file
+            """
             with open(path) as f:
                 return len(f.read().split("\n"))
-        # Scan directory and return LoC data
+
         def dir_loc(path):
-            py_loc=0
-            html_loc=0
-            for dirpath,dirnames,filenames in os.walk(path):
+            """
+            Scan directory and count lines of code
+            """
+            py_loc = 0
+            html_loc = 0
+            tests_loc = 0
+            for dirpath, dirnames, filenames in os.walk(path):
+                in_tests = "tests" in dirpath.split(os.sep)
                 for f in filenames:
                     path=os.path.join(dirpath,f)
                     if f.endswith(".py"):
-                        py_loc+=lines(path)
+                        if in_tests:
+                            tests_loc += lines(path)
+                        else:
+                            py_loc += lines(path)
                     elif f.endswith(".html"):
-                        html_loc+=lines(path)
-            return py_loc,html_loc
+                        html_loc += lines(path)
+            return py_loc, html_loc, tests_loc
+
         data=[]
         # Scan modules
         for m in [m for m in settings.INSTALLED_APPS if m.startswith("noc.")]:
@@ -38,18 +54,18 @@ class Reportreportloc(SimpleReport):
             data+=[SectionRow(module_name)]
             # Scan models
             py_loc=lines(os.path.join(m,"models.py"))
-            tests_loc,x=dir_loc(os.path.join(m,"tests"))
+            _, _, tests_loc = dir_loc(os.path.join(m, "tests"))
             data+=[["Model","models.py",py_loc,0,tests_loc]]
             # Scan Migrations
-            py_loc,html_loc=dir_loc(os.path.join(m,"migrations"))
-            data+=[["Migrations","",py_loc,html_loc,0]]
+            py_loc, _, _ = dir_loc(os.path.join(m, "migrations"))
+            data+=[["Migrations","",py_loc,0,0]]
             # Scan Management
             for dirpath,dirnames,filenames in os.walk(os.path.join(m,"management","commands")):
                 for f in [f for f in filenames if f.endswith(".py") and f!="__init__.py"]:
                     py_loc=lines(os.path.join(dirpath,f))
                     data+=[["Management",f[:-3],py_loc,0,0]]
             # Scan Templates
-            py_loc,html_loc=dir_loc(os.path.join(m,"templates"))
+            py_loc,html_loc,_=dir_loc(os.path.join(m,"templates"))
             data+=[["Templates","",py_loc,html_loc,0]]
             # Scan applications
             for app in [d for d in os.listdir(os.path.join(m,"apps")) if not d.startswith(".")]:
@@ -76,8 +92,8 @@ class Reportreportloc(SimpleReport):
                         continue
                     pp=d.split(os.sep)
                     profile=".".join(pp[-2:])
-                    py_loc,html_loc=dir_loc(d)
-                    data+=[["Profile",profile,py_loc,html_loc,0]]
+                    py_loc,html_loc,tests_loc=dir_loc(d)
+                    data+=[["Profile",profile,py_loc,html_loc,tests_loc]]
             # Scan other
             py_loc=0
             for dirpath,dirnames,filenames in os.walk(m):
