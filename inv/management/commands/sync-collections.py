6d4bed692646ebd87a5ed0132f11c84d005ed4eb
Dmitry Volodin <dvolodin7@gmail.com>
2011-07-29 00:59:12 +0400

    Fix circular reference bug and unlock inventory collections

--- inv/management/commands/sync-collections.py
+++ inv/management/commands/sync-collections.py
@@ -10,6 +10,7 @@
 ## Python modules
 from __future__ import with_statement
 import os
+import pprint
 ## Django modules
 from django.core.management.base import BaseCommand, CommandError
 from django.utils import simplejson
@@ -19,16 +20,185 @@ from noc.fm.models import *
 from noc.lib.debug import error_report
 
 
+class CollectionSync(object):
+    def __init__(self, app, name, doc):
+        self.app = app
+        self.name = name
+        self.doc = doc
+        self.builtin_ids = set()  # existing builtin ids
+        self.unique = set()  # unique fields
+        self.ref_cache = {}  # model -> field -> key -> value
+
+    def get_data(self):
+        # Look  <app>/collections/<name>/ tree for JSON files
+        for dirpath, dirnames, files in os.walk(os.path.join(self.app,
+                                                             "collections",
+                                                             self.name)):
+            for f in files:
+                if f.startswith(".") or not f.endswith(".json"):
+                    continue
+                # Load JSON
+                path = os.path.join(dirpath, f)
+                with open(path, "r") as jf:
+                    try:
+                        data = simplejson.JSONDecoder().decode(jf.read())
+                    except ValueError, why:
+                        self.die("JSON error in %s: %s" % (path, why))
+                # Data must be either dict or list
+                if type(data) == type({}):
+                    # Convert dict to list
+                    data = [data]
+                if type(data) != type([]):
+                    self.die("Invalid JSON data type: %s" % type(data))
+                for d in data:
+                    yield d
+
+    def log(self, msg):
+        print "    " + msg
+    
+    def die(self, msg):
+        raise CommandError("noc.%s: %s" % (self.name, msg))
+
+    def get_item_label(self, d):
+        return ", ".join([u"%s = '%s'" % (u, d[u])
+                          for u in self.unique if u in d])
+
+    def lookup(self, ref, field, key):
+        field = str(field)
+        if ref not in self.ref_cache:
+            self.ref_cache[ref] = {field: {}}
+        if field not in self.ref_cache[ref]:
+            self.ref_cache[ref][field] = {}
+        if key in self.ref_cache[ref][field]:
+            return self.ref_cache[ref][field][key]
+        else:
+            try:
+                v = ref.objects.get(**{field: key})
+            except ref.DoesNotExist:
+                self.die("lookup for %s.%s == '%s' has been failed" % (
+                    ref._meta["collection"], field, key))
+            self.ref_cache[ref][field][key] = v
+            return v
+
+    def sync_item(self, d, allow_partial=True):
+        changed = False
+        created = False
+        partial = False
+        # Forcefully set is_builtin
+        d["is_builtin"] = True
+        # Build key for lookup
+        k = dict([(u, d[u]) for u in self.unique if u in d])
+        # Find existing record
+        obj = self.doc.objects.filter(**k).first()
+        if not obj:
+            # Create record if not found
+            obj = self.doc()
+            created = True
+        # Compare attributes
+        for i, v in d.items():
+            i = str(i)
+            # Ignore id field
+            if i == "id":
+                continue
+            # Dereference ref_field__field lookups
+            if "__" in i and not i.startswith("__"):
+                i, f = i.split("__")
+                if i not in self.doc._fields:
+                    self.die("Invalid lookup field: %s" % i)
+                ref = self.doc._fields[i].document_type
+                v = self.lookup(ref, f, v)
+                cv = getattr(obj, i)
+                if cv is None or cv.id != v.id:
+                    changed = True
+                    setattr(obj, i, v)
+                continue
+            # Process fields
+            field = self.doc._fields[i]
+            if (type(field) == ListField and
+                isinstance(field.field, EmbeddedDocumentField)):
+                # ListField(EmbeddedDocumentField(...))
+                v = []
+                edoc = field.field.document_type
+                for dd in d[i]:
+                    for ii in dd.keys():
+                        if "__" in ii and not ii.startswith("__"):
+                            vv = dd[ii]
+                            ii, f = ii.split("__")
+                            ref = edoc._fields[ii].document_type
+                            if allow_partial and self.doc == ref:
+                                # Circular reference
+                                # Can be unresolved
+                                # for this moment
+                                try:
+                                    dd[ii] = self.lookup(ref, f, vv)
+                                except CommandError:
+                                    # Try to skip and resolve later
+                                    partial = True
+                                    continue
+                            else:
+                                dd[ii] = self.lookup(ref, f, vv)
+                    dd = dict([(str(x), y) for x, y in dd.items()])
+                    v += [edoc(**dd)]
+                if getattr(obj, i) != v:
+                    changed = True
+                    setattr(obj, i, v)
+            elif getattr(obj, i) != v:
+                # Other types
+                changed = True
+                setattr(obj, i, v)
+        # Save and report changes if exists
+        if created:
+            self.log("Creating: %s" % unicode(obj))
+            obj.save()
+        elif changed:
+            self.log("Updating: %s" % unicode(obj))
+            obj.save()
+        return str(obj.id), partial
+
+    def sync(self):
+        print "Syncing noc.%s:" % self.name
+        # Get builtin ids
+        self.builtin_ids = set([str(o._id) for o
+                                in self.doc.objects.filter(is_builtin=True)])
+        # Define set of unique fields
+        self.unique = set()
+        for index in self.doc._meta["unique_indexes"]:
+            for f, flag in index:
+                self.unique.add(f)
+        # Sync items
+        retry = []
+        for d in self.get_data():
+            item_id, to_retry = self.sync_item(d, True)
+            try:
+                self.builtin_ids.remove(item_id)
+            except KeyError:
+                pass
+            if to_retry:
+                retry += [d]
+        # Retry circular references
+        for d in retry:
+            self.log("Refining: %s" % self.get_item_label(d))
+            item_id, to_retry = self.sync_item(d, False)
+            if to_retry:
+                self.die("Unable to resolve circular reference for %s" % (
+                    self.get_item_label(d)))
+        # Remove obsolete builtins
+        if self.builtin_ids:
+            for o in self.doc.objects.filter(id__in=self.builtin_ids):
+                self.log("Removing: %s" % unicode(o))
+                o.delete()
+
+
 class Command(BaseCommand):
     help = "Syncronize built-in inventory collections"
 
     collections = [
-        #("inv", [
-        #    # Inventory
-        #    ("sockets", Socket),
-        #    ("vendors", Vendor),
-        #    ("models", Model)
-        #]),
+        ("inv", [
+            # Inventory
+            ("sockets", Socket),
+            ("vendors", Vendor),
+            ("models", Model)
+        ]),
         ("fm", [
             # Fault management
             ("alarmseverities", AlarmSeverity),
@@ -40,151 +210,11 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
-            self._handle(*args, **options)
+            # Sync all collections
+            for app, collections in self.collections:
+                for collection, doc in collections:
+                    CollectionSync(app, collection, doc).sync()
         except CommandError, why:
             raise CommandError(why)
         except:
             error_report()
-
-    def _handle(self, *args, **options):
-        def lookup(doc, field, key):
-            field = str(field)
-            if ref not in ref_cache:
-                ref_cache[doc] = {field: {}}
-            if field not in ref_cache[doc]:
-                ref_cache[doc][field] = {}
-            if key in ref_cache[doc][field]:
-                return ref_cache[doc][field][key]
-            else:
-                try:
-                    v = ref.objects.get(**{field: key})
-                except ref.DoesNotExist:
-                    raise CommandError("%s: lookup for %s.%s == '%s' has been failed" % (
-                        collection, doc._meta["collection"], field, key))
-                ref_cache[doc][field][key] = v
-                return v
-        
-        def fail(collection, msg):
-            if not collection.startswith("noc."):
-                collection = "noc." + collection
-            raise CommandError("%s: %s" % (collection, msg))
-            
-        # For each system collection
-        ref_cache = {}  # model -> field -> key -> value
-        for app, collections in self.collections:
-            for collection, doc in collections:
-                c_root = os.path.join(app, "collections")
-                print "Syncing noc.%s" % collection
-                builtin_ids = set([str(o._id) for o
-                                   in doc.objects.filter(is_builtin=True)])
-                # Define set of unique fields
-                unique = set()
-                for index in doc._meta["unique_indexes"]:
-                    for f, flag in index:
-                        unique.add(f)
-                # Scan for json
-                for dirpath, dirnames, files in os.walk(os.path.join(c_root,
-                                                                     collection)):
-                    for f in files:
-                        if f.startswith(".") or not f.endswith(".json"):
-                            continue
-                        # Load json
-                        path = os.path.join(dirpath, f)
-                        with open(path, "r") as jf:
-                            try:
-                                data = simplejson.JSONDecoder().decode(jf.read())
-                            except ValueError, why:
-                                fail(collection, "JSON error in %s: %s" % (path,
-                                                                           why))
-                                # Data must be either dict or list
-                        if type(data) == type({}):
-                            # Convert dict to list
-                            data = [data]
-                        if type(data) != type([]):
-                            fail(collection,
-                                 "invalid JSON data type: %s" % type(data))
-                        # Process data items (dicts)
-                        for d in data:
-                            changed = False
-                            created = False
-                            # Forcefully set is_builtin
-                            d["is_builtin"] = True
-                            # Build key for lookup
-                            k = dict([(u, d[u]) for u in unique if u in d])
-                            # Find existing record
-                            obj = doc.objects.filter(**k).first()
-                            if not obj:
-                                # Create record if not found
-                                obj = doc()
-                                created = True
-                            else:
-                                try:
-                                    builtin_ids.remove(str(obj._id))
-                                except KeyError:
-                                    pass
-                            # Compare attributes
-                            for i, v in d.items():
-                                i = str(i)
-                                # Ignore id field
-                                if i == "id":
-                                    continue
-                                # Dereference ref_field__field lookups
-                                if "__" in i and not i.startswith("__"):
-                                    i, f = i.split("__")
-                                    if i not in doc._fields:
-                                        fail("Invalid lookup field: %s" % i)
-                                    ref = doc._fields[i].document_type
-                                    v = lookup(ref, f, v)
-                                    cv = getattr(obj, i)
-                                    if cv is None or cv.id != v.id:
-                                        changed = True
-                                        setattr(obj, i, v)
-                                    continue
-                                # Process fields
-                                field = doc._fields[i]
-                                if (type(field) == ListField and
-                                    isinstance(field.field, EmbeddedDocumentField)):
-                                    # ListField(EmbeddedDocumentField(...))
-                                    v = []
-                                    edoc = field.field.document_type
-                                    for dd in d[i]:
-                                        for ii in dd.keys():
-                                            if "__" in ii and not ii.startswith("__"):
-                                                vv = dd.pop(ii)
-                                                ii, f = ii.split("__")
-                                                ref = edoc._fields[ii].document_type
-                                                if doc == ref:
-                                                    # Circular reference
-                                                    # Can be unresolved
-                                                    # for this moment
-                                                    try:
-                                                        dd[ii] = lookup(ref, f, vv)
-                                                    except CommandError:
-                                                        # Try to skip and resolve later
-                                                        continue
-                                                else:
-                                                    dd[ii] = lookup(ref, f, vv)
-                                        dd = dict([(str(x), y) for x, y in dd.items()])
-                                        v += [edoc(**dd)]
-                                    if getattr(obj, i) != v:
-                                        changed = True
-                                        setattr(obj, i, v)
-                                elif getattr(obj, i) != v:
-                                    # Other types
-                                    changed = True
-                                    setattr(obj, i, v)
-                            # Save and report changes if exists
-                            if created or changed:
-                                t = ", ".join([u"%s = '%s'" % (x, y)
-                                               for x, y in k.items()])
-                                print "    %s in noc.%s: %s" % (
-                                    "Creating" if created else "Updating",
-                                    collection,
-                                    t
-                                )
-                                obj.save()
-                # Remove hanging builtins
-                if builtin_ids:
-                    for o in doc.objects.filter(id__in=builtin_ids):
-                        print "    Removing from noc.%s: %s" % (collection, unicode(o))
-                        o.delete()
