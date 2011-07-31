054390f44b369ee7ef2fdb9f6139a85c8214eca1
Dmitry Volodin <dvolodin7@gmail.com>
2011-07-31 23:08:56 +0400

    Peer.get_peer()

--- main/pyrules/ae_network_bgp_peer_down.py
+++ main/pyrules/ae_network_bgp_peer_down.py
@@ -15,19 +15,18 @@ from noc.peer.models import Peer
 
 @pyrule
 def ae_network_bgp_peer_down(alarm):
-    peer = alarm.vars["peer"]
-    for p in Peer.objects.all():
-        if (p.remote_ip.split("/")[0] == peer
-            or (p.remote_backup_ip and p.remote_backup_ip.split("/")[0] == p)):
-            return {
-                "local_as": p.local_asn.asn,
-                "remote_as": p.remote_asn,
-                "local_ip": p.local_ip,
-                "remote_ip": p.remote_ip,
-                "local_backup_ip": p.local_backup_ip,
-                "remote_backup_ip": p.remote_backup_ip,
-                "import_filter": p.import_filter,
-                "export_filter": p.export_filter,
-                "description": p.description
-            }
-    return {}
+    p = Peer.get_peer(alarm.vars["peer"])
+    if not p:
+        return {}
+    else:
+        return {
+            "local_as": p.local_asn.asn,
+            "remote_as": p.remote_asn,
+            "local_ip": p.local_ip,
+            "remote_ip": p.remote_ip,
+            "local_backup_ip": p.local_backup_ip,
+            "remote_backup_ip": p.remote_backup_ip,
+            "import_filter": p.import_filter,
+            "export_filter": p.export_filter,
+            "description": p.description
+        }
