b8153d76f5fcf44eafe2a7a458ab300dfdc22842
Dmitry Volodin <dvolodin7@gmail.com>
2011-08-04 13:29:23 +0400

    "Test" admin action

--- fm/apps/eventtrigger/views.py
+++ fm/apps/eventtrigger/views.py
@@ -6,11 +6,13 @@
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 
+## NOC modules
+import re
 ## Django modules
 from django.contrib import admin
 ## NOC modules
-from noc.lib.app import ModelApplication
-from noc.fm.models import EventTrigger
+from noc.lib.app import ModelApplication, view, HasPerm
+from noc.fm.models import EventTrigger, EventClass
 
 
 class EventTriggerAdmin(admin.ModelAdmin):
@@ -18,9 +20,29 @@ class EventTriggerAdmin(admin.ModelAdmin):
             "time_pattern", "selector", "notification_group",
             "template", "pyrule"]
     list_filter = ["is_enabled"]
+    actions = ["test_triggers"]
+
+    def test_triggers(self, request, queryset):
+        return self.app.response_redirect("test/%s/" % ",".join(
+                                                [str(p.id) for p in queryset]))
+    test_triggers.short_description = "Test selected triggers"
 
 
 class EventTriggerApplication(ModelApplication):
     model = EventTrigger
     model_admin = EventTriggerAdmin
     menu = "Setup | Event Triggers"
+
+    @view(url=r"^test/(?P<objects>\d+(?:,\d+)*)/$",
+          url_name="test", access=HasPerm("change"))
+    def view_test(self, request, objects):
+        ecn = sorted([c.name for c in EventClass.objects.all()])
+        r = []
+        for o_id in objects.split(","):
+            t = self.get_object_or_404(EventTrigger, id=int(o_id))
+            rx = re.compile(t.event_class_re)
+            r += [{
+                    "trigger": t,
+                    "classes": [c for c in ecn if rx.search(c) is not None]
+                }]
+        return self.render(request, "test.html", r=r)
