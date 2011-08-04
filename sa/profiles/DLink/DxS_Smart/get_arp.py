de641dcbc9e4f1d654f68a9944db92fae02bdf87
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2011-08-04 14:50:51 +0300

    Add DLink.DxS_Smart profile to SA

---
+++ sa/profiles/DLink/DxS_Smart/get_arp.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## DLink.DxS_Smart.get_arp
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+""" 
+""" 
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetARP
+import re
+
+class Script(NOCScript):
+    name="DLink.DxS_Smart.get_arp" 
+    implements=[IGetARP]
+    rx_line=re.compile(r"(?P<ip>[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\s+(?P<mac>\S+)\s+ARPA\s+(?P<interface>\S+)\s+\S+", re.MULTILINE)
+    def execute(self):
+        r=[]
+        for match in self.rx_line.finditer(self.cli("debug info")):
+            r+=[{
+                "ip":match.group("ip"),
+                "mac":match.group("mac"),
+                "interface":match.group("interface"),
+            }]
+        return r
---
+++ sa/profiles/DLink/DxS_Smart/ping.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## DLink.DxS_Smart.ping
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IPing
+from noc.sa.profiles.DLink.DxS_Smart import DES1210
+import re
+
+class Script(NOCScript):
+    name="DLink.DxS_Smart.ping"
+    implements=[IPing]
+    rx_result=re.compile(r"^\s*(?P<count>\d+) Packets Transmitted, (?P<success>\d+) Packets Received, \d+% Packets Loss",re.MULTILINE|re.DOTALL|re.IGNORECASE)
+
+    @NOCScript.match(DES1210)
+    def execute_ping(self,address):
+        cmd="ping %s"%address
+        match=self.rx_result.search(self.cli(cmd))
+        if not match:
+            raise self.NotSupportedError()
+        return {
+            "success": match.group("success"),
+            "count"  : match.group("count"),
+        }
+
+    @NOCScript.match()
+    def execute_ping_other(self,address):
+        raise self.NotSupportedError()
