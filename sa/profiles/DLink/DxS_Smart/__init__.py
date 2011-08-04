1211e8d0ad2286f7489222be57ce25046a4f3af1
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2011-08-04 14:50:51 +0300

    Add DLink.DxS_Smart profile to SA

---
+++ sa/profiles/DLink/DxS_Smart/__init__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Vendor: D-Link
+## OS:     DxS
+## Compatible:
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+import re
+import noc.sa.profiles
+from noc.sa.protocols.sae_pb2 import TELNET
+
+class Profile(noc.sa.profiles.Profile):
+    name="DLink.DxS_Smart"
+    supported_schemes=[TELNET]
+    pattern_username="([Uu]ser ?[Nn]ame|[Ll]ogin):"
+    pattern_password="[Pp]ass[Ww]ord:"
+    pattern_more="--More--"
+    pattern_unpriveleged_prompt=r"^\S+:(3|6|user|operator)#"
+    pattern_syntax_error=r"% Invalid (Command|input detected at)"
+    command_super="enable admin"
+    pattern_prompt=r"(?P<hostname>\S+(:\S+)*)[#>]"
+    command_disable_pager=""
+    command_more=" "
+    command_exit="logout"
+    command_save_config="save"
+    config_volatile=["^%.*?$"]
+    ##
+    ## Version comparison
+    ## Version format:
+    ## <major>.<minor><sep><patch>
+    ##
+    rx_ver=re.compile(r"\d+")
+    def cmp_version(self, x, y):
+        return cmp([int(z) for z in self.rx_ver.findall(x)], [int(z) for z in self.rx_ver.findall(y)])
+
+## DES-1210-series
+def DES1210(v):
+    return v["platform"].startswith("DES-1210")
---
+++ sa/profiles/DLink/DxS_Smart/get_mac_address_table.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## DLink.DxS_Smart.get_mac_address_table
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetMACAddressTable
+import re
+
+class Script(NOCScript):
+    name="DLink.DxS_Smart.get_mac_address_table"
+    implements=[IGetMACAddressTable]
+    rx_line=re.compile(r"(?P<vlan_id>\d+)\s+(?P<mac>\S+)\s+(?P<type>Learnt|Static)\s+(?P<interfaces>\S+)",re.MULTILINE)
+    def execute(self,interface=None,vlan=None,mac=None):
+        cmd="debug info"
+        r=[]
+        for match in self.rx_line.finditer(self.cli(cmd)):
+            m_interface = match.group("interfaces")
+            m_vlan = match.group("vlan_id")
+            m_mac = match.group("mac")
+            if ((interface is None and vlan is None and mac is None) \
+            or (interface is not None and interface == m_interface) \
+            or (vlan is not None and vlan == m_vlan) \
+            or (mac is not None and mac == m_mac)):
+                r+=[{
+                    "vlan_id"   : m_vlan,
+                    "mac"       : m_mac,
+                    "interfaces": [m_interface],
+                    "type"      : {"Learnt":"D","Static":"S",}[match.group("type")],
+                }]
+        return r
