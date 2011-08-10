71aa8521fa136ab5c29d30c4c08fcc850c2f2a69
Dmitry Volodin <dvolodin7@gmail.com>
2011-08-10 11:16:45 +0400

    SystemTemplate.notify_users

--- main/models.py
+++ main/models.py
@@ -1305,12 +1305,39 @@ class SystemTemplate(models.Model):
     def __unicode__(self):
         return self.name
 
-    def render_subject(self, **kwargs):
-        return self.template.render_subject(**kwargs)
+    def render_subject(self, LANG=None, **kwargs):
+        return self.template.render_subject(lang=LANG, **kwargs)
 
-    def render_body(self, **kwargs):
-        return self.template.render_body(**kwargs)
+    def render_body(self, LANG=None, **kwargs):
+        return self.template.render_body(lang=LANG, **kwargs)
 
+    @classmethod
+    def notify_users(cls, name, users, **kwargs):
+        """
+        Send notifications via template to users
+        :param name: System template name
+        :param users: List of User instances or id's
+        """
+        # Find system template by name
+        try:
+            t = cls.objects.get(name=name)
+        except cls.DoesNotExist:
+            return
+        # Fix users
+        u_list = []
+        for u in users:
+            if type(u) in (types.IntType, types.LongType):
+                try:
+                    u_list += [User.objects.get(id=u)]
+                except User.DoesNotExist:
+                    continue
+            elif type(u) in (types.StringType, types.UnicodeType):
+                u_list += [User.objects.get(username=u)]
+            elif isinstance(u, User):
+                u_list += [u]
+        # Left only active users
+        u_list = [u for u in u_list if u.is_active]
+        # Send notifications
 
 class Checkpoint(models.Model):
     """
