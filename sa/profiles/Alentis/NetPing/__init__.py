f13a3f18b89c5cf172aea82c206dd170c569f1c8
Dmitry Volodin <dvolodin7@gmail.com>
2011-08-18 00:03:10 +0400

    Alentis.NetPing profile

---
+++ sa/profiles/Alentis/NetPing/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Vendor: Alentis
+## OS:     NetPing
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+import noc.sa.profiles
+from noc.sa.protocols.sae_pb2 import HTTP
+
+
+class Profile(noc.sa.profiles.Profile):
+    name = "Alentis.NetPing"
+    supported_schemes = [HTTP]
