05391180072fe759a9ab9b900007384d2cf0b81a
Dmitry Volodin <dvolodin7@gmail.com>
2011-08-19 07:39:00 +0400

    AlliedTelesis.AT9400 profile

---
+++ sa/profiles/AlliedTelesis/AT9400/get_mac_address_table.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## AlliedTelesis.AT9400.get_mac_address_table
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Python modules
+import re
+## NOC modules
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetMACAddressTable
+
+
+class Script(NOCScript):
+    name = "AlliedTelesis.AT9400.get_mac_address_table"
+    implements = [IGetMACAddressTable]
+    rx_line = re.compile(r"^\s*(?P<vlan_id>\d+)\s+(?P<mac>[:0-9a-fA-F]+)\s+(?P<interfaces>\d+)\s+(?P<type>[\(\)\,\-\w\s]+)$")
+
+    def execute(self, interface=None, vlan=None, mac=None):
+        cmd = "show switch fdb"
+        if mac is not None:
+            cmd += " address=%s" % mac
+        if interface is not None:
+            cmd += " port(s)=%s" % interface
+        if vlan is not None:
+            cmd += " vlan=%s" % vlan
+        self.cli("terminal datadump")
+        vlans = self.cli(cmd)
+        vlans = self.strip_first_lines(vlans, 4)
+        r = []
+        for l in vlans.split("\n"):
+            match = self.rx_line.match(l.strip())
+            if match:
+                r += [{
+                    "vlan_id"   : match.group("vlan_id"),
+                    "mac"       : match.group("mac"),
+                    "interfaces": [match.group("interfaces")],
+                    "type"      : {"Dynamic":"D", "Static":"S",
+                                   "Static (fixed,non-aging)":"S",
+                                   "Multicast":"M"}[match.group("type")],
+                }]
+        return r
---
+++ sa/profiles/AlliedTelesis/AT9400/get_version.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## AlliedTelesis.AT9400.get_version
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Python modules
+import re
+## NOC modules
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetVersion
+
+
+class Script(NOCScript):
+    name = "AlliedTelesis.AT9400.get_version"
+    cache = True
+    implements = [IGetVersion]
+    rx_ver = re.compile(r"^Model Name ...... (?P<platform>AT[/\w-]+).+^Application ..... ATS63 v(?P<version>\S+(\s\S+))\s\s", re.MULTILINE | re.DOTALL)
+
+    def execute(self):
+        if self.snmp and self.access_profile.snmp_ro:
+            try:
+                pl = self.snmp.get("1.3.6.1.4.1.207.8.17.1.3.1.6.1")
+                ver = self.snmp.get("1.3.6.1.4.1.207.8.17.1.3.1.5.1")
+                return {
+                    "vendor"    : "Allied Telesis",
+                    "platform"  : pl,
+                    "version"   : string.lstrip(ver, "v"),
+                }
+            except self.snmp.TimeOutError:
+                pass
+        v = self.cli("show system")
+        match = self.rx_ver.search(v)
+        return {
+            "vendor"  : "Allied Telesis",
+            "platform": match.group("platform"),
+            "version" : match.group("version"),
+        }
