0fd0b745f7182d1fca9367dc96efc44712864241
Dmitry Volodin <dvolodin7@gmail.com>
2011-08-30 17:26:40 +0400

    Bugfix

--- main/migrations/0027_style.py
+++ main/migrations/0027_style.py
@@ -1,11 +1,10 @@
 # encoding: utf-8
 import datetime
 from south.db import db
-from south.v2 import SchemaMigration
 from django.db import models
 from noc.main.models import *
 
-class Migration(SchemaMigration):
+class Migration:
 
     def forwards(self):
         # Model 'Color'
