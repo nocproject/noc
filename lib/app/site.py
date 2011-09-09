03c3388d191916d5711043a9a722e6b8be8b14f9
Dmitry Volodin <dv@nocproject.org>
2011-09-09 10:11:25 +0400

    HTTP-method aware request routing and optimized menu generation

--- lib/app/site.py
+++ lib/app/site.py
@@ -12,52 +12,18 @@ import types
 import glob
 import os
 import urllib
+import hashlib
 ## Django modules
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
 from django.utils.http import urlquote
 from django.conf.urls.defaults import *
 from django.core.urlresolvers import *
 from django.conf import settings
 from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.utils.simplejson.encoder import JSONEncoder
 ## NOC modules
 from noc.settings import INSTALLED_APPS
 from noc.lib.debug import error_report
-
-
-class Menu(object):
-    """
-    Application menu. Populated by Site object.
-    """
-    def __init__(self, app, title=None):
-        if title:
-            self.title = title
-        else:
-            # Try to fetch title from __init__.py
-            try:
-                m = __import__("noc.%s" % app, {}, {}, ["MODULE_NAME"])
-                self.title = m.MODULE_NAME
-            except ImportError:
-                self.title = app
-        self.app = app
-        self.submenus = []
-        self.items = []  # (title,url,access)
-
-    def get_submenu(self, title):
-        """
-        Returns Menu object assotiated with title
-        """
-        for sm in self.submenus:
-            if sm.title == title:
-                return sm
-        sm = Menu(self.app, title)
-        self.submenus += [sm]
-        return sm
-
-    def add_item(self, title, full_url, access):
-        """
-        Add menu item
-        """
-        self.items += [(title, full_url, access)]
 
 
 class DynamicMenu(object):
@@ -74,14 +40,35 @@ class DynamicMenu(object):
 class ProxyNode:
     pass
 
+HTTP_METHODS = set(["GET", "POST", "PUT", "DELETE"])
 
 class URL(object):
     """
     URL Data wrapper
     """
-    def __init__(self, url, name=None):
+    def __init__(self, url, name=None, method=None):
         self.url = url
         self.name = name
+        if method is None:
+            self.method = HTTP_METHODS
+        else:
+            if isinstance(method, basestring):
+                method = [method]
+            if type(method) not in (types.ListType, types.TupleType):
+                raise TypeError("Invalid type for 'method'")
+            for m in method:
+                if m not in HTTP_METHODS:
+                    raise ValueError("Invalid method '%s'" % m)
+            self.method = set(method)
+
+    def __repr__(self):
+        return "<URL %s>" % unicode(self)
+
+    def __unicode__(self):
+        s = self.url
+        if self.name:
+            s += ", name='%s'" % self.name
+        return s
 
 
 class Site(object):
@@ -94,7 +81,7 @@ class Site(object):
         self.urlpatterns = patterns("")
         self.urlresolvers = {}  # (module,appp) -> RegexURLResolver
         self.menu = []
-        self.app_menu = {}  # Module -> menu
+        self.menu_index = {}  # id -> menu item
         self.reports = []  # app_id -> title
         self.views = ProxyNode()  # Named views proxy
         self.testing_mode = hasattr(settings, "NOC_TEST")
@@ -110,21 +97,22 @@ class Site(object):
         """
         Register named application view
         """
-        if mod_ns and app_ns and name:
-            if not hasattr(self.views, mod_ns):
-                setattr(self.views, mod_ns, ProxyNode())
-            n = getattr(self.views, mod_ns)
-            if not hasattr(n, app_ns):
-                setattr(n, app_ns, ProxyNode())
-            n = getattr(n, app_ns)
-            setattr(n, name, view)
+        if not hasattr(self.views, mod_ns):
+            setattr(self.views, mod_ns, ProxyNode())
+        n = getattr(self.views, mod_ns)
+        if not hasattr(n, app_ns):
+            setattr(n, app_ns, ProxyNode())
+        n = getattr(n, app_ns)
+        setattr(n, name, view)
 
     def view_urls(self, view):
         """
         Generator returning view's URL objects
         """
         if isinstance(view.url, basestring):  # view.url is string type
-            yield URL(view.url, name=getattr(view, "url_name", None))
+            yield URL(view.url,
+                      name=getattr(view, "url_name", None),
+                      method=getattr(view, "method", None))
         elif isinstance(view.url, URL):  # Explicit URL object
             yield view.url
         elif (isinstance(view.url, types.ListType) or
@@ -138,57 +126,6 @@ class Site(object):
                     raise Exception("Invalid URL object: %s" % str(o))
         else:
             raise Exception("Invalid URL object: %s" % str(view.url))
-
-    def register_view(self, app, view):
-        """
-        Register application view
-        """
-        # Prepare namespaces
-        mod_ns = app.module
-        app_ns = app.app
-        # Install module URL resolver
-        try:
-            mr = self.urlresolvers[app.module, None]
-        except KeyError:
-            mr = patterns("")
-            self.urlpatterns += [RegexURLResolver("^%s/" % app.module, mr,
-                                                  namespace=mod_ns)]
-            self.urlresolvers[app.module, None] = mr
-        # Install application URL resolver
-        try:
-            ar = self.urlresolvers[app.module, app.app]
-        except KeyError:
-            ar = patterns("")
-            mr += [RegexURLResolver("^%s/" % app.app, ar, namespace=app_ns)]
-            self.urlresolvers[app.module, app.app] = ar
-        # Install view
-        sv = self.site_view(app, view)
-        for u in self.view_urls(view):
-            ar += [RegexURLPattern(u.url, sv, name=u.name)]
-            # Register named view
-            self.register_named_view(mod_ns, app_ns, u.name, view)
-        # Install Menu
-        if hasattr(view, "menu"):
-            # Construct full url to menu item
-            # <!> Dirty hack, skip ^ in url
-            full_url = "/%s/%s/%s" % (app.module, app.app, view.url[1:])
-            if full_url.endswith("$"):
-                full_url = full_url[:-1]
-                menu = view.menu
-                if callable(menu):
-                    menu = menu(app)
-                if menu:
-                    try:
-                        mm = self.app_menu[app.module]
-                    except KeyError:
-                        mm = Menu(app.module)
-                        self.menu += [mm]
-                        self.app_menu[app.module] = mm
-                    menu_path = menu.split("|")
-                    while len(menu_path) > 1:
-                        mm = mm.get_submenu(menu_path.pop(0).strip())
-                    mm.add_item(menu_path[0].strip(), full_url,
-                                self.site_access(app, view))
 
     def login(self, request):
         """
@@ -204,18 +141,27 @@ class Site(object):
         """
         return lambda user: view.access.check(app, user)
 
-    def site_view(self, app, view):
+    def site_view(self, app, view_map):
         """
         Decorator for application view
         """
         # Render view
         def inner(request, *args, **kwargs):
-            if not request.user or not view.access.check(app, request.user):
+            try:
+                v = view_map[request.method]
+            except KeyError:
+                return HttpResponseNotFound("No handler for '%s' method" % request.method)
+            if not request.user or not v.access.check(app, request.user):
                 return self.login(request)
             try:
-                return view(request, *args, **kwargs)
+                r = v(request, *args, **kwargs)
             except PermissionDenied, why:
                 return HttpResponse(why)
+            if not isinstance(r, HttpResponse):
+                return HttpResponse(JSONEncoder(ensure_ascii=False).encode(r),
+                                    mimetype="text/json; charset=utf-8")
+            else:
+                return r
 
         # Render view in testing mode
         def inner_test(request, *args, **kwargs):
@@ -224,12 +170,49 @@ class Site(object):
             except:
                 #print error_report()
                 raise
+
         from access import PermissionDenied
         # Return actual handler
         if self.testing_mode:
             return inner_test
         else:
             return inner
+
+    def add_to_menu(self, app, v):
+        if callable(v.menu):
+            menu = v.menu(app)
+        else:
+            menu = v.menu
+        if not menu:
+            return
+        path = [app.module]
+        parts = [x.strip() for x in menu.split("|")]
+        root = self.menu[-1]
+        while len(parts) > 1:
+            p = parts.pop(0)
+            path += [p]
+            exists = False
+            for n in root["children"]:
+                if p == n["title"]:
+                    exists = True
+                    break
+            if exists:
+                root = n
+            else:
+                r = {"title": p, "children": []}
+                self.set_menu_id(r, path)
+                root["children"] += [r]
+                root = r
+        path += parts
+        app.menu_url = ("/%s/%s/%s" % (app.module, app.app,
+                                       v.url[1:])).replace("$", "")
+        r = {
+            "title": parts[0],
+            "app": app,
+            "access": self.site_access(app, v)
+        }
+        self.set_menu_id(r, path)
+        root["children"] += [r]
 
     def register(self, app_class):
         """
@@ -244,24 +227,66 @@ class Site(object):
         # Initialize application
         app = app_class(self)
         self.apps[app_id] = app
+        # Install module URL resolver
+        try:
+            mr = self.urlresolvers[app.module, None]
+        except KeyError:
+            mr = patterns("")
+            self.urlpatterns += [RegexURLResolver("^%s/" % app.module, mr,
+                                                  namespace=app.module)]
+            self.urlresolvers[app.module, None] = mr
+        # Install application URL resolver
+        try:
+            ar = self.urlresolvers[app.module, app.app]
+        except KeyError:
+            ar = patterns("")
+            mr += [RegexURLResolver("^%s/" % app.app, ar, namespace=app.app)]
+            self.urlresolvers[app.module, app.app] = ar
         # Register application views
+        umap = {}  # url -> [(URL, view)]
         for view in app.get_views():
             if hasattr(view, "url"):
-                self.register_view(app, view)
+                for u in self.view_urls(view):
+                    m = umap.get(u.url, [])
+                    m += [(u, view)]
+                    umap[u.url] = m
+        for url in umap:
+            mm = {}
+            names = set()
+            for u, v in umap[url]:
+                for m in u.method:
+                    #if m in mm:
+                    #    raise ValueError("Overlapping methods for same URL")
+                    mm[m] = v
+                if hasattr(v, "menu") and v.menu:
+                    self.add_to_menu(app, v)
+                if u.name:
+                    names.add(u.name)
+            sv = self.site_view(app, mm)
+            ar += [RegexURLPattern(u.url, sv, name=u.name)]
+            for n in names:
+                self.register_named_view(app.module, app.app, n, sv)
 
     def autodiscover(self):
         """
         Auto-load and initialize all application classes
         """
         for app in [a for a in INSTALLED_APPS if a.startswith("noc.")]:
-            n, m = app.split(".")
-            # Install application menues
+            m = app.split(".")[1]
+            # Get module name
+            mod_name = __import__(app, {}, {}, ["MODULE_NAME"]).MODULE_NAME
+            r = {"title": mod_name, "children": []}
+            self.set_menu_id(r, [m])
+            self.menu += [r]
+            # Initialize application
             for f in glob.glob("%s/apps/*/views.py" % m):
-                d, _ = os.path.split(f)
+                d = os.path.split(f)[0]
+                # Skip application loading if denoted by DISABLED file
                 if os.path.isfile(os.path.join(d, "DISABLED")):
                     continue
                 __import__(".".join(["noc"] + f[:-3].split(os.path.sep)),
                            {}, {}, "*")
+            continue
             # Try to install dynamic menus
             menu = None
             try:
@@ -270,6 +295,7 @@ class Site(object):
                 continue
             if menu:
                 self.app_menu[m].submenus += menu.DYNAMIC_MENUS
+        self.sort_menu()
 
     def application_by_class(self, app_class):
         """
@@ -295,6 +321,26 @@ class Site(object):
             return reverse(url, args=args, kwargs=kw) + query
         else:
             return url
+
+    def sort_menu(self):
+        """
+        Sort application menu
+        """
+        def sorted_menu(c):
+            c = sorted(c, key=lambda x: x["title"])
+            for m in c:
+                if "children" in m:
+                    m["children"] = sorted_menu(m["children"])
+            return c
+
+        for m in self.menu:
+            m["children"] = sorted_menu(m["children"])
+
+    def set_menu_id(self, item, path):
+        menu_id = hashlib.sha1(" | ".join(path)).hexdigest()
+        item["id"] = menu_id
+        self.menu_index[menu_id] = item
+
 
 ##
 ## Global application site instance
