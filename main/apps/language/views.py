c0c4f3961e7099d2ff8b9ffa763efe337daa187a
Dmitry Volodin <dv@nocproject.org>
2011-09-13 01:54:39 +0400

    main.language application demo

--- main/apps/language/views.py
+++ main/apps/language/views.py
@@ -2,23 +2,18 @@
 ##----------------------------------------------------------------------
 ## Language Manager
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2010 The NOC Project
+## Copyright (C) 2007-2011 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-from django.contrib import admin
-from noc.lib.app import ModelApplication
+
+## Django modules
+from django.utils.translation import ugettext as _
+## NOC modules
+from noc.lib.app import ExtModelApplication
 from noc.main.models import Language
-##
-## Admin for Language
-##
-class LanguageAdmin(admin.ModelAdmin):
-    list_display=["name","native_name","is_active"]
-    search_fields=["name","native_name"]
-    list_filter=["is_active"]
-##
-##
-##
-class LanguageApplication(ModelApplication):
-    model=Language
-    model_admin=LanguageAdmin
-    menu="Setup | Languages"
+
+
+class LanguageApplication(ExtModelApplication):
+    title = _("Languages")
+    model = Language
+    menu = "Setup | Languages"
