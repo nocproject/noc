e087e2ade8fb8ca497ca15af6a4cb2b0724271ea
Dmitry Volodin <dv@nocproject.org>
2011-09-14 14:45:00 +0400

    First working implementation

--- main/apps/language/js/Model.js
+++ main/apps/language/js/Model.js
@@ -7,8 +7,8 @@
 console.debug("Defining NOC.main.language.Model");
 
 Ext.define("NOC.main.language.Model", {
-    //extend: "NOC.core.Model",
     extend: "Ext.data.Model",
+    rest_url: "/main/language/",
 
     fields: [
         {
@@ -31,27 +31,5 @@ Ext.define("NOC.main.language.Model", {
             type: "boolean",
             defaultValue: false
         }
-    ],
-
-    //url: "/main/language/"
-    proxy: {
-        type: "rest",
-        url: "/main/language/",
-        pageParam: "__page",
-        startParam: "__start",
-        limitParam: "__limit",
-        sortParam: "__sort",
-        extraParams: {
-            "__format": "ext"
-        },
-        reader: {
-            type: "json",
-            root: "data",
-            totalProperty: "total",
-            successProperty: "success"
-        },
-        writer: {
-            type: "json"
-        }    
-    }
+    ]
 });
