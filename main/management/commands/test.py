057e17191d2da7d7473d34517856b7f27b018518
Dmitry Volodin <dv@nocproject.org>
2011-09-16 02:39:26 +0400

    PEP8 formatting

--- main/management/commands/test.py
+++ main/management/commands/test.py
@@ -2,25 +2,31 @@
 ##----------------------------------------------------------------------
 ## Run noc tests
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2009 The NOC Project
+## Copyright (C) 2007-2011 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
+
+## Python modules
+import sys
+from optparse import make_option
+## Django modules
 from django.core.management.base import BaseCommand
 from django.core import management
+## Third-party modules
 from south.management.commands import MigrateAndSyncCommand
-from optparse import make_option
-import sys
-##
-## Customized version of Django's test
-##
+
+
 class Command(BaseCommand):
+    """
+    Customized version of django's test
+    """
     option_list = BaseCommand.option_list + (
-        make_option("--coverage",action="store_true",dest="coverage",default=True,
-            help="Generate Coverage report (default)"),
-        make_option("--no-coverage",action="store_false",dest="coverage",
-            help="Skip Coverage report"),
-        make_option("--reuse-db",action="store_true",dest="reuse_db",default=False,
-            help="Do not create testing DB when exists"),
+        make_option("--coverage", action="store_true", dest="coverage",
+                    default=True, help="Generate Coverage report (default)"),
+        make_option("--no-coverage", action="store_false", dest="coverage",
+                    help="Skip Coverage report"),
+        make_option("--reuse-db", action="store_true", dest="reuse_db",
+                    default=False, help="Do not create testing DB when exists"),
     )
     help = 'Runs the test suite for the specified applications, or the entire project if no apps are specified.'
     args = '[appname ...]'
@@ -30,21 +36,22 @@ class Command(BaseCommand):
     def handle(self, *test_labels, **options):
         from django.conf import settings
         from django.test.utils import get_runner
-    
+
         verbosity = int(options.get("verbosity", 1))
         interactive = options.get("interactive", True)
-        coverage = options.get("coverage",True)
-        reuse_db = options.get("reuse_db",False)
+        coverage = options.get("coverage", True)
+        reuse_db = options.get("reuse_db", False)
         test_runner = get_runner(settings)
-        
-        if len(test_labels) == 1 and test_labels[0].startswith("noc.sa.profiles"):
+
+        if (len(test_labels) == 1 and
+            test_labels[0].startswith("noc.sa.profiles")):
             reuse_db = True
         # Install south migrations hook
         management.get_commands()
         management._commands["syncdb"] = MigrateAndSyncCommand()
         # Run tests
-        failures = test_runner(test_labels, verbosity=verbosity,
+        failures = test_runner(test_labels=test_labels, verbosity=verbosity,
                                interactive=interactive,
                                coverage=coverage, reuse_db=reuse_db)
         if failures:
-            sys.exit(bool(failures))
+            sys.exit(1 if failures else 0)
