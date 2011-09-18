c282f9d25af55d07682e1b8d78e6c4895f97d5fb
Dmitry Volodin <dv@nocproject.org>
2011-09-18 17:57:28 +0400

    PEP8 formatting

--- lib/app/access.py
+++ lib/app/access.py
@@ -7,106 +7,143 @@
 ##----------------------------------------------------------------------
 from django.db.models import Q
 import noc.main.models
-##
-class PermissionDenied(Exception): pass
-##
-## Basic Permission class.
-## Each permission must implement ``check`` method
-## and additional queryset method
-##
+
+
+class PermissionDenied(Exception):
+    """
+    Basic Permission Denied exception
+    """
+    pass
+
+
 class Permission(object):
+    """
+    Basic Permission class.
+    Each permission must implement ``check`` method
+    and optional queryset method
+    """
     def __init__(self):
-        self.app=None
-    ##
-    ## Must return Q object restricting the list of available objects
-    ##
-    def queryset(self,request):
+        self.app = None
+
+    def queryset(self, request):
+        """
+        Get _Q_ object restricting the list of available objects
+        """
         return Q()
-    ##
-    ## Called to check wrether user has access
-    ## For granular permissions obj=None means check user has application access
-    ##
-    def check(self,app,user,obj=None):
-        raise Exception("Not implemented")
-    ##
-    ## Return human-readable representation of permission set
-    ## Applicable only for granular permissions
-    ##
-    def display(self,user):
+
+    def check(self, app, user, obj=None):
+        """
+        Called to check wrether user has access
+        For granular permissions obj=None
+        means check user has application access
+        """
+        raise NotImplementedError()
+
+    def display(self, user):
+        """
+        Return human-readable representation of permission set
+        Applicable only for granular permissions
+        """
         return ""
-    ##
-    def __or__(self,r):
-        return ORPermission(self,r)
-    ##
-    def __and__(self,r):
-        return ANDPermission(self,r)
-##
-## Boolean logic permission
-##
+
+    def __or__(self, r):
+        """
+        _or_ operator implementation
+        """
+        return ORPermission(self, r)
+
+    def __and__(self, r):
+        """
+        _and_ operator implementation
+        """
+        return ANDPermission(self, r)
+
+
 class LogicPermision(Permission):
-    def __init__(self,l,r):
-        super(Permission,self).__init__()
-        self.l=l
-        self.r=r
-##
-## Permit if left or right part permit
-##
+    """
+    Boolean logic permission. Used to combine two permissions
+    using logic condition
+    """
+    def __init__(self, l, r):
+        super(Permission, self).__init__()
+        self.l = l
+        self.r = r
+
+
 class ORPermission(LogicPermision):
-    def check(self,app,user,obj=None):
-        return self.l.check(app,user,obj) or self.r.check(app,user,obj)
-    def queryset(self,request):
-        return self.l.queryset(request)|self.r.queryset(request)
-##
-## Permit if left and right part permit
-##
+    """
+    _or_ combination. Permit if either left or right conditions are met
+    """
+    def check(self, app, user, obj=None):
+        return self.l.check(app, user, obj) or self.r.check(app, user, obj)
+
+    def queryset(self, request):
+        return self.l.queryset(request) | self.r.queryset(request)
+
+
 class ANDPermission(LogicPermision):
-    def check(self,app,user,obj=None):
-        return self.l.check(app,user,obj) and self.r.check(app,user,obj)
-    def queryset(self,request):
-        return self.l.queryset(request)&self.r.queryset(request)
-##
-## Permit access
-##
+    """
+    _and_ combination. Permit if both left and right conditions are met
+    """
+    def check(self, app, user, obj=None):
+        return self.l.check(app, user, obj) and self.r.check(app, user, obj)
+
+    def queryset(self, request):
+        return self.l.queryset(request) & self.r.queryset(request)
+
+
 class Permit(Permission):
-    def check(self,app,user,obj=None):
+    """
+    Always permit
+    """
+    def check(self, app, user, obj=None):
         return True
-##
-## Deny access
-##
+
+
 class Deny(Permission):
-    def check(self,app,user,obj=None):
+    """
+    Always deny
+    """
+    def check(self, app, user, obj=None):
         return False
-##
-## Permit access to logged users
-##
+
+
 class PermitLogged(Permission):
-    def check(self,app,user,obj=None):
+    """
+    Permit any authenticated user
+    """
+    def check(self, app, user, obj=None):
         return user.is_authenticated()
-##
-## Permit access to superusers
-##
+
+
 class PermitSuperuser(Permission):
-    def check(self,app,user,obj=None):
+    """
+    Permit superusers
+    """
+    def check(self, app, user, obj=None):
         return user.is_superuser
-##
-## Permit access if user has permission ``perm``
-##
+
+
 class HasPerm(Permission):
-    def __init__(self,perm):
-        super(HasPerm,self).__init__()
-        self.perm=perm
-    
+    """
+    Permit if the user has permission _perm_
+    """
+    def __init__(self, perm):
+        super(HasPerm, self).__init__()
+        self.perm = perm
+
     def __repr__(self):
-        if hasattr(self,"perm_id"):
-            return "<HasPerm '%s' object at 0x%x>"%(self.perm_id,id(self))
+        if hasattr(self, "perm_id"):
+            return "<HasPerm '%s' object at 0x%x>" % (self.perm_id, id(self))
         else:
-            return "<HasPerm object at 0x%x>"%id(self)
-    
-    def get_permission(self,app):
+            return "<HasPerm object at 0x%x>" % id(self)
+
+    def get_permission(self, app):
         if ":" in self.perm:
             return self.perm
         else:
-            return "%s:%s:%s"%(app.module,app.app,self.perm)
+            return "%s:%s:%s" % (app.module, app.app, self.perm)
 
-    def check(self,app,user,obj=None):
-        return noc.main.models.Permission.has_perm(user,self.get_permission(app))
+    def check(self, app, user, obj=None):
+        return noc.main.models.Permission.has_perm(user,
+                                                   self.get_permission(app))
