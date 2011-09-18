58b97801fb36c1f5bf5714bd257f0260a4b89af9
Dmitry Volodin <dv@nocproject.org>
2011-09-18 18:29:58 +0400

    PEP8 formatting

--- main/management/commands/sync-perm.py
+++ main/management/commands/sync-perm.py
@@ -2,29 +2,33 @@
 ##----------------------------------------------------------------------
 ## Syncronize permissions
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2009 The NOC Project
+## Copyright (C) 2007-2011 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-"""
-"""
+
+## Django modules
 from django.core.management.base import BaseCommand
+## NOC modules
 from noc.lib.app import site
 from noc.main.models import Permission
-##
-## Command handler
-##
+
+
 class Command(BaseCommand):
-    help="Syncronize permissions"
+    """
+    ./noc sync-perm
+    """
+    help = "Syncronize permissions"
+
     def handle(self, *args, **options):
-        new_perms=set()
+        new_perms = set()
         for app in site.apps.values():
-            new_perms=new_perms.union(app.get_permissions())
-        old_perms=set(Permission.objects.values_list("name",flat=True))
+            new_perms = new_perms.union(app.get_permissions())
+        old_perms = set(Permission.objects.values_list("name", flat=True))
         # New permissions
-        for name in new_perms-old_perms:
+        for name in new_perms - old_perms:
             Permission(name=name).save()
-            print "+ %s"%name
+            print "+ %s" % name
         # Deleted permissions
-        for name in old_perms-new_perms:
-            print "- %s"%name
+        for name in old_perms - new_perms:
+            print "- %s" % name
             Permission.objects.get(name=name).delete()
