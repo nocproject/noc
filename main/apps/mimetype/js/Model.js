3ae694293b9bae6276852c047db91941f383061f
Dmitry Volodin <dv@nocproject.org>
2011-09-19 17:39:13 +0400

    main.mimetype application has been rewritten as ExtModelApplication

---
+++ main/apps/mimetype/js/Model.js
@@ -0,0 +1,43 @@
+//---------------------------------------------------------------------
+// main.mimetype Model
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2011 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.main.mimetype.Model");
+
+Ext.define("NOC.main.mimetype.Model", {
+    extend: "Ext.data.Model",
+    rest_url: "/main/mimetype/",
+
+    fields: [
+        {
+            name: "id",
+            type: "int"
+        },
+
+        {
+            name: "extension",
+            type: "string"
+        },
+
+        {
+            name: "mime_type",
+            type: "string"
+        }
+    ],
+    
+    validations: [
+        {
+            field: "extension",
+            type: "format",
+            matcher: /^\.[a-zA-Z0-9\-]+$/
+        },
+
+        {
+            field: "mime_type",
+            type: "format",
+            matcher: /^[a-zA-Z0-9\-]+\/[a-zA-Z0-9\-]+$/
+        }
+    ]
+});
