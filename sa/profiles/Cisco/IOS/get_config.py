94307fe3214cd09fe337030e6e3d905933d0b689
Dmitry Volodin <dv@nocproject.org>
2011-09-19 14:50:31 +0400

    PEP8 formatting

--- sa/profiles/Cisco/IOS/get_config.py
+++ sa/profiles/Cisco/IOS/get_config.py
@@ -1,17 +1,21 @@
 # -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2009 The NOC Project
+## Cisco.IOS.get_config
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-"""
-"""
-import noc.sa.script
+
+## NOC modules
+from noc.sa.script import Script as NOCScript
 from noc.sa.interfaces import IGetConfig
 
-class Script(noc.sa.script.Script):
-    name="Cisco.IOS.get_config"
-    implements=[IGetConfig]
+
+class Script(NOCScript):
+    name = "Cisco.IOS.get_config"
+    implements = [IGetConfig]
+
     def execute(self):
-        config=self.cli("show running-config")
-        config=self.strip_first_lines(config,3)
+        config = self.cli("show running-config")
+        config = self.strip_first_lines(config, 3)
         return self.cleaned_config(config)
