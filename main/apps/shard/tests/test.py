5f6bf4cae4698fbcbcf05733bde9e78726aea903
Dmitry Volodin <dv@nocproject.org>
2011-09-21 02:17:46 +0400

    main.shard has been rewritten as ExtModelApplication

--- main/apps/shard/tests/test.py
+++ main/apps/shard/tests/test.py
@@ -1,11 +1,30 @@
 # -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
-## shard Test
+## main.shard unittes
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2009 The NOC Project
+## Copyright (C) 2007-2011 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-from noc.lib.test import ModelApplicationTestCase
 
-class shardTestCase(ModelApplicationTestCase):
-    pass
+## NOC modules
+from noc.lib.test import RestModelTestCase
+
+
+class ShardTestCase(RestModelTestCase):
+    app = "main.shard"
+
+    scenario = [
+        {
+            "GET": {
+                "name": "Test"
+            },
+            "POST": {
+                "name": "Test",
+                "is_active": False,
+                "description": "Testing Shard"
+            },
+            "PUT": {
+                "is_active": True
+            }
+        }
+    ]
