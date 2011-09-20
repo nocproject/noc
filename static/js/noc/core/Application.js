d632a2db2ac7d58301963e89c08a27761975329c
Dmitry Volodin <dv@nocproject.org>
2011-09-20 17:19:55 +0400

    NOC.core.Application is base class for NOC.node.ModelApplication

---
+++ static/js/noc/core/Application.js
@@ -0,0 +1,11 @@
+//---------------------------------------------------------------------
+// NOC.core.Application
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2011 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.core.Application");
+
+Ext.define("NOC.core.Application", {
+    extend: "Ext.panel.Panel"
+});
