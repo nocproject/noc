20db0923c69645f87026ed1aca8c777093086d5e
Dmitry Volodin <dv@nocproject.org>
2011-09-21 15:10:46 +0400

    NOC.core.Lookup and NOC.core.LookupField classes

---
+++ static/js/noc/core/Lookup.js
@@ -0,0 +1,33 @@
+//---------------------------------------------------------------------
+// NOC.core.Lookup -
+// Simple lookup Store with id and label fields
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2011 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.core.Lookup");
+
+Ext.define("NOC.core.Lookup", {
+    extend: "Ext.data.Store",
+    fields: ["id", "label"],
+    //autoLoad: true,
+    constructor: function(config) {
+        this.callParent(config);
+        this.setProxy(Ext.create("Ext.data.RestProxy", {
+            url: this.url,
+            pageParam: "__page",
+            startParam: "__start",
+            limitParam: "__limit",
+            sortParam: "__sort",
+            extraParams: {
+                "__format": "ext"
+            },
+            reader: {
+                type: "json",
+                root: "data",
+                totalProperty: "total",
+                successProperty: "success"
+            }
+        }));
+    }
+});
