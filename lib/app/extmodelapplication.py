05f8e914f80dd886871bfaecfeeb03f6698f584c
Dmitry Volodin <dv@nocproject.org>
2011-09-21 15:04:34 +0400

    Query parameter for lookups

--- lib/app/extmodelapplication.py
+++ lib/app/extmodelapplication.py
@@ -13,15 +13,19 @@ from django.utils.encoding import is_protected_type
 from django.http import HttpResponse
 from django.utils.simplejson.decoder import JSONDecoder
 from django.utils.simplejson.encoder import JSONEncoder
+from django.db.models.fields import CharField
+from django.db.models import Q
 ## NOC modules
 from extapplication import ExtApplication, view
 
 
 class ExtModelApplication(ExtApplication):
     model = None  # Django model to expose
-    
+    query_fields = []  # Use all unique fields by default
+    query_condition = "startswith"
+
     pk_field_name = None  # Set by constructor
-    
+
     # REST return codes and messages
     OK = 200
     CREATED = 201
@@ -34,20 +38,44 @@ class ExtModelApplication(ExtApplication):
     INTERNAL_ERROR = 500
     NOT_IMPLEMENTED = 501
     THROTTLED = 503
-    
+
     ignored_params = ["_dc"]
     page_param = "__page"
     start_param = "__start"
     limit_param = "__limit"
     sort_param = "__sort"
     format_param = "__format"  # List output format
+    query_param = "__query"
 
     def __init__(self, *args, **kwargs):
         super(ExtModelApplication, self).__init__(*args, **kwargs)
         self.pk_field_name = self.model._meta.pk.name
+        if not self.query_fields:
+            self.query_fields = ["%s__%s" % (f.name, self.query_condition)
+                                 for f in self.model._meta.fields
+                                 if f.unique and isinstance(f, CharField)]
 
-    def queryset(self, request):
-        return self.model.objects.all()
+    def get_Q(self, request, query):
+        """
+        Prepare Q statement for query
+        """
+        def get_q(f):
+            if "__" not in f:
+                return "%s__%s" % (f, self.query_condition)
+            else:
+                return f
+
+        return reduce(lambda x, y: x | Q(get_q(y)), self.query_fields[1:],
+                      Q(**{get_q(self.query_fields[0]): query}))
+
+    def queryset(self, request, query=None):
+        """
+        Filter records for lookup
+        """
+        if query and self.query_fields:
+            return self.model.objects.filter(self.get_Q(request, query))
+        else:
+            return self.model.objects.all()
 
     def deserialize(self, data):
         return JSONDecoder(encoding="utf8").decode(data)
@@ -68,7 +96,7 @@ class ExtModelApplication(ExtApplication):
             if p in q:
                 del q[p]
         for p in (self.limit_param, self.page_param, self.start_param,
-            self.format_param, self.sort_param):
+            self.format_param, self.sort_param, self.query_param):
             if p in q:
                 del q[p]
         return q
@@ -82,16 +110,22 @@ class ExtModelApplication(ExtApplication):
                 r[f.name] = getattr(o, f.name)._get_pk_val()
         return r
 
-    @view(method=["GET"], url="^$", access="read", api=True)
-    def api_list(self, request):
+    def instance_to_lookup(self, o):
+        return {
+            "id": o.id,
+            "label": unicode(o)
+        }
+
+    def list_data(self, request, formatter):
         """
-        Returns a list of available objects
+        Returns a list of requested object objects
         """
         q = dict(request.GET.items())
         limit = q.get(self.limit_param)
         page = q.get(self.page_param)
         start = q.get(self.start_param)
         format = q.get(self.format_param)
+        query = q.get(self.query_param)
         ordering = []
         if format == "ext" and self.sort_param in q:
             for r in self.deserialize(q[self.sort_param]):
@@ -100,7 +134,7 @@ class ExtModelApplication(ExtApplication):
                 else:
                     ordering += [r["property"]]
         q = self.cleaned_query(q)
-        data = self.queryset(request).filter(**q)
+        data = self.queryset(request, query).filter(**q)
         # Apply sorting
         if ordering:
             data = data.order_by(*ordering)
@@ -108,7 +142,7 @@ class ExtModelApplication(ExtApplication):
             total = data.count()
         if start is not None and limit is not None:
             data = data[int(start):int(start) + int(limit)]
-        out = [self.instance_to_dict(o) for o in data]
+        out = [formatter(o) for o in data]
         if format == "ext":
             out = {
                 "total": total,
@@ -116,6 +150,14 @@ class ExtModelApplication(ExtApplication):
                 "data": out
             }
         return self.response(out, status=self.OK)
+        
+    @view(method=["GET"], url="^$", access="read", api=True)
+    def api_list(self, request):
+        return self.list_data(request, self.instance_to_dict)
+
+    @view(method=["GET"], url=r"^lookup/$", access="lookup", api=True)
+    def api_lookup(self, request):
+        return self.list_data(request, self.instance_to_lookup)
 
     @view(method=["POST"], url="^$", access="create", api=True)
     def api_create(self, request):
