4b295accfe9c9eea59e1765f39673feee40a3d24
Dmitry Volodin <dv@nocproject.org>
2011-09-23 00:12:22 +0400

    PEP8 formatting

--- manage.py
+++ manage.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python
-import os,sys,site,logging
+
+# Python modules
+import os
+import sys
+import site
+import logging
+
 # Adjust paths
-d=os.path.dirname(sys.argv[0])
+d = os.path.dirname(sys.argv[0])
 if not d:
-    d=os.getcwd()
-contrib=os.path.abspath(os.path.join(d,"contrib","lib"))
-sys.path.insert(0,contrib)
-sys.path.insert(0,os.path.abspath(os.path.join(d,"..")))
-sys.path.insert(0,d)
+    d = os.getcwd()
+contrib = os.path.abspath(os.path.join(d, "contrib", "lib"))
+sys.path.insert(0, contrib)
+sys.path.insert(0, os.path.abspath(os.path.join(d, "..")))
+sys.path.insert(0, d)
 # Install eggs from contrib/lib
 site.addsitedir(contrib)
 
 try:
-    import settings # Assumed to be in the same directory.
+    import settings  # Assumed to be in the same directory.
 except ImportError:
     import sys
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
@@ -20,13 +26,16 @@ except ImportError:
 from django.core.management import execute_manager
 
 if __name__ == "__main__":
-    if len(sys.argv)>1 and sys.argv[1] not in ["test","syncdb","migrate","debug-script","script-test","topo-test"]:
+    if (len(sys.argv) > 1 and
+        sys.argv[1] not in ("test", "syncdb", "migrate", "debug-script",
+                            "script-test", "topo-test")):
         # Initialize models
-        os.environ["DJANGO_SETTINGS_MODULE"]="noc.settings"
+        os.environ["DJANGO_SETTINGS_MODULE"] = "noc.settings"
         import noc.urls
-    if len(sys.argv)>1 and sys.argv[1] in ("runserver", "debug-script"):
+    if len(sys.argv) > 1 and sys.argv[1] in ("runserver", "debug-script"):
         # Set loglevel to DEBUG
         #logging.root.setLevel(logging.DEBUG)
-        logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(message)s')
+        logging.basicConfig(level=logging.DEBUG,
+                            format='%(asctime)s %(message)s')
     # Execute command
     execute_manager(settings)
