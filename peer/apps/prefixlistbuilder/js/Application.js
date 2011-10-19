a5760bd581b17194eaaeb7b6308bbd6f6db43bdb
Stanislav Svirid <count@ngs.ru>
2011-10-19 20:14:33 +0700

    NOC-38: Dimplify form. Fix resize issue. Add description to fields.

--- peer/apps/prefixlistbuilder/js/Application.js
+++ peer/apps/prefixlistbuilder/js/Application.js
@@ -9,117 +9,102 @@ console.debug("Defining NOC.peer.prefixlistbuilder.Application");
 Ext.define("NOC.peer.prefixlistbuilder.Application", {
     extend: "NOC.core.Application",
     requires: ["NOC.peer.peeringpoint.LookupField", "Ext.ux.form.UCField"],
-    layout: {
-        type: "vbox",
-        align: "center"
-    },
+    layout: { type: 'vbox', align: 'stretch' },
+    border: 0,
     items :  [{
-        xtype: 'form',
-        title: 'Build prefix lists',
-        frame: true,
-        url: "/peer/prefixlistbuilder/",
-        padding: 4,
-        bodyPadding: 4,
-        style: {
-            marginTop: "10px"
-        },
-        listeners : {
-            added : function(comp, container, pos, eOpts) {
-                comp.resultPanel = Ext.create("Ext.panel.Panel", {
-                            title: "Prefix list",
-                            layout: "fit",
-                            flex: 1,
-                            width: "95%",
-                            padding: 10,
-                            border: false,
-                            items: [{
-                                xtype: "textareafield",
-                                flex: 1,
-                                resizable: false
-                            }]
-                        });
-                container.add(comp.resultPanel);
-                Ext.applyIf(comp.getForm(), { waitMsgTarget : comp.resultPanel.id });
-            }
-        },
-        defaults: {
-            enableKeyEvents: true,
-            listeners: {
-                specialkey: function(field, key) {
-                    if (field.xtype != "textfield")
-                        return;
-                    var get_button = function(scope, name) {
-                        return scope.ownerCt.down("toolbar").getComponent(name);
-                    }
-                    switch(key.getKey()) {
-                        case Ext.EventObject.ENTER:
-                            var b = get_button(field, "build");
-                            key.stopEvent();
-                            b.handler.call(b);
-                            break;
-                        case Ext.EventObject.ESC:
-                            var b = get_button(field, "reset");
-                            key.stopEvent();
-                            b.handler.call(b);
-                    }
-                }
-            }
-        },
-        items: [
-            {
-                xtype: "textfield",
-                fieldLabel: "Name",
-                name: "name",
-                emptyText: "Prefix list name ...",
-                allowBlank: true,
-                regex: /^[0-9a-zA-Z_\-]*$/
-            },
-            {
-                xtype: "peer.peeringpoint.LookupField",
-                fieldLabel: "Peering Point",
-                name: "peering_point",
-                allowBlank: false
-            },
-            {
-                xtype: "textfield",
-                fieldLabel: "AS-SET",
-                name: "as_set",
-                emptyText: "AS or AS-set",
-                allowBlank: false,
-                regex: /^AS(\d+|-\w+)(\s+AS(\d+|-\w+))*$/,
-                plugins: [ 'ucfield' ]
-            }
-        ],
-        buttonAlign: "left",
-        buttons: [
-            {
-                text: "Build",
-                itemId: "build",
-                formBind: true,
-                disabled: true,
-                handler : function() {
-                    var form = this.up("form").getForm();
-                    form.submit({
-                        method: "GET",
-                        submitEmptyText : false,
-                        params : { __format: "ext" },
-                        waitMsg : "Compute prefix list",
-                        success: function(form, action) {
-                            var r = form.owner.resultPanel;
-                            r.setTitle("Prefix list: " + action.result.name);
-                            r.items.items[0].setValue(action.result.prefix_list);
+        xtype: "container",
+        items: [{
+            xtype: 'form',
+            border: 0,
+            url: "/peer/prefixlistbuilder/",
+            padding: 4,
+            bodyPadding: 4,
+            waitMsgTarget : true,
+            defaults: {
+                enableKeyEvents: true,
+                listeners: {
+                    specialkey: function(field, key) {
+                        if (field.xtype != "textfield")
+                            return;
+                        var get_button = function(scope, name) {
+                            return scope.ownerCt.down("toolbar").getComponent(name);
                         }
-                    });
-                }
+                        switch(key.getKey()) {
+                            case Ext.EventObject.ENTER:
+                                var b = get_button(field, "build");
+                                key.stopEvent();
+                                b.handler.call(b);
+                                break;
+                            case Ext.EventObject.ESC:
+                                var b = get_button(field, "reset");
+                                key.stopEvent();
+                                b.handler.call(b);
+                        }
+                    }
+                },
+                xtype: "textfield",
+                msgTarget : "side",
+                size: 30
             },
-            {
-                text: "Reset",
-                itemId: "reset",
-                disabled: false,
-                handler: function() {
-                    this.up("form").getForm().reset();
+            items: [
+                {
+                    fieldLabel: "Name",
+                    name: "name",
+                    emptyText: "Prefix list name ...",
+                    allowBlank: true,
+                    regex: /^[0-9a-zA-Z_\-]*$/,
+                    invalidText: "Prefix list name must contains only 0-9,a-z,A-Z,'-','_'"
+                },
+                {
+                    xtype: "peer.peeringpoint.LookupField",
+                    fieldLabel: "Peering Point",
+                    name: "peering_point",
+                    allowBlank: false
+                },
+                {
+                    fieldLabel: "AS-SET",
+                    name: "as_set",
+                    emptyText: "AS or AS-set",
+                    allowBlank: false,
+                    regex: /^AS(\d+|-\w+)(\s+AS(\d+|-\w+))*$/,
+                    invalidText: "Enter list valid ASnumber (ex., AS1234) or as sets (ex. AS-MEGASET)",
+                    plugins: [ 'ucfield' ]
                 }
-            }
-        ]
-    }]
+            ],
+            buttonAlign: "left",
+            buttons: [
+                {
+                    text: "Build",
+                    itemId: "build",
+                    formBind: true,
+                    disabled: true,
+                    handler : function() {
+                        var form = this.up("form").getForm();
+                        form.submit({
+                            method: "GET",
+                            submitEmptyText : false,
+                            params : { __format: "ext" },
+                            waitMsg : "Compute prefix list",
+                            success: function(form, action) {
+                                form.owner.up("panel").down("textareafield").setValue(action.result.prefix_list);
+                            }
+                        });
+                    }
+                },
+                {
+                    text: "Reset",
+                    itemId: "reset",
+                    disabled: false,
+                    handler: function() {
+                        this.up("form").getForm().reset();
+                    }
+                }
+            ]
+        }]
+    },
+    {
+        xtype: "textarea",
+        flex: 1
+    }
+    ]
 });
