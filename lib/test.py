03707e34ae06bf68522f9d022dae9da863b06cc5
Dmitry Volodin <dv@nocproject.org>
2011-11-01 23:37:49 +0300

    Additional PEP8 formatting

--- lib/test.py
+++ lib/test.py
@@ -587,12 +587,12 @@ class ActivatorStub(object):
     """
     TimeOutError = SocketTimeoutError
     def __init__(self, test):
-        self.to_save_output=None
-        self.servers=None
-        self.factory=None
-        self.log_cli_sessions=None
-        self.test=test
-        self.use_canned_session=True
+        self.to_save_output = None
+        self.servers = None
+        self.factory = None
+        self.log_cli_sessions = None
+        self.test = test
+        self.use_canned_session = True
     
     def on_script_exit(self, script):
         pass
@@ -617,30 +617,32 @@ class ActivatorStub(object):
     
     def get_motd(self):
         return self.test.motd
-    
-##
-## Canned beef base class
-##
-rx_timestamp=re.compile(r"^\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d(\.\d+)?$")
+
+
 class ScriptTestCase(unittest.TestCase):
+    """
+    Canned beef base class
+    """
     maxDiff = None
-    script=None
-    vendor=None
-    platform=None
-    version=None
-    input={}
-    result=None
-    motd=""
-    cli=None
-    snmp_get={}
-    snmp_getnext={}
-    mock_get_version=False # Emulate get_version_call
+    script = None
+    vendor = None
+    platform = None
+    version = None
+    input = {}
+    result = None
+    motd = ""
+    cli = None
+    snmp_get = {}
+    snmp_getnext = {}
+    mock_get_version = False  # Emulate get_version_call
     ignore_timestamp_mismatch = False
-    
+
+    rx_timestamp = re.compile(r"^\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d(\.\d+)?$")
+
     def clean_timestamp(self, r):
         if isinstance(r, basestring):
             # Process strings
-            if rx_timestamp.match(r):
+            if self.rx_timestamp.match(r):
                 # Fill timestamp by zeroes
                 return "0000-00-00T00:00:00.000000"
             else:
@@ -648,37 +650,39 @@ class ScriptTestCase(unittest.TestCase):
         elif type(r) in (types.ListType, types.TupleType):
             # Iterate lists
             return [self.clean_timestamp(x) for x in r]
-        elif type(r)==types.DictType:
+        elif type(r) == types.DictType:
             # Iterate hashes
-            return dict([(k, self.clean_timestamp(v)) for k,v in r.items()])
+            return dict([(k, self.clean_timestamp(v)) for k, v in r.items()])
         else:
             # Return unprocessed
             return r
     
     def test_script(self):
-        p=self.script.split(".")
-        profile=profile_registry[".".join(p[:2])]
+        p = self.script.split(".")
+        profile = profile_registry[".".join(p[:2])]
         # Prepare access profile
-        a=AccessProfile()
-        a.profile=profile.name
+        a = AccessProfile()
+        a.profile = profile.name
         if self.snmp_get or self.snmp_getnext:
-            a.snmp_ro="public"
+            a.snmp_ro = "public"
         # Run script.
-        script=script_registry[self.script](profile(),ActivatorStub(self),a,**self.input)
+        script = script_registry[self.script](profile(), ActivatorStub(self),
+                                              a, **self.input)
         # Install mock get_version into cache, if necessary
         s=self.script.split(".")
         if self.mock_get_version and s[-1]!="get_version":
             # Install version info into script call cache
             version={"vendor": self.vendor, "platform": self.platform, "version": self.version}
-            script.set_cache("%s.%s.get_version"%(s[0],s[1]), {}, version)
+            script.set_cache("%s.%s.get_version" % (s[0], s[1]), {}, version)
         script.run()
         # Parse script result
         if script.result:
             result=cPickle.loads(script.result)
             if self.ignore_timestamp_mismatch:
-                self.assertEquals(self.clean_timestamp(result), self.clean_timestamp(self.result))
+                self.assertEquals(self.clean_timestamp(result),
+                                  self.clean_timestamp(self.result))
             else:
                 self.assertEquals(result, self.result)
         else:
             print script.error_traceback
-            self.assertEquals(script.error_traceback,None)
+            self.assertEquals(script.error_traceback, None)
