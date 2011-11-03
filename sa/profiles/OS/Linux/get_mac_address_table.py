9e28d7700159d86afc53a49878b780fd6f6f63e4
GNU Linux <linux.gnu@yandex.ru>
2011-11-03 10:06:03 +0700

    user: GNU Linux <linux.gnu@yandex.ru> branch 'default' changed sa/profiles/OS/Linux/get_mac_address_table.py

--- sa/profiles/OS/Linux/get_mac_address_table.py
+++ sa/profiles/OS/Linux/get_mac_address_table.py
@@ -21,14 +21,13 @@ class Script(noc.sa.script.Script):
     rx_bridge_int = re.compile(r"^\s+(?P<interface>\S+)", re.MULTILINE)
     rx_showmacs = re.compile(r"^\s*(?P<port>\d+)\s+(?P<mac>\S+)\s+(no|yes)\s+(?P<type>\S+)$", re.MULTILINE)
 
-    def execute(self,interface=None,vlan=None,mac=None):
+    def execute(self, interface=None, vlan=None, mac=None):
         vlans = {}
-        for vlan in self.rx_vlan.finditer(self.cli("cat /proc/net/vlan/config")):
-            vlan_id = vlan.group("vlan")
+        for vl in self.rx_vlan.finditer(self.cli("cat /proc/net/vlan/config")):
+            vlan_id = vl.group("vlan")
             if vlan_id not in vlans:
                 vlans.update( { vlan_id : [] } )
-            vlans[vlan_id].append(vlan.group("interface"))
-        print vlans
+            vlans[vlan_id].append(vl.group("interface"))
 
         bridges = {}
         br = self.cli("brctl show").split("\n")
@@ -47,20 +46,43 @@ class Script(noc.sa.script.Script):
                     match = self.rx_bridge_int.search(br[i])
 
 ### TODO :)
-#        cmd = "brctl show"
 #        if mac is not None:
 #            cmd = "brctl show %s"%self.profile.convert_mac(mac)
-#        if interface is not None:
-#            cmd += " interface %s"%interface
-#        if vlan is not None:
-#            bridge = 'br' + vlan
-#            cmd = "brctl showmacs %s"%bridge
-
+### This will work only when name of bridge looks like: "'br'+vlan_id"
+### We need found more universal way for bind VLAN to bridge, but how???
+### Configuration file for vlans is in difirent place in eche Linux distribution. Also I don't find any commands or records in /proc...
         r = []
+        if vlan is not None:
+            bridge = 'br' + str(vlan)
+            for match in self.rx_showmacs.finditer(self.cli("brctl showmacs %s"%bridge)):
+                if match.group("type") == "0.00":
+                    typ = "S"
+                else:
+                    typ = "D"
+                interfaces = bridges[bridge][int(match.group("port"))-1]
+                if '.' in interfaces:
+                    interfaces = interfaces.split('.')
+                    interfaces = interfaces[0]
+                if interface is not None:
+                    if interface == interfaces:
+                        r.append( {
+                                    "vlan_id"   : vlan,
+                                    "mac"       : match.group("mac"),
+                                    "interfaces": [interfaces],
+                                    "type"      : typ,
+                                    } )
+                else:
+                    r.append( {
+                                "vlan_id"   : vlan,
+                                "mac"       : match.group("mac"),
+                                "interfaces": [interfaces],
+                                "type"      : typ,
+                                } )
+#            if not r:
+#                raise Exception("Not implemented")
+            return r
+
         for vlan_id in vlans:
-# This will work only when name of bridge looks like: "'br'+vlan_id"
-# We need found more universal way for bind VLAN to bridge, but how???
-# Configuration file for vlans is in difirent place in eche Linux distribution. Also I don't find any commands or records in /proc...
             bridge = 'br' + vlan_id
             for match in self.rx_showmacs.finditer(self.cli("brctl showmacs %s"%bridge)):
                 if match.group("type") == "0.00":
@@ -71,12 +93,21 @@ class Script(noc.sa.script.Script):
                 if '.' in interfaces:
                     interfaces = interfaces.split('.')
                     interfaces = interfaces[0]
-                r.append( {
-                    "vlan_id"   : vlan_id,
-                    "mac"       : match.group("mac"),
-                    "interfaces": [interfaces],
-                    "type"      : typ,
-                } )
+                if interface is not None:
+                    if interface == interfaces:
+                        r.append( {
+                                    "vlan_id"   : vlan_id,
+                                    "mac"       : match.group("mac"),
+                                    "interfaces": [interfaces],
+                                    "type"      : typ,
+                                    } )
+                else:
+                    r.append( {
+                                "vlan_id"   : vlan_id,
+                                "mac"       : match.group("mac"),
+                                "interfaces": [interfaces],
+                                "type"      : typ,
+                                } )
 
 #        if not r:
 #            raise Exception("Not implemented")
