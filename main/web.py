7f7c1b043ba1e0df663c2c166bdc5398f010c883
Dmitry Volodin <dv@nocproject.org>
2011-11-08 23:58:22 +0300

    NOC-84: Stop forked noc-web children properly

--- main/web.py
+++ main/web.py
@@ -8,6 +8,9 @@
 
 ## Python modules
 import logging
+import os
+import errno
+import signal
 ## Django modules
 import django.core.handlers.wsgi
 ## Third-party modules
@@ -15,9 +18,9 @@ import tornado.ioloop
 import tornado.web
 import tornado.wsgi
 import tornado.httpserver
+from tornado.process import cpu_count
 ## NOC modules
 from noc.lib.daemon import Daemon
-from noc.lib.debug import error_report
 
 
 class AppStaticFileHandler(tornado.web.StaticFileHandler):
@@ -84,13 +87,52 @@ class Web(Daemon):
         ])
         logging.info("Loading site")
         logging.info("Listening %s:%s" % (address, port))
-        server = tornado.httpserver.HTTPServer(application)
-        server.bind(port, address)
-        # Fork multiple instances
-        server.start(self.config.getint("web", "workers"))
-        # Import and autodiscover site
-        # Called within function to initialize all process independently
-        import noc.lib.nosql  # Connect to mongodb
+        # Create tornado server
+        self.server = tornado.httpserver.HTTPServer(application)
+        self.server.bind(port, address)
+        # Run children
+        nc = self.config.getint("web", "workers")
+        if nc == 0:
+            nc = cpu_count()
+        self.t_children = set()
+        while True:
+            # Run children
+            while len(self.t_children) < nc:
+                pid = os.fork()
+                if pid == 0:
+                    self.children_loop()
+                else:
+                    logging.info("Running child %d" % pid)
+                    self.t_children.add(pid)
+            # Wait for status
+            try:
+                pid, status = os.wait()
+            except OSError, e:
+                if e.errno == errno.EINTR:
+                    continue
+                raise
+            if pid not in self.t_children:
+                continue
+            self.t_children.remove(pid)
+            logging.info("Exiting child %d" % pid)
+
+    def children_loop(self):
+        self.t_children = None
+        # Initialize pending sockets
+        sockets = self.server._pending_sockets
+        self.server._pending_sockets = []
+        self.server.add_sockets(sockets)
+        # Connect to mongodb
+        import noc.lib.nosql
+        # Initialize site
         from noc.lib.app import site
         site.autodiscover()
+        # Run children's I/O loop
         tornado.ioloop.IOLoop.instance().start()
+
+    def SIGTERM(self, signo, frame):
+        if self.t_children:
+            for pid in self.t_children:
+                logging.info("Stopping child %d" % pid)
+                os.kill(pid, signal.SIGTERM)
+        os._exit(0)
