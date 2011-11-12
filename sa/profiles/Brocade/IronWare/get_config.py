277e9dd4642a274171e06cfd7d2326c07044a51b
Dmitry Volodin <dv@nocproject.org>
2011-11-12 21:18:10 +0300

    NOC-142: Brocade.IronWare profile

---
+++ sa/profiles/Brocade/IronWare/get_config.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Brocade.IronWare.get_config
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetConfig
+
+
+class Script(NOCScript):
+    """
+    Brocade.IronWare.get_config
+    """
+    name = "Brocade.IronWare.get_config"
+    implements = [IGetConfig]
+
+    def execute(self):
+        config = self.cli("show config")
+        config = self.strip_first_lines(config, 2)
+        return self.cleaned_config(config)
