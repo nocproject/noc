a9495168a74d8abbefc43f291f3583d5908a7b77
Dmitry Volodin <dv@nocproject.org>
2011-11-13 12:20:10 +0300

    Fix activator unittests

--- sa/tests/activator.py
+++ sa/tests/activator.py
@@ -2,35 +2,51 @@
 ##----------------------------------------------------------------------
 ## Unittests for Activator
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2009 The NOC Project
+## Copyright (C) 2007-2011 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
+
+## NOC modules
 from noc.lib.test import ModelTestCase
-from noc.sa.models import *
-##
-##
-##
+from noc.sa.models import Activator
+from noc.main.models import Shard
+
+
 class ActivatorTestCase(ModelTestCase):
-    model=Activator
-    fixtures=["activator.json"]
-    
+    model = Activator
+
     def get_data(self):
+        shard = Shard.objects.all()[0]
         return [
-            {"name":"A3","is_active":True,"ip":"10.0.0.1","to_ip":"10.0.0.1"},
-            {"name":"A4","is_active":True,"ip":"10.0.0.2","to_ip":"10.0.0.2"}
+            {
+                "name": "A1",
+                "is_active": True,
+                "ip": "192.168.0.2",
+                "to_ip": "192.168.0.2",
+                "shard": shard
+            },
+
+            {
+                "name": "A2",
+                "is_active": True,
+                "ip": "192.168.1.0",
+                "to_ip": "192.168.1.255",
+                "shard": shard
+            },
+
+            {
+                "name": "A3",
+                "is_active": True,
+                "ip": "10.0.0.1",
+                "to_ip": "10.0.0.1",
+                "shard": shard
+            },
+
+            {
+                "name": "A4",
+                "is_active": True,
+                "ip": "10.0.0.2",
+                "to_ip": "10.0.0.2",
+                "shard": shard
+            }
         ]
-    
-    #def test_check_ip_access(self):
-    #    a1=Activator.objects.get(name="A1")
-    #    a2=Activator.objects.get(name="A2")
-    #    for ip,r1,r2 in [
-    #        ("192.168.0.1", False, False),
-    #        ("192.168.0.2", True,  False),
-    #        ("192.168.0.3", False, False),
-    #        ("192.168.0.255", False, False),
-    #        ("192.168.1.0", False, True),
-    #        ("192.168.1.128", False, True),
-    #        ("192.168.1.255", False, True),
-    #        ("192.168.2.0", False, False)
-    #        ]:
-    #        self.assertEquals(Activator.check_ip_access(ip),r1 or r2)
