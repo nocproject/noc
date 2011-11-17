c378f25d6f5f251b4f556a3497c54c3b4290311d
Dmitry Volodin <dv@nocproject.org>
2011-11-17 13:29:23 +0300

    NOC-149: ICommands.ignore_cli_errors parameter

--- sa/profiles/Generic/commands.py
+++ sa/profiles/Generic/commands.py
@@ -2,19 +2,29 @@
 ##----------------------------------------------------------------------
 ## Generic.commands
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2009 The NOC Project
+## Copyright (C) 2007-2011 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-"""
-"""
-import noc.sa.script
+
+## NOC modules
+from noc.sa.script import Script as NOCScript
 from noc.sa.interfaces import ICommands
-##
-## Execute a list of CLI commands and return a list of results
-##
-class Script(noc.sa.script.Script):
-    name="Generic.commands"
-    implements=[ICommands]
-    requires=[]
-    def execute(self,commands):
-        return [self.cli(c) for c in commands]
+
+
+class Script(NOCScript):
+    """
+    Execute a list of CLI commands and return a list of results
+    """
+    name = "Generic.commands"
+    implements = [ICommands]
+    requires = []
+
+    def execute(self, commands, ignore_cli_errors=False):
+        def safe_cli(c):
+            try:
+                return self.cli(c)
+            except self.CLISyntaxError, why:
+                return "%%ERROR: %s" % str(why)
+
+        cli = safe_cli if ignore_cli_errors else self.cli
+        return [cli(c) for c in commands]
--- sa/profiles/Generic/configure.py
+++ sa/profiles/Generic/configure.py
@@ -2,24 +2,35 @@
 ##----------------------------------------------------------------------
 ## Generic.configure
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2009 The NOC Project
+## Copyright (C) 2007-2011 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-"""
-"""
+
+## Python modules
 from __future__ import with_statement
-import noc.sa.script
+## NOC modules
+from noc.sa.script import Script as NOCScript
 from noc.sa.interfaces import ICommands
-##
-## Enter a configuration mode and execute a list of CLI commands. return a list of results
-##
-class Script(noc.sa.script.Script):
-    name="Generic.configure"
-    implements=[ICommands]
-    requires=[]
-    def execute(self,commands):
+
+
+class Script(NOCScript):
+    """
+    Enter a configuration mode and execute a list of CLI commands.
+    return a list of results
+    """
+    name = "Generic.configure"
+    implements = [ICommands]
+    requires = []
+
+    def execute(self, commands, ignore_cli_errors=False):
+        def safe_cli(c):
+            try:
+                return self.cli(c)
+            except self.CLISyntaxError, why:
+                return "%%ERROR: %s" % str(why)
+
+        cli = safe_cli if ignore_cli_errors else self.cli
         with self.configure():
-            r=[self.cli(c) for c in commands]
+            r = [cli(c) for c in commands]
         self.save_config()
         return r
-
