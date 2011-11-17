5d87aca3e33be1d96dad9a1c12bcb89b1a9ebe23
Dmitry Volodin <dv@nocproject.org>
2011-11-17 13:30:05 +0300

    NOC-149: CommandSnippet.ignore_cli_errors field

---
+++ sa/migrations/0045_snippet_ignore_errors.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Add CommandSnippet.ignore_cli_errors
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Third-party modules
+from south.db import db
+## NOC modules
+from noc.sa.models import *
+
+
+class Migration:
+    def forwards(self):
+        db.add_column("sa_commandsnippet", "ignore_cli_errors",
+                      models.BooleanField(_("Ignore CLI errors"),
+                                          default=False))
+
+    def backwards(self):
+        db.delete_column("sa_commandsnippet", "ignore_cli_errors")
