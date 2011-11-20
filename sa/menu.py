4f01cdf8dec8501d87af0254f0d74bf8bba92a28
Dmitry Volodin <dv@nocproject.org>
2011-11-20 14:45:42 +0300

    NOC-44: Restore dynamic menu functionality

--- sa/menu.py
+++ sa/menu.py
@@ -17,18 +17,22 @@ class SnippetsMenu(DynamicMenu):
     Snippets submenu
     """
     title = "Snippets"
+    icon = "icon_page_go"
+
     @property
     def items(self):
-        run_perm = HasPerm("sa:runsnippet:run")
-        for s in CommandSnippet.objects.filter(display_in_menu=True).order_by("name"):
+        run_perm = HasPerm("sa:runsnippet:launch")
+        for s in CommandSnippet.objects.filter(display_in_menu=True).order_by(
+            "name"):
             if s.permission_name:
                 s_perm = HasPerm(s.effective_permission_name)
             else:
                 s_perm = PermitLogged()
             yield (s.name,
-                   site.reverse("sa:runsnippet:snippet", s.id),
-                   lambda user: (run_perm & s_perm).check(None, user))
-        
+                   "sa.runsnippet",
+                   lambda user: (run_perm & s_perm).check(None, user)
+                )
+
 
 DYNAMIC_MENUS = [
     SnippetsMenu()
