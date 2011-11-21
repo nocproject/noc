4169312dae7e4bb564a787551bd1965ec63b967d
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2011-11-21 14:47:13 +0200

    Add "description" field to Cisco.IOS.get_switchport

--- sa/profiles/Cisco/IOS/get_switchport.py
+++ sa/profiles/Cisco/IOS/get_switchport.py
@@ -22,6 +22,21 @@ class Script(NOCScript):
     rx_line = re.compile(r"\n\nName:\s+", re.MULTILINE)
     rx_body = re.compile(r"^(?P<interface>\S+).+^Administrative Mode: (?P<amode>.+).+^Operational Mode: (?P<omode>.+).+^Administrative Trunking Encapsulation:.+^Access Mode VLAN: (?P<avlan>\d+) \(.+\).+Trunking Native Mode VLAN: (?P<nvlan>\d+) \(.+\).+Trunking VLANs Enabled: (?P<vlans>.+)Pruning VLANs Enabled:", re.MULTILINE | re.DOTALL)
 
+    rx_descr_if = re.compile(r"^(?P<interface>\S+)\s+(?:up|down|admin down|deleted)\s+(?:up|down)\s+(?P<description>.+)")
+
+    def get_description(self):
+        r = []
+        s = self.cli("show interfaces description", cached=True)
+        for l in s.split("\n"):
+            match = self.rx_descr_if.match(l.strip())
+            if not match:
+                continue
+            r+=[{
+                "interface": self.profile.convert_interface_name(match.group("interface")),
+                "description": match.group("description")
+            }]
+        return r
+
     def execute(self):
         r = []
         try:
@@ -50,21 +65,26 @@ class Script(NOCScript):
                 untagged = match.group("avlan")
                 tagged = []
 
+            shortname = self.profile.convert_interface_name(interface)
             members = []
             if interface.startswith("Po"):
-                shortname = self.profile.convert_interface_name(interface)
                 for p in self.scripts.get_portchannel():
                     if p["interface"] == shortname:
                         members = p["members"]
 
-            r += [{
+            iface = {
                 "interface"     : interface,
                 "status"        : match.group("omode").strip() != "down",
-                #"description"    : "Dummy";
                 "tagged"        : tagged,
                 "untagged"      : int(untagged),
                 "members"       : members,
                 "802.1Q Enabled": trunk,
                 "802.1ad Tunnel": False,
-            }]
+            }
+
+            for p in self.get_description():
+                if p["interface"] == shortname:
+                    iface["description"] = p["description"]
+
+            r += [iface]
         return r
