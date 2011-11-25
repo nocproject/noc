b0a23d971583687beb87ad8c5446371f727ea072
Stanislav Svirid <s.svirid@sibset-team.ru>
2011-11-25 12:47:55 +0600

    NOC-161: fix iframe resize

--- main/apps/desktop/js/IFramePanel.js
+++ main/apps/desktop/js/IFramePanel.js
@@ -7,19 +7,17 @@
 console.debug("Defining NOC.main.desktop.IFramePanel");
 
 Ext.define("NOC.main.desktop.IFramePanel", {
-    extend: "Ext.Panel",
+    extend: "Ext.Container",
+    layout: 'fit',
     initComponent: function() {
         this.items = [{
-            html: "<iframe style='border: none; width: 100%; height: 100%' src='" + this.noc.url + "'/>"
+            xtype : "component",
+            style : 'border: none',
+            autoEl : {
+                tag : "iframe",
+                src : this.noc.url
+            }
         }];
-        NOC.main.desktop.IFramePanel.superclass.initComponent.call(this);
-        this.on("resize", this.on_resize);
-    },
-    //
-    on_resize: function() {
-        // @todo: calculate IFRAME size properly
-        var iframe = this.items.first().el.down("iframe");
-        iframe.setWidth(this.getWidth());
-        iframe.setHeight(this.getHeight());
+        this.callParent(arguments);
     }
 });
