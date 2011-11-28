f2450d3b383712ecc4e40a106e83f9e5f0a2d2a9
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2011-11-28 11:36:54 +0200

    Fix SNMP in DLink.DxS.get_interface_status

--- sa/profiles/DLink/DxS/get_interface_status.py
+++ sa/profiles/DLink/DxS/get_interface_status.py
@@ -11,34 +11,40 @@ from noc.sa.script import Script as NOCScript
 from noc.sa.interfaces import IGetInterfaceStatus
 import re
 
+
 class Script(NOCScript):
-    name="DLink.DxS.get_interface_status"
-    implements=[IGetInterfaceStatus]
-    rx_line=re.compile(r"^\s*(?P<interface>\S+)\s+(?P<type>FE|GE|10GE|Fiber|1000BASE\-T|1000BASE\-X|10GBASE-R)\s+Link (?P<status>Up|Down)\s+(?P<test>\S+)\s+.*$",re.IGNORECASE|re.MULTILINE)
+    name = "DLink.DxS.get_interface_status"
+    implements = [IGetInterfaceStatus]
+    rx_line = re.compile(r"^\s*(?P<interface>\S+)\s+(?P<type>FE|GE|10GE|Fiber|1000BASE\-T|1000BASE\-X|10GBASE-R)\s+Link (?P<status>Up|Down)\s+(?P<test>\S+)\s+.*$", re.IGNORECASE | re.MULTILINE)
+
     def execute(self,interface=None):
-        # Not tested. Must be identical in different vendors
+        r = []
+        # Try SNMP first
         if self.snmp and self.access_profile.snmp_ro:
             try:
                 # Get interface status
-                r=[]
-                for n,s in self.snmp.join_tables("1.3.6.1.2.1.31.1.1.1.1","1.3.6.1.2.1.2.2.1.8",bulk=True): # IF-MIB::ifName, IF-MIB::ifOperStatus
-                    if not n.startswith("802.1Q Encapsulation Tag") \
-                    and (interface is not None and interface == n):
-                        r+=[{"interface":n,"status":int(s)==1}] # ifOperStatus up(1)
+                for n, s in self.snmp.join_tables("1.3.6.1.2.1.31.1.1.1.1", "1.3.6.1.2.1.2.2.1.8", bulk=True): # IF-MIB
+                    if '802.1Q Encapsulation Tag' not in n and 'System' not in n:
+                        iface = n.split('/')[1]
+                        if interface is not None:
+                            if interface == iface:
+                                r = [{"interface" : iface, "status" : int(s)==1}]
+                        else:
+                            r.append({"interface" : iface, "status" : int(s)==1})
                 return r
             except self.snmp.TimeOutError:
                 pass
+
         # Fallback to CLI
         if interface is None:
             interface = "all"
         try:
-            s=self.cli("cable_diag ports %s"%interface)
+            s = self.cli("cable_diag ports %s" % interface)
         except self.CLISyntaxError:
             raise self.NotSupportedError()
 
-        r=[]
         for match in self.rx_line.finditer(s):
-            r+=[{
+            r += [{
                 "interface" : match.group("interface"),
                 "status"    : match.group("status").lower() == "up"
                 }]
