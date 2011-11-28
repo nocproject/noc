82540158f775727c1247ddecc4fb74bcdf858cf1
Stanislav Svirid <s.svirid@sibset-team.ru>
2011-11-28 17:10:41 +0600

    Don't send POST request when delete menu items (on reload)

--- main/apps/desktop/js/NavTreeStore.js
+++ main/apps/desktop/js/NavTreeStore.js
@@ -9,9 +9,12 @@ console.debug("Defining NOC.main.desktop.NavTreeStore");
 Ext.define("NOC.main.desktop.NavTreeStore", {
     extend: "Ext.data.TreeStore",
     storeId: "NOC.main.desktop.NavTreeStore",
-    
+
     proxy: {
         type: "ajax",
-        url: "/main/desktop/navigation/"
-    }    
+        url: "/main/desktop/navigation/",
+        destroy: Ext.emptyFn,
+        create: Ext.emptyFn,
+        update: Ext.emptyFn
+    }
 });
