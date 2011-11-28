d230fc2c435707fc2c7a6c544a87312e960d073e
Dmitry Volodin <dv@nocproject.org>
2011-11-28 22:11:38 +0300

    NOC-15: ./noc csv-import --resolve=... option

--- main/management/commands/csv-import.py
+++ main/management/commands/csv-import.py
@@ -9,21 +9,31 @@
 ## Python modules
 from __future__ import with_statement
 import sys
+from optparse import make_option
 ## Django modules
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 from django.db import transaction
 from django.contrib.contenttypes.models import ContentType
 ## NOC modules
-from noc.lib.csvutils import csv_import
+from noc.lib.csvutils import csv_import, IR_FAIL, IR_SKIP, IR_UPDATE
 
 
 class Command(BaseCommand):
     help = "Import data from csv file"
 
+    option_list = BaseCommand.option_list + (
+        make_option("-r", "--resolve", action="store", dest="resolve",
+                    default="fail"),
+    )
+
     def _usage(self):
         print "Usage:"
-        print "%s csv-import <model> <file1> .. <fileN>" % (sys.argv[0])
-        print "Where <model> is one of:"
+        print "%s csv-import [--resolve=action] <model> <file1> .. <fileN>" % (sys.argv[0])
+        print "<action> is one of:"
+        print "        fail - fail when record is already exists"
+        print "        skip - skip duplicated records"
+        print "        update - update duplicated records"
+        print "<model> is one of:"
         for t in ContentType.objects.all():
             print "%s.%s" % (t.app_label, t.model)
         sys.exit(1)
@@ -40,12 +50,21 @@ class Command(BaseCommand):
                                         model=model).model_class()
         except ContentType.DoesNotExist:
             self._usage()
+        #
+        try:
+            resolve = {
+                "fail": IR_FAIL,
+                "skip": IR_SKIP,
+                "update": IR_UPDATE
+            }[options["resolve"]]
+        except KeyError:
+            raise CommandError("Invalid resolve option: %s" % options["resolve"])
         # Begin import
         transaction.enter_transaction_management()
         for f in args[1:]:
             print "Importing %s" % f
             with open(f) as f:
-                count, error = csv_import(m, f)
+                count, error = csv_import(m, f, resolution=resolve)
                 if count is None:
                     print "... Error: %s" % error
                     sys.exit(1)
