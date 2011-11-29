cf7a1cca0571e9bc8c5c2ba275156f35421f04ff
Dmitry Volodin <dv@nocproject.org>
2011-11-29 11:35:51 +0300

    NOC-34: /main/desktop/theme/<id> and /main/desktop/theme/lookup/ handlers

---
+++ main/apps/desktop/js/ThemeLookup.js
@@ -0,0 +1,12 @@
+//---------------------------------------------------------------------
+// NOC.main.desktop.ThemeLookup
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2011 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.main.desktop.ThemeLookup");
+
+Ext.define("NOC.main.desktop.ThemeLookup", {
+    extend: "NOC.core.Lookup",
+    url: "/main/desktop/theme/lookup/"
+});
