0f6edcc124a85b15c6193274cae8be73acb664f0
GNU Linux <linux.gnu@yandex.ru>
2011-11-29 18:02:47 +0700

    user: GNU Linux <linux.gnu@yandex.ru> branch 'default'
    Add new script: get_interface_status.py
    added sa/profiles/Zyxel/ZyNOS_EE/get_interface_status.py
    added sa/profiles/Zyxel/ZyNOS_EE/tests/Zyxel_ES_2024EE_3_50_LI_2__get_interface_status_0001.py
    
    Minor bugfix and remove coments.
    changed sa/profiles/Zyxel/ZyNOS_EE/__init__.py
    
    Add SNMP support.
    changed sa/profiles/Zyxel/ZyNOS_EE/get_chassis_id.py

---
+++ sa/profiles/Zyxel/ZyNOS_EE/get_interface_status.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Zyxel.ZyNOS_EE.get_interface_status
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Python modules
+import re
+## NOC modules
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetInterfaceStatus
+
+class Script(NOCScript):
+    name = "Zyxel.ZyNOS_EE.get_interface_status"
+    implements = [IGetInterfaceStatus]
+
+    rx_link = re.compile(r"^( |)(?P<interface>\d+)\s+\d+\s+(?P<status>\d)\s+\S+\s+\S+\s+\S+$", re.MULTILINE)
+
+    def execute(self, interface=None):
+        r = []
+
+        # Try SNMP first
+        if self.snmp and self.access_profile.snmp_ro:
+            try:
+                if interface is None:
+                    # Join # IF-MIB::ifIndex, IF-MIB::ifOperStatus
+                    for n, s in self.snmp.join_tables("1.3.6.1.2.1.2.2.1.1",
+                                                      "1.3.6.1.2.1.2.2.1.8",
+                                                      bulk=True,
+                                                      max_index=1023):
+                        r.append( {"interface": n, "status": int(s) == 1} )  # ifOperStatus up(1)
+                    return r
+                else:
+                    n = self.snmp.get("1.3.6.1.2.1.2.2.1.1.%d" % int(interface))
+                    s = self.snmp.get("1.3.6.1.2.1.2.2.1.8.%d" % int(interface))
+                    return [{"interface":n, "status":int(s) == 1}]
+            except self.snmp.TimeOutError:
+                pass
+
+        # Fallback to CLI
+        try:
+            s = self.cli("sys sw portstatus")
+        except self.CLISyntaxError:
+            raise self.NotSupportedError()
+        for match in self.rx_link.finditer(s):
+            if interface is None:
+                r.append( {
+                        "interface": match.group("interface"),
+                        "status": match.group("status") == '1'
+                        } )
+            else:
+                iface = match.group("interface")
+                if interface == iface:
+                    r = [{
+                        "interface": iface,
+                        "status": match.group("status") == '1'
+                        }]
+        if not r:
+            raise self.NotSupportedError()
+        return r
