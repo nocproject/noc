23b856a9ad12858c08c160f3fdded2330a0f3e70
GNU Linux <linux.gnu@yandex.ru>
2011-11-29 12:27:08 +0700

    user: GNU Linux <linux.gnu@yandex.ru> branch 'default'
    Bug fix!
    
    changed sa/profiles/OS/Linux/get_interface_status.py
    changed sa/profiles/OS/Linux/tests/OS_NanoStation2_3_6_1_4866_110330_1244_get_interface_status_0001.py
    changed sa/profiles/OS/Linux/tests/OS_RG_14xx_1_0_get_interface_status_0001.py

--- sa/profiles/OS/Linux/get_interface_status.py
+++ sa/profiles/OS/Linux/get_interface_status.py
@@ -17,22 +17,41 @@ class Script(NOCScript):
     implements = [IGetInterfaceStatus]
 
     rx_if_name = re.compile(r"^(?P<ifname>\S+)\s+Link encap:")
-    rx_if_status = re.compile(r"^\s+UP+\s+(?P<status>.+)+\s+MTU:+\d+\s+Metric:+\d+")
+    rx_if_status = re.compile(r"^\s+UP+(\s|BROADCAST\s)+(?P<status>.+)+\s")
 
-    def execute(self):
+    def execute(self, interface=None):
         r = []
-        for s in self.cli("ifconfig").split('\n'):
-            match = self.rx_if_name.search(s)
+        s = self.cli("ifconfig").split('\n')
+        for i in range(len(s)):
+            l = s[i]
+            match = self.rx_if_name.search(l)
             if match:
                 if_name = match.group("ifname")
-                continue
-            match = self.rx_if_status.search(s)
-            if match:
-                r += [{
-                    "interface" : if_name,
-                    "status"    : 'RUNNING' in match.group("status"),
-                    }]
-                continue
+                if interface:
+                    if if_name == interface:
+                        i += 1
+                        l = s[i]
+                        while l.strip()[:4] == 'inet':
+                            i += 1
+                            l = s[i]
+                        status = self.rx_if_status.search(l)
+                        if status:
+                            r = [{
+                                "interface" : if_name,
+                                "status"    : 'RUNNING' in status.group("status"),
+                                }]
+                elif '.' not in if_name and ( if_name[:3] == 'eth' or if_name[:3] == 'ath' ):
+                    i += 1
+                    l = s[i]
+                    while l.strip()[:4] == 'inet':
+                        i += 1
+                        l = s[i]
+                    status = self.rx_if_status.search(l)
+                    if status:
+                        r += [{
+                            "interface" : if_name,
+                            "status"    : 'RUNNING' in status.group("status"),
+                            }]
         if not r:
             raise Exception("Not implemented")
         return r
