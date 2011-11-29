d7bffa813130b746f3be8a826e492d3366d1d2bb
GNU Linux <linux.gnu@yandex.ru>
2011-11-29 18:02:47 +0700

    user: GNU Linux <linux.gnu@yandex.ru> branch 'default'
    Add new script: get_interface_status.py
    added sa/profiles/Zyxel/ZyNOS_EE/get_interface_status.py
    added sa/profiles/Zyxel/ZyNOS_EE/tests/Zyxel_ES_2024EE_3_50_LI_2__get_interface_status_0001.py
    
    Minor bugfix and remove coments.
    changed sa/profiles/Zyxel/ZyNOS_EE/__init__.py
    
    Add SNMP support.
    changed sa/profiles/Zyxel/ZyNOS_EE/get_chassis_id.py

--- sa/profiles/Zyxel/ZyNOS_EE/__init__.py
+++ sa/profiles/Zyxel/ZyNOS_EE/__init__.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
 ## Vendor: Zyxel
-## HW:     EE
-## OS:     ZyNOS
+## OS:     ZyNOS_EE
 ##----------------------------------------------------------------------
 ## Copyright (C) 2007-2011 The NOC Project
 ## See LICENSE for details
@@ -12,19 +11,14 @@
 import noc.sa.profiles
 from noc.sa.protocols.sae_pb2 import TELNET, SSH
 
-
 class Profile(noc.sa.profiles.Profile):
     name = "Zyxel.ZyNOS_EE"
     supported_schemes = [TELNET, SSH]
 #    pattern_username="User name:"
-    pattern_password = "Password:"
-#    pattern_unpriveleged_prompt=r"^\S+?>"
-    pattern_prompt = r"^\S+?>"
+    pattern_password = "Password: "
+    pattern_prompt = r"^\S+?> "
     pattern_more = r"^-- more --.*?$"
-    command_super = "enable"
     command_more = " "
-    command_enter_config = "config"
-    command_leave_config = "exit"
     command_exit = "exit"
     command_save_config = "config save"
-    pattern_syntax_error = "Invalid command"
+    pattern_syntax_error = r"^Valid commands are:"
--- sa/profiles/Zyxel/ZyNOS_EE/get_chassis_id.py
+++ sa/profiles/Zyxel/ZyNOS_EE/get_chassis_id.py
@@ -12,14 +12,23 @@ import re
 from noc.sa.script import Script as NOCScript
 from noc.sa.interfaces import IGetChassisID
 
-
 class Script(NOCScript):
     name = "Zyxel.ZyNOS_EE.get_chassis_id"
+    implements = [IGetChassisID]
     cache = True
+
     rx_ver = re.compile(r"^\sMAC Address\s:\s+(?P<id>\S+).",
                         re.IGNORECASE | re.MULTILINE | re.DOTALL)
-    implements = [IGetChassisID]
 
     def execute(self):
-        match = self.re_search(self.rx_ver, self.cli("sys mrd atsh"))
+        # Try SNMP first
+        if self.snmp and self.access_profile.snmp_ro:
+            try:
+                mac = self.snmp.get("1.3.6.1.2.1.17.1.1.0", cached=True)
+                return mac
+            except self.snmp.TimeOutError:
+                pass
+
+        # Fallback to CLI
+        match = self.re_search(self.rx_ver, self.cli("sys mrd atsh", cached=True))
         return match.group("id")
