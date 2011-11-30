d8b618ef19976f967dd51b0dbc413088becaf48a
Dmitry Volodin <dv@nocproject.org>
2011-11-30 21:55:34 +0300

    NOC-65: 3Com.SuperStack sends \r on connect

--- sa/profiles/3Com/SuperStack/__init__.py
+++ sa/profiles/3Com/SuperStack/__init__.py
@@ -3,23 +3,24 @@
 ## Vendor: 3Com
 ## OS:     SuperStack
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2010 The NOC Project
+## Copyright (C) 2007-2011 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-"""
-"""
+
+## NOC modules
 import noc.sa.profiles
-from noc.sa.protocols.sae_pb2 import TELNET,SSH
+from noc.sa.protocols.sae_pb2 import TELNET
+
 
 class Profile(noc.sa.profiles.Profile):
-    name="3Com.SuperStack"
-    supported_schemes=[TELNET]
-    pattern_username="Login:"
-    pattern_password="Password:"
-    pattern_prompt=r"^Select menu option.*:"
-    pattern_more=[
-        (r"Escape character is '\^\]'\.", "\r"),
+    name = "3Com.SuperStack"
+    supported_schemes = [TELNET]
+    pattern_username = "Login:"
+    pattern_password = "Password:"
+    pattern_prompt = r"^Select menu option.*:"
+    pattern_more = [
         (r"Enter <CR> for more or 'q' to quit--:", "\r"),
-        ]
-    command_submit="\r"
-    convert_mac=noc.sa.profiles.Profile.convert_mac_to_dashed
+    ]
+    command_submit = "\r"
+    telnet_send_on_connect = "\r"
+    convert_mac = noc.sa.profiles.Profile.convert_mac_to_dashed
