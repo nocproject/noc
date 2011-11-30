248f2238abc9bd235b69dbc758821c35f8fdbaa4
Dmitry Volodin <dv@nocproject.org>
2011-11-30 22:19:15 +0300

    NOC-145: noc.lib.nbsocket is converted into directory

---
+++ lib/nbsocket/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Clean and lightweight non-blocking socket I/O implementation
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+from lib.nbsocket.basesocket import *
