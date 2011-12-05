c4969bd38904ef52730824a3c5800e14d6fd7fa7
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2011-12-05 11:04:05 +0200

    Add DLink.DVG profile

---
+++ sa/profiles/DLink/DVG/get_version.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## DLink.DVG.get_version
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Python modules
+import re
+## NOC modules
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetVersion
+
+class Script(NOCScript):
+    name = "DLink.DVG.get_version"
+    implements = [IGetVersion]
+    cache = True
+
+    rx_platform = re.compile(r"^Software \[(==|\S+ ==) Ver\(+(?P<version>\S+)\s+\S+\s+\S+\)\s+PId\(+\S+\)\s+Drv\(+\S+\)\s+Hw\(+(?P<platform>\S+)+\) (== |== (?P<hardware>\S+))+\]$", re.MULTILINE)
+
+    platforms_cli = {
+        "??????"     : "DVG-2101S",
+        "DSA"        : "DVG-2102S",
+        "PNP1632-32" : "DVG-4032S",
+        "SA7S4"      : "DVG-5004S",
+        "TSO"        : "DVG-7111S",
+                    }
+
+    platforms_snmp = {
+        "3.2.10"    : "DVG-2101S",
+        "?.?.?"     : "DVG-2102S",
+        "?.?.?"     : "DVG-4032S",
+        "1.2.1"     : "DVG-5004S",
+        "?.?.?"     : "DVG-7111S",
+                    }
+
+    def execute(self):
+        # Try SNMP first
+        if self.snmp and self.access_profile.snmp_ro:
+            try:
+                platform = self.snmp.get("1.3.6.1.2.1.1.2.0", cached=True)
+                platform = platform.split(', ')
+                l = len(platform) - 1
+                platform = platform[l-2]+'.'+platform[l-1]+'.'+platform[l]
+                platform = self.platforms_snmp.get(platform.split(')')[0], '????')
+                return {
+                        "vendor"     : "DLink",
+                        "platform"   : platform,
+                        "version"    : '1.02.38.x',
+                        }
+            except self.snmp.TimeOutError:
+                pass
+
+        # Fallback to CLI
+        plat = self.cli("GET STATUS HARDWARE", cached=True)
+        match = self.rx_platform.search(plat)
+        platform = self.platforms_cli.get(match.group("platform"), '????')
+        r = {
+            "vendor"     : "DLink",
+            "platform"   : platform,
+            "version"    : match.group("version"),
+            }
+        if match.group("hardware"):
+            r["attributes"]["HW version"] = match.group("hardware")
+        return r
---
+++ sa/profiles/DLink/DVG/ping.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## DLink.DVG.ping
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Python modules
+import re
+## NOC modules
+import noc.sa.script
+from noc.sa.interfaces import IPing
+
+class Script(noc.sa.script.Script):
+    name = "DLink.DVG.ping"
+    implements = [IPing]
+
+    rx_result = re.compile(r"^(?P<count>\d+) packets transmitted, (?P<success>\d+) (packets received|received), \d+% packet loss$", re.MULTILINE)
+    rx_stat = re.compile(r"^round-trip min/avg/max = (?P<min>.+)/(?P<avg>.+)/(?P<max>.+) ms$", re.MULTILINE)
+
+    def execute(self, address, count=None, source_address=None, size=None, df=None):
+        cmd="ping %s"%address
+        if count:
+            cmd += " %d"%int(count)
+        else:
+            cmd += " 5"
+        if size:
+            cmd += " %d"%int(size)
+        else:
+            cmd += " 64"
+        ping = self.cli(cmd)
+        result = self.rx_result.search(ping)
+        if result:
+            r = {
+                "success" : result.group("success"),
+                "count"   : result.group("count"),
+                }
+        else:
+            raise self.NotSupportedError()
+        stat = self.rx_stat.search(ping)
+        if stat:
+            r.update({
+                    "min" : stat.group("min"),
+                    "avg" : stat.group("avg"),
+                    "max" : stat.group("max"),
+                    })
+        return r
