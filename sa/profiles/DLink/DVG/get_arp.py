54e7ff3f60e60128fe22ff98e462f78189f43a61
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2011-12-05 11:12:46 +0200

    NOC-146 PEP8 formatting

--- sa/profiles/DLink/DVG/get_arp.py
+++ sa/profiles/DLink/DVG/get_arp.py
@@ -10,6 +10,7 @@
 import noc.sa.script
 from noc.sa.interfaces import IGetARP
 
+
 class Script(noc.sa.script.Script):
     name = "DLink.DVG.get_arp"
     implements = [IGetARP]
@@ -17,12 +18,17 @@ class Script(noc.sa.script.Script):
 
     def execute(self):
         r = []
-# BUG http://bt.nocproject.org/browse/NOC-36
+        # BUG http://bt.nocproject.org/browse/NOC-36
         # Only one way: SNMP.
         if self.snmp and self.access_profile.snmp_ro:
             try:
-                for ip, mac, i in self.snmp.join_tables("1.3.6.1.2.1.4.22.1.3", "1.3.6.1.2.1.4.22.1.2", "1.3.6.1.2.1.4.22.1.1", bulk=True, cached=True): # IP-MIB
-                    r.append( {"ip" : ip, "mac" : mac, "interface" : self.snmp.get("1.3.6.1.2.1.2.2.1.2"+'.'+i, cached=True) } ) # IF-MIB
+                for ip, mac, i in self.snmp.join_tables("1.3.6.1.2.1.4.22.1.3",
+                                                        "1.3.6.1.2.1.4.22.1.2",
+                                                        "1.3.6.1.2.1.4.22.1.1",
+                                                        bulk=True,
+                                                        cached=True):  # IP-MIB
+                    r += [{"ip": ip, "mac": mac, "interface": self.snmp.get(
+                        "1.3.6.1.2.1.2.2.1.2" + '.' + i, cached=True)}]  # IF-MIB
                 return r
             except self.snmp.TimeOutError:
                 raise self.NotSupportedError()
--- sa/profiles/DLink/DVG/get_version.py
+++ sa/profiles/DLink/DVG/get_version.py
@@ -12,28 +12,31 @@ import re
 from noc.sa.script import Script as NOCScript
 from noc.sa.interfaces import IGetVersion
 
+
 class Script(NOCScript):
     name = "DLink.DVG.get_version"
     implements = [IGetVersion]
     cache = True
 
-    rx_platform = re.compile(r"^Software \[(==|\S+ ==) Ver\(+(?P<version>\S+)\s+\S+\s+\S+\)\s+PId\(+\S+\)\s+Drv\(+\S+\)\s+Hw\(+(?P<platform>\S+)+\) (== |== (?P<hardware>\S+))+\]$", re.MULTILINE)
+    rx_platform = re.compile(
+        r"^Software \[(==|\S+ ==) Ver\(+(?P<version>\S+)\s+\S+\s+\S+\)\s+PId\(+\S+\)\s+Drv\(+\S+\)\s+Hw\(+(?P<platform>\S+)+\) (== |== (?P<hardware>\S+))+\]$",
+        re.MULTILINE)
 
     platforms_cli = {
-        "??????"     : "DVG-2101S",
-        "DSA"        : "DVG-2102S",
-        "PNP1632-32" : "DVG-4032S",
-        "SA7S4"      : "DVG-5004S",
-        "TSO"        : "DVG-7111S",
-                    }
+        "??????": "DVG-2101S",
+        "DSA": "DVG-2102S",
+        "PNP1632-32": "DVG-4032S",
+        "SA7S4": "DVG-5004S",
+        "TSO": "DVG-7111S",
+        }
 
     platforms_snmp = {
-        "3.2.10"    : "DVG-2101S",
-        "?.?.?"     : "DVG-2102S",
-        "?.?.?"     : "DVG-4032S",
-        "1.2.1"     : "DVG-5004S",
-        "?.?.?"     : "DVG-7111S",
-                    }
+        "3.2.10": "DVG-2101S",
+        "?.?.?": "DVG-2102S",
+        "?.?.?": "DVG-4032S",
+        "1.2.1": "DVG-5004S",
+        "?.?.?": "DVG-7111S",
+        }
 
     def execute(self):
         # Try SNMP first
@@ -42,13 +45,15 @@ class Script(NOCScript):
                 platform = self.snmp.get("1.3.6.1.2.1.1.2.0", cached=True)
                 platform = platform.split(', ')
                 l = len(platform) - 1
-                platform = platform[l-2]+'.'+platform[l-1]+'.'+platform[l]
-                platform = self.platforms_snmp.get(platform.split(')')[0], '????')
+                platform = (platform[l - 2] + '.' + platform[l - 1] +
+                            '.' + platform[l])
+                platform = self.platforms_snmp.get(platform.split(')')[0],
+                                                   '????')
                 return {
-                        "vendor"     : "DLink",
-                        "platform"   : platform,
-                        "version"    : '1.02.38.x',
-                        }
+                    "vendor": "DLink",
+                    "platform": platform,
+                    "version": '1.02.38.x',
+                    }
             except self.snmp.TimeOutError:
                 pass
 
@@ -57,9 +62,9 @@ class Script(NOCScript):
         match = self.rx_platform.search(plat)
         platform = self.platforms_cli.get(match.group("platform"), '????')
         r = {
-            "vendor"     : "DLink",
-            "platform"   : platform,
-            "version"    : match.group("version"),
+            "vendor": "DLink",
+            "platform": platform,
+            "version": match.group("version"),
             }
         if match.group("hardware"):
             r["attributes"]["HW version"] = match.group("hardware")
