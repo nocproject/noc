7aa0b9a0d24392cf16319bebb5775523da0ced78
Dmitry Volodin <dv@nocproject.org>
2011-12-08 11:09:23 +0300

    NOC-145: Protocols are moved into noc.lib.nbsocket.protocols

--- lib/nbsocket/protocols/__init__.py
+++ lib/nbsocket/protocols/__init__.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
-## Clean and lightweight non-blocking socket I/O implementation
+## Protocol parsers
 ##----------------------------------------------------------------------
 ## Copyright (C) 2007-2011 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 
-from noc.lib.nbsocket.exceptions import *
-from noc.lib.nbsocket.protocols import *
-from noc.lib.nbsocket.basesocket import *
-from noc.lib.nbsocket.socketfactory import *
+## NOC modules
+from noc.lib.nbsocket.protocols.base import Protocol
