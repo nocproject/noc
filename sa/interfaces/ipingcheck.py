a08f7fbf6deb2bf9213fb86652035e29361a9525
Dmitry Volodin <dv@nocproject.org>
2011-12-08 23:40:49 +0300

    NOC-76: Better error reporting

--- sa/interfaces/ipingcheck.py
+++ sa/interfaces/ipingcheck.py
@@ -3,17 +3,18 @@
 ## IPingCheck
 ## SAE service to check address availability
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2010 The NOC Project
+## Copyright (C) 2007-2011 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-"""
-"""
+
+## NOC modules
 from base import *
 
+
 class IPingCheck(Interface):
-    activator_name=StringParameter()
-    addresses=ListOfParameter(IPParameter())
-    returns=ListOfParameter(DictParameter(attrs={
-        "ip"     : IPParameter(),
-        "status" : BooleanParameter()
+    activator_name = StringParameter()
+    addresses = ListOfParameter(IPParameter())
+    returns = ListOfParameter(DictParameter(attrs={
+        "ip": IPParameter(),
+        "status": BooleanParameter()
     }))
