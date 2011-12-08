a131c3249e98245a594a2b43f3bda574b6029d0c
GNU Linux <linux.gnu@yandex.ru>
2011-12-08 19:24:25 +0700

    user: GNU Linux <linux.gnu@yandex.ru> branch 'default'
    Add new NOC script get_portchannel.
    added sa/profiles/Linksys/SPS2xx/get_portchannel.py

---
+++ sa/profiles/Linksys/SPS2xx/get_portchannel.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Linksys.SPS2xx.get_portchannel
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Python modules
+import re
+## NOC modules
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetPortchannel
+
+class Script(NOCScript):
+    name = "Linksys.SPS2xx.get_portchannel"
+    implements = [IGetPortchannel]
+
+    rx_trunk = re.compile(r"^(?P<port>ch\d+)\s+(?P<type1>\S+):\s+(?P<interfaces1>\S+)+(\s+(?P<type2>\S+):\s+(?P<interfaces2>\S+)$|$)", re.MULTILINE)
+
+    def execute(self):
+        r = []
+        # Try SNMP first
+        # TODO snmp support
+
+        # Fallback to CLI
+        for match in self.rx_trunk.finditer(self.cli("show interfaces port-channel")):
+            typ = match.group("type1")
+            mem = match.group("interfaces1").split('/')
+            l = len(mem)
+            mem1 = self.expand_rangelist(mem[l-1])
+            members = []
+            mem2 = ''
+            for j in range(l-1):
+                mem2 += mem[j] + '/'
+            for i in mem1:
+                members.append(mem2 + str(i))
+            mem = match.group("interfaces2")
+            if mem:
+                typ = match.group("type2")
+                mem = mem.split('/')
+                l = len(mem)
+                mem1 = self.expand_rangelist(mem[l-1])
+                mem2 = ''
+                for j in range(l-1):
+                    mem2 += mem[j] + '/'
+                for i in mem1:
+                    members.append(mem2 + str(i))
+            r += [{
+                "interface" : match.group("port"),
+                "type"      : "L" if typ == "Non-candidate" else "S", # TODO batter type detection
+                "members"   : members,
+                }]
+        return r
