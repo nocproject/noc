8ac56823d4c35708153e8d3191c8c2aab804efca
Dmitry Volodin <dv@nocproject.org>
2011-12-08 17:07:32 +0300

    PEP8 formatting

--- sa/profiles/Audiocodes/Mediant2000/__init__.py
+++ sa/profiles/Audiocodes/Mediant2000/__init__.py
@@ -3,16 +3,16 @@
 ## Vendor: Audiocodes
 ## OS:     Mediant2000
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2009 The NOC Project
+## Copyright (C) 2007-2011 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-"""
-"""
-import noc.sa.profiles
-from noc.sa.protocols.sae_pb2 import TELNET,SSH,HTTP
 
-class Profile(noc.sa.profiles.Profile):
-    name="Audiocodes.Mediant2000"
-    supported_schemes=[TELNET,SSH,HTTP]
-    pattern_more="^ -- More --"
+## NOC modules
+from noc.sa.profiles import Profile as NOCProfile
+
+
+class Profile(NOCProfile):
+    name = "Audiocodes.Mediant2000"
+    supported_schemes = [NOCProfile.TELNET, NOCProfile.SSH, NOCProfile.HTTP]
+    pattern_more = "^ -- More --"
 
--- sa/profiles/Audiocodes/Mediant2000/get_version.py
+++ sa/profiles/Audiocodes/Mediant2000/get_version.py
@@ -1,39 +1,45 @@
 # -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2009 The NOC Project
+## Copyright (C) 2007-2011 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-"""
-"""
-import noc.sa.script
+
+## Python modules
+import re
+## NOC modules
+from noc.sa.script import Script as NOCScript
 from noc.sa.interfaces import IGetVersion
 from noc.lib.text import strip_html_tags
-import re
 
-rx_ver=re.compile(r"^Board type: (?P<platform>.+), firmware version (?P<version>\S+)",re.MULTILINE|re.DOTALL)
-rx_html_ver=re.compile(r"Version ID:\s+(?P<version>\S+)",re.MULTILINE|re.DOTALL)
 
-class Script(noc.sa.script.Script):
-    name="Audiocodes.Mediant2000.get_version"
-    cache=True
-    implements=[IGetVersion]
+class Script(NOCScript):
+    name = "Audiocodes.Mediant2000.get_version"
+    cache = True
+    implements = [IGetVersion]
+
+    rx_ver = re.compile(
+        r"^Board type: (?P<platform>.+), firmware version (?P<version>\S+)",
+        re.MULTILINE | re.DOTALL)
+    rx_html_ver = re.compile(r"Version ID:\s+(?P<version>\S+)",
+                             re.MULTILINE | re.DOTALL)
+
     def execute(self):
-        if self.access_profile.scheme in [self.TELNET,self.SSH]:
-            v=self.cli("show info")
-            match=rx_ver.search(v)
+        if self.access_profile.scheme in [self.TELNET, self.SSH]:
+            v = self.cli("show info")
+            match = self.rx_ver.search(v)
             return {
-                "vendor"    : "Audiocodes",
-                "platform"  : match.group("platform"),
-                "version"   : match.group("version"),
-            }
-        elif self.access_profile.scheme==self.HTTP:
-            v=self.http.get("/SoftwareVersion")
-            v=strip_html_tags(v)
-            match=rx_html_ver.search(v)
+                "vendor": "Audiocodes",
+                "platform": match.group("platform"),
+                "version": match.group("version"),
+                }
+        elif self.access_profile.scheme == self.HTTP:
+            v = self.http.get("/SoftwareVersion")
+            v = strip_html_tags(v)
+            match = self.rx_html_ver.search(v)
             return {
-                "vendor"    : "Audiocodes",
-                "platform"  : "Mediant2000",
-                "version"   : match.group("version"),
-            }
+                "vendor": "Audiocodes",
+                "platform": "Mediant2000",
+                "version": match.group("version"),
+                }
         else:
             raise Exception("Unsupported access scheme")
