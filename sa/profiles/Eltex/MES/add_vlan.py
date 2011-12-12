384ce7fa9699117b40bdcf52be0e0fe6b566618d
GNU Linux <linux.gnu@yandex.ru>
2011-12-12 17:35:59 +0700

    user: GNU Linux <linux.gnu@yandex.ru>
    PEP8 formating.
    branch 'default'
    changed sa/profiles/Eltex/MES/add_vlan.py

--- sa/profiles/Eltex/MES/add_vlan.py
+++ sa/profiles/Eltex/MES/add_vlan.py
@@ -12,6 +12,7 @@ from __future__ import with_statement
 import noc.sa.script
 from noc.sa.interfaces import IAddVlan
 
+
 class Script(noc.sa.script.Script):
     name = "Eltex.MES.add_vlan"
     implements = [IAddVlan]
@@ -19,14 +20,14 @@ class Script(noc.sa.script.Script):
     def execute(self, vlan_id, name, tagged_ports):
         a = ''
         if not self.scripts.has_vlan(vlan_id=vlan_id):
-            a = 1;
+            a = 1
         with self.configure():
             if a:
                 self.cli("vlan database")
-                self.cli("vlan %d"%vlan_id)
+                self.cli("vlan %d" % vlan_id)
                 self.cli("exit")
-                self.cli("interface vlan %d"%vlan_id)
-                self.cli("name %s"%name)
+                self.cli("interface vlan %d" % vlan_id)
+                self.cli("name %s" % name)
                 self.cli("exit")
             if tagged_ports:
                 ports = ''
@@ -35,10 +36,10 @@ class Script(noc.sa.script.Script):
                         ports = ports + ',' + port
                     else:
                         ports = port
-                self.cli("interface range %s"%ports)
+                self.cli("interface range %s" % ports)
 ### 802.1q
-#                self.cli("switchport general allowed vlan add %d tagged"%vlan_id)
+#                self.cli("switchport general allowed vlan add %d tagged" % vlan_id)
 ## trunk
-                self.cli("switchport trunk allowed vlan add  %d"%vlan_id)
+                self.cli("switchport trunk allowed vlan add  %d" % vlan_id)
         self.save_config()
         return True
