19f17c49198c6b3c5089ea0629dbc01bbf66cd57
Dmitry Volodin <dv@nocproject.org>
2011-12-14 11:27:12 +0300

    NOC-213: ./noc import-mib --force option

--- fm/management/commands/import-mib.py
+++ fm/management/commands/import-mib.py
@@ -2,11 +2,12 @@
 ##----------------------------------------------------------------------
 ## Load MIBs
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2009 The NOC Project
+## Copyright (C) 2007-2011 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-"""
-"""
+
+## Python modules
+from optparse import make_option
 ## Django modules
 from django.core.management.base import BaseCommand, CommandError
 ## NOC modules
@@ -15,10 +16,14 @@ from noc.fm.models import MIB, MIBRequiredException
 
 class Command(BaseCommand):
     help = "Import MIBs into database"
+    option_list = BaseCommand.option_list + (
+        make_option("-f", "--force", dest="force", action="store_true",
+                    default=False),
+        )
 
     def handle(self, *args, **options):
         for a in args:
             try:
-                MIB.load(a)
+                MIB.load(a, force=options.get("force"))
             except MIBRequiredException, why:
                 raise CommandError(why)
