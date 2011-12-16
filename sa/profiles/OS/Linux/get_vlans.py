034037b885940ac3b645451c9f84b4d96a76307a
GNU Linux <linux.gnu@yandex.ru>
2011-12-16 11:07:27 +0700

    PEP8 formating. changed sa/profiles/OS/Linux/get_arp.py changed sa/profiles/OS/Linux/get_chassis_id.py changed sa/profiles/OS/Linux/get_config.py changed sa/profiles/OS/Linux/get_fqdn.py changed sa/profiles/OS/Linux/get_interface_status.py changed sa/profiles/OS/Linux/get_local_users.py changed sa/profiles/OS/Linux/get_mac_address_table.py changed sa/profiles/OS/Linux/get_version.py changed sa/profiles/OS/Linux/get_vlans.py changed sa/profiles/OS/Linux/highlight.py changed sa/profiles/OS/Linux/ping.py

--- sa/profiles/OS/Linux/get_vlans.py
+++ sa/profiles/OS/Linux/get_vlans.py
@@ -12,23 +12,27 @@ import re
 from noc.sa.script import Script as NOCScript
 from noc.sa.interfaces import IGetVlans
 
-rx_vlan = re.compile(r"^(?P<interface>\S+)\s+\|+\s+(?P<vlan>\d+)\s+\|+\s+\S+$", re.MULTILINE)
 
 class Script(NOCScript):
     name = "OS.Linux.get_vlans"
     implements = [IGetVlans]
 
+    rx_vlan = re.compile(
+        r"^(?P<interface>\S+)\s+\|+\s+(?P<vlan>\d+)\s+\|+\s+\S+$",
+        re.MULTILINE)
+
     def execute(self):
         r = []
-        for vlan in rx_vlan.finditer(self.cli("cat /proc/net/vlan/config")):
+        vconfig = self.cli("cat /proc/net/vlan/config")
+        for vlan in self.rx_vlan.finditer(vconfig):
             vlan_id = vlan.group("vlan")
 # TODO vlan name... Looking for way find vlan name in GNU/Linux and *BSD
             name = 'br' + vlan_id
-            if {"vlan_id" : vlan_id, "name" : name } not in r:
-                r.append( {
-                            "vlan_id" : vlan_id,
-                            "name"    : name,
-                            } )
+            if {"vlan_id": vlan_id, "name": name} not in r:
+                r.append({
+                            "vlan_id": vlan_id,
+                            "name": name,
+                            })
         if not r:
             raise Exception("Not implemented")
         return r
