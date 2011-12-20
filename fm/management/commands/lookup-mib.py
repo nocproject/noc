2bdc6a6e6b4ec3f2727a5b1177c7bcca235bea3a
Dmitry Volodin <dv@nocproject.org>
2011-12-20 09:55:27 +0300

    NOC-245: ./noc lookup-mib command

---
+++ fm/management/commands/lookup-mib.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Lookup MIB
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Python modules
+import re
+## Django modules
+from django.core.management.base import BaseCommand, CommandError
+## NOC modules
+from noc.fm.models import MIB
+
+
+class Command(BaseCommand):
+    help = "Lookup MIBs"
+
+    rx_oid = re.compile("^\d+(\.\d+)+")
+
+    def handle(self, *args, **options):
+        for a in args:
+            self.lookup_mib(a)
+
+    def lookup_mib(self, v):
+        if self.rx_oid.match(v):
+            # oid -> name
+            print MIB.get_name(v)
+        else:
+            print MIB.get_oid(v)
