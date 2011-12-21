ac483663d9d101463f6ad779a74b11973e1447e6
Dmitry Volodin <dv@nocproject.org>
2011-12-21 23:18:41 +0300

    NOC-224: Schedule interface discovery on config changed

--- main/pyrules/refresh_config.py
+++ main/pyrules/refresh_config.py
@@ -11,14 +11,23 @@
 ## Copyright (C) 2007-2011 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-import datetime
 
-DELAY = 10  # Minutes to delay config fetch
+## Python modules
+import datetime
+## NOC modules
+from noc.inv.models import DiscoveryStatusInterface
+
+CONFIG_DELAY = 10  # Minutes to delay config fetch
+INTERFACE_DELAY = 15  # Minutes to delay interface fetch
 
 
 @pyrule
 def refresh_config(event):
-    next_pull = event.timestamp + datetime.timedelta(minutes=DELAY)
+    # Check managed object is managed
+    if not event.managed_object.is_managed:
+        return
+    # Schedule config fetch
+    next_pull = event.timestamp + datetime.timedelta(minutes=CONFIG_DELAY)
     if next_pull < datetime.datetime.now():  # next_pull points to the past
         return
     try:
@@ -27,3 +36,6 @@ def refresh_config(event):
         return  # No config found
     config.next_pull = next_pull
     config.save()
+    # Schedule interface fetch
+    DiscoveryStatusInterface.reschedule(event.managed_object,
+                                        INTERFACE_DELAY * 60)
