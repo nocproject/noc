b7107e407796e4a95aeadcb8363500d766899d2f
Dmitry Volodin <dv@nocproject.org>
2011-12-21 23:30:41 +0300

    PEP8 formatting

--- vc/apps/vcdomain/views.py
+++ vc/apps/vcdomain/views.py
@@ -2,32 +2,40 @@
 ##----------------------------------------------------------------------
 ## VCDomain Manager
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2010 The NOC Project
+## Copyright (C) 2007-2011 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-from django.contrib import admin
-from noc.lib.app import ModelApplication
-from noc.vc.models import VCDomain,VCDomainProvisioningConfig
-##
-## VCDomain admin
-##
-##
-## Admin form for VCDomain
-##
-class VCDomainProvisioningConfigAdmin(admin.TabularInline):
-    model=VCDomainProvisioningConfig
-    extra=3
-    
-class VCDomainAdmin(admin.ModelAdmin):
-    list_display=["name","type","enable_provisioning","enable_vc_bind_filter","description"]
-    search_fields=["name"]
-    list_filter=["type","enable_provisioning","enable_vc_bind_filter"]
-    inlines=[VCDomainProvisioningConfigAdmin]
 
-##
-## VCDomain application
-##
+## Django modules
+from django.contrib import admin
+## NOC modules
+from noc.lib.app import ModelApplication
+from noc.vc.models import VCDomain, VCDomainProvisioningConfig
+
+
+class VCDomainProvisioningConfigAdmin(admin.TabularInline):
+    """
+    Inline admin for VC Domain
+    """
+    model = VCDomainProvisioningConfig
+    extra = 3
+
+
+class VCDomainAdmin(admin.ModelAdmin):
+    """
+    VC Domain Admin
+    """
+    list_display = ["name", "type", "enable_provisioning",
+                    "enable_vc_bind_filter", "description"]
+    search_fields = ["name"]
+    list_filter = ["type", "enable_provisioning", "enable_vc_bind_filter"]
+    inlines = [VCDomainProvisioningConfigAdmin]
+
+
 class VCDomainApplication(ModelApplication):
-    model=VCDomain
-    model_admin=VCDomainAdmin
-    menu="Setup | VC Domains"
+    """
+    VC Domain application
+    """
+    model = VCDomain
+    model_admin = VCDomainAdmin
+    menu = "Setup | VC Domains"
