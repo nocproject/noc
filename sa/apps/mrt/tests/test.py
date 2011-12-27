c335277623de7f27da957db01ada572ee7d9e702
Dmitry Volodin <dv@nocproject.org>
2011-12-27 21:56:23 +0300

    NOC-210: sa.mrt application

---
+++ sa/apps/mrt/tests/test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## sa.mrt unittests
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+from noc.lib.test import AjaxTestCase
+
+
+class mrtTestCase(AjaxTestCase):
+    pass
