9a6fafdf951831cc700ddec02f901d2c9712bc26
Dmitry Volodin <dv@nocproject.org>
2012-01-10 14:49:14 +0400

    NOC-286: TMS-compliant tile server

---
+++ gis/apps/tms/js/Application.js
@@ -0,0 +1,13 @@
+//---------------------------------------------------------------------
+// gis.tile application
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2012 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.gis.tile.Application");
+
+Ext.define("NOC.gis.tile.Application", {
+    extend: "NOC.core.Application",
+    //requires: [],
+    items: []
+});
