ec3419b669759002a2ffdab88b3b948f305c6cc0
Dmitry Volodin <dv@nocproject.org>
2012-01-10 17:05:34 +0400

    NOC-287: gis.srs application

---
+++ gis/apps/srs/js/Lookup.js
@@ -0,0 +1,12 @@
+//---------------------------------------------------------------------
+// NOC.gis.srs.Lookup
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2012 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.gis.srs.Lookup");
+
+Ext.define("NOC.gis.srs.Lookup", {
+    extend: "NOC.core.Lookup",
+    url: "/gis/srs/lookup/"
+});
---
+++ gis/apps/srs/js/Model.js
@@ -0,0 +1,31 @@
+//---------------------------------------------------------------------
+// gis.srs Model
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2011 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.gis.srs.Model");
+
+Ext.define("NOC.gis.srs.Model", {
+    extend: "Ext.data.Model",
+    rest_url: "/gis/srs/",
+
+    fields: [
+        {
+            name: "srid",
+            type: "int"
+        },
+        {
+            name: "auth_name",
+            type: "string"
+        },
+        {
+            name: "auth_srid",
+            type: "int"
+        },
+        {
+            name: "proj4text",
+            type: "string"
+        }
+    ]
+});
