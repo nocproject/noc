76f77e216557c3fa0828d5d98a91d0e2d21c5bd8
GNU Linux <linux.gnu@yandex.ru>
2012-01-11 12:14:13 +0700

    Minor bug fix. changed sa/profiles/Eltex/MES/get_switchport.py
    Add SNMP support!
    changed sa/profiles/Linksys/SPS2xx/get_switchport.py

--- sa/profiles/Linksys/SPS2xx/get_switchport.py
+++ sa/profiles/Linksys/SPS2xx/get_switchport.py
@@ -50,6 +50,123 @@ class Script(NOCScript):
         except self.CLISyntaxError:
             pass
 
+        # Try snmp first
+        if self.snmp and self.access_profile.snmp_ro:
+            def hex2bin(ports):
+                bin = [
+                    '0000', '0001', '0010', '0011',
+                    '0100', '0101', '0110', '0111',
+                    '1000', '1001', '1010', '1011',
+                    '1100', '1101', '1110', '1111',
+                    ]
+                ports = ["%02x" % ord(c) for c in ports]
+                p = ''
+                for c in ports:
+                   for i in range(len(c)):
+                        p += bin[int(c[i], 16)]
+                return p
+            try:
+                # Make a list of tags for each interface or portchannel
+                port_vlans = {}
+                for vlan, name in self.snmp.join_tables(
+                    "1.3.6.1.2.1.17.7.1.4.2.1.3", "1.3.6.1.2.1.17.7.1.4.3.1.1",
+                    bulk=True):
+
+                    oid = "1.3.6.1.2.1.17.7.1.4.2.1.5.0." + str(vlan)
+                    ports = self.snmp.get(oid)
+                    s = hex2bin(ports)
+                    un = []
+                    for i in range(len(s)):
+                        if s[i] == '1':
+                            oid = "1.3.6.1.2.1.31.1.1.1.1." + str(i + 1)
+                            iface = self.snmp.get(oid, cached=True)
+                            if iface not in port_vlans:
+                                port_vlans.update(
+                                    {iface: {
+                                        "tagged": [],
+                                        "untagged": '',
+                                        }
+                                    })
+                            port_vlans[iface]["untagged"] = vlan
+                            un += [str(i + 1)]
+
+                    oid = "1.3.6.1.2.1.17.7.1.4.2.1.4.0." + str(vlan)
+                    ports = self.snmp.get(oid)
+                    s = hex2bin(ports)
+                    for i in range(len(s)):
+                        if s[i] == '1' and str(i + 1) not in un:
+                            oid = "1.3.6.1.2.1.31.1.1.1.1." + str(i + 1)
+                            iface = self.snmp.get(oid, cached=True)
+                            if iface not in port_vlans:
+                                port_vlans.update(
+                                    {iface: {
+                                        "tagged": [],
+                                        "untagged": '',
+                                        }
+                                    })
+                            port_vlans[iface]["tagged"].append(vlan)
+
+                # Get switchport description
+                port_descr = {}
+                for iface, description in self.snmp.join_tables(
+                    "1.3.6.1.2.1.31.1.1.1.1", "1.3.6.1.2.1.31.1.1.1.18",
+                    bulk=True):
+                    port_descr.update({iface: description})
+
+                # Get switchport data and overall result
+                r = []
+                swp = {}
+                write = False
+                for name in interface_status:
+                    if name in portchannel_members:
+                        for p in portchannels:
+                            if name in p["members"]:
+                                name = p["interface"]
+                                status = False
+                                for interface in p["members"]:
+                                    if interface_status.get(interface):
+                                        status = True
+                                description = port_descr[name]
+                                if not description:
+                                    description = ''
+                                members = p["members"]
+                                portchannels.remove(p)
+                                write = True
+                                break
+                    else:
+                        if interface_status.get(name):
+                            status = True
+                        else:
+                            status = False
+                        description = port_descr[name]
+                        if not description:
+                            description = ''
+                        members = []
+                        write = True
+                    if write:
+                        if name not in port_vlans:
+                            tagged = []
+                        else:
+                            tagged = port_vlans[name]["tagged"]
+                        swp = {
+                                "status": status,
+                                "description": description,
+                                "802.1Q Enabled": len(port_vlans.get(name, '')) > 0,
+                                "802.1ad Tunnel": vlan_stack_status.get(name, False),
+                                "tagged": tagged,
+                                }
+                        if name in port_vlans:
+                            if port_vlans[name]["untagged"]:
+                                swp["untagged"] = port_vlans[name]["untagged"]
+                        swp["interface"] = name
+                        swp["members"] = members
+                        r.append(swp)
+                        write = False
+                return r
+            except self.snmp.TimeOutError:
+                pass
+
+        # Fallback to CLI
         # Make a list of tags for each interface or portchannel
         port_vlans = {}
         port_channels = portchannels
