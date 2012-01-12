1f19ed65d92aef16107d49bdad2b58ec2a4dbde4
Dmitry Volodin <dv@nocproject.org>
2012-01-12 17:57:38 +0400

    NOC-293: Stub gis.area implementation

---
+++ gis/apps/area/tests/test.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## gis.area unittes
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2012 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+from noc.lib.test import RestModelTestCase
+
+
+class AreaTestCase(RestModelTestCase):
+    app = "gis.area"
+
+    scenario = [
+        {
+            "GET": {
+                # key: value
+            },
+            "POST": {
+                # key: value
+            },
+            "PUT": {
+                # key: value
+            }
+        }
+    ]
