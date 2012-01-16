d874d3efa848b0b4b07274fd29dbf00bb3246e4e
Dmitry Volodin <dv@nocproject.org>
2012-01-16 14:54:26 +0400

    PEP8 formatting

--- sa/interfaces/__init__.py
+++ sa/interfaces/__init__.py
@@ -1,17 +1,25 @@
 # -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2009 The NOC Project
+## NOC interfaces
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-"""
-"""
+
+## Python modules
+import os
+import sys
+import inspect
+## NOC modules
 from noc.sa.interfaces.base import *
-import os,inspect,sys
+
 # Interface autoloader
-im=sys.modules["noc.sa.interfaces"]
-for f in [f for f in os.listdir(__path__[0]) if not f.startswith(".") and f.endswith(".py") and f not in ["__init__.py","base.py"]]:
-    m=__import__("noc.sa.interfaces."+f[:-3],{},{},"*")
+im = sys.modules["noc.sa.interfaces"]
+for f in [f for f in os.listdir(__path__[0]) if
+          not f.startswith(".") and f.endswith(".py") and f not in (
+              "__init__.py", "base.py")]:
+    m = __import__("noc.sa.interfaces.%s" % f[:-3], {}, {}, "*")
     for cn in dir(m):
-        c=getattr(m,cn)
-        if inspect.isclass(c) and issubclass(c,Interface) and c!=Interface:
-            setattr(im,c.__name__,c)
+        c = getattr(m, cn)
+        if inspect.isclass(c) and issubclass(c, Interface) and c != Interface:
+            setattr(im, c.__name__, c)
