b104640d40531184200855bbb93cd1d8e6ac2733
Alexey Shapovalov <a.shapovalov@omsk.multinex.ru>
2012-01-19 17:51:31 +0700

    NOC-297 mac parameter, pep8, beef

--- sa/profiles/Zyxel/ZyNOS/get_mac_address_table.py
+++ sa/profiles/Zyxel/ZyNOS/get_mac_address_table.py
@@ -2,37 +2,45 @@
 ##----------------------------------------------------------------------
 ## Zyxel.ZyNOS.get_mac_address_table
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2010 The NOC Project
+## Copyright (C) 2007-2012 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-""" 
-""" 
-import noc.sa.script
-from noc.sa.interfaces import IGetMACAddressTable
+"""
+"""
+## Python modules
 import re
+## NOC modules
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetMACAddressTable
 
-rx_line=re.compile(r"^(?P<interfaces>\d+)\s+(?P<vlan_id>\d+)\s+(?P<mac>\S+)\s+(?P<type>\S+)$")
 
-class Script(noc.sa.script.Script):
-    name="Zyxel.ZyNOS.get_mac_address_table" 
-    implements=[IGetMACAddressTable]
-    def execute(self,interface=None,vlan=None,mac=None):
-        cmd="show mac address-table" 
+class Script(NOCScript):
+    name = "Zyxel.ZyNOS.get_mac_address_table"
+    implements = [IGetMACAddressTable]
+
+    rx_line = re.compile(r"^(?P<interfaces>\d+)\s+(?P<vlan_id>\d+)\s+" \
+                          "(?P<mac>\S+)\s+(?P<type>\S+)\s*$", re.MULTILINE)
+
+    def execute(self, interface=None, vlan=None, mac=None):
+        cmd = "show mac address-table"
         if interface is not None:
-            cmd+=" port %s"%interface
+            cmd += " port %s" % interface
         elif vlan is not None:
-            cmd+=" vlan %s"%vlan
+            cmd += " vlan %s" % vlan
         else:
-            cmd+=" all"
-        macs=self.cli(cmd)
-        r=[]
-        for l in macs.split("\n"):
-            match=rx_line.match(l.strip())
-            if match:
-                r.append({
-                    "vlan_id"   : match.group("vlan_id"),
-                    "mac"       : match.group("mac"),
-                    "interfaces": [match.group("interfaces")],
-                    "type"      : {"Dynamic":"D","Static":"S"}[match.group("type")],
-                })
+            cmd += " all"
+        macs = self.cli(cmd)
+        r = []
+        for match in self.rx_line.finditer(macs):
+            mac_address = match.group("mac")
+            if mac is not None and mac.lower() != mac_address:
+                continue
+            r.append({
+                "vlan_id": match.group("vlan_id"),
+                "mac": match.group("mac"),
+                "interfaces": [match.group("interfaces")],
+                "type": {"Dynamic": "D",
+                         "Static": "S"}[match.group("type")],
+            })
+
         return r
