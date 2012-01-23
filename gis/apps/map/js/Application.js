b59f685d5a82f7f9baefc14ab9985161779a5965
Dmitry Volodin <dv@nocproject.org>
2012-01-23 22:57:26 +0400

    Fix Google Maps

--- gis/apps/map/js/Application.js
+++ gis/apps/map/js/Application.js
@@ -9,14 +9,18 @@ console.debug("Defining NOC.gis.map.Application");
 Ext.define("NOC.gis.map.Application", {
     extend: "NOC.core.Application",
     layout: "fit",
-    requires: ["NOC.gis.area.LookupField"],
+    requires: [
+        "NOC.gis.area.LookupField",
+        "NOC.gis.overlay.LookupField"
+    ],
 
     initComponent: function() {
         // Create layer store
         this.layer_store = Ext.create("Ext.data.Store", {
             fields: ["layer"],
             data: []
-        })
+        });
+        this.overlays = {};  // id -> OpenLayers.Layer.Vector instance
         // Create times
         Ext.apply(this, {
             dockedItems: [{
@@ -26,6 +30,7 @@ Ext.define("NOC.gis.map.Application", {
                     {
                         xtype: "gis.area.LookupField",
                         fieldLabel: "Area",
+                        labelWidth: 30,
                         name: "area",
                         allowBlank: true,
                         listeners: {
@@ -37,6 +42,7 @@ Ext.define("NOC.gis.map.Application", {
                     },
                     Ext.create("Ext.form.ComboBox", {
                         fieldLabel: "Base Layer",
+                        labelWidth: 60,
                         store: this.layer_store,
                         queryMode: "local",
                         displayField: "layer",
@@ -50,6 +56,36 @@ Ext.define("NOC.gis.map.Application", {
                             }
                         }
                     }),
+                    {
+                        xtype: "gis.overlay.LookupField",
+                        fieldLabel: "Overlays",
+                        labelWidth: 50,
+                        name: "overlays",
+                        allowBlank: true,
+                        multiSelect: true,
+                        listeners: {
+                            change: function(combo, new_value, old_value, opts) {
+                                console.log(new_value, old_value);
+                                var a = this.up().up();
+                                // Disable deselected overlays
+                                var i;
+                                if(old_value) {
+                                    for(i = 0; i < old_value.length; i++) {
+                                        var overlay_id = old_value[i];
+                                        if(new_value.indexOf(overlay_id) < 0)
+                                            a.deactivate_overlay(overlay_id);
+                                    }
+                                }
+                                // Enable selected overlays
+                                for(i = 0; i < new_value.length; i++) {
+                                    var overlay_id = new_value[i];
+                                    if((old_value == undefined)
+                                        || (old_value.indexOf(overlay_id) < 0))
+                                        a.activate_overlay(overlay_id);
+                                }
+                            }
+                        }
+                    },
                     "->",
                     {
                         xtype: "checkboxfield",
@@ -107,6 +143,8 @@ Ext.define("NOC.gis.map.Application", {
                     if(!init_GoogleAPI) {
                         init_GoogleAPI = true;
                         urls.push("http://maps.google.com/maps/api/js?sensor=false");
+                        // Dirty hack to ensure Google Maps API is loaded
+                        urls.push("http://maps.gstatic.com/intl/en_us/mapfiles/api-3/7/8a/main.js");
                     }
                     break;
             }
@@ -122,11 +160,13 @@ Ext.define("NOC.gis.map.Application", {
             Ext.Msg.alert("Error", "Incomplete GIS setup");
             return;
         }
+        this.proj_geo = new OpenLayers.Projection("EPSG:4326");
+        this.proj_mercator = new OpenLayers.Projection("EPSG:900913");
         var map_div = "ol-map-" + this.id;
         // Create OpenLayers map
         this.ol_map = new OpenLayers.Map(map_div, {
-            projection: "EPSG:900913",
-            displayProjection: "EPSG:900913",
+            projection: this.proj_mercator,
+            displayProjection: this.proj_mercator,
             controls: []
         });
         //
@@ -173,7 +213,9 @@ Ext.define("NOC.gis.map.Application", {
             new OpenLayers.Control.PanZoomBar(),
             new OpenLayers.Control.KeyboardDefaults(),
             new OpenLayers.Control.ScaleLine({geodesic: true}),
-            // new OpenLayers.Control.MousePosition({}),
+            new OpenLayers.Control.MousePosition({
+                displayProjection: this.proj_geo
+            }),
             new OpenLayers.Control.Graticule({visible: false})
         ]);
         // Center map
@@ -205,14 +247,51 @@ Ext.define("NOC.gis.map.Application", {
                     this.ol_map.zoomToMaxExtent();
                     return;
                 }
-                var proj = new OpenLayers.Projection("EPSG:4326");
                 var bounds = new OpenLayers.Bounds();
                 bounds.extend(new OpenLayers.LonLat(area.SW[0], area.SW[1]));
                 bounds.extend(new OpenLayers.LonLat(area.NE[0], area.NE[1]));
-                bounds.transform(proj, this.ol_map.getProjectionObject());
+                bounds.transform(this.proj_geo, this.ol_map.getProjectionObject());
                 console.log(bounds);
                 this.ol_map.zoomToExtent(bounds, true);
             }
         });
+    },
+    //
+    // Activate overlay
+    //
+    activate_overlay: function(overlay_id) {
+        var layer = this.overlays[overlay_id];
+        if(layer) {
+            layer.setVisibility(true);
+        } else {
+            // Get overlay info
+            Ext.Ajax.request({
+                method: "GET",
+                url: "/gis/overlay/" + overlay_id + "/",
+                scope: this,
+                success: function(response) {
+                    // Create overlay
+                    var info = Ext.decode(response.responseText);
+                    var layer = new OpenLayers.Layer.Vector(info.name, {
+                            protocol: new OpenLayers.Protocol.HTTP({
+                                url: "/gis/overlay/gate/" + info.gate_id + "/",
+                                format: new OpenLayers.Format.GeoJSON({
+                                    externalProjection: this.proj_geo,
+                                    internalProjection: this.proj_mercator
+                                })
+                            }),
+                            strategies: [new OpenLayers.Strategy.BBOX()]
+                        });
+                        this.overlays[overlay_id] = layer;
+                        this.ol_map.addLayer(layer);
+                }
+            });
+        }
+    },
+    //
+    // Deactivate overlay
+    //
+    deactivate_overlay: function(overlay_id) {
+        this.overlays[overlay_id].setVisibility(false);
     }
 });
