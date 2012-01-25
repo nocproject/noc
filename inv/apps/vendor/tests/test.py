a34a59345af5eb90abd738f3ef004dd6ecf662db
Dmitry Volodin <dv@nocproject.org>
2012-01-25 17:19:21 +0400

    NOC-322: noc.vendor application

---
+++ inv/apps/vendor/tests/test.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## inv.vendor unittes
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2012 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+from noc.lib.test import RestModelTestCase
+
+
+class VendorTestCase(RestModelTestCase):
+    app = "inv.vendor"
+
+    scenario = [
+        {
+            "GET": {
+                # key: value
+            },
+            "POST": {
+                # key: value
+            },
+            "PUT": {
+                # key: value
+            }
+        }
+    ]
---
+++ inv/apps/vendor/views.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## inv.vendor application
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2012 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+from noc.lib.app import ExtDocApplication, view
+from noc.inv.models import Vendor
+
+
+class VendorApplication(ExtDocApplication):
+    """
+    Vendor application
+    """
+    title = "Vendor"
+    menu = "Setup | Vendors"
+    model = Vendor
