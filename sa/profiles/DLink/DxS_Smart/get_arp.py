23dff85a8450c282a9152d3a3be5aa96c07ff0a5
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2012-01-26 13:33:10 +0200

    PEP8 Formatting

--- sa/profiles/DLink/DxS_Smart/get_arp.py
+++ sa/profiles/DLink/DxS_Smart/get_arp.py
@@ -5,20 +5,22 @@
 ## Copyright (C) 2007-2011 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-""" 
-""" 
+"""
+"""
 from noc.sa.script import Script as NOCScript
 from noc.sa.interfaces import IGetARP
 import re
 
+
 class Script(NOCScript):
-    name="DLink.DxS_Smart.get_arp" 
-    implements=[IGetARP]
-    rx_line=re.compile(r"(?P<ip>[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\s+(?P<mac>\S+)\s+ARPA\s+(?P<interface>\S+)\s+\S+", re.MULTILINE)
+    name = "DLink.DxS_Smart.get_arp"
+    implements = [IGetARP]
+    rx_line = re.compile(r"(?P<ip>[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\s+(?P<mac>\S+)\s+ARPA\s+(?P<interface>\S+)\s+\S+", re.MULTILINE)
+
     def execute(self):
-        r=[]
+        r = []
         for match in self.rx_line.finditer(self.cli("debug info")):
-            r+=[{
+            r += [{
                 "ip":match.group("ip"),
                 "mac":match.group("mac"),
                 "interface":match.group("interface"),
--- sa/profiles/DLink/DxS_Smart/get_mac_address_table.py
+++ sa/profiles/DLink/DxS_Smart/get_mac_address_table.py
@@ -11,13 +11,15 @@ from noc.sa.script import Script as NOCScript
 from noc.sa.interfaces import IGetMACAddressTable
 import re
 
+
 class Script(NOCScript):
-    name="DLink.DxS_Smart.get_mac_address_table"
-    implements=[IGetMACAddressTable]
-    rx_line=re.compile(r"(?P<vlan_id>\d+)\s+(?P<mac>\S+)\s+(?P<type>Learnt|Static)\s+(?P<interfaces>\S+)",re.MULTILINE)
-    def execute(self,interface=None,vlan=None,mac=None):
-        cmd="debug info"
-        r=[]
+    name = "DLink.DxS_Smart.get_mac_address_table"
+    implements = [IGetMACAddressTable]
+    rx_line = re.compile(r"(?P<vlan_id>\d+)\s+(?P<mac>\S+)\s+(?P<type>Learnt|Static)\s+(?P<interfaces>\S+)", re.MULTILINE)
+
+    def execute(self, interface=None, vlan=None, mac=None):
+        cmd = "debug info"
+        r = []
         for match in self.rx_line.finditer(self.cli(cmd)):
             m_interface = match.group("interfaces")
             m_vlan = match.group("vlan_id")
@@ -26,10 +28,10 @@ class Script(NOCScript):
             or (interface is not None and interface == m_interface) \
             or (vlan is not None and vlan == m_vlan) \
             or (mac is not None and mac == m_mac)):
-                r+=[{
-                    "vlan_id"   : m_vlan,
-                    "mac"       : m_mac,
+                r += [{
+                    "vlan_id": m_vlan,
+                    "mac": m_mac,
                     "interfaces": [m_interface],
-                    "type"      : {"Learnt":"D","Static":"S",}[match.group("type")],
+                    "type": {"Learnt":"D", "Static":"S"}[match.group("type")]
                 }]
         return r
