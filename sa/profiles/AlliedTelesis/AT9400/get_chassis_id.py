3ca1e8da4f58750c58547a8c3067f3efa1ad6ea5
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2012-01-26 11:37:29 +0200

    PEP8 Formatting

--- sa/profiles/AlliedTelesis/AT9400/get_chassis_id.py
+++ sa/profiles/AlliedTelesis/AT9400/get_chassis_id.py
@@ -11,12 +11,13 @@ from noc.sa.script import Script as NOCScript
 from noc.sa.interfaces import IGetChassisID
 import re
 
-class Script(NOCScript):
-    name="AlliedTelesis.AT9400.get_chassis_id"
-    cache=True
-    rx_ver=re.compile(r"^MAC Address \.+ (?P<id>\S+)",re.IGNORECASE|re.MULTILINE)
-    implements=[IGetChassisID]
-    def execute(self):
-        match=self.re_search(self.rx_ver, self.cli("show system", cached=True))
-        return match.group("id")
 
+class Script(NOCScript):
+    name = "AlliedTelesis.AT9400.get_chassis_id"
+    cache = True
+    rx_ver = re.compile(r"^MAC Address \.+ (?P<id>\S+)", re.IGNORECASE | re.MULTILINE)
+    implements = [IGetChassisID]
+
+    def execute(self):
+        match = self.re_search(self.rx_ver, self.cli("show system", cached=True))
+        return match.group("id")
