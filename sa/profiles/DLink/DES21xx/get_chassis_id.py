49796c74a2d8d6e14b2a07ddde414f0d0346e215
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2012-01-26 13:42:53 +0200

    PEP8 Formatting

--- sa/profiles/DLink/DES21xx/get_chassis_id.py
+++ sa/profiles/DLink/DES21xx/get_chassis_id.py
@@ -11,11 +11,13 @@ from noc.sa.script import Script as NOCScript
 from noc.sa.interfaces import IGetChassisID
 import re
 
+
 class Script(NOCScript):
-    name="DLink.DES21xx.get_chassis_id"
-    cache=True
-    rx_ver=re.compile(r"^MAC address:(?P<id>\S+)",re.IGNORECASE|re.MULTILINE)
-    implements=[IGetChassisID]
+    name = "DLink.DES21xx.get_chassis_id"
+    cache = True
+    rx_ver = re.compile(r"^MAC address:(?P<id>\S+)", re.IGNORECASE | re.MULTILINE)
+    implements = [IGetChassisID]
+
     def execute(self):
-        match=self.re_search(self.rx_ver, self.cli("show switch"))
+        match = self.re_search(self.rx_ver, self.cli("show switch"))
         return match.group("id")
--- sa/profiles/DLink/DES21xx/get_mac_address_table.py
+++ sa/profiles/DLink/DES21xx/get_mac_address_table.py
@@ -2,58 +2,60 @@
 ##----------------------------------------------------------------------
 ## DLink.DES21xx.get_mac_address_table
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2010 The NOC Project
+## Copyright (C) 2007-2011 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-""" 
-""" 
+"""
+"""
 import noc.sa.script
 from noc.sa.interfaces import IGetMACAddressTable
 from noc.sa.interfaces.base import MACAddressParameter
 import re
 
+
 class Script(noc.sa.script.Script):
-    name="DLink.DES21xx.get_mac_address_table"
-    implements=[IGetMACAddressTable]
-    rx_line=re.compile(r"^\d+\s+(?P<interfaces>\S+)\s+(?P<vlan_id>\d+)\s+(?P<mac>\S+)$")
-    def execute(self,interface=None,vlan=None,mac=None):
-        cmd="show fdb port "
+    name = "DLink.DES21xx.get_mac_address_table"
+    implements = [IGetMACAddressTable]
+    rx_line = re.compile(r"^\d+\s+(?P<interfaces>\S+)\s+(?P<vlan_id>\d+)\s+(?P<mac>\S+)$")
+
+    def execute(self, interface=None, vlan=None, mac=None):
+        cmd = "show fdb port "
         if interface is not None:
-            cmd+="%s"%interface
-            macs=self.cli(cmd)
+            cmd += "%s" % interface
+            macs = self.cli(cmd)
         else:
             macs = ""
             for s in self.scripts.get_interface_status():
                 macs += self.cli(cmd + "%s" % s["interface"])
-        r=[]
+        r = []
         for l in macs.split("\n"):
-            match=self.rx_line.match(l.strip())
+            match = self.rx_line.match(l.strip())
             if match:
                 r.append({
-                    "vlan_id"   : match.group("vlan_id"),
-                    "mac"       : match.group("mac"),
+                    "vlan_id": match.group("vlan_id"),
+                    "mac": match.group("mac"),
                     "interfaces": [match.group("interfaces")],
-                    "type"      : "D"
+                    "type": "D"
                 })
         # Static MAC address table
-        macs=self.cli("show smac")
+        macs = self.cli("show smac")
         for l in macs.split("\n"):
-            match=self.rx_line.match(l.strip())
+            match = self.rx_line.match(l.strip())
             if match:
                 iface = match.group("interfaces")
                 if interface is not None:
                     if interface == iface:
                         r.append({
-                            "vlan_id"   : match.group("vlan_id"),
-                            "mac"       : match.group("mac"),
+                            "vlan_id": match.group("vlan_id"),
+                            "mac": match.group("mac"),
                             "interfaces": [iface],
-                            "type"      : "S"
+                            "type": "S"
                         })
                 else:
                     r.append({
-                        "vlan_id"   : match.group("vlan_id"),
-                        "mac"       : match.group("mac"),
+                        "vlan_id": match.group("vlan_id"),
+                        "mac": match.group("mac"),
                         "interfaces": [iface],
-                        "type"      : "S"
+                        "type": "S"
                     })
         return r
