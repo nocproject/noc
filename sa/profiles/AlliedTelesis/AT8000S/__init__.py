70efec908c6130f576663c45043f6af5b9476174
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2012-01-27 13:28:30 +0200

    PEP8 Formatting

--- sa/profiles/AlliedTelesis/AT8000S/__init__.py
+++ sa/profiles/AlliedTelesis/AT8000S/__init__.py
@@ -10,21 +10,22 @@
 """
 """
 import noc.sa.profiles
-from noc.sa.protocols.sae_pb2 import TELNET,SSH
+from noc.sa.protocols.sae_pb2 import TELNET, SSH
+
 
 class Profile(noc.sa.profiles.Profile):
-    name="AlliedTelesis.AT8000S"
-    supported_schemes=[TELNET,SSH]
-    pattern_username="User Name:"
-    pattern_unpriveleged_prompt=r"^\S+?>"
-    pattern_prompt=r"^\S+?#"
-    pattern_more=[
-        (r"^More: <space>,  Quit: q, One line: <return>"," "),
-        (r"^.*?\[Yes/press any key for no\]\.*","Y"),
+    name = "AlliedTelesis.AT8000S"
+    supported_schemes = [TELNET, SSH]
+    pattern_username = "User Name:"
+    pattern_unpriveleged_prompt = r"^\S+?>"
+    pattern_prompt = r"^\S+?#"
+    pattern_more = [
+        (r"^More: <space>,  Quit: q, One line: <return>", " "),
+        (r"^.*?\[Yes/press any key for no\]\.*", "Y"),
         ]
-    command_super="enable"
-    command_enter_config="configure"
-    command_leave_config="end"
-    command_save_config="copy running-config startup-config"
-    command_disable_pager="terminal datadump"
-    config_volatile=[r"^\s*(?P<day>3[01]|[0-2]{0,1}\d)-(?P<month>Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)-(?P<year>\d{4}) (?P<time>\d\d:\d\d:\d\d) %\W*"]
+    command_super = "enable"
+    command_enter_config = "configure"
+    command_leave_config = "end"
+    command_save_config = "copy running-config startup-config"
+    command_disable_pager = "terminal datadump"
+    config_volatile = [r"^\s*(?P<day>3[01]|[0-2]{0,1}\d)-(?P<month>Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)-(?P<year>\d{4}) (?P<time>\d\d:\d\d:\d\d) %\W*"]
