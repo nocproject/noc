17a51a4161abc0480363116d0f6941a521681bf5
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2012-01-27 14:33:31 +0200

    Fix previous commit PEP8 Formatting

--- sa/profiles/AlliedTelesis/AT8500/get_config.py
+++ sa/profiles/AlliedTelesis/AT8500/get_config.py
@@ -2,20 +2,21 @@
 ##----------------------------------------------------------------------
 ## AlliedTelesis.AT8500.get_config
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2010 The NOC Project
+## Copyright (C) 2007-2012 The NOC Project
 ## coded by azhur
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 """
 """
-#from __future__ import with_statement
-import noc.sa.script
+from noc.sa.script import Script as NOCScript
 from noc.sa.interfaces import IGetConfig
 
-class Script(noc.sa.script.Script):
-    name="AlliedTelesis.AT8500.get_config"
-    implements=[IGetConfig]
+
+class Script(NOCScript):
+    name = "AlliedTelesis.AT8500.get_config"
+    implements = [IGetConfig]
+
     def execute(self):
         #self.cli("terminal datadump")
-        config=self.cli("show config dynamic")
+        config = self.cli("show config dynamic")
         return self.cleaned_config(config)
