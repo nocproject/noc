7caa34cf53100d9474d4fdbfe724f9861c96bf64
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2012-01-27 14:21:33 +0200

    PEP8 Formatting

--- sa/profiles/AlliedTelesis/AT9400/get_arp.py
+++ sa/profiles/AlliedTelesis/AT9400/get_arp.py
@@ -5,22 +5,24 @@
 ## Copyright (C) 2007-2011 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-""" 
-""" 
+"""
+"""
 from noc.sa.script import Script as NOCScript
 from noc.sa.interfaces import IGetARP
 import re
 
+
 class Script(NOCScript):
-    name="AlliedTelesis.AT9400.get_arp" 
-    implements=[IGetARP]
-    rx_line=re.compile(r"^(?P<interface>\S+)\s+(?P<ip>[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\s+(?P<mac>\S+)\s+\S+\s*$", re.MULTILINE)
+    name = "AlliedTelesis.AT9400.get_arp"
+    implements = [IGetARP]
+    rx_line = re.compile(r"^(?P<interface>\S+)\s+(?P<ip>[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\s+(?P<mac>\S+)\s+\S+\s*$", re.MULTILINE)
+
     def execute(self):
-        r=[]
+        r = []
         for match in self.rx_line.finditer(self.cli("show ip arp")):
-            r+=[{
-                "ip":match.group("ip"),
-                "mac":match.group("mac"),
-                "interface":match.group("interface"),
+            r + =[{
+                "ip": match.group("ip"),
+                "mac": match.group("mac"),
+                "interface": match.group("interface"),
             }]
         return r
