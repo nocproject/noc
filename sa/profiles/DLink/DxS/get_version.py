9b1d3c809964ed8061a92afb2d8bbafaf7e36a80
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2012-01-31 17:18:16 +0200

    PEP8 Formatting

--- sa/profiles/DLink/DxS/get_version.py
+++ sa/profiles/DLink/DxS/get_version.py
@@ -17,8 +17,10 @@ class Script(noc.sa.script.Script):
     cache = True
     implements = [IGetVersion]
     rx_ver = re.compile(r"Device Type\s+:\s+(?P<platform>\S+).+Boot PROM Version\s+:\s+(?:Build\s+)?(?P<bootprom>\S+).+Firmware Version\s+:\s+(?:Build\s+)?(?P<version>\S+).+Hardware Version\s+:\s+(?P<hardware>\S+)", re.MULTILINE | re.DOTALL)
-    rx_fwt = re.compile(r"Firmware Type\s+:\s+(?P<fwt>\S+)\s*\n", re.MULTILINE | re.DOTALL)
-    rx_ser = re.compile(r"Serial Number\s+:\s+(?P<serial>\S+)\s*\n", re.MULTILINE | re.DOTALL)
+    rx_fwt = re.compile(r"Firmware Type\s+:\s+(?P<fwt>\S+)\s*\n",
+        re.MULTILINE | re.DOTALL)
+    rx_ser = re.compile(r"Serial Number\s+:\s+(?P<serial>\S+)\s*\n",
+        re.MULTILINE | re.DOTALL)
 
     def execute(self):
         s = self.cli("show switch", cached=True)
