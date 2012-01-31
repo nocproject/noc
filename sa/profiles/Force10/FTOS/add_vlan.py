3b21cb66f45515853f3453682d19f0db13887e4a
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2012-01-31 17:45:33 +0200

    PEP8 Formatting

--- sa/profiles/Force10/FTOS/add_vlan.py
+++ sa/profiles/Force10/FTOS/add_vlan.py
@@ -11,15 +11,17 @@ from __future__ import with_statement
 import noc.sa.script
 from noc.sa.interfaces import IAddVlan
 
+
 class Script(noc.sa.script.Script):
-    name="Force10.FTOS.add_vlan"
-    implements=[IAddVlan]
-    def execute(self,vlan_id,name,tagged_ports):
+    name = "Force10.FTOS.add_vlan"
+    implements = [IAddVlan]
+
+    def execute(self, vlan_id, name, tagged_ports):
         with self.configure():
-            self.cli("interface vlan %d"%vlan_id)
-            self.cli("name %s"%name)
+            self.cli("interface vlan %d" % vlan_id)
+            self.cli("name %s" % name)
             for tp in tagged_ports:
-                self.cli("tagged %s"%tp)
+                self.cli("tagged %s" % tp)
             self.cli("no shutdown")
             self.cli("exit")
         self.save_config()
--- sa/profiles/Force10/FTOS/get_interface_status.py
+++ sa/profiles/Force10/FTOS/get_interface_status.py
@@ -11,33 +11,38 @@ import noc.sa.script
 from noc.sa.interfaces import IGetInterfaceStatus
 import re
 
-rx_interface_status=re.compile(r"^(?P<interface>\S+\s+\S+)\s+is\s+\S+,\s+line\s+protocol\s+is\s+(?P<status>up|down).*$",re.IGNORECASE)
+rx_interface_status = re.compile(r"^(?P<interface>\S+\s+\S+)\s+is\s+\S+,\s+line\s+protocol\s+is\s+(?P<status>up|down).*$", re.IGNORECASE)
+
 
 class Script(noc.sa.script.Script):
-    name="Force10.FTOS.get_interface_status"
-    implements=[IGetInterfaceStatus]
-    def execute(self,interface=None):
+    name = "Force10.FTOS.get_interface_status"
+    implements = [IGetInterfaceStatus]
+
+    def execute(self, interface=None):
         if self.snmp and self.access_profile.snmp_ro:
             try:
                 # Get interface status
-                r=[]
-                for n,s in self.snmp.join_tables("1.3.6.1.2.1.31.1.1.1.1","1.3.6.1.2.1.2.2.1.8",bulk=True): # IF-MIB::ifName, IF-MIB::ifOperStatus
-                    r+=[{"interface":n,"status":int(s)==1}] # ifOperStatus up(1)
+                r = []
+                # IF-MIB::ifName, IF-MIB::ifOperStatus
+                for n, s in self.snmp.join_tables("1.3.6.1.2.1.31.1.1.1.1",
+                    "1.3.6.1.2.1.2.2.1.8", bulk=True):
+                    # ifOperStatus up(1)
+                    r += [{"interface": n, "status": int(s) == 1}]
                 return r
             except self.snmp.TimeOutError:
                 pass
         # Fallback to CLI
-        r=[]
+        r = []
         if interface:
-            cmd="show interface %s | grep \"line protocol is\""%interface
+            cmd = "show interface %s | grep \"line protocol is\"" % interface
         else:
-            cmd="show interface | grep \"line protocol is\""
-        
+            cmd = "show interface | grep \"line protocol is\""
+
         for l in self.cli(cmd).splitlines():
-            match=rx_interface_status.match(l)
+            match = rx_interface_status.match(l)
             if match:
-                r+=[{
-                    "interface" : match.group("interface"),
-                    "status"    : match.group("status")=="up"
-                    }]
+                r += [{
+                    "interface": match.group("interface"),
+                    "status": match.group("status") == "up"
+                }]
         return r
