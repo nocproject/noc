60fc8c27bced0420f45b21cfdc30b29896c79281
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2012-02-03 12:30:08 +0200

    PE8 Formatting

--- sa/profiles/Force10/FTOS/get_mac_address_table.py
+++ sa/profiles/Force10/FTOS/get_mac_address_table.py
@@ -11,28 +11,30 @@ import noc.sa.script
 from noc.sa.interfaces import IGetMACAddressTable
 import re
 
-rx_line=re.compile(r"^\s*(?P<vlan_id>\d+)\s+(?P<mac>\S+)\s+(?P<type>\S+)\s+(?P<interfaces>\S+\s+\S+).*$")
+rx_line = re.compile(r"^\s*(?P<vlan_id>\d+)\s+(?P<mac>\S+)\s+(?P<type>\S+)\s+(?P<interfaces>\S+\s+\S+).*$")
+
 
 class Script(noc.sa.script.Script):
-    name="Force10.FTOS.get_mac_address_table"
-    implements=[IGetMACAddressTable]
-    def execute(self,interface=None,vlan=None,mac=None):
-        cmd="show mac-address-table"
+    name = "Force10.FTOS.get_mac_address_table"
+    implements = [IGetMACAddressTable]
+
+    def execute(self, interface=None, vlan=None, mac=None):
+        cmd = "show mac-address-table"
         if mac is not None:
-            cmd+=" address %s"%self.profile.convert_mac(mac)
+            cmd += " address %s" % self.profile.convert_mac(mac)
         if interface is not None:
-            cmd+=" interface %s"%interface
+            cmd += " interface %s" % interface
         if vlan is not None:
-            cmd+=" vlan %s"%vlan
-        vlans=self.cli(cmd)
-        r=[]
+            cmd += " vlan %s" % vlan
+        vlans = self.cli(cmd)
+        r = []
         for l in vlans.split("\n"):
-            match=rx_line.match(l.strip())
+            match = rx_line.match(l.strip())
             if match:
                 r.append({
-                    "vlan_id"   : match.group("vlan_id"),
-                    "mac"       : match.group("mac"),
+                    "vlan_id": match.group("vlan_id"),
+                    "mac": match.group("mac"),
                     "interfaces": [match.group("interfaces")],
-                    "type"      : {"Dynamic":"D"}[match.group("type")],
+                    "type": {"Dynamic": "D"}[match.group("type")],
                 })
         return r
--- sa/profiles/Force10/FTOS/get_spanning_tree.py
+++ sa/profiles/Force10/FTOS/get_spanning_tree.py
@@ -12,80 +12,88 @@ from noc.sa.interfaces import IGetSpanningTree
 from noc.lib.text import parse_table
 import re
 
+
 class Script(noc.sa.script.Script):
-    name="Force10.FTOS.get_spanning_tree"
-    implements=[IGetSpanningTree]
+    name = "Force10.FTOS.get_spanning_tree"
+    implements = [IGetSpanningTree]
     ##
     ## MSTP Mode parsing
     ##
-    rx_mstp_instance_list=re.compile(r"^\s*(\d+)",re.MULTILINE)
-    rx_mstp_region=re.compile(r"MST Region Name:\s+(?P<region>\S+).*Revision:\s+(?P<revision>\d+)",re.MULTILINE|re.DOTALL|re.IGNORECASE)
-    rx_mstp_vlans=re.compile(r"^MSTI \d VLANs mapped\s+(?P<vlans>.+?)$",re.MULTILINE)
-    rx_mstp_root=re.compile(r"^Root\s+ID\s+Priority\s+(?P<root_priority>\d+),\s+Address\s+(?P<root_id>\S+)",re.MULTILINE)
-    rx_mstp_bridge=re.compile(r"^Bridge\s+ID\s+Priority\s+(?P<bridge_priority>\d+),\s+Address\s+(?P<bridge_id>\S+)",re.MULTILINE)
+    rx_mstp_instance_list = re.compile(r"^\s*(\d+)", re.MULTILINE)
+    rx_mstp_region = re.compile(r"MST Region Name:\s+(?P<region>\S+).*Revision:\s+(?P<revision>\d+)", re.MULTILINE | re.DOTALL | re.IGNORECASE)
+    rx_mstp_vlans = re.compile(
+        r"^MSTI \d VLANs mapped\s+(?P<vlans>.+?)$", re.MULTILINE)
+    rx_mstp_root = re.compile(r"^Root\s+ID\s+Priority\s+(?P<root_priority>\d+),\s+Address\s+(?P<root_id>\S+)", re.MULTILINE)
+    rx_mstp_bridge = re.compile(r"^Bridge\s+ID\s+Priority\s+(?P<bridge_priority>\d+),\s+Address\s+(?P<bridge_id>\S+)", re.MULTILINE)
+
     def process_mstp(self):
         # Get Instances List
-        v=self.cli("show spanning-tree mst configuration")
-        match=self.rx_mstp_region.search(v)
-        r={
-            "mode"      : "MSTP",
-            "instances" : [],
-            "configuration" : {
-                "MSTP" : {
-                    "region"   : match.group("region"),
-                    "revision" : match.group("revision"),
+        v = self.cli("show spanning-tree mst configuration")
+        match = self.rx_mstp_region.search(v)
+        r = {
+            "mode": "MSTP",
+            "instances": [],
+            "configuration": {
+                "MSTP": {
+                    "region": match.group("region"),
+                    "revision": match.group("revision"),
                 }
             }
-        }        
-        for instance_id in ["0"]+self.rx_mstp_instance_list.findall(v):
+        }
+        for instance_id in ["0"] + self.rx_mstp_instance_list.findall(v):
             # Get instance data
-            ri={"id" : int(instance_id), "interfaces": []}
-            v=self.cli("show spanning-tree msti %s brief"%instance_id)
+            ri = {"id": int(instance_id), "interfaces": []}
+            v = self.cli("show spanning-tree msti %s brief" % instance_id)
             # Get VLAN mapping
-            match=self.rx_mstp_vlans.search(v)
-            ri["vlans"]=match.group(1)
+            match = self.rx_mstp_vlans.search(v)
+            ri["vlans"] = match.group(1)
             # Get Root ID and priority
-            match=self.rx_mstp_root.search(v)
-            ri["root_id"]=match.group("root_id")
-            ri["root_priority"]=match.group("root_priority")
+            match = self.rx_mstp_root.search(v)
+            ri["root_id"] = match.group("root_id")
+            ri["root_priority"] = match.group("root_priority")
             # Get Bridge ID and priority
-            match=self.rx_mstp_bridge.search(v)
-            ri["bridge_id"]=match.group("bridge_id")
-            ri["bridge_priority"]=match.group("bridge_priority")
+            match = self.rx_mstp_bridge.search(v)
+            ri["bridge_id"] = match.group("bridge_id")
+            ri["bridge_priority"] = match.group("bridge_priority")
             # Process interfaces
-            s1,s2,s3=v.split("\n\nInterface")
-            for interface,port_id,priority,_,state,_,desg_bridge,desg_port_id in parse_table(s2):
-                desg_bridge_priority,desg_bridge_id=desg_bridge.split()
-                i={
-                    "interface" : interface,
-                    "port_id"   : port_id,
-                    "state"     : {"dis":"disabled","blk":"discarding","??":"learning","fwd":"forwarding"}[state.lower()], #@todo: refine states
-                    "priority"  : priority,
-                    "designated_bridge_id"       : desg_bridge_id,
-                    "designated_bridge_priority" : desg_bridge_priority,
-                    "designated_port_id"         : desg_port_id,
+            s1, s2, s3 = v.split("\n\nInterface")
+            for interface, port_id, priority, _, state, _, desg_bridge, desg_port_id in parse_table(s2):
+                desg_bridge_priority, desg_bridge_id = desg_bridge.split()
+                i = {
+                    "interface": interface,
+                    "port_id": port_id,
+                    "state": {
+                        "dis": "disabled",
+                        "blk": "discarding",
+                        "??": "learning",
+                        "fwd": "forwarding"
+                    }[state.lower()],  # @todo: refine states
+                    "priority": priority,
+                    "designated_bridge_id": desg_bridge_id,
+                    "designated_bridge_priority": desg_bridge_priority,
+                    "designated_port_id": desg_port_id,
                 }
-                ri["interfaces"]+=[i]
-            for i,s in zip(ri["interfaces"],parse_table(s3)):
-                interface,role,port_id,priority,cost,status,cost2,link_type,edge,boundary=s
-                i["role"]={
-                    "dis"    : "disabled",
-                    "?"      : "alternate",
-                    "backup" : "backup",
-                    "root"   : "root",
-                    "desg"   : "designated",
-                    "???"    : "master",
-                    "????"   : "nonstp",
-                    "_"      : "unknown"}[role.lower()] #@todo: refine roles
-                i["point_to_point"]="P2P" in link_type.upper()
-                i["edge"]=True if edge.lower().startswith("y") else False
+                ri["interfaces"] += [i]
+            for i, s in zip(ri["interfaces"], parse_table(s3)):
+                interface, role, port_id, priority, cost, status, cost2, link_type, edge, boundary = s
+                i["role"] = {
+                    "dis": "disabled",
+                    "?": "alternate",
+                    "backup": "backup",
+                    "root": "root",
+                    "desg": "designated",
+                    "???": "master",
+                    "????": "nonstp",
+                    "_": "unknown"}[role.lower()]  # @todo: refine roles
+                i["point_to_point"] = "P2P" in link_type.upper()
+                i["edge"] = True if edge.lower().startswith("y") else False
             # Append instance to result
-            r["instances"]+=[ri]
+            r["instances"] += [ri]
         return r
-    
+
     def execute(self):
         # Check if is MSTP
-        v=self.cli("show spanning-tree mst configuration")
+        v = self.cli("show spanning-tree mst configuration")
         if v and not v.startswith("%"):
             return self.process_mstp()
         raise self.NotSupportedError()
