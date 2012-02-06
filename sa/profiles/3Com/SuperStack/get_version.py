7f459fbdba832dad8d51cfa6e140b0082983cef9
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2012-02-06 11:18:20 +0200

    PEP8 Formatting

--- sa/profiles/3Com/SuperStack/get_version.py
+++ sa/profiles/3Com/SuperStack/get_version.py
@@ -11,21 +11,25 @@ import noc.sa.script
 from noc.sa.interfaces import IGetVersion
 import re
 
-rx_version=re.compile(r"Operational Version\s+(?P<version>\S+)",re.MULTILINE|re.DOTALL)
-rx_platform=re.compile(r"\-+3Com\s(?P<platform>[A-Za-z\d\s]+)\-+.*",re.MULTILINE|re.DOTALL)
+rx_version = re.compile(r"Operational Version\s+(?P<version>\S+)",
+    re.MULTILINE | re.DOTALL)
+rx_platform = re.compile(r"\-+3Com\s(?P<platform>[A-Za-z\d\s]+)\-+.*",
+    re.MULTILINE | re.DOTALL)
+
 
 class Script(noc.sa.script.Script):
-    name="3Com.SuperStack.get_version"
-    cache=True
-    implements=[IGetVersion]
+    name = "3Com.SuperStack.get_version"
+    cache = True
+    implements = [IGetVersion]
+
     def execute(self):
-        p=self.motd
-        platform=rx_platform.search(p).group("platform")
-        v=self.cli("system display")
-        v=v.replace(":","")
-        version=rx_version.search(v).group("version")
+        p = self.motd
+        platform = rx_platform.search(p).group("platform")
+        v = self.cli("system display")
+        v = v.replace(":", "")
+        version = rx_version.search(v).group("version")
         return {
-            "vendor"    : "3Com",
-            "platform"  : platform,
-            "version"   : version,
+            "vendor": "3Com",
+            "platform": platform,
+            "version": version
         }
