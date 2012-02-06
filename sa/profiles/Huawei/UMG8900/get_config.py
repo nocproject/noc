b806b69af0bc280fd44761086c66e0c857b6f68e
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2012-02-06 11:45:49 +0200

    PEP8 Formatting

--- sa/profiles/Huawei/UMG8900/get_config.py
+++ sa/profiles/Huawei/UMG8900/get_config.py
@@ -8,9 +8,11 @@
 import noc.sa.script
 from noc.sa.interfaces import IGetConfig
 
+
 class Script(noc.sa.script.Script):
-    name="Huawei.UMG8900.get_config"
-    implements=[IGetConfig]
+    name = "Huawei.UMG8900.get_config"
+    implements = [IGetConfig]
+
     def execute(self):
-        config=self.cli("dsp cfg;")
+        config = self.cli("dsp cfg;")
         return self.cleaned_config(config)
