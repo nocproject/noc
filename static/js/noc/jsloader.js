53ba2403ea993a2b3d9144bc23b4061c9ec18131
Dmitry Volodin <dv@nocproject.org>
2012-02-10 15:31:10 +0400

    Script loader offers callback function to continue loading

--- static/js/noc/jsloader.js
+++ static/js/noc/jsloader.js
@@ -1,11 +1,16 @@
 _noc_loaded_scripts = {};
+window._noc_load_callback = null;
 
 function load_scripts(urls, scope, callback) {
     var load = function(u, cb) {
         var url = u.pop();
         if(url == undefined)
-            cb.call(scope);
+            cb.call(scope); // All scripts loaded
         else {
+            // URL starting with "@" will call _noc_load_callback on load
+            var next_on_load = url.substring(0, 1) != "@";
+            if(!next_on_load)
+                url = url.substring(1);
             if(_noc_loaded_scripts[url]) {
                 // Already loaded
                 cb.call(scope);
@@ -17,12 +22,14 @@ function load_scripts(urls, scope, callback) {
 
                 var head = document.getElementsByTagName("head")[0];
                 head.appendChild(script_node);
-                var next = function() {load(u, cb);}
-                script_node.onreadystatechange = function() {
-                    if(this.readyState == "complete")
-                        cb.call(scope);
-                };
-                script_node.onload = next;
+                window._noc_load_callback = function() {load(u, cb);}
+                if(next_on_load) {
+                    script_node.onreadystatechange = function() {
+                        if(this.readyState == "complete")
+                            cb.call(scope);
+                    };
+                    script_node.onload = window._noc_load_callback;
+                }
                 _noc_loaded_scripts[url] = true;
             }
         }
