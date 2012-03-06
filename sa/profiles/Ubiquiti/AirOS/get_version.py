9d6d01b395ae7247f72bdf9e985ea23d313147cb
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2012-03-06 17:05:12 +0200

    PEP8 Formatting

--- sa/profiles/Ubiquiti/AirOS/get_version.py
+++ sa/profiles/Ubiquiti/AirOS/get_version.py
@@ -11,20 +11,23 @@ import noc.sa.script
 from noc.sa.interfaces import IGetVersion
 import re
 
-rx_version=re.compile("\.v(?P<version>[^@]+)@")
+rx_version = re.compile("\.v(?P<version>[^@]+)@")
+
 
 class Script(noc.sa.script.Script):
-    name="Ubiquiti.AirOS.get_version"
-    cache=True
-    implements=[IGetVersion]
+    name = "Ubiquiti.AirOS.get_version"
+    cache = True
+    implements = [IGetVersion]
+
     def execute(self):
-        ps1=self.cli("echo $PS1|sed 's/#/@/'") # Replace # with @ to prevent prompt matching
-        v_match=rx_version.search(ps1)
-        board=self.cli("grep board.name /etc/board.info")
-        l,r=board.split("=")
-        board=r.strip()
+        # Replace # with @ to prevent prompt matching
+        ps1 = self.cli("echo $PS1|sed 's/#/@/'")
+        v_match = rx_version.search(ps1)
+        board = self.cli("grep board.name /etc/board.info")
+        l, r = board.split("=")
+        board = r.strip()
         return {
-            "vendor"   : "Ubiquiti",
-            "platform" : board,
-            "version"  : v_match.group("version"),
+            "vendor": "Ubiquiti",
+            "platform": board,
+            "version": v_match.group("version")
         }
