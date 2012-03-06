ee8fa19804a04292149168baea3525525f19d5b4
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2012-03-06 15:17:53 +0200

    PEP8 Formatting

--- sa/profiles/InfiNet/WANFlexX/get_version.py
+++ sa/profiles/InfiNet/WANFlexX/get_version.py
@@ -11,17 +11,20 @@ import noc.sa.script
 from noc.sa.interfaces import IGetVersion
 import re
 
-rx_ver=re.compile(r"^(?P<platform>.+?)\s+WANFleX\s+(?P<version>\S+)",re.MULTILINE|re.DOTALL)
+rx_ver = re.compile(r"^(?P<platform>.+?)\s+WANFleX\s+(?P<version>\S+)",
+    re.MULTILINE | re.DOTALL)
+
 
 class Script(noc.sa.script.Script):
-    name="InfiNet.WANFlexX.get_version"
-    cache=True
-    implements=[IGetVersion]
+    name = "InfiNet.WANFlexX.get_version"
+    cache = True
+    implements = [IGetVersion]
+
     def execute(self):
-        v=self.cli("system version")
-        match=rx_ver.search(v.strip())
+        v = self.cli("system version")
+        match = rx_ver.search(v.strip())
         return {
-            "vendor"    : "InfiNet",
-            "platform"  : match.group("platform"),
-            "version"   : match.group("version"),
+            "vendor": "InfiNet",
+            "platform": match.group("platform"),
+            "version": match.group("version")
         }
