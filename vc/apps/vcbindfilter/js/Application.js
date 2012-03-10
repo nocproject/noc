43520ea276517e5c81376461890beb5aa0f9916d
Dmitry Volodin <dv@nocproject.org>
2012-03-10 10:39:22 +0400

    vc.vcbindfilter rewritten as ExtModelApplication

---
+++ vc/apps/vcbindfilter/js/Application.js
@@ -0,0 +1,76 @@
+//---------------------------------------------------------------------
+// vc.vcbindfilter application
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2012 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.vc.vcbindfilter.Application");
+
+Ext.define("NOC.vc.vcbindfilter.Application", {
+    extend: "NOC.core.ModelApplication",
+    requires: [
+        "NOC.vc.vcbindfilter.Model",
+        "NOC.vc.vcdomain.LookupField",
+        "NOC.ip.vrf.LookupField",
+        "NOC.vc.vcfilter.LookupField"
+    ],
+    model: "NOC.vc.vcbindfilter.Model",
+    columns: [
+        {
+            text: "VC Domain",
+            dataIndex: "vc_domain__label"
+        },
+        {
+            text: "VRF",
+            dataIndex: "vrf__label"
+        },
+        {
+            text: "AFI",
+            dataIndex: "afi",
+            renderer: function(v) {
+                return "IPv" + v;
+            }
+        },
+        {
+            text: "Prefix",
+            dataIndex: "prefix"
+        },
+        {
+            text: "VC Filter",
+            dataIndex: "vc_filter__label"
+        }
+    ],
+    fields: [
+        {
+            name: "vc_domain",
+            xtype: "vc.vcdomain.LookupField",
+            fieldLabel: "VC Domain",
+            allowBlank: false
+        },
+        {
+            name: "vrf",
+            xtype: "ip.vrf.LookupField",
+            fieldLabel: "VRF",
+            allowBlank: false
+        },
+        {
+            name: "afi",
+            xtype: "combobox",
+            fieldLabel: "Address Family",
+            allowBlank: false,
+            store: [["4", "IPv4"], ["6", "IPv6"]]
+        },
+        {
+            name: "prefix",
+            xtype: "textfield",
+            fieldLabel: "Prefix",
+            allowBlank: false
+        },
+        {
+            name: "vc_filter",
+            xtype: "vc.vcfilter.LookupField",
+            fieldLabel: "VC Filter",
+            allowBlank: false
+        }
+    ]
+});
--- vc/apps/vcbindfilter/tests/test.py
+++ vc/apps/vcbindfilter/tests/test.py
@@ -1,12 +1,29 @@
 # -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
-## vcbindfilter Test
+## vc.vcbindfilter unittes
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2009 The NOC Project
+## Copyright (C) 2007-2012 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-from noc.lib.test import ModelApplicationTestCase
-from django.utils import simplejson as json
 
-class vcbindfilterTestCase(ModelApplicationTestCase):
-    pass
+## NOC modules
+from noc.lib.test import RestModelTestCase, unittest
+
+
+@unittest.skip("Not ready yet")
+class VCBindFilterTestCase(RestModelTestCase):
+    app = "vc.vcbindfilter"
+
+    scenario = [
+        {
+            "GET": {
+                # key: value
+            },
+            "POST": {
+                # key: value
+            },
+            "PUT": {
+                # key: value
+            }
+        }
+    ]
