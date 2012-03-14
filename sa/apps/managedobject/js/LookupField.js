123b1744c156ad4ed10f180e8da975598b7637a9
Dmitry Volodin <dv@nocproject.org>
2012-03-14 12:53:08 +0400

    NOC.sa.managedobject.Lookup and NOC.sa.managedobject.LookupField JS classes

---
+++ sa/apps/managedobject/js/LookupField.js
@@ -0,0 +1,13 @@
+//---------------------------------------------------------------------
+// NOC.sa.managedobject.Lookup
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2012 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.sa.managedobject.LookupField");
+
+Ext.define("NOC.sa.managedobject.LookupField", {
+    extend: "NOC.core.LookupField",
+    alias: "widget.sa.managedobject.LookupField",
+    requires: ["NOC.sa.managedobject.Lookup"]
+});
