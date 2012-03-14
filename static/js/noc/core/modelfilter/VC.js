1e5b2688cccd3639cfbabcf104c68ddebf294e21
Dmitry Volodin <dv@nocproject.org>
2012-03-14 17:33:10 +0400

    VC filter

---
+++ static/js/noc/core/modelfilter/VC.js
@@ -0,0 +1,44 @@
+//---------------------------------------------------------------------
+// NOC.core.modelfilter.VC
+// VC model filter
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2012 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.core.modelfilter.VC");
+
+Ext.define("NOC.core.modelfilter.VC", {
+    extend: "NOC.core.modelfilter.Base",
+
+    initComponent: function() {
+        var me = this;
+
+        Ext.apply(this, {
+            items: [
+                {
+                    xtype: "numberfield",
+                    name: me.name,
+                    itemId: me.name,
+                    width: 180,
+                    listeners: {
+                        change: {
+                            scope: me,
+                            fn: me.onChange
+                        }
+                    }
+                }
+            ]
+        });
+        me.callParent();
+        me.field = me.getComponent(me.name);
+    },
+
+    getFilter: function() {
+        var me = this,
+            r = {},
+            value = me.field.getValue();
+        if(value)
+            r[this.name + "__vc"] = value;
+        return r;
+    }
+});
