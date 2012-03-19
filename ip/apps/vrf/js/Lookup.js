9468e2bf6115301cb8a7a4c7ad7f50b54cc950ef
Dmitry Volodin <dv@nocproject.org>
2012-03-19 09:17:06 +0400

    NOC.ip.vrf.Lookup and .LookupField

---
+++ ip/apps/vrf/js/Lookup.js
@@ -0,0 +1,12 @@
+//---------------------------------------------------------------------
+// NOC.ip.vrf.Lookup
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2012 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.ip.vrf.Lookup");
+
+Ext.define("NOC.ip.vrf.Lookup", {
+    extend: "NOC.core.Lookup",
+    url: "/ip/vrf/lookup/"
+});
