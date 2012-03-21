cf39463f7dcfb66ee74f31efd2ac09fa88039355
Dmitry Volodin <dv@nocproject.org>
2012-03-21 10:36:35 +0400

    Reworked ./noc newapp

--- main/templates/newapp/modelapplication/js/Model.js
+++ main/templates/newapp/modelapplication/js/Model.js
@@ -1,7 +1,7 @@
 //---------------------------------------------------------------------
 // {{module}}.{{app}} Model
 //---------------------------------------------------------------------
-// Copyright (C) 2007-2011 The NOC Project
+// Copyright (C) 2007-{{year}} The NOC Project
 // See LICENSE for details
 //---------------------------------------------------------------------
 console.debug("Defining NOC.{{module}}.{{app}}.Model");
@@ -10,40 +10,5 @@ Ext.define("NOC.{{module}}.{{app}}.Model", {
     extend: "Ext.data.Model",
     rest_url: "/{{module}}/{{app}}/",
 
-    fields: [
-        {
-            name: "id",
-            type: "string"
-        },
-
-        {% for f in fields %}
-        {
-            name: "{{f.name}}",
-            {% if f.type == "CharField" %}
-            type: "string",
-            {% if f.default %}
-            defaultValue: "{{f.default}}"
-            {% endif %}
-            {% endif %}
-            {% if f.type == "TextField" %}
-            type: "string",
-            {% if f.default %}
-            defaultValue: "{{f.default}}"
-            {% endif %}
-            {% endif %}
-            {% if f.type == "IntegerField" %}
-            type: "int",
-            {% if f.default %}
-            defaultValue: {{f.default}}
-            {% endif %}
-            {% endif %}
-            {% if f.type == "BooleanField" %}
-            type: "boolean",
-            {% if f.default != None %}
-            defaultValue: {% if f.default %}true{% else %}false{% endif %}
-            {% endif %}
-            {% endif %}
-        }{% if not forloop.last %},{% endif %}
-        {% endfor %}
-    ]
+    fields: {{js_fields|safe}}
 });
