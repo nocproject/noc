3d9c9299664023201092243c2d2f42047f0e3b7a
Dmitry Volodin <dv@nocproject.org>
2012-03-22 14:49:25 +0400

    vc.vc: Refactored "Add First Free" form

--- vc/apps/vc/js/AddFirstFreeForm.js
+++ vc/apps/vc/js/AddFirstFreeForm.js
@@ -4,6 +4,8 @@
 // Copyright (C) 2007-2012 The NOC Project
 // See LICENSE for details
 //---------------------------------------------------------------------
+// @todo: Auto-fill VC-domain if selected in filter
+//---------------------------------------------------------------------
 console.debug("Defining NOC.vc.vc.AddFirstFreeForm");
 
 Ext.define("NOC.vc.vc.AddFirstFreeForm", {
@@ -16,74 +18,72 @@ Ext.define("NOC.vc.vc.AddFirstFreeForm", {
     autoShow: true,
     closable: false,
     modal: true,
-    callback: null,
+    app: null,
     closable: true,
 
-    items: [
-        {
-            xtype: "form",
+    initComponent: function() {
+        var me = this;
+        Ext.apply(me, {
             items: [
                 {
-                    xtype: "vc.vcdomain.LookupField",
-                    name: "vc_domain",
-                    fieldLabel: "VC Domain",
-                    allowBlank: false
-                },
+                    xtype: "form",
+                    items: [
+                        {
+                            xtype: "vc.vcdomain.LookupField",
+                            name: "vc_domain",
+                            fieldLabel: "VC Domain",
+                            allowBlank: false
+                        },
+                        {
+                            xtype: "vc.vcfilter.LookupField",
+                            name: "vc_filter",
+                            fieldLabel: "VC Filter",
+                            allowBlank: false
+                        }
+                    ]
+                }
+            ],
+            buttons: [
                 {
-                    xtype: "vc.vcfilter.LookupField",
-                    name: "vc_filter",
-                    fieldLabel: "VC Filter",
-                    allowBlank: false
+                    text: "Add First Free",
+                    itemId: "add",
+                    iconCls: "icon_add",
+                    scope: me,
+                    /*formBind: true,
+                    disabled: true,*/
+                    handler: me.onAddFirstFree
                 }
             ]
-        }
-    ],
-    buttons: [
-        {
-            text: "Reset",
-            itemId: "reset",
-            iconCls: "icon_cancel",
-            handler: function() {
-                this.up("form").getForm().reset();
-            }
-        },
-
-        {
-            text: "Add First Free",
-            itemId: "add",
-            iconCls: "icon_add",
-            /*formBind: true,
-            disabled: true,*/
-            handler: function() {
-                var me = this.up("window");
-                if(me.callback) {
-                    var r = me.down("form").getForm().getValues();
-                    Ext.Ajax.request({
-                        method: "GET",
-                        url: "/vc/vc/find_free/",
-                        params: {
-                            vc_domain: r.vc_domain,
-                            vc_filter: r.vc_filter
-                        },
-                        scope: me,
-                        success: function(response) {
-                            var vc = Ext.decode(response.responseText);
-                            if(!vc) {
-                                // No Free VC
-                                Ext.Msg.alert("Error", "No free VC found");
-                                me.close();
-                                return;
-                            }
-                            me.close();
-                            me.callback(r.vc_domain, vc);
-                        },
-                        failure: function() {
-                            // @todo: Report Error
-                            me.close();
-                        }
-                    });
+        });
+        me.callParent();
+    },
+    // Called when "Add button pressed"
+    onAddFirstFree: function() {
+        var me = this;
+        var r = me.down("form").getForm().getValues();
+        Ext.Ajax.request({
+            method: "GET",
+            url: "/vc/vc/find_free/",
+            params: {
+                vc_domain: r.vc_domain,
+                vc_filter: r.vc_filter
+            },
+            scope: me,
+            success: function(response) {
+                var vc = Ext.decode(response.responseText);
+                if(!vc) {
+                    // No Free VC
+                    Ext.Msg.alert("Error", "No free VC found");
+                    me.close();
+                    return;
                 }
+                me.close();
+                me.app.onNewRecord({vc_domain: r.vc_domain, l1: vc});
+            },
+            failure: function() {
+                // @todo: Report Error
+                me.close();
             }
-        }
-    ]
+        });
+    }
 });
