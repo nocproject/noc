9843ca491e58265e796e182cac2da9dce06f097c
Dmitry Volodin <dv@nocproject.org>
2012-03-27 21:38:04 +0400

    Code has been reorganized

--- static/js/noc/core/Lookup.js
+++ static/js/noc/core/Lookup.js
@@ -10,24 +10,28 @@ console.debug("Defining NOC.core.Lookup");
 Ext.define("NOC.core.Lookup", {
     extend: "Ext.data.Store",
     fields: ["id", "label"],
+    url: null,
     //autoLoad: true,
     constructor: function(config) {
-        this.callParent(config);
-        this.setProxy(Ext.create("Ext.data.RestProxy", {
-            url: this.url,
-            pageParam: "__page",
-            startParam: "__start",
-            limitParam: "__limit",
-            sortParam: "__sort",
-            extraParams: {
-                "__format": "ext"
-            },
-            reader: {
-                type: "json",
-                root: "data",
-                totalProperty: "total",
-                successProperty: "success"
-            }
-        }));
+        var me = this;
+        Ext.apply(me, {
+            proxy: Ext.create("Ext.data.RestProxy", {
+                url: me.url,
+                pageParam: "__page",
+                startParam: "__start",
+                limitParam: "__limit",
+                sortParam: "__sort",
+                extraParams: {
+                    "__format": "ext"
+                },
+                reader: {
+                    type: "json",
+                    root: "data",
+                    totalProperty: "total",
+                    successProperty: "success"
+                }
+                })
+        });
+        me.callParent(config);
     }
 });
