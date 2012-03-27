806c2300ee0bb77f54431357eaf3db1ef04a4185
Dmitry Volodin <dv@nocproject.org>
2012-03-27 21:36:39 +0400

    Cisco.IOS.get_mpls_vpn

--- sa/profiles/Cisco/IOS/get_mpls_vpn.py
+++ sa/profiles/Cisco/IOS/get_mpls_vpn.py
@@ -1,40 +1,41 @@
 # -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
-## Cisco.IOS.get_vrf
+## Cisco.IOS.get_mpls_vpn
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2009 The NOC Project
+## Copyright (C) 2007-2012 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-"""
-"""
+
 ## Python modules
 import re
 ## NOC modules
 from noc.sa.script import Script as NOCScript
-from noc.sa.interfaces import IGetVRF
+from noc.sa.interfaces import IGetMPLSVPN
 
 
 class Script(NOCScript):
-    name = "Cisco.IOS.get_vrf"
-    implements = [IGetVRF]
+    name = "Cisco.IOS.get_mpls_vpn"
+    implements = [IGetMPLSVPN]
 
     rx_line = re.compile(r"^\s+(?P<vrf>.+?)\s+"
                          r"(?P<rd>\S+:\S+)\s+(?P<iface>.+?)\s*$")
     rx_cont = re.compile("^\s{6,}(?P<iface>.+?)\s*$")
 
     def execute(self, **kwargs):
-        vrfs = []
+        vpns = []
         v = self.cli("show ip vrf")
         for l in v.splitlines():
             match = self.rx_line.match(l)
             if match:
-                vrfs += [{
+                vpns += [{
+                    "type": "VRF",
+                    "status": True,
                     "name": match.group("vrf"),
                     "rd": match.group("rd"),
                     "interfaces": [match.group("iface")]
                 }]
-            elif vrfs:
+            elif vpns:
                 match = self.rx_cont.match(l)
                 if match:
-                    vrfs[-1]["interfaces"] += [match.group("iface")]
-        return vrfs
+                    vpns[-1]["interfaces"] += [match.group("iface")]
+        return vpns
