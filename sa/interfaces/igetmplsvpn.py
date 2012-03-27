35e3bd8ea854266de2fc5f184e9827bdad384792
Dmitry Volodin <dv@nocproject.org>
2012-03-27 21:36:02 +0400

    IGetMPLSVPN interfaces replaces IGetVRF

--- sa/interfaces/igetmplsvpn.py
+++ sa/interfaces/igetmplsvpn.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
-## IGetVRF
+## IGetMPLSVPN interface
 ##----------------------------------------------------------------------
 ## Copyright (C) 2007-2012 The NOC Project
 ## See LICENSE for details
@@ -10,9 +10,16 @@
 from base import *
 
 
-class IGetVRF(Interface):
+class IGetMPLSVPN(Interface):
     returns = ListOfParameter(element=DictParameter(attrs={
+        # VPN type
+        "type": StringParameter(choices=["VRF", "VPLS", "VLL"]),
+        # VPN state. True - active, False - inactive
+        "status": BooleanParameter(default=True),
+        # Unique VPN name
         "name": StringParameter(),
+        # RD
         "rd": RDParameter(),
+        # List of interfaces
         "interfaces": ListOfParameter(element=InterfaceNameParameter())
     }))
