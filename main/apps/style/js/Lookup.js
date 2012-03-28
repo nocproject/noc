67e61a78abb07bbb80316ce84322cc833bf24018
Dmitry Volodin <dv@nocproject.org>
2012-03-28 17:43:02 +0400

    main.style lookup fields

---
+++ main/apps/style/js/Lookup.js
@@ -0,0 +1,12 @@
+//---------------------------------------------------------------------
+// NOC.ip.vrfgroup.Lookup
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2012 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.main.style.Lookup");
+
+Ext.define("NOC.main.style.Lookup", {
+    extend: "NOC.core.Lookup",
+    url: "/main/style/lookup/"
+});
