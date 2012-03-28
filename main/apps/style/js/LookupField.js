b88529c3cf1958cd14555bd1529f0a7a72165448
Dmitry Volodin <dv@nocproject.org>
2012-03-28 17:43:02 +0400

    main.style lookup fields

---
+++ main/apps/style/js/LookupField.js
@@ -0,0 +1,13 @@
+//---------------------------------------------------------------------
+// NOC.main.style.Lookup
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2012 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.main.style.LookupField");
+
+Ext.define("NOC.main.style.LookupField", {
+    extend: "NOC.core.LookupField",
+    alias: "widget.main.style.LookupField",
+    requires: ["NOC.main.style.Lookup"]
+});
