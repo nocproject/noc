040226151af39179c6471620b3bb5ac9741aeefe
Dmitry Volodin <dv@nocproject.org>
2012-04-05 10:11:53 +0400

    NOC-438: VRF-aware IGetARP interface

--- sa/interfaces/igetarp.py
+++ sa/interfaces/igetarp.py
@@ -2,17 +2,21 @@
 ##----------------------------------------------------------------------
 ## IGetARP - interface to query ARP cache
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2009 The NOC Project
+## Copyright (C) 2007-2012 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-"""
-"""
+
+## NOC modules
 from base import *
 
+
 class IGetARP(Interface):
-    returns=ListOfParameter(element=DictParameter(attrs={
-            "ip"         : IPv4Parameter(),
-            "mac"        : MACAddressParameter(required=False), # NONE for incomplete entries
-            "interface"  : StringParameter(required=False),     # NONE for incomplete entries
-            }))
+    vrf = StringParameter(required=False)
+    returns = ListOfParameter(element=DictParameter(attrs={
+        "ip": IPv4Parameter(),
+        # NONE for incomplete entries
+        "mac": MACAddressParameter(required=False),
+        # NONE for incomplete entries
+        "interface": InterfaceNameParameter(required=False),
+    }))
     template = "interfaces/igetarp.html"
