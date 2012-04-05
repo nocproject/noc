98ad85078b4cc5edbeb6a9455a129735c611ffa7
Dmitry Volodin <dv@nocproject.org>
2012-04-05 10:20:21 +0400

    NOC-440: VRF-aware Juniper.JUNOS.get_arp script

--- sa/profiles/Juniper/JUNOS/get_arp.py
+++ sa/profiles/Juniper/JUNOS/get_arp.py
@@ -5,18 +5,23 @@
 ## Copyright (C) 2007-2009 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-"""
-"""
-import noc.sa.script
-from noc.sa.interfaces import IGetARP
+
+## Python modules
 import re
+## NOC modules
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetARP
 
-rx_line = re.compile(r"^(?P<mac>[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2})\s+(?P<ip>\d+\.\d+\.\d+\.\d+)\s+(?P<interface>\S+)")
-
-
-class Script(noc.sa.script.Script):
+class Script(NOCScript):
     name = "Juniper.JUNOS.get_arp"
     implements = [IGetARP]
 
-    def execute(self):
-        return self.cli("show arp no-resolve", list_re=rx_line)
+    rx_line = re.compile(
+        r"^(?P<mac>[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2})\s+(?P<ip>\d+\.\d+\.\d+\.\d+)\s+(?P<interface>\S+)")
+
+    def execute(self, vrf=None):
+        if vrf:
+            cmd = "show arp no-resolve vpn %s" % vrf
+        else:
+            cmd = "show arp no-resolve"
+        return self.cli(cmd, list_re=self.rx_line)
