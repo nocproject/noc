68228501179ab10c38f8446ebeaaa46184d81104
Dmitry Volodin <dv@nocproject.org>
2012-04-07 18:06:18 +0400

    ModelStore.sync() method accepts callbacks

--- static/js/noc/core/ModelStore.js
+++ static/js/noc/core/ModelStore.js
@@ -9,21 +9,52 @@ console.debug("Defining NOC.core.ModelStore");
 Ext.define("NOC.core.ModelStore", {
     extend: "Ext.data.Store",
     filterParams: undefined,
+    url: undefined,
+    model: undefined,
+    remoteSort: true,
+    remoteFilter: true,
 
-    initComponent: function() {
-        var me = this;
+    constructor: function(config) {
+        var me = this,
+            model = Ext.create(config.model);
 
-        me.addEvents(
-            /**
-             * Fires on succes load() operation
-             */
-            "loadsuccess",
-            /**
-             * Fires on failed load() operation
-             */
-            "loadfailed"
-        );
-        me.callParent();
+        Ext.apply(config, {
+            model: config.model,
+            proxy: Ext.create("Ext.data.RestProxy", {
+                url: model.rest_url,
+                pageParam: "__page",
+                startParam: "__start",
+                limitParam: "__limit",
+                sortParam: "__sort",
+                extraParams: {
+                    "__format": "ext"
+                },
+                reader: {
+                    type: "json",
+                    root: "data",
+                    totalProperty: "total",
+                    successProperty: "success"
+                },
+                writer: {
+                    type: "json"
+                },
+                listeners: {
+                    exception: {
+                        scope: me,
+                        fn: me.onSyncException
+                    }
+                }
+            }),
+            listeners: {
+                write: {
+                    scope: me,
+                    fn: me.onSyncWrite
+                }
+            },
+            syncConfig: {}
+        });
+        //me.syncConfig = {};
+        me.callParent([config]);
     },
 
     setFilterParams: function(config) {
@@ -48,5 +79,25 @@ Ext.define("NOC.core.ModelStore", {
         });
         // Continue loading
         me.callParent([config]);
+    },
+    // override sync()
+    sync: function(config) {
+        var me = this,
+            conf = config || {};
+        console.log("store.sync", config);
+        me.syncConfig = Ext.Object.merge({}, conf);
+        me.callParent();
+    },
+    onSyncWrite: function() {
+        var me = this;
+        Ext.callback(me.syncConfig.success,
+            me.syncConfig.scope || me);
+    },
+    onSyncException: function(proxy, response, op, opts) {
+        var me = this,
+            status = Ext.decode(response.responseText);
+        status.status = response.status;
+        Ext.callback(me.syncConfig.failure,
+            me.syncConfig.scope || me, [response, op, status]);
     }
 });
