3777db317a9b7142150419aaca6328a621632df1
Dmitry Volodin <dv@nocproject.org>
2012-04-08 15:35:57 +0400

    PEP8 formatting

--- main/apps/style/views.py
+++ main/apps/style/views.py
@@ -2,31 +2,36 @@
 ##----------------------------------------------------------------------
 ## Style Manager
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2010 The NOC Project
+## Copyright (C) 2007-2012 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
+
+## Django modules
 from django.contrib import admin
+## NOC modules
 from noc.lib.app import ModelApplication
 from noc.main.models import Style
 ##
 ## Style sample
 ##
 def color_sample(obj):
-    return u"<div style='padding: 4px; %s'> Sample text </div>"%obj.style
-color_sample.short_description="Sample"
-color_sample.allow_tags=True
+    return u"<div style='padding: 4px; %s'> Sample text </div>" % obj.style
+
+color_sample.short_description = "Sample"
+color_sample.allow_tags = True
 
 ##
 ## Style admin
 ##
 class StyleAdmin(admin.ModelAdmin):
-    list_display=["name","is_active",color_sample,"description"]
-    search_fields=["name"]
-    list_filter=["is_active"]
+    list_display = ["name", "is_active", color_sample, "description"]
+    search_fields = ["name"]
+    list_filter = ["is_active"]
+
 ##
 ## Style application
 ##
 class StyleApplication(ModelApplication):
-    model=Style
-    model_admin=StyleAdmin
-    menu="Setup | Styles"
+    model = Style
+    model_admin = StyleAdmin
+    menu = "Setup | Styles"
