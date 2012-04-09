fc3d23a53f0bc869aa16a91d3049ffbc87cb02ce
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2012-04-09 17:48:16 +0300

    PEP8 Formatting

--- sa/interfaces/igetspanningtree.py
+++ sa/interfaces/igetspanningtree.py
@@ -8,9 +8,10 @@
 """
 """
 from base import *
-##
+
+
 class IGetSpanningTree(Interface):
-    returns=DictParameter(attrs={
+    returns = DictParameter(attrs={
         # Spanning-tree mode. Must be one of:
         #    None        - Spanning-tree is disabled
         #    STP         - IEEE 802.1d Spanning Tree Protocol
@@ -18,60 +19,60 @@ class IGetSpanningTree(Interface):
         #    MSTP        - IEEE 802.1Q-2003 Multiple Spanning Tree Protocol
         #    PVST+       - Per-Vlan Spanning Tree Plus - Cisco extension to STP
         #    rapid-PVST+ - Rapind Per-Vlan Spanning Tree Plus - Cisco extension to RSTP
-        "mode"      : StringParameter(choices=["None", "STP", "RSTP", "MSTP", "PVST+", "rapid-PVST+"]),
+        "mode": StringParameter(choices=["None", "STP", "RSTP", "MSTP", "PVST+", "rapid-PVST+"]),
         #
-        "configuration" : DictParameter(attrs={
+        "configuration": DictParameter(attrs={
             # MSTP-specific configuration
-            "MSTP" : DictParameter(attrs={
+            "MSTP": DictParameter(attrs={
                 # MST Region
-                "region"  : StringParameter(),
+                "region": StringParameter(),
                 # MST Revision
                 "revision": IntParameter(),
-            },required=False),
-        },required=False),
+            }, required=False),
+        }, required=False),
         # List of STP instances.
         # For mode==None, list must be empty
         # For STP and RSTP, only one instance id 0 must be present
         # For PVST mode - instance id is vlan id
         # For MSTP mode - instance id is MSTP instance id
-        "instances" : ListOfParameter(element=DictParameter(attrs={
+        "instances": ListOfParameter(element=DictParameter(attrs={
             # Instnce ID
-            "id"      : IntParameter(),
+            "id": IntParameter(),
             # VLAN IDs mapped to instance.
             # Must be 1-4095 for STP and RSTP
             # IDs mapped to particular instance for MSTP
             # AND single VLAN id for *PVST+
-            "vlans"   : VLANIDMapParameter(),
+            "vlans": VLANIDMapParameter(),
             # Root bridge id, equal to bridge_id for root bridge
-            "root_id" : MACAddressParameter(),
+            "root_id": MACAddressParameter(),
             # Root bridge priority
-            "root_priority" : IntParameter(),
+            "root_priority": IntParameter(),
             # Bridge ID
-            "bridge_id" : MACAddressParameter(),
+            "bridge_id": MACAddressParameter(),
             # Bridge priority
-            "bridge_priority" : IntParameter(),
+            "bridge_priority": IntParameter(),
             # Interfaces
-            "interfaces" : ListOfParameter(element=DictParameter(attrs={
+            "interfaces": ListOfParameter(element=DictParameter(attrs={
                 # Interface name
-                "interface" : InterfaceNameParameter(),
+                "interface": InterfaceNameParameter(),
                 # Local port id
-                "port_id"   : StringParameter(),
+                "port_id": StringParameter(),
                 # Interface state
-                "state"     : StringParameter(choices=["disabled","discarding","learning","forwarding","unknown"]),
+                "state": StringParameter(choices=["disabled", "discarding", "learning", "forwarding", "unknown"]),
                 # Interface role
-                "role"      : StringParameter(choices=["disabled","alternate","backup","root","designated","master","nonstp","unknown"]),
+                "role": StringParameter(choices=["disabled", "alternate", "backup", "root", "designated", "master", "nonstp", "unknown"]),
                 # Port priority
-                "priority"  : IntParameter(),
+                "priority": IntParameter(),
                 # Designated bridge ID
-                "designated_bridge_id" : MACAddressParameter(),
+                "designated_bridge_id": MACAddressParameter(),
                 # Designated bridge priority
-                "designated_bridge_priority" : IntParameter(),
+                "designated_bridge_priority": IntParameter(),
                 # Designated port id
-                "designated_port_id" : StringParameter(),
+                "designated_port_id": StringParameter(),
                 # P2P indicator
-                "point_to_point"     : BooleanParameter(),
+                "point_to_point": BooleanParameter(),
                 # MSTP EdgePort
-                "edge"              : BooleanParameter(),
+                "edge": BooleanParameter()
             }))
         })),
     })
