aeaa6b7221be4396bab66f342ff6c27944265f6f
Dmitry Volodin <dv@nocproject.org>
2012-04-09 14:41:51 +0400

    Cisco.IOSXR.convert_interface_name fix

--- sa/profiles/Cisco/IOSXR/__init__.py
+++ sa/profiles/Cisco/IOSXR/__init__.py
@@ -3,16 +3,20 @@
 ## Vendor: Cisco
 ## OS:     IOS XR
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2011 The NOC Project
+## Copyright (C) 2007-2012 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-import noc.sa.profiles
-from noc.sa.protocols.sae_pb2 import TELNET, SSH
+
+# Python modules
+import re
+# NOC modules
+from noc.sa.profiles import Profile as NOCProfile
+from noc.sa.interfaces import InterfaceTypeError
 
 
-class Profile(noc.sa.profiles.Profile):
+class Profile(NOCProfile):
     name = "Cisco.IOSXR"
-    supported_schemes = [TELNET, SSH]
+    supported_schemes = [NOCProfile.TELNET, NOCProfile.SSH]
     pattern_more = r"^ --More--"
     pattern_unpriveleged_prompt = r"^\S+?>"
     pattern_syntax_error = r"% Invalid input detected at"
@@ -20,9 +24,22 @@ class Profile(noc.sa.profiles.Profile):
     command_super = "enable"
     pattern_prompt = r"^(?P<hostname>\S+?)(?:-\d+)?(?:\(config[^\)]*\))?#"
     requires_netmask_conversion = True
-    convert_mac = noc.sa.profiles.Profile.convert_mac_to_cisco
-    convert_interface_name = noc.sa.profiles.Profile.convert_interface_name_cisco
-    config_volatile = [r"^ntp clock-period .*?^"]
+    convert_mac = NOCProfile.convert_mac_to_cisco
+
+    rx_interface_name = re.compile(
+        r"^(?P<type>[a-z\-]+)\s*(?P<number>\d+(/\d+)*(?:\.\d+)?(?:/CPU\d+(?:/\d+)*)?)$",
+        re.IGNORECASE)
+
+    def convert_interface_name(self, s):
+        """
+        MgmtEth0/1/CPU0/0
+        GigabitEthernet0/2/0/0.1000
+        """
+        match = self.rx_interface_name.match(s)
+        if not match:
+            raise InterfaceTypeError("Invalid interface '%s'" % s)
+        return s
+
 
     def generate_prefix_list(self, name, pl):
         """
