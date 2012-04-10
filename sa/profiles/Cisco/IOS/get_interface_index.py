f91af0556d4aef9814ed3d3172e2635d26810732
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2012-04-10 14:54:56 +0300

    Add Cisco.IOS.get_interface_index

---
+++ sa/profiles/Cisco/IOS/get_interface_index.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Cisco.IOS.get_interface_index
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2012 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetIfIndex
+import re
+
+
+class Script(NOCScript):
+    name = "Cisco.IOS.get_interface_index"
+    implements = [IGetIfIndex]
+    rx_line = re.compile(
+        r"Interface = \S+, Ifindex = (?P<index>\d+)")
+
+    def execute(self, interface):
+        try:
+            c = self.cli("show snmp mib ifmib ifindex %s" % interface)
+        except self.CLISyntaxError:
+            return None
+
+        match = self.re_search(self.rx_line, c)
+        if match:
+            return match.group("index")
+        return None
