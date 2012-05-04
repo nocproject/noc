f4b31c2dd3d28f6a562aec4a3a94b8d32f3cc178
Dmitry Volodin <dv@nocproject.org>
2012-05-04 11:23:07 +0400

    NOC-497: fm.ignoreeventrule rewritten as ExtApplication

---
+++ fm/apps/ignoreeventrule/js/Model.js
@@ -0,0 +1,40 @@
+//---------------------------------------------------------------------
+// fm.ignoreeventrule Model
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2012 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.fm.ignoreeventrule.Model");
+
+Ext.define("NOC.fm.ignoreeventrule.Model", {
+    extend: "Ext.data.Model",
+    rest_url: "/fm/ignoreeventrule/",
+
+    fields: [
+        {
+            name: "id",
+            type: "int"
+        },
+        {
+            name: "name",
+            type: "string"
+        },
+        {
+            name: "left_re",
+            type: "string"
+        },
+        {
+            name: "right_re",
+            type: "string"
+        },
+        {
+            name: "is_active",
+            type: "boolean",
+            defaultValue: true
+        },
+        {
+            name: "description",
+            type: "string"
+        }
+    ]
+});
