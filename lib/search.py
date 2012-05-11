633f844ab801d8570a43340401537f8b5cebbb5f
Dmitry Volodin <dv@nocproject.org>
2012-05-11 09:32:39 +0400

    PEP8

--- lib/search.py
+++ lib/search.py
@@ -2,34 +2,43 @@
 ##----------------------------------------------------------------------
 ## Search engine with plugin support
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2009 The NOC Project
+## Copyright (C) 2007-2012 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-"""
-"""
+
+## Python modules
+import types
+## NOC modules
 from noc.lib.app import site
 from noc.main.models import search_methods
-import types
-##
-## Wrapper for search results
-##
+
+
 class SearchResult(object):
-    def __init__(self,url,title,text,relevancy=1.0):
-        if type(url) in [types.TupleType,types.ListType]:
-            self.url=site.reverse(*url)
+    """
+    Wrapper for search results
+    """
+    def __init__(self, url, title, text, relevancy=1.0):
+        if type(url) in [types.TupleType, types.ListType]:
+            self.url = site.reverse(*url)
         else:
-            self.url=url
-        self.title=title
-        self.text=text
-        self.relevancy=relevancy
-    
-    def __cmp__(self,other):
-        return cmp(other.relevancy,self.relevancy)
-##
-## Interface to built-in search system
-##
-def search(user,query,limit=100):
-    r=[]
+            self.url = url
+        self.title = title
+        self.text = text
+        self.relevancy = relevancy
+
+    def __cmp__(self, other):
+        return cmp(other.relevancy, self.relevancy)
+
+
+def search(user, query, limit=100):
+    """
+    Interface to built-in search system
+    :param user:
+    :param query:
+    :param limit:
+    :return:
+    """
+    r = []
     for sm in search_methods:
-        r+=list(sm(user,query,limit))
+        r += list(sm(user, query, limit))
     return sorted(r)[:limit]
