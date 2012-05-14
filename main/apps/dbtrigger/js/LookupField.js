006bda5ca6f278e83d7e5577d9e865161742201c
Dmitry Volodin <dv@nocproject.org>
2012-05-14 17:39:38 +0400

    NOC-506: main.dbtrigger has been rewritten as ExtModelApplication

---
+++ main/apps/dbtrigger/js/LookupField.js
@@ -0,0 +1,13 @@
+//---------------------------------------------------------------------
+// NOC.main.dbtrigger.Lookup
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2012 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.main.dbtrigger.LookupField");
+
+Ext.define("NOC.main.dbtrigger.LookupField", {
+    extend: "NOC.core.LookupField",
+    alias: "widget.main.dbtrigger.LookupField",
+    requires: ["NOC.main.dbtrigger.Lookup"]
+});
