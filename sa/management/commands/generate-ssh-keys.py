985d96e4f60cfa386e22624e86142526068dadd7
Dmitry Volodin <dv@nocproject.org>
2012-05-23 14:13:50 +0400

    PEP8

--- sa/management/commands/generate-ssh-keys.py
+++ sa/management/commands/generate-ssh-keys.py
@@ -12,48 +12,53 @@ import os
 import ConfigParser
 ## Django modules
 from django.core.management.base import BaseCommand
-from optparse import OptionParser, make_option
 ## NOC modules
 from noc.sa.script.ssh.keys import generate_pair
 
+
 class Command(BaseCommand):
-    help="Generate ssh keys"
-    
-    ##
-    ## Read noc-launcher config and retrieve all activator instances configs
-    ##
+    help = "Generate ssh keys"
+
     def get_activator_configs(self):
-        config=ConfigParser.SafeConfigParser()
+        """
+        Read noc-launcher config and retrieve all activator instances configs
+        :return:
+        """
+        config = ConfigParser.SafeConfigParser()
         config.read("etc/noc-launcher.defaults")
         config.read("etc/noc-launcher.conf")
-        return [config.get("noc-activator", c) for c in config.options("noc-activator") if c.startswith("config")]
-    
-    ##
-    ## Returns ssh key path
-    ##
+        return [config.get("noc-activator", c) for c in
+                config.options("noc-activator") if
+                c.startswith("config")]
+
     def get_ssh_key_path(self, path):
+        """
+        Returns ssh key path
+        :param path:
+        :return:
+        """
         if path.endswith(".conf"):
-            d_path=path[:-5]+".defaults"
+            d_path = path[:-5] + ".defaults"
         else:
-            d_path=path+".defaults"
-        config=ConfigParser.SafeConfigParser()
+            d_path = path + ".defaults"
+        config = ConfigParser.SafeConfigParser()
         config.read(d_path)
         config.read(path)
         return config.get("ssh", "key")
-    
-    ##
-    ## Create ssh keys when necessary
-    ##
+
     def check_key(self, path):
-        pub_path=path+".pub"
+        """
+        Create ssh keys when necessary
+        :param path:
+        :return:
+        """
+        pub_path = path + ".pub"
         if not os.path.exists(path) or not os.path.exists(pub_path):
-            print "Generating RSA keys: (%s, %s)"%(path, pub_path)
+            print "Generating RSA keys: (%s, %s)" % (path, pub_path)
             generate_pair(path)
-    
-    ##
-    ##
+
     def handle(self, *args, **options):
-        paths=set([self.get_ssh_key_path(c) for c in self.get_activator_configs()])
+        paths = set([self.get_ssh_key_path(c) for c in
+                     self.get_activator_configs()])
         for path in paths:
             self.check_key(path)
-    
