8f8a2746a914913fc8fdce6bc5fa490bcb87d0f1
Dmitry Volodin <dv@nocproject.org>
2012-05-25 10:08:03 +0400

    PEP8 formatting

--- lib/text.py
+++ lib/text.py
@@ -2,7 +2,7 @@
 ##----------------------------------------------------------------------
 ## Various text-processing utilities
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2009 The NOC Project
+## Copyright (C) 2007-2012 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 import re
@@ -19,8 +19,10 @@ import re
 ## ddd   eee     fff
 ## Will be parsed down to the [["a","b","c"],["ddd","eee","fff"]]
 ##
-rx_header_start=re.compile(r"^\s*[-=]+\s+[-=]+")
-rx_col=re.compile(r"^(\s*)([\-]+|[=]+)")
+rx_header_start = re.compile(r"^\s*[-=]+\s+[-=]+")
+rx_col = re.compile(r"^(\s*)([\-]+|[=]+)")
+
+
 def parse_table(s):
     """
     >>> parse_table("First Second Third\\n----- ------ -----\\na     b       c\\nddd   eee     fff\\n")
@@ -28,57 +30,69 @@ def parse_table(s):
     >>> parse_table("First Second Third\\n----- ------ -----\\na             c\\nddd   eee     fff\\n")
     [['a', '', 'c'], ['ddd', 'eee', 'fff']]
     """
-    columns=None
-    r=[]
-    columns=[]
+    r = []
+    columns = []
     for l in s.splitlines():
         if not l.strip():
-            columns=[]
+            columns = []
             continue
-        if rx_header_start.match(l): # Column delimiters found. try to determine column's width
-            columns=[]
-            x=0
+        if rx_header_start.match(l):
+            # Column delimiters found. try to determine column's width
+            columns = []
+            x = 0
             while l:
-                match=rx_col.match(l)
+                match = rx_col.match(l)
                 if not match:
                     break
-                columns.append((x+len(match.group(1)),x+len(match.group(1))+len(match.group(2))))
-                x+=match.end()
-                l=l[match.end():]
-        elif columns: # Fetch cells
-            r.append([l[f:t].strip() for f,t in columns])
+                columns.append((x + len(match.group(1)),
+                                x + len(match.group(1)) + len(
+                                    match.group(2))))
+                x += match.end()
+                l = l[match.end():]
+        elif columns:  # Fetch cells
+            r.append([l[f:t].strip() for f, t in columns])
     return r
+
 ##
 ## Convert HTML to plain text
 ##
-rx_html_tags=re.compile("</?[^>+]+>",re.MULTILINE|re.DOTALL)
+rx_html_tags = re.compile("</?[^>+]+>", re.MULTILINE | re.DOTALL)
+
+
 def strip_html_tags(s):
-    t=rx_html_tags.sub("",s)
-    for k,v in [("&nbsp;"," "),("&lt;","<"),("&gt;",">"),("&amp;","&")]:
-        t=t.replace(k,v)
+    t = rx_html_tags.sub("", s)
+    for k, v in [("&nbsp;", " "), ("&lt;", "<"), ("&gt;", ">"),
+        ("&amp;", "&")]:
+        t = t.replace(k, v)
     return t
+
 
 ##
 ## Convert XML to list of elements
 ##
-def xml_to_table(s,root,row):
+def xml_to_table(s, root, row):
     """
     >>> xml_to_table('<?xml version="1.0" encoding="UTF-8" ?><response><action><row><a>1</a><b>2</b></row><row><a>3</a><b>4</b></row></action></response>','action','row')
     [{'a': '1', 'b': '2'}, {'a': '3', 'b': '4'}]
     """
     # Detect root element
-    match=re.search(r"<%s>(.*)</%s>"%(root,root),s,re.DOTALL|re.IGNORECASE)
+    match = re.search(r"<%s>(.*)</%s>" % (root, root), s,
+        re.DOTALL | re.IGNORECASE)
     if not match:
         return []
-    s=match.group(1)
-    row_re=re.compile(r"<%s>(.*?)</%s>"%(row,row),re.DOTALL|re.IGNORECASE)
-    item_re=re.compile(r"<([^\]+])>(.*?)</\1>",re.DOTALL|re.IGNORECASE)
-    r=[]
+    s = match.group(1)
+    row_re = re.compile(r"<%s>(.*?)</%s>" % (row, row),
+        re.DOTALL | re.IGNORECASE)
+    item_re = re.compile(r"<([^\]+])>(.*?)</\1>",
+        re.DOTALL | re.IGNORECASE)
+    r = []
     for m in [x for x in row_re.split(s) if x]:
-        data=item_re.findall(m)
+        data = item_re.findall(m)
         if data:
-            r+=[dict(data)]
+            r += [dict(data)]
     return r
+
+
 ##
 ## Convert list of values to string of ranges
 ##
@@ -99,30 +113,34 @@ def list_to_ranges(s):
     >>> list_to_ranges(range(1,4001))
     '1-4000'
     """
+
     def f():
-        if last_start==last_end:
+        if last_start == last_end:
             return str(last_start)
         else:
-            return "%d-%d"%(last_start,last_end)
-    last_start=None
-    last_end=None
-    r=[]
+            return "%d-%d" % (last_start, last_end)
+
+    last_start = None
+    last_end = None
+    r = []
     for i in sorted(s):
-        if last_end is not None and i==last_end+1:
-            last_end+=1
+        if last_end is not None and i == last_end + 1:
+            last_end += 1
         else:
             if last_start is not None:
-                r+=[f()]
-            last_start=i
-            last_end=i
+                r += [f()]
+            last_start = i
+            last_end = i
     if last_start is not None:
-        r+=[f()]
+        r += [f()]
     return ",".join(r)
-    
+
 ##
 ## Convert range string to a list of integers
 ##
-rx_range=re.compile(r"^(\d+)\s*-\s*(\d+)$")
+rx_range = re.compile(r"^(\d+)\s*-\s*(\d+)$")
+
+
 def ranges_to_list(s):
     """
     >>> ranges_to_list("1")
@@ -134,28 +152,29 @@ def ranges_to_list(s):
     >>> ranges_to_list("1, 10-12, 15, 17-19")
     [1, 10, 11, 12, 15, 17, 18, 19]
     """
-    r=[]
+    r = []
     for p in s.split(","):
-        p=p.strip()
+        p = p.strip()
         try:
-            r+=[int(p)]
+            r += [int(p)]
             continue
         except:
             pass
-        match=rx_range.match(p)
+        match = rx_range.match(p)
         if not match:
             raise SyntaxError
-        f,t=[int(x) for x in match.groups()]
-        if f>=t:
+        f, t = [int(x) for x in match.groups()]
+        if f >= t:
             raise SyntaxError
-        for i in range(f,t+1):
-            r+=[i]
+        for i in range(f, t + 1):
+            r += [i]
     return sorted(r)
+
 
 ##
 ## Replace regular expression group with pattern
 ##
-def replace_re_group(expr,group,pattern):
+def replace_re_group(expr, group, pattern):
     """
     >>> replace_re_group("nothing","(?P<groupname>","groupvalue")
     'nothing'
@@ -164,34 +183,35 @@ def replace_re_group(expr,group,pattern):
     >>> replace_re_group("the (?P<groupname> nested (test)>)","(?P<groupname>","groupvalue")
     'the groupvalue'
     """
-    r=""
-    lg=len(group)
+    r = ""
+    lg = len(group)
     while expr:
-        idx=expr.find(group)
-        if idx==-1:
-            return r+expr # No more groups found
-        r+=expr[:idx]
-        expr=expr[idx+lg:]
-        level=1 # Level of parenthesis nesting
+        idx = expr.find(group)
+        if idx == -1:
+            return r + expr  # No more groups found
+        r += expr[:idx]
+        expr = expr[idx + lg:]
+        level = 1  # Level of parenthesis nesting
         while expr:
-            c=expr[0]
-            expr=expr[1:]
-            if c=="\\":
+            c = expr[0]
+            expr = expr[1:]
+            if c == "\\":
                 # Skip quoted character
-                expr=expr[1:]
+                expr = expr[1:]
                 continue
-            elif c=="(":
+            elif c == "(":
                 # Increase nesting level
-                level+=1
+                level += 1
                 continue
-            elif c==")":
+            elif c == ")":
                 # Decrease nesting level
-                level-=1
-                if level==0:
+                level -= 1
+                if level == 0:
                     # Replace with pattern and search for next
-                    r+=pattern
+                    r += pattern
                     break
-    return r+expr
+    return r + expr
+
 
 def indent(text, n=4):
     """
