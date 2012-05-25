b9ab595d7771608286373e238ebc2adb6a73cf36
Dmitry Volodin <dv@nocproject.org>
2012-05-25 13:12:48 +0400

    Huawei S5300 SNMP get_version compatibility

--- sa/profiles/Huawei/VRP/get_version.py
+++ sa/profiles/Huawei/VRP/get_version.py
@@ -1,22 +1,25 @@
 # -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2009 The NOC Project
+## Copyright (C) 2007-2012 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-"""
-"""
-import noc.sa.script
-from noc.sa.interfaces import IGetVersion
+
+## Python modules
 import re
+## NOC modules
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetVersion
 
-rx_ver = re.compile(r"^VRP.+Software, Version (?P<version>[^ ,]+),? .*?Quidway (?P<platform>(?:NetEngine\s+)?\S+)[^\n]+uptime", re.MULTILINE | re.DOTALL | re.IGNORECASE)
-rx_ver_snmp = re.compile(r"Versatile Routing Platform Software.*?Version (?P<version>[^ ,]+),? .*?Quidway (?P<platform>(?:NetEngine\s+)?[^ \t\n\r\f\v\-]+)[^\n]+", re.MULTILINE | re.DOTALL | re.IGNORECASE)
-rx_ver_snmp2 = re.compile(r"(?P<platform>(?:\S+\s+)?S\d+(?:[A-Z]+-[A-Z]+)?(?:\d+\S+)?)\s+Huawei\sVersatile\sRouting\sPlatform\sSoftware.*Version\s(?P<version>\d+\.\d+)\s\(S\d+\s(?P<image>\S+)+\).*", re.MULTILINE | re.DOTALL | re.IGNORECASE)
 
-class Script(noc.sa.script.Script):
+class Script(NOCScript):
     name = "Huawei.VRP.get_version"
     cache = True
     implements = [IGetVersion]
+
+    rx_ver = re.compile(r"^VRP.+Software, Version (?P<version>[^ ,]+),? .*?Quidway (?P<platform>(?:NetEngine\s+)?\S+)[^\n]+uptime", re.MULTILINE | re.DOTALL | re.IGNORECASE)
+    rx_ver_snmp = re.compile(r"Versatile Routing Platform Software.*?Version (?P<version>[^ ,]+),? .*?Quidway (?P<platform>(?:NetEngine\s+)?[^ \t\n\r\f\v\-]+)[^\n]+", re.MULTILINE | re.DOTALL | re.IGNORECASE)
+    rx_ver_snmp2 = re.compile(r"(?P<platform>(?:\S+\s+)?S\d+(?:[A-Z]+-[A-Z]+)?(?:\d+\S+)?)\s+Huawei\sVersatile\sRouting\sPlatform\sSoftware.*Version\s(?P<version>\d+\.\d+)\s\(S\d+\s(?P<image>\S+)+\).*", re.MULTILINE | re.DOTALL | re.IGNORECASE)
+    rx_ver_snmp3 = re.compile(r"^\s*VRP.+Software, Version (?P<version>\S+) \((?P<platform>S\S+) (?P<image>\S+)", re.MULTILINE | re.DOTALL | re.IGNORECASE)
 
     def execute(self):
         v = ""
@@ -33,21 +36,20 @@ class Script(noc.sa.script.Script):
                 v = self.cli("display version", cached=True)
             except self.CLISyntaxError:
                 raise self.NotSupportedError()
-        rx = self.find_re([rx_ver, rx_ver_snmp, rx_ver_snmp2], v)
-        for match in rx.finditer(v):
-            platform = match.group("platform")
-            # Convert NetEngine to NE
-            if platform.lower().startswith("netengine "):
-                n, p = platform.split(" ", 1)
-                platform = "NE%s" % p.strip().upper()
-            r = {
-                "vendor": "Huawei",
-                "platform": platform,
-                "version": match.group("version")
-            }
-            if self.snmp and self.access_profile.snmp_ro:
-                img = rx_ver_snmp2.search(v)
-                if img:
-                    r.update({"attributes": { }})
-                    r["attributes"].update({"image": img.group("image")})
-            return r
+        rx = self.find_re([self.rx_ver, self.rx_ver_snmp,
+                           self.rx_ver_snmp2, self.rx_ver_snmp3], v)
+        match = rx.search(v)
+        platform = match.group("platform")
+        # Convert NetEngine to NE
+        if platform.lower().startswith("netengine "):
+            n, p = platform.split(" ", 1)
+            platform = "NE%s" % p.strip().upper()
+        r = {
+            "vendor": "Huawei",
+            "platform": platform,
+            "version": match.group("version")
+        }
+        if "image" in match.groupdict():
+            image = match.group("image")
+            r["attributes"] = {"image": image}
+        return r
