f97587986e9f69796b4b1abc0fbcb29a01629f26
Dmitry Volodin <dv@nocproject.org>
2012-05-30 08:22:12 +0400

    NOC-522: sa.reportfailedscript application

---
+++ sa/apps/reportfailedscript/views.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Failed Scripts Report
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2012 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+from noc.lib.app.simplereport import SimpleReport, TableColumn
+from noc.sa.models import FailedScriptLog
+
+
+class ReportObjectsSummary(SimpleReport):
+    title = "Failed Scripts"
+
+    def get_data(self, **kwargs):
+        data = [
+            (
+                r.timestamp,
+                r.managed_object,
+                r.address,
+                r.script,
+                r.error_code,
+                r.error_text
+            ) for r in FailedScriptLog.objects.order_by("-timestamp")
+        ]
+        return self.from_dataset(
+            title=self.title,
+            columns=[
+                "Timestamp", "Managed Object", "Address",
+                "Script", "Code", "Error"
+            ],
+            data=data)
