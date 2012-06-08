b0c16b512fdcd82920c947872d5fc7d713a6c71d
Dmitry Volodin <dv@nocproject.org>
2012-06-08 21:25:07 +0400

    Log Prefix and IP discovery progress into MongoDB collection

---
+++ ip/apps/reportdiscoveredaddresses/tests/test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## ip.reportdiscoveredprefixes unittests
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2012 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+from noc.lib.test import ReportApplicationTestCase
+
+
+class reportdiscoveredprefixesTestCase(ReportApplicationTestCase):
+    pass
