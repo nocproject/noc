96b12447525bbbbc5f591414a5d8b7c36aff07ef
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2012-06-08 14:54:28 +0300

    Update Alcatel.AOS profile

--- sa/profiles/Alcatel/AOS/get_vlans.py
+++ sa/profiles/Alcatel/AOS/get_vlans.py
@@ -2,7 +2,7 @@
 ##----------------------------------------------------------------------
 ## Alcatel.AOS.get_vlans
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2009 The NOC Project
+## Copyright (C) 2007-2012 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 """
@@ -12,6 +12,8 @@ from noc.sa.interfaces import IGetVlans
 import re
 
 rx_vlan_line = re.compile(
+    r"^\s*(?P<vlan_id>\d{1,4})(\s+\S+){9}\s+o(?:n|ff)\s+(?P<name>(\S+\s*)+?)\s*$")
+rx_vlan1_line = re.compile(
     r"^\s*(?P<vlan_id>\d{1,4})(\s+\S+){9}\s+(?P<name>(\S+\s*)+?)\s*$")
 
 
@@ -25,9 +27,15 @@ class Script(noc.sa.script.Script):
         for l in vlans.split("\n"):
             match = rx_vlan_line.match(l.strip())
             if match:
-                name = match.group("name")
                 r.append({
                     "vlan_id": int(match.group("vlan_id")),
-                    "name": name
+                    "name": match.group("name")
                 })
+            else:
+                match = rx_vlan1_line.match(l.strip())
+                if match:
+                    r.append({
+                        "vlan_id": int(match.group("vlan_id")),
+                        "name": match.group("name")
+                    })
         return r
