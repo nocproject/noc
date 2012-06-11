6c8b9e06785c72f8d0d3eab1c51ffe31d5684d49
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2012-06-11 11:19:04 +0300

    Update Alcatel.AOS profile

---
+++ sa/profiles/Alcatel/AOS/get_arp.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Alcatel.AOS.get_arp
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2012 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetARP
+import re
+
+rx_line = re.compile(
+    r"^\s*(?P<ip>\d+\S+)\s+(?P<mac>\S+)\s+\S+\s+(?:P|A|V){,3}\s+(?P<interface>\d\S+)", re.MULTILINE)
+
+
+class Script(NOCScript):
+    name = "Alcatel.AOS.get_arp"
+    implements = [IGetARP]
+
+    def execute(self):
+        r = []
+        for match in rx_line.finditer(self.cli("show arp")):
+            r += [{
+                "ip": match.group("ip"),
+                "mac": match.group("mac"),
+                "interface": match.group("interface")
+            }]
+        return r
---
+++ sa/profiles/Alcatel/AOS/get_mac_address_table.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Alcatel.AOS.get_mac_address_table
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2012 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetMACAddressTable
+import re
+
+rx_line = re.compile(
+    r"^\s*VLAN\s+(?P<vlan_id>\d+)\s+(?P<mac>\S+)\s+(?P<type>\S+)\s+\S+\s+\S+\s+(?P<interfaces>\S+)", re.MULTILINE)
+
+
+class Script(NOCScript):
+    name = "Alcatel.AOS.get_mac_address_table"
+    implements = [IGetMACAddressTable]
+
+    def execute(self):
+        v = self.cli("show mac-address-table")
+        r = []
+        for match in rx_line.finditer(v):
+            mac = match.group("mac")
+            if mac == "*":
+                continue
+            r += [{
+                "vlan_id": match.group("vlan_id"),
+                "mac": mac,
+                "interfaces": [match.group("interfaces")],
+                "type": {
+                    "learned":"D",
+                    "permanent":"S"
+                }[match.group("type").lower()]
+            }]
+        return r
