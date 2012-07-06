f1fd0df423fbcbab61ee476bffd46face34b08eb
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2012-07-06 13:17:46 +0300

    Add DLink.DFL profile

---
+++ sa/profiles/DLink/DFL/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Vendor: D-Link
+## OS:     DFL
+## Compatible:
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2012 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Python modules
+import re
+## NOC modules
+from noc.sa.profiles import Profile as NOCProfile
+
+
+class Profile(NOCProfile):
+    name = "DLink.DFL"
+    supported_schemes = [NOCProfile.TELNET, NOCProfile.SSH]
+    pattern_username = "([Uu]ser ?[Nn]ame|[Ll]ogin):"
+    pattern_password = "[Pp]ass[Ww]ord:"
+    pattern_syntax_error = r"Error: Unknown command:"
+    pattern_prompt = r"^(?P<hostname>\S+(:\S+)*):/> "
+    command_more = "a"
+    command_exit = "logout"
+    config_volatile = ["^%.*?$"]
---
+++ sa/profiles/DLink/DFL/get_arp.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## DLink.DFL.get_arp
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2012 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetARP
+import re
+
+
+class Script(NOCScript):
+    name = "DLink.DFL.get_arp"
+    implements = [IGetARP]
+    rx_iface = re.compile(r"^ARP cache of iface (?P<interface>\S+)")
+    rx_line = re.compile(r"^\s+\S+\s+(?P<ip>[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\s+=\s+(?P<mac>\S+)\s+Expire=\d+$", re.MULTILINE)
+
+    def execute(self):
+        r = []
+        iface = ""
+        for l in self.cli("arp").splitlines():
+            match_iface = self.rx_iface.match(l)
+            if match_iface:
+                iface = match_iface.group("interface")
+            match_line = self.rx_line.match(l)
+            if match_line:
+                ip = match_line.group("ip")
+                mac = match_line.group("mac")
+                if iface:
+                    r += [{"ip": ip, "mac": mac, "interface": iface}]
+        return r
