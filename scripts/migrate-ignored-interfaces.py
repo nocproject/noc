ba4be4ef04eedd1fbbc737e69d13a1062848c37a
Dmitry Volodin <dv@nocproject.org>
2012-07-20 15:56:47 +0400

    migrate-ignored-interfaces.py script

---
+++ scripts/migrate-ignored-interfaces.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Apply ignored_interfaces to given interface profile
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2012 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Bootstrap
+import set_env
+set_env.setup(use_django=True)
+
+from noc.inv.models import Interface, InterfaceProfile
+from noc.sa.models import ManagedObjectAttribute
+
+def migrate(profile):
+    print "Setting profile %s" % profile.name
+    for a in ManagedObjectAttribute.objects.filter(key="ignored_interfaces"):
+        mo = a.managed_object
+        ignored = []
+        for iface in Interface.objects.filter(managed_object=mo.id):
+            if mo.is_ignored_interface(iface.name):
+                iface.profile = profile
+                iface.save()
+                ignored += [iface.name]
+        if ignored:
+            print "%s: %s" % (
+                mo.name, ", ".join(ignored)
+            )
+            a.delete()
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) != 2:
+        print "Usage"
+        print "%s <interface profile name>" % sys.argv[0]
+        sys.exit(1)
+
+    profile = InterfaceProfile.objects.filter(name=sys.argv[1]).first()
+    if not profile:
+        print "Invalid interface profile. Existing profiles are:"
+        for profile in InterfaceProfile.objects.all():
+            print profile
+
+    migrate(profile)
