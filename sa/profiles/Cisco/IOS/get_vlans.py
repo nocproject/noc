86d22339054996201f0874ff0a58b4f0a9a9ff9c
roshin <none@none>
2012-08-07 18:52:05 +0400

    bugfix

--- sa/profiles/Cisco/IOS/get_vlans.py
+++ sa/profiles/Cisco/IOS/get_vlans.py
@@ -50,7 +50,8 @@ class Script(NOCScript):
     ##
     rx_vlan_dot1q = re.compile(r"^Total statistics for 802.1Q VLAN (?P<vlan_id>\d{1,4}):", re.MULTILINE)
 
-    @NOCScript.match(platform__regex=r"^(C?[123][789]\d\d|72\d\d|ASR)")
+    @NOCScript.match(platform__regex=r"^([123][78]\d\d|72\d\d|C[23]900|ASR\d+)")
+
     def execute_vlan_switch(self):
         try:
             vlans = self.cli("show vlan-switch")
