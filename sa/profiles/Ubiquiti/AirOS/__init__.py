2d278aa498a66e34cfebd590cce8b8e5378bd28c
Dmitry Volodin <dv@nocproject.org>
2012-08-10 21:47:43 +0400

    Ubiquity.AirOS: Adaprive pattern_prompt

--- sa/profiles/Ubiquiti/AirOS/__init__.py
+++ sa/profiles/Ubiquiti/AirOS/__init__.py
@@ -18,6 +18,6 @@ class Profile(noc.sa.profiles.Profile):
     pattern_username = "([Uu][Bb][Nn][Tt] login|[Ll]ogin):"
     pattern_password = "[Pp]assword:"
     pattern_more = "CTRL\+C.+?a All"
-    pattern_prompt = r"^\S+?#"
+    pattern_prompt = r"^\S+?\.v(?P<version>\S+)#"
     command_more = "a"
     config_volatile = ["^%.*?$"]
