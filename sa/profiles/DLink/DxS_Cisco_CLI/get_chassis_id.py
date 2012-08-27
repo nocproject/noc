880ca457aec34c18cc662fa8a480150e0f4d9614
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2012-08-27 10:03:44 +0300

    NOC-640 Add DLink.DxS_Cisco_CLI.get_chassis_id script

---
+++ sa/profiles/DLink/DxS_Cisco_CLI/get_chassis_id.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## DLink.DxS_Cisco_CLI.get_chassis_id
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2012 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetChassisID
+import re
+
+
+class Script(NOCScript):
+    name = "DLink.DxS_Cisco_CLI.get_chassis_id"
+    cache = True
+    rx_ver = re.compile(r"^Hardware is  VLAN, address is (?P<id>\S+)\s+",
+        re.IGNORECASE | re.MULTILINE)
+    implements = [IGetChassisID]
+
+    def execute(self):
+        match = self.re_search(self.rx_ver, self.cli("show interfaces vlan 1",
+            cached=True))
+        return match.group("id")
