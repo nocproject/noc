2ab127fa4d14a004e5289db40aa063c3a1bbf6b9
Dmitry Volodin <dv@nocproject.org>
2012-08-28 17:26:46 +0400

    NOC-625: NOC.peer.as.Lookup/LookupField

---
+++ peer/apps/as/js/LookupField.js
@@ -0,0 +1,12 @@
+//---------------------------------------------------------------------
+// NOC.peer.as.Lookup
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2012 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.peer.as.LookupField");
+
+Ext.define("NOC.peer.as.LookupField", {
+    extend: "NOC.core.LookupField",
+    requires: ["NOC.peer.as.Lookup"]
+});
