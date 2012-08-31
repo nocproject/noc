daede4d4c14bed1a9746137d45aae5ed10d4038c
Dmitry Volodin <dv@nocproject.org>
2012-09-01 00:19:25 +0400

    AlarmJob base class

---
+++ fm/correlator/jobs/base.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## AlarmJob base class
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2012 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+from noc.lib.scheduler.intervaljob import IntervalJob
+from noc.fm.models import ActiveAlarm
+
+
+class AlarmJob(IntervalJob):
+    model = ActiveAlarm  # self.object is an ActiveAlarm instance
+
+    def __init__(self, *args, **kwargs):
+        super(AlarmJob, self).__init__(*args, **kwargs)
+        self.cleared = False
+
+    def get_managed_object(self):
+        return self.object.managed_object
+
+    def clear_alarm(self, message):
+        self.info("Clearing alarm %s: %s" % (self.object.id, message))
+        self.object.clear_alarm(message)
+        self.cleared = True
+
+    def get_schedule(self, status):
+        if self.cleared:
+            return None  # Remove schedule
+        else:
+            return super(AlarmJob, self).get_schedule(status)
