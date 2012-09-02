8576a9c86b7b0723a293290b212893a800664795
Dmitry Volodin <dv@nocproject.org>
2012-09-02 15:12:29 +0400

    NOC-469: Paging in managed object lookup field

--- sa/apps/managedobject/js/LookupField.js
+++ sa/apps/managedobject/js/LookupField.js
@@ -9,5 +9,7 @@ console.debug("Defining NOC.sa.managedobject.LookupField");
 Ext.define("NOC.sa.managedobject.LookupField", {
     extend: "NOC.core.LookupField",
     alias: "widget.sa.managedobject.LookupField",
-    requires: ["NOC.sa.managedobject.Lookup"]
+    requires: ["NOC.sa.managedobject.Lookup"],
+    width: 260,
+    pageSize: true
 });
