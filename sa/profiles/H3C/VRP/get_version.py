fa508fdf681d22bca74f0d7da95afa3aa390419c
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2012-09-14 10:38:51 +0300

    Update H3C.VRP.get_version script

--- sa/profiles/H3C/VRP/get_version.py
+++ sa/profiles/H3C/VRP/get_version.py
@@ -2,7 +2,7 @@
 ##----------------------------------------------------------------------
 ## H3C.VRP.get_version
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2011 The NOC Project
+## Copyright (C) 2007-2012 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 """
@@ -13,7 +13,6 @@ import re
 from noc.sa.script import Script as NOCScript
 from noc.sa.interfaces import IGetVersion
 
-rx_ver = re.compile(r"^.*?Switch\s(?P<platform>.+?)\sSoftware\s\Version\s3Com\sOS\sV(?P<version>.+?)$", re.MULTILINE | re.DOTALL | re.IGNORECASE)
 
 
 class Script(NOCScript):
@@ -21,12 +20,32 @@ class Script(NOCScript):
     cache = True
     implements = [IGetVersion]
 
+
+    rx_ver = re.compile(r"^.*?Switch\s(?P<platform>.+?)\sSoftware\s\Version"
+        r"\s3Com\sOS\sV(?P<version>.+?)$",
+        re.MULTILINE | re.DOTALL | re.IGNORECASE)
+    rx_ver1 = re.compile(r"^Comware\sSoftware,\s\Version\s(?P<version>.+?),.+"
+        r"^H3C (?P<platform>\S+) uptime is",
+        re.MULTILINE | re.DOTALL | re.IGNORECASE)
+    rx_hw = re.compile(r"Hardware Version is (?P<hardware>\S+)")
+    rx_boot = re.compile(r"Bootrom Version is (?P<bootprom>\S+)")
+
     def execute(self):
         v = self.cli("display version")
-        match = rx_ver.search(v)
-        platform = match.group("platform")
-        return {
+        match = self.rx_ver.search(v)
+        if not match:
+            match = self.rx_ver1.search(v)
+        r = {
             "vendor": "H3C",
-            "platform": platform,
+            "platform": match.group("platform"),
             "version": match.group("version")
         }
+        hw = self.rx_hw.search(v)
+        boot = self.rx_boot.search(v)
+        if hw or boot:
+            r.update({"attributes": {}})
+            if hw:
+                r["attributes"].update({"HW version": hw.group("hardware")})
+            if boot:
+                r["attributes"].update({"Boot PROM": boot.group("bootprom")})
+        return r
