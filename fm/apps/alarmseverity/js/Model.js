0a03a2817a7acbffe060b5abc2e0a03e43b85331
Dmitry Volodin <dv@nocproject.org>
2012-09-21 09:56:14 +0400

    NOC-706: Missed js files

---
+++ fm/apps/alarmseverity/js/Model.js
@@ -0,0 +1,39 @@
+//---------------------------------------------------------------------
+// fm.alarmseverity Model
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2012 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.fm.alarmseverity.Model");
+
+Ext.define("NOC.fm.alarmseverity.Model", {
+    extend: "Ext.data.Model",
+    rest_url: "/fm/alarmseverity/",
+
+    fields: [
+        {
+            name: "id",
+            type: "string"
+        },
+        {
+            name: "style",
+            type: "int"
+        },
+        {
+            name: "description",
+            type: "string"
+        },
+        {
+            name: "severity",
+            type: "int"
+        },
+        {
+            name: "is_builtin",
+            type: "boolean"
+        },
+        {
+            name: "name",
+            type: "string"
+        }
+    ]
+});
