80a615ba166716ccbb8458b881ab03e32ba0d619
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2012-09-24 12:27:26 +0300

    Fix H3C.VRP.get_version script

--- sa/profiles/H3C/VRP/get_version.py
+++ sa/profiles/H3C/VRP/get_version.py
@@ -14,18 +14,16 @@ from noc.sa.script import Script as NOCScript
 from noc.sa.interfaces import IGetVersion
 
 
-
 class Script(NOCScript):
     name = "H3C.VRP.get_version"
     cache = True
     implements = [IGetVersion]
 
-
     rx_ver = re.compile(r"^.*?Switch\s(?P<platform>.+?)\sSoftware\s\Version"
         r"\s3Com\sOS\sV(?P<version>.+?)$",
         re.MULTILINE | re.DOTALL | re.IGNORECASE)
-    rx_ver1 = re.compile(r"^Comware\sSoftware,\s\Version\s(?P<version>.+?),.+"
-        r"^(H3C )?(?P<platform>\S+) uptime is",
+    rx_ver1 = re.compile(r"^Comware\sSoftware,\s\Version\s(?P<version>.+?),"
+        r"\sRelease\s(?P<release>.+?)$.+?^(H3C )?(?P<platform>\S+) uptime is",
         re.MULTILINE | re.DOTALL | re.IGNORECASE)
     rx_hw = re.compile(r"Hardware Version is (?P<hardware>\S+)")
     rx_boot = re.compile(r"Bootrom Version is (?P<bootprom>\S+)")
@@ -35,11 +33,18 @@ class Script(NOCScript):
         match = self.rx_ver.search(v)
         if not match:
             match = self.rx_ver1.search(v)
-        r = {
-            "vendor": "H3C",
-            "platform": match.group("platform"),
-            "version": match.group("version")
-        }
+            r = {
+                "vendor": "H3C",
+                "platform": match.group("platform"),
+                "version": match.group("version") + "." + \
+                    match.group("release")
+            }
+        else:
+            r = {
+                "vendor": "H3C",
+                "platform": match.group("platform"),
+                "version": match.group("version")
+            }
         hw = self.rx_hw.search(v)
         boot = self.rx_boot.search(v)
         if hw or boot:
