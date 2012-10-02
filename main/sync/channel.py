ac78e5f9e541b2fe66fade44e702d9f1d470d06b
Dmitry Volodin <dv@nocproject.org>
2012-10-02 11:36:48 +0400

    NOC-728: noc-sync daemon and scheduler support

---
+++ main/sync/channel.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Sync channel
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2012 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Python modules
+import logging
+
+
+class Channel(object):
+    def __init__(self, daemon, channel, name, config):
+        self.daemon = daemon
+        self.channel = channel
+        self.name = name
+        self.root = "/queue/sync/%s/" % "/".join(self.channel.split("/")[:-1])
+
+    def die(self, msg):
+        self.daemon.die("[%s] %s" % (self.channel, msg))
+
+    def info(self, msg):
+        logging.info("[%s] %s" % (self.channel ,msg))
+
+    def init(self):
+        self.daemon.subscribe(self.root + self.name + "/",
+            self.on_msg)
+        self.list()
+
+    def send(self, msg, destination):
+        return self.daemon.send(msg, destination)
+
+    def list(self):
+        """
+        Request LIST command
+        :return:
+        """
+        self.send({
+            "cmd": "list",
+            "channel": self.name,
+        }, self.root)
+
+    def verify(self, objects):
+        self.send({
+            "cmd": "verify",
+            "channel": self.name,
+            "objects": list(objects)
+        }, self.root)
+
+    def on_msg(self, destination, msg):
+        cmd = msg.get("cmd")
+        if not cmd:
+            return
+        if cmd == "list":
+            if "items" in msg:
+                self.on_list(msg["items"])
+        elif cmd == "verify":
+            if "object" in msg and "data" in msg:
+                self.on_verify(msg["object"], msg["data"])
+        elif cmd == "request":
+            if "request" in msg:
+                if msg["request"] == "list":
+                    self.list()
+                elif msg["request"] == "verify" and "object" in msg:
+                    self.verify([msg["object"]])
+
+    def on_list(self, items):
+        """
+        :param items: Dict of name -> version
+        :return:
+        """
+        pass
+
+    def on_verify(self, object, data):
+        pass
---
+++ main/sync/daemon.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## noc-sync daemon
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2012 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Python modules
+import logging
+import threading
+## NOC modules
+from noc.lib.daemon import Daemon
+from noc.lib.stomp.threadclient import ThreadedSTOMPClient
+from noc.lib.modutils import load_subclasses
+from channel import Channel
+
+
+class SyncDaemon(Daemon):
+    daemon_name = "noc-sync"
+
+    def __init__(self):
+        self.channels = {}  # name -> Channel instance
+        super(SyncDaemon, self).__init__()
+        logging.info("Running noc-sync")
+
+    def load_config(self):
+        super(SyncDaemon, self).load_config()
+        # STOMP settings
+        self.stomp_host = self.config.get("stomp", "host")
+        self.stomp_port = self.config.getint("stomp", "port")
+        self.stomp_client_id = self.config.get("stomp", "client_id")
+        # Load channels
+        for ch in self.config.sections():
+            if "/" not in ch:
+                continue
+            self.load_channel(ch)
+
+    def load_channel(self, channel):
+        if not self.config.getboolean(channel, "enabled"):
+            logging.info("Skipping disabled channel %s" % channel)
+            return
+        parts = channel.split("/")
+        ch_name = parts.pop(-1)
+        c_type = self.config.get(channel, "type", None)
+        if c_type:
+            parts += c_type.split("/")
+        mn = "noc.main.sync.channels.%s" % ".".join(parts)
+        c = load_subclasses(mn, Channel)
+        if not c or len(c) != 1:
+            self.die("Unable to load channel %s" % channel)
+        config = {}
+        for opt in self.config.options(channel):
+            config[opt] = self.config.get(channel, opt)
+        self.channels[channel] = c[0](self, channel, ch_name, config)
+
+    def run(self):
+        self.stomp_client = ThreadedSTOMPClient(
+            self.stomp_host, self.stomp_port,
+            client_id=self.stomp_client_id)
+        self.stomp_client.start()
+        for c in self.channels:
+            self.channels[c].init()
+        import time
+        time.sleep(1000)
+
+    def send(self, message, destination,
+             receipt=False, persistent=False, expires=None):
+        self.stomp_client.send(message, destination,
+            receipt=receipt, persistent=persistent, expires=expires)
+
+    def subscribe(self, destination, callback):
+        self.stomp_client.subscribe(destination, callback)
