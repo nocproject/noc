fcbf4aa42ab74b09b9f3843242a9485d41428ed1
Dmitry Volodin <dv@nocproject.org>
2012-10-02 11:24:09 +0400

    STOMP subscribers in noc-scheduler

--- main/scheduler/daemon.py
+++ main/scheduler/daemon.py
@@ -21,21 +21,29 @@ class SchedulerDaemon(Daemon):
     daemon_name = "noc-scheduler"
 
     def __init__(self):
+        self.stomp_host = None
+        self.stomp_port = None
+        self.stomp_client_id = None
         super(SchedulerDaemon, self).__init__()
         logging.info("Running noc-scheduler")
         self.periodic_thread = None
         self.scheduler = None
         self.stomp_client = None
 
+    def load_config(self):
+        super(SchedulerDaemon, self).load_config()
+        self.stomp_host = self.config.get("stomp", "host")
+        self.stomp_port = self.config.getint("stomp", "port")
+        self.stomp_client_id = self.config.get("stomp", "client_id")
+
     def run(self):
+        self.stomp_client = ThreadedSTOMPClient(
+            self.stomp_host, self.stomp_port,
+            client_id=self.stomp_client_id)
+        self.stomp_client.start()
         self.scheduler = JobScheduler(self)
         self.periodic_thread = PeriodicScheduler(self)
         self.periodic_thread.start()
-        self.stomp_client = ThreadedSTOMPClient(
-            "127.0.0.1", 19705,
-            client_id="noc-scheduler"
-        )
-        self.stomp_client.start()
         self.scheduler.run()
 
     def send(self, message, destination,
--- main/scheduler/scheduler.py
+++ main/scheduler/scheduler.py
@@ -8,8 +8,10 @@
 
 ## Python modules
 import os
+from collections import defaultdict
 ## NOC modules
 from noc.lib.scheduler.scheduler import Scheduler
+from jobs.subscriber import SubscriberJob
 from noc.settings import INSTALLED_APPS
 
 
@@ -21,6 +23,8 @@ class JobScheduler(Scheduler):
             initial_submit=daemon is not None,
             reset_running=daemon is not None
         )
+        # Subscribers
+        self.subscribers = defaultdict(list)  # Destination -> [jobs]
         # Install application jobs
         for app in INSTALLED_APPS:
             if not app.startswith("noc."):
@@ -29,3 +33,28 @@ class JobScheduler(Scheduler):
             pp = os.path.join(*p)
             if os.path.isdir(pp):
                 self.register_all(pp)
+
+    def register_job_class(self, cls):
+        if issubclass(cls, SubscriberJob):
+            job = cls(self)
+            dst = job.get_destination()
+            self.info("Subscribing job class %s to %s" % (
+                job.name, dst))
+            self.subscribers[dst] += [job]
+            self.daemon.stomp_client.subscribe(dst, self.on_msg)
+        else:
+            return super(JobScheduler, self).register_job_class(cls)
+
+    def on_msg(self, destination, body):
+        """
+        Respond to STOMP messages
+        :param destination:
+        :param body:
+        :return:
+        """
+        self.debug("Receiving STOMP message to destination %s: %r" % (
+            destination, body))
+        # @todo: threaded
+        if destination in self.subscribers:
+            for job in self.subscribers[destination]:
+                job.handler(destination, body)
