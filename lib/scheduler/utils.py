2d5afdf8709b436c4e972d815ae341866c2dd241
Dmitry Volodin <dv@nocproject.org>
2012-10-08 13:38:57 +0400

    Delay zone provisioning to reduce system load

--- lib/scheduler/utils.py
+++ lib/scheduler/utils.py
@@ -53,6 +53,46 @@ def submit_job(scheduler_name, job_class, key=None,
     })
 
 
+def sliding_job(scheduler_name, job_class, key=None,
+               ts=None, delta=None, data=None, cutoff_delta=0):
+    #
+    if ts is None:
+        ts = datetime.datetime.now()
+        if delta:
+            ts += datetime.timedelta(seconds=delta)
+    # Check the job exists
+    now = datetime.datetime.now()
+    c = get_db()["noc.schedules.%s" % scheduler_name]
+    j = c.find_one({
+        Scheduler.ATTR_CLASS: job_class,
+        Scheduler.ATTR_KEY: key
+    })
+    if j:
+        cutoff = j[Scheduler.ATTR_SCHEDULE]["cutoff"]
+        if ts <= cutoff:
+            # Slide job
+            c.update({
+                "_id": j["_id"]
+            }, {
+                "$set": {
+                    Scheduler.ATTR_TS: ts
+                }
+            })
+    else:
+        # Submit job
+        cutoff = now + datetime.timedelta(seconds=cutoff_delta)
+        c.insert({
+            Scheduler.ATTR_CLASS: job_class,
+            Scheduler.ATTR_KEY: key,
+            Scheduler.ATTR_STATUS: Scheduler.S_WAIT,
+            Scheduler.ATTR_TS: ts,
+            Scheduler.ATTR_DATA: data,
+            Scheduler.ATTR_SCHEDULE: {
+                "cutoff": cutoff
+            }
+        })
+
+
 def sync_request(channels, request, object=None, delta=None):
     if not channels:
         return
