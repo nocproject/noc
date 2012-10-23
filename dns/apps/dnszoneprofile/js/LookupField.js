8052d82f5dbfca521d7761d4b9dd70d385e44ac2
Dmitry Volodin <dv@nocproject.org>
2012-10-23 09:46:08 +0400

    dns.dnszoneprofile: Lookup fields

---
+++ dns/apps/dnszoneprofile/js/LookupField.js
@@ -0,0 +1,13 @@
+//---------------------------------------------------------------------
+// NOC.dns.dnszone.Lookup
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2012 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.dns.dnszoneprofile.LookupField");
+
+Ext.define("NOC.dns.dnszoneprofile.LookupField", {
+    extend: "NOC.core.LookupField",
+    alias: "widget.dns.dnszoneprofile.LookupField",
+    requires: ["NOC.dns.dnszoneprofile.Lookup"]
+});
