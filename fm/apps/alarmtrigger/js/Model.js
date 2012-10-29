da8d58d0e49ac10d7c690a28dc2bfc2947d2c716
Dmitry Volodin <dv@nocproject.org>
2012-10-29 17:28:14 +0400

    NOC-797: fm.alarmtrigger has been rewritten as Ext Application

---
+++ fm/apps/alarmtrigger/js/Model.js
@@ -0,0 +1,82 @@
+//---------------------------------------------------------------------
+// fm.alarmtrigger Model
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2012 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.fm.alarmtrigger.Model");
+
+Ext.define("NOC.fm.alarmtrigger.Model", {
+    extend: "Ext.data.Model",
+    rest_url: "/fm/alarmtrigger/",
+
+    fields: [
+        {
+            name: "id",
+            type: "int"
+        },
+        {
+            name: "name",
+            type: "string"
+        },
+        {
+            name: "is_enabled",
+            type: "boolean",
+            defaultValue: true
+        },
+        {
+            name: "alarm_class_re",
+            type: "string"
+        },
+        {
+            name: "condition",
+            type: "string",
+            defaultValue: "True"
+        },
+        {
+            name: "time_pattern",
+            type: "int"
+        },
+        {
+            name: "time_pattern__label",
+            type: "string",
+            persist: false
+        },
+        {
+            name: "selector",
+            type: "int"
+        },
+        {
+            name: "selector__label",
+            type: "string",
+            persist: false
+        },
+        {
+            name: "notification_group",
+            type: "int"
+        },
+        {
+            name: "notification_group__label",
+            type: "string",
+            persist: false
+        },
+        {
+            name: "template",
+            type: "int"
+        },
+        {
+            name: "template__label",
+            type: "string",
+            persist: false
+        },
+        {
+            name: "pyrule",
+            type: "int"
+        },
+        {
+            name: "pyrule__label",
+            type: "string",
+            persist: false
+        }
+    ]
+});
