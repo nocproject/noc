d2939384bca6274829ca0b484c9ff8a49fa513d4
Dmitry Volodin <dv@nocproject.org>
2012-10-29 17:28:14 +0400

    NOC-797: fm.alarmtrigger has been rewritten as Ext Application

--- fm/apps/alarmtrigger/views.py
+++ fm/apps/alarmtrigger/views.py
@@ -1,26 +1,20 @@
 # -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
-## AlarmTrigger Manager
+## fm.alarmtrigger application
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2011 The NOC Project
+## Copyright (C) 2007-2012 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 
-## Django modules
-from django.contrib import admin
 ## NOC modules
-from noc.lib.app import ModelApplication
+from noc.lib.app import ExtModelApplication, view
 from noc.fm.models import AlarmTrigger
 
 
-class AlarmTriggerAdmin(admin.ModelAdmin):
-    list_display = ["name", "is_enabled", "alarm_class_re", "condition",
-            "time_pattern", "selector", "notification_group",
-            "template", "pyrule"]
-    list_filter = ["is_enabled"]
-
-
-class AlarmTriggerApplication(ModelApplication):
-    model = AlarmTrigger
-    model_admin = AlarmTriggerAdmin
+class AlarmTriggerApplication(ExtModelApplication):
+    """
+    AlarmTrigger application
+    """
+    title = "Alarm Triggers"
     menu = "Setup | Alarm Triggers"
+    model = AlarmTrigger
