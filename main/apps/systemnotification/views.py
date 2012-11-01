1f569a97d10b09bbd1a4db9fb95c9bfd5ba16730
Alexander Maksimkin <6a6axster@gmail.com>
2012-11-01 22:52:30 +0400

    NOC-815: main.systemnotification rewritted as Ext Application

--- main/apps/systemnotification/views.py
+++ main/apps/systemnotification/views.py
@@ -1,22 +1,21 @@
 # -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
-## SystemNotification Manager
+## main.systemnotification application
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2010 The NOC Project
+## Copyright (C) 2007-2012 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-from django.contrib import admin
-from noc.lib.app import ModelApplication
+
+## NOC modules
+from noc.lib.app import ExtModelApplication, view
 from noc.main.models import SystemNotification
-##
-## System Notification Admin
-##
-class SystemNotificationAdmin(admin.ModelAdmin):
-    list_display=["name","notification_group"]
-##
-## SystemNotification application
-##
-class SystemNotificationApplication(ModelApplication):
-    model=SystemNotification
-    model_admin=SystemNotificationAdmin
-    menu="Setup | System Notifications"
+
+class SystemNotificationApplication(ExtModelApplication):
+    """
+    SystemNotification application
+    """
+    title = "System Notifications"
+    menu = "Setup | System Notifications"
+    model = SystemNotification
+    query_fields = ["name__icontains"]
+
