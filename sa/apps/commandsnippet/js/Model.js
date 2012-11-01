72057764679b5bc0f6ec510fccac562361d3afb5
Alexander Maksimkin <6a6axster@gmail.com>
2012-11-01 13:48:33 +0400

    NOC-801: sa.commandsnippet have rewritten as Ext Application

---
+++ sa/apps/commandsnippet/js/Model.js
@@ -0,0 +1,77 @@
+//---------------------------------------------------------------------
+// sa.commandsnippet Model
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2011 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.sa.commandsnippet.Model");
+
+Ext.define("NOC.sa.commandsnippet.Model", {
+    extend: "Ext.data.Model",
+    rest_url: "/sa/commandsnippet/",
+
+    fields: [
+        {
+            name: "id",
+            type: "string"
+        },
+        {
+            name: "selector__label",
+            type: "string"
+        },
+        {
+            name: "name",
+            type: "string"
+        },
+        {
+            name: "description",
+            type: "string"
+        },
+        {
+            name: "snippet",
+            type: "string"
+        },
+        {
+            name: "change_configuration",
+            type: "boolean",
+            defaultValue: false
+        },
+        {
+            name: "selector",    
+            type: "string"
+        },
+        {
+            name: "is_enabled", 
+            type: "boolean",
+            defaultValue: false
+        },
+        {
+            name: "timeout",
+            defaultValue: "60",
+            type: "int"
+        },
+        {
+            name: "require_confirmation",
+            type: "boolean",
+            defaultValue: false
+        },
+        {
+            name: "ignore_cli_errors",
+            type: "boolean",
+            defaultValue: false
+        },
+        {
+            name: "permission_name",
+            type: "string"
+        },
+        {
+            name: "display_in_menu",
+            type: "boolean",
+            defaultValue: false
+        },
+        {
+            name: "tags",
+            type: "string"
+        }
+    ]
+});
--- sa/apps/commandsnippet/views.py
+++ sa/apps/commandsnippet/views.py
@@ -1,55 +1,24 @@
 # -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
-## CommandSnippet Manager
+## main.commandsnippet application
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2011 The NOC Project
+## Copyright (C) 2007-2012 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 
-## Django modules
-from django.contrib import admin
-from django import forms
-from django.template import Template, TemplateSyntaxError
 ## NOC modules
-from noc.lib.app import ModelApplication
+from noc.lib.app import ExtModelApplication, view
 from noc.sa.models import CommandSnippet
 
 
-def permission(o):
-    p = o.effective_permission_name
-    if p:
-        return p
-    else:
-        return ""
-permission.short_description = "Permission"
-
-
-class CommandSnippetForm(forms.ModelForm):
-    """Validation form"""
-    class Meta:
-        model=CommandSnippet
-    
-    def clean_snippet(self):
-        s = self.cleaned_data["snippet"]
-        try:
-            Template(s)
-        except TemplateSyntaxError, why:
-            raise forms.ValidationError("Template syntax error: %s" % why)
-        return s
-    
-
-class CommandSnippetAdmin(admin.ModelAdmin):
-    """CommandSnippet Admin"""
-    form = CommandSnippetForm
-    list_display = ["name", "is_enabled", "selector", "description",
-            "require_confirmation", "ignore_cli_errors", permission,
-            "display_in_menu"]
-    list_filter = ["require_confirmation", "ignore_cli_errors",
-                   "is_enabled", "permission_name"]
-
-
-class CommandSnippetApplication(ModelApplication):
-    """CommandSnippet application"""
-    model = CommandSnippet
-    model_admin = CommandSnippetAdmin
+class CommandSnippetApplication(ExtModelApplication):
+    """
+    commandsnippet application
+    """
+    title = "Command Snippets"
     menu = "Setup | Command Snippets"
+    model = CommandSnippet
+    query_fields = ["name__icontains"]
+
+# @todo: syntax checking of snippet
+
