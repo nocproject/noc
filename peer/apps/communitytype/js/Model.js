b548d083f75e3a3f0f07f69e4a56041b923638f6
Alexander Maksimkin <6a6axster@gmail.com>
2012-11-13 17:53:33 +0400

    NOC-840: peer.communitytype has rewritted as Ext Application

---
+++ peer/apps/communitytype/js/Model.js
@@ -0,0 +1,23 @@
+//---------------------------------------------------------------------
+// peer.communitytype Model
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2011 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.peer.communitytype.Model");
+
+Ext.define("NOC.peer.communitytype.Model", {
+    extend: "Ext.data.Model",
+    rest_url: "/peer/communitytype/",
+
+    fields: [
+        {
+            name: "id",
+            type: "string"
+        },
+        {
+            name: "name",
+            type: "string"
+        }
+    ]
+});
