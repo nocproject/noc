e57e4333815cda81754b0874992abcb02e9aa0ca
Alexander Maksimkin <6a6axster@gmail.com>
2012-11-20 22:48:35 +0400

    NOC-849: peer.asset has rewrited as Ext Application. Some Vtypes validator added. ColumnWrap renderer added.

--- peer/apps/asset/tests/test.py
+++ peer/apps/asset/tests/test.py
@@ -1,12 +1,29 @@
 # -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
-## asset Test
+## peer.asset unittest
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2009 The NOC Project
+## Copyright (C) 2007-2012 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-from noc.lib.test import ModelApplicationTestCase
-from django.utils import simplejson as json
 
-class assetTestCase(ModelApplicationTestCase):
-    pass
+## NOC modules
+from noc.lib.test import RestModelTestCase, unittest
+
+@unittest.skip("Not ready")
+class ASSetTestCase(RestModelTestCase):
+    app = "peer.asset"
+
+    scenario = [
+        {
+            "GET": {
+                # key: value
+            },
+            "POST": {
+                # key: value
+            },
+            "PUT": {
+                # key: value
+            }
+        }
+    ]
+
