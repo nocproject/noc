8856fb372c919d3c6ff8aa5103df080216030fab
Alexander Maksimkin <6a6axster@gmail.com>
2012-11-20 15:05:26 +0400

    Add a profile Cisco.SCOS

---
+++ sa/profiles/Cisco/SCOS/__init__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Vendor: Cisco
+## OS:     SCOS
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2012 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Python modules
+import re
+## NOC modules
+from noc.sa.profiles import Profile as NOCProfile
+
+
+class Profile(NOCProfile):
+    name = "Cisco.SCOS"
+    supported_schemes = [NOCProfile.TELNET, NOCProfile.SSH]
+    pattern_more = [
+        (r"--More--", " "),
+        (r"\?\s*\[confirm\]", "\n")
+    ]
+    pattern_unpriveleged_prompt = r"^\S+?>"
+    pattern_syntax_error = r"% invalid input |% Ambiguous command:|% Incomplete command."
+#    command_disable_pager = "terminal length 0"
+    command_super = "enable"
+    command_enter_config = "configure"
+    command_leave_config = "exit"
+    command_save_config = "copy running-config startup-config\n"
+    pattern_prompt = r"^(?P<hostname>[a-zA-Z0-9]\S*?)(?:-\d+)?(?:\(config[^\)]*\))?#"
+    requires_netmask_conversion = True
+    convert_mac = NOCProfile.convert_mac_to_cisco
+
---
+++ sa/profiles/Cisco/SCOS/get_version.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Cisco.SCOS.get_version
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+## Python modules
+import re
+## NOC modules
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetVersion
+
+
+class Script(NOCScript):
+    name = "Cisco.SCOS.get_version"
+    cache = True
+    implements = [IGetVersion]
+
+    rx_version = re.compile(r"^System version: Version (?P<version>[^\n]+)",
+        re.MULTILINE | re.DOTALL)
+    rx_platform = re.compile(r"((Product ID\s+:\s+)|(Platform:\s+))(?P<platform>\S+)",
+        re.MULTILINE | re.DOTALL)
+    rx_prod_id = re.compile(r"Product S\/N\s+: (?P<prod_id>\S+)",
+        re.MULTILINE | re.DOTALL)
+    rx_chass_sn = re.compile(r"SCE8000 Chassis\n(.+\n)serial-num\s+:\s+(?P<chass_sn>\S+)",
+        re.MULTILINE | re.DOTALL)
+    
+
+    def execute(self):
+        v = self.cli("show version", cached=True)
+        match = self.rx_version.search(v)
+        if match:    
+            version = match.group("version")
+        match = self.rx_platform.search(v)
+        if match:
+            platform = match.group("platform")
+        match = self.rx_prod_id.search(v)
+        r = {
+            "vendor": "Cisco",
+            "platform": platform,
+            "version": version,
+            "attributes": {}
+            }
+        match = self.rx_prod_id.search(v)
+        if match:
+            r["attributes"].update({"product_id": match.group("prod_id")})
+        match = self.rx_chass_sn.search(v)
+        if match:
+            r["attributes"].update({"chassis_sn": match.group("chass_sn")})
+        return r
+
