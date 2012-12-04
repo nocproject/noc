c72b624cfe2604f67060b4c2db9461d336676797
Dmitry Volodin <dv@nocproject.org>
2012-12-04 16:39:12 +0400

    PEP8 formatting

--- lib/rpsl.py
+++ lib/rpsl.py
@@ -2,24 +2,25 @@
 ##----------------------------------------------------------------------
 ## Routing Policy Specification Language (RFC2622) routines
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2009 The NOC Project
+## Copyright (C) 2007-2012 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 
-## rpsl_format(rpsl)
-## idents RPSL by aligning left side of values
-## Emply strings and strings without ':' are silently dropped
-RPLS_IDENT=20
-def rpsl_format(rpsl,ident=None):
+RPLS_IDENT = 20
+
+
+def rpsl_format(rpsl, ident=None):
     """
+    Idents RPSL by aligning left side of values
+    Emply strings and strings without ':' are silently dropped
     >>> rpsl_format("key1: value1\\nkey2: value2\\n\\nto drop\\n",7)
     'key1:  value1\\nkey2:  value2\\n'
     """
     if ident is None:
-        ident=RPLS_IDENT
-    out=[]
-    mask="%%-%ds%%s"%ident
+        ident = RPLS_IDENT
+    out = []
+    mask = "%%-%ds%%s" % ident
     for l in [x for x in rpsl.split("\n") if ":" in x]:
-        k,v=l.split(":",1)
-        out.append(mask%(k.strip()+":",v.strip()))
-    return "\n".join(out)+"\n"
+        k, v = l.split(":", 1)
+        out += [mask % (k.strip() + ":", v.strip())]
+    return "\n".join(out) + "\n"
