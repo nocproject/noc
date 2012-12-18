c798c9606c23cdbe58fbebc6b14132a07bd48602
Dmitry Volodin <dv@nocproject.org>
2012-12-19 01:14:37 +0400

    NOC-877: cdp_discovery

---
+++ inv/discovery/jobs/cdp_discovery.py
@@ -0,0 +1,53 @@
+## -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## CDP Link Discovery Job
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2012 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+from link_discovery import LinkDiscoveryJob
+from noc.settings import config
+from noc.inv.models.discoveryid import DiscoveryID
+
+
+class CDPLinkDiscoveryJob(LinkDiscoveryJob):
+    """
+    CDP Link Discovery
+    """
+    name = "cdp_discovery"
+    map_task = "get_cdp_neighbors"
+    method = "lldp"
+    ignored = not config.getboolean("cdp_discovery", "enabled")
+    initial_submit_interval = config.getint("cdp_discovery",
+        "initial_submit_interval")
+    initial_submit_concurrency = config.getint("cdp_discovery",
+        "initial_submit_concurrency")
+
+    def process_result(self, object, result):
+        self.n_cache = {}  # device_id -> object
+        for n in result["neighbors"]:
+            remote_object = self.get_neighbor(n["device_id"])
+            if not remote_object:
+                continue
+            remote_port = remote_object.profile.convert_interface_name(
+                n["remote_interface"])
+            self.submit_candidate(
+                n["local_interface"], remote_object, remote_port)
+
+    def get_neighbor(self, device_id):
+        """
+        Find neighbor by chassis id and chassis subtype
+        :param device_id:
+        :return:
+        """
+        # Get cached
+        n = self.n_cache.get(device_id)
+        if n:
+            return n
+        n = DiscoveryID.objects.filter(hostname=device_id).first()
+        if n:
+            n = n.object
+        self.n_cache[device_id] = n
+        return n
