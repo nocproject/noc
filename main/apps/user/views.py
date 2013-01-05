cbd72174d573637cdd857e27f4e8da42c5caa51a
Dmitry Volodin <dv@nocproject.org>
2013-01-06 00:35:43 +0400

    NOC-798: Restore missed password change link

--- main/apps/user/views.py
+++ main/apps/user/views.py
@@ -10,6 +10,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.shortcuts import get_object_or_404
 from django.utils.translation import ugettext_lazy as _
 ## NOC modules
@@ -22,6 +23,10 @@ class UserChangeForm(forms.ModelForm):
     username = forms.RegexField(label=_("Username"), max_length=75, regex=r"^[\w.@+-]+$",
         help_text=_("Required. 75 characters or fewer.  Letters, digits and @/./+/-/_ only"),
         error_message=_("This value must contain only letters, digits and @/./+/-/_."))
+    password = ReadOnlyPasswordHashField(label=_("Password"),
+            help_text=_("Raw passwords are not stored, so there is no way to see "
+                        "this user's password, but you can change the password "
+                        "using <a href=\"password/\">this form</a>."))
     noc_user_permissions = forms.CharField(label="User Access", widget=AccessWidget, required=False)
 
     class Meta:
