499e0202f54e8826e48b44a63e87c47d1161bb39
Dmitry Volodin <dv@nocproject.org>
2013-01-16 16:00:02 +0400

    NOC-901: Fix

--- inv/discovery/jobs/rep_discovery.py
+++ inv/discovery/jobs/rep_discovery.py
@@ -29,30 +29,31 @@ class REPLinkDiscoveryJob(LinkDiscoveryJob):
         if not first_mac or not last_mac:
             return  # ID discovery is incomplete
         for segment in result:
+            topology = segment["topology"]
             # Find own ports
-            o = [i for i, p in enumerate(segment)
+            o = [i for i, p in enumerate(topology)
                  if first_mac <= p["mac"] <= last_mac]
             if not o:
                 continue  # Not found
             elif len(o) == 2:
                 # Inside the ring
                 self.submit_pair(object,
-                    segment[o[0]], segment[o[0] - 1])
+                    topology[o[0]], topology[o[0] - 1])
                 self.submit_pair(object,
-                    segment[o[1]], segment[o[1] + 1])
+                    topology[o[1]], topology[o[1] + 1])
             elif len(o) == 1:
                 # End of the ring
                 if o[0] == 0:
                     # First
                     self.submit_pair(object,
-                        segment[o[0]], segment[o[0] + 1])
+                        topology[o[0]], topology[o[0] + 1])
                 else:
                     # Last
                     self.submit_pair(object,
-                        segment[o[0]], segment[o[0] - 1])
+                        topology[o[0]], topology[o[0] - 1])
             else:
                 # Something strange
-                self.error("Invalid REP discovery result: %r" % segment)
+                self.error("Invalid REP discovery result: %r" % topology)
                 continue
 
     def submit_pair(self, object, local_info, remote_info):
