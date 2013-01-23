57d1d20ca98d11ac9d2d22dca481adda2bf07f21
Dmitry Volodin <dv@nocproject.org>
2013-01-23 13:17:49 +0400

    Network Map: Shape selection form

---
+++ inv/apps/map/js/SelectIconForm.js
@@ -0,0 +1,60 @@
+//---------------------------------------------------------------------
+// Icon Selection window
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2012 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+// @todo: Auto-fill VC-domain if selected in filter
+//---------------------------------------------------------------------
+console.debug("Defining NOC.inv.map.SelectIconForm");
+
+Ext.define("NOC.inv.map.SelectIconForm", {
+    extend: "Ext.Window",
+    requires: [
+        "NOC.main.ref.stencil.LookupField"
+    ],
+    title: "Select Shape",
+    autoShow: true,
+    closable: false,
+    modal: true,
+    app: null,
+    closable: true,
+
+    initComponent: function() {
+        var me = this;
+        Ext.apply(me, {
+            items: [
+                {
+                    xtype: "form",
+                    items: [
+                        {
+                            xtype: "main.ref.stencil.LookupField",
+                            name: "shape",
+                            fieldLabel: "Shape",
+                            allowBlank: false
+                        }
+                    ]
+                }
+            ],
+            buttons: [
+                {
+                    text: "Set",
+                    itemId: "set",
+                    iconCls: "icon_add",
+                    scope: me,
+                    /*formBind: true,
+                    disabled: true,*/
+                    handler: me.onSet
+                }
+            ]
+        });
+        me.callParent();
+    },
+    // Called when "Set" button pressed
+    onSet: function() {
+        var me = this;
+        var r = me.down("form").getForm().getValues();
+        me.close();
+        me.app.setIcon(r.shape);
+    }
+});
