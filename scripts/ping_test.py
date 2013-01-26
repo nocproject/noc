1de3c4072aaf173e5e2b4cfbf7785ff3fbc63aeb
Dmitry Volodin <dv@nocproject.org>
2013-01-26 19:51:20 +0400

    ping_test script

---
+++ scripts/ping_test.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import threading
+import sys
+import readline
+# NOC modules
+import set_env
+
+set_env.setup(use_django=False)
+
+from noc.lib.nbsocket.socketfactory import SocketFactory
+from noc.lib.nbsocket.pingsocket import Ping4Socket
+from noc.lib.validators import is_ipv4
+
+
+def cb(*args):
+    print "%% RESULT: %s %s" % (args[0], args[1])
+
+
+def loop():
+    sf = SocketFactory()
+    ps = Ping4Socket(sf)
+    print "Running socket factory"
+    t = threading.Thread(target=sf.run)
+    t.setDaemon(True)
+    t.start()
+    print "READY"
+    while True:
+        try:
+            ip = raw_input("IP> ")
+        except EOFError:
+            print "%% STOP"
+            sys.exit(0)
+        ip = ip.strip()
+        if not ip:
+            continue
+        if not is_ipv4(ip):
+            print "%% ERROR: Invalid IPv4"
+            continue
+        ps.ping(ip, count=3, callback=cb)
+
+if __name__ == "__main__":
+    loop()
