593862e2379cae6accbaa2fe6cebb5306aa5d3cf
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2013-01-30 10:22:13 +0200

    Fix Alcatel.AOS.ping script

--- sa/profiles/Alcatel/AOS/ping.py
+++ sa/profiles/Alcatel/AOS/ping.py
@@ -15,14 +15,20 @@ import re
 class Script(noc.sa.script.Script):
     name = "Alcatel.AOS.ping"
     implements = [IPing]
-    rx_result = re.compile(r"^(?P<success>\d+)\s+packets transmitted,\s+(?P<count>\d+)\s+packets received,\s+\d+%\s+packet loss?"
-                           r"\nround-trip \(ms\)\s+min/avg/max\s+=\s+(?P<min>\d+)/(?P<avg>\d+)/(?P<max>\d+)?",
-                           re.MULTILINE | re.DOTALL)
+    rx_result = re.compile(
+        r"^(?P<success>\d+)\s+packets transmitted,\s+(?P<count>\d+)\s+"
+        r"packets received,\s+\d+%\s+packet loss?\nround-trip \(ms\)\s+"
+        r"min/avg/max\s+=\s+(?P<min>\d+)/(?P<avg>\d+)/(?P<max>\d+)?",
+        re.MULTILINE | re.DOTALL)
 
-    def execute(self, address, count=None, source_address=None, size=None, df=None):
-        cmd = "ping %s count 5" % address
+    def execute(self, address, count=None, source_address=None, size=None, \
+        df=None):
+
+        cmd = "ping %s" % address
         if count:
             cmd += " count %d" % int(count)
+        else:
+            cmd += " count 5"
         if source_address:
             cmd += " source %s" % source_address
         if size:
@@ -31,8 +37,10 @@ class Script(noc.sa.script.Script):
             cmd += " df-bit"
         pr = self.cli(cmd)
         match = self.rx_result.search(pr)
-        return {"success": match.group("success"),
-                "count": match.group("count"),
-                "min": match.group("min"),
-                "avg": match.group("avg"),
-                "max": match.group("max"), }
+        return {
+            "success": match.group("success"),
+            "count": match.group("count"),
+            "min": match.group("min"),
+            "avg": match.group("avg"),
+            "max": match.group("max")
+        }
