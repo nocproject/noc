c72298f99caede085442e455b95e642e8b220a0d
Dmitry Volodin <dv@nocproject.org>
2013-02-03 20:37:56 +0400

    ADMIN_MEDIA_PREFIX setting has been removed

--- settings.py
+++ settings.py
@@ -75,7 +75,6 @@ MEDIA_URL = ""
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = "/media/"
 STATIC_URL = "/media/"
 
 # Make this unique, and don"t share it with anybody.
