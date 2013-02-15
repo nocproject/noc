9feb369e37dbee6942f46e77a8e9de5d4c477367
Skripnik Artem <skripnik.al@gmail.com>
2013-02-15 17:22:21 +0200

    Fix get_chassis_id.py

--- sa/profiles/DLink/DxS_Cisco_CLI/get_chassis_id.py
+++ sa/profiles/DLink/DxS_Cisco_CLI/get_chassis_id.py
@@ -16,14 +16,18 @@ class Script(NOCScript):
     name = "DLink.DxS_Cisco_CLI.get_chassis_id"
     cache = True
     rx_ver = re.compile(r"^Hardware is  VLAN, address is (?P<id>\S+)\s+",
-        re.IGNORECASE | re.MULTILINE)
+                        re.IGNORECASE | re.MULTILINE)
+    rx_mac_from = re.compile(r"^\s+Chassis\s+ID\s+:\s+(?P<mac_from>\S+)",
+                             re.IGNORECASE | re.MULTILINE)
     implements = [IGetChassisID]
 
     def execute(self):
+        match = self.re_search(self.rx_mac_from, self.cli("show lldp local-information global", cached=True))
+        mac_from = match.group("mac_from")
         match = self.re_search(self.rx_ver, self.cli("show interfaces vlan 1",
-            cached=True))
-        mac = match.group("id")
+                               cached=True))
+        mac_to = match.group("id")
         return {
-            "first_chassis_mac": mac,
-            "last_chassis_mac": mac
+            "first_chassis_mac": mac_from,
+            "last_chassis_mac": mac_to
         }
