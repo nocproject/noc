782a87a83e8ae74f4a5335ab05f30439034277bf
Dmitry Volodin <dv@nocproject.org>
2013-02-19 23:07:32 +0400

    AuditTrail has been refactored to separate module

--- main/models/__init__.py
+++ main/models/__init__.py
@@ -141,44 +141,7 @@ if settings.IS_WEB:
 downloader_registry.register_all()
 
 
-class AuditTrail(models.Model):
-    """
-    Audit Trail
-    """
-    class Meta:
-        verbose_name = "Audit Trail"
-        verbose_name_plural = "Audit Trail"
-        ordering = ["-timestamp"]
-    user = models.ForeignKey(User, verbose_name="User")
-    timestamp = models.DateTimeField("Timestamp", auto_now=True)
-    model = models.CharField("Model", max_length=128)
-    db_table = models.CharField("Table", max_length=128)
-    operation = models.CharField("Operation", max_length=1,
-                                 choices=[("C", "Create"), ("M", "Modify"),
-                                          ("D", "Delete")])
-    subject = models.CharField("Subject", max_length=256)
-    body = models.TextField("Body")
-
-    @classmethod
-    def log(cls, sender, instance, operation, message):
-        """
-        Log into audit trail
-        """
-        user = get_user()  # Retrieve user from thread local storage
-        if not user or not user.is_authenticated():
-            return  # No user initialized, no audit trail
-        subject = unicode(instance)
-        if len(subject) > 127:
-            # Narrow subject
-            subject = subject[:62] + " .. " + subject[-62:]
-        AuditTrail(
-            user=user,
-            model=sender.__name__,
-            db_table=sender._meta.db_table,
-            operation=operation,
-            subject=subject,
-            body=message
-        ).save()
+from audittrail import AuditTrail
 
 
 class CustomFieldEnumGroup(models.Model):
@@ -1675,7 +1638,7 @@ class Checkpoint(models.Model):
     Checkpoint is a marked moment in time
     """
     class Meta:
-        verbose_name = _("Checkpoing")
+        verbose_name = _("Checkpoint")
         verbose_name_plural = _("Checkpoints")
 
     timestamp = models.DateTimeField(_("Timestamp"))
