787834203b97dbd92b2c857090a504571ab4de25
Dmitry Volodin <dv@nocproject.org>
2013-02-20 23:51:42 +0400

    NOC-953: main.timepattern has been rewritten as ExtApplication

---
+++ main/apps/timepattern/js/TermModel.js
@@ -0,0 +1,24 @@
+//---------------------------------------------------------------------
+// main.timepattern TermModel
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2013 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.main.timepattern.TermModel");
+
+Ext.define("NOC.main.timepattern.TermModel", {
+    extend: "Ext.data.Model",
+    rest_url: "/main/timepattern/{{parent}}/terms/",
+    parentField: "time_pattern_id",
+
+    fields: [
+        {
+            name: "id",
+            type: "int"
+        },
+        {
+            name: "term",
+            type: "string"
+        }
+    ]
+});
--- main/apps/timepattern/views.py
+++ main/apps/timepattern/views.py
@@ -1,73 +1,48 @@
 # -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
-## TimePattern Manager
+## main.timepattern application
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2010 The NOC Project
+## Copyright (C) 2007-2013 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-from django.contrib import admin
-from django import forms
-from django.shortcuts import get_object_or_404
-from noc.lib.app import ModelApplication,HasPerm
-from noc.main.models import TimePattern,TimePatternTerm
+
+## Python modules
 import datetime
-##
-## TimePattern admin
-##
-class TimePatternTermForm(forms.ModelForm):
-    class Meta:
-        model=TimePatternTerm
-    def clean_term(self):
-        try:
-            TimePatternTerm.check_syntax(self.cleaned_data["term"])
-        except SyntaxError,why:
-            raise forms.ValidationError(why)
-        return self.cleaned_data["term"]
-    
-class TimePatternTermAdmin(admin.TabularInline):
-    extra=5
-    model=TimePatternTerm
-    form=TimePatternTermForm
-    
-class TimePatternAdmin(admin.ModelAdmin):
-    list_display=["name"]
-    search_fields=["name"]
-    inlines=[TimePatternTermAdmin]
-    actions=["test"]
-    ##
-    ## Test Selected Time Patterns
-    ##
-    def test(self,request,queryset):
-        return self.app.response_redirect("test/%s/"%",".join([str(p.id) for p in queryset]))
-    test.short_description="Test selected Time Patterns"
-##
-## Test Time Patterns Form
-##    
-class TestTimePatternsForm(forms.Form):
-    time=forms.DateTimeField(input_formats=["%d.%m.%Y %H:%M:%S"])
-##
-## TimePattern application
-##
-class TimePatternApplication(ModelApplication):
-    model=TimePattern
-    model_admin=TimePatternAdmin
-    menu="Setup | Time Patterns"
-    icon = "icon_time"
-    ##
-    ## Test Selected Time Patterns
-    ##
-    def view_test(self,request,objects):
-        tp=[get_object_or_404(TimePattern,id=int(x)) for x in objects.split(",")]
-        result=[]
-        if request.POST:
-            form=TestTimePatternsForm(request.POST)
-            if form.is_valid():
-                t=form.cleaned_data["time"]
-                result=[{"pattern":p,"result":p.match(t)} for p in tp]
-        else:
-            now=datetime.datetime.now()
-            s="%02d.%02d.%04d %02d:%02d:%02d"%(now.day,now.month,now.year,now.hour,now.minute,now.second)
-            form=TestTimePatternsForm(initial={"time":s})
-        return self.render(request,"test.html",{"form":form,"result":result})
-    view_test.url=r"^test/(?P<objects>\d+(?:,\d+)*)/$"
-    view_test.access=HasPerm("change")
+## NOC modules
+from noc.lib.app import ExtModelApplication, view
+from noc.main.models.timepattern import TimePattern
+from noc.main.models.timepatternterm import TimePatternTerm
+from noc.lib.app.modelinline import ModelInline
+from noc.sa.interfaces import ModelParameter, ListOfParameter, StringParameter
+
+
+class TimePatternApplication(ExtModelApplication):
+    """
+    TimePattern application
+    """
+    title = "Time Pattern"
+    menu = "Setup | Time Patterns"
+    model = TimePattern
+
+    terms = ModelInline(TimePatternTerm)
+
+    @view(url="^actions/test/", method=["POST"],
+          access="read", api=True,
+          validate={
+              "ids": ListOfParameter(element=ModelParameter(TimePattern)),
+              "date": StringParameter(required=True),
+              "time": StringParameter(required=True),
+          })
+    def api_action_test(self, request, ids, date=None, time=None):
+        d = "%sT%s" % (date, time)
+        dt = datetime.datetime.strptime(d, "%Y-%m-%dT%H:%M")
+        return {
+            "ts": dt.isoformat(),
+            "result": [{
+                "id": p.id,
+                "name": p.name,
+                "result": p.match(dt)
+            } for p in ids]
+        }
+
+
