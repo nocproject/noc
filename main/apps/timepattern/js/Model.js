2eeb76c083262db68e8a649dcb6dcde435984c58
Dmitry Volodin <dv@nocproject.org>
2013-02-26 22:58:48 +0400

    main.timepattern: Commit missed files

---
+++ main/apps/timepattern/js/Model.js
@@ -0,0 +1,27 @@
+//---------------------------------------------------------------------
+// main.timepattern Model
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2013 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.main.timepattern.Model");
+
+Ext.define("NOC.main.timepattern.Model", {
+    extend: "Ext.data.Model",
+    rest_url: "/main/timepattern/",
+
+    fields: [
+        {
+            name: "id",
+            type: "int"
+        },
+        {
+            name: "name",
+            type: "string"
+        },
+        {
+            name: "description",
+            type: "string"
+        }
+    ]
+});
