b9fca79927857af5ed7d3ead1cefac6174ec0518
Alexey Shapovalov <Aleksey.Shapovalov@omsk.mts.ru>
2013-03-16 14:08:59 +0700

    NOC-963 Add support of XGS-4728F

--- sa/profiles/Zyxel/ZyNOS/get_interface_status.py
+++ sa/profiles/Zyxel/ZyNOS/get_interface_status.py
@@ -2,14 +2,13 @@
 ##----------------------------------------------------------------------
 ## Zyxel.ZyNOS.get_interface_status
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2011 The NOC Project
+## Copyright (C) 2007-2013 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-"""
-"""
-## Python modules
+
+# Python modules
 import re
-## NOC modules
+# NOC modules
 from noc.sa.script import Script as NOCScript
 from noc.sa.interfaces import IGetInterfaceStatus
 
@@ -17,7 +16,9 @@ from noc.sa.interfaces import IGetInterfaceStatus
 class Script(NOCScript):
     name = "Zyxel.ZyNOS.get_interface_status"
     implements = [IGetInterfaceStatus]
-    rx_link = re.compile(r"Port Info\s+Port NO\.\s+:(?P<interface>\d+)\s*Link\s+:(?P<status>(10+M/[F|H]\s*(Copper|SFP)?)|Down)", re.MULTILINE | re.IGNORECASE)
+    rx_link = re.compile(r"Port Info\s+Port NO\.\s+:(?P<interface>\d+)\s*Link"
+                    r"\s+:(?P<status>(1[02]+[MG]/[FH]\s*(Copper|SFP)?)|Down)",
+                    re.MULTILINE | re.IGNORECASE)
 
     def execute(self, interface=None):
         if self.snmp and self.access_profile.snmp_ro:
@@ -25,15 +26,21 @@ class Script(NOCScript):
                 r = []
                 if interface is None:
                     # Join # IF-MIB::ifName, IF-MIB::ifOperStatus
+                    # use max_index to skip vlans
                     for n, s in self.snmp.join_tables("1.3.6.1.2.1.31.1.1.1.1",
                                                       "1.3.6.1.2.1.2.2.1.8",
                                                       bulk=True,
                                                       max_index=1023):
-                        r += [{"interface": n, "status": int(s) == 1}]  # ifOperStatus up(1)
+#                        if n == "enet0":  # tmp - skip Outbound management
+#                            continue
+                        # ifOperStatus up(1)
+                        r += [{"interface": n, "status": int(s) == 1}]
                     return r
                 else:
-                    n = self.snmp.get("1.3.6.1.2.1.31.1.1.1.1.%d" % int(interface))
-                    s = self.snmp.get("1.3.6.1.2.1.2.2.1.8.%d" % int(interface))
+                    n = self.snmp.get("1.3.6.1.2.1.31.1.1.1.1.%d"
+                                    % int(interface))
+                    s = self.snmp.get("1.3.6.1.2.1.2.2.1.8.%d"
+                                    % int(interface))
                     return [{"interface":n, "status":int(s) == 1}]
             except self.snmp.TimeOutError:
                 pass
