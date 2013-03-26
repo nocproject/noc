955c24d8de74819462840ddc23229ebfa57b03d9
Dmitry Volodin <dv@nocproject.org>
2013-03-26 13:09:56 +0400

    NOC-973: Rewrite main.prefixtable as ExtApplication

---
+++ main/apps/prefixtable/js/PrefixModel.js
@@ -0,0 +1,28 @@
+//---------------------------------------------------------------------
+// main.prefixtable Model
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2013 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.main.prefixtable.PrefixModel");
+
+Ext.define("NOC.main.prefixtable.PrefixModel", {
+    extend: "Ext.data.Model",
+    rest_url: "/main/prefixtable/{{parent}}/prefixes/",
+    parentField: "table_id",
+
+    fields: [
+        {
+            name: "id",
+            type: "int"
+        },
+        {
+            name: "afi",
+            type: "string"
+        },
+        {
+            name: "prefix",
+            type: "string"
+        },
+    ]
+});
