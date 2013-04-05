f84a4684b3794bf164816c288b7eacdbf8b833dd
Dmitry Volodin <dv@nocproject.org>
2013-04-05 16:15:17 +0400

    Cleanup obsolete indexes

---
+++ inv/migrations/0005_cleanup_sub_indexes.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Cleanup SubInterface's is_* indexes
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2013 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+from noc.lib.nosql import get_db
+
+class Migration:
+    def forwards(self):
+        c = get_db().noc.subinterfaces
+        for i in ("is_ipv4_1", "is_ipv6_1", "is_bridge_1"):
+            try:
+                c.drop_index(i)
+            except:
+                pass
+
+    def backwards(self):
+        pass
