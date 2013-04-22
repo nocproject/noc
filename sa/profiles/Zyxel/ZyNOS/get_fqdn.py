81be489d5c48e2693350e23d27300039e08a5dba
Alexey Shapovalov <Aleksey.Shapovalov@omsk.mts.ru>
2013-04-22 10:58:42 +0700

    NOC-987 Zyxel.ZyNOS.get_fqdn

---
+++ sa/profiles/Zyxel/ZyNOS/get_fqdn.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Zyxel.ZyNOS.get_fqdn
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2013 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Python modules
+import re
+## NOC modules
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetFQDN
+
+
+class Script(NOCScript):
+    name = "Zyxel.ZyNOS.get_fqdn"
+    implements = [IGetFQDN]
+
+    rx_hostname = re.compile(r"^System Name\s+:\s+(?P<hostname>\S+)$",
+                            re.MULTILINE)
+
+    def execute(self):
+        fqdn = ""
+        v = self.cli("show system-information")
+        match = self.rx_hostname.search(v)
+        if match:
+            fqdn = match.group("hostname")
+        return fqdn
