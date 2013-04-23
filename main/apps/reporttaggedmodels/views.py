767d7db7ecb37a22fa17ffda055cb9024330adbf
Dmitry Volodin <dv@nocproject.org>
2013-04-23 11:08:53 +0400

    main.reporttaggedmodels: Fix

--- main/apps/reporttaggedmodels/views.py
+++ main/apps/reporttaggedmodels/views.py
@@ -2,28 +2,34 @@
 ##----------------------------------------------------------------------
 ## Tagged Models Report
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2010 The NOC Project
+## Copyright (C) 2007-2013 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
+
+## Django modules
+from django.db import models as django_models
+## NOC modules
 from noc.lib.app.simplereport import SimpleReport
 from noc.settings import INSTALLED_APPS
-from django.db import models as django_models
-##
-##
-##
+
+
 class ReportTaggedModels(SimpleReport):
-    title="Tagged Models"
-    def get_data(self,**kwargs):
-        data=[]
+    title = "Tagged Models"
+
+    def get_data(self, **kwargs):
+        seen = set()
         for app in INSTALLED_APPS:
             if app.startswith("noc."):
-                models=app+".models"
-                module=__import__(models,{},{},"*")
+                models = app + ".models"
+                module = __import__(models, {}, {}, "*")
                 for n in dir(module):
-                    obj=getattr(module,n)
+                    obj = getattr(module, n)
                     try:
-                        if issubclass(obj,django_models.Model) and hasattr(obj,"tags"):
-                            data+=[[app[4:],obj._meta.verbose_name]]
+                        if (issubclass(obj, django_models.Model) and
+                                hasattr(obj, "tags")):
+                            seen.add(obj)
                     except:
                         pass
-        return self.from_dataset(title=self.title,columns=["Module","Model"],data=data)
+        data = sorted((m._meta.app_label, m._meta.verbose_name) for m in seen)
+        return self.from_dataset(title=self.title,
+                                 columns=["Module", "Model"], data=data)
