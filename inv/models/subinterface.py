3cd17721e71a562be345b5ad065084a4aec4886f
Dmitry Volodin <dv@nocproject.org>
2013-04-23 09:14:51 +0400

    SubInterface: Tunnel configuration

--- inv/models/subinterface.py
+++ inv/models/subinterface.py
@@ -28,6 +28,14 @@ SUBINTERFACE_PROTOCOLS = (
     .element.attrs["subinterfaces"]
     .element.attrs["enabled_protocols"].element.choices)
 
+TUNNEL_TYPES = (
+    IGetInterfaces.returns
+    .element.attrs["interfaces"]
+    .element.attrs["subinterfaces"]
+    .element.attrs["tunnel"]
+    .attrs["type"].choices
+)
+
 
 class SubInterface(Document):
     meta = {
@@ -46,6 +54,7 @@ class SubInterface(Document):
         ForwardingInstance, required=False)
     name = StringField()
     description = StringField(required=False)
+    mtu = IntField(required=False)
     mac = StringField(required=False)
     vlan_ids = ListField(IntField(), default=[])
     enabled_afi = ListField(StringField(
@@ -61,6 +70,11 @@ class SubInterface(Document):
     tagged_vlans = ListField(IntField(), default=[])
     # ip_unnumbered_subinterface
     ifindex = IntField(required=False)
+    # Tunnel services
+    tunnel_type = StringField(
+        choices=[(x, x) for x in TUNNEL_TYPES], required=False)
+    tunnel_local_address = StringField(required=False)
+    tunnel_remote_address = StringField(required=False)
 
     def __unicode__(self):
         return "%s %s" % (self.interface.managed_object.name, self.name)
