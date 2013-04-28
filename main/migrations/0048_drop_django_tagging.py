7d924f7b786caa8bd671016cebdda887ca0bfbed
Dmitry Volodin <dv@nocproject.org>
2013-04-28 20:58:00 +0400

    Fix migration

--- main/migrations/0048_drop_django_tagging.py
+++ main/migrations/0048_drop_django_tagging.py
@@ -12,8 +12,9 @@ from south.db import db
 
 class Migration:
     def forwards(self):
-        db.drop_table("tagging_taggeditem")
-        db.drop_table("tagging_tag")
+        for t in ["tagging_taggeditem", "tagging_tag"]:
+            if db.execute("SELECT COUNT(*) FROM pg_class WHERE relname='%s'" % t)[0][0] == 1:
+                db.drop_table(t)
 
     def backwards(self):
         pass
