a09afc3f1d113c9e12f8312d4b612f336a14bfc3
Dmitry Volodin <dv@nocproject.org>
2013-04-29 00:58:50 +0400

    Juniper.JUNOSe: Prefix list generation

--- sa/profiles/Juniper/JUNOSe/__init__.py
+++ sa/profiles/Juniper/JUNOSe/__init__.py
@@ -25,17 +25,42 @@ class Profile(NOCProfile):
     pattern_more = r"^ --More-- "
     command_more = " "
     pattern_syntax_error = r"% Invalid input detected at"
-    config_volatile = [r"^! Configuration script being generated on.*?^",
-                       r"^(Please wait\.\.\.)\n",
-                       r"^(\.+)\n"]
+    config_volatile = [
+        r"^! Configuration script being generated on.*?^",
+        r"^(Please wait\.\.\.)\n",
+        r"^(\.+)\n"
+    ]
     rogue_chars = ["\r", "\x00", "\x0d"]
 
-    ##
-    ## Common forms are:
-    ##    X.Y.Z release-A.B
-    ##    X.Y.Z patch-A.B
     @classmethod
-    def cmp_version(self, v1, v2):
+    def cmp_version(cls, v1, v2):
+        """
+        Common forms are:
+            X.Y.Z release-A.B
+            X.Y.Z patch-A.B
+        :param cls:
+        :param v1:
+        :param v2:
+        :return:
+        """
         def convert(v):
             return v.replace(" patch-", ".").replace(" release-", ".")
+
         return NOCProfile.cmp_version(convert(v1), convert(v2))
+
+    def generate_prefix_list(self, name, pl):
+        """
+        Generate prefix list.
+        :param name: Prefix list name
+        :param pl: List of (prefix, min len, max len)
+        :return:
+        """
+        me = "ip prefix-list %s permit %%s" % name
+        mne = "ip prefix-list %s permit %%s le %%d" % name
+        r = ["no ip prefix-list %s" % name]
+        for prefix, min_len, max_len in pl:
+            if min_len == max_len:
+                r += [me % prefix]
+            else:
+                r += [mne % (prefix, max_len)]
+        return "\n".join(r)
