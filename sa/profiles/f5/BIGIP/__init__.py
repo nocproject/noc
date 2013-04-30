5123707e52e45aebbb0d36ce6298405763594e3d
Dmitry Volodin <dv@nocproject.org>
2013-04-30 21:50:53 +0400

    f5.BIGIP.get_vlans: Ported to tmsh

--- sa/profiles/f5/BIGIP/__init__.py
+++ sa/profiles/f5/BIGIP/__init__.py
@@ -22,3 +22,42 @@ class Profile(noc.sa.profiles.Profile):
         (r"^\(END\)", "q")
     ]
     command_exit = "quit"
+
+    def parse_blocks(self, script, v):
+        def clean(v):
+            if v.startswith("| "):
+                return v[2:]
+            else:
+                return v
+
+        in_header = False
+        header = []
+        data = []
+        for s in v.splitlines():
+            s = s.strip()
+            if not s:
+                # End of block
+                if header:
+                    h = clean(header[0])
+                    d = "\n".join([clean(x) for x in data])
+                    yield h, d
+                    hader = []
+                    data = []
+            elif s.startswith("----------"):
+                if not in_header:
+                    # Begin of header
+                    in_header = True
+                    header = []
+                    data = []
+                else:
+                    # End of header
+                    in_header = False
+            else:
+                if in_header:
+                    header += [s]
+                else:
+                    data += [s]
+
+    def setup_script(self, script):
+        self.add_script_method(
+            script, "parse_blocks", self.parse_blocks)
