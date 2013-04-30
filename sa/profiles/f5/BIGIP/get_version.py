1926308710dd6d48e072714149b8d72ac73cf9cf
Dmitry Volodin <dv@nocproject.org>
2013-04-30 17:42:13 +0400

    f5.BIGIP.get_version: Rewritten to tmsh

--- sa/profiles/f5/BIGIP/get_version.py
+++ sa/profiles/f5/BIGIP/get_version.py
@@ -2,28 +2,34 @@
 ##----------------------------------------------------------------------
 ## f5.BIGIP.get_version
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2009 The NOC Project
+## Copyright (C) 2007-2013 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-"""
-"""
-import noc.sa.script
-from noc.sa.interfaces import IGetVersion
+
+## Python modules
 import re
+## NOC modules
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetVersion
 
 rx_ver = re.compile("BIG-IP Version (?P<version>.+?)$", re.MULTILINE)
 
 
-class Script(noc.sa.script.Script):
+class Script(NOCScript):
     name = "f5.BIGIP.get_version"
     cache = True
     implements = [IGetVersion]
 
+    rx_version = re.compile(
+        r"Product\s+(?P<platform>\S+).+"
+        r"Version\s+(?P<version>\S+)"
+    )
+
     def execute(self):
-        v = self.cli("b version")
-        match = rx_ver.search(v)
+        v = self.cli("show /sys version")
+        match = self.rx_version.search(v)
         return {
             "vendor": "f5",
-            "platform": "BIG-IP",
+            "platform": match.group("product"),
             "version": match.group("version")
         }
