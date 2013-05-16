babd59e7e0f95b8612aec41fbcc94c83ef42a8ae
Dmitry Volodin <dv@nocproject.org>
2013-05-17 00:28:30 +0400

    Force10.SFTOS.get_arp

---
+++ sa/profiles/Force10/SFTOS/get_arp.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+# Force10.SFTOS.get_arp
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2013 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetVlans
+from noc.lib.text import parse_table
+
+
+class Script(NOCScript):
+    name = "Force10.SFTOS.get_arp"
+    implements = [IGetVlans]
+
+    def execute(self):
+        return [{
+            "ip": x[0],
+            "mac": x[1],
+            "interface": x[2]
+        } for x in parse_table(self.cli("show arp"))]
