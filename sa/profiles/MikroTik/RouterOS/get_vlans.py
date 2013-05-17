7107aebd30a5940a097740830dc8dbc7216522bd
Dmitry Volodin <dv@nocproject.org>
2013-05-17 13:47:04 +0400

    MikroTik.RouterOS.get_vlans: Rewritten to use the .cli_detail

--- sa/profiles/MikroTik/RouterOS/get_vlans.py
+++ sa/profiles/MikroTik/RouterOS/get_vlans.py
@@ -2,13 +2,10 @@
 ##----------------------------------------------------------------------
 ## MikroTik.RouterOS.get_vlans
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2011 The NOC Project
+## Copyright (C) 2007-2013 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-"""
-"""
-## Python modules
-import re
+
 ## NOC modules
 from noc.sa.script import Script as NOCScript
 from noc.sa.interfaces import IGetVlans
@@ -17,18 +14,11 @@ from noc.sa.interfaces import IGetVlans
 class Script(NOCScript):
     name = "MikroTik.RouterOS.get_vlans"
     implements = [IGetVlans]
-    rx_vlan = re.compile(
-        r"vlan-id=(?P<vlanid>\d+) ports=", re.MULTILINE | re.DOTALL)
 
     def execute(self):
         try:
-            v = self.cli("interface ethernet switch vlan print terse")
-            v = self.cli("interface ethernet switch vlan print terse")
+            v = self.cli_detail("interface ethernet switch vlan print detail")
         except self.CLISyntaxError:
             raise self.NotSupportedError()
 
-        r = []
-        for match in self.rx_vlan.finditer(v):
-            vlan = match.group('vlanid')
-            r.append({"vlan_id": int(vlan)})
-        return r
+        return  [{"vlan_id": d["vlan-id"]} for n, f, d in v if not f]
