ce4a06c6bf2835a9225145f98349586f66d6b835
Dmitry Volodin <dv@nocproject.org>
2013-05-22 15:57:23 +0400

    ASSet model has been refactored to the separate module

---
+++ peer/models/asset.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## ASSet model
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2011 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Django modules
+from django.db import models
+## NOC module
+from noc.lib.fields import TagsField
+from noc.lib.app.site import site
+from noc.lib.rpsl import rpsl_format
+
+
+class ASSet(models.Model):
+    class Meta:
+        verbose_name = "ASSet"
+        verbose_name_plural = "ASSets"
+
+    name = models.CharField("Name", max_length=32, unique=True)
+    description = models.CharField("Description", max_length=64)
+    members = models.TextField("Members", null=True, blank=True)
+    rpsl_header = models.TextField("RPSL Header", null=True, blank=True)
+    rpsl_footer = models.TextField("RPSL Footer", null=True, blank=True)
+    tags = TagsField("Tags", null=True, blank=True)
+
+    def __unicode__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return site.reverse("peer:asset:change", self.id)
+
+    @property
+    def member_list(self):
+        if self.members is None:
+            return []
+        m = sorted(self.members.replace(",", " ")
+            .replace("\n", " ")
+            .replace("\r", " ").upper().split())
+        return m
+
+    @property
+    def rpsl(self):
+        sep = "remark: %s" % ("-" * 72)
+        s = []
+        if self.rpsl_header:
+            s += self.rpsl_header.split("\n")
+        s += ["as-set: %s" % self.name]
+        for m in self.member_list:
+            s += ["members: %s" % m]
+        if self.rpsl_footer:
+            s += [sep]
+            s += self.rpsl_footer.split("\n")
+        return rpsl_format("\n".join(s))
