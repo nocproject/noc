add62aa833f7736ab65b052da1674e357dc117d7
Dmitry Volodin <dv@nocproject.org>
2013-05-23 09:23:04 +0400

    ASSet migration for Project Management integration

---
+++ peer/migrations/0039_project_asset.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## AS.project and Peer.project
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2012 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Django modules
+from django.db import models
+## Third-party modules
+from south.db import db
+
+
+class Migration:
+    depends_on = (
+        ("project", "0001_initial"),
+    )
+
+    def forwards(self):
+        # Create .project
+        Project = db.mock_model(
+            model_name="Project",
+            db_table="project_project", db_tablespace="",
+            pk_field_name="id", pk_field_type=models.AutoField)
+        for t in ["peer_asset"]:
+            db.add_column(
+                t, "project",
+                models.ForeignKey(
+                    Project, verbose_name="Project",
+                    null=True, blank=True))
+
+    def backwards(self):
+        for t in ["peer_asset"]:
+            db.drop_column(t, "project_id")
