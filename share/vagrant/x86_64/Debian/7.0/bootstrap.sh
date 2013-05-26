c66e949a98dd525a93146fa0c8fdec4844dc9ddd
Dmitry Volodin <dv@nocproject.org>
2013-05-27 01:51:42 +0400

    Fix Debian 7.0 vagrant setup

--- share/vagrant/x86_64/Debian/7.0/bootstrap.sh
+++ share/vagrant/x86_64/Debian/7.0/bootstrap.sh
@@ -9,6 +9,12 @@
 
 PROGNAME=`basename $0`
 DIST=/opt/noc/share/vagrant/x86_64/Debian/7.0
+if [ "$1" = "--test" ]; then
+    TEST=0
+    echo "Running in the test mode...."
+else
+    TEST=1
+fi
 
 error_exit ( ) {
     echo "$PROGNAME: ${1:-'Unknown error'}" 1>&2
@@ -24,7 +30,8 @@ if [ $? -eq 0 ]; then
     # Fetch and restart
     hg pull -u || error_exit "Failed to fetch updates"
     cd $START_DIR
-    exec $0
+    echo "NOC has been updated. Restarting"
+    exec $0 $1
 fi
 
 # Put upgrade.conf in place
@@ -32,10 +39,17 @@ cp $DIST/files/upgrade.conf etc/upgrade.conf || error_exit "Cannot copy upgrade.
 # Put nginx.conf in place
 rm -f /etc/nginx/sites-enabled/default
 cp $DIST/files/nginx.conf /etc/nginx/sites-available/noc.conf || error_exit "Cannot copy nginx config"
-ln -s /etc/nginx/sites-available/noc.conf /etc/nginx/sites-enabled/noc.conf || error_exit "Cannot set up nginx config"
+if [ ! -h /etc/nginx/sites-enabled/noc.conf ]; then
+    ln -s /etc/nginx/sites-available/noc.conf /etc/nginx/sites-enabled/noc.conf || error_exit "Cannot set up nginx config"
+fi
 # Put init script
 cp $DIST/files/noc-launcher /etc/init.d/ || error_exit "Cannot install init file"
 update-rc.d noc-launcher start
+#
+if [ $TEST -eq 0 ]; then
+    echo "Stopping all NOC processes"
+    /etc/init.d/noc-launcher stop
+fi
 # Run NOC's upgrade process
 ./scripts/upgrade || error_exit "Failed to upgrade NOC"
 # Set up configs
@@ -50,7 +64,9 @@ UPDATE sa_activator SET auth='thenocproject';
 COMMIT;
 __EOF__
 [ $? -ne 0 ] && error_exit "Failed to set activator password"
-# Run nginx
-/etc/init.d/nginx restart
-# Run NOC
-/etc/init.d/noc-launcher start
+if [ $TEST -eq 1 ]; then
+    # Run nginx
+    /etc/init.d/nginx restart
+    # Run NOC
+    /etc/init.d/noc-launcher start
+fi
