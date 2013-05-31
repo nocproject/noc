e2aa9fb0965981111e532eceeb83874803aa6ea9
Dmitry Volodin <dv@nocproject.org>
2013-05-31 14:26:45 +0400

    PEP8

--- sa/profiles/Cisco/IOSXR/get_version.py
+++ sa/profiles/Cisco/IOSXR/get_version.py
@@ -2,11 +2,10 @@
 ##----------------------------------------------------------------------
 ## Cisco.IOSXR.get_version
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2011 The NOC Project
+## Copyright (C) 2007-2013 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-"""
-"""
+
 ## Python modules
 import re
 ## NOC modules
@@ -19,7 +18,11 @@ class Script(NOCScript):
     cache = True
     implements = [IGetVersion]
 
-    rx_ver = re.compile(r"^Cisco IOS XR Software, Version\s+(?P<version>\S+)\[\S+\].+cisco\s+(?P<platform>\S+)( Series)? \(\S+\) processor with \d+", re.MULTILINE | re.DOTALL)
+    rx_ver = re.compile(
+        r"^Cisco IOS XR Software, Version\s+(?P<version>\S+)\[\S+\].+"
+        r"cisco\s+(?P<platform>\S+)( Series)? \(\S+\) processor with \d+",
+        re.MULTILINE | re.DOTALL
+    )
     rx_snmp_ver = re.compile(r"Cisco IOS XR Software \(Cisco (?P<platform>\S+)\s+\w+\).+\s+Version\s+(?P<version>\S+)\[\S+\]")
 
     def execute(self):
@@ -28,16 +31,16 @@ class Script(NOCScript):
                 v = self.snmp.get("1.3.6.1.2.1.1.1.0", cached=True)  # sysDescr.0
                 match = self.re_search(self.rx_snmp_ver, v)
                 return {
-                    "vendor"     : "Cisco",
-                    "platform"   : match.group("platform"),
-                    "version"    : match.group("version")
+                    "vendor": "Cisco",
+                    "platform": match.group("platform"),
+                    "version": match.group("version")
                 }
             except self.snmp.TimeOutError:
                 pass
         v = self.cli("show version", cached=True)
         match = self.re_search(self.rx_ver, v)
         return {
-            "vendor"    : "Cisco",
-            "platform"  : match.group("platform"),
-            "version"   : match.group("version")
+            "vendor": "Cisco",
+            "platform": match.group("platform"),
+            "version": match.group("version")
         }
