0402e9c3896e0c02cd8a753526d6498320faf0e8
Dmitry Volodin <dv@nocproject.org>
2013-06-04 01:58:57 +0400

    Huawei.VRP.get_lldp_neighbors: Rewritten

--- sa/profiles/Huawei/VRP/get_lldp_neighbors.py
+++ sa/profiles/Huawei/VRP/get_lldp_neighbors.py
@@ -2,103 +2,98 @@
 ##----------------------------------------------------------------------
 ## Huawei.VRP.get_lldp_neighbors
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2010 The NOC Project
+## Copyright (C) 2007-2013 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-"""
-"""
+
 ## Python modules
 import re
 ## NOC modules
 from noc.sa.script import Script as NOCScript
 from noc.sa.interfaces import IGetLLDPNeighbors, MACAddressParameter
+from noc.lib.text import parse_kv
 
 
 class Script(NOCScript):
     name = "Huawei.VRP.get_lldp_neighbors"
     implements = [IGetLLDPNeighbors]
 
-    ##
-    ## No lldp on VRP3
-    ##
     @NOCScript.match(version__startswith="3.")
     def execute_vrp3(self):
+        """
+        No LLDP on VRP3
+        :return:
+        """
         raise self.NotSupportedError()
 
-    ##
-    ## Other (VRP5 style)
-    ##
-    rx_ifc_line = re.compile(
-        r"(?P<local_if>[^\n]+) has \d*[1-9]+\d* neighbor[^\n]+\n(?P<tail>.*)",
-        re.MULTILINE | re.DOTALL | re.IGNORECASE)
+    rx_iface_sep = re.compile(
+        r"^(\S+)\s+has\s+\d+\s+neighbors", re.MULTILINE
+    )
+
+    rx_neighbor_split = re.compile(
+        r"^Neighbor index\s*:\s+\d+",
+        re.MULTILINE
+    )
+
+    CHASSIS_TYPES = {
+        "macaddress": 4,
+        "interfacename": 6,
+        "local": 7
+    }
+
+    PORT_TYPES = {
+        "interfacealias": 1,
+        "macaddress": 3,
+        "interfacename": 5,
+        "local": 7
+    }
+
+    CAPS = {
+        "NA": 0, "other": 1, "repeater": 2, "bridge": 4,
+        "WLAN": 8, "router": 16, "telephone": 32,
+        "cable": 64, "station": 128
+    }
 
     @NOCScript.match()
     def execute_other(self):
+        """
+        VRP5 style
+        :return:
+        """
         r = []
-        i = {}
         try:
-            lldp = self.cli("display lldp neighbor")
+            v = self.cli("display lldp neighbor")
         except self.CLISyntaxError:
             raise self.NotSupportedError()
-        while True:
-            match = self.rx_ifc_line.search(lldp)
-            if not match:
-                break
-            pre = lldp[:match.start()]
-            lldp = match.group("tail")
-            if pre:
-                if i:
-                    i["neighbors"] += [parse_neighbor(pre)]
-                    r += [i]
-                i = {
-                    "local_interface": match.group("local_if"),
-                    "neighbors": []
-                }
-        if lldp and "neighbors" in i:
-            i["neighbors"] += [parse_neighbor(lldp)]
-            r += [i]
-        return r
-
-
-def parse_neighbor(text):
-    rx_ngh_line = re.compile(
-        r"Neighbor[^\n]+\n(?P<neighbor>.*?Expired time[^\n]+)",
-        re.MULTILINE | re.DOTALL | re.IGNORECASE)
-    rx_neigh = re.compile(
-        r"Chassis\s*ID\s*:\s*(?P<id>\S+).*?Port\s*ID\s*(sub)*type\s*:\s*"
-        r"(?P<p_type>\S+).*?Port\s*ID\s*:\s*(?P<p_id>\S+).*?Sys.*?name\s*:\s*"
-        r"(?P<name>[^\n]+).*?Sys.*?cap.*?enabled\s*:\s*(?P<capability>[^\n]+)",
-        re.MULTILINE | re.IGNORECASE | re.DOTALL)
-    n = []
-    for match_n in rx_ngh_line.finditer(text):
-        for match_data in rx_neigh.finditer(match_n.group("neighbor")):
-            n = {"remote_chassis_id_subtype": 4}
-            if match_data:
-                n["remote_port_subtype"] = {
-                    "macAddress": 3,
-                    "interfaceName": 5,
-                    "local": 7
-                }[match_data.group("p_type")]
+        il = self.rx_iface_sep.split(v)[1:]
+        for local_iface, data in zip(il[::2], il[1::2]):
+            neighbors = []
+            for ndata in self.rx_neighbor_split.split(data)[1:]:
+                n = parse_kv({
+                    "chassis type": "remote_chassis_id_subtype",
+                    "chassis id": "remote_chassis_id",
+                    "port id type": "remote_port_subtype",
+                    "port id": "remote_port",
+                    "system capabilities enabled": "remote_capabilities",
+                    "system name": "remote_system_name"
+                }, ndata)
+                # Convert chassis id
+                n["remote_chassis_id_subtype"] = self.CHASSIS_TYPES[n["remote_chassis_id_subtype"].lower()]
+                if n["remote_chassis_id_subtype"] == 3:
+                    n["remote_chassis_id"] = MACAddressParameter().clean(n["remote_chassis_id"])
+                # Convert port id
+                n["remote_port_subtype"] = self.PORT_TYPES[n["remote_port_subtype"].lower()]
                 if n["remote_port_subtype"] == 3:
-                    n["remote_port"] = \
-                        MACAddressParameter().clean(match_data.group("p_id"))
-                else:
-                    n["remote_port"] = match_data.group("p_id")
-                n["remote_chassis_id"] = match_data.group("id")
-                n["remote_system_name"] = match_data.group("name")
-                # Get capability
-                cap = 0
-                if len(match_data.group("capability").strip().split(", ")) > 1:
-                    capstring = \
-                        match_data.group("capability").strip().split(", ")
-                else:
-                    capstring = \
-                        match_data.group("capability").strip().split()
-                for c in capstring:
-                        cap |= {
-                        "NA": 0, "other": 1, "repeater": 2, "bridge": 4,
-                        "WLAN": 8, "router": 16, "telephone": 32,
-                        "cable": 64, "station": 128
-                        }[c]
-                n["remote_capabilities"] = cap
-    return n
+                    n["remote_port"] = MACAddressParameter().clean(n["remote_port"])
+                # Process capabilities
+                caps = 0
+                for c in n.get("remote_capabilities", "").split(","):
+                    caps |= self.CAPS[c.strip()]
+                n["remote_capabilities"] = caps
+                neighbors += [n]
+            if neighbors:
+                r += [{
+                    "local_interface": local_iface,
+                    "neighbors": neighbors
+                }]
+        return r
