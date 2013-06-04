f82a0d2b82e8fa4f133e8952ce53d556dacab9b2
Dmitry Volodin <dv@nocproject.org>
2013-06-05 01:59:04 +0400

    Performance Management: New data model

---
+++ pm/models/ts.py
@@ -0,0 +1,50 @@
+## -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## PMTS model
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2013 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Python modules
+import datetime
+import time
+## NOC Modules
+from noc.lib.nosql import (Document, StringField, IntField,
+                           PlainReferenceField, IntSequence)
+from storage import PMStorage
+
+
+## TS id
+seq_ts = IntSequence("pm.ts")
+
+
+class PMTS(Document):
+    """
+    Time Series
+    """
+    meta = {
+        "collection": "noc.pm.ts",
+        "allow_inheritance": False
+    }
+
+    ts_id = IntField(primary_key=True, default=seq_ts.next)
+    name = StringField(unique=True)
+    storage = PlainReferenceField(PMStorage)
+    description = StringField()
+    # type =
+
+    def __unicode__(self):
+        return self.name
+
+    def register(self, timestamp, value):
+        if isinstance(timestamp, datetime.datetime):
+            timestamp = time.mktime(timestamp.timetuple())
+        self.storage.register([self.ts_id, timestamp, value])
+
+    def iwindow(self, t0, t1):
+        if isinstance(t0, datetime.datetime):
+            t0 = time.mktime(t0.timetuple())
+        if isinstance(t1, datetime.datetime):
+            t1 = time.mktime(t1.timetuple())
+        return self.storage.iwindow(t0, t1, self.ts_id)
