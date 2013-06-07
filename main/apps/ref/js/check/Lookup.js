c70866f1b67b7d4edf3ef9d8b452a5b2a3bb1a46
Dmitry Volodin <dv@nocproject.org>
2013-06-07 11:39:32 +0400

    main.ref: check lookup

---
+++ main/apps/ref/js/check/Lookup.js
@@ -0,0 +1,12 @@
+//---------------------------------------------------------------------
+// NOC.main.check.Lookup
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2012 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.main.ref.check.Lookup");
+
+Ext.define("NOC.main.ref.check.Lookup", {
+    extend: "NOC.core.Lookup",
+    url: "/main/ref/check/lookup/"
+});
