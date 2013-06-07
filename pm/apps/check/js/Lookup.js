f1d63641273beabe7139f80afbfca85ff38bc031
Dmitry Volodin <dv@nocproject.org>
2013-06-07 15:13:06 +0400

    pm.check application skeleton

---
+++ pm/apps/check/js/Lookup.js
@@ -0,0 +1,12 @@
+//---------------------------------------------------------------------
+// NOC.pm.check.Lookup
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2013 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.pm.check.Lookup");
+
+Ext.define("NOC.pm.check.Lookup", {
+    extend: "NOC.core.Lookup",
+    url: "/pm/check/lookup/"
+});
