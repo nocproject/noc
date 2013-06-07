c540cf7a0479d2945a13f1d9a1f43d9162e13158
Dmitry Volodin <dv@nocproject.org>
2013-06-07 11:54:03 +0400

    noc-pmprobe daemon

---
+++ pm/pmprobe/checks/base.py
@@ -0,0 +1,107 @@
+## -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## BaseCheck
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2013 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Python modules
+import inspect
+import os
+import time
+import random
+
+
+class BaseCheck(object):
+    # Unique handler name
+    name = None
+    # Handler description
+    description = None
+    # DictParameter instance to validate output
+    parameters = {}
+    # Name of derived time series
+    time_series = []
+
+    def __init__(self, daemon, id, config, ts_map):
+        self.daemon = daemon
+        self.id = id
+        self.config = config
+        self.new_config = None
+        self.interval = None
+        self.t0 = None
+        self.label = "%s-%s" % (self.name, id)
+        self.ts_map = ts_map
+
+    def set_config(self, config):
+        """
+        Replace current config after handle() return
+        :param config:
+        :return:
+        """
+        self.new_config = config
+
+    def apply_config(self):
+        """
+        Apply new config.
+        Called outside the handle() call
+        :return:
+        """
+        if self.new_config:
+            self.config = self.new_config
+            self.new_config = None
+
+    def set_interval(self, interval):
+        if interval == self.interval:
+            return
+        self.t0 = time.time() - random.random() * interval
+        self.interval = interval
+
+    def get_next_time(self):
+        dt = time.time() - self.t0
+        n = dt // self.interval
+        return self.t0 + (n + 1) * self.interval
+
+    def map_result(self, r):
+        o = {}
+        for ts in r:
+            if ts in self.ts_map:
+                o[self.ts_map[ts]] = r[ts]
+        return o
+
+    def handle(self):
+        """
+        Run checks and return dict of time series name -> value
+        :return:
+        """
+        return {}
+
+    def get_form(self):
+        """
+
+        :return:
+        """
+        return []
+
+
+class CheckRegistry(dict):
+    def register_all(self):
+        for f in os.listdir("pm/pmprobe/checks/"):
+            if not f.endswith(".py") or f == "base.py":
+                continue
+            mn = "noc.pm.pmprobe.checks.%s" % f[:-3]
+            m = __import__(mn, {}, {}, "*")
+            for on in dir(m):
+                o = getattr(m, on)
+                if (inspect.isclass(o) and
+                        issubclass(o, BaseCheck) and
+                        o.__module__.startswith(mn)):
+                    assert o.name
+                    self[o.name] = o
+
+    @property
+    def choices(self):
+        return [(x, x) for x in sorted(self)]
+
+check_registry = CheckRegistry()
+check_registry.register_all()
---
+++ pm/pmprobe/daemon.py
@@ -0,0 +1,133 @@
+## -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## noc-pmprobe daemon
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2012 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Python modules
+from threading import Lock, Thread
+import bisect
+import time
+import logging
+## NOC modules
+from noc.lib.daemon import Daemon
+from noc.lib.stomp.threadclient import ThreadedSTOMPClient
+from noc.pm.pmprobe.checks.base import check_registry
+from noc.lib.threadpool import Pool
+from noc.lib.debug import error_report
+
+
+class PMProbeDaemon(Daemon):
+    daemon_name = "noc-pmprobe"
+
+    def __init__(self, *args, **kwargs):
+        self.checks = {}
+        self.queue_lock = Lock()
+        self.pending_queue = []  # (timestamp, check)
+        self.queue_runner_thread = None
+        self.thread_pool = None
+        self.data_lock = Lock()
+        self.data = []
+        super(PMProbeDaemon, self).__init__(*args, **kwargs)
+
+    def load_config(self):
+        super(PMProbeDaemon, self).load_config()
+        self.probe_name = self.config.get("probe", "name")
+        self.stomp_host = self.config.get("stomp", "host")
+        self.stomp_port = self.config.getint("stomp", "port")
+        self.stomp_client_id = "%s_%s" % (
+            self.config.get("stomp", "client_id"), self.probe_name)
+        self.stomp_login = self.config.get("stomp", "login")
+        self.stomp_password = self.config.get("stomp", "password")
+
+    def run(self):
+        # Prepare thread pool
+        self.thread_pool = Pool()
+        # Start queue runner
+        self.queue_runner_thread = Thread(target=self.queue_runner)
+        self.queue_runner_thread.daemon = True
+        self.queue_runner_thread.start()
+        # Run STOMP client
+        self.stomp_client = ThreadedSTOMPClient(
+            host=self.stomp_host,
+            port=self.stomp_port,
+            login=self.stomp_login,
+            passcode=self.stomp_password,
+            client_id=self.stomp_client_id
+        )
+        self.stomp_client.start()
+        self.stomp_client.subscribe(
+            "/queue/pm/config/%s/" % self.probe_name,
+            self.on_config)
+        self.stomp_client.send({"probe": self.probe_name},
+                               "/queue/pm/config/")
+        self.queue_runner_thread.join()
+
+    def on_config(self, destination, body):
+        with self.queue_lock:
+            for c in body:
+                if c["id"] in self.checks:
+                    # Change existing check
+                    check = self.checks[c["id"]]
+                    check.set_config(c["config"])
+                    check.set_interval(c["interval"])
+                else:
+                    # New check
+                    logging.debug("Configuring check: "
+                                  "%s id=%s config=%s" % (
+                        c["check"], c["id"], c["config"]))
+                    check = check_registry[c["check"]](
+                        self, c["id"], c["config"], c["ts"])
+                    check.set_interval(c["interval"])
+                    self.checks[c["id"]] = check
+                    # Schedule
+                    bisect.insort_right(self.pending_queue,
+                                        (check.get_next_time(), check))
+
+    def queue_runner(self):
+        while True:
+            # Run pending tasks
+            t = time.time()
+            to_sleep = True
+            with self.queue_lock:
+                while (self.pending_queue and
+                       self.pending_queue[0][0] <= t):
+                    _, check = self.pending_queue.pop(0)
+                    self.thread_pool.run(
+                        check.label,
+                        self.check_runner, (check,))
+                    to_sleep = False
+            # Spool pending measurements
+            if self.data:
+                with self.data_lock:
+                    self.stomp_client.send(self.data, "/queue/pm/data/")
+                    self.data = []
+            elif to_sleep:
+                time.sleep(1)  # @todo: Detect time
+
+    def check_runner(self, check):
+        """
+        Started by thread pool
+        :param check:
+        :return:
+        """
+        logging.debug("Running check: %s" % check.label)
+        # Apply pending config changes
+        check.apply_config()
+        # Run check
+        timestamp = int(time.time())
+        try:
+            r = check.map_result(check.handle())
+            # Spool data
+            with self.data_lock:
+                for ts in r:
+                    self.data += [(ts, timestamp, r[ts])]
+        except:
+            error_report()
+        logging.debug("Check completed: %s" % check.label)
+        # Reschedule check
+        with self.queue_lock:
+            bisect.insort_right(self.pending_queue,
+                                (check.get_next_time(), check))
