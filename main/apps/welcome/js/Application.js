e7874b5024221979a475f61dace465cf83111ce4
Dmitry Volodin <dv@nocproject.org>
2013-06-28 15:10:23 +0400

    main.welcome: Use Handlebars' template

--- main/apps/welcome/js/Application.js
+++ main/apps/welcome/js/Application.js
@@ -8,26 +8,16 @@ console.debug("Defining NOC.main.welcome.Application");
 
 Ext.define("NOC.main.welcome.Application", {
     extend: "NOC.core.Application",
-    items: [
-        {
-            xtype: "container",
-            html: "Welcome",
-            bodyPadding: 4
-        }
+    requires: [
+        "NOC.main.welcome.templates.Welcome"
     ],
+    appId: "main.welcome",
+
     initComponent: function() {
-        this.callParent();
-        Ext.Ajax.request({
-            method: "GET",
-            url: "/main/welcome/",
-            scope: this,
-            success: function(response) {
-                this.removeAll();
-                this.add({
-                    xtype: "component",
-                    html: Ext.decode(response.responseText)
-                });
-            }
+        var me = this;
+        Ext.apply(me, {
+            html: me.templates.Welcome({})
         });
+        me.callParent();
     }
 });
--- main/apps/welcome/views.py
+++ main/apps/welcome/views.py
@@ -2,20 +2,16 @@
 ##----------------------------------------------------------------------
 ## main.welcome application
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2011 The NOC Project
+## Copyright (C) 2007-2013 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 
 ## NOC modules
-from noc.lib.app import ExtApplication, view, PermitLogged
+from noc.lib.app import ExtApplication, view
 
 
-class WelcomeAppplication(ExtApplication):
+class WelcomeApplication(ExtApplication):
     """
     main.welcome application
     """
     title = "Welcome"
-
-    @view(method=["GET"], url=r"^$", access=PermitLogged(), api=True)
-    def api_welcome(self, request):
-        return self.render_template("welcome.html")
