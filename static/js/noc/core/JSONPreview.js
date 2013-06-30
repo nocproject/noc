2bb42d14a5292dcae711f1487a9247357d963974
Dmitry Volodin <dv@nocproject.org>
2013-06-30 19:58:53 +0400

    NOC.core.JSONPreview

---
+++ static/js/noc/core/JSONPreview.js
@@ -0,0 +1,70 @@
+//---------------------------------------------------------------------
+// NOC.core.JSONPreview
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2013 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.core.JSONPreview");
+
+Ext.define("NOC.core.JSONPreview", {
+    extend: "Ext.panel.Panel",
+    msg: "",
+    layout: "fit",
+    app: null,
+    restUrl: null,
+    previewName: null,
+
+    initComponent: function() {
+        var me = this;
+
+        Ext.apply(me, {
+            dockedItems: [{
+                xtype: "toolbar",
+                dock: "top",
+                items: [
+                    {
+                        itemId: "close",
+                        text: "Close",
+                        iconCls: "icon_arrow_undo",
+                        scope: me,
+                        handler: me.onClose
+                    }
+                ]
+            }],
+            items: [{
+                xtype: "container",
+                autoScroll: true,
+                bodyPadding: 4
+            }]
+        });
+        me.callParent();
+        //
+        me.urlTemplate = Handlebars.compile(me.restUrl);
+        me.titleTemplate = Handlebars.compile(me.previewName);
+    },
+    //
+    preview: function(record) {
+        var me = this,
+            url = me.urlTemplate(record.data);
+        me.setTitle(me.titleTemplate(record.data));
+        Ext.Ajax.request({
+            url: url,
+            method: "GET",
+            scope: me,
+            success: function(response) {
+                var json = Ext.decode(response.responseText);
+                me.items.first().update("<pre>" + json + "</pre>");
+                //NOC.SyntaxHighlight.highlight(me.items.first(),
+                //    json, "json");
+            },
+            failure: function() {
+                NOC.error("Failed to get JSON")
+            }
+        });
+    },
+    //
+    onClose: function() {
+        var me = this;
+        me.app.showForm();
+    }
+});
