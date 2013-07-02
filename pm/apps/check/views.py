78c26e3c48f6da4ddac18300ee4adcc79062207d
Dmitry Volodin <dv@nocproject.org>
2013-07-02 14:32:33 +0400

    pm.check: Quick dashboard

--- pm/apps/check/views.py
+++ pm/apps/check/views.py
@@ -9,6 +9,7 @@
 ## NOC modules
 from noc.lib.app import ExtDocApplication, view
 from noc.pm.models.check import PMCheck
+from noc.pm.models.ts import PMTS
 from noc.pm.pmprobe.checks.base import check_registry
 
 
@@ -28,3 +29,10 @@ class PMCheckApplication(ExtDocApplication):
             for c in check_registry
             if check_registry[c].form)
         return li
+
+    @view(url=r"^(?P<id>[0-9a-f]{24})/ts/", method=["GET"], access="read",
+          api=True)
+    def api_ts(self, request, id):
+        check = self.get_object_or_404(PMCheck, id=id)
+        return dict((t.ts_id, t.name)
+                    for t in PMTS.objects.filter(check=check))
