289f7891cb6d46d79b950bf0aa328b97fcb769df
Dmitry Volodin <dv@nocproject.org>
2013-08-19 08:11:28 +0400

    fm.classificationrule: "Test Rule" button
    --HG--
    branch : feature/fm-new-ui

---
+++ fm/apps/classificationrule/js/TestForm.js
@@ -0,0 +1,86 @@
+//---------------------------------------------------------------------
+// fm.classificationrule rule test form
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2013 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.fm.classificationrule.TestForm");
+
+Ext.define("NOC.fm.classificationrule.TestForm", {
+    extend: "Ext.panel.Panel",
+    app: null,
+
+    initComponent: function() {
+        var me = this;
+
+        me.dataField = Ext.create("Ext.form.field.TextArea", {
+            name: "data",
+            labelAlign: "top",
+            fieldLabel: "Event id or JSON code",
+            width: 400,
+            height: 200
+        });
+
+        Ext.apply(me, {
+            dockedItems: [
+                {
+                    xtype: "toolbar",
+                    dock: "top",
+                    items: [
+                        {
+                            text: "Close",
+                            glyph: NOC.glyph.arrow_left,
+                            scope: me,
+                            handler: me.onClose
+                        },
+                        {
+                            text: "Test",
+                            glyph: NOC.glyph.question_sign,
+                            scope: me,
+                            handler: me.onTest
+                        }
+                    ]
+                }
+            ],
+            items: [
+                me.dataField
+            ]
+        });
+        me.callParent();
+    },
+    //
+    onClose: function() {
+        var me = this;
+        me.app.showForm();
+    },
+    //
+    onTest: function() {
+        var me = this,
+            data = me.dataField.getValue();
+        // @todo: Validation
+        Ext.Ajax.request({
+            url: "/fm/classificationrule/test/",
+            method: "post",
+            scope: me,
+            jsonData: {
+                data: data,
+                patterns: me.app.getPatterns(),
+                event_class: me.app.currentRecord.get("event_class")
+            },
+            success: function(response) {
+                var me = this;
+                me.onSuccess(Ext.decode(response.responseText));
+            },
+            failure: function() {
+                NOC.error("Failed to test rule")
+            }
+        });
+    },
+    //
+    onSuccess: function(data) {
+        var me = this,
+            item = me.app.showItem(me.app.ITEM_TEST_RESULT);
+        item.preview({data: data});
+        console.log(data);
+    }
+});
--- fm/apps/classificationrule/views.py
+++ fm/apps/classificationrule/views.py
@@ -6,9 +6,14 @@
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 
+## Python modules
+import re
 ## NOC modules
 from noc.lib.app import ExtDocApplication, view
 from noc.fm.models import EventClassificationRule
+from noc.fm.models.eventclass import EventClass
+from noc.lib.validators import is_objectid
+from noc.fm.models import get_event
 
 
 class EventClassificationRuleApplication(ExtDocApplication):
@@ -25,3 +30,119 @@ class EventClassificationRuleApplication(ExtDocApplication):
     def api_json(self, request, id):
         rule = self.get_object_or_404(EventClassificationRule, id=id)
         return rule.to_json()
+
+    @view(url="^test/$", method=["POST"], access="test", api=True)
+    def api_test(self, request):
+        q = self.deserialize(request.raw_post_data)
+        errors = []
+        patterns = []
+        result = False
+        # Get data
+        data = None
+        vars = {}
+        required_vars = set()
+        r_patterns = []
+        event_class = None
+        if "data" in q:
+            if is_objectid(q["data"]):
+                event = get_event(q["data"])
+                if event:
+                    data = event.raw_vars.copy()
+                    data["profile"] = event.managed_object.profile_name
+                else:
+                    errors += ["Event not found: %s" % q["data"]]
+            else:
+                # Decode json
+                e = self.deserialize(q["data"])
+                if isinstance(e, list):
+                    e = e[0]
+                if not isinstance(e, dict) or "raw_vars" not in e:
+                    errors += ["Invalid JSON data"]
+                else:
+                    data = e["raw_vars"]
+                    if "profile" in e:
+                        data["profile"] = e["profile"]
+        # Check event class
+        if "event_class" in q:
+            event_class = self.get_object_or_404(EventClass,
+                                                 id=q["event_class"])
+            # @todo: Get required vars
+        # Check patterns
+        if "patterns" in q:
+            for p in q["patterns"]:
+                if "key_re" in p and "value_re" in p:
+                    k = None
+                    v = None
+                    try:
+                        k = re.compile(p["key_re"])
+                    except re.error, why:
+                        errors += [
+                            "Invalid key regular expression <<<%s>>>: %s" % (
+                                p["key_re"], why)]
+                    try:
+                        v = re.compile(p["value_re"])
+                    except re.error, why:
+                        errors += [
+                            "Invalid value regular expression <<<%s>>>: %s" % (
+                                p["value_re"], why)]
+                    if k and v:
+                        patterns += [(k, v)]
+        # Try to match rule
+        if patterns and not errors:
+            s_patterns = []
+            i_patterns = []
+            for pk, pv in patterns:
+                matched = False
+                for k in data:
+                    k_match = pk.search(k)
+                    if k_match:
+                        v_match = pv.search(data[k])
+                        if v_match:
+                            # Line match
+                            # Update vars
+                            v = {}
+                            v.update(k_match.groupdict())
+                            v.update(v_match.groupdict())
+                            vars.update(v)
+                            # Save patterns
+                            s_patterns += [{
+                                "key": k,
+                                "value": data[k],
+                                "key_re": pk.pattern,
+                                "value_re": pv.pattern,
+                                "vars": v
+                            }]
+                            matched = True
+                            break
+                if not matched:
+                    i_patterns = [
+                        {
+                            "key": None,
+                            "value": None,
+                            "key_re": pk.pattern,
+                            "value_re": pv.pattern,
+                            "vars": {}
+                        }
+                    ]
+            if s_patterns and not i_patterns:
+                result = True
+            r_patterns = s_patterns + i_patterns
+        # @todo: Fill event class template
+        if event_class:
+            subject = "XXX"
+            body = "YYY"
+        # Check expression
+        r = {
+            "result": result
+        }
+        if errors:
+            r["errors"] = errors
+        if vars:
+            r["vars"] = vars
+        if r_patterns:
+            r["patterns"] = r_patterns
+        if subject:
+            r["subject"] = subject
+        if body:
+            r["body"] = body
+        return r
