8d83b91499e7acfadbec53326dafc2b7ce19d141
Dmitry Volodin <dv@nocproject.org>
2013-08-21 12:49:20 +0400

    Fix typo
    --HG--
    branch : feature/fm-new-ui

--- lib/app/extdocapplication.py
+++ lib/app/extdocapplication.py
@@ -184,20 +184,20 @@ class ExtDocApplication(ExtApplication):
             if q:
                 if self.queryset(request).filter(**q).first():
                     return self.response(status=self.CONFLICT)
-            o = self.model()
-            for k, v in attrs.items():
-                if k != self.pk and "__" not in k:
-                    setattr(o, k, v)
-            o.save()
-            format = request.GET.get(self.format_param)
-            if format == "ext":
-                r = {
-                    "success": True,
-                    "data": self.instance_to_dict(o)
-                }
-            else:
-                r = self.instance_to_dict(o)
-            return self.response(r, status=self.CREATED)
+        o = self.model()
+        for k, v in attrs.items():
+            if k != self.pk and "__" not in k:
+                setattr(o, k, v)
+        o.save()
+        format = request.GET.get(self.format_param)
+        if format == "ext":
+            r = {
+                "success": True,
+                "data": self.instance_to_dict(o)
+            }
+        else:
+            r = self.instance_to_dict(o)
+        return self.response(r, status=self.CREATED)
 
     @view(method=["GET"], url="^(?P<id>[0-9a-f]{24}|\d+)/?$",
           access="read", api=True)
