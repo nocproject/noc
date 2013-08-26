a41abf4d5b1fffb47b397d4a503557d616edc7ba
Dmitry Volodin <dv@nocproject.org>
2013-08-26 22:38:31 +0400

    Show mongodb version in "System Version" report
    --HG--
    branch : develop

--- main/apps/reportsystemversion/views.py
+++ main/apps/reportsystemversion/views.py
@@ -2,35 +2,37 @@
 ##----------------------------------------------------------------------
 ## System Version Report
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2010 The NOC Project
+## Copyright (C) 2007-2013 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-from __future__ import with_statement
+
+## Python modules
+import os
+import sys
+## NOC modules
 from noc.lib.app.simplereport import *
 from noc.lib.version import get_version
-import os,sys,csv
-import django
-##
-##
-##
+from noc.lib.nosql import get_connection
+
+
 class ReportSystemVersion(SimpleReport):
-    title="System Version"
-    def get_data(self,**kwargs):
+    title = "System Version"
+
+    def get_data(self, **kwargs):
+        si = get_connection().server_info()
         versions=[
-            ["NOC"    ,    get_version()],
+            ["NOC", get_version()],
             SectionRow("Host Software"),
-            ["OS"     ,    " ".join(os.uname())],
-            ["Python" ,    sys.version],
+            ["OS", " ".join(os.uname())],
+            ["Python", sys.version],
             ["PostgreSQL", self.execute("SELECT VERSION()")[0][0]],
-            SectionRow("Contributed Software (contrib/)"),
+            ["MongoDB", "%s (%dbit)" % (si["version"], si["bits"])]
         ]
-        cv_path=os.path.join("contrib","lib","VERSION.csv")
-        if os.path.exists(cv_path):
-            with open(cv_path) as f:
-                r=csv.reader(f)
-                for row in r:
-                    versions+=[row]
         versions+=[SectionRow("Python Path")]
         for p in sys.path:
-            versions+=[("",p)]
-        return self.from_dataset(title=self.title,columns=["Component","Version"],data=versions)
+            versions += [("", p)]
+        return self.from_dataset(
+            title=self.title,
+            columns=["Component", "Version"],
+            data=versions
+        )
