451209318099790a8466d722830b87c29b8e48b6
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2013-09-03 10:18:46 +0300

    NOC-1120 Add some shapes
    --HG--
    branch : develop

--- inv/apps/map/js/SelectIconForm.js
+++ inv/apps/map/js/SelectIconForm.js
@@ -31,6 +31,8 @@ Ext.define("NOC.inv.map.SelectIconForm", {
                             xtype: "main.ref.stencil.LookupField",
                             name: "shape",
                             fieldLabel: "Shape",
+                            labelWidth: 40,
+                            minWidth: 300,
                             allowBlank: false
                         }
                     ]
