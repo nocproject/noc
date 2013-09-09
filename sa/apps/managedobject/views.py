77bd0c866c42f9df5814e4c11b3d61acd7dcbd88
Dmitry Volodin <dv@nocproject.org>
2013-09-09 18:05:47 +0400

    sa.managedobject: Links panel
    --HG--
    branch : feature/sa-managedobject

--- sa/apps/managedobject/views.py
+++ sa/apps/managedobject/views.py
@@ -10,8 +10,11 @@
 from noc.lib.app import ExtModelApplication, view
 from noc.sa.models import ManagedObject, ManagedObjectAttribute
 from noc.inv.models.link import Link
+from noc.inv.models.interface import Interface
+from noc.inv.models.pendinglinkcheck import PendingLinkCheck
 from noc.lib.app.modelinline import ModelInline
 from noc.lib.app.repoinline import RepoInline
+from mongoengine.queryset import Q as MQ
 
 
 class ManagedObjectApplication(ExtModelApplication):
@@ -55,6 +58,39 @@ class ManagedObjectApplication(ExtModelApplication):
                         "remote_object__label": ri.managed_object.name,
                         "remote_interface": str(ri.id),
                         "remote_interface__label": ri.name,
-                        "discovery_method": link.discovery_method
+                        "discovery_method": link.discovery_method,
+                        "commited": True,
+                        "local_description": li.description,
+                        "remote_description": ri.description
                     }]
+        # Get pending links
+        q = MQ(local_object=o.id) | MQ(remote_object=o.id)
+        for link in PendingLinkCheck.objects.filter(q):
+            if link.local_object.id == o.id:
+                ro = link.remote_object
+                lin = link.local_interface
+                rin = link.remote_interface
+            else:
+                ro = link.local_object
+                lin = link.remote_interface
+                rin = link.local_interface
+            li = Interface.objects.filter(managed_object=o.id, name=lin).first()
+            if not li:
+                continue
+            ri = Interface.objects.filter(managed_object=ro.id, name=rin).first()
+            if not ri:
+                continue
+            result += [{
+                "id": str(link.id),
+                "local_interface": str(li.id),
+                "local_interface__label": li.name,
+                "remote_object": ro.id,
+                "remote_object__label": ro.name,
+                "remote_interface": str(ri.id),
+                "remote_interface__label": ri.name,
+                "discovery_method": link.method,
+                "commited": False,
+                "local_description": li.description,
+                "remote_description": ri.description
+            }]
         return result
