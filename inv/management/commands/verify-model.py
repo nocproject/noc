ec4ea25fc4fb0d481c1c393e7e694790cf6f886c
Dmitry Volodin <dv@nocproject.org>
2013-10-06 18:54:32 +0400

    ./noc verify-model command
    --HG--
    branch : feature/inventory

---
+++ inv/management/commands/verify-model.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Link management CLI interface
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2012 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Python modules
+from optparse import OptionParser, make_option
+from collections import defaultdict
+## Django modules
+from django.core.management.base import BaseCommand, CommandError
+## NOC modules
+from noc.inv.models.objectmodel import ObjectModel
+
+
+class Command(BaseCommand):
+    help = "Verify models"
+
+    def handle(self, *args, **options):
+        CHECK_MAP = {
+            "Electrical | RJ45": self.check_ct_rj45,
+            "Electrical | Power | IEC 60320 C14": self.check_ct_c14
+        }
+        for m in ObjectModel.objects.all():
+            self.errors = []
+            for c in m.connections:
+                self.common_check(c)
+                check = CHECK_MAP.get(c.type.name)
+                if check:
+                    check(c)
+            if self.errors:
+                print "%s errors:" % m.name
+                for e in self.errors:
+                    print "    %s" % e
+
+    def e(self, connection, msg):
+        self.errors += ["%s: %s" % (connection.name, msg)]
+
+    def common_check(self, c):
+        if c.gender not in c.type.genders:
+            self.e(c, "Invalid gender '%s' for connection type '%s' (Must me one of '%s')" % (
+                c.gender, c.type.name, c.type.genders
+            ))
+
+    def check_ct_rj45(self, c):
+        if c.direction != "s":
+            self.e(c, "RJ45 must have direction 's' (has '%s')" % c.direction)
+        if not c.protocols:
+            self.e(c, "RJ45 must have at least one protocol")
+
+    def check_ct_c14(self, c):
+        if c.direction != "s":
+            self.e(c, "C14 must have direction 's' (has '%s')" % c.direction)
+        if not c.protocols:
+            self.e(c, "C14 must have at least one protocol")
