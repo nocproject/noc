48dff6696fd9591e8d5a74b4e8d2da6aab2f9c74
Dmitry Volodin <dv@nocproject.org>
2013-11-19 17:39:27 +0400

    inv.reportobjectsummary
    --HG--
    branch : feature/inventory

---
+++ inv/apps/reportobjectsummary/tests/test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## inv.reportobjectsummary unittests
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2013 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+from noc.lib.test import ReportApplicationTestCase
+
+
+class reportobjectsummaryTestCase(ReportApplicationTestCase):
+    pass
