0e7f2b229a30df9e9da809ac61f4501d96c8f0bb
Dmitry Volodin <dv@nocproject.org>
2013-12-01 15:10:32 +0400

    inv.inv: Change rack position
    --HG--
    branch : feature/inventory

---
+++ inv/apps/inv/js/plugins/rack/RackLoadPanel.js
@@ -0,0 +1,116 @@
+//---------------------------------------------------------------------
+// inv.inv.plugins.inventory RackLoadPanel
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2013 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.inv.inv.plugins.rack.RackLoadPanel");
+
+Ext.define("NOC.inv.inv.plugins.rack.RackLoadPanel", {
+    extend: "NOC.core.ApplicationPanel",
+    requires: [
+        "NOC.inv.inv.plugins.rack.RackLoadModel"
+    ],
+    app: null,
+    autoScroll: true,
+    layout: "fit",
+
+    initComponent: function() {
+        var me = this;
+
+        me.store = Ext.create("Ext.data.Store", {
+            model: "NOC.inv.inv.plugins.rack.RackLoadModel"
+        });
+
+        me.grid = Ext.create("Ext.grid.Panel", {
+            store: me.store,
+            columns: [
+                {
+                    text: "Name",
+                    dataIndex: "name",
+                    width: 100
+                },
+                {
+                    text: "Model",
+                    dataIndex: "model",
+                    width: 200
+                },
+                {
+                    text: "Units",
+                    dataIndex: "units",
+                    width: 50
+                },
+                {
+                    text: "Front pos",
+                    dataIndex: "position_front",
+                    width: 50,
+                    editor: "numberfield"
+                },
+                {
+                    text: "Rear pos",
+                    dataIndex: "position_rear",
+                    width: 50,
+                    editor: "numberfield"
+                }
+            ],
+            selType: "cellmodel",
+            plugins: [
+                Ext.create("Ext.grid.plugin.CellEditing", {
+                    clicksToEdit: 2
+                })
+            ],
+            listeners: {
+                scope: me,
+                validateedit: Ext.bind(me.onCellValidateEdit, me),
+                edit: Ext.bind(me.onCellEdit, me)
+            }
+        });
+
+        Ext.apply(me, {
+            items: [
+                me.grid
+            ]
+        });
+        me.callParent();
+    },
+    //
+    preview: function(data) {
+        var me = this;
+        me.store.loadData(data.content);
+    },
+    //
+    onCellValidateEdit: function(editor, e, eOpts) {
+        var me = this;
+        return true;
+    },
+    //
+    onCellEdit: function(editor, e) {
+        var me = this;
+        console.log("EDIT", arguments);
+        if(e.field == "position_front") {
+            e.record.set("position_rear", 0);
+        }
+        if(e.field == "position_rear") {
+            e.record.set("position_front", 0);
+        }
+        // Submit
+        Ext.Ajax.request({
+            url: "/inv/inv/" + me.app.currentId + "/plugin/rack/rackload/",
+            method: "POST",
+            scope: me,
+            jsonData: {
+                "cid": e.record.get("id"),
+                "position_front": e.record.get("position_front"),
+                "position_rear": e.record.get("position_rear")
+            },
+            loadMask: me,
+            success: function() {
+                NOC.info("Position has been changed");
+                // @todo: Reset dirty flag
+            },
+            failure: function() {
+                NOC.error("Failed to save");
+            }
+        });
+    }
+});
