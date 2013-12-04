45ed95facc20472a07e0de6f189c530a10268853
Dmitry Volodin <dv@nocproject.org>
2013-12-04 11:26:17 +0400

    inv.reportcompare: Report weight
    --HG--
    branch : feature/inventory

--- inv/apps/reportcompare/views.py
+++ inv/apps/reportcompare/views.py
@@ -20,13 +20,26 @@ class ReportCompareApplication(SimpleReport):
         for v in Vendor.objects.order_by("name"):
             vd = []
             for m in ObjectModel.objects.filter(
-                    vendor=v.id, data__management__managed=True):
+                    vendor=v.id, data__management__managed=True).order_by("name"):
+                ru = m.get_data("rackmount", "units")
+                if ru:
+                    ru = "%sU" % ru
+                else:
+                    ru = ""
+                weight = m.get_data("weight", "weight")
+                if weight:
+                    weight = str(weight)
+                    if m.get_data("weight", "is_recursive"):
+                        weight += "+"
+                else:
+                    weight = ""
                 vd += [(
                     m.name,
                     m.get_data("dimensions", "width") or "",
                     m.get_data("dimensions", "height") or "",
                     m.get_data("dimensions", "depth") or "",
-                    m.get_data("rackmount", "units") or ""
+                    ru,
+                    weight
                 )]
             if vd:
                 data += [SectionRow(name=v.name)]
@@ -35,7 +48,7 @@ class ReportCompareApplication(SimpleReport):
         return self.from_dataset(
             title=self.title,
             columns=[
-                "Model", "W", "H", "D", "RU"
+                "Model", "W", "H", "D", "RU", "Weight (kg)"
             ],
             data=data, enumerate=True
         )
