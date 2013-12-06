af3e55d96fbdd227db4099f237fdfd3ca9237949
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2013-12-06 11:52:44 +0200

    NOC-1129 Fix AlliedTelesis.AT9900.get_vlans script
    --HG--
    branch : develop

--- sa/profiles/AlliedTelesis/AT9900/get_vlans.py
+++ sa/profiles/AlliedTelesis/AT9900/get_vlans.py
@@ -2,32 +2,26 @@
 ##----------------------------------------------------------------------
 ## AlliedTelesis.AT9900.get_vlans
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2011 The NOC Project
+## Copyright (C) 2007-2013 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-
-## Python modules
-import re
-## NOC modules
+"""
+"""
 from noc.sa.script import Script as NOCScript
 from noc.sa.interfaces import IGetVlans
+import re
 
 
 class Script(NOCScript):
     name = "AlliedTelesis.AT9900.get_vlans"
     implements = [IGetVlans]
-    rx_vlan_name_line = re.compile(r"^\sName\s\.*\s(?P<name>\S+)", re.MULTILINE)
-    rx_vlan_id_line = re.compile(r"^\sIdentifier\s\.*\s(?P<vlan_id>\d{1,4})", re.MULTILINE)
+    rx_vlan = re.compile(r"Name \.+ (?P<vlanname>\S+)\n Identifier \.+ (?P<vlanid>\d+)\n")
 
     def execute(self):
-        vlans = self.cli("show vlan")
         r = []
-        for l in vlans.split("\n"):
-            match_name = self.rx_vlan_name_line.match(l)
-            if match_name:
-                name = match_name.group("name")
-                match_id = self.rx_vlan_id_line.match(l)
-                if match_id:
-                    vlan_id = int(match_id.group("vlan_id"))
-                    r += [{"name": name, "vlan_id": vlan_id}]
+        for match in self.rx_vlan.finditer(self.cli("show vlan")):
+            r.append({
+                "vlan_id": int(match.group('vlanid')),
+                "name": match.group('vlanname')
+            })
         return r
