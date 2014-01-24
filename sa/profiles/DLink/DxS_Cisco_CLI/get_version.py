6f6c97eba7c0c36c4b30ea917b976acf3f4bbf29
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2014-01-24 10:36:36 +0200

    Add suport to D-Link DGS-6600 series
    --HG--
    branch : develop

--- sa/profiles/DLink/DxS_Cisco_CLI/get_version.py
+++ sa/profiles/DLink/DxS_Cisco_CLI/get_version.py
@@ -21,14 +21,39 @@ class Script(NOCScript):
         r"\s+:\s+(?P<hversion>\S+?),?\s+System software version"
         r"\s+:\s+v(?P<version>\S+?),?\s+Release\(\d+\)",
         re.MULTILINE | re.DOTALL)
+    rx_ver1 = re.compile(
+        r"^(?P<platform>D\S+)\s+System Version\s*.+?"
+        r"Backplane H/W version:(?P<hversion>\S+)\s+.+?"
+        r"Serial#:(?P<serial>\S+)\s*\n", re.MULTILINE | re.DOTALL)
+    rx_ver2 = re.compile(
+        r"Bootloader:\s+(?P<bootprom>\S+)\s*\n\s+Runtime:\s+(?P<version>\S+)",
+        re.MULTILINE | re.DOTALL)
 
     def execute(self):
-        match = self.re_search(self.rx_ver, self.cli("show version"))
-        return {
-            "vendor": "DLink",
-            "platform": match.group("platform"),
-            "version": match.group("version"),
-            "attributes": {
-                "HW version": match.group("hversion")
+        c = self.cli("show version")
+        match = self.rx_ver.search(c)
+        if match:
+            return {
+                "vendor": "DLink",
+                "platform": match.group("platform"),
+                "version": match.group("version"),
+                "attributes": {
+                    "HW version": match.group("hversion")
+                }
             }
-        }
+        else:
+            match = self.rx_ver1.search(c)
+            if match:
+                match1 = self.rx_ver2.search(c)
+                return {
+                    "vendor": "DLink",
+                    "platform": match.group("platform"),
+                    "version": match1.group("version"),
+                    "attributes": {
+                        "Boot PROM": match1.group("bootprom"),
+                        "HW version": match.group("hversion"),
+                        "Serial Number": match.group("serial")
+                    }
+                }
+            else:
+                raise self.NotSupportedError()
