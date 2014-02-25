6e8ed4894b697e6835d23b193c017883f51250f1
Dmitry Volodin <dv@nocproject.org>
2014-02-25 07:52:56 +0400

    Fix UnicodeDecodeError during logging
    --HG--
    branch : develop

--- inv/models/object.py
+++ inv/models/object.py
@@ -129,7 +129,7 @@ class Object(Document):
         """
         c = self.get_p2p_connection(name)[0]
         if c:
-            self.log("'%s' disconnected" % name,
+            self.log(u"'%s' disconnected" % name,
                      system="CORE", op="DISCONNECT")
             c.delete()
 
@@ -188,11 +188,11 @@ class Object(Document):
             ],
             data=data
         ).save()
-        self.log("%s:%s -> %s:%s" % (self, name, remote_object, remote_name),
+        self.log(u"%s:%s -> %s:%s" % (self, name, remote_object, remote_name),
                  system="CORE", op="CONNECT")
         # Disconnect from container on o-connection
         if lc.direction == "o" and self.container:
-            self.log("Remove from %s" % self.container,
+            self.log(u"Remove from %s" % self.container,
                      system="CORE", op="REMOVE")
             self.container = None
             self.save()
@@ -231,7 +231,7 @@ class Object(Document):
             data=data or {},
             type=type or None
         ).save()
-        self.log("%s:%s -> %s:%s" % (self, name, remote_object, remote_name),
+        self.log(u"%s:%s -> %s:%s" % (self, name, remote_object, remote_name),
                  system="CORE", op="CONNECT")
 
     def put_into(self, container):
