93a2b5fc9ce158b70f3c5d4398a3d77c63e2036b
Dmitry Volodin <dv@nocproject.org>
2014-04-01 15:02:22 +0400

    project.project: Replace preview window with panel
    --HG--
    branch : develop

--- project/apps/project/js/ProjectResources.js
+++ project/apps/project/js/ProjectResources.js
@@ -7,31 +7,64 @@
 console.debug("Defining NOC.project.project.ProjectResources");
 
 Ext.define("NOC.project.project.ProjectResources", {
-    extend: "Ext.Window",
+    extend: "Ext.panel.Panel",
     app: null,
     vc: null,
     interfaces: [],
-    width: 600,
-    height: 400,
-    modal: true,
-    autoShow: true,
-    closable: true,
-    autoScroll: "auto",
-    maximizable: true,
+    layout: "fit",
+    autoScroll: true,
 
     initComponent: function() {
         var me = this;
+
+        me.closeButton = Ext.create("Ext.button.Button", {
+            text: "Close",
+            glyph: NOC.glyph.arrow_left,
+            scope: me,
+            handler: me.onClose
+        });
+
         Ext.apply(me, {
-            title: Ext.String.format("Allocated resources for {0}: {1}", me.project.get("code"), me.project.get("name")),
             items: [
                 {
                     xtype: "panel",
                     padding: 4,
                     layout: "fit",
-                    html: me.app.templates.AllocatedResources(me.data)
+                    html: ""
+                }
+            ],
+            dockedItems: [
+                {
+                    xtype: "toolbar",
+                    dock: "top",
+                    items: [
+                        me.closeButton,
+                        me.refreshButton
+                    ]
                 }
             ]
         });
         me.callParent();
+    },
+    //
+    preview: function(record) {
+        var me = this;
+        Ext.Ajax.request({
+            url: "/project/project/" + record.get("id") + "/resources/",
+            method: "GET",
+            scope: me,
+            success: function(response) {
+                var data = Ext.decode(response.responseText);
+                me.items.items[0].update(me.app.templates.AllocatedResources(data));
+            },
+            failure: function() {
+                NOC.error("Failed to get resources");
+            }
+        });
+    },
+    //
+    onClose: function() {
+        var me = this;
+        me.app.showForm();
     }
 });
