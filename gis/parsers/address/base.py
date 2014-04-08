2f63ebca2eea1dcd71f9e6ed3f96c3af2107bb6d
Dmitry Volodin <dv@nocproject.org>
2014-04-08 22:03:18 +0400

    FIAS parser: Download OKTMO archive and check for FIAS database
    --HG--
    branch : develop

--- gis/parsers/address/base.py
+++ gis/parsers/address/base.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
-## Parse and load FIAS data
+## Parse and load address data
 ##----------------------------------------------------------------------
 ## Copyright (C) 2007-2014 The NOC Project
 ## See LICENSE for details
@@ -8,6 +8,10 @@
 
 ## Python modules
 import logging
+import gzip
+import os
+## Third-party modules
+import requests
 ## NOC modules
 from noc.gis.models.division import Division
 
@@ -24,6 +28,9 @@ class AddressParser(object):
     def info(self, msg):
         print "[%s] %s" % (self.name, msg)
 
+    def error(self, msg):
+        print "[%s: ERROR] %s" % (self.name, msg)
+
     def get_top(self):
         rf = Division.objects.filter(type="A", name=self.TOP_NAME).first()
         if rf:
@@ -38,3 +45,25 @@ class AddressParser(object):
 
     def sync(self):
         pass
+
+    def download_file(self, url, path, auto_deflate=False):
+        self.info("Downloading %s" % url)
+        r = requests.get(url, stream=True)
+        size = int(r.headers["content-length"])
+        chunk_size = 4 * 1024 * 1024
+        self.info("    .... %d bytes" % size)
+        with open(path, "w") as f:
+            for chunk in r.iter_content(chunk_size=chunk_size):
+                if chunk:
+                    f.write(chunk)
+                    f.flush()
+        if auto_deflate:
+            o = os.path.splitext(path)[0]
+            with gzip.open(path, "rb") as f:
+                with open(o, "w") as ff:
+                    while True:
+                        chunk = f.read(chunk_size)
+                        if not chunk:
+                            break
+                        ff.write(chunk)
+            os.unlink(path)
