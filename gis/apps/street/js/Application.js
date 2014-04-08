d2185e15ac98f5af9f4008dd68574278e8b43d35
Dmitry Volodin <dv@nocproject.org>
2014-04-08 23:36:09 +0400

    gis.street application
    --HG--
    branch : develop

---
+++ gis/apps/street/js/Application.js
@@ -0,0 +1,95 @@
+//---------------------------------------------------------------------
+// gis.street application
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2014 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.gis.street.Application");
+
+Ext.define("NOC.gis.street.Application", {
+    extend: "NOC.core.ModelApplication",
+    requires: [
+        "NOC.gis.street.Model",
+        "NOC.gis.division.LookupField",
+        "Ext.ux.form.DictField"
+    ],
+    model: "NOC.gis.street.Model",
+    search: true,
+    initComponent: function() {
+        var me = this;
+
+        Ext.apply(me, {
+            columns: [
+                {
+                    text: "Short",
+                    dataIndex: "short_name",
+                    width: 50
+                },
+                {
+                    text: "Name",
+                    dataIndex: "name",
+                    width: 300
+                },
+                {
+                    text: "Active",
+                    dataIndex: "is_active",
+                    renderer: NOC.render.Bool,
+                    width: 25
+                },
+                {
+                    text: "Parent",
+                    dataIndex: "full_parent",
+                    flex: 1,
+                    sort: false
+                }
+            ],
+            fields: [
+                {
+                    name: "parent",
+                    xtype: "gis.division.LookupField",
+                    fieldLabel: "Parent"
+                },
+                {
+                    name: "name",
+                    xtype: "textfield",
+                    fieldLabel: "Name",
+                    allowBlank: false
+                },
+                {
+                    name: "short_name",
+                    xtype: "textfield",
+                    fieldLabel: "Short Name",
+                    allowBlank: true
+                },
+                {
+                    name: "is_active",
+                    xtype: "checkboxfield",
+                    boxLabel: "Active"
+                },
+                {
+                    xtype: "dictfield",
+                    name: "data",
+                    fieldLabel: "Data",
+                    allowBlank: true,
+                    columns: [
+                        {
+                            text: "Name",
+                            dataIndex: "name",
+                            width: 100,
+                            editor: "textfield",
+                            renderer: "htmlEncode"
+                        },
+                        {
+                            text: "Value",
+                            dataIndex: "value",
+                            flex: 1,
+                            editor: "textfield",
+                            renderer: "htmlEncode"
+                        }
+                    ]
+                }
+            ]
+        });
+        me.callParent();
+    }
+});
---
+++ gis/apps/street/js/Model.js
@@ -0,0 +1,57 @@
+//---------------------------------------------------------------------
+// gis.division Model
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2014 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.gis.street.Model");
+
+Ext.define("NOC.gis.street.Model", {
+    extend: "Ext.data.Model",
+    rest_url: "/gis/street/",
+
+    fields: [
+        {
+            name: "id",
+            type: "string"
+        },
+        {
+            name: "end_date",
+            type: "date"
+        },
+        {
+            name: "short_name",
+            type: "string"
+        },
+        {
+            name: "parent",
+            type: "string"
+        },
+        {
+            name: "parent__label",
+            type: "string"
+        },
+        {
+            name: "full_parent",
+            type: "string",
+            persist: false
+        },
+        {
+            name: "is_active",
+            type: "boolean",
+            defaultValue: true
+        },
+        {
+            name: "data",
+            type: "auto"
+        },
+        {
+            name: "start_date",
+            type: "date"
+        },
+        {
+            name: "name",
+            type: "string"
+        }
+    ]
+});
