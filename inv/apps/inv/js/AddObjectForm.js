ae60555ac23cc5c7d0f4683392a9f5587a608f0b
Dmitry Volodin <dv@nocproject.org>
2014-04-18 10:41:02 +0400

    inv.inv: Refactor add object form
    --HG--
    branch : develop

--- inv/apps/inv/js/AddObjectForm.js
+++ inv/apps/inv/js/AddObjectForm.js
@@ -1,25 +1,21 @@
 //---------------------------------------------------------------------
-// inv.inv AddGroupForm
+// inv.inv AddObject form
 //---------------------------------------------------------------------
-// Copyright (C) 2007-2013 The NOC Project
+// Copyright (C) 2007-2014 The NOC Project
 // See LICENSE for details
 //---------------------------------------------------------------------
-console.debug("Defining NOC.inv.inv.AddGroupForm");
+console.debug("Defining NOC.inv.inv.AddObjectForm");
 
-Ext.define("NOC.inv.inv.AddGroupForm", {
-    extend: "Ext.window.Window",
+Ext.define("NOC.inv.inv.AddObjectForm", {
+    extend: "Ext.panel.Panel",
     requires: ["NOC.inv.objectmodel.LookupField"],
-    modal: true,
-    autoShow: true,
-    closeable: true,
-    title: "Add Group",
-    groupContainer: null,
     app: null,
-    width: 400,
+    groupContainer: null,
 
     initComponent: function() {
         var me = this,
             title;
+
         if(me.groupContainer) {
             title = "Create new object in '" + me.groupContainer.get("name") + "'";
         } else {
@@ -35,44 +31,57 @@ Ext.define("NOC.inv.inv.AddGroupForm", {
             },
             items: [
                 {
-                    xtype: "textfield",
-                    name: "name",
-                    fieldLabel: "Name",
-                    allowBlank: false
-                },
-                {
                     xtype: "inv.objectmodel.LookupField",
                     name: "type",
                     fieldLabel: "Type",
                     allowBlank: false
-                }
-            ],
-            buttons: [
-                {
-                    text: "Close",
-                    scope: me,
-                    handler: me.onPressClose
                 },
                 {
-                    text: "Add",
-                    glyph: NOC.glyph.plus,
-                    scope: me,
-                    handler: me.onPressAdd
+                    xtype: "textfield",
+                    name: "name",
+                    fieldLabel: "Name",
+                    allowBlank: false
                 }
             ]
         });
 
         Ext.apply(me, {
             title: title,
-            items: [me.form]
+            items: [me.form],
+            dockedItems: [
+                {
+                    xtype: "toolbar",
+                    dock: "top",
+                    items: [
+                        {
+                            text: "Close",
+                            scope: me,
+                            glyph: NOC.glyph.arrow_left,
+                            handler: me.onPressClose
+                        },
+                        {
+                            text: "Add",
+                            glyph: NOC.glyph.plus,
+                            scope: me,
+                            handler: me.onPressAdd
+                        }
+                    ]
+                }
+            ]
         });
         me.callParent();
     },
-    //
+
+    setContainer: function(c) {
+        var me = this;
+        me.groupContainer = c;
+    },
+
     onPressClose: function() {
         var me = this;
-        me.close();
+        me.app.showItem(me.app.ITEM_MAIN);
     },
+
     onPressAdd: function() {
         var me = this,
             values = me.form.getValues();
@@ -86,7 +95,7 @@ Ext.define("NOC.inv.inv.AddGroupForm", {
             },
             scope: me,
             success: function() {
-                me.close();
+                me.app.showItem(me.app.ITEM_MAIN);
                 if(me.groupContainer) {
                     me.app.store.reload({node: me.groupContainer});
                 } else {
@@ -98,4 +107,5 @@ Ext.define("NOC.inv.inv.AddGroupForm", {
             }
         });
     }
+
 });
