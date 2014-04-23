0e37543322185d38df598e60b3be89e814b38e56
Dmitry Volodin <dv@nocproject.org>
2014-04-23 11:38:59 +0400

    ./noc notebook command to run IPython Notebook
    --HG--
    branch : develop

---
+++ main/management/commands/notebook.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Run Shell within IPython notebook
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2014 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Django modules
+from django.core.management.base import BaseCommand, CommandError
+from optparse import make_option
+
+
+class Command(BaseCommand):
+    help = "Run Notebook"
+
+    def handle(self, *args, **options):
+        from django.conf import settings
+        from django.db.models.loading import get_models
+        get_models()
+        try:
+            from IPython.html.notebookapp import NotebookApp
+        except ImportError:
+            from IPython.frontend.html.notebook import notebookapp
+            NotebookApp = notebookapp.NotebookApp
+
+        app = NotebookApp.instance()
+        ipython_arguments = getattr(
+            settings, "IPYTHON_ARGUMENTS",
+            ["--ext", "django_extensions.management.notebook_extension"]
+        )
+        app.initialize(ipython_arguments)
+        app.start()
