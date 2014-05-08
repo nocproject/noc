12afb2f1880bb0c3ab4d473e08baf67a7f654e77
Dmitry Volodin <dv@nocproject.org>
2014-05-08 19:36:56 +0400

    ./noc notebook: Check ipython is installed
    --HG--
    branch : develop

--- main/management/commands/notebook.py
+++ main/management/commands/notebook.py
@@ -6,6 +6,8 @@
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 
+## Third-party modules
+import pkg_resources
 ## Django modules
 from django.core.management.base import BaseCommand, CommandError
 from optparse import make_option
@@ -17,6 +19,12 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         from django.conf import settings
         from django.db.models.loading import get_models
+        ## Check ipython is installed
+        try:
+            pkg_resources.require("ipython")
+        except pkg_resources.DistributionNotFound:
+            raise CommandError("ipython is not installed!\nPlease install ipython with:\n    ./bin/pip install ipython")
+        ## Load models
         get_models()
         try:
             from IPython.html.notebookapp import NotebookApp
