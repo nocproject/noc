379deb6663c0754521bfc737207e015f86acf233
Dmitry Volodin <dv@nocproject.org>
2014-05-10 01:09:03 +0400

    Vyatta.Vyatta.reboot
    --HG--
    branch : develop

--- sa/profiles/Vyatta/Vyatta/__init__.py
+++ sa/profiles/Vyatta/Vyatta/__init__.py
@@ -3,21 +3,24 @@
 ## Vendor: Vyatta
 ## OS:     Vyatta
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2010 The NOC Project
+## Copyright (C) 2007-2014 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-"""
-"""
+
+## NOC modules
 import noc.sa.profiles
-from noc.sa.protocols.sae_pb2 import TELNET,SSH
+from noc.sa.protocols.sae_pb2 import TELNET, SSH
+
 
 class Profile(noc.sa.profiles.Profile):
-    name="Vyatta.Vyatta"
-    supported_schemes=[TELNET,SSH]
-    pattern_username=r"[Ll]ogin: (?!\S+)"
-    pattern_prompt=r"^(?P<username>\S+)@(?P<hostname>\S+):[^$]+\$ "
-    pattern_more=r"^:"
-    command_more=" "
-    command_disable_pager="terminal length 0"
-    command_enter_config="configure"
-    command_leave_config="commit\nexit"
+    name = "Vyatta.Vyatta"
+    supported_schemes = [TELNET, SSH]
+    pattern_username = r"[Ll]ogin: (?!\S+)"
+    pattern_prompt = r"^(?P<username>\S+)@(?P<hostname>\S+):[^$]+\$ "
+    pattern_more = [
+        (r"^:", " "),
+        (r"\[confirm\]", "\n")
+    ]
+    command_disable_pager = "terminal length 0"
+    command_enter_config = "configure"
+    command_leave_config = "commit\nexit"
