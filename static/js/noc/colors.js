986ddba9eb392c6cca20a8280d093192cdf4037c
Dmitry Volodin <dv@nocproject.org>
2014-06-16 13:50:17 +0400

    NOC.colors helper for centralized colors reference
    --HG--
    branch : develop

---
+++ static/js/noc/colors.js
@@ -0,0 +1,30 @@
+//---------------------------------------------------------------------
+// NOC colors
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2014 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+
+Ext.ns("NOC.colors");
+//---------------------------------------------------------------------
+// Base colors
+//
+NOC.colors.clouds = "#ecf0f1";
+NOC.colors.silver = "#bdc3c7";
+NOC.colors.wetasphalt = "#34495e";
+NOC.colors.midnightblue = "#2c3e50";
+NOC.colors.concrete = "#95a5a6";
+NOC.colors.asbestos = "#7f8c8d";
+NOC.colors.pomegranate = "#c0392b";
+NOC.colors.emerald = "#2ecc71";
+NOC.colors.piterriver = "#3498db";
+NOC.colors.sunflower = "#f1c40f";
+NOC.colors.black = "#232323";
+NOC.colors.while = "#ffffff";
+
+//---------------------------------------------------------------------
+// Actions
+//
+NOC.colors.add = NOC.colors.emerald;
+NOC.colors.delete = NOC.colors.pomegranate;
+NOC.colors.view = NOC.colors.sunflower;
--- static/js/noc/ux/form/MultiIntervalField.js
+++ static/js/noc/ux/form/MultiIntervalField.js
@@ -35,14 +35,14 @@ Ext.define("Ext.ux.form.MultiIntervalField", {
                         {
                             tooltip: "Add",
                             glyph: NOC.glyph.plus_sign,
-                            color: "#2ecc71",
+                            color: NOC.colors.add,
                             scope: me,
                             handler: me.onAddRecord
                         },
                         {
                             tooltip: "Remove",
                             glyph: NOC.glyph.minus_sign,
-                            color: "#c0392b",
+                            color: NOC.colors.delete,
                             scope: me,
                             handler: me.onRemoveRecord
                         }
