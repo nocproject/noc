76f95c5ed12d99a3312d06df0e44fc8ca7e07035
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2014-07-16 10:22:04 +0300

    Fix DLink.DGS3100.get_portchannel script
    --HG--
    branch : develop

--- sa/profiles/DLink/DGS3100/get_portchannel.py
+++ sa/profiles/DLink/DGS3100/get_portchannel.py
@@ -2,7 +2,7 @@
 ##----------------------------------------------------------------------
 ## DLink.DGS3100.get_portchannel
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2011 The NOC Project
+## Copyright (C) 2007-2014 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 """
@@ -15,7 +15,14 @@ import re
 class Script(NOCScript):
     name = "DLink.DGS3100.get_portchannel"
     implements = [IGetPortchannel]
-    rx_trunk = re.compile(r"Group ID\s+:\s+(?P<trunk>\d+).+?Type\s+:\s+(?P<type>\S+).+?Member Port\s+:\s+(?P<members>\S+).+?Status\s+:\s+(?P<status>\S+)", re.MULTILINE | re.DOTALL)
+    rx_trunk = re.compile(
+        r"Group ID\s+:\s+(?P<trunk>\d+).+?Type\s+:\s+(?P<type>\S+).+?"
+        r"Member Port\s+:\s+(?P<members>\S+).+?Status\s+:\s+(?P<status>\S+)",
+        re.MULTILINE | re.DOTALL)
+    rx_trunk1 = re.compile(
+        r"Group ID\s+:\s+(?P<trunk>\d+)\n"
+        r"Member Port\s+:\s+(?P<members>\S+).+?Status\s+:\s+(?P<status>\S+)",
+        re.MULTILINE | re.DOTALL)
 
     def execute(self):
         try:
@@ -29,5 +36,12 @@ class Script(NOCScript):
                     "interface": "T%s" % match.group("trunk"),
                     "members": self.expand_rangelist(match.group("members")),
                     "type": "L" if match.group("type").lower() == "lacp" else "S"
+                    }]
+        for match in self.rx_trunk1.finditer(t):
+            if match.group("status").lower() == "enabled":
+                r += [{
+                    "interface": "T%s" % match.group("trunk"),
+                    "members": self.expand_rangelist(match.group("members")),
+                    "type": "S"
                     }]
         return r
