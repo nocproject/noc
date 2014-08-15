d9eee817214bf9487094b996d29d5774f066f66e
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2014-08-15 18:48:13 +0300

    NOC-1411 Update DLink.DGS3100 profile
    --HG--
    branch : develop

--- sa/profiles/DLink/DGS3100/get_interface_status.py
+++ sa/profiles/DLink/DGS3100/get_interface_status.py
@@ -2,7 +2,7 @@
 ##----------------------------------------------------------------------
 ## DLink.DGS3100.get_interface_status
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2011 The NOC Project
+## Copyright (C) 2007-2014 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 """
@@ -15,7 +15,10 @@ import re
 class Script(NOCScript):
     name = "DLink.DGS3100.get_interface_status"
     implements = [IGetInterfaceStatus]
-    rx_line = re.compile(r"^\s*(?P<interface>\S+)\s+(Enabled|Disabled)\s+\S+\s+(?P<status>.+)\s+(Enabled|Disabled)\s*$", re.IGNORECASE | re.MULTILINE)
+    rx_line = re.compile(
+        r"^\s*(?P<interface>\S+)\s+(Enabled|Disabled)\s+\S+\s+"
+        r"(?P<status>.+)\s+(Enabled|Disabled)\s*$",
+        re.IGNORECASE | re.MULTILINE)
 
     def execute(self, interface=None):
         # Not tested. Must be identical in different vendors
@@ -24,11 +27,14 @@ class Script(NOCScript):
                 # Get interface status
                 r = []
                 # IF-MIB::ifName, IF-MIB::ifOperStatus
-                for n, s in self.snmp.join_tables("1.3.6.1.2.1.31.1.1.1.1", "1.3.6.1.2.1.2.2.1.8", bulk=True):
-                    if not n.startswith("802.1Q Encapsulation Tag") \
-                    and (interface is not None and interface == n):
-                        # ifOperStatus up(1)
-                        r += [{"interface":n, "status":int(s) == 1}]
+                for n, s in self.snmp.join_tables(
+                        "1.3.6.1.2.1.31.1.1.1.1",
+                        "1.3.6.1.2.1.2.2.1.8", bulk=True):
+                    if not n.startswith("802.1Q Encapsulation Tag"):
+                        if interface is not None and interface == n:
+                            r += [{"interface": n, "status": int(s) == 1}]
+                        else:
+                            r += [{"interface": n, "status": int(s) == 1}]
                 return r
             except self.snmp.TimeOutError:
                 pass
@@ -45,5 +51,5 @@ class Script(NOCScript):
             r += [{
                 "interface": match.group("interface"),
                 "status": match.group("status").strip() != "Link Down"
-                }]
+            }]
         return r
