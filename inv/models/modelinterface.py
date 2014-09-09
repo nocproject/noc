331dbab481778776dece6f21ec378843a7120654
Dmitry Volodin <dv@nocproject.org>
2014-09-09 10:49:44 +0400

    ModelInterfaceAttr.clean() conflicts with mongoengine's one
    --HG--
    branch : feature/graphite

--- inv/models/modelinterface.py
+++ inv/models/modelinterface.py
@@ -53,7 +53,7 @@ class ModelInterfaceAttr(EmbeddedDocument):
             self.is_const == v.is_const
         )
 
-    def clean(self, value):
+    def _clean(self, value):
         return getattr(self, "clean_%s" % self.type)(value)
 
     def clean_str(self, value):
@@ -143,5 +143,5 @@ class ModelInterface(Document):
             v = d[i_name]
             for a in mi.attrs:
                 if a.name in v:
-                    v[a.name] = T_MAP[a.type].clean(v[a.name])
+                    v[a.name] = T_MAP[a.type]._clean(v[a.name])
         return d
