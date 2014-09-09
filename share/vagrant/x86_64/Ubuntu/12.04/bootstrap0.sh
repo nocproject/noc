478bc6c739f8bfbf97048952638860b1d7bd145a
Dmitry Volodin <dv@nocproject.org>
2014-09-09 16:29:16 +0400

    Debian/Ubuntu bootstrap: libffi dependency
    --HG--
    branch : feature/graphite

--- share/vagrant/x86_64/Ubuntu/12.04/bootstrap0.sh
+++ share/vagrant/x86_64/Ubuntu/12.04/bootstrap0.sh
@@ -73,6 +73,8 @@ aptinstall python-dev
 aptinstall python-virtualenv
 aptinstall libgmp10
 aptinstall libgmp-dev
+aptinstall libffi
+aptinstall libffi-dev
 aptinstall nginx
 aptinstall postgresql
 aptinstall libpq-dev
