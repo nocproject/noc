7d3efa50345f73ebb68e3e7a904c61ec6ad0e290
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2014-09-09 09:59:00 +0300

    NOC-1394 Partially update Alentis.NetPing profile
    --HG--
    branch : develop

--- sa/profiles/Alentis/NetPing/get_inventory.py
+++ sa/profiles/Alentis/NetPing/get_inventory.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
-## Alentis.NetPing.get_version
+## Alentis.NetPing.get_inventory
 ##----------------------------------------------------------------------
 ## Copyright (C) 2007-2014 The NOC Project
 ## See LICENSE for details
@@ -10,23 +10,23 @@
 import re
 ## NOC modules
 from noc.sa.script import Script as NOCScript
-from noc.sa.interfaces import IGetVersion
+from noc.sa.interfaces import IGetInventory
 
 
 class Script(NOCScript):
-    name = "Alentis.NetPing.get_version"
-    implements = [IGetVersion]
+    name = "Alentis.NetPing.get_inventory"
+    implements = [IGetInventory]
     cache = True
 
     rx_snmp = re.compile(
-        r"^(?P<platform>\S+), FW v(?P<version>\S+)$")
+        r"^(?P<platform>\S+), FW \S+$")
 
     rx_plat = re.compile(
         r"^var devname='+(?P<platform>.+)+';$",
         re.MULTILINE)
 
-    rx_ver = re.compile(
-        r"^var fwver='v+(?P<version>\S+)+';$",
+    rx_rev = re.compile(
+        r"^var hwmodel=\d+;var hwver=+(?P<revision>\d+)+;",
         re.MULTILINE)
 
     def execute(self):
@@ -35,14 +35,12 @@ class Script(NOCScript):
             try:
                 ver = self.snmp.get("1.3.6.1.2.1.1.1.0", cached=True)
                 match = self.rx_snmp.search(ver)
-                if match:
-                    platform = match.group("platform")
-                    version = match.group("version")
-                return {
-                        "vendor": "Alentis",
-                        "platform": platform,
-                        "version": version
-                        }
+                return [{
+                    "type": "CHASSIS",
+                    "number": "1",
+                    "vendor": "Alentis",
+                    "part_no": [match.group("platform")]
+                }]
             except self.snmp.TimeOutError:
                 pass
 
@@ -50,16 +48,18 @@ class Script(NOCScript):
         data = self.http.get("/devname.cgi")
         match = self.rx_plat.search(data)
         platform = match.group("platform")
-        match = self.rx_ver.search(data)
-        version = match.group("version")
+        match = self.rx_rev.search(data)
+        revision = match.group("revision")
 
         data = self.profile.var_data(self, "/setup_get.cgi")
 
-        return {
-                "vendor": "Alentis",
-                "platform": platform,
-                "version": version,
-                "attributes": {
-                        "Serial Number": data["serialnum"]
-                    }
-                }
+        return [{
+            "type": "CHASSIS",
+            "number": "1",
+            "builtin": False,
+            "vendor": "Alentis",
+            "part_no": [platform],
+            "revision": revision,
+            "serial": data["serialnum"],
+            "description": data["location"].encode('UTF8')
+        }]
