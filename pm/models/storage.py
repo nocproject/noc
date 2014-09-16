ef14c994f8cc99d01c59e2c3f3629bd3ae67971b
Dmitry Volodin <dv@nocproject.org>
2014-09-16 21:43:02 +0400

    Update ProbeConfig on Storage change
    --HG--
    branch : feature/graphite

--- pm/models/storage.py
+++ pm/models/storage.py
@@ -7,6 +7,7 @@
 ##----------------------------------------------------------------------
 
 ## Third-party modules
+import mongoengine.signals
 from mongoengine.document import Document, EmbeddedDocument
 from mongoengine.fields import (ListField, EmbeddedDocumentField,
                                 StringField, BooleanField, IntField)
@@ -95,3 +96,10 @@ class Storage(Document):
                 for c in selectable
             ]
         }
+
+##
+from probeconfig import ProbeConfig
+mongoengine.signals.post_save.connect(
+    ProbeConfig.on_change_storage,
+    sender=Storage
+)
