1cf04fc120f8b192174f5518083ee9b6d74b99ef
Dmitry Volodin <dv@nocproject.org>
2014-09-30 08:52:09 +0400

    ColorFormatter has been moved to noc.lib.log
    --HG--
    branch : feature/graphite

--- lib/log.py
+++ lib/log.py
@@ -20,3 +20,58 @@ class PrefixLoggerAdapter(logging.LoggerAdapter):
 
     def process(self, msg, kwargs):
         return self.pattern % msg, kwargs
+
+
+class ColorFormatter(logging.Formatter):
+    """
+    Colored terminal formatter
+    """
+    DEFAULT_LOG_COLORS = {
+        logging.DEBUG: 4,  # Blue
+        logging.INFO: 2,  # Green
+        logging.WARNING: 3,  # Yellow
+        logging.ERROR: 1,  # Red
+    }
+
+    def __init__(self, *args, **kwargs):
+        self._colors = {}
+        self._end_color = ""
+        self.setup_colors()
+        super(ColorFormatter, self).__init__(*args, **kwargs)
+
+    def format(self, record):
+        def safe_unicode(s):
+            try:
+                return unicode(s)
+            except UnicodeDecodeError:
+                return repr(s)
+
+        try:
+            message = record.getMessage()
+            assert isinstance(message, (str, unicode))
+            record.message = safe_unicode(message)
+        except Exception as e:
+            record.message = "Bad message (%r): %r" % (e, record.__dict__)
+        record.asctime = self.formatTime(record, self.datefmt)
+        record.color = ""
+        if record.levelno in self._colors:
+            record.color = self._colors[record.levelno]
+        record.endcolor = self._end_color
+        formatted = self._fmt % record.__dict__
+        return formatted.replace("\n", "\n    ")
+
+    def setup_colors(self):
+        """
+        Set up terminal colors
+        """
+        import curses
+        self._colors = {}
+        fg_color = (curses.tigetstr("setaf") or
+                    curses.tigetstr("setf") or
+                    "")
+        for level in self.DEFAULT_LOG_COLORS:
+            self._colors[level] = unicode(
+                curses.tparm(fg_color, self.DEFAULT_LOG_COLORS[level]),
+                "ascii"
+            )
+        self._end_color = unicode(curses.tigetstr("sgr0"), "ascii")
