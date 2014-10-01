1012d2b015aedc918c115d16f62f7f6f2db2eb52
Dmitry Volodin <dv@nocproject.org>
2014-10-01 15:16:22 +0400

    Move DNS to the new synchronization framework
    --HG--
    branch : feature/graphite/common-sync

--- dns/models/dnsserver.py
+++ dns/models/dnsserver.py
@@ -10,7 +10,8 @@
 from django.utils.translation import ugettext_lazy as _
 from django.db import models
 ## NOC modules
-from noc.lib.fields import INETField
+from noc.lib.fields import INETField, DocumentReferenceField
+from noc.main.models.sync import Sync
 
 
 class DNSServer(models.Model):
@@ -32,13 +33,7 @@ class DNSServer(models.Model):
     ip = INETField(_("IP"), null=True, blank=True)
     description = models.CharField(_("Description"), max_length=128,
         blank=True, null=True)
-    sync_channel = models.CharField(_("Sync channel"),
-        max_length=64, blank=True, null=True)
+    sync = DocumentReferenceField(Sync, blank=True, null=True)
 
     def __unicode__(self):
         return self.name
-
-    def save(self, *args, **kwargs):
-        super(DNSServer, self).save(*args, **kwargs)
-        from dnszone import DNSZone
-        DNSZone.touch(self.name)
