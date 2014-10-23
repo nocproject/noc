e9b31bedfbed504acd8651c35b10d3ecd85c7744
Dmitry Volodin <dv@nocproject.org>
2014-10-23 14:58:48 +0400

    Daemon can report performance metrics to remote Graphite UDP collector
    --HG--
    branch : feature/graphite

--- lib/perf.py
+++ lib/perf.py
@@ -10,11 +10,15 @@
 import time
 import os
 import logging
+import socket
 
 logger = logging.getLogger(__name__)
 
 ENABLE_STATS = False
 BASE_DIR = None
+REPORT_COLLECTOR = None
+REPORT_INTERVAL = None
+METRICS_PER_PACKET = 15
 
 metrics = {}
 
@@ -137,14 +141,53 @@ class MetricsHub(object):
             self.__dict__[key].set(value)
 
 
-def enable_stats(enabled=True, base_dir=None):
-    global ENABLE_STATS, BASE_DIR
+def enable_stats(enabled=True, base_dir=None,
+                 report_collector=None, report_interval=60):
+    global ENABLE_STATS, BASE_DIR, REPORT_COLLECTOR, REPORT_INTERVAL
     ENABLE_STATS = enabled
+    logger.info("Stats are %s", "enabled" if enabled else "disabled")
     if base_dir:
+        logger.info("Timing base directory is %s", base_dir)
         BASE_DIR = base_dir
+    if report_collector:
+        logger.info(
+            "Reporting performance metrics to udp://%s every %s secords",
+            report_collector, report_interval
+        )
+        addr, port = report_collector.split(":")
+        REPORT_COLLECTOR = (addr, int(port))
+        REPORT_INTERVAL = report_interval
+        # Run reporter thread
+        import threading
+        logger.info("Running reporter thread")
+        t = threading.Thread(name="perf", target=reporter)
+        t.daemon = True
+        t.start()
 
 
 def dump():
     """Dump all metrics"""
     for m in sorted(metrics):
         print "%s: %s" % (m, metrics[m].get())
+
+
+def reporter():
+    """Report thread handler"""
+    global REPORT_COLLECTOR, REPORT_INTERVAL
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    while REPORT_COLLECTOR:
+        time.sleep(REPORT_INTERVAL)
+        ts = int(time.time())
+        data = [
+            "%s %s %s\n" % (m, metrics[m].get(), ts)
+            for m in metrics
+        ]
+        while data:
+            msg = "".join(data[:METRICS_PER_PACKET])
+            data = data[METRICS_PER_PACKET:]
+            logger.debug("Sending to udp://%s:\n%s",
+                         REPORT_COLLECTOR, msg)
+            try:
+                sock.sendto(msg, REPORT_COLLECTOR)
+            except socket.error, why:
+                logger.info("Socket error: %s", why)
