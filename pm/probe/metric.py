be5cca320c1786e83bd5fbeddd846d084f2c7245
Dmitry Volodin <dv@nocproject.org>
2014-10-25 17:25:37 +0400

    Better SNMP counter stepback handling
    --HG--
    branch : feature/graphite

--- pm/probe/metric.py
+++ pm/probe/metric.py
@@ -7,11 +7,14 @@
 ##----------------------------------------------------------------------
 
 ## Python modules
+import logging
+## NOC modules
 from policy import FeedPolicy
-
 
 MAX32 = 0xFFFFFFFFL
 MAX64 = 0xFFFFFFFFFFFFFFFFL
+
+logger = logging.getLogger(__name__)
 
 
 class Metric(object):
@@ -23,10 +26,12 @@ class Metric(object):
         self.convert = None
         self.last_value = None
         self.last_time = None
+        self.last_result = None
         self.convert = None
         self.scale = None
         self.cvt = None
         self.policy = FeedPolicy()
+        self.max_counter = None
         self.locked_convert = False  # Convert changed by set_convert
 
     def configure(self, metric, metric_type, thresholds, convert,
@@ -48,6 +53,7 @@ class Metric(object):
         r = self.cvt(t, v)
         self.last_time = t
         self.last_value = v
+        self.last_result = r
         if r is not None:
             r *= self.scale
             self.daemon.sender.feed(self.policy, self.metric, int(t), r)
@@ -64,10 +70,27 @@ class Metric(object):
         if not self.last_time or not self.last_value:
             return None
         if v < self.last_value:
-            # Counter wrap
-            mc = MAX64 if self.last_value >= MAX32 else MAX32
-            self.last_value -= mc
-        return (v - self.last_value) / (t - self.last_time)
+            # Counter decreased, either wrap or stepback
+            if self.max_counter is None:
+                self.max_counter = MAX64 if self.last_value >= MAX32 else MAX32
+            # Direct distance
+            d_direct = self.last_value - v
+            # Counter wrap distance
+            d_wrap = v + (self.max_counter - self.last_value)
+            if d_direct < d_wrap:
+                # Possible counter stepback
+                logger.info(
+                    "Possible counter stepback for %s: %s -> %s",
+                    self.metric, self.last_value, v
+                )
+                # Repeat last result, even if None
+                return self.last_result
+            else:
+                # Counter wrap
+                return d_wrap / (t - self.last_time)
+        else:
+            # Normal counter increment
+            return (v - self.last_value) / (t - self.last_time)
 
     def convert_derive(self, t, v):
         if not self.last_time or not self.last_value:
