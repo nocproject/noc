2c5c471ae9a2234d460200170715e537614a426e
Dmitry Volodin <dv@nocproject.org>
2014-11-06 15:53:37 +0300

    Slow Operations handled asynchronously when called from web interface
    --HG--
    branch : feature/graphite

--- lib/app/extapplication.py
+++ lib/app/extapplication.py
@@ -16,6 +16,7 @@ from application import Application, view
 from access import HasPerm, PermitLogged
 from noc.lib.serialize import json_decode, json_encode
 from noc.main.models.favorites import Favorites
+from noc.main.models.slowop import SlowOp
 
 
 class ExtApplication(Application):
@@ -229,6 +230,39 @@ class ExtApplication(Application):
             Favorites(user=request.user, app=self.app_id,
                 favorites=[item]).save()
         return True
+
+    @view(url="^futures/(?P<f_id>[0-9a-f]{24})/$", method=["GET"],
+          access="launch", api=True)
+    def api_future_status(self, request, f_id):
+        op = self.get_object_or_404(SlowOp, id=f_id,
+                                    app_id=self.get_app_id(),
+                                    user=request.user.username)
+        if op.is_ready():
+            # Note: the slow operation will be purged by TTL index
+            result = op.result()
+            if isinstance(result, Exception):
+                return self.render_json({
+                    "success": False,
+                    "message": "Error",
+                    "traceback": str(result)
+                }, status=self.INTERNAL_ERROR)
+            else:
+                return result
+        else:
+            return self.response_accepted(request.path)
+
+    def submit_slow_op(self, request, fn, *args, **kwargs):
+        f = SlowOp.submit(
+            fn,
+            self.get_app_id(), request.user.username,
+            *args, **kwargs
+        )
+        if f.done():
+            return f.result()
+        else:
+            return self.response_accepted(
+                location="%sfutures/%s/" % (self.base_url, f.slow_op.id)
+            )
 
     @view(url="^templates/(?P<name>[0-9a-zA-Z_/]+)\.js$", access=True)
     def view_template(self, request, name):
