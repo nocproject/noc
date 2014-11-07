a27a1c55bf0d2fa333525bdef9b66bccd17cf221
Dmitry Volodin <dv@nocproject.org>
2014-11-08 01:32:16 +0300

    Better CHOICES BER handling. SNMP v2 GETNEXT request builder
    --HG--
    branch : feature/graphite

---
+++ lib/snmp/consts.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Various SNMP constants
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2013 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Versions
+SNMP_v1 = 0
+SNMP_v2c = 1
+SNMP_v2p = 2
+SNMP_v2u = 2
+## PDU Types
+PDU_GET_REQUEST = 0
+PDU_GETNEXT_REQUEST = 1
+PDU_RESPONSE = 2
+PDU_GETBULK_REQUEST = 5
