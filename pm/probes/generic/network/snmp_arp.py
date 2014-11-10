58a678f3f4abfa12b052def6c5f743d89ec9dfc8
Dmitry Volodin <dv@nocproject.org>
2014-11-10 22:35:21 +0300

    SNMPARPProbe fix
    --HG--
    branch : feature/graphite

--- pm/probes/generic/network/snmp_arp.py
+++ pm/probes/generic/network/snmp_arp.py
@@ -1,5 +1,5 @@
 # # -*- coding: utf-8 -*-
-##----------------------------------------------------------------------
+# #----------------------------------------------------------------------
 ## Generic SNMP ARP Probes
 ##----------------------------------------------------------------------
 ## Copyright (C) 2007-2014 The NOC Project
@@ -17,15 +17,19 @@ class SNMPARPProbe(Probe):
             preference=metric.PREF_COMMON)
     def get_object_arp_rfc1213(self, address, snmp__ro, caps):
         return self.snmp_count(
-                mib["RFC1213-MIB::ipNetToMediaPhysAddress"],
-                address, community=snmp__ro)
+            mib["RFC1213-MIB::ipNetToMediaPhysAddress"],
+            address, community=snmp__ro,
+            bulk=caps.get("SNMP | Bulk", False)
+        )
 
     @metric("IP | ARP | Count",
             caps=["SNMP"],
             preference=metric.PREF_COMMON + metric.PREF_BETTER)
     def get_interface_arp_rfc1213(self, address, snmp__ro,
-                                  interface_ifindex, caps):
-        filter = lambda oid, value: int(oid.split(".")[-5]) == interface_ifindex
+                                  interface__ifindex, caps):
+        filter = lambda oid, value: int(oid.split(".")[-5]) == interface__ifindex
         return self.snmp_count(
-                mib["RFC1213-MIB::ipNetToMediaPhysAddress"],
-                address, community=snmp__ro, filter=filter)
+            mib["RFC1213-MIB::ipNetToMediaPhysAddress"],
+            address, community=snmp__ro, filter=filter,
+            bulk=caps.get("SNMP | Bulk", False)
+        )
