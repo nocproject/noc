05ad395df92c4614158daa6704e0d1bd4edc4e69
Dmitry Volodin <dv@nocproject.org>
2014-11-10 10:09:21 +0300

    Generic IP | ARP | Count probes
    --HG--
    branch : feature/graphite

---
+++ pm/probes/generic/network/snmp_arp.py
@@ -0,0 +1,31 @@
+# # -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Generic SNMP ARP Probes
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2014 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+from noc.pm.probes.base import Probe, metric
+from noc.lib.mib import mib
+
+
+class SNMPARPProbe(Probe):
+    @metric("IP | ARP | Count",
+            caps=["SNMP"],
+            preference=metric.PREF_COMMON)
+    def get_object_arp_rfc1213(self, address, snmp__ro, caps):
+        return self.snmp_count(
+                mib["RFC1213-MIB::ipNetToMediaPhysAddress"],
+                address, community=snmp__ro)
+
+    @metric("IP | ARP | Count",
+            caps=["SNMP"],
+            preference=metric.PREF_COMMON + metric.PREF_BETTER)
+    def get_interface_arp_rfc1213(self, address, snmp__ro,
+                                  interface_ifindex, caps):
+        filter = lambda oid, value: int(oid.split(".")[-5]) == interface_ifindex
+        return self.snmp_count(
+                mib["RFC1213-MIB::ipNetToMediaPhysAddress"],
+                address, community=snmp__ro, filter=filter)
