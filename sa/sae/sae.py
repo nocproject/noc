1a429af8ff6c777d938260e43f833a8623cc73f8
Dmitry Volodin <dv@nocproject.org>
2014-11-11 10:12:41 +0300

    Managed object's capabilities are passed to Script.caps attribute. Script.snmp.getnext() switches to GETBULK automatically when SNMP | Bulk capability is available
    --HG--
    branch : feature/graphite

--- sa/sae/sae.py
+++ sa/sae/sae.py
@@ -414,6 +414,21 @@ class SAE(Daemon):
             a = r.kwargs.add()
             a.key = str(k)
             a.value = cPickle.dumps(v)
+        # Capabilities
+        caps = object.get_caps()
+        for c in sorted(caps):
+            a = r.access_profile.caps.add()
+            a.capability = c
+            v = caps[c]
+            if isinstance(v, float):
+                a.float_value = v
+            elif isinstance(v, bool):
+                a.bool_value = v
+            elif isinstance(v, (int, long)):
+                a.int_value = v
+            else:
+                a.str_value = str(v)
+        #
         if object.profile_name == "NOC.SAE":
             self.run_sae_script(r, script_callback)
         else:
