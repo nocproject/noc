c3cf67d781c76f68dd2007e86a46602acceed430
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2014-12-08 15:15:19 +0200

    NOC-1429 Fix DLink.DGS3100.get_portchannel script
    --HG--
    branch : develop

--- sa/profiles/DLink/DGS3100/get_portchannel.py
+++ sa/profiles/DLink/DGS3100/get_portchannel.py
@@ -19,29 +19,34 @@ class Script(NOCScript):
         r"Group ID\s+:\s+(?P<trunk>\d+)\n"
         r"Member Port\s+:\s*(?P<members>\S+)*\n"
         r"Active Port\s+:\s*(?P<active>\S+)*\n"
-        r"Status\s+:\s+(?P<status>\S+)\s+",
-        re.MULTILINE | re.DOTALL)
+        r"Status\s+:\s+(?P<status>\S+)\s+", re.MULTILINE | re.DOTALL)
     rx_type = re.compile(
-        r"create link_aggregation group_id (?P<group_id>\d+) type (?P<type>\S+)\s+",
-        re.MULTILINE | re.DOTALL)
+        r"create link_aggregation group_id (?P<group_id>\d+) "
+        r"type (?P<type>\S+)\s+", re.MULTILINE | re.DOTALL)
 
     def execute(self):
         try:
             c = self.cli("show link_aggregation")
-            t = self.cli("show config running include link_aggregation")
         except self.CLISyntaxError:
             raise self.NotSupportedError()
         r = []
         for match in self.rx_trunk.finditer(c):
-            if match.group("status").lower() == "enable" and match.group("members") is not None:
+            if match.group("status").lower() == "enable" \
+            and match.group("members") is not None:
                 r += [{
                     "interface": "ch%s" % match.group("trunk"),
-                    "members": self.expand_interface_range(self.profile.open_brackets(match.group("members"))),
+                    "members": self.expand_interface_range(
+                        self.profile.open_brackets(match.group("members"))),
                     "type": "S"
                     }]
-        for match in self.rx_type.finditer(t):
-            if match.group("type") == "lacp":
-                for i in r:
-                    if i["interface"] == "ch" + match.group("group_id"):
-                       i["type"] = "L"
+        if len(r) > 0:
+            try:
+                t = self.cli("show config running include link_aggregation")
+            except self.CLISyntaxError:
+                raise self.NotSupportedError()
+            for match in self.rx_type.finditer(t):
+                if match.group("type") == "lacp":
+                    for i in r:
+                        if i["interface"] == "ch" + match.group("group_id"):
+                            i["type"] = "L"
         return r
