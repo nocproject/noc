14219b916e6548e21609400bff776c9590c07e8c
Dmitry Volodin <dv@nocproject.org>
2014-12-10 09:13:45 +0300

    PM: Drop legacy PostgreSQL tables and procedures
    --HG--
    branch : develop

--- pm/migrations/0003_fix_ts_indexes.py
+++ pm/migrations/0003_fix_ts_indexes.py
@@ -4,11 +4,7 @@ from noc.lib.nosql import get_db
 
 class Migration:
     def forwards(self):
-        c = get_db().noc.pm.ts
-        info = c.index_information()
-        for name in info:
-            if info[name]["key"] == [(u"name", 1)]:
-                c.drop_index(name)
+        pass
 
     def backwards(self):
         pass
