d54b98f2b9defb20a332f5c525b989b354dd05b6
Dmitry Volodin <dv@nocproject.org>
2015-01-05 13:11:09 +0300

    NOC-1514: Fix *Local classification rules* report
    --HG--
    branch : develop

--- fm/apps/reportlocalrules/views.py
+++ fm/apps/reportlocalrules/views.py
@@ -12,13 +12,21 @@ from django.utils.html import escape
 from noc.lib.app.reportapplication import ReportApplication
 from noc.fm.models import EventClassificationRule
 from noc.lib.text import indent
+from noc.main.models.collectioncache import CollectionCache
+
 
 class ReportClassificationRules(ReportApplication):
     title = "Local Classification Rules (JSON)"
-    
+
     def report_html(self):
+        def is_local(r):
+            return not bool(CollectionCache.objects.filter(uuid=r.uuid))
+
         r = ["["]
-        r += [",\n".join([indent(r.to_json())
-                          for r in EventClassificationRule.objects.filter(is_builtin=False)])]
-        r += ["]",""]
+        r += [",\n".join([
+            indent(r.to_json())
+            for r in EventClassificationRule.objects.order_by("name")
+            if is_local(r)
+        ])]
+        r += ["]", ""]
         return "<pre>" + escape("\n".join(r)) + "</pre>"
