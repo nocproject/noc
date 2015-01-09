967a68677fe3d469eacae33a462e916ad33a7dc7
Dmitry Volodin <dv@nocproject.org>
2015-01-09 17:58:06 +0300

    support.account application, cp.nocproject.org integration, account registration
    --HG--
    branch : develop

---
+++ support/apps/account/js/AccountPanel.js
@@ -0,0 +1,236 @@
+//---------------------------------------------------------------------
+// support.account AccountPanel
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2015 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.support.account.AccountPanel");
+
+Ext.define("NOC.support.account.AccountPanel", {
+    extend: "Ext.panel.Panel",
+    requires: [
+        "NOC.main.ref.country.LookupField"
+    ],
+    app: null,
+    title: "Account",
+    layout: "fit",
+    autoScroll: true,
+    closable: false,
+
+    minPasswordLength: 8,
+    notRegisteredText: "Account is not registred. Please register account to get access to additional support and services",
+    registeredText: "Account is registred. You have access to additional support and serivces",
+    industries: [
+        ["ISP", "Internet Service Provider"],
+        ["DC", "Hosting & Datacenter Service Provider"],
+        ["ASP", "Application Service Provider"],
+        ["PSP", "Payment Service Provider"],
+        ["SI", "System Intergration"],
+        ["SD", "Software Development"],
+        ["EDU", "Educational"],
+        ["SCI", "Science&Research"],
+        ["GOV", "Government"]
+    ],
+
+    initComponent: function() {
+        var me = this;
+
+        me.attachButton = Ext.create("Ext.button.Button", {
+            text: "Attach existing account",
+            glyph: NOC.glyph.plus_circle,
+            scope: me,
+            handler: me.onAttachAccount
+        });
+
+        me.changePasswordButton = Ext.create("Ext.button.Button", {
+            text: "Change password",
+            glyph: NOC.glyph.lock,
+            scope: me,
+            handler: me.onChangePassword
+        });
+
+        me.saveButton = Ext.create("Ext.button.Button", {
+            text: "Save",
+            glyph: NOC.glyph.save,
+            scope: me,
+            handler: me.onSave,
+            formBind: true,
+            disabled: true
+        });
+
+        me.infoText = Ext.create("Ext.container.Container", {
+            border: 2,
+            html: self.notRegisteredText,
+            cls: "noc-info-balloon"
+        });
+
+        me.form = Ext.create("Ext.form.Panel", {
+            padding: 4,
+            border: 0,
+            items: [
+                me.infoText,
+                {
+                    xtype: "displayfield",
+                    name: "uuid",
+                    fieldLabel: "UUID",
+                    itemId: "uuid"
+                },
+                {
+                    xtype: "textfield",
+                    name: "name",
+                    fieldLabel: "Name",
+                    allowBlank: false,
+                    regex: /^[a-z0-9\.\-_]+$/i
+                },
+                {
+                    xtype: "textfield",
+                    name: "email",
+                    fieldLabel: "Mail",
+                    allowBlank: false,
+                    vtype: "email"
+                },
+                {
+                    xtype: "main.ref.country.LookupField",
+                    name: "country",
+                    fieldLabel: "Country"
+                },
+                {
+                    xtype: "combobox",
+                    name: "language",
+                    fieldLabel: "Preferred Language",
+                    allowBlank: false,
+                    store: [
+                        ["EN", "English"],
+                        ["RU", "Русский"]
+                    ],
+                    value: "EN"
+                },
+                {
+                    xtype: "fieldset",
+                    title: "Industries",
+                    layout: {
+                        type: "table",
+                        columns: 2
+                    },
+                    items: me.getIndustriesFields()
+                },
+                {
+                    xtype: "textfield",
+                    name: "password",
+                    fieldLabel: "Password",
+                    itemId: "password",
+                    inputType: "password",
+                    allowBlank: false,
+                    minLength: me.minPasswordLength
+                },
+                {
+                    xtype: "textfield",
+                    name: "password2",
+                    fieldLabel: "Retype password",
+                    itemId: "password2",
+                    inputType: "password",
+                    vtype: "password",
+                    peerField: "password",
+                    allowBlank: false,
+                    minLength: me.minPasswordLength
+                }
+            ],
+            dockedItems: [{
+                xtype: "toolbar",
+                dock: "top",
+                items: [
+                    me.saveButton,
+                    me.attachButton,
+                    me.changePasswordButton
+                ]
+            }]
+        });
+
+        Ext.apply(me, {
+            items: [
+                me.form
+            ]
+        });
+        me.callParent();
+    },
+    //
+    setNotRegistered: function() {
+        var me = this;
+        me.attachButton.show();
+        me.changePasswordButton.hide();
+        me.infoText.setHtml(me.notRegisteredText);
+        me.infoText.addCls("noc-warning");
+        me.infoText.removeCls("noc-info");
+        me.form.getComponent("password").show();
+        me.form.getComponent("password").setDisabled(false);
+        me.form.getComponent("password2").show();
+        me.form.getComponent("password2").setDisabled(false);
+        me.form.getComponent("uuid").hide();
+    },
+    //
+    setRegistered: function(data) {
+        var me = this;
+        me.attachButton.hide();
+        me.changePasswordButton.show();
+        me.infoText.setHtml(me.registeredText);
+        me.infoText.addCls("noc-info");
+        me.infoText.removeCls("noc-warning");
+        me.form.getComponent("password").hide();
+        me.form.getComponent("password").setDisabled(true);
+        me.form.getComponent("password2").hide();
+        me.form.getComponent("password2").setDisabled(true);
+        me.form.getComponent("uuid").show();
+        me.form.getForm().setValues(data);
+    },
+    //
+    onSave: function() {
+        var me = this,
+            values;
+        if(!me.form.isValid()) {
+            NOC.error("Error in data");
+            return;
+        }
+        values = me.form.getValues();
+        delete values["password2"];
+        Ext.Ajax.request({
+            url: "/support/account/account/",
+            method: "POST",
+            jsonData: values,
+            scope: me,
+            success: function(response) {
+                var data = Ext.decode(response.responseText);
+                if(data.status) {
+                    NOC.info("Account saved");
+                    me.app.getInfo();
+                } else {
+                    NOC.error(data.message);
+                }
+            },
+            failure: function() {
+                NOC.error("Failed to save account");
+            }
+        });
+    },
+    //
+    onAttachAccount: function() {
+        var me = this;
+        Ext.create("NOC.support.account.AttachAccountPanel", {app: me.app}).show();
+    },
+    //
+    onChangePassword: function() {
+        var me = this;
+        Ext.create("NOC.support.account.ChangeCredentials", {app: me.app}).show();
+    },
+    //
+    getIndustriesFields: function() {
+        var me = this;
+        return me.industries.map(function(v) {
+            return {
+                xtype: "checkbox",
+                name: "ind_" + v[0],
+                boxLabel: v[1],
+                inputValue: true
+            }
+        });
+    }
+});
---
+++ support/apps/account/views.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## support.account application
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2015 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+from noc.lib.app import ExtApplication, view
+from noc.support.cp import CPClient
+from noc.sa.interfaces.base import StringParameter, REStringParameter
+
+
+class AccountApplication(ExtApplication):
+    """
+    support.account application
+    """
+    title = "Account"
+    menu = "Setup | Account"
+
+    @view(url="^$", method=["GET"], access="launch", api=True)
+    def api_get(self, request):
+        c = CPClient()
+        data = {}
+        if c.has_account():
+            data["account"] = c.account_info()
+            for i in data["account"].get("industries", []):
+                data["account"]["ind_%s" % i] = True
+        if c.has_system():
+            data["system"] = c.system_info()
+        return data
+
+    @view(url="^account/attach/$", method=["POST"], access="launch", api=True,
+          validate={
+              "name": StringParameter(),
+              "password": StringParameter(required=False)
+          }
+    )
+    def api_attach_account(self, request, name, password):
+        c = CPClient()
+        if c.has_account():
+            self.response_forbidden()
+        try:
+            c.attach_account(name, password)
+        except CPClient.Error, why:
+            return {"status": False, "message": str(why)}
+        return {"status": True, "message": "Ok"}
+
+    @view(url="^account/$", method=["POST"], access="launch", api=True,
+          validate={
+              "name": REStringParameter("^[a-zA-Z0-9\.\-_]+$"),
+              "email": StringParameter(),
+              "country": REStringParameter("^[A-Z]{2}$"),
+              "language": REStringParameter("^[A-Z]{2}$"),
+              "password": StringParameter(required=False)
+          }
+    )
+    def api_save_account(self, request, name, email, password=None,
+                         country=None, language=None,
+                         *args, **kwargs):
+        industries = [k[4:] for k in kwargs
+                      if k.startswith("ind_") and kwargs[k]]
+        c = CPClient()
+        if c.has_account():
+            try:
+                c.update_account(email, country=country,
+                                 language=language,
+                                 industries=industries)
+            except CPClient.Error, why:
+                return {"status": False, "message": str(why)}
+        else:
+            # Create account
+            try:
+                c.create_account(name, email, password,
+                                 country=country, language=language,
+                                 industries=industries)
+            except CPClient.Error, why:
+                return {"status": False, "message": str(why)}
+        return {"status": True, "message": "Account saved"}
+
+    @view(url="^account/change_password/$", method=["POST"],
+          access="launch", api=True,
+          validate={
+              "old_password": StringParameter(),
+              "new_password": StringParameter()
+          }
+    )
+    def api_change_password(self, request, old_password, new_password,
+                         *args, **kwargs):
+        c = CPClient()
+        if not c.has_account():
+            return {"status": False, "message": "Account is not registred"}
+        if old_password != c.account_password:
+            return {"status": False, "message": "Invalid password"}
+        c.change_password(new_password)
+        return {"status": True, "message": "Password has been changed"}
+
+    @view(url="^system/$", method=["POST"], access="launch", api=True,
+          validate={
+              "name": REStringParameter("^[a-zA-Z0-9\.\-_]+$"),
+              "type": StringParameter(required=False),
+              "description": StringParameter(required=False)
+          }
+    )
+    def api_save_system(self, request, name, type=None, description=None,
+                         *args, **kwargs):
+        c = CPClient()
+        if c.has_system():
+            try:
+                c.update_system(name, type, description=description)
+            except CPClient.Error, why:
+                return {"status": False, "message": str(why)}
+        else:
+            # Create account
+            try:
+                c.create_system(name, type, description=description)
+            except CPClient.Error, why:
+                return {"status": False, "message": str(why)}
+        return {"status": True, "message": "System saved"}
