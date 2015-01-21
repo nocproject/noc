e539f3f33098e8a670247e769bf076aab06b745b
Gnu Linux <gnu-linux>
2015-01-21 05:03:03 +0000

    __init__.py prompt fix
    --HG--
    branch : gnu-linux/__init__py-prompt-fix-1421667379480

--- sa/profiles/OS/Linux/__init__.py
+++ sa/profiles/OS/Linux/__init__.py
@@ -18,7 +18,7 @@ class Profile(noc.sa.profiles.Profile):
     pattern_username = "^((?!Last)\S+ login|[Ll]ogin):"
     pattern_password = "^[Pp]assword:"
     pattern_unpriveleged_prompt = r"^\w+.+\w+.~ ?\$"
-    pattern_prompt = r"^(\[?\s*root@(?P<hostname>\S+)\]?(\s+|:)\S+\s*(#|$)\s*|\S+:~>\s+|\[admin@(?P<hostname>\S+):/root\])"
+    pattern_prompt = r"^(\[?\s*root@(?P<hostname>\S+)\]?(\s+|:)\S+\s*(#|$)\s*|\S+:~>\s+|\[admin@\S+:/root\])"
     pattern_syntax_error = r"^(-\w+: \w+: not found|-\w+: \w+: No such file or directory|\w+: \w+: command not found|\w+: \w+: \w+: No such file or directory)"
     command_disable_pager = "export LANG=en_GB.UTF-8"
     command_super = "su"
