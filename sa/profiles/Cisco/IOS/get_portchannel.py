3344e62d5cef50f12743b303797c20d9a0799442
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2015-01-23 15:52:05 +0200

    NOC-1520 Fix Cisco.IOS.get_portchannel script
    --HG--
    branch : develop

--- sa/profiles/Cisco/IOS/get_portchannel.py
+++ sa/profiles/Cisco/IOS/get_portchannel.py
@@ -16,12 +16,35 @@ class Script(noc.sa.script.Script):
     name = "Cisco.IOS.get_portchannel"
     implements = [IGetPortchannel]
 
+    # Member 0 : GigabitEthernet0/2 , Full-duplex, 1000Mb/s
+    rx_po2_members = re.compile(
+        r"^\s+Member\s+\d+\s+:\s+(?P<interface>.+?)\s+.*$")
+
     def execute(self):
         r = []
         try:
             s = self.cli("show interfaces status | i ^Po[0-9]+")
         except self.CLISyntaxError:
-            return []
+            # one more try
+            try:
+                s = self.cli("show interfaces description | i ^Po[0-9]+_")
+            except self.CLISyntaxError:
+                return []
+            for l in s.splitlines():
+                pc, rest = l.split(" ", 1)
+                pc = pc[2:]
+                v = self.cli("show interfaces port-channel {0} | i Member_[0-9]+".format(pc))
+                out_if = {
+                    "interface": "Po %s" % pc,
+                    "members": [],
+                    "type": "S",  # <!> TODO: port-channel type detection
+                }
+                for line in v.splitlines():
+                    match = self.rx_po2_members.match(line)
+                    if match:
+                        out_if["members"].append(match.group('interface'))
+                r += [out_if]
+                return r
         for l in s.splitlines():
             pc, rest = l.split(" ", 1)
             pc = pc[2:]
