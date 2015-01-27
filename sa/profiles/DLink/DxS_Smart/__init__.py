f535b35f270d1016ab894f82584e3e48d9050654
Dmitry Volodin <dv@nocproject.org>
2015-01-27 13:44:46 +0300

    DLink.DxS_Smart: Fix pattern_syntax_error
    --HG--
    branch : develop

--- sa/profiles/DLink/DxS_Smart/__init__.py
+++ sa/profiles/DLink/DxS_Smart/__init__.py
@@ -24,8 +24,10 @@ class Profile(noc.sa.profiles.Profile):
         ("CTRL\+C.+?(a All)|(r Refresh)", "a")
     ]
     pattern_unpriveleged_prompt = r"^\S+:(3|6|user|operator)#"
-    pattern_syntax_error = r"(% Invalid (Command|input detected at))|"
-    r"((Available commands|Next possible completions):)"
+    pattern_syntax_error = r"(?:% Invalid Command|" \
+                           r"% Invalid input detected at|" \
+                           r"Available commands:|" \
+                           r"Next possible completions:)"
     command_super = "enable admin"
     pattern_prompt = r"(?P<hostname>\S+(:\S+)*)[#>]"
     command_disable_pager = ""
