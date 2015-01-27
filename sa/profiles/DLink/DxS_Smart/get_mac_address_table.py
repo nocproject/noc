166a8ac29b1ab7145bec95672bd91e307c84a408
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2015-01-27 11:32:48 +0200

    This is a draft version of the script
    --HG--
    branch : develop

--- sa/profiles/DLink/DxS_Smart/get_mac_address_table.py
+++ sa/profiles/DLink/DxS_Smart/get_mac_address_table.py
@@ -2,7 +2,7 @@
 ##----------------------------------------------------------------------
 ## DLink.DxS_Smart.get_mac_address_table
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2014 The NOC Project
+## Copyright (C) 2007-2015 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 
@@ -21,6 +21,9 @@ class Script(NOCScript):
     rx_line = re.compile(
         r"(?P<vlan_id>\d+)\s+(?P<mac>\S+)\s+(?P<type>Learnt|Static)\s+"
         r"(?P<interfaces>\S+)", re.MULTILINE)
+    rx_line1 = re.compile(
+        r"^(?P<vlan_id>\d+)\s+(?P<vlan_name>\S+)?\s+(?P<mac>\S+)\s+"
+        "(?P<interface>\d+)\s+(?P<type>Dynamic|Static)", re.MULTILINE)
 
     def execute(self, interface=None, vlan=None, mac=None):
         r = []
@@ -80,20 +83,43 @@ class Script(NOCScript):
                 pass
 
         # Fallback to CLI
-        cmd = "debug info"
         r = []
-        for match in self.rx_line.finditer(self.cli(cmd)):
-            m_interface = match.group("interfaces")
-            m_vlan = match.group("vlan_id")
-            m_mac = match.group("mac")
-            if ((interface is None and vlan is None and mac is None)
+        cmd = "debug info"
+        try:
+            s = self.cli("debug info")
+            for match in self.rx_line.finditer(s):
+                m_interface = match.group("interfaces")
+                m_vlan = match.group("vlan_id")
+                m_mac = match.group("mac")
+                if ((interface is None and vlan is None and mac is None)
                     or (interface is not None and interface == m_interface)
                     or (vlan is not None and vlan == m_vlan)
                     or (mac is not None and mac == m_mac)):
-                r += [{
-                    "vlan_id": m_vlan,
-                    "mac": m_mac,
-                    "interfaces": [m_interface],
-                    "type": {"Learnt": "D", "Static": "S"}[match.group("type")]
-                }]
+                    r += [{
+                        "vlan_id": m_vlan,
+                        "mac": m_mac,
+                        "interfaces": [m_interface],
+                        "type": {"Learnt": "D", "Static": "S"}[match.group("type")]
+                    }]
+            return r
+        except self.CLISyntaxError:
+            pass
+        cmd = "show fdb"
+        if mac is not None:
+            cmd += " mac_address %s" % mac
+        if interface is not None:
+            cmd += " port %s" % interface
+        if vlan is not None:
+            cmd += " vlanid %d" % vlan
+        s = self.cli(cmd)
+        for match in self.rx_line1.finditer(s):
+            m_interface = match.group("interface")
+            m_vlan = match.group("vlan_id")
+            m_mac = match.group("mac")
+            r += [{
+                "vlan_id": m_vlan,
+                "mac": m_mac,
+                "interfaces": [m_interface],
+                "type": {"Learnt": "D", "Static": "S"}[match.group("type")]
+            }]
         return r
