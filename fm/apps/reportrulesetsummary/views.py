425cab77149a28776c77947cace8eff74b63cf87
Dmitry Volodin <dv@nocproject.org>
2015-01-27 23:33:23 +0300

    fm.reportrulesetsummary: Fix builtin items count
    --HG--
    branch : develop

--- fm/apps/reportrulesetsummary/views.py
+++ fm/apps/reportrulesetsummary/views.py
@@ -8,32 +8,35 @@
 
 ## NOC modules
 from noc.lib.app.simplereport import SimpleReport, TableColumn
-from noc.fm.models import AlarmClass, EventClass, EventClassificationRule
+from noc.fm.models import (AlarmClass, EventClass,
+                           EventClassificationRule)
+from noc.lib.collection import Collection
 
 
 class ReportRulesetSummary(SimpleReport):
     title = "Ruleset Summary"
 
     def get_data(self, **kwargs):
-        def get_count(m):
+        def get_count(cn, m):
+            collection = Collection(cn, m)
+            collection.load()
             total = m.objects.count()
-            builtin = m.objects.filter(is_builtin=True).count()
+            builtin = len(collection.items)
             local = total - builtin
             return [builtin, local, total]
 
         data = [
-            ["Alarm Classes"] + get_count(AlarmClass),
-            ["Event Classies"] + get_count(EventClass),
-            ["Classification Rules"] + get_count(EventClassificationRule)
+            ["Alarm Classes"] + get_count("fm.alarmclasses", AlarmClass),
+            ["Event Classies"] + get_count("fm.eventclasses", EventClass),
+            ["Classification Rules"] + get_count("fm.eventclassificationrules", EventClassificationRule)
         ]
 
-        return self.from_dataset(title=self.title,
-                                 columns=["",
-                                          TableColumn("Builtin", align="right",
-                                                      format="integer"),
-                                          TableColumn("Local", align="right",
-                                                      format="integer"),
-                                          TableColumn("Total", align="right",
-                                                      format="integer")
-                                         ],
-                                 data=data)
+        return self.from_dataset(
+            title=self.title,
+            columns=[
+                "",
+                TableColumn("Builtin", align="right", format="integer"),
+                TableColumn("Local", align="right", format="integer"),
+                TableColumn("Total", align="right", format="integer")
+            ],
+            data=data)
