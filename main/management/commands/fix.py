1905e7f1c72c7014d176795726ee5ad3776be51a
Dmitry Volodin <dv@nocproject.org>
2015-02-11 10:15:52 +0300

    ./noc fix: Additional error reporting
    --HG--
    branch : develop

--- main/management/commands/fix.py
+++ main/management/commands/fix.py
@@ -7,36 +7,10 @@
 ##----------------------------------------------------------------------
 
 ## Python modules
-from optparse import make_option
-import os
-import stat
 import sys
 ## Django modules
 from django.core.management.base import BaseCommand, CommandError
 ## NOC modules
-from noc.lib.collection import Collection, DereferenceError
-from noc.main.models.doccategory import DocCategory
-from noc.gis.models.layer import Layer
-from noc.inv.models.technology import Technology
-from noc.inv.models.vendor import Vendor
-from noc.inv.models.modelinterface import ModelInterface
-from noc.inv.models.connectiontype import ConnectionType
-from noc.inv.models.connectionrule import ConnectionRule
-from noc.inv.models.objectmodel import ObjectModel
-from noc.inv.models.capability import Capability
-from noc.fm.models.oidalias import OIDAlias
-from noc.fm.models.syntaxalias import SyntaxAlias
-from noc.fm.models.mibalias import MIBAlias
-from noc.fm.models.mibpreference import MIBPreference
-from noc.fm.models.enumeration import Enumeration
-from noc.fm.models.alarmseverity import AlarmSeverity
-from noc.fm.models.alarmclass import AlarmClass
-from noc.fm.models.eventclass import EventClass
-from noc.fm.models.eventclassificationrule import EventClassificationRule
-from noc.fm.models.cloneclassificationrule import CloneClassificationRule
-from noc.pm.models.metrictype import MetricType
-from noc.lib.serialize import json_decode
-from noc.lib.fileutils import read_file
 from noc.lib.debug import error_report
 
 
@@ -47,10 +21,14 @@ class Command(BaseCommand):
     help = "Fix database"
 
     def handle(self, *args, **kwargs):
-        self.fix_inv_root()
-        self.fix_inv_lost_and_found()
-        self.fix_inv_orphans()
-        self.fix_metricsettings()
+        try:
+            self.fix_inv_root()
+            self.fix_inv_lost_and_found()
+            self.fix_inv_orphans()
+            self.fix_metricsettings()
+        except:
+            error_report()
+            sys.exit(1)
 
     def info(self, msg, *args):
         if args:
