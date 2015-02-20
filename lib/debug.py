0ce972bc186dd77b95754928f5cdcb4bfc6bc552
Dmitry Volodin <dv@nocproject.org>
2015-02-20 09:03:34 +0300

    Log critical database error and exit Fix: b55f28ea-47eb-5ac0-a57d-b5d718f68b0d
    --HG--
    branch : develop

--- lib/debug.py
+++ lib/debug.py
@@ -217,6 +217,8 @@ def check_fatal_errors(t, v):
         die("Failed to connect PostgreSQL: %s", v)
     elif xn == "psycopg2.InterfaceError" and "connection already closed" in v:
         die("PostgreSQL connection closed: %s", v)
+    elif xn == "django.db.utils.DatabaseError":
+        die("PostgreSQL database error: %s", v)
 
 
 def get_traceback(reverse=TRACEBACK_REVERSE, fp=None):
