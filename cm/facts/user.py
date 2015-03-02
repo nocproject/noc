ad37ea37721f150a0c9c02e02b82257863f0b5db
Dmitry Volodin <dv@nocproject.org>
2015-03-02 20:49:51 +0300

    Cisco IOS parser: Collect facts about local users
    --HG--
    branch : feature/validation

---
+++ cm/facts/user.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Local user
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2015 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+from base import BaseFact
+
+
+class User(BaseFact):
+    ATTRS = ["name", "level", "groups"]
+
+    def __init__(self, name=None, level=None, groups=None):
+        self.name = name
+        self.level = level
+        self.groups = groups
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        self._name = value or None
+        
+    @property
+    def level(self):
+        return self._level
+    
+    @level.setter
+    def level(self, value):
+        self._level = value
+
+    @property
+    def groups(self):
+        return self._groups
+    
+    @groups.setter
+    def groups(self, value):
+        self._groups = value or []
