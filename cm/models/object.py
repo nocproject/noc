04c599278edc98f429bc29f57cb907b2a2499ac3
Dmitry Volodin <dv@nocproject.org>
2015-03-17 00:18:49 +0300

    noc.cm.models has been refactored to separate modules
    --HG--
    branch : feature/validation

--- cm/models/object.py
+++ cm/models/object.py
@@ -1,60 +1,24 @@
 # -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
-## Configuration Management models
+## Configuration Management Object
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2011 The NOC Project
+## Copyright (C) 2007-2015 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 
 ## Python modules
 import os
 import datetime
-import logging
-import types
-import difflib
 ## Django modules
 from django.db import models
 from django.db.models import Q
-from django.contrib.auth.models import User
 ## NOC modules
-from noc.sa.profiles import profile_registry
 from noc.settings import config
 from noc.lib.fileutils import rewrite_when_differ, read_file, is_differ, in_dir
-from noc.lib.validators import is_int
 from noc.cm.vcs import vcs_registry
-from noc.sa.models import AdministrativeDomain, ManagedObject
-from noc.main.models import NotificationGroup
-from noc.lib.app.site import site
-
-
-profile_registry.register_all()
-vcs_registry.register_all()
-
-#
-OBJECT_TYPES = ["config", "dns", "prefix-list", "rpsl"]
-OBJECT_TYPE_CHOICES = [(x, x) for x in OBJECT_TYPES if x != "config"]
-
-
-class ObjectNotify(models.Model):
-    class Meta:
-        verbose_name = "Object Notify"
-        verbose_name_plural = "Object Notifies"
-
-    type = models.CharField("Type", max_length=16, choices=OBJECT_TYPE_CHOICES)
-    administrative_domain = models.ForeignKey(AdministrativeDomain,
-                                              verbose_name="Administrative Domain",
-                                              blank=True, null=True)
-    notify_immediately = models.BooleanField("Notify Immediately")
-    notify_delayed = models.BooleanField("Notify Delayed")
-    notification_group = models.ForeignKey(NotificationGroup,
-                                           verbose_name="Notification Group")
-
-    def __unicode__(self):
-        return u"(%s, %s, %s)" % (self.type, self.administrative_domain,
-                                  self.notification_group)
-
-    def get_absolute_url(self):
-        return site.reverse("cm:objectnotify:change", self.id)
+from noc.lib.validators import is_int
+from objectnotify import ObjectNotify
+from noc.main.models.notificationgroup import NotificationGroup
 
 
 class Object(models.Model):
@@ -280,149 +244,7 @@ class Object(models.Model):
             return True
         return False
 
-    @classmethod
-    def search(cls, user, query, limit):
-        """
-        Search engine plugin
-        :param cls:
-        :param user:
-        :param query:
-        :param limit:
-        :return:
-        """
-        for o in [o for o in cls.objects.all() if
-                  o.repo_path and o.has_access(user)]:
-            try:
-                data = o.data
-                if query in o.repo_path:  # If repo_path matches
-                    yield SearchResult(
-                        url=("cm:%s:view" % o.repo_name, o.id),
-                        title="CM: " + unicode(o),
-                        text=unicode(o),
-                        relevancy=1.0,  # No weighted search yes
-                    )
-                elif data and query in data:  # Dumb substring search in config
-                    idx = data.index(query)
-                    idx_s = max(0, idx - 100)
-                    idx_e = min(len(data), idx + len(query) + 100)
-                    text = data[idx_s:idx_e]
-                    yield SearchResult(
-                        url=("cm:%s:view" % o.repo_name, o.id),
-                        title="CM: " + unicode(o),
-                        text=text,
-                        relevancy=1.0,  # No weighted search yes
-                    )
-            except UnicodeDecodeError:  # Skip unicode errors in configs
-                pass
 
-
-class PrefixList(Object):
-    class Meta:
-        verbose_name = "Prefix List"
-        verbose_name_plural = "Prefix Lists"
-
-    repo_name = "prefix-list"
-
-    @classmethod
-    def build_prefix_lists(cls):
-        from noc.peer.models import PeeringPoint, WhoisCache
-
-        for pp in PeeringPoint.objects.all():
-            profile = pp.profile
-            for name, filter_exp in pp.generated_prefix_lists:
-                prefixes = WhoisCache.resolve_as_set_prefixes_maxlen(filter_exp)
-                pl = profile.generate_prefix_list(name, prefixes)
-                yield (pp, name, pl, prefixes)
-
-    @classmethod
-    def global_pull(cls):
-        from noc.peer.models import PrefixListCache, PrefixListCachePrefix
-
-        objects = {}
-        for o in PrefixList.objects.all():
-            objects[o.repo_path] = o
-        c_objects = set()  # peering_point/name
-        logging.debug("PrefixList.global_pull(): building prefix lists")
-        for peering_point, pl_name, pl, prefixes in cls.build_prefix_lists():
-            logging.debug(
-                "PrefixList.global_pull(): writing %s/%s (%d lines)" % (
-                peering_point.hostname, pl_name, len(pl.split("\n"))))
-            path = os.path.join(peering_point.hostname, pl_name)
-            if path in objects:
-                o = objects[path]
-                del objects[path]
-            else:
-                o = PrefixList(repo_path=path)
-                o.save()
-            o.write(pl)
-            # Populate cache
-            cname = "%s/%s" % (peering_point.hostname, pl_name)
-            try:
-                c = PrefixListCache.objects.get(peering_point=peering_point.id,
-                                                name=pl_name)
-                if c.cmp_prefixes(prefixes):
-                    logging.debug("Updating cache for %s" % cname)
-                    c.changed = datetime.datetime.now()
-                    c.prefixes = [PrefixListCachePrefix(prefix=prefix,
-                                                        min=min, max=max)
-                                  for prefix, min, max in prefixes]
-                    c.save()
-            except PrefixListCache.DoesNotExist:
-                logging.debug("Writing cache for %s" % cname)
-                PrefixListCache(peering_point=peering_point.id,
-                                name=pl_name,
-                                prefixes=[PrefixListCachePrefix(prefix=prefix,
-                                                                min=min,
-                                                                max=max)
-                                          for prefix, min, max in
-                                          prefixes]).save()
-            c_objects.add("%s/%s" % (peering_point.hostname, pl_name))
-        # Remove deleted prefix lists
-        for o in objects.values():
-            o.delete()
-        # Remove unused cache entries
-        for o in PrefixListCache.objects.all():
-            n = "%s/%s" % (o.peering_point.hostname, o.name)
-            if n not in c_objects:
-                o.delete()
-
-
-class RPSL(Object):
-    class Meta:
-        verbose_name = "RPSL Object"
-        verbose_name_plural = "RPSL Objects"
-
-    repo_name = "rpsl"
-
-    @classmethod
-    def global_pull(cls):
-        def global_pull_class(name, c, name_fun):
-            objects = {}
-            for o in RPSL.objects.filter(repo_path__startswith=name + os.sep):
-                objects[o.repo_path] = o
-            for a in c.objects.all():
-                if not a.rpsl:
-                    continue
-                path = os.path.join(name, name_fun(a))
-                if path in objects:
-                    o = objects[path]
-                    del objects[path]
-                else:
-                    o = RPSL(repo_path=path)
-                    o.save()
-                o.write(a.rpsl)
-            for o in objects.values():
-                o.delete()
-
-        from noc.peer.models import AS, ASSet, PeeringPoint
-        from noc.dns.models import DNSZone
-
-        logging.debug("RPSL.global_pull(): building RPSL")
-        global_pull_class("inet-rtr", PeeringPoint, lambda a: a.hostname)
-        global_pull_class("as", AS, lambda a: "AS%d" % a.asn)
-        global_pull_class("as-set", ASSet, lambda a: a.name)
-        global_pull_class("domain", DNSZone, lambda a: a.name)
-
-    @classmethod
-    def global_push(cls):
-        pass
+## Avoid circular references
+from prefixlist import PrefixList
+from rpsl import RPSL
