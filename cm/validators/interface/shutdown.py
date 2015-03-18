cbeec0174d6980c5bd443d8e3cb280ab7bedfaef
Dmitry Volodin <dv@nocproject.org>
2015-03-18 11:53:07 +0300

    *Interface MUST BE shutdown* validator
    --HG--
    branch : feature/validation

---
+++ cm/validators/interface/shutdown.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Interface | Shutdown
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2015 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+from noc.cm.validators.clipsinterface import CLIPSInterfaceValidator
+
+
+class InterfaceShutdownValidator(CLIPSInterfaceValidator):
+    TITLE = "Interface *MUST BE* shutdown"
+    DESCRIPTION = """
+        Interface must be administratively down
+    """
+    TAGS = ["shutdown"]
+    RULES = """
+    (defrule {{RULENAME}}
+        ?i <- (interface (name "{{name}}") (admin_status 1))
+        =>
+        (assert
+            (error (name "Interface | Shutdown")
+                   (obj ?i)))
+    )
+    """
