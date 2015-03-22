d293cdbe28f7098a953ee27907f4dcf1f9334458
Dmitry Volodin <dv@nocproject.org>
2015-03-22 21:56:17 +0300

    Cisco.IOSXR.get_cdp_neighbors: Fix multiple neighbors parsing
    --HG--
    branch : feature/validation

--- sa/profiles/Cisco/IOSXR/get_cdp_neighbors.py
+++ sa/profiles/Cisco/IOSXR/get_cdp_neighbors.py
@@ -2,37 +2,38 @@
 ##----------------------------------------------------------------------
 ## Cisco.IOSXR.get_cdp_neighbors
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2014 The NOC Project
+## Copyright (C) 2007-2015 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-"""
-"""
-import noc.sa.script
-from noc.sa.interfaces import IGetCDPNeighbors
+
+## Python modules
 import re
+## NOC modules
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces.igetcdpneighbors import IGetCDPNeighbors
 
 
-class Script(noc.sa.script.Script):
+class Script(NOCScript):
     name = "Cisco.IOSXR.get_cdp_neighbors"
     implements = [IGetCDPNeighbors]
-    rx_entry = re.compile(r"Device\sID:\s(?P<device_id>\S+).+"
-        r"Interface:\s(?P<local_interface>\S+)\nPort\sID\s"
-        r"\(outgoing port\):\s(?P<remote_interface>\S+)",
-        re.MULTILINE | re.DOTALL | re.IGNORECASE)
+    rx_split = re.compile("^--------+\n", re.MULTILINE)
 
     def execute(self):
         device_id = self.scripts.get_fqdn()
         s = self.cli("show cdp neighbors detail")
-        # Get neighbors
-        neighbors = []
-        for match in self.rx_entry.finditer(s):
-            neighbors += [{
-                "device_id": match.group("device_id"),
-                "local_interface": match.group("local_interface"),
-                "remote_interface": match.group("remote_interface")
-            }]
+        r = []
+        for n in self.rx_split.split(s)[1:]:
+            neighbor = {}
+            for l in n.splitlines():
+                sl = l.lower()
+                if sl.startswith("device id"):
+                    neighbor["device_id"] = l.split(":", 1)[-1].strip()
+                elif sl.startswith("interface"):
+                    neighbor["local_interface"] = l.split(":", 1)[-1].strip()
+                elif sl.startswith("port id"):
+                    neighbor["remote_interface"] = l.split(":", 1)[-1].strip()
+            r += [neighbor]
         return {
             "device_id": device_id,
-            "neighbors": neighbors
+            "neighbors": r
         }
-
