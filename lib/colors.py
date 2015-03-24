0fe76abd59dc3143818857e3de01f32d01afce99
Dmitry Volodin <dv@nocproject.org>
2015-03-24 22:45:59 +0300

    PEP8 formatting
    --HG--
    branch : develop

--- lib/colors.py
+++ lib/colors.py
@@ -8,16 +8,18 @@
 
 ## Python modules
 import math
+
+
 ##
 ## HSV -> RGB convertor, for Python 2.5 compatibility
 ##
-def hsv_to_rgb(h,s,v):
-    h=float(h)
-    hi=math.floor(h/60.0)%6
-    f=(h/60.0)-math.floor(h/60.0)
-    p=v*(1.0-s)
-    q=v*(1.0-(f*s))
-    t=v*(1.0-((1.0-f)*s))
+def hsv_to_rgb(h, s, v):
+    h = float(h)
+    hi = math.floor(h / 60.0) % 6
+    f = (h / 60.0) - math.floor(h / 60.0)
+    p = v * (1.0 - s)
+    q = v * (1.0 - (f * s))
+    t = v * (1.0 - ((1.0 - f) * s))
     return {
         0: (v, t, p),
         1: (q, v, p),
@@ -26,6 +28,7 @@ def hsv_to_rgb(h,s,v):
         4: (t, p, v),
         5: (v, p, q),
     }[hi]
+
 
 ##
 ## Generate N contrast colors
@@ -41,22 +44,43 @@ def get_colors(N):
     >>> list(get_colors(4))
     ['#ff0000', '#00ffff', '#7fff00', '#7f00ff']
     """
-    MP=12       # Maximum points on color circle
-    p=min(N,MP) # Points on color circle
-    d=360//p    # Step on color circle
-    V=255 # Value
-    S=1   # Saturation
-    hs=[]
+    MP = 12  # Maximum points on color circle
+    p = min(N, MP)  # Points on color circle
+    d = 360 // p  # Step on color circle
+    V = 255  # Value
+    S = 1  # Saturation
+    hs = []
     while N:
         if not hs:
             # Rebuild colors
-            hs=[i*d for i in range(p)]
-        H=hs.pop(len(hs)/2 if N%2 else 0)
+            hs = [i * d for i in range(p)]
+        H = hs.pop(len(hs) / 2 if N % 2 else 0)
         # Yield current color
-        yield "#%02x%02x%02x"%(hsv_to_rgb(H,S,V))
-        N=N-1
+        yield "#%02x%02x%02x" % (hsv_to_rgb(H, S, V))
+        N = N - 1
         if not hs:
             # Reduce value for next round
-            V=V*3//4
-    
+            V = V * 3 // 4
 
+
+def get_float_pallete(N):
+    """
+    Generate tuple of floats for reportlab colors
+    """
+    MP = 12  # Maximum points on color circle
+    p = min(N, MP)  # Points on color circle
+    d = 360 // p  # Step on color circle
+    V = 255  # Value
+    S = 1  # Saturation
+    hs = []
+    while N:
+        if not hs:
+            # Rebuild colors
+            hs = [i * d for i in range(p)]
+        H = hs.pop(len(hs) / 2 if N % 2 else 0)
+        # Yield current color
+        yield [float(x)/256.0 for x in hsv_to_rgb(H, S, V)]
+        N = N - 1
+        if not hs:
+            # Reduce value for next round
+            V = V * 3 // 4
