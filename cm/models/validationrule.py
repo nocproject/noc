631323d5ae2bf5ad388d1a8df44ea541b9ebccbd
Dmitry Volodin <dv@nocproject.org>
2015-04-03 23:56:44 +0300

    ValidationRule: Detach from policy on delete
    --HG--
    branch : develop

--- cm/models/validationrule.py
+++ cm/models/validationrule.py
@@ -6,15 +6,20 @@
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 
+## Python modules
+import logging
 ## Third-party modules
 from mongoengine.document import Document, EmbeddedDocument
 from mongoengine.fields import (StringField, BooleanField, DictField,
                                 ListField, EmbeddedDocumentField)
+from mongoengine.signals import pre_delete
 ## NOC modules
 from noc.sa.models.managedobject import ManagedObject
 from noc.sa.models.managedobjectselector import ManagedObjectSelector
 from noc.lib.nosql import ForeignKeyField
 from noc.lib.solutions import get_solution
+
+logger = logging.getLogger(__name__)
 
 A_DISABLE = ""
 A_INCLUDE = "I"
@@ -84,3 +89,16 @@ class ValidationRule(Document):
             return get_solution(self.handler)
         else:
             return None
+
+    @classmethod
+    def on_delete(cls, sender, document, **kwargs):
+        from validationpolicy import ValidationPolicy
+        logger.info("Deleting rule %s", document.name)
+        for vp in ValidationPolicy.objects.filter(rules__rule=document):
+            logger.info("Removing rule %s from policy %s",
+                        document.name, vp.name)
+            vp.rules = [r for r in vp.rules if r.rule.id != document.id]
+            vp.save()
+
+
+pre_delete.connect(ValidationRule.on_delete, sender=ValidationRule)
