c2a00af6a2371da1a2b89cf11add93e0845d2f77
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2015-04-30 12:57:27 +0300

    Update IPing interface
    --HG--
    branch : develop

--- sa/interfaces/iping.py
+++ sa/interfaces/iping.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2009 The NOC Project
+## Copyright (C) 2007-2015 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 """
@@ -10,6 +10,11 @@ from base import *
 
 class IPing(Interface):
     address = IPParameter()
+    count = IntParameter(required=False)
+    source_address = IPParameter(required=False)
+    size = IntParameter(required=False)
+    df = BooleanParameter(required=False)
+    vrf = StringParameter(required=False)
     returns = DictParameter(attrs={
         "success": IntParameter(),
         "count": IntParameter()
