19b175ec8c3820ccfcca0c301c429f5484da59a7
Dmitry Volodin <dv@nocproject.org>
2015-05-21 09:12:59 +0300

    NOC-1591: Fix Eltex.MES.get_lldp_neighbors
    --HG--
    branch : develop

--- sa/profiles/Eltex/MES/get_lldp_neighbors.py
+++ sa/profiles/Eltex/MES/get_lldp_neighbors.py
@@ -2,7 +2,7 @@
 ##----------------------------------------------------------------------
 ## Eltex.MES.get_lldp_neighbors
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2012 The NOC Project
+## Copyright (C) 2007-2015 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 
@@ -10,20 +10,24 @@
 import re
 ## NOC modules
 from noc.sa.script import Script as NOCScript
-from noc.sa.interfaces import IGetLLDPNeighbors
+from noc.sa.interfaces.igetlldpneighbors import IGetLLDPNeighbors
 from noc.sa.interfaces.base import MACAddressParameter
 from noc.lib.validators import is_int, is_ipv4
+from noc.lib.text import parse_table
 
 
 class Script(NOCScript):
     name = "Eltex.MES.get_lldp_neighbors"
     implements = [IGetLLDPNeighbors]
 
-    rx_line1 = re.compile(
-        r"^(?P<interface>\S+)\s+(?P<chassis_id>\S+)\s+(\d+/|)(?P<port_id1>\S+)\s+(?P<name>\S+)+(?P<capabilities>.+)\s+\d+\s*$")
-
-    rx_line2 = re.compile(r"^\s+(?P<port_id2>\S+)\s+$")
     rx_mac = re.compile(r"^[0-9a-f]{4}\.[0-9a-f]{4}\.[0-9a-f]{4}$")
+
+    CAPS_MAP = {
+        "O": 1, "r": 2, "B": 4,
+        "W": 8, "R": 16, "T": 32,
+        "C": 64, "S": 128, "D": 256,
+        "H": 512, "TP": 1024,
+    }
 
     def execute(self):
         r = []
@@ -31,19 +35,20 @@ class Script(NOCScript):
         # Try SNMP first
         if self.snmp and self.access_profile.snmp_ro:
             try:
-
-# lldpRemLocalPortNum
-# lldpRemChassisIdSubtype lldpRemChassisId
-# lldpRemPortIdSubtype lldpRemPortId
-# lldpRemSysName lldpRemSysCapEnabled
-                for v in self.snmp.get_tables(
-                    ["1.0.8802.1.1.2.1.4.1.1.2",
-                    "1.0.8802.1.1.2.1.4.1.1.4", "1.0.8802.1.1.2.1.4.1.1.5",
-                    "1.0.8802.1.1.2.1.4.1.1.6", "1.0.8802.1.1.2.1.4.1.1.7",
-                    "1.0.8802.1.1.2.1.4.1.1.9", "1.0.8802.1.1.2.1.4.1.1.12"
-                    ], bulk=True):
-                    local_interface = self.snmp.get(
-                        "1.3.6.1.2.1.31.1.1.1.1." + v[1], cached=True)
+                # lldpRemLocalPortNum
+                # lldpRemChassisIdSubtype lldpRemChassisId
+                # lldpRemPortIdSubtype lldpRemPortId
+                # lldpRemSysName lldpRemSysCapEnabled
+                for v in self.snmp.get_tables(["1.0.8802.1.1.2.1.4.1.1.2",
+                                               "1.0.8802.1.1.2.1.4.1.1.4",
+                                               "1.0.8802.1.1.2.1.4.1.1.5",
+                                               "1.0.8802.1.1.2.1.4.1.1.6",
+                                               "1.0.8802.1.1.2.1.4.1.1.7",
+                                               "1.0.8802.1.1.2.1.4.1.1.9",
+                                               "1.0.8802.1.1.2.1.4.1.1.12"
+                                               ], bulk=True):
+                    local_interface = self.snmp.get("1.3.6.1.2.1.31.1.1.1.1." +
+                                                    v[1], cached=True)
                     remote_chassis_id_subtype = v[2]
                     remotechassisid = ":".join(["%02x" % ord(c) for c in v[3]])
                     remote_port_subtype = v[4]
@@ -70,68 +75,49 @@ class Script(NOCScript):
             except self.snmp.TimeOutError:
                 pass
         """
-
         # Fallback to CLI
-        lldp = self.cli("show lldp neighbors").split("\n")
-        for i in range(len(lldp)):
-            match =  self.rx_line1.search(lldp[i])
-            if match:
-                local_interface = match.group("interface")
-                remote_chassis_id = match.group("chassis_id")
-                remote_port = match.group("port_id1")
-                remote_system_name = match.group("name")
+        lldp = self.cli("show lldp neighbors")
+        for link in parse_table(lldp, allow_wrap=True):
+            local_interface = link[0]
+            remote_chassis_id = link[1]
+            remote_port = link[2]
+            remote_system_name = link[3]
+            # Build neighbor data
+            # Get capability
+            cap = 0
+            for c in link[4].split(","):
+                c = c.strip()
+                if c:
+                    cap |= self.CAPS_MAP[c]
 
-                # Build neighbor data
-                # Get capability
-                cap = 0
-                for c in match.group("capabilities").split(","):
-                    c = c.strip()
-                    if c:
-                        cap |= {
-                            "O": 1, "r": 2, "B": 4,
-                            "W": 8, "R": 16, "T": 32,
-                            "C": 64, "S": 128, "D": 256,
-                            "H": 512, "TP": 1024,
-                        }[c]
-
-                i += 1
-                match =  self.rx_line2.search(lldp[i])
-                if match:
-                    remote_port = remote_port + match.group("port_id2")
-                else:
-                    i -= 1
-                remote_port = remote_port.replace("GigabitEthernet", "Gi ")
-                remote_port = remote_port.replace("FastEthernet", "Fa ")
-                remote_port = remote_port.replace("TenGigabitEthernet", "Te ")
-
-                # Get remote port subtype
+            # Get remote port subtype
+            remote_port_subtype = 5
+            if self.rx_mac.match(remote_port):
+                # Actually macAddress(3)
+                # Convert MAC to common form
+                remote_port = MACAddressParameter().clean(remote_port)
+                remote_port_subtype = 3
+            elif is_ipv4(remote_port):
+                # Actually networkAddress(4)
+                remote_port_subtype = 4
+            elif is_int(remote_port):
+                # Actually local(7)
                 remote_port_subtype = 5
-                if self.rx_mac.match(remote_port):
-                    # Actually macAddress(3)
-                    # Convert MAC to common form
-                    remote_port = MACAddressParameter().clean(remote_port)
-                    remote_port_subtype = 3
-                elif is_ipv4(remote_port):
-                    # Actually networkAddress(4)
-                    remote_port_subtype = 4
-                elif is_int(remote_port):
-                    # Actually local(7)
-#                   remote_port_subtype = 7  # Whi 7? 7 don't work!!!
-                    remote_port_subtype = 5
 
-                i = {"local_interface": local_interface, "neighbors": []}
-                n = {
-                    "remote_chassis_id": remote_chassis_id,
-                    "remote_port": remote_port,
-                    "remote_capabilities": cap,
-                    "remote_port_subtype": remote_port_subtype,
-                    }
-                if remote_system_name:
-                    n["remote_system_name"] = remote_system_name
-
-            # TODO:
-#               n["remote_chassis_id_subtype"] = 4
-
-                i["neighbors"].append(n)
-                r.append(i)
+            i = {
+                "local_interface": local_interface,
+                "neighbors": []
+            }
+            n = {
+                "remote_chassis_id": remote_chassis_id,
+                "remote_port": remote_port,
+                "remote_capabilities": cap,
+                "remote_port_subtype": remote_port_subtype,
+            }
+            if remote_system_name:
+                n["remote_system_name"] = remote_system_name
+            # @todo:
+            # n["remote_chassis_id_subtype"] = 4
+            i["neighbors"] += [n]
+            r += [i]
         return r
