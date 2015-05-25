9c80aa08ce4c2f17295c58233b14980596d56e98
Dmitry Volodin <dv@nocproject.org>
2015-05-25 08:06:04 +0300

    Juniper.JUNOS.get_dom_status: Code cleanup
    --HG--
    branch : develop

--- sa/profiles/Juniper/JUNOS/get_dom_status.py
+++ sa/profiles/Juniper/JUNOS/get_dom_status.py
@@ -5,13 +5,12 @@
 ## Copyright (C) 2007-2015 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-"""
-"""
+
 ## Python modules
 import re
 ## NOC modules
 from noc.sa.script import Script as NOCScript
-from noc.sa.interfaces import IGetDOMStatus
+from noc.sa.interfaces.igetdomstatus import IGetDOMStatus
 
 
 class Script(NOCScript):
@@ -22,7 +21,7 @@ class Script(NOCScript):
     rx_phy_name = re.compile(r"^(?P<ifname>\S+)")
     rx_temp = re.compile(
         r"Module temperature\s+:\s+(?P<temp>\S+) degrees")
-    rx_beas = re.compile(r"Laser bias current\s+:\s+(?P<bias>\S+) mA")
+    rx_bias = re.compile(r"Laser bias current\s+:\s+(?P<bias>\S+) mA")
     rx_tx_dbm = re.compile(
         r"Laser output power\s+:\s+\S+ mW / (?P<tx_dbm>\S+) dBm")
     rx_rx_dbm = re.compile(
@@ -35,15 +34,15 @@ class Script(NOCScript):
             name = self.re_search(self.rx_phy_name, I).group("ifname")
             temp_c = self.re_search(self.rx_temp, I).group("temp")
             voltage_v = 0
-            current_ma = self.re_search(self.rx_beas, I).group("bias")
+            current_ma = self.re_search(self.rx_bias, I).group("bias")
             optical_tx_dbm = self.re_search(self.rx_tx_dbm, I).group("tx_dbm")
             optical_rx_dbm = self.re_search(self.rx_rx_dbm, I).group("rx_dbm")
-            r.append({
+            r += [{
                 "interface": name,
                 "temp_c": temp_c,
                 "voltage_v": voltage_v,
                 "current_ma": current_ma,
                 "optical_rx_dbm": optical_rx_dbm,
                 "optical_tx_dbm": optical_tx_dbm
-            })
+            }]
         return r
