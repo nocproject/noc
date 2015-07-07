f51dcfe3211f41f4e221a9bdf846f4cacb9e25f0
Dmitry Volodin <dv@nocproject.org>
2015-07-07 11:59:36 +0300

    IGetInterfaceStatusExt interface and Generic.get_interface_status_ex script
    --HG--
    branch : develop

---
+++ sa/interfaces/igetinterfacestatusex.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## IGetInterfaceStatusEx
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2015 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+from base import (Interface, ListOfParameter, DictParameter,
+                  InterfaceNameParameter, BooleanParameter,
+                  IntParameter)
+
+
+class IGetInterfaceStatusEx(Interface):
+    """
+    Returns extended interface status for all available interfaces
+    including port channels and SVI
+    """
+    returns = ListOfParameter(element=DictParameter(attrs={
+        "interface": InterfaceNameParameter(),
+        "admin_status": BooleanParameter(),
+        "oper_status": BooleanParameter(),
+        "full_duplex": BooleanParameter(default=True),
+        "last_change": IntParameter(required=False),
+        # Input speed, kbit/s
+        "in_speed": IntParameter(required=False),
+        # Output speed, kbit/s
+        "out_speed": IntParameter(required=False),
+        # Configured bandwidth, kbit/s
+        "bandwidth": IntParameter(required=False)
+    }))
