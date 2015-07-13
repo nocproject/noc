8a1c90f714a8d0496a1ad3403d44b15a9ecb6629
Dmitry Volodin <dv@nocproject.org>
2015-07-13 14:03:38 +0300

    Huawei.VRP.get_version: ME60-X16 support
    --HG--
    branch : develop

--- sa/profiles/Huawei/VRP/get_version.py
+++ sa/profiles/Huawei/VRP/get_version.py
@@ -16,14 +16,37 @@ class Script(NOCScript):
     cache = True
     implements = [IGetVersion]
 
-    rx_ver = re.compile(r"^VRP.+Software, Version (?P<version>[^ ,]+),? .*?\n\s*(?:Quidway|Huawei) (?P<platform>(?:NetEngine\s+)?\S+)[^\n]+uptime", re.MULTILINE | re.DOTALL | re.IGNORECASE)
-    rx_ver_snmp = re.compile(r"Versatile Routing Platform Software.*?Version (?P<version>[^ ,]+),? .*?\n\s*(?:Quidway|Huawei) (?P<platform>(?:NetEngine\s+)?[^ \t\n\r\f\v\-]+)[^\n]+", re.MULTILINE | re.DOTALL | re.IGNORECASE)
-    rx_ver_snmp2 = re.compile(r"(?P<platform>(?:\S+\s+)?S\d+(?:[A-Z]+-[A-Z]+)?(?:\d+\S+)?)\s+Huawei\sVersatile\sRouting\sPlatform\sSoftware.*Version\s(?P<version>\d+\.\d+)\s\(S\d+\s(?P<image>\S+)+\).*", re.MULTILINE | re.DOTALL | re.IGNORECASE)
-    rx_ver_snmp3 = re.compile(r"^\s*VRP.+Software, Version (?P<version>\S+) \((?P<platform>S\S+|CX\d+) (?P<image>[^)]+)", re.MULTILINE | re.DOTALL | re.IGNORECASE)
+    rx_ver = re.compile(
+        r"^VRP.+Software, Version (?P<version>[^ ,]+),? .*?\n"
+        r"\s*(?:Quidway|Huawei) (?P<platform>(?:NetEngine\s+|MultiserviceEngine\s+)?\S+)[^\n]+uptime",
+        re.MULTILINE | re.DOTALL | re.IGNORECASE
+    )
+    rx_ver_snmp = re.compile(
+        r"Versatile Routing Platform Software.*?"
+        r"Version (?P<version>[^ ,]+),? .*?\n"
+        r"\s*(?:Quidway|Huawei) (?P<platform>(?:NetEngine\s+)?"
+        r"[^ \t\n\r\f\v\-]+)[^\n]+",
+        re.MULTILINE | re.DOTALL | re.IGNORECASE
+    )
+    rx_ver_snmp2 = re.compile(
+        r"(?P<platform>(?:\S+\s+)?S\d+(?:[A-Z]+-[A-Z]+)?(?:\d+\S+)?)"
+        r"\s+Huawei\sVersatile\sRouting\sPlatform"
+        r"\sSoftware.*Version\s(?P<version>\d+\.\d+)\s"
+        r"\(S\d+\s(?P<image>\S+)+\).*",
+        re.MULTILINE | re.DOTALL | re.IGNORECASE
+    )
+    rx_ver_snmp3 = re.compile(
+        r"^\s*VRP.+Software, Version (?P<version>\S+)\s+"
+        r"\((?P<platform>S\S+|CX\d+) (?P<image>[^)]+)",
+        re.MULTILINE | re.DOTALL | re.IGNORECASE
+    )
+
     rx_ver_snmp4 = re.compile(
         r"Huawei Versatile Routing Platform Software.*?"
         r"Version (?P<version>\S+) .*?"
-        r"(?P<platform>MultiserviceEngine \S+)", re.MULTILINE | re.DOTALL | re.IGNORECASE)
+        r"(?P<platform>MultiserviceEngine \S+)",
+        re.MULTILINE | re.DOTALL | re.IGNORECASE
+    )
 
     def execute(self):
         v = ""
@@ -40,9 +63,13 @@ class Script(NOCScript):
                 v = self.cli("display version", cached=True)
             except self.CLISyntaxError:
                 raise self.NotSupportedError()
-        rx = self.find_re([self.rx_ver, self.rx_ver_snmp,
-                           self.rx_ver_snmp2, self.rx_ver_snmp3,
-                           self.rx_ver_snmp4], v)
+        rx = self.find_re([
+            self.rx_ver,
+            self.rx_ver_snmp,
+            self.rx_ver_snmp2,
+            self.rx_ver_snmp3,
+            self.rx_ver_snmp4
+        ], v)
         match = rx.search(v)
         platform = match.group("platform")
         # Convert NetEngine to NE
