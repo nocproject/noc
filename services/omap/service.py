f4432750af604d2b80a26344f2ba81bb2bc5d09b
Dmitry Volodin <dv@nocproject.org>
2015-07-28 00:57:00 +0300

    omap service
    --HG--
    branch : feature/microservices

---
+++ services/omap/service.py
@@ -0,0 +1,38 @@
+#!./bin/python
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Object Mapper service
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2015 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Python modules
+import os
+## NOC modules
+from noc.lib.service.base import Service
+from noc.sa.interfaces.base import StringParameter
+from api.omap import OMapAPI
+
+
+class OMapService(Service):
+    name = "nm"
+    # One active object mapper per environment
+    leader_group_name = "omap-%(env)s"
+
+    # Dict parameter containing values accepted
+    # via dynamic configuration
+    config_interface = {
+        "loglevel": StringParameter(
+            default=os.environ.get("NOC_LOGLEVEL", "info"),
+            choices=["critical", "error", "warning", "info", "debug"]
+        )
+    }
+
+    api = Service.api + [
+        OMapAPI
+    ]
+
+
+if __name__ == "__main__":
+    OMapService().start()
