e5cc11ea256f9c7f64c19362b018567288b2712e
Dmitry Volodin <dv@nocproject.org>
2015-08-20 10:58:45 +0300

    snmp_get rewritten using UDPSocket, new snmp_count() function
    --HG--
    branch : feature/microservices

--- lib/ioloop/snmp.py
+++ lib/ioloop/snmp.py
@@ -11,19 +11,21 @@ import logging
 import socket
 import errno
 ## Third-party modules
-from tornado.ioloop import IOLoop
-from tornado.concurrent import TracebackFuture
-from tornado.util import errno_from_exception
+from tornado.gen import coroutine, Return
 ## NOC modules
 from noc.lib.snmp.version import SNMP_v2c
-from noc.lib.snmp.get import get_pdu, parse_get_response
-from noc.lib.snmp.error import NO_ERROR, SNMPError, TIMED_OUT
-from noc.lib.debug import error_report
+from noc.lib.snmp.get import (get_pdu, getnext_pdu, getbulk_pdu,
+                              parse_get_response)
+from noc.lib.snmp.error import (NO_ERROR, NO_SUCH_NAME,
+                                SNMPError, TIMED_OUT)
+from noc.lib.ioloop.udp import UDPSocket
 
 _ERRNO_WOULDBLOCK = (errno.EWOULDBLOCK, errno.EAGAIN)
 logger = logging.getLogger(__name__)
+BULK_MAX_REPETITIONS = 20
 
 
+@coroutine
 def snmp_get(address, oids, port=161,
              community="public",
              version=SNMP_v2c,
@@ -33,58 +35,6 @@ def snmp_get(address, oids, port=161,
     Perform SNMP get request and returns Future to be used
     inside @tornado.gen.coroutine
     """
-    def on_event(fd, events):
-        try:
-            if events & IOLoop.WRITE:
-                # Delayed send
-                sock.sendto(pdu, (address, port))
-                ioloop.update_handler(fd, IOLoop.READ)
-            elif events & IOLoop.READ:
-                # Process incoming PDU
-                data, addr = sock.recvfrom(4096)
-                ioloop.remove_handler(sock.fileno())
-                sock.close()
-                resp = parse_get_response(data)
-                if resp.error_status != NO_ERROR:
-                    oid = None
-                    if resp.error_index and resp.varbinds:
-                        oid = resp.varbinds[resp.error_index - 1][0]
-                    logger.debug("[%s] SNMP error: %s %s",
-                                 address, oid, resp.error_status)
-                    try:
-                        raise SNMPError(code=resp.error_status, oid=oid)
-                    except SNMPError as e:
-                        future.set_exception(e)
-                else:
-                    # Success
-                    if oid_map:
-                        result = {}
-                        for k, v in resp.varbinds:
-                            if k in oid_map:
-                                result[oid_map[k]] = v
-                    else:
-                        result = resp.varbinds[0][1]
-                    logger.debug("[%s] GET result %s",
-                                 address, result)
-                    future.set_result(result)
-        except Exception as e:
-            error_report()
-            ioloop.remove_handler(sock.fileno())
-            sock.close()
-
-    def on_timeout():
-        """
-        Check future is not timed out
-        """
-        if future.running():
-            logger.debug("[%s] SNMP GET timed out", address)
-            ioloop.remove_handler(sock.fileno())
-            sock.close()
-            try:
-                raise SNMPError(code=TIMED_OUT, oid=oids[0])
-            except Exception as e:
-                future.set_exception(e)
-
     oid_map = {}
     if isinstance(oids, basestring):
         oids = [oids]
@@ -94,33 +44,86 @@ def snmp_get(address, oids, port=161,
     else:
         raise ValueError("oids must be either string or dict")
     logger.debug("[%s] SNMP GET %s", address, oids)
-    ioloop = ioloop or IOLoop.current()
-    pdu = get_pdu(community, oids)
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.setblocking(0)
-    future = TracebackFuture()
-    # Try to send data immediately
+    sock = UDPSocket(ioloop=ioloop)
+    sock.settimeout(timeout)
+    # Send GET PDU
+    pdu = get_pdu(community=community, oids=oids)
+    # Wait for result
     try:
-        sock.sendto(pdu, (address, port))
-        ioloop.add_handler(
-            sock.fileno(),
-            on_event,
-            IOLoop.READ
-        )
-    except socket.error as e:
-        c = errno_from_exception(e)
-        if c in _ERRNO_WOULDBLOCK:
-            ioloop.add_handler(
-                sock.fileno(),
-                on_event,
-                IOLoop.WRITE
-            )
+        yield sock.sendto(pdu, (address, port))
+        data, addr = yield sock.recvfrom(4096)
+    except socket.timeout:
+        raise SNMPError(code=TIMED_OUT, oid=oids[0])
+    resp = parse_get_response(data)
+    if resp.error_status != NO_ERROR:
+        oid = None
+        if resp.error_index and resp.varbinds:
+            oid = resp.varbinds[resp.error_index - 1][0]
+        logger.debug("[%s] SNMP error: %s %s",
+                     address, oid, resp.error_status)
+        raise SNMPError(code=resp.error_status, oid=oid)
+    else:
+        # Success
+        if oid_map:
+            result = {}
+            for k, v in resp.varbinds:
+                if k in oid_map:
+                    result[oid_map[k]] = v
         else:
-            sock.close()
-            future.exception(e)
-    # Add timeout handler
-    ioloop.call_later(
-        timeout,
-        on_timeout
-    )
-    return future
+            result = resp.varbinds[0][1]
+        logger.debug("[%s] GET result %s", address, result)
+        raise Return(result)
+
+
+@coroutine
+def snmp_count(address, oid, port=161,
+               community="public",
+               version=SNMP_v2c,
+               timeout=10,
+               bulk=False,
+               filter=None,
+               max_repetitions=BULK_MAX_REPETITIONS,
+               ioloop=None):
+    """
+    Perform SNMP get request and returns Future to be used
+    inside @tornado.gen.coroutine
+    """
+    logger.debug("[%s] SNMP COUNT %s", address, oid)
+    if not filter:
+        filter = lambda x, y: True
+    poid = oid + "."
+    result = 0
+    sock = UDPSocket(ioloop=ioloop)
+    sock.settimeout(timeout)
+    while True:
+        # Get PDU
+        if bulk:
+            pdu = getbulk_pdu(community, oid,
+                              max_repetitions=max_repetitions)
+        else:
+            pdu = getnext_pdu(community, oid)
+        # Send request and wait for response
+        try:
+            yield sock.sendto(pdu, (address, port))
+            data, addr = yield sock.recvfrom(4096)
+        except socket.timeout:
+            raise SNMPError(code=TIMED_OUT, oid=oid)
+        # Parse response
+        resp = parse_get_response(data)
+        if resp.error_status == NO_SUCH_NAME:
+            # NULL result
+            break
+        elif resp.error_status != NO_ERROR:
+            # Error
+            raise SNMPError(code=resp.error_status, oid=oid)
+        else:
+            # Success value
+            for oid, v in resp.varbinds:
+                if oid.startswith(poid):
+                    # Next value
+                    if filter(oid, v):
+                        result += 1
+                else:
+                    logger.debug("[%s] COUNT result %s",
+                                 address, result)
+                    raise Return(result)
