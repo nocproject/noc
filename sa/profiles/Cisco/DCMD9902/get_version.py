94b4e349163b7b2d682f4d78798059ba58fa098c
Николай Федосеев <mikifeynman@gmail.com>
2015-08-27 07:51:05 +0000

    get_version.py for Cisco DCM D9902
    --HG--
    branch : develop

---
+++ sa/profiles/Cisco/DCMD9902/get_version.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+__author__ = 'FeNikS'
+##----------------------------------------------------------------------
+## Cisco.DCMD9902.get_version
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2015 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Python modules
+import re
+## NOC modules
+from noc.sa.script import Script as NOCScript
+from noc.sa.interfaces import IGetVersion
+
+rx_version = re.compile(r"BUILD=(?P<ver>.*?)$",
+                        re.DOTALL|re.MULTILINE)
+
+class Script(NOCScript):
+    name = "Cisco.DCMD9902.get_version"
+    implements = [IGetVersion]
+
+    def execute(self):
+        version = ''
+        data = self.cli("cat /app/bin/versions")
+        match = rx_version.search(data)
+        if match:
+            version = match.group("ver")
+
+        return {
+            "vendor": "Cisco",
+            "platform": "DCM D9902",
+            "version": version if version else "Unknown"
+        }
