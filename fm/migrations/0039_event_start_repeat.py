ac2dbff03c6259639358c402bfb34a88920e7fd9
Dmitry Volodin <dv@nocproject.org>
2015-10-04 12:44:57 +0300

    Do not require anyAction role
    --HG--
    branch : feature/microservices

--- fm/migrations/0039_event_start_repeat.py
+++ fm/migrations/0039_event_start_repeat.py
@@ -8,17 +8,17 @@
 class Migration:
     def forwards(self):
         from noc.lib.nosql import get_db
-
-        f = """db.%(collection)s.find({repeats: {$exists: false}}).forEach(
-            function(e) {
-                e.repeats = 1;
-                e.start_timestamp = e.timestamp;
-                db.%(collection)s.save(e);
-            })
-        """
         db = get_db()
-        db.eval(f % {"collection": "noc.events.active"})
-        db.eval(f % {"collection": "noc.events.archive"})
+        for c in [db.noc.events.active, db.noc.events.archive]:
+            # @todo: Set start_timestamp = timestamp
+            c.update(
+                {"repeats": {"$exists": False}},
+                {
+                    "$set": {
+                        "repeats": 1
+                    }
+                }
+            )
 
     def backwards(self):
         pass
