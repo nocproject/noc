c7acafba5a8d15f98d1706da736ef4380c81e68e
Dmitry Volodin <dv@nocproject.org>
2015-10-14 17:46:41 +0300

    ./noc service: Do not show system services as URLs
    --HG--
    branch : feature/microservices

--- commands/service.py
+++ commands/service.py
@@ -13,24 +13,30 @@ from noc.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
+    system_services = ["mongod", "postgres", "consul"]
+
     def handle(self, *args, **options):
         self.out("Creating Consul Client", 1)
         c = consul.Consul()
         self.out("Requesting services", 1)
-        s_mask = "%-20s %-10s %-30s %s"
+        s_mask = "%-20s %-12s %-40s %s"
         print s_mask % ("Service", "Node", "URL", "Tags")
         index, service = c.catalog.services()
         for sn in service:
             index, instances = c.catalog.service(sn)
             for si in instances:
+                if si["ServiceName"] in self.system_services:
+                    url = "%s:%s" % (si["Address"], si["ServicePort"])
+                else:
+                    url = "http://%s:%s/api/%s/" % (
+                        si["Address"], si["ServicePort"],
+                        si["ServiceName"]
+                    )
                 print s_mask % (
                     si["ServiceName"],
                     si["Node"],
-                    "http://%s:%s/api/%s/" % (
-                        si["Address"], si["ServicePort"],
-                        si["ServiceName"]
-                    ),
-                    ""
+                    url,
+                    ",".join(si.get("ServiceTags", []) or [])
                 )
 
 if __name__ == "__main__":
