6a2376ab812c8cce522d60f650f7c8106f167fe7
Dmitry Volodin <dv@nocproject.org>
2015-10-23 16:01:29 +0300

    Vyatta.Vyatta.get_version: Recognize VyOS
    --HG--
    branch : feature/microservices

--- sa/profiles/Vyatta/Vyatta/get_version.py
+++ sa/profiles/Vyatta/Vyatta/get_version.py
@@ -2,31 +2,38 @@
 ##----------------------------------------------------------------------
 ## Vyatta.Vyatta.get_version
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2010 The NOC Project
+## Copyright (C) 2007-2015 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-"""
-"""
+
+
 ## Python modules
 import re
 ## NOC modules
-from noc.sa.script import Script as NOCScript
-from noc.sa.interfaces import IGetVersion
-##
-## Vyatta.Vyatta.get_version
-##
-class Script(NOCScript):
-    name="Vyatta.Vyatta.get_version"
-    cache=True
-    implements=[IGetVersion]
-    
-    rx_ver=re.compile(r"^Version:\s+(?P<version>\S+)",re.MULTILINE)
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetversion import IGetVersion
+
+
+class Script(BaseScript):
+    name = "Vyatta.Vyatta.get_version"
+    cache = True
+    interface = IGetVersion
+
+    rx_ver = re.compile(r"^Version:\s+(?P<version>(?:VyOS )?\S+)",
+                        re.MULTILINE)
+
     def execute(self):
-        v=self.cli("show version")
-        match=self.re_search(self.rx_ver, v)
+        v = self.cli("show version")
+        match = self.re_search(self.rx_ver, v)
+        version = match.group("version")
+        if "VyOS" in version:
+            vendor, version = match.group("version").split(" ")
+            platform = "VyOS"
+        else:
+            vendor = "Vyatta"
+            platform = "VC"
         return {
-            "vendor"    : "Vyatta",
-            "platform"  : "VC",
-            "version"   : match.group("version"),
+            "vendor": vendor,
+            "platform": platform,
+            "version": version
         }
-    
