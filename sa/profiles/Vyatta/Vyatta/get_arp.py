3e18331b520ffe89009455ecc059deb55b063d97
Dmitry Volodin <dv@nocproject.org>
2015-10-23 16:02:00 +0300

    Vyatta.Vyatta.get_arp: Use new SA API
    --HG--
    branch : feature/microservices

--- sa/profiles/Vyatta/Vyatta/get_arp.py
+++ sa/profiles/Vyatta/Vyatta/get_arp.py
@@ -9,13 +9,13 @@
 ## Python modules
 import re
 ## NOC modules
-from noc.sa.script import Script as NOCScript
+from noc.core.script.base import BaseScript
 from noc.sa.interfaces.igetarp import IGetARP
 
 
-class Script(NOCScript):
+class Script(BaseScript):
     name = "Vyatta.Vyatta.get_arp"
-    implements = [IGetARP]
+    interface = IGetARP
     
     rx_line = re.compile(
         r"^(?P<ip>\d+\.\d+\.\d+\.\d+)\s+\S+\s+"
