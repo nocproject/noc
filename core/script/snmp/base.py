f812aa93993a7c4a689db0d059259d2a6a8a748e
Dmitry Volodin <dv@nocproject.org>
2015-10-27 01:42:06 +0300

    Script.snmp.get_table and .join_tables
    --HG--
    branch : feature/microservices

--- core/script/snmp/base.py
+++ core/script/snmp/base.py
@@ -78,3 +78,67 @@ class SNMP(object):
 
         self.get_ioloop().run_sync(run)
         return self.result
+
+    def getnext(self, oid, community_suffix=None, bulk=None, min_index=None,
+                max_index=None, cached=False, only_first=False):
+        pass
+
+    def get_table(self, oid, community_suffix=None, bulk=False,
+                  min_index=None, max_index=None, cached=False):
+        """
+        GETNEXT wrapper. Returns a hash of <index> -> <value>
+        """
+        r = {}
+        for o, v in self.getnext(oid, community_suffix=community_suffix,
+                                 bulk=bulk, min_index=min_index,
+                                 max_index=max_index, cached=cached):
+            r[int(o.split(".")[-1])] = v
+        return r
+
+    def join_tables(self, oid1, oid2, community_suffix=None, bulk=False,
+                    min_index=None, max_index=None, cached=False):
+        """
+        Generator returning a rows of two snmp tables joined by index
+        """
+        t1 = self.get_table(oid1, community_suffix=community_suffix, bulk=bulk,
+                            min_index=min_index, max_index=max_index,
+                            cached=cached)
+        t2 = self.get_table(oid2, community_suffix=community_suffix, bulk=bulk,
+                            min_index=min_index, max_index=max_index,
+                            cached=cached)
+        for k1, v1 in t1.items():
+            try:
+                yield (v1, t2[k1])
+            except KeyError:
+                pass
+
+    def get_tables(self, oids, community_suffix=None, bulk=False,
+                      min_index=None, max_index=None, cached=False):
+        """
+        Query list of SNMP tables referenced by oids and yields
+        tuples of (key, value1, ..., valueN)
+
+        :param oids: List of OIDs
+        :param community_suffix: Optional suffix to be added to community
+        :param bulk: Use BULKGETNEXT if true
+        :param min_index:
+        :param max_index:
+        :param cached:
+        :return:
+        """
+        def gen_table(oid):
+            l = len(oid) + 1
+            for o, v in self.getnext(oid, community_suffix=community_suffix,
+                                     bulk=bulk, min_index=min_index,
+                                     max_index=max_index, cached=cached):
+                yield tuple([int(x) for x in o[l:].split(".")]), v
+
+        # Retrieve tables
+        tables = [dict(gen_table(oid)) for oid in oids]
+        # Generate index
+        index = set()
+        for t in tables:
+            index.update(t)
+        # Yield result
+        for i in sorted(index):
+            yield [".".join([str(x) for x in i])] + [t.get(i) for t in tables]
