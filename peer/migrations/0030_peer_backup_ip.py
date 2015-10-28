7453764c56c62ee196aa6c18ad1991238ddcf2b2
Dmitry Volodin <dv@nocproject.org>
2015-10-28 14:21:43 +0300

    Fix migration
    --HG--
    branch : feature/microservices

--- peer/migrations/0030_peer_backup_ip.py
+++ peer/migrations/0030_peer_backup_ip.py
@@ -1,17 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from south.db import db
-from django.db import models
-from noc.peer.models import *
+from noc.lib.fields import INETField
+
 
 class Migration:
-    
     def forwards(self):
         # Adding field 'Peer.remote_backup_ip'
-        db.add_column('peer_peer', 'remote_backup_ip', INETField("Remote Backup IP",null=True,blank=True))
+        db.add_column('peer_peer', 'remote_backup_ip',
+                      INETField("Remote Backup IP", null=True,
+                                blank=True))
         # Adding field 'Peer.local_backup_ip'
-        db.add_column('peer_peer', 'local_backup_ip', INETField("Local Backup IP",null=True,blank=True))
-    
+        db.add_column('peer_peer', 'local_backup_ip',
+                      INETField("Local Backup IP", null=True,
+                                blank=True))
+
     def backwards(self):
         # Deleting field 'Peer.remote_backup_ip'
         db.delete_column('peer_peer', 'remote_backup_ip')
