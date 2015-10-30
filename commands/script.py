caa285219fb6553d3654c9cd11b0aa5236fe7839
Dmitry Volodin <dv@nocproject.org>
2015-10-30 10:28:21 +0300

    ./noc script command
    --HG--
    branch : feature/microservices

---
+++ commands/script.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## ./noc script
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2015 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Python modules
+import os
+import argparse
+import pprint
+import logging
+## NOC modules
+from noc.core.management.base import BaseCommand
+from noc.lib.validators import is_int
+from noc.core.script.loader import loader
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--config",
+            action="store",
+            dest="config",
+            default=os.environ.get("NOC_CONFIG", "etc/noc.yml"),
+            help="Configuration path"
+        )
+        parser.add_argument(
+            "--pretty",
+            action="store_true",
+            dest="pretty",
+            default=False,
+            help="Pretty-print output"
+        )
+        parser.add_argument(
+            "script",
+            nargs=1,
+            help="Script name"
+        )
+        parser.add_argument(
+            "object_name",
+            nargs=1,
+            help="Object name"
+        )
+        parser.add_argument(
+            "arguments",
+            nargs=argparse.REMAINDER,
+            help="Arguments passed to script"
+        )
+
+    def handle(self, config, script, object_name, arguments, pretty,
+               *args, **options):
+        # @todo: --quiet and --debug options
+        logging.getLogger().setLevel(logging.DEBUG)
+        # Get object
+        obj = self.get_object(object_name[0])
+        # Build credentials
+        credentials = self.get_credentials(obj)
+        # Load script
+        script = script[0]
+        if "." not in script:
+            script = "%s.%s" % (obj.profile_name, script)
+        script_class = loader.get_script(script)
+        if not script_class:
+            self.die("Failed to load script %s" % script_class)
+        # Run script
+        service = ServiceStub(pool=obj.pool.name)
+        scr = script_class(
+            service=service,
+            credentials=credentials,
+            capabilities=obj.get_caps(),
+            version=None,
+            timeout=3600
+        )
+        result = scr.run()
+        if pretty:
+            pprint.pprint(result)
+        else:
+            print result
+
+    def get_object(self, object_name):
+        """
+        Resolve object by name or by id
+        """
+        from noc.sa.models.managedobject import ManagedObject
+        from django.db.models import Q
+
+        q = Q(name=object_name)
+        if is_int(object_name):
+            q = Q(id=int(object_name)) | q
+        try:
+            return ManagedObject.objects.get(q)
+        except ManagedObject.DoesNotExist:
+            self.die("Object is not found: %s" % object_name)
+
+    def get_credentials(self, obj):
+        """
+        Returns object's credentials
+        """
+        creds = obj.credentials
+
+        credentials = {
+            "address": obj.address,
+            "user": creds.user,
+            "password": creds.password,
+            "super_password": creds.super_password,
+            "path": obj.remote_path
+        }
+        if creds.snmp_ro:
+            credentials["snmp_version"] = "v2c"
+            credentials["snmp_ro"] = creds.snmp_ro
+        if obj.scheme in (1, 2):
+            credentials["cli_protocol"] = {
+                1: "telnet",
+                2: "ssh"
+            }[obj.scheme]
+        return credentials
+
+
+class ServiceStub(object):
+    class ServiceConfig(object):
+        def __init__(self, pool):
+            self.pool = pool
+
+    def __init__(self, pool):
+        self.config = self.ServiceConfig(pool=pool)
+
+
+if __name__ == "__main__":
+    Command().run()
