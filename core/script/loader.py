d148a5a655fe2dff1f3b2f65b31e218b68eb9e1e
Dmitry Volodin <dv@nocproject.org>
2015-11-01 23:59:01 +0300

    Generic scripts
    --HG--
    branch : feature/microservices

--- core/script/loader.py
+++ core/script/loader.py
@@ -11,8 +11,12 @@ import sys
 import glob
 import logging
 import inspect
-import os
 import threading
+
+import os
+import re
+
+
 ## NOC modules
 from base import BaseScript
 
@@ -20,6 +24,10 @@ logger = logging.getLogger(__name__)
 
 
 class ScriptLoader(object):
+    rx_requires = re.compile(
+        "^\s+requires\s*=\s*\[([^\]]*)\]", re.DOTALL | re.MULTILINE
+    )
+
     def __init__(self):
         self.scripts = {}  # Load scripts
         self.lock = threading.Lock()
@@ -37,7 +45,18 @@ class ScriptLoader(object):
                 if not self.is_valid_name(name):
                     logger.error("Invalid script name")
                     return None
-                module_name = "sa.profiles.%s" % name
+                vendor, system, sn = name.split(".")
+                if os.path.exists(
+                        os.path.join(
+                            "sa", "profiles", vendor, system,
+                            "%s.py" % sn
+                        )
+                ):
+                    # Common script
+                    module_name = "sa.profiles.%s" % name
+                else:
+                    # Generic script
+                    module_name = "sa.profiles.Generic.%s" % sn
                 try:
                     sm = __import__(module_name, {}, {}, "*")
                     for n in dir(sm):
@@ -76,12 +95,44 @@ class ScriptLoader(object):
         """
         Scan all available scripts
         """
+        # Load generic scripts
+        generics = {}  # Name -> dependencies
+        for path in glob.glob("sa/profiles/Generic/*.py"):
+            if path.endswith("/__init__.py"):
+                continue
+            gn = path.rsplit(os.sep)[-1][:-3]
+            with open(path) as f:
+                data = f.read()
+                # Scan for requires = [..]
+                match = self.rx_requires.search(data)
+                if match:
+                    generics[gn] = [
+                        s.strip()[1:-1]
+                        for s in match.group(1).split(",")
+                        if s.strip()
+                    ]
+        # Load common scripts
         ns = set()
+        profiles = set()
         for path in glob.glob("sa/profiles/*/*/*.py"):
             vendor, system, name = path.split(os.sep)[-3:]
             name = name[:-3]
             if name != "__init__":
                 ns.add("%s.%s.%s" % (vendor, system, name))
+                profiles.add("%s.%s" % (vendor, system))
+        # Apply generic scripts
+        for p in profiles:
+            for g in generics:
+                fgn = "%s.%s" % (p, g)
+                if fgn in ns:
+                    # Generic overriden with common script
+                    continue
+                if any(1 for r in generics[g]
+                       if "%s.%s" % (p, r) not in ns):
+                    continue  # Unsatisfied dependency
+                # Add generic script
+                ns.add(fgn)
+        #
         with self.lock:
             self.all_scripts = ns
 
