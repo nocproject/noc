19795b7ccda4ac0d5359c6b64dc77e7149fee731
Dmitry Volodin <dv@nocproject.org>
2015-11-05 01:58:04 +0300

    ChassisDS datasource
    --HG--
    branch : feature/microservices

--- inv/datasources.py
+++ inv/datasources.py
@@ -10,6 +10,9 @@
 from noc.lib.datasource import DataSource
 from noc.inv.models.interface import Interface
 from noc.inv.models.subinterface import SubInterface
+from noc.inv.models.discoveryid import DiscoveryID
+from noc.inv.models.subinterface import SubInterface
+from noc.sa.interfaces.base import MACAddressParameter
 
 
 class InterfaceDS(DataSource):
@@ -54,5 +57,27 @@ class InterfaceDS(DataSource):
         link = self._interface.link
         if link:
             return str(link.id)
+        else:
+            return None
+
+
+class ChassisDS(DataSource):
+    _name = "inv.ChassisDS"
+
+    def __init__(self, mac=None, ipv4=None):
+        self._object = None
+        if mac:
+            mac = MACAddressParameter.clean(mac)
+            self._object = DiscoveryID.find_object(mac)
+        if ipv4:
+            if SubInterface.objects.filter(
+                    ipv4_addresses=ipv4).count() == 1:
+                self._object =  SubInterface.objects.filter(
+                    ipv4_addresses=ipv4).first().managed_object
+
+    @property
+    def object(self):
+        if self._object:
+            return self._object
         else:
             return None
