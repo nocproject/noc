7fb9bcecafb4bb8207f6c079aa8b0d6255d6fe74
Dmitry Volodin <dv@nocproject.org>
2015-11-26 16:19:40 +0300

    Generic.get_ifindexes: Check snmp is available
    --HG--
    branch : feature/microservices

--- sa/profiles/Generic/get_ifindexes.py
+++ sa/profiles/Generic/get_ifindexes.py
@@ -20,18 +20,19 @@ class Script(BaseScript):
 
     def execute(self):
         r = {}
-        try:
-            for oid, v in self.snmp.getnext(mib["IF-MIB::ifDescr"]):
-                try:
-                    v = self.profile.convert_interface_name(v)
-                except InterfaceTypeError, why:
-                    self.logger.info(
-                        "Ignoring unknown interface %s: %s",
-                        v, why
-                    )
-                    continue
-                ifindex = int(oid.split(".")[-1])
-                r[v] = ifindex
-        except self.snmp.TimeOutError:
-            pass
+        if self.has_snmp():
+            try:
+                for oid, v in self.snmp.getnext(mib["IF-MIB::ifDescr"]):
+                    try:
+                        v = self.profile.convert_interface_name(v)
+                    except InterfaceTypeError, why:
+                        self.logger.info(
+                            "Ignoring unknown interface %s: %s",
+                            v, why
+                        )
+                        continue
+                    ifindex = int(oid.split(".")[-1])
+                    r[v] = ifindex
+            except self.snmp.TimeOutError:
+                pass
         return r
