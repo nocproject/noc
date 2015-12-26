fa7b42931a191250e92cedb50f28fb964ce096fe
Dmitry Volodin <dv@nocproject.org>
2015-12-26 19:18:07 +0300

    ETL: Administrative division loader
    --HG--
    branch : feature/microservices

---
+++ core/etl/loader/admdiv.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Administrative division loader
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2015 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+from base import BaseLoader
+from noc.gis.models.division import Division
+
+
+class AdmDivLoader(BaseLoader):
+    """
+    Administrative division loader
+    """
+    name = "admdiv"
+    model = Division
+    fields = [
+        "id",
+        "parent",
+        "name",
+        "short_name"
+    ]
+
+    _mappings = {
+        "parent": "admdiv"
+    }
