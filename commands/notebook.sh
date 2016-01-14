3a65e430fc98f7650598afe571d6715e8e4b36aa
Dmitry Volodin <dv@nocproject.org>
2016-01-14 13:52:16 +0300

    Remove deprecated files
    --HG--
    branch : feature/microservices

--- commands/notebook.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-source ./commands/base.sh
-
-if [ ! -d ".ipython" ]; then
-    mkdir .ipython || error_exit "Cannot create .ipython directory"
-fi
-exec ./bin/ipython notebook --notebook-dir=.ipython
