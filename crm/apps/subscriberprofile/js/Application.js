0080d37a08882e9bc96251f8a9823cd00135debb
Dmitry Volodin <dv@nocproject.org>
2016-01-15 01:12:58 +0300

    Various crm application adjustments
    --HG--
    branch : feature/microservices

--- crm/apps/subscriberprofile/js/Application.js
+++ crm/apps/subscriberprofile/js/Application.js
@@ -14,6 +14,7 @@ Ext.define("NOC.crm.subscriberprofile.Application", {
     ],
     model: "NOC.crm.subscriberprofile.Model",
     search: true,
+    rowClassField: "row_class",
 
     initComponent: function() {
         var me = this;
