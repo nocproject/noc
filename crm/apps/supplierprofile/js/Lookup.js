4b6732abcaa662956ea714782a042829fcc23aa3
Dmitry Volodin <dv@nocproject.org>
2016-01-14 17:49:22 +0300

    SupplierProfile model and crm.supplierprofile application
    --HG--
    branch : feature/microservices

---
+++ crm/apps/supplierprofile/js/Lookup.js
@@ -0,0 +1,12 @@
+//---------------------------------------------------------------------
+// NOC.crm.supplierprofile.Lookup
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2016 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.crm.supplierprofile.Lookup");
+
+Ext.define("NOC.crm.supplierprofile.Lookup", {
+    extend: "NOC.core.Lookup",
+    url: "/crm/supplierprofile/lookup/"
+});
