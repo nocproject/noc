c7fe768ebb98e76be0d151c82781d8d911ddb11d
Dmitry Volodin <dv@nocproject.org>
2016-01-27 03:30:30 +0300

    Fix schedule_discovery handler
    --HG--
    branch : feature/microservices

--- fm/handlers/event/discovery.py
+++ fm/handlers/event/discovery.py
@@ -2,22 +2,14 @@
 ##----------------------------------------------------------------------
 ## Discovery handlers
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2013 The NOC Project
+## Copyright (C) 2007-2016 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 
-## NOC modules
-from noc.inv.discovery.scheduler import get_scheduler
-
 DELAY = 600
-discovery_scheduler = get_scheduler()
-
 
 def schedule_discovery(event):
     """
     Reschedule discovery processes
     """
-    # Check managed object is managed
-    if not event.managed_object.is_managed:
-        return
     event.managed_object.run_discovery(DELAY)
