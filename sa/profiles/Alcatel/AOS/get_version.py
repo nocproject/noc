f6c8c257a373c5fc99cbd8ac0d007638984df828
Dmitry Volodin <dv@nocproject.org>
2016-01-29 03:21:33 +0300

    Alcatel.AOS.get_version: Code cleanup
    --HG--
    branch : feature/microservices

--- sa/profiles/Alcatel/AOS/get_version.py
+++ sa/profiles/Alcatel/AOS/get_version.py
@@ -2,22 +2,15 @@
 ##----------------------------------------------------------------------
 ## Alcatel.AOS.get_version
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2012 The NOC Project
+## Copyright (C) 2007-2016 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-"""
-"""
+
+## Python modules
+import re
+## NOC modules
 from noc.core.script.base import BaseScript
 from noc.sa.interfaces.igetversion import IGetVersion
-import re
-
-rx_sys = re.compile(r"Module in slot.+?Model.*?Name:\s+(?P<platform>.+?),$",
-    re.MULTILINE | re.DOTALL)
-rx_ver = re.compile(r"System.*?Description:\s+(?P<version>.+?)\s.*$",
-    re.MULTILINE | re.DOTALL)
-rx_ver1 = re.compile(
-    r"System.*?Description:\s+Alcatel-Lucent\s+\S+\s+(?P<version>\S+)\s.*$",
-    re.MULTILINE | re.DOTALL)
 
 
 class Script(BaseScript):
@@ -25,13 +18,21 @@ class Script(BaseScript):
     cache = True
     interface = IGetVersion
 
+    rx_sys = re.compile(r"Module in slot.+?Model.*?Name:\s+(?P<platform>.+?),$",
+        re.MULTILINE | re.DOTALL)
+    rx_ver = re.compile(r"System.*?Description:\s+(?P<version>.+?)\s.*$",
+        re.MULTILINE | re.DOTALL)
+    rx_ver1 = re.compile(
+        r"System.*?Description:\s+Alcatel-Lucent\s+\S+\s+(?P<version>\S+)\s.*$",
+        re.MULTILINE | re.DOTALL)
+
     def execute(self):
         v = self.cli("show ni")
-        match_sys = rx_sys.search(v)
+        match_sys = self.rx_sys.search(v)
         v = self.cli("show system")
-        match_ver = rx_ver.search(v)
+        match_ver = self.rx_ver.search(v)
         if match_ver.group("version") == "Alcatel-Lucent":
-            match_ver = rx_ver1.search(v)
+            match_ver = self.rx_ver1.search(v)
         return {
             "vendor": "Alcatel",
             "platform": match_sys.group("platform"),
