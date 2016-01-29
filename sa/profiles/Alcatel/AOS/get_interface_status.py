403a2de757249f2e59229910ed9b356bab02ac20
Dmitry Volodin <dv@nocproject.org>
2016-01-29 03:04:52 +0300

    Fix Alcatel.AOS.get_interface_status
    --HG--
    branch : feature/microservices

--- sa/profiles/Alcatel/AOS/get_interface_status.py
+++ sa/profiles/Alcatel/AOS/get_interface_status.py
@@ -2,14 +2,16 @@
 ##----------------------------------------------------------------------
 ## Alcatel.AOS.get_interface_status
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2013 The NOC Project
+## Copyright (C) 2007-2016 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-"""
-"""
+
+## Python modules
+import re
+## NOC modules
 from noc.core.script.base import BaseScript
 from noc.sa.interfaces.igetinterfacestatus import IGetInterfaceStatus
-import re
+from noc.lib.mib import mib
 
 
 class Script(BaseScript):
@@ -20,16 +22,16 @@ class Script(BaseScript):
         re.IGNORECASE | re.MULTILINE)
 
     def execute(self, interface=None):
-        # Not tested. Must be identical in different vendors
         if self.has_snmp():
             try:
                 # Get interface status
                 r = []
                 # IF-MIB::ifName, IF-MIB::ifOperStatus
-                for n, s in self.snmp.join_tables("1.3.6.1.2.1.31.1.1.1.1",
-                                                  "1.3.6.1.2.1.2.2.1.8",
-                                                  bulk=True):
-                    r += [{"interface":n, "status":int(s) == 1}]
+                for i, n, s in self.snmp.join([
+                    mib["IF-MIB::ifName"],
+                    mib["IF-MIB::ifOperStatus"]
+                ]):
+                    r += [{"interface": n, "status":int(s) == 1}]
                 return r
             except self.snmp.TimeOutError:
                 pass
@@ -38,5 +40,5 @@ class Script(BaseScript):
             r += [{
                 "interface": match.group("interface"),
                 "status": match.group("status").lower() == "up"
-                }]
+            }]
         return r
