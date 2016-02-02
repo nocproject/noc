06c7b43062c9b3d55d8ca3cd11ca84e35047a9bf
Dmitry Volodin <dv@nocproject.org>
2016-02-02 23:03:56 +0300

    Siklu.EH.get_interfaces
    --HG--
    branch : feature/microservices

--- sa/profiles/Siklu/EH/__init__.py
+++ sa/profiles/Siklu/EH/__init__.py
@@ -19,14 +19,15 @@ class Profile(BaseProfile):
     pattern_prompt = r"^\S+?>"
     command_submit = "\r"
 
-
     def cleaned_input(self, input):
-        print ">>>>>>>>>>>>>>>>>>>>>>>> %r" % input
         rx_strip_cmd_repeat = re.compile(r'.+\x1b\[\d+G\r?\n(.*)',
             re.MULTILINE | re.DOTALL)
         match = rx_strip_cmd_repeat.search(input)
         if match:
             input = match.group(1)
         input = super(Profile, self).cleaned_input(input)
-        print "<<<<<<<<<<<<<<<<<<<<<<<< %r" % input
         return input
+
+    def convert_interface_name(self, s):
+        if s.lower().startswith("eth"):
+            return s.lower("eth%s" % s[3:].strip())
