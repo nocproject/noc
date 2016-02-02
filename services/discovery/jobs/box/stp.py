4f4439325fd5769746a08107f90308bdfee0d0a6
Dmitry Volodin <dv@nocproject.org>
2016-02-02 09:53:20 +0300

    discovery: working stp discovery
    --HG--
    branch : feature/microservices

--- services/discovery/jobs/box/stp.py
+++ services/discovery/jobs/box/stp.py
@@ -34,84 +34,12 @@ class STPCheck(TopologyDiscoveryCheck):
             rdmap = self.get_designated_ports(remote_object)
             for li, rpid in roots[ro]:
                 ri = rdmap.get(rpid)
-                if not ri:
-                    continue
-
-        # remote object -> [(local, remote), ..]
-        candidates = defaultdict(set)
-        loops = {}  # first interface, second interface
-        # Check local side
-        for li, ro, ri in self.iter_neighbors(self.object):
-            # Resolve remote object
-            remote_object = self.get_neighbor(ro)
-            if not remote_object:
-                self.logger.debug(
-                    "Remote object '%s' is not found. Skipping",
-                    ro
-                )
-                continue
-            # Resolve remote interface name
-            remote_interface = self.get_remote_interface(
-                remote_object,
-                ri
-            )
-            if not remote_interface:
-                self.logger.debug(
-                    "Cannot resolve remote interface %s:%r. Skipping",
-                    remote_object.name, ri
-                )
-                continue
-            # Detecting loops
-            if remote_object.id == self.object.id:
-                loops[li] = remote_interface
-                if (remote_interface in loops and loops[remote_interface] == li):
-                    self.logger.debug(
-                        "Loop link detected: %s:%s - %s:%s",
-                        self.object.name, li,
-                        self.object.name, remote_interface)
+                if ri:
+                    # Link found
                     self.confirm_link(
                         self.object, li,
-                        remote_object, remote_interface
+                        remote_object, ri
                     )
-                continue
-            # Submitting candidates
-            self.logger.debug(
-                "Link candidate: %s:%s - %s:%s",
-                self.object.name, li,
-                remote_object.name, remote_interface
-            )
-            candidates[remote_object].add((li, remote_interface))
-
-        # Checking candidates from remote side
-        for remote_object in candidates:
-            if (self.required_script and
-                    self.required_script not in remote_object.scripts):
-                self.logger.debug(
-                    "Remote object '%s' does not support %s script. "
-                    "Cannot confirm links",
-                    remote_object.name
-                )
-            confirmed = set()
-            for li, ro_id, ri in self.iter_neighbors(remote_object):
-                ro = self.get_neighbor(ro_id)
-                if not ro or ro.id != self.object.id:
-                    continue  # To other objects
-                remote_interface = self.get_remote_interface(
-                    self.object,
-                    ri
-                )
-                if remote_interface:
-                    confirmed.add((remote_interface, li))
-            for l, r in candidates[remote_object] & confirmed:
-                self.confirm_link(
-                    self.object, l,
-                    remote_object, r
-                )
-            for l, r in candidates[remote_object] - confirmed:
-                self.reject_link(
-                    self.object, l,
-                    remote_object, r
-                )
 
     def get_root_ports(self):
         """
