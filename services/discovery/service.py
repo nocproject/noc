f00efd1ec2361ae6cbf3ebd7dfdb43950dc3b5ce
Dmitry Volodin <dv@nocproject.org>
2016-02-04 01:34:41 +0300

    discovery: Send collected metrics to InfluxDB
    --HG--
    branch : feature/microservices

--- services/discovery/service.py
+++ services/discovery/service.py
@@ -7,9 +7,12 @@
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 
+# Python modules
+import threading
 # Third-party modules
 import tornado.ioloop
 import tornado.gen
+import tornado.httpclient
 ## NOC modules
 from noc.core.service.base import Service
 from noc.core.scheduler.scheduler import Scheduler
@@ -23,9 +26,21 @@ class DiscoveryService(Service):
     def __init__(self):
         super(DiscoveryService, self).__init__()
         self.scheduler = None
+        self.metrics = []
+        self.metrics_lock = threading.Lock()
+        self.send_callback = None
 
     @tornado.gen.coroutine
     def on_activate(self):
+        # Send spooled messages every 250ms
+        self.logger.debug("Stating message sender task")
+        self.send_callback = tornado.ioloop.PeriodicCallback(
+            self.send_metrics,
+            250,
+            self.ioloop
+        )
+        self.send_callback.start()
+        #
         self.scheduler = Scheduler(
             "discovery",
             pool=self.config.pool,
@@ -33,7 +48,50 @@ class DiscoveryService(Service):
             max_threads=self.config.max_threads,
             ioloop=self.ioloop
         )
+        self.scheduler.service = self
         self.scheduler.run()
+
+    @tornado.gen.coroutine
+    def send_metrics(self):
+        """
+        Send collected metrics to InfluxDB
+        """
+        with self.metrics_lock:
+            if not self.metrics:
+                return
+            msg = "\n".join(self.metrics)
+            self.metrics = []
+        # Send collected metrics
+        for s in self.resolve_service("influxdb"):
+            client = tornado.httpclient.AsyncHTTPClient(force_instance=True)
+            try:
+                response = yield client.fetch(
+                    # @todo: Configurable database name
+                    "http://%s/write?db=noc" % s,
+                    method="POST",
+                    body=msg
+                )
+                # @todo: Check for 204
+                msg = ""
+            except tornado.httpclient.HTTPError as e:
+                self.logger.error(
+                    "Failed to spool collected metrics to %s: %s",
+                    s, str(e)
+                )
+            except Exception as e:
+                self.logger.error(
+                    "Failed to spool collected metrics to %s: %s",
+                    s, str(e)
+                )
+            client.close()
+        if msg:
+            # Return metrics to queue
+            with self.metrics_lock:
+                self.metrics = [msg] + self.metrics
+
+    def register_metrics(self, batch):
+        with self.metrics_lock:
+            self.metrics += [batch]
 
 
 if __name__ == "__main__":
