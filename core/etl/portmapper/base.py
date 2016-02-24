8082d5df87b610f40f78147811be11257d67aa78
Dmitry Volodin <dv@nocproject.org>
2016-02-24 17:42:24 +0300

    ./noc nri-link command
    --HG--
    branch : feature/microservices

--- core/etl/portmapper/base.py
+++ core/etl/portmapper/base.py
@@ -19,10 +19,10 @@ class BasePortMapper(object):
     NOC's format converted to NRI's one via methods decorated with
     @to_mappings()
     """
-    _from_profile_mappings = {}
-    _from_platform_mappings = {}
-    _to_profile_mappings = {}
-    _to_platform_mappings = {}
+    _profile_to_local = {}
+    _platform_to_local = {}
+    _profile_to_remote = {}
+    _platform_to_remote = {}
 
     class __metaclass__(type):
         """
@@ -34,14 +34,14 @@ class BasePortMapper(object):
                 mm = getattr(n, m)
                 if hasattr(mm, "_match"):
                     for d in mm._match:
-                        if d["from"] and d["profile"]:
-                            m._from_profile_mappings[d["profile"]] = mm
-                        elif d["from"] and d["platform"]:
-                            m._from_platform_mappings[d["platform"]] = mm
-                        elif not d["from"] and d["profile"]:
-                            m._to_profile_mappings[d["profile"]] = mm
-                        elif not d["from"] and d["plaform"]:
-                            m._to_platform_mappings[d["platform"]] = mm
+                        if d["local"] and d["profile"]:
+                            n._profile_to_local[d["profile"]] = mm
+                        elif d["local"] and d["platform"]:
+                            n._platform_to_local[d["platform"]] = mm
+                        elif not d["local"] and d["profile"]:
+                            n._profile_to_remote[d["profile"]] = mm
+                        elif not d["local"] and d["plaform"]:
+                            n._platform_to_remote[d["platform"]] = mm
             return n
 
     def __init__(self, managed_object):
@@ -54,12 +54,12 @@ class BasePortMapper(object):
         Convert interface name from NRI to NOC's conventions
         """
         if self.platform:
-            mm = self._to_platform_mappings.get(self.platform)
+            mm = self._platform_to_local.get(self.platform)
             if mm:
-                return mm(name)
-        mm = self._to_profile_mappings.get(self.profile)
+                return mm(self, name)
+        mm = self._profile_to_local.get(self.profile)
         if mm:
-            return mm(name)
+            return mm(self, name)
         else:
             return None
 
@@ -68,31 +68,31 @@ class BasePortMapper(object):
         Convert interface name
         """
         if self.platform:
-            mm = self._from_platform_mappings.get(self.platform)
+            mm = self._platform_to_remote.get(self.platform)
             if mm:
-                return mm(name)
-        mm = self._from_profile_mappings.get(self.profile)
+                return mm(self, name)
+        mm = self._profile_to_remote.get(self.profile)
         if mm:
-            return mm(name)
+            return mm(self, name)
         else:
             return None
 
 
-def from_mapping(profile=None, platform=None):
+def to_local(profile=None, platform=None):
     def wrap(f):
         if not hasattr(f, "_match"):
             f._match = []
-        f._match += [{"from": True, "profile": profile, "platform": platform}]
+        f._match += [{"local": True, "profile": profile, "platform": platform}]
         return f
 
     return wrap
 
 
-def to_mapping(profile=None, platform=None):
+def to_remote(profile=None, platform=None):
     def wrap(f):
         if not hasattr(f, "_match"):
             f._match = []
-        f._match += [{"from": False, "profile": profile, "platform": platform}]
+        f._match += [{"local": False, "profile": profile, "platform": platform}]
         return f
 
     return wrap
