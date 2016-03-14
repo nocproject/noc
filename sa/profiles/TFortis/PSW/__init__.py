1228c9f9cd24fb757d0587af8c282c9199997765
Aleksey Shirokih <freeseacher@gmail.com>
2016-03-14 20:00:53 +0300

    TFortis profile
    --HG--
    branch : feature/microservices

---
+++ sa/profiles/TFortis/PSW/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Vendor: TFortis
+## OS:     PSW
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+from noc.core.profile.base import BaseProfile
+
+
+class Profile(BaseProfile):
+    name = "TFortis.PSW"
+    pattern_username = "User Name>"
+    pattern_password = "User Password>"
+    pattern_prompt = r"^TFortis .+#"
+
+    def convert_interface_name(self, s):
+        return s
