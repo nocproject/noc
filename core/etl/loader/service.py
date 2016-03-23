00db119bcb3415aff366a4b31ea7f76ce033349a
Dmitry Volodin <dv@nocproject.org>
2016-03-23 15:03:34 +0300

    Simplified service model
    --HG--
    branch : feature/microservices

---
+++ core/etl/loader/service.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Service loader
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+from base import BaseLoader
+from noc.sa.models.service import Service
+
+
+class ServiceLoader(BaseLoader):
+    """
+    Service loader
+    """
+    name = "service"
+    model = Service
+    fields = [
+        "id",
+        "parent",
+        "subscriber",
+        "profile",
+        "ts",
+        "logical_status",
+        "logical_status_start",
+        "agreement_id",
+        "order_id",
+        "stage_id",
+        "stage_name",
+        "stage_start",
+        "account_id",
+        "address",
+        "managed_object",
+        "nri_port",
+        "cpe_serial",
+        "cpe_mac",
+        "cpe_model",
+        "cpe_group"
+    ]
+
+    mapped_fields = {
+        "parent": "service",
+        "subscriber": "subscriber",
+        "managed_object": "managed_object"
+    }
