e89c2fc6f6d5caac3540e7c7f90e0d8f6f7fd37e
Dmitry Volodin <dv@nocproject.org>
2016-03-29 10:48:15 +0300

    etl: MO to TT system mappings
    --HG--
    branch : feature/microservices

---
+++ core/etl/loader/ttmap.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## TTMap loader
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+from base import BaseLoader
+from noc.inv.models.extnrittmap import ExtNRITTMap
+
+
+class TTMapLoader(BaseLoader):
+    name = "ttmap"
+    model = ExtNRITTMap
+    fields = [
+        "id",
+        "managed_object",
+        "tt_system",
+        "queue",
+        "remote_id"
+    ]
+
+    mapped_fields = {
+        "managed_object": "managedobject",
+        "tt_system": "ttsystem"
+    }
