fd1f2a7d4a747afd10f3f5e7a19d23ec6ec0cd31
Dmitry Volodin <dv@nocproject.org>
2016-03-30 13:07:53 +0300

    card: TT Card
    --HG--
    branch : feature/microservices

---
+++ services/card/cards/tt.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## TT card handler
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Python modules
+import datetime
+import operator
+## Third-party modules
+import cachetools
+## NOC modules
+from base import BaseCard
+from noc.fm.models.ttsystem import TTSystem
+
+from noc.fm.models.utils import get_alarm
+from noc.inv.models.object import Object
+from noc.fm.models.activealarm import ActiveAlarm
+from noc.fm.models.archivedalarm import ArchivedAlarm
+
+
+class TTCard(BaseCard):
+    default_template_name = "tt"
+
+    tts_cache = cachetools.TTLCache(
+        maxsize=100,
+        ttl=60,
+        missing=lambda x: TTSystem.objects.filter(name=x).first()
+    )
+
+    def dereference(self, id):
+        if ":" not in id:
+            return None
+        tts_name, tt_id = id.split(":", 1)
+        tts = self.tts_cache[tts_name]
+        if not tts:
+            return None
+        tt = tts.get_system().get_tt(tt_id)
+        if tt:
+            tt["tt_system_name"] = tts_name
+            tt["full_id"] = id
+            return tt
+        else:
+            return None
+
+    def get_data(self):
+        r = self.object.copy()
+        if r["resolved"]:
+            r["duration"] = r["close_ts"] - r["open_ts"]
+        else:
+            r["dunration"] = None
+        return r
