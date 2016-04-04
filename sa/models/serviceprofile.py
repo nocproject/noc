cbaf4daed0ae1fc74bdd6239046408d73a6e6704
Dmitry Volodin <dv@nocproject.org>
2016-04-04 12:14:06 +0300

    ServiceProfile: Update all interfaces' profiles on change
    --HG--
    branch : feature/microservices

--- sa/models/serviceprofile.py
+++ sa/models/serviceprofile.py
@@ -12,10 +12,13 @@ import operator
 from mongoengine.document import Document
 from mongoengine.fields import StringField, ReferenceField
 from noc.inv.models.interfaceprofile import InterfaceProfile
+from noc.core.model.decorator import on_save
+from noc.core.defer import call_later
 
 import cachetools
 
 
+@on_save
 class ServiceProfile(Document):
     meta = {
         "collection": "noc.serviceprofiles"
@@ -38,3 +41,30 @@ class ServiceProfile(Document):
     @cachetools.cachedmethod(operator.attrgetter("_id_cache"))
     def get_by_id(cls, id):
         return ServiceProfile.objects.filter(id=id).first()
+
+    def on_save(self):
+        if "interface_profile" in self._changed_fields:
+            call_later(
+                "noc.sa.models.serviceprofile.refresh_interface_profiles",
+                sp_id=self.id,
+                ip_id=self.interface_profile.id if self.interface_profile else None
+            )
+
+
+def refresh_interface_profiles(sp_id, ip_id):
+    from service import Service
+    from noc.inv.models.interface import Interface
+    svc = [
+        x["_id"]
+        for x in Service._get_collection().find(
+            {"profile": sp_id}, {"_id": 1}
+        )
+    ]
+    if not svc:
+        return
+    bulk = Interface._get_collection().initialize_unordered_bulk_op()
+    bulk.find({
+        "_id": {"$in": svc}
+    }).update({
+        "$set": {"profile": ip_id}
+    })
