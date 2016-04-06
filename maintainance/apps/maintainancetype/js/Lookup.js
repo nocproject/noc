0569905734501edf237b6b3ffb5d04eaf64f74d3
Dmitry Volodin <dv@nocproject.org>
2016-04-06 17:37:50 +0300

    maintainance module skeleton
    --HG--
    branch : feature/microservices

---
+++ maintainance/apps/maintainancetype/js/Lookup.js
@@ -0,0 +1,12 @@
+//---------------------------------------------------------------------
+// NOC.maintainance.maintainancetype.Lookup
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2016 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.maintainance.maintainancetype.Lookup");
+
+Ext.define("NOC.maintainance.maintainancetype.Lookup", {
+    extend: "NOC.core.Lookup",
+    url: "/maintainance/maintainancetype/lookup/"
+});
---
+++ maintainance/apps/maintainancetype/js/Model.js
@@ -0,0 +1,31 @@
+//---------------------------------------------------------------------
+// maintainance.maintainancetype Model
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2016 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.maintainance.maintainancetype.Model");
+
+Ext.define("NOC.maintainance.maintainancetype.Model", {
+    extend: "Ext.data.Model",
+    rest_url: "/maintainance/maintainancetype/",
+
+    fields: [
+        {
+            name: "id",
+            type: "string"
+        },
+        {
+            name: "description",
+            type: "string"
+        },
+        {
+            name: "name",
+            type: "string"
+        },
+        {
+            name: "suppress_alarms",
+            type: "boolean"
+        }
+    ]
+});
