b99bf53a67e89e31e4e1151088d72f7dcfc52c44
Dmitry Volodin <dv@nocproject.org>
2016-04-09 18:51:05 +0300

    EdgeCore.ES.get_metrics: Fix oids
    --HG--
    branch : feature/microservices

--- sa/profiles/EdgeCore/ES/get_metrics.py
+++ sa/profiles/EdgeCore/ES/get_metrics.py
@@ -14,10 +14,10 @@ class Script(GetMetricsScript):
 
     SNMP_OIDS = GetMetricsScript.merge_oids({
         "CPU | Usage": [
-            ("SNMP", "1.3.6.1.4.1.259.6.10.94.1.39.2.1", "gauge", 1)
+            ("SNMP", "1.3.6.1.4.1.259.6.10.94.1.39.2.1.0", "gauge", 1)
         ],
         "Memory | Usage": [
-            ("SNMP", "1.3.6.1.4.1.259.6.10.94.1.39.3.2", "gauge", 1)
+            ("SNMP", "1.3.6.1.4.1.259.6.10.94.1.39.3.2.0", "gauge", 1)
         ]
 
     })
