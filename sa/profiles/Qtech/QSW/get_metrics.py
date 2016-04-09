fa4138ef1ee5ce6ec5b216b2f799446f661676fd
Aleksey Shirokih <freeseacher@gmail.com>
2016-04-09 17:06:57 +0300

    CPU for Qtech. Memory in non uniform format
    --HG--
    branch : feature/microservices

---
+++ sa/profiles/Qtech/QSW/get_metrics.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Qtech.QSW.get_metrics
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+from noc.sa.profiles.Generic.get_metrics import Script as GetMetricsScript
+
+
+class Script(GetMetricsScript):
+    name = "Qtech.QSW.get_metrics"
+
+    SNMP_OIDS = GetMetricsScript.merge_oids({
+        "CPU | Usage": [
+            ("SNMP", "1.3.6.1.4.1.27514.100.1.11.10.0", "gauge", 1)
+        ],
+        "Memory | Total": [
+            ("SNMP", "1.3.6.1.4.1.27514.100.1.11.6.0", "gauge", 1)
+        ],
+        "Memory | Used": [
+            ("SNMP", "1.3.6.1.4.1.27514.100.1.11.6.0", "gauge", 1)
+        ]
+
+    })
