ceb8ba7483f9dda180620febb0a610a286eb5ef9
Aleksey Shirokih <freeseacher@gmail.com>
2016-04-10 21:52:32 +0300

    Alcatel.TIMOS get subscribers
    --HG--
    branch : feature/microservices

---
+++ sa/profiles/Alcatel/TIMOS/get_metrics.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Huawei.VRP.get_metrics
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+from noc.sa.profiles.Generic.get_metrics import Script as GetMetricsScript
+from noc.core.script.metrics import percent
+
+
+class Script(GetMetricsScript):
+    name = "Huawei.VRP.get_metrics"
+
+    SNMP_OIDS = GetMetricsScript.merge_oids({
+        "Subscribers | IPOE": [
+            ("SNMP", "1.3.6.1.4.1.6527.3.1.2.33.1.107.1.65.1", "gauge", 1)
+        ],
+    })
