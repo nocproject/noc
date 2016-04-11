c973a8827ad4b22cf930c2715c6ea0f173d7a840
Aleksey Shirokih <freeseacher@gmail.com>
2016-04-11 11:40:41 +0300

    EdgeCore.ES.reboot
    --HG--
    branch : feature/microservices

---
+++ sa/profiles/EdgeCore/ES/reboot.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## EdgeCore.ES.reboot
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.ireboot import IReboot
+
+
+class Script(BaseScript):
+    name = "EdgeCore.ES.reboot"
+    interface = IReboot
+
+    def execute(self):
+        self.cli("reload \ny", nowait=True)
