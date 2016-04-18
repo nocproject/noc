b883cd4b3c33787b8ce8a520ba610a6d0b2885e1
Dmitry Volodin <dv@nocproject.org>
2016-04-19 00:40:17 +0300

    login: change credentials
    --HG--
    branch : feature/microservices

--- services/login/backends/local.py
+++ services/login/backends/local.py
@@ -9,6 +9,7 @@
 ## NOC modules
 from base import BaseAuthBackend
 from noc.main.models import User
+from noc.core.translation import ugettext as _
 
 
 class LocalBackend(BaseAuthBackend):
@@ -16,6 +17,16 @@ class LocalBackend(BaseAuthBackend):
         try:
             user = User.objects.get(username=user)
         except User.DoesNotExist:
-            raise self.LoginError("User does not exists")
+            raise self.LoginError(_("User does not exists"))
         if not user.check_password(password):
-            raise self.LoginError("Invalid password")
+            raise self.LoginError(_("Invalid password"))
+
+    def change_credentials(self, user=None, old_password=None,
+                           new_password=None, **kwargs):
+        try:
+            user = User.objects.get(username=user)
+        except User.DoesNotExist:
+            raise self.LoginError(_("User does not exists"))
+        if not user.check_password(old_password):
+            raise self.LoginError(_("Invalid password"))
+        user.set_password(new_password)
