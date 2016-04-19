28f8d2afdde76a7614aba9817b1144777780f724
Dmitry Volodin <dv@nocproject.org>
2016-04-19 12:15:45 +0300

    links to outages and heatmap in main application
    --HG--
    branch : feature/microservices

---
+++ fm/apps/heatmap/views.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## fm.totaloutage
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+from noc.lib.app.extapplication import ExtApplication
+from noc.core.translation import ugettext as _
+
+
+class HeatmapApplication(ExtApplication):
+    title = _("Heatmap")
+    menu = _("Heatmap")
+    glyph = "globe"
+    link = "/api/card/view/alarmheat/1/"
