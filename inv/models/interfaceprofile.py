da76e893dfd1c99e740224579a29b9b9443c9e85
Dmitry Volodin <dv@nocproject.org>
2016-04-20 16:23:48 +0300

    InterfaceProfile.weight for weight-based severity calculation
    --HG--
    branch : feature/microservices

--- inv/models/interfaceprofile.py
+++ inv/models/interfaceprofile.py
@@ -10,7 +10,7 @@
 from mongoengine.document import Document, EmbeddedDocument
 from mongoengine.fields import (StringField, BooleanField,
                                 ReferenceField, FloatField, ListField,
-                                EmbeddedDocumentField)
+                                EmbeddedDocumentField, IntField)
 ## NOC modules
 from noc.lib.nosql import ForeignKeyField
 from noc.main.models.style import Style
@@ -60,17 +60,13 @@ class InterfaceProfile(Document):
     )
     # Collect mac addresses on interface
     mac_discovery = BooleanField(default=False)
-    # check_link alarm job interval settings
-    # Either None or T0,I0,T1,I1,...,Tn-1,In-1,,In
-    # See MultiIntervalJob settings for details
-    check_link_interval = StringField(default=",60")
     # Send up/down notifications
     status_change_notification = ForeignKeyField(NotificationGroup,
                                                  required=False)
-    # Count as customer port in alarms summary
-    is_customer = BooleanField(default=False)
     # Interface profile metrics
     metrics = ListField(EmbeddedDocumentField(InterfaceProfileMetrics))
+    # Alarm weight
+    weight = IntField(default=0)
 
     def __unicode__(self):
         return self.name
