81796efd1c9f0ed0a5ed1cef21079ccc0776985d
Ilya Somov <pnpwin@gmail.com>
2016-04-30 14:54:21 +0300

    sa: Huawei VRP fix get_version and get_config
    --HG--
    branch : sa-huawei-vrp

--- sa/profiles/Huawei/VRP/get_config.py
+++ sa/profiles/Huawei/VRP/get_config.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2009 The NOC Project
+## Huawei.VRP.get_config
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 """
 """
+## NOC modules
 from noc.core.script.base import BaseScript
 from noc.sa.interfaces.igetconfig import IGetConfig
 
@@ -14,5 +17,7 @@ class Script(BaseScript):
     interface = IGetConfig
 
     def execute(self):
+        self.cli("undo terminal monitor")
         config = self.cli("display current-configuration")
+        config = self.profile.clean_spaces(config)
         return self.cleaned_config(config)
