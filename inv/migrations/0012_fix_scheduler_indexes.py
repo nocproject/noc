fa7cf801c15acab559bb327db20afb308864d8bf
Dmitry Volodin <dv@nocproject.org>
2016-05-10 11:45:01 +0300

    Rebuild scheduler index
    --HG--
    branch : feature/microservices

---
+++ inv/migrations/0012_fix_scheduler_indexes.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Drop scheduler indexes
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Python modules
+import logging
+## NOC modules
+from noc.lib.nosql import get_db
+
+logger = logging.getLogger(__name__)
+
+
+class Migration(object):
+    def forwards(self):
+        db = get_db()
+        for c in db.collection_names():
+            if c.startswith("noc.schedules."):
+                db[c].drop_index("ts_1")
+
+    def backwards(self):
+        pass
