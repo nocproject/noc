18d80177056b6a949efac6d8947f6a23ad7845cf
Dmitry Volodin <dv@nocproject.org>
2016-05-11 19:32:55 +0300

    BER TLV parsing speedup
    --HG--
    branch : feature/microservices

--- speedup/ber.pyx
+++ speedup/ber.pyx
@@ -8,6 +8,61 @@
 
 from libc.stdio cimport snprintf
 
+def parse_tlv_header(bytes msg):
+    """
+    Parse TLV header
+    :returns tag_class, tag_id, is_primitive, is implicit, offset, length
+    """
+    cdef unsigned char* ptr
+    cdef unsigned char v, c
+    cdef int tag_class, tag_id, i, skip, l, tl
+    cdef bint is_primitive, is_implicit
+
+    ptr = msg
+    v = ptr[0]
+    # 0xc0 == 11000000
+    tag_class = v & 0xc0
+    # 0x20 == 00100000
+    if v & 0x20:
+        is_primitive = False
+    else:
+        is_primitive = True
+    #
+    is_implicit = False
+    # 0x1f == 00011111
+    tag_id = v & 0x1f
+    #
+    skip = 1
+    if tag_id == 0x1f:
+        # high-tag number form
+        tag_id = 0
+        while True:
+            c = ptr[skip]
+            skip += 1
+            tag_id = (tag_id << 7) + (c & 0x7f)
+            if not (c & 0x80):
+                break
+    elif v & 0x80:
+        # Implicit types
+        if is_primitive and ptr[1] == 0:
+            tag_id = 5
+        else:
+            is_implicit = True
+    # Parse length
+    l = ptr[skip]
+    skip += 1
+    if l & 0x80:
+        # Long form
+        tl = l & 0x7f
+        l = 0
+        while tl:
+            l = (l << 8) + ptr[skip]
+            skip += 1
+            tl -= 1
+    #
+    return tag_class, tag_id, is_primitive, is_implicit, skip, l
+
+
 def parse_p_oid(bytes msg):
     """
     >>> BERDecoder().parse_p_oid("+\\x06\\x01\\x02\\x01\\x01\\x05\\x00")
@@ -16,7 +71,8 @@ def parse_p_oid(bytes msg):
     cdef unsigned int b, i
     cdef char v
     cdef char[1024] out
-    cdef char* ptr, o_ptr
+    cdef char* ptr
+    cdef char* o_ptr
 
     ptr = msg
     optr = out
