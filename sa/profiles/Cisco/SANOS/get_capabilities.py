bbae083f9b8397761896a4b6791e5551c5b6e480
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2016-05-16 04:51:40 +0300

    Add Cisco.SANOS profile
    --HG--
    branch : feature/microservices

---
+++ sa/profiles/Cisco/SANOS/get_capabilities.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Cisco.SANOS.get_capabilities_ex
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+from noc.sa.profiles.Generic.get_capabilities import Script as BaseScript
+from noc.sa.profiles.Generic.get_capabilities import false_on_cli_error
+from noc.lib.mib import mib
+
+
+class Script(BaseScript):
+    name = "Cisco.SANOS.get_capabilities"
+
+    @false_on_cli_error
+    def has_cdp(self):
+        """
+        Check box has cdp enabled
+        """
+        r = self.cli("show cdp neighbors")
+        return "% CDP is not enabled" not in r
---
+++ sa/profiles/Cisco/SANOS/get_config.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Cisco.SANOS.get_config
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetconfig import IGetConfig
+
+
+class Script(BaseScript):
+    name = "Cisco.SANOS.get_config"
+    interface = IGetConfig
+
+    def execute(self):
+        config = self.cli("show running-config")
+        return self.cleaned_config(config)
