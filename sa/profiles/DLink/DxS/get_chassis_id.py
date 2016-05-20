69f10338bc6ddbcfdea8c358d2ba1d8e9b94e562
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2016-05-20 16:30:50 +0300

    Update DLink.DxS.get_chassis_id profile
    --HG--
    branch : feature/microservices

--- sa/profiles/DLink/DxS/get_chassis_id.py
+++ sa/profiles/DLink/DxS/get_chassis_id.py
@@ -2,7 +2,7 @@
 ##----------------------------------------------------------------------
 ## DLink.DxS.get_chassis_id
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2011 The NOC Project
+## Copyright (C) 2007-2016 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 """
@@ -15,11 +15,29 @@ import re
 class Script(BaseScript):
     name = "DLink.DxS.get_chassis_id"
     cache = True
-    rx_ver = re.compile(r"^MAC Address\s+:\s*(?P<id>\S+)",
-        re.IGNORECASE | re.MULTILINE)
     interface = IGetChassisID
 
+    rx_ver = re.compile(r"^MAC Address\s+:\s*(?P<id>\S+)",
+        re.IGNORECASE | re.MULTILINE)
+    rx_line = re.compile(
+        r"^\s*\d+\s+(?:\S+\s+)?"
+        r"([0-9A-F]{2}-[0-9A-F]{2}-[0-9A-F]{2}-[0-9A-F]{2}-"
+        r"[0-9A-F]{2}-[0-9A-F]{2})\s+CPU\s+Self\s*(?:\S*\s*)?$",
+        re.MULTILINE)
+
     def execute(self):
+        try:
+            v = self.cli("show fdb static", cached=True)
+            macs = self.rx_line.findall(v)
+            if macs:
+                macs.sort()
+                return [{
+                    "first_chassis_mac": f,
+                    "last_chassis_mac": t
+                } for f, t in self.macs_to_ranges(macs)]
+        except:
+            pass
+
         match = self.re_search(self.rx_ver, self.cli("show switch",
             cached=True))
         mac = match.group("id")
