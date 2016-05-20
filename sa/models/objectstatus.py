9e4fe980d60126bd5c16eed686e361fe77abc51d
Dmitry Volodin <dv@nocproject.org>
2016-05-20 13:19:25 +0300

    ObjectStatus.set_status speedup
    --HG--
    branch : feature/microservices

--- sa/models/objectstatus.py
+++ sa/models/objectstatus.py
@@ -52,11 +52,11 @@ class ObjectStatus(Document):
     def set_status(cls, object, status, ts=None):
         from noc.fm.models.outage import Outage
 
-        s = cls.objects.filter(object=object.id).first()
-        if s:
-            if s.status != status:
-                s.status = status
-                s.save()
-        else:
-            cls(object=object.id, status=status).save()
+        ObjectStatus._get_collection().update({
+            "_id": object.id
+        }, {
+            "$set": {
+                "status": status
+            }
+        }, upsert=True)
         Outage.register_outage(object, not status, ts=ts)
