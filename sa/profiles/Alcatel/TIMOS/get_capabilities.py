393808c15e14108d56c433483cd9f5898eaddb3a
Aleksey Shirokih <freeseacher@gmail.com>
2016-05-25 12:08:33 +0300

    Alcatel.TIMOS has_lldp
    --HG--
    branch : feature/microservices

--- sa/profiles/Alcatel/TIMOS/get_capabilities.py
+++ sa/profiles/Alcatel/TIMOS/get_capabilities.py
@@ -6,9 +6,11 @@
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 
+## Python modules
+import re
 ## NOC modules
 from noc.sa.profiles.Generic.get_capabilities import Script as BaseScript
-
+from noc.sa.profiles.Generic.get_capabilities import false_on_cli_error
 
 class Script(BaseScript):
     name = "Alcatel.TIMOS.get_capabilities"
@@ -16,3 +18,12 @@ class Script(BaseScript):
     CHECK_SNMP_GET = {
         "BRAS | IPoE": "1.3.6.1.4.1.6527.3.1.2.33.1.107.1.65.1"
     }
+    rx_lldp = re.compile(r"Admin Enabled\s+: True")
+
+    @false_on_cli_error
+    def has_lldp(self):
+        """
+        Check box has lldp enabled
+        """
+        cmd = self.cli("show system lldp | match Admin")
+        return self.rx_lldp.search(cmd) is not None
