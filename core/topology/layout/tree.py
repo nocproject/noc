131f93eaa21362622d302726b428fedb01d53edb
Dmitry Volodin <dv@nocproject.org>
2016-05-27 11:54:43 +0300

    inv.map: Automatical tree layout
    --HG--
    branch : feature/microservices

---
+++ core/topology/layout/tree.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Tree layout class
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Python modules
+import math
+from collections import defaultdict
+## Third-party modules
+import networkx as nx
+import numpy as np
+## NOC modules
+from base import LayoutBase
+
+
+class TreeLayout(LayoutBase):
+    # Horizontal step
+    DX = 100
+    # Vertical step
+    DY = 100
+
+    def get_layout(self):
+        T = self.topology
+        uplinks = T.get_uplinks()
+        w = sum(self.get_width(n) for n in uplinks)
+        x0 = 0
+        pos = {}
+        for u in uplinks:
+            pos.update(self.get_pos(u, x0, w))
+            x0 += T.G.node[u]["tree_width"]
+        return pos
+
+    def get_width(self, node, uplink=None):
+        """
+        Calculate children tree width
+        """
+        T = self.topology
+        downlinks = list(n for n in nx.all_neighbors(T.G, node) if n != uplink)
+        w = 0
+        for d in downlinks:
+            w += self.get_width(d, node)
+        w = max(w, 1)
+        T.G.node[node]["tree_width"] = w
+        T.G.node[node]["tree_downlinks"] = downlinks
+        return w
+
+    def get_pos(self, node, x0, total_w, level=0):
+        T = self.topology
+        n = T.G.node[node]
+        downlinks = n["tree_downlinks"]
+        x = x0 + n["tree_width"] // 2
+        if downlinks and len(downlinks) % 2 == 0 and x > total_w // 2:
+            x -= 1
+        pos = {
+            node: np.array([
+                x * self.DX,
+                level * self.DY
+            ])
+        }
+        for d in downlinks:
+            pos.update(
+                self.get_pos(d, x0, total_w, level + 1)
+            )
+            x0 += T.G.node[d]["tree_width"]
+        return pos
