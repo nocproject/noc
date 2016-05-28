4bf4ab5ec7f1ee4b9b4a71caaa7641906d4a3246
vean <none@none>
2016-05-28 10:35:11 +0500

    Add ASA multiple context support
    --HG--
    branch : add/ASAContext

--- sa/profiles/Cisco/ASA/get_version.py
+++ sa/profiles/Cisco/ASA/get_version.py
@@ -17,21 +17,33 @@ class Script(BaseScript):
     name = "Cisco.ASA.get_version"
     cache = True
     interface = IGetVersion
+
     rx_ver = re.compile(
         r"Cisco (?:Adaptive|PIX) Security Appliance Software Version (?P<version>\S+)"
-        r".+System image file is \".+?:/(?P<image>.+?)\""
         r".+Hardware:\s+(?P<platform>.+?),",
+        re.MULTILINE | re.DOTALL
+    )
+    rx_image = re.compile(
+        r".+System image file is \".+?:/(?P<image>.+?)\"",
         re.MULTILINE | re.DOTALL
     )
 
     def execute(self):
+        if self.capabilities.get("Cisco | ASA | Security | Context | Mode"):
+            if self.capabilities["Cisco | ASA | Security | Context | Mode"] == "multiple":
+                self.cli("changeto system")
+
         v = self.cli("show version")
         match = self.re_search(self.rx_ver, v)
+        try:
+            image = self.re_search(self.rx_image, v).group("image")
+        except self.UnexpectedResultError:
+            image = ""
         return {
             "vendor": "Cisco",
             "platform": match.group("platform"),
             "version": match.group("version"),
             "attributes": {
-                "image": match.group("image"),
+                "image": image,
             }
         }
