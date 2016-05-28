131f3e8f99dd9c86981e3c2e1a57584dccd8bace
vean <none@none>
2016-05-28 10:35:11 +0500

    Add ASA multiple context support
    --HG--
    branch : add/ASAContext

---
+++ sa/profiles/Cisco/ASA/get_capabilities.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Cisco.ASA.get_capabilities_ex
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2015 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+## Python modules
+import re
+## NOC modules
+from noc.sa.profiles.Generic.get_capabilities import Script as BaseScript
+from noc.sa.profiles.Generic.get_capabilities import false_on_cli_error
+
+
+class Script(BaseScript):
+    name = "Cisco.ASA.get_capabilities"
+
+    sec_mode = re.compile(r".+?: (?P<mode>multiple|single|noconfirm)\s+", re.MULTILINE | re.DOTALL)
+
+    @false_on_cli_error
+    def execute_platform(self, caps):
+        caps["Cisco | ASA | Security | Context | Mode"] = "nosupport"
+        v = self.cli("show mode")
+        if "Invalid input detected at" not in v:
+            match = self.re_search(self.sec_mode, v)
+            sec_mode = match.group("mode")
+            self.logger.debug('Mode {0}'.format(sec_mode))
+
+            caps["Cisco | ASA | Security | Context | Mode"] = sec_mode
--- sa/profiles/Cisco/ASA/get_config.py
+++ sa/profiles/Cisco/ASA/get_config.py
@@ -3,7 +3,8 @@
 ## Copyright (C) 2007-2013 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-
+## Python modules
+import re
 ## NOC modules
 from noc.core.script.base import BaseScript
 from noc.sa.interfaces.igetconfig import IGetConfig
@@ -13,7 +14,37 @@ class Script(BaseScript):
     name = "Cisco.ASA.get_config"
     interface = IGetConfig
 
-    def execute(self):
-        config = self.cli("more system:running-config")
+    context_line = re.compile(r".*?(?P<contextname>\S+)\s+(?P<class>\S+)\s+(?P<interfaces>\S+)\s+(?P<url>.+)",
+                              re.MULTILINE | re.DOTALL)
+
+    def get_config(self, path):
+        config = self.cli("more " + path)
         config = self.strip_first_lines(config, 3)
         return self.cleaned_config(config)
+
+    def execute(self):
+        if self.capabilities.get("Cisco | ASA | Security | Context | Mode"):
+            if self.capabilities["Cisco | ASA | Security | Context | Mode"] == "multiple":
+                self.cli("changeto system")
+                v = self.cli("show context")
+                contexts = []
+                for l in v.splitlines():
+                    l = l.strip()
+                    if l == '':
+                        continue
+                    match = self.re_search(self.context_line, l)
+                    if match.group("contextname") == "Context" or match.group("contextname") == "Total":
+                        continue
+                    contexts.append({"name": match.group("contextname"),
+                                    "url": match.group("url")})
+
+                complete_config = self.get_config("system:running-config")
+                for c in contexts:
+                    # config = self.get_config("system:running-config")
+                    config = self.get_config(c["url"])
+                    complete_config += "!{0}{1}{2}\n{3}\n".format("=" * 40, c["name"], "=" * 40, config)
+                return complete_config
+        else:
+            config = self.cli("more system:running-config")
+            config = self.strip_first_lines(config, 3)
+            return self.cleaned_config(config)
