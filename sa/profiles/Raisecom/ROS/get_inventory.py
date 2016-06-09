5c5ceda2ea02147128de375771d324cc4891e0c9
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2016-06-09 12:53:46 +0300

    Add Raisecom.ROS.get_inventory script
    --HG--
    branch : feature/microservices

---
+++ sa/profiles/Raisecom/ROS/get_inventory.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Raisecom.ROS.get_inventory
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+## NOC modules
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetinventory import IGetInventory
+
+
+class Script(BaseScript):
+    name = "Raisecom.ROS.get_inventory"
+    interface = IGetInventory
+
+    def execute(self):
+        v = self.profile.get_version(self)
+        return [{
+            "type": "CHASSIS",
+            "number": "1",
+            "vendor": "RAISECOM",
+            "part_no": v["platform"],
+            "revision": v["hw_rev"],
+            "serial": v["serial"]
+        }]
