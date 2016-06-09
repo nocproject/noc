7140b273913e7aadb800328c94e121618a22efee
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2016-06-09 11:46:46 +0300

    Update Raisecom.ROS profile
    --HG--
    branch : feature/microservices

--- sa/profiles/Raisecom/ROS/__init__.py
+++ sa/profiles/Raisecom/ROS/__init__.py
@@ -8,6 +8,9 @@
 ##----------------------------------------------------------------------
 """
 """
+## Python modules
+import re
+## NOC modules
 from noc.core.profile.base import BaseProfile
 
 
@@ -18,3 +21,18 @@ class Profile(BaseProfile):
     command_super = "enable"
     pattern_prompt = r"^\S+?#"
     command_more = " "
+
+    rx_ver = re.compile(
+        r"Product name: (?P<platform>\S+)\s*\n"
+        r"ROS\s+Version\s*(?P<version>\S+)\.\s*\(Compiled.+\)\s*\n"
+        r"Support ipv6\s*:\s*\S+\s*\n"
+        r"Bootstrap\s*Version\s*(?P<bootstrap>\S+)\s*\n"
+        r"FPGA Version\s*\n"
+        r"Hardware\s*\S+\s*Version Rev\.\s*(?P<hw_rev>\S+)\s*\n\n"
+        r"System MacAddress is\s*:\s*(?P<mac>\S+)\s*\n"
+        r"Serial number\s*:\s*(?P<serial>\S+)\s*\n",
+        re.MULTILINE)
+
+    def get_version(self, script):
+        c = script.cli("show version", cached=True)
+        return self.rx_ver.search(c).groupdict()
--- sa/profiles/Raisecom/ROS/get_version.py
+++ sa/profiles/Raisecom/ROS/get_version.py
@@ -2,28 +2,32 @@
 ##----------------------------------------------------------------------
 ## Raisecom.ROS.get_version
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2009 The NOC Project
+## Copyright (C) 2007-2016 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 """
 """
+## Python modules
+import re
+## NOC modules
 from noc.core.script.base import BaseScript
 from noc.sa.interfaces.igetversion import IGetVersion
-import re
-
-rx_ver = re.compile(r"^Product name:\s+(?P<platform>\S+).*ROS  Version ROS_(?P<version>.+?)\.\(Compiled", re.MULTILINE | re.DOTALL)
 
 
 class Script(BaseScript):
     name = "Raisecom.ROS.get_version"
-    cache = True
     interface = IGetVersion
+    cache = True
 
     def execute(self):
-        v = self.cli("show version")
-        match = rx_ver.search(v)
+        v = self.profile.get_version(self)
         return {
             "vendor": "Raisecom",
-            "platform": match.group("platform"),
-            "version": match.group("version")
+            "platform": v["platform"],
+            "version": v["version"],
+            "attributes": {
+                "Serial Number": v["serial"],
+                "Boot PROM": v["bootstrap"],
+                "HW version": v["hw_rev"]
+            }
         }
