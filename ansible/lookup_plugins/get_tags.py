1bd6f7d7b0128e34529d9dc0058684e13e1ad6fd
Aleksey Shirokih <freeseacher@gmail.com>
2016-06-11 01:09:50 +0300

    Add tags lookup method
    --HG--
    branch : feature/microservices

---
+++ ansible/lookup_plugins/get_tags.py
@@ -0,0 +1,14 @@
+from __future__ import (absolute_import, division, print_function)
+
+from ansible.plugins.lookup import LookupBase
+from __main__ import cli
+
+class LookupModule(LookupBase):
+
+    def run(self, terms='', **kwargs):
+        tag = cli.get_opt('tags')
+        if tag:
+            tags = cli.get_opt('tags').split(',')
+        else:
+            tags = []
+        return [ tags ]
