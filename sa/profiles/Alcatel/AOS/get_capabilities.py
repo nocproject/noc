63e66d62778d05afb08115f093d181f4d50d5a79
Dmitry Volodin <dv@nocproject.org>
2016-06-14 16:38:03 +0300

    Alcatel.AOS.get_capability: Check UDLD capability
    --HG--
    branch : feature/microservices

--- sa/profiles/Alcatel/AOS/get_capabilities.py
+++ sa/profiles/Alcatel/AOS/get_capabilities.py
@@ -17,11 +17,21 @@ class Script(BaseScript):
     name = "Alcatel.AOS.get_capabilities"
 
     rx_lldp = re.compile(r"LLDP Mode\s+= Bridge Mode")
+    rx_udld = re.compile("Global UDLD Status\s*:\s*(?P<status>\S+)")
 
     @false_on_cli_error
     def has_lldp(self):
         """
-        Check box has lldp enabled
+        Check box has LLDP enabled
         """
         cmd = self.cli("show lldp LOCAL-SYSTEM")
         return self.rx_lldp.search(cmd) is not None
+
+    @false_on_cli_error
+    def has_udld(self):
+        """
+        Check box has UDLD enabled
+        """
+        r = self.cli("show udld configuration")
+        match = self.rx_udld.search(r)
+        return match and match.group("status") == "enabled"
