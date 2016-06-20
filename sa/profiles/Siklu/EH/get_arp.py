80b2c1de01745292a6e723e82f0755f056794ce8
Dmitry Volodin <dv@nocproject.org>
2016-06-20 15:42:18 +0300

    Siklu.EH.get_arp
    --HG--
    branch : feature/microservices

---
+++ sa/profiles/Siklu/EH/get_arp.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Siklu.EH.get_arp
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetarp import IGetARP
+
+
+class Script(BaseScript):
+    name = "Siklu.EH.get_arp"
+    interface = IGetARP
+
+    def execute(self):
+        v = self.cli("show arp")
+        r = []
+        for l in v.splitlines():
+            parts = l.split()
+            if len(parts) == 3 and parts[2] == "dynamic":
+                r += [{
+                    "ip": parts[0],
+                    "mac": parts[1]
+                }]
+        return r
