baf979cc4488e5a2583472f2846a18c423ec6923
Dmitry Volodin <dv@nocproject.org>
2016-06-21 11:11:01 +0300

    DLink.DxS.ping: DES-1210-28/ME support
    --HG--
    branch : feature/microservices

--- sa/profiles/DLink/DxS/ping.py
+++ sa/profiles/DLink/DxS/ping.py
@@ -2,23 +2,32 @@
 ##----------------------------------------------------------------------
 ## DLink.DxS.ping
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2011 The NOC Project
+## Copyright (C) 2007-2016 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-"""
-"""
+
+## Python modules
+import re
+## NOC modules
 from noc.core.script.base import BaseScript
 from noc.sa.interfaces.iping import IPing
-import re
 
 
 class Script(BaseScript):
     name = "DLink.DxS.ping"
     interface = IPing
-    rx_result = re.compile(r"^\s*Packets: Sent =\s*(?P<count>\d+), Received =\s*(?P<success>\d+), Lost =\s*\d+", re.MULTILINE | re.DOTALL | re.IGNORECASE)
+    rx_result = re.compile(
+        r"^\s*Packets: Sent =\s*(?P<count>\d+), Received =\s*(?P<success>\d+), Lost =\s*\d+",
+        re.MULTILINE | re.DOTALL | re.IGNORECASE
+    )
+    # DES-1210-28/ME
+    rx_result_des1210 = re.compile(
+        r"(?P<count>\d+)\s+Packets Transmitted,\s+(?P<success>\d+)\s+Packets Received,\s+\d+%\s+Packets Loss",
+        re.MULTILINE | re.DOTALL | re.IGNORECASE
+    )
 
-    def execute(self, address, count=None, source_address=None, size=None,
-    df=None):
+    def execute(self, address, count=None, source_address=None,
+                size=None, df=None, *args, **kwargs):
         cmd = "ping %s" % address
         if count:
             cmd += " times %d" % int(count)
@@ -26,14 +35,14 @@ class Script(BaseScript):
             cmd += " times 5"
         if source_address:
             cmd += " source_ip %s" % source_address
-        # Don't implemented, may be in future firmware revisions ?
+        # Not implemented, may be in future firmware revisions ?
         #if size:
         #    cmd+=" size %d"%int(size)
         #if df:
         #    cmd+=" df-bit"
-        match = self.rx_result.search(self.cli(cmd))
-        if not match:
-            raise self.NotSupportedError()
+        r = self.cli(cmd)
+        rx = self.find_re([self.rx_result, self.rx_result_des1210], r)
+        match = rx.search(r)
         r = {
             "success": int(match.group("success")),
             "count": int(match.group("count")),
