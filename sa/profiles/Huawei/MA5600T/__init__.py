785e134fea339d2f413f6386473ce5e98f09b356
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2016-06-23 14:14:49 +0300

    Initial support for Huawei.MA5600T profile
    --HG--
    branch : feature/microservices

---
+++ sa/profiles/Huawei/MA5600T/__init__.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Vendor: Huawei
+## OS:     MA5600T
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from noc.core.profile.base import BaseProfile
+import re
+
+
+class Profile(BaseProfile):
+    name = "Huawei.MA5600T"
+    pattern_username = r"^>>User name:"
+    pattern_password = r"^>>(?:User )?[Pp]assword:"
+    pattern_more = [
+        (r"^---- More", " "),
+        (r"\[<frameId/slotId>\]", "\n"),
+        (r"\(y/n\) \[n\]", "y\n"),
+        (r"\[to\]\:", "\n")
+    ]
+    pattern_unpriveleged_prompt = r"^(?P<hostname>(?!>)\S+?)>"
+    pattern_prompt = r"^(?P<hostname>(?!>)\S+?)(?:-\d+)?(?:\(config\S*[^\)]*\))?#"
+    pattern_syntax_error = r"% Unknown command"
+    command_more = " "
+    config_volatile = ["^%.*?$"]
+    command_disable_pager="length 0"
+    command_super = "enable"
+    command_enter_config = "configure terminal"
+    command_leave_config = "quit"
+    command_save_config = "save\ny\n"
+
+    rx_slots = re.compile("^\s*\d+", re.MULTILINE)
+    rx_ports = re.compile("^\s*\d+\s+(?P<type>ADSL|GE)\s+", re.MULTILINE)
+
+    def get_slots_n(self, script):
+        i = -1
+        v = script.cli("display board 0", cached=True)
+        for match in self.rx_slots.finditer(v):
+            i += 1
+        return i
+
+    def get_ports_n(self, script, slot_no):
+        i = -1
+        t = ""
+        v = script.cli(("display board 0/%d" % slot_no), cached=True)
+        for match in self.rx_ports.finditer(v):
+            i += 1
+            t = match.group("type")
+        return {"n": i, "t": t}
+
+    def fill_ports(self, script):
+        n = self.get_slots_n(script)
+        r = []
+        i = 0
+        while i < n:
+            r += [self.get_ports_n(script, i)]
+            i += 1
+        return r
---
+++ sa/profiles/Huawei/MA5600T/get_chassis_id.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Huawei.MA5600T.get_chassis_id
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetchassisid import IGetChassisID
+import re
+
+
+class Script(BaseScript):
+    name = "Huawei.MA5600T.get_chassis_id"
+    interface = IGetChassisID
+    rx_mac = re.compile(
+        r"^\s*Current MAC address of active mainboard:\s+(?P<mac>\S+)",
+        re.MULTILINE)
+
+    def execute(self):
+        match = self.re_search(self.rx_mac,
+            self.cli("display sysman mac-address"))
+        mac = match.group("mac")
+        return {
+            "first_chassis_mac": mac,
+            "last_chassis_mac": mac
+        }
