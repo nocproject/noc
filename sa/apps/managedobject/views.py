be48bea3f68364895fde201bc04edc07130132ee
Dmitry Volodin <dv@nocproject.org>
2016-06-24 00:02:04 +0300

    Remove PendingLinkCheck code
    --HG--
    branch : feature/microservices

--- sa/apps/managedobject/views.py
+++ sa/apps/managedobject/views.py
@@ -26,7 +26,6 @@ from noc.inv.models.link import Link
 from noc.inv.models.interface import Interface
 from noc.inv.models.interfaceprofile import InterfaceProfile
 from noc.inv.models.subinterface import SubInterface
-from noc.inv.models.pendinglinkcheck import PendingLinkCheck
 from noc.lib.app.modelinline import ModelInline
 from noc.lib.app.repoinline import RepoInline
 from noc.main.models.resourcestate import ResourceState
@@ -170,82 +169,10 @@ class ManagedObjectApplication(ExtModelApplication):
                         "remote_interface": str(ri.id),
                         "remote_interface__label": ri.name,
                         "discovery_method": link.discovery_method,
-                        "commited": True,
                         "local_description": li.description,
                         "remote_description": ri.description
                     }]
-        # Get pending links
-        q = MQ(local_object=o.id) | MQ(remote_object=o.id)
-        for link in PendingLinkCheck.objects.filter(q):
-            if link.local_object.id == o.id:
-                ro = link.remote_object
-                lin = link.local_interface
-                rin = link.remote_interface
-            else:
-                ro = link.local_object
-                lin = link.remote_interface
-                rin = link.local_interface
-            li = Interface.objects.filter(managed_object=o.id, name=lin).first()
-            if not li:
-                continue
-            ri = Interface.objects.filter(managed_object=ro.id, name=rin).first()
-            if not ri:
-                continue
-            result += [{
-                "id": str(link.id),
-                "local_interface": str(li.id),
-                "local_interface__label": li.name,
-                "remote_object": ro.id,
-                "remote_object__label": ro.name,
-                "remote_interface": str(ri.id),
-                "remote_interface__label": ri.name,
-                "discovery_method": link.method,
-                "commited": False,
-                "local_description": li.description,
-                "remote_description": ri.description
-            }]
         return result
-
-    @view(url="^link/approve/$", method=["POST"],
-          access="change_link", api=True)
-    def api_link_approve(self, request):
-        d = json_decode(request.raw_post_data)
-        plc = self.get_object_or_404(PendingLinkCheck, id=d.get("link"))
-        li = Interface.objects.filter(
-            managed_object=plc.local_object.id,
-            name=plc.local_interface
-            ).first()
-        if not li:
-            return {
-                "success": False,
-                "error": "Interface not found: %s:%s" % (
-                    plc.local_object.name, plc.local_interface)
-            }
-        ri = Interface.objects.filter(
-            managed_object=plc.remote_object.id,
-            name=plc.remote_interface
-            ).first()
-        if not ri:
-            return {
-                "success": False,
-                "error": "Interface not found: %s:%s" % (
-                    plc.remote_object.name, plc.remote_interface)
-            }
-        li.link_ptp(ri, method=plc.method + "+manual")
-        plc.delete()
-        return {
-            "success": True
-        }
-
-    @view(url="^link/reject/$", method=["POST"],
-          access="change_link", api=True)
-    def api_link_reject(self, request):
-        d = json_decode(request.raw_post_data)
-        plc = self.get_object_or_404(PendingLinkCheck, id=d.get("link"))
-        plc.delete()
-        return {
-            "success": True
-        }
 
     def check_mrt_access(self, request, name):
         # @todo: Check object's access
