f39345725b86c909da8faffadfad7c3faa78930d
Dmitry Volodin <dv@nocproject.org>
2016-07-05 10:31:23 +0300

    ./noc psql command
    --HG--
    branch : feature/microservices

---
+++ commands/psql.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# env file deployed by tower and contain following environment variables
+# PGPASSFILE=var/etc/postgres/.pgpass
+# PGDATABASE, PGHOST, PGPORT, PGUSER
+source var/etc/postgres/env
+psql
