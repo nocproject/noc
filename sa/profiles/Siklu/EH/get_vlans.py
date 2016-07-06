7467a8c0bb76a09380bae9d8d70d8f34b371a7b1
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2016-07-06 13:01:36 +0300

    Add Siklu.EH.get_vlans script
    --HG--
    branch : feature/microservices

---
+++ sa/profiles/Siklu/EH/get_vlans.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Siklu.EH.get_vlans
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetvlans import IGetVlans
+import re
+
+
+class Script(BaseScript):
+    name = "Siklu.EH.get_vlans"
+    interface = IGetVlans
+    rx_vlan = re.compile(
+        r"^\s*\S+\s+(?P<vlanid>\d+)\s+\d+\s+\S+\s+\S+\s+\S+\s*\n",
+        re.MULTILINE)
+
+    def execute(self):
+        r = []
+        c = self.cli("show vlan\n")
+        for match in self.rx_vlan.finditer(c):
+            vlan_id = int(match.group('vlanid'))
+            if vlan_id == 1:
+                continue
+            found = False
+            for v in r:
+                if v["vlan_id"] == vlan_id:
+                    found = True
+                    break
+            if not found:
+                r += [{"vlan_id": vlan_id}]
+        return r
