257a6157d3ecf9a37baf1a9e2c3231a28606826d
Dmitry Volodin <dv@nocproject.org>
2016-07-14 10:13:45 +0300

    Additional i18n
    --HG--
    branch : feature/microservices

---
+++ services/web/apps/peer/__init__.py
@@ -0,0 +1,13 @@
+## -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Settings for "peer" module
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+from noc.core.translation import ugettext as _
+
+
+MODULE_NAME = _("Peering Management")
