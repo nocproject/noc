a71040ca8f2e39d2c816483ddc65757f7fcd63c0
Dmitry Volodin <dv@nocproject.org>
2016-07-25 11:00:54 +0300

    Remove unused code
    --HG--
    branch : feature/microservices

--- services/web/apps/wf/__init__.py
+++ services/web/apps/wf/__init__.py
@@ -1,15 +1,8 @@
-# -*- coding: utf-8 -*-
+## -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
-## wf models
+## Settings for "wf" module
 ##----------------------------------------------------------------------
 ## Copyright (C) 2007-2013 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-
-## NOC modules
-from solution import Solution
-from workflow import Workflow
-from variable import Variable
-from lane import Lane
-from node import Node
-from process import Process
+MODULE_NAME = "Workflow"
