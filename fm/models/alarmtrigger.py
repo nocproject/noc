e7ad1449a2c23200c17962089b8bfc544156fd0a
Dmitry Volodin <dv@nocproject.org>
2016-08-02 16:24:57 +0300

    Fix alarmtriggers
    --HG--
    branch : feature/microservices

--- fm/models/alarmtrigger.py
+++ fm/models/alarmtrigger.py
@@ -12,6 +12,7 @@ from django.db import models
 from noc.sa.models.managedobjectselector import ManagedObjectSelector
 from noc.main.models.timepattern import TimePattern
 from noc.main.models.notificationgroup import NotificationGroup
+from noc.main.models.template import Template
 from noc.main.models.pyrule import PyRule
 
 
@@ -19,27 +20,27 @@ class AlarmTrigger(models.Model):
     class Meta:
         db_table = "fm_alarmtrigger"
         app_label = "fm"
-        verbose_name = _("Alarm Trigger")
-        verbose_name_plural = _("Alarm Triggers")
+        verbose_name = "Alarm Trigger"
+        verbose_name_plural = "Alarm Triggers"
 
-    name = models.CharField(_("Name"), max_length=64, unique=True)
-    is_enabled = models.BooleanField(_("Is Enabled"), default=True)
-    alarm_class_re = models.CharField(_("Alarm class RE"), max_length=256)
-    condition = models.CharField(_("Condition"), max_length=256, default="True")
+    name = models.CharField("Name", max_length=64, unique=True)
+    is_enabled = models.BooleanField("Is Enabled", default=True)
+    alarm_class_re = models.CharField("Alarm class RE", max_length=256)
+    condition = models.CharField("Condition", max_length=256, default="True")
     time_pattern = models.ForeignKey(TimePattern,
-                                     verbose_name=_("Time Pattern"),
+                                     verbose_name="Time Pattern",
                                      null=True, blank=True)
     selector = models.ForeignKey(ManagedObjectSelector,
-                                 verbose_name=_("Managed Object Selector"),
+                                 verbose_name="Managed Object Selector",
                                  null=True, blank=True)
     notification_group = models.ForeignKey(NotificationGroup,
-                                           verbose_name=_("Notification Group"),
+                                           verbose_name="Notification Group",
                                            null=True, blank=True)
-    template = models.ForeignKey(NOCTemplate,
-                                 verbose_name=_("Template"),
+    template = models.ForeignKey(Template,
+                                 verbose_name="Template",
                                  null=True, blank=True)
     pyrule = models.ForeignKey(PyRule,
-                               verbose_name=_("pyRule"),
+                               verbose_name="pyRule",
                                null=True, blank=True,
                                limit_choices_to={"interface": "IAlarmTrigger"})
 
