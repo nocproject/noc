82e1e290ebad3426e8085460bb981caa68072d8a
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2016-08-04 16:49:19 +0300

    Update Iskratel.MSAN profile
    --HG--
    branch : feature/microservices

--- sa/profiles/Iskratel/MSAN/get_config.py
+++ sa/profiles/Iskratel/MSAN/get_config.py
@@ -1,27 +1,23 @@
 # -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
-## Iskratel.MSAN.get_inventory
+## Iskratel.MSAN.get_config
 ##----------------------------------------------------------------------
 ## Copyright (C) 2007-2016 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-"""
-"""
+
 ## NOC modules
 from noc.core.script.base import BaseScript
-from noc.sa.interfaces.igetinventory import IGetInventory
+from noc.sa.interfaces.igetconfig import IGetConfig
 
 
 class Script(BaseScript):
-    name = "Iskratel.MSAN.get_inventory"
-    interface = IGetInventory
+    name = "Iskratel.MSAN.get_config"
+    interface = IGetConfig
 
     def execute(self):
-        v = self.profile.get_hardware(self)
-        return [{
-            "type": "LINECARD",
-            "number": v["number"],
-            "vendor": "ISKRATEL",
-            "part_no": v["part_no"],
-            "serial": v["serial"]
-        }]
+        try:
+            config = self.cli("show running-config")
+        except self.CLISyntaxError:
+            raise self.NotSupportedError()
+        return self.cleaned_config(config)
