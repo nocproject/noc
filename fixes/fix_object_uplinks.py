a38fb472269752284c400b4ca560690edfb570ba
Dmitry Volodin <dv@nocproject.org>
2016-08-10 16:53:33 +0300

    fix: fix_object_uplinks
    --HG--
    branch : feature/microservices

---
+++ fixes/fix_object_uplinks.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Update ObjectUplink
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+from noc.inv.models.networksegment import NetworkSegment
+from noc.inv.models.objectuplink import ObjectUplink
+from noc.core.topology.segment import SegmentTopology
+
+
+def fix():
+    for ns in NetworkSegment.objects.all():
+        st = SegmentTopology(ns)
+        ObjectUplink.update_uplinks(
+            st.get_object_uplinks()
+        )
