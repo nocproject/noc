37f7d2072dc331827a2139860e3ee46c6636a4a0
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2016-08-11 08:27:06 +0300

    Add get_tech_support script
    --HG--
    branch : feature/microservices

---
+++ sa/profiles/Juniper/JUNOS/get_tech_support.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Juniper.JUNOS.get_tech_support
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+import re
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igettechsupport import IGetTechSupport
+
+
+class Script(BaseScript):
+    name = "Juniper.JUNOS.get_tech_support"
+    interface = IGetTechSupport
+
+    def execute(self):
+        c = self.cli("request support information")
+        return unicode(c, "utf8", "ignore").encode("utf8")
