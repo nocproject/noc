577909f3e0e87d4a0e7551bfbfd5312b21a60d7c
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2016-08-16 17:43:14 +0300

    Add initial support for 3Com.SuperStack3 profile
    --HG--
    branch : feature/microservices

---
+++ sa/profiles/3Com/SuperStack3/get_arp.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## 3Com.SuperStack3.get_arp
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetarp import IGetARP
+import re
+
+
+class Script(BaseScript):
+    name = "3Com.SuperStack3.get_arp"
+    interface = IGetARP
+
+    rx_line = re.compile(
+        r"^(?P<interface>\S+)\s+(?P<ip>[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\s+"
+        r"(?P<mac>\S+)\s+\S+\s*$", re.MULTILINE)
+
+    def execute(self, interface=None):
+        r = []
+        cmd = "protocol ip arp"
+        if interface is not None:
+            cmd += " find %s" % interface
+        else:
+            cmd += " detail all"
+        for match in self.rx_line.finditer(self.cli(cmd)):
+            mac = match.group("mac")
+            interface = match.group("interface")
+            if mac.lower() == "ff-ff-ff-ff-ff-ff":
+                continue
+            if not interface.startswith("1:"):
+                interface = "1:" + interface
+            r += [{
+                "interface": interface,
+                "ip": match.group("ip"),
+                "mac": mac
+            }]
+        return r
