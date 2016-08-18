74aa524aaa04f25fe8ac7a0956c249e9bda1b829
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2016-08-18 13:02:21 +0300

    Update Alcatel.TIMOS profile
    --HG--
    branch : feature/microservices

--- sa/profiles/Alcatel/TIMOS/__init__.py
+++ sa/profiles/Alcatel/TIMOS/__init__.py
@@ -16,11 +16,12 @@ class Profile(BaseProfile):
     name = "Alcatel.TIMOS"
     pattern_username = "[Ll]ogin: "
     pattern_password = "[Pp]assword: "
+    pattern_prompt = r"^\S+?#"
+    pattern_more = r"^Press any key to continue.*$"
+    pattern_syntax_error = r"Error: Bad command\.|Error: Invalid parameter\."
     command_disable_pager = "environment no more"
     command_exit="logout"
-    pattern_prompt = r"^\S+?#"
     config_volatile = [r"^# Finished.*$", r"^# Generated.*$"]
-    pattern_more = r"^Press any key to continue.*$"
     command_more = " "
     rogue_chars = [
         re.compile(r"\r\s+\r"),
