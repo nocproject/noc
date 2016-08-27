c48cebfef4cbaace5cbd06e6670123908e4900b2
Aleksey Shirokih <shirokih@nocproject.org>
2016-08-27 23:09:44 +0300

    dev path init
    --HG--
    branch : feature/docker

---
+++ bundle/run.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+currrent_dir=$(pwd)
+
+compile_login () {
+    if [ ! -f ../ui/login/index.html ]; then
+        cd ../ui/src/login
+        echo "Compiling login ..."
+        docker-compose up
+        cd ${currrent_dir}
+    fi
+}
+start_dbs() {
+    docker-compose up -d mongo \
+                         postgres \
+                         nsqd \
+                         nsqlookupd \
+                         clickhouse
+    sleep 2
+}
+
+start_sae() {
+    docker-compose up -d sae \
+                         discovery \
+                         activator.default \
+                         scheduler \
+                         omap \
+                         ping \
+                         syslogcollector \
+                         trapcollector
+}
+
+start_web() {
+    docker-compose up -d web card login grafana front
+}
+
+start_pm() {
+    docker-compose up -d influxdb pmwriter
+}
+
+start_fm() {
+    docker-compose up -d classifier \
+                         correlator \
+                         mailsender
+}
+compile_login
+start_dbs
+start_sae
+start_web
+start_pm
+start_fm
