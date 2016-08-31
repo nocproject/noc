ac68a1889782fbbe6dd7330bd501329367309668
Dmitry Volodin <dv@nocproject.org>
2016-08-31 14:37:55 +0300

    configurable topology RCA window
    --HG--
    branch : feature/microservices

--- fm/handlers/alarm/managed_object.py
+++ fm/handlers/alarm/managed_object.py
@@ -10,13 +10,21 @@
 from noc.inv.models.objectuplink import ObjectUplink
 from noc.fm.models.activealarm import ActiveAlarm
 from noc.core.perf import metrics
+from noc.core.config.base import config
+from noc.lib.dateutils import total_seconds
 
 
 def topology_rca(alarm, seen=None):
+    def can_correlate(a1, a2):
+        return (
+            not config.topology_rca_window or
+            total_seconds(a1.timestamp - a2.timestamp) <= config.topology_rca_window
+        )
+
     seen = seen or set()
+    seen.add(alarm.id)
     if alarm.root or alarm.id in seen:
         return  # Already correlated
-    seen.add(alarm.id)
     o_id = alarm.managed_object.id
     # Get neighbor objects
     neighbors = set()
@@ -39,9 +47,14 @@ def topology_rca(alarm, seen=None):
     # Correlate with uplinks
     if uplinks and len([na[o] for o in uplinks if o in na]) == len(uplinks):
         # All uplinks are faulty
-        # Correlate with the first one (shortest path)
-        alarm.set_root(na[uplinks[0]])
-        metrics["alarm_correlated_topology"] += 1
+        # uplinks are ordered according to path length
+        # Correlate with first applicable
+        for u in uplinks:
+            a = na[u]
+            if can_correlate(alarm, a):
+                alarm.set_root(na[uplinks[0]])
+                metrics["alarm_correlated_topology"] += 1
+                break
     # Correlate neighbors' alarms
     for d in na:
         topology_rca(na[d], seen)
