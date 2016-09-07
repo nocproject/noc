678e344122ae6184b6cccb32dd511a0cd0f3d813
Aleksey Shirokih <shirokih@nocproject.org>
2016-09-07 08:57:50 +0300

    fix
    --HG--
    branch : feature/docker

--- bundle/scripts/migrate.sh
+++ bundle/scripts/migrate.sh
@@ -6,7 +6,7 @@ echo "Launching migrate --ignore-ghost-migrations"
 ./noc migrate --ignore-ghost-migrations
 echo "Launching sync-perm"
 ./noc sync-perm
-Echo "Launching collection sync"
+echo "Launching collection sync"
 ./noc collection sync
 echo "Launching sync-mibs"
 ./noc sync-mibs
