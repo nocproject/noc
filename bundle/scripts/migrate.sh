7ee3bdf3114ec0ff7c4806d3d3705dbbb8ae7965
Aleksey Shirokih <shirokih@nocproject.org>
2016-09-07 08:15:08 +0300

    launch sync perms and collection sync
    --HG--
    branch : feature/docker

--- bundle/scripts/migrate.sh
+++ bundle/scripts/migrate.sh
@@ -4,7 +4,9 @@ echo "Launching syncdb --noinput"
 ./noc syncdb --noinput
 echo "Launching migrate --ignore-ghost-migrations"
 ./noc migrate --ignore-ghost-migrations
-echo "Launching collection sync"
-./noc collection sync
 echo "Launching sync-perm"
+./noc sync-perm
+Echo "Launching collection sync"
+./noc collection sync
+echo "Launching sync-mibs"
 ./noc sync-mibs
