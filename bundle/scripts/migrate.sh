df83bb1584cee72ef96e17f804a79745650b8d8d
Aleksey Shirokih <shirokih@nocproject.org>
2016-09-07 07:22:06 +0300

    import collections during migration
    --HG--
    branch : feature/docker

--- bundle/scripts/migrate.sh
+++ bundle/scripts/migrate.sh
@@ -4,7 +4,7 @@ echo "Launching syncdb --noinput"
 ./noc syncdb --noinput
 echo "Launching migrate --ignore-ghost-migrations"
 ./noc migrate --ignore-ghost-migrations
-echo "Launching sync-perm"
-./noc sync-perm
+echo "Launching collection sync"
+./noc collection sync
 echo "Launching sync-perm"
 ./noc sync-mibs
