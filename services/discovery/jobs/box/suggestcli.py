df38e7741238a9dd6da6bd6289c7cca4db512364
Andrey Vertiprakhov <aversanta@gmail.com>
2016-09-10 08:24:09 +0500

    Add logging problem on suggest
    --HG--
    branch : feature/microservices

--- services/discovery/jobs/box/suggestcli.py
+++ services/discovery/jobs/box/suggestcli.py
@@ -39,6 +39,7 @@ class SuggestCLICheck(DiscoveryCheck):
                 )
                 return
         self.logger.info("Failed to guess CLI credentials")
+        self.set_problem("Failed to guess CLI credentials")
 
     def check_login(self, user, password, super_password):
         self.logger.info("Checking %s/%s/%s", user, password, super_password)
