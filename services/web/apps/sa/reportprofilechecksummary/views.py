b012170c9b64b191c6581908c90de08a11f5465d
Andrey Vertiprakhov <aversanta@gmail.com>
2016-09-15 16:02:19 +0500

    Add to Profile Discovery Summary Report Bad SNMP and CLI count fields
    --HG--
    branch : feature/microservices

--- services/web/apps/sa/reportprofilechecksummary/views.py
+++ services/web/apps/sa/reportprofilechecksummary/views.py
@@ -9,20 +9,27 @@
 """
 
 from noc.lib.app.simplereport import SimpleReport, SectionRow
+from noc.lib.nosql import get_db
 from noc.main.models.pool import Pool
-from noc.core.translation import ugettext as _
 from noc.sa.models.managedobject import ManagedObject
+from noc.core.translation import ugettext as _
 
 
 class ReportFilterApplication(SimpleReport):
-    title = _("ManagedObject Profile Check Summary")
+    title = _("Managed Object Profile Check Summary")
 
     def get_data(self, **kwargs):
         data = []
-        # pools = Pool.objects.filter()
         for p in Pool.objects.order_by("name"):
             data += [SectionRow(name=p.name)]
+
             is_managed = ManagedObject.objects.filter(is_managed=True, pool=p, profile_name="Generic.Host")
+            is_managed_in = ["discovery-noc.services.discovery.jobs.box.job.BoxDiscoveryJob-" +
+                             str(m.id) for m in is_managed]
+            bad_snmp_cred = get_db()["noc.joblog"].find({"problems.suggest_snmp": "Failed to guess SNMP community",
+                                                         "_id": {"$in": is_managed_in}}).count()
+            bad_cli_cred = get_db()["noc.joblog"].find({"problems.suggest_cli": "Failed to guess CLI credentials",
+                                                        "_id": {"$in": is_managed_in}}).count()
             calc = {
                 _("Not Managed"): ManagedObject.objects.filter(is_managed=False, pool=p).count(),
                 _("Is Managed"): ManagedObject.objects.filter(is_managed=True, pool=p).count(),
@@ -31,6 +38,8 @@ class ReportFilterApplication(SimpleReport):
                 _("Generic.Host is Managed"): is_managed.count(),
                 _("Generic.Host is Managed ping"): len([s for s in is_managed if s.get_status()]),
                 _("Generic.Host is Managed not ping"): len([s for s in is_managed if not s.get_status()]),
+                _("Generic.Host is Managed bad SNMP Credential"): bad_snmp_cred,
+                _("Generic.Host is Managed bad CLI Credential"): bad_cli_cred,
             }
 
             for c in calc:
