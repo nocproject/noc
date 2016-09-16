4fb62c9a91413c254f4d4cfa0715e05e79295505
Dmitry Volodin <dv@nocproject.org>
2016-09-16 10:30:42 +0300

    ETL: Always sort extracted data in alphabetical order
    --HG--
    branch : feature/microservices

---
+++ fixes/fix_etl_order.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Fix ETL extraction sorting order
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Python modules
+import os
+import shutil
+import glob
+import gzip
+import csv
+
+PATTERNS = [
+    "var/import/*/*/import.csv.gz",
+    "var/import/*/*/archive/*.csv.gz",
+]
+
+
+def fix():
+    for pattern in PATTERNS:
+        for path in glob.glob(pattern):
+            print("Fixing order in %s:" % path)
+            tmp_path = path + "~"
+            with gzip.open(path, "r") as f_in:
+                reader = csv.reader(f_in)
+                data = sorted(row for row in reader)
+            with gzip.open(tmp_path, "w") as f_out:
+                writer = csv.writer(f_out)
+                writer.writerows(data)
+            os.unlink(path)
+            shutil.move(tmp_path, path)
+            print("    ... done")
