5dea39730db5f775894e69299a7fab5f23353205
Dmitry Zakharov <d.a.zakharov@gmail.com>
2016-09-20 11:54:31 +0300

    add tree element
    --HG--
    branch : feature/microservices/tree

---
+++ ui/web/core/modelfilter/Tree.js
@@ -0,0 +1,54 @@
+//---------------------------------------------------------------------
+// NOC.core.modelfilter.Combo
+// Combo lookup model filter
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2016 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.core.modelfilter.Tree");
+
+Ext.define("NOC.core.modelfilter.Tree", {
+    extend: "NOC.core.modelfilter.Base",
+    lookup: null,  // module.app
+    referrer: null, // Referrer application id
+
+    requires: [
+        "NOC.core.TreeField"
+    ],
+
+    initComponent: function() {
+        var me = this,
+            wn = "NOC." + me.lookup + ".TreeField",
+            tree = Ext.create(wn, {
+                labelAlign: "top",
+                fieldLabel: me.title,
+                width: 180,
+                restUrl: '/' + me.lookup.replace('.', '/'),
+                listeners: {
+                    scope: me,
+                    clear: me.onChange,
+                    change: me.onChange
+                }
+            });
+        Ext.apply(me, {
+            items: [tree]
+        });
+        me.callParent();
+        me.tree = tree;
+    },
+
+    getFilter: function() {
+        var me = this,
+            r = {},
+            v;
+
+        if(me.tree.fieldValue) {
+            v = me.tree.getFieldValue().id;
+            if(v) {
+                r[me.name] = v;
+            }
+            return r;
+        }
+        return r;
+    }
+});
