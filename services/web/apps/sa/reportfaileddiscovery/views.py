d246adfc347cd3329d0a57e2b33fec2abed2c055
Andrey Vertiprakhov <aversanta@gmail.com>
2016-09-24 09:18:12 +0500

    Add options ObjectProfile in Failed Discovery Report and Fix some report
    --HG--
    branch : feature/microservices

--- services/web/apps/sa/reportfaileddiscovery/views.py
+++ services/web/apps/sa/reportfaileddiscovery/views.py
@@ -14,38 +14,34 @@ from noc.lib.app.simplereport import SimpleReport, TableColumn
 from noc.lib.nosql import get_db
 from noc.main.models.pool import Pool
 from noc.sa.models.managedobject import ManagedObject
+from noc.sa.models.managedobjectprofile import ManagedObjectProfile
 from noc.core.translation import ugettext as _
+
+
+class ReportForm(forms.Form):
+    pool = forms.ModelChoiceField(
+        label=_("Managed Objects Pools"),
+        required=True,
+        queryset=Pool.objects.order_by("name"))
+    obj_profile = forms.ModelChoiceField(
+        label=_("Managed Objects Profile"),
+        required=False,
+        queryset=ManagedObjectProfile.objects.order_by("name"))
 
 
 class ReportFilterApplication(SimpleReport):
     title = _("Failed Discovery")
+    form = ReportForm
 
-    def get_form(self):
-        class RForm(forms.Form):
-            pool = forms.ModelChoiceField(
-                label=_("Managed Objects Pools"),
-                required=True,
-                queryset=Pool.objects.order_by("name"))
-
-        self.customize_form(RForm, "mo_pool", search=True)
-        return RForm
-
-    def get_data(self, **kwargs):
-        q = {}
-        for k in kwargs:
-            v = kwargs[k]
-            if v:
-                if k == "description":
-                    q[k + "__icontains"] = v
-                else:
-                    q[k] = v
-
+    def get_data(self, pool, obj_profile, **kwargs):
         data = []
         job_logs = get_db()["noc.joblog"].find({"$and": [{"problems": {"$ne": {  }}},
                                                          {"problems": {"$exists": "true"}}]})
 
         for discovery in job_logs:
-            mo = ManagedObject.objects.filter(id=int(discovery["_id"].split("-")[2]), pool=q["pool"])
+            mo = ManagedObject.objects.filter(id=int(discovery["_id"].split("-")[2]), pool=pool)
+            if obj_profile:
+                mo = mo.filter(object_profile=obj_profile)
             if not mo:
                 continue
             for method in discovery["problems"]:
