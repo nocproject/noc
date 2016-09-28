4d3f9ed7fd18b723bf807bb6873a9233950a98f5
Dmitry Volodin <dv@nocproject.org>
2016-09-28 23:10:01 +0300

    noc fix: fix_selector_cache
    --HG--
    branch : feature/microservices

---
+++ fixes/fix_selector_cache.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Rebuild SelectorCache
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+from noc.sa.models.selectorcache import refresh
+
+
+def fix():
+    refresh()
