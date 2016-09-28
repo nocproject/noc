18dfc85895e42034e59e53b9767be5021e66a77f
Dmitry Volodin <dv@nocproject.org>
2016-09-28 23:12:07 +0300

    Code cleanup
    --HG--
    branch : feature/microservices

--- sa/profiles/MikroTik/RouterOS/get_capabilities.py
+++ sa/profiles/MikroTik/RouterOS/get_capabilities.py
@@ -7,8 +7,7 @@
 ##----------------------------------------------------------------------
 
 ## NOC modules
-from noc.sa.profiles.Generic.get_capabilities import Script as BaseScript
-from noc.sa.profiles.Generic.get_capabilities import false_on_cli_error
+from noc.sa.profiles.Generic.get_capabilities import Script as BaseScript, false_on_cli_error
 
 
 class Script(BaseScript):
