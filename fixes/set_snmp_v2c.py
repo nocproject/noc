fcc18c073ff161a11b8e1d2a3d1484ae1c519da4
Dmitry Volodin <dv@nocproject.org>
2016-09-29 00:32:47 +0300

    Fix SNMP v1 selection
    --HG--
    branch : feature/microservices

---
+++ fixes/set_snmp_v2c.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Set SNMP | v2c capability if SNMP cap is set
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+from noc.sa.models.managedobject import ManagedObject
+
+
+def fix():
+    for mo in ManagedObject.objects.all():
+        caps = mo.get_caps()
+        if caps.get("SNMP") and not caps.get("SNMP | v1") and not caps.get("SNMP | v2c"):
+            mo.update_caps({
+                "SNMP | v2c": True
+            })
