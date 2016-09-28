288f469a22faf882c986a5b947448cc88b300c85
Dmitry Volodin <dv@nocproject.org>
2016-09-28 23:11:19 +0300

    Prohibit BaseProfile loading from noc.sa.profiles
    --HG--
    branch : feature/microservices

--- sa/profiles/__init__.py
+++ sa/profiles/__init__.py
@@ -5,6 +5,3 @@
 ## Copyright (C) 2007-2015 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-
-# Backward compatibility
-from noc.core.profile.base import BaseProfile as Profile
