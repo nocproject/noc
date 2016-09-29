9970f800f2052ffdd9276583d93d9121359ecfc5
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2016-09-29 14:13:38 +0300

    Add Angtel.Topaz profile
    --HG--
    branch : feature/microservices

---
+++ sa/profiles/Angtel/Topaz/ping.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Angtel.Topaz.ping
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.iping import IPing
+from noc.lib.validators import is_ipv4, is_ipv6
+import re
+
+
+class Script(BaseScript):
+    name = "Angtel.Topaz.ping"
+    interface = IPing
+    rx_result1 = re.compile(
+        r"(?P<count>\d+) packets transmitted, (?P<success>\d+) packets received, \d+% packet loss\n"
+        r"round-trip \(ms\) min/avg/max = (?P<min>\d+)/(?P<avg>\d+)/(?P<max>\d+)", re.MULTILINE | re.DOTALL)
+    rx_result2 = re.compile(
+        r"(?P<count>\d+) packets transmitted, (?P<success>\d+) packets received, \d+% packet loss")
+
+    def execute(self, address, count=None, source_address=None, size=None,
+    df=None, vrf=None):
+        if is_ipv4(address):
+            cmd = "ping ip %s" % address
+        elif is_ipv6(address):
+            cmd = "ping ipv6 %s" % address
+        if count:
+            cmd += " count %d" % int(count)
+        if size:
+            cmd += " size %d" % int(size)
+        s = self.cli(cmd)
+        match = self.rx_result1.search(s)
+        if match:
+            return {
+                "success": match.group("success"),
+                "count": match.group("count"),
+                "min": match.group("min"),
+                "avg": match.group("avg"),
+                "max": match.group("max")
+            }
+        else:
+            match = self.rx_result2.search(s)
+            return {
+                "success": match.group("success"),
+                "count": match.group("count")
+            }
