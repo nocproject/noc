a1cf6a6aa582dbcd4334d7243d8e3805b2f918d5
Dmitry Volodin <dv@nocproject.org>
2016-10-02 21:24:03 +0300

    Reworked setup.py
    --HG--
    branch : feature/docker

--- setup.py
+++ setup.py
@@ -1,127 +1,80 @@
 #!/usr/bin/env python
 ##----------------------------------------------------------------------
-## Distutils setup.py
+## NOC setup.py
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2011 The NOC Project
+## Copyright (C) 2007-2016 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 
-from __future__ import with_statement
+import glob
 from distutils.core import setup
-import distutils.command.sdist
-from distutils.command.install import INSTALL_SCHEMES
-import subprocess
-import os
+from distutils.command.sdist import sdist
+from distutils.extension import Extension
 
-##
-## Prefix to where noc to be installed
-##
-PREFIX = "/opt/noc"
+VERSION_PATH = "VERSION"
+DESCRIPTION_PATH = "README"
+REQUIREMENTS_PATH = "requirements.txt"
+SPEEDUPS = "speedup/*.pyx"
 
 
 def get_version():
     """
     Read actual NOC version from VERSION file
-    :return:
+    :return: Version as a string
     """
-    with open("VERSION") as f:
+    with open(VERSION_PATH) as f:
         return f.read().strip()
 
-MANIFEST = []
 
-
-def get_manifest():
+def get_long_description():
     """
-    Build distribution manifest
+    Read long NOC description
     :return:
     """
-    global MANIFEST
-    if not MANIFEST:
-        if os.path.exists(".hg"):
-            # Rebuild MANIFEST file every time mercurial repo found
-            proc = subprocess.Popen(["hg", "locate"], stdout=subprocess.PIPE,
-                                                    stderr=subprocess.PIPE)
-            stdout, stderr = proc.communicate()
-            mf = stdout.splitlines()
-            if os.path.exists(".hgsub"):
-                with open(".hgsub") as sf:
-                    for l in sf:
-                        if "=" not in l:
-                            continue
-                        sr, r = l.split("=", 1)
-                        sr = sr.strip()
-                        proc = subprocess.Popen(["hg", "-R", sr, "locate"],
-                                                stdout=subprocess.PIPE,
-                                                stderr=subprocess.PIPE)
-                        stdout, stderr = proc.communicate()
-                        mf += [sr + "/" + x for x in stdout.splitlines()]
-            mf += ["MANIFEST"]
-            with open("MANIFEST", "w") as f:
-                f.write("\n".join(mf))
-        with open("MANIFEST") as f:
-            MANIFEST = [n for n in f.read().splitlines() if
-                        not n.startswith(".hg")]
-    return MANIFEST
+    with open(DESCRIPTION_PATH) as f:
+        return f.read()
 
 
-def get_packages():
+def get_requirements():
     """
-    Return list of packages
+    Build requirements from requirements.txt
     :return:
     """
-    return [""] + [f[:-12].replace(os.sep, ".") for f in get_manifest() if
-                   f.endswith("__init__.py") and f != "__init__.py"]
+    r = []
+    with open(REQUIREMENTS_PATH) as f:
+        for l in r:
+            l = l.strip()
+            if not l:
+                continue
+            pkg, op, version = None, None, None
+            for op in ("==", ">="):
+                if op in l:
+                    pkg, version = l.split(op)
+                    break
+            if not pkg:
+                continue
+            r += ["%s (%s%s)" % (pkg, op, version)]
+    return r
 
 
-def get_data():
-    """
-    Return data files
-    :return:
-    """
-    data = {}
-    for df in get_manifest():
-        d, f = os.path.split(df)
-        if d not in data:
-            data[d] = [df]
-        else:
-            data[d].append(df)
-    return data.items()
+class NOCSdist(sdist):
+    def run(self):
+        from Cython.Build import cythonize
+        cythonize(SPEEDUPS)
+        sdist.run(self)
 
-
-class noc_sdist(distutils.command.sdist.sdist):
-    """
-    Get file list for sdist from MANIFEST
-    """
-    def get_file_list(self):
-        self.filelist.files = get_manifest()
-
-##
-## Monkeypatch distutils to install noc to the desired location
-##
-for scheme in INSTALL_SCHEMES.values():
-    scheme["purelib"] = PREFIX
-    scheme["data"] = PREFIX
-##
-## Pass control to the setuptools
-##
-setup(name="noc",
-      version=get_version(),
-      description="Network Operation Center's OSS",
-      author="Dmitry Volodin",
-      author_email="dvolodin7@google.com",
-      url="http://nocproject.org/",
-      license="BSD",
-      long_description="""NOC Project is an Operation Support System (OSS) for telecom companies,
-service providers, and enterprise Network Operation Centers (NOC).
-Areas covered by NOC include fault management, service activation/provisioning, multi-VRF address space management,
-configuration management, DNS provisioning, peering management, RPSL and BGP filter generation, and reporting.""",
-      cmdclass={"sdist": noc_sdist},
-      packages=get_packages(),
-      data_files=get_data(),
-      provides=["noc"],
-      requires=[
-          "psycopg2 (>= 2.0.5)",
-          "pycrypto (>= 2.3)",
-          "pymongo (>= 1.1)"
-          ]
-      )
+setup(
+    name="noc",
+    version=get_version(),
+    author="nocproject.org",
+    url="http://nocproject.org",
+    author_email="pkg@nocproject.org",
+    license="BSD",
+    description="The Open-Source OSS for telecom, internet and service providers",
+    long_description=get_long_description(),
+    requires=get_requirements(),
+    cmdclass={
+        "sdist": NOCSdist
+    },
+    ext_modules=[Extension("speedup", ["*.c"])]
+)
