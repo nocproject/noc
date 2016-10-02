e19c03bba86ab487ff90158f7cd32442cb0360ef
Dmitry Volodin <dv@nocproject.org>
2016-10-02 11:23:20 +0300

    ./noc etl --system argument
    --HG--
    branch : feature/microservices

--- commands/etl.py
+++ commands/etl.py
@@ -19,6 +19,11 @@ class Command(BaseCommand):
     CONF = "etc/etl.yml"
 
     def add_arguments(self, parser):
+        parser.add_argument(
+            "--system",
+            action="append",
+            help="System to extract"
+        )
         subparsers = parser.add_subparsers(dest="cmd")
         # load command
         load_parser = subparsers.add_parser("load")
@@ -41,13 +46,15 @@ class Command(BaseCommand):
     def handle(self, cmd, *args, **options):
         return getattr(self, "handle_%s" % cmd)(*args, **options)
 
-    def handle_load(self, *args, **options):
+    def handle_load(self, system=None, *args, **options):
         from noc.core.etl.loader.chain import LoaderChain
 
         config = self.get_config()
-        for system in config:
-            chain = LoaderChain(system["system"])
-            for d in system.get("data"):
+        for s in config:
+            if system and s["system"] not in system:
+                continue
+            chain = LoaderChain(s["system"])
+            for d in s.get("data"):
                 chain.get_loader(d["type"])
             # Add & Modify
             for l in chain:
@@ -57,28 +64,32 @@ class Command(BaseCommand):
             for l in reversed(list(chain)):
                 l.purge()
 
-    def handle_extract(self, *args, **options):
+    def handle_extract(self, system=None, *args, **options):
         extractors = set(options.get("extractors", []))
         config = self.get_config()
-        for system in config:
-            system_config = system.get("config", {})
-            for x_config in reversed(system.get("data", [])):
+        for s in config:
+            if system and s["system"] not in system:
+                continue
+            system_config = s.get("config", {})
+            for x_config in reversed(s.get("data", [])):
                 config = system_config.copy()
                 config.update(x_config.get("config", {}))
                 if extractors and x_config["type"] not in extractors:
                     continue
                 xc = get_handler(x_config["extractor"])
-                extractor = xc(system["system"], config=config)
+                extractor = xc(s["system"], config=config)
                 extractor.extract()
 
-    def handle_check(self, *args, **options):
+    def handle_check(self, system=None, *args, **options):
         from noc.core.etl.loader.chain import LoaderChain
         config = self.get_config()
         n_errors = 0
         summary = []
-        for system in config:
-            chain = LoaderChain(system["system"])
-            for d in system.get("data"):
+        for s in config:
+            if system and s["system"] not in system:
+                continue
+            chain = LoaderChain(s["system"])
+            for d in s.get("data"):
                 chain.get_loader(d["type"])
             # Check
             for l in chain:
@@ -87,19 +98,21 @@ class Command(BaseCommand):
                     s = "%d errors" % n
                 else:
                     s = "OK"
-                summary += ["%s.%s: %s" % (system["system"], l.name, s)]
+                summary += ["%s.%s: %s" % (s["system"], l.name, s)]
                 n_errors += n
         if summary:
             self.stdout.write("Summary:\n")
             self.stdout.write("\n".join(summary) + "\n")
         return 1 if n_errors else 0
 
-    def handle_diff(self, *args, **options):
+    def handle_diff(self, system=None, *args, **options):
         from noc.core.etl.loader.chain import LoaderChain
         config = self.get_config()
-        for system in config:
-            chain = LoaderChain(system["system"])
-            for d in system.get("data"):
+        for s in config:
+            if system and s["system"] not in system:
+                continue
+            chain = LoaderChain(s["system"])
+            for d in s.get("data"):
                 chain.get_loader(d["type"])
             # Check
             for l in chain:
