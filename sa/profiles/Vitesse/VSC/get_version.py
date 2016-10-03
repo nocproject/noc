86dd53af5ef74fcf77716fc98ac461b22eaa52d9
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2016-10-03 15:17:03 +0300

    Add initial support for Vitesse.VSC profile
    --HG--
    branch : feature/microservices

---
+++ sa/profiles/Vitesse/VSC/get_version.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Vitesse.VSC.get_version
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Python modules
+import re
+## NOC modules
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetversion import IGetVersion
+
+
+class Script(BaseScript):
+    name = "Vitesse.VSC.get_version"
+    cache = True
+    interface = IGetVersion
+
+    rx_platform = re.compile(
+        r"^\s*Chipset ID\s+: (?P<platform>\S+).*\n", re.MULTILINE)
+    rx_version = re.compile(
+        r"^\s*Software Version\s+: CEServices \(standalone\) (?P<version>.+)\n",
+        re.MULTILINE)
+
+    def execute(self):
+        v = self.cli("show version", cached=True)
+        match1 = self.re_search(self.rx_platform, v)
+        match2 = self.re_search(self.rx_version, v)
+        return {
+            "vendor": " Vitesse",
+            "platform": match1.group("platform"),
+            "version": match2.group("version")
+        }
---
+++ sa/profiles/Vitesse/VSC/get_vlans.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Vitesse.VSC.get_vlans
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+import re
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetvlans import IGetVlans
+
+
+class Script(BaseScript):
+    name = "Vitesse.VSC.get_vlans"
+    interface = IGetVlans
+
+    rx_vlan = re.compile(
+        "^\s*(?P<vlan_id>\d+)\s{2,5}(?P<name>\S*)", re.MULTILINE)
+
+    def execute(self):
+        r = []
+        for match in self.rx_vlan.finditer(self.cli("show vlan")):
+            vlan_id = int(match.group("vlan_id"))
+            if vlan_id == 1:
+                continue
+            name = match.group("name")
+            if name:
+                r += [{"vlan_id": vlan_id, "name": name}]
+            else:
+                r += [{"vlan_id": vlan_id}]
+        return r
