7ba062fdab1c39cba4551a1a130193b06054a719
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2016-10-03 15:17:03 +0300

    Add initial support for Vitesse.VSC profile
    --HG--
    branch : feature/microservices

---
+++ sa/profiles/Vitesse/VSC/get_ipv6_neighbor.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Vitesse.VSC.get_ipv6_neighbor
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Python modules
+import re
+## NOC modules
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetipv6neighbor import IGetIPv6Neighbor
+
+
+class Script(BaseScript):
+    name = "Vitesse.VSC.get_ipv6_neighbor"
+    interface = IGetIPv6Neighbor
+
+    rx_line = re.compile(
+        r"^(?P<ip>[0-9a-fA-F:\.]+) via (?P<interface>\S+):\s+"
+        r"(?P<mac>\S+)\s+(?P<state>\S+)\s*$")
+
+    s_map = {
+        "INCMP": "incomplete",
+        "Permanent/REACHABLE": "reachable",
+        "STALE": "stale",
+        "DELAY": "delay",
+        "PROBE": "probe"
+    }
+
+    def execute(self, vrf=None):
+        # Get states
+        cmd = "show ipv6 neighbor"
+        r =  self.cli(cmd, list_re=self.rx_line)
+        # Remap states
+        for n in r:
+            n["state"] = self.s_map[n["state"]]
+        return r
