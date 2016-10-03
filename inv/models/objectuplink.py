6e49e3a5cc9c34bd4cf488e1815b27a80970c605
Dmitry Volodin <dv@nocproject.org>
2016-10-03 11:47:53 +0300

    ObjectUplink: .uplinks_for_object() and .uplinks_for_objects() helpers
    --HG--
    branch : feature/microservices

--- inv/models/objectuplink.py
+++ inv/models/objectuplink.py
@@ -31,3 +31,28 @@ class ObjectUplink(Document):
                 }
             })
         bulk.execute()
+
+    @classmethod
+    def uplinks_for_object(cls, object):
+        if hasattr(object, "id"):
+            object = object.id
+        for d in ObjectUplink._get_collection().find({"_id": object}):
+            return d["uplinks"]
+        return []
+
+    @classmethod
+    def uplinks_for_objects(cls, objects):
+        """
+        Returns uplinks for list of objects
+        :param objects: List of object
+        :return: dict of object id -> uplinks
+        """
+        o = []
+        for obj in objects:
+            if hasattr(obj, "id"):
+                obj = obj.id
+            o += [obj]
+        uplinks = dict((obj, []) for obj in o)
+        for d in ObjectUplink._get_collection().find({"_id": {"$in": o}}):
+            uplinks[d["_id"]] = d["uplinks"]
+        return uplinks
