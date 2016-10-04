8ee4567f464dd1041deee9f32e8e54f387b2650a
Dmitry Zakharov <d.a.zakharov@gmail.com>
2016-10-04 10:56:28 +0300

    optimization LookupField
    --HG--
    branch : feature/microservices

--- ui/web/core/LookupField.js
+++ ui/web/core/LookupField.js
@@ -68,7 +68,12 @@ Ext.define("NOC.core.LookupField", {
         }
         me.callParent();
         me.on("specialkey", me.onSpecialKey, me, {delay: 100});
-        me.on("beforequery", me.onBeforeQuery, me);
+        me.on('change', function(element, newValue) {
+            if(newValue === null) {
+                me.clearValue();
+                me.fireEvent('clear');
+            }
+        });
     },
 
     getLookupData: function() {
