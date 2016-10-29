cd9832e6cf555bff1253b64143d29cf63f31e762
Dmitry Zakharov <d.a.zakharov@gmail.com>
2016-10-29 12:32:50 +0300

    login on Ext JS
    --HG--
    branch : feature/microservices

---
+++ ui/login/app/LoginView.js
@@ -0,0 +1,143 @@
+//---------------------------------------------------------------------
+// NOC.login application
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2016 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.LoginView");
+
+Ext.define('NOC.LoginView', {
+    extend: 'Ext.window.Window'
+    , xtype: 'login'
+    , autoShow: true
+    , closable: false
+    , resizable: false
+    , title: __("NOC Login")
+    , layout: 'fit'
+    , modal: true
+    , defaultListenerScope: true
+    , defaultFocus: 'user'
+    , referenceHolder: true
+    , requires: [
+        'Ext.form.Panel',
+        'Ext.window.Toast'
+    ]
+    , viewModel: 'default'
+    , items: {
+        xtype: 'form'
+        , padding: 15
+        , reference: 'loginForm'
+        , fixed: true
+        , autoEl: {
+            tag: 'form'
+        }
+        , defaults: {
+            anchor: '100%'
+            , allowBlank: false
+            , enableKeyEvents: true
+            , xtype: 'textfield'
+        }
+        , items: [{
+            xtype: 'displayfield'
+            , value: __('Type username and password:')
+            , hideLabel: true
+        }, {
+            xtype: 'textfield'
+            , itemId: 'user'
+            , name: 'user'
+            , bind: '{user}'
+            , fieldLabel: __('User')
+            , blankText: __('User name cannot be empty')
+            // , listeners: {
+            //     afterrender: 'afterRender'
+            // }
+        }, {
+            xtype: 'textfield'
+            , itemId: 'password'
+            , name: 'password'
+            , bind: '{password}'
+            , fieldLabel: __('Password')
+            , blankText: __('Password cannot be empty')
+            , inputType: 'password'
+            // , listeners: {
+            //     afterrender: 'afterRender'
+            // }
+        }, {
+            xtype: 'button'
+            , margin: '15 0 0 200'
+            , width: 50
+            , formBind: true
+            , handler: 'onLoginClick'
+            , text: __('Login')
+            , autoEl: {
+                // tag: 'input',
+                // type: 'submit'
+            }
+            , listeners: {
+                beforerender: function() {
+                    if(!Ext.isIE) {
+                        Ext.apply(this.autoEl, {type: 'submit'})
+                    }
+                }
+            }
+        }]
+    }
+    , onLoginClick: function() {
+        var me = this,
+            params = Ext.encode({
+                jsonrpc: '2.0',
+                method: 'login',
+                params: [me.getViewModel().getData()]
+            });
+        if(params !== undefined) {
+            Ext.Ajax.request({
+                url: '/api/login/'
+                , params: params
+                , method: 'POST'
+                , success: me.onLoginSuccess
+                , failure: me.onLoginFailure
+            });
+        }
+    }
+    , onLoginFailure: function() {
+        Ext.toast({
+            html: __('Failed to log in'),
+            align: 't',
+            spacing: 0,
+            paddingY: 0,
+            width: '100%'
+        });
+    }
+    , onLoginSuccess: function(response) {
+        var me = this;
+        try {
+            var o = Ext.decode(response.responseText);
+            if(true !== o.result) {
+                me.onLoginFailure();
+            } else {
+                var param = Ext.urlDecode(location.search);
+                if('uri' in param) {
+                    document.location = param.uri;
+                } else {
+                    document.location = '/';
+                }
+            }
+        } catch(e) {
+            Ext.toast({
+                html: __('Failed to log in'),
+                align: 't',
+                spacing: 0,
+                paddingY: 0,
+                width: '100%'
+            });
+        }
+    }
+
+// , afterRender: function(cmp) {
+//     if(cmp.hasOwnProperty('inputEl')) {
+//         cmp.inputEl.set({
+//             autocomplete: 'on'
+//         });
+//     }
+// }
+});
