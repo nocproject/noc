cb6e87152863c777dfd9a10c21e9b28c770ab6cf
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2016-11-03 14:11:18 +0200

    Add initial support for Nateks.FlexGain profile
    --HG--
    branch : feature/microservices

---
+++ sa/profiles/Nateks/FlexGain/get_version.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Nateks.FlexGain.get_version
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetversion import IGetVersion
+import re
+
+
+class Script(BaseScript):
+    name = "Nateks.FlexGain.get_version"
+    cache = True
+    interface = IGetVersion
+
+    rx_ver = re.compile(
+        r"Nateks\s*\n"
+        r"System Description:(?P<platform>.+)\n"
+        r"Hardware Version:(?P<hardware>\S+)\s*\n"
+        r"Firmware Version:(?P<bootprom>\S+)\s*\n"
+        r"Software Version:(?P<software>\S+)", re.MULTILINE)
+
+    def execute(self):
+        match = self.rx_ver.search(self.cli("show version"))
+        return {
+            "vendor": "Nateks",
+            "platform": match.group("platform").strip(),
+            "version": match.group("software"),
+            "attributes": {
+                "Boot PROM": match.group("bootprom"),
+                "HW version": match.group("hardware"),
+            }
+        }
