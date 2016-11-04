6447d4f06cf7c6d1a4beb2b27598126ffb4526d9
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2016-11-04 16:34:03 +0200

    Update Nateks.FlexGain profile
    --HG--
    branch : feature/microservices

---
+++ sa/profiles/Nateks/FlexGain/ping.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Nateks.FlexGain.ping
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.iping import IPing
+import re
+
+
+class Script(BaseScript):
+    name = "Nateks.FlexGain.ping"
+    interface = IPing
+    rx_result = re.compile(
+        r"^\s*(?P<count>\d+) packets transmitted, (?P<success>\d+) packets "
+        r"received, \d+\.\d+% packet loss\nround-trip min/avg/max/stddev = "
+        r"(?P<min>\d+\.\d+)/(?P<avg>\d+\.\d+)/(?P<max>\d+\.\d+)/\d+\.\d+ ms",
+        re.MULTILINE | re.DOTALL | re.IGNORECASE)
+    rx_result1 = re.compile(
+        r"^\s*(?P<count>\d+) packets transmitted, (?P<success>\d+) packets "
+        r"received, \d+\.\d+% packet loss\n",
+        re.MULTILINE | re.DOTALL | re.IGNORECASE)
+
+    def execute(self, address, count=None, source_address=None, size=None,
+    df=None):
+        cmd = "ping %s" % address
+        if count:
+            cmd += " count %d" % int(count)
+        if size:
+            cmd += " size %d" % int(size)
+        s = self.cli(cmd)
+        match = self.rx_result.search(s)
+        if match:
+            return {
+                "success": match.group("success"),
+                "count": match.group("count"),
+                "min": match.group("min"),
+                "avg": match.group("avg"),
+                "max": match.group("max")
+            }
+        else:
+            match = self.rx_result1.search(s)
+            return {
+                "success": match.group("success"),
+                "count": match.group("count")
+            }
