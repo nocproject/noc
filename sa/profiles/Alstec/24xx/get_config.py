bb2b9f2ae5b280300ed20d76c9d57e14f1664cca
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2016-11-08 16:55:27 +0200

    Add Alstec.24xx profile
    --HG--
    branch : feature/microservices

---
+++ sa/profiles/Alstec/24xx/get_config.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Alstec.24xx.get_config
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetconfig import IGetConfig
+
+
+class Script(BaseScript):
+    name = "Alstec.24xx.get_config"
+    interface = IGetConfig
+    cache = True
+
+    def execute(self):
+        config = self.cli("show running-config", cached=True)
+        return self.cleaned_config(config)
