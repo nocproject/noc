160f339805277a4fce9e4529333154edcca23dae
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2016-11-09 10:09:28 +0200

    Copy ALS.7200 to Alsitec.7200 profile
    --HG--
    branch : feature/microservices

---
+++ sa/profiles/Alstec/7200/get_arp.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Alstec.7200.get_arp
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetarp import IGetARP
+import re
+
+
+class Script(BaseScript):
+    name = "Alstec.7200.get_arp"
+    interface = IGetARP
+
+    rx_line = re.compile(
+        r"^(?P<mac>\S+)\s+(?P<ip>[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\s+"
+        r"(?P<interface>\S+)\s*", re.MULTILINE)
+
+    def execute(self, interface=None):
+        r = []
+        for match in self.rx_line.finditer(self.cli("show arp switch")):
+            if (interface is not None) \
+            and (interface != match.group("interface")):
+                continue
+            r.append(match.groupdict())
+        cards = self.profile.fill_cards(self)
+        for c in cards:
+            if c["s"]:
+                self.cli("join 0/%s" % c["n"])
+                for match in self.rx_line.finditer(self.cli("show arp switch")):
+                    mac = match.group("mac")
+                    ip = match.group("ip")
+                    found = False
+                    for i in r:
+                        if (i["ip"] == ip) and (i["mac"] == mac):
+                            found = True
+                            break
+                    if not found:
+                        r += [{
+                            "mac": mac,
+                            "ip": ip,
+                            "interface": "1/%s" % match.group("interface")
+                        }]
+                self.cli("quit")
+        return r
---
+++ sa/profiles/Alstec/7200/get_version.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Alstec.7200.get_version
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+## Python modules
+import re
+## NOC modules
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetversion import IGetVersion
+
+
+class Script(BaseScript):
+    name = "Alstec.7200.get_version"
+    interface = IGetVersion
+    cache = True
+
+    rx_ver = re.compile(
+        r"^Machine Type\.+ ALS24300 System - 16 GE, 4\s*\n"
+        r"^\s+GE/Stack\s*\n"
+        r"^Burned In MAC Address\.+ (?P<mac>\S+)\s*\n"
+        r"^Software Version\.+ (?P<version>\S+)",
+        re.MULTILINE | re.DOTALL)
+
+    def execute(self):
+        v = self.cli("show version")
+        match = self.rx_ver.search(v)
+        return {
+            "vendor": "Alsitec",
+            "platform": "7200",
+            "version": match.group("version")
+        }
