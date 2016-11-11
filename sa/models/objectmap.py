3afe84b05159991a4b7bf76c965c4e56a25f60b0
teroni <wad804@gmail.com>
2016-11-11 06:01:31 +0000

    пустой список для ping при пустой базе
    --HG--
    branch : teroni/-ping--1478844040376

--- sa/models/objectmap.py
+++ sa/models/objectmap.py
@@ -152,6 +152,8 @@ class ObjectMap(Document):
         Returns a dict of IP -> {"id": ..., "status": ..., "interval": ...}
         """
         om = cls.get_object_mappings(pool)
+        if not om:
+            return {}
         return dict((k.replace("_", "."), om["ping_sources"][k])
                     for k in om["ping_sources"])
 
