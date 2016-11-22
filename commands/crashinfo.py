084aa7765cd4f35ea335c2fd406ec5301a3fbdb3
Dmitry Volodin <dv@nocproject.org>
2016-11-22 12:13:32 +0300

    ./noc crashinfo clear all
    --HG--
    branch : feature/microservices

--- commands/crashinfo.py
+++ commands/crashinfo.py
@@ -98,6 +98,12 @@ class Command(BaseCommand):
             self.stdout.write("\n\n")
 
     def handle_clear(self, clear_uuids, *args, **options):
+        if clear_uuids == "all":
+            clear_uuids = [
+                fn[:-5]
+                for fn in os.listdir(self.PREFIX)
+                if fn.endswith(".json")
+            ]
         for u in clear_uuids:
             path = os.path.join(self.PREFIX, "%s.json" % u)
             if not os.path.exists(path):
