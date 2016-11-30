09db6e672779ff00d0c40e16d002d20a732a329d
Andrey Vertiprakhov <aversanta@gmail.com>
2016-12-01 00:07:08 +0500

    Fix some profiles
    --HG--
    branch : feature/microservices

--- sa/profiles/Angtel/Topaz/get_lldp_neighbors.py
+++ sa/profiles/Angtel/Topaz/get_lldp_neighbors.py
@@ -10,6 +10,7 @@
 import re
 ## NOC modules
 from noc.core.script.base import BaseScript
+from noc.lib.text import parse_table
 from noc.sa.interfaces.igetlldpneighbors import IGetLLDPNeighbors
 from noc.sa.interfaces.base import MACAddressParameter
 from noc.lib.validators import is_int, is_ipv4, is_ipv6
@@ -25,50 +26,47 @@ class Script(BaseScript):
         r"(?P<port_id>\S+)\s+(?P<system_name>.*)\s+(?P<caps>\S+)\s+\d+",
         re.IGNORECASE)
 
+    CAPS = {
+        "": 0, "O": 1, "r": 2,
+        "B": 4, "W": 8, "R": 16,
+        "T": 32, "D": 64, "H": 128
+    }
+
     def execute(self):
         r = []
+        data = []
         try:
             v = self.cli("show lldp neighbors")
         except self.CLISyntaxError:
             raise self.NotSupportedError()
-        for l in v.split("\n"):
-            match = self.rx_line.search(l)
-            if match:
-                chassis_id = match.group("system_id")
+        v = v.replace("\n\n", "\n")
+        for l in parse_table(v):
+            if not l[0]:
+                data[-1] = [s[0]+s[1] for s in zip(data[-1], l)]
+                continue
+            data += [l]
+
+        for d in data:
+                chassis_id = d[1]
                 if is_ipv4(chassis_id) or is_ipv6(chassis_id):
-                    chassis_id_subtype = 5
+                        chassis_id_subtype = 5
                 else:
-                    try:
-                        MACAddressParameter().clean(chassis_id)
-                        chassis_id_subtype = 4
-                    except ValueError:
-                        chassis_id_subtype = 7
-                port_id = match.group("port_id")
+                        try:
+                            MACAddressParameter().clean(chassis_id)
+                            chassis_id_subtype = 4
+                        except ValueError:
+                            chassis_id_subtype = 7
+                port_id = d[2]
                 if is_ipv4(port_id) or is_ipv6(port_id):
-                    port_id_subtype = 4
+                        port_id_subtype = 4
                 else:
-                    try:
-                        MACAddressParameter().clean(port_id)
-                        port_id_subtype = 3
-                    except ValueError:
-                        port_id_subtype = 7
-                caps = 0
-                if "O" in match.group("caps"):
-                    caps += 1
-                elif "r" in match.group("caps"):
-                    caps += 2
-                elif "B" in match.group("caps"):
-                    caps += 4
-                elif "W" in match.group("caps"):
-                    caps += 8
-                elif "R" in match.group("caps"):
-                    caps += 16
-                elif "T" in match.group("caps"):
-                    caps += 32
-                elif "D" in match.group("caps"):
-                    caps += 64
-                elif "H" in match.group("caps"):
-                    caps += 128
+                        try:
+                            MACAddressParameter().clean(port_id)
+                            port_id_subtype = 3
+                        except ValueError:
+                            port_id_subtype = 7
+                caps = sum([self.CAPS[s.strip()] for s in d[4].split(",")])
+
                 neighbor = {
                     "remote_chassis_id": chassis_id,
                     "remote_chassis_id_subtype": chassis_id_subtype,
@@ -76,10 +74,15 @@ class Script(BaseScript):
                     "remote_port_subtype": port_id_subtype,
                     "remote_capabilities": caps
                 }
+                """
                 if match.group("system_name"):
                     neighbor["remote_system_name"] = match.group("system_name")
+                """
+                neighbor["remote_system_name"] = d[3]
                 r += [{
-                    "local_interface": match.group("port"),
+                    # "local_interface": match.group("port"),
+                    "local_interface": d[0],
                     "neighbors": [neighbor]
                 }]
+
         return r
