96eaa270980fb72bed3fd607952b9a990365d82b
Aleksey Shirokih <shirokih@nocproject.org>
2016-12-04 10:52:52 +0300

    setup reworked
    --HG--
    branch : feature/docker

--- setup.py
+++ setup.py
@@ -6,10 +6,8 @@
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 
-import glob
-from distutils.core import setup
-from distutils.command.sdist import sdist
-from distutils.extension import Extension
+import glob, os
+from setuptools import find_packages, Extension, setup
 
 VERSION_PATH = "VERSION"
 DESCRIPTION_PATH = "README"
@@ -35,33 +33,23 @@ def get_long_description():
         return f.read()
 
 
-def get_requirements():
-    """
-    Build requirements from requirements.txt
-    :return:
-    """
-    r = []
-    with open(REQUIREMENTS_PATH) as f:
-        for l in r:
-            l = l.strip()
-            if not l:
-                continue
-            pkg, op, version = None, None, None
-            for op in ("==", ">="):
-                if op in l:
-                    pkg, version = l.split(op)
-                    break
-            if not pkg:
-                continue
-            r += ["%s (%s%s)" % (pkg, op, version)]
-    return r
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
 
-class NOCSdist(sdist):
-    def run(self):
-        from Cython.Build import cythonize
-        cythonize(SPEEDUPS)
-        sdist.run(self)
+def get_data_files(extentions, directories):
+    results = []
+
+    for root, dirs, files in os.walk("."):
+        for file in files:
+            if file.endswith(extentions):
+                results.append((root, map(lambda f: root + "/" + f, files)))
+
+    for root, dirs, files in os.walk("."):
+        if root.endswith(directories):
+            results.append((root, map(lambda f: root + "/" + f, files)))
+    return results
+
 
 setup(
     name="noc",
@@ -72,9 +60,16 @@ setup(
     license="BSD",
     description="The Open-Source OSS for telecom, internet and service providers",
     long_description=get_long_description(),
-    requires=get_requirements(),
-    cmdclass={
-        "sdist": NOCSdist
-    },
-    ext_modules=[Extension("speedup", ["*.c"])]
+    # install_requires=requirements,
+    package_dir={"noc": "."},
+    data_files=get_data_files(
+        (".css", ".html", ".js", ".gif", ".hbs", ".svg",
+         ".json", ".sh", ".txt",
+         ".mo", ".po", ".pot",
+         "VERSION", "LICENSE", "AUTHORS"),
+        ("scripts/*", "etc/*", "commands/*")),
+    packages=["noc.%s" % x for x in find_packages()],
+    ext_modules=[Extension("noc/speedup/ber", ["speedup/ber.c"]),
+                 Extension("noc/speedup/ip", ["speedup/ip.c"])],
+    zip_safe=False
 )
