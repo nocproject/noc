aa6bc89b7eb4310b90a4a894b8ff84c80f0f3af4
EKbfh <earthquake_90@mail.ru>
2016-12-05 12:11:45 +0300

    Cisco WLC 5508 profile, begining.
    --HG--
    branch : feature/microservices

---
+++ sa/profiles/Cisco/WLC/get_fqdn.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Cisco.WLC.get_config
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Python modules
+import re
+## NOC modules
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetfqdn import IGetFQDN
+
+
+class Script(BaseScript):
+    name = "Cisco.WLC.get_fqdn"
+    interface = IGetFQDN
+    rx_hostname = re.compile(r"^hostname\s+(?P<hostname>\S+)", re.MULTILINE)
+    rx_domain_name = re.compile(r"^ip domain[ \-]name\s+(?P<domain>\S+)",
+        re.MULTILINE)
+
+    def execute(self):
+        if self.has_snmp():
+            try:
+                # sysName.0
+                v = self.snmp.get("1.3.6.1.2.1.1.5.0", cached=True)
+                if v:
+                   return v
+            except self.snmp.TimeOutError:
+                pass
+        v = self.cli(
+            "show running-config | include ^(hostname|ip domain.name)")
+        fqdn = []
+        match = self.rx_hostname.search(v)
+        if match:
+            fqdn += [match.group("hostname")]
+        match = self.rx_domain_name.search(v)
+        if match:
+            fqdn += [match.group("domain")]
+        return ".".join(fqdn)
---
+++ sa/profiles/Cisco/WLC/get_version.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Cisco.WLC.get_version
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetversion import IGetVersion
+import re
+
+rx_ver = re.compile(r"^Product Version\.+\s+(?P<version>\S+)",
+    re.MULTILINE | re.DOTALL)
+rx_inv = re.compile("^PID:\s+(?P<platform>\S+)\,",
+    re.MULTILINE | re.DOTALL)
+
+
+class Script(BaseScript):
+    name = "Cisco.WLC.get_version"
+    cache = True
+    interface = IGetVersion
+
+    def execute(self):
+        v = self.cli("show sysinfo")
+        match = rx_ver.search(v)
+        version = match.group("version")
+        v = self.cli("show inventory")
+        match = rx_inv.search(v)
+        platform = match.group("platform")
+        return {
+            "vendor": "Cisco",
+            "platform": platform,
+            "version": version,
+        }
