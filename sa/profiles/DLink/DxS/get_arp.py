7587d368094eb513e9dbb95ded5808cf10f4e4fe
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2016-12-08 14:23:54 +0200

    Fix DLink.DxS.get_arp script
    --HG--
    branch : feature/microservices

--- sa/profiles/DLink/DxS/get_arp.py
+++ sa/profiles/DLink/DxS/get_arp.py
@@ -16,16 +16,24 @@ class Script(BaseScript):
     name = "DLink.DxS.get_arp"
     interface = IGetARP
     rx_line = re.compile(
-        r"^(?P<interface>\S+)\s+(?P<ip>[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\s+"
+        r"^(?P<interface>\S*)\s+(?P<ip>[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\s+"
         r"(?P<mac>\S+)\s+\S+\s*$", re.MULTILINE)
 
     def execute(self, interface=None):
         r = []
+        iface_old = ''
         for match in self.rx_line.finditer(self.cli("show arpentry")):
-            if match.group("mac") == "FF-FF-FF-FF-FF-FF":
+            mac = match.group("mac")
+            if mac == "FF-FF-FF-FF-FF-FF":
                 continue
-            if (interface is not None) \
-            and (interface != match.group("interface")):
+            iface = match.group("interface")
+            if not iface:
+                # DES-1210-28/ME B2 with firmware 6.09.B047 can hide
+                # interface name in some rare cases
+                iface = iface_old
+            else:
+                iface_old = iface
+            if (interface is not None) and (interface != iface):
                 continue
-            r.append(match.groupdict())
+            r += [{'interface': iface, 'ip': match.group("ip"), 'mac': mac}]
         return r
