69090241e60685b23f059e13678d77a694d38b28
Dmitry Volodin <dv@nocproject.org>
2016-12-09 00:15:18 +0300

    TTSystem.get_by_id()
    --HG--
    branch : feature/microservices

--- fm/models/ttsystem.py
+++ fm/models/ttsystem.py
@@ -8,6 +8,7 @@
 
 ## Python modules
 import operator
+from threading import Lock
 ## Third-party modules
 from mongoengine.document import Document
 from mongoengine.fields import StringField, ListField
@@ -15,6 +16,7 @@ import cachetools
 ## NOC modules
 from noc.core.handler import get_handler
 
+id_lock = Lock()
 
 
 class TTSystem(Document):
@@ -33,12 +35,18 @@ class TTSystem(Document):
     tags = ListField(StringField())
 
     _id_cache = cachetools.TTLCache(maxsize=100, ttl=60)
+    _name_cache = cachetools.TTLCache(maxsize=100, ttl=60)
 
     def __unicode__(self):
         return self.name
 
     @classmethod
-    @cachetools.cachedmethod(operator.attrgetter("_id_cache"))
+    @cachetools.cachedmethod(operator.attrgetter("_id_cache"), lock=lambda _: id_lock)
+    def get_by_id(cls, id):
+        return TTSystem.objects.filter(id=id).first()
+
+    @classmethod
+    @cachetools.cachedmethod(operator.attrgetter("_name_cache"), lock=lambda _: id_lock)
     def get_by_name(cls, name):
         t = TTSystem.objects.filter(name=name).first()
         if t:
