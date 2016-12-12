9018f31352d166a5b4958d70af86a6c40e9d53cb
Andrey Vertiprakhov <aversanta@gmail.com>
2016-12-12 14:37:11 +0500

    Fix Alstec.24xx get_arp
    --HG--
    branch : feature/microservices

--- sa/profiles/Alstec/24xx/get_arp.py
+++ sa/profiles/Alstec/24xx/get_arp.py
@@ -10,6 +10,7 @@
 import re
 from noc.core.script.base import BaseScript
 from noc.sa.interfaces.igetarp import IGetARP
+from noc.sa.interfaces.base import MACAddressParameter
 from noc.lib.text import parse_table
 
 
@@ -34,6 +35,10 @@ class Script(BaseScript):
         r"^Default\sGateway\.+\s(?P<ip>[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\s*", re.MULTILINE
     )
 
+    rx_vlan_gw = re.compile(
+        r"^Management\sVLAN\sID\.+\s*(?P<MAC_address>\d+)\s*\n", re.MULTILINE
+    )
+
     def execute(self, interface=None):
         r = []
         v = self.cli("show network")
@@ -41,17 +46,26 @@ class Script(BaseScript):
         ip = self.rx_ip.findall(v)
         mac = self.rx_mac.findall(v)
         gw = self.rx_ip_gw.findall(v)
+        gw_vlan = self.rx_vlan_gw.findall(v)
 
         try:
             macs = parse_table(self.cli("show mac-addr-table vlan 1"))
-            mac_gw = macs[0][0]
+            try:
+                mac_gw = MACAddressParameter().clean(macs[0][0])
+            except ValueError:
+                macs = parse_table(self.cli("show mac-addr-table vlan %s" % gw_vlan[0]))
+                mac_gw = MACAddressParameter().clean([mac[0] for mac in macs if mac[3] == "Learned"][0])
         except self.CLISyntaxError:
             macs = parse_table(self.cli("show mac-addr-table"))
             print(macs)
-            mac_gw = [mac[0] for mac in macs if mac[2] == "1"][0]
+            f_mac_gw = [mac[0] for mac in macs if mac[2] == "1"]
+            if not f_mac_gw:
+                f_mac_gw = [mac[0] for mac in macs if mac[2] == gw_vlan[0] and mac[3] == "Learned"]
+            mac_gw = f_mac_gw[0]
 
         if mac_gw:
             r += [{"ip": gw[0], "mac": mac_gw}]
+
         r += [{"ip": ip[0], "mac": mac[0]}]
 
         return r
