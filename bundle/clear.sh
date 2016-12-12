1187a3c7ea210e1f501d38910566e95cd23c2004
Aleksey Shirokih <shirokih@nocproject.org>
2016-12-12 05:09:49 +0300

    nomad one. currently abondon.
    --HG--
    branch : feature/docker

--- bundle/clear.sh
+++ bundle/clear.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-docker-compose stop # just stop
-docker-compose rm -vf # remove containers
+dckrc="/usr/local/bin/docker-compose -f docker-compose-dbs.yml \
+                -f docker-compose-discovery.yml \
+                -f docker-compose-fm.yml \
+                -f docker-compose-infra.yml \
+                -f docker-compose-ping.yml \
+                -f docker-compose-sae.yml \
+                -f docker-compose-web.yml \
+                -f docker-compose-pm.yml \
+                -f docker-compose.yml"
+${dckrc} stop # just stop
+${dckrc} rm -vf # remove containers
 echo "Removing dbs"
 sudo rm -r ../../noc_data/mongo ../../noc_data/postgres   #drop dbs
 echo "Removing so files"
