2d0002d36c3cd3b4e66a80eccbed61f54b634caa
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2016-12-12 11:23:28 +0200

    Fix script name
    --HG--
    branch : feature/microservices

--- sa/profiles/Cisco/WLC/get_cdp_neighbors.py
+++ sa/profiles/Cisco/WLC/get_cdp_neighbors.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ##----------------------------------------------------------------------
-## Cisco.IOS.get_cdp_neighbors
+## Cisco.WLC.get_cdp_neighbors
 ##----------------------------------------------------------------------
 ## Copyright (C) 2007-2016 The NOC Project
 ## See LICENSE for details
@@ -13,7 +13,7 @@ import re
 
 
 class Script(BaseScript):
-    name = "Cisco.IOS.get_cdp_neighbors"
+    name = "Cisco.WLC.get_cdp_neighbors"
     interface = IGetCDPNeighbors
     rx_entry = re.compile(
         r"Device ID: (?P<device_id>\S+).+? IP address: (?P<remote_ip>\S+).+?"
