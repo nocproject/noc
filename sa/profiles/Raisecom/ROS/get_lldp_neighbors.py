6a5f2db7e26272d398ecd6f0eba10146148d8cac
Andrey Vertiprakhov <aversanta@gmail.com>
2016-12-14 21:48:00 +0500

    Fix Raisecom.ROS profile
    --HG--
    branch : feature/microservices

--- sa/profiles/Raisecom/ROS/get_lldp_neighbors.py
+++ sa/profiles/Raisecom/ROS/get_lldp_neighbors.py
@@ -20,19 +20,19 @@ class Script(BaseScript):
     interface = IGetLLDPNeighbors
 
     rx_lldp = re.compile(
-        r"Port\s+port(?P<port>\d+) has\s+1 remotes:\n\n"
-        r"Remote 1\n"
-        r"\-+\n"
+        r"Port\s+port(?P<port>\d+)\s*has\s+1\s*remotes:\n\n"
+        r"Remote\s*1\s*\n"
+        r"\s*\-+\n"
         r"ChassisIdSubtype:\s+(?P<ch_type>\S+)\s*\n"
         r"ChassisId:\s+(?P<ch_id>\S+)\s*\n"
         r"PortIdSubtype:\s+(?P<port_id_subtype>\S+)\s*\n"
         r"PortId:\s+(?P<port_id>.+)\s*\n"
         r"PortDesc:\s+(?P<port_descr>.+)\s*\n"
         r"SysName:\s+(?P<sys_name>.+)\s*\n"
-        r"SysDesc:\s+(?P<sys_descr>.+)\s*\n"
+        r"(?P<sys_descr>[\S\s]*?)\n"
         r"SysCapSupported:\s+(?P<sys_caps_supported>\S+)\s*\n"
         r"SysCapEnabled:\s+(?P<sys_caps_enabled>\S+)\s*\n",
-        re.MULTILINE)
+        re.MULTILINE | re.IGNORECASE)
 
     def execute(self):
         r = []
@@ -64,7 +64,7 @@ class Script(BaseScript):
             }
             if match.group("sys_name") != "N/A":
                 n["remote_system_name"] = match.group("sys_name")
-            if match.group("sys_descr") != "N/A":
+            if match.group("sys_descr") != "N/A" and "\n" not in match.group("sys_descr"):
                 n["remote_system_description"] = match.group("sys_descr")
             if match.group("port_descr") != "N/A":
                 n["remote_port_description"] = match.group("port_descr")
