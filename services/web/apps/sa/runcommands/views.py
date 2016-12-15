f817c20ed821e7b926e7583b59b6ea2f456212f6
Dmitry Volodin <dv@nocproject.org>
2016-12-15 18:48:36 +0300

    sa.runcommands: Snippets and actions forms API calls
    --HG--
    branch : feature/microservices

--- services/web/apps/sa/runcommands/views.py
+++ services/web/apps/sa/runcommands/views.py
@@ -8,7 +8,10 @@
 
 ## NOC modules
 from noc.lib.app.extapplication import ExtApplication
+from noc.lib.app.application import view
 from noc.core.translation import ugettext as _
+from noc.sa.models.commandsnippet import CommandSnippet
+from noc.sa.models.action import Action
 
 
 class RunCommandsApplication(ExtApplication):
@@ -20,3 +23,43 @@ class RunCommandsApplication(ExtApplication):
             "sa:objectlist:read"
         ]
     }
+
+    @view(url="^form/snippet/(?P<snippet_id>\d+)/$", method=["GET"],
+          access="launch", api=True)
+    def api_form_snippet(self, request, snippet_id):
+        snippet = self.get_object_or_404(CommandSnippet,
+                                         id=int(snippet_id))
+        r = []
+        vars = snippet.vars
+        for k in vars:
+            cfg = {
+                "name": k,
+                "fieldLabel": k,
+                "allowBlank": not vars[k].get("required", False)
+            }
+            t = vars[k].get("type")
+            if t == "int":
+                cfg["xtype"] = "numberfield"
+            else:
+                cfg["xtype"] = "textfield"
+            r += [cfg]
+        return r
+
+    @view(url="^form/action/(?P<action_id>[0-9a-f]{24})/$", method=["GET"],
+          access="launch", api=True)
+    def api_form_action(self, request, action_id):
+        action = self.get_object_or_404(CommandSnippet,
+                                         id=int(action_id))
+        r = []
+        for p in action.params:
+            cfg = {
+                "name": p.name,
+                "fieldLabel": p.description or p.name,
+                "allowBlank": not p.is_required
+            }
+            if p.type == "int":
+                cfg["xtype"] = "numberfield"
+            else:
+                cfg["xtype"] = "textfield"
+            r += [cfg]
+        return r
