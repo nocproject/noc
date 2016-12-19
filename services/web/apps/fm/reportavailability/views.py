88593a7eccd19c27d298ed9db49bc5a959d73a09
Andrey Vertiprakhov <aversanta@gmail.com>
2016-12-20 01:40:25 +0500

    Some fix and remove interval from Availability report
    --HG--
    branch : feature/microservices

--- services/web/apps/fm/reportavailability/views.py
+++ services/web/apps/fm/reportavailability/views.py
@@ -16,27 +16,38 @@ from django.contrib.admin.widgets import AdminDateWidget
 from noc.fm.models.outage import Outage
 from noc.sa.models.managedobject import ManagedObject
 from noc.sa.models.useraccess import UserAccess
-from noc.lib.app.simplereport import SimpleReport, TableColumn, PredefinedReport
+from noc.lib.app.simplereport import SimpleReport, TableColumn, PredefinedReport, SectionRow
 from noc.lib.dateutils import total_seconds
 from noc.lib.nosql import Q
+from pymongo import ReadPreference
 from noc.core.translation import ugettext as _
 
 
 class ReportForm(forms.Form):
+    """
     interval = forms.ChoiceField(choices=[
         (0, _("Range")),
-        (1, _("1 day")),
-        (7, _("1 week")),
-        (30, _("1 month"))
+        (1, _("last day")),
+        (7, _("last week")),
+        (30, _("last month"))
     ], label=_("Inteval"))
+    """
     from_date = forms.CharField(
         widget=AdminDateWidget,
         label=_("From Date"),
-        required=False
+        required=True
     )
     to_date = forms.CharField(
         widget=AdminDateWidget,
         label=_("To Date"),
+        required=False
+    )
+    skip_avail = forms.BooleanField(
+        label=_("Skip full available"),
+        required=False
+    )
+    skip_zero_avail = forms.BooleanField(
+        label=_("Skip zero available"),
         required=False
     )
 
@@ -46,77 +57,88 @@ class ReportAvailabilityApplication(SimpleReport):
     form = ReportForm
     predefined_reports = {
         "1d": PredefinedReport(
-            _("Availability (1 day)"), {
+            _("Availability (last day)"), {
                 "interval": 1
             }
         ),
         "7d": PredefinedReport(
-            _("Availability (7 days)"), {
+            _("Availability (last week)"), {
                 "interval": 7
             }
         ),
         "30d": PredefinedReport(
-            _("Availability (30 day)"), {
+            _("Availability (last month)"), {
                 "interval": 30
             }
         )
     }
 
-    def get_availability(self, days):
+    @staticmethod
+    def get_availability(start_date, stop_date, skip_zero_avail=False):
         now = datetime.datetime.now()
-        d = datetime.timedelta(days=days)
-        b = now - d
-        outages = defaultdict(int)
+        d = stop_date
+        b = start_date
+        outages = defaultdict(list)
+        td = total_seconds(d - b)
         q = Q(start__gte=b) | Q(stop__gte=b) | Q(stop__exists=False)
         for o in Outage.objects.filter(q):
             start = max(o.start, b)
-            stop = o.stop if o.stop else now
-            outages[o.object] += total_seconds(stop - start)
-        td = total_seconds(d)
+            stop = o.stop if o.stop else d
+            if total_seconds(stop - start) == td and skip_zero_avail:
+                continue
+            outages[o.object] += [total_seconds(stop - start)]
         # Normalize to percents
-        return dict((o, (td - outages[o]) * 100.0 / td) for o in outages)
+        return dict((o, ((td - sum(outages[o])) * 100.0 / td, int(sum(outages[o])), len(outages[o]))) for o in outages)
 
-    def get_data(self, request, interval=1, from_date=None, to_date=None, **kwargs):
+    def get_data(self, request, interval=1, from_date=None, to_date=None,
+                 skip_avail=False, skip_zero_avail=False, **kwargs):
         """
         a1 = self.get_availability(1)
         a7 = self.get_availability(7)
         a30 = self.get_availability(30)
         """
-        interval = int(interval)
+
         if not from_date:
-            interval = 1
-        if not interval:
-            if not to_date:
-                to_date = datetime.datetime.now().strftime("%d.%m.%Y")
-            sub = datetime.datetime.strptime(to_date, "%d.%m.%Y") - \
-                  datetime.datetime.strptime(from_date, "%d.%m.%Y")
-            interval = sub.days
-        a = self.get_availability(interval)
-        r = []
+            from_date = datetime.datetime.now() - datetime.timedelta(days=interval)
+        else:
+            from_date = datetime.datetime.strptime(from_date, "%d.%m.%Y")
+
+        if not to_date or from_date == to_date:
+            to_date = from_date + datetime.timedelta(days=1)
+        else:
+            to_date = datetime.datetime.strptime(to_date, "%d.%m.%Y") + datetime.timedelta(days=1)
+
+        a = self.get_availability(start_date=from_date, stop_date=to_date, skip_zero_avail=skip_zero_avail)
+        r = [SectionRow("Report from %s to %s" % (from_date, to_date))]
         mos = ManagedObject.objects.filter(is_managed=True)
+
         if not request.user.is_superuser:
-            mos = ManagedObject.objects.filter(
+            mos = mos.filter(
                 administrative_domain__in=UserAccess.get_domains(request.user))
+        if skip_avail:
+            mos = mos.filter(id__in=list(a))
         for o in mos:
-            r += [(
+            s = [
                 o.administrative_domain.name,
                 o.name,
-                o.profile_name,
-                o.platform,
                 o.address,
-                a.get(o.id, 100)
-            )]
+                o.profile_name
+            ]
+            s.extend(a.get(o.id, (100, 0, 0)))
+            r += [s]
             """
             a1.get(o.id, 100),
             a7.get(o.id, 100),
             a30.get(o.id, 100)
             """
+        print r
         return self.from_dataset(
             title=self.title,
             columns=[
-                _("Adm. Domain"),
-                _("Managed Object"), _("Profile"), _("Platform"), _("Address"),
-                TableColumn("%d %s" % (interval, _("Day")), align="right", format="percent"),
+                _("Adm. Domain"), _("Managed Object"), _("Address"), _("Profile"),
+                TableColumn(_("Avail"), align="right", format="percent"),
+                TableColumn(_("Total avail (sec)"), align="right", format="numeric"),
+                _("Count outages")
             ],
             data=r,
             enumerate=True
