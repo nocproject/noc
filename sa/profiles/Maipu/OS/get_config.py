09740235a58b8b752c68819cef155e29e1e45f9d
e_zombie <none@none>
2016-12-20 12:54:05 +0400

    Add basic profile and profilecheckrule for Maipu.com MES-3124F
    --HG--
    branch : feature/microservices

---
+++ sa/profiles/Maipu/OS/get_config.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+
+## NOC modules
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetconfig import IGetConfig
+
+
+
+class Script(BaseScript):
+    name = "Maipu.OS.get_config"
+    interface = IGetConfig
+
+    def execute(self):
+        self.cli("enable")
+        config = self.cli("show running-config")
+        return self.cleaned_config(config)
