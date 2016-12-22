910123118f1d563cbf2b3bd6fd0fc23a8fcd9663
Nikolay Miscenkov <nikolaj@4p.lv>
2016-12-22 15:06:25 +0200

    Change command "bcm cablediag" to "cable-diagnostics" "bcm cablediag" is support on es-2108-g(3.80) (tnx Alexey Shapovalov) Add cable distance to all pair.
    changed sa/profiles/Zyxel/ZyNOS/get_copper_tdr_diag.py
    
    --HG--
    branch : Zyxel_Fix_get_copper_tdr_diag

--- sa/profiles/Zyxel/ZyNOS/get_copper_tdr_diag.py
+++ sa/profiles/Zyxel/ZyNOS/get_copper_tdr_diag.py
@@ -2,7 +2,7 @@
 ##----------------------------------------------------------------------
 ## Zyxel.ZyNOS.get_copper_tdr_diag
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2014 The NOC Project
+## Copyright (C) 2007-2016 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 
@@ -17,36 +17,47 @@ class Script(BaseScript):
     name = "Zyxel.ZyNOS.get_copper_tdr_diag"
     interface = IGetCopperTDRDiag
 
-    rx_link = re.compile(r"^port (?P<interface>fe\d+): cable\s+(?:\S+)\s+"
-                        "\(2 pairs.*\).\s+pair A (?P<status_p1>\S+), "
-                        "length (?P<len_p1>\d+) metres.\s+pair B "
-                        "(?P<status_p2>\S+), length (?P<len_p2>\d+) metres$",
+    rx_link = re.compile(r"pairA:?\s+(?P<status_p1>\w+)(\s+(?P<len_p1>\d+\.\d+|N\/A)\s+(?P<len_fault_p1>\d+\.\d+|N\/A))?"
+            ".*pairB:?\s+(?P<status_p2>\w+)(\s+(?P<len_p2>\d+\.\d+|N\/A)\s+(?P<len_fault_p2>\d+\.\d+|N\/A))?"
+            ".*pairC:?\s+(?P<status_p3>\w+)(\s+(?P<len_p3>\d+\.\d+|N\/A)\s+(?P<len_fault_p3>\d+\.\d+|N\/A))?"
+            ".*pairD:?\s+(?P<status_p4>\w+)(\s+(?P<len_p4>\d+\.\d+|N\/A)\s+(?P<len_fault_p4>\d+\.\d+|N\/A))?",
                         re.MULTILINE | re.DOTALL)
 
-    def execute(self, interface=None):
+    def convert_to_cm(self, distance):
+        try:
+            distance = float(distance) * 100
+            return distance
+        except (ValueError, TypeError):
+            pass
+        return 0 
+    def convert_status(self, zstatus):
+        status = {"Ok": "T", "Open": "O", "Short": "S"} [zstatus]
+        return status 
+    def execute(self, interface):
         r = []
-        with self.zynos_mode():
-            s = self.cli("bcm cablediag %s" % interface)
+        s = self.cli("cable-diagnostics %s" % interface)
         for match in self.rx_link.finditer(s):
-            r += [{"interface": match.group("interface"),
+            r += [{"interface": interface,
                     "pairs": [{
                         "pair": 1,
-                        "status": {"Ok": "T", "Open": "O", "Short": "S"}
-                            [match.group("status_p1")],
-                        "distance_cm": int(match.group("len_p1")) * 100,
+                        "status": self.convert_status(match.group("status_p1")), 
+                        "distance_cm": self.convert_to_cm(match.group("len_p1")), 
+                        "distance_fault_cm": self.convert_to_cm(match.group("len_fault_p1")), 
                     }, {
                         "pair": 2,
-                        "status": {"Ok": "T", "Open": "O", "Short": "S"}
-                            [match.group("status_p2")],
-                        "distance_cm": int(match.group("len_p2")) * 100,
+                        "status": self.convert_status(match.group("status_p2")), 
+                        "distance_cm": self.convert_to_cm(match.group("len_p2")), 
+                        "distance_fault_cm": self.convert_to_cm(match.group("len_fault_p2")), 
                     }, {
                         "pair": 3,
-                        "status": "N",
-                        "distance_cm": 0,
+                        "status": self.convert_status(match.group("status_p3")), 
+                        "distance_cm": self.convert_to_cm(match.group("len_p3")), 
+                        "distance_fault_cm": self.convert_to_cm(match.group("len_fault_p3")), 
                     }, {
                         "pair": 4,
-                        "status": "N",
-                        "distance_cm": 0,
+                        "status": self.convert_status(match.group("status_p4")), 
+                        "distance_cm": self.convert_to_cm(match.group("len_p4")), 
+                        "distance_fault_cm": self.convert_to_cm(match.group("len_fault_p4")), 
                     }]
             }]
 
