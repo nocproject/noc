698710460b7ecc32a9166a1b66945c74e1f50f8a
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2016-12-26 10:54:27 +0200

    Update HP.ProCurve profile
    --HG--
    branch : feature/microservices

---
+++ sa/profiles/HP/ProCurve/get_capabilities.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## HP.ProCurve.get_capabilities.ex
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Python modules
+import re
+## NOC modules
+from noc.sa.profiles.Generic.get_capabilities import Script as BaseScript
+from noc.sa.profiles.Generic.get_capabilities import false_on_cli_error
+
+
+class Script(BaseScript):
+    name = "HP.ProCurve.get_capabilities"
+
+    @false_on_cli_error
+    def has_lldp(self):
+        """
+        Check box has lldp enabled
+        """
+        r = self.cli("show lldp config")
+        return "LLDP Enabled [Yes] : Yes" in r
+
+    @false_on_cli_error
+    def has_stp(self):
+        """
+        Check box has stp enabled
+        """
+        r = self.cli("show spanning-tree")
+        return "STP Enabled   : Yes" in r
