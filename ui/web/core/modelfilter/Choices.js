84ada353f3dc4fcd1f9c6c6b2ad29a742be8775b
Dmitry Volodin <dv@nocproject.org>
2016-12-30 15:58:07 +0300

    ModelApplication: *choices* filter
    --HG--
    branch : feature/microservices

---
+++ ui/web/core/modelfilter/Choices.js
@@ -0,0 +1,60 @@
+//---------------------------------------------------------------------
+// NOC.core.modelfilter.Choices
+// Boolean model filter
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2012 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.core.modelfilter.Choices");
+
+Ext.define("NOC.core.modelfilter.Choices", {
+    extend: "NOC.core.modelfilter.Base",
+
+    initComponent: function() {
+        var me = this;
+
+        me.combo = Ext.create("Ext.form.field.ComboBox", {
+            fieldLabel: me.title,
+            labelAlign: "top",
+            width: 180,
+            store: me.store,
+            listeners: {
+                scope: me,
+                select: me.onChange,
+                specialkey: me.onSpecialKey
+            }
+        });
+
+        Ext.apply(me, {
+            items: [me.combo]
+        });
+        me.callParent();
+    },
+
+    getFilter: function() {
+        var me = this,
+            v = me.combo.getValue(),
+            r = {};
+        if(v) {
+            r[me.name] = v;
+        }
+        return r;
+    },
+
+    setFilter: function(filter) {
+        var me = this;
+        if(me.name in filter) {
+            me.combo.setValue(filter[me.name]);
+        }
+    },
+
+    onSpecialKey: function(field, e) {
+        var me = this;
+        switch(e.keyCode) {
+            case e.ESC:
+                me.combo.clearValue();
+                me.onChange();
+                break;
+        }
+    }
+});
