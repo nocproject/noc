8f5270edfe73a29c1052939b14f6a141e071bfce
Dmitry Volodin <dv@nocproject.org>
2017-01-10 17:43:14 +0300

    django: Use signed cookies as session storage
    --HG--
    branch : feature/microservices

--- main/models/usersession.py
+++ main/models/usersession.py
@@ -11,7 +11,6 @@ import datetime
 ## Third-party modules
 from mongoengine.document import Document
 from mongoengine.fields import StringField, IntField
-from mongoengine.django.sessions import MongoSession
 
 
 class UserSession(Document):
@@ -30,29 +29,3 @@ class UserSession(Document):
     @classmethod
     def unregister(cls, session_key):
         UserSession.objects.filter(session_key=session_key).delete()
-
-    @classmethod
-    def active_sessions(cls, user=None, group=None):
-        """
-        Calculate current active sessions for user and group
-        """
-        ids = []
-        if user:
-            ids += [user.id]
-        if group:
-            ids += group.user_set.values_list("id", flat=True)
-        n = 0
-        now = datetime.datetime.now()
-        for us in UserSession.objects.filter(user_id__in=ids):
-            s = MongoSession.objects.filter(session_key=us.session_key).first()
-            if s:
-                # Session exists
-                if s.expire_date < now:
-                    # Expired session
-                    s.delete()
-                else:
-                    n += 1  # Count as active
-            else:
-                # Hanging session, schedule to kill
-                us.delete()
-        return n
