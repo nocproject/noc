a4e83557a89903cff42c584129248c59cf9a6aea
Dmitry Volodin <dv@nocproject.org>
2017-01-14 12:08:45 +0300

    sa.managedobjectprofile: Fix metrics labels
    --HG--
    branch : feature/microservices

--- services/web/apps/sa/managedobjectprofile/views.py
+++ services/web/apps/sa/managedobjectprofile/views.py
@@ -10,6 +10,7 @@
 from noc.lib.app.extmodelapplication import ExtModelApplication, view
 from noc.sa.models.managedobjectprofile import ManagedObjectProfile
 from noc.core.translation import ugettext as _
+from noc.pm.models.metrictype import MetricType
 
 
 class ManagedObjectProfileApplication(ExtModelApplication):
@@ -25,3 +26,12 @@ class ManagedObjectProfileApplication(ExtModelApplication):
 
     def field_mo_count(self, o):
         return o.managedobject_set.count()
+
+    def instance_to_dict(self, o, fields=None):
+        d = super(ManagedObjectProfileApplication, self).instance_to_dict(o, fields=fields)
+        if d["metrics"]:
+            for m in d["metrics"]:
+                mt = MetricType.get_by_id(m["metric_type"])
+                if mt:
+                    m["metric_type__label"] = mt.name
+        return d
