ea70019b4efbe1525412029ad4bdda4e9e39db5f
Andrey Vertiprakhov <aversanta@gmail.com>
2017-01-16 10:00:06 +0500

    Move metrics to json
    --HG--
    branch : feature/friendlymetrics

--- sa/profiles/Siklu/EH/get_metrics.py
+++ sa/profiles/Siklu/EH/get_metrics.py
@@ -11,19 +11,3 @@ from noc.sa.profiles.Generic.get_metrics import Script as GetMetricsScript
 
 class Script(GetMetricsScript):
     name = "Siklu.EH.get_metrics"
-
-    SNMP_OIDS = GetMetricsScript.merge_oids({
-        "Environment | Voltage": [
-            ("SNMP", "1.3.6.1.4.1.31926.1.1.0", "gauge", 1)
-        ],
-        "Environment | Temperature": [
-            ("SNMP", "1.3.6.1.4.1.31926.1.2.0", "gauge", 1)
-        ],
-        "Radio | CINR": [
-            ("SNMP", "1.3.6.1.4.1.31926.2.1.1.18.1", "gauge", 1)
-        ],
-        "Radio | RSSI": [
-            ("SNMP", "1.3.6.1.4.1.31926.2.1.1.19.1", "gauge", 1)
-        ]
-
-    })
