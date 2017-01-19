2f142424d39532f06cacd39c222227e38bbdcfb8
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2017-01-19 09:29:10 +0200

    Remove debugging
    --HG--
    branch : feature/microservices

--- sa/profiles/H3C/VRP/get_interfaces.py
+++ sa/profiles/H3C/VRP/get_interfaces.py
@@ -113,10 +113,12 @@ class Script(BaseScript):
         v = self.cli("display interface")
         il = self.rx_iface_sep.split(v)[1:]
         for full_ifname, data in zip(il[::2], il[1::2]):
+            """
             print "%s" % full_ifname
             if full_ifname.startswith("Vlan"):
                 print "%s" % full_ifname
                 quit()
+            """
             ifname = self.profile.convert_interface_name(full_ifname)
             if ifname.startswith("NULL"):
                 continue
