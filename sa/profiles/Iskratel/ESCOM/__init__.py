f4eb671680ebb311bdd5dc9b8535eb7d0c90cebd
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2017-01-20 11:37:38 +0200

    Add Iskratel.ESCOM profile
    --HG--
    branch : feature/microservices

---
+++ sa/profiles/Iskratel/ESCOM/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Vendor: Iskratel
+## OS:     ESCOM
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Python modules
+import re
+## NOC modules
+from noc.core.profile.base import BaseProfile
+
+
+class Profile(BaseProfile):
+    name = "Iskratel.ESCOM"
+    pattern_unpriveleged_prompt = r"^(?P<hostname>\S+)\s*>"
+    pattern_prompt = r"^(?P<hostname>\S+)\s*#"
+    pattern_syntax_error = r"% Unrecognized command|% Wrong number of parameters"
+    command_super = "enable"
+    command_disable_pager = "terminal datadump"
+    pattern_more = "More: <space>,  Quit: q or CTRL+Z, One line: <return>"
+    command_more = "a"
