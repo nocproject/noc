d84198307ac934cd4c61fb02bc6d8bf755659f39
Aleksey Shirokih <shirokih@nocproject.org>
2017-01-26 21:53:52 +0300

    sanitize chars before processing template
    --HG--
    branch : feature/microservices

--- services/web/apps/pm/ddash/dashboards/ipsla.py
+++ services/web/apps/pm/ddash/dashboards/ipsla.py
@@ -28,11 +28,11 @@ class IPSLADashboard(BaseDashboard):
 
     def render(self):
         context = {
-            "device": self.object.name,
+            "device": self.object.name.replace('\"', ''),
             "ip": self.object.address,
             "device_id": self.object.id,
             "segment": self.object.segment.id,
-            "probes": [{"name": probe.name, "value": probe.tests[0].target} for
+            "probes": [{"name": probe.name.replace('\"', ''), "value": probe.tests[0].target} for
                        probe in SLAProbe.objects.filter(managed_object=self.object.id)]
         }
         self.logger.info("Context with data: %s" % context)
--- services/web/apps/pm/ddash/dashboards/link.py
+++ services/web/apps/pm/ddash/dashboards/link.py
@@ -29,14 +29,14 @@ class LinkDashboard(BaseDashboard):
     def render(self):
         mos = self.object
         context = {
-            "device_a": mos.interfaces[0].managed_object.name,
-            "device_b": mos.interfaces[1].managed_object.name,
+            "device_a": mos.interfaces[0].managed_object.name.replace('\"', ''),
+            "device_b": mos.interfaces[1].managed_object.name.replace('\"', ''),
             "interface_a": {
                 "name": mos.interfaces[0].name,
-                "descr": mos.interfaces[0].description or mos.interfaces[0].name},
+                "descr": mos.interfaces[0].description.replace('\"', '') or mos.interfaces[0].name},
             "interface_b": {
                 "name": mos.interfaces[1].name,
-                "descr": mos.interfaces[1].description or mos.interfaces[1].name},
+                "descr": mos.interfaces[1].description.replace('\"', '') or mos.interfaces[1].name},
             "segment": mos.managed_objects[0].segment.id,
             "device_a_id": mos.managed_objects[0].id,
             "device_b_id": mos.managed_objects[1].id,
