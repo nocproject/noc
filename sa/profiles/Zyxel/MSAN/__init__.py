0a8707e98cc0fba51a5276d46dd6b30cbdde7263
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2017-02-02 12:22:52 +0200

    Fix Zyxel.MSAN profile
    --HG--
    branch : feature/microservices

--- sa/profiles/Zyxel/MSAN/__init__.py
+++ sa/profiles/Zyxel/MSAN/__init__.py
@@ -16,7 +16,7 @@ from noc.core.profile.base import BaseProfile
 
 class Profile(BaseProfile):
     name = "Zyxel.MSAN"
-    pattern_prompt = r"^(?P<hostname>[a-zA-Z0-9-_\.\s/]+)?[#>]\s*"
+    pattern_prompt = r"^(?P<hostname>[a-zA-Z0-9-_\.`\s/]+)?[#>]\s*"
     # pattern_unpriveleged_prompt = r"^(?P<hostname>[a-zA-Z0-9-_\.\s/]+)?>\s*"
     pattern_syntax_error = "((Unknown|invalid) (command|input)|Commands are:)"
     pattern_more = [
