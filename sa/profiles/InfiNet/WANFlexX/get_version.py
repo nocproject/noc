1c0d4ce2dab0fed39c53fe9f72feb4bbed075873
Alexey Shapovalov <alexsystems@list.ru>
2017-02-07 11:02:10 +0000

    get_version.py edited online with Bitbucket
    --HG--
    branch : ashapovalov/infinet-profile-fixes-and-improvements-1486464654530

--- sa/profiles/InfiNet/WANFlexX/get_version.py
+++ sa/profiles/InfiNet/WANFlexX/get_version.py
@@ -2,7 +2,7 @@
 ##----------------------------------------------------------------------
 ## InfiNet.WANFlexX.get_version
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2009 The NOC Project
+## Copyright (C) 2007-2017 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 """
@@ -11,18 +11,18 @@ from noc.core.script.base import BaseScript
 from noc.sa.interfaces.igetversion import IGetVersion
 import re
 
-rx_ver = re.compile(r"^(?P<platform>.+?)\s+WANFleX\s+(?P<version>\S+)",
-    re.MULTILINE | re.DOTALL)
-
 
 class Script(BaseScript):
     name = "InfiNet.WANFlexX.get_version"
     cache = True
     interface = IGetVersion
+    
+    rx_ver = re.compile(r"^(?P<platform>.+?)\s+WANFleX\s+(?P<version>\S+)",
+                        re.MULTILINE)
 
     def execute(self):
-        v = self.cli("system version")
-        match = rx_ver.search(v.strip())
+        cmd = self.cli("system version")
+        match = self.rx_ver.search(cmd.strip())
         return {
             "vendor": "InfiNet",
             "platform": match.group("platform"),
