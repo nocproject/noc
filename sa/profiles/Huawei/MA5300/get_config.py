a94c84a50e174120fa74c45245c400048528259b
e_zombie <none@none>
2017-02-09 16:15:57 +0400

    add Huawei MA5300 object profile
    original code Bee26
    
    --HG--
    branch : huawei.ma5300

---
+++ sa/profiles/Huawei/MA5300/get_config.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------
+## Huawei.MA5300.get_config
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2017 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+## NOC modules
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetconfig import IGetConfig
+
+
+class Script(BaseScript):
+    name = "Huawei.MA5300.get_config"
+    interface = IGetConfig
+
+    def execute(self):
+#	self.cli("cls")
+        self.cli("conf t")
+        self.cli("line vty 0 3")
+#        self.cli("length 0")
+#        self.cli("no length")
+        
+        config = self.cli("show running-config")
+        return config
+#        return self.cleaned_config(config)
+
