ce0137cbc797d627b12d27c33c1a4581f923c7dc
Andrey Vertiprakhov <aversanta@gmail.com>
2017-02-13 16:52:41 +0500

    Add Slot OID generator for Juniper.JunOSe
    --HG--
    branch : feature/microservices

--- sa/profiles/Juniper/JUNOSe/get_metrics.py
+++ sa/profiles/Juniper/JUNOSe/get_metrics.py
@@ -11,6 +11,43 @@ from noc.lib.text import parse_table
 from noc.core.script.metrics import percent_usage
 
 
+class SlotRule(OIDRule):
+
+    name = "slot"
+
+    def iter_oids(self, script, metric):
+        """
+                SlotNumber
+                2 for ERX-3xx models
+                6 for ERX-7xx models
+                13 for ERX-14xx models
+                16 for E320 models
+                :param metrics:
+                :return:
+                """
+        juniSystemSlotLevel = [1]
+        juniSystemSlotNumber = range(0, 17)
+        i = 0
+        r = {}
+
+        for sn in juniSystemSlotNumber:
+            for sl in juniSystemSlotLevel:
+                # r[i] = "%d.%d.%d" % (fi, si, cp)
+                r[str(i)] = "%d.%d" % (sn, sl)
+                # r[str(i)] = {"juniSystemSlotNumber": sn, "juniSystemSlotLevel": sl}
+                i += 1
+
+        for i in r:
+            if self.is_complex:
+                gen = [mib[self.expand(o, {"hwSlotIndex": r[i]})] for o in self.oid]
+                if gen:
+                    yield tuple(gen), self.type, self.scale, {"slot": i}
+            else:
+                oid = mib[self.expand(self.oid, {"hwSlotIndex": r[i]})]
+                if oid:
+                    yield oid, self.type, self.scale, {"slot": i}
+
+
 class Script(GetMetricsScript):
     name = "Juniper.JUNOSe.get_metrics"
 
