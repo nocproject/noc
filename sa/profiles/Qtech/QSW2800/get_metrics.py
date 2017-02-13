4a951975eade7456b6853252ddf569bb56aa620c
Andrey Vertiprakhov <aversanta@gmail.com>
2017-02-13 16:50:57 +0500

    Add Slot OID generator for Qtech.QSW2800
    --HG--
    branch : feature/microservices

--- sa/profiles/Qtech/QSW2800/get_metrics.py
+++ sa/profiles/Qtech/QSW2800/get_metrics.py
@@ -7,10 +7,38 @@
 ##----------------------------------------------------------------------
 
 from noc.sa.profiles.Generic.get_metrics import Script as GetMetricsScript
-from noc.core.script.metrics import percent
-
+from noc.sa.profiles.Generic.get_metrics import OIDRule
+from noc.lib.mib import mib
 
 
 class Script(GetMetricsScript):
     name = "Qtech.QSW2800.get_metrics"
 
+
+                    
+class SlotRule(OIDRule):
+
+    name = "slot"
+
+    def iter_oids(self, script, metric):
+        sysSlotIndex = [0]
+        i = 1
+        r = {}
+
+        if script.has_capability("Stack | Members"):
+            sysSlotIndex = range(1, script.capabilities["Stack | Members"] + 1)
+
+        for ms in sysSlotIndex:
+            r[str(i)] = "%d" % ms
+            # r[str(i)] = {"healthModuleSlot": ms}
+            i += 1
+
+        for i in r:
+            if self.is_complex:
+                gen = [mib[self.expand(o, {"hwSlotIndex": r[i]})] for o in self.oid]
+                if gen:
+                    yield tuple(gen), self.type, self.scale, {"slot": i}
+            else:
+                oid = mib[self.expand(self.oid, {"hwSlotIndex": r[i]})]
+                if oid:
+                    yield oid, self.type, self.scale, {"slot": i}
