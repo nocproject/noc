58d2cdf5b41282f82f3a5a567e5cf6605b5d3a29
Dmitry Volodin <dv@nocproject.org>
2017-02-17 01:02:04 +0300

    IGetLACPNeighbors interface
    --HG--
    branch : feature/microservices

---
+++ sa/interfaces/igetlacpneighbors.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## IGetLACPNeighbors
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2017 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOc modules
+from base import (Interface, IntParameter, InterfaceNameParameter,
+                  MACAddressParameter, DictListParameter)
+
+
+class IGetLACPNeighbors(Interface):
+    returns = DictListParameter(attrs={
+        # LAG ID
+        "lag_id": IntParameter(),
+        # Aggregated interface name
+        "interface": InterfaceNameParameter(),
+        # Local system id
+        "system_id": MACAddressParameter(),
+        # List of bundled interfaces
+        "bundle": DictListParameter(attrs={
+            # Local physical interface name
+            "interface": InterfaceNameParameter(),
+            # Local LACP port id
+            "local_port_id": IntParameter(),
+            # remote system id
+            "remote_system_id": MACAddressParameter(),
+            # remote system's peer port id
+            "remote_port_id": IntParameter()
+        })
+    })
