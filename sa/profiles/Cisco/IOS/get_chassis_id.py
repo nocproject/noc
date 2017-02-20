0ac93d374a2a40d5b1638d69da4d419dd3e4c104
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2017-02-20 15:22:59 +0200

    Update Cisco.IOS profile
    --HG--
    branch : feature/microservices

--- sa/profiles/Cisco/IOS/get_chassis_id.py
+++ sa/profiles/Cisco/IOS/get_chassis_id.py
@@ -85,7 +85,7 @@ class Script(BaseScript):
         r"MAC Address block size\s*:\s*(?P<count>\d+)",
         re.DOTALL)
 
-    @BaseScript.match(platform__regex=r"ASR100[0-4]")
+    @BaseScript.match(platform__regex=r"ASR100[0-6]")
     def execute_IOSXE(self):
         v = self.cli("show diag chassis eeprom detail")
         macs = []
