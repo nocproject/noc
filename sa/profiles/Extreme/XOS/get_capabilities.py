6b346a85ee174568a621eced005a04543797f577
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2017-02-21 12:54:38 +0200

    Update Extreme.XOS profile
    --HG--
    branch : feature/microservices

---
+++ sa/profiles/Extreme/XOS/get_capabilities.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Extreme.XOS.get_capabilities
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2017 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Python modules
+import re
+## NOC modules
+from noc.sa.profiles.Generic.get_capabilities import Script as BaseScript
+from noc.sa.profiles.Generic.get_capabilities import false_on_cli_error
+
+
+class Script(BaseScript):
+    name = "Extreme.XOS.get_capabilities"
+
+    rx_lldp = re.compile(r"^\s*\d+\s+Enabled\s+Enabled", re.MULTILINE)
+    rx_cdp = re.compile(r"^\s*CDP \S+ enabled ports\s+:\s+\d+", re.MULTILINE)
+
+    @false_on_cli_error
+    def has_lldp(self):
+        """
+        Check box has lldp enabled
+        """
+        cmd = self.cli("show lldp")
+        return self.rx_lldp.search(cmd) is not None
+
+    @false_on_cli_error
+    def has_cdp(self):
+        """
+        Check box has CDP enabled
+        """
+        cmd = self.cli("show cdp")
+        return self.rx_cdp.search(cmd) is not None
---
+++ sa/profiles/Extreme/XOS/get_cdp_neighbors.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Extreme.XOS.get_cdp_neighbors
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2017 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetcdpneighbors import IGetCDPNeighbors
+import re
+
+
+class Script(BaseScript):
+    name = "Extreme.XOS.get_cdp_neighbors"
+    interface = IGetCDPNeighbors
+
+    rx_dev_id = re.compile("CDP Device ID\s+:\s+(?P<device_id>\S+)")
+    rx_entry = re.compile(
+        r"^\s*(?P<local_interface>\d+)\s+(?P<device_id>\S+)\s+\d+\s+Ver\S+\s+"
+        r"(?P<remote_interface>\S+)", re.MULTILINE)
+
+    def execute(self):
+        match = self.rx_dev_id.search(self.cli("show cdp"))
+        device_id = match.group("device_id")
+        neighbors = []
+        for match in self.rx_entry.finditer(self.cli("show cdp ports")):
+            neighbors += [match.groupdict()]
+        return {
+            "device_id": device_id,
+            "neighbors": neighbors
+        }
