876a5b528bdb9abda6c06378225cbd1992baa8cd
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2017-03-03 15:06:11 +0200

    Add MikroTik.RouterOS.get_dot11_associations script
    --HG--
    branch : feature/microservices

---
+++ sa/profiles/MikroTik/RouterOS/get_dot11_associations.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## MikroTik.RouterOS.get_dot11_associations
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2017 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetdot11associations import IGetDot11Associations
+
+
+class Script(BaseScript):
+    name = "MikroTik.RouterOS.get_dot11_associations"
+    interface = IGetDot11Associations
+
+    def execute(self, interface=None):
+        cmd = "/interface wireless registration-table print stats without-paging"
+        if interface is not None:
+            cmd += " where interface=%s" % interface
+        try:
+            v = self.cli_detail(cmd)
+        except self.CLISyntaxError:
+            return []
+        return [{
+            "mac": r["mac-address"],
+            "ip": r["last-ip"].split()[0]
+        } for n, f, r in v]
