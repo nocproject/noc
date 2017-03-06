52547e8a84afbb8853fa8ef2a1d0efce9b1eb4f7
Dmitry Volodin <dv@nocproject.org>
2017-03-06 08:54:51 +0300

    ManagedObject: Create functional index to speedup prefix lookups
    --HG--
    branch : feature/microservices

---
+++ sa/migrations/0138_managedobject_address_index.py
@@ -0,0 +1,10 @@
+from south.db import db
+from django.db import models
+
+
+class Migration:
+    def forwards(self):
+        db.execute("CREATE INDEX x_managedobject_addressprefix ON sa_managedobject (CAST(address AS inet))")
+
+    def backwards(self):
+        db.drop_index("x_managedobject_addressprefix")
