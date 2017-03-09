035d42d14576156e31648b87749d534dcc2936dd
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2017-03-09 16:23:52 +0200

    Add initial support for Eltex.ESR profile
    --HG--
    branch : feature/microservices

---
+++ sa/profiles/Eltex/ESR/get_chassis_id.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Eltex.ESR.get_chassis_id
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2017 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Python modules
+import re
+## NOC modules
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetchassisid import IGetChassisID
+
+
+class Script(BaseScript):
+    name = "Eltex.ESR.get_chassis_id"
+    interface = IGetChassisID
+    cache = True
+
+    rx_mac = re.compile(
+        r"^\s*System MAC address:\s+(?P<mac>\S+)", re.MULTILINE)
+
+    def execute(self):
+        c = self.scripts.get_system()
+        match = self.rx_mac.search(c)
+        return {
+            "first_chassis_mac": match.group("mac"),
+            "last_chassis_mac": match.group("mac")
+        }
---
+++ sa/profiles/Eltex/ESR/get_mac_address_table.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Eltex.ESR.get_mac_address_table
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2017 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetmacaddresstable import IGetMACAddressTable
+from noc.lib.text import parse_table
+
+
+class Script(BaseScript):
+    name = "Eltex.ESR.get_mac_address_table"
+    interface = IGetMACAddressTable
+
+    def execute(self, interface=None, vlan=None, mac=None):
+        r = []
+        c = self.cli("show mac address-table")
+        for vlan_id, mac, port, mtype in parse_table(c, footer="\d+ valid mac entries"):
+            r += [{
+                "vlan_id": vlan_id,
+                "mac": mac,
+                "interfaces": [port],
+                "type": {
+                    "dynamic": "D",
+                    "static": "S",
+                    "permanent": "S",
+                    "self": "S"
+                }[mtype.lower()],
+            }]
+        return r
