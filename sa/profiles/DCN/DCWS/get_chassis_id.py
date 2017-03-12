f649a43e5b1eea330eac3f52dc36370c571447ce
smile@21.1.168.192.in-addr.arpa <smile@21.1.168.192.in-addr.arpa>
2017-03-12 22:15:59 +0300

    Add DCN Controller - DCWS profile
    --HG--
    branch : DCWS

---
+++ sa/profiles/DCN/DCWS/get_chassis_id.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## DCN.DCWS.get_chassis_id
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2017 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+## Python modules
+import re
+## NOC modules
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetchassisid import IGetChassisID
+from noc.lib.mac import MAC
+
+
+class Script(BaseScript):
+    name = "DCN.DCWS.get_chassis_id"
+    cache = True
+    interface = IGetChassisID
+    
+    rx_first = re.compile(r"^\s*CPU\s+MAC\s+(?P<fmac>\S+)\n", re.MULTILINE)   
+    rx_last = re.compile(r"^\s*VLAN\s+MAC\s+(?P<lmac>\S+)\n", re.MULTILINE)
+
+    def execute(self):
+        ver = self.cli("show version", cached=True)
+        fmatch = self.re_search(self.rx_first, ver)
+        lmatch = self.re_search(self.rx_last, ver)
+        return [{
+                        "first_chassis_mac": fmatch.group("fmac"),
+                        "last_chassis_mac": lmatch.group("lmac")
+                    }]
