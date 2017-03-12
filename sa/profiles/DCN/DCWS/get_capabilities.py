41cc7eaa52eec87f4846cb19e5857b8cfdbd4f72
smile@21.1.168.192.in-addr.arpa <smile@21.1.168.192.in-addr.arpa>
2017-03-12 22:15:59 +0300

    Add DCN Controller - DCWS profile
    --HG--
    branch : DCWS

---
+++ sa/profiles/DCN/DCWS/get_capabilities.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## DCN.DCWS.get_capabilities
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2017 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+from noc.sa.profiles.Generic.get_capabilities import Script as BaseScript, false_on_cli_error
+
+
+class Script(BaseScript):
+    name = "DCN.DCWS.get_capabilities"
+    cache = True
+
+    def execute_platform(self, caps):
+        if self.match_version(platform__regex="^DCWS*"):
+            caps["CPE | Controller"] = True
