6e89d87e84918a00b4280d66973bea685bd5f676
smile@21.1.168.192.in-addr.arpa <smile@21.1.168.192.in-addr.arpa>
2017-03-15 21:29:18 +0300

    Add Wilis FWBD AP profile
    --HG--
    branch : Wilis

---
+++ sa/profiles/Willis/FWBD/get_version.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Vendor: Wilis
+## OS:     FWBD
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2017 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+'''
+'''
+## Python modules
+import re
+## NOC modules
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetversion import IGetVersion
+
+
+class Script(BaseScript):
+    name = "Wilis.FWBD.get_version"
+    cache = True
+    interface = IGetVersion
+    
+    def execute(self):
+        c = self.cli("show software version", cached=True)
+        line = c.split(" ")
+        res = line[2].split(".", 2)
+        vendor = res[0]
+        platform = res[1]
+        sres = res[2].split(".")
+        sw = sres[0] + "." + sres[1]
+        return {
+            "vendor": vendor,
+            "platform": platform,
+            "version": sw,
+            }
