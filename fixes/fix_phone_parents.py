58d43d96598171fe6bf68fb09a62ba9234a1626d
Dmitry Volodin <dv@nocproject.org>
2017-03-16 17:11:48 +0300

    fix: phone_parents
    --HG--
    branch : feature/microservices

---
+++ fixes/fix_phone_parents.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Fix broken phone parents
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2017 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## Python modules
+from collections import defaultdict
+## NOC modules
+from noc.phone.models.phonerange import PhoneRange
+from noc.phone.models.phonenumber import PhoneNumber
+
+
+def fix():
+    # Fix phone ranges with broken parents
+    range_id = set()
+    parents = defaultdict(list)  # parent -> ids
+    for d in PhoneRange._get_collection().find({}, {"_id": 1, "parent": 1}):
+        range_id.add(d["_id"])
+        if d.get("parent"):
+            parents[d["parent"]] += [d["_id"]]
+    pset = set(parents)
+    for p in pset - range_id:
+        for pr in parents[p]:
+            r = PhoneRange.get_by_id(pr)
+            r.save()
+    # Fix phone numbers with broken parents
+    nparents = defaultdict(list)
+    for d in PhoneNumber._get_collection().find({}, {"_id": 1, "phone_range": 1}):
+        nparents[d["phone_range"]] += [d["_id"]]
+    pset = set(nparents)
+    for p in pset - range_id:
+        for pn in nparents[p]:
+            n = PhoneNumber.get_by_id(pn)
+            n.save()
