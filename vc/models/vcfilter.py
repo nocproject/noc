c7330bde8bbce9e84341bbaffe85f1757ec5a743
Dmitry Volodin <dv@nocproject.org>
2017-03-16 11:03:35 +0300

    VCFilter.get_by_id()
    --HG--
    branch : feature/microservices

--- vc/models/vcfilter.py
+++ vc/models/vcfilter.py
@@ -2,17 +2,22 @@
 ##----------------------------------------------------------------------
 ## VCFilter model
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2012 The NOC Project
+## Copyright (C) 2007-2017 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 
 ## Python modules
 import re
-## Django modules
+from threading import Lock
+import operator
+## Third-party modules
 from django.db import models
+import cachetools
+## NOC modules
 from noc.core.model.decorator import on_delete_check
 
 rx_vc_filter = re.compile(r"^\s*\d+\s*(-\d+\s*)?(,\s*\d+\s*(-\d+)?)*$")
+id_lock = Lock()
 
 
 @on_delete_check(check=[
@@ -34,15 +39,25 @@ class VCFilter(models.Model):
     expression = models.CharField("Expression", max_length=256)
     description = models.TextField("Description", null=True, blank=True)
 
+    _id_cache = cachetools.TTLCache(maxsize=100, ttl=60)
+
+    @classmethod
+    @cachetools.cachedmethod(operator.attrgetter("_id_cache"), lock=lambda _: id_lock)
+    def get_by_id(cls, id):
+        try:
+            return VCFilter.objects.get(id=id)
+        except VCFilter.DoesNotExist:
+            return None
+
     def __unicode__(self):
         return self.name
 
-    def save(self):
+    def save(self, *args, **kwargs):
         """
         Check expression before save
         """
         VCFilter.compile(self.expression)
-        super(VCFilter, self).save()
+        super(VCFilter, self).save(*args, **kwargs)
 
     @classmethod
     def compile(cls, expression):
