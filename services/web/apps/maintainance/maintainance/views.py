62354c523c89b8296ae282c2c3f62953ac85a146
Andrey Vertiprakhov <aversanta@gmail.com>
2017-03-21 13:57:59 +0500

    View maintainance affected objects API
    --HG--
    branch : feature/microservices

--- services/web/apps/maintainance/maintainance/views.py
+++ services/web/apps/maintainance/maintainance/views.py
@@ -9,6 +9,7 @@
 ## NOC modules
 from noc.lib.app.extdocapplication import ExtDocApplication, view
 from noc.maintainance.models.maintainance import Maintainance
+from noc.sa.models.managedobject import ManagedObject
 from noc.core.translation import ugettext as _
 
 
@@ -21,3 +22,21 @@ class MaintainanceApplication(ExtDocApplication):
     model = Maintainance
     query_condition = "icontains"
     query_fields = ["subject"]
+
+    @view(url="(?P<id>[0-9a-f]{24})/objects/", method=["GET"],
+          access="read", api=True)
+    def api_test(self, request, id):
+        o = self.get_object_or_404(Maintainance, id=id)
+        return [
+            {
+                "id": mo.id,
+                "name": mo.name,
+                "is_managed": mo.is_managed,
+                "profile": mo.profile_name,
+                # "platform": mo.platform,
+                # "administrative_domain": unicode(mo.administrative_domain),
+                "address": mo.address,
+                "description": mo.description,
+                "tags": mo.tags
+            } for mo in ManagedObject.objects.filter(id__in=o.currently_affected)
+        ]
