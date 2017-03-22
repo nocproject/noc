b40fdfe60e98b926165556ca7d75a2aba45ddb6f
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2017-03-22 17:58:57 +0200

    Add Alstec.MSPU profile
    --HG--
    branch : feature/microservices

---
+++ sa/profiles/Alstec/MSPU/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Vendor: Alstec
+## OS:     MSPU
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2017 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+import re
+## NOC modules
+from noc.core.profile.base import BaseProfile
+
+
+class Profile(BaseProfile):
+    name = "Alstec.MSPU"
+    pattern_prompt = r"^\S+\$> "
+    pattern_more = r"^--More-- or \(q\)uit$"
+    pattern_syntax_error = r"\^ error"
+    command_exit = "exit"
---
+++ sa/profiles/Alstec/MSPU/get_chassis_id.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Alstec.MSPU.get_chassis_id
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2017 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetchassisid import IGetChassisID
+import re
+
+
+class Script(BaseScript):
+    name = "Alstec.MSPU.get_chassis_id"
+    interface = IGetChassisID
+    cache = True
+
+    def execute(self):
+        r = []
+        v = self.scripts.get_mac_address_table()
+        for i in v:
+            if i["type"] == "C":
+                r += [{
+                    "first_chassis_mac": i["mac"],
+                    "last_chassis_mac": i["mac"]
+                }]
+        return r
