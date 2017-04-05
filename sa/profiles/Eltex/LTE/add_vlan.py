d8c339ef637c8a0df80447ad425d74c3fe6157b9
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2017-04-05 14:09:23 +0300

    Update Eltex.LTE profile
    --HG--
    branch : feature/microservices

---
+++ sa/profiles/Eltex/LTE/add_vlan.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Eltex.LTE.add_vlan
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2017 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.iaddvlan import IAddVlan
+
+
+class Script(BaseScript):
+    name = "Eltex.LTE.add_vlan"
+    interface = IAddVlan
+
+    def execute(self, vlan_id, name, tagged_ports):
+        with self.profile.switch(self):
+                self.cli("configure")
+                self.cli("vlan %d" % vlan_id)
+                if name:
+                    self.cli("name %s" % name)
+                if tagged_ports:
+                    for port in tagged_ports:
+                        self.cli("tagged %s" % port)
+                self.cli("exit")
+                self.cli("exit")
+        self.save_config()
+        return True
---
+++ sa/profiles/Eltex/LTE/remove_vlan.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Eltex.LTE.remove_vlan
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2017 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+
+## NOC modules
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.iremovevlan import IRemoveVlan
+
+
+class Script(BaseScript):
+    name = "Eltex.LTE.remove_vlan"
+    interface = IRemoveVlan
+
+    def execute(self, vlan_id):
+        if not self.scripts.has_vlan(vlan_id=vlan_id):
+            return False
+        with self.profile.switch(self):
+            self.cli("configure")
+            self.cli("no vlan %d" % vlan_id)
+            self.cli("exit")
+        self.save_config()
+        return True
