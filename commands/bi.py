c13d30229f78b4e7b1e064d3995eac4de9712a62
Dmitry Volodin <dv@nocproject.org>
2017-04-13 23:30:40 +0300

    ./noc bi extract dumps dictionary data and XML definition to file
    --HG--
    branch : feature/microservices

--- commands/bi.py
+++ commands/bi.py
@@ -18,10 +18,13 @@ from noc.core.etl.bi.extractor.reboots import RebootsExtractor
 from noc.core.etl.bi.extractor.alarms import AlarmsExtractor
 from noc.core.clickhouse.connect import connection
 from noc.core.clickhouse.model import Model
+from noc.core.clickhouse.dictionary import Dictionary
 
 
 class Command(BaseCommand):
-    PREFIX = "var/bi"
+    DATA_PREFIX = "var/bi"
+    DICT_XML_PREFIX = "var/bi-dict"
+    DICT_DATA_PREFIX = "var/bi-dict-data"
 
     EXTRACTORS = [
         RebootsExtractor,
@@ -66,30 +69,50 @@ class Command(BaseCommand):
         for ecls in self.EXTRACTORS:
             start = self.get_last_extract(ecls.name) or t0
             stop = now - datetime.timedelta(seconds=ecls.extract_delay)
-            e = ecls(start=start, stop=stop, prefix=self.PREFIX)
+            e = ecls(start=start, stop=stop, prefix=self.DATA_PREFIX)
             self.stdout.write("Extracting %s (%s - %s)\n" % (
                 e.name, start, stop
             ))
             e.extract()
             self.set_last_extract(ecls.name, e.last_ts or stop)
+        # Extract dictionaries
+        for dcls in Dictionary.iter_cls():
+            # Temporary data
+            dpath = os.path.join(self.DICT_DATA_PREFIX, "%s.tsv.tmp" % dcls._meta.name)
+            self.stdout.write("Extracting dictionary data to %s\n" % dpath)
+            with open(dpath, "w") as f:
+                dcls.dump(f)
+            # Temporary XML
+            xpath = os.path.join(self.DICT_XML_PREFIX, "%s.xml.tml" % dcls._meta.name)
+            self.stdout.write("Extracting dictionary XML to %s\n" % xpath)
+            with open(xpath, "w") as f:
+                f.write(dcls.get_config())
+            # Move temporary data
+            df = dpath[:-4]
+            self.stdout.write("Renaming dictionary data to %s\n" % df)
+            os.rename(dpath, df)
+            # Move temporary XML
+            xf = xpath[:-4]
+            self.stdout.write("Rename dictionary XML to %s\n" % xf)
+            os.rename(xpath, xf)
 
     def handle_clean(self, *args, **options):
         for ecls in self.EXTRACTORS:
             stop = self.get_last_extract(ecls.name)
             if not stop:
                 continue
-            e = ecls(start=stop, stop=stop, prefix=self.PREFIX)
+            e = ecls(start=stop, stop=stop, prefix=self.DATA_PREFIX)
             e.clean()
 
     def handle_load(self):
         ch = connection()
         ch.ensure_db()
         files = defaultdict(list)
-        for f in sorted(os.listdir(self.PREFIX)):
+        for f in sorted(os.listdir(self.DATA_PREFIX)):
             if not f.endswith(".tsv.gz"):
                 continue
             x = f.split("-", 1)[0]
-            files[x] += [os.path.join(self.PREFIX, f)]
+            files[x] += [os.path.join(self.DATA_PREFIX, f)]
         for mn in files:
             self.stdout.write("Loading %s\n" % mn)
             model = Model.get_model_class(mn)
