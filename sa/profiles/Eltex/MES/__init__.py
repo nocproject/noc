a9553c55ebe2b0d80338b2bc6c79f112267f7e35
Maksim13 <none@none>
2017-04-13 16:24:06 +0300

    Fix Eltex.MES profile
    --HG--
    branch : Eltex.MES.New

--- sa/profiles/Eltex/MES/__init__.py
+++ sa/profiles/Eltex/MES/__init__.py
@@ -3,7 +3,7 @@
 ## Vendor: Eltex
 ## OS:     MES
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2011 The NOC Project
+## Copyright (C) 2007-2017 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 
@@ -15,8 +15,9 @@ class Profile(BaseProfile):
     name = "Eltex.MES"
     pattern_more = [
         (r"^More: <space>,  Quit: q, One line: <return>$", " "),
-        (r"\[Yes/press any key for no\]", "Y")
-        ]
+        (r"\[Yes/press any key for no\]", "Y"),
+        (r"<return>, Quit: q or <ctrl>", " ")
+    ]
     pattern_unpriveleged_prompt = r"^\S+> "
     pattern_syntax_error = r"^% (Unrecognized command|Incomplete command|Wrong number of parameters or invalid range, size or characters entered)$"
     command_disable_pager = "terminal datadump"
@@ -26,3 +27,40 @@ class Profile(BaseProfile):
     command_save_config = "copy running-config startup-config"
     pattern_prompt = r"^\S+#"
     convert_interface_name = BaseProfile.convert_interface_name_cisco
+
+    INTERFACE_TYPES = {
+        "As": "physical",  # Async
+        "AT": "physical",  # ATM
+        "At": "physical",  # ATM
+        "BV": "aggregated",  # BVI
+        "Bu": "aggregated",  # Bundle
+        "C": "physical",  # @todo: fix
+        "Ca": "physical",  # Cable
+        "CD": "physical",  # CDMA Ix
+        "Ce": "physical",  # Cellular
+        "et": "physical",  # Ethernet
+        "fa": "physical",  # FastEthernet
+        "gi": "physical",  # GigabitEthernet
+        "Gr": "physical",  # Group-Async
+        "Lo": "loopback",  # Loopback
+        "M": "management",  # @todo: fix
+        "MF": "aggregated",  # Multilink Frame Relay
+        "Mf": "aggregated",  # Multilink Frame Relay
+        "Mu": "aggregated",  # Multilink-group interface
+        # "PO": "physical",    # Packet OC-3 Port Adapter
+        "Po ": "aggregated",  # Port-channel/Portgroup
+        "Po": "aggregated",  # Port-channel/Portgroup
+        "R": "aggregated",  # @todo: fix
+        "SR": "physical",  # Spatial Reuse Protocol
+        "Se": "physical",  # Serial
+        "te": "physical",  # TenGigabitEthernet
+        "Tu": "tunnel",  # Tunnel
+        "VL": "SVI",  # VLAN, found on C3500XL
+        "Vl": "SVI",  # Vlan
+        "vl": "SVI",  # vlan Eltex ver 1.1.38
+        "XT": "SVI"  # Extended Tag ATM
+    }
+
+    @classmethod
+    def get_interface_type(cls, name):
+        return cls.INTERFACE_TYPES.get(name[:2])
--- sa/profiles/Eltex/MES/get_capabilities.py
+++ sa/profiles/Eltex/MES/get_capabilities.py
@@ -16,12 +16,32 @@ from noc.sa.profiles.Generic.get_capabilities import false_on_cli_error
 class Script(BaseScript):
     name = "Eltex.MES.get_capabilities"
 
-    rx_lldp = re.compile(r"LLDP state: Enabled?")
+    rx_lldp_en = re.compile(r"LLDP state: Enabled?")
+    rx_gvrp_en = re.compile(r"GVRP Feature is currently Enabled on the device?")
+    rx_stp_en = re.compile(r"Spanning tree enabled mode?")
+
 
     @false_on_cli_error
     def has_lldp(self):
         """
         Check box has lldp enabled on Eltex
         """
-        cmd = self.cli("show lldp configuration")
-        return self.rx_lldp.search(cmd) is not None
+        cmd = self.cli("show lldp configuration", ignore_errors = True)
+        return self.rx_lldp_en.search(cmd) is not None
+
+    @false_on_cli_error
+    def has_stp(self):
+        """
+        Check box has stp enabled on Eltex
+        """
+        cmd = self.cli("show spanning-tree", ignore_errors = True)
+        return self.rx_stp_en.search(cmd) is not None
+
+    @false_on_cli_error
+    def has_gvrp(self):
+        """
+        Check box has gvrp enabled on Eltex
+        """
+        cmd = self.cli("show gvrp configuration", ignore_errors = True)
+        return self.rx_gvrp_en.search(cmd) is not None
+        # Get GVRP interfaces
