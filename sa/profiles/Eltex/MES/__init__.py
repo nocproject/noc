32dd1a810fdf78f8e13d1448baa8eab4388b26e1
Maksim <sysfar@gmail.com>
2017-04-13 13:45:19 +0000

    __init__.py edited online with Bitbucket
    --HG--
    branch : Eltex.MES.New

--- sa/profiles/Eltex/MES/__init__.py
+++ sa/profiles/Eltex/MES/__init__.py
@@ -3,7 +3,7 @@
 ## Vendor: Eltex
 ## OS:     MES
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2017 The NOC Project
+## Copyright (C) 2007-2011 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 
@@ -17,7 +17,7 @@ class Profile(BaseProfile):
         (r"^More: <space>,  Quit: q, One line: <return>$", " "),
         (r"\[Yes/press any key for no\]", "Y"),
         (r"<return>, Quit: q or <ctrl>", " ")
-    ]
+        ]
     pattern_unpriveleged_prompt = r"^\S+> "
     pattern_syntax_error = r"^% (Unrecognized command|Incomplete command|Wrong number of parameters or invalid range, size or characters entered)$"
     command_disable_pager = "terminal datadump"
@@ -29,38 +29,32 @@ class Profile(BaseProfile):
     convert_interface_name = BaseProfile.convert_interface_name_cisco
 
     INTERFACE_TYPES = {
-        "As": "physical",  # Async
-        "AT": "physical",  # ATM
-        "At": "physical",  # ATM
-        "BV": "aggregated",  # BVI
-        "Bu": "aggregated",  # Bundle
-        "C": "physical",  # @todo: fix
-        "Ca": "physical",  # Cable
-        "CD": "physical",  # CDMA Ix
-        "Ce": "physical",  # Cellular
-        "et": "physical",  # Ethernet
-        "fa": "physical",  # FastEthernet
-        "gi": "physical",  # GigabitEthernet
-        "Gr": "physical",  # Group-Async
-        "Lo": "loopback",  # Loopback
-        "M": "management",  # @todo: fix
-        "MF": "aggregated",  # Multilink Frame Relay
-        "Mf": "aggregated",  # Multilink Frame Relay
-        "Mu": "aggregated",  # Multilink-group interface
-        # "PO": "physical",    # Packet OC-3 Port Adapter
-        "Po ": "aggregated",  # Port-channel/Portgroup
-        "Po": "aggregated",  # Port-channel/Portgroup
-        "R": "aggregated",  # @todo: fix
-        "SR": "physical",  # Spatial Reuse Protocol
-        "Se": "physical",  # Serial
-        "te": "physical",  # TenGigabitEthernet
-        "Tu": "tunnel",  # Tunnel
-        "VL": "SVI",  # VLAN, found on C3500XL
-        "Vl": "SVI",  # Vlan
-        "vl": "SVI",  # vlan Eltex ver 1.1.38
-        "XT": "SVI"  # Extended Tag ATM
-    }
+               "as": "physical",    # Async
+               "at": "physical",    # ATM
+               "bv": "aggregated",  # BVI
+               "bu": "aggregated",  # Bundle
+               #"C": "physical",     # @todo: fix
+               "ca": "physical",    # Cable
+               "cd": "physical",    # CDMA Ix
+               "ce": "physical",    # Cellular
+               "et": "physical",    # Ethernet
+               "fa": "physical",    # FastEthernet
+               "gi": "physical",    # GigabitEthernet
+               "gr": "physical",    # Group-Async
+               "lo": "loopback",    # Loopback
+               #"M": "management",   # @todo: fix
+               "mf": "aggregated",  # Multilink Frame Relay
+               "mu": "aggregated",  # Multilink-group interface
+               "po": "aggregated",  # Port-channel/Portgroup
+               #"R": "aggregated",   # @todo: fix
+               "sr": "physical",    # Spatial Reuse Protocol
+               "se": "physical",    # Serial
+               "te": "physical",    # TenGigabitEthernet
+               "tu": "tunnel",      # Tunnel
+               "vl": "SVI",         # VLAN, found on C3500XL
+               "xt": "SVI"          # Extended Tag ATM
+               }
 
     @classmethod
     def get_interface_type(cls, name):
-        return cls.INTERFACE_TYPES.get(name[:2])
+        return cls.INTERFACE_TYPES.get((name[:2]).lower())                                    
