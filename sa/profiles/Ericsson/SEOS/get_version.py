af1491c2d2016efc05d6c0f471d21774d79fdf11
Maksim13 <none@none>
2017-04-16 12:36:13 +0300

    fix Ericsson get_version
    --HG--
    branch : Ericsson

--- sa/profiles/Ericsson/SEOS/get_version.py
+++ sa/profiles/Ericsson/SEOS/get_version.py
@@ -2,7 +2,7 @@
 ##----------------------------------------------------------------------
 ## Ericsson.SEOS.get_version
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2011 The NOC Project
+## Copyright (C) 2007-2017 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 
@@ -17,14 +17,22 @@ class Script(BaseScript):
     name = "Ericsson.SEOS.get_version"
     cache = True
     interface = IGetVersion
-    rx_ver = re.compile(r"^[^-]*-(?P<version>[^-]+)-.*",
-                        re.MULTILINE | re.DOTALL)
+
+    rx_ver = re.compile(r"^Active SBL\s+:\s+CXP:\s+(?P<version>\S+.*)\s+"
+                        r"^Passive NPU\s+:\s+CXP:\s+[\S\s]+"
+                        r"^Active BNS\s+:\s+CXCR:\s+(?P<sw_backup>\S+.*)$",
+                        re.MULTILINE)
 
     def execute(self):
-        v = self.cli("show version", cached=True)
-        match = self.re_search(self.rx_ver, v)
-        return {
-            "vendor"   : "Ericsson",
-            "platform" : "SEOS",
-            "version"  : match.group("version"),
-        }
+        ver = self.cli("show version", cached=True)
+        for match in self.rx_ver.finditer(ver):
+            version = match.group("version")
+            sw_backup = match.group("sw_backup")
+            return {
+                "vendor": "Ericsson",
+                "platform": "SEOS",
+                "version": version,
+                "attributes": {
+                    "sw_backup": sw_backup
+                }
+            }
