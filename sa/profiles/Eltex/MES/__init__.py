9db8e9603bd37c272acf0f5eba86cbc9b10816a3
Maksim13 <none@none>
2017-04-20 20:07:16 +0300

    Fix Eltex.MES __init__
    --HG--
    branch : feature/dcs

--- sa/profiles/Eltex/MES/__init__.py
+++ sa/profiles/Eltex/MES/__init__.py
@@ -3,7 +3,7 @@
 ## Vendor: Eltex
 ## OS:     MES
 ##----------------------------------------------------------------------
-## Copyright (C) 2007-2011 The NOC Project
+## Copyright (C) 2007-2017 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
 
@@ -16,16 +16,17 @@ class Profile(BaseProfile):
     pattern_more = [
         (r"^More: <space>,  Quit: q, One line: <return>$", " "),
         (r"\[Yes/press any key for no\]", "Y"),
-        (r"<return>, Quit: q or <ctrl>", " ")
+        (r"<return>, Quit: q or <ctrl>", " "),
+        (r"q or <ctrl>+z", " ")
         ]
-    pattern_unpriveleged_prompt = r"^\S+> "
+    pattern_unpriveleged_prompt = r"^(?P<hostname>\S+)> "
     pattern_syntax_error = r"^% (Unrecognized command|Incomplete command|Wrong number of parameters or invalid range, size or characters entered)$"
     command_disable_pager = "terminal datadump"
     command_super = "enable"
     command_enter_config = "configure"
     command_leave_config = "end"
     command_save_config = "copy running-config startup-config"
-    pattern_prompt = r"^\S+#"
+    pattern_prompt = r"^(?P<hostname>\S+)#"
     convert_interface_name = BaseProfile.convert_interface_name_cisco
 
     INTERFACE_TYPES = {
