158b469d792c70076412d22be397618daefcc11f
Aleksey Shirokih <shirokih@nocproject.org>
2017-04-20 21:20:58 +0300

    do not log health checks
    --HG--
    branch : feature/dcs

--- core/service/base.py
+++ core/service/base.py
@@ -673,6 +673,8 @@ class Service(object):
         if status == 200 and uri == "/mon/" and method == "GET":
             self.logger.debug("Monitoring request (%s)", remote_ip)
             self.perf_metrics["mon_requests"] += 1
+        elif status == 200 and uri == "/health/" and method == "GET":
+            pass
         else:
             self.logger.info(
                 "%s %s (%s) %.2fms",
