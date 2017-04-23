3bd07fecc98d356a8e6e1c83bf26677f278f50fe
Andrey Vertiprakhov <aversanta@gmail.com>
2017-04-23 12:37:37 +0500

    Fix alarmdetail report max_duration filter
    --HG--
    branch : feature/microservices

--- services/web/apps/fm/reportalarmdetail/views.py
+++ services/web/apps/fm/reportalarmdetail/views.py
@@ -141,7 +141,9 @@ class ReportAlarmDetailApplication(ExtApplication):
                 [("timestamp", 1)]):
             dt = a["clear_timestamp"] - a["timestamp"]
             duration = dt.days * 86400 + dt.seconds
-            if duration and (duration < min_duration or duration > max_duration):
+            if duration and duration < min_duration:
+                continue
+            if duration and max_duration and duration > max_duration:
                 continue
             total_objects = sum(
                 ss["summary"] for ss in a["total_objects"])
