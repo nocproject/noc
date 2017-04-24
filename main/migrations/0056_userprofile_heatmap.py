18b0867cf2c381132321924cbb326bbc925b2e0a
Dmitry Volodin <dv@nocproject.org>
2017-04-24 20:09:03 +0300

    Heatmap positioning via user profile
    --HG--
    branch : feature/microservices

---
+++ main/migrations/0056_userprofile_heatmap.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from south.db import db
+from django.db import models
+
+class Migration:
+
+    def forwards(self):
+        db.add_column(
+            "main_userprofile",
+            "heatmap_lon",
+            models.FloatField("Longitude", blank=True, null=True))
+        db.add_column(
+            "main_userprofile",
+            "heatmap_lat",
+            models.FloatField("Latitude", blank=True, null=True))
+        db.add_column(
+            "main_userprofile",
+            "heatmap_zoom",
+            models.IntegerField("Zoom", blank=True, null=True))
+
+    def backwards(self):
+        db.delete_column("main_userprofile", "heatmap_lon")
+        db.delete_column("main_userprofile", "heatmap_lat")
+        db.delete_column("main_userprofile", "heatmap_zoom")
