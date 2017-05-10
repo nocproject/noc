ba3bba1fed40fb98e1a2f9447a57c3aa37aa56e4
Smile@DESKTOP-P9JN73U <Smile@DESKTOP-P9JN73U>
2017-05-10 22:28:28 +0300

    add Raisecom.RCIOS profile
    --HG--
    branch : feature/dcs

---
+++ sa/profiles/Raisecom/RCIOS/get_config.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Raisecom.RCIOS.get_config
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2017 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetconfig import IGetConfig
+
+
+class Script(BaseScript):
+    name = "Raisecom.RCIOS.get_config"
+    interface = IGetConfig
+
+    def execute(self):
+        config = self.cli("show running-config")
+        config = self.strip_first_lines(config, 1)
+        return self.cleaned_config(config)
