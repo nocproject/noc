607357961c1b2c7f891f5bd035cfb8399b259daa
Maksim <none@none>
2017-05-11 16:02:36 +0300

    add Telindus.SHDSL profile
    --HG--
    branch : feature/dcs

---
+++ sa/profiles/Telindus/SHDSL/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Vendor: Ruckus
+## OS:     AP
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2016 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+'''
+'''
+
+from noc.core.profile.base import BaseProfile
+import re
+
+class Profile(BaseProfile):
+    name = "Telindus.SHDSL"
