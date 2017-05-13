5e181eabf68327f7df225d42344090c8d3ed48bd
Dmitry Volodin <dv@nocproject.org>
2017-05-13 13:12:05 +0300

    discovery: Shutdown threadpool executor outside of ioloop
    --HG--
    branch : feature/dcs

--- services/discovery/service.py
+++ services/discovery/service.py
@@ -72,7 +72,7 @@ class DiscoveryService(Service):
     @tornado.gen.coroutine
     def on_deactivate(self):
         if self.scheduler:
-            self.scheduler.shutdown()
+            self.add_close_callback(self.scheduler.shutdown)
 
 if __name__ == "__main__":
     DiscoveryService().start()
