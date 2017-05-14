f76ed93fa6d5392c2d1437e5144efbb9db814c29
Dmitry Zakharov <d.a.zakharov@gmail.com>
2017-05-14 11:32:57 +0300

    filter: add lookup to Vendor and Platform
    --HG--
    branch : feature/microservices

---
+++ ui/web/sa/vendor/LookupField.js
@@ -0,0 +1,12 @@
+//---------------------------------------------------------------------
+// NOC.inv.vendor.Lookup
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2012 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.sa.vendor.LookupField");
+
+Ext.define("NOC.sa.vendor.LookupField", {
+    extend: "NOC.core.LookupField",
+    alias: "widget.sa.vendor.LookupField"
+});
