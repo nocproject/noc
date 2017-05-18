5d02ed13371592c1cff3c69ebae55bf06715d357
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2017-05-18 10:58:12 +0300

    Fix Raisecom.RCIOS profile
    --HG--
    branch : feature/microservices

--- sa/profiles/Raisecom/RCIOS/__init__.py
+++ sa/profiles/Raisecom/RCIOS/__init__.py
@@ -16,26 +16,32 @@ from noc.core.profile.base import BaseProfile
 
 class Profile(BaseProfile):
     name = "Raisecom.RCIOS"
-    pattern_more = "--More-- "
     pattern_username = r"^([Uu]ser ?[Nn]ame|[Ll]ogin): ?"
     pattern_unpriveleged_prompt = r"^(?P<hostname>\S+)> "
     command_super = "enable"
-    pattern_prompt = r"^(?P<hostname>\S+)#"
-    command_more = " "
+    pattern_prompt = r"^(?P<hostname>\S+)# "
     command_exit = "exit"
     pattern_syntax_error = r"% \".+\"  (?:Unknown command.)"
 
     INTERFACE_TYPES = {
-                   "3g": "tunnel",    
-                   "du": "loopback",    
-                   "et": "physical",  
-                   "lo": "loopback",  
-                   "sh": "physical",     
+                   "3g": "tunnel",
+                   "du": "other",
+                   "et": "physical",
+                   "lo": "loopback",
+                   "sh": "physical",
                    "si": "physical",
                    "tu": "tunnel",
                    "vl": "SVI",
                    }
 
+    def setup_script(self, script):
+        if script.parent is None:
+            s_password = script.credentials.get("super_password", "")
+            self.pattern_more = [
+                (r"^--More-- ", " "),
+                (r"^Enable: ",  s_password +"\n")
+            ]
+
     @classmethod
     def get_interface_type(cls, name):
-        return cls.INTERFACE_TYPES.get((name[:2]).lower())      
+        return cls.INTERFACE_TYPES.get((name[:2]).lower())
