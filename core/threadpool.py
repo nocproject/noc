1dff412fa4d5ffe92466514782d2fcbd24f69ada
Dmitry Volodin <dv@nocproject.org>
2017-05-18 11:57:52 +0300

    ThreadPoolExecutor: Locking optimizations and additional metrics
    --HG--
    branch : feature/microservices

--- core/threadpool.py
+++ core/threadpool.py
@@ -7,17 +7,18 @@
 ##----------------------------------------------------------------------
 
 ## Python modules
-import Queue
 import threading
+import thread
 import logging
 import itertools
+import time
+from collections import deque
 ## Third-party modules
 from concurrent.futures import Future
-from atomiclong import AtomicLong
 
 logger = logging.getLogger(__name__)
 
-DEFAULT_IDLE_TIMEOUT = 300
+DEFAULT_IDLE_TIMEOUT = 30
 DEFAULT_SHUTDOWN_TIMEOUT = 60
 
 
@@ -27,18 +28,71 @@ class ThreadPoolExecutor(object):
                  name=None):
         self.max_workers = max_workers
         self.threads = set()
-        self.threads_lock = threading.Lock()
-        self.queue = Queue.Queue()
+        self.mutex = threading.Lock()
+        self.queue = deque()
         self.to_shutdown = False
         self.idle_timeout = idle_timeout or None
         self.shutdown_timeout = shutdown_timeout or None
-        self.idle_workers = AtomicLong(0)
+        self.submitted_tasks = 0
         self.worker_id = itertools.count()
         self.name = name or "threadpool"
         self.done_event = threading.Event()
+        self.started = time.time()
+        self.waiters = []
+
+    def _put(self, item):
+        with self.mutex:
+            if not len(self.waiters) and len(self.threads) < self.max_workers:
+                # Start new thread
+                name = "worker-%s" % self.worker_id.next()
+                t = threading.Thread(target=self.worker, name=name)
+                t.setDaemon(True)
+                self.threads.add(t)
+                t.start()
+            # Enqueue task
+            self.queue.append(item)
+            self.submitted_tasks += 1
+            if self.waiters:
+                e = self.waiters.pop(0)
+                e.release()
+
+    def _get(self, timeout):
+        e = None
+        endtime = None
+        while True:
+            with self.mutex:
+                if self._qsize():
+                    return self.queue.popleft()
+                # Waiting lock
+                if not e:
+                    e = thread.allocate_lock()
+                e.acquire()
+                self.waiters.insert(0, e)
+            # Wait for condition or timeout
+            t = time.time()
+            if not endtime:
+                endtime = t + timeout
+            delay = 0.0005
+            while True:
+                ready = e.acquire(False)
+                if ready:
+                    break
+                remaining = endtime - t
+                if remaining <= 0.0:
+                    try:
+                        self.waiters.remove(e)
+                    except ValueError:
+                        pass
+                    raise IdleTimeout()
+                delay = min(delay * 2, remaining, 0.05)
+                time.sleep(delay)
+                t = time.time()
+
+    def _qsize(self):
+        return len(self.queue)
 
     def set_max_workers(self, max_workers):
-        with self.threads_lock:
+        with self.mutex:
             if max_workers < self.max_workers:
                 # Reduce pool
                 l = len(self.threads)
@@ -48,31 +102,19 @@ class ThreadPoolExecutor(object):
             self.max_workers = max_workers
 
     def stop_one_worker(self):
-        self.queue.put((None, None, None, None))
+        self._put((None, None, None, None))
 
     def submit(self, fn, *args, **kwargs):
         if self.to_shutdown:
             raise RuntimeError(
                 "Cannot schedule new task after shutdown")
         future = Future()
-        self.queue.put((future, fn, args, kwargs))
-        self.adjust_workers()
+        self._put((future, fn, args, kwargs))
         return future
-
-    def adjust_workers(self):
-        with self.threads_lock:
-            while (self.queue.qsize() > self.idle_workers.value and
-                    len(self.threads) < self.max_workers):
-                name = "worker-%s" % self.worker_id.next()
-                t = threading.Thread(target=self.worker, name=name)
-                t.setDaemon(True)
-                self.threads.add(t)
-                self.idle_workers += 1
-                t.start()
 
     def shutdown(self):
         logging.info("Shutdown")
-        with self.threads_lock:
+        with self.mutex:
             self.to_shutdown = True
         for _ in range(len(self.threads)):
             self.stop_one_worker()
@@ -83,20 +125,15 @@ class ThreadPoolExecutor(object):
     def worker(self):
         t = threading.current_thread()
         logger.debug("Starting worker thread %s", t.name)
-        self.idle_workers -= 1
         try:
             while not self.to_shutdown:
                 try:
-                    self.idle_workers += 1
-                    future, fn, args, kwargs = self.queue.get(
-                        block=True,
-                        timeout=self.idle_timeout
+                    future, fn, args, kwargs = self._get(
+                        self.idle_timeout
                     )
-                except Queue.Empty:
+                except IdleTimeout:
                     logger.debug("Closing idle thread")
                     break
-                finally:
-                    self.idle_workers -= 1
                 if not future:
                     logging.debug("Worker %s has no future. Stopping",
                                   t.name)
@@ -105,13 +142,12 @@ class ThreadPoolExecutor(object):
                     continue
                 try:
                     result = fn(*args, **kwargs)
+                    future.set_result(result)
                 except BaseException as e:
                     future.set_exception(e)
-                else:
-                    future.set_result(result)
         finally:
             logger.debug("Stopping worker thread %s", t.name)
-            with self.threads_lock:
+            with self.mutex:
                 self.threads.remove(t)
                 if self.to_shutdown and not len(self.threads):
                     self.done_event.set()
@@ -122,8 +158,8 @@ class ThreadPoolExecutor(object):
         without overflowing thread limits
         :return: 
         """
-        with self.threads_lock:
-            return ((self.queue.qsize() < self.idle_workers.value)
+        with self.mutex:
+            return ((self._qsize() < len(self.waiters))
                     or (self.max_workers > len(self.threads)))
 
     def get_free_workers(self):
@@ -131,11 +167,11 @@ class ThreadPoolExecutor(object):
         Returns amount of available workers for non-blocking submit
         :return: 
         """
-        with self.threads_lock:
+        with self.mutex:
             return (self.max_workers -
                     len(self.threads) -
-                    self.queue.qsize() +
-                    self.idle_workers.value)
+                    self._qsize() +
+                    len(self.waiters))
 
     def apply_metrics(self, d):
         """
@@ -143,12 +179,19 @@ class ThreadPoolExecutor(object):
         :param d: 
         :return: 
         """
-        with self.threads_lock:
+        with self.mutex:
             workers = len(self.threads)
-            idle = self.idle_workers.value
+            idle = len(self.waiters)
             d.update({
                 "%s_max_workers" % self.name: self.max_workers,
                 "%s_workers" % self.name: workers,
                 "%s_idle_workers" % self.name: idle,
-                "%s_running_workers" % self.name: workers - idle
+                "%s_running_workers" % self.name: workers - idle,
+                "%s_submitted_tasks" % self.name: self.submitted_tasks,
+                "%s_queued_jobs" % self.name: len(self.queue),
+                "%s_uptime" % self.name: time.time() - self.started
             })
+
+
+class IdleTimeout(Exception):
+    pass
