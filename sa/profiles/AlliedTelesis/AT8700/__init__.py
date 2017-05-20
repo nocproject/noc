8bcd8ae5a0d803d71f27e345c30cc84127020ae1
vean <none@none>
2017-05-20 15:57:52 +0500

    Add AlliedTelesis.AT8700 profiles
    --HG--
    branch : feature/microservices

---
+++ sa/profiles/AlliedTelesis/AT8700/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Vendor: Allied Telesis
+## OS:     AT8700
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2012 The NOC Project
+## coded by azhur
+## See LICENSE for details
+##----------------------------------------------------------------------
+"""
+"""
+from noc.core.profile.base import BaseProfile
+
+
+class Profile(BaseProfile):
+    name = "AlliedTelesis.AT8700"
+    pattern_more = r"^--More--\s*\(<space> = next page, <CR> = one line, C = continuous, Q = quit\)"
+    command_more = "c"
+    command_submit = "\r"
+    username_submit = "\r"
+    command_save_config = "save configuration"
+    # pattern_prompt = r"^(?P<hostname>[\S\s]+)>"
+    pattern_prompt = r"^(Manager)\s*(?P<hostname>\S+)>"
+    command_exit = "logout"
+
