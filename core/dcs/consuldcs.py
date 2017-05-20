e880676677dbaa6119875930ad512197d91ed3bc
Dmitry Volodin <dv@nocproject.org>
2017-05-20 14:50:48 +0300

    Consul DCS: Fix
    --HG--
    branch : feature/microservices

--- core/dcs/consuldcs.py
+++ core/dcs/consuldcs.py
@@ -123,8 +123,8 @@ class ConsulResolver(ResolverBase):
             if old_index == index:
                 continue  # Timed out
             r = dict(
-                (svc["ID"], "%s:%s" % (svc["Service"]["Address"],
-                                       svc["Service"]["Port"]))
+                (svc["Service"]["ID"], "%s:%s" % (
+                    svc["Service"]["Address"], svc["Service"]["Port"]))
                 for svc in services
             )
             self.set_services(r)
