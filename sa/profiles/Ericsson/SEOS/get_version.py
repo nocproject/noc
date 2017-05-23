1ee7839546c01681add7f42b190c1256f2804085
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2017-05-23 11:24:36 +0300

    Update Ericsson.SEOS profile
    --HG--
    branch : feature/microservices

--- sa/profiles/Ericsson/SEOS/get_version.py
+++ sa/profiles/Ericsson/SEOS/get_version.py
@@ -19,12 +19,12 @@ class Script(BaseScript):
     interface = IGetVersion
 
     rx_ver = re.compile(r"^Active SBL\s+:\s+CXP:\s+(?P<version>\S+.*)\s+"
-                        r"^Passive NPU\s+:\s+CXP:\s+[\S\s]+"
+                        r"^Passive (?:NPU|SBL)\s+:\s+CXP:\s+[\S\s]+"
                         r"^Active BNS\s+:\s+CXCR:\s+(?P<sw_backup>\S+.*)$",
                         re.MULTILINE)
 
     def execute(self):
-        ver = self.cli("show version", cached=True)
+        ver = self.cli_clean("show version", cached=True)
         for match in self.rx_ver.finditer(ver):
             version = match.group("version")
             sw_backup = match.group("sw_backup")
