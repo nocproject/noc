e7c1fc47b1f795e64aafe6a21edbcf741ef35656
Maksim <none@none>
2017-05-23 18:13:15 +0300

    Fix Telindus init.py
    --HG--
    branch : feature/microservices

--- sa/profiles/Telindus/SHDSL/__init__.py
+++ sa/profiles/Telindus/SHDSL/__init__.py
@@ -6,13 +6,10 @@
 ## Copyright (C) 2007-2017 The NOC Project
 ## See LICENSE for details
 ##----------------------------------------------------------------------
-'''
-'''
-
 from noc.core.profile.base import BaseProfile
-import re
 
 class Profile(BaseProfile):
     name = "Telindus.SHDSL"
     username_submit = "\r"
     password_submit = "\r"
+    command_exit = "DISCONNECT"
