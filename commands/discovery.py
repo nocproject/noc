d8694b3520b5d40283fdf4310cf3a4e6b7094478
Dmitry Volodin <dv@nocproject.org>
2017-05-24 17:32:07 +0300

    ./noc discovery: Fix deprecated scheduler method usage
    --HG--
    branch : feature/microservices

--- commands/discovery.py
+++ commands/discovery.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
-##----------------------------------------------------------------------
-## discovery commands
-##----------------------------------------------------------------------
-## Copyright (C) 2007-2016 The NOC Project
-## See LICENSE for details
-##----------------------------------------------------------------------
+# ----------------------------------------------------------------------
+# discovery commands
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2017 The NOC Project
+# See LICENSE for details
+# ----------------------------------------------------------------------
 
-## Python modules
+# Python modules
+from __future__ import print_function
 import argparse
 import time
-## NOC modules
+# NOC modules
 from noc.core.management.base import BaseCommand
 from noc.core.handler import get_handler
 from noc.sa.models.managedobjectselector import ManagedObjectSelector
@@ -92,7 +93,7 @@ class Command(BaseCommand):
             Job.ATTR_KEY: mo.id
         })
         if job_args:
-            self.stdout.write("Job ID: %s\n" % job_args["_id"])
+            self.print("Job ID: %s" % job_args["_id"])
         else:
             job_args = {
                 Job.ATTR_ID: "fakeid",
@@ -102,25 +103,25 @@ class Command(BaseCommand):
         job = get_handler(jcls)(scheduler, job_args)
         if job.context_version:
             ctx_key = job.get_context_cache_key()
-            self.stdout.write("Loading job context from %s\n" % ctx_key)
+            self.print("Loading job context from %s" % ctx_key)
             ctx = cache.get(ctx_key, version=job.context_version)
             if not ctx:
-                self.stdout.write("Job context is empty\n")
+                self.print("Job context is empty")
             job.load_context(ctx)
         job.dereference()
         job.handler()
         if scheduler.service.metrics:
             for m in scheduler.service.metrics:
-                self.stdout.write("Collected metric: %s\n" % m)
+                self.print("Collected metric: %s" % m)
         if job.context_version and job.context:
-            scheduler.run_cache_thread()
-            self.stdout.write("Saving job context to %s\n" % ctx_key)
+            self.print("Saving job context to %s" % ctx_key)
             scheduler.cache_set(
                 key=ctx_key,
                 value=job.context,
                 version=job.context_version
             )
-            time.sleep(2)
+            scheduler.apply_cache_ops()
+            time.sleep(3)
 
 
 class ServiceStub(object):
