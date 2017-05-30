0676af4be9f690e043ac7aa55ec82153769d888c
Andrey Vertiprakhov <aversanta@gmail.com>
2017-05-31 02:37:21 +0500

    Fix in Qtech.QSW2800.get_chassis_id
    --HG--
    branch : feature/microservices

--- sa/profiles/Qtech/QSW2800/get_chassis_id.py
+++ sa/profiles/Qtech/QSW2800/get_chassis_id.py
@@ -19,29 +19,34 @@ class Script(BaseScript):
     cache = True
 
     rx_mac = re.compile(r"^\s*\S+\s+MAC\s+(?P<mac>\S+)$",
-        re.MULTILINE | re.IGNORECASE)
+                        re.MULTILINE | re.IGNORECASE)
     rx_mac_old = re.compile(r"^\d+\s+(?P<mac>\S+)\s+\S+\s+\S+\s+CPU$",
-        re.MULTILINE | re.IGNORECASE)
+                            re.MULTILINE | re.IGNORECASE)
 
     def execute(self):
+        r = []
         v = self.scripts.get_version()
-        if v["version"].startswith("7") \
-        or self.match_version(version__gte="6.3.100.12"):
+        if v["version"].startswith("7") or self.match_version(version__gte="6.3.100.12"):
             macs = []
             cmd = self.cli("show version")
             for match in self.rx_mac.finditer(cmd):
                 macs += [match.group("mac")]
             macs.sort()
-            return [{
+            r = [{
                 "first_chassis_mac": f,
                 "last_chassis_mac": t
             } for f, t in self.macs_to_ranges(macs)]
-        else:
-            cmd = self.cli("show mac-address-table static | i CPU")
-            match = self.rx_mac_old.match(cmd)
+        if not r:
+            # If not SystemID in version command
+            vlan_cmd = self.cli("show mac-address-table static | i CPU")
+            match = self.rx_mac_old.match(vlan_cmd)
             if match:
-                mac = match.group("mac")
+                vlan_mac = match.group("mac")
+            iface_cmd = self.cli("show interface | i address is")
+            iface_match = iface_cmd.splitlines()[0].split()[-1]
             return {
-                "first_chassis_mac": mac,
-                "last_chassis_mac": mac
+                "first_chassis_mac": vlan_mac,
+                "last_chassis_mac": iface_match
             }
+
+        return r
