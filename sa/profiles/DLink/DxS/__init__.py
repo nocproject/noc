70e79e7dbfc55431551341e878036643c0701a5f
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2017-05-30 17:48:54 +0300

    Fix DLink.DxS script
    --HG--
    branch : feature/microservices

--- sa/profiles/DLink/DxS/__init__.py
+++ sa/profiles/DLink/DxS/__init__.py
@@ -3,7 +3,7 @@
 # Vendor: D-Link
 # OS:     DxS
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2016 The NOC Project
+# Copyright (C) 2007-2017 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
@@ -156,7 +156,7 @@ class Profile(BaseProfile):
         r"(?P<addr_learning>Enabled|Disabled)\s*"
         r"((?P<trap_state>Enabled|Disabled)\s*)?"
         r"((?P<asd>\-)\s*)?"
-        r"(\n\s+(?P<mdix>Auto|MDI|MDIX|Cross|\-)\s*)?"
+        r"(\n\s+(?P<mdix>Auto|MDI|MDIX|Cross|Normal|\-)\s*)?"
         r"(\n\s*Desc(ription)?:\s*?(?P<desc>.*?))?$",
         re.MULTILINE)
 
