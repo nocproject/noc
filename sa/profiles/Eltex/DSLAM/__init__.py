63c7fba5c541f4215d9167fa51ddbc16f60003fa
Maksim <none@none>
2017-06-16 12:47:43 +0300

    Fix Eltex.DSLAM get_interfaces
    --HG--
    branch : feature/microservices

--- sa/profiles/Eltex/DSLAM/__init__.py
+++ sa/profiles/Eltex/DSLAM/__init__.py
@@ -6,8 +6,8 @@
 # Copyright (C) 2007-2017 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
-
 # Python modules
+import re
 # NOC modules
 from noc.core.profile.base import BaseProfile
 
@@ -30,3 +30,41 @@ class Profile(BaseProfile):
     command_enter_config = "configure"
     command_leave_config = "exit"
     command_save_config = "save"
+    rx_header = re.compile("(\S+)")
+
+    def iter_items(self, s):
+        def iter_lines(s):
+            lines = s.splitlines()
+            # Skip until empty line
+            if "" in lines:
+                i = lines.index("") + 1
+            else:
+                i = 0
+            ll = len(lines) - 2
+            while i <= ll:
+                yield lines[i], lines[i + 1]
+                i += 3
+
+        def parse_line(key, value):
+            pos = [match.start() for match in self.rx_header.finditer(key)]
+            ranges = [(s, e) for s, e in zip(pos, pos[1:])]
+            if not ranges:
+                return {}
+            ranges += [(ranges[-1][1], None)]
+            d = {}
+            for s, e in ranges:
+                d[key[s:e].strip()] = value[s:e].strip()
+            return d
+
+        d = {}
+        for kl, vl in iter_lines(s):
+            if not vl or not kl:
+                continue
+            if kl[0] == " ":
+                d.update(parse_line(kl, vl))
+            else:
+                if d:
+                    yield d
+                d = parse_line(kl, vl)
+        if d:
+            yield d
