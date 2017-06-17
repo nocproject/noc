25d777fd3b635e710f9ba849488ac9488a899730
Maksim <none@none>
2017-06-17 17:29:43 +0300

    Fix Proscend.SHDSL get_interfaces
    --HG--
    branch : feature/microservices

---
+++ sa/profiles/Proscend/SHDSL/get_interfaces.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+##----------------------------------------------------------------------
+## Proscend.SHDSL.get_interfaces
+##----------------------------------------------------------------------
+## Copyright (C) 2007-2017 The NOC Project
+## See LICENSE for details
+##----------------------------------------------------------------------
+## Python modules
+import re
+## NOC modules
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetinterfaces import IGetInterfaces
+from noc.sa.interfaces.base import MACAddressParameter
+
+class Script(BaseScript):
+    name = "Proscend.SHDSL.get_interfaces"
+    interface = IGetInterfaces
+    cache=True
+
+    INTERFACE_TYPES = {
+
+            "lan": "physical",  # No comment
+            "mgm": "unknown",  # No comment
+            "loo": "loopback",  # No comment
+            "shd": "physical",  # No comment
+        }
+    @classmethod
+    def get_interface_type(cls, name):
+         c = cls.INTERFACE_TYPES.get(name[:3].lower())
+         return c
+
+    def execute(self):
+        interfaces = []
+        # Try SNMP first
+        if self.has_snmp():
+            try:
+                for v in self.snmp.getnext("1.3.6.1.2.1.2.2.1.1", cached=True):
+                    i = v[1]
+                    name = self.snmp.get("1.3.6.1.2.1.2.2.1.2." + str(i))
+                    iftype = self.get_interface_type(name)
+                    if not name:
+                        self.logger.info(
+                            "Ignoring unknown interface type: '%s", iftype
+                        )
+                        continue
+                    s = self.snmp.get("1.3.6.1.2.1.2.2.1.6."+ str(i))
+                    if s:
+                        mac = MACAddressParameter().clean(s)
+                    astat = self.snmp.get("1.3.6.1.2.1.2.2.1.7."+ str(i))
+                    if astat == 1:
+                        a_stat = True
+                    else:
+                        a_stat = False
+                    ostat = self.snmp.get("1.3.6.1.2.1.2.2.1.8."+ str(i))
+                    if ostat == 1:
+                        o_stat = True
+                    else:
+                        o_stat = False
+                    #print repr("%s\n" % admin_status)
+                    interfaces += [{
+                            "type": iftype,
+                            "name": name.replace("softwareLoopback", "lo"),
+                            "mac": mac,
+                            "admin_status": a_stat,
+                            "oper_status": o_stat,
+                            "ifindex": i,
+                            "subinterfaces": [{
+                                "name": name.replace("softwareLoopback", "lo"),
+                                "admin_status": a_stat,
+                                "oper_status": o_stat,
+                                "mac": mac,
+                                "ifindex": i,
+                                "enabled_afi": ["BRIDGE"]
+                            }]
+                        }]
+                return [{"interfaces": interfaces}]    
+            except self.snmp.TimeOutError:
+                pass
