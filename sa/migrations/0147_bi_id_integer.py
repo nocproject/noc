8285c3e9752a1e9b0d274637194a0732306f34ab
Andrey Vertiprakhov <aversanta@gmail.com>
2017-06-22 15:16:40 +0500

    Rollback BI ID migrations to Decimal
    --HG--
    branch : feature/microservices

---
+++ sa/migrations/0147_bi_id_integer.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from south.db import db
+from django.db import models
+
+
+class Migration:
+    def forwards(self):
+        db.execute("ALTER TABLE sa_managedobject ALTER COLUMN bi_id TYPE int")
+        db.execute("ALTER TABLE sa_administrativedomain ALTER COLUMN bi_id TYPE int")
+        db.execute("ALTER TABLE sa_authprofile ALTER COLUMN bi_id TYPE int")
+        db.execute("ALTER TABLE sa_terminationgroup ALTER COLUMN bi_id TYPE int")
+        db.execute("ALTER TABLE sa_managedobjectprofile ALTER COLUMN bi_id TYPE int")
+
+    def backwards(self):
+        db.execute("ALTER TABLE sa_managedobject ALTER COLUMN bi_id TYPE decimal(20,0)")
+        db.execute("ALTER TABLE sa_administrativedomain ALTER COLUMN bi_id TYPE decimal(20,0)")
+        db.execute("ALTER TABLE sa_authprofile ALTER COLUMN bi_id TYPE decimal(20,0)")
+        db.execute("ALTER TABLE sa_terminationgroup ALTER COLUMN bi_id TYPE decimal(20,0)")
+        db.execute("ALTER TABLE sa_managedobjectprofile ALTER COLUMN bi_id TYPE decimal(20,0)")
