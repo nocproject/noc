77806b1ff487c592d9bc4bd22083e6b2e721448a
vean <none@none>
2017-06-23 17:27:09 +0500

    Fix bi_id rollback
    --HG--
    branch : feature/microservices

--- sa/migrations/0147_bi_id_integer.py
+++ sa/migrations/0147_bi_id_integer.py
@@ -5,15 +5,25 @@ from django.db import models
 
 class Migration:
     def forwards(self):
-        db.execute("ALTER TABLE sa_managedobject ALTER COLUMN bi_id TYPE int")
-        db.execute("ALTER TABLE sa_administrativedomain ALTER COLUMN bi_id TYPE int")
-        db.execute("ALTER TABLE sa_authprofile ALTER COLUMN bi_id TYPE int")
-        db.execute("ALTER TABLE sa_terminationgroup ALTER COLUMN bi_id TYPE int")
-        db.execute("ALTER TABLE sa_managedobjectprofile ALTER COLUMN bi_id TYPE int")
+        db.execute("UPDATE sa_managedobject SET bi_id=NULL")
+        db.execute("ALTER TABLE sa_managedobject ALTER COLUMN bi_id TYPE bigint")
+        db.execute("UPDATE sa_administrativedomain SET bi_id=NULL")
+        db.execute("ALTER TABLE sa_administrativedomain ALTER COLUMN bi_id TYPE bigint")
+        db.execute("UPDATE sa_authprofile SET bi_id=NULL")
+        db.execute("ALTER TABLE sa_authprofile ALTER COLUMN bi_id TYPE bigint")
+        db.execute("UPDATE sa_terminationgroup SET bi_id=NULL")
+        db.execute("ALTER TABLE sa_terminationgroup ALTER COLUMN bi_id TYPE bigint")
+        db.execute("UPDATE sa_managedobjectprofile SET bi_id=NULL")
+        db.execute("ALTER TABLE sa_managedobjectprofile ALTER COLUMN bi_id TYPE bigint")
 
     def backwards(self):
+        db.execute("UPDATE sa_managedobject SET bi_id=NULL")
         db.execute("ALTER TABLE sa_managedobject ALTER COLUMN bi_id TYPE decimal(20,0)")
+        db.execute("UPDATE sa_administrativedomain SET bi_id=NULL")
         db.execute("ALTER TABLE sa_administrativedomain ALTER COLUMN bi_id TYPE decimal(20,0)")
+        db.execute("UPDATE sa_authprofile SET bi_id=NULL")
         db.execute("ALTER TABLE sa_authprofile ALTER COLUMN bi_id TYPE decimal(20,0)")
+        db.execute("UPDATE sa_terminationgroup SET bi_id=NULL")
         db.execute("ALTER TABLE sa_terminationgroup ALTER COLUMN bi_id TYPE decimal(20,0)")
+        db.execute("UPDATE sa_managedobjectprofile SET bi_id=NULL")
         db.execute("ALTER TABLE sa_managedobjectprofile ALTER COLUMN bi_id TYPE decimal(20,0)")
