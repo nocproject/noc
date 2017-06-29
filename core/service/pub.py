bbc8b994e1ae23f371c145ceb642334711e1a7bc
Dmitry Volodin <dv@nocproject.org>
2017-06-29 21:05:22 +0300

    synchronous nsq publishing via intenal http client
    --HG--
    branch : feature/microservices

--- core/service/pub.py
+++ core/service/pub.py
@@ -3,7 +3,7 @@
 # ----------------------------------------------------------------------
 # NSQ Publish API
 # ----------------------------------------------------------------------
-# Copyright (C) 2007-2016 The NOC Project
+# Copyright (C) 2007-2017 The NOC Project
 # See LICENSE for details
 # ----------------------------------------------------------------------
 
@@ -11,7 +11,8 @@
 import logging
 # Third-party modules
 import ujson
-import pycurl
+# NOC modules
+from noc.core.http.client import fetch_sync
 
 
 logger = logging.getLogger(__name__)
@@ -20,28 +21,18 @@ NSQD_URL = "http://127.0.0.1:4151/"
 NSQD_PUB_URL = NSQD_URL + "pub"
 NSQD_MPUB_URL = NSQD_URL + "mpub"
 
-
-def _post(url, body):
-    c = pycurl.Curl()
-    c.setopt(c.URL, url)
-    c.setopt(c.POST, 1)
-    c.setopt(c.POSTFIELDS, body)
-    c.setopt(c.NOPROXY, "*")
-    # c.setopt(c.RESOLVE, ["%s:%s" % (l, l.split(":")[0])])
-    c.setopt(c.TIMEOUT, 60)
-    c.setopt(c.CONNECTTIMEOUT, 10)
-    try:
-        c.perform()
-    except pycurl.error as e:
-        raise Exception(str(e))
-    finally:
-        c.close()
+NSQD_CONNECT_TIMEOUT = 3
+NSQD_REQUEST_TIMEOUT = 30
 
 
 def pub(topic, data):
     logger.debug("Publish to topic %s", topic)
-    data = ujson.dumps(data)
-    _post(
-        "%s?topic=%s" % (NSQD_PUB_URL, topic),
-        data
+    code, headers, body = fetch_sync(
+            "%s?topic=%s" % (NSQD_PUB_URL, topic),
+            method="POST",
+            body=ujson.dumps(data),
+            connect_timeout=NSQD_CONNECT_TIMEOUT,
+            request_timeout=NSQD_REQUEST_TIMEOUT
     )
+    if code != 200:
+        raise Exception("Cannot publish: %s %s" % (code, body))
