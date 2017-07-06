bf7ae1833909ff99908b021c23552244eb5644a0
Dmitry Volodin <dv@nocproject.org>
2017-07-06 11:23:56 +0300

    service: Fix subscribe
    --HG--
    branch : feature/config

--- core/service/base.py
+++ core/service/base.py
@@ -643,14 +643,14 @@ class Service(object):
         metric_decode_fail = "nsq_msg_decode_fail_%s" % t
         metric_processed = "nsq_msg_processed_%s" % t
         metric_deferred = "nsq_msg_deferred_%s" % t
-        lookupd = config.nsqlookupd.addresses
+        lookupd = [str(a) for a in config.nsqlookupd.addresses]
         self.logger.info("Subscribing to %s/%s (lookupd: %s)",
                          topic, channel, ", ".join(lookupd))
         self.nsq_readers[handler] = NSQReader(
             message_handler=call_raw_handler if raw else call_json_handler,
             topic=topic,
             channel=channel,
-            lookupd_http_addresses=config.nsqlookupd.addresses,
+            lookupd_http_addresses=lookupd,
             **kwargs
         )
 
