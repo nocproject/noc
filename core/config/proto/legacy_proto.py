7f613d4d43ea77dc22debf99077504acad685870
Dmitry Volodin <dv@nocproject.org>
2017-07-07 18:01:10 +0300

    config: Fix legacy protocol
    --HG--
    branch : feature/config

--- core/config/proto/legacy_proto.py
+++ core/config/proto/legacy_proto.py
@@ -67,22 +67,15 @@ class LegacyProtocol(BaseProtocol):
                     d = d[p]
                 return d
 
-        def set_path(d, key, value):
-            parts = key.split(".")
-            for p in parts[:-1]:
-                if p not in d:
-                    d[p] = {}
-                d = d[p]
-            d[parts[-1]] = value
-
         svc_pool = os.environ.get("NOC_POOL", "")
         svc_node = os.environ.get("NOC_NODE", "")
 
-        cfg = {}
         with open(self.PATH) as f:
             data = yaml.load(f)["config"]
         for legacy_key in self.NOC_MAPPINGS:
             v = get_path(data, legacy_key)
             if v is not None:
-                set_path(cfg, self.NOC_MAPPINGS[legacy_key], v)
-        self.config.update(cfg)
+                self.config.set_parameter(
+                    self.NOC_MAPPINGS[legacy_key],
+                    v
+                )
