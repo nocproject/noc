4275a4de580eb551a6bf50e25465353eef87b2d8
Dmitry Volodin <dv@nocproject.org>
2017-07-07 18:31:32 +0300

    config: Fix legacy loading
    --HG--
    branch : feature/config

--- core/config/proto/legacy_proto.py
+++ core/config/proto/legacy_proto.py
@@ -46,35 +46,21 @@ class LegacyProtocol(BaseProtocol):
     }
 
     def load(self):
-        def get_path(d, key):
+        def get_path(conf, key):
+            d = conf
             parts = key.split(".")
-            if parts[0] == "noc" or "-" not in parts[0]:
-                # Global config
-                for p in parts:
-                    if p not in d:
-                        return None
-                    d = d[p]
-                return d
-            else:
-                # Service config
-                selector = "%s-%s-%s" % (parts[0], svc_pool, svc_node)
-                if selector not in d:
-                    return None
-                d = d[selector]
-                for p in parts[1:]:
-                    if p not in d:
-                        return None
-                    d = d[p]
-                return d
-
-        svc_pool = os.environ.get("NOC_POOL", "")
-        svc_node = os.environ.get("NOC_NODE", "")
+            for p in parts:
+                d = d.get(p)
+                if d is None:
+                    break
+            return d
 
         with open(self.PATH) as f:
             data = yaml.load(f)["config"]
         for legacy_key in self.NOC_MAPPINGS:
             v = get_path(data, legacy_key)
             if v is not None:
+                print legacy_key, self.NOC_MAPPINGS[legacy_key], v
                 self.config.set_parameter(
                     self.NOC_MAPPINGS[legacy_key],
                     v
