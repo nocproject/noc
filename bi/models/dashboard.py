1af3b1eb99442170ff76683ba7c62120027ba15e
Andrey Vertiprakhov <aversanta@gmail.com>
2017-07-10 08:44:08 +0500

    Fix BI User/Group permissions API
    --HG--
    branch : feature/microservices

--- bi/models/dashboard.py
+++ bi/models/dashboard.py
@@ -75,3 +75,52 @@ class Dashboard(Document):
             if level == DAL_ADMIN:
                     return level
         return level
+
+    def save(self, force_insert=False, validate=True, clean=True,
+             write_concern=None,  cascade=None, cascade_kwargs=None,
+             _refs=None, save_condition=None, **kwargs):
+        # Split DashBoard Acces to {User, level}, {Group, level}
+        # self.update(add_to_set__access=[parent_1, parent_2, parent_1])
+        if "access" in getattr(self, '_changed_fields', []):
+            # Check unique
+            processed = []
+            access = []
+            for da in sorted(self.access, reverse=True):
+                # Deduplicate rights
+                # @todo changing priority (reverse order)
+                if da.user and "u%d" % da.user.id in processed:
+                    continue
+                elif da.group and "g%d" % da.group.id in processed:
+                    continue
+                if da.user and da.group:
+                    # Split User and Group rights
+                    access += [DashboardAccess(user=da.user.id, level=da.level),
+                               DashboardAccess(group=da.group.id, level=da.level)]
+                    processed += ["u%d" % da.user.id, "g%d" % da.group.id]
+                    continue
+                access += [da]
+                if da.user:
+                    processed += ["u%d" % da.user.id]
+                if da.group:
+                    processed += ["g%d" % da.group.id]
+            self.access = access
+
+        super(Dashboard, self).save(force_insert=force_insert, validate=validate, clean=clean,
+                                    write_concern=write_concern,  cascade=cascade, cascade_kwargs=cascade_kwargs,
+                                    _refs=_refs, save_condition=save_condition, **kwargs)
+
+    def clean_access(self, item=None):
+        """
+        Clean access rights
+        update2 = {"$push": {"access": {"$each": [{"user": i.user.id, "level": i.level} for i in items]}}}
+        :param item: All, user, group
+        :return:
+        """
+        match = {"_id": self.id}
+        if item == "user":
+            update = {"$pull": {"access": {"user": {"$exists": True}}}}
+        elif item == "group":
+            update = {"$pull": {"access": {"group": {"$exists": True}}}}
+        else:
+            update = {"$pull": "access"}
+        self._get_collection().update(match, update)
