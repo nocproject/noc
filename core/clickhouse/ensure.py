42d18ee332ef1119cf06fba1ca81195ebb611fe2
Dmitry Volodin <dv@nocproject.org>
2017-07-10 09:08:37 +0300

    MetricScope: Create and modify database tables
    --HG--
    branch : feature/chmetrics

---
+++ core/clickhouse/ensure.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------
+# Ensure ClickHouse database schema
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2017 The NOC Project
+# See LICENSE for details
+# ----------------------------------------------------------------------
+
+# Python modules
+import os
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def ensure_bi_models():
+    from noc.core.clickhouse.model import Model
+
+    logger.info("Ensuring BI models:")
+    models = set()
+    # Get models
+    for f in os.listdir("bi/models"):
+        if f.startswith("_") or not f.endswith(".py"):
+            continue
+        mn = f[:-3]
+        model = Model.get_model_class(mn)
+        if model:
+            models.add(model)
+    # Ensure fields
+    changed = False
+    for model in models:
+        logger.info("Ensure table %s" % model._meta.db_table)
+        changed |= model.ensure_table()
+    return changed
+
+
+def ensure_pm_scopes():
+    from noc.pm.models.metricscope import MetricScope
+    logger.info("Ensuring PM scopes")
+    changed = False
+    for s in MetricScope.objects.all():
+        logger.info("Ensure scope %s" % s.table_name)
+        changed |= s.ensure_table()
+    return changed
