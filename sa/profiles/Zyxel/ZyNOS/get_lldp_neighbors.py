c94763167adc8f63a3c8267f85c22db0e485442e
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2017-07-10 15:34:31 +0300

    pep8 formatting
    --HG--
    branch : feature/microservices

--- sa/profiles/Zyxel/ZyNOS/get_lldp_neighbors.py
+++ sa/profiles/Zyxel/ZyNOS/get_lldp_neighbors.py
@@ -6,11 +6,11 @@
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
-#NOC modules
+# NOC modules
 from noc.core.script.base import BaseScript
 from noc.sa.interfaces.igetlldpneighbors import IGetLLDPNeighbors
-from noc.lib.validators import is_int, is_ipv4
-#Python standard modules
+from noc.lib.validators import is_int, is_ipv4, is_mac
+# Python standard modules
 import re
 
 
@@ -21,7 +21,8 @@ class Script(BaseScript):
     rx_summary_split = re.compile(
         r"^LocalPort.+?\n", re.MULTILINE | re.IGNORECASE)
     rx_s_line = re.compile(r"(?P<local_if>\d+)\s+[0-9a-f:]+\s+.+?$")
-    rx_remote_port = re.compile("^\s*Port id:(?P<remote_if>.*)",
+    rx_remote_port = re.compile(
+        "^\s*Port id:(?P<remote_if>.*)",
         re.MULTILINE | re.IGNORECASE)
     rx_remote_port_desc = re.compile(
         "^\s*Port Description:(?P<remote_if_desc>.*)",
@@ -32,18 +33,18 @@ class Script(BaseScript):
     rx_chassis_id = re.compile(
         r"^\s*Chassis id:\s*(?P<id>\S+)", re.MULTILINE | re.IGNORECASE)
     rx_enabled_caps = re.compile(
-        "^\s*System Capabilities Enabled:\s*(?P<caps>((other|repeater|bridge|router|wlan-access-point|telephone|docsis-cable-device|station-only)\s+)+)\s*$",
+        "^\s*System Capabilities Enabled:\s*"
+        r"(?P<caps>((other|repeater|bridge|router|wlan-access-point"
+        r"|telephone|docsis-cable-device|station-only)\s+)+)\s*$",
         re.MULTILINE | re.IGNORECASE)
     rx_system = re.compile(
         r"^\s*System Name:\s*(?P<name>\S+)", re.MULTILINE | re.IGNORECASE)
     rx_system_desc = re.compile(
         r"^\s*System Description:\s*(?P<desc>.*)",
         re.MULTILINE | re.IGNORECASE)
-    rx_mac = re.compile(
-        r"^[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}$")
 
     def execute(self):
-        r=[]
+        r = []
         try:
             v = self.cli("sh lldp info remote")
         except self.CLISyntaxError:
@@ -52,7 +53,7 @@ class Script(BaseScript):
         v = self.rx_summary_split.split(v)[1]
         lldp_interfaces = []
 
-        #Get lldp interfaces
+        # Get lldp interfaces
         for l in v.splitlines():
             l = l.strip()
             if not l:
@@ -62,24 +63,26 @@ class Script(BaseScript):
                 continue
             lldp_interfaces += [match.group('local_if')]
 
-        #Get lldp neighbors
+        # Get lldp neighbors
         for local_if in lldp_interfaces:
             i = {
                 "local_interface": local_if,
                 "neighbors": []
             }
 
-            #Get neighbor details
+            # Get neighbor details
             try:
-                v = self.cli("sh lldp info remote interface port-channel %s" % local_if)
+                v = self.cli(
+                    "sh lldp info remote interface port-channel %s" % local_if
+                )
             except self.CLISyntaxError:
                 raise self.NotSupportedError()
 
-            #Get remote port
+            # Get remote port
             match = self.re_search(self.rx_remote_port, v)
             remote_port = match.group("remote_if").strip()
 
-            #match = self.re_search(self.rx_remote_port_desc, v)
+            # match = self.re_search(self.rx_remote_port_desc, v)
             match = self.rx_remote_port_desc.search(v)
             if match:
                 remote_port_desc = match.group('remote_if_desc').strip()
@@ -88,16 +91,17 @@ class Script(BaseScript):
 
             match = self.rx_remote_port_subtype.search(v)
             if match:
-                remote_port_subtype_str = match.group('remote_if_subtype').strip()
+                remote_port_subtype_str = \
+                    match.group('remote_if_subtype').strip()
             else:
                 remote_port_subtype_str = ''
 
-            #Get remote port subtype from "Port ID Subtype" field
+            # Get remote port subtype from "Port ID Subtype" field
             if remote_port_subtype_str == 'local-assigned':
-                remote_port_subtype = 7 #Local
+                remote_port_subtype = 7  # Local
             else:
                 remote_port_subtype = 5
-                if self.rx_mac.match(remote_port):
+                if is_mac(remote_port):
                     # Actually macAddress(3)
                     remote_port_subtype = 3
                 elif is_ipv4(remote_port):
@@ -116,13 +120,13 @@ class Script(BaseScript):
             if remote_port_desc:
                 n["remote_port_description"] = remote_port_desc
 
-            #Get Chassis ID
+            # Get Chassis ID
             match = self.rx_chassis_id.search(v)
             if not match:
                 continue
             n["remote_chassis_id"] = match.group("id")
 
-            #Get capabilities
+            # Get capabilities
             cap = 0
             match = self.rx_enabled_caps.search(v)
             if match:
@@ -131,12 +135,13 @@ class Script(BaseScript):
                     if c:
                         cap |= {
                             "other": 1, "repeater": 2, "bridge": 4,
-                            "wlan-access-point": 8, "router": 16, "telephone": 32,
-                            "docsis-cable-device": 64, "station-only": 128
+                            "wlan-access-point": 8, "router": 16,
+                            "telephone": 32, "docsis-cable-device": 64,
+                            "station-only": 128
                         }[c.lower()]
             n["remote_capabilities"] = cap
 
-            #Get system name
+            # Get system name
             match = self.rx_system.search(v)
             if match:
                 n["remote_system_name"] = match.group("name")
