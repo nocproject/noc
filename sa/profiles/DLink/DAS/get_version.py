73a5ba660c587e5ec93e621e1b7247bfbcfaf304
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2017-08-08 17:02:33 +0300

    Fix DLink.DAS.get_version script
    --HG--
    branch : feature/microservices

--- sa/profiles/DLink/DAS/get_version.py
+++ sa/profiles/DLink/DAS/get_version.py
@@ -23,12 +23,20 @@ class Script(BaseScript):
         r"^Object-id\s+: (?P<sys_oid>\S+)\s*\n"
         r"^Up Time\(HH:MM:SS\)\s+: .+\n"
         r"^HwVersion\s+: (?P<hardware>\S+)\s*\n"
-        r"^CPLDVersion\s+: .+\n"
+        r"(^CPLDVersion\s+: .+\n)?"
         r"^CPSwVersion\s+: .+\n"
-        r"^CPSwVersion\(Build\): (?P<version>\S+)",
+        r"^CPSwVersion\(Build\): (?P<version>\S+).*\n"
+        r"^DPSwVersion\s+: .+\n",
         re.MULTILINE
     )
-
+    rx_ver2 = re.compile(
+        r"^Object-id\s+: (?P<sys_oid>\S+)\s*\n"
+        r"^Up Time\(HH:MM:SS\)\s+: .+\n"
+        r"^HwVersion\s+: (?P<hardware>\S+)\s*\n"
+        r"^CPSwVersion\s+: (?P<version>\S+)\s*\n"
+        r"^DPSwVersion\s+: .+",
+        re.MULTILINE
+    )
     OID_TABLE = {
         "1.3.6.1.4.1.171.10.65.1": "DAS-32xx",
         "1.3.6.1.4.1.3646.1300.11": "DAS-3248-DC",
@@ -46,6 +54,8 @@ class Script(BaseScript):
         match = self.rx_descr.search(v)
         platform = match.group("platform")
         match = self.rx_ver.search(v)
+        if not match:
+            match = self.rx_ver2.search(v)
         if not platform.startswith("DAS-"):
             platform = self.OID_TABLE[match.group("sys_oid")]
         return {
