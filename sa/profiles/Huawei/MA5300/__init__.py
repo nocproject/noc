2c4403a1295fe5e4ce9ee5e79f5fe780ec111a4d
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2017-08-16 16:58:39 +0300

    Fix Huawei.MA5300 profile
    --HG--
    branch : feature/microservices

--- sa/profiles/Huawei/MA5300/__init__.py
+++ sa/profiles/Huawei/MA5300/__init__.py
@@ -32,6 +32,16 @@ class Profile(BaseProfile):
     pattern_unpriveleged_prompt = \
         r"^(?P<hostname>[a-zA-Z0-9-_\.\/()]+)(?:-[a-zA-Z0-9/]+)*>$"
     pattern_syntax_error = \
-        r"(% Unknown command, the error locates at \'^\'|  Logged Fail!)"
+        r"(% Unknown command, the error locates at \'^\'|  Logged Fail!|"
+        r"System is busy, please try after a while)"
+    rogue_chars = [
+        re.compile(r"\n\r\s+Line \d+ operating, attempt of the Line -\d+ denied!\n\r"),
+        "\r"
+    ]
 
-#  Logged Fail!
+    def setup_session(self, script):
+        script.cli("config", ignore_errors=True)
+        script.cli("terminal type vt100", ignore_errors=True)
+        script.cli("history size 0", ignore_errors=True)
+        script.cli("length 0", cached=True, ignore_errors=True)
+        script.cli("cls", ignore_errors=True)
