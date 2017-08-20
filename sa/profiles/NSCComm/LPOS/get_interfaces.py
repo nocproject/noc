3a9b79be2733340db4496da15660d914dedeeffb
Maksim13 <none@none>
2017-08-20 14:07:05 +0300

    Fix NSCComm get_interfaces
    --HG--
    branch : feature/microservices

--- sa/profiles/NSCComm/LPOS/get_interfaces.py
+++ sa/profiles/NSCComm/LPOS/get_interfaces.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# - coding: utf-8 -*-
 # ---------------------------------------------------------------------
 # NSCComm.LPOS.get_interfaces
 # ---------------------------------------------------------------------
@@ -29,7 +29,14 @@ class Script(BaseScript):
     rx_vlan = re.compile(
         r"^vlan id (?P<vlan_id>\d+).*\n"
         r"^name\s*: (?P<vlan_name>.+)\s*\n"
-        r"^member of ports\s*: (?P<ports>.+)\s*\n",
+        r"^member of ports\s*: (?P<ports>.+)\s*",
+        re.MULTILINE
+    )
+    rx_vlan2 = re.compile(
+        r"^vlan id (?P<vlan_id>\d+).*\n"
+        r"^name\s*: (?P<vlan_name>.+)\s*\n"
+        r"^untagged on ports:\s+(?P<uport>.+)\s*"
+        r"^tagged on ports\s*:\s+(?P<tports>.+)\s*",
         re.MULTILINE
     )
     def execute(self):
@@ -51,11 +58,23 @@ class Script(BaseScript):
             vlan_id = v["vlan_id"]
             c = self.cli("vlan %s -s" % vlan_id)
             match = self.rx_vlan.search(c)
-            for port in match.group("ports").split(","):
-                for i in interfaces:
-                    if port == i["name"]:
-                        i["subinterfaces"][0]["tagged_vlans"] += [vlan_id]
-                        break
+            if match:
+                for port in match.group("ports").split(","):
+                    for i in interfaces:
+                        if port == i["name"]:
+                            i["subinterfaces"][0]["tagged_vlans"] += [vlan_id]
+                            break
+            else:
+                match = self.rx_vlan2.search(c)
+                for uport in match.group("uport").split(","):
+                    for i in interfaces:
+                        if uport and uport == i["name"]:
+                            i["subinterfaces"][0]["untagged_vlan"] = vlan_id
+                for tports in match.group("tports").split(","):
+                    for i in interfaces:
+                        if tports == i["name"]:
+                            i["subinterfaces"][0]["tagged_vlans"] += [vlan_id]
+                            break
         v = self.cli("ipconfig")
         match = self.rx_ip.search(v)
         ip_address = match.group("ip_address")
