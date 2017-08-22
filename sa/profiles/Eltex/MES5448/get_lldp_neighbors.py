3da20d95bfcdc4c402f5962be07d4b1717de662f
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2017-08-22 09:55:28 +0300

    Fix Eltex.MES5448.get_lldp_neighbors script
    --HG--
    branch : feature/microservices

--- sa/profiles/Eltex/MES5448/get_lldp_neighbors.py
+++ sa/profiles/Eltex/MES5448/get_lldp_neighbors.py
@@ -21,26 +21,28 @@ class Script(BaseScript):
     interface = IGetLLDPNeighbors
 
     rx_detail = re.compile(
-        r"^Chassis ID Subtype: (?P<chassis_id_subtype>.+)\n"
-        r"^Chassis ID: (?P<chassis_id>.+)\n"
-        r"^Port ID Subtype: (?P<port_id_subtype>.+)\n"
-        r"^Port ID: (?P<port_id>.+)\n"
-        r"^System Name:(?P<system_name>.*)\n"
-        r"^System Description:(?P<system_description>.*)\n"
-        r"^Port Description:(?P<port_description>.*)\n"
-        r"^System Capabilities Supported:.*\n"
+        r"^Chassis ID Subtype: (?P<chassis_id_subtype>.+?)\n"
+        r"^Chassis ID: (?P<chassis_id>.+?)\n"
+        r"^Port ID Subtype: (?P<port_id_subtype>.+?)\n"
+        r"^Port ID: (?P<port_id>.+?)\n"
+        r"^System Name:(?P<system_name>.*?)\n"
+        r"^System Description:(?P<system_description>.*?)\n"
+        r"^Port Description:(?P<port_description>.*?)\n"
+        r"^System Capabilities Supported:.*?\n"
         r"^System Capabilities Enabled:(?P<caps>.*?)\n",
         re.MULTILINE | re.DOTALL
     )
     CAPS_MAP = {
         "repeater": 2,
         "bridge": 4,
+        "WLAN access point": 8,
         "router": 16
     }
     CHASSIS_SUBTYPE = {
         "MAC Address": 4
     }
     PORT_SUBTYPE = {
+        "Interface Alias": 1,
         "MAC Address": 3,
         "Interface Name": 5,
         "Local": 7
@@ -52,36 +54,35 @@ class Script(BaseScript):
             if not i[1]:
                 continue
             c = self.cli("show lldp remote-device detail %s" % i[0])
-            match = self.rx_detail.search(c)
             iface = {
                 "local_interface": i[0],
                 "neighbors": []
             }
-            cap = 0
-            for c in match.group("caps").split(","):
-                c = c.strip()
-                if c:
-                    cap |= self.CAPS_MAP[c]
-
-            n = {
-                "remote_chassis_id": match.group("chassis_id").strip(),
-                "remote_chassis_id_subtype": self.CHASSIS_SUBTYPE[
-                    match.group("chassis_id_subtype").strip()
-                ],
-                "remote_port":  match.group("port_id").strip(),
-                "remote_port_subtype": self.PORT_SUBTYPE[
-                    match.group("port_id_subtype").strip()
-                ],
-                "remote_capabilities": cap,
-            }
-            if match.group("system_name").strip():
-                n["remote_system_name"] = match.group("system_name").strip()
-            if match.group("system_description").strip():
-                n["remote_system_description"] = \
+            for match in self.rx_detail.finditer(c):
+                cap = 0
+                for c in match.group("caps").split(","):
+                    c = c.strip()
+                    if c:
+                        cap |= self.CAPS_MAP[c]
+                n = {
+                    "remote_chassis_id": match.group("chassis_id").strip(),
+                    "remote_chassis_id_subtype": self.CHASSIS_SUBTYPE[
+                        match.group("chassis_id_subtype").strip()
+                    ],
+                    "remote_port":  match.group("port_id").strip(),
+                    "remote_port_subtype": self.PORT_SUBTYPE[
+                        match.group("port_id_subtype").strip()
+                    ],
+                    "remote_capabilities": cap,
+                }
+                if match.group("system_name").strip():
+                    n["remote_system_name"] = match.group("system_name").strip()
+                if match.group("system_description").strip():
+                    n["remote_system_description"] = \
                     match.group("system_description").strip()
-            if match.group("port_description").strip():
-                n["remote_port_description"] = \
-                    match.group("port_description").strip()
-            iface["neighbors"] += [n]
+                if match.group("port_description").strip():
+                    n["remote_port_description"] = \
+                        match.group("port_description").strip()
+                iface["neighbors"] += [n]
             r += [iface]
         return r
