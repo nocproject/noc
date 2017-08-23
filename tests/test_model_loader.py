98ed13689d28b5e673618fe97b22f81025a8d821
Dmitry Volodin <dv@nocproject.org>
2017-08-23 10:42:59 +0300

    tests: Model loading
    --HG--
    branch : feature/microservices

--- tests/test_model_loader.py
+++ tests/test_model_loader.py
@@ -2,25 +2,44 @@
 # ---------------------------------------------------------------------
 # Test model loader
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2015 The NOC Project
+# Copyright (C) 2007-2017 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
 # Third-party modules
-from nose2.tools import params
+import pytest
 # NOC modules
 from noc.models import iter_model_id, get_model, get_model_id
 
 
 def test_iter_model_id():
-    """ Check loader.iter_scripts() returns values """
-    assert len(list(iter_model_id())) > 0
+    """
+    Check loader.iter_scripts() returns values
+    """
+    total_models = len(list(iter_model_id()))
+    assert total_models > 0
 
 
-@params(*tuple(iter_model_id()))
+@pytest.fixture(params=iter_model_id())
+def model_id(request):
+    return request.param
+
+
 def test_model_loading(model_id):
-    """ Check model can be loaded """
+    """
+    Check model referred by id can be loaded
+    :param model_id:
+    :return:
+    """
     model = get_model(model_id)
-    assert model, "Cannot load model %s" % model_id
-    n_model_id = get_model_id(model)
-    assert model_id == n_model_id, "Model ID mismatch for %s" % model_id
+    assert model is not None, "Cannot load model %s" % model_id
+
+
+def test_model_id(model_id):
+    """
+    Check model has same model_id as referred
+    """
+    model = get_model(model_id)
+    if model:
+        real_model_id = get_model_id(model)
+        assert real_model_id == model_id
