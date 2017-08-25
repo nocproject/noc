f8c8ea6b2bd147b3f6651d92a501405986874678
Boris Pokhozhaev <nifigator@gmail.com>
2017-08-25 12:12:23 +0700

    Add Eltex.SMG profile
    --HG--
    branch : add.eltexsmgprofile.2017082502

---
+++ sa/profiles/Eltex/SMG/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# Vendor: Eltex
+# OS:     SMG
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2017 The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+
+# Python modules
+import re
+# NOC modules
+from noc.core.profile.base import BaseProfile
+
+
+class Profile(BaseProfile):
+    name = "Eltex.SMG"
+    #pattern_username = r"^\S+ login: "
+    #pattern_prompt = r"^(?P<hostname>\S+)# "
+    pattern_prompt = r"(SMG2016> )|(/[\w/]+ # )"
+    command_exit = "exit"
