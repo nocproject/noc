2fad57ac4989a06cd08e9340b65e8ad70dd17b40
Andrey Vertiprakhov <aversanta@gmail.com>
2017-09-02 00:13:11 +0500

    Migrate Alcatel.7302.get_metrics script to CH
    --HG--
    branch : feature/microservices

--- sa/profiles/Alcatel/7302/get_metrics.py
+++ sa/profiles/Alcatel/7302/get_metrics.py
@@ -23,6 +23,7 @@ class SlotRule(OIDRule):
     def iter_oids(self, script, metric):
         i = 4353
         # 4353 - nt-a slot num
+        # chassis, slot, module, name
         r = {}
         if script.has_capability("Stack | Member Ids"):
             sys_slot_index = [0] + [int(index) for index in script.capabilities["Stack | Member Ids"].split(" | ")]
@@ -35,19 +36,21 @@ class SlotRule(OIDRule):
             if ms in [0]:
                 # nt-a card - 3 sensors
                 for s_i in range(1, 5):
-                    r["nt-a_s%d" % s_i] = "%d.%d" % (ms, s_i)
+                    # r["nt-a_s%d" % s_i] = "%d.%d" % (ms, s_i)
+                    r[("1", "1", "0", "nt-a_s%d" % s_i, )] = "%d.%d" % (ms, s_i)
                 continue
             # r[str(i)] = {"healthModuleSlot": ms}
             for s_i in range(1, 3):
                 # Two sensors on cards
-                r["lt:1/1/%d_s%d" % (ms, s_i)] = "%d.%d" % (i + ms - 2 if ms not in [2, 3] else i + 8 + ms, s_i)
+                # r["lt:1/1/%d_s%d" % (ms, s_i)] = "%d.%d" % (i + ms - 2 if ms not in [2, 3] else i + 8 + ms, s_i)
+                r[("1", "1", str(ms), "lt_s%d" % s_i, )] = "%d.%d" % (i + ms - 2 if ms not in [2, 3] else i + 8 + ms, s_i)
 
         for i in r:
             if self.is_complex:
                 gen = [mib[self.expand(o, {"hwSlotIndex": r[i]})] for o in self.oid]
                 if gen:
-                    yield tuple(gen), self.type, self.scale, {"slot": i}
+                    yield tuple(gen), self.type, self.scale, list(i)
             else:
                 oid = mib[self.expand(self.oid, {"hwSlotIndex": r[i]})]
                 if oid:
-                    yield oid, self.type, self.scale, {"slot": i}
+                    yield oid, self.type, self.scale, list(i)
