ebc90ed1747866b76217157400c9ef74b1ed80cb
Maksim <none@none>
2017-09-02 22:23:35 +0300

    Fix ReportMetrics for grafana dashboard
    --HG--
    branch : feature/microservices

--- services/web/apps/inv/reportmetrics/views.py
+++ services/web/apps/inv/reportmetrics/views.py
@@ -257,7 +257,7 @@ class ReportTraffic(SimpleReport):
                 "q_group": ["interface"]
             },
             "load_cpu": {
-                "url": "%(path)s?title=cpu&biid=%(biid)s&obj=%(oname)s&from=%(from)s&to=%(to)s",
+                "url": """%(path)s?title=cpu&biid=%(biid)s&obj=%(oname)s&from=%(from)s&to=%(to)s""",
                 "columns": [TableColumn(_("Memory | Usage %"), align="right"),
                             TableColumn(_("CPU | Usage %"), align="right")]
             },
