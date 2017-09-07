e91e02d78da306367d29819e49c92888b14b7cf5
Andrey Vertiprakhov <aversanta@gmail.com>
2017-09-07 23:17:23 +0500

    Fix GENERIC_PROFILE import
    --HG--
    branch : feature/moversion

--- services/discovery/jobs/box/sla.py
+++ services/discovery/jobs/box/sla.py
@@ -11,6 +11,7 @@ import six
 # NOC modules
 from noc.services.discovery.jobs.base import DiscoveryCheck
 from noc.sla.models.slaprobe import SLAProbe
+from noc.core.profile.loader import GENERIC_PROFILE
 
 
 class SLACheck(DiscoveryCheck):
