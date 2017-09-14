619af38c254796fd6e9157aa54d609ce581cd02b
Maksim <none@none>
2017-09-14 10:45:42 +0300

    Add get_chassis_id for Rotek Profile
    --HG--
    branch : feature/microservices

---
+++ sa/profiles/Rotek/RTBS/get_chassis_id.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# Rotek.RTBS.get_chassis_id
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2017 The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+
+
+# Python modules
+import re
+# NOC modules
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetchassisid import IGetChassisID
+from noc.core.mac import MAC
+
+
+class Script(BaseScript):
+    name = "Rotek.RTBS.get_chassis_id"
+    cache = True
+    interface = IGetChassisID
+
+    def execute(self):
+        # Try SNMP first
+        if self.has_snmp():
+            try:
+                base = self.snmp.get("1.3.6.1.2.1.2.2.1.6.1")
+                return [{
+                    "first_chassis_mac": base,
+                    "last_chassis_mac": base
+                }]
+            except self.snmp.TimeOutError:
+                pass
+
+        # Fallback to CLI
+        raise Exception("Not implemented")
