548ca382fd701058abb405399f1cfdb92feda57e
Dmitry Volodin <dv@nocproject.org>
2017-09-15 19:02:23 +0300

    fix_platform_full_name: Increased reliability
    --HG--
    branch : feature/microservices

--- fixes/fix_platform_full_name.py
+++ fixes/fix_platform_full_name.py
@@ -6,10 +6,33 @@
 # See LICENSE for details
 # ----------------------------------------------------------------------
 
+# Python modules
+from pymongo import UpdateOne
 # NOC modules
-from noc.inv.models.platform import Platform
+from noc.lib.nosql import get_db
 
 
 def fix():
+    # Initialize with distinct values
+    coll = get_db()["noc.platforms"]
+    bulk = []
+    for d in coll.find({
+        "full_name": {
+            "$exists": False
+        }
+    }, {"_id": 1}):
+        bulk += [UpdateOne({
+            "_id": d["_id"]
+        }, {
+            "$set": {"full_name": str(d["_id"])}
+        })]
+    if bulk:
+        coll.bulk_write(bulk)
+    fix_full_name()
+
+
+def fix_full_name():
+    from noc.inv.models.platform import Platform
+
     for p in Platform.objects.all():
         p.save()
