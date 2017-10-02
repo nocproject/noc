e4ae7dc59f0a718b0fd762781cc5ed28fd754511
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2017-10-02 17:05:48 +0300

    Update DLink.DxS profile
    --HG--
    branch : feature/microservices

--- sa/profiles/DLink/DVG/get_version.py
+++ sa/profiles/DLink/DVG/get_version.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # ---------------------------------------------------------------------
-# DLink.DVG.get_version
+# DLink.DxS.get_version
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2011 The NOC Project
+# Copyright (C) 2007-2017 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
@@ -11,63 +11,48 @@ import re
 # NOC modules
 from noc.core.script.base import BaseScript
 from noc.sa.interfaces.igetversion import IGetVersion
+from noc.sa.profiles.DLink.DxS import get_platform
 
 
 class Script(BaseScript):
-    name = "DLink.DVG.get_version"
-    interface = IGetVersion
+    name = "DLink.DxS.get_version"
     cache = True
-
-    rx_platform = re.compile(
-        r"^Software \[(==|\S+ ==) Ver\(+(?P<version>\S+)\s+\S+\s+\S+\)\s+"
-        r"PId\(+\S+\)\s+Drv\(+\S+\)\s+Hw\(+(?P<platform>\S+)+\) "
-        r"(== |== (?P<hardware>\S+))+\]$",
-        re.MULTILINE)
-
-    platforms_cli = {
-        "??????": "DVG-2101S",
-        "DSA": "DVG-2102S",
-        "PNP1632-32": "DVG-4032S",
-        "SA7S4": "DVG-5004S",
-        "TSO": "DVG-7111S"
-        }
-
-    platforms_snmp = {
-        "3.2.10": "DVG-2101S",
-        "?.?.?": "DVG-2102S",
-        "?.?.?": "DVG-4032S",
-        "1.2.1": "DVG-5004S",
-        "?.?.?": "DVG-7111S"
-        }
+    interface = IGetVersion
+    rx_ver = re.compile(
+        r"Device Type\s+:\s*(?P<platform>\S+).+"
+        r"(?:Boot PROM|System [Bb]oot)\s+"
+        r"[Vv]ersion\s+:\s*(?:Build\s+)?(?P<bootprom>\S+).+"
+        r"[Ff]irmware [Vv]ersion(?: 1)?\s+:\s*(?:Build\s+)?(?P<version>\S+).+"
+        r"[Hh]ardware [Vv]ersion\s+:\s*(?P<hardware>\S+)",
+        re.MULTILINE | re.DOTALL)
+    rx_fwt = re.compile(
+        r"(?:Firmware Type|System [Ff]irmware [Vv]ersion)\s+:\s*"
+        r"(?P<fwt>\S+)\s*\n", re.MULTILINE | re.DOTALL)
+    rx_ser = re.compile(
+        r"(?:[Ss]erial [Nn]umber|Device S/N)\s+:\s*(?P<serial>\S+)\s*\n",
+        re.MULTILINE | re.DOTALL)
 
     def execute(self):
-        # Try SNMP first
-        if self.has_snmp():
-            try:
-                platform = self.snmp.get("1.3.6.1.2.1.1.2.0", cached=True)
-                platform = platform.split(', ')
-                l = len(platform) - 1
-                platform = (platform[l - 2] + '.' + platform[l - 1] +
-                            '.' + platform[l])
-                platform = self.platforms_snmp.get(platform.split(')')[0],
-                                                   '????')
-                return {
-                    "vendor": "DLink",
-                    "platform": platform,
-                    "version": '1.02.38.x',
-                    }
-            except self.snmp.TimeOutError:
-                pass
-
-        # Fallback to CLI
-        plat = self.cli("GET STATUS HARDWARE", cached=True)
-        match = self.rx_platform.search(plat)
-        platform = self.platforms_cli.get(match.group("platform"), '????')
+        s = self.scripts.get_switch()
+        match = self.re_search(self.rx_ver, s)
         r = {
             "vendor": "DLink",
-            "platform": platform,
-            "version": match.group("version")
+            "platform": get_platform(
+                match.group("platform"), match.group("hardware")
+            ),
+            "version": match.group("version"),
+            "attributes": {
+                "Boot PROM": match.group("bootprom"),
+                "HW version": match.group("hardware"),
             }
-        if match.group("hardware"):
-            r["attributes"]["HW version"] = match.group("hardware")
+        }
+        ser = self.rx_ser.search(s)
+        if (
+            ser and ser.group("serial") != "System" and
+            ser.group("serial") != "Power"
+        ):
+            r["attributes"]["Serial Number"] = ser.group("serial")
+        fwt = self.rx_fwt.search(s)
+        if fwt and (fwt.group("fwt") != match.group("version")):
+            r["attributes"]["Firmware Type"] = fwt.group("fwt")
         return r
