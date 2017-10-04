53437e46c6829d9a46970298d4e7216e7fc598c6
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2017-10-04 12:06:13 +0300

    Fix Eltex.MA4000.get_lldp_neighbors script
    --HG--
    branch : feature/microservices

--- sa/profiles/Eltex/MA4000/get_lldp_neighbors.py
+++ sa/profiles/Eltex/MA4000/get_lldp_neighbors.py
@@ -11,43 +11,47 @@ import re
 # NOC modules
 from noc.core.script.base import BaseScript
 from noc.sa.interfaces.igetlldpneighbors import IGetLLDPNeighbors
-from noc.sa.interfaces.base import MACAddressParameter
-from noc.lib.validators import is_int, is_ipv4, is_ipv6
+from noc.lib.validators import is_int, is_ipv4, is_ipv6, is_mac
 from noc.lib.text import parse_table
-from noc.core.mac import MAC
 
 
 class Script(BaseScript):
     name = "Eltex.MA4000.get_lldp_neighbors"
     interface = IGetLLDPNeighbors
 
+    rx_neighbor = re.compile(
+        r"^Device ID: (?P<chassis_id>\S+)\s*\n"
+        r"^Port ID: (?P<port_id>\S+)\s*\n"
+        r"^Time To Live: \S+\s*\n\n"
+        r"^Port description:(?P<port_descr>.*)\n"
+        r"^System name:(?P<system_name>.*)\n"
+        r"^System description:(?P<system_descr>.*)\n"
+        r"^Capabilities:(?P<caps>.*?)\n\n",
+        re.MULTILINE | re.DOTALL
+    )
+
     def execute(self):
         r = []
         t = parse_table(self.cli("show lldp neighbor"), allow_wrap=True)
         for i in t:
-            chassis_id = i[1]
+            c = self.cli("show lldp neighbor %s" % i[0])
+            match = self.rx_neighbor.search(c)
+            chassis_id = match.group("chassis_id")
             if is_ipv4(chassis_id) or is_ipv6(chassis_id):
                 chassis_id_subtype = 5
+            elif is_mac(chassis_id):
+                chassis_id_subtype = 4
             else:
-                try:
-                    MACAddressParameter().clean(chassis_id)
-                    chassis_id_subtype = 4
-                except ValueError:
-                    chassis_id_subtype = 7
-            port_id = i[2]
+                chassis_id_subtype = 7
+            port_id = match.group("port_id")
             if is_ipv4(port_id) or is_ipv6(port_id):
                 port_id_subtype = 4
+            elif is_mac(port_id):
+                port_id_subtype = 3
             else:
-                try:
-                    MACAddressParameter().clean(port_id)
-                    port_id_subtype = 3
-                except ValueError:
-                    port_id_subtype = 7
-            # Do no remove these lines
-            if (chassis_id == "") or (port_id == ""):
-                continue
+                port_id_subtype = 7
             caps = 0
-            for c in i[4].split(","):
+            for c in match.group("caps").split(","):
                 c = c.strip()
                 if c:
                     caps |= {
@@ -62,8 +66,15 @@ class Script(BaseScript):
                 "remote_port_subtype": port_id_subtype,
                 "remote_capabilities": caps
             }
-            if i[3]:
-                neighbor["remote_system_name"] = i[3]
+            port_descr = match.group("port_descr").strip()
+            if port_descr:
+                neighbor["remote_port_description"] = port_descr
+            system_name = match.group("system_name").strip()
+            if system_name:
+                neighbor["remote_system_name"] = system_name
+            system_descr = match.group("system_descr").strip()
+            if system_descr:
+                neighbor["remote_system_description"] = system_descr
             r += [{
                 "local_interface": i[0],
                 "neighbors": [neighbor]
