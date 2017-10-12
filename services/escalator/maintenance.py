e06a9d102da6bb47486fe14ef2463ca6d304f19a
Andrey Vertiprakhov <aversanta@gmail.com>
2017-10-12 23:48:19 +0500

    Fix maintenance job
    --HG--
    branch : feature/microservices

--- services/escalator/maintenance.py
+++ services/escalator/maintenance.py
@@ -10,9 +10,9 @@
 import logging
 # NOC modules
 from noc.maintainance.models.maintainance import Maintainance
+from noc.sa.models.managedobject import ManagedObject
 from noc.fm.models.ttsystem import TTSystem
 from noc.core.perf import metrics
-from noc.inv.models.extnrittmap import ExtNRITTMap
 
 logger = logging.getLogger(__name__)
 
@@ -32,18 +32,16 @@ def start_maintenance(maintenance_id):
                     maintenance_id, m.escalation_tt)
         return
     # Get external TT system
-    d = ExtNRITTMap._get_collection().find_one({
-        "managed_object": m.escalate_managed_object.id
-    })
+    d = m.escalate_managed_object.tt_system_id
     if not d:
         logger.info("[%s] No TT mapping for object %s(%s)",
                     maintenance_id, m.escalate_managed_object.name,
                     m.escalate_managed_object.address)
         return
-    tt_system = TTSystem.get_by_id(d["tt_system"])
+    tt_system = m.escalate_managed_object.tt_system
     if not tt_system:
         logger.info("[%s] Cannot find TT system '%s'",
-                    maintenance_id, d["tt_system"])
+                    maintenance_id, m.escalate_managed_object)
         return
     tts = tt_system.get_system()
     try:
@@ -52,8 +50,8 @@ def start_maintenance(maintenance_id):
             queue=1,
             obj=d["remote_id"],
             reason=0,
-            subject=subject,
-            body=body,
+            subject=m.subject,
+            body=m.body,
             login="correlator",
             timestamp=m.start
         )
@@ -68,21 +66,16 @@ def start_maintenance(maintenance_id):
             })
             if d:
                 objects = [x["object"] for x in d["affected_objects"]]
-                for d in ExtNRITTMap._get_collection().find({
-                    "managed_object": {
-                        "$in": list(objects)
-                    },
-                    "tt_system": tt_system.id
-                }):
+                for d in ManagedObject.objects.filter(id__in=list(objects)):
                     logger.info(
                         "[%s] Appending object %s to group TT %s",
                         maintenance_id,
-                        d["managed_object"],
+                        d,
                         gtt
                     )
                     tts.add_to_group_tt(
                         gtt,
-                        d["remote_id"]
+                        d.tt_system_id
                     )
         metrics["maintenance_tt_create"] += 1
     except tts.TTError as e:
