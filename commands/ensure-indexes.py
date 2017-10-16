17b24cba9f582bdb5cf8f29263bfeec71a382212
Dmitry Volodin <dv@nocproject.org>
2017-10-16 14:33:01 +0300

    ensure-indexes: Collection renaming
    --HG--
    branch : feature/microservices

--- commands/ensure-indexes.py
+++ commands/ensure-indexes.py
@@ -15,19 +15,29 @@ from noc.models import get_model, iter_model_id, is_document
 
 class Command(BaseCommand):
     def handle(self, host=None, port=None, *args, **options):
-        import noc.lib.nosql
+        from noc.lib.nosql import get_db
 
+        db = get_db()
+        collections = set(db.collection_names())
         for model_id in iter_model_id():
             model = get_model(model_id)
             if not model:
                 self.die("Invalid model: %s" % model_id)
             if not is_document(model):
                 continue
+            # Rename collections when necessary
+            legacy_collections = model._meta.get("legacy_collections", [])
+            for old_name in legacy_collections:
+                if old_name in collections:
+                    new_name = model._meta["collection"]
+                    self.print("[%s] Renaming %s to %s" % (model_id, old_name, new_name))
+                    db[old_name].rename(new_name)
+                    break
             # Ensure only documents with auto_create_index == False
             if model._meta.get('auto_create_index', True):
                 continue
             # Index model
-            self.print("Checking indexes for %s" % model_id)
+            self.print("[%s] Checking indexes" % model_id)
             model.ensure_indexes()
         # @todo: Detect changes
         self.print("OK")
