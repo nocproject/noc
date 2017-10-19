6f0227b491b96e9e8c626a5ff4d76ded4eec6d7d
Andrey Vertiprakhov <aversanta@gmail.com>
2017-10-20 02:32:21 +0500

    Fix default chunk size for bi extract
    --HG--
    branch : feature/microservices

--- config.py
+++ config.py
@@ -66,7 +66,7 @@ class Config(BaseConfig):
         reboot_interval = SecondsParameter(default="1M")
         extract_delay_reboots = SecondsParameter(default="1h")
         clean_delay_reboots = SecondsParameter(default="1d")
-        chunk_size = IntParameter(default=4000)
+        chunk_size = IntParameter(default=3000)
         extract_window = SecondsParameter(default="1d")
 
     brand = StringParameter(default="NOC")
