4f1c73b1d705fd425821498f471618a8fda7aa4b
Maksmile13 <sysfar@gmail.com>
2017-10-28 19:58:41 +0300

    Fix NAG.SNR profile

---
+++ sa/profiles/NAG/SNR/get_capabilities.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# NAG.SNR.get_capabilities
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2017 The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+
+# Python modules
+import re
+# NOC modules
+from noc.lib.text import parse_table
+from noc.sa.profiles.Generic.get_capabilities import Script as BaseScript
+from noc.sa.profiles.Generic.get_capabilities import false_on_cli_error
+
+
+class Script(BaseScript):
+    name = "NAG.SNR.get_capabilities"
+
+    rx_lldp_en = re.compile(r"LLDP has been enabled globally?")
+
+    @false_on_cli_error
+    def has_lldp(self):
+        """
+        Check box has lldp enabled on SNR
+        """
+        cmd = self.cli("show lldp", ignore_errors=True)
+        return self.rx_lldp_en.search(cmd) is not None
