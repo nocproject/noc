bd7b9b091448ba6e6d47f829d22cd0f13c3d9345
Maksmile13 <sysfar@gmail.com>
2017-10-29 17:07:47 +0300

    Fix Rotek get_interface_status_ex

--- sa/profiles/Rotek/RTBS/get_interface_status_ex.py
+++ sa/profiles/Rotek/RTBS/get_interface_status_ex.py
@@ -22,7 +22,7 @@ class Script(BaseScript):
         if "::" in oid:
             oid = mib[oid]
         r = {}
-        for oid, v in self.snmp.getnext(oid, max_repetitions=40):
+        for oid, v in self.snmp.getnext(oid, max_repetitions=5):
             ifindex = int(oid.split(".")[-1])
             r[ifindex] = v
         return r
@@ -39,7 +39,7 @@ class Script(BaseScript):
         # ifIndex -> ifName mapping
         r = {}  # ifindex -> data
         unknown_interfaces = []
-        for oid, name in self.snmp.getnext(mib["IF-MIB::ifDescr"]):
+        for oid, name in self.snmp.getnext(mib["IF-MIB::ifDescr"], max_repetitions=5):
             try:
                 v = self.profile.convert_interface_name(name)
             except InterfaceTypeError, why:
@@ -83,65 +83,30 @@ class Script(BaseScript):
         if unknown_interfaces:
             self.logger.info("%d unknown interfaces has been ignored",
                              len(unknown_interfaces))
+
         return r.values()
+
     def get_data2(self):
         # ifIndex -> ifName mapping
-        ss = {}
         r = {}  # ifindex -> data
         unknown_interfaces = []
-        for soid, sname in self.snmp.getnext("1.3.6.1.4.1.32761.3.5.1.2.1.1.4"):
+        ver = self.snmp.get("1.3.6.1.2.1.1.2.0")
+        #check sysObjectID
+        obj = ver.split(".")[-1]
+        for soid, sname in self.snmp.getnext("1.3.6.1.4.1.%s.3.5.1.2.1.1.4" % obj):
             sifindex = int(soid.split(".")[-1])
-            ss[sifindex] = sname
-        for soid, sname in self.snmp.getnext("1.3.6.1.4.1.41752.3.5.1.2.1.1.4"):
-            sifindex = int(soid.split(".")[-1])
-            ss[sifindex] = sname
-        for oid, name in self.snmp.getnext(mib["IF-MIB::ifDescr"]):
-            try:
-                v = self.profile.convert_interface_name(name)
-            except InterfaceTypeError, why:
-                self.logger.debug(
-                    "Ignoring unknown interface %s: %s",
-                    name, why
-                )
-                unknown_interfaces += [name]
-                continue
-            ifindex =  int(oid.split(".")[-1])
-            for i in ss.items():
-                if int(i[0]) == ifindex:
-                    v= "%s.%s" % (v, i[1])
-                    r[ifindex] = {
-                        "interface": v
-                    }
-        # Apply ifAdminStatus
-        self.apply_table(r, "IF-MIB::ifAdminStatus", "admin_status", lambda x: x == 1)
-        # Apply ifOperStatus
-        self.apply_table(r, "IF-MIB::ifOperStatus", "oper_status", lambda x: x == 1)
-        # Apply ifSpeed
-        s_table = self.get_iftable("IF-MIB::ifSpeed")
-        highspeed = set()
-        for ifindex in r:
-            s = s_table.get(ifindex)
-            if s is not None:
-                s = int(s)
-                if s == 4294967295:
-                    highspeed.add(ifindex)
-                elif s:
-                    r[ifindex]["in_speed"] = s // 1000
-                    r[ifindex]["out_speed"] = s // 1000
+            name = self.snmp.get("1.3.6.1.2.1.2.2.1.2.%s" % sifindex)
+            if_speed = self.snmp.get("1.3.6.1.2.1.2.2.1.5.%s" % sifindex)
+            admin_status = self.snmp.get("1.3.6.1.2.1.2.2.1.7.%s" % sifindex)
+            oper_status = self.snmp.get("1.3.6.1.2.1.2.2.1.8.%s" % sifindex)
+            r[sifindex] = {
+                "interface": "%s.%s" % (name, sname),
+                "in_speed": if_speed // 1000,
+                "out_speed": if_speed // 1000,
+                "admin_status": admin_status,
+                "oper_status": oper_status
 
-        # Refer to ifHighSpeed if necessary
-        if highspeed:
-            hs_table = self.get_iftable("IF-MIB::ifHighSpeed")
-            for ifindex in highspeed:
-                s = hs_table.get(ifindex)
-                if s is not None:
-                    s = int(s)
-                    if s:
-                        r[ifindex]["in_speed"] = s * 1000
-                        r[ifindex]["out_speed"] = s * 1000
-        if unknown_interfaces:
-            self.logger.info("%d unknown interfaces has been ignored",
-                             len(unknown_interfaces))
+            }
         return r.values()
 
     def execute(self):
