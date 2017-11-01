601b2106e0fdb93c73309567a9fcb59a0f88e433
aversant <aversanta@gmail.com>
2017-11-01 17:26:05 +0500

    noc/noc#442 Apply patch then fix SNMPv1 caps in SAE.

--- services/sae/api/sae.py
+++ services/sae/api/sae.py
@@ -21,6 +21,7 @@ from noc.core.cache.decorator import cachedmethod
 from noc.core.dcs.base import ResolutionError
 from noc.config import config
 
+
 class SAEAPI(API):
     """
     SAE API
@@ -133,6 +134,9 @@ class SAEAPI(API):
             data = cursor.fetchall()
         if not data:
             raise APIError("Object is not found")
+        # Build capabilities
+        capabilities = ObjectCapabilities.get_capabilities(object_id)
+        # Get object credentials
         (name, is_managed, profile,
          vendor, platform, version,
          scheme, address, port, user, password,
@@ -172,9 +176,13 @@ class SAEAPI(API):
             "raise_privileges": raise_privileges,
             "access_preference": access_preference
         }
-        if snmp_ro:
-            credentials["snmp_version"] = "v2c"
-            credentials["snmp_ro"] = snmp_ro
+        if snmp_ro and "SNMP" in capabilities:
+            if "SNMP | v2c" in capabilities:
+                credentials["snmp_version"] = "v2c"
+                credentials["snmp_ro"] = snmp_ro
+            elif "SNMP | v1" in capabilities:
+                credentials["snmp_version"] = "v1"
+                credentials["snmp_ro"] = snmp_ro
         if scheme in (1, 2):
             credentials["cli_protocol"] = {
                 1: "telnet",
@@ -197,8 +205,6 @@ class SAEAPI(API):
                 version["image"] = sw_image
         else:
             version = None
-        # Build capabilities
-        capabilities = ObjectCapabilities.get_capabilities(object_id)
         return dict(
             profile=Profile.get_by_id(profile).name,
             pool_id=pool_id,
