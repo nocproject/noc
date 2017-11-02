69857b578abe7586bd97b91e0c7ebf9d2c51d07e
uncol <d.a.zakharov@gmail.com>
2017-11-02 11:03:07 +0300

    remote query and restore

--- ui/web/core/TagsField.js
+++ ui/web/core/TagsField.js
@@ -10,25 +10,41 @@ console.debug("Defining NOC.core.TagsField");
 Ext.define("NOC.core.TagsField", {
     extend: "Ext.form.field.Tag",
     alias: ["widget.tagsfield"],
-    forceSelection: false,
     displayField: "label",
     valueField: "label",
-    queryMode: "local",
-    queryParam: "__query",
-    createNewOnEnter: true,
-    createNewOnBlur: true,
+    queryParam: "__q",
+    queryMode: "remote",
+    autoLoadOnValue: true,
+    // forceSelection: false,
+    // triggerAction: 'query',
+    // createNewOnEnter: true,
+    // createNewOnBlur: true,
 
     initComponent: function() {
         var me = this;
 
         Ext.apply(me, {
             store: {
-                fields: ['label', 'id'],
-                data: []
-            },
-            createNewOnEnter: true,
-            createNewOnBlur: true,
-            displayField: 'label'
+                fields: ["label"],
+                // pageSize: 25,
+                proxy: {
+                    type: "rest",
+                    url: '/main/tag/ac_lookup/',
+                    pageParam: "__page",
+                    startParam: "__start",
+                    limitParam: "__limit",
+                    sortParam: "__sort",
+                    extraParams: {
+                        "__format": "ext"
+                    },
+                    reader: {
+                        type: "json",
+                        rootProperty: "data",
+                        totalProperty: "total",
+                        successProperty: "success"
+                    }
+                }
+            }
         });
         me.callParent();
     }
