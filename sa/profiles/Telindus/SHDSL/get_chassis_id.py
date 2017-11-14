de31adc692bf581c7ab0a86fb742e46bffc9ef0f
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2017-11-14 12:50:28 +0300

    Update get_chassis_id.py

--- sa/profiles/Telindus/SHDSL/get_chassis_id.py
+++ sa/profiles/Telindus/SHDSL/get_chassis_id.py
@@ -5,12 +5,11 @@
 # Copyright (C) 2007-2017 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
-# Python modules
-import re
+
+
 # NOC modules
 from noc.core.script.base import BaseScript
 from noc.sa.interfaces.igetchassisid import IGetChassisID
-from noc.core.mac import MAC
 
 
 class Script(BaseScript):
