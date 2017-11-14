db954f02989d1fb51c7dc06c13ae9594afea3543
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2017-11-14 12:53:22 +0300

    Update get_interfaces.py

--- sa/profiles/Telindus/SHDSL/get_interfaces.py
+++ sa/profiles/Telindus/SHDSL/get_interfaces.py
@@ -7,8 +7,6 @@
 # ---------------------------------------------------------------------
 
 
-# Python modules
-import re
 # NOC modules
 from noc.core.script.base import BaseScript
 from noc.sa.interfaces.igetinterfaces import IGetInterfaces
