0df934106281372d926bd0f1da1311537ec4de18
Maksim <sysfar@gmail.com>
2017-11-21 17:21:02 +0300

    Fix Eltex.MES Run Commands - conf t

--- sa/profiles/Eltex/MES/__init__.py
+++ sa/profiles/Eltex/MES/__init__.py
@@ -29,7 +29,9 @@ class Profile(BaseProfile):
     command_enter_config = "configure"
     command_leave_config = "end"
     command_save_config = "copy running-config startup-config"
-    pattern_prompt = r"^(?P<hostname>[A-Za-z0-9-_ \:\.\*\'\,\(\)\/]+)#"
+    pattern_prompt = \
+        r"^(?P<hostname>[A-Za-z0-9-_ \:\.\*\'\,\(\)\/]+)?" \
+        r"(?:\(config[^\)]*\))?#"
     convert_interface_name = BaseProfile.convert_interface_name_cisco
 
     INTERFACE_TYPES = {
