3a2d725bc6246af97e78b19b3c5d6bc709b20fa5
aversant <aversanta@gmail.com>
2017-11-22 10:16:30 +0500

    Fix Ruckus to PEP

--- sa/profiles/Ruckus/SmartZone/get_config.py
+++ sa/profiles/Ruckus/SmartZone/get_config.py
@@ -5,8 +5,8 @@
 # Copyright (C) 2007-2017 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
-# Python modules
-import re
+
+
 # NOC modules
 from noc.core.script.base import BaseScript
 from noc.sa.interfaces.igetconfig import IGetConfig
