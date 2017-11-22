3ff3ee2c39a97aa4ae25f548f32b748cb3349ed9
Kirill Petrov <archibeardrinker@gmail.com>
2017-11-22 18:54:19 +0300

    Update get_capabilities.py

--- sa/profiles/Cisco/SMB/get_capabilities.py
+++ sa/profiles/Cisco/SMB/get_capabilities.py
@@ -17,22 +17,29 @@ class Script(BaseScript):
         """
         Check box has cdp enabled
         """
-        # ciscoCdpMIB::cdpGlobalRun
-        r = self.snmp.get("1.3.6.1.4.1.9.9.23.1.3.1.0")
+        r = 0
+        if self.has_snmp():
+            # ciscoCdpMIB::cdpGlobalRun
+            r = self.snmp.get("1.3.6.1.4.1.9.9.23.1.3.1.0")
         return r == 1
+        
 
     def has_lldp_snmp(self):
         """
         Check box has lldp enabled
         """
-        # rlLldpEnabled
-        r = self.snmp.get("1.3.6.1.4.1.9.6.1.101.110.1.1.1.0")
+        r = 0
+        if self.has_snmp():
+            # rlLldpEnabled
+            r = self.snmp.get("1.3.6.1.4.1.9.6.1.101.110.1.1.1.0")
         return r == 1
 
     def has_stp_snmp(self):
         """
         Check box has stp enabled
         """
-        # rldot1dStpEnable
-        r = self.snmp.get("1.3.6.1.4.1.9.6.1.101.57.2.3.0")
+        r = 0
+        if self.has_snmp():
+            # rldot1dStpEnable
+            r = self.snmp.get("1.3.6.1.4.1.9.6.1.101.57.2.3.0")
         return r == 1
