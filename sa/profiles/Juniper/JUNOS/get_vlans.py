c9e1108ed53abd8749821b49aa14dfb829c39c1d
Dmitry S. Luhtionov <dmitryluhtionov@gmail.com>
2017-11-27 14:17:47 +0200

    Fix Juniper.JUNOS.get_vlans script

--- sa/profiles/Juniper/JUNOS/get_vlans.py
+++ sa/profiles/Juniper/JUNOS/get_vlans.py
@@ -2,7 +2,7 @@
 # ---------------------------------------------------------------------
 # Juniper.JUNOS.get_vlans
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2013 The NOC Project
+# Copyright (C) 2007-2017 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
@@ -17,7 +17,14 @@ class Script(BaseScript):
     name = "Juniper.JUNOS.get_vlans"
     interface = IGetVlans
 
-    rx_vlan_line = re.compile(r"^(?P<name>\S+)\s+(?P<vlan_id>\d+)\s+")
+    rx_vlan_line = re.compile(
+        r"^((?P<routing_instance>\S+)\s+)?(?P<name>\S+)\s+(?P<vlan_id>\d+)\s+")
 
     def execute(self):
-        return self.cli("show vlan brief", list_re=self.rx_vlan_line)
+        try:
+            self.cli("show vlans")
+        except self.CLISyntaxError:
+            # Juniper do not clear wrong input line
+            self.cli("\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08")
+            return []
+        return self.cli("show vlans brief", list_re=self.rx_vlan_line)
