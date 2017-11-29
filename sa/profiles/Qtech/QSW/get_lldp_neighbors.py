cc243459d5c2c8af8322befea0219a99f096a7a9
Maksim <sysfar@gmail.com>
2017-11-29 11:31:28 +0300

    Fix Qtech.QSW profile (init, caps, lldp)

--- sa/profiles/Qtech/QSW/get_lldp_neighbors.py
+++ sa/profiles/Qtech/QSW/get_lldp_neighbors.py
@@ -11,7 +11,6 @@ import re
 # NOC modules
 from noc.core.script.base import BaseScript
 from noc.sa.interfaces.igetlldpneighbors import IGetLLDPNeighbors
-from noc.sa.interfaces.base import MACAddressParameter
 from noc.lib.validators import is_int, is_ipv4, is_mac
 
 
