d14a8fde7a4f6033d50ff220529d03de5a0f6b75
Maksim <sysfar@gmail.com>
2017-11-29 17:42:14 +0300

    Fix Qtech.QSW profile

--- sa/profiles/Qtech/QSW/get_lldp_neighbors.py
+++ sa/profiles/Qtech/QSW/get_lldp_neighbors.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # ---------------------------------------------------------------------
-# Qtech.QSW.get_switchport
+# Qtech.QSW.get_lldp_neighbors
 # ---------------------------------------------------------------------
 # Copyright (C) 2007-2017 The NOC Project
 # See LICENSE for details
@@ -10,300 +10,134 @@
 import re
 # NOC modules
 from noc.core.script.base import BaseScript
-from noc.sa.interfaces.igetswitchport import IGetSwitchport
+from noc.sa.interfaces.igetlldpneighbors import IGetLLDPNeighbors
+from noc.lib.validators import is_int, is_ipv4, is_mac
 
 
 class Script(BaseScript):
-    name = "Qtech.QSW.get_switchport"
-    interface = IGetSwitchport
+    name = "Qtech.QSW.get_lldp_neighbors"
+    interface = IGetLLDPNeighbors
 
-    rx_interface = re.compile(
-        r"^\s*(?:Fast|Gigabit)?\s*Ethernet\s+(?P<interface>\S+)\s+(?:is|current state:)\s+(enabled|disabled),\s+port\s+link\s+is\s+(up|down)")
-    rx_mode = re.compile(r"^\s*Port\s+mode\s*:\s*(?P<mode>\S+)$")
-    rx_vlan_t = re.compile(r"^\s*Vlan\s+allowed\s*:\s*(?P<vlans>\S+)$")
-    rx_vlan_at = re.compile(r"^\s*Tagged\s+VLAN\s+ID\s*:\s*(?P<vlans>\S+)$")
-    rx_vlan_au = re.compile(r"^\s*Untagged\s+VLAN\s+ID\s*:\s*(?P<vlans>\S+)$")
+    rx_line = re.compile(
+        r"^Interface Ethernet (?P<interface>\S+)\s*\n"
+        r"^Port\s+LLDP:\s+\S+\s+Pkt\s+Tx:\s+\d+\s+Pkt\s+Rx:\s+\d+\s*\n"
+        r"^Total neighbor count: \d+\s*\n\n"
+        r"^Neighbor \(\d+\):\s*\n"
+        r"^TTL: \d+\(s\)\s*\n"
+        r"^Chassis ID:\s+(?P<chassis_id>\S+)\s*\n"
+        r"^Port ID:( port)? (?P<port_id>\S+)\s*\n"
+        r"^System Name: (?P<name>\S+)\s*\n"
+        r"(^System Description: (?P<system_description>.+)\s*\n)?"
+        r"(^Port Description: (?P<port_description>.+)\s*\n)?",
+        re.MULTILINE)
 
-    rx_description = re.compile(
-        r"^\s*(?P<interface>e\S+)(?P<description>.*)\n", re.MULTILINE)
-    #rx_channel_description = re.compile(
-    #r"^(?P<interface>Po\d+)\s+((?P<description>\S+)|)$", re.MULTILINE)
-    #rx_vlan_stack = re.compile(
-    #r"^(?P<interface>\S+)\s+(?P<role>\S+)\s*$", re.IGNORECASE)  # TODO
-
+    rx_mac = re.compile(r"^[0-9a-f]{4}\.[0-9a-f]{4}\.[0-9a-f]{4}$")
 
     def execute(self):
-
-        #TODO
-        # Get portchannels
-        #portchannels = self.scripts.get_portchannel()
-        portchannels = []
-        portchannel_members = []
-        #for p in portchannels:
-            #portchannel_members += p["members"]
-
-        # Get interafces status
-        interface_status = {}
-        port_vlans = {}
-        for s in self.scripts.get_interface_status():
-            interface_status[s["interface"]] = s["status"]
-
-        #TODO
-        # Get 802.1ad status if supported
-        vlan_stack_status = {}
+        r = []
+        # Try SNMP first
         """
-        try:
-            cmd = self.cli("show vlan-stacking")
-            for match in self.rx_vlan_stack.finditer(cmd):
-                if match.group("role").lower() == "tunnel":
-                    vlan_stack_status[int(match.group("interface"))] = True
-        except self.CLISyntaxError:
-            pass
-        """
-        # Try snmp first
-        """
-        # SNMP not working!
+        # SNMP not working
+
         if self.has_snmp():
-            def hex2bin(ports):
-                bin = [
-                    '0000', '0001', '0010', '0011',
-                    '0100', '0101', '0110', '0111',
-                    '1000', '1001', '1010', '1011',
-                    '1100', '1101', '1110', '1111',
-                    ]
-                ports = ["%02x" % ord(c) for c in ports]
-                p = ''
-                for c in ports:
-                   for i in range(len(c)):
-                        p += bin[int(c[i], 16)]
-                return p
             try:
-                # Make a list of tags for each interface or portchannel
-                port_vlans = {}
+
+        # lldpRemLocalPortNum
+        # lldpRemChassisIdSubtype lldpRemChassisId
+        # lldpRemPortIdSubtype lldpRemPortId
+        # lldpRemSysName lldpRemSysCapEnabled
                 for v in self.snmp.get_tables(
-                    ["1.3.6.1.2.1.17.7.1.4.2.1.3",
-                    "1.3.6.1.2.1.17.7.1.4.2.1.4",
-                    "1.3.6.1.2.1.17.7.1.4.2.1.5"], bulk=True):
-                    tagged = v[2]
-                    untagged = v[3]
+                    ["1.0.8802.1.1.2.1.4.1.1.2",
+                    "1.0.8802.1.1.2.1.4.1.1.4", "1.0.8802.1.1.2.1.4.1.1.5",
+                    "1.0.8802.1.1.2.1.4.1.1.6", "1.0.8802.1.1.2.1.4.1.1.7",
+                    "1.0.8802.1.1.2.1.4.1.1.9", "1.0.8802.1.1.2.1.4.1.1.12"
+                    ], bulk=True):
+                    local_interface = self.snmp.get(
+                        "1.3.6.1.2.1.31.1.1.1.1." + v[1], cached=True)
+                    remote_chassis_id_subtype = v[2]
+                    remotechassisid = ":".join(["%02x" % ord(c) for c in v[3]])
+                    remote_port_subtype = v[4]
+                    if remote_port_subtype == 7:
+                        remote_port_subtype = 5
+                    remote_port = v[5]
+                    remote_system_name = v[6]
+                    remote_capabilities = v[7]
 
-                    s = hex2bin(untagged)
-                    un = []
-                    for i in range(len(s)):
-                        if s[i] == '1':
-                            oid = "1.3.6.1.2.1.31.1.1.1.1." + str(i + 1)
-                            iface = self.snmp.get(oid, cached=True)
-                            if iface not in port_vlans:
-                                port_vlans.update(
-                                    {iface: {
-                                        "tagged": [],
-                                        "untagged": '',
-                                        }
-                                    })
-                            port_vlans[iface]["untagged"] = v[1]
-                            un += [str(i + 1)]
-
-                    s = hex2bin(tagged)
-                    for i in range(len(s)):
-                        if s[i] == '1' and str(i + 1) not in un:
-                            oid = "1.3.6.1.2.1.31.1.1.1.1." + str(i + 1)
-                            iface = self.snmp.get(oid, cached=True)
-                            if iface not in port_vlans:
-                                port_vlans.update(
-                                    {iface: {
-                                        "tagged": [],
-                                        "untagged": '',
-                                        }
-                                    })
-                            port_vlans[iface]["tagged"].append(v[1])
-
-                # Get switchport description
-                port_descr = {}
-                for iface, description in self.snmp.join_tables(
-                    "1.3.6.1.2.1.31.1.1.1.1", "1.3.6.1.2.1.31.1.1.1.18"):
-                    port_descr.update({iface: description})
-
-                # Get switchport data and overall result
-                r = []
-                swp = {}
-                write = False
-                for name in interface_status:
-                    if name in portchannel_members:
-                        for p in portchannels:
-                            if name in p["members"]:
-                                name = p["interface"]
-                                status = False
-                                for interface in p["members"]:
-                                    if interface_status.get(interface):
-                                        status = True
-                                description = port_descr[name]
-                                if not description:
-                                    description = ''
-                                members = p["members"]
-                                portchannels.remove(p)
-                                write = True
-                                break
-                    else:
-                        if interface_status.get(name):
-                            status = True
-                        else:
-                            status = False
-                        description = port_descr[name]
-                        if not description:
-                            description = ''
-                        members = []
-                        write = True
-                    if write:
-                        if name not in port_vlans:
-                            tagged = []
-                        else:
-                            tagged = port_vlans[name]["tagged"]
-                        swp = {
-                                "status": status,
-                                "description": description,
-                                "802.1Q Enabled": len(port_vlans.get(name,
-                                                '')) > 0,
-                                "802.1ad Tunnel": vlan_stack_status.get(name,
-                                                False),
-                                "tagged": tagged,
-                                }
-                        if name in port_vlans:
-                            if port_vlans[name]["untagged"]:
-                                swp["untagged"] = port_vlans[name]["untagged"]
-                        swp["interface"] = name
-                        swp["members"] = members
-                        r.append(swp)
-                        write = False
+                    i = {"local_interface": local_interface, "neighbors": []}
+                    n = {
+                        "remote_chassis_id_subtype": remote_chassis_id_subtype,
+                        "remote_chassis_id": remotechassisid,
+                        "remote_port_subtype": remote_port_subtype,
+                        "remote_port": remote_port,
+                        "remote_capabilities": remote_capabilities,
+                        }
+                    if remote_system_name:
+                        n["remote_system_name"] = remote_system_name
+                    i["neighbors"].append(n)
+                    r.append(i)
                 return r
+
             except self.snmp.TimeOutError:
                 pass
         """
 
         # Fallback to CLI
-        # Make a list of tags for each interface or portchannel
-        port_vlans = {}
-        port_channels = portchannels
+        try:
+            lldp = self.cli("show lldp interface")
+        except self.CLISyntaxError:
+            raise self.NotSupportedError()
+        for match in self.rx_line.finditer(lldp):
+            local_interface = match.group("interface")
+            remote_chassis_id = match.group("chassis_id")
+            remote_port = match.group("port_id")
+            remote_system_name = match.group("name")
+            system_description = match.group("system_description")
+            port_description = match.group("port_description")
 
+            # Build neighbor data
+            # Get capability
+            cap = 0
+            """
+            for c in match.group("capabilities").split(","):
+            if cap:
+                c = c.strip()
+                if c:
+                    cap |= {
+                        "O": 1, "r": 2, "B": 4,
+                        "W": 8, "R": 16, "T": 32,
+                        "C": 64, "S": 128, "D": 256,
+                        "H": 512, "TP": 1024,
+                    }[c]
+            """
+            # Get remote port subtype
+            remote_port_subtype = 5
+            if is_ipv4(remote_port):
+                # Actually networkAddress(4)
+                remote_port_subtype = 4
+            elif is_mac(remote_port):
+                # Actually macAddress(3)
+                remote_port_subtype = 3
+            elif is_int(remote_port):
+                # Actually local(7)
+                remote_port_subtype = 7
 
-        iface_conf = self.cli("show interface")
-        # Correct Qtech BUG:
-        iface_conf = iface_conf.replace("\n\n", "\n")
-        iface_conf = iface_conf.splitlines()
-        i = 0
-        L = len(iface_conf) - 2
-        while i < L:
-            match = self.rx_interface.match(iface_conf[i])
-            if match:
-                interface = match.group("interface")
-            else:
-                i += 1
-                continue
+            i = {"local_interface": local_interface, "neighbors": []}
+            n = {
+                "remote_chassis_id": remote_chassis_id,
+                "remote_port": remote_port,
+                "remote_capabilities": cap,
+                "remote_port_subtype": remote_port_subtype,
+            }
+            if remote_system_name and remote_system_name != "NULL":
+                n["remote_system_name"] = remote_system_name
+            if system_description and system_description != "NULL":
+                n["remote_system_description"] = system_description
+            if port_description and port_description != "NULL":
+                n["remote_port_description"] = port_description
 
-            if interface in portchannel_members:
-                for p in port_channels:
-                    if interface in p["members"]:
-                        interface = p["interface"]
-                        port_channels.remove(p)
-                        break
-            if interface not in port_vlans:
-                port_vlans.update({interface: {
-                                        "tagged": [],
-                                        "untagged": '',
-                                        }
-                                })
+                # TODO:
+                # n["remote_chassis_id_subtype"] = 4
 
-            i += 1
-            match_mod = self.rx_mode.match(iface_conf[i])
-            while not match_mod:
-                i += 1
-                match_mod = self.rx_mode.match(iface_conf[i])
-            mode = match_mod.group("mode")
-
-            i += 1
-            if 'trunk' == mode:
-                match = self.rx_vlan_t.match(iface_conf[i])
-                if match:
-                    vlans = match.group("vlans")
-                    list_vlans = self.expand_rangelist(vlans)
-                    port_vlans[interface]["tagged"] = list_vlans
-
-            elif 'access' == mode or 'hybrid' == mode:
-                match = self.rx_vlan_at.match(iface_conf[i])
-                if match:
-                    vlans = match.group("vlans")
-                    list_vlans = self.expand_rangelist(vlans)
-                    port_vlans[interface]["tagged"] = list_vlans
-
-                i += 1
-                match = self.rx_vlan_au.match(iface_conf[i])
-                if match:
-                    vl = match.group("vlans")
-                    vlans = vl.split(',')[0]
-                    if "-" in vlans:
-                        vlans = vlans.split("-")[0]
-                    port_vlans[interface]["untagged"] = vlans
-
-
-        iface_conf = []
-        # Why portchannels=[] ???????
-        # Get portchannels onse more!!!
-#        portchannels = self.scripts.get_portchannel()
-        portchannels = []
-        # Get switchport data and overall result
-        r = []
-        swp = {}
-        write = False
-        cmd = self.cli("show description interface")
-        for match in self.rx_description.finditer(cmd):
-            name = match.group("interface")
-            if name in portchannel_members:
-                for p in portchannels:
-                    if name in p["members"]:
-                        name = p["interface"]
-                        status = False
-                        for interface in p["members"]:
-                            if interface_status.get(interface):
-                                status = True
-                        cmd = "show description interface %s" % name
-                        desc = self.cli(cmd)
-                        match = self.rx_channel_description.search(desc)
-                        if match:
-                            description = match.group("description")
-                            if not description:
-                                description = ''
-                        else:
-                            description = ''
-                        members = p["members"]
-                        portchannels.remove(p)
-                        write = True
-                        break
-            else:
-                if interface_status.get(name):
-                    status = True
-                else:
-                    status = False
-                description = match.group("description")
-                if not description:
-                    description = ''
-                members = []
-                write = True
-            if write:
-                if name not in port_vlans:
-                    tagged = []
-                else:
-                    tagged = port_vlans[name]["tagged"]
-                swp = {
-                        "status": status,
-                        "description": description.strip(),
-                        "802.1Q Enabled": len(port_vlans.get(name, [])) > 0,
-                        "802.1ad Tunnel": vlan_stack_status.get(name, False),
-                        "tagged": tagged,
-                        }
-                if name in port_vlans:
-                    if port_vlans[name]["untagged"]:
-                        swp["untagged"] = port_vlans[name]["untagged"]
-                swp["interface"] = name
-                swp["members"] = members
-                r.append(swp)
-                write = False
+            i["neighbors"].append(n)
+            r.append(i)
         return r
