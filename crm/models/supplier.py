cd1c932d3081b9720a97a0afe85eb94f687a5396
Dmitry Volodin <dv@nocproject.org>
2017-12-01 00:39:47 +0300

    Supplier caching

--- crm/models/supplier.py
+++ crm/models/supplier.py
@@ -8,13 +8,18 @@
 
 # Python modules
 from __future__ import absolute_import
+from threading import Lock
+import operator
 # Third-party modules
 from mongoengine.document import Document
 from mongoengine.fields import StringField, ListField, BooleanField
+import cachetools
 # NOC modules
 from .supplierprofile import SupplierProfile
 from noc.lib.nosql import PlainReferenceField
 from noc.wf.models.state import State
+
+id_lock = Lock()
 
 
 class Supplier(Document):
@@ -34,5 +39,12 @@ class Supplier(Document):
     state = PlainReferenceField(State)
     tags = ListField(StringField())
 
+    _id_cache = cachetools.TTLCache(maxsize=100, ttl=60)
+
     def __unicode__(self):
         return self.name
+
+    @classmethod
+    @cachetools.cachedmethod(operator.attrgetter("_id_cache"), lock=lambda _: id_lock)
+    def get_by_id(cls, id):
+        return Supplier.objects.filter(id=id).first()
