ab79b30b5b76a0821ccce15272f2c0ec23642bf9
Dmitry Volodin <dv@nocproject.org>
2017-12-01 00:39:35 +0300

    Subscriber caching

--- crm/models/subscriber.py
+++ crm/models/subscriber.py
@@ -8,14 +8,19 @@
 
 # Python modules
 from __future__ import absolute_import
+from threading import Lock
+import operator
 # Third-party modules
 from mongoengine.document import Document
 from mongoengine.fields import StringField, ListField
+import cachetools
 # NOC modules
 from .subscriberprofile import SubscriberProfile
 from noc.main.models.remotesystem import RemoteSystem
 from noc.lib.nosql import PlainReferenceField
 from noc.wf.models.state import State
+
+id_lock = Lock()
 
 
 class Subscriber(Document):
@@ -44,5 +49,12 @@ class Subscriber(Document):
     # Object id in remote system
     remote_id = StringField()
 
+    _id_cache = cachetools.TTLCache(maxsize=100, ttl=60)
+
     def __unicode__(self):
         return self.name
+
+    @classmethod
+    @cachetools.cachedmethod(operator.attrgetter("_id_cache"), lock=lambda _: id_lock)
+    def get_by_id(cls, id):
+        return Subscriber.objects.filter(id=id).first()
