88873cfc23520d1bb4d6a68e0bd6f204e21cfdbe
Dmitry Volodin <dv@nocproject.org>
2017-12-04 12:58:45 +0300

    @workflow decorator sets default state

--- core/wf/decorator.py
+++ core/wf/decorator.py
@@ -6,8 +6,12 @@
 # See LICENSE for details
 # ----------------------------------------------------------------------
 
+# Python modules
+import logging
 # NOC modules
 from noc.models import is_document
+
+logger = logging.getLogger(__name__)
 
 
 def fire_event(self, event):
@@ -84,6 +88,44 @@ def model_set_state(self, state):
     self.state.on_enter_state(self)
 
 
+def _on_document_post_save(sender, document, *args, **kwargs):
+    if document.state is None:
+        # No state, set default one
+        # Get workflow
+        profile = getattr(document, getattr(document, "PROFILE_LINK", "profile"))
+        if not profile:
+            logger.debug("[%s] Cannot set default state: No profile", document)
+            return
+        new_state = profile.workflow.get_default_state()
+        if not new_state:
+            logger.debug(
+                "[%s] Cannot set default state: No default state for workflow %s",
+                document, profile.workflow.name)
+            return
+        logger.debug("[%s] Set initial state to '%s'",
+                     document, new_state.name)
+        document.set_state(new_state)
+
+
+def _on_model_post_save(sender, instance, *args, **kwargs):
+    if instance.state is None:
+        # No state, set default one
+        # Get workflow
+        profile = getattr(instance, getattr(instance, "PROFILE_LINK", "profile"))
+        if not profile:
+            logger.debug("[%s] Cannot set default state: No profile", instance)
+            return
+        new_state = profile.workflow.get_default_state()
+        if not new_state:
+            logger.debug(
+                "[%s] Cannot set default state: No default state for workflow %s",
+                instance, profile.workflow.name)
+            return
+        logger.debug("[%s] Set initial state to '%s'",
+                     instance, new_state.name)
+        instance.set_state(new_state)
+
+
 def workflow(cls):
     """
     @workflow decorator denotes models which have .state
@@ -99,10 +141,20 @@ def workflow(cls):
     cls.fire_transition = fire_transition
     if is_document(cls):
         # MongoEngine model
+        from mongoengine import signals as mongo_signals
         cls.set_state = document_set_state
+        mongo_signals.post_save.connect(
+            _on_document_post_save,
+            sender=cls
+        )
     else:
         # Django model
+        from django.db.models import signals as django_signals
         cls.set_state = model_set_state
+        django_signals.post_save.connect(
+            _on_model_post_save,
+            sender=cls
+        )
     cls.fire_transition = fire_transition
     cls.fire_event = fire_event
     return cls
