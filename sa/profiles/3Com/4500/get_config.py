8e089a486bf3283b240c27e79b05fd584c764aa6
savichev <savicheval@gmail.com>
2017-12-04 15:03:19 +0300

    get_config.py

---
+++ sa/profiles/3Com/4500/get_config.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------
+# 3Com.4500.get_config
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2013 The NOC Project
+# See LICENSE for details
+# ----------------------------------------------------------------------
+
+# NOC modules
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetconfig import IGetConfig
+
+
+class Script(BaseScript):
+    name = "3Com.4500.get_config"
+    interface = IGetConfig
+
+    def execute(self):
+        config = self.cli("display current-configuration")
+        return self.cleaned_config(config)
