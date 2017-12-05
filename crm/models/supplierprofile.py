5b3737c5a477d97f4ad36c6553ebdadd75d9d008
Dmitry Volodin <dv@nocproject.org>
2017-12-05 10:56:00 +0300

    crm: Common remote system integration scheme

--- crm/models/supplierprofile.py
+++ crm/models/supplierprofile.py
@@ -8,13 +8,16 @@
 
 # Third-party modules
 from mongoengine.document import Document
-from mongoengine.fields import StringField, ListField
+from mongoengine.fields import StringField, ListField, LongField
 # NOC modules
 from noc.lib.nosql import ForeignKeyField, PlainReferenceField
+from noc.main.models.remotesystem import RemoteSystem
 from noc.main.models.style import Style
 from noc.wf.models.workflow import Workflow
+from noc.core.bi.decorator import bi_sync
 
 
+@bi_sync
 class SupplierProfile(Document):
     meta = {
         "collection": "noc.supplierprofiles",
@@ -27,6 +30,13 @@ class SupplierProfile(Document):
     workflow = PlainReferenceField(Workflow)
     style = ForeignKeyField(Style, required=False)
     tags = ListField(StringField())
+    # Integration with external NRI and TT systems
+    # Reference to remote system object has been imported from
+    remote_system = PlainReferenceField(RemoteSystem)
+    # Object id in remote system
+    remote_id = StringField()
+    # Object id in BI
+    bi_id = LongField(unique=True)
 
     def __unicode__(self):
         return self.name
