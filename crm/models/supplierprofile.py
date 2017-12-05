0122b2bae6ceae7bbe36e395b1c5bf5cc8831081
Dmitry Volodin <dv@nocproject.org>
2017-12-05 19:56:56 +0300

    SubscriberProfile, SupplierProfile: .get_by_id() helper

--- crm/models/supplierprofile.py
+++ crm/models/supplierprofile.py
@@ -6,15 +6,21 @@
 # See LICENSE for details
 # ----------------------------------------------------------------------
 
+# Python modules
+import operator
+from threading import Lock
 # Third-party modules
 from mongoengine.document import Document
 from mongoengine.fields import StringField, ListField, LongField
+import cachetools
 # NOC modules
 from noc.lib.nosql import ForeignKeyField, PlainReferenceField
 from noc.main.models.remotesystem import RemoteSystem
 from noc.main.models.style import Style
 from noc.wf.models.workflow import Workflow
 from noc.core.bi.decorator import bi_sync
+
+id_lock = Lock()
 
 
 @bi_sync
@@ -38,5 +44,12 @@ class SupplierProfile(Document):
     # Object id in BI
     bi_id = LongField(unique=True)
 
+    _id_cache = cachetools.TTLCache(maxsize=100, ttl=60)
+
     def __unicode__(self):
         return self.name
+
+    @classmethod
+    @cachetools.cachedmethod(operator.attrgetter("_id_cache"), lock=lambda _: id_lock)
+    def get_by_id(cls, id):
+        return SupplierProfile.objects.filter(id=id).first()
