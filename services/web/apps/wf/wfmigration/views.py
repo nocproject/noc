c1d25acd7791f4f927621955d36aec3eea713d74
Dmitry Volodin <dv@nocproject.org>
2017-12-05 23:47:13 +0300

    workflow migration tool

---
+++ services/web/apps/wf/wfmigration/views.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------
+# wf.wfmigration application
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2017 The NOC Project
+# See LICENSE for details
+# ----------------------------------------------------------------------
+
+# NOC modules
+from noc.lib.app import ExtDocApplication
+from noc.wf.models.wfmigration import WFMigration
+from noc.core.translation import ugettext as _
+
+
+class WFMigrationApplication(ExtDocApplication):
+    """
+    WFMigration application
+    """
+    title = "WF Migration"
+    menu = [_("Setup"), _("Migrations")]
+    model = WFMigration
