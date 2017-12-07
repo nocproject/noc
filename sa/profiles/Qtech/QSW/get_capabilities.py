7cf2b87280b39dfac0d506cc8d85e2cfb36fd041
Maksim <sysfar@gmail.com>
2017-12-07 14:21:03 +0300

    Add Qtech.QSW get_capabilities

--- sa/profiles/Qtech/QSW/get_capabilities.py
+++ sa/profiles/Qtech/QSW/get_capabilities.py
@@ -15,7 +15,7 @@ class Script(BaseScript):
     name = "Qtech.QSW.get_capabilities"
 
     @false_on_cli_error
-    def has_lldp(self):
+    def has_lldp_cli(self):
         """
         Check box has lldp enabled
         """
