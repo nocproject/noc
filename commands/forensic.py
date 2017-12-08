a547325ff1db3d3e21c279709c1543fdf2b84f7c
Dmitry Volodin <dv@nocproject.org>
2017-12-08 18:13:07 +0300

    ./noc forensic incomplete --watch option

--- commands/forensic.py
+++ commands/forensic.py
@@ -12,6 +12,7 @@ import sys
 import re
 from collections import namedtuple
 import operator
+import time
 # NOC modules
 from noc.core.management.base import BaseCommand
 
@@ -28,50 +29,70 @@ class Command(BaseCommand):
         r"\[<([^\]]+)\]"
     )
 
+    REFRESH_INTERVAL = 1
+
     def add_arguments(self, parser):
         subparsers = parser.add_subparsers(
             dest="cmd",
             help="sub-commands help"
         )
         # sync
-        subparsers.add_parser(
+        incomplete_parser = subparsers.add_parser(
             "incomplete",
             help="Show incomplete operations"
+        )
+        incomplete_parser.add_argument(
+            "--watch",
+            action="store_true",
+            help="Watch mode"
         )
 
     def handle(self, cmd, *args, **options):
         getattr(self, "handle_%s" % cmd)(*args, **options)
 
-    def handle_incomplete(self):
+    def handle_incomplete(self, watch=False, *args, **kwargs):
+        def show():
+            self.print("\x1b[2J\x1b[1;1H%s Spans: %d/%d" % (time.strftime("%H:%M:%S"), n_closed, n_open))
+            self.print("Timestamp\t\tID\t\t\t\t\tServer\t\tService\t\tLabel")
+            for s in sorted(spans.values(), key=operator.attrgetter("ts")):
+                self.print("%s\t%s\t%s\t%s\t%s" % (s.ts, s.id, s.server, s.service, s.label))
+            if not spans:
+                self.print("  No spans")
+
         spans = {}
+        next_show = 0
+        n_open = 0
+        n_closed = 0
         for line in sys.stdin:
+            time.sleep(0.5)
             line = line.strip()
             if "[forensic] [>" in line:
                 # Open span
                 match = self.rx_open.search(line)
-                if not match:
-                    continue
-                s = SpanData(
-                    ts=match.group(1),
-                    id=match.group(2),
-                    server=match.group(3),
-                    service=match.group(4),
-                    label=match.group(5)
-                )
-                spans[s.id] = s
+                if match:
+                    s = SpanData(
+                        ts=match.group(1),
+                        id=match.group(2),
+                        server=match.group(3),
+                        service=match.group(4),
+                        label=match.group(5)
+                    )
+                    spans[s.id] = s
+                    n_open += 1
             elif "[forensic] [<" in line:
                 # Close span
                 match = self.rx_close.search(line)
-                if not match:
-                    continue
-                sid = match.group(2)
-                if sid in spans:
-                    del spans[sid]
-        if spans:
-            self.print("Timestamp\t\tID\t\t\t\t\tServer\t\tService\t\tLabel")
-        for s in sorted(spans.values(), key=operator.attrgetter("ts")):
-            self.print("%s\t%s\t%s\t%s\t%s" % (s.ts, s.id, s.server, s.service, s.label))
-
+                if match:
+                    sid = match.group(2)
+                    if sid in spans:
+                        del spans[sid]
+                        n_closed += 1
+            if watch:
+                t = time.time()
+                if t > next_show:
+                    next_show = t + self.REFRESH_INTERVAL
+                    show()
+        show()
 
 if __name__ == "__main__":
     Command().run()
