4e677db667556fd9827748822297e565f0a3f91d
Dmitry Volodin <dv@nocproject.org>
2017-12-15 12:44:56 +0300

    Allocation Group

---
+++ ui/web/inv/allocationgroup/LookupField.js
@@ -0,0 +1,13 @@
+//---------------------------------------------------------------------
+// NOC.inv.allocationgroup.Lookup
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2017 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.inv.allocationgroup.LookupField");
+
+Ext.define("NOC.inv.allocationgroup.LookupField", {
+    extend: "NOC.core.LookupField",
+    alias: "widget.inv.allocationgroup.LookupField",
+    uiStyle: "medium"
+});
---
+++ ui/web/inv/allocationgroup/Model.js
@@ -0,0 +1,48 @@
+//---------------------------------------------------------------------
+// inv.allocationgroup Model
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2017 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.inv.allocationgroup.Model");
+
+Ext.define("NOC.inv.allocationgroup.Model", {
+    extend: "Ext.data.Model",
+    rest_url: "/inv/allocationgroup/",
+
+    fields: [
+        {
+            name: "id",
+            type: "string"
+        },
+        {
+            name: "remote_id",
+            type: "string"
+        },
+        {
+            name: "name",
+            type: "string"
+        },
+        {
+            name: "remote_system",
+            type: "string"
+        },
+        {
+            name: "remote_system__label",
+            type: "string",
+            persist: false
+        },
+        {
+            name: "bi_id",
+            type: "string"
+        },
+        {
+            name: "description",
+            type: "string"
+        },
+        {
+            name: "tags",
+            type: "auto"
+        }
+    ]
+});
