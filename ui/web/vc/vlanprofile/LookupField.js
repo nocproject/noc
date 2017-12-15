772d60930836165c1b90ca0c12f1eb91f9bba5d0
Dmitry Volodin <dv@nocproject.org>
2017-12-15 14:56:49 +0300

    VLAN Profile

---
+++ ui/web/vc/vlanprofile/LookupField.js
@@ -0,0 +1,12 @@
+//---------------------------------------------------------------------
+// NOC.vc.vlanprofile.Lookup
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2017 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.vc.vlanprofile.LookupField");
+
+Ext.define("NOC.vc.vlanprofile.LookupField", {
+    extend: "NOC.core.LookupField",
+    alias: "widget.vc.vlanprofile.LookupField"
+});
