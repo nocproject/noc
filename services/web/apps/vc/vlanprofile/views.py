967316322d787188c8540b8fae752ac98297ab10
Dmitry Volodin <dv@nocproject.org>
2017-12-15 14:58:33 +0300

    VLAN Profile backend

---
+++ services/web/apps/vc/vlanprofile/views.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------
+# vc.vlanprofile application
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2017 The NOC Project
+# See LICENSE for details
+# ----------------------------------------------------------------------
+
+# NOC modules
+from noc.lib.app import ExtDocApplication, view
+from noc.vc.models.vlanprofile import VLANProfile
+from noc.core.translation import ugettext as _
+
+
+class VLANProfileApplication(ExtDocApplication):
+    """
+    VLANProfile application
+    """
+    title = "VLAN Profile"
+    menu = [_("Setup"), _("VLAN Profiles")]
+    model = VLANProfile
+
+    def field_row_class(self, o):
+        return o.style.css_class_name if o.style else ""
