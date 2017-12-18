833201a8a492d9a181ea5d48051e915a642a0b23
ircop <wingman@noc.ip-home.net>
2017-12-18 17:41:40 +0300

    discussion fix

--- sa/profiles/DLink/DGS3100/get_capabilities.py
+++ sa/profiles/DLink/DGS3100/get_capabilities.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # ---------------------------------------------------------------------
-# DLink.DxS.get_capabilities
+# DLink.DGS3100.get_capabilities
 # ---------------------------------------------------------------------
 # Copyright (C) 2007-2017 The NOC Project
 # See LICENSE for details
@@ -25,7 +25,7 @@ class Script(BaseScript):
         Check box has lldp enabled
         """
         cmd = self.cli("show lldp")
-        return self.rx_lldp.search(cmd) is not None
+        return bool(self.rx_lldp.search(cmd))
 
     @false_on_cli_error
     def has_stp_cli(self):
