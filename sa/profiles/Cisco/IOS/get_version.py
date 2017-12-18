c4ba7acc502645b4892f095ad0cb2036c838e00b
Dmitry S. Luhtionov <dmitryluhtionov@gmail.com>
2017-12-18 11:41:07 +0200

    Update Cisco.IOS.get_version to report proper platform

--- sa/profiles/Cisco/IOS/get_version.py
+++ sa/profiles/Cisco/IOS/get_version.py
@@ -2,7 +2,7 @@
 # ---------------------------------------------------------------------
 # Cisco.IOS.get_version
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2011 The NOC Project
+# Copyright (C) 2007-2017 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
@@ -19,7 +19,7 @@ class Script(BaseScript):
     cache = True
     interface = IGetVersion
 
-    rx_ver = re.compile(
+    rx_version = re.compile(
         r"^(?:Cisco IOS Software( \[Everest\])?,.*?|IOS \(tm\)) (IOS[\-\s]XE Software,\s)?"
         r"(?P<platform>.+?) Software \((?P<image>[^)]+)\), (Experimental )?"
         r"Version (?P<version>[^\s,]+)", re.MULTILINE | re.DOTALL)
@@ -32,36 +32,131 @@ class Script(BaseScript):
         re.IGNORECASE | re.MULTILINE)
     rx_invalid_platforms = re.compile(
         r"IOS[\-\s]XE|EGR|Catalyst L3 Switch|s\d+\S+")
+    rx_item = re.compile(
+        r"^NAME: \"(?P<name>[^\"]+)\", DESCR: \"(?P<descr>[^\"]+)\"\n"
+        r"PID:\s+(?P<pid>\S+)?\s*,\s+VID:\s+(?P<vid>\S+)?\s*, "
+        r"SN:\s(?P<serial>\S+)?", re.MULTILINE | re.DOTALL)
+    rx_7100 = re.compile(
+        r"^(?:uBR|CISCO)?71(?:20|40|11|14)(-\S+)? "
+        r"(?:Universal Broadband Router|chassis)")
+    rx_ver = re.compile(
+        r"Model revision number\s+:\s+(?P<revision>\S+)\s*\n"
+        r"Motherboard revision number\s+:\s+\S+\s*\n"
+        r"Model number\s+:\s+(?P<part_no>\S+)\s*\n"
+        r"System serial number\s+:\s+(?P<serial>\S+)\s*\n",
+        re.IGNORECASE | re.MULTILINE | re.DOTALL)
+    rx_ver1 = re.compile(
+        r"^cisco (?P<part_no>\S+) \(\S+\) processor( "
+        r"\(revision(?P<revision>.+?)\))? with",
+        re.IGNORECASE | re.MULTILINE)
+    IGNORED_SERIAL = set([
+        "H22L714"
+    ])
+    IGNORED_NAMES = set([
+        "c7201"
+    ])
 
     def execute(self):
         if self.has_snmp():
+            # Try to find other varians, like this
+            # https://wiki.opennms.org/wiki/Hardware_Inventory_Entity_MIB
+            platform = ""
             try:
                 v = self.snmp.get(mib["SNMPv2-MIB::sysDescr.0"], cached=True)
                 if v:
+                    s = ""
                     match = self.re_search(self.rx_snmp_ver, v)
                     platform = match.group("platform")
+                    # inventory
+                    p = self.snmp.get("1.3.6.1.2.1.47.1.1.1.1.2.1001")
+                    if p and p.startswith("WS-C"):
+                        platform = p
+                        s = self.snmp.get("1.3.6.1.2.1.47.1.1.1.1.11.1001")
+                    else:
+                        # Found in WS-C3650-48TD
+                        p = self.snmp.get("1.3.6.1.2.1.47.1.1.1.1.2.1000")
+                        if p and p.startswith("WS-C"):
+                            platform = p
+                            s = self.snmp.get("1.3.6.1.2.1.47.1.1.1.1.11.1000")
+                        else:
+                            # CISCO-ENTITY-MIB::entPhysicalModelName
+                            p = self.snmp.get("1.3.6.1.2.1.47.1.1.1.1.13.1")
+                            if p:
+                                if p.startswith("CISCO"):
+                                    p = p[5:]
+                                if p.endswith("-CHASSIS"):
+                                    p = p[:-8]
+                                platform = p
                     if not self.rx_invalid_platforms.search(platform):
-                        return {
+                        r = {
                             "vendor": "Cisco",
-                            "platform": match.group("platform"),
+                            "platform": platform,
                             "version": match.group("version"),
                             "image": match.group("image"),
                         }
+                        if s:
+                            r["attributes"] = {}
+                            r["attributes"]["Serial Number"] = s
+                        return r
             except self.snmp.TimeOutError:
                 pass
-        v = self.cli("show version", cached=True)
-        match = self.re_search(self.rx_ver, v)
-        platform = match.group("platform")
-        if self.rx_invalid_platforms.search(platform):
-            # Process IOS XE platform
-            pmatch = self.re_search(self.rx_platform, v)
-            if pmatch:
-                platform = pmatch.group("platform")
-            if platform.startswith("WS-"):
-                platform = platform[3:]
-        return {
+
+        # Fallback to CLI
+        c = ""
+        try:
+            v = self.cli("show inventory raw")
+            i = 0
+            for match in self.rx_item.finditer(v):
+                serial = None
+                name = match.group("name")
+                pid = match.group("pid")
+                descr = match.group("descr")
+                if name in self.IGNORED_NAMES:
+                    continue
+                if (
+                    (
+                        i == 0 or pid.startswith("CISCO") or
+                        pid.startswith("WS-C")
+                    ) and
+                    not pid.startswith("WS-CAC-") and
+                    not pid.endswith("-MB") and
+                    "Clock" not in descr and "VTT FRU" not in descr and
+                    "C2801 Motherboard " not in descr
+                ):
+                    if pid in ("", "N/A"):
+                        if self.rx_7100.search(descr):
+                            pid = "CISCO7100"
+                    if (len(pid) - len(descr) == 2) and pid[len(descr)] == "-":
+                        pid = descr
+                    platform = pid
+                    serial = match.group("serial")
+                    break
+                i = 1
+            if serial in self.IGNORED_SERIAL:
+                serial = None
+        except self.CLISyntaxError:
+            c = self.cli("show version", cached=True)
+            match = self.rx_ver.search(c)
+            if match:
+                platform = match.group("part_no")
+                serial = match.group("serial")
+            else:
+                match = self.rx_ver1.search(c)
+                if match:
+                    platform = match.group("part_no")
+                    serial = None
+                else:
+                    raise self.NotSupportedError()
+        if c == "":
+            c = self.cli("show version", cached=True)
+        match = self.rx_version.search(c)
+        r = {
             "vendor": "Cisco",
             "platform": platform,
             "version": match.group("version"),
             "image": match.group("image"),
         }
+        if bool(serial):
+            r["attributes"] = {}
+            r["attributes"]["Serial Number"] = serial
+        return r
