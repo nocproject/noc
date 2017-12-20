4d8016333cce8b07565aee7bb6cefffbc7d73569
Фантомас Михалыч <fantmas@gmail.com>
2017-12-20 17:43:25 +0300

    Update views.py

--- services/web/apps/inv/reportifacestatus/views.py
+++ services/web/apps/inv/reportifacestatus/views.py
@@ -11,42 +11,32 @@ from django import forms
 from noc.lib.app.simplereport import SimpleReport
 from noc.sa.models.useraccess import UserAccess
 from noc.sa.models.managedobject import ManagedObject
-from noc.inv.models.object import Object
-from noc.sa.models.managedobjectselector import ManagedObjectSelector
 from noc.core.translation import ugettext as _
 from noc.inv.models.interfaceprofile import InterfaceProfile
 from noc.inv.models.interface import Interface
-from noc.lib.text import list_to_ranges, split_alnum
-
+from noc.lib.text import list_to_ranges
 class ReportForm(forms.Form):
     sel = forms.ModelChoiceField(
             label=_("Interface Profile"),
             required=True,
             queryset=InterfaceProfile.objects.order_by("name"))
-
 class ReportIfaceStatus(SimpleReport):
     title = _("Отчет статуса интерфейсов")
     form = ReportForm
-
     def get_data(self, request, sel):
         data = []
         qs = ManagedObject.objects
         if not request.user.is_superuser:
             qs = ManagedObject.objects.filter(administrative_domain__in=UserAccess.get_domains(request.user))
-
         columns = [_("Managed Objects"), _("Switch availability status"), _("IP address"), _("Model"), _("Software"), _("Port name"), _("Port status"), _("Link status"), _("VLAN id")]
-
         ip = InterfaceProfile.objects.get(name=sel.name)
         interf = Interface.objects.filter(profile=ip, type="physical")
-
         for i in interf:
             si = list(i.subinterface_set.filter(enabled_afi="BRIDGE"))
             if len(si) == 1:
                 si = si[0]
-
             try:
                 tagged = (list_to_ranges(si.tagged_vlans).replace(",", ", ")).strip()
-
                 data += [[ i.managed_object.name,
                            "Is available" if i.managed_object.get_status() == True else "Not available",
                            i.managed_object.address,
@@ -59,7 +49,6 @@ class ReportIfaceStatus(SimpleReport):
                         ]]
             except:
                 pass
-
         return self.from_dataset(
             title=self.title,
             columns=columns,
