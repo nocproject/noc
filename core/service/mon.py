a4a779f7c7b74d5a0f143ab6ab413d456f0a1cab
Aleksey Shirokih <shirokih@nocproject.org>
2017-12-27 23:23:30 +0300

    add login metrics

--- core/service/mon.py
+++ core/service/mon.py
@@ -7,13 +7,37 @@
 # ----------------------------------------------------------------------
 
 # Third-party modules
+import string
 import tornado.web
 import ujson
 
+TR = string.maketrans(".-\"", "___")
+
 
 class MonRequestHandler(tornado.web.RequestHandler):
+    """
+    Response with general purpose monitoring data
+    """
+
     def initialize(self, service):
         self.service = service
 
     def get(self):
-        self.write(ujson.dumps(self.service.get_mon_data()))
+        """
+        Resolves tuples from metrics name and responds with json
+        :return:
+        """
+        mdata = self.service.get_mon_data()
+        response = {}
+        for key in mdata:
+            if isinstance(key, tuple):
+                metric_name = key[0]
+                for k in key[1:]:
+                    metric_name += "_" + "_".join(k)
+            else:
+                metric_name = key.lower()
+            cleared_name = str(metric_name).translate(TR)
+            response.update({cleared_name: mdata[key]})
+        self.write(
+            ujson.dumps(response, indent=2, sort_keys=True)
+        )
