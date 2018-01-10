61604d8173185037a7f131be8adb51b82636918f
Dmitry Volodin <dv@nocproject.org>
2018-01-10 20:43:43 +0300

    ./noc ensure-index: Drop stale unique indexes

--- commands/ensure-indexes.py
+++ commands/ensure-indexes.py
@@ -37,11 +37,42 @@ class Command(BaseCommand):
             if model._meta.get('auto_create_index', True):
                 continue
             # Index model
-            self.print("[%s] Checking indexes" % model_id)
-            model.ensure_indexes()
+            self.index_model(model_id, model)
         # @todo: Detect changes
         self.print("OK")
 
+    def index_model(self, model_id, model):
+        """
+        Create necessary indexes for model
+        :param model_id: model id
+        :param model: model class
+        :return:
+        """
+        self.print("[%s] Checking indexes" % model_id)
+        coll = model._get_collection()
+        # Get existing unique indexes
+        idx_info = coll.index_information()
+        x_indexes = {}  # fields -> name
+        for xn in idx_info:
+            if idx_info[xn].get("unique", False):
+                fields = ",".join(str(k[0]) for k in idx_info[xn]["key"])
+                x_indexes[fields] = xn
+        # Get declared unique indexes
+        if x_indexes:
+            xspecs = model._meta["index_specs"]
+            for xi in xspecs:
+                if not xi.get("unique", False):
+                    continue
+                fields = ",".join(str(k[0]) for k in xi["fields"])
+                if fields in x_indexes:
+                    del x_indexes[fields]
+            # Delete state unique indexes
+            for fn in x_indexes:
+                self.print("[%s] Drop stale unique index %s" % (model_id, x_indexes[fn]))
+                coll.drop_index(x_indexes[fn])
+        # Apply indexes
+        model.ensure_indexes()
+        #self.print("[%s] Indexes: %s || %s" % (model_id, model.list_indexes(), model._get_collection().index_information()))
 
 if __name__ == "__main__":
     Command().run()
