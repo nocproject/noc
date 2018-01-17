e50117a49b6667b4d8ed2317be4c822e3f40f31a
Dmitry Volodin <dv@nocproject.org>
2018-01-17 17:23:30 +0300

    nsq sync publisher

---
+++ core/nsq/error.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------
+# NSQ Errors
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2018 The NOC Project
+# See LICENSE for details
+# ----------------------------------------------------------------------
+
+# NOC modules
+from noc.core.error import NOCError
+
+
+class NSQPubError(NOCError):
+    pass
