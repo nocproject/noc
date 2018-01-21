928ba742716d13c2837571f3873abb6749420c48
Maksim <sysfar@gmail.com>
2018-01-21 18:56:38 +0300

    Add Hikvision Profile

---
+++ sa/profiles/Hikvision/DS2CD/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# Vendor: Hikvision
+# OS:     DS2CD
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2018 The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+
+from noc.core.profile.base import BaseProfile
+
+class Profile(BaseProfile):
+    name = "Hikvision.DS2CD"
+
