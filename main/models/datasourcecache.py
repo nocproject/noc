ec140c2d8467e382acfbcd74ba3d8d06fad38995
Dmitry Volodin <dv@nocproject.org>
2018-01-23 14:03:22 +0300

    #543: Optimize chunked datasource storage

--- main/models/datasourcecache.py
+++ main/models/datasourcecache.py
@@ -2,7 +2,7 @@
 # ----------------------------------------------------------------------
 # DataSourceCache
 # ----------------------------------------------------------------------
-# Copyright (C) 2007-2017 The NOC Project
+# Copyright (C) 2007-2018 The NOC Project
 # See LICENSE for details
 # ----------------------------------------------------------------------
 
@@ -17,8 +17,9 @@ from mongoengine.fields import (StringField, BinaryField,
 import bson
 
 logger = logging.getLogger(__name__)
-CURRENT_VERSION = 2
+CURRENT_VERSION = 3
 # 16777216 Max MongoDB Document size - 16Mb
+# bzip decoder usually reduces size
 MAX_DATA_SIZE = 16 * 1024 * 1024 - 1024 * 1024
 
 
@@ -38,6 +39,8 @@ class DataSourceCache(Document):
     expires = DateTimeField()
     chunks = IntField(min_value=0, max_value=5)
     version = IntField()
+    # Next chunk name
+    next_name = StringField()
 
     @classmethod
     def get_data(cls, name):
@@ -46,56 +49,63 @@ class DataSourceCache(Document):
         :param name:
         :return:
         """
-        d = cls._get_collection().find_one({"_id": name})
-        if not d:
-            return None
-        if d["version"] < CURRENT_VERSION:
-            # Version bump, rebuild cache
-            return None
-        if d.get("chunks") > 0:
-            for i in range(0, d["chunks"]):
-                chunk = cls._get_collection().find_one({"_id": "%s_%02d" % (name, i)})
-                d["data"] += chunk["data"]
-        return cls.decode(d["data"])
+        data = []
+        coll = DataSourceCache._get_collection()
+        while name:
+            d = coll.find_one({"_id": name})
+            if not d:
+                # Not found or broken chain
+                return None
+            if d["version"] != CURRENT_VERSION:
+                # Version bump, rebuild cache
+                return None
+            data += [d["data"]]
+            # Proceed to next chunk when necessary
+            name = d.get("next_name", None)
+        # Finally, decode result
+        # avoid string catenation whenever possible
+        return cls.decode("".join(data) if len(data) > 1 else data[0])
 
     @classmethod
-    def set_data(cls, name, data, ttl, chunks=0):
-        # Compressed size: 17425159
+    def set_data(cls, name, data, ttl):
+        """
+        Write data to cache
+        :param name:
+        :param data:
+        :param ttl:
+        :return:
+        """
         data = cls.encode(data)
-        size = len(data)
-        if size > MAX_DATA_SIZE:
-            logger.warning("[%s] Compressed size data: %s over max size %s" % (name, size, MAX_DATA_SIZE))
-            chunks = size // MAX_DATA_SIZE
-        cls._get_collection().update({
-            "_id": name
-        }, {
-            "$set": {
-                "data": bson.Binary(data[:MAX_DATA_SIZE]),
-                "version": CURRENT_VERSION,
-                "expires": datetime.datetime.now() + datetime.timedelta(seconds=ttl),
-                "chunks": chunks
-            },
-            "$setOnInsert": {
-                "name": name
-            }
-        }, upsert=True)
-        for i in range(0, chunks):
-            data = data[MAX_DATA_SIZE * (i + 1):MAX_DATA_SIZE * (i + 2)]
-            name = "%s_%02d" % (name, i)
-            # cls.set_data(name, data, ttl, chunks, chunk=True)
-            cls._get_collection().update({
+        coll = DataSourceCache._get_collection()
+        n_chunk = 0
+        fmt_chunk_name = "%s.%%d" % name
+        expires = datetime.datetime.now() + datetime.timedelta(seconds=ttl),
+        while data:
+            # Split chunk and rest of data
+            chunk, data = data[:MAX_DATA_SIZE], data[MAX_DATA_SIZE:]
+            # Generate next chunk name when data left
+            if data:
+                n_chunk += 1
+                next_name = fmt_chunk_name % n_chunk
+            else:
+                next_name = None
+            logger.info("Writing chunk %s", name)
+            # Update chunk
+            coll.update_one({
                 "_id": name
             }, {
                 "$set": {
-                    "data": bson.Binary(data),
+                    "data": bson.Binary(chunk),
                     "version": CURRENT_VERSION,
-                    "expires": datetime.datetime.now() + datetime.timedelta(seconds=ttl),
-                    "chunks": chunks
+                    "expires": expires,
+                    "next_name": next_name
                 },
                 "$setOnInsert": {
                     "name": name
                 }
             }, upsert=True)
+            # Name for next chunk
+            name = next_name
 
     @classmethod
     def encode(cls, data):
