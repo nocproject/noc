4db4bf594d0b8261a443dd84409afb0bc121243a
Andrey Vertiprahov <aversanta@gmail.com>
2018-01-28 11:01:11 +0500

    Fix PEP8

--- sa/models/managedobject.py
+++ sa/models/managedobject.py
@@ -489,7 +489,7 @@ class ManagedObject(Model):
 
     @classmethod
     @cachetools.cachedmethod(operator.attrgetter("_bi_id_cache"),
-                  lock=lambda _: id_lock)
+                             lock=lambda _: id_lock)
     def get_by_bi_id(cls, id):
         mo = ManagedObject.objects.filter(bi_id=id)[:1]
         if mo:
@@ -813,7 +813,7 @@ class ManagedObject(Model):
             return default
         try:
             return int(v)
-        except:
+        except:  # noqa
             return default
 
     def set_attr(self, name, value):
@@ -989,7 +989,7 @@ class ManagedObject(Model):
         engine = Engine(self)
         try:
             engine.check()
-        except:
+        except:  # noqa
             logger.error("Failed to validate config for %s", self.name)
             error_report()
 
