c15a6f110b68d51a29b39a70b36fe22670b462db
Dmitry Volodin <dv@nocproject.org>
2018-01-30 19:55:41 +0300

    PEP8

--- services/web/apps/main/ref/views.py
+++ services/web/apps/main/ref/views.py
@@ -2,7 +2,7 @@
 # ---------------------------------------------------------------------
 # main.ref application
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2017 The NOC Project
+# Copyright (C) 2007-2018 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
@@ -24,7 +24,6 @@ from noc.main.models.notificationgroup import USER_NOTIFICATION_METHOD_CHOICES
 from noc.cm.validators.base import validator_registry
 from noc.core.profile.loader import loader as profile_loader
 from noc.core.script.loader import loader as script_loader
-from noc.core.translation import ugettext as _
 
 
 class RefAppplication(ExtApplication):
@@ -87,16 +86,16 @@ class RefAppplication(ExtApplication):
         :return:
         """
         return sorted(({"id": s[0], "label": s[1]} for s in stencil_registry.choices),
-            key=lambda x: x["label"])
+                      key=lambda x: x["label"])
 
     def build_model(self):
         """
         Model Names
         :return:
         """
-        return sorted(({"id": m._meta.db_table,
-                        "label": m._meta.db_table}
-            for m in models.get_models()),
+        return sorted(
+            ({"id": m._meta.db_table, "label": m._meta.db_table}
+             for m in models.get_models()),
             key=lambda x: x["label"])
 
     def build_modcol(self):
@@ -243,4 +242,3 @@ class RefAppplication(ExtApplication):
             }
         else:
             return data
-
