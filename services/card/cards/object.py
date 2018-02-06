38e2207889581d40cd85620333782e790806f076
Alexey Zakharov <fantmas@gmail.com>
2018-02-06 16:41:02 +0300

    Update object.py

--- services/card/cards/object.py
+++ services/card/cards/object.py
@@ -9,14 +9,16 @@
 # NOC modules
 from base import BaseCard
 from noc.inv.models.object import Object
-
+from noc.sa.models.managedobject import ManagedObject
 
 class ObjectCard(BaseCard):
+    ''' ObjectCard '''
     name = "object"
     default_template_name = "object"
     model = Object
-
+    
     def get_data(self):
+        ''' get_data '''
         # Get path
         path = [{
             "id": self.object.id,
@@ -27,22 +29,34 @@ class ObjectCard(BaseCard):
             c = Object.get_by_id(c)
             if not c:
                 break
-            path.insert(0, {
-                "id": c.id,
-                "name": c.name
-            })
+            if c.name != "Root":
+                path.insert(0, {
+                    "id": c.id,
+                    "name": c.name
+                })
             c = c.container
+        
         # Get children
+        
         children = []
-        for o in Object.objects.filter(container=self.object.id):
+        
+        for o in ManagedObject.objects.filter(container=self.object.id):
             children += [{
                 "id": o.id,
                 "name": o.name,
-                "object": o
+                "address": o.address,
+                "platform": o.platform.name if o.platform else "Unknown",
+                "version": o.version.version if o.version else "",
+                "description": o.description,
+                "object_profile": o.object_profile.id,
+                "object_profile_name": o.object_profile.name,
+                "segment": o.segment
             }]
-        #
+
         return {
             "object": self.object,
             "path": path,
+            "location": self.object.get_data("address", "text"),
+            "id": self.object.id,
             "children": children
         }
