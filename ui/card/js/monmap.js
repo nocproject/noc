3b530926652e2eab248be92e37c8984deed32549
uncol <d.a.zakharov@gmail.com>
2018-02-14 23:54:13 +0300

    fix monmap filter

--- ui/card/js/monmap.js
+++ ui/card/js/monmap.js
@@ -7,11 +7,12 @@
 var Monmap = function() {
     this.map = null;
     this.mapFilter = {statuses: [], isNotInit: true};
+    this.refreshRange = 10000;
+    this.timeoutId = null;
 };
 
 Monmap.prototype.initialize = function(lon, lat, zoom) {
-    var me = this,
-        q = this.parseQuerystring(),
+    var q = this.parseQuerystring(),
         lon = q.lon ? parseFloat(q.lon) : lon || 135.656987,
         lat = q.lat ? parseFloat(q.lat) : lat || 55.005569,
         scale = q.zoom ? parseInt(q.zoom) : zoom || 5;
@@ -28,8 +29,8 @@ Monmap.prototype.initialize = function(lon, lat, zoom) {
     this.map.setView([lat, lon], scale);
     // Init markerCluster
     // doc: https://github.com/Leaflet/Leaflet.markercluster
-    this.clusters = L.markerClusterGroup({
-        chunkedLoading: true,
+    this.markerClusterGroup = L.markerClusterGroup({
+        chunkedLoading: false,
         spiderfyDistanceMultiplier: 2.5,
         // spiderfyOnMaxZoom: false,
         // showCoverageOnHover: false,
@@ -68,7 +69,7 @@ Monmap.prototype.initialize = function(lon, lat, zoom) {
             });
         }
     });
-    me.poll_data();
+    this.poll_data();
 };
 
 Monmap.prototype.parseQuerystring = function() {
@@ -88,8 +89,8 @@ Monmap.prototype.run = function(maintenance, lon, lat, zoom) {
 };
 
 Monmap.prototype.poll_data = function() {
-    var me = this,
-        bbox = me.map.getBounds(),
+    var me = this;
+    var bbox = me.map.getBounds(),
         w = bbox.getWest(),
         e = bbox.getEast(),
         n = bbox.getNorth(),
@@ -97,18 +98,27 @@ Monmap.prototype.poll_data = function() {
         zoom = me.map.getZoom();
 
     $("#summary").html('<i class="fa fa-spinner fa-spin"></i>' + "Loading ...");
-    // $.getJSON("/ui/card/js/data.json", function(data) { // test
-    $.ajax("/api/card/view/monmap/ajax/?z=" + zoom + "&w=" + w + "&e=" + e + "&n=" + n + "&s=" + s + "&maintenance=" + this.maintenance + "&object_id=" + this.objectId).done(function(data) {
+    $.getJSON("/ui/card/js/data.json")
+    .done(function(data) {              // test
+        // $.ajax("/api/card/view/monmap/ajax/?z=" + zoom + "&w=" + w + "&e=" + e + "&n=" + n + "&s=" + s + "&maintenance=" + me.maintenance + "&object_id=" + me.objectId)
+        // .done(function(data) {
         // Init statuses
         if(me.mapFilter.isNotInit) {
             me.mapFilter.isNotInit = false;
             me.mapFilter.statuses = $(data.summary).find('span')
             .map(function() {
                 var id = $(this).attr('id');
-                return {id: id, value: "1", group: L.featureGroup.subGroup(me.clusters)}
+                $(this).css("opacity", "1");
+                return {id: id, value: "1", group: L.featureGroup.subGroup(me.markerClusterGroup)}
             }).get();
+        } else {
+            me.map.removeLayer(me.markerClusterGroup);
+            me.markerClusterGroup.clearLayers();
+            me.mapFilter.statuses
+            .forEach(function(v) {
+                v.group.clearLayers();
+            });
         }
-        me.clusters.clearLayers();
         for(var i = 0; i < data.objects.length; i++) {
             var a = data.objects[i];
             var color, fillColor;
@@ -159,21 +169,27 @@ Monmap.prototype.poll_data = function() {
 
             var marker = L.circleMarker(L.latLng(a.y, a.x), markerOptions);
             marker.bindPopup(title);
-            [].concat.apply([], a.objects
+            // distribution by groups
+            var statuses = [].concat.apply([], a.objects // flatMap
                 .map(function(e) {
                     return e.services
                 })
             )
+            .filter(function(x, i, a) { // unique values
+                return a.indexOf(x) === i;
+            });
+            statuses
             .forEach(function(statusId) {
                 var group;
-                var groups = me.mapFilter.statuses.filter(function(e) {
+                var groups = me.mapFilter.statuses
+                .filter(function(e) {
                     return e.id === statusId;
                 });
 
                 if(groups.length) {
                     group = groups[0].group;
                 } else {
-                    var status = {id: statusId, value: "1", group: L.featureGroup.subGroup(me.clusters)};
+                    var status = {id: statusId, value: "1", group: L.featureGroup.subGroup(me.markerClusterGroup)};
                     group = status.group;
                     me.mapFilter.statuses.push(status);
                 }
@@ -183,45 +199,69 @@ Monmap.prototype.poll_data = function() {
         me.mapFilter.statuses.forEach(function(value) {
             value.group.addTo(me.map);
         });
-        me.map.addLayer(me.clusters);
+        me.map.addLayer(me.markerClusterGroup);
         var summary = $('#summary');
         // Replace summary
         summary.html(data.summary);
         // Attach control
-        summary.find('i,span').click(me, function(e) {
-            e.data.filterBy(this, $(this).attr('id'), $(this).prop('tagName') === "I");
+        summary.find("i,span").click(me, function(e) {
+            var opacity = $(this).css("opacity") || "1";
+            var toggled = opacity === "1" ? "0.5" : "1";
+
+            if($(this).prop("tagName") === "I") {
+                e.data.filterByType($(this).attr('id'), toggled);
+            } else {
+                e.data.filterByStatus($(this).attr('id'), toggled);
+            }
+
         });
+        // Add refresh control
+        $('<label style="font-size: 20px" for="refreshRange">Refresh Time:&nbsp;</label><select style="font-size: 20px" id="refreshRange"><option value="900000">15 min</option><option value="120000">2 min</option><option value="20000">20 sec</option><option value="10000">10 sec</option></select>')
+        .appendTo(summary)
+        // summary.find("#refreshRange")
+        .change(me, function(e) {
+            e.data.refreshRange = $(this).val();
+            clearTimeout(e.data.timeoutId);
+            e.data.poll_data();
+        });
+        $("#refreshRange").val(me.refreshRange);
+
         // restore status state
         if(!me.mapFilter.isNotInit) { // restore control state
             me.mapFilter.statuses.forEach(function(v) {
-                me.filterByStatus($("#" + v.id), v.id, v.value);
+                me.filterByStatus(v.id, v.value);
             });
+            me.markerClusterGroup.refreshClusters();
         }
-        setTimeout(function() {
+        clearTimeout(me.timeoutId);
+        me.timeoutId = setTimeout(function() {
             me.poll_data();
-        }, 900000);
+        }, me.refreshRange);
     });
 };
 
-Monmap.prototype.filterByType = function(el, typeId, toggled) {
-    var me = this;
-    $(el).css("opacity", toggled);
-    $(el).parent().siblings().children().css("opacity", toggled);
+Monmap.prototype.filterByType = function(typeId, value) {
+    var el = $("#" + typeId);
 
-    $(el).parent().siblings().children().each(function(i, v) {
-        me.filterByStatus(v, $(v).attr("id"), toggled);
-    });
+    $(el).css("opacity", value);
+    $(el).parent().siblings().children().css("opacity", value);
+
+    $(el).parent().siblings().children().get()
+    .forEach(function(v) {
+        this.filterByStatus($(v).attr("id"), value);
+    }, this);
     // Save control state
     this.mapFilter.statuses = this.mapFilter.statuses
     .map(function(v) {
         if(v.id.indexOf(typeId + "-") === 0) {
-            v.value = toggled;
+            v.value = value;
         }
         return v;
     });
 };
 
-Monmap.prototype.filterByStatus = function(el, statusId, toggled) {
+Monmap.prototype.filterByStatus = function(statusId, value) {
+    var el = $("#" + statusId);
     var icon = $(el).parent().siblings().children("i");
     var iconOpacity = icon.css("opacity") || 1;
     var otherStatuses = $(el).parent().siblings().children("span")
@@ -229,46 +269,51 @@ Monmap.prototype.filterByStatus = function(el, statusId, toggled) {
         return $(this).css("opacity");
     }).get()
     .filter(function(e) {
-        return e !== toggled;
+        return e !== value;
     }).length;
 
-    $(el).css("opacity", toggled);
+    $(el).css("opacity", value);
     if(!otherStatuses) {
-        icon.css("opacity", toggled);
+        icon.css("opacity", value);
     }
 
-    if(toggled === "1" && iconOpacity === "0.5") { // when one of statuses on and icon off
+    if(value === "1" && iconOpacity === "0.5") { // when one of statuses on and icon off
         icon.css("opacity", "1");
     }
-    var groups = this.mapFilter.statuses.filter(function(e) {
-        return e.id === statusId;
-    });
-    if(groups.length > 0) {
-        var group = groups[0].group;
-        if(toggled === '1') {
-            group.addTo(this.map);
-        } else {
-            group.removeFrom(this.map);
-        }
-    }
+
     // Save control state
     this.mapFilter.statuses = this.mapFilter.statuses
     .map(function(v) {
         if(v.id === statusId) {
-            v.value = toggled;
+            v.value = value;
         }
         return v;
     });
-};
 
-Monmap.prototype.filterBy = function(el, id, isType) {
-    var me = this;
-    var opacity = $(el).css("opacity") || "1";
-    var toggle = opacity === "1" ? "0.5" : "1";
+    var groups = this.mapFilter.statuses.filter(function(e) {
+        return e.id === statusId;
+    });
+    if(groups.length > 0) {
+        if(value === '1') {
+            groups[0].group.addTo(this.map);
+        } else {
+            // groups[0].group.removeFrom(this.map);
+            this.mapFilter.statuses
+            .forEach(function(value) {
+                value.group.removeFrom(this.map);
+            }, this);
 
-    if(isType) {
-        me.filterByType(el, id, toggle);
-    } else {
-        me.filterByStatus(el, id, toggle);
+            var showGroups = this.mapFilter.statuses
+            .filter(function(e) {
+                return e.value === "1"
+            });
+            if(showGroups.length) {
+                showGroups
+                .forEach(function(value) {
+                    value.group.addTo(this.map);
+                }, this);
+            }
+        }
     }
+    this.markerClusterGroup.refreshClusters();
 };
