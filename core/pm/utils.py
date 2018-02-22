c3258cf3cf2e2824d59fff4ec18a913fc2d9dd92
Andrey Vertiprahov <aversanta@gmail.com>
2018-02-22 22:55:37 +0500

    Add path to pm.utils.get_objects_metrics

--- core/pm/utils.py
+++ core/pm/utils.py
@@ -8,10 +8,9 @@
 
 # Python modules
 import datetime
-from collections import defaultdict
+from collections import defaultdict, Iterable
 import itertools
 # NOC modules
-from noc.sa.models.managedobject import ManagedObject
 from noc.sa.models.managedobjectprofile import ManagedObjectProfile
 from noc.core.clickhouse.connect import connection as ch_connection
 from noc.core.clickhouse.error import ClickhouseError
@@ -20,15 +19,20 @@ from noc.pm.models.metrictype import MetricType
 
 
 def get_objects_metrics(managed_objects):
+    from noc.sa.models.managedobject import ManagedObject
     """
 
     :param managed_objects:
     :return:
     """
-    # Object Metrics
+    if not isinstance(managed_objects, Iterable):
+        managed_objects = [managed_objects]
 
+    # Object Metrics
     bi_map = {str(getattr(mo, "bi_id", mo)): mo for mo in managed_objects}
-    query_interval = ManagedObjectProfile.get_max_metrics_interval() * 1.5
+    query_interval = ManagedObjectProfile.get_max_metrics_interval(
+        set(mo.object_profile.id for mo in ManagedObject.objects.filter(bi_id__in=bi_map.keys()))
+    ) * 1.5
     from_date = datetime.datetime.now() - datetime.timedelta(seconds=query_interval or 3600)
     from_date = from_date.replace(microsecond=0)
 
@@ -45,31 +49,33 @@ def get_objects_metrics(managed_objects):
 
     ch = ch_connection()
     mtable = []  # mo_id, mac, iface, ts
-    metric_map = defaultdict(dict)
+    metric_map = {}
     last_ts = {}  # mo -> ts
 
     for table, fields in itertools.groupby(sorted(mmm, key=lambda x: x[0]), key=lambda x: x[0]):
         fields = list(fields)
-        SQL = """SELECT managed_object, argMax(ts, ts), %s
+        SQL = """SELECT managed_object, argMax(ts, ts), arrayStringConcat(path) as path, %s
               FROM %s
               WHERE
                 date >= toDate('%s')
                 AND ts >= toDateTime('%s')
                 AND managed_object IN (%s)
-              GROUP BY managed_object
+              GROUP BY managed_object, path
               """ % (", ".join(["argMax(%s, ts) as %s" % (f[1], f[1]) for f in fields]),
                      table,
                      from_date.date().isoformat(), from_date.isoformat(sep=" "),
                      ", ".join(bi_map))
         try:
             for result in ch.execute(post=SQL):
-                mo_bi_id, ts = result[:2]
+                mo_bi_id, ts, path = result[:3]
                 mo = bi_map.get(mo_bi_id)
                 i = 0
-                for r in result[2:]:
+                for r in result[3:]:
                     f_name = fields[i][2]
-                    mtable += [[mo, ts, r]]
-                    metric_map[mo][f_name] = r
+                    mtable += [[mo, ts, path, r]]
+                    if mo not in metric_map:
+                        metric_map[mo] = defaultdict(dict)
+                    metric_map[mo][path][f_name] = r
                     last_ts[mo] = max(ts, last_ts.get(mo, ts))
                     i += 1
         except ClickhouseError:
@@ -78,14 +84,16 @@ def get_objects_metrics(managed_objects):
 
 
 def get_interface_metrics(managed_objects):
+    from noc.sa.models.managedobject import ManagedObject
     # mo = self.object
+    if not isinstance(managed_objects, Iterable):
+        managed_objects = [managed_objects]
     bi_map = {str(getattr(mo, "bi_id", mo)): mo for mo in managed_objects}
     query_interval = ManagedObjectProfile.get_max_metrics_interval(
-        ManagedObject.objects.filter(bi_id__in=bi_map.keys())
+        set(mo.object_profile.id for mo in ManagedObject.objects.filter(bi_id__in=bi_map.keys()))
     ) * 1.5
     from_date = datetime.datetime.now() - datetime.timedelta(seconds=query_interval or 3600)
     from_date = from_date.replace(microsecond=0)
-
     SQL = """SELECT managed_object, arrayStringConcat(path) as iface, argMax(ts, ts), argMax(load_in, ts), argMax(load_out, ts), argMax(errors_in, ts), argMax(errors_out, ts)
             FROM interface
             WHERE
