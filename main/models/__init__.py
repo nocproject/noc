b32e306adc68a51f2971c360f115e5316069b8c2
Dmitry Volodin <dv@nocproject.org>
2018-02-28 22:53:35 +0300

    Schedule refactored to noc.main.models.schedule

--- main/models/__init__.py
+++ main/models/__init__.py
@@ -27,65 +27,12 @@ from resourcestate import ResourceState
 from pyrule import PyRule, NoPyRuleException
 from timepattern import TimePattern
 from timepatternterm import TimePatternTerm
-from notificationgroup import (
-    NotificationGroup, NotificationGroupUser, NotificationGroupOther
-)
-
-
-
+from notificationgroup import NotificationGroup, NotificationGroupUser, NotificationGroupOther
 from userprofile import UserProfile, UserProfileManager
 from userprofilecontact import UserProfileContact
 from dbtrigger import DBTrigger, model_choices
 from systemnotification import SystemNotification
-
-class Schedule(models.Model):
-    class Meta:
-        verbose_name = _("Schedule")
-        verbose_name_plural = _("Schedules")
-        ordering = ["periodic_name"]
-
-    periodic_name = models.CharField(_("Periodic Task"), max_length=64)
-    is_enabled = models.BooleanField(_("Enabled?"), default=False)
-    time_pattern = models.ForeignKey(TimePattern,
-                                     verbose_name=_("Time Pattern"))
-    run_every = models.PositiveIntegerField(_("Run Every (secs)"),
-                                     default=86400)
-    timeout = models.PositiveIntegerField(_("Timeout (secs)"),
-                                     null=True, blank=True)
-    last_run = models.DateTimeField(_("Last Run"), blank=True, null=True)
-    last_status = models.BooleanField(_("Last Status"), default=True)
-
-    def __unicode__(self):
-        return u"%s:%s" % (self.periodic_name, self.time_pattern.name)
-
-    @property
-    def periodic(self):
-        return periodic_registry[self.periodic_name]
-
-    def mark_run(self, start_time, status):
-        """Set last run"""
-        self.last_run = start_time
-        self.last_status = status
-        self.save()
-
-    @classmethod
-    def get_tasks(cls):
-        """Get tasks required to run"""
-        now = datetime.datetime.now()
-        return [s for s in Schedule.objects.filter(is_enabled=True)
-                if (s.time_pattern.match(now) and
-                   (s.last_run is None or
-                    s.last_run + datetime.timedelta(seconds=s.run_every) <= now))]
-
-    @classmethod
-    def reschedule(cls, name, days=0, minutes=0, seconds=0):
-        """Reschedule tasks with name to launch immediately"""
-        t = Schedule.objects.filter(periodic_name=name)[0]
-        t.last_run = (datetime.datetime.now() -
-                      datetime.timedelta(days=days, minutes=minutes,
-                                         seconds=seconds))
-        t.save()
-
+from schedule import Schedule
 from prefixtable import PrefixTable, PrefixTablePrefix
 from template import Template
 from systemtemtemplate import SystemTemplate
