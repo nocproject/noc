f82d1f80968bde3b25a352e587aebcb588eadc11
Andrey Vertiprahov <aversanta@gmail.com>
2018-03-13 18:55:05 +0500

    Fix lint.

--- sa/interfaces/igetslaprobes.py
+++ sa/interfaces/igetslaprobes.py
@@ -6,11 +6,12 @@
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
-
+# Python modules
+from __future__ import absolute_import
 # NOC modules
 from noc.core.interface.base import BaseInterface
-from base import (DictListParameter, StringParameter,
-                  StringListParameter, BooleanParameter)
+from .base import (DictListParameter, StringParameter,
+                   StringListParameter, BooleanParameter)
 
 
 class IGetSLAProbes(BaseInterface):
