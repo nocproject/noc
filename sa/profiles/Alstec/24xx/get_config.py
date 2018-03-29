a5c4daaa9bda392c07507154f6b2f6f87f9e9f0a
Dmitry S. Luhtionov <dmitryluhtionov@gmail.com>
2018-03-29 19:13:52 +0300

    Fix Alstec.24xx profile

--- sa/profiles/Alstec/24xx/get_config.py
+++ sa/profiles/Alstec/24xx/get_config.py
@@ -2,10 +2,12 @@
 # ---------------------------------------------------------------------
 # Alstec.24xx.get_config
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2016 The NOC Project
+# Copyright (C) 2007-2018 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
+# Python modules
+import re
 # NOC modules
 from noc.core.script.base import BaseScript
 from noc.sa.interfaces.igetconfig import IGetConfig
@@ -16,6 +18,11 @@ class Script(BaseScript):
     interface = IGetConfig
     cache = True
 
+    rx_remove = re.compile(
+        r"^!System Up Time.+\n!Current SNTP Synchronized Time.+\n",
+        re.MULTILINE)
+
     def execute(self):
         config = self.cli("show running-config", cached=True)
+        config = self.rx_remove.sub("", config)
         return self.cleaned_config(config)
