bba6b1d3d16dd96353b66da6fd04bcfad0ec634b
Gabalino <hohlovap@gmail.com>
2018-04-01 19:36:32 +0300

    Upload New File get_fqdn.py

---
+++ sa/profiles/IBM/NOS/get_fqdn.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# IBM.NOS.get_fqdn
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2013 The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+
+# Python modules
+import re
+# NOC modules
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetfqdn import IGetFQDN
+
+
+class Script(BaseScript):
+    name = "IBM.NOS.get_fqdn"
+    interface = IGetFQDN
+    rx_hostname = re.compile(r'^hostname\s+\"(?P<hostname>\S+)\"', re.MULTILINE)
+    rx_domain_name = re.compile(r"^ip dns domain\-name\s+(?P<domain>\S+)",
+        re.MULTILINE)
+
+    def execute(self):
+        if not self.has_snmp():
+            try:
+                # sysName.0
+                v = self.snmp.get("1.3.6.1.2.1.1.5.0", cached=True)
+                if v:
+                   return v
+            except self.snmp.TimeOutError:
+                pass
+        h = self.cli(
+            "show running-config | include hostname")
+        d = self.cli(
+            "show running-config | include domain-name")
+        fqdn = []
+        match = self.rx_hostname.search(h)
+        if match:
+            fqdn += [match.group("hostname")]
+        match = self.rx_domain_name.search(d)
+        if match:
+            fqdn += [match.group("domain")]
+        return ".".join(fqdn)
