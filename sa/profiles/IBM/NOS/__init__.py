bffbd6204d0dc086711cfd8f67195f0ea91509b5
Gabalino <hohlovap@gmail.com>
2018-04-01 19:30:29 +0300

    Upload New File __init__.py

---
+++ sa/profiles/IBM/NOS/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# Vendor: IBM
+# OS:     NOS
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2018 The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+
+# Python modules
+import re
+# NOC modules
+from noc.core.profile.base import BaseProfile
+from noc.sa.interfaces.base import InterfaceTypeError
+
+
+class Profile(BaseProfile):
+    name = "IBM.NOS"
+    pattern_more = r"^--More--"
+    pattern_unprivileged_prompt = r"^\S+?>"
+    pattern_syntax_error = r"% Invalid input detected at"
+    command_disable_pager = "terminal-length 0"
+    command_super = "enable"
+    command_exit = "exit"
+    pattern_prompt = r"^(?P<hostname>\S+?)(?:-\d+)?(?:\(config[^\)]*\))?#"
+    requires_netmask_conversion = True
+    convert_mac = BaseProfile.convert_mac_to_cisco
+
