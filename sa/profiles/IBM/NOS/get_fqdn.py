acd9ff5001b93b568b9b7a0689c57b1968f7076d
Gabalino <hohlovap@gmail.com>
2018-04-02 06:37:17 +0300

    Replace get_fqdn.py

--- sa/profiles/IBM/NOS/get_fqdn.py
+++ sa/profiles/IBM/NOS/get_fqdn.py
@@ -17,14 +17,12 @@ class Script(BaseScript):
     name = "IBM.NOS.get_fqdn"
     interface = IGetFQDN
     rx_hostname = re.compile(r'^hostname\s+\"(?P<hostname>\S+)\"', re.MULTILINE)
-    rx_domain_name = re.compile(r"^ip dns domain\-name\s+(?P<domain>\S+)",
-        re.MULTILINE)
+    rx_domain_name = re.compile(r"^ip dns domain\-name\s+(?P<domain>\S+)", re.MULTILINE)
 
     def execute(self):
         if not self.has_snmp():
             try:
-                # sysName.0
-                v = self.snmp.get("1.3.6.1.2.1.1.5.0", cached=True)
+                v = self.snmp.get("1.3.6.1.2.1.1.5.0", cached=True)  # sysName.0
                 if v:
                    return v
             except self.snmp.TimeOutError:
