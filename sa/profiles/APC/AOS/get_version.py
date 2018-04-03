633148ee9ef3db2060952e4ff17fe029b03a2b06
Andrey Vertiprahov <aversanta@gmail.com>
2018-04-03 08:33:40 +0500

    Fix get_version SNMP for old version.

--- sa/profiles/APC/AOS/get_version.py
+++ sa/profiles/APC/AOS/get_version.py
@@ -12,6 +12,7 @@ import re
 from noc.core.script.base import BaseScript
 from noc.sa.interfaces.igetversion import IGetVersion
 from noc.lib.text import parse_kv
+from noc.core.mib import mib
 
 
 class Script(BaseScript):
@@ -29,14 +30,17 @@ class Script(BaseScript):
     def execute_snmp(self, **kwargs):
         platform = self.snmp.get("1.3.6.1.2.1.33.1.1.2.0")
         firmware = self.snmp.get("1.3.6.1.2.1.33.1.1.3.0")
+        if not platform:
+            # Use SysName for old version
+            platform = self.snmp.get(mib["SNMPv2-MIB::sysName", 0])
         return {
             "vendor": "APC",
             "platform": platform,
-            # "version": firmware
             "version": firmware
         }
 
     def execute_cli(self):
+        # Not worked for Menu CLI
         m = self.motd
         if m:
             match = self.rx_platform.search(m)
