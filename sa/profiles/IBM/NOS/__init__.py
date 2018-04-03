06951e5e0ba554883b8861d59f6275b069cbeffa
Gabalino <hohlovap@gmail.com>
2018-04-03 19:42:45 +0300

    Replace __init__.py

--- sa/profiles/IBM/NOS/__init__.py
+++ sa/profiles/IBM/NOS/__init__.py
@@ -16,11 +16,13 @@ from noc.core.profile.base import BaseProfile
 class Profile(BaseProfile):
     name = "IBM.NOS"
     pattern_more = r"^--More--"
+    pattern_prompt = r"^\S+?#"
     pattern_unprivileged_prompt = r"^\S+?>"
     pattern_syntax_error = r"% Invalid input detected at"
     command_disable_pager = "terminal-length 0"
     command_super = "enable"
     command_exit = "exit"
-    pattern_prompt = r"^(?P<hostname>\S+?)(?:-\d+)?(?:\(config[^\)]*\))?#"
+    command_save_config = "copy running-config startup-config\n"
     requires_netmask_conversion = True
     convert_mac = BaseProfile.convert_mac_to_cisco
+    config_volatile = ["^ntp message\-digest\-key .*?^", "^access user .*?^"]
