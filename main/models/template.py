dbf013c2018b301e86962f607176f8311436db20
Dmitry Volodin <dv@nocproject.org>
2018-04-04 12:38:32 +0300

    main.template: Check template syntax before saving

--- main/models/template.py
+++ main/models/template.py
@@ -2,7 +2,7 @@
 # ---------------------------------------------------------------------
 # Template model
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2017 The NOC Project
+# Copyright (C) 2007-2018 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
@@ -11,12 +11,28 @@ from threading import Lock
 import operator
 # Third-party modules
 from django.db import models
+from django.core.exceptions import ValidationError
 import jinja2
 import cachetools
+# NOC modules
+from noc.core.model.decorator import on_delete_check
 
 id_lock = Lock()
 
 
+def template_validator(value):
+    if not value:
+        return
+    try:
+        jinja2.Template(value)
+    except jinja2.exceptions.TemplateSyntaxError as e:
+        raise ValidationError(str(e))
+
+
+@on_delete_check(check=[
+    ("ip.AddressProfile", "name_template"),
+    ("ip.AddressProfile", "fqdn_template")
+])
 class Template(models.Model):
     class Meta:
         app_label = "main"
@@ -26,8 +42,8 @@ class Template(models.Model):
         ordering = ["name"]
 
     name = models.CharField("Name", unique=True, max_length=128)
-    subject = models.TextField("Subject")
-    body = models.TextField("Body")
+    subject = models.TextField("Subject", validators=[template_validator])
+    body = models.TextField("Body", validators=[template_validator])
 
     _id_cache = cachetools.TTLCache(maxsize=100, ttl=60)
 
