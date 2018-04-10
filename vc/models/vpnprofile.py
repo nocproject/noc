0a34c90728482662dc68776c3d390dce7462d59f
Dmitry Volodin <dv@nocproject.org>
2018-04-10 22:29:22 +0300

    Fix VPN.default_prefix_profile

--- vc/models/vpnprofile.py
+++ vc/models/vpnprofile.py
@@ -12,6 +12,7 @@ import operator
 # Third-party modules
 from mongoengine.document import Document
 from mongoengine.fields import StringField, LongField, ListField
+from mongoengine.errors import ValidationError
 import cachetools
 # NOC modules
 from noc.main.models.remotesystem import RemoteSystem
@@ -56,6 +57,9 @@ class VPNProfile(Document):
     name_template = ForeignKeyField(Template)
     #
     style = ForeignKeyField(Style)
+    # For vrf type -- default prefix profile
+    # Used to create AFI root prefixes
+    default_prefix_profile = PlainReferenceField("ip.PrefixProfile")
     #
     tags = ListField(StringField())
     # Integration with external NRI and TT systems
@@ -81,3 +85,7 @@ class VPNProfile(Document):
     @cachetools.cachedmethod(operator.attrgetter("_bi_id_cache"), lock=lambda _: id_lock)
     def get_by_bi_id(cls, id):
         return VPNProfile.objects.filter(bi_id=id).first()
+
+    def clean(self):
+        if self.type == "vrf" and not self.default_prefix_profile:
+            raise ValidationError("default_prefix_profile must be set for vrf type")
