2a4c92fb2f5c2bfe400add89d47d60949b95d7fe
Dmitry S. Luhtionov <dmitryluhtionov@gmail.com>
2018-04-18 11:44:44 +0300

    Fix Alstec.24xx profile

--- sa/profiles/Alstec/24xx/get_interfaces.py
+++ sa/profiles/Alstec/24xx/get_interfaces.py
@@ -30,22 +30,13 @@ class Script(BaseScript):
         r"(^vlan tagging (?P<tagged>\S+)\s*\n)?",
         re.MULTILINE)
     rx_ip = re.compile(
-        r"^IP Address\.+ (?P<ip_address>\S+)\s*\n"
-        r"^Subnet Mask\.+ (?P<ip_subnet>\S+)\s*\n"
-        r"^Default Gateway\.+ \S+\s*\n"
-        r"^IPv6 AutoConfig Mode\.+ \S+\s*\n"
-        r"^IPv6 address\.+ (?P<ipv6_address>\S+)\s*\n"
-        r"^MAC Address\.+ (?P<mac>\S+)\s*\n"
-        r"^Management VLAN ID\.+ (?P<vlan_id>\d+)\s*\n",
-        re.MULTILINE)
-    rx_ip2 = re.compile(
-        r"^\s*IP Address\s*\.+ (?P<ip_address>\S+)\s*?"
-        r"^\s*Subnet Mask\s*\.+ (?P<ip_subnet>\S+)\s*.+"
-        r"^\s*(MAC Address|Burned In MAC Address)\s*\.+ (?P<mac>\S+)\s*.+"
+        r"^\s*IP Address\s*\.+ (?P<ip_address>\S+)\s*\n"
+        r"^\s*Subnet Mask\s*\.+ (?P<ip_subnet>\S+)\s*\n"
+        r"(^\s*Default Gateway\s*\.+ \S+\s*\n)?"
+        r"(^\s*IPv6 AutoConfig Mode\s*\.+ \S+\s*\n)?"
+        r"(^\s*IPv6 (address|Prefix is)\s*\.+ (?P<ipv6_address>\S+)\s*\n)?"
+        r"^\s*(MAC Address|Burned In MAC Address)\s*\.+ (?P<mac>\S+)\s*\n"
         r"^\s*Management VLAN ID\s*\.+ (?P<vlan_id>\d+)\s*\n",
-        re.MULTILINE | re.DOTALL)
-    rx_ipv6 = re.compile(
-        r"^(IPv6 address|IPv6 Prefix is )\.+ (?P<ipv6_address>\S+)\s*",
         re.MULTILINE)
 
     def execute(self):
@@ -84,7 +75,7 @@ class Script(BaseScript):
             interfaces += [iface]
             snmp_ifindex += 1
         v = self.cli("show network", cached=True)
-        match = self.rx_ip2.search(v)
+        match = self.rx_ip.search(v)
         ip_address = match.group("ip_address")
         ip_subnet = match.group("ip_subnet")
         ip_address = "%s/%s" % (ip_address, IPv4.netmask_to_len(ip_subnet))
@@ -105,8 +96,7 @@ class Script(BaseScript):
                 "vlan_ids": [int(match.group("vlan_id"))]
             }]
         }
-        match = self.rx_ipv6.search(v)
-        if match:
+        if match.group("ipv6_address") is not None:
             iface["subinterfaces"][0]["enabled_afi"] += ['IPv6']
             iface["subinterfaces"][0]["ipv6_addresses"] = [match.group("ipv6_address")]
         interfaces += [iface]
