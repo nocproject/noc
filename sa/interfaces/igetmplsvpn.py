b764650b98c03fbcadb9a20a75c76584920f7635
Dmitry Volodin <dv@nocproject.org>
2018-04-20 13:19:22 +0300

    Futurize

--- sa/interfaces/igetmplsvpn.py
+++ sa/interfaces/igetmplsvpn.py
@@ -6,10 +6,12 @@
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
+# Python modules
+from __future__ import absolute_import
 # NOC modules
 from noc.core.interface.base import BaseInterface
-from base import (ListOfParameter, DictParameter, InterfaceNameParameter,
-                  StringParameter, BooleanParameter, RDParameter)
+from .base import (ListOfParameter, DictParameter, InterfaceNameParameter,
+                   StringParameter, BooleanParameter, RDParameter)
 
 
 class IGetMPLSVPN(BaseInterface):
