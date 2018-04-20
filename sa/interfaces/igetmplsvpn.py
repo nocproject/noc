d7c61da681474da090c0e42e63ef694284d01576
Dmitry Volodin <dv@nocproject.org>
2018-04-20 13:17:12 +0300

    IGetMPLSVPN.rt_export and rt_import fields

--- sa/interfaces/igetmplsvpn.py
+++ sa/interfaces/igetmplsvpn.py
@@ -2,7 +2,7 @@
 # ---------------------------------------------------------------------
 # IGetMPLSVPN interface
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2014 The NOC Project
+# Copyright (C) 2007-2017 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
@@ -18,12 +18,22 @@ class IGetMPLSVPN(BaseInterface):
         "type": StringParameter(choices=["VRF", "VPLS", "VLL"]),
         # VPN state. True - active, False - inactive
         "status": BooleanParameter(default=True),
-        # Unique VPN name
+        # Box-unique VPN name
         "name": StringParameter(),
         # Optional description
         "description": StringParameter(required=False),
         # RD, may be omitted for VRF-lite
         "rd": RDParameter(required=False),
+        # Route-target export for VRF
+        "rt_export": ListOfParameter(
+            element=RDParameter(),
+            required=False
+        ),
+        # Route-target import for VRF
+        "rt_import": ListOfParameter(
+            element=RDParameter(),
+            required=False
+        ),
         # List of interfaces
         "interfaces": ListOfParameter(element=InterfaceNameParameter())
     }))
