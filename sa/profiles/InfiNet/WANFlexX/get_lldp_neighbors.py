e48b265808e6c7c5e8d235cd8dc2ba0fc0d19ec3
Izya12 <Izya12@gmail.com>
2018-04-27 08:56:11 +0500

    fix E101

--- sa/profiles/InfiNet/WANFlexX/get_lldp_neighbors.py
+++ sa/profiles/InfiNet/WANFlexX/get_lldp_neighbors.py
@@ -17,9 +17,9 @@ from noc.core.mac import MAC
 class Script(BaseScript):
     name = "InfiNet.WANFlexX.get_lldp_neighbors"
     interface = IGetLLDPNeighbors
-    rx_int = re.compile(r"Table on\s*(?P<interface>\S+)",re.MULTILINE)
-    rx_ChassisID = re.compile(r"ChassisID:    \|\s*(?P<chassis_id>\S+)\s\((?P<chassis_subtype>[a-z]*)",re.MULTILINE)
-    rx_PortID = re.compile(r"PortID:       \|\s*(?P<port_id>\S+)\s\((?P<port_subtype>[a-zN]*)",re.MULTILINE)
+    rx_int = re.compile(r"Table on\s*(?P<interface>\S+)", re.MULTILINE)
+    rx_ChassisID = re.compile(r"ChassisID:    \|\s*(?P<chassis_id>\S+)\s\((?P<chassis_subtype>[a-z]*)", re.MULTILINE)
+    rx_PortID = re.compile(r"PortID:       \|\s*(?P<port_id>\S+)\s\((?P<port_subtype>[a-zN]*)", re.MULTILINE)
     rx_mac = re.compile(r"^[0-9a-f]{4}\.[0-9a-f]{4}\.[0-9a-f]{4}$")
 
     def execute_cli(self):
@@ -33,20 +33,21 @@ class Script(BaseScript):
             result += [{
                 "local_interface": match.group("interface"),
                 "neighbors": [{
-		    "remote_chassis_id_subtype": {
-			"Interface alias": 1,
-                        "Port component": 2,
-                        "Local": 7,
-                        "ifName": 5,
-			"mac": 3
-		    }[self.rx_ChassisID.search(lldp).group("chassis_subtype")],
-                    "remote_chassis_id": self.rx_ChassisID.search(lldp).group("chassis_id"),
-                    "remote_port_subtype": {
-                        "Interface alias": 1,
-                        "Port component": 2,
-                        "Local": 7,
-                        "ifName": 5,
-                        "mac": 3
+			"remote_chassis_id_subtype": {
+				"Interface alias": 1,
+                        	"Port component": 2,
+                        	"Local": 7,
+                        	"ifName": 5,
+				"mac": 3
+		    		}
+			[self.rx_ChassisID.search(lldp).group("chassis_subtype")],
+			"remote_chassis_id": self.rx_ChassisID.search(lldp).group("chassis_id"),
+			"remote_port_subtype": {
+				"Interface alias": 1,
+				"Port component": 2,
+				"Local": 7,
+				"ifName": 5,
+				"mac": 3
                     }[self.rx_PortID.search(lldp).group("port_subtype")],
                     "remote_port": self.rx_PortID.search(lldp).group("port_id")
                 }]
