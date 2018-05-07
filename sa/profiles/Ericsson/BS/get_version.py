7f8a1495e7385dc18977a47aa138cdb2563e6e65
smile <sysfar@gmail.com>
2018-05-07 13:07:36 +0300

    some fix for Ericsson BS LTE

--- sa/profiles/Ericsson/BS/get_version.py
+++ sa/profiles/Ericsson/BS/get_version.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # ---------------------------------------------------------------------
-# Ericsson.BS.get_interfaces
+# Ericsson.BS.get_version
 # ---------------------------------------------------------------------
 # Copyright (C) 2007-2017 The NOC Project
 # See LICENSE for details
@@ -13,20 +13,24 @@ from noc.sa.interfaces.igetversion import IGetVersion
 
 
 class Script(BaseScript):
-    name = "Ericsson.BS.get_interfaces"
+    name = "Ericsson.BS.get_version"
     interface = IGetVersion
 
-    rx_sn = re.compile("serial number:\s+(?P<serial>\S+)\)?", re.MULTILINE)
+    rx_sn = re.compile("serial number:\s+(?P<serial>\S+)\).", re.MULTILINE)
 
     def execute(self):
         v = self.cli("csti info")
         match = self.rx_sn.search(v)
+        if match:
+            serial = match.group("serial")
+        else:
+            serial = "None"
         r = {
             "vendor": "Ericsson",
             "platform": "eNodeB",
             "version": "None",
             "attributes": {
-                "Serial Number": match.group("serial")
+                "Serial Number": serial
             }
         }
         return r
