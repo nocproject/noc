2df7d3522c7efbb2b5963697c36d8dd89a202dc5
Dmitry Volodin <dv@nocproject.org>
2018-05-08 16:39:51 +0300

    ExtStorage

---
+++ main/models/extstorage.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------
+# ExtStorage model
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2018 The NOC Project
+# See LICENSE for details
+# ----------------------------------------------------------------------
+
+# Python modules
+from threading import Lock
+import operator
+# Third-party modules
+from mongoengine.document import Document
+from mongoengine.fields import StringField, BooleanField
+from fs import open_fs
+from fs.errors import FSError
+import cachetools
+
+id_lock = Lock()
+
+
+class ExtStorage(Document):
+    meta = {
+        "collection": "extstorages",
+        "strict": False,
+        "auto_create_index": False
+    }
+
+    name = StringField(unique=True)
+    url = StringField()
+    description = StringField()
+    enable_config_mirror = BooleanField(default=False)
+
+    _id_cache = cachetools.TTLCache(maxsize=100, ttl=60)
+
+    Error = FSError
+
+    def __unicode__(self):
+        return self.name
+
+    @classmethod
+    @cachetools.cachedmethod(operator.attrgetter("_id_cache"), lock=lambda _: id_lock)
+    def get_by_id(cls, id):
+        return ExtStorage.objects.filter(id=id).first()
+
+    def open_fs(self):
+        return open_fs(self.url)
---
+++ services/web/apps/main/extstorage/views.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------
+# main.extstorage application
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2018 The NOC Project
+# See LICENSE for details
+# ----------------------------------------------------------------------
+
+# NOC modules
+from noc.lib.app.extdocapplication import ExtDocApplication
+from noc.main.models.extstorage import ExtStorage
+from noc.core.translation import ugettext as _
+
+
+class ExtStorageApplication(ExtDocApplication):
+    """
+    ExtStorage application
+    """
+    title = _("Ext. Storage")
+    menu = [_("Setup"), _("Ext. Storages")]
+    model = ExtStorage
