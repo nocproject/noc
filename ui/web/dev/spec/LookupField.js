ea1d7ad8b612812d82eb71b5505f1f31a12aa8e7
Dmitry Volodin <dv@nocproject.org>
2018-05-14 15:17:27 +0300

    dev.spec application

---
+++ ui/web/dev/spec/LookupField.js
@@ -0,0 +1,13 @@
+//---------------------------------------------------------------------
+// NOC.dev.spec.Lookup
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2018 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.dev.spec.LookupField");
+
+Ext.define("NOC.dev.spec.LookupField", {
+    extend: "NOC.core.LookupField",
+    alias: "widget.dev.spec.LookupField",
+    uiStyle: "medium"
+});
