ab4d3ed43a352f688fdec06db68c5cadc0eff934
Andrey Vertiprahov <aversanta@gmail.com>
2018-05-24 20:46:36 +0500

    Fix PEP8

--- sa/interfaces/igetchassisid.py
+++ sa/interfaces/igetchassisid.py
@@ -6,6 +6,9 @@
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
+# Python modules
+from __future__ import absolute_import
+# NOC modules
 from noc.core.interface.base import BaseInterface
 from .base import DictListParameter, MACAddressParameter
 
