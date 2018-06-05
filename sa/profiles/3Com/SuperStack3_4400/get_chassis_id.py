a49ecbec44fd8b8b42f3ed720e9100cf597acdbb
ezombie <lv77@mail.ru>
2018-06-05 15:16:05 +0300

    Add new file

---
+++ sa/profiles/3Com/SuperStack3_4400/get_chassis_id.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------
+# 3Com.SuperStack3_4400.get_chassis_id
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2016 The NOC Project
+# See LICENSE for details
+# ----------------------------------------------------------------------
+
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetchassisid import IGetChassisID
+
+
+class Script(BaseScript):
+    name = "3Com.SuperStack3_4400.get_chassis_id"
+    cache = True
+    interface = IGetChassisID
+
+    def execute(self):
+        v = self.profile.get_hardware(self)
+        return {
+            "first_chassis_mac": v["mac"],
+            "last_chassis_mac": v["mac"]
+        }
