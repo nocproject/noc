03b5a8f5fb5c67dab7fafb3f4f512bb728a8a6e7
EKbfh <earthquake_90@mail.ru>
2018-06-06 13:39:30 +0300

    Fix fix to fix more MO without filter

--- fixes/fix_link_objects.py
+++ fixes/fix_link_objects.py
@@ -7,18 +7,12 @@
 # ----------------------------------------------------------------------
 
 from __future__ import print_function
-# Third-party modules
-from mongoengine.queryset import Q
 # NOC modules
 from noc.inv.models.link import Link
 
 
 def fix():
-    for l in Link.objects.filter(
-            Q(linked_objects__exists=False) |
-            Q(linked_segments__exists=False) |
-            Q(type__exists=False)
-    ).timeout(False):
+    for l in Link.objects.filter().timeout(False):
         try:
             l.save()
         except AssertionError:
