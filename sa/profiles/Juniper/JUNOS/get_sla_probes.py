6f177234c1430fd5c4a2b1982a0cce3383281397
Dmitry S. Luhtionov <dmitryluhtionov@gmail.com>
2018-06-13 19:36:38 +0300

    Fix Juniper.JUNOS.get_sla_probes script

--- sa/profiles/Juniper/JUNOS/get_sla_probes.py
+++ sa/profiles/Juniper/JUNOS/get_sla_probes.py
@@ -2,7 +2,7 @@
 # ---------------------------------------------------------------------
 # Juniper.JUNOS.get_sla_probes
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2016 The NOC Project
+# Copyright (C) 2007-2018 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
@@ -19,8 +19,9 @@ class Script(BaseScript):
 
     rx_res = re.compile(
         r"^\s+Owner: (?P<owner>\S+), Test: (?P<test>\S+)\s*\n"
-        r"^\s+Target address: (?P<target>\S+), Probe type: (?P<type>\S+), "
-        r"Test size: \d+ probes\s*\n"
+        r"^\s+Target address: (?P<target>\S+), Probe type: (?P<type>\S+)(,|\n)"
+        r"(^\s+Routing Instance Name: \S+\s*\n)?"
+        r"\s+Test size: \d+ probes\s*\n"
         r"^\s+Probe results:\s*\n"
         r"^\s+Response received,.+,(?P<hw_timestamp>.+)\n",
         re.MULTILINE)
@@ -37,20 +38,12 @@ class Script(BaseScript):
         r = []
         v = self.cli("show services rpm probe-results")
         for match in self.rx_res.finditer(v):
-            owner = match.group("owner")
-            test = {
+            r += [{
+                "group": match.group("owner"),
                 "name": match.group("test"),
                 "type": self.TEST_TYPES[match.group("type")],
                 "target": match.group("target"),
                 "hw_timestamp": match.group("hw_timestamp").strip() !=
                 "No hardware timestamps"
-            }
-            found = False
-            for rpm in r:
-                if rpm["name"] == owner:
-                    rpm["tests"] += [test]
-                    found = True
-                    break
-            if not found:
-                r += [{"name": owner, "tests": [test]}]
+            }]
         return r
