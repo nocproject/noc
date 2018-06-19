8291e690df9a9673cc027c9248ba38756b1f0b3e
uncol <d.a.zakharov@gmail.com>
2018-06-19 16:03:58 +0300

    reduce line

--- config.py
+++ config.py
@@ -211,7 +211,8 @@ class Config(BaseConfig):
         traefik = BooleanParameter(default=False)
         cpclient = BooleanParameter(default=False)
         telemetry = BooleanParameter(default=False, help="Enable internal telemetry export to Clickhouse")
-        consul_healthchecks = BooleanParameter(default=True, help="While registering serive in consul also register health check")
+        consul_healthchecks = BooleanParameter(default=True,
+                                               help="While registering serive in consul also register health check")
         service_registration = BooleanParameter(default=True, help="Permit consul self registration")
         pypy = BooleanParameter(default=False)
         forensic = BooleanParameter(default=False)
