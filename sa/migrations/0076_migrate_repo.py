2d8ea929b355d9d6825a2ef7e90f03181dddc34e
Dmitry Volodin <dv@nocproject.org>
2018-06-20 00:52:26 +0300

    migration: Use proper python interpreter to call migrate_repo

--- sa/migrations/0076_migrate_repo.py
+++ sa/migrations/0076_migrate_repo.py
@@ -2,12 +2,12 @@
 # ---------------------------------------------------------------------
 # Migrate mercurial repo to GridVCS
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2013 The NOC Project
+# Copyright (C) 2007-2018 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
-
 # Python modules
+import sys
 import os
 import subprocess
 
@@ -18,7 +18,7 @@ class Migration:
     def forwards(self):
         if (os.path.isfile(self.SCRIPT) and
                 os.access(self.SCRIPT, os.X_OK)):
-            subprocess.check_call([self.SCRIPT, "config"])
+            subprocess.check_call([sys.executable, self.SCRIPT, "config"])
 
     def backwards(self):
         pass
