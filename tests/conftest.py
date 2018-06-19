9ec5466ecd2b402ed2d0891ecddca6948015a5a8
Dmitry Volodin <dv@nocproject.org>
2018-06-20 01:20:54 +0300

    tests: database fixture

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------
+# pytest configuration
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2018 The NOC Project
+# See LICENSE for details
+# ----------------------------------------------------------------------
+
+# Python modules
+from __future__ import absolute_import
+# NOC modules
+from .fixtures.database import database
