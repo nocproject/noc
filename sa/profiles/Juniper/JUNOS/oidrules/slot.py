0e62ed03f4334a4b1640d85ff91b5a079638a17a
smile <sysfar@gmail.com>
2018-06-20 11:48:21 +0300

    Fix slot Rule

--- sa/profiles/Juniper/JUNOS/oidrules/slot.py
+++ sa/profiles/Juniper/JUNOS/oidrules/slot.py
@@ -2,7 +2,7 @@
 # ---------------------------------------------------------------------
 # Juniper.JUNOS.SlotRule
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2018 The NOC Project
+# Copyright (C) 2007-2016 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
@@ -26,9 +26,23 @@ class SlotRule(OIDRule):
         :return:
         """
         for i, desc in script.snmp.getnext("1.3.6.1.4.1.2636.3.1.13.1.5"):
-            if "PIC" in desc or "MIC" in desc or "Routing Engine" in desc or "FPC" in desc:
-                slotid = i[len("1.3.6.1.4.1.2636.3.1.13.1.5") + 1:]
-                oid = mib[self.expand(self.oid, {"hwSlotIndex": slotid})]
-                path = ["0", "0", "0", desc] if "CPU" in metric.metric else ["0", "", desc]
+            slotid = i[len("1.3.6.1.4.1.2636.3.1.13.1.5") + 1:]
+            oid = mib[self.expand(self.oid, {"hwSlotIndex": slotid})]
+            if "PIC" in desc or "MIC" in desc:
+                path = [int(slotid.split(".")[1]) - 1, int(slotid.split(".")[2]) - 1, int(slotid.split(".")[3]), desc] \
+                    if "CPU" in metric.metric or "Environment" in metric.metric \
+                    else [int(slotid.split(".")[1]) - 1, int(slotid.split(".")[2]) - 1, desc]
+                if oid:
+                    yield oid, self.type, self.scale, path
+            elif "FPC" in desc:
+                path = [int(slotid.split(".")[1]) - 1, slotid.split(".")[2], slotid.split(".")[3], desc] \
+                    if "CPU" in metric.metric or "Environment" in metric.metric \
+                    else [int(slotid.split(".")[1]) - 1, slotid.split(".")[2], desc]
+                if oid:
+                    yield oid, self.type, self.scale, path
+            elif "Routing Engine" in desc:
+                path = [slotid.split(".")[1], slotid.split(".")[2], slotid.split(".")[3], desc] \
+                    if "CPU" in metric.metric or "Environment" in metric.metric \
+                    else [int(slotid.split(".")[1]), int(slotid.split(".")[2]), desc]
                 if oid:
                     yield oid, self.type, self.scale, path
