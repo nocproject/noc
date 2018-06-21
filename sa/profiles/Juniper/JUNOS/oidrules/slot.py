a9d6ec4aee37198883f75244c776b941a5ac38d1
Dmitry S. Luhtionov <dmitryluhtionov@gmail.com>
2018-06-21 17:57:59 +0300

    Fix Juniper.JUNOS.oidrules.slot script

--- sa/profiles/Juniper/JUNOS/oidrules/slot.py
+++ sa/profiles/Juniper/JUNOS/oidrules/slot.py
@@ -2,7 +2,7 @@
 # ---------------------------------------------------------------------
 # Juniper.JUNOS.SlotRule
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2016 The NOC Project
+# Copyright (C) 2007-2018 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
@@ -26,28 +26,31 @@ class SlotRule(OIDRule):
         :return:
         """
         for i, desc in script.snmp.getnext("1.3.6.1.4.1.2636.3.1.13.1.5"):
+            if desc.startswith("PIC:") or desc in ["midplane", "FPM Board"]:
+                # `midplane` and `FPM Board` always return zero
+                # PIC partially duplicate velues with FPC
+                continue
             slotid = i[len("1.3.6.1.4.1.2636.3.1.13.1.5") + 1:]
             oid = mib[self.expand(self.oid, {"hwSlotIndex": slotid})]
-            if "MIC" in desc:
+            if "MS-MIC" in desc or "MS-MPC" in desc or "MS-DPC" in desc:
                 path = [int(slotid.split(".")[1]) - 1, int(slotid.split(".")[2]) - 1, int(slotid.split(".")[3]), desc] \
                     if "CPU" in metric.metric or "Environment" in metric.metric \
                     else [int(slotid.split(".")[1]) - 1, int(slotid.split(".")[2]) - 1, desc]
-                if oid:
-                    yield oid, self.type, self.scale, path
+                yield oid, self.type, self.scale, path
+            elif desc.startswith("MIC:"):
+                # Only MS modules return values in this slot
+                continue
             elif "FPC" in desc:
                 path = [int(slotid.split(".")[1]) - 1, slotid.split(".")[2], slotid.split(".")[3], desc] \
                     if "CPU" in metric.metric or "Environment" in metric.metric \
                     else [int(slotid.split(".")[1]) - 1, slotid.split(".")[2], desc]
-                if oid:
-                    yield oid, self.type, self.scale, path
+                yield oid, self.type, self.scale, path
             elif "Routing Engine" in desc:
                 path = [slotid.split(".")[1], slotid.split(".")[2], slotid.split(".")[3], desc] \
                     if "CPU" in metric.metric or "Environment" in metric.metric \
                     else [int(slotid.split(".")[1]), int(slotid.split(".")[2]), desc]
-                if oid:
-                    yield oid, self.type, self.scale, path
+                yield oid, self.type, self.scale, path
             else:
                 if "Environment" in metric.metric:
                     path = [slotid.split(".")[1], slotid.split(".")[2], slotid.split(".")[3], desc]
-                    if oid:
-                        yield oid, self.type, self.scale, path
+                    yield oid, self.type, self.scale, path
