517fe443150f2c82e609bafeb89f9be952b9abeb
Dmitry Volodin <dv@nocproject.org>
2018-06-21 15:44:55 +0300

    tests: Collection loading

--- tests/test_0001.py
+++ tests/test_0001.py
@@ -5,3 +5,23 @@
 # Copyright (C) 2007-2018 The NOC Project
 # See LICENSE for details
 # ----------------------------------------------------------------------
+
+# Third-party modules
+import pytest
+# NOC modules
+from noc.core.collection.base import Collection
+
+
+@pytest.fixture(params=list(Collection.iter_collections()))
+def collection(request):
+    return request.param
+
+
+@pytest.mark.usefixtures("database")
+def test_collection_load(database, collection):
+    """
+    Load collection
+    :param database:
+    :return:
+    """
+    collection.sync()
