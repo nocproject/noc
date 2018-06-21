7177b5e5aeb1f55c7a378fa6075c2f8aad90ce4e
Dmitry Volodin <dv@nocproject.org>
2018-06-21 16:46:51 +0300

    PEP8

--- kb/migrations/0002_kbentrypreviewlog.py
+++ kb/migrations/0002_kbentrypreviewlog.py
@@ -1,4 +1,3 @@
-
 from south.db import db
 from django.db import models
 
@@ -6,20 +5,33 @@ from django.db import models
 class Migration(object):
     def forwards(self):
 
-
         # Mock Models
-        KBEntry = db.mock_model(model_name='KBEntry', db_table='kb_kbentry', db_tablespace='', pk_field_name='id', pk_field_type=models.AutoField)
-        User = db.mock_model(model_name='User', db_table='auth_user', db_tablespace='', pk_field_name='id', pk_field_type=models.AutoField)
+        KBEntry = db.mock_model(
+            model_name="KBEntry",
+            db_table="kb_kbentry",
+            db_tablespace="",
+            pk_field_name="id",
+            pk_field_type=models.AutoField
+        )
+        User = db.mock_model(
+            model_name="User",
+            db_table="auth_user",
+            db_tablespace="",
+            pk_field_name="id",
+            pk_field_type=models.AutoField
+        )
 
-        # Model 'KBEntryPreviewLog'
-        db.create_table('kb_kbentrypreviewlog', (
-            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('kb_entry', models.ForeignKey(KBEntry,verbose_name="KB Entry")),
-            ('timestamp', models.DateTimeField("Timestamp",auto_now_add=True)),
-            ('user', models.ForeignKey(User,verbose_name=User))
-        ))
+        # Model "KBEntryPreviewLog"
+        db.create_table(
+            "kb_kbentrypreviewlog", (
+                ("id", models.AutoField(verbose_name="ID", primary_key=True, auto_created=True)),
+                ("kb_entry", models.ForeignKey(KBEntry, verbose_name="KB Entry")),
+                ("timestamp", models.DateTimeField("Timestamp", auto_now_add=True)),
+                ("user", models.ForeignKey(User, verbose_name=User))
+            )
+        )
 
-        db.send_create_signal('kb', ['KBEntryPreviewLog'])
+        db.send_create_signal("kb", ["KBEntryPreviewLog"])
 
     def backwards(self):
-        db.delete_table('kb_kbentrypreviewlog')
+        db.delete_table("kb_kbentrypreviewlog")
