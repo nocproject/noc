e20a9b56ddf76e70cdad2db67ead301c22281299
Dmitry Volodin <dv@nocproject.org>
2018-06-21 15:44:55 +0300

    tests: Collection loading

--- tests/test_0000.py
+++ tests/test_0000.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # ----------------------------------------------------------------------
-# <describe module here>
+# Database migrations
 # ----------------------------------------------------------------------
 # Copyright (C) 2007-2018 The NOC Project
 # See LICENSE for details
