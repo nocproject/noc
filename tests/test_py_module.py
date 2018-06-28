8fab1f4b8da7c67e8883070e2634841a0549e9f9
Dmitry Volodin <dv@nocproject.org>
2018-06-28 13:34:02 +0300

    tests: Check empty __init__.py is really empty

--- tests/test_py_module.py
+++ tests/test_py_module.py
@@ -9,17 +9,25 @@
 # Python modules
 import subprocess
 import os
+import ast
 # Third-party modules
 import pytest
 
+_ls_data = None
+
 
 def _git_ls():
+    global _ls_data
+
+    if _ls_data is not None:
+        return _ls_data
     try:
         data = subprocess.check_output(["git", "ls-tree", "HEAD", "-r", "--name-only"])
-        return data.splitlines()
+        _ls_data = data.splitlines()
     except OSError:
         # No git
-        return []
+        _ls_data = []
+    return _ls_data
 
 
 def get_py_modules_list():
@@ -43,7 +51,7 @@ def get_py_modules_list():
     return result
 
 
-@pytest.fixture(params=get_py_modules_list())
+@pytest.fixture(scope="module", params=get_py_modules_list())
 def py_module(request):
     return request.param
 
@@ -51,3 +59,15 @@ def py_module(request):
 def test_import(py_module):
     m = __import__(py_module, {}, {}, "*")
     assert m
+
+
+@pytest.fixture(scope="module", params=[f for f in _git_ls() if f.endswith("__init__.py")])
+def py_init(request):
+    return request.param
+
+
+def test_init(py_init):
+    with open(py_init) as f:
+        data = f.read()
+    n = compile(data, py_init, "exec", ast.PyCF_ONLY_AST)
+    assert bool(n.body) or not bool(data), "__init__.py must be empty"
