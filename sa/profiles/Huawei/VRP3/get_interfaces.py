0c66f183ef92b789d0041ec674f78e0d906212f9
smile <sysfar@gmail.com>
2018-06-30 13:45:29 +0300

    Fix get_interfaces and add get_ifindexes for Huawei.VRP3

--- sa/profiles/Huawei/VRP3/get_interfaces.py
+++ sa/profiles/Huawei/VRP3/get_interfaces.py
@@ -2,7 +2,7 @@
 # ---------------------------------------------------------------------
 # Huawei.VRP3.get_interfaces
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2016 The NOC Project
+# Copyright (C) 2007-2018 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 """
@@ -17,6 +17,11 @@ class Script(BaseScript):
     name = "Huawei.VRP3.get_interfaces"
     interface = IGetInterfaces
 
+    TYPE = {
+        "ADL": "ADSL",
+        "SHDS": "SHDSL"
+    }
+
     rx_mac = re.compile(r"^\s*MAC address:\s+(?P<mac>\S+)")
     rx_ip = re.compile(
         r"^\s*IP address\s*:\s+(?P<ip>\S+)\s*\n"
@@ -24,8 +29,13 @@ class Script(BaseScript):
         re.MULTILINE)
     rx_vlan = re.compile(r"^\s+Inband VLAN is\s+(?P<vlanid>\d+)")
     rx_pvc = re.compile(
-        r"^\s*\d+\s+ADL\s+0/(?P<ifname>\d+/\d+)\s+(?P<vpi>\d+)\s+"
+        r"^\s*\d+\s+(?P<type>\S+)\s+(?P<ifname>\d+/\d+/\d+)\s+(?P<vpi>\d+)\s+"
         r"(?P<vci>\d+)\s+LAN\s+0/0/(?P<vlan>\d+)\s+\S+\s+\S+\s+\d+\s+\d+\s*\n",
+        re.MULTILINE)
+
+    rx_pvc2 = re.compile(
+        r"\s+\d+\s+LAN\s+0/0/(?P<vlan>\d+)\s+\S+\s+\S+\s+"
+        r"(?P<type>\S+)\s+(?P<ifname>\d+/\d+/\d+)\s+(?P<vpi>\d+)\s+(?P<vci>\d+)\s.*\n",
         re.MULTILINE)
 
     def execute(self):
@@ -34,43 +44,80 @@ class Script(BaseScript):
         for v in self.scripts.get_vlans():
             vlans += [v["vlan_id"]]
         iface = {
-            "name": "0/0",
+            "name": "FE:0/0/1",
             "type": "physical",
             "subinterfaces": [{
-                "name": "0/0",
+                "name": "FE:0/0/1",
+                "enabled_afi": ["BRIDGE"],
+                "tagged_vlans": vlans
+            }]
+        }
+        interfaces += [iface]
+        iface = {
+            "name": "FE:0/0/2",
+            "type": "physical",
+            "subinterfaces": [{
+                "name": "FE:0/0/2",
                 "enabled_afi": ["BRIDGE"],
                 "tagged_vlans": vlans
             }]
         }
         interfaces += [iface]
         with self.configure():
-            for match in self.rx_pvc.finditer(self.cli("show pvc all")):
-                ifname = match.group("ifname")
-                sub = {
-                    "name": ifname,
-                    "enabled_afi": ["BRIDGE", "ATM"],
-                    "vpi": int(match.group("vpi")),
-                    "vci": int(match.group("vci")),
-                    "vlan_ids": int(match.group("vlan"))
-                }
-                found = False
-                for i in interfaces:
-                    if ifname == i["name"]:
-                        i["subinterfaces"] += [sub]
-                        found = True
-                        break
-                if not found:
-                    iface = {
+            c = self.cli("show pvc all")
+            if self.rx_pvc.search(c):
+                for match in self.rx_pvc.finditer(c):
+
+                    ifname = "%s:%s" % (self.TYPE.get(match.group("type")), match.group("ifname"))
+                    sub = {
                         "name": ifname,
-                        "type": "physical",
-                        "subinterfaces": [sub]
+                        "enabled_afi": ["BRIDGE", "ATM"],
+                        "vpi": int(match.group("vpi")),
+                        "vci": int(match.group("vci")),
+                        "vlan_ids": int(match.group("vlan"))
                     }
-                    interfaces += [iface]
+                    found = False
+                    for i in interfaces:
+                        if ifname == i["name"]:
+                            i["subinterfaces"] += [sub]
+                            found = True
+                            break
+                    if not found:
+                        iface = {
+                            "name": ifname,
+                            "type": "physical",
+                            "subinterfaces": [sub]
+                        }
+                        interfaces += [iface]
+            else:
+                for match in self.rx_pvc2.finditer(c):
+                    ifname = "%s:%s" % (self.TYPE.get(match.group("type")), match.group("ifname"))
+                    sub = {
+                        "name": ifname,
+                        "enabled_afi": ["BRIDGE", "ATM"],
+                        "vpi": int(match.group("vpi")),
+                        "vci": int(match.group("vci")),
+                        "vlan_ids": int(match.group("vlan"))
+                    }
+                    found = False
+                    for i in interfaces:
+                        if ifname == i["name"]:
+                            i["subinterfaces"] += [sub]
+                            found = True
+                            break
+                    if not found:
+                        iface = {
+                            "name": ifname,
+                            "type": "physical",
+                            "subinterfaces": [sub]
+                        }
+                        interfaces += [iface]
+
         match = self.re_search(self.rx_mac,
-            self.cli("show atmlan mac-address"))
+                               self.cli("show atmlan mac-address"))
         mac = match.group("mac")
         match = self.re_search(self.rx_ip,
-            self.cli("show atmlan ip-address"))
+                               self.cli("show atmlan ip-address"))
         addr = match.group("ip")
         mask = match.group("mask")
         ip_address = "%s/%s" % (addr, IPv4.netmask_to_len(mask))
@@ -82,7 +129,7 @@ class Script(BaseScript):
             "name": "mgmt",
             "type": "SVI",
             "admin_status": True,  # always True, since inactive
-            "oper_status": True,   # SVIs aren't shown at all
+            "oper_status": True,  # SVIs aren't shown at all
             "mac": mac,
             "subinterfaces": [{
                 "name": "mgmt",
