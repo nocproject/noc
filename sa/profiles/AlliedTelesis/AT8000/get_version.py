ddea12ab01f349c43ee1f3971caaf901e3bafa8e
Dmitry S. Luhtionov <dmitryluhtionov@gmail.com>
2018-07-03 17:18:07 +0300

    Add AlliedTelesis.AT8000 profile

---
+++ sa/profiles/AlliedTelesis/AT8000/get_version.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# AlliedTelesis.AT8000.get_version
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2018 The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+"""
+"""
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetversion import IGetVersion
+import re
+
+
+class Script(BaseScript):
+    name = "AlliedTelesis.AT8000.get_version"
+    cache = True
+    interface = IGetVersion
+
+    rx_ver = re.compile(
+        r"^\s*Application Software Version\s*\.+ AT-S39 v(?P<version>\S+)\s*\n", re.MULTILINE)
+    rx_platform = re.compile(
+        r"^\s*Serial Number\s*\.+ (?P<serial>\S+)\s*\n"
+        r"^\s*Model Name\s*\.+ (?P<platform>AT\S+)\s*\n", re.MULTILINE)
+    rx_bootprom = re.compile(
+        r"^\s*Bootloader Version\s*\.+ \S+ v(?P<bootprom>\S+)\s*\n", re.MULTILINE)
+
+    def execute_cli(self):
+        v = self.cli("show system")
+        match = self.rx_platform.search(v)
+        return {
+            "vendor": "Allied Telesis",
+            "platform": match.group("platform"),
+            "version": self.rx_ver.search(v).group("version"),
+            "attributes": {
+                "Boot PROM": self.rx_bootprom.search(v).group("bootprom"),
+                "Serial Number": match.group("serial")
+            }
+        }
---
+++ sa/profiles/AlliedTelesis/AT8000/get_vlans.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# AlliedTelesis.AT8000.get_vlans
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2018 The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+"""
+"""
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetvlans import IGetVlans
+
+
+class Script(BaseScript):
+    name = "AlliedTelesis.AT8000.get_vlans"
+    interface = IGetVlans
+
+    def execute_cli(self):
+        r = []
+        v = self.cli("show vlan", cached=True)
+        for match in self.profile.rx_vlan.finditer(v):
+            r += [{
+                "vlan_id": match.group("vlan_id"),
+                "name": match.group("name")
+            }]
+        return r
