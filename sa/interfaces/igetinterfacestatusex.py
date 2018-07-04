0bbd1f3f1ce645f5e1efaecd0b56dce3e188afb8
smile <sysfar@gmail.com>
2018-07-04 15:50:38 +0300

    Update get_interface_status_ex, add ifindexes for oid. The collection of interface statuses occurs only for physical interfaces.

--- sa/interfaces/igetinterfacestatusex.py
+++ sa/interfaces/igetinterfacestatusex.py
@@ -5,8 +5,11 @@
 # Copyright (C) 2007-2018 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
+# Python modules
+from __future__ import absolute_import
+# NOC modules
 from noc.core.interface.base import BaseInterface
-from base import (
+from .base import (
     DictListParameter, InterfaceNameParameter, BooleanParameter,
     IntParameter
 )
