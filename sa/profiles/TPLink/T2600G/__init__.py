31aaafd8446c71fd9c77e3a91551309cd7031ef9
EKbfh <earthquake_90@mail.ru>
2018-07-05 15:17:16 +0300

    TPLink get int status + init_ modifications

--- sa/profiles/TPLink/T2600G/__init__.py
+++ sa/profiles/TPLink/T2600G/__init__.py
@@ -6,8 +6,9 @@
 # Copyright (C) 2007-2016 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
-"""
-"""
+
+# Python modules
+import re
 
 from noc.core.profile.base import BaseProfile
 
@@ -17,6 +18,7 @@ class Profile(BaseProfile):
     pattern_username = r"^User:"
     pattern_more = r"Press any key to continue \(Q to quit\)"
     pattern_unprivileged_prompt = r"^\S+?>"
+    pattern_syntax_error = r".*(?:Error: (Invalid parameter.)|(Bad command)|(Missing parameter data)).*"
     command_super = "enable"
     pattern_prompt = \
         r"^(?P<hostname>[a-zA-Z0-9/.]\S{0,35})(?:[-_\d\w]+)?" \
@@ -31,7 +33,7 @@ class Profile(BaseProfile):
     command_exit = "exit"
     command_save_config = "copy running-config startup-config\n"
     requires_netmask_conversion = True
-
+    rx_ifname = re.compile(r"^((Gi|.*)\d\/\d\/)*(?P<number>\d+).*$")
     matchers = {
         "is_platform_T2600G": {
             "platform": {
@@ -39,3 +41,16 @@ class Profile(BaseProfile):
             }
         }
     }
+
+    def convert_interface_name(self, s):
+        """
+        >>> Profile().convert_interface_name("gigabitEthernet 1/0/24 : copper")
+        'Gi1/0/24'
+        >>> Profile().convert_interface_name("24")
+        'Gi1/0/24'
+        """
+        match = self.rx_ifname.match(s)
+        if match:
+            return "Gi1/0/%d" % int(match.group("number"))
+        else:
+            return s
---
+++ sa/profiles/TPLink/T2600G/get_interface_status.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2013 The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+
+# Python modules
+import time
+# NOC modules
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetinterfacestatus import IGetInterfaceStatus
+
+
+class Script(BaseScript):
+    name = "TPLink.T2600G.get_interface_status"
+    interface = IGetInterfaceStatus
+
+    def execute(self, interface=None):
+        r = []
+        # Try SNMP first
+        if self.has_snmp():
+            try:
+                for n, s in self.snmp.join_tables("1.3.6.1.2.1.31.1.1.1.1",
+                    "1.3.6.1.2.1.2.2.1.8"):  # IF-MIB
+                    if n[:3] == 'AUX' or n[:4] == 'Vlan' \
+                    or n[:4] == 'port':
+                        continue
+                    n = self.profile.convert_interface_name(n)
+                    if interface:
+                        if n == interface:
+                            r.append({
+                                "interface": n,
+                                "status": int(s) == 1
+                                })
+                    else:
+                        r.append({
+                            "interface": n,
+                            "status": int(s) == 1
+                            })
+                return r
+            except self.snmp.TimeOutError:
+                pass
+
+        # Fallback to CLI
+        raise Exception("Not implemented")
