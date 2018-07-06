0369a4f9f67c9a6080685afc0646a97454f0ba5e
ezombie <lv77@mail.ru>
2018-07-06 11:06:25 +0300

    Update get_fqdn.py

--- sa/profiles/Linux/RHEL/get_fqdn.py
+++ sa/profiles/Linux/RHEL/get_fqdn.py
@@ -10,12 +10,17 @@
 from noc.core.script.base import BaseScript
 from noc.sa.interfaces.igetfqdn import IGetFQDN
 
-
 class Script(BaseScript):
     name = "Linux.RHEL.get_fqdn"
     interface = IGetFQDN
 
-    def execute(self):
+    def execute_snmp(self, **kwargs):
+        # sysName.0
+        v = self.snmp.get("1.3.6.1.2.1.1.5.0", cached=True)
+        if v:
+            return v
+
+    def execute_cli(self, **kwargs):
         if self.has_snmp():
             # and self.access_profile.snmp_ro:
             try:
