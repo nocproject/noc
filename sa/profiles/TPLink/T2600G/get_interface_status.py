24bf5657726717e71cceaa6b8024e491961fa772
EKbfh <earthquake_90@mail.ru>
2018-07-06 11:31:04 +0300

    Fix naming and remove try/except

--- sa/profiles/TPLink/T2600G/get_interface_status.py
+++ sa/profiles/TPLink/T2600G/get_interface_status.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # ---------------------------------------------------------------------
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2013 The NOC Project
+# Copyright (C) 2007-2018 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
@@ -15,33 +15,28 @@ class Script(BaseScript):
     name = "TPLink.T2600G.get_interface_status"
     interface = IGetInterfaceStatus
 
-    def execute(self, interface=None):
+    def execute_snmp(self, interface=None):
         r = []
-        # Try SNMP first
-        if self.has_snmp():
-            try:
-                for n, s in self.snmp.join_tables(
-                    "1.3.6.1.2.1.31.1.1.1.1",
-                    "1.3.6.1.2.1.2.2.1.8"
-                ):  # IF-MIB
-                    if (n[:3] == 'AUX' or n[:4] == 'Vlan' or
-                            n[:4] == 'port'):
-                        continue
-                    n = self.profile.convert_interface_name(n)
-                    if interface:
-                        if n == interface:
-                            r.append({
-                                "interface": n,
-                                "status": int(s) == 1
-                            })
-                    else:
-                        r.append({
-                            "interface": n,
-                            "status": int(s) == 1
-                        })
-                return r
-            except self.snmp.TimeOutError:
-                pass
+        for n, s in self.snmp.join_tables(
+            "1.3.6.1.2.1.31.1.1.1.1",
+            "1.3.6.1.2.1.2.2.1.8"
+        ):  # IF-MIB
+            if (n[:3] == 'AUX' or n[:4] == 'Vlan' or
+                    n[:4] == 'port'):
+                continue
+            n = self.profile.convert_interface_name(n)
+            if interface:
+                if n == interface:
+                    r.append({
+                        "interface": n,
+                        "status": int(s) == 1
+                    })
+            else:
+                r.append({
+                    "interface": n,
+                    "status": int(s) == 1
+                })
+        return r
 
         # Fallback to CLI
         raise Exception("Not implemented")
