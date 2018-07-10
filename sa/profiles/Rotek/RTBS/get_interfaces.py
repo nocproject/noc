4c186813371d1207e7b72ffee747ffbde4c0ee3b
smile <sysfar@gmail.com>
2018-07-10 19:29:03 +0300

    Update cli and snmp for get_interfaces Rotek.RTBS

--- sa/profiles/Rotek/RTBS/get_interfaces.py
+++ sa/profiles/Rotek/RTBS/get_interfaces.py
@@ -2,10 +2,9 @@
 # ---------------------------------------------------------------------
 # Rotek.RTBS.get_interfaces
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2017 The NOC Project
+# Copyright (C) 2007-2018 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
-
 
 # Python modules
 import re
@@ -20,18 +19,17 @@ class Script(BaseScript):
     interface = IGetInterfaces
 
     rx_sh_int = re.compile(
-        r"^(?P<ifindex>\d+):\s+(?P<ifname>(e|l|t|b|r|g)\S+):\s"
+        r"^(?P<ifindex>\d+):\s+(?P<ifname>(e|l|t|b|r|g|n)\S+):\s"
         r"<(?P<flags>.*?)>\s+mtu\s+(?P<mtu>\d+).+?\n"
         r"^\s+link/\S+(?:\s+(?P<mac>[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}))?\s+.+?\n"
-        r"(?:^\s+inet\s+(?P<ip>\d+\S+)\s+)?",
-        re.MULTILINE | re.DOTALL)
+        r"(?:^\s+inet\s+(?P<ip>\d+\S+)\s+)?", re.MULTILINE | re.DOTALL
+    )
 
     rx_status = re.compile(r"^(?P<status>UP|DOWN\S+)", re.MULTILINE)
 
-    def execute(self):
+    def execute_snmp(self):
         interfaces = []
         ss = {}
-        ssid = {}
         # Try SNMP first
         if self.has_snmp():
             try:
@@ -40,17 +38,33 @@ class Script(BaseScript):
                     ieee_mode = self.snmp.get("1.3.6.1.4.1.32761.3.5.1.2.1.1.2.%s" % sifindex)
                     freq = self.snmp.get("1.3.6.1.4.1.32761.3.5.1.2.1.1.7.%s" % sifindex)
                     channel = self.snmp.get("1.3.6.1.4.1.32761.3.5.1.2.1.1.8.%s" % sifindex)
-                    channelbandwidth = self.snmp.get("1.3.6.1.4.1.32761.3.5.1.2.1.1.9.%s" % sifindex)
-                    ss[sifindex] = {"ssid": sname, "ieee_mode": ieee_mode,
-                        "channel": channel, "freq": freq, "channelbandwidth": channelbandwidth}
+                    channelbandwidth = self.snmp.get(
+                        "1.3.6.1.4.1.32761.3.5.1.2.1.1.9.%s" % sifindex
+                    )
+                    ss[sifindex] = {
+                        "ssid": sname,
+                        "ieee_mode": ieee_mode,
+                        "channel": channel,
+                        "freq": freq,
+                        "channelbandwidth": channelbandwidth
+                    }
+
                 for soid, sname in self.snmp.getnext("1.3.6.1.4.1.41752.3.5.1.2.1.1.4"):
                     sifindex = int(soid.split(".")[-1])
                     ieee_mode = self.snmp.get("1.3.6.1.4.1.41752.3.5.1.2.1.1.2.%s" % sifindex)
                     freq = self.snmp.get("1.3.6.1.4.1.41752.3.5.1.2.1.1.7.%s" % sifindex)
                     channel = self.snmp.get("1.3.6.1.4.1.41752.3.5.1.2.1.1.8.%s" % sifindex)
-                    channelbandwidth = self.snmp.get("1.3.6.1.4.1.41752.3.5.1.2.1.1.9.%s" % sifindex)
-                    ss[sifindex] = {"ssid": sname, "ieee_mode": ieee_mode,
-                        "channel": channel, "freq": freq, "channelbandwidth": channelbandwidth}
+                    channelbandwidth = self.snmp.get(
+                        "1.3.6.1.4.1.41752.3.5.1.2.1.1.9.%s" % sifindex
+                    )
+                    ss[sifindex] = {
+                        "ssid": sname,
+                        "ieee_mode": ieee_mode,
+                        "channel": channel,
+                        "freq": freq,
+                        "channelbandwidth": channelbandwidth
+                    }
+
                 for v in self.snmp.getnext("1.3.6.1.2.1.2.2.1.1", cached=True):
                     ifindex = v[1]
                     name = self.snmp.get("1.3.6.1.2.1.2.2.1.2.%s" % str(ifindex))
@@ -58,9 +72,7 @@ class Script(BaseScript):
                     if "peer" in name:
                         continue
                     if not name:
-                        self.logger.info(
-                            "Ignoring unknown interface type: '%s", iftype
-                        )
+                        self.logger.info("Ignoring unknown interface type: '%s", iftype)
                         continue
                     mac = self.snmp.get("1.3.6.1.2.1.2.2.1.6.%s" % str(ifindex))
                     mtu = self.snmp.get("1.3.6.1.2.1.2.2.1.4.%s" % str(ifindex))
@@ -81,15 +93,17 @@ class Script(BaseScript):
                         "admin_status": admin_status,
                         "oper_status": oper_status,
                         "snmp_ifindex": ifindex,
-                        "subinterfaces": [{
-                            "name": name,
-                            "mac": mac,
-                            "snmp_ifindex": ifindex,
-                            "admin_status": admin_status,
-                            "oper_status": oper_status,
-                            "mtu": mtu,
-                            "enabled_afi": ["BRIDGE"]
-                        }]
+                        "subinterfaces": [
+                            {
+                                "name": name,
+                                "mac": mac,
+                                "snmp_ifindex": ifindex,
+                                "admin_status": admin_status,
+                                "oper_status": oper_status,
+                                "mtu": mtu,
+                                "enabled_afi": ["BRIDGE"]
+                            }
+                        ]
                     }
                     interfaces += [iface]
                     for i in ss.items():
@@ -108,27 +122,38 @@ class Script(BaseScript):
                                 "admin_status": admin_status,
                                 "oper_status": oper_status,
                                 "snmp_ifindex": ifindex,
-                                "description": "ssid_broadcast=%s, ieee_mode=%s, channel=%s, freq=%sGHz, channelbandwidth=%sMHz" % (ssid_broadcast,
-                                    i[1]["ieee_mode"], i[1]["channel"], i[1]["freq"], i[1]["channelbandwidth"]),
-                                "subinterfaces": [{
-                                    "name": vname,
-                                    "mac": mac,
-                                    "snmp_ifindex": ifindex,
-                                    "admin_status": admin_status,
-                                    "oper_status": oper_status,
-                                    "mtu": mtu,
-                                    "enabled_afi": ["BRIDGE"]
-                                }]
+                                "description": "ssid_broadcast=%s, ieee_mode=%s, channel=%s,"
+                                               "freq=%sGHz, channelbandwidth=%sMHz" % (
+                                                   ssid_broadcast, i[1]["ieee_mode"], i[1]["channel"],
+                                                   i[1]["freq"], i[1]["channelbandwidth"]
+                                               ),
+                                "subinterfaces": [
+                                    {
+                                        "name": vname,
+                                        "mac": mac,
+                                        "snmp_ifindex": ifindex,
+                                        "admin_status": admin_status,
+                                        "oper_status": oper_status,
+                                        "mtu": mtu,
+                                        "enabled_afi": ["BRIDGE"]
+                                    }
+                                ]
                             }
                             interfaces += [iface]
                 return [{"interfaces": interfaces}]
             except self.snmp.TimeOutError:
                 pass
+
+    def execute_cli(self):
+        interfaces = []
+        ssid = {}
         # GO CLI
         i = ["ra0", "ra1"]
         for ri in i:
             s = self.cli("show interface %s ssid" % ri)
             v = self.cli("show interface %s vlan-to-ssid" % ri)
+            if "vlan-to-ssid not configured" in v:
+                continue
             a = self.cli("show interface %s ssid-broadcast" % ri)
             i = self.cli("show interface %s ieee-mode" % ri)
             c = self.cli("show interface %s channel" % ri)
@@ -139,8 +164,14 @@ class Script(BaseScript):
             ieee_mode = "IEEE 802.%s" % i.split(":")[1].strip()
             channel = c.split(":")[1].strip()
             freq = f.split(":")[1].strip()
-            ssid[ri] = {"ssid": res, "vlan": resv, "ssid_broadcast": ssid_broadcast, "ieee_mode": ieee_mode,
-                        "channel": channel, "freq": freq}
+            ssid[ri] = {
+                "ssid": res,
+                "vlan": resv,
+                "ssid_broadcast": ssid_broadcast,
+                "ieee_mode": ieee_mode,
+                "channel": channel,
+                "freq": freq
+            }
         with self.profile.shell(self):
             v = self.cli("ip a", cached=True)
             for match in self.rx_sh_int.finditer(v):
@@ -162,14 +193,15 @@ class Script(BaseScript):
                     "admin_status": a_stat,
                     "oper_status": o_status,
                     "snmp_ifindex": match.group("ifindex"),
-                    "subinterfaces": [{
-                        "name": ifname,
-                        "mtu": match.group("mtu"),
-                        "admin_status": a_stat,
-                        "oper_status": o_status,
-                        "snmp_ifindex": match.group("ifindex"),
-
-                    }]
+                    "subinterfaces": [
+                        {
+                            "name": ifname,
+                            "mtu": match.group("mtu"),
+                            "admin_status": a_stat,
+                            "oper_status": o_status,
+                            "snmp_ifindex": match.group("ifindex"),
+                        }
+                    ]
                 }
                 if mac:
                     iface["mac"] = mac
@@ -189,18 +221,22 @@ class Script(BaseScript):
                             "oper_status": o_status,
                             "mac": mac,
                             "snmp_ifindex": match.group("ifindex"),
-                            "description": "ssid_broadcast=%s, ieee_mode=%s, channel=%s, freq=%s" % (
-                            ri[1]["ssid_broadcast"], ri[1]["ieee_mode"], ri[1]["channel"], ri[1]["freq"]),
-                            "subinterfaces": [{
-                                "name": "%s.%s" % (ifname, ri[1]["ssid"]),
-                                "enabled_afi": ["BRIDGE"],
-                                "admin_status": a_stat,
-                                "oper_status": o_status,
-                                "mac": mac,
-                                "snmp_ifindex": match.group("ifindex"),
-                                "untagged_vlan": int(ri[1]["vlan"]),
-
-                            }]
+                            "description": "ssid_broadcast=%s, ieee_mode=%s, channel=%s, freq=%s" %
+                                           (
+                                               ri[1]["ssid_broadcast"], ri[1]["ieee_mode"], ri[1]["channel"],
+                                               ri[1]["freq"]
+                                           ),
+                            "subinterfaces": [
+                                {
+                                    "name": "%s.%s" % (ifname, ri[1]["ssid"]),
+                                    "enabled_afi": ["BRIDGE"],
+                                    "admin_status": a_stat,
+                                    "oper_status": o_status,
+                                    "mac": mac,
+                                    "snmp_ifindex": match.group("ifindex"),
+                                    "untagged_vlan": int(ri[1]["vlan"]),
+                                }
+                            ]
                         }
                         interfaces += [iface]
         return [{"interfaces": interfaces}]
