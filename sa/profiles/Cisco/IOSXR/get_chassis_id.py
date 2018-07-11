9192d61de78111c7d5a2812b697b8af383fab18d
Andrey Pazychev <anp135@yandex.ru>
2018-07-11 17:30:18 +0300

    Fix local chassis MAC looking

--- sa/profiles/Cisco/IOSXR/get_chassis_id.py
+++ sa/profiles/Cisco/IOSXR/get_chassis_id.py
@@ -26,17 +26,28 @@ class Script(BaseScript):
     )
 
     def execute(self):
-        v = self.cli("admin show diag chassis eeprom-info")
         macs = []
-        for f, t in [(mac, MAC(mac).shift(int(count) - 1))
-                for mac, count in self.rx_range.findall(v)]:
-            if macs and MAC(f).shift(-1) == macs[-1][1]:
-                macs[-1][1] = t
-            else:
-                macs += [[f, t]]
-        return [
-            {
-                "first_chassis_mac": f,
-                "last_chassis_mac": t
-            } for f, t in macs
-        ]
+        if self.has_snmp():
+            try:
+                v = self.snmp.get("1.0.8802.1.1.2.1.3.2.0", cached=True)
+                return {
+                    "first_chassis_mac": v,
+                    "last_chassis_mac": v
+                }
+            except self.snmp.TimeOutError:
+                pass
+        else:
+            #Для CRS-16/S 4.3.2 команда ниже не выводит МАС коробки, после перебора всего дерева команд show/admin show, решение было найдено только через SNMP
+            v = self.cli("admin show diag chassis eeprom-info")
+            for f, t in [(mac, MAC(mac).shift(int(count) - 1))
+                    for mac, count in self.rx_range.findall(v)]:
+                if macs and MAC(f).shift(-1) == macs[-1][1]:
+                    macs[-1][1] = t
+                else:
+                    macs += [[f, t]]
+            return [
+                {
+                    "first_chassis_mac": f,
+                    "last_chassis_mac": t
+                } for f, t in macs
+            ]
