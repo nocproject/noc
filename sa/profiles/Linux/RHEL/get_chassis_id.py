09349abca2e12c684f88fed4297be56ed589e0ad
ezombie <lv77@mail.ru>
2018-07-25 14:21:27 +0300

    Update get_chassis_id.py

--- sa/profiles/Linux/RHEL/get_chassis_id.py
+++ sa/profiles/Linux/RHEL/get_chassis_id.py
@@ -2,7 +2,7 @@
 # ---------------------------------------------------------------------
 # Linux.RHEL.get_chassis_id
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2017 The NOC Project
+# Copyright (C) 2007-2018 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
@@ -17,24 +17,18 @@ class Script(BaseScript):
     interface = IGetChassisID
 
     def execute_snmp(self, **kwargs):
-        # Try SNMP first
-        if self.snmp:
-            try:
-                mac = self.snmp.get("1.3.6.1.2.1.2.2.1.6.2", cached=True)
-                if mac is not None:
-                    return {
-                        "first_chassis_mac": mac,
-                        "last_chassis_mac": mac
-                    }
-                else:
-                    return [{
-                        "first_chassis_mac": "00:00:00:00:00:00",
-                        "last_chassis_mac": "00:00:00:00:00:00"
-                    }]
-
-            except self.snmp.TimeOutError:
-                # pass
-                return
+        mac = self.snmp.get("1.3.6.1.2.1.2.2.1.6.2", cached=True)
+        if mac is not None:
+            return {
+                "first_chassis_mac": mac,
+                "last_chassis_mac": mac
+                }
+        else:
+            return [{
+                    "first_chassis_mac": "00:00:00:00:00:00",
+                    "last_chassis_mac": "00:00:00:00:00:00"
+                }]
 
     # TODO: use cli
-    # def execute_cli(self, **kwargs):
+    def execute_cli(self, **kwargs):
+        return []
