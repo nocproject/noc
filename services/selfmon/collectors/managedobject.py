91e3198b9593328b73846bac5f1790caa8ccc88e
Dmitry Volodin <dv@nocproject.org>
2018-08-06 19:33:50 +0300

    selfmon service

---
+++ services/selfmon/collectors/managedobject.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------
+# ManagedObjectCollector
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2018 The NOC Project
+# See LICENSE for details
+# ----------------------------------------------------------------------
+
+# Python modules
+from __future__ import absolute_import
+# NOC modules
+from noc.main.models.pool import Pool
+from .base import BaseCollector
+
+
+class ManagedObjectCollector(BaseCollector):
+    name = "managedobject"
+
+    SQL_POOL_MO = """SELECT 
+      pool AS pool_id, 
+      COUNT(CASE WHEN is_managed THEN 1 END) AS pool_managed, 
+      COUNT(CASE WHEN NOT is_managed THEN 1 END) AS pool_unmanaged
+    FROM sa_managedobject
+    GROUP BY pool
+    """
+
+    def iter_metrics(self):
+        for pool_id, pool_managed, pool_unmanaged in self.pg_execute(self.SQL_POOL_MO):
+            pool = Pool.get_by_id(pool_id)
+            if not pool:
+                continue
+            yield "managedobject_%s_managed" % pool.name, pool_managed
+            yield "managedobject_%s_unmanaged" % pool.name, pool_managed
+            yield "managedobject_%s_total" % pool.name, pool_managed + pool_unmanaged
--- services/selfmon/loader.py
+++ services/selfmon/loader.py
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------------
-# DataSource loader
+# Collector loader loader
 # ----------------------------------------------------------------------
 # Copyright (C) 2007-2017 The NOC Project
 # See LICENSE for details
@@ -10,32 +10,21 @@ from __future__ import absolute_import
 import os
 import inspect
 # NOC modules
-from .datasources.base import BaseDataSource
+from .collectors.base import BaseCollector
 from noc.config import config
 
-BASE_PREFIX = os.path.join("services", "datasource", "datasources")
+BASE_PREFIX = os.path.join("services", "selfmon", "collectors")
 PATHS = config.get_customized_paths(BASE_PREFIX)
 
-# ds name -> ds cls
-DS_MAP = {}
 
-
-def get_datasource(name):
-    global DS_MAP
-
-    if not DS_MAP:
-        load_datasources()
-    return DS_MAP.get(name)
-
-
-def load_datasources():
-    global DS_MAP, PATHS, BASE_PREFIX
+def iter_collectors():
+    global PATHS
 
     for path in PATHS:
         if not os.path.exists(path):
             continue
         b, _ = path.split(BASE_PREFIX)
-        for f in os.listdir(path):
+        for f in sorted(os.listdir(path)):
             if not f.endswith(".py") or f.startswith("_") or f == "base.py":
                 continue
             if b:
@@ -50,8 +39,7 @@ def load_datasources():
                 o = getattr(m, n)
                 if (
                     inspect.isclass(o) and
-                    issubclass(o, BaseDataSource) and
+                    issubclass(o, BaseCollector) and
                     o.__module__ == m.__name__
                 ):
-                    DS_MAP[o.name] = o
-                    break
+                    yield o
