f5fa33fee075cdd0d04637b3d7f31867e2d75acb
Dmitry Volodin <dv@nocproject.org>
2018-08-11 17:01:45 +0300

    main.ref: modelid lookup

---
+++ ui/web/main/ref/modelid/LookupField.js
@@ -0,0 +1,13 @@
+//---------------------------------------------------------------------
+// NOC.main.ref.modelid.Lookup
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2018 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.main.ref.modelid.LookupField");
+
+Ext.define("NOC.main.ref.modelid.LookupField", {
+    extend: "NOC.core.LookupField",
+    alias: "widget.main.ref.modelid.LookupField",
+    uiStyle: "medium"
+});
