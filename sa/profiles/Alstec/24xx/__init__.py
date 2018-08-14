7cde9db59a89a5fff75630ee72f061585e17b2f3
Andrey Vertiprahov <aversanta@gmail.com>
2018-08-14 22:30:28 +0500

    Fix Alstec.ALS24xx prompt for configure mode.

--- sa/profiles/Alstec/24xx/__init__.py
+++ sa/profiles/Alstec/24xx/__init__.py
@@ -15,7 +15,7 @@ class Profile(BaseProfile):
     name = "Alstec.24xx"
     pattern_username = r"^User:"
     pattern_unprivileged_prompt = r"^(?P<hostname>[ \S]+) >"
-    pattern_prompt = r"^(?P<hostname>[ \S]+)(\s\(\S+\)){0,3} #"
+    pattern_prompt = r"^(?P<hostname>[ \S]+?)(\s\([\S ]+?\)){0,3} #"
     pattern_more = [(r"^--More-- or \(q\)uit$", " "),
                     (r"This operation may take a few minutes.\n"
                      r"Management interfaces will not be available during this time.\n"
