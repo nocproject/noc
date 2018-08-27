f647210cbed9e3256550d7fefaff327f40f6d598
uncol <d.a.zakharov@gmail.com>
2018-08-27 14:03:16 +0300

    filter: full reset

--- ui/web/core/ModelApplication.js
+++ ui/web/core/ModelApplication.js
@@ -1127,6 +1127,11 @@ Ext.define("NOC.core.ModelApplication", {
     onResetFilters: function() {
         var me = this;
         me.filterPanel.getForm().reset();
+        Ext.each(me.filterPanel.query(".button[enableToggle]"),
+            function(b) {
+                b.toggle(false);
+                b.fireHandler();
+            });
         me.onFilter();
     },
     //
@@ -1262,7 +1267,7 @@ Ext.define("NOC.core.ModelApplication", {
     },
     //
     onInlineCancel: function(editor, context) {
-        if(Ext.isEmpty(context.originalValue)){
+        if(Ext.isEmpty(context.originalValue)) {
             context.grid.store.removeAt(context.rowIdx);
         }
     },
