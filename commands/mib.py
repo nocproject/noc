d0c25af7d00ed8c5c4385daa01b1bdf32e4418b2
Andrey Vertiprahov <aversanta@gmail.com>
2018-08-30 18:35:07 +0500

    Port make-cmib to mib command.

--- commands/mib.py
+++ commands/mib.py
@@ -8,11 +8,16 @@
 
 # Python modules
 from __future__ import print_function
+import logging
 import argparse
 import re
+import os
+import datetime
 # NOC modules
 from noc.core.management.base import BaseCommand
 from noc.core.service.client import open_sync_rpc, RPCError
+from noc.fm.models.mib import MIB, MIBData
+from noc.services.mib.api.mib import MIBAPI
 from noc.core.error import ERR_MIB_MISSED
 
 
@@ -37,8 +42,28 @@ class Command(BaseCommand):
             nargs=argparse.REMAINDER,
             help="SNMP OIDs"
         )
+        # Make cmib "Make compiled MIB for SA and PM scripts"
+        make_cmib_parser = subparsers.add_parser("make_cmib")
+        make_cmib_parser.add_argument("-o", "--output",
+                                      dest="output", default="")
+        make_cmib_parser.add_argument(
+            "oids",
+            nargs=argparse.REMAINDER,
+            help="SNMP OIDs"
+        )
+        # lookup
+        lookup_parser.add_argument(
+            "oids",
+            nargs=argparse.REMAINDER,
+            help="SNMP OIDs"
+        )
         # import
         import_parser = subparsers.add_parser("import")
+        import_parser.add_argument(
+            "--local",
+            action="store_true",
+            help="Not use mib service for import"
+        )
         import_parser.add_argument(
             "paths",
             nargs=argparse.REMAINDER,
@@ -78,28 +103,89 @@ class Command(BaseCommand):
         except RPCError as e:
             self.die("RPC Error: %s" % e)
 
+    def handle_make_cmib(self, oids, *args, **kwargs):
+        if len(oids) != 1:
+            self.stdout.write("Specify one MIB\n")
+            self.die("")
+        if kwargs["output"]:
+            self.prepare_dirs(kwargs["output"])
+            self.print("Opening file %s", kwargs["output"])
+            f = open(kwargs["output"], "w")
+            f = f.write
+        else:
+            self.print("Dumping to stdout")
+            f = self.print
+        mib = oids[0]
+        try:
+            m = MIB.objects.get(name=mib)
+        except MIB.DoesNotExist:
+            self.stdout.write("MIB not found: %s\n" % mib)
+            self.die("")
+        year = datetime.date.today().year
+        r = [
+            "# -*- coding: utf-8 -*-",
+            "# ----------------------------------------------------------------------",
+            "# %s" % mib,
+            "#     Compiled MIB",
+            "#     Do not modify this file directly",
+            "#     Run ./noc make-cmib instead",
+            "# ----------------------------------------------------------------------",
+            "# Copyright (C) 2007-%s The NOC Project" % year,
+            "# See LICENSE for details",
+            "# ----------------------------------------------------------------------",
+            "",
+            "# MIB Name",
+            "NAME = \"%s\"" % mib,
+            "# Metadata",
+            "LAST_UPDATED = \"%s\"" %
+            m.last_updated.isoformat().split("T")[0],
+            "COMPILED = \"%s\"" % datetime.date.today().isoformat(),
+            "# MIB Data: name -> oid",
+            "MIB = {"
+        ]
+        rr = []
+        for md in sorted(
+                MIBData.objects.filter(mib=m.id),
+                key=lambda x: [int(y) for y in x.oid.split(".")]
+        ):
+            rr += ["    \"%s\": \"%s\"" % (md.name, md.oid)]
+        r += [",\n".join(rr)]
+        r += ["}", ""]
+        data = "\n".join(r)
+        f(data)
+
+    def prepare_dirs(self, path):
+        d = os.path.dirname(path)
+        if not os.path.isdir(d):
+            self.print("Creating directory %s", d)
+            os.makedirs(d)
+
     def handle_import(self, paths, *args, **kwargs):
         left_paths = paths
         while left_paths:
             done = set()
             for p in left_paths:
-                if self.upload_mib(p):
+                if self.upload_mib(p, **kwargs):
                     done.add(p)
             if not done:
                 # Cannot load additional mibs
                 self.die("Cannot load MIBs: %s" % ", ".join(left_paths))
-            left_paths = [x for x in left_paths if x in done]
+            left_paths = [x for x in left_paths if x not in done]
 
-    def upload_mib(self, path):
+    def upload_mib(self, path, local=False):
         """
         Upload mib from file
         :param path:
+        :param local:
         :return:
         """
         with open(path) as f:
             data = f.read()
         try:
-            svc = open_sync_rpc("mib")
+            if local:
+                svc = MIBAPI(ServiceStub(), None, None)
+            else:
+                svc = open_sync_rpc("mib")
             r = svc.compile(data)
             if r.get("status"):
                 return True
@@ -108,6 +194,14 @@ class Command(BaseCommand):
             self.die("Cannot upload %s: %s" % (path, r.get("msg")))
         except RPCError as e:
             self.die("RPC Error: %s" % e)
+
+
+class ServiceStub(object):
+    def __init__(self):
+        self.service_id = "stub"
+        self.logger = logging.getLogger(__name__)
+        self.address = "127.0.0.1"
+        self.port = 0
 
 
 if __name__ == "__main__":
