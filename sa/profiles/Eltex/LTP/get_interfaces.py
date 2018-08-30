1dbb2e6280be59c3b5c66999f8d8249dd80cad59
Dmitry S. Luhtionov <dmitryluhtionov@gmail.com>
2018-08-30 16:31:55 +0300

    Update Eltex.LTP profile

--- sa/profiles/Eltex/LTP/get_interfaces.py
+++ sa/profiles/Eltex/LTP/get_interfaces.py
@@ -27,6 +27,7 @@ class Script(BaseScript):
         re.MULTILINE
     )
     rx_mac = re.compile(r"^\s+MAC address: (?P<mac>\S+)", re.MULTILINE)
+    rx_status = re.compile(r"^.+\d\s+(?P<oper_status>up|down)", re.MULTILINE)
 
     def normalize_ifname(self, port):
         port = port.strip()
@@ -127,6 +128,13 @@ class Script(BaseScript):
                 if match:
                     i["mac"] = match.group("mac")
                     i["subinterfaces"][0]["mac"] = match.group("mac")
+                try:
+                    c = self.cli("show interfaces status %s" % i["name"])
+                    match = self.rx_status.search(c)
+                    i["oper_status"] = match.group("oper_status") == "up"
+                    i["subinterfaces"][0]["oper_status"] = match.group("oper_status") == "up"
+                except self.CLISyntaxError:
+                    pass
         c = self.cli("show management")
         match = self.rx_mgmt.search(c)
         ip_address = "%s/%s" % (
@@ -135,8 +143,12 @@ class Script(BaseScript):
         iface = {
             "name": "management",
             "type": "SVI",
+            "admin_status": True,
+            "oper_status": True,
             "subinterfaces": [{
                 "name": "management",
+                "admin_status": True,
+                "oper_status": True,
                 "enabled_afi": ["IPv4"],
                 "ipv4_addresses": [ip_address],
                 "vlan_ids": int(match.group("vlan_id"))
---
+++ sa/profiles/Eltex/LTP/get_lldp_neighbors.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# Eltex.LTP.get_lldp_neighbors
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2018 The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+
+# Python modules
+import re
+# NOC modules
+from noc.sa.profiles.Generic.get_lldp_neighbors import Script as BaseScript
+from noc.sa.interfaces.igetlldpneighbors import IGetLLDPNeighbors
+from noc.sa.interfaces.base import MACAddressParameter
+from noc.lib.validators import is_int, is_ipv4, is_ipv6, is_mac
+from noc.lib.text import parse_table
+
+
+class Script(BaseScript):
+    name = "Eltex.LTP.get_lldp_neighbors"
+    interface = IGetLLDPNeighbors
+
+    CAPS_MAP = {
+        "O": 1, "r": 2, "B": 4,
+        "W": 8, "R": 16, "T": 32,
+        "C": 64, "S": 128, "D": 256,
+        "H": 512, "TP": 1024,
+    }
+
+    rx_detail = re.compile(
+        r"^Port description:(?P<port_descr>.*)\n",
+        re.MULTILINE
+    )
+
+    def execute_cli(self):
+        r = []
+        with self.profile.switch(self):
+            lldp = self.cli("show lldp neighbors")
+            for link in parse_table(lldp, allow_wrap=True):
+                local_interface = link[0]
+                remote_chassis_id = link[1]
+                remote_port = link[2]
+                remote_system_name = link[3]
+                # Get capability
+                cap = 0
+                for c in link[4].split(","):
+                    c = c.strip()
+                    if c:
+                        cap |= self.CAPS_MAP[c]
+
+                if (is_ipv4(remote_chassis_id) or is_ipv6(remote_chassis_id)):
+                    remote_chassis_id_subtype = 5
+                elif is_mac(remote_chassis_id):
+                    remote_chassis_id = MACAddressParameter().clean(remote_chassis_id)
+                    remote_chassis_id_subtype = 4
+                else:
+                    remote_chassis_id_subtype = 7
+
+                # Get remote port subtype
+                remote_port_subtype = 1
+                if is_ipv4(remote_port):
+                    # Actually networkAddress(4)
+                    remote_port_subtype = 4
+                elif is_mac(remote_port):
+                    # Actually macAddress(3)
+                    # Convert MAC to common form
+                    remote_port = MACAddressParameter().clean(remote_port)
+                    remote_port_subtype = 3
+                elif is_int(remote_port):
+                    # Actually local(7)
+                    remote_port_subtype = 7
+                i = {
+                    "local_interface": local_interface,
+                    "neighbors": []
+                }
+                n = {
+                    "remote_chassis_id": remote_chassis_id,
+                    "remote_chassis_id_subtype": remote_chassis_id_subtype,
+                    "remote_port": remote_port,
+                    "remote_port_subtype": remote_port_subtype,
+                    "remote_capabilities": cap,
+                }
+                if remote_system_name:
+                    n["remote_system_name"] = remote_system_name
+
+                try:
+                    c = self.cli("show lldp neighbors interface %s" % local_interface)
+                    match = self.rx_detail.search(c)
+                    if match:
+                        if match.group("port_descr").strip():
+                            port_descr = match.group("port_descr").strip()
+                            n["remote_port_description"] = port_descr
+                except Exception:
+                    pass
+                i["neighbors"] += [n]
+                r += [i]
+        return r
