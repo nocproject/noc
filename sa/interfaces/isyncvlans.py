da020ad004f364f58a232d7e780bfd5065d66d46
Andrey Vertiprahov <aversanta@gmail.com>
2018-09-03 08:59:09 +0500

    Fix PEP8.

--- sa/interfaces/isyncvlans.py
+++ sa/interfaces/isyncvlans.py
@@ -8,15 +8,15 @@
 from __future__ import absolute_import
 # NOC Modules
 from noc.core.interface.base import BaseInterface
-from .base import (ListOfParameter, DictParameter,
+from .base import (ListOfParameter, DictParameter, DictListParameter,
                    VLANIDParameter, StringParameter, StringListParameter)
 
 
 class ISyncVlans(BaseInterface):
-    vlans = ListOfParameter(element=DictParameter(attrs={
+    vlans = DictListParameter(attrs={
         "vlan_id": VLANIDParameter(),
         "name": StringParameter(required=False)
-    }))
+    })
     tagged_ports = StringListParameter(default=[])
     returns = DictParameter(attrs={
         "created": ListOfParameter(element=VLANIDParameter()),
