8b5e426178fce3fa9dc48da69040fbc37edd61a7
Andrey Vertiprahov <aversanta@gmail.com>
2018-09-11 14:41:16 +0500

    Fix selfmong default config.

--- lib/nosql.py
+++ lib/nosql.py
@@ -58,10 +58,19 @@ RECURSIVE_REFERENCE_CONSTANT = "self"
 
 
 def get_connection():
+    """
+
+    :return:
+    :rtype: pymongo.collection.Connection
+    """
     return mongoengine.connection._get_connection()
 
-
 def get_db():
+    """
+
+    :return:
+    :rtype: pymongo.database.Database
+    """
     return mongoengine.connection._get_db()
 
 
