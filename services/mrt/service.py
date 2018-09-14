cb2d8e46b12ab7a2f26fcd40ad3124b211dd9dc4
Your Name <aversanta@gmail.com>
2018-09-14 10:57:52 +0500

    Fix MRT services relative import in non-package.

--- services/mrt/service.py
+++ services/mrt/service.py
@@ -7,11 +7,9 @@
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
-# Python modules
-from __future__ import absolute_import
 # NOC modules
 from noc.core.service.base import Service
-from .mrt import MRTRequestHandler
+from noc.services.mrt.mrt import MRTRequestHandler
 from noc.config import config
 
 
