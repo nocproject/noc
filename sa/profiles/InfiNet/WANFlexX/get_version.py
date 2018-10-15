48840be70d4f81f64e02e8e2796d4c0cc6a17153
NOC user <noc@node1.knd-oil.ru>
2018-10-15 15:24:11 +0500

    get_version

--- sa/profiles/InfiNet/WANFlexX/get_version.py
+++ sa/profiles/InfiNet/WANFlexX/get_version.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 # ---------------------------------------------------------------------
 # InfiNet.WANFlexX.get_version
+# sergey.sadovnikov@gmail.com
 # ---------------------------------------------------------------------
 # Copyright (C) 2007-2017 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 """
 """
+# Python modules
+import re
+
 from noc.core.script.base import BaseScript
 from noc.sa.interfaces.igetversion import IGetVersion
-import re
 
 
 class Script(BaseScript):
@@ -17,14 +20,18 @@ class Script(BaseScript):
     cache = True
     interface = IGetVersion
 
-    rx_ver = re.compile(r"^(?P<platform>.+?)\s+WANFleX\s+(?P<version>\S+)",
-                        re.MULTILINE)
+    rx_platform = re.compile(r"PN:(?P<platform>.+)")
+    rx_ver = re.compile(r"WANFleX\s+(?P<version>\S+)")
 
     def execute(self):
-        cmd = self.cli("system version")
-        match = self.rx_ver.search(cmd.strip())
-        return {
+        v = self.cli("system version", cached=True)
+        match = self.re_search(self.rx_ver, v)
+        version = match.group("version")
+        match = self.re_search(self.rx_platform, v)
+        platform = match.group("platform")
+        r = {
             "vendor": "InfiNet",
-            "platform": match.group("platform"),
-            "version": match.group("version")
+            "platform": platform,
+            "version": version
         }
+        return r
