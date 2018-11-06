dde5f1dc9a848b112c084084b5f9b0c32e437288
Dmitry Volodin <dv@nocproject.org>
2018-11-06 09:30:32 +0300

    RepoPreview MVVC

--- ui/web/core/RepoPreview.js
+++ ui/web/core/RepoPreview.js
@@ -20,46 +20,67 @@ Ext.define('NOC.core.RepoPreview', {
     // preview_theme: NOC.settings.preview_theme,
     preview_theme: 'monokai',
     mode: 'groovy',
+    viewModel: {
+        data: {
+            currentSelection: null,
+            diffSelection: null
+        },
+        formulas: {
+            canPrevChange: {
+                bind: {
+                    bindTo: "{currentSelection.selection}"
+                },
+                get: function(t) {
+                    return t && t.store.indexOf(t.data) < t.store.data.length - 1
+                }
+            },
+            canNextChange: {
+                bind: {
+                    bindTo: "{currentSelection.selection}"
+                },
+                get: function(t) {
+                    return t && t.store.indexOf(t.data) > 0
+                }
+            },
+            canSideBySide: {
+                bind: {
+                    bindTo: "{diffSelection.selection}"
+                },
+                get: function(t) {
+                    return !!t
+                }
+            },
+            canSwap: {
+                bind: {
+                    bindTo: "{diffSelection.selection}"
+                },
+                get: function(t) {
+                    return !!t
+                }
+            }
+        }
+    },
 
     initComponent: function() {
         var me = this;
 
-        // me.revSlider = Ext.create('NOC.core.RevisionSlider', {
-        //     label: __('Version'),
-        //     labelWidth: 45,
-        //     onChange: Ext.bind(me.onSlideRev, me),
-        //     width: 200
-        // });
-        //
-        // me.diffSlider = Ext.create('NOC.core.RevisionSlider', {
-        //     label: __('Compare'),
-        //     labelWidth: 55,
-        //     onChange: Ext.bind(me.onSlideDiff, me),
-        //     width: 200
-        // });
-
-        // me.compareModeButton = Ext.create("Ext.button.Button", {
-        //     text: __("Compare Mode"),
-        //     tooltip: __("Switch on/off compare mode"),
-        //     enableToggle: true,
-        //     pressed: false,
-        //     scope: me,
-        //     handler: me.onCompare
-        // });
-
         me.sideBySideModeButton = Ext.create('Ext.button.Button', {
             text: __('Side-By-Side'),
             tooltip: __('Switch on/off side-by-side mode'),
+            glyph: NOC.glyph.columns,
             enableToggle: true,
             pressed: false,
-            disabled: true,
             scope: me,
-            handler: me.onSideBySide
+            handler: me.onSideBySide,
+            bind: {
+                disabled: "{!canSideBySide}"
+            }
         });
 
         me.downloadButton = Ext.create('Ext.button.Button', {
             text: __('Download'),
             tooltip: __('Download config'),
+            glyph: NOC.glyph.download,
             scope: me,
             handler: me.onDownload
         });
@@ -96,12 +117,12 @@ Ext.define('NOC.core.RepoPreview', {
                 scope: me,
                 select: me.onSelectRev,
                 specialkey: me.onRevSpecialKey
-            }
+            },
+            reference: "currentSelection"
         });
 
         me.diffCombo = Ext.create('Ext.form.ComboBox', {
             fieldLabel: __('Compare'),
-            disabled: true,
             labelWidth: 45,
             labelAlign: 'right',
             width: 210,
@@ -132,7 +153,8 @@ Ext.define('NOC.core.RepoPreview', {
                 scope: me,
                 select: me.onSelectDiff,
                 specialkey: me.onDiffSpecialKey
-            }
+            },
+            reference: "diffSelection"
         });
 
         me.resetButton = Ext.create('Ext.button.Button', {
@@ -146,53 +168,32 @@ Ext.define('NOC.core.RepoPreview', {
         me.nextDiffButton = Ext.create('Ext.button.Button', {
             glyph: NOC.glyph.arrow_up,
             tooltip: __('Next change'),
-            disabled: true,
             scope: me,
-            handler: me.onNextDiff
+            handler: me.onNextDiff,
+            bind: {
+                disabled: "{!canNextChange}"
+            }
         });
         //
         me.prevDiffButton = Ext.create('Ext.button.Button', {
             glyph: NOC.glyph.arrow_down,
             tooltip: __('Previous change'),
-            disabled: true,
             scope: me,
-            handler: me.onPrevDiff
+            handler: me.onPrevDiff,
+            bind: {
+                disabled: "{!canPrevChange}"
+            }
         });
         //
         me.swapRevButton = Ext.create('Ext.button.Button', {
             glyph: NOC.glyph.exchange,
             tooltip: __('Swap revisions'),
-            disabled: true,
             scope: me,
-            handler: me.onSwapRev
+            handler: me.onSwapRev,
+            bind: {
+                disabled: "{!canSwap}"
+            }
         });
-        //
-        // me.lastDayButton = Ext.create("Ext.button.Button", {
-        //     text: __("Day"),
-        //     tooltip: __("Last day's changes"),
-        //     toogleGroup: "diffrange",
-        //     scope: me,
-        //     handler: me.onLastPressed,
-        //     diffRange: 1
-        // });
-        //
-        // me.lastWeekButton = Ext.create("Ext.button.Button", {
-        //     text: __("Week"),
-        //     tooltip: __("Last week's changes"),
-        //     toogleGroup: "diffrange",
-        //     scope: me,
-        //     handler: me.onLastPressed,
-        //     diffRange: 7
-        // });
-        //
-        // me.lastMonthButton = Ext.create("Ext.button.Button", {
-        //     text: __("Month"),
-        //     tooltip: __("Last month's changes"),
-        //     toogleGroup: "diffrange",
-        //     scope: me,
-        //     handler: me.onLastPressed,
-        //     diffRange: 30
-        // });
 
         me.cmContainer = this.getCmContainer();
 
@@ -213,25 +214,15 @@ Ext.define('NOC.core.RepoPreview', {
                     me.resetButton,
                     '-',
                     me.revCombo,
-                    // me.revSlider,
                     '-',
                     me.swapRevButton,
-                    // me.compareModeButton,
                     '-',
                     me.diffCombo,
-                    // me.diffSlider,
                     '-',
-                    me.nextDiffButton,
                     me.prevDiffButton,
+                    me.nextDiffButton,
                     me.sideBySideModeButton,
                     me.downloadButton
-                    // me.lastDayButton,
-                    // me.lastWeekButton,
-                    // me.lastMonthButton
-                    // "->",
-                    // me.diffCombo,
-                    // me.nextDiffButton,
-                    // me.prevDiffButton
                 ]
             }],
             items: [me.cmContainer],
@@ -290,21 +281,8 @@ Ext.define('NOC.core.RepoPreview', {
             lineNumbers: true,
             styleActiveLine: true
         });
-        // change the codemirror css
-        // var css = Ext.util.CSS.getRule('.CodeMirror');
-        // if(css) {
-        //     // css.style.height = '100%';
-        //     css.style.position = 'relative';
-        //     css.style.overflow = 'hidden';
-        // }
-        // css = Ext.util.CSS.getRule('.CodeMirror-Scroll');
-        // if(css) {
-        //     css.style.height = '100%';
-        // }
         me.setTheme(me.preview_theme);
         me.viewer.setOption('mode', me.mode);
-        me.sideBySideModeButton.setDisabled(true);
-        // me.compareModeButton.toggle(false);
         me.sideBySideModeButton.toggle(false);
         CodeMirror.autoLoadMode(me.viewer, me.mode);
     },
@@ -354,12 +332,6 @@ Ext.define('NOC.core.RepoPreview', {
             success: function(response) {
                 me.renderText(Ext.decode(response.responseText));
                 mask.hide();
-                // if(me.historyHashPrefix) {
-                //     me.app.setHistoryHash(
-                //         me.currentRecord.get("id"),
-                //         me.historyHashPrefix
-                //     );
-                // }
             },
             failure: function() {
                 mask.hide();
@@ -380,18 +352,11 @@ Ext.define('NOC.core.RepoPreview', {
                 me.diffCombo.setValue(null);
                 me.revCombo.setValue(null);
                 me.sideBySideModeButton.toggle(false);
-                // console.log(me.revCombo.getValue());
-                // console.log(me.diffCombo.getValue());
                 me.revCombo.store.loadData(data);
                 me.diffCombo.store.loadData(data);
                 if(data.length > 0) {
                     me.revCombo.select([me.revCombo.store.getAt(0)]);
                 }
-                me.nextDiffButton.setDisabled(true);
-                me.prevDiffButton.setDisabled(data.length === 1);
-
-                // me.revSlider.setStore(data);
-                // me.diffSlider.setStore(data);
                 Ext.callback(callback, me);
             },
             failure: function() {
@@ -410,13 +375,6 @@ Ext.define('NOC.core.RepoPreview', {
             success: function(response) {
                 me.renderText(Ext.decode(response.responseText));
                 mask.hide();
-                // if(me.historyHashPrefix) {
-                //     me.app.setHistoryHash(
-                //         me.currentRecord.get("id"),
-                //         me.historyHashPrefix,
-                //         rev
-                //     );
-                // }
             },
             failure: function() {
                 NOC.error(__('Failed to get text'));
@@ -468,14 +426,6 @@ Ext.define('NOC.core.RepoPreview', {
                 success: function(response) {
                     me.renderText(Ext.decode(response.responseText), 'diff');
                     mask.hide();
-                    // if(me.historyHashPrefix) {
-                    //     me.app.setHistoryHash(
-                    //         me.currentRecord.get("id"),
-                    //         me.historyHashPrefix,
-                    //         rev1,
-                    //         rev2
-                    //     );
-                    // }
                 },
                 failure: function() {
                     NOC.error(__('Failed to get diff'));
@@ -515,14 +465,10 @@ Ext.define('NOC.core.RepoPreview', {
             if(!Ext.isFunction(me.viewer.getMode)) {
                 me.removeViewer();
                 // save state
-                var sd = me.sideBySideModeButton.disabled;
                 var sp = me.sideBySideModeButton.pressed;
-                // var cp = me.compareModeButton.pressed;
                 me.initViewer();
                 // restore
-                me.sideBySideModeButton.setDisabled(sd);
                 me.sideBySideModeButton.toggle(sp);
-                // me.compareModeButton.toggle(cp);
             }
             me.viewer.setValue(text);
         }
@@ -532,9 +478,6 @@ Ext.define('NOC.core.RepoPreview', {
     onSelectRev: function(combo, records, eOpts) {
         var me = this;
         me.requestRevision(records.get('id'));
-        me.diffCombo.setDisabled(false);
-        me.swapRevButton.setDisabled(false);
-        me.sideBySideModeButton.setDisabled(false);
     },
     //
     requestCurrentDiff: function() {
@@ -548,7 +491,6 @@ Ext.define('NOC.core.RepoPreview', {
     onSelectDiff: function(combo, records, eOpts) {
         var me = this;
         me.requestCurrentDiff();
-        me.sideBySideModeButton.setDisabled(false);
     },
     //
     onRevSpecialKey: function(combo, evt, opts) {
@@ -562,7 +504,7 @@ Ext.define('NOC.core.RepoPreview', {
         var me = this;
         if(evt.getKey() === evt.ESC) {
             combo.clearValue();
-            me.onSelectRev(me.revCombo, [me.revCombo.picker.getSelectionModel().lastSelected]);
+            me.requestRevision(me.revCombo.getPicker().getSelectionModel().lastSelected.get("id"));
         }
     },
     // Returns current selection index or null
@@ -587,12 +529,7 @@ Ext.define('NOC.core.RepoPreview', {
         } else {
             dIndex = rIndex + 1;
         }
-        me.sideBySideModeButton.setDisabled(false);
         me.setRevIndex(me.diffCombo, dIndex);
-        me.prevDiffButton.setDisabled(dIndex >= me.revCombo.store.data.length - 1);
-        me.nextDiffButton.setDisabled(false);
-        me.diffCombo.setDisabled(false);
-        me.swapRevButton.setDisabled(false);
         me.requestCurrentDiff();
     },
     //
@@ -605,12 +542,7 @@ Ext.define('NOC.core.RepoPreview', {
             me.setRevIndex(me.revCombo, rIndex);
         }
         dIndex = rIndex + 1;
-        me.sideBySideModeButton.setDisabled(false);
         me.setRevIndex(me.diffCombo, dIndex);
-        me.prevDiffButton.setDisabled(false);
-        me.nextDiffButton.setDisabled(rIndex <= 0);
-        me.diffCombo.setDisabled(false);
-        me.swapRevButton.setDisabled(false);
         me.requestCurrentDiff();
     },
     //
@@ -622,65 +554,12 @@ Ext.define('NOC.core.RepoPreview', {
         me.setRevIndex(me.diffCombo, rIndex);
         me.requestCurrentDiff();
     },
-    //
-    // onLastPressed: function(button, evt) {
-    //     var me = this,
-    //         store = me.revCombo.store,
-    //         rl = store.data.length - 1,
-    //         i0 = me.getRevIndex(me.revCombo),
-    //         t0 = i0 === 0 ? +(new Date()) : +store.getAt(0).get("ts"),
-    //         t1 = t0 - button.diffRange * 86400000,
-    //         i1;
-    //     if(i0 === 0 && +store.getAt(0).get("ts") <= t1) {
-    //         NOC.info(__("Nothing changed"));
-    //         return;
-    //     }
-    //     for(i1 = i0 + 1; i1 < rl; i1++) {
-    //         if(+store.getAt(i1).get("ts") <= t1)
-    //             break;
-    //     }
-    //     me.setRevIndex(me.revCombo, i0);
-    //     me.diffCombo.setDisabled(false);
-    //     me.swapRevButton.setDisabled(false);
-    //     me.setRevIndex(me.diffCombo, i1);
-    //     me.requestCurrentDiff();
-    // },
-    //
+
     onReset: function() {
         var me = this;
         me.requestText();
         me.requestRevisions();
     },
-    //
-    // historyRevision: function(record, rev) {
-    //     var me = this;
-    //     me.startPreview(record);
-    //     me.requestRevisions(function() {
-    //         me.setRevIndex(
-    //             me.revCombo,
-    //             me.revCombo.store.findExact("id", rev)
-    //         );
-    //         me.diffCombo.setDisabled(false);
-    //     });
-    //     me.requestRevision(rev);
-    // },
-    // //
-    // historyDiff: function(record, rev1, rev2) {
-    //     var me = this;
-    //     me.startPreview(record);
-    //     me.requestRevisions(function() {
-    //         me.diffCombo.setDisabled(false);
-    //         me.setRevIndex(
-    //             me.revCombo,
-    //             me.revCombo.store.findExact("id", rev2)
-    //         );
-    //         me.setRevIndex(
-    //             me.diffCombo,
-    //             me.diffCombo.store.findExact("id", rev1)
-    //         );
-    //     });
-    //     me.requestDiff(rev1, rev2);
-    // },
     //
     getCmContainer: function() {
         var me = this;
@@ -696,42 +575,11 @@ Ext.define('NOC.core.RepoPreview', {
         });
     },
     //
-    // onSlideRev: function() {
-    //     var me = this,
-    //         ids = me.getIds();
-    //     if(me.compareModeButton.pressed) {
-    //         me.requestDiff(ids.leftId, ids.rightId);
-    //     } else {
-    //         me.requestRevision(ids.leftId);
-    //     }
-    // },
-    // //
-    // onSlideDiff: function(slider, value) {
-    //     var me = this,
-    //         ids = me.getIds();
-    //     me.compareModeButton.toggle(true);
-    //     me.sideBySideModeButton.setDisabled(false);
-    //     me.requestDiff(ids.leftId, ids.rightId);
-    // },
-    //
     onBack: function() {
         var me = this;
-        // me.revSlider.slider.fireEvent('hideTip');
-        // me.diffSlider.slider.fireEvent('hideTip');
         me.onClose();
     },
     //
-    // onCompare: function() {
-    //     var me = this,
-    //         ids = me.getIds();
-    //     if(me.compareModeButton.pressed) {
-    //         me.requestDiff(ids.leftId, ids.rightId);
-    //         me.sideBySideModeButton.setDisabled(false);
-    //     } else {
-    //         me.sideBySideModeButton.setDisabled(true);
-    //         me.requestRevision(ids.leftId);
-    //     }
-    // },
     onSideBySide: function() {
         var me = this,
             ids = me.getIds();
@@ -750,8 +598,11 @@ Ext.define('NOC.core.RepoPreview', {
         return {
             leftId: me.revCombo.getValue(),
             rightId: me.diffCombo.getValue()
-            // rightId: me.diffSlider.getRevId(),
-            // leftId: me.revSlider.getRevId()
         };
+    },
+    //
+    setLoading: function(cfg) {
+        var me = this;
+        return me.cmContainer.setLoading(cfg)
     }
 });
