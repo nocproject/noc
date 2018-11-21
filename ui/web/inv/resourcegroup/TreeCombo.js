25797371c53dce8e404246bcccd38670295a71c0
Dmitry Volodin <dv@nocproject.org>
2018-11-21 15:40:00 +0300

    ResourceGroup: TreeCombo

--- ui/web/inv/resourcegroup/TreeCombo.js
+++ ui/web/inv/resourcegroup/TreeCombo.js
@@ -1,12 +1,12 @@
 //---------------------------------------------------------------------
-// NOC.inv.networksegment.TreeCombo
+// NOC.inv.resourcegroup.TreeCombo
 //---------------------------------------------------------------------
 // Copyright (C) 2007-2018 The NOC Project
 // See LICENSE for details
 //---------------------------------------------------------------------
-console.debug("Defining NOC.inv.networksegment.TreeCombo");
+console.debug("Defining NOC.inv.resourcegroup.TreeCombo");
 
-Ext.define("NOC.inv.networksegment.TreeCombo", {
+Ext.define("NOC.inv.resourcegroup.TreeCombo", {
     extend: "NOC.core.TreeCombo",
-    alias: "widget.inv.networksegment.TreeCombo"
+    alias: "widget.inv.resourcegroup.TreeCombo"
 });
