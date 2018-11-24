0fe983b8ccd5f2c0d9bf6198f515dbdb0471ec9e
Dmitry Volodin <dv@nocproject.org>
2018-11-24 13:16:41 +0300

    Remove excessive metrics configuration

--- sla/models/slaprofile.py
+++ sla/models/slaprofile.py
@@ -35,41 +35,6 @@ class SLAProfileMetrics(EmbeddedDocument):
     enable_periodic = BooleanField(default=True)
     # Send metrics to persistent store
     is_stored = BooleanField(default=True)
-    # Window depth
-    window_type = StringField(
-        max_length=1,
-        choices=[
-            ("m", "Measurements"),
-            ("t", "Time")
-        ]
-    )
-    # Window size. Depends on window type
-    # * m - amount of measurements
-    # * t - time in seconds
-    window = IntField(default=1)
-    # Window function
-    # Accepts window as a list of [(timestamp, value)]
-    # and window_config
-    # and returns float value
-    window_function = StringField(choices=wf_choices, default="last")
-    # Window function configuration
-    window_config = StringField()
-    # Convert window function result to percents of interface bandwidth
-    window_related = BooleanField(default=False)
-    # Threshold settings
-    # Raise error if window_function result is below *low_error*
-    low_error = FloatField(required=False)
-    # Raise warning if window_function result is below *low_warn*
-    low_warn = FloatField(required=False)
-    # Raise warning if window_function result is above *high_warn*
-    high_warn = FloatField(required=False)
-    # Raise error if window_function result is above *high_err*
-    high_error = FloatField(required=False)
-    # Severity weights
-    low_error_weight = IntField(default=10)
-    low_warn_weight = IntField(default=1)
-    high_warn_weight = IntField(default=1)
-    high_error_weight = IntField(default=10)
     # Threshold processing
     threshold_profile = ReferenceField(ThresholdProfile)
 
