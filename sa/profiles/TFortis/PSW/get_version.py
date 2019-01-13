caa79c40381f3abce78e467b95159ac2cfe5feec
Andrey Vertiprahov <aversanta@gmail.com>
2019-01-13 10:57:27 +0500

    Fix TFortis.get_version script.

--- sa/profiles/TFortis/PSW/get_version.py
+++ sa/profiles/TFortis/PSW/get_version.py
@@ -20,17 +20,20 @@ class Script(BaseScript):
     rx_html_ver = re.compile(
         r"Firmware version(?P<version>.*)Bootloader version(?P<bootloader>.*)\sMAC")
     rx_html_platform = re.compile(r"^TFortis (?P<platform>.*)\x00")
+    rx_html_platform2 = re.compile(r"(?P<platform>PSW-\S+)")
 
     def execute(self):
         v = self.http.get("/header_name.shtml", eof_mark="</html>")
         v = strip_html_tags(v)
         platform = self.rx_html_platform.search(v)
+        if not platform:
+            platform = self.rx_html_platform2.search(v)
         v = self.http.get("/main.shtml", eof_mark="</html>")
         v = strip_html_tags(v)
         match = self.rx_html_ver.search(v)
         return {
             "vendor": "TFortis",
-            "platform": platform.group("platform"),
+            "platform": platform.group("platform").strip("\x00"),
             "version": match.group("version"),
             "attributes": {
                 "Bootloader": match.group("bootloader"),
