2adfebdfdd8957d1db36bf007d360d8913452e1d
Dmitry Volodin <dv@nocproject.org>
2019-01-18 12:05:48 +0300

    ManagedObjectProfile: Refer to `default` caps profile by default

--- sa/models/managedobjectprofile.py
+++ sa/models/managedobjectprofile.py
@@ -365,7 +365,11 @@ class ManagedObjectProfile(models.Model):
     # Object id in BI
     bi_id = models.BigIntegerField(unique=True)
     # Caps discovery settings
-    caps_profile = DocumentReferenceField(CapsProfile, null=False, blank=False)
+    caps_profile = DocumentReferenceField(
+        CapsProfile,
+        null=False, blank=False,
+        default=CapsProfile.get_default_profile
+    )
     # Object alarms can be escalated
     escalation_policy = models.CharField(
         "Escalation Policy",
