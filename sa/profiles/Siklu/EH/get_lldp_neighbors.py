cf30eff977fc5f5933730597a7d34b1def793891
Andrey Vertiprahov <aversanta@gmail.com>
2019-01-24 17:51:39 +0000

    Merge branch 'fix-avs-siklu-lldp-225' into 'master'
    Fix Siklu.EH.get_lldp_neighbors.
    
    See merge request noc/noc!1577
    
    (cherry picked from commit fde89231d6d0c7f80c9232eca4ccb070c4726c33)
    
    5c843ff8 Fix Siklu.EH.get_lldp_neighbors.

--- sa/profiles/Siklu/EH/get_lldp_neighbors.py
+++ sa/profiles/Siklu/EH/get_lldp_neighbors.py
@@ -11,6 +11,10 @@ import re
 # NOC modules
 from noc.core.script.base import BaseScript
 from noc.sa.interfaces.igetlldpneighbors import IGetLLDPNeighbors
+from noc.core.lldp import LLDP_CHASSIS_SUBTYPE_MAC, LLDP_CHASSIS_SUBTYPE_NETWORK_ADDRESS,\
+    LLDP_CHASSIS_SUBTYPE_INTERFACE_NAME, LLDP_CHASSIS_SUBTYPE_LOCAL, LLDP_PORT_SUBTYPE_ALIAS,\
+    LLDP_PORT_SUBTYPE_MAC, LLDP_PORT_SUBTYPE_NAME, LLDP_PORT_SUBTYPE_LOCAL
+from noc.lib.validators import is_mac
 
 
 class Script(BaseScript):
@@ -18,26 +22,26 @@ class Script(BaseScript):
     interface = IGetLLDPNeighbors
 
     CHASSIS_TYPES = {
-        "macaddress": 4,
-        "mac address": 4,
-        "mac-addr": 4,
-        "network-addr": 4,
-        "interfacename": 6,
-        "interface name": 6,
-        "local": 7
+        "macaddress": LLDP_CHASSIS_SUBTYPE_MAC,
+        "mac address": LLDP_CHASSIS_SUBTYPE_MAC,
+        "mac-addr": LLDP_CHASSIS_SUBTYPE_MAC,
+        "network-addr": LLDP_CHASSIS_SUBTYPE_NETWORK_ADDRESS,
+        "interfacename": LLDP_CHASSIS_SUBTYPE_INTERFACE_NAME,
+        "interface name": LLDP_CHASSIS_SUBTYPE_INTERFACE_NAME,
+        "local": LLDP_CHASSIS_SUBTYPE_LOCAL
     }
 
     PORT_TYPES = {
-        "interfacealias": 1,
-        "interface alias": 1,
-        "interface-alias": 1,
-        "macaddress": 3,
-        "mac address": 3,
-        "mac-addr": 3,
-        "interfacename": 5,
-        "interface-name": 5,
-        "interface name": 5,
-        "local": 7
+        "interfacealias": LLDP_PORT_SUBTYPE_ALIAS,
+        "interface alias": LLDP_PORT_SUBTYPE_ALIAS,
+        "interface-alias": LLDP_PORT_SUBTYPE_ALIAS,
+        "macaddress": LLDP_PORT_SUBTYPE_MAC,
+        "mac address": LLDP_PORT_SUBTYPE_MAC,
+        "mac-addr": LLDP_PORT_SUBTYPE_MAC,
+        "interfacename": LLDP_PORT_SUBTYPE_NAME,
+        "interface-name": LLDP_PORT_SUBTYPE_NAME,
+        "interface name": LLDP_PORT_SUBTYPE_NAME,
+        "local": LLDP_PORT_SUBTYPE_LOCAL
     }
 
     rx_ecfg = re.compile(
@@ -69,9 +73,15 @@ class Script(BaseScript):
                     "neighbors": []
                 }]
                 return r
+            remote_chassis_id = cfg["chassis-id"]
+            remote_chassis_type = self.CHASSIS_TYPES[cfg["chassis-id-subtype"]]
+            if cfg["chassis-id-subtype"] == 'network-addr' and is_mac(cfg["port-id"]):
+                # Network address is default 192.168.0.1
+                remote_chassis_id = cfg["port-id"]
+                remote_chassis_type = LLDP_CHASSIS_SUBTYPE_MAC
             neighbor = {
-                "remote_chassis_id": cfg["port-id"],
-                "remote_chassis_id_subtype": self.CHASSIS_TYPES[cfg["chassis-id-subtype"]],
+                "remote_chassis_id": remote_chassis_id,
+                "remote_chassis_id_subtype": remote_chassis_type,
                 "remote_port": cfg["port-id"],
                 "remote_port_subtype": self.PORT_TYPES[cfg["port-id-subtype"]]
             }
