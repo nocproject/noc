c484befa4b11f8e1eba56c35b9b6966f37b1da5c
Andrey Vertiprahov <aversanta@gmail.com>
2019-01-31 13:19:43 +0000

    Merge branch 'patch-avs-add-metric-huawei-ma5600t' into 'master'
    Add Interface Errors BIP metric to Huawei.MA5600T.
    
    See merge request noc/noc!1594
    
    (cherry picked from commit d166174f2719024747e2c79db711e13ac439303a)
    
    a72c1137 Add Interface Errors BIP metric to Huawei.MA5600T.

--- sa/profiles/Huawei/MA5600T/get_metrics.py
+++ sa/profiles/Huawei/MA5600T/get_metrics.py
@@ -21,12 +21,15 @@ class Script(GetMetricsScript):
               "laser bias current(ma)": "current_ma",
               "temperature(c)": "temp_c",
               "voltage(v)": "voltage_v",
-              "olt rx ont optical power(dbm)": "optical_rx_dbm_cpe"}
+              "olt rx ont optical power(dbm)": "optical_rx_dbm_cpe",
+              "upstream frame bip error count": "optical_errors_bip_out",
+              "downstream frame bip error count": "optical_errors_bip_in"}
 
     @metrics(
         ["Interface | DOM | RxPower", "Interface | DOM | TxPower",
          "Interface | DOM | Voltage", "Interface | DOM | Bias Current"
-         "Interface | DOM | Temperature"],
+         "Interface | DOM | Temperature",
+         "Interface | Errors | BIP | In", "Interface | Errors | BIP | Out"],
         # has_capability="Network | PON | OLT",
         access="C",  # CLI version
         volatile=False
@@ -36,13 +39,19 @@ class Script(GetMetricsScript):
         self.collect_cpe_metrics(metrics)
 
     def collect_cpe_metrics(self, metrics):
+        # ifaces = set(m.path[-1].split("/")[:2] for m in metrics)
         onts = self.scripts.get_cpe_status()  # Getting ONT List
         self.cli("config")
-        self.cli("interface gpon 0/0")  # Fix from cpes
-        for cc in onts:
+        iface = None
+        for cc in sorted(onts, key=lambda x: x["interface"].split("/")):
             if cc.get("status", "") != "active":
                 continue
             frame, slot, port, cpe_id = cc["id"].split("/")
+            if iface != (frame, slot):
+                if iface is not None:
+                    self.cli("quit")
+                iface = (frame, slot)
+                self.cli("interface gpon %s/%s" % iface)  # Fix from cpes
             ipath = [cc["global_id"], "", "", "0"]
             mpath = ["", "", "", "/".join([frame, slot, port])]
             v = self.cli("display ont optical-info %s %s" % (port, cpe_id))
@@ -86,6 +95,20 @@ class Script(GetMetricsScript):
                                 metric="Interface | DOM | TxPower",
                                 path=ipath,
                                 value=float(m["optical_tx_dbm"]),
+                                multi=True)
+            v = self.cli("display statistics ont-line-quality %s %s" % (port, cpe_id))
+            m = parse_kv(self.kv_map, v)
+            if m.get("optical_errors_bip_out") is not None:
+                self.set_metric(id=("Interface | Errors | BIP | Out", mpath),
+                                metric="Interface | Errors | BIP | Out",
+                                path=ipath,
+                                value=int(m["optical_errors_bip_out"]),
+                                multi=True)
+            if m.get("optical_errors_bip_in") is not None:
+                self.set_metric(id=("Interface | Errors | BIP | In", mpath),
+                                metric="Interface | Errors | BIP | In",
+                                path=ipath,
+                                value=int(m["optical_errors_bip_in"]),
                                 multi=True)
 
         self.cli("quit")
