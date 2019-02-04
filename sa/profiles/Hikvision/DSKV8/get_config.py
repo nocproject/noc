598e6ef983780996376dbaf4ff25a81a9dee69b3
Dmitry S. Luhtionov <dmitryluhtionov@gmail.com>
2019-02-04 17:50:57 +0200

    Add Hikvision.DSKV8 profile

---
+++ sa/profiles/Hikvision/DSKV8/get_config.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# Hikvision.DSKV8.get_config
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2019 The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+
+# Python modules
+import xml.etree.ElementTree as ElementTree
+from copy import copy
+import six
+# NOC modules
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetconfig import IGetConfig
+
+
+class Script(BaseScript):
+    name = "Hikvision.DSKV8.get_config"
+    cache = True
+    interface = IGetConfig
+
+    def xml_2_dict(self, r, root=True):
+        if root:
+            t = r.tag.split("}")[1][0:]
+            return {t: self.xml_2_dict(r, False)}
+        d = copy(r.attrib)
+        if r.text:
+            d["_text"] = r.text
+        for x in r.findall("./*"):
+            t = x.tag.split("}")[1][0:]
+            if t not in d:
+                d[t] = []
+            d[t].append(self.xml_2_dict(x, False))
+        return d
+
+    def execute(self):
+        c = ""
+        v = self.http.get("/ISAPI/Streaming/channels", json=False, cached=True, use_basic=True)
+        root = ElementTree.fromstring(v)
+        v = self.xml_2_dict(root)
+        channels = v['StreamingChannelList']['StreamingChannel']
+        i = 1
+        for o in channels:
+            c += "StreamingChannel:\n"
+            c += "    id: %s\n" % o['id'][0]['_text']
+            c += "    channelName: \"%s\"\n" % o['channelName'][0]['_text']
+            c += "    enabled: %s\n" % o['enabled'][0]['_text']
+            video = o['Video'][0]
+            c += "Video:\n"
+            for key, value in sorted(six.iteritems(video)):
+                if key == "_text" or isinstance(value, six.string_types):
+                    continue
+                c += "    %s: %s\n" % (key, value[0]['_text'])
+        v = self.http.get("/ISAPI/Image/channels/1/color", json=False, cached=True, use_basic=True)
+        root = ElementTree.fromstring(v)
+        v = self.xml_2_dict(root)
+        color = v['Color']
+        c += "Color:\n"
+        for key, value in sorted(six.iteritems(color)):
+            if key == "_text" or isinstance(value, six.string_types):
+                continue
+            c += "    %s: %s\n" % (key, value[0]['_text'])
+
+        try:
+            v = self.http.get("/ISAPI/Image/channels/1/WDR", json=False, cached=True, use_basic=True)
+            root = ElementTree.fromstring(v)
+            v = self.xml_2_dict(root)
+            c += "WDR:\n"
+            c += "    mode: %s\n" % v['WDR']['mode'][0]['_text']
+            c += "    WDRLevel: %s\n" % v['WDR']['WDRLevel'][0]['_text']
+        except self.http.HTTPError:
+            pass
+
+        try:
+            v = self.http.get("/ISAPI/Image/channels/1/BLC", json=False, cached=True, use_basic=True)
+            root = ElementTree.fromstring(v)
+            v = self.xml_2_dict(root)
+            c += "BLC:\n"
+            c += "    enabled: %s\n" % v['BLC']['enabled'][0]['_text']
+            c += "    BLCMode: %s\n" % v['BLC']['BLCMode'][0]['_text']
+        except self.http.HTTPError:
+            pass
+
+        try:
+            v = self.http.get("/ISAPI/System/Video/inputs/channels/1/overlays/capabilities", json=False, cached=True, use_basic=True)
+            root = ElementTree.fromstring(v)
+            v = self.xml_2_dict(root)
+            overlay = v['VideoOverlay']['TextOverlayList'][0]['TextOverlay']
+            if overlay:
+                c += "Overlays:\n"
+            i = 1
+            for o in overlay:
+                text = o['displayText'][0]
+                if text:
+                    c += "    TextOverlay%d: \"%s\"\n" % (i, text['_text'])
+                else:
+                    c += "    TextOverlay%d: \"\"\n" % i
+                i = i + 1
+        except self.http.HTTPError:
+            pass
+
+        try:
+            v = self.http.get("/ISAPI/System/time", json=False, cached=True, use_basic=True)
+            root = ElementTree.fromstring(v)
+            v = self.xml_2_dict(root)
+            timeMode = v['Time']['timeMode'][0]['_text']
+            c += "Time:\n"
+            c += "    timeMode: %s\n" % timeMode
+        except self.http.HTTPError:
+            pass
+
+        try:
+            v = self.http.get("/ISAPI/System/time/ntpServers", json=False, cached=True, use_basic=True)
+            root = ElementTree.fromstring(v)
+            v = self.xml_2_dict(root)
+            ntp_servers = v['NTPServerList']['NTPServer']
+            if ntp_servers:
+                c += "NTP:\n"
+            i = 1
+            for o in ntp_servers:
+                text = o['ipAddress'][0]['_text']
+                c += "    NTPServer%d: %s\n" % (i, text)
+        except self.http.HTTPError:
+            pass
+
+        return c
---
+++ sa/profiles/Hikvision/DSKV8/get_local_users.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# Hikvision.DSKV8.get_local_users
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2019 The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+
+# Python modules
+import xml.etree.ElementTree as ElementTree
+from copy import copy
+# NOC modules
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetlocalusers import IGetLocalUsers
+
+
+class Script(BaseScript):
+    name = "Hikvision.DSKV8.get_local_users"
+    interface = IGetLocalUsers
+
+    def xml_2_dict(self, r, root=True):
+        if root:
+            t = r.tag.split("}")[1][0:]
+            return {t: self.xml_2_dict(r, False)}
+        d = copy(r.attrib)
+        if r.text:
+            d["_text"] = r.text
+        for x in r.findall("./*"):
+            t = x.tag.split("}")[1][0:]
+            if t not in d:
+                d[t] = []
+            d[t].append(self.xml_2_dict(x, False))
+        return d
+
+    def execute(self):
+        r = []
+        v = self.http.get("/ISAPI/Security/users", json=False, cached=True, use_basic=True)
+        root = ElementTree.fromstring(v)
+        v = self.xml_2_dict(root)
+        users = v['UserList']['User']
+
+        for i in users:
+            r += [{
+                "username": i['userName'][0]['_text'],
+                "class": {
+                    "Administrator": "superuser",
+                    "Viewer": "operator",
+                }[i['userLevel'][0]['_text']],
+                "is_active": True
+            }]
+        return r
