ae69c57937e991e166fad3ff89221d6427572401
Andrey Vertiprahov <aversanta@gmail.com>
2019-03-01 17:40:48 +0000

    Merge branch 'parse_table_tabs' into 'master'
    parse_table: Mixed tabs/space processing
    
    See merge request noc/noc!1660
    
    (cherry picked from commit dc294ff1272987be78e1c43cbd376215672728ac)
    
    f9f8f6a6 Fix parse_table function
    d30f267c Add test
    447e8e72 Hide mac
    552a1f96 parse_table: `expand_columns` option and `allow_extend` optimization
    f370dec4 Add more test to text parse_table.
    27f73d93 Fix parse_table test.
    d2a7e04c Fix PEP8.
    03ee388e Add more test.

--- tests/test_text.py
+++ tests/test_text.py
@@ -49,6 +49,163 @@ from noc.lib.text import parse_table
             ["CIST", "1-11,15-122,124-250,253,257-300,302-445,447-709,720-759,770-879,901-3859,3861-4094"],
             ["1", "12-14,123,251-252,254-256,301,446,710-719,760-769,880-900,3860"]
         ]
+    ),
+    (
+        """Flags:  D - down        P - bundled in port-channel
+        I - stand-alone s - suspended
+        H - Hot-standby (LACP only)
+        R - Layer3      S - Layer2
+        U - in use      f - failed to allocate aggregator
+
+        M - not in use, minimum links not met
+        u - unsuitable for bundling
+        w - waiting to be aggregated
+        d - default port
+
+
+Number of channel-groups in use: 7
+Number of aggregators:           7
+
+Group  Port-channel  Protocol    Ports
+------+-------------+-----------+-----------------------------------------------
+11     Po11(SD)        LACP      Gi1/20(D)   Gi1/21(D)
+12     Po12(SD)        LACP      Gi2/16(D)
+13     Po13(SU)        LACP      Te3/2(P)
+14     Po14(SU)        LACP      Te3/4(P)    Te4/4(P)
+15     Po15(SU)        LACP      Gi2/6(P)    Gi2/14(P)
+16     Po16(SU)        LACP      Te3/3(P)    Te4/3(P)
+17     Po17(SU)        LACP      Gi2/3(P)    Gi2/19(P)   Gi2/20(P)
+                                 Gi2/22(P)
+""",
+        {"allow_wrap": True, "max_width": 120},
+        [['11', 'Po11(SD)', 'LACP', 'Gi1/20(D)   Gi1/21(D)'],
+         ['12', 'Po12(SD)', 'LACP', 'Gi2/16(D)'],
+         ['13', 'Po13(SU)', 'LACP', 'Te3/2(P)'],
+         ['14', 'Po14(SU)', 'LACP', 'Te3/4(P)    Te4/4(P)'],
+         ['15', 'Po15(SU)', 'LACP', 'Gi2/6(P)    Gi2/14(P)'],
+         ['16', 'Po16(SU)', 'LACP', 'Te3/3(P)    Te4/3(P)'],
+         ['17', 'Po17(SU)', 'LACP', 'Gi2/3(P)    Gi2/19(P)   Gi2/20(P)Gi2/22(P)']]
+    ),
+    (
+        'Vlan    Mac Address       Type       Ports\n'
+        '----    -----------       ----       -----\n'
+        'All\t1111.2222.3333\t  STATIC     CPU\n'
+        '611\t1111.2223.3efc\t  DYNAMIC    g0/2\n'
+        '611\t1111.2223.3cdc\t  DYNAMIC    g0/2\n'
+        '611\t1111.2223.4010\t  DYNAMIC    g0/2\n'
+        '1\t1111.2224.1fd8\t  DYNAMIC    g0/1\n'
+        '1\t1111.2225.0bb1\t  DYNAMIC    g0/2\n'
+        '611\t1111.2223.6bfc\t  DYNAMIC    g0/2\n'
+        '611\t1111.2223.42d8\t  DYNAMIC    g0/2\n'
+        '611\t1111.2223.6bf8\t  DYNAMIC    g0/2\n'
+        '611\t1111.2223.42dc\t  DYNAMIC    g0/2\n'
+        '611\t1111.2226.0001\t  DYNAMIC    g0/2\n'
+        '611\t1111.2223.3cd8\t  DYNAMIC    g0/2\n'
+        '611\t1111.2223.3ef8\t  DYNAMIC    g0/2\n'
+        '611\t1111.2223.4014\t  DYNAMIC    g0/2\n'
+        '611\t1111.2227.3480\t  DYNAMIC    g0/2\n'
+        '1\t1111.2228.a16d\t  DYNAMIC    g0/2\n'
+        '611\t1111.2223.38e0\t  DYNAMIC    g0/2\n'
+        '1\t1111.2229.a16c\t  DYNAMIC    g0/2\n'
+        '611\t1111.2223.38e4\t  DYNAMIC    g0/2\n'
+        '611\t1111.222a.2e1e\t  DYNAMIC    g0/2\n',
+        {"expand_columns": True},
+        [
+            ["All", "1111.2222.3333", "STATIC", "CPU"],
+            ["611", "1111.2223.3efc", "DYNAMIC", "g0/2"],
+            ["611", "1111.2223.3cdc", "DYNAMIC", "g0/2"],
+            ["611", "1111.2223.4010", "DYNAMIC", "g0/2"],
+            ["1", "1111.2224.1fd8", "DYNAMIC", "g0/1"],
+            ["1", "1111.2225.0bb1", "DYNAMIC", "g0/2"],
+            ["611", "1111.2223.6bfc", "DYNAMIC", "g0/2"],
+            ["611", "1111.2223.42d8", "DYNAMIC", "g0/2"],
+            ["611", "1111.2223.6bf8", "DYNAMIC", "g0/2"],
+            ["611", "1111.2223.42dc", "DYNAMIC", "g0/2"],
+            ["611", "1111.2226.0001", "DYNAMIC", "g0/2"],
+            ["611", "1111.2223.3cd8", "DYNAMIC", "g0/2"],
+            ["611", "1111.2223.3ef8", "DYNAMIC", "g0/2"],
+            ["611", "1111.2223.4014", "DYNAMIC", "g0/2"],
+            ["611", "1111.2227.3480", "DYNAMIC", "g0/2"],
+            ["1", "1111.2228.a16d", "DYNAMIC", "g0/2"],
+            ["611", "1111.2223.38e0", "DYNAMIC", "g0/2"],
+            ["1", "1111.2229.a16c", "DYNAMIC", "g0/2"],
+            ["611", "1111.2223.38e4", "DYNAMIC", "g0/2"],
+            ["611", "1111.222a.2e1e", "DYNAMIC", "g0/2"]
+        ]
+    ),
+    ("""ifIndex     ifDescr                                Interface
+----------  -------------------------------------  ---------
+         1  Switch  1 - Port  0                    GigabitEthernet 1/1
+         2  Switch  1 - Port  1                    GigabitEthernet 1/2
+         3  Switch  1 - Port  2                    GigabitEthernet 1/3
+         4  Switch  1 - Port  3                    GigabitEthernet 1/4
+         5  Switch  1 - Port  4                    GigabitEthernet 1/5
+         6  Switch  1 - Port  5                    GigabitEthernet 1/6
+         7  Switch  1 - Port  6                    GigabitEthernet 1/7
+         8  Switch  1 - Port  7                    GigabitEthernet 1/8
+         9  Switch  1 - Port  8                    GigabitEthernet 1/9
+        10  Switch  1 - Port  9                    GigabitEthernet 1/10
+        11  Switch  1 - Port 10                    GigabitEthernet 1/11
+        12  Switch  1 - Port 11                    GigabitEthernet 1/12
+        13  Switch  1 - Port 12                    2.5GigabitEthernet 1/1
+""",
+     {"allow_wrap": True, "max_width": 80},
+     [['1', 'Switch  1 - Port  0', 'GigabitEthernet 1/1'],
+      ['2', 'Switch  1 - Port  1', 'GigabitEthernet 1/2'],
+      ['3', 'Switch  1 - Port  2', 'GigabitEthernet 1/3'],
+      ['4', 'Switch  1 - Port  3', 'GigabitEthernet 1/4'],
+      ['5', 'Switch  1 - Port  4', 'GigabitEthernet 1/5'],
+      ['6', 'Switch  1 - Port  5', 'GigabitEthernet 1/6'],
+      ['7', 'Switch  1 - Port  6', 'GigabitEthernet 1/7'],
+      ['8', 'Switch  1 - Port  7', 'GigabitEthernet 1/8'],
+      ['9', 'Switch  1 - Port  8', 'GigabitEthernet 1/9'],
+      ['10', 'Switch  1 - Port  9', 'GigabitEthernet 1/10'],
+      ['11', 'Switch  1 - Port 10', 'GigabitEthernet 1/11'],
+      ['12', 'Switch  1 - Port 11', 'GigabitEthernet 1/12'],
+      ['13', 'Switch  1 - Port 12', '2.5GigabitEthernet 1/1']]
+     ),
+    ("""
+LLDP Remote Device Summary
+
+Local
+Interface  RemID    Chassis ID            Port ID             System Name
+---------  -------  --------------------  ------------------  ------------------
+0/9        1        11:22:33:44:55:66     GigabitEthernet2/0/9  SS-MS-1
+0/10
+
+""",
+     {"allow_extend": True},
+     [['0/9', '1', '11:22:33:44:55:66', 'GigabitEthernet2/0/9', 'SS-MS-1'],
+      ['0/10', '', '', '', '']]
+     ),
+    ("""
+LLDP Remote Device Summary
+
+Local
+Interface  RemID    Chassis ID            Port ID             System Name
+---------  -------  --------------------  ------------------  ------------------
+0/9        3        11:22:33:44:55:66     GigabitEthernet0/0/8  qqq0-sasasasa-rr11
+0/10       1        11:22:33:44:55:67     gi1/1/1             e22-a777-b774-1
+
+""",
+     {"allow_extend": True},
+     [['0/9', '3', '11:22:33:44:55:66', 'GigabitEthernet0/0/8', 'qqq0-sasasasa-rr11'],
+      ['0/10', '1', '11:22:33:44:55:67', 'gi1/1/1', 'e22-a77']]
+     ),
+    (
+        """
+
+ Port       Device ID         Port ID        System Name    Capabilities  TTL
+------- ----------------- --------------- ----------------- ------------ -----
+g1      00:11:22:33:44:55 GigabitEthernet SS555_XXXX_Skeeee     B, R      109
+                          0/0/3           x_333_Stack
+g2      00:11:22:33:44:55 GigabitEthernet SS555_XXXX_Skeeee     B, R      109
+                          1/0/3           x_333_Stack
+
+""",
+        {"allow_extend": True, "allow_wrap": True},
+        [['g1', '00:11:22:33:44:55', 'GigabitEthernet0/0/3', 'SS555_XXXX_Skeeeex_333_Stack', 'B, R', '109'],
+         ['g2', '00:11:22:33:44:55', 'GigabitEthernet1/0/3', 'SS555_XXXX_Skeeeex_333_Stack', 'B, R', '109']]
     )
 ])
 def test_parse_table(value, kwargs, expected):
