e06972a10ace7e14560e505839cfa4129a9632e9
Dmitry S. Luhtionov <dmitryluhtionov@gmail.com>
2019-03-20 09:34:14 +0200

    Fix

--- sa/profiles/ZTE/ZXA10/get_lldp_neighbors.py
+++ sa/profiles/ZTE/ZXA10/get_lldp_neighbors.py
@@ -27,8 +27,8 @@ class Script(BaseScript):
         r"^Chassis ID: (?P<chassis_id>\S+) \| (?P<chassis_id_type>.+)\n"
         r"^Peer Port: (?P<port_id>\S+) \| (?P<port_id_type>.+)\n"
         r"^.+\n"
-        r"^System Name: (?P<system_name>.+)\n"
-        r"^System Description: (?P<system_descr>.+)\n",
+        r"(^System Name: (?P<system_name>.+)\n)?"
+        r"(^System Description: (?P<system_descr>.+)\n)?",
         re.MULTILINE
     )
     CHASSIS_TYPES = {
@@ -51,8 +51,6 @@ class Script(BaseScript):
             remote_chassis_id_subtype = self.CHASSIS_TYPES[match1.group("chassis_id_type").strip()]
             remote_port = match1.group("port_id")
             remote_port_subtype = self.PORT_TYPES[match1.group("port_id_type").strip()]
-            remote_system_name = match1.group("system_name").strip()
-            remote_system_description = match1.group("system_descr").strip()
             remote_capabilities = 0
             i = {"local_interface": local_interface, "neighbors": []}
             n = {
@@ -62,10 +60,10 @@ class Script(BaseScript):
                 "remote_port": remote_port,
                 "remote_capabilities": remote_capabilities
             }
-            if remote_system_name:
-                n["remote_system_name"] = remote_system_name
-            if remote_system_description:
-                n["remote_system_description"] = remote_system_description
+            if match1.group("system_name") and match1.group("system_name").strip():
+                n["remote_system_name"] = match1.group("system_name").strip()
+            if match1.group("system_descr") and match1.group("system_descr").strip():
+                n["remote_system_description"] = match1.group("system_descr").strip()
             i["neighbors"].append(n)
             r.append(i)
         return r
