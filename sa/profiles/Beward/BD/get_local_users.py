7d4c6ebc97b4298464d44a8380a6faf9e414d2b4
Andrey Vertiprahov <aversanta@gmail.com>
2019-03-27 10:26:37 +0500

    Add Beward.BD.get_local_users script.

---
+++ sa/profiles/Beward/BD/get_local_users.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# Beward.BD.get_local_users
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2019 The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+
+# NOC modules
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetlocalusers import IGetLocalUsers
+
+
+class Script(BaseScript):
+    name = "Beward.BD.get_local_users"
+    interface = IGetLocalUsers
+
+    def execute(self):
+        r = []
+        res = self.http.get("/cgi-bin/admin/privacy.cgi", use_basic=True)
+        for line in res.splitlines()[1:]:
+            if not line:
+                continue
+            r += [{
+                "username": line.split(":")[0],
+                "class": "admin"  # not API for permission
+            }]
+
+        return r
