0abfda2d504de2252d81583a1e8f2a7cc5b4637e
Andrey Vertiprahov <aversanta@gmail.com>
2019-03-27 16:02:22 +0000

    Add Dahua.DH.get_local_version script.

---
+++ sa/profiles/Dahua/DH/get_local_users.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# Dahua.DH.get_local_users
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2019 The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+
+# NOC modules
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetlocalusers import IGetLocalUsers
+
+
+class Script(BaseScript):
+    name = "Dahua.DH.get_local_users"
+    interface = IGetLocalUsers
+
+    def execute(self):
+        r = {}
+        users_info = self.http.get("/cgi-bin/userManager.cgi?action=getUserInfoAll")
+        for line, value in self.profile.parse_tokens(users_info):
+            line_id = line[1]
+            if line_id not in r:
+                r[line_id] = {}
+            if line[2] in {"Id", "Name", "Group"}:
+                if line[2] == "Name":
+                    r[line_id]["username"] = value
+                elif line[2] == "Group":
+                    r[line_id]["class"] = value
+        return r.values()
--- sa/profiles/Dahua/DH/profile.py
+++ sa/profiles/Dahua/DH/profile.py
@@ -7,6 +7,8 @@
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
+# Python modules
+import re
 # NOC modules
 from noc.core.profile.base import BaseProfile
 
@@ -16,7 +18,23 @@ class Profile(BaseProfile):
     enable_http_session = True
     http_request_middleware = ["digestauth"]
 
+    rx_depth = re.compile(r"\S+(\[[\S\d]+\])")
+
     @staticmethod
     def parse_equal_output(string):
         r = dict(l.split("=", 1) for l in string.splitlines())
         return r
+
+    def parse_tokens(self, string):
+        for line in string.splitlines():
+            key, value = line.split("=")
+            path = []
+            for p in key.split("."):
+                if self.rx_depth.match(p):
+                    p1, p2 = p.split("[", 1)
+                    p2 = [int(x.strip("[]")) for x in p2.split("][")]
+                else:
+                    p1, p2 = p, []
+                path += [p1]
+                path += p2
+            yield path, value
