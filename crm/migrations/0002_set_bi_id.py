f525a323d41bb90263e84fc8fd59a85d3801eafb
kk <kk@buhaha.net>
2019-04-10 16:55:26 +0300

    crm_migrations. code refactoring.

--- crm/migrations/0002_set_bi_id.py
+++ crm/migrations/0002_set_bi_id.py
@@ -2,13 +2,15 @@
 # ----------------------------------------------------------------------
 # Initialize bi_id fields
 # ----------------------------------------------------------------------
-# Copyright (C) 2007-2017 The NOC Project
+# Copyright (C) 2007-2019 The NOC Project
 # See LICENSE for details
 # ----------------------------------------------------------------------
-
+"""
+"""
+# Python modules
+import bson
 # Third-party modules
 from pymongo import UpdateOne
-import bson
 # NOC modules
 from noc.core.bi.decorator import bi_hash
 from noc.lib.nosql import get_db
@@ -20,26 +22,11 @@ class Migration(object):
     def forwards(self):
         # Update mongodb collections
         mdb = get_db()
-        for coll_name in ("noc.subscribers", "noc.suppliers",
-                          "noc.subscriberprofiles", "noc.supplierprofiles"):
+        for coll_name in ("noc.subscribers", "noc.suppliers", "noc.subscriberprofiles", "noc.supplierprofiles"):
             coll = mdb[coll_name]
             updates = []
-            for d in coll.find({
-                "bi_id": {
-                    "$exists": False
-                }
-            }, {
-                "_id": 1
-            }):
-                updates += [
-                    UpdateOne({
-                        "_id": d["_id"]
-                    }, {
-                        "$set": {
-                            "bi_id": bson.Int64(bi_hash(d["_id"]))
-                        }
-                    })
-                ]
+            for d in coll.find({"bi_id": {"$exists": False}}, {"_id": 1}):
+                updates += [UpdateOne({"_id": d["_id"]}, {"$set": {"bi_id": bson.Int64(bi_hash(d["_id"]))}})]
                 if len(updates) >= MONGO_CHUNK:
                     coll.bulk_write(updates)
                     updates = []
