47f41d667907958576b842746eb8f2d5260d62fd
kk <kk@buhaha.net>
2019-04-10 21:16:58 +0300

    gis_migrations. code refactoring.

--- gis/migrations/0002_world_area.py
+++ gis/migrations/0002_world_area.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
-# ---------------------------------------------------------------------
-# Copyright (C) 2007-2018 The NOC Project
+# ----------------------------------------------------------------------
+# world area
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2019 The NOC Project
 # See LICENSE for details
-# ---------------------------------------------------------------------
-
+# ----------------------------------------------------------------------
+"""
+"""
 # NOC modules
 from noc.lib.nosql import get_db
 
@@ -12,14 +15,16 @@ class Migration(object):
     def forwards(self):
         areas = get_db().noc.gis.areas
         if not areas.count_documents({"name": "World"}):
-            areas.insert({
-                "name": "World",
-                "is_active": True,
-                "min_zoom": 0,
-                "max_zoom": 5,
-                "SW": [-180.0, -90.0],
-                "NE": [179.999999, 89.999999]
-            })
+            areas.insert(
+                {
+                    "name": "World",
+                    "is_active": True,
+                    "min_zoom": 0,
+                    "max_zoom": 5,
+                    "SW": [-180.0, -90.0],
+                    "NE": [179.999999, 89.999999]
+                }
+            )
 
     def backwards(self):
         pass
--- gis/migrations/0004_migrate_geodata.py
+++ gis/migrations/0004_migrate_geodata.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
-# ---------------------------------------------------------------------
-# Copyright (C) 2007-2014 The NOC Project
+# ----------------------------------------------------------------------
+# migrate geodata
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2019 The NOC Project
 # See LICENSE for details
-# ---------------------------------------------------------------------
-
+# ----------------------------------------------------------------------
+"""
+"""
 # Python modules
+from __future__ import print_function
 import json
 # Third-party modules
 from pymongo.errors import BulkWriteError
 from pymongo import InsertOne
-# NOC modules
 from south.db import db
 # NOC modules
 from noc.lib.nosql import get_db, ObjectId
@@ -17,11 +20,9 @@ from noc.lib.nosql import get_db, ObjectId
 
 class Migration(object):
     def forwards(self):
-        if db.execute(
-            """
-            select count(*) from pg_class where relname='gis_geodata'
-            """
-        )[0][0] == 0:
+        if db.execute("""
+                select count(*) from pg_class where relname='gis_geodata'
+                """)[0][0] == 0:
             return  # No PostGIS
         c = get_db().noc.geodata
         bulk = []
@@ -30,19 +31,14 @@ class Migration(object):
             FROM gis_geodata
         """):
             data = json.loads(data)
-            bulk += [InsertOne({
-                "layer": ObjectId(layer),
-                "object": ObjectId(object),
-                "label": label,
-                "data": data
-            })]
+            bulk += [InsertOne({"layer": ObjectId(layer), "object": ObjectId(object), "label": label, "data": data})]
         if bulk:
             print("Commiting changes to database")
             try:
                 c.bulk_write(bulk)
                 print("Database has been synced")
             except BulkWriteError as e:
-                print("Bulk write error: '%s'", e.details)
+                print(("Bulk write error: '%s'", e.details))
                 print("Stopping check")
         # Leave table for further analisys
         # db.drop_table("gis_geodata")
