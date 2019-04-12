08f37be9bb377fe388738a11b0fd063ab560ef34
kk <kk@buhaha.net>
2019-04-12 15:16:57 +0300

    phone_migrations. code refactoring.

--- phone/migrations/0002_migrate_termination_groups.py
+++ phone/migrations/0002_migrate_termination_groups.py
@@ -2,10 +2,11 @@
 # ----------------------------------------------------------------------
 # Convert TerminationGroups to ResourceGroups
 # ----------------------------------------------------------------------
-# Copyright (C) 2007-2018 The NOC Project
+# Copyright (C) 2007-2019 The NOC Project
 # See LICENSE for details
 # ----------------------------------------------------------------------
-
+"""
+"""
 # Third-party modules
 from pymongo import UpdateMany
 # NOC modules
@@ -13,9 +14,7 @@ from noc.lib.nosql import get_db
 
 
 class Migration(object):
-    depends_on = [
-        ("sa", "0184_managedobject_migrate_termination_groups")
-    ]
+    depends_on = [("sa", "0184_managedobject_migrate_termination_groups")]
 
     def forwards(self):
         # Get migrated termination groups, created by 0184 migration
@@ -35,27 +34,25 @@ class Migration(object):
         for cname in ["noc.phoneranges", "noc.phonenumbers"]:
             coll = db[cname]
             bulk = []
-            for d in coll.aggregate([
-                {
-                    "$group": {
-                        "_id": "$termination_group"
-                    }
-                }
-            ]):
+            for d in coll.aggregate([{"$group": {"_id": "$termination_group"}}]):
                 if not d.get("_id"):
                     continue
                 rg_id = rg_map[d["_id"]]
-                bulk += [UpdateMany({
-                    "termination_group": d["_id"]
-                }, {
-                    "$set": {
-                        "static_client_groups": [rg_id],
-                        "effective_client_groups": [rg_id],
-                    },
-                    "$unset": {
-                        "termination_group": ""
-                    }
-                })]
+                bulk += [
+                    UpdateMany(
+                        {
+                            "termination_group": d["_id"]
+                        }, {
+                            "$set": {
+                                "static_client_groups": [rg_id],
+                                "effective_client_groups": [rg_id],
+                            },
+                            "$unset": {
+                                "termination_group": ""
+                            }
+                        }
+                    )
+                ]
             if bulk:
                 coll.bulk_write(bulk)
 
--- phone/migrations/0003_workflow.py
+++ phone/migrations/0003_workflow.py
@@ -2,10 +2,11 @@
 # ----------------------------------------------------------------------
 # Set workflow fields
 # ----------------------------------------------------------------------
-# Copyright (C) 2007-2018 The NOC Project
+# Copyright (C) 2007-2019 The NOC Project
 # See LICENSE for details
 # ----------------------------------------------------------------------
-
+"""
+"""
 # Third-party modules
 import bson
 # NOC modules
@@ -13,9 +14,7 @@ from noc.lib.nosql import get_db
 
 
 class Migration(object):
-    depends_on = [
-        ("wf", "0001_default_wf")
-    ]
+    depends_on = [("wf", "0001_default_wf")]
 
     def forwards(self):
         s_map = {
@@ -28,32 +27,31 @@ class Migration(object):
         }
         db = get_db()
         db["noc.phonerangeprofiles"].update_many(
-            {}, {
+            {},
+            {
                 "$set": {
                     "workflow": bson.ObjectId("5a1d078e1bb627000151a17d")  # Default
                 }
-            })
+            }
+        )
         db["noc.phoneranges"].update_many(
-            {}, {
+            {},
+            {
                 "$set": {
                     "state": bson.ObjectId("5a1d07b41bb627000151a18b")  # Ready
                 }
             }
         )
         db["noc.phonenumberprofiles"].update_many(
-            {}, {
+            {},
+            {
                 "$set": {
                     "workflow": bson.ObjectId("5a01d980b6f529000100d37a")  # Default Resource
                 }
-            })
+            }
+        )
         for s in s_map:
-            db["noc.phonenumbers"].update_many({
-                "status": s
-            }, {
-                "$set": {
-                    "state": s_map[s]
-                }
-            })
+            db["noc.phonenumbers"].update_many({"status": s}, {"$set": {"state": s_map[s]}})
 
     def backwards(self):
         pass
