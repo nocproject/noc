fefd51a892a486d8a54c708317a6ff928df833ab
Dmitry S. Luhtionov <dmitryluhtionov@gmail.com>
2019-04-16 20:52:35 +0300

    Fix Qtech.QSW2500 script

--- sa/profiles/Qtech/QSW2500/__init__.py
+++ sa/profiles/Qtech/QSW2500/__init__.py
@@ -3,7 +3,7 @@
 # Vendor: Qtech
 # OS:     QSW2500
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2017 The NOC Project
+# Copyright (C) 2007-2019 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
@@ -40,14 +40,14 @@ class Profile(BaseProfile):
         r"(?:\(config[^\)]*\))?#"
 
     rx_ver = re.compile(
-        r"^Product name: (?P<platform>\S+)\s*\n"
-        r"^QOS\s+Version QOS_(?P<version>\S+)\.QSW.+\n"
-        r"^Bootstrap\s+Version Bootstrap_(?P<bootprom>\S+)\.QSW.+\n"
+        r"^\s*Product name: (?P<platform>\S+)\s*\n"
+        r"^\s*QOS\s+[Vv]ersion QOS_(?P<version>\S+)\.QSW.+\n"
+        r"^\s*(Bootstrap\s+Version )?Bootstrap_(?P<bootprom>\S+)\.QSW.+\n"
         r"(^.*\n)?"
-        r"^Hardware QSW\S+ Version Rev\.(?P<hardware>\S+)\s*\n"
+        r"^\s*Hardware QSW\S+ (Version )?Rev\.(?P<hardware>\S+)\s*\n"
         r"^\s*\n"
-        r"^System MacAddress is\s*:\s*(?P<mac>\S+)\s*\n"
-        r"^Serial number: (?P<serial>\S+)\s*\n",
+        r"^\s*(System )?[Mm][Aa][Cc]\s*[Aa]ddress( is)?\s*:\s*(?P<mac>\S+)\s*\n"
+        r"^\s*Serial number: (?P<serial>\S+)\s*\n",
         re.MULTILINE
     )
 
