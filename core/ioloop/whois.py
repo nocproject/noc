9c883091b16e7118b51e610ed2fbd23da4d3d0d2
Dmitry Volodin <dv@nocproject.org>
2019-04-18 15:09:10 +0300

    Domain expiration check handlers

---
+++ core/ioloop/whois.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------
+# Whois client
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2019 The NOC Project
+# See LICENSE for details
+# ----------------------------------------------------------------------
+
+# Python modules
+import logging
+# Third-party modules
+from tornado.tcpclient import TCPClient
+import tornado.gen
+import tornado.ioloop
+# NOC modules
+from noc.lib.validators import is_fqdn
+
+DEFAULT_WHOIS_SERVER = "whois.ripe.net"
+DEFAULT_WHOIS_PORT = 43
+DEFAULT_TIMEOUT = 60
+
+logger = logging.getLogger(__name__)
+
+VERISIGN_MAP = {
+    "domain name": "domain",
+    "name server": "nserver",
+    "creation date": "created",
+    "registry expiry date": "paid-till"
+}
+
+
+def parse_verisign(data):
+    """
+    Parse whois response from verisign
+    :param data:
+    :return:
+    """
+    r = []
+    for line in data.splitlines():
+        if not line.startswith("  "):
+            continue
+        line = line.strip()
+        k, v = line.split(":", 1)
+        k = k.strip().lower()
+        k = VERISIGN_MAP.get(k, k)
+        r += [(k, v.strip())]
+    return r
+
+
+def parse_response(data):
+    """
+    Parse whois response
+    :param data:
+    :return:
+    """
+    if data.startswith(" ") and ">>> Last update of whois database" in data:
+        return parse_verisign(data)
+
+
+@tornado.gen.coroutine
+def whois_async(query, fields=None):
+    """
+    Perform whois request
+    :param query:
+    :param fields:
+    :return:
+    """
+    logger.debug("whois %s", query)
+    # Get appropriate whois server
+    if is_fqdn(query):
+        # Use TLD.whois-servers.net for domain lookup
+        tld = query.split(".")[-1]
+        server = "%s.whois-servers.net" % tld
+    else:
+        server = DEFAULT_WHOIS_SERVER
+    # Perform query
+    try:
+        client = TCPClient()
+        stream = yield client.connect(server, DEFAULT_WHOIS_PORT)
+    except IOError as e:
+        logger.error("Cannot resolve host '%s': %s", server, e)
+        raise tornado.gen.Return()
+    try:
+        yield stream.write(str(query) + "\r\n")
+        data = yield stream.read_until_close()
+    finally:
+        yield stream.close()
+    data = parse_response(data)
+    if fields:
+        data = [(k, v) for k, v in data if k in fields]
+    raise tornado.gen.Return(data)
+
+
+def whois(query, fields=None):
+    @tornado.gen.coroutine
+    def _whois():
+        result = yield whois_async(query, fields)
+        r.append(result)
+
+    ioloop = tornado.ioloop.IOLoop.instance()
+    r = []
+    ioloop.run_sync(_whois)
+    return r[0]
---
+++ dns/handlers/expiration.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------
+# Domain Expiration tools
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2019 The NOC Project
+# See LICENSE for details
+# ----------------------------------------------------------------------
+
+# Python modules
+import logging
+import datetime
+# NOC modules
+from noc.config import config
+from noc.core.ioloop.whois import whois
+from noc.dns.models.dnszone import DNSZone, ZONE_FORWARD
+from noc.main.models.systemnotification import SystemNotification
+from noc.lib.dateutils import humanize_timedelta
+
+logger = logging.getLogger(__name__)
+
+
+def update():
+    """
+    Update domains expiration
+    :return:
+    """
+    for zone in DNSZone.objects.filter(type=ZONE_FORWARD):
+        wdata = whois(zone.name, {"paid-till"})
+        if not wdata or len(wdata) != 1:
+            logger.info("[%s] Cannot check domain expiration", zone.name)
+            continue
+        ts = wdata[0][1]
+        if "T" in ts:
+            ts = ts.split("T", 1)[0]
+        paid_till = datetime.datetime.strptime(ts, "%Y-%m-%d").date()
+        if zone.paid_till == paid_till:
+            continue
+        logger.info("[%s] Changing expiry time: %s -> %s", zone.name, zone.paid_till, paid_till)
+        zone.paid_till = paid_till
+        zone.save()
+
+
+def notify():
+    """
+    Notify domain expiration
+    :return:
+    """
+    def get_table(data):
+        # Format table
+        max_w = max(len(z) for z, x in data)
+        mask = "%%%d | %%s" % max_w
+        return "\n".join(mask % (z, str(x)) for z, x in data)
+
+    today = datetime.date.today()
+    delta = datetime.timedelta(seconds=config.dns.warn_before_expired)
+    shutoff = today + delta
+    expiring = [
+        (z.name, z.paid_till)
+        for z in DNSZone.objects.filter(
+            type=ZONE_FORWARD, paid_till__isnull=False, paid_till__lte=shutoff
+        ).order_by("paid_till")
+    ]
+    if not expiring:
+        return
+    # Send notification
+    h_delta = humanize_timedelta(delta)
+    SystemNotification.notify(
+        "dns.domain_expiration_warning",
+        subject="%d domains to be expired in %d" % (len(expiring), h_delta),
+        body="Following domains are to be expired in %d:\n%s\n" % (h_delta, get_table(expiring))
+    )
+    # Get expired domains
+    expired = [r for r in expiring if r[1] <= today]
+    SystemNotification.notify(
+        "dns.domain_expired",
+        subject="%d domains are expiring" % len(expired),
+        body="Following domains are expired:\n%s\n" % get_table(expired)
+    )
