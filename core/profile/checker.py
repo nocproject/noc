93d7670662d53b9c2e9577eb6c268e9816be215e
Dmitry Volodin <dv@nocproject.org>
2019-04-30 15:28:17 +0300

    ProfileChecker class

---
+++ core/profile/checker.py
@@ -0,0 +1,258 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------
+# Profile checker
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2019 The NOC Project
+# See LICENSE for details
+# ----------------------------------------------------------------------
+
+# Python modules
+import logging
+import operator
+from threading import Lock
+import re
+# Third-party modules
+from builtins import str, object
+import cachetools
+# NOC modules
+from noc.core.log import PrefixLoggerAdapter
+from noc.core.error import NOCError
+from noc.core.service.client import open_sync_rpc, RPCError
+from noc.sa.models.profilecheckrule import ProfileCheckRule
+from noc.core.mib import mib
+from noc.core.snmp.version import SNMP_v1, SNMP_v2c
+
+rules_lock = Lock()
+
+
+class ProfileChecker(object):
+    base_logger = logging.getLogger("profilechecker")
+    _rules_cache = cachetools.TTLCache(10, ttl=60)
+    _re_cache = {}
+
+    def __init__(self, address, pool, logger=None, snmp_community=None,
+                 calling_service="profilechecker", snmp_version=SNMP_v2c):
+        self.address = address
+        self.pool = pool
+        self.logger = PrefixLoggerAdapter(
+            logger or self.base_logger,
+            "%s][%s" % (self.pool, self.address)
+        )
+        self.result_cache = {}  # (method, param) -> result
+        self.error = None
+        self.snmp_community = snmp_community
+        self.calling_service = calling_service
+        self.snmp_version = snmp_version
+
+    def get_profile(self):
+        """
+        Returns profile for object, or None when not known
+        """
+        snmp_result = ""
+        http_result = ""
+        for ruleset in self.get_rules():
+            for (method, param), actions in ruleset:
+                try:
+                    result = self.do_check(method, param)
+                    if not result:
+                        continue
+                    if "snmp" in method:
+                        snmp_result = result
+                    if "http" in method:
+                        http_result = result
+                    for match_method, value, action, profile, rname in actions:
+                        if self.is_match(result, match_method, value):
+                            self.logger.info("Matched profile: %s (%s)", profile, rname)
+                            # @todo: process MAYBE rule
+                            return profile
+                except NOCError as e:
+                    self.logger.error(e.message)
+                    self.error = str(e.message)
+                    return None
+        if snmp_result or http_result:
+            self.error = "Not find profile for OID: %s or HTTP string: %s" % (snmp_result, http_result)
+        elif not snmp_result:
+            self.error = "Cannot fetch snmp data, check device for SNMP access"
+        elif not http_result:
+            self.error = "Cannot fetch HTTP data, check device for HTTP access"
+        self.logger.info("Cannot detect profile: %s", self.error)
+        return None
+
+    def get_error(self):
+        """
+        Get error message
+        :return:
+        """
+        return self.error
+
+    @cachetools.cachedmethod(operator.attrgetter("_rules_cache"), lock=lambda _: rules_lock)
+    def get_rules(self):
+        """
+        Load ProfileCheckRules and return a list, grouped by preferences
+        [{
+            (method, param) -> [(
+                    match_method,
+                    value,
+                    action,
+                    profile,
+                    rule_name
+                ), ...]
+
+        }]
+        """
+        self.logger.info("Compiling \"Profile Check rules\"")
+        d = {}  # preference -> (method, param) -> [rule, ..]
+        for r in ProfileCheckRule.objects.all().order_by("preference"):
+            if "snmp" in r.method and r.param.startswith("."):
+                self.logger.error("Bad SNMP in ruleset \"%s\", Skipping..." % r.name)
+                continue
+            if r.preference not in d:
+                d[r.preference] = {}
+            k = (r.method, r.param)
+            if k not in d[r.preference]:
+                d[r.preference][k] = []
+            d[r.preference][k] += [(
+                r.match_method,
+                r.value,
+                r.action,
+                r.profile,
+                r.name
+            )]
+        return [list(d[p].items()) for p in sorted(d)]
+
+    @cachetools.cachedmethod(operator.attrgetter("_re_cache"))
+    def get_re(self, regexp):
+        return re.compile(regexp)
+
+    def do_check(self, method, param):
+        """
+        Perform check
+        """
+        self.logger.debug("do_check(%s, %s)", method, param)
+        if (method, param) in self.result_cache:
+            self.logger.debug("Using cached value")
+            return self.result_cache[method, param]
+        h = getattr(self, "check_%s" % method, None)
+        if not h:
+            self.logger.error("Invalid check method '%s'. Ignoring", method)
+            return None
+        result = h(param)
+        self.result_cache[method, param] = result
+        return result
+
+    def check_snmp_v2c_get(self, param):
+        """
+        Perform SNMP v2c GET. Param is OID or symbolic name
+        """
+        if self.snmp_version is None:
+            self.logger.error("SNMP is not supported. Ignoring")
+            raise NOCError(msg="No SNMP support")
+        if not self.snmp_community:
+            self.logger.error("No SNMP credentials. Ignoring")
+            raise NOCError(msg="No SNMP credentials")
+        try:
+            param = mib[param]
+        except KeyError:
+            self.logger.error("Cannot resolve OID '%s'. Ignoring", param)
+            return None
+        if self.snmp_version == SNMP_v1:
+            return self.snmp_v1_get(param)
+        elif self.snmp_version == SNMP_v2c:
+            return self.snmp_v2c_get(param)
+        else:
+            raise NOCError(msg="Unsupported SNMP version")
+
+    def check_http_get(self, param):
+        """
+        Perform HTTP GET check. Param can be URL path or :<port>/<path>
+        """
+        url = "http://%s%s" % (self.address, param)
+        return self.http_get(url)
+
+    def check_https_get(self, param):
+        """
+        Perform HTTPS GET check. Param can be URL path or :<port>/<path>
+        """
+        url = "https://%s%s" % (self.address, param)
+        return self.https_get(url)
+
+    def is_match(self, result, method, value):
+        """
+        Returns True when result matches value
+        """
+        if method == "eq":
+            return result == value
+        elif method == "contains":
+            return value in result
+        elif method == "re":
+            return bool(self.get_re(value).search(result))
+        else:
+            self.logger.error("Invalid match method '%s'. Ignoring", method)
+            return False
+
+    def snmp_v1_get(self, param):
+        """
+        Perform SNMP v1 request. May be overridden for testing
+        :param param:
+        :return:
+        """
+        self.logger.info("SNMP v1 GET: %s", param)
+        try:
+            return open_sync_rpc(
+                "activator",
+                pool=self.pool,
+                calling_service=self.calling_service
+            ).snmp_v1_get(
+                self.address,
+                self.snmp_community,
+                param
+            )
+        except RPCError as e:
+            self.logger.error("RPC Error: %s", e)
+            return None
+
+    def snmp_v2c_get(self, param):
+        """
+        Perform SNMP v2c request. May be overridden for testing
+        :param param:
+        :return:
+        """
+        self.logger.info("SNMP v2c GET: %s", param)
+        try:
+            return open_sync_rpc(
+                "activator",
+                pool=self.pool,
+                calling_service=self.calling_service
+            ).snmp_v2c_get(
+                self.address,
+                self.snmp_community,
+                param
+            )
+        except RPCError as e:
+            self.logger.error("RPC Error: %s", e)
+            return None
+
+    def http_get(self, url):
+        """
+        Perform HTTP request. May be overridden for testing
+        :param url: Request URL
+        :return:
+        """
+        self.logger.info("HTTP Request: %s", url)
+        try:
+            return open_sync_rpc(
+                "activator",
+                pool=self.pool,
+                calling_service=self.calling_service
+            ).http_get(url)
+        except RPCError as e:
+            self.logger.error("RPC Error: %s", e)
+            return None
+
+    def https_get(self, url):
+        """
+        Perform HTTP request. May be overridden for testing
+        :param url: Request URL
+        :return:
+        """
+        return self.http_get(url)
--- services/discovery/jobs/box/profile.py
+++ services/discovery/jobs/box/profile.py
@@ -2,21 +2,17 @@
 # ---------------------------------------------------------------------
 # Profile check
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2016 The NOC Project
+# Copyright (C) 2007-2019 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
 # Python modules
-import re
 import threading
 import cachetools
-import operator
 # NOC modules
 from noc.services.discovery.jobs.base import DiscoveryCheck
-from noc.sa.models.profilecheckrule import ProfileCheckRule
-from noc.core.mib import mib
-from noc.core.service.client import open_sync_rpc, RPCError
-from noc.core.error import NOCError
+from noc.core.profile.checker import ProfileChecker
+from noc.core.snmp.version import SNMP_v1, SNMP_v2c
 
 rules_lock = threading.Lock()
 
@@ -54,185 +50,36 @@ class ProfileCheck(DiscoveryCheck):
         """
         Returns profile for object, or None when not known
         """
-        self.result_cache = {}  # (method, param) -> result
-        snmp_result = ""
-        http_result = ""
-        message = "Cannot detect profile"
-        fatal = False
-        for ruleset in self.get_rules():
-            for (method, param), actions in ruleset:
-                try:
-                    result = self.do_check(method, param)
-                    if not result:
-                        continue
-                    if "snmp" in method:
-                        snmp_result = result
-                    if "http" in method:
-                        http_result = result
-                    for match_method, value, action, profile, rname in actions:
-                        if self.is_match(result, match_method, value):
-                            self.logger.info("Matched profile: %s (%s)",
-                                             profile, rname)
-                            # @todo: process MAYBE rule
-                            return profile
-                except NOCError as e:
-                    self.logger.error(e.message)
-                    return None
-        self.logger.info(message)
-        if snmp_result or http_result:
-            self.logger.info("SNMP Result: %s, HTTP Result: %s", snmp_result, http_result)
-            # message = "Cannot find profile in \"Profile Check Rules\", OID: %s" % snmp_result
-            message = "Not find profile for OID: %s or HTTP string: %s" % (snmp_result, http_result)
-        elif not snmp_result:
-            message = "Cannot fetch snmp data, check device for SNMP access"
-            self.logger.info("Cannot fetch SNMP data, check device access for SNMP")
-        elif not http_result:
-            message = "Cannot fetch HTTP data, check device for HTTP access"
-            self.logger.info("Cannot fetch http data, check device for HTTP access")
-
-        self.set_problem(
-            alarm_class="Discovery | Guess | Profile",
-            message=message,
-            fatal=fatal
-        )
-        self.logger.debug("Result %s" % self.job.problems)
-        return None
-
-    @cachetools.cachedmethod(operator.attrgetter("_rules_cache"), lock=lambda _: rules_lock)
-    def get_rules(self):
-        """
-        Load ProfileCheckRules and return a list, groupped by preferences
-        [{
-            (method, param) -> [(
-                    match_method,
-                    value,
-                    action,
-                    profile,
-                    rule_name
-                ), ...]
-
-        }]
-        """
-        self.logger.info("Compiling \"Profile Check rules\"")
-        d = {}  # preference -> (method, param) -> [rule, ..]
-        for r in ProfileCheckRule.objects.all().order_by("preference"):
-            if "snmp" in r.method and r.param.startswith("."):
-                self.logger.error("Bad SNMP in ruleset \"%s\", Skipping..." % r.name)
-                continue
-            if r.preference not in d:
-                d[r.preference] = {}
-            k = (r.method, r.param)
-            if k not in d[r.preference]:
-                d[r.preference][k] = []
-            d[r.preference][k] += [(
-                r.match_method,
-                r.value,
-                r.action,
-                r.profile,
-                r.name
-            )]
-        return [d[p].items() for p in sorted(d)]
-
-    def do_check(self, method, param):
-        """
-        Perform check
-        """
-        self.logger.debug("do_check(%s, %s)", method, param)
-        if (method, param) in self.result_cache:
-            self.logger.debug("Using cached value")
-            return self.result_cache[method, param]
-        h = getattr(self, "check_%s" % method, None)
-        if not h:
-            self.logger.error("Invalid check method '%s'. Ignoring",
-                              method)
-            return None
-        result = h(param)
-        self.result_cache[method, param] = result
-        return result
-
-    def check_snmp_v2c_get(self, param):
-        """
-        Perform SNMP v2c GET. Param is OID or symbolic name
-        """
+        # Get SNMP version and community
         if hasattr(self.object, "_suggest_snmp") and self.object._suggest_snmp:
             # Use guessed community
             # as defined one may be invalid
-            snmp_ro = self.object._suggest_snmp[0]
-            if not self.snmp_version_def:
-                self.snmp_version_def = self.object._suggest_snmp[2]
+            snmp_community = self.object._suggest_snmp[0]
+            snmp_version = {
+                "snmp_v1_get": SNMP_v1,
+                "snmp_v2c_get": SNMP_v2c
+            }[self.object._suggest_snmp[2]]
         else:
-            # @todo caps for version
-            snmp_ro = self.object.credentials.snmp_ro
-            if not self.snmp_version_def:
-                self.snmp_version_def = "snmp_v2c_get"
-                caps = self.object.get_caps()
-                if caps.get("SNMP | v2c") is False or caps.get("SNMP | v2c") is None:
-                    self.snmp_version_def = "snmp_v1_get"
-        if not snmp_ro:
-            self.logger.error("No SNMP credentials. Ignoring")
-            raise NOCError(msg="No SNMP credentials")
-        try:
-            param = mib[param]
-        except KeyError:
-            self.logger.error("Cannot resolve OID '%s'. Ignoring",
-                              param)
-            return None
-        self.logger.info("Use %s for request", self.snmp_version_def)
-        try:
-            return open_sync_rpc(
-                "activator",
-                pool=self.object.pool.name,
-                calling_service="discovery"
-            ).__getattr__(self.snmp_version_def)(
-                self.object.address,
-                snmp_ro,
-                param
-            )
-        except RPCError as e:
-            self.logger.error("RPC Error: %s", e)
-            return None
-
-    def check_http_get(self, param):
-        """
-        Perform HTTP GET check. Param can be URL path or :<port>/<path>
-        """
-        url = "http://%s%s" % (self.object.address, param)
-        try:
-            return open_sync_rpc(
-                "activator",
-                pool=self.object.pool.name,
-                calling_service="discovery"
-            ).http_get(url)
-        except RPCError as e:
-            self.logger.error("RPC Error: %s", e)
-            return None
-
-    def check_https_get(self, param):
-        """
-        Perform HTTPS GET check. Param can be URL path or :<port>/<path>
-        """
-        url = "https://%s%s" % (self.object.address, param)
-        try:
-            return open_sync_rpc(
-                "activator",
-                pool=self.object.pool.name,
-                calling_service="discovery"
-            ).http_get(url)
-        except RPCError as e:
-            self.logger.error("RPC Error: %s", e)
-            return None
-
-    def is_match(self, result, method, value):
-        """
-        Returns True when result matches value
-        """
-        if method == "eq":
-            return result == value
-        elif method == "contains":
-            return value in result
-        elif method == "re":
-            return bool(re.search(value, result))
-        else:
-            self.logger.error("Invalid match method '%s'. Ignoring",
-                              method)
-            return False
+            snmp_community = self.object.credentials.snmp_ro
+            caps = self.object.get_caps()
+            if caps.get("SNMP | v2c") is False or caps.get("SNMP | v2c") is None:
+                snmp_version = SNMP_v1
+            else:
+                snmp_version = SNMP_v2c
+        #
+        checker = ProfileChecker(
+            self.object.address, self.object.pool.name,
+            logger=self.logger, calling_service="discovery",
+            snmp_community=snmp_community, snmp_version=snmp_version
+        )
+        profile = checker.get_profile()
+        if profile:
+            return profile
+        # Report problem
+        self.set_problem(
+            alarm_class="Discovery | Guess | Profile",
+            message=checker.get_error(),
+            fatal=False
+        )
+        self.logger.debug("Result %s" % self.job.problems)
+        return None
