74b2d936c184031f0b531d6a23c84ffc0b572879
Dmitry Volodin <dv@nocproject.org>
2019-04-30 13:05:14 +0300

    Load collections' json in test job

--- tests/test_0003_collections.py
+++ tests/test_0003_collections.py
@@ -2,12 +2,13 @@
 # ----------------------------------------------------------------------
 # Test collection loading
 # ----------------------------------------------------------------------
-# Copyright (C) 2007-2018 The NOC Project
+# Copyright (C) 2007-2019 The NOC Project
 # See LICENSE for details
 # ----------------------------------------------------------------------
 
 # NOC modules
 import operator
+import os
 # Third-party modules
 import pytest
 # NOC modules
@@ -17,6 +18,11 @@ from noc.core.collection.base import Collection
 @pytest.fixture(params=list(Collection.iter_collections()), ids=operator.attrgetter("name"))
 def collection(request):
     return request.param
+
+
+@pytest.mark.xfail
+def test_collection_path(collection):
+    assert any(os.path.isdir(p) for p in collection.get_path())
 
 
 @pytest.mark.usefixtures("database")
