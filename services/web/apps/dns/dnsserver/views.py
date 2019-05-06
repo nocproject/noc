81c84513e99c5d051ec865ddb946c408aaa6f27f
kk <kk@buhaha.net>
2019-05-06 09:05:45 +0300

    flake8_f401_webapp_dns

--- services/web/apps/dns/dnsserver/views.py
+++ services/web/apps/dns/dnsserver/views.py
@@ -2,12 +2,12 @@
 # ---------------------------------------------------------------------
 # dns.dnsserver application
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2016 The NOC Project
+# Copyright (C) 2007-2019 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
 # NOC modules
-from noc.lib.app.extmodelapplication import ExtModelApplication, view
+from noc.lib.app.extmodelapplication import ExtModelApplication
 from noc.dns.models.dnsserver import DNSServer
 from noc.core.translation import ugettext as _
 
