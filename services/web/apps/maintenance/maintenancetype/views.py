db211b88d9cbefd683fd7c06e5f183de1b84a4bf
kk <kk@buhaha.net>
2019-05-06 10:26:04 +0300

    flake8_f401_webapp_maintenance

--- services/web/apps/maintenance/maintenancetype/views.py
+++ services/web/apps/maintenance/maintenancetype/views.py
@@ -2,12 +2,12 @@
 # ---------------------------------------------------------------------
 # maintenance.maintenancetype application
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2016 The NOC Project
+# Copyright (C) 2007-2019 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
 # NOC modules
-from noc.lib.app.extdocapplication import ExtDocApplication, view
+from noc.lib.app.extdocapplication import ExtDocApplication
 from noc.maintenance.models.maintenancetype import MaintenanceType
 from noc.core.translation import ugettext as _
 
