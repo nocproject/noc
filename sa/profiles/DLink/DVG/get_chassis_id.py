4d792bf324ca1b62821791208de6c14d815e09ca
bee26 <bee26@ya.ru>
2019-05-13 07:52:37 +0000

    Merge branch 'dlink_dvg' into 'master'
    Fix DLink.DVG.get_chassis_id script
    
    See merge request noc/noc!1847
    
    (cherry picked from commit ac6a6321c3c8e9261b85d914554cfc532ca6924b)
    
    6839d483 Fix DLink.DVG.get_chassis_id script
    16af0901 Fix

--- sa/profiles/DLink/DVG/get_chassis_id.py
+++ sa/profiles/DLink/DVG/get_chassis_id.py
@@ -2,7 +2,7 @@
 # ---------------------------------------------------------------------
 # DLink.DVG.get_chassis_id
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2018 The NOC Project
+# Copyright (C) 2007-2019 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
@@ -20,18 +20,34 @@ class Script(BaseScript):
     # Always try SNMP first
     always_prefer = "S"
 
-    rx_mac = re.compile(r"^WAN MAC \[+(?P<mac>\S+)+\]$", re.MULTILINE)
+    rx_mac1 = re.compile(r"^WAN MAC \[+(?P<mac>\S+)+\]$", re.MULTILINE)
+    rx_mac2 = re.compile(r"Link encap:Ethernet\s+HWaddr (?P<mac>\S+)")
     SNMP_GET_OIDS = {
-        "SNMP": mib["IF-MIB::ifPhysAddress", 2]
+        "SNMP": [mib["IF-MIB::ifPhysAddress", 2]]
     }
 
     def execute_cli(self):
         r = self.cli("GET STATUS WAN", cached=True)
-        match = self.re_search(self.rx_mac, r)
-        if not match:
-            raise self.NotSupportedError()
-        mac = match.group("mac")
-        return [{
-            "first_chassis_mac": mac,
-            "last_chassis_mac": mac
-        }]
+        match = self.rx_mac1.search(r)
+        if match:
+            return [{
+                "first_chassis_mac": match.group("mac"),
+                "last_chassis_mac": match.group("mac")
+            }]
+
+        r = self.cli("ifconfig", cached=True)
+        macs = set()
+        for k in self.rx_mac2.findall(r):
+            try:
+                macs.add(k)
+            except ValueError:
+                pass
+        if macs:
+            macs = list(macs)
+            macs.sort()
+            return [{
+                "first_chassis_mac": f,
+                "last_chassis_mac": t
+            } for f, t in self.macs_to_ranges(macs)]
+
+        raise self.NotSupportedError()
