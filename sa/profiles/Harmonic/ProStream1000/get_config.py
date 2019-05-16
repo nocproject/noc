01c2cb03c18dc028732c21e3aff3ace9fe5eeec0
Andrey Vertiprahov <aversanta@gmail.com>
2019-05-16 06:18:59 +0000

    Merge branch 'noc-profiles-urllib' into 'master'
    Remove urllib usage in profiles
    
    See merge request noc/noc!2060
    
    (cherry picked from commit 3c24e59eb054442cf42f7c64c51544f55395f789)
    
    8a98c479 Remove urllib usage in profiles

--- sa/profiles/Harmonic/ProStream1000/get_config.py
+++ sa/profiles/Harmonic/ProStream1000/get_config.py
@@ -1,48 +1,31 @@
-__author__ = 'FeNikS'
 # -*- coding: utf-8 -*-
-
 # ---------------------------------------------------------------------
 # Harmonic.ProStream1000.get_config
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2018 The NOC Project
+# Copyright (C) 2007-2019 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
+# Python modules
 import re
+from xml.dom.minidom import parseString
 # NOC modules
 from noc.core.script.base import BaseScript
 from noc.sa.interfaces.igetconfig import IGetConfig
-# Python modules
-import urllib2
-from xml.dom.minidom import parseString
 
 
 class Script(BaseScript):
     name = "Harmonic.ProStream1000.get_config"
     interface = IGetConfig
 
-    data = '<AFRICA><Platform ID=\"1\" Action=\"GET_TREE\" /></AFRICA>'
+    DATA = '<AFRICA><Platform ID=\"1\" Action=\"GET_TREE\" /></AFRICA>'
     rx_sub = re.compile('\n\t+\n+', re.MULTILINE | re.DOTALL)
 
-    def execute(self):
-        url = 'http://' + self.access_profile.address + '/BrowseConfig'
-        user = self.access_profile.user
-        passw = self.access_profile.password
-
-        password_manager = urllib2.HTTPPasswordMgrWithDefaultRealm()
-        password_manager.add_password(None, url, user, passw)
-        auth_manager = urllib2.HTTPBasicAuthHandler(password_manager)
-        opener = urllib2.build_opener(auth_manager)
-        urllib2.install_opener(opener)
-
-        req = urllib2.Request(url, self.data)
-        response = urllib2.urlopen(req)
-
-        config = response.read()
+    def execute(self, **kwargs):
+        config = self.http.get("/BrowseConfig", data=self.DATA, use_basic=True)
         config = self.strip_first_lines(config, 1)
         config = parseString(config)
         config = config.toprettyxml()
-        config = self.rx_sub.sub('\n', config)
+        config = self.rx_sub.sub("\n", config)
         config = config.replace(">\n</", "></")
-
         return config
