a318ef13b20a15a764091c0b47a2516a9abcc93f
pilotsanya <adubnickiy@gmail.com>
2019-05-20 13:08:34 +0700

    fix comment

--- sa/profiles/TPLink/T2600G/get_mac_address_table.py
+++ sa/profiles/TPLink/T2600G/get_mac_address_table.py
@@ -5,8 +5,7 @@
 # Copyright (C) 2007-2019 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
-"""
-"""
+# Python modules
 import re
 # NOC modules
 from noc.core.script.base import BaseScript
