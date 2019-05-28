7989e9443ad151fee29fe04a46c018f7db057495
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2019-05-28 18:43:56 +0100

    Update Angtel.Topaz profile

--- sa/profiles/Angtel/Topaz/get_interface_status.py
+++ sa/profiles/Angtel/Topaz/get_interface_status.py
@@ -2,11 +2,10 @@
 # ---------------------------------------------------------------------
 # Angtel.Topaz.get_interface_status
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2016 The NOC Project
+# Copyright (C) 2007-2019 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
-"""
-"""
+
 # Python modules
 import re
 # NOC modules
@@ -17,15 +16,17 @@ from noc.sa.interfaces.igetinterfacestatus import IGetInterfaceStatus
 class Script(BaseScript):
     name = "Angtel.Topaz.get_interface_status"
     interface = IGetInterfaceStatus
+    cache = True
 
     rx_port = re.compile(
         r"^(?P<port>(?:Fa|Gi|Te|Po)\S+)\s+\S+\s+\S+\s+\S+\s+\S+\s+\S+\s+"
         r"(?P<oper_status>Up|Down|Not Present)",
         re.MULTILINE | re.IGNORECASE)
 
-    def execute(self, interface=None):
+    def execute_cli(self, interface=None):
         r = []
-        for match in self.rx_port.finditer(self.cli("show interfaces status")):
+        v = self.cli("show interfaces status", cached=True)
+        for match in self.rx_port.finditer(v):
             if (interface is not None) and (interface == match.group('port')):
                 return [{
                     "interface": match.group('port'),
