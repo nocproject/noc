79418c3f5ad9a85f6f4d13716e0e23ff722411c6
belkir <belouskir@gmail.com>
2019-06-10 16:42:03 +0300

    fixed H3C get_version for old devices like H3C S3100-8T-SI

--- sa/profiles/H3C/VRP/get_version.py
+++ sa/profiles/H3C/VRP/get_version.py
@@ -2,11 +2,10 @@
 # ---------------------------------------------------------------------
 # H3C.VRP.get_version
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2012 The NOC Project
+# Copyright (C) 2007-2019 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
-"""
-"""
+
 # Python modules
 import re
 # NOC modules
@@ -23,23 +22,23 @@ class Script(BaseScript):
     rx_ver = re.compile(r"^.*?Switch\s(?P<platform>.+?)\sSoftware\s\Version"
                         r"\s3Com\sOS\sV(?P<version>.+?)$",
                         re.MULTILINE | re.DOTALL | re.IGNORECASE)
-    rx_ver1 = re.compile(r"^\s*Comware\sSoftware,\s\Version\s(?P<version>.+?),"
-                         r"\sRelease\s(?P<release>.+?)$.+?(H3C )?(?P<platform>\S+) uptime is",
+    rx_ver1 = re.compile(r"^\s*Comware\sSoftware,\s\Version\s(?P<version>.+?),\s"
+                         r"(Release)?(?P<release>\w+(\s)?\w+).*(H3C )(?P<platform>[a-zA-Z0-9\-]+) uptime is",
                          re.MULTILINE | re.DOTALL | re.IGNORECASE)
     rx_hw = re.compile(r"Hardware Version is (?P<hardware>\S+)")
     rx_boot = re.compile(r"Bootrom Version is (?P<bootprom>\S+)")
 
-    def execute(self):
+    def execute_cli(self, **kwargs):
         v = self.cli("display version")
-        snmp_sn = self.snmp.get(mib["ENTITY-MIB::entPhysicalSerialNum.1"])
+        snmp_sn1 = self.snmp.get(mib["ENTITY-MIB::entPhysicalSerialNum.1"]) if self.has_snmp() else None
+        snmp_sn2 = self.snmp.get(mib["ENTITY-MIB::entPhysicalSerialNum.2"]) if self.has_snmp() else None
         match = self.rx_ver.search(v)
         if not match:
             match = self.rx_ver1.search(v)
             r = {
                 "vendor": "H3C",
                 "platform": match.group("platform"),
-                "version": match.group("version") + "." + \
-                           match.group("release")
+                "version": match.group("version") + "." + match.group("release")
             }
         else:
             r = {
@@ -49,12 +48,13 @@ class Script(BaseScript):
             }
         hw = self.rx_hw.search(v)
         boot = self.rx_boot.search(v)
-        if hw or boot or snmp_sn:
-            r.update({"attributes": {}})
-            if snmp_sn:
-                r["attributes"].update({"Serial Number": snmp_sn})
-            if hw:
-                r["attributes"].update({"HW version": hw.group("hardware")})
-            if boot:
-                r["attributes"].update({"Boot PROM": boot.group("bootprom")})
+        r["attributes"] = {}
+        if snmp_sn1:
+            r["attributes"]["Serial Number"] = snmp_sn1
+        elif snmp_sn2:
+            r["attributes"]["Serial Number"] = snmp_sn2
+        if hw:
+            r["attributes"]["HW version"] = hw.group("hardware")
+        if boot:
+            r["attributes"]["Boot PROM"] = boot.group("bootprom")
         return r
