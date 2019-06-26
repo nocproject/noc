4319034b0fbdee58cce1a3bf840a6fb9b5a4a342
Dmitry S. Luhtionov <dmitryluhtionov@gmail.com>
2019-06-26 18:07:02 +0300

    Update Eltex.ESR profile

--- sa/profiles/Eltex/ESR/get_capabilities.py
+++ sa/profiles/Eltex/ESR/get_capabilities.py
@@ -2,12 +2,10 @@
 # ---------------------------------------------------------------------
 # Eltex.ESR.get_capabilities
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2017 The NOC Project
+# Copyright (C) 2007-2019 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
-# Python modules
-import re
 # NOC modules
 from noc.sa.profiles.Generic.get_capabilities import Script as BaseScript
 from noc.sa.profiles.Generic.get_capabilities import false_on_cli_error
@@ -22,4 +20,11 @@ class Script(BaseScript):
         Check box has IPv6 ND enabled
         """
         self.cli("show ipv6 neighbors")
+        return True
+
+    @false_on_cli_error
+    def has_lldp_cli(self):
+        """
+        Check box has LLDP enabled
+        """
         return True
