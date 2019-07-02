62cf1da88bd9eec845d30ea4ecb71fdf5d6f4149
Dmitry S. Luhtionov <dmitryluhtionov@gmail.com>
2019-07-02 14:08:36 +0300

    Fix MikroTik.RouterOS.get_cdp_neighbors script

--- sa/profiles/MikroTik/RouterOS/get_cdp_neighbors.py
+++ sa/profiles/MikroTik/RouterOS/get_cdp_neighbors.py
@@ -2,11 +2,11 @@
 # ---------------------------------------------------------------------
 # MikroTik.RouterOS.get_cdp_neighbors
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2016 The NOC Project
+# Copyright (C) 2007-2019 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
-"""
-"""
+
+# NOC modules
 from noc.core.script.base import BaseScript
 from noc.sa.interfaces.igetcdpneighbors import IGetCDPNeighbors
 
@@ -19,25 +19,26 @@ class Script(BaseScript):
         device_id = self.scripts.get_fqdn()
         interfaces = []
         for n, f, r in self.cli_detail(
-        "/interface print detail without-paging where type=\"ether\""):
+            '/interface print detail without-paging where type="ether"'
+        ):
             interfaces += [r["name"]]
         # Get neighbors
         neighbors = []
-        for n, f, r in self.cli_detail(
-        "/ip neighbor print detail without-paging"):
+        for n, f, r in self.cli_detail("/ip neighbor print detail without-paging"):
             platform = r["platform"]
             if platform == "MikroTik":
                 continue
             if r["interface"] not in interfaces:
                 continue
-            neighbors += [{
-                "device_id": r["identity"],
-                "local_interface": r["interface"],
-                "remote_interface": r["interface-name"],
-                "remote_ip": r["address"],
-                "platform": platform
-            }]
-        return {
-            "device_id": device_id,
-            "neighbors": neighbors
-        }
+            if r.get("interface-name") is None or r.get("address") is None:
+                continue
+            neighbors += [
+                {
+                    "device_id": r["identity"],
+                    "local_interface": r["interface"],
+                    "remote_interface": r["interface-name"],
+                    "remote_ip": r["address"],
+                    "platform": platform,
+                }
+            ]
+        return {"device_id": device_id, "neighbors": neighbors}
