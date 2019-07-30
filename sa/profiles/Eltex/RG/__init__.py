3a7a71433ffcc8fffe8471822d2505ae4f6d2df3
MaksimSmile13 <sysfar@gmail.com>
2019-07-30 18:39:45 +0100

    Обновить sa/profiles/Eltex/RG/get_config.py, sa/profiles/Eltex/RG/__init__.py файлах

--- sa/profiles/Eltex/RG/__init__.py
+++ sa/profiles/Eltex/RG/__init__.py
@@ -3,7 +3,7 @@
 # Vendor: Eltex
 # OS:     RG
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2017 The NOC Project
+# Copyright (C) 2007-2019 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
@@ -15,13 +15,13 @@ class Profile(BaseProfile):
     name = "Eltex.RG"
     pattern_username = "^\S+ [Ll]ogin:"
     pattern_password = "^[Pp]assword:"
-    pattern_prompt = r"^\S+\$ "
+    pattern_syntax_error = r"Permission denied"
+    pattern_unprivileged_prompt = r"^\S+@(?P<hostname>\S+):~\$"
+    pattern_prompt = r"^\S+@(?P<hostname>\S+):~#"
     command_exit = "exit"
     command_more = "\n"
 
-    PLATFORMS = {
-        "46": "RG-1404GF-W"
-    }
+    PLATFORMS = {"46": "RG-1404GF-W"}
 
     @classmethod
     def get_platforms(cls, name):
