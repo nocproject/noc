64fef9ea0949cdd463c1fe58c28fd07a8efa554a
Dmitry Volodin <dv@nocproject.org>
2019-08-06 14:24:03 +0300

    Drop unused fields

---
+++ sa/migrations/0199_managedobject_clear_fields.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------
+# Remove unused legacy fields from ManagedObject model
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2019 The NOC Project
+# See LICENSE for details
+# ----------------------------------------------------------------------
+
+# NOC modules
+from noc.core.migration.base import BaseMigration
+
+
+class Migration(BaseMigration):
+    def migrate(self):
+        if self.db.has_column("sa_managedobject", "collector_id"):
+            self.db.delete_column("sa_managedobject", "collector_id")
