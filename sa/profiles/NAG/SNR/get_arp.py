f905f410aca3de11eb10ff7d76290e0460230257
Andrey Yemelyanov <anyem1980@yandex.ru>
2019-08-13 07:56:36 +0100

    Update NAG.SNR.get_arp.py

--- sa/profiles/NAG/SNR/get_arp.py
+++ sa/profiles/NAG/SNR/get_arp.py
@@ -2,11 +2,13 @@
 # ---------------------------------------------------------------------
 # NAG.SNR.get_arp
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2016 The NOC Project
+# Copyright (C) 2007-2019 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
 # Python modules
+from builtins import str
+
 # NOC modules
 from noc.core.script.base import BaseScript
 from noc.sa.interfaces.igetarp import IGetARP
@@ -17,23 +19,12 @@ class Script(BaseScript):
     interface = IGetARP
     cache = True
 
-    def execute(self):
+    def execute_snmp(self):
         r = []
-        # Try SNMP first
-        if self.has_snmp():
-            try:
-                for v in self.snmp.get_tables(
-                    ["1.3.6.1.2.1.4.22.1.1", "1.3.6.1.2.1.4.22.1.2", "1.3.6.1.2.1.4.22.1.3"],
-                    bulk=True,
-                ):
-                    iface = self.snmp.get("1.3.6.1.2.1.31.1.1.1.1." + v[1], cached=True)  # IF-MIB
-                    mac = ":".join(["%02x" % ord(c) for c in v[2]])
-                    ip = ["%02x" % ord(c) for c in v[3]]
-                    ip = ".".join(str(int(c, 16)) for c in ip)
-                    r.append({"ip": ip, "mac": mac, "interface": iface})
-                return r
-            except self.snmp.TimeOutError:
-                pass
-
-        # Fallback to CLI
-        raise Exception("Not implemented")
+        for v in self.snmp.get_tables(
+            ["1.3.6.1.2.1.4.22.1.1", "1.3.6.1.2.1.4.22.1.2", "1.3.6.1.2.1.4.22.1.3"], bulk=True
+        ):
+            iface = self.snmp.get("1.3.6.1.2.1.31.1.1.1.1." + str(v[1]), cached=True)  # IF-MIB
+            mac = ":".join(["%02x" % ord(c) for c in v[2]])
+            r.append({"ip": v[3], "mac": mac, "interface": iface})
+        return r
