0f1d2dbff0258128556fd6785e396b92e4b618b4
Andrey Vertiprahov <aversanta@gmail.com>
2019-08-18 19:12:31 +0500

    Backport. NSN.TIMOS.get_lldp_neighbors port descripton regex.

--- sa/profiles/NSN/TIMOS/get_lldp_neighbors.py
+++ sa/profiles/NSN/TIMOS/get_lldp_neighbors.py
@@ -8,6 +8,7 @@
 
 # Python modules
 import re
+
 # NOC modules
 from noc.core.script.base import BaseScript
 from noc.sa.interfaces.igetlldpneighbors import IGetLLDPNeighbors
@@ -25,10 +26,17 @@ class Script(BaseScript):
         r"^Chassis Id Subtype\s+:\s(?P<remote_chassis_id_subtype>\d)\s.*\n"
         r"^Chassis Id\s+:\s(?P<remote_chassis_id>\S+)\s*\n"
         r"^PortId Subtype\s+:\s(?P<remote_port_subtype>\d)\s.*\n"
-        r"^Port\sId\s+:\s(?P<remote_port>.+?)\n"
-        r"^Port\sDescription\s+:\s(?P<remote_port_description>.+?)\n"
+        r"^Port\sId\s+:\s(?P<remote_port>(.+\n?){1,3}?)\n"  # Port Id : 70:6F:72:74:20:32:37\n"port 27"
+        r"^Port\sDescription\s+:\s(?P<remote_port_description>.+\n?.+?)\n"
         r"^System\sName\s+:\s(?P<remote_system_name>\S+)\n",
-        re.MULTILINE)
+        re.MULTILINE,
+    )
+    rx_port_descr = re.compile(
+        r"(?P<remote_port_name>\S+),"
+        r"\s*(?P<remote_port_type>.+),\n?"
+        r"\s*\"(?P<remote_port_description>.+)"
+    )
+    rx_port_id = re.compile(r"^\s*(?P<remote_port>.+)\s+\"(?P<port_name>.+)\"", re.DOTALL)
 
     CAPS_MAP = {
         "other": 1,
@@ -39,7 +47,7 @@ class Script(BaseScript):
         "telephone": 32,
         "docsis": 64,
         "station": 128,
-        "cvlan": 0
+        "cvlan": 0,
     }
 
     NOT_SPECIFIED = "(not specified)"
@@ -65,8 +73,19 @@ class Script(BaseScript):
             match_obj = self.rx_remote_info.search(v)
             pri = match_obj.groupdict()
             pri["remote_capabilities"] = self.fixcaps(pri["remote_capabilities"])
-            if 'n/a' in pri['remote_system_name']:
-                del pri['remote_system_name']
+            if "n/a" in pri["remote_system_name"]:
+                del pri["remote_system_name"]
+            # print("Match Port Description", self.rx_port_descr.match(pri["remote_port_description"]))
+            if self.rx_port_descr.match(pri["remote_port_description"]):
+                pri["remote_port_description"] = (
+                    self.rx_port_descr.match(pri["remote_port_description"])
+                    .group("remote_port_description")
+                    .strip(' "')
+                )
+            elif "n/a" in pri["remote_port"]:
+                del pri["remote_port_description"]
+            if self.rx_port_id.match(pri["remote_port"]):
+                pri["remote_port"] = self.rx_port_id.match(pri["remote_port"]).group("port_name")
             return pri
 
     def execute_cli(self):
@@ -79,16 +98,18 @@ class Script(BaseScript):
             match = self.rx_some.search(line)
             if not match:
                 continue
-            port = match.group('port')
-            local_lldp = self.cli('show port %s ethernet detail | match IfIndex' % port)
+            port = match.group("port")
+            local_lldp = self.cli("show port %s ethernet detail | match IfIndex" % port)
             lldp_match = self.rx_local_lldp.search(local_lldp)
             if not lldp_match:
                 continue
-            local_interface_id = str(lldp_match.group('local_interface_id'))
+            local_interface_id = str(lldp_match.group("local_interface_id"))
             pri = self.get_port_info(port)
-            r += [{
-                "local_interface": port,
-                "local_interface_id": local_interface_id,
-                "neighbors": [pri]
-            }]
+            r += [
+                {
+                    "local_interface": port,
+                    "local_interface_id": local_interface_id,
+                    "neighbors": [pri],
+                }
+            ]
         return r
