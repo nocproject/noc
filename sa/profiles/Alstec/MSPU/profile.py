9fab675855d02bc57bcda4d298e86191821e8927
bee26 <bee26@ya.ru>
2019-09-06 10:22:38 +0000

    Merge branch 'alstec_mspu' into 'master'
    Update Alstec.MSPU profile
    
    See merge request noc/noc!2578
    
    (cherry picked from commit e24cd88c4e9b78b641b13b02df60d1f778d2c6fd)
    
    9110f0ed Update Alstec.MSPU profile

--- sa/profiles/Alstec/MSPU/profile.py
+++ sa/profiles/Alstec/MSPU/profile.py
@@ -3,7 +3,7 @@
 # Vendor: Alstec
 # OS:     MSPU
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2017 The NOC Project
+# Copyright (C) 2007-2019 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
@@ -17,3 +17,17 @@ class Profile(BaseProfile):
     pattern_more = r"^--More-- or \(q\)uit$"
     pattern_syntax_error = r"\^ error"
     command_exit = "exit"
+
+    INTERFACE_TYPES = {
+        "ad": "physical",  # adsl
+        "up": "physical",  # uplink
+        "lo": "loopback",
+        "br": "SVI",  # brUIK
+        "hb": "SVI",  # hbr
+        "he": "SVI",  # heoa
+        "hu": "SVI",  # huplink
+    }
+
+    @classmethod
+    def get_interface_type(cls, name):
+        return cls.INTERFACE_TYPES.get(name[:2].lower())
