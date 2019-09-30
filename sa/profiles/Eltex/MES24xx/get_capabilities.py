665eea04ecd589564ae588c18db5ff0b64be4d49
Andrey Vertiprahov <aversanta@gmail.com>
2019-09-30 19:28:38 +0100

    Merge branch 'eltex_mes24xx' into 'master'
    Add support for Eltex.MES24xx profile
    
    See merge request noc/noc!2662
    
    (cherry picked from commit 227e9bd74b3ae016765f23e94bb05a8a8d5ad341)
    
    65746fe5 Add initial support for Eltex.MES24xx profile
    b596d943 Fix import
    691bbfc9 Fix
    5c195764 Add rst file for Eltex.MES24xx profile
    cd8ade1e Eltex.MES24xx.ping fix typo.
    6941acc4 Fix
    8871bc52 Add Eltex.MES24xx.get_ifindexes script.
    7b145408 Eltex.MES24xx.get_chassis_id. Add SNMP support.

---
+++ sa/profiles/Eltex/MES24xx/get_capabilities.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# Eltex.MES24xx.get_capabilities
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2019 The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+
+# NOC modules
+from noc.sa.profiles.Generic.get_capabilities import Script as BaseScript
+from noc.sa.profiles.Generic.get_capabilities import false_on_cli_error
+
+
+class Script(BaseScript):
+    name = "Eltex.MES24xx.get_capabilities"
+
+    @false_on_cli_error
+    def has_lldp_cli(self):
+        """
+        Check box has lldp enabled on Eltex
+        """
+        cmd = self.cli("show lldp", ignore_errors=True)
+        return "LLDP is disabled" not in cmd
