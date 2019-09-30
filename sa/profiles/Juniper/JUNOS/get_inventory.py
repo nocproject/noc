b53e8f8b4dabcf22703fe6540c03c70b6544f5b6
Andrey Vertiprahov <aversanta@gmail.com>
2019-09-30 17:12:56 +0100

    release-19.3:Backport!2672

--- sa/profiles/Juniper/JUNOS/get_inventory.py
+++ sa/profiles/Juniper/JUNOS/get_inventory.py
@@ -2,7 +2,7 @@
 # ---------------------------------------------------------------------
 # Juniper.JUNOS.get_inventory
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2018 The NOC Project
+# Copyright (C) 2007-2019 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
@@ -28,7 +28,7 @@ class Script(BaseScript):
     rx_part = re.compile(
         r"^\s*(?P<name>\S+(?: \S+)+?)\s+"
         r"(?P<revision>rev \d+|\S{1,6})?\s+"
-        r"(?P<part_no>\d{3}-\d{6}|NON-JNPR|BUILTIN)\s+"
+        r"(?P<part_no>\d{3}-\d{6}|NON-JNPR|UNKNOWN|BUILTIN)\s+"
         r"(?P<serial>\S+)\s+"
         r"(?P<rest>.+)$",
         re.IGNORECASE,
@@ -40,7 +40,9 @@ class Script(BaseScript):
         "POWER SUPPLY": "PEM",
         "PSU": "PSU",
         "ROUTING ENGINE": "RE",
+        "AFEB": "AFEB",
         "CB": "SCB",
+        "MGMT BRD": "MGMT",
         "FPC": "FPC",
         "MPC": "FPC",
         "MIC": "MIC",
@@ -52,16 +54,22 @@ class Script(BaseScript):
 
     IGNORED = {
         "RE": {
-            "750-026468",  # EX2200-24T-4G
-            "750-026331",  # EX2200-48P-4G, POE
-            "750-033065",  # EX4200-24T, 8 POE
-            "750-021258",  # EX4200-24F
-            "750-034594",  # RE-SRX210HE
-            "710-017560",  # 710-017560
-            "750-021778",  # RE-SRX210B
             "710-015273",  # RE-J4350-2540
             "710-017560",  # RE-J2320-2000
-        }
+            "750-021258",  # EX4200-24F
+            "750-021778",  # RE-SRX210B
+            "750-026331",  # EX2200-48P-4G, POE
+            "750-026468",  # EX2200-24T-4G
+            "750-033063",  # EX4200-48T, 8 POE
+            "750-033065",  # EX4200-24T, 8 POE
+            "750-033072",  # EX4200-48T, 8 POE
+            "750-033073",  # EX4200-24T, 8 POE
+            "750-033075",  # EX4200-24F
+            "750-034594",  # RE-SRX210HE
+            "750-036562",  # 750-036562
+            "750-045404",  # EX4550-32F
+        },
+        "AFEB": {"BUILTIN"},  # Forwarding Engine Processor
     }
 
     def parse_hardware(self, v):
@@ -69,12 +77,13 @@ class Script(BaseScript):
         Parse "show chassis hardware"
         and yeld name, revision, part_no, serial, description
         """
+        chassis_no = None
         for line in v.splitlines():
             line = line.strip()
             if not line:
                 continue
             if line.startswith("node"):
-                self.chassis_no = line[4:-1]
+                chassis_no = line[4:-1]
                 continue
             match = self.rx_part.search(line)
             if match:
@@ -83,15 +92,22 @@ class Script(BaseScript):
                 match = self.rx_chassis.search(line)
                 if match:
                     rev = match.group("revision")
-                    yield ("Chassis", rev, None, match.group("serial"), match.group("rest"))
+                    yield (
+                        chassis_no,
+                        "Chassis",
+                        rev,
+                        None,
+                        match.group("serial"),
+                        match.group("rest"),
+                    )
 
-    def execute(self):
-        self.chassis_no = None
+    def execute_cli(self):
+        virtual_chassis = None
         v = self.cli("show chassis hardware")
         objects = []
         chassis_sn = set()
         p_hardware = self.parse_hardware(v)
-        for name, revision, part_no, serial, description in p_hardware:
+        for chassis_no, name, revision, part_no, serial, description in p_hardware:
             builtin = False
             # Detect type
             t, number = self.get_type(name)
@@ -102,11 +118,12 @@ class Script(BaseScript):
                 continue
             # Discard virtual chassis and ignored part numbers
             if description == "Virtual Chassis":
+                virtual_chassis = True
                 continue
             if t in self.IGNORED and part_no in self.IGNORED[t]:
                 continue
             # Detect vendor
-            if part_no == "NON-JNPR":
+            if part_no in ("NON-JNPR", "UNKNOWN"):
                 vendor = "NONAME"
             else:
                 vendor = "JUNIPER"
@@ -116,7 +133,17 @@ class Script(BaseScript):
                 chassis_sn.add(serial)
             elif t == "FPC":
                 if description.startswith("EX4"):
+                    # Avoid duplicate `CHASSIS` type on some EX switches
+                    has_chassis = False
+                    if not virtual_chassis:
+                        for i in objects:
+                            if i["type"] == "CHASSIS":
+                                has_chassis = True
+                                break
+                    if has_chassis:
+                        continue
                     t = "CHASSIS"
+                    part_no = description.split()[0].upper()
                     chassis_sn.add(serial)
             elif t == "XCVR":
                 if vendor == "NONAME":
@@ -124,11 +151,11 @@ class Script(BaseScript):
                         part_no = self.UNKNOWN_XCVR
                     else:
                         part_no = self.get_trans_part_no(serial, description)
-            elif serial == "BUILTIN" or serial in chassis_sn:
+            if serial == "BUILTIN" or serial in chassis_sn and t != "CHASSIS":
                 builtin = True
                 part_no = []
-            if t == "CHASSIS" and number is None and self.chassis_no is not None:
-                number = self.chassis_no
+            if t == "CHASSIS" and number is None and chassis_no is not None:
+                number = chassis_no
             # Submit object
             objects += [
                 {
@@ -168,6 +195,9 @@ class Script(BaseScript):
         elif len(n) == 3:
             # SFP+-10G-LR
             t, s, m = n
+        elif len(n) == 4 and description.startswith("SFP-1000BASE-BX10-"):
+            # SFP-1000BASE-BX10-U, SFP-1000BASE-BX10-D
+            return "NoName | Transceiver | 1G | SFP BX10%s" % n[-1]
         else:
             self.logger.error("Cannot detect transceiver type: '%s'", description)
             return self.UNKNOWN_XCVR
