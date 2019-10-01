d2776da3451a4a478583d142282378d68b245f42
uncol <d.a.zakharov@gmail.com>
2019-10-01 12:18:38 +0300

    add theme-name variable

--- ui/web/builder/src/index.js
+++ ui/web/builder/src/index.js
@@ -7,21 +7,27 @@ const tar = require('tar-fs');
 const destDir = 'dist';
 // const args = process.argv.slice(2);
 const themeName = 'gray';
-const assetDirs = [
-    '.pkg_cache/ui/pkg/fontawesome/fonts',
-    `.pkg_cache/ui/pkg/extjs/classic/theme-${themeName}/resources/images`
-];
 let queue = load_packages();
 
 fs.mkdirSync(destDir);
+
+// ToDo test theme-noc, perhaps need other resources!
+function assets(theme) {
+    const assetDirs = [
+        '.pkg_cache/ui/pkg/fontawesome/fonts',
+        `.pkg_cache/ui/pkg/extjs/classic/theme-${theme}/resources/images`
+    ];
+    assetDirs.forEach(dir => {
+        const name = dir.substr(dir.lastIndexOf('/') + 1);
+        tar.pack(dir).pipe(tar.extract(`${destDir}/${name}`));
+    });
+}
+
 Promise.all(queue)
 .then(values => {
         console.log(values);
         console.log('loading done');
-        assetDirs.forEach(dir => {
-            const name = dir.substr(dir.lastIndexOf('/') + 1);
-            tar.pack(dir).pipe(tar.extract(`${destDir}/${name}`));
-        });
+        assets(themeName);
         build_css(themeName);
         build_js.application('bundle_noc', themeName);
         build_js.vendor('bundle_vendor', themeName);
