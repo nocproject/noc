ed8e9ebf77de348de09a560791a0a24d0b0a226b
Andrey Vertiprahov <aversanta@gmail.com>
2019-10-09 20:00:54 +0500

    Huawei.MA5600T.get_inventory. Fix ma5626g chassis.

--- sa/profiles/Huawei/MA5600T/get_inventory.py
+++ sa/profiles/Huawei/MA5600T/get_inventory.py
@@ -278,7 +278,7 @@ class Script(BaseScript):
                 i_type = "FAN"
             elif item_name.startswith("slot") and "PWC" in item.type:
                 i_type = "PWR"
-            elif item_name.startswith("main_board"):
+            elif item_name.startswith("main_board") and "Assembly Chassis" not in item.description:
                 i_type = "BOARD"
             elif item_name.startswith("daughter_board"):
                 i_type = "DAUGHTERBOARD"
