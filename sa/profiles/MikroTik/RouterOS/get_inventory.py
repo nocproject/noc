8c7731d20ecf2d11132a99d09e29b73dabb814d9
Andrey Yemelyanov <anyem1980@yandex.ru>
2019-10-11 14:32:46 +0100

    Update MikroTik.RouterOS.get_inventory.py: add get transceivers info.

--- sa/profiles/MikroTik/RouterOS/get_inventory.py
+++ sa/profiles/MikroTik/RouterOS/get_inventory.py
@@ -6,6 +6,9 @@
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
+# Python modules
+import re
+
 # NOC modules
 from noc.core.script.base import BaseScript
 from noc.sa.interfaces.igetinventory import IGetInventory
@@ -15,11 +18,25 @@ class Script(BaseScript):
     name = "MikroTik.RouterOS.get_inventory"
     interface = IGetInventory
 
+    rx_sfp = re.compile(
+        r"(?:^\s+sfp-link-length-\d+um: (?P<ll>\d+)m\s*\n)?"
+        r"(?:^\s+sfp-link-length-copper: (?P<copper>\d+)m\s*\n)?"
+        r"^\s+sfp-vendor-name: (?P<vendor>.+)\s*\n"
+        r"^\s+sfp-vendor-part-number: (?P<part_no>.+)\s*\n"
+        r"^\s+sfp-vendor-revision: (?P<revision>\S+)\s*\n"
+        r"^\s+sfp-vendor-serial: (?P<serial>\S+)\s*\n"
+        r"(?:^\s+sfp-manufacturing-date: (?P<date>\S+)\s*\n)?"
+        r"(?:^\s+sfp-wavelength: (?P<nm>\d+)nm\s*$)?",
+        re.MULTILINE,
+    )
+    rx_rate = re.compile(r"^\s+rate: (?P<rate>\S+)\s*\n", re.MULTILINE)
+
     def execute_cli(self):
+        i = []
         v = self.scripts.get_version()
         platform = v["platform"]
         if platform not in ["x86", "CHR"]:
-            return [
+            i += [
                 {
                     "type": "CHASSIS",
                     "vendor": "MikroTik",
@@ -27,4 +44,80 @@ class Script(BaseScript):
                     "serial": v["attributes"]["Serial Number"],
                 }
             ]
-        return []
+            eth = {}
+            for n, f, r in self.cli_detail("/interface ethernet print detail without-paging"):
+                iface = r["default-name"]
+                eth[str(n)] = iface
+            for port in range(len(eth)):
+                inv = self.cli("/interface ethernet monitor " + str(port) + " once")
+                rt = self.rx_rate.search(inv)
+                if rt:
+                    rate = rt.group("rate")
+                else:
+                    rate = None
+                match = self.rx_sfp.search(inv)
+                if match:
+                    vendor = match.group("vendor")
+                    description = match.group("part_no")
+                    if vendor == "" or vendor == "OEM":
+                        vendor = "NONAME"
+                        part = "NoName | Transceiver | "
+                        if match.group("nm"):
+                            nm = int(match.group("nm"))
+                        else:
+                            nm = 0
+                        if match.group("ll"):
+                            ll = int(match.group("ll"))
+                        else:
+                            ll = 0
+                        if match.group("copper"):
+                            copper = int(match.group("copper"))
+                            if copper == 3:
+                                part_no = "SFP+-10G-CU3M"
+                            elif copper == 1:
+                                part_no = "SFP-H10GB-CU1M"
+                            else:
+                                part_no = part + "10G | SFP+ Twinax"
+                        elif (description.startswith("SFP+") or rate == "10Gbps") and ll > 0:
+                            if nm == 850:
+                                part_no = part + "10G | SFP+ SR"
+                            elif ll > 500 and ll <= 10000:
+                                part_no = part + "10G | SFP+ LR"
+                            elif ll > 10000 and ll <= 40000:
+                                part_no = part + "10G | SFP+ ER"
+                            elif ll > 40000:
+                                part_no = part + "10G | SFP+ ZR"
+                            else:
+                                part_no = part + "10G | SFP+"
+                        elif rate == "1Gbps":
+                            if nm == 850:
+                                part_no = part + "1G | SFP SX"
+                            elif nm == 1310 and ll <= 10000:
+                                part_no = part + "1G | SFP LX"
+                            elif nm == 1310 and ll > 10000 and ll <= 40000:
+                                part_no = part + "1G | SFP EX"
+                            elif nm == 1550 and ll > 40000:
+                                part_no = part + "1G | SFP ZX"
+                            else:
+                                part_no = part + "1G | SFP"
+                        else:
+                            part_no = part + "Unknown SFP"
+                    else:
+                        part_no = description
+                    x = {
+                        "type": "XCVR",
+                        "vendor": match.group("vendor"),
+                        "serial": match.group("serial"),
+                        "part_no": [part_no],
+                        "number": eth[str(port)],
+                        "revision": match.group("revision"),
+                    }
+                    date = match.group("date")
+                    if date is not None:
+                        dt = date.split("-")
+                        year = "20" + dt[0]
+                        parts = [year, dt[1], dt[2]]
+                        mfd = "-".join(parts)
+                        x["mfg_date"] = mfd
+                    i += [x]
+        return i
