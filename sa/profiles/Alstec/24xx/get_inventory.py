adba14279635a6a04cfe89b81f7eae9926fc85f2
Andrey Vertiprahov <aversanta@gmail.com>
2019-11-06 18:23:35 +0000

    Merge branch 'patch-avs-alstec-metrics' into 'master'
    Alstec.24xx. Add get_inventory script.
    
    See merge request noc/noc!2796
    
    (cherry picked from commit 35103c67639ea375e0ea8ec22982408bae04d41d)
    
    aa67c135 Alstec.24xx. Add get_inventory script.
    0dccdc28 Fix typo.
    929a10d5 Fix typo.

---
+++ sa/profiles/Alstec/24xx/get_inventory.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# Alstec.24xx.get_inventory
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2019 The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+
+# NOC modules
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetinventory import IGetInventory
+
+
+class Script(BaseScript):
+    name = "Alstec.24xx.get_inventory"
+    interface = IGetInventory
+    port_map = {10: "1", 18: "2", 26: "3"}  # 16, 2  # 8, 2  # 24, 2
+
+    def execute_snmp(self, **kwargs):
+        platform = self.snmp.get("1.3.6.1.4.1.27142.1.1.1.1.1.3.0")
+        serial = self.snmp.get("1.3.6.1.4.1.27142.1.1.1.1.1.4.0")
+        revision = self.snmp.get("1.3.6.1.4.1.27142.1.1.1.1.1.14.0")
+        port_num = self.snmp.get("1.3.6.1.2.1.2.1.0")
+        if port_num in self.port_map:
+            platform = "%s-0%s" % (platform, self.port_map[port_num])
+        return [
+            {
+                "type": "CHASSIS",
+                "vendor": "Alstec",
+                "part_no": platform,
+                "revision": revision,
+                "serial": serial,
+            }
+        ]
