cc730a8f8ded6594c7443a77dc4f1c61b7793f49
Andrey Vertiprahov <aversanta@gmail.com>
2019-11-27 13:48:22 +0000

    ECI.HiFOCuS. Add get_inventory, Improvement SNMP work.

--- sa/profiles/ECI/HiFOCuS/get_interfaces.py
+++ sa/profiles/ECI/HiFOCuS/get_interfaces.py
@@ -7,16 +7,16 @@
 # -------------------------------------------------------------------
 
 # Python modules
-from __future__ import print_function
+import re
 
-# import re
+# Third-party modules
 import six
 
 # NOC modules
 from noc.core.script.base import BaseScript
 from noc.sa.interfaces.igetinterfaces import IGetInterfaces
-
-# from noc.core.ip import IPv4
+from noc.core.ip import IPv4
+from noc.core.mib import mib
 
 
 class Script(BaseScript):
@@ -25,23 +25,146 @@ class Script(BaseScript):
     reuse_cli_session = False
     keep_cli_session = False
 
-    def execute_cli(self, **kwargs):
-        boards = self.profile.get_boards(self)
-        shelf_num = 0
+    rx_table_spliter = re.compile(r"(?:\+-*)+\n")
+
+    rx_snmp_iface = re.compile(
+        r".+:\s*Shelf-(?P<shelf>\d+)\s*Interface-(?P<slot>\d+)\s*"
+        r"Port-(?P<port>\d+)\s*Mani-(?P<mani>\d+)\s*"
+        r"Layer-(?P<layer>\d+)\s*Loc-(?P<loc>\d+)\s*Uni-(?P<uni>\d+)\s*"
+    )
+
+    rx_iface_name = re.compile(r"(?P<ifname>\S+)(\s*.\(.+(?P<unit_num>\d+)\):|)")
+    rx_iface_mac = re.compile(r"(?:Ethernet address is|HWaddr)\s+(?P<mac>\S+)")
+    rx_iface_mtu = re.compile(r"(Maximum Transfer Unit size is|MTU:)\s*(?P<mtu>\d+)")
+    rx_iface_ifindex = re.compile(r"ifindex:(?P<ifindex>\d+)")
+    rx_iface_l3_address1 = re.compile(
+        r"\s+inet\s+(?P<address>\S+)\s+"
+        r"mask\s+(?P<netmask>\S+)\s*"
+        r"(broadcast\s+(?P<broadcast>\S+))?",
+        re.MULTILINE,
+    )
+    rx_iface_l3_address2 = re.compile(
+        r"\s+Internet address:\s+(?P<address>\S+)\n"
+        r"(?:\s+Broadcast address:\s+(?P<broadcast>\S+)\n)?"
+        r"\s+Netmask\s+(?P<netmask>\S+)\s*Subnetmask\s+(?P<subnetmask>\S+)",
+        re.MULTILINE,
+    )
+    rx_l3_iface_splitter = rx_splitter = re.compile(r"^(?P<ifname>\S+\s+\(.+\):|\S+)", re.MULTILINE)
+
+    def get_l3_interfaces(self):
+        v = self.cli("IFSHOW")
         interfaces = {}
-        for board in boards:
-            if board["card_type"].startswith("ATUC"):
-                # xDSL cards
-                v = self.cli("GXTURPOP %d %d" % (shelf_num, board["slot"]))
-                for row in self.profile.parse_table(v):
-                    ifindex, port = row[:2]
-                    port = "%d/%d/%d" % (shelf_num, board["slot"], int(port))
-                    interfaces[port] = {
-                        "name": port,
-                        "type": "physical",
-                        "admin_status": True,
-                        "oper_status": True,
-                        "ifindex": ifindex,
-                        "subinterfaces": [],  # "enabled_afi": ["ATM"],
-                    }
+        ifname = None
+        for block in self.rx_l3_iface_splitter.split(v)[1:]:
+            if not block:
+                continue
+            match = self.rx_iface_name.match(block)
+            if match:
+                ifname = match.group("ifname")
+                if match.group("unit_num"):
+                    ifname += match.group("unit_num")
+                iftype = self.profile.get_interface_type(ifname)
+                interfaces[ifname] = {
+                    "name": ifname,
+                    "type": iftype,
+                    "admin_status": True,
+                    "oper_status": True,
+                    "subinterfaces": [{"name": ifname, "admin_status": True, "oper_status": True}],
+                }
+                continue
+            match = self.rx_iface_mac.search(block)
+            if match and match.group("mac") != "00:00:00:00:00:00":
+                interfaces[ifname]["mac"] = match.group("mac")
+            match = self.rx_iface_mtu.search(block)
+            if match:
+                interfaces[ifname]["mtu"] = match.group("mtu")
+            match = self.rx_iface_ifindex.search(block)
+            if match:
+                interfaces[ifname]["ifindex"] = match.group("ifindex")
+            l3_addresses = []
+            for l3 in self.rx_iface_l3_address1.finditer(block):
+                # format inet 224.0.0.1  mask 240.0.0.0
+                address = l3.group("address")
+                netmask = str(IPv4.netmask_to_len(l3.group("netmask")))
+                l3_addresses += ["%s/%s" % (address, netmask)]
+            for l3 in self.rx_iface_l3_address2.finditer(block):
+                # format      Internet address: 192.168.20.1
+                #      Broadcast address: 192.168.20.255
+                #      Netmask 0xffffff00 Subnetmask 0xffffff00
+                address = l3.group("address")
+                netmask = format(int(l3.group("netmask"), 16), "b").count("1")
+                l3_addresses += ["%s/%s" % (address, netmask)]
+            if l3_addresses:
+                interfaces[ifname]["subinterfaces"][0]["ipv4_addresses"] = l3_addresses
+                interfaces[ifname]["subinterfaces"][0]["enabled_afi"] = ["IPv4"]
+        return interfaces
+
+    def execute_snmp(self, **kwargs):
+        interfaces = {}
+        for oid, name in self.snmp.getnext(mib["IF-MIB::ifDescr"]):
+            ifindex = int(oid.split(".")[-1])
+            match = self.rx_snmp_iface.match(name)
+            if match:
+                ifname = "%(shelf)s/%(slot)s/%(port)s/%(mani)s" % match.groupdict()
+            else:
+                ifname = name
+            if ifname not in interfaces:
+                interfaces[ifname] = {
+                    "name": ifname,
+                    "type": "physical",
+                    "admin_status": True,
+                    "oper_status": True,
+                    "ifindex": ifindex,
+                    "subinterfaces": [
+                        {
+                            "name": "%s.%s:%s" % (ifname, 0, 0),
+                            "type": "physical",
+                            "admin_status": True,
+                            "oper_status": True,
+                            "enabled_afi": ["ATM"],
+                        }
+                    ],
+                }
+        interfaces.update(self.get_l3_interfaces())
+        return [{"interfaces": sorted(six.itervalues(interfaces), key=lambda x: x["name"])}]
+
+    def execute_cli(self, **kwargs):
+        # "EXISTSH ALL"
+        interfaces = {}
+        # boards = self.profile.get_boards(self)
+        try:
+            v = self.cli("ginv all", cached=True)
+        except self.CLISyntaxError:
+            raise NotImplementedError
+        for line in self.rx_table_spliter.split(v):
+            if not line.startswith("|"):
+                continue
+            row = [x.strip() for x in line.strip("|\n").split("|")]
+            if row[0] == "SH":
+                # header
+                continue
+            shelf, slot, port, mani, _, sw_ver, boot_ver, hw_ver, serial, _, _ = row
+            if int(port) == 0:
+                continue
+            # detail = self.cli("ginv %s %s %s %s" % (shelf, slot, port, mani))
+            ifname = "%s/%s/%s/%s" % (shelf, slot, port, mani)
+            interfaces[ifname] = {
+                "name": ifname,
+                "type": "physical",
+                "admin_status": True,
+                "oper_status": True,
+                # "ifindex": None,
+                "subinterfaces": [],  # "enabled_afi": ["ATM"],
+            }
+            interfaces[ifname]["subinterfaces"] += [
+                {
+                    "name": "%s.%s:%s" % (ifname, 0, 0),
+                    "type": "physical",
+                    "admin_status": True,
+                    "oper_status": True,
+                    "enabled_afi": ["ATM"],
+                    # "ifindex": None,
+                }
+            ]
+        interfaces.update(self.get_l3_interfaces())
         return [{"interfaces": sorted(six.itervalues(interfaces), key=lambda x: x["name"])}]
--- sa/profiles/ECI/HiFOCuS/get_version.py
+++ sa/profiles/ECI/HiFOCuS/get_version.py
@@ -6,27 +6,18 @@
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
-from noc.core.script.base import BaseScript
-from noc.sa.interfaces.igetversion import IGetVersion
+# Python modules
 import re
 
 
+# NOC modules
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetversion import IGetVersion
+
+
 class Script(BaseScript):
-    name = "ECI.HiFOCuS.get_version"
-    cache = True
-    interface = IGetVersion
-    reuse_cli_session = False
-    keep_cli_session = False
-
-    rx_platform = re.compile(r"\|\|\s+0\s+\|\|\s+(?P<platform>.+)\s*\n")
-
-    rx_ver = re.compile(
-        r"^\s*NI CARD TYPE\s+: (?P<cardtype>.+)\n" r"^\s*NI SW VERSION NAME\s+: (?P<version>.+)\n",
-        re.MULTILINE,
-    )
-
     """
-       shelfType:rs
+    shelfType:rs
     1-SAM_120
     2-SAM_240
     3-SAM_480
@@ -45,9 +36,54 @@ class Script(BaseScript):
     25-HF F52
     """
 
-    # def execute_snmp(self, **kwargs):
-    #     ".1.3.6.1.4.1.1286.1.3.3.1.1.2.131072"
-    #     pass
+    name = "ECI.HiFOCuS.get_version"
+    cache = True
+    interface = IGetVersion
+    reuse_cli_session = False
+    keep_cli_session = False
+    always_prefer = "S"
+
+    rx_platform = re.compile(r"\|\|\s+0\s+\|\|\s+(?P<platform>.+)\s*\n")
+
+    rx_ver = re.compile(
+        r"^\s*NI CARD TYPE\s+: (?P<cardtype>.+)\n" r"^\s*NI SW VERSION NAME\s+: (?P<version>.+)\n",
+        re.MULTILINE,
+    )
+
+    shelf_type_map = {
+        1: "SAM_120",
+        2: "SAM_240",
+        3: "SAM_480",
+        4: "FLEX_RAM",
+        7: "SAM_960",
+        8: "HF_M82",
+        11: "HF_M82",
+        12: "HF_M41",
+        14: "HF_M82C",
+        15: "HF_M61",
+        19: "SAM_960E",
+        20: "MINI_RAM_24V",
+        21: "GPOWER_RAM_24V",
+        22: "GPOWER_RAM_48V",
+        23: "HF F152",
+        24: "HF F61",
+        25: "HF F52",
+    }
+
+    def execute_snmp(self, **kwargs):
+        # Get shelf
+        r = self.snmp.getnext("1.3.6.1.4.1.1286.1.3.3.5.2.3.1.5", only_first=True)
+        oid, shelf_type = r[0]
+        # Use shelf type as platform
+        platform = self.shelf_type_map.get(int(shelf_type))
+        r = self.snmp.getnext("1.3.6.1.4.1.1286.1.3.3.1.1.2", only_first=True)
+        oid, version = r[0]
+        return {
+            "vendor": "ECI",
+            "platform": platform,
+            "version": version,
+            # "attributes": {"cardtype": cardtype},
+        }
 
     def execute_cli(self, **kwargs):
         try:
