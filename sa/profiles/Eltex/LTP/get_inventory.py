8978aea9fdf07a52558e37b611a1292236787beb
Andrey Vertiprahov <aversanta@gmail.com>
2019-12-03 10:21:09 +0500

    Fix flake.

--- sa/profiles/Eltex/LTP/get_inventory.py
+++ sa/profiles/Eltex/LTP/get_inventory.py
@@ -29,14 +29,14 @@ class Script(BaseScript):
 
     def execute_snmp(self, **kwargs):
         v = self.scripts.get_version()
-        r = [{"type": "CHASSIS", "vendor": "ELTEX", "part_no": v["platform"],}]
+        r = [{"type": "CHASSIS", "vendor": "ELTEX", "part_no": v["platform"]}]
         if "attributes" in v:
             r[-1]["serial"] = v["attributes"]["Serial Number"]
             r[-1]["revision"] = v["attributes"]["HW version"]
         pwr_num = self.snmp.get("1.3.6.1.4.1.35265.1.22.1.17.1.2.1")
         pwr_pn = self.snmp.get("1.3.6.1.4.1.35265.1.22.1.17.1.3.1")
         pwr_pn = pwr_pn.split()[0]
-        r += [{"type": "PWR", "vendor": "ELTEX", "part_no": pwr_pn, "number": pwr_num,}]
+        r += [{"type": "PWR", "vendor": "ELTEX", "part_no": pwr_pn, "number": pwr_num}]
         return r
 
     def execute_cli(self, **kwargs):
