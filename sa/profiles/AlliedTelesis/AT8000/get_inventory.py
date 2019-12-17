858ad8abafac7876b4ffa668bb9c3742039de523
bee26 <bee26@ya.ru>
2019-12-17 19:25:06 +0000

    fix_get_inventory1

---
+++ sa/profiles/AlliedTelesis/AT8000/get_inventory.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# AlliedTelesis.AT8000.get_inventory
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2019 The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+
+# NOC modules
+from noc.sa.profiles.Generic.get_inventory import Script as BaseScript
+from noc.sa.interfaces.igetinventory import IGetInventory
+
+
+class Script(BaseScript):
+    name = "AlliedTelesis.AT8000.get_inventory"
+    interface = IGetInventory
