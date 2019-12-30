f936165dae65251c3fe861485c0db7870949c49b
Andrey Vertiprahov <aversanta@gmail.com>
2019-12-30 17:03:29 +0000

    noc/noc#1088 Add ifindex and mac to confdb interface meta.

--- core/confdb/syntax/meta/interfaces.py
+++ core/confdb/syntax/meta/interfaces.py
@@ -16,6 +16,11 @@ from ..patterns import ANY, INTEGER
 INTERFACES_META_SYNTAX = DEF(
     "meta",
     [
+        DEF("mac", [DEF(INTEGER, name="mac", required=True, gen="make_interfaces_meta_mac")]),
+        DEF(
+            "ifindex",
+            [DEF(INTEGER, name="ifindex", required=True, gen="make_interfaces_meta_ifindex")],
+        ),
         DEF(
             "profile",
             [
