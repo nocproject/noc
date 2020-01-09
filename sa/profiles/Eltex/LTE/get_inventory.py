1d1c5d8c297f580242792fba6627ad623e1a0bc0
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2020-01-09 16:10:39 +0200

    Fix Eltex.LTE.get_inventory script

--- sa/profiles/Eltex/LTE/get_inventory.py
+++ sa/profiles/Eltex/LTE/get_inventory.py
@@ -2,24 +2,15 @@
 # ---------------------------------------------------------------------
 # Eltex.LTE.get_inventory
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2019 The NOC Project
+# Copyright (C) 2007-2020 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
 # NOC modules
-from noc.core.script.base import BaseScript
+from noc.sa.profiles.Generic.get_inventory import Script as BaseScript
 from noc.sa.interfaces.igetinventory import IGetInventory
 
 
 class Script(BaseScript):
     name = "Eltex.LTE.get_inventory"
     interface = IGetInventory
-    cache = True
-
-    def execute_cli(self):
-        v = self.scripts.get_version()
-        p = {"type": "CHASSIS", "vendor": "ELTEX", "part_no": v["platform"]}
-        if v.get("attributes", {}).get("Serial Number", ""):
-            p["serial"] = v["attributes"]["Serial Number"]
-
-        return [p]
