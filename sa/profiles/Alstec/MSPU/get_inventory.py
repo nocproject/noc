46818eee61a95d3fc55d8615742e879de4e2bbbb
kk <kk@buhaha.net>
2020-01-17 19:05:30 +0000

    fix Alstec, Eltex pon/rg, qsw8200, dlink get_inventory

--- sa/profiles/Alstec/MSPU/get_inventory.py
+++ sa/profiles/Alstec/MSPU/get_inventory.py
@@ -7,20 +7,10 @@
 # ---------------------------------------------------------------------
 
 # NOC modules
-from noc.core.script.base import BaseScript
+from noc.sa.profiles.Generic.get_inventory import Script as BaseScript
 from noc.sa.interfaces.igetinventory import IGetInventory
 
 
 class Script(BaseScript):
     name = "Alstec.MSPU.get_inventory"
-    cache = True
     interface = IGetInventory
-
-    def execute_cli(self):
-        v = self.scripts.get_version()
-        r = {"type": "CHASSIS", "vendor": "ALSTEC", "part_no": v["platform"]}
-        if "HW version" in v["attributes"]:
-            r["revision"] = v["attributes"]["HW version"]
-        if "Serial Number" in v["attributes"]:
-            r["serial"] = v["attributes"]["Serial Number"]
-        return [r]
--- sa/profiles/DLink/DxS_Industrial_CLI/get_inventory.py
+++ sa/profiles/DLink/DxS_Industrial_CLI/get_inventory.py
@@ -2,27 +2,15 @@
 # ---------------------------------------------------------------------
 # DLink.DxS_Industrial_CLI.get_inventory
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2018 The NOC Project
+# Copyright (C) 2007-2019 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
 # NOC modules
-from noc.core.script.base import BaseScript
+from noc.sa.profiles.Generic.get_inventory import Script as BaseScript
 from noc.sa.interfaces.igetinventory import IGetInventory
 
 
 class Script(BaseScript):
     name = "DLink.DxS_Industrial_CLI.get_inventory"
     interface = IGetInventory
-
-    def execute(self):
-        v = self.scripts.get_version()
-        p = {
-            "type": "CHASSIS",
-            "vendor": "DLINK",
-            "part_no": [v["platform"]],
-            "revision": v["attributes"]["HW version"],
-        }
-        if "Serial Number" in v["attributes"]:
-            p["serial"] = v["attributes"]["Serial Number"]
-        return [p]
