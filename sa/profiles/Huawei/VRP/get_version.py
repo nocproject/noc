5ef5bf0ba6f8b01d7364d8b7112da370feda2c69
Andrey Vertiprahov <aversanta@gmail.com>
2020-01-26 18:12:44 +0000

    Merge branch 'fix-avs-hua-get_version' into 'master'
    Huawei.VRP. Add S3328 to hw_series.
    
    See merge request noc/noc!3105
    
    (cherry picked from commit 679c887aa582242f7fb5d650ec8f44a93bb92456)
    
    9a6fe0fc Huawei.VRP. Add S3328 to hw_series.

--- sa/profiles/Huawei/VRP/get_version.py
+++ sa/profiles/Huawei/VRP/get_version.py
@@ -92,7 +92,7 @@ class Script(BaseScript):
     rx_hw_version = re.compile(r"HUAWEI\s(?P<platform>\S+)\s")
     rx_hw_extended_platform = re.compile(r"(?P<platform>\S+)[-,](CX|LS)\S+[-,].+")
     BAD_PLATFORM = ["", "Quidway S5600-HI"]
-    hw_series = {"S2300", "S5300"}
+    hw_series = {"S2300", "S5300", "S3328"}
 
     def fix_platform_name(self, platform):
         """
