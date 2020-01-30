5a500577b64a6d23f5752fdcc2cd98244a42ac5b
smile <sysfar@gmail.com>
2020-01-30 18:42:37 +0300

    Add iface threshold profile handler

---
+++ fm/handlers/thresholds/iface.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------
+#  Interface event handlers
+# ----------------------------------------------------------------------
+#  Copyright (C) 2007-2020 The NOC Project
+#  See LICENSE for details
+# ----------------------------------------------------------------------
+
+# Python modules
+import logging
+import datetime
+import time
+
+# NOC modules
+from noc.inv.models.interface import Interface
+from noc.pm.models.metrictype import MetricType
+from noc.core.pm.utils import get_interface_metrics
+
+logger = logging.getLogger(__name__)
+
+
+def humanize_speed(speed, type_speed):
+
+    d_speed = {
+        "bit/s": [(1000000000, "Gbit/s"), (1000000, "Mbit/s"), (1000, "kbit/s")],
+        "kbit/s": [(1000000, "Gbit/s"), (1000, "Mbit/s")],
+    }
+
+    if speed < 1000 and speed > 0:
+        return "%s " % speed
+    for t, n in d_speed.get(type_speed):
+        if speed >= t:
+            if speed // t * t == speed:
+                return "%d %s" % (speed // t, n)
+            else:
+                return "%.2f %s" % (float(speed) / t, n)
+
+
+def handler(mo, event):
+    threshold_interval = 60
+    # Date Time Block
+    from_date = datetime.datetime.now() - datetime.timedelta(hours=6)
+    date_limit = datetime.datetime.now() - datetime.timedelta(days=6)
+    # interval = (to_date - from_date).days
+    ts_from_date = time.mktime(from_date.timetuple())
+    ts_date_limit = time.mktime(date_limit.timetuple())
+    if ts_from_date < ts_date_limit:
+        ts_from_date = ts_date_limit
+    metric = MetricType.objects.get(name=event["metric"])
+    periodic_interval = mo.object.object_profile.periodic_discovery_interval
+    if event["window_type"] == "m":
+        threshold_interval = int(periodic_interval) * int(event["window"])
+    iface = Interface.objects.get(
+        name=event["path"].split("|")[-1::][0].strip(), managed_object=mo.object
+    )
+    try:
+        if iface:
+            event["interface"] = event["path"].split("|")[-1::][0].strip()
+            if iface.description:
+                event["description"] = str(iface.description)
+            event["threshold_interval"] = int(threshold_interval) / 60
+            event["ts_from_date"] = str(int(ts_from_date * 1000))
+            if "Load" in event["metric"]:
+                if iface.in_speed and iface.out_speed:
+                    if "In" in event["metric"]:
+                        iface_speed = iface.in_speed
+                    else:
+                        iface_speed = iface.out_speed
+                    event["percent"] = round((100.0 / int(iface_speed)) * ((event["value"]) / 1000))
+                event["convert_value"] = humanize_speed(event["value"], metric.measure)
+            if "Duplex" in event["metric"]:
+                if event["value"] != 2:
+                    logger.debug("Value %s is not True" % event["value"])
+                    return None
+            if "Status" in event["metric"]:
+                if "Admin" in event["metric"]:
+                    if iface.is_linked:
+                        linked_object = iface.link.interfaces
+                        if linked_object and len(linked_object) == 2:
+                            linked_object.remove(iface)
+                            event["linked_object"] = linked_object[0].managed_object.name
+                            event["linked_object_interface"] = linked_object[0].name
+                            event["linked_object_status"] = linked_object[
+                                0
+                            ].managed_object.get_status()
+                else:
+                    ifaces_metrics, last_ts = get_interface_metrics(mo.object)
+                    im = ifaces_metrics[mo.object][event["path"].split("|")[-1::][0].strip()]
+                    error_in = im.get("Interface | Errors | In")
+                    error_out = im.get("Interface | Errors | Out")
+                    if error_in is not None:
+                        event["error_in"] = error_in
+                    if error_out is not None:
+                        event["error_out"] = error_out
+                    if iface.is_linked:
+                        linked_object = iface.link.interfaces
+                        if linked_object and len(linked_object) == 2:
+                            linked_object.remove(iface)
+                            event["linked_object"] = linked_object[0].managed_object.name
+                            event["linked_object_interface"] = linked_object[0].name
+                            event["linked_object_status"] = linked_object[
+                                0
+                            ].managed_object.get_status()
+                            linked_ifaces_metrics, linked_last_ts = get_interface_metrics(
+                                linked_object[0].managed_object
+                            )
+                            if linked_ifaces_metrics:
+                                lim = linked_ifaces_metrics[linked_object[0].managed_object][
+                                    linked_object[0].name
+                                ]
+                                linked_error_in = lim.get("Interface | Errors | In")
+                                linked_error_out = lim.get("Interface | Errors | Out")
+                                if linked_error_in is not None:
+                                    event["linked_error_in"] = linked_error_in
+                                if linked_error_out is not None:
+                                    event["linked_error_out"] = linked_error_out
+            if "Errors" in event["metric"]:
+                event["current_value"] = event["value"] * (threshold_interval * 300.0)
+
+            return event
+    except Exception as e:
+        logger.info("Error: \n %s" % (event["path"].split("|")[-1::][0].strip(), e))
+        return event
