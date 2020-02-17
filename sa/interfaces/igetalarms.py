832ac12733e1b2c911bf41c42975598e6b5b6b61
Andrey Vertiprahov <aversanta@gmail.com>
2020-02-17 13:42:40 +0000

    noc/noc#1184 IGetAlarms SA interface.

---
+++ sa/interfaces/igetalarms.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# IGetAlarms - interface to query alarm device table
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2020 The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+
+# Python modules
+from __future__ import absolute_import
+from datetime import datetime
+
+# NOC modules
+from noc.core.interface.base import BaseInterface
+from .base import (
+    StringParameter,
+    DateTimeParameter,
+    StringListParameter,
+    DictParameter,
+    DictListParameter,
+)
+
+
+class IGetAlarms(BaseInterface):
+    """
+    * ts -
+    * id - local device alarm id, if empty - generate by hash on timestamp and path and vendor_code
+    * path - list component (chassis, interface, board) affected alarm. Example CPE - [<CPE_ID>]
+    * vendor_code - local AlarmClass on device
+    * message - Full alarm message
+    * attributes - additional sctructured vars on alarm
+    """
+
+    returns = DictListParameter(
+        attrs={
+            # Time message created
+            "ts": DateTimeParameter(default=datetime.now()),
+            # Message id
+            "id": StringParameter(),
+            # Component path to generated message (ex. chassis, slot, interface)
+            "path": StringListParameter(required=False),
+            # Message category (ex facility or tag for syslog)
+            "vendor_code": StringParameter(),
+            # Message full text
+            "message": StringParameter(),
+            # Additional attributes
+            "attributes": DictParameter(required=False),
+        }
+    )
