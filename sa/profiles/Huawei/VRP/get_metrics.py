0c06d6c386b45623fb7d10f15b11a58cc19c4d9e
Andrey Vertiprahov <aversanta@gmail.com>
2020-03-26 13:43:21 +0000

    Merge branch 'fix-avs-huawei' into 'master'
    Huawei. Disable CLI metrics.
    
    See merge request noc/noc!3337
    
    (cherry picked from commit 73dade62b7f9d8291a991a595a56e272ffb285e4)
    
    1d2abc73 Huawei. Disable CLI metrics.
    5df49372 Fix import.

--- sa/profiles/Huawei/VRP/get_metrics.py
+++ sa/profiles/Huawei/VRP/get_metrics.py
@@ -9,11 +9,8 @@
 # Python modules
 from __future__ import absolute_import
 
-# Third-party modules
-import six
-
 # NOC modules
-from noc.sa.profiles.Generic.get_metrics import Script as GetMetricsScript, metrics
+from noc.sa.profiles.Generic.get_metrics import Script as GetMetricsScript
 from .oidrules.slot import SlotRule
 from .oidrules.sslot import SSlotRule
 
@@ -23,19 +20,19 @@ class Script(GetMetricsScript):
 
     OID_RULES = [SlotRule, SSlotRule]
 
-    @metrics(
-        ["Interface | Errors | CRC", "Interface | Errors | Frame"],
-        has_capability="DB | Interfaces",
-        volatile=False,
-        access="C",  # CLI version
-    )
-    def get_vrp_interface_metrics(self, metrics):
-        v = self.cli("display interface")
-        ifdata = self.profile.parse_ifaces(v)
-        for iface, data in six.iteritems(ifdata):
-            iface = self.profile.convert_interface_name(iface)
-            ipath = ["", "", "", iface]
-            if "CRC" in data:
-                self.set_metric(id=("Interface | Errors | CRC", ipath), value=int(data["CRC"]))
-            if "Frames" in data:
-                self.set_metric(id=("Interface | Errors | Frame", ipath), value=int(data["Frames"]))
+    # @metrics(
+    #     ["Interface | Errors | CRC", "Interface | Errors | Frame"],
+    #     has_capability="DB | Interfaces",
+    #     volatile=False,
+    #     access="C",  # CLI version
+    # )
+    # def get_vrp_interface_metrics(self, metrics):
+    #     v = self.cli("display interface")
+    #     ifdata = self.profile.parse_ifaces(v)
+    #     for iface, data in six.iteritems(ifdata):
+    #         iface = self.profile.convert_interface_name(iface)
+    #         ipath = ["", "", "", iface]
+    #         if "CRC" in data:
+    #             self.set_metric(id=("Interface | Errors | CRC", ipath), value=int(data["CRC"]))
+    #         if "Frames" in data:
+    #             self.set_metric(id=("Interface | Errors | Frame", ipath), value=int(data["Frames"]))
