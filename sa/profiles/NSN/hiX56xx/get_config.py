504e3ff5f505659cb34b1b3a48fc67c4d9a59ab9
bee26 <bee26@ya.ru>
2020-03-31 18:59:05 +0100

    fix_get_config_NSN.hiX56xx

--- sa/profiles/NSN/hiX56xx/get_config.py
+++ sa/profiles/NSN/hiX56xx/get_config.py
@@ -3,7 +3,7 @@
 # NSN.hiX56xx.get_config
 # sergey.sadovnikov@gmail.com
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2019 The NOC Project
+# Copyright (C) 2007-2020 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 """
@@ -16,7 +16,11 @@ class Script(BaseScript):
     name = "NSN.hiX56xx.get_config"
     interface = IGetConfig
 
-    def execute_cli(self, **kwargs):
-        config = self.cli("show running-config")
-        config = self.strip_first_lines(config, 3)
+    def execute_cli(self, policy="r", **kwargs):
+        assert policy in ("r", "s")
+        if policy == "s":
+            config = self.cli("show startup-config")
+        else:
+            config = self.cli("show running-config")
+        config = self.strip_first_lines(config, 2)
         return self.cleaned_config(config)
