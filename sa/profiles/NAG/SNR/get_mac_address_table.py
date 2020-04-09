d8b8a5160e3b296bc6dbc3abb2101dc46a6e2569
Dmitry Volodin <dvolodin7@gmail.com>
2020-04-09 10:41:31 +0000

    Merge branch 'fix-avs-mac-address-table' into 'master'
    Generic.get_mac_address_table. Fix getting mac address tables by SNMP.
    
    See merge request noc/noc!3420
    
    (cherry picked from commit 9cf55aea8e9483c2bfc1fb154dc140e62d922c42)
    
    75e4bed3 Generic.get_mac_address_table. Fix getting mac address tables by SNMP.
    be7db4d6 Fix black.

--- sa/profiles/NAG/SNR/get_mac_address_table.py
+++ sa/profiles/NAG/SNR/get_mac_address_table.py
@@ -8,59 +8,10 @@
 
 # Python modules
 # NOC modules
-from noc.core.script.base import BaseScript
+from noc.sa.profiles.Generic.get_mac_address_table import Script as BaseScript
 from noc.sa.interfaces.igetmacaddresstable import IGetMACAddressTable
 
 
 class Script(BaseScript):
     name = "NAG.SNR.get_mac_address_table"
     interface = IGetMACAddressTable
-    cached = True
-
-    def execute_snmp(self, interface=None, vlan=None, mac=None):
-        r = []
-        vlan_oid = []
-        if mac is not None:
-            mac = mac.lower()
-        for v in self.snmp.get_tables(["1.3.6.1.2.1.17.7.1.2.2.1.2"], bulk=True):
-            vlan_oid.append(v[0])
-        # mac iface type
-        for v in self.snmp.get_tables(
-            ["1.3.6.1.2.1.17.4.3.1.1", "1.3.6.1.2.1.17.4.3.1.2", "1.3.6.1.2.1.17.4.3.1.3"],
-            bulk=True,
-        ):
-            if v[1]:
-                chassis = ":".join(["%02x" % ord(c) for c in v[1]])
-                if mac is not None:
-                    if chassis == mac:
-                        pass
-                    else:
-                        continue
-            else:
-                continue
-            if int(v[3]) > 3 or int(v[3]) < 1:
-                continue
-            iface = self.snmp.get("1.3.6.1.2.1.31.1.1.1.1." + str(v[2]), cached=True)  # IF-MIB
-            if interface is not None:
-                if iface == interface:
-                    pass
-                else:
-                    continue
-            for i in vlan_oid:
-                if v[0] in i:
-                    vlan_id = int(i.split(".")[0])
-                    break
-            if vlan is not None:
-                if vlan_id == vlan:
-                    pass
-                else:
-                    continue
-            r += [
-                {
-                    "interfaces": [iface],
-                    "mac": chassis,
-                    "type": {"3": "D", "2": "S", "1": "S"}[str(v[3])],
-                    "vlan_id": vlan_id,
-                }
-            ]
-        return r
