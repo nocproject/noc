056a0e0c2e5104c5c15a97f043f8ab68433571b6
MaksimSmile13 <sysfar@gmail.com>
2020-04-09 15:47:43 +0000

    Merge branch 'update/profile_eltex_mes5448' into 'master'
    Eltex.MES5448 update profile
    
    See merge request noc/noc!3393
    
    (cherry picked from commit 1b68303e74de7073e4cc6b13e94e123e8f8b9a0a)
    
    53cdc726 Eltex.MES5448 update prifile
    de7cc6ae Black fix
    818af583 fix Flake8
    a8b6c42c fix year
    c438bca0 Add sensor status and temperature metrics

--- sa/profiles/Eltex/MES5448/profile.py
+++ sa/profiles/Eltex/MES5448/profile.py
@@ -3,12 +3,25 @@
 # Vendor: Eltex
 # OS:     MES5448
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2019 The NOC Project
+# Copyright (C) 2007-2020 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
+import re
 
 # NOC modules
 from noc.core.profile.base import BaseProfile
+from noc.core.comp import smart_text
+
+rx_cpu = re.compile(
+    r"5\s+Secs\s+\((?:\s+|)(?P<cpu5sec>\S+)\%\)\s+60\s+\Secs\s+\((?:\s+|)(?P<cpu60sec>\S+)\%\)\s+300\s+\Secs\s+\((?:\s+|)(?P<cpu300sec>\S+)\%\)"
+)
+
+
+def render_regexp(oid, value):
+    value = smart_text(value, errors="ignore")
+    match = rx_cpu.search(value)
+    if match:
+        return int(float(match.group("cpu60sec")))
 
 
 class Profile(BaseProfile):
@@ -23,3 +36,4 @@ class Profile(BaseProfile):
     )
     command_super = "enable"
     command_disable_pager = "terminal length 0"
+    snmp_display_hints = {"1.3.6.1.4.1.4413.1.1.1.1.4.9.0": render_regexp}
