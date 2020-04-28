3f4aac1f8ba2c5222807d1f9891188698ca067c1
Your Name <aversanta@gmail.com>
2020-04-28 20:51:37 +0500

    Add to Service to on_delete_check.

--- crm/models/subscriber.py
+++ crm/models/subscriber.py
@@ -25,11 +25,13 @@ from noc.wf.models.state import State
 from noc.project.models.project import Project
 from noc.core.wf.decorator import workflow
 from noc.core.bi.decorator import bi_sync
+from noc.core.model.decorator import on_delete_check
 
 id_lock = Lock()
 
 
 @bi_sync
+@on_delete_check(check=[("sa.Service", "subscriber")])
 @workflow
 @six.python_2_unicode_compatible
 class Subscriber(Document):
