341cfcd6a4c044951024429a625ef9b4bd4c4ae2
Andrey Vertiprahov <aversanta@gmail.com>
2020-04-28 16:31:24 +0000

    Merge branch 'patch-add-poo-toreport-poison' into 'master'
    Add pool to reportdiscoverypoison.
    
    See merge request noc/noc!3560
    
    (cherry picked from commit 0b96009d42074719e1d8f55c3f8942bb30e736eb)
    
    b6e9671a Add pool to reportdiscoverypoison.
    95678df2 Fix typo.

--- services/web/apps/inv/reportdiscoverypoison/views.py
+++ services/web/apps/inv/reportdiscoverypoison/views.py
@@ -6,17 +6,31 @@
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
+# Third-party modules
+from django import forms
+
 # NOC modules
 from noc.lib.app.simplereport import SimpleReport, SectionRow
 from noc.inv.models.discoveryid import DiscoveryID
 from noc.sa.models.managedobject import ManagedObject
+from noc.main.models.pool import Pool
 from noc.core.translation import ugettext as _
+
+
+class ReportForm(forms.Form):
+    pool = forms.ChoiceField(
+        label=_("Pool"),
+        required=False,
+        initial=None,
+        choices=[(x, x) for x in Pool.objects.order_by("name").scalar("name")] + [(None, "-")],
+    )
 
 
 class ReportDiscoveryIDPoisonApplication(SimpleReport):
     title = _("Discovery ID cache poison")
+    form = ReportForm
 
-    def get_data(self, request, **kwargs):
+    def get_data(self, request, pool=None, **kwargs):
 
         data = []
         # Find object with equal ID
@@ -30,6 +44,7 @@ class ReportDiscoveryIDPoisonApplication(SimpleReport):
                 # Empty DiscoveryID
                 continue
             data_c = []
+            pool_c = set()
             reason = "Other"
 
             for r in DiscoveryID._get_collection().find(
@@ -40,16 +55,17 @@ class ReportDiscoveryIDPoisonApplication(SimpleReport):
                 if len(data_c) > 0:
                     if mo.address == data_c[-1][1]:
                         reason = _("Duplicate MO")
-                    elif not mo.is_managed == data_c[-1][3]:
+                    elif not mo.is_managed == data_c[-1][4]:
                         reason = _("MO is move")
-
-                data_c += [(mo.name, mo.address, mo.profile.name, mo.is_managed)]
-
+                pool_c.add(mo.pool.name)
+                data_c.append((mo.name, mo.address, mo.profile.name, mo.pool.name, mo.is_managed))
+            if pool and pool not in pool_c:
+                continue
             data += [SectionRow(name="%s %s" % (f["_id"][0], reason))]
             data += data_c
 
         return self.from_dataset(
             title=self.title,
-            columns=[_("Managed Object"), _("Address"), _("Profile"), _("is managed")],
+            columns=[_("Managed Object"), _("Address"), _("Profile"), _("Pool"), _("is managed")],
             data=data,
         )
