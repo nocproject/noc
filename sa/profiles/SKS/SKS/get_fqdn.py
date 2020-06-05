f2dfc8aefe7fe7f3a2d92b3331c447c843ced7d5
Andrey Vertiprahov <aversanta@gmail.com>
2020-06-05 18:05:02 +0500

    Add get_fqdn to profiles.

---
+++ sa/profiles/SKS/SKS/get_fqdn.py
@@ -0,0 +1,17 @@
+# ---------------------------------------------------------------------
+# SKS.SKS.get_fqdn
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2020 The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+
+# NOC modules
+from noc.sa.profiles.Generic.get_fqdn import Script as BaseScript
+from noc.sa.interfaces.igetfqdn import IGetFQDN
+
+
+class Script(BaseScript):
+    name = "SKS.SKS.get_fqdn"
+    interface = IGetFQDN
+
+    always_prefer = "S"
