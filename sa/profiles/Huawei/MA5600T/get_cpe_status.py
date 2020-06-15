ab6d971db4d877470c1982bbdad2f50715a67211
Your Name <aversanta@gmail.com>
2020-06-15 18:03:36 +0500

    Huawei.MA5600T.get_cpe_status. Fix decode serial.

--- sa/profiles/Huawei/MA5600T/get_cpe_status.py
+++ sa/profiles/Huawei/MA5600T/get_cpe_status.py
@@ -14,6 +14,8 @@ from noc.core.script.base import BaseScript
 from noc.sa.interfaces.igetcpe import IGetCPE
 from noc.core.text import parse_table_header
 from noc.core.mib import mib
+from noc.core.snmp.render import render_bin
+from noc.core.comp import smart_text
 
 
 class Script(BaseScript):
@@ -108,7 +110,9 @@ class Script(BaseScript):
             [
                 mib["HUAWEI-XPON-MIB::hwGponDeviceOntSn"],
                 mib["HUAWEI-XPON-MIB::hwGponDeviceOntDespt"],
-            ]
+            ],
+            bulk=False,
+            display_hints={mib["HUAWEI-XPON-MIB::hwGponDeviceOntSn"]: render_bin},
         ):
             ifindex, ont_id = ont_index.split(".")
             ont_id = "%s/%s" % (names[int(ifindex)], ont_id)
@@ -116,9 +120,9 @@ class Script(BaseScript):
                 "interface": names[int(ifindex)],
                 "status": "inactive",
                 "id": ont_id,
-                "global_id": codecs.encode(ont_serial, "hex").upper(),
+                "global_id": smart_text(codecs.encode(ont_serial, "hex")).upper(),
                 "type": "ont",
-                "serial": codecs.encode(ont_serial, "hex").upper(),
+                "serial": smart_text(codecs.encode(ont_serial, "hex")).upper(),
                 "description": ont_descr,
                 "location": "",
             }
