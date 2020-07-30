f9b2365815af69c5248f6ef34d2ea85217609073
uncol <d.a.zakharov@gmail.com>
2020-07-30 08:50:31 +0300

    combobox fix

--- ui/web/fm/alarm/view/grids/Lookup.js
+++ ui/web/fm/alarm/view/grids/Lookup.js
@@ -73,6 +73,9 @@ Ext.define("NOC.fm.alarm.view.grids.Lookup", {
     },
     initComponent: function() {
         this.store.proxy.url = this.url;
+        // Fix combobox with remote paging
+        this.pickerId = me.getId() + '_picker';
+        // end
         this.callParent();
     }
 });
