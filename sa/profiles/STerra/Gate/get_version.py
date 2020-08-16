a9ebbb493c2cc316d9c1ff4ae481f123944120bd
MaksimSmile13 <sysfar@gmail.com>
2020-08-16 13:03:41 +0000

    Merge branch 'update/kripto_metrics' into 'master'
    Add metrics for Gate.
    
    See merge request noc/noc!4022
    
    (cherry picked from commit 73361ef3213506dbeb95996792b44ec0834f72ea)
    
    abc9181f Add metrics for Gate.
    e73e799a Add VPN IPSec ddash.
    f64b1fde fix black

--- sa/profiles/STerra/Gate/get_version.py
+++ sa/profiles/STerra/Gate/get_version.py
@@ -17,13 +17,21 @@ class Script(BaseScript):
     name = "STerra.Gate.get_version"
     cache = True
     interface = IGetVersion
-    rx_ver = re.compile(r"(?P<ver>\d.\d.\d\S+\s#\d\s\S.+)", re.MULTILINE)
+    rx_ver = re.compile(r"(?P<version>\d.\d.\d\S+\s#\d\s\S.+)", re.MULTILINE)
+    rx_ver42 = re.compile(
+        r"S-Terra\s(?P<platform>\S+\s\d.\d.\d+),\s.+, Version (?P<version>\S+), ", re.MULTILINE
+    )
 
     def execute_snmp(self):
         version = "Unknown"
-        s = self.snmp.get("1.3.6.1.2.1.1.1.0")
+        platform = "Gate"
+        s = self.snmp.get("1.3.6.1.4.1.9.9.25.1.1.1.2.7")
         match = self.rx_ver.search(s)
+        match42 = self.rx_ver42.search(s)
         if match:
-            version = match.group("ver")
-        result = {"vendor": "STerra", "version": version, "platform": "Gate"}
+            version = match.group("version")
+        if match42:
+            version = match42.group("version")
+            platform = match42.group("platform")
+        result = {"vendor": "STerra", "version": version, "platform": platform}
         return result
