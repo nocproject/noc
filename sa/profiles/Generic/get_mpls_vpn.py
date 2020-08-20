456c5ef4bb56187f4c270babd8146046afeef8ed
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2020-08-20 11:28:47 +0100

    Fix Generic.get_mpls_vpn script

--- sa/profiles/Generic/get_mpls_vpn.py
+++ sa/profiles/Generic/get_mpls_vpn.py
@@ -24,7 +24,10 @@ class Script(BaseScript):
     VRF_TYPE_MAP = {"rt_export": {"2", "3"}, "rt_import": {"1", "3"}}
 
     def execute_snmp(self):
-        names = {x: y for y, x in self.scripts.get_ifindexes().items()}
+        names = {
+            x["ifindex"]: x["interface"]
+            for x in self.scripts.get_interface_properties(enable_ifindex=True)
+        }
         r = {}
         for conf_id, vrf_vpn_id, vrf_rd, vrf_descr, vrf_oper in self.snmp.get_tables(
             [
@@ -50,7 +53,22 @@ class Script(BaseScript):
             [mib["MPLS-L3VPN-STD-MIB::mplsL3VpnIfConfRowStatus"]]
         ):
             conf_id, ifindex = conf_id.rsplit(".", 1)
-            r[conf_id]["interfaces"] += [names[int(ifindex)]]
+            if int(ifindex) in names:
+                """
+                Some Junipers vendor, may hide interfaces from ifTable output:
+                IF-MIB::ifIndex.4088 = INTEGER: 4088
+                IF-MIB::ifIndex.4089 = INTEGER: 4089
+                IF-MIB::ifIndex.4091 = INTEGER: 4091
+                IF-MIB::ifIndex.4092 = INTEGER: 4092
+                IF-MIB::ifIndex.4094 = INTEGER: 4094
+                IF-MIB::ifIndex.4095 = INTEGER: 4095
+                IF-MIB::ifIndex.4096 = INTEGER: 4096
+                """
+                r[conf_id]["interfaces"] += [names[int(ifindex)]]
+            else:
+                self.logger.warning(
+                    "Hidden from ifTable interface (w ifindex %s) will be skipped", ifindex
+                )
         for conf_id, vrf_rt, vrf_rt_decr in self.snmp.get_tables(
             [
                 mib["MPLS-L3VPN-STD-MIB::mplsL3VpnVrfRT"],
