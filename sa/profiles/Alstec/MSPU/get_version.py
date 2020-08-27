1dde9eaf367d6cb902df82a7f0ca5ec5945265af
Your Name <aversanta@gmail.com>
2020-08-27 19:07:22 +0500

    Alstec.MSPU.get_version. Fix HappyBaby platform regex.

--- sa/profiles/Alstec/MSPU/get_version.py
+++ sa/profiles/Alstec/MSPU/get_version.py
@@ -22,18 +22,23 @@ class Script(BaseScript):
         r"Chip\s\d:\sHW\sVer\s*(?P<hw_ver>\S+)\s*FW\sVer\s(?P<sw_ver>\S+)", re.IGNORECASE
     )
     rx_ver2 = re.compile(r"^software v\.(?P<version>\S+) ", re.MULTILINE)
+    rx_ver3 = re.compile(r"Build:\s+MKC-IP\s+(?P<platform>\S+)\s+(?P<version>\S+\s+\(\S+\))")
 
     def execute_cli(self):
+        platform = "MSPU"
         try:
             c = self.cli("context dslam version ", cached=True)
             match = self.rx_ver1.search(c)
             return {
                 "vendor": "Alstec",
-                "platform": "MSPU",
+                "platform": platform,
                 "version": match.group("sw_ver"),
                 "attributes": {"HW version": match.group("hw_ver")},
             }
         except self.CLISyntaxError:
             c = self.cli("version", cached=True)
             match = self.rx_ver2.search(c)
-            return {"vendor": "Alstec", "platform": "MSPU", "version": match.group("version")}
+            if not match:
+                match = self.rx_ver3.search(c)
+                platform = match.group("platform")
+            return {"vendor": "Alstec", "platform": platform, "version": match.group("version")}
