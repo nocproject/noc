a18997820ee81d5f966eab63beee43c147584f7a
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2020-09-09 08:08:59 +0100

    Add Enterasys.EOS profile

---
+++ sa/profiles/Enterasys/EOS/profile.py
@@ -0,0 +1,14 @@
+# ---------------------------------------------------------------------
+# Vendor: Enterasys
+# OS:     EOS
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2020 The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+
+# NOC modules
+from noc.core.profile.base import BaseProfile
+
+
+class Profile(BaseProfile):
+    name = "Enterasys.EOS"
