a03eb6b696cf3303da75e0e07cf31dbd72b685f0
Andrey Vertiprahov <aversanta@gmail.com>
2020-09-10 14:45:08 +0000

    Merge branch 'noc-fix-confdb-and-org' into 'master'
    noc/noc#1335 ConfDB: Fix `and` inside `or` combination
    
    See merge request noc/noc!4303
    
    (cherry picked from commit 971bac5dd35797e8636e15df8828517f2c7a5e93)
    
    ffcb80a0 ConfDB: Fix `and` inside `or` combination

---
+++ tests/confdb/engine/test_or.py
@@ -0,0 +1,29 @@
+# ----------------------------------------------------------------------
+# Test engine's `or` operator
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2020 The NOC Project
+# See LICENSE for details
+# ----------------------------------------------------------------------
+
+# Third-party modules
+import pytest
+
+# NOC modules
+from noc.core.confdb.engine.base import Engine
+
+
+@pytest.mark.parametrize(
+    "input,query,output",
+    [
+        ({}, "False() or True()", [{}]),
+        ({}, "(False() and False()) or True()", [{}]),
+        ({}, "(True() and True()) or True()", [{}]),
+        ({}, "(False() and False()) or False()", []),
+        ({}, "(True() and True()) or False()", [{}]),
+        ({}, "(True() and True() and True()) or False()", [{}]),
+        ({}, "(True() and True() and True()) or True()", [{}]),
+    ],
+)
+def test_org(input, query, output):
+    e = Engine()
+    assert list(e.query(query, **input)) == output
