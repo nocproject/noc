ab52ec450aa19b4bee8edcfc47e65fa7aa444def
Your Name <aversanta@gmail.com>
2020-09-22 20:54:52 +0500

    DLink.DxS.get_metrics. Fix SNMP Error when 'CPU | Usage' metric.

--- sa/profiles/DLink/DxS/get_metrics.py
+++ sa/profiles/DLink/DxS/get_metrics.py
@@ -16,13 +16,17 @@ class Script(GetMetricsScript):
     @metrics(["CPU | Usage"], volatile=False, access="S")
     def get_cpu_metrics(self, metrics):
         # DLINK-AGENT-MIB::agentCPUutilizationIn5sec
-        cpu = self.snmp.get("1.3.6.1.4.1.171.12.1.1.6.1.0")
-        if cpu is None:
-            v = self.snmp.get(mib["SNMPv2-MIB::sysDescr", 0], cached=True)
-            if v.startswith("DES-3200"):  # need testing
-                cpu = self.snmp.get("1.3.6.1.4.1.171.12.1.1.6.1")
-            elif v.startswith("DGS-3212SR") or v.startswith("DGS-3312SR"):
-                cpu = self.snmp.get("1.3.6.1.4.1.171.11.55.2.2.1.4.1.0")
+        cpu = None
+        try:
+            cpu = self.snmp.get("1.3.6.1.4.1.171.12.1.1.6.1.0")
+            if cpu is None:
+                v = self.snmp.get(mib["SNMPv2-MIB::sysDescr", 0], cached=True)
+                if v.startswith("DES-3200"):  # need testing
+                    cpu = self.snmp.get("1.3.6.1.4.1.171.12.1.1.6.1")
+                elif v.startswith("DGS-3212SR") or v.startswith("DGS-3312SR"):
+                    cpu = self.snmp.get("1.3.6.1.4.1.171.11.55.2.2.1.4.1.0")
+        except Exception:
+            pass
 
         if cpu is not None:
             self.set_metric(id=("CPU | Usage", None), value=round(float(cpu)))
