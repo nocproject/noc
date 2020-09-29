5fc52592c3b1b008bdc5eaef1866a886db44dc11
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2020-09-29 14:16:37 +0000

    Merge branch 'fix_get_lldp_neighbors_NSN.TIMOS' into 'master'
    fix_get_lldp_neighbors_NSN.TIMOS
    
    See merge request noc/noc!4376
    
    (cherry picked from commit e8c7ed046df017907c8dd17929f06a2e6716ddb2)
    
    c58a2b15 fix_get_lldp_neighbors_NSN.TIMOS
    ec2dac96 Update sa/profiles/NSN/TIMOS/get_capabilities.py
    d2a539bd Update get_lldp_neighbors.py
    9777706b Update get_capabilities.py
    9c6cc8d0 Update sa/profiles/NSN/TIMOS/get_interfaces.py
    34987a55 Update get_interfaces.py
    64d1b45a Update get_interfaces.py
    d739e71f Update get_lldp_neighbors.py

---
+++ sa/profiles/NSN/TIMOS/get_ifindexes.py
@@ -0,0 +1,17 @@
+# ---------------------------------------------------------------------
+# NSN.TIMOS.get_ifindexes
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2020 The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+
+# NOC modules
+from noc.sa.profiles.Generic.get_ifindexes import Script as BaseScript
+from noc.sa.interfaces.igetifindexes import IGetIfindexes
+
+
+class Script(BaseScript):
+    name = "NSN.TIMOS.get_ifindexes"
+    interface = IGetIfindexes
+
+    INTERFACE_NAME_OID = "IF-MIB::ifName"
