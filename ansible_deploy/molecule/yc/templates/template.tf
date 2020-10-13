46b8f01863b675c49bec35024782d6a95e6cc37b
EKbfh <earthquake_90@mail.ru>
2020-10-13 14:00:09 +0300

    test add

---
+++ ansible_deploy/molecule/yc/templates/template.tf
@@ -0,0 +1,54 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "terraform-providers/yandex"
+    }
+  }
+}
+
+provider "yandex" {
+  token     = "{{ molecule_yml.driver.token }}"
+  cloud_id  = "{{ molecule_yml.driver.cloud_id }}"
+  folder_id = "{{ molecule_yml.driver.folder_id }}"
+  zone      = "{{ molecule_yml.driver.zone }}"
+}
+
+resource "yandex_compute_instance" "vm-1" {
+  name = "noc-node-{{ item.distr }}-{{ lookup('env','CI_JOB_ID') | default(60|random,true) }}"
+
+  resources {
+    cores         = 4
+    memory        = 4
+    core_fraction = 100
+  }
+
+  platform_id = "standard-v2"
+
+  scheduling_policy {
+    preemptible = true
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "{{ molecule_yml.driver.image_id }}"
+      size     = 20
+    }
+  }
+
+  network_interface {
+    subnet_id = "{{ molecule_yml.driver.subnet_id }}"
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "{{ item.ssh_user }}:${file("{{ molecule_yml.driver.ssh_identity_file_pub }}")}",
+    serial-port-enable = 1  }
+}
+
+output "internal_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface[0].ip_address
+}
+
+output "external_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface[0].nat_ip_address
+}
