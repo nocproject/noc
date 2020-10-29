9ade2926e1a345a0eefc6718125f6c8c74891235
Andrey Vertiprahov <aversanta@gmail.com>
2020-10-29 18:12:05 +0000

    Merge branch 'fix-H3C_get_portchannel' into 'master'
    fix for H3C.VRP.get_portchannel script
    
    See merge request noc/noc!4532
    
    (cherry picked from commit 6e6274e69253773a1d37a7668dd8c44630295d88)
    
    0a561fc0 fix for H3C.VRP.get_portchannel script

--- sa/profiles/H3C/VRP/get_portchannel.py
+++ sa/profiles/H3C/VRP/get_portchannel.py
@@ -1,9 +1,13 @@
+# -*- coding: utf-8 -*-
 # ---------------------------------------------------------------------
 # H3C.VRP.get_portchannel
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2018 The NOC Project
+# Copyright (C) 2007-2020 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
+
+# PYTHON modules
+import re
 
 # NOC modules
 from noc.core.script.base import BaseScript
@@ -14,11 +18,35 @@ class Script(BaseScript):
     name = "H3C.VRP.get_portchannel"
     interface = IGetPortchannel
 
+    rx_po_members = re.compile(
+        r"^(?P<interface>\S+):\s*\n"
+        r"^Aggregation Interface: (?P<agg_interface>\S+)\n"
+        r"^(?P<is_dynamic>Local:)?",
+        re.MULTILINE,
+    )
+
     def execute(self):
+        r, s = [], ""
         try:
-            r = self.cli("display link-aggregation summary")
-            if "No link-aggregation" in r:
+            s = self.cli("display link-aggregation member-port")
+            if "No link-aggregation" in r or "No link aggregation" in r:
                 return []
         except self.CLISyntaxError:
             return []
-        raise self.NotSupportedError()
+
+        for match in self.rx_po_members.finditer(s):
+            found = False
+            for i in r:
+                if i["interface"] == match.group("agg_interface"):
+                    i["members"] += [match.group("interface")]
+                    found = True
+                    break
+            if not found:
+                r += [
+                    {
+                        "interface": match.group("agg_interface"),
+                        "type": "L" if match.group("is_dynamic") else "S",
+                        "members": [match.group("interface")],
+                    }
+                ]
+        return r
