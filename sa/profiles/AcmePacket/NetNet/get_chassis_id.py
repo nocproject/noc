cfebd6511ab0a775598fb9dd8356f853cb15f9b4
MaksimSmile13 <sysfar@gmail.com>
2020-11-02 08:24:26 +0000

    Merge branch 'patch-avs-acmepak-metrics' into 'master'
    Add AcmePacket.NetNet profile.
    
    See merge request noc/noc!4406
    
    (cherry picked from commit e28dc9bdba99131f0877421e334fbe8fb6c4d4ed)
    
    67c933d5 Add AcmePacket.NetNet profile.
    1643ac5e Fix get_version name.
    ea9cbfcd Rename Telephony scope to PhonePeer.
    6d5e0f4e Fix typo.
    b43156c6 Remove PhonePeer MetricScope.

---
+++ sa/profiles/AcmePacket/NetNet/get_chassis_id.py
@@ -0,0 +1,18 @@
+# ----------------------------------------------------------------------
+# AcmePacket.NetNet.get_chassis_id
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2020 The NOC Project
+# See LICENSE for details
+# ----------------------------------------------------------------------
+
+# NOC modules
+from noc.sa.profiles.Generic.get_chassis_id import Script as BaseScript
+from noc.sa.interfaces.igetchassisid import IGetChassisID
+from noc.core.mib import mib
+
+
+class Script(BaseScript):
+    name = "AcmePacket.NetNet.get_chassis_id"
+    interface = IGetChassisID
+
+    SNMP_GETNEXT_OIDS = {"SNMP": [mib["IF-MIB::ifPhysAddress"]]}
---
+++ sa/profiles/AcmePacket/NetNet/get_fqdn.py
@@ -0,0 +1,15 @@
+# ---------------------------------------------------------------------
+# AcmePacket.NetNet.get_fqdn
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2020 The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+
+# NOC modules
+from noc.sa.profiles.Generic.get_fqdn import Script as BaseScript
+from noc.sa.interfaces.igetfqdn import IGetFQDN
+
+
+class Script(BaseScript):
+    name = "AcmePacket.NetNet.get_fqdn"
+    interface = IGetFQDN
