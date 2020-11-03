edc6ef4b781dbddd88e03b68c5d8a424be0cde8e
Your Name <aversanta@gmail.com>
2020-11-03 19:37:49 +0500

    Add schedule_discovery_config handler to events.discovery.

--- fm/handlers/event/discovery.py
+++ fm/handlers/event/discovery.py
@@ -7,6 +7,7 @@
 
 # NOC modules
 from noc.config import config
+from noc.core.defer import call_later
 
 DELAY = config.correlator.discovery_delay
 
@@ -16,3 +17,16 @@ def schedule_discovery(event):
     Reschedule discovery processes
     """
     event.managed_object.run_discovery(DELAY)
+
+
+def schedule_discovery_config(event):
+    """
+    Reschedule discovery config processes
+    """
+    call_later(
+        event.managed_object.id,
+        job_class=event.managed_object.BOX_DISCOVERY_JOB,
+        _checks=["config"],
+        max_runs=1,
+        delay=DELAY,
+    )
