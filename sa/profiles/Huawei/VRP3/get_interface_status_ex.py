5cd879a2fceeb209e0c2699474a3a80285e794bd
Andrey Vertiprahov <aversanta@gmail.com>
2020-11-08 13:00:46 +0000

    Merge branch 'fix-vr3-snmp-timeout-trace' into 'master'
    Huawei.VRP3.get_interface_status_ex. Fix trace when SNMP Timeout.
    
    See merge request noc/noc!4610
    
    (cherry picked from commit db2bc259f734298d015d3a2c296057623ddaa599)
    
    e44c907a Huawei.VRP3.get_interface_status_ex. Fix trace when SNMP Timeout.

--- sa/profiles/Huawei/VRP3/get_interface_status_ex.py
+++ sa/profiles/Huawei/VRP3/get_interface_status_ex.py
@@ -14,11 +14,15 @@ class Script(BaseScript):
     name = "Huawei.VRP3.get_interface_status_ex"
     interface = IGetInterfaceStatusEx
 
+    MAX_REPETITIONS = 10
+
     def execute_snmp(self, interfaces=None, **kwargs):
         r = self.get_data(interfaces=interfaces, raw_speed_value=True)
         for x in r:
             # ADSL port return value as kb/sec
-            if x["interface"].startswith("FE"):
+            # For SNMP Timeout we getting interfaces only list
+            if x["interface"].startswith("FE") and "out_speed" in x:
                 x["out_speed"] = x["out_speed"] // 1000
+            if x["interface"].startswith("FE") and "in_speed" in x:
                 x["in_speed"] = x["in_speed"] // 1000
         return r
