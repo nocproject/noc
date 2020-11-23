33348c029ce702934fd4eef8d30684c00f1177fc
Andrey Vertiprahov <aversanta@gmail.com>
2020-11-23 15:25:54 +0000

    Merge branch 'fix-avs-dlink-ifspeed' into 'master'
    DLink.DxS.get_metrics. Add 'Interface | Speed' metric to script.
    
    See merge request noc/noc!4713
    
    (cherry picked from commit 103831c357c9b4df2131427609ac4d18c2c22fac)
    
    4ebb982b DLink.DxS.get_metrics. Add 'Interface | Speed' metric to script.

--- sa/profiles/DLink/DxS/get_metrics.py
+++ sa/profiles/DLink/DxS/get_metrics.py
@@ -30,3 +30,48 @@ class Script(GetMetricsScript):
 
         if cpu is not None:
             self.set_metric(id=("CPU | Usage", None), value=round(float(cpu)))
+
+    @metrics(["Interface | Speed"], volatile=False, access="S")
+    def get_interface_speed(self, metrics):
+        oids = {mib["IF-MIB::ifSpeed", m.ifindex]: m for m in metrics if m.ifindex}
+        result = self.snmp.get_chunked(
+            oids=list(oids),
+            chunk_size=self.get_snmp_metrics_get_chunk(),
+            timeout_limits=self.get_snmp_metrics_get_timeout(),
+        )
+        ts = self.get_ts()
+        high_speed_oids = {}
+        for r in result:
+            mc = oids[r]
+            if result[r] in {1410065408, 4294967295}:
+                # Need ifHighSpeed metric
+                high_speed_oids[mib["IF-MIB::ifHighSpeed", mc.ifspeed]] = mc
+                continue
+            self.set_metric(
+                id=mc.id,
+                metric=mc.metric,
+                value=float(result[r]),
+                ts=ts,
+                path=mc.path,
+                type="gauge",
+                scale=1,
+            )
+        # Getting ifHighSpeed
+        if high_speed_oids:
+            self.logger.info("[Interface | Speed] Getting ifHighSpeed oids: %s", high_speed_oids)
+            results = self.snmp.get_chunked(
+                oids=list(high_speed_oids),
+                chunk_size=self.get_snmp_metrics_get_chunk(),
+                timeout_limits=self.get_snmp_metrics_get_timeout(),
+            )
+            for r in results:
+                mc = high_speed_oids[r]
+                self.set_metric(
+                    id=mc.id,
+                    metric=mc.metric,
+                    value=float(results[r]),
+                    ts=ts,
+                    path=mc.path,
+                    type="gauge",
+                    scale=1000000,
+                )
