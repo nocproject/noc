86c8b135dc00dd3ca7db3fdaf75dbc8591b6d887
uncol <d.a.zakharov@gmail.com>
2020-12-01 20:27:38 +0300

    sova/rtk#896

---
+++ ui/web/fm/event/ApplicationModel.js
@@ -0,0 +1,15 @@
+//---------------------------------------------------------------------
+// fm.event application view model
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2020 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.fm.event.ApplicationModel");
+
+Ext.define("NOC.fm.event.ApplicationModel", {
+    extend: "Ext.app.ViewModel",
+    alias: "viewmodel.fm.event",
+    data: {
+        filter: {}
+    }
+});
