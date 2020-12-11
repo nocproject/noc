d5433c0cedac5388c080ad41630a6b78dbbcfcb3
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2020-12-11 15:22:15 +0000

    Update Eltex.TAU profile

--- sa/profiles/Eltex/TAU/get_chassis_id.py
+++ sa/profiles/Eltex/TAU/get_chassis_id.py
@@ -1,22 +1,25 @@
 # ---------------------------------------------------------------------
 # Eltex.TAU.get_chassis_id
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2018 The NOC Project
+# Copyright (C) 2007-2020 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
 # NOC modules
-from noc.core.script.base import BaseScript
+from noc.sa.profiles.Generic.get_chassis_id import Script as BaseScript
 from noc.sa.interfaces.igetchassisid import IGetChassisID
 from noc.core.mac import MAC
 
 
 class Script(BaseScript):
     name = "Eltex.TAU.get_chassis_id"
-    cache = True
     interface = IGetChassisID
+    cache = True
+    always_prefer = "S"
 
-    def execute(self):
+    SNMP_GET_OIDS = {"SNMP": ["1.3.6.1.4.1.35265.4.15.0"]}
+
+    def execute_cli(self):
         c = self.cli("show hwaddr")
         s = c.split(":", 1)[1].strip()
         return {"first_chassis_mac": MAC(s), "last_chassis_mac": MAC(s)}
