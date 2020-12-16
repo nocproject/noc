9d92b5c69a9cb2d810ee7954d90718deed7e665c
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2020-12-16 08:53:19 +0000

    Update Eltex.TAU profile

--- sa/profiles/Eltex/TAU/get_chassis_id.py
+++ sa/profiles/Eltex/TAU/get_chassis_id.py
@@ -5,6 +5,9 @@
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
+# Python modules
+import re
+
 # NOC modules
 from noc.sa.profiles.Generic.get_chassis_id import Script as BaseScript
 from noc.sa.interfaces.igetchassisid import IGetChassisID
@@ -15,11 +18,23 @@ class Script(BaseScript):
     name = "Eltex.TAU.get_chassis_id"
     interface = IGetChassisID
     cache = True
-    always_prefer = "S"
+    always_prefer = "C"
 
+    # Working only on TAU-36
     SNMP_GET_OIDS = {"SNMP": ["1.3.6.1.4.1.35265.4.15.0"]}
 
+    rx_shell_mac = re.compile(r"^(?:Factory|LAN) MAC: (?P<mac>\S+)", re.MULTILINE)
+    rx_shell_mac_tau8 = re.compile(r"^(?P<mac>\S+)$")
+
     def execute_cli(self):
-        c = self.cli("show hwaddr")
-        s = c.split(":", 1)[1].strip()
+        if self.is_tau4:
+            c = self.cli("cat /tmp/.board_desc", cached=True)
+            match = self.rx_shell_mac.search(c)
+        elif self.is_tau8:
+            c = self.cli("cat /tmp/board_mac", cached=True)
+            match = self.rx_shell_mac_tau8.search(c)
+        else:
+            c = self.cli("cat /tmp/factory", cached=True)
+            match = self.rx_shell_mac.search(c)
+        s = match.group("mac")
         return {"first_chassis_mac": MAC(s), "last_chassis_mac": MAC(s)}
--- sa/profiles/Eltex/TAU/get_interfaces.py
+++ sa/profiles/Eltex/TAU/get_interfaces.py
@@ -20,55 +20,59 @@ class Script(BaseScript):
     interface = IGetInterfaces
 
     rx_sh_int = re.compile(
-        r"^(?P<ifname>\S+)\s+Link\sencap:(?P<itype>\S+)\s+"
-        r"(?:HWaddr\s+(?P<mac>\S+)|Loopback)(:?\s+inet\s+addr:(?P<ip>\S+)\s+"
-        r"(?:(|\S+\s+)Mask:(?P<mask>\S+))|\s+)\s+\S.+MTU:(?P<mtu>\S+)",
+        r"^(?P<ifname>\S+)\s+Link\sencap:(?P<itype>\S+)\s+(?:HWaddr\s+(?P<mac>\S+)|Loopback)\s*\n"
+        r"^(?:\s+inet\s+addr:\s*(?P<ip>\S+)\s+(?:(|\S+\s+)Mask:(?P<mask>\S+))\s*\n)?"
+        r"^(?:\s+inet6\s+addr:\s*(?P<ip6>\S+)\s+Scope:\S+\s*\n)?"
+        r"^\s+\S.+MTU:(?P<mtu>\S+)",
         re.MULTILINE | re.IGNORECASE,
     )
 
     def execute_cli(self):
         interfaces = []
-        with self.profile.shell(self):
-            v = self.cli("ifconfig", cached=True)
-            for line in v.split("\n\n"):
-                match = self.rx_sh_int.search(line)
-                if match:
-                    ifname = match.group("ifname")
-                    sub = {
-                        "name": ifname,
-                        "mtu": match.group("mtu"),
-                        "admin_status": True,
-                        "oper_status": True,
-                        "enabled_afi": ["BRIDGE"],
-                    }
-                    if match.group("ip"):
-                        ip_address = match.group("ip")
-                        ip_subnet = match.group("mask")
-                        ip_address = "%s/%s" % (ip_address, IPv4.netmask_to_len(ip_subnet))
-                        sub["enabled_afi"] = ["IPv4"]
-                        sub["ipv4_addresses"] = [ip_address]
-                    if "." in ifname:
-                        parent, vlan = ifname.split(".")
-                        sub["vlan_ids"] = int(vlan)
-                        found = False
-                        for i in interfaces:
-                            if i["name"] == parent:
-                                i["subinterfaces"] += [sub]
-                                found = True
-                                break
-                        if found:
-                            continue
-                    iface = {
-                        "type": self.profile.get_interface_type(ifname),
-                        "name": ifname,
-                        "mtu": match.group("mtu"),
-                        "admin_status": True,
-                        "oper_status": True,
-                        "subinterfaces": [],
-                    }
-                    if match.group("mac"):
-                        mac = match.group("mac")
-                        iface["mac"] = mac
-                    iface["subinterfaces"] = [sub]
-                    interfaces += [iface]
+        v = self.cli("ifconfig", cached=True)
+        for line in v.split("\n\n"):
+            match = self.rx_sh_int.search(line)
+            if match:
+                ifname = match.group("ifname")
+                sub = {
+                    "name": ifname,
+                    "mtu": match.group("mtu"),
+                    "admin_status": True,
+                    "oper_status": True,
+                    "enabled_afi": ["BRIDGE"],
+                }
+                if match.group("ip"):
+                    ip_address = match.group("ip")
+                    ip_subnet = match.group("mask")
+                    ip_address = "%s/%s" % (ip_address, IPv4.netmask_to_len(ip_subnet))
+                    sub["enabled_afi"] += ["IPv4"]
+                    sub["ipv4_addresses"] = [ip_address]
+                if match.group("ip6"):
+                    ip6_address = match.group("ip6")
+                    sub["enabled_afi"] += ["IPv6"]
+                    sub["ipv6_addresses"] = [ip6_address]
+                if "." in ifname:
+                    parent, vlan = ifname.split(".")
+                    sub["vlan_ids"] = int(vlan)
+                    found = False
+                    for i in interfaces:
+                        if i["name"] == parent:
+                            i["subinterfaces"] += [sub]
+                            found = True
+                            break
+                    if found:
+                        continue
+                iface = {
+                    "type": self.profile.get_interface_type(ifname),
+                    "name": ifname,
+                    "mtu": match.group("mtu"),
+                    "admin_status": True,
+                    "oper_status": True,
+                    "subinterfaces": [],
+                }
+                if match.group("mac"):
+                    mac = match.group("mac")
+                    iface["mac"] = mac
+                iface["subinterfaces"] = [sub]
+                interfaces += [iface]
         return [{"interfaces": interfaces}]
