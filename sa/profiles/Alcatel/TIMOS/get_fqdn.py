89cc19456464ba1fbb1dbbe4eeede07a4cdbb7e3
MaksimSmile13 <sysfar@gmail.com>
2020-12-17 17:29:25 +0000

    Add get_fqdn scripts on profiles

---
+++ sa/profiles/Alcatel/TIMOS/get_fqdn.py
@@ -0,0 +1,17 @@
+# ---------------------------------------------------------------------
+# Alcatel.TIMOS.get_fqdn
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2020 The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+
+# Python modules
+from noc.sa.profiles.Generic.get_fqdn import Script as BaseScript
+
+# NOC modules
+from noc.sa.interfaces.igetfqdn import IGetFQDN
+
+
+class Script(BaseScript):
+    name = "Alcatel.TIMOS.get_fqdn"
+    interface = IGetFQDN
