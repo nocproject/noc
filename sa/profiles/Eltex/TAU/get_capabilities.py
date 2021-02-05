963b84892a47e1f2069bdcd3fbcc14126e2691e2
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2021-02-05 14:53:56 +0000

    Add Eltex.TAU.get_lldp_neighbors script

--- sa/profiles/Eltex/TAU/get_capabilities.py
+++ sa/profiles/Eltex/TAU/get_capabilities.py
@@ -1,15 +1,24 @@
 # ---------------------------------------------------------------------
 # Eltex.TAU.get_capabilities
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2020 The NOC Project
+# Copyright (C) 2007-2021 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
 # NOC modules
 from noc.sa.profiles.Generic.get_capabilities import Script as BaseScript
+from noc.sa.profiles.Generic.get_capabilities import false_on_cli_error
 
 
 class Script(BaseScript):
     name = "Eltex.TAU.get_capabilities"
 
     CHECK_SNMP_GET = {"SNMP | OID | fxsMonitoring": "1.3.6.1.4.1.35265.1.9.10.5.0"}
+
+    @false_on_cli_error
+    def has_lldp_cli(self):
+        """
+        Check box has lldp enabled
+        """
+        cmd = self.cli("lldpctl", cached=True)
+        return "Unable to connect to lldpd daemon" not in cmd
