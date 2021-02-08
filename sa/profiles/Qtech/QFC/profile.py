5df0a153bc7b8a174ed0642ff280383e7261d073
Andrey Vertiprahov <aversanta@gmail.com>
2021-02-08 16:24:28 +0000

    Merge branch 'fix-avs-qtech-qfc-snmp' into 'master'
    Qtech.QFC. Add custom SNMP parser.
    
    See merge request noc/noc!5011
    
    (cherry picked from commit 37cced76625cd1b37399fb8a2c768b244b325ded)
    
    c5d84fd6 Qtech.QFC. Add custom SNMP parser.

--- sa/profiles/Qtech/QFC/profile.py
+++ sa/profiles/Qtech/QFC/profile.py
@@ -12,7 +12,32 @@ import re
 
 # NOC module
 from noc.core.profile.base import BaseProfile
-from noc.core.snmp.get import parse_get_response_strict
+from noc.core.snmp.get import GetResponse, BERDecoder, PDU_RESPONSE
+
+
+def parse_get_response(pdu: bytes, display_hints=None) -> GetResponse:
+    """
+    Common response parser
+    :param pdu:
+    :param display_hints:
+    :return:
+    """
+    decoder = BERDecoder(display_hints=display_hints)
+    data = decoder.parse_sequence(pdu)[0]
+    pdu = data[2]
+    if pdu[0] != PDU_RESPONSE:
+        raise ValueError("Invalid response PDU type: %s" % pdu[0])
+    error_index = pdu[3]
+    if pdu[2]:
+        # Always get 1 OID on query. If error return bad value
+        error_index = 1
+    return GetResponse(
+        community=data[1],
+        request_id=pdu[1],
+        error_status=pdu[2],
+        error_index=error_index,
+        varbinds=pdu[4],
+    )
 
 
 class Profile(BaseProfile):
@@ -22,7 +47,7 @@ class Profile(BaseProfile):
     # Timeout for snmp GET request
     snmp_metrics_get_timeout = 5
 
-    snmp_response_parser = parse_get_response_strict
+    snmp_response_parser = parse_get_response  # For error_index return bad value
 
     matchers = {
         "is_lite": {"platform": {"$regex": "QFC-PBIC-LITE"}},
