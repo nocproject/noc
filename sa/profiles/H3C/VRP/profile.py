d949bfb975264708503e7f0728c2759a7cec9942
Andrey Vertiprahov <aversanta@gmail.com>
2021-02-09 06:44:01 +0000

    Merge branch 'fix-H3C_version_matchers' into 'master'
    H3C fix version matchers regex
    
    See merge request noc/noc!4973
    
    (cherry picked from commit 497274885d91537faf7058e529148b29de1ce7eb)
    
    a655c336 H3C fix version matchers regex
    ad0f93b4 fix black for Mikrotik get_mac_address_table.py script
    ec1c2c60 Merge branch 'master' of https://code.getnoc.com/noc/noc into fix-H3C_version_matchers

--- sa/profiles/H3C/VRP/profile.py
+++ sa/profiles/H3C/VRP/profile.py
@@ -24,10 +24,10 @@ class Profile(BaseProfile):
     pattern_syntax_error = r"% Wrong parameter|% Unrecognized command found at"
 
     matchers = {
-        "is_old_version": {"version": {"$regex": r"3.02.*"}},
-        "is_310_version": {"version": {"$regex": r"3.10.*"}},
-        "is_52_version": {"version": {"$regex": r"5.2\S+"}},
-        "is_53_version": {"version": {"$regex": r"5.3\S+"}},
+        "is_old_version": {"version": {"$regex": r"3\.02.*"}},
+        "is_310_version": {"version": {"$regex": r"3\.10.*"}},
+        "is_52_version": {"version": {"$regex": r"5\.2\S+"}},
+        "is_53_version": {"version": {"$regex": r"5\.3\S+"}},
         "is_S3600_platform": {"platform": {"$regex": r".*S3600.*"}},
     }
 
