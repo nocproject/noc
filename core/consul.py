81da19e61c5674c2f6d35a5539f255536cf19c20
Dmitry Volodin <dv@nocproject.org>
2021-04-22 13:24:26 +0300

    consul: Remove duplicated code

--- core/consul.py
+++ core/consul.py
@@ -1,93 +1,56 @@
 # ----------------------------------------------------------------------
 # Consul client
 # ----------------------------------------------------------------------
-# Copyright (C) 2007-2020 The NOC Project
+# Copyright (C) 2007-2021 The NOC Project
 # See LICENSE for details
 # ----------------------------------------------------------------------
 
 # Third-party modules
-import tornado.httpclient
 import consul.base
-import consul.tornado
 
 # NOC modules
 from noc.config import config
+from noc.core.http.client import fetch
 
-
-CONSUL_CONNECT_TIMEOUT = config.consul.connect_timeout
-CONSUL_REQUEST_TIMEOUT = config.consul.request_timeout
-CONSUL_NEAR_RETRY_TIMEOUT = config.consul.near_retry_timeout
-
+ConsulRepeatableCodes = {500, 598, 599}
 ConsulRepeatableErrors = consul.base.Timeout
 
 
-class ConsulHTTPClient(consul.tornado.HTTPClient):
+class ConsulHTTPClient(consul.base.HTTPClient):
     """
-    Gentler version of tornado http client
+    asyncio version of consul http client
     """
 
-    async def _request(self, callback, request):
-        client = tornado.httpclient.AsyncHTTPClient(force_instance=True, max_clients=1)
-        try:
-            response = await client.fetch(request)
-        except tornado.httpclient.HTTPError as e:
-            if e.code == 599:
-                raise consul.base.Timeout
-            response = e.response
-        finally:
-            client.close()
-            # Resolve CurlHTTPClient circular dependencies
-            client._force_timeout_callback = None
-            client._multi = None
-        return callback(self.response(response))
+    async def _request(self, callback, url, method="GET", body=None):
+        code, headers, body = await fetch(
+            url,
+            method=method,
+            body=body,
+            connect_timeout=config.consul.connect_timeout,
+            request_timeout=config.consul.request_timeout,
+            validate_cert=self.verify,
+        )
+        if code in ConsulRepeatableCodes:
+            raise consul.base.Timeout
+        return callback(consul.base.Response(code=code, headers=headers, body=body))
 
     def get(self, callback, path, params=None):
-        uri = self.uri(path, params)
-        request = tornado.httpclient.HTTPRequest(
-            uri,
-            method="GET",
-            validate_cert=self.verify,
-            connect_timeout=CONSUL_CONNECT_TIMEOUT,
-            request_timeout=CONSUL_REQUEST_TIMEOUT,
-        )
-        return self._request(callback, request)
+        url = self.uri(path, params)
+        return self._request(callback, url, method="GET")
 
     def put(self, callback, path, params=None, data=""):
-        uri = self.uri(path, params)
-        request = tornado.httpclient.HTTPRequest(
-            uri,
-            method="PUT",
-            body="" if data is None else data,
-            validate_cert=self.verify,
-            connect_timeout=CONSUL_CONNECT_TIMEOUT,
-            request_timeout=CONSUL_REQUEST_TIMEOUT,
-        )
-        return self._request(callback, request)
+        url = self.uri(path, params)
+        return self._request(callback, url, method="PUT", body="" if data is None else data)
 
     def delete(self, callback, path, params=None):
-        uri = self.uri(path, params)
-        request = tornado.httpclient.HTTPRequest(
-            uri,
-            method="DELETE",
-            validate_cert=self.verify,
-            connect_timeout=CONSUL_CONNECT_TIMEOUT,
-            request_timeout=CONSUL_REQUEST_TIMEOUT,
-        )
-        return self._request(callback, request)
+        url = self.uri(path, params)
+        return self._request(callback, url, method="DELETE")
 
     def post(self, callback, path, params=None, data=""):
-        uri = self.uri(path, params)
-        request = tornado.httpclient.HTTPRequest(
-            uri,
-            method="POST",
-            body=data,
-            validate_cert=self.verify,
-            connect_timeout=CONSUL_CONNECT_TIMEOUT,
-            request_timeout=CONSUL_REQUEST_TIMEOUT,
-        )
-        return self._request(callback, request)
+        url = self.uri(path, params)
+        return self._request(callback, url, method="POST", body=data)
 
 
 class ConsulClient(consul.base.Consul):
     def connect(self, host, port, scheme, verify=True, cert=None):
-        return ConsulHTTPClient(host, port, scheme, verify=verify, cert=cert)
+        return ConsulHTTPClient(host, port, scheme, verify=verify)
