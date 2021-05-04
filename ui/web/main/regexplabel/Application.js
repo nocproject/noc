a4b1b45b416d6d7542806b2ab8eef7a02338a3fc
Andrey Vertiprahov <aversanta@gmail.com>
2021-05-04 07:20:29 +0000

    noc/noc#1574 Add RegexLabel

---
+++ ui/web/main/regexplabel/Application.js
@@ -0,0 +1,127 @@
+//---------------------------------------------------------------------
+// main.prefixtable application
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2013 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.main.regexplabel.Application");
+
+Ext.define("NOC.main.regexplabel.Application", {
+    extend: "NOC.core.ModelApplication",
+    requires: [
+        "NOC.core.LabelField",
+        "NOC.main.regexplabel.Model"
+    ],
+    model: "NOC.main.regexplabel.Model",
+    search: true,
+    columns: [
+        {
+            text: __("Name"),
+            dataIndex: "name",
+            width: 200
+        },
+        {
+            text: __("Description"),
+            dataIndex: "description",
+            flex: 1
+        }
+    ],
+    fields: [
+        {
+            name: "name",
+            xtype: "textfield",
+            fieldLabel: __("Name"),
+            allowBlank: false
+        },
+        {
+            name: "description",
+            xtype: "textarea",
+            fieldLabel: __("Description"),
+            allowBlank: true
+        },
+        {
+            name: "regexp",
+            xtype: "textfield",
+            fieldLabel: __("REGEX"),
+            allowBlank: false
+        },
+        {
+          name: "flag_multiline",
+          xtype: "checkbox",
+          boxLabel: __("Multiline"),
+          allowBlank: true
+        },
+        {
+          name: "flag_dotall",
+          xtype: "checkbox",
+          boxLabel: __("DotALL"),
+          allowBlank: true
+        },
+        {
+            name: "labels",
+            xtype: "labelfield",
+            fieldLabel: __("Labels"),
+            allowBlank: true,
+            query: {
+                "enable_managedobject": true
+            },
+        },
+        {
+          xtype: "fieldset",
+          layout: "vbox",
+          title: __("Enable"),
+          defaults: {
+            padding: 4,
+            labelAlign: "right"
+          },
+          items: [
+        {
+          xtype: "fieldset",
+          layout: "hbox",
+          title: __("ManagedObject"),
+          defaults: {
+            padding: 4,
+            labelAlign: "right"
+          },
+          items: [
+            {
+              name: "enable_managedobject_name",
+              xtype: "checkbox",
+              boxLabel: __("Managed Object Name")
+            },
+            {
+              name: "enable_managedobject_address",
+              xtype: "checkbox",
+              boxLabel: __("Managed Object Address")
+            },
+              {
+              name: "enable_managedobject_description",
+              xtype: "checkbox",
+              boxLabel: __("Managed Object Description")
+            }
+            ]
+        },
+        {
+          xtype: "fieldset",
+          layout: "hbox",
+          title: __("Interface"),
+          defaults: {
+            padding: 4,
+            labelAlign: "right"
+          },
+          items: [
+            {
+              name: "enable_interface_name",
+              xtype: "checkbox",
+              boxLabel: __("Interface Name")
+            },
+            {
+              name: "enable_interface_description",
+              xtype: "checkbox",
+              boxLabel: __("Interface Description")
+            }
+            ]
+        }
+        ]},
+    ]
+});
---
+++ ui/web/main/regexplabel/LookupField.js
@@ -0,0 +1,13 @@
+//---------------------------------------------------------------------
+// NOC.main.regexplabel.Lookup
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2013 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.main.regexplabel.LookupField");
+
+Ext.define("NOC.main.regexplabel.LookupField", {
+    extend: "NOC.core.ComboBox",
+    alias: "widget.main.regexplabel.LookupField",
+    uiStyle: "medium-combo"
+});
