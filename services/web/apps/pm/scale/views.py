c4ec65bcc72888731c31dc7843795001284b711a
Dmitry Volodin <dvolodin7@gmail.com>
2021-06-02 09:36:59 +0000

    Metric Scale

---
+++ services/web/apps/pm/scale/views.py
@@ -0,0 +1,21 @@
+# ----------------------------------------------------------------------
+# pm.scale application
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2021 The NOC Project
+# See LICENSE for details
+# ----------------------------------------------------------------------
+
+# NOC modules
+from noc.lib.app.extdocapplication import ExtDocApplication
+from noc.pm.models.scale import Scale
+from noc.core.translation import ugettext as _
+
+
+class ScaleApplication(ExtDocApplication):
+    """
+    Scale application
+    """
+
+    title = "Scale"
+    menu = [_("Setup"), _("Scale")]
+    model = Scale
---
+++ ui/web/pm/scale/Model.js
@@ -0,0 +1,44 @@
+//---------------------------------------------------------------------
+// pm.scale Model
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2021 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.pm.scale.Model");
+
+Ext.define("NOC.pm.scale.Model", {
+    extend: "Ext.data.Model",
+    rest_url: "/pm/scale/",
+
+    fields: [
+        {
+            name: "id",
+            type: "string"
+        },
+        {
+            name: "name",
+            type: "string"
+        },
+        {
+            name: "uuid",
+            type: "string"
+        },
+        {
+            name: "code",
+            type: "string"
+        },
+        {
+            name: "label",
+            type: "string"
+        },
+        {
+            name: "base",
+            type: "int",
+            defaultValue: 10
+        },
+        {
+            name: "exp",
+            type: "int"
+        }
+    ]
+});
