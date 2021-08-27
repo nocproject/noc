3ffaf97120340b6f30f19b8f584fb98ebaa8d02d
Dmitry Volodin <dv@nocproject.org>
2021-08-27 09:53:37 +0200

    noc-agent: Send version in the zeroconf requests

--- rust/agent/src/meta.rs
+++ rust/agent/src/meta.rs
@@ -1,19 +1,8 @@
 // ---------------------------------------------------------------------
-// agent library
+// Package metadata
 // ---------------------------------------------------------------------
 // Copyright (C) 2007-2021 The NOC Project
 // See LICENSE for details
 // ---------------------------------------------------------------------
 
-pub mod agent;
-pub mod cli;
-pub mod collectors;
-pub mod config;
-pub mod error;
-mod feature;
-pub mod meta;
-pub mod proto;
-pub mod sender;
-pub mod state;
-pub mod sysid;
-pub mod timing;
+pub const VERSION: &str = env!("CARGO_PKG_VERSION");
