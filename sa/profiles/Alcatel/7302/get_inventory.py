161ea81e24a381fd6ca22f4b3f30a937c1de2c0e
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2021-10-19 13:26:03 +0300

    Update Alcatel.7302 profile

--- sa/profiles/Alcatel/7302/get_inventory.py
+++ sa/profiles/Alcatel/7302/get_inventory.py
@@ -41,13 +41,19 @@ class Script(BaseScript):
                 r[-1]["serial"] = sn
             if b_revision is not None:
                 r[-1]["revision"] = b_revision
+        platform = self.port_map[slots]
+        v = self.snmp.get("1.3.6.1.4.1.637.61.1.23.2.1.3.1")
+        if v == "LEEU":
+            platform = platform + "XD"
+        elif v == "LNEU":
+            platform = platform + "FD"
         r.insert(
             0,
             {
                 "number": "0",
                 "type": "CHASSIS",
                 "vendor": "Alcatel",
-                "part_no": self.port_map[slots],
+                "part_no": platform,
             },
         )
         return r
