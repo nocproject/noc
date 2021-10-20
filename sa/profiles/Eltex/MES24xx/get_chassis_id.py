f77d68d4318f871d753eb8a56109f41922e523bd
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2021-10-20 14:37:42 +0300

    Fix Eltex.MES24xx.get_chassis_id script

--- sa/profiles/Eltex/MES24xx/get_chassis_id.py
+++ sa/profiles/Eltex/MES24xx/get_chassis_id.py
@@ -1,7 +1,7 @@
 # ----------------------------------------------------------------------
 # Eltex.MES24xx.get_chassis_id
 # ----------------------------------------------------------------------
-# Copyright (C) 2007-2020 The NOC Project
+# Copyright (C) 2007-2021 The NOC Project
 # See LICENSE for details
 # ----------------------------------------------------------------------
 
@@ -19,8 +19,17 @@ class Script(BaseScript):
     cache = True
 
     rx_mac = re.compile(r"^Chassis Id\s+:\s+(?P<mac>\S+)", re.MULTILINE)
+    rx_mac2 = re.compile(r"^\s+Address\s+(?P<mac>\S+)", re.MULTILINE)
 
     def execute_cli(self, **kwargs):
         match = self.rx_mac.search(self.cli("show lldp local"))
-        # do not use show spanning-tree switch default - if stp disabled output 00:00:00:00:00:00 mac
-        return {"first_chassis_mac": match.group("mac"), "last_chassis_mac": match.group("mac")}
+        # do not use `show spanning-tree switch default`
+        # if stp disabled - output 00:00:00:00:00:00 mac
+        if match:  # MES2428, fw 10.1.9.1 return empty string
+            return {"first_chassis_mac": match.group("mac"), "last_chassis_mac": match.group("mac")}
+        # Last resort
+        v = self.cli("show spanning-tree")
+        match = self.rx_mac2.search(v)
+        if match and "This bridge is the root" in v:
+            return {"first_chassis_mac": match.group("mac"), "last_chassis_mac": match.group("mac")}
+        raise self.NotSupportedError()
