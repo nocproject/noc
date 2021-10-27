ab7054a8100fcc9c8d9d190c81833eaa226d9080
sysfaray <sysfar@gmail.com>
2021-10-27 18:05:14 +0300

    Maintenance Add indexes

--- maintenance/models/maintenance.py
+++ maintenance/models/maintenance.py
@@ -58,7 +58,15 @@ class Maintenance(Document):
         "collection": "noc.maintenance",
         "strict": False,
         "auto_create_index": False,
-        "indexes": [("start", "is_completed"), "administrative_domain"],
+        "indexes": [
+            "start",
+            "stop",
+            ("start", "is_completed"),
+            "subject",
+            "administrative_domain",
+            "direct_objects.object",
+            "direct_segments.segment",
+        ],
         "legacy_collections": ["noc.maintainance"],
     }
 
