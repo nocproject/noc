6180cde90b8265aa5872d0492f739d5ca802a068
Michael R <rmiclee@gmail.com>
2021-11-19 17:49:08 +0300

    Some more fixes

--- services/mib/paths/jsonrpc_mib.py
+++ services/mib/paths/jsonrpc_mib.py
@@ -19,23 +19,22 @@ router = APIRouter()
 @router.post("/api/mib/")
 @router.post("/api/mib")
 def api_mib(incoming_call: JSONRemoteProcedureCall):
-    if incoming_call.method in MIBAPI.get_methods():
-        service = get_service()
-        api = MIBAPI(service, None, None)
-        api_method = getattr(api, incoming_call.method)
-        try:
-            result = api_method(*incoming_call.params)
-            error = None
-        except Exception as e:
-            result = None
-            error = 'Failed: {}'.format(e)
-        return {
-            "result": result,
-            "error": error,
-            'id': incoming_call.id
-        }
-    else:
+    if incoming_call.method not in MIBAPI.get_methods():
         return {
             "error": "Invalid method: '{}'".format(incoming_call.method),
             'id': incoming_call.id
         }
+    service = get_service()
+    api = MIBAPI(service, None, None)
+    api_method = getattr(api, incoming_call.method)
+    try:
+        result = api_method(*incoming_call.params)
+        error = None
+    except Exception as e:
+        result = None
+        error = 'Failed: {}'.format(e)
+    return {
+        "result": result,
+        "error": error,
+        'id': incoming_call.id
+    }
