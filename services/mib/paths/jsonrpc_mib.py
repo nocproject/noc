b0c44ac2dfade591b75398145c60e2fc24c4c132
Michael R <rmiclee@gmail.com>
2021-11-22 10:54:31 +0300

    Change single quotes to double quotes

--- services/mib/paths/jsonrpc_mib.py
+++ services/mib/paths/jsonrpc_mib.py
@@ -18,13 +18,13 @@ from noc.services.mib.api.mib import MIBAPI
 router = APIRouter()
 
 
-@router.post('/api/mib/')
-@router.post('/api/mib')
+@router.post("/api/mib/")
+@router.post("/api/mib")
 def api_mib(incoming_call: JSONRemoteProcedureCall):
     if incoming_call.method not in MIBAPI.get_methods():
         return {
-            'error': f"Invalid method: '{incoming_call.method}'",
-            'id': incoming_call.id
+            "error": f"Invalid method: '{incoming_call.method}'",
+            "id": incoming_call.id
         }
     service = get_service()
     api = MIBAPI(service, None, None)
@@ -34,12 +34,12 @@ def api_mib(incoming_call: JSONRemoteProcedureCall):
     try:
         result = api_method(*incoming_call.params)
     except NOCError as e:
-        error = f'Failed: {e}'
+        error = f"Failed: {e}"
     except Exception as e:
         error_report()
-        error = f'Failed: {e}'
+        error = f"Failed: {e}"
     return {
-        'result': result,
-        'error': error,
-        'id': incoming_call.id
+        "result": result,
+        "error": error,
+        "id": incoming_call.id
     }
