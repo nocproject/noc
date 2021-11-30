d60fc26e3e0861b533b2d531a6c9943b5b817738
EKbfh <earthquake_90@mail.ru>
2021-11-30 17:49:33 +0300

    add logging

--- scripts/deploy/change-ip.py
+++ scripts/deploy/change-ip.py
@@ -130,7 +130,7 @@ if __name__ == "__main__":
 
     set_hosts_address(my_ip)
     change_ip_everywhere(ALL_PATHS, old_ip_address, my_ip)
-    
+
     print("Restarting services")
     os.system(f"chown nats {NATS_DEB_PATH}")
     os.system("systemctl restart nats-server")
