2c2d0033432900728542a05866752251d260fab1
Michael R <rmiclee@gmail.com>
2021-12-02 12:43:10 +0300

    Fix res_list variable usage

--- services/mrt/paths/mrt.py
+++ services/mrt/paths/mrt.py
@@ -36,17 +36,14 @@ logger = logging.getLogger(__name__)
 
 router = APIRouter()
 
-res_list = []
 
-
-async def _write_chunk(obj):
-    global res_list
+async def _write_chunk(res_list, obj):
     data = smart_text(orjson.dumps(obj))
     res_list.append("%s|%s" % (len(data), data))
     logger.debug("%s|%s" % (len(data), data))
 
 
-async def _run_script(oid, script, args, span_id=0, bi_id=None):
+async def _run_script(res_list, oid, script, args, span_id=0, bi_id=None):
     current_user = User.get_by_username("admin")
     service = get_service()
     with Span(
@@ -58,7 +55,7 @@ async def _run_script(oid, script, args, span_id=0, bi_id=None):
         client=current_user,
     ) as span:
         try:
-            await _write_chunk({"id": str(oid), "running": True})
+            await _write_chunk(res_list, {"id": str(oid), "running": True})
             logger.debug("[%s] Run script %s %s %s", span.context, oid, script, args)
             r = await service.sae.script(oid, script, args)
             metrics["mrt_success"] += 1
@@ -89,7 +86,7 @@ async def api_mrt(req: List[MRTScript], current_user: User = Depends(get_current
     service = get_service()
     metrics["mrt_requests"] += 1
     # Disable nginx proxy buffering
-    #self.set_header("X-Accel-Buffering", "no")
+    # self.set_header("X-Accel-Buffering", "no")
     # Object ids
     ids = set(int(d.id) for d in req if hasattr(d, "id") and hasattr(d, "script"))
     logger.info(
@@ -116,22 +113,23 @@ async def api_mrt(req: List[MRTScript], current_user: User = Depends(get_current
                 "[%s] Enable telemetry for task, user: %s", span.span_id, current_user
             )
         futures = set()
+        res_list = []
         for d in req:
             if not hasattr(d, "id") or not hasattr(d, "script"):
                 continue
             oid = int(d.id)
             if oid not in ids:
-                await _write_chunk({"id": str(d["id"]), "error": "Access denied"})
+                await _write_chunk(res_list, {"id": str(d["id"]), "error": "Access denied"})
                 metrics["mrt_access_denied"] += 1
                 continue
             while len(futures) >= config.mrt.max_concurrency:
                 done, futures = await asyncio.wait(futures, return_when=asyncio.FIRST_COMPLETED)
                 for f in done:
                     r = await f
-                    await _write_chunk(r)
+                    await _write_chunk(res_list, r)
             futures.add(
                 _run_script(
-                    oid, d.script, dict(d.args), span_id=span.span_id, bi_id=ids.get(oid)
+                    res_list, oid, d.script, dict(d.args), span_id=span.span_id, bi_id=ids.get(oid)
                 )
             )
         # Wait for rest
@@ -139,6 +137,6 @@ async def api_mrt(req: List[MRTScript], current_user: User = Depends(get_current
             done, futures = await asyncio.wait(futures, return_when=asyncio.FIRST_COMPLETED)
             for f in done:
                 r = await f
-                await _write_chunk(r)
+                await _write_chunk(res_list, r)
     logger.info("Done")
     return Response(content=r"\n".join(res_list), media_type="text/html")
