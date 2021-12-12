5b70b7c851f070d3658a5b4b06d1b9ddec58fbf4
Dmitry Volodin <dv@nocproject.org>
2021-12-12 11:11:39 +0100

    Locks: Prevent nested usage

--- core/lock/base.py
+++ core/lock/base.py
@@ -6,10 +6,13 @@
 # ----------------------------------------------------------------------
 
 # Python modules
-from typing import Optional, Iterable, List
+from typing import Optional, Iterable, List, Set
 from abc import ABC, abstractmethod
+from contextvars import ContextVar
 
 DEFAULT_TTL = 60.0
+
+locked_items: ContextVar[Optional[Set[str]]] = ContextVar("locked_items", default=None)
 
 
 class BaseLock(ABC):
@@ -46,6 +49,8 @@ class BaseLock(ABC):
         with lock.acquire(["obj1", "ob2"]):
             ...
         ```
+        Nested locks are not allowed. Attempting to acquire lock under the existing
+        lock will raise RuntimeError
         """
         return Token(self, items, ttl=ttl)
 
@@ -74,8 +79,22 @@ class Token(object):
         self.lock_id: Optional[str] = None
 
     def __enter__(self):
+        if locked_items.get() is not None:
+            raise RuntimeError("Attempting use of nested locks")
         self.lock_id = self.lock.acquire_by_items(self.items, ttl=self.ttl)
+        locked_items.set(set(self.items))
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self.lock_id:
             self.lock.release_by_lock_id(self.lock_id)
+        locked_items.set(None)
+
+
+def get_locked_items() -> Set[str]:
+    """
+    Get a list of currently locked items
+    """
+    r = locked_items.get()
+    if r is None:
+        return set()
+    return r
