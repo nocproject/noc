becddd119299f6f3799d5ddf0c65e7c13700e519
Andrey Vertiprahov <aversanta@gmail.com>
2021-12-12 12:13:37 +0000

    ResourcePool

---
+++ services/web/apps/inv/resourcepool/views.py
@@ -0,0 +1,21 @@
+# ----------------------------------------------------------------------
+# inv.resourcepool application
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2021 The NOC Project
+# See LICENSE for details
+# ----------------------------------------------------------------------
+
+# NOC modules
+from noc.lib.app.extdocapplication import ExtDocApplication
+from noc.inv.models.resourcepool import ResourcePool
+from noc.core.translation import ugettext as _
+
+
+class ResourcePoolApplication(ExtDocApplication):
+    """
+    ResourcePool application
+    """
+
+    title = "ResourcePool"
+    menu = [_("Setup"), _("Resource Pool")]
+    model = ResourcePool
