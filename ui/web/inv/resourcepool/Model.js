4caaba6b03ef5cc14ac2d5ec38f69a4a6fb57b63
Andrey Vertiprahov <aversanta@gmail.com>
2021-12-12 12:13:37 +0000

    ResourcePool

---
+++ ui/web/inv/resourcepool/Model.js
@@ -0,0 +1,47 @@
+//---------------------------------------------------------------------
+// inv.resourcepool Model
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2021 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.inv.resourcepool.Model");
+
+Ext.define("NOC.inv.resourcepool.Model", {
+    extend: "Ext.data.Model",
+    rest_url: "/inv/resourcepool/",
+
+    fields: [
+        {
+            name: "id",
+            type: "string"
+        },
+        {
+            name: "name",
+            type: "string"
+        },
+        {
+            name: "description",
+            type: "string"
+        },
+        {
+            name: "type",
+            type: "string"
+        },
+        {
+            name: "is_unique",
+            type: "boolean"
+        },
+        {
+            name: "strategy",
+            type: "string"
+        },
+        {
+            name: "api_code",
+            type: "string"
+        },
+        {
+            name: "api_role",
+            type: "string"
+        },
+    ]
+});
