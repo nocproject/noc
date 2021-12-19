8eeb52f3c91b7b8da50dbaf409c8394c5b0a1549
Andrey Vertiprahov <aversanta@gmail.com>
2021-12-19 21:44:23 +0500

    Fix migration

--- vc/migrations/0028_create_default_vlan_profile.py
+++ vc/migrations/0028_create_default_vlan_profile.py
@@ -4,38 +4,40 @@
 # Copyright (C) 2007-2021 The NOC Project
 # See LICENSE for details
 
+# Third-pary modules
+from bson import ObjectId
+
 # NOC modules
 from noc.core.migration.base import BaseMigration
 
 
 class Migration(BaseMigration):
     def migrate(self):
-        ...
         # Create default VLAN Profile
-        # self.mongo_db["vlanprofiles"].insert_one(
-        #     {
-        #         "_id": ObjectId("61bee6a55c42c21338453612"),
-        #         "name": "default",
-        #         "workflow": ObjectId("5a01d980b6f529000100d37a"),
-        #         "labels": [],
-        #         "effective_labels": [],
-        #         "bi_id": 2881867143753311142,
-        #     }
-        # )
-        # # Create default L2 Domain Profile
-        # self.mongo_db["l2domainprofiles"].insert_one(
-        #     {
-        #         "_id": ObjectId("61bee6f45c42c21338453613"),
-        #         "name": "default",
-        #         "workflow": ObjectId("5a01d980b6f529000100d37a"),
-        #         "pools": [],
-        #         "labels": [],
-        #         "effective_labels": [],
-        #         "bi_id": 1009096612210647130,
-        #         "vlan_discovery_policy": "E",
-        #     }
-        # )
-        # # Create default L2 Domain
+        self.mongo_db["vlanprofiles"].insert_one(
+            {
+                "_id": ObjectId("61bee6a55c42c21338453612"),
+                "name": "default",
+                "workflow": ObjectId("5a01d980b6f529000100d37a"),
+                "labels": [],
+                "effective_labels": [],
+                "bi_id": 2881867143753311142,
+            }
+        )
+        # Create default L2 Domain Profile
+        self.mongo_db["l2domainprofiles"].insert_one(
+            {
+                "_id": ObjectId("61bee6f45c42c21338453613"),
+                "name": "default",
+                "workflow": ObjectId("5a01d980b6f529000100d37a"),
+                "pools": [],
+                "labels": [],
+                "effective_labels": [],
+                "bi_id": 1009096612210647130,
+                "vlan_discovery_policy": "E",
+            }
+        )
+        # Create default L2 Domain
         # self.mongo_db["l2domains"].insert_one(
         #     {
         #         "_id": ObjectId("61bee7425c42c21338453614"),
