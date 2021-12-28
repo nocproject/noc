b16341c7fc5bdd690ba5f860d26529182a37c0f3
sysfaray <sysfar@gmail.com>
2021-12-28 14:03:10 +0300

    Add Close link down alarm if Admin Down

--- services/discovery/jobs/periodic/interfacestatus.py
+++ services/discovery/jobs/periodic/interfacestatus.py
@@ -39,6 +39,39 @@ class InterfaceStatusCheck(DiscoveryCheck):
     def get_profiles(cls, x):
         return list(InterfaceProfile.objects.filter(status_discovery__ne="d"))
 
+    def iface_alarm(self, ostatus, astatus, iface):
+        alarm_class = AlarmClass.get_by_name("Network | Link | Link Down")
+        msg = {
+            "reference": f"e:{self.object.id}:{alarm_class.id}:{iface.name}",
+            "alarm_class": alarm_class.name,
+        }
+        if iface.profile.status_discovery == "ca" and astatus is False:
+            msg["$op"] = "clear"
+            self.logger.info(
+                f"Clear {alarm_class.name}: on interface {iface.name}. Reason: Admin Status Down"
+            )
+        if iface.profile.status_discovery in ["c", "rc", "ca"] and ostatus:
+            msg["$op"] = "clear"
+            self.logger.info(f"Clear {alarm_class.name}: on interface {iface.name}")
+        if iface.profile.status_discovery == "rc" and ostatus is False and astatus is True:
+            msg["$op"] = "raise"
+            msg["vars"] = [
+                {
+                    "interface": iface.name,
+                }
+            ]
+            self.logger.info(f"Raise {alarm_class.name}: on interface {iface.name}")
+        if msg.get("$op"):
+            stream, partition = self.object.alarms_stream_and_partition
+            self.service.publish(
+                orjson.dumps(msg),
+                stream=stream,
+                partition=partition,
+            )
+            self.logger.debug(
+                "Dispose: %s", orjson.dumps(msg, option=orjson.OPT_INDENT_2).decode("utf-8")
+            )
+
     def handler(self):
         def get_interface(name):
             if_name = interfaces.get(name)
@@ -86,35 +119,19 @@ class InterfaceStatusCheck(DiscoveryCheck):
             changes = self.update_if_changed(iface, kwargs, ignore_empty=list(kwargs), bulk=bulk)
             self.log_changes("Interface %s status has been changed" % i["interface"], changes)
             ostatus = i.get("oper_status")
+            astatus = i.get("admin_status")
             if iface.oper_status != ostatus and ostatus is not None:
                 self.logger.info("[%s] set oper status to %s", i["interface"], ostatus)
+                if iface.profile.status_discovery in ["c", "rc", "ca"]:
+                    self.iface_alarm(ostatus, astatus, iface)
                 iface.set_oper_status(ostatus)
-                alarm_class = AlarmClass.get_by_name("Network | Link | Link Down")
-                msg = {
-                    "reference": f"e:{self.object.id}:{alarm_class.id}:{i['interface']}",
-                    "alarm_class": alarm_class.name,
-                }
-                if iface.profile.status_discovery in ["c", "rc"] and ostatus:
-                    msg["$op"] = "clear"
-                    self.logger.info(f"Clear {alarm_class.name}: on interface {i['interface']}")
-                if iface.profile.status_discovery == "rc" and ostatus is False:
-                    msg["$op"] = "raise"
-                    msg["vars"] = [
-                        {
-                            "interface": i["interface"],
-                        }
-                    ]
-                    self.logger.info(f"Raise {alarm_class.name}: on interface {i['interface']}")
-                if msg.get("$op"):
-                    stream, partition = self.object.alarms_stream_and_partition
-                    self.service.publish(
-                        orjson.dumps(msg),
-                        stream=stream,
-                        partition=partition,
-                    )
-                    self.logger.debug(
-                        "Dispose: %s", orjson.dumps(msg, option=orjson.OPT_INDENT_2).decode("utf-8")
-                    )
+            if (
+                iface.profile.status_discovery == ["ca"]
+                and iface.admin_status != astatus
+                and astatus is not None
+            ):
+                self.iface_alarm(ostatus, astatus, iface)
+
         if bulk:
             self.logger.info("Committing changes to database")
             try:
