d2a372ab871f2e5223f406685c8264e022553a09
Michael R <rmiclee@gmail.com>
2022-01-21 11:42:55 +0300

    Implement base class for NBI API

---
+++ services/nbi/base.py
@@ -0,0 +1,68 @@
+# ----------------------------------------------------------------------
+# NBI API Base Class
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2022 The NOC Project
+# See LICENSE for details
+# ----------------------------------------------------------------------
+
+# Third-party modules
+from fastapi import APIRouter
+
+# NOC modules
+from noc.core.service.loader import get_service
+
+
+API_ACCESS_HEADER = "X-NOC-API-Access"
+
+FORBIDDEN_MESSAGE = "<html><title>403: Forbidden</title><body>403: Forbidden</body></html>"
+
+
+class NBIAPI(object):
+    """
+    NBI API Base Class
+    """
+
+    # API name
+    api_name = None
+    # Tags for OpenAPI documentation
+    openapi_tags = []
+
+    def __init__(self, router: APIRouter):
+        self.service = get_service()
+        self.logger = self.service.logger
+        self.router = router
+        self.setup_routes()
+
+    @classmethod
+    def access_tokens_set(cls):
+        return {"nbi:*", f"nbi:{cls.api_name}"}
+
+    def access_granted(self, access_header):
+        """
+        Checks that each access_header contains at least one required token
+        """
+        a_set = set(access_header.split(","))
+        if self.access_tokens_set() & a_set:
+            return True
+        return False
+
+    def get_routes(self):
+        """
+        Returns a list of available API methods
+        """
+        raise NotImplementedError
+
+    def setup_routes(self):
+        """
+        Setup FastAPI router by adding routes
+        """
+        for route in self.get_routes():
+            self.router.add_api_route(
+                path=route["path"],
+                methods=[route["method"]],  # ["POST"]
+                endpoint=route["endpoint"],
+                response_model=route["response_model"],
+                name=route["name"],
+                description=route["description"],
+                tags=self.openapi_tags,
+            )
--- services/nbi/paths/objectstatus.py
+++ services/nbi/paths/objectstatus.py
@@ -6,59 +6,73 @@
 # ----------------------------------------------------------------------
 
 # Python modules
-from typing import List
+from typing import List, Callable
 
 # Third-party modules
 from fastapi import APIRouter, Response, Header
 from pydantic import BaseModel
 
 # NOC modules
-from noc.core.service.loader import get_service
 from noc.sa.models.objectstatus import ObjectStatus
-
-API_NAME = __name__.split(".")[-1]
-
-API_ACCESS_HEADER = "X-NOC-API-Access"
-ACCESS_TOKENS_SET = {"nbi:*", f"nbi:{API_NAME}"}
-
-FORBIDDEN_MESSAGE = "<html><title>403: Forbidden</title><body>403: Forbidden</body></html>"
+from ..base import NBIAPI, API_ACCESS_HEADER, FORBIDDEN_MESSAGE
 
 router = APIRouter()
-
-service = get_service()
-executor = service.get_executor("max")
 
 
 class RequestModel(BaseModel):
     objects: List[str]
 
 
-def _access_granted(access_header):
-    a_set = set(access_header.split(","))
-    if ACCESS_TOKENS_SET & a_set:
-        return True
-    return False
+class Status(BaseModel):
+    id: str
+    status: bool
 
 
-def _handler(access_header, objects_):
-    if not _access_granted(access_header):
-        return 403, FORBIDDEN_MESSAGE
-    # Validate
-    try:
-        objects = [int(o) for o in objects_]
-    except ValueError as e:
-        return 400, "Bad request: %s" % e
-    statuses = ObjectStatus.get_statuses(objects)
-    r = {"statuses": [{"id": str(o), "status": statuses.get(o, False)} for o in objects]}
-    return 200, r
+class ResponseModel(BaseModel):
+    statuses: List[Status]
 
 
-@router.post("/api/nbi/objectstatus")
-async def api_objectstatus(
-    req: RequestModel, access_header: str = Header(..., alias=API_ACCESS_HEADER)
-):
-    code, result = await executor.submit(_handler, access_header, req.objects)
-    if isinstance(result, str):
-        return Response(status_code=code, content=result, media_type="text/plain")
-    else:
-        return result
+class _ObjectStatusAPI(NBIAPI):
+    api_name = "objectstatus"
+    openapi_tags = ["objectstatus API"]
+
+    def get_routes(self):
+        route = {
+            "path": "/api/nbi/objectstatus",
+            "method": "POST",
+            "endpoint": self.get_objectstatus_handler(),
+            "response_model": ResponseModel,
+            "name": "objectstatus",
+            "description": "Get current statuses for one or more Managed Objects.",
+        }
+        return [route]
+
+    def get_objectstatus_handler(self) -> Callable:
+        async def objectstatus_handler(
+            req: RequestModel, access_header: str = Header(..., alias=API_ACCESS_HEADER)
+        ):
+            def _handler(objects_):
+                if not self.access_granted(access_header):
+                    return 403, FORBIDDEN_MESSAGE
+                # Validate
+                try:
+                    objects = [int(o) for o in objects_]
+                except ValueError as e:
+                    return 400, "Bad request: %s" % e
+                statuses = ObjectStatus.get_statuses(objects)
+                r = {
+                    "statuses": [{"id": str(o), "status": statuses.get(o, False)} for o in objects]
+                }
+                return 200, r
+
+            code, result = _handler(req.objects)
+            if isinstance(result, str):
+                return Response(status_code=code, content=result, media_type="text/plain")
+            else:
+                return result
+
+        return objectstatus_handler
+
+
+# Install router
+_ObjectStatusAPI(router)
