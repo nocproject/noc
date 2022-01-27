e49c36441a81409140627eb9bef47a7f55a0c8c1
Andrey Vertiprahov <aversanta@gmail.com>
2022-01-27 05:59:41 +0000

    Iskratel.MSAN. Add SNMP proto to profile.

--- sa/profiles/Iskratel/MSAN/get_interfaces.py
+++ sa/profiles/Iskratel/MSAN/get_interfaces.py
@@ -11,7 +11,8 @@ import re
 from collections import defaultdict
 
 # NOC modules
-from noc.core.script.base import BaseScript
+# from noc.core.script.base import BaseScript
+from noc.sa.profiles.Generic.get_interfaces import Script as BaseScript
 from noc.sa.interfaces.igetinterfaces import IGetInterfaces
 from noc.core.ip import IPv4
 
