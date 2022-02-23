950b42537146a2cf0c5b50df4ef4f9438362114e
Andrey Vertiprahov <aversanta@gmail.com>
2022-02-23 23:44:31 +0500

    Add avail field.

--- core/datasources/managedobjectds.py
+++ core/datasources/managedobjectds.py
@@ -17,6 +17,7 @@ from .base import FieldInfo, BaseDataSource
 from noc.sa.models.managedobject import ManagedObject
 from noc.sa.models.authprofile import AuthProfile
 from noc.sa.models.profile import Profile
+from noc.sa.models.objectstatus import ObjectStatus
 from noc.inv.models.capability import Capability
 from noc.inv.models.platform import Platform
 from noc.inv.models.firmware import Firmware
@@ -39,8 +40,14 @@ class ManagedObjectDS(BaseDataSource):
             internal_name="object_profile__name",
         ),
         FieldInfo(name="hostname", description="Object Hostname", internal_name="id"),
-        # FieldInfo(name="status", description="Object Status"),
+        FieldInfo(
+            name="status",
+            description="Object Admin Status",
+            type="bool",
+            internal_name="is_managed",
+        ),
         FieldInfo(name="address", description="Object IP Address"),
+        # Inventory fields
         FieldInfo(name="vendor", description="Object Vendor"),
         FieldInfo(name="model", description="Object Model", internal_name="platform"),
         FieldInfo(name="sw_version", description="Object Firmware", internal_name="version"),
@@ -74,17 +81,25 @@ class ManagedObjectDS(BaseDataSource):
             name="auth_profile",
             description="Object Authentication Profile",
         ),
+        # Stat fields
         FieldInfo(
             name="link_count",
-            description="ManagedObject links count",
+            description="Object links count",
             internal_name="links",
             type="int64",
         ),
         FieldInfo(
             name="physical_iface_count",
-            description="ManagedObject physical interfaces",
+            description="Object physical interfaces",
             internal_name="caps",
             type="int64",
+        ),
+        # Oper fields
+        FieldInfo(
+            name="avail",
+            description="Object Availability Status",
+            internal_name="id",
+            type="bool",
         ),
     ]
 
@@ -127,7 +142,7 @@ class ManagedObjectDS(BaseDataSource):
         if kwargs.get("require_index") and "id" not in q_fields:
             q_fields += ["id"]
         mos = ManagedObject.objects.filter()
-        extra_select, hostname_map, segment_map = {}, {}, {}
+        extra_select, hostname_map, segment_map, avail_map = {}, {}, {}, {}
         if not fields or "attr_hwversion" in fields:
             extra_select["attr_hwversion"] = cls.ATTR_QUERY % "HW version"
         if not fields or "attr_bootprom" in fields:
@@ -152,11 +167,20 @@ class ManagedObjectDS(BaseDataSource):
                 .with_options(read_preference=ReadPreference.SECONDARY_PREFERRED)
                 .find({}, {"name": 1})
             }
-        for mo in mos.values(*q_fields):
+        if not fields or "avail" in fields:
+            avail_map = {
+                val["object"]: val["status"]
+                for val in ObjectStatus._get_collection()
+                .with_options(read_preference=ReadPreference.SECONDARY_PREFERRED)
+                .find({"object": {"$exists": 1}}, {"object": 1, "status": 1})
+            }
+        for mo in mos.values(*q_fields).iterator():
             if "caps" in mo:
                 caps = cls.get_caps(mo.pop("caps"))
             else:
                 caps = {}
+            if "is_managed" in mo:
+                mo["status"] = mo.pop("is_managed")
             if "links" in mo:
                 links = mo.pop("links")
                 mo["link_count"] = len(links)
@@ -175,7 +199,9 @@ class ManagedObjectDS(BaseDataSource):
             if hostname_map:
                 mo["hostname"] = hostname_map.get(mo["id"])
             if segment_map:
-                mo["hostname"] = segment_map.get(mo["segment"])
+                mo["segment"] = segment_map.get(mo["segment"])
+            if avail_map:
+                mo["avail"] = avail_map.get(mo["id"])
             if "auth_profile" in mo:
                 mo["auth_profile"] = (
                     AuthProfile.get_by_id(mo["auth_profile"]).name if mo["auth_profile"] else None
