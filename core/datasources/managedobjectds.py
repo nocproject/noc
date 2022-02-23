aaefa0007ecf606d78ee8eea2f213c4d1482253d
Andrey Vertiprahov <aversanta@gmail.com>
2022-02-23 23:03:21 +0500

    Add Hostname field.

--- core/datasources/managedobjectds.py
+++ core/datasources/managedobjectds.py
@@ -10,6 +10,7 @@ from typing import Optional, Iterable, Dict, Any, List
 
 # Third-party modules
 import pandas as pd
+from pymongo.read_preferences import ReadPreference
 
 # NOC modules
 from .base import FieldInfo, BaseDataSource
@@ -21,6 +22,7 @@ from noc.inv.models.platform import Platform
 from noc.inv.models.firmware import Firmware
 from noc.inv.models.vendor import Vendor
 from noc.inv.models.networksegment import NetworkSegment
+from noc.inv.models.discoveryid import DiscoveryID
 from noc.project.models.project import Project
 
 
@@ -36,7 +38,7 @@ class ManagedObjectDS(BaseDataSource):
             description="Object Profile Name",
             internal_name="object_profile__name",
         ),
-        # FieldInfo(name="hostname", description="Object Hostname"),
+        FieldInfo(name="hostname", description="Object Hostname", internal_name="id"),
         # FieldInfo(name="status", description="Object Status"),
         FieldInfo(name="address", description="Object IP Address"),
         FieldInfo(name="vendor", description="Object Vendor"),
@@ -113,7 +115,7 @@ class ManagedObjectDS(BaseDataSource):
 
     @classmethod
     async def query(cls, fields: Optional[Iterable[str]] = None, *args, **kwargs) -> pd.DataFrame:
-        data = [mm async for mm in cls.iter_query(fields, with_index=True)]
+        data = [mm async for mm in cls.iter_query(fields, require_index=True)]
         return pd.DataFrame.from_records(data, index="id")
 
     @classmethod
@@ -122,10 +124,10 @@ class ManagedObjectDS(BaseDataSource):
     ) -> Iterable[Dict[str, Any]]:
         fields = set(fields or [])
         q_fields = [f.internal_name or f.name for f in cls.fields if not fields or f.name in fields]
-        if "with_index" in kwargs:
+        if kwargs.get("require_index") and "id" not in q_fields:
             q_fields += ["id"]
         mos = ManagedObject.objects.filter()
-        extra_select = {}
+        extra_select, hostname_map, segment_map = {}, {}, {}
         if not fields or "attr_hwversion" in fields:
             extra_select["attr_hwversion"] = cls.ATTR_QUERY % "HW version"
         if not fields or "attr_bootprom" in fields:
@@ -136,6 +138,20 @@ class ManagedObjectDS(BaseDataSource):
             extra_select["attr_serialnumber"] = cls.ATTR_QUERY % "Serial Number"
         if extra_select:
             mos = mos.extra(select=extra_select)
+        if not fields or "hostname" in fields:
+            hostname_map = {
+                val["object"]: val["hostname"]
+                for val in DiscoveryID._get_collection()
+                .with_options(read_preference=ReadPreference.SECONDARY_PREFERRED)
+                .find({"hostname": {"$exists": 1}}, {"object": 1, "hostname": 1})
+            }
+        if not fields or "segment" in fields:
+            segment_map = {
+                str(n["_id"]): n["name"]
+                for n in NetworkSegment._get_collection()
+                .with_options(read_preference=ReadPreference.SECONDARY_PREFERRED)
+                .find({}, {"name": 1})
+            }
         for mo in mos.values(*q_fields):
             if "caps" in mo:
                 caps = cls.get_caps(mo.pop("caps"))
@@ -147,27 +163,27 @@ class ManagedObjectDS(BaseDataSource):
             if not fields or "physical_iface_count" in fields:
                 mo["physical_iface_count"] = caps.get("DB | Interfaces", 0)
             if "profile" in mo:
-                mo["profile"] = Profile.get_by_id(mo["profile"]).name if mo["profile"] else ""
+                mo["profile"] = Profile.get_by_id(mo["profile"]).name if mo["profile"] else None
             if "platform" in mo:
                 platform = mo.pop("platform")
-                mo["model"] = Platform.get_by_id(platform).name if platform else ""
+                mo["model"] = Platform.get_by_id(platform).name if platform else None
             if "sw_version" in mo:
                 sw_version = mo.pop("sw_version")
-                mo["version"] = Firmware.get_by_id(sw_version).version if sw_version else ""
+                mo["version"] = Firmware.get_by_id(sw_version).version if sw_version else None
             if "vendor" in mo:
-                mo["vendor"] = Vendor.get_by_id(mo["vendor"]).name if mo["vendor"] else ""
+                mo["vendor"] = Vendor.get_by_id(mo["vendor"]).name if mo["vendor"] else None
+            if hostname_map:
+                mo["hostname"] = hostname_map.get(mo["id"])
+            if segment_map:
+                mo["hostname"] = segment_map.get(mo["segment"])
             if "auth_profile" in mo:
                 mo["auth_profile"] = (
-                    AuthProfile.get_by_id(mo["auth_profile"]).name if mo["auth_profile"] else ""
-                )
-            if "segment" in mo:
-                mo["segment"] = (
-                    NetworkSegment.get_by_id(mo["segment"]).name if mo["segment"] else ""
+                    AuthProfile.get_by_id(mo["auth_profile"]).name if mo["auth_profile"] else None
                 )
             if "administrative_domain__name" in mo:
                 mo["administrative_domain"] = mo.pop("administrative_domain__name")
             if "object_profile__name" in mo:
                 mo["object_profile"] = mo.pop("object_profile__name")
             if "project" in mo:
-                mo["project"] = Project.get_by_id(mo["project"]).name if mo["project"] else ""
+                mo["project"] = Project.get_by_id(mo["project"]).name if mo["project"] else None
             yield mo
