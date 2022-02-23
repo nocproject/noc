a75f9b78003f339869f64746b760c4f4014d02ac
Andrey Vertiprahov <aversanta@gmail.com>
2022-02-23 22:12:58 +0500

    Add Attributes fields.

--- core/datasources/managedobjectds.py
+++ core/datasources/managedobjectds.py
@@ -14,12 +14,14 @@ import pandas as pd
 # NOC modules
 from .base import FieldInfo, BaseDataSource
 from noc.sa.models.managedobject import ManagedObject
+from noc.sa.models.authprofile import AuthProfile
+from noc.sa.models.profile import Profile
 from noc.inv.models.capability import Capability
 from noc.inv.models.platform import Platform
 from noc.inv.models.firmware import Firmware
 from noc.inv.models.vendor import Vendor
 from noc.inv.models.networksegment import NetworkSegment
-from noc.sa.models.authprofile import AuthProfile
+from noc.project.models.project import Project
 
 
 class ManagedObjectDS(BaseDataSource):
@@ -28,6 +30,12 @@ class ManagedObjectDS(BaseDataSource):
     fields = [
         FieldInfo(name="id", description="Object Id", type="int64"),
         FieldInfo(name="name", description="Object Name"),
+        FieldInfo(name="profile", description="Profile Name"),
+        FieldInfo(
+            name="object_profile",
+            description="Object Profile Name",
+            internal_name="object_profile__name",
+        ),
         # FieldInfo(name="hostname", description="Object Hostname"),
         # FieldInfo(name="status", description="Object Status"),
         FieldInfo(name="address", description="Object IP Address"),
@@ -35,18 +43,13 @@ class ManagedObjectDS(BaseDataSource):
         FieldInfo(name="model", description="Object Model", internal_name="platform"),
         FieldInfo(name="sw_version", description="Object Firmware", internal_name="version"),
         # Attributes fields
-        # FieldInfo(
-        #     name="attr_hwversion", description="Object HW Version Attribute", internal_name="attrs"
-        # ),
-        # FieldInfo(
-        #     name="attr_bootprom", description="Object Boot Prom Attribute", internal_name="attrs"
-        # ),
-        # FieldInfo(name="attr_patch", description="Object Patch Attribute", internal_name="attrs"),
-        # FieldInfo(
-        #     name="attr_serialnumber",
-        #     description="Object Serial Number Attribute",
-        #     internal_name="attrs",
-        # ),
+        FieldInfo(name="attr_hwversion", description="Object HW Version Attribute"),
+        FieldInfo(name="attr_bootprom", description="Object Boot Prom Attribute"),
+        FieldInfo(name="attr_patch", description="Object Patch Attribute"),
+        FieldInfo(
+            name="attr_serialnumber",
+            description="Object Serial Number Attribute",
+        ),
         # Location Fields
         FieldInfo(
             name="administrativedomain",
@@ -83,6 +86,12 @@ class ManagedObjectDS(BaseDataSource):
         ),
     ]
 
+    ATTR_QUERY = """
+    SELECT value
+    FROM sa_managedobjectattribute
+    WHERE key='%s' AND managed_object_id=sa_managedobject.id
+    """
+
     @classmethod
     def get_caps(cls, *args: List[Dict[str, Any]]) -> Dict[str, Any]:
         """
@@ -115,7 +124,19 @@ class ManagedObjectDS(BaseDataSource):
         q_fields = [f.internal_name or f.name for f in cls.fields if not fields or f.name in fields]
         if "with_index" in kwargs:
             q_fields += ["id"]
-        for mo in ManagedObject.objects.values(*q_fields):
+        mos = ManagedObject.objects.filter()
+        extra_select = {}
+        if not fields or "attr_hwversion" in fields:
+            extra_select["attr_hwversion"] = cls.ATTR_QUERY % "HW version"
+        if not fields or "attr_bootprom" in fields:
+            extra_select["attr_bootprom"] = cls.ATTR_QUERY % "Boot PROM"
+        if not fields or "attr_patch" in fields:
+            extra_select["attr_patch"] = cls.ATTR_QUERY % "Patch Version"
+        if not fields or "attr_serialnumber" in fields:
+            extra_select["attr_serialnumber"] = cls.ATTR_QUERY % "Serial Number"
+        if extra_select:
+            mos = mos.extra(select=extra_select)
+        for mo in mos.values(*q_fields):
             if "caps" in mo:
                 caps = cls.get_caps(mo.pop("caps"))
             else:
@@ -125,6 +146,8 @@ class ManagedObjectDS(BaseDataSource):
                 mo["link_count"] = len(links)
             if not fields or "physical_iface_count" in fields:
                 mo["physical_iface_count"] = caps.get("DB | Interfaces", 0)
+            if "profile" in mo:
+                mo["profile"] = Profile.get_by_id(mo["profile"]).name if mo["profile"] else ""
             if "platform" in mo:
                 platform = mo.pop("platform")
                 mo["model"] = Platform.get_by_id(platform).name if platform else ""
@@ -143,4 +166,8 @@ class ManagedObjectDS(BaseDataSource):
                 )
             if "administrative_domain__name" in mo:
                 mo["administrative_domain"] = mo.pop("administrative_domain__name")
+            if "object_profile__name" in mo:
+                mo["object_profile"] = mo.pop("object_profile__name")
+            if "project" in mo:
+                mo["project"] = Project.get_by_id(mo["project"]).name if mo["project"] else ""
             yield mo
