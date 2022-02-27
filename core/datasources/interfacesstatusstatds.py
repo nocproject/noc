504f8e387098ed344ffc40d2517ef2920f2d8837
Andrey Vertiprahov <aversanta@gmail.com>
2022-02-27 04:36:46 +0000

    Additional Report Datasources

---
+++ core/datasources/interfacesstatusstatds.py
@@ -0,0 +1,112 @@
+# ----------------------------------------------------------------------
+# Interface Status Stat Datasource
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2022 The NOC Project
+# See LICENSE for details
+# ----------------------------------------------------------------------
+
+# Python Modules
+from typing import Optional, Iterable, Dict, Any, List
+
+# Third-party modules
+import pandas as pd
+from pymongo.read_preferences import ReadPreference
+
+# NOC modules
+from .base import FieldInfo, BaseDataSource
+from noc.inv.models.interface import Interface
+
+
+class InterfacesStatusStatDS(BaseDataSource):
+    name = "interfacesstatusstat"
+
+    # "Up/10G", "Up/1G", "Up/100M", "Up/10M", "Down/-", "-"
+    fields = [
+        FieldInfo(name="managed_object_id", type="int64"),
+        FieldInfo(name="Up_10G", type="int64"),
+        FieldInfo(name="Up_1G", type="int64"),
+        FieldInfo(name="Up_100M", type="int64"),
+        FieldInfo(name="Up_10M", type="int64"),
+        FieldInfo(name="Down_-", type="int64"),
+        FieldInfo(name="-", type="int64"),
+    ]
+
+    @staticmethod
+    def humanize_speed(speed: Optional[int]) -> str:
+        if not speed:
+            return "-"
+        for t, n in [(1000000, "G"), (1000, "M"), (1, "k")]:
+            if speed >= t:
+                if speed // t * t == speed:
+                    return "%d%s" % (speed // t, n)
+                else:
+                    return "%.2f%s" % (float(speed) / t, n)
+        return str(speed)
+
+    @classmethod
+    def get_result(cls, fields: Dict[str, int], statuses: List[Dict[str, int]]) -> Dict[str, int]:
+        # @todo Column filter
+        r = fields.copy()
+        for ss in statuses:
+            oper_status = {True: "Up", False: "Down", None: "-"}[ss.get("oper_status")]
+            key = f'{oper_status}_{cls.humanize_speed(ss.get("in_speed"))}'
+            if key in fields:
+                r[key] = int(ss["count"])
+        return r
+
+    @classmethod
+    async def query(cls, fields: Optional[Iterable[str]] = None, *args, **kwargs) -> pd.DataFrame:
+        data = [mm async for mm in cls.iter_query(fields, require_index=True)]
+        return pd.DataFrame.from_records(data, index="managed_object_id")
+
+    @classmethod
+    async def iter_query(
+        cls, fields: Optional[Iterable[str]] = None, *args, **kwargs
+    ) -> Iterable[Dict[str, Any]]:
+
+        match = {"type": "physical"}
+        query_fields = {ff.name: None for ff in cls.fields if not fields or ff.name in fields}
+        for data in (
+            Interface._get_collection()
+            .with_options(read_preference=ReadPreference.SECONDARY_PREFERRED)
+            .aggregate(
+                [
+                    {"$match": match},
+                    {
+                        "$group": {
+                            "_id": {
+                                "in_speed": "$in_speed",
+                                "managed_object": "$managed_object",
+                                "oper_status": "$oper_status",
+                            },
+                            "count": {"$sum": 1},
+                        }
+                    },
+                    {
+                        "$project": {
+                            "in_speed": "$_id.in_speed",
+                            "oper_status": "$_id.oper_status",
+                            "count": 1,
+                            "_id": 0,
+                            "managed_object": "$_id.managed_object",
+                        }
+                    },
+                    {
+                        "$group": {
+                            "_id": "$managed_object",
+                            "result": {
+                                "$push": {
+                                    "count": "$count",
+                                    "in_speed": "$in_speed",
+                                    "oper_status": "$oper_status",
+                                }
+                            },
+                        }
+                    },
+                ],
+                allowDiskUse=True,
+            )
+        ):
+            r = cls.get_result(query_fields, data["result"])
+            r["managed_object_id"] = data["_id"]
+            yield r
---
+++ core/datasources/managedobjectlocationds.py
@@ -0,0 +1,129 @@
+# ----------------------------------------------------------------------
+# Managed Object Location Datasource
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2022 The NOC Project
+# See LICENSE for details
+# ----------------------------------------------------------------------
+
+# Python Modules
+from typing import Optional, Iterable, Dict, Any
+
+# Third-party modules
+import pandas as pd
+from pymongo.read_preferences import ReadPreference
+
+# NOC modules
+from .base import FieldInfo, BaseDataSource
+from noc.inv.models.object import Object
+from noc.sa.models.managedobject import ManagedObject
+
+
+class ManagedObjectLocationDS(BaseDataSource):
+    name = "managedobjectlocationds"
+
+    fields = [
+        FieldInfo(name="managed_object_id", type="int64"),
+        FieldInfo(name="container_id", type="int64"),
+        FieldInfo(name="location_address"),
+    ]
+
+    @classmethod
+    async def query(cls, fields: Optional[Iterable[str]] = None, *args, **kwargs) -> pd.DataFrame:
+        data = [mm async for mm in cls.iter_query(fields, require_index=True)]
+        return pd.DataFrame.from_records(data, index="container_id")
+
+    @classmethod
+    async def iter_query(
+        cls, fields: Optional[Iterable[str]] = None, *args, **kwargs
+    ) -> Iterable[Dict[str, Any]]:
+        match = {"data.interface": "address"}
+        value = (
+            Object._get_collection()
+            .with_options(read_preference=ReadPreference.SECONDARY_PREFERRED)
+            .aggregate(
+                [
+                    {"$match": match},
+                    {
+                        "$project": {
+                            "data": {
+                                "$filter": {
+                                    "input": "$data",
+                                    "as": "d1",
+                                    "cond": {
+                                        "$and": [
+                                            {"$eq": ["$$d1.interface", "address"]},
+                                            {
+                                                "$or": [
+                                                    {"$not": ["$$d1.scope"]},
+                                                    {"$eq": ["$$d1.scope", ""]},
+                                                ]
+                                            },
+                                            {"$eq": ["$$d1.attr", "text"]},
+                                        ]
+                                    },
+                                }
+                            },
+                            "parent_name": "$name",
+                        }
+                    },
+                    {
+                        "$project": {
+                            "parent_address": {"$arrayElemAt": ["$data.value", 0]},
+                            "parent_name": 1,
+                            "_id": 1,
+                        }
+                    },
+                    {
+                        "$lookup": {
+                            "from": "noc.objects",
+                            "localField": "_id",
+                            "foreignField": "container",
+                            "as": "child_cont",
+                        }
+                    },
+                    {
+                        "$project": {
+                            "parent_address": 1,
+                            "parent_name": 1,
+                            "_id": 1,
+                            "child_cont._id": 1,
+                            "child_cont.name": 1,
+                        }
+                    },
+                    {"$unwind": {"path": "$child_cont", "preserveNullAndEmptyArrays": True}},
+                ]
+            )
+        )
+        r = {}
+        managed_object_map = {
+            container: mo_id
+            for mo_id, container in (ManagedObject.objects.filter().values_list("id", "container"))
+        }
+        for v in value:
+            cid = str(v["_id"])
+            if "child_cont" in v and "parent_address" in v and str(v["child_cont"]["_id"]) not in r:
+                # r[str(v["child_cont"]["_id"])] = v["parent_address"].strip()
+                # cont_map[str(v["child_cont"]["_id"])] = v["parent_address"].strip()
+                r = {
+                    "container_id": str(v["child_cont"]["_id"]),
+                    "location_address": v["parent_address"].strip(),
+                }
+                if r["container_id"] in managed_object_map:
+                    r["managed_object_id"] = managed_object_map[r["container_id"]]
+                yield r
+            if cid not in r and "parent_address" in v:
+                # r[cid] = v["parent_address"].strip()
+                r = {"container_id": cid, "location_address": v["parent_address"].strip()}
+                if r["container_id"] in managed_object_map:
+                    r["managed_object_id"] = managed_object_map[r["container_id"]]
+                yield r
+
+        # for mo_id, container in (
+        #         ManagedObject.objects.filter(id__in=self.sync_ids)
+        #                 .values_list("id", "container")
+        #                 .order_by("id")
+        # ):
+        #     if container in r:
+        #         yield mo_id, r[container]
+        #     elif container in cont_map:
+        #         yield mo_id, cont_map[container]
