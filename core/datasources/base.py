e862f12ed0893a7f6f85875e356c05d43121b015
Andrey Vertiprahov <aversanta@gmail.com>
2022-04-13 18:49:20 +0500

    Add query_sync method to BaseDataSource.

--- core/datasources/base.py
+++ core/datasources/base.py
@@ -7,6 +7,7 @@
 
 # Python modules
 from dataclasses import dataclass
+from functools import partial
 
 # Third-party modules
 import pandas as pd
@@ -28,6 +29,12 @@ class BaseDataSource(object):
 
     name: str
     fields: List[FieldInfo]
+
+    @classmethod
+    def query_sync(cls, fields: Optional[Iterable[str]] = None, *args, **kwargs) -> pd.DataFrame:
+        from noc.core.ioloop.util import run_sync
+
+        return run_sync(partial(cls.query, fields, *args, **kwargs))
 
     @classmethod
     async def query(cls, fields: Optional[Iterable[str]] = None, *args, **kwargs) -> pd.DataFrame:
