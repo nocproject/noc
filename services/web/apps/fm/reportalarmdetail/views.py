293438fc829164f6c72385f7fa64cb5782fbcf36
Andrey Vertiprahov <aversanta@gmail.com>
2022-04-16 17:25:59 +0500

    Fix ReportAlarmDetail output name format.

--- services/web/apps/fm/reportalarmdetail/views.py
+++ services/web/apps/fm/reportalarmdetail/views.py
@@ -8,12 +8,12 @@
 # Python modules
 import datetime
 from io import BytesIO
-from functools import partial
 from zipfile import ZipFile, ZIP_DEFLATED
 from tempfile import TemporaryFile
 
 # Third-party modules
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotFound
+import pandas as pd
 
 # NOC modules
 from noc.lib.app.extapplication import ExtApplication, view
@@ -21,32 +21,39 @@ from noc.sa.interfaces.base import StringParameter, IntParameter, ObjectIdParame
 from noc.sa.models.useraccess import UserAccess
 from noc.sa.models.administrativedomain import AdministrativeDomain
 from noc.core.translation import ugettext as _
-from noc.lib.app.reportdatasources.base import ReportDataSource
-from noc.lib.app.reportdatasources.loader import loader
 from noc.core.comp import smart_bytes
-from noc.core.ioloop.util import run_sync
 from noc.core.datasources.loader import loader as ds_loader
+from noc.core.datasources.base import BaseDataSource
 
 
-class ReportDsAlarms(object):
-    def __init__(self, filters=None):
-        self.datasource = ds_loader["reportdsalarms"]
-        self.result = None
-        self.filters = filters or {}
+def get_column_width(name):
+    excel_column_format = {
+        "ID": 6,
+        "OBJECT_NAME": 38,
+        "OBJECT_STATUS": 10,
+        "OBJECT_PROFILE": 17,
+        "OBJECT_PLATFORM": 25,
+        "AVAIL": 6,
+        "ADMIN_DOMAIN": 25,
+        "PHYS_INTERFACE_COUNT": 5,
+    }
+    name = name.upper()
+    if name.startswith("Up") or name.startswith("Down") or name.startswith("-"):
+        return 8
+    elif name.startswith("ADM_PATH"):
+        return excel_column_format["ADMIN_DOMAIN"]
+    elif name in excel_column_format:
+        return excel_column_format[name]
+    return 15
 
-    def run(self, start, end):
-        self.filters["start"] = start
-        self.filters["end"] = end
-        self.result = run_sync(partial(self.datasource.query, **self.filters))
 
-    def report_csv(self):
-        return self.result.to_csv()
-
-    def report_xlsx(self):
-        response = BytesIO()
-        self.result.to_excel(response)
-        response.seek(0)
-        return response.getvalue()
+def get_col_widths(dataframe: "pd.DataFrame"):
+    # First we find the maximum length of the index column
+    idx_max = max([len(str(s)) for s in dataframe.index.values] + [len(str(dataframe.index.name))])
+    # Then, we concatenate this to the max of the lengths of column name and its values for each column, left to right
+    return [idx_max] + [
+        max([len(str(s)) for s in dataframe[col].values] + [len(col)]) for col in dataframe.columns
+    ]
 
 
 class ReportAlarmDetailApplication(ExtApplication):
@@ -55,6 +62,31 @@ class ReportAlarmDetailApplication(ExtApplication):
 
     SEGMENT_PATH_DEPTH = 7
     CONTAINER_PATH_DEPTH = 7
+
+    HEADER_ROW = {
+        "root_id": _("ROOT_ID"),
+        "from_ts": _("FROM_TS"),
+        "to_ts": _("TO_TS"),
+        "duration": _("DURATION_SEC"),
+        "object_name": _("OBJECT_NAME"),
+        "object_address": _("OBJECT_ADDRESS"),
+        "object_hostname": _("OBJECT_HOSTNAME"),
+        "object_profile": _("OBJECT_PROFILE"),
+        "object_object_profile": _("OBJECT_PROFILE"),
+        "object_admdomain": _("OBJECT_ADMDOMAIN"),
+        "object_platform": _("OBJECT_PLATFORM"),
+        "object_version": _("OBJECT_VERSION"),
+        "object_project": _("OBJECT_PROJECT"),
+        "alarm_class": _("ALARM_CLASS"),
+        "alarm_subject": _("ALARM_SUBJECT"),
+        "maintenance": _("MAINTENANCE"),
+        "objects": _("OBJECTS"),
+        "subscribers": _("SUBSCRIBERS"),
+        "tt": _("TT"),
+        "escalation_ts": _("ESCALATION_TS"),
+        "location": _("LOCATION"),
+        "container_address": _("CONTAINER_ADDRESS"),
+    }
 
     @view(
         "^download/$",
@@ -103,8 +135,13 @@ class ReportAlarmDetailApplication(ExtApplication):
         ids=None,
         enable_autowidth=False,
     ):
-        filters = []
         d_filters = {}
+        out_columns = []
+        for cc in columns.split(","):
+            if cc == "alarm_id":
+                # out_columns.append("alarm_id")
+                continue
+            out_columns.append(cc)
 
         ads = []
         if administrative_domain:
@@ -131,6 +168,8 @@ class ReportAlarmDetailApplication(ExtApplication):
             td = datetime.datetime.strptime(to_date, "%d.%m.%Y") + datetime.timedelta(days=1)
         else:
             return HttpResponseBadRequest(_("One param - FROM_DATE or IDS required"))
+        d_filters["start"] = fd
+        d_filters["end"] = td
         for name, values in [
             ("min_duration", min_duration),
             ("max_duration", max_duration),
@@ -147,57 +186,86 @@ class ReportAlarmDetailApplication(ExtApplication):
             if not values:
                 continue
             if values and isinstance(values, list):
-                filters += [{"name": name, "values": values}]
+                # filters += [{"name": name, "values": values}]
                 d_filters[name] = values
             elif values:
-                filters += [{"name": name, "values": [values]}]
+                # filters += [{"name": name, "values": [values]}]
                 d_filters[name] = [values]
         if source in ["long_archive"]:
             report_ds = "reportdsalarmsbiarchive"
             o_format = "csv_zip"
-
             if td - fd > datetime.timedelta(days=390):
                 return HttpResponseBadRequest(
                     _(
                         "Report more than 1 year not allowed. If nedeed - request it from Administrator"
                     )
                 )
+            report: "BaseDataSource" = ds_loader[report_ds]
+            out_columns = [ff.name for ff in report.fields]
         else:
             report_ds = "reportdsalarms"
-        report: ReportDataSource = loader[report_ds]
+            report: "BaseDataSource" = ds_loader[report_ds]
         if not report:
             return HttpResponseNotFound(_(f"Report DataSource {report_ds} Not found"))
-        # data = report(
-        #     fields=columns.split(","),
-        #     objectids=ids or [],
-        #     allobjectids=False,
-        #     start=fd,
-        #     end=td,
-        #     filters=filters,
-        # )
-        # report = ReportDsAlars(d_filters)
-        data = ReportDsAlarms(d_filters)
-        data.run(start=fd, end=td)
 
-        filename = f'alarm_detail_report_{datetime.datetime.now().strftime("%Y%m%d")}'
+        data = report.query_sync(fields=out_columns, **d_filters)
+
+        filename = f'alarms_detail_report_{datetime.datetime.now().strftime("%Y%m%d")}'
         if o_format == "csv":
-            response = HttpResponse(data.report_csv(), content_type="text/csv")
-            response["Content-Disposition"] = 'attachment; filename="%s"' % filename
+            response = HttpResponse(
+                data.to_csv(
+                    header=[self.HEADER_ROW.get(cc, cc) for cc in out_columns],
+                    sep=";",
+                    quotechar='"',
+                ),
+                content_type="text/csv",
+            )
+            response["Content-Disposition"] = f'attachment; filename="{filename}.csv"'
             return response
         elif o_format == "csv_zip":
             response = BytesIO()
             f = TemporaryFile(mode="w+b")
-            f.write(smart_bytes(data.report_csv()))
+            f.write(
+                smart_bytes(
+                    data.to_csv(
+                        header=[self.HEADER_ROW.get(cc, cc) for cc in out_columns],
+                        columns=out_columns,
+                        sep=";",
+                        quotechar='"',
+                    )
+                )
+            )
             f.seek(0)
             with ZipFile(response, "w", compression=ZIP_DEFLATED) as zf:
                 zf.writestr(filename, f.read())
-                zf.filename = "%s.zip" % filename
+                zf.filename = "%s.csv" % filename
             response.seek(0)
             response = HttpResponse(response.getvalue(), content_type="application/zip")
-            response["Content-Disposition"] = 'attachment; filename="%s.zip"' % filename
+            response["Content-Disposition"] = f'attachment; filename="{filename}.zip"'
             return response
         elif o_format == "xlsx":
-            response = HttpResponse(data.report_xlsx(), content_type="application/vnd.ms-excel")
-            response["Content-Disposition"] = 'attachment; filename="alarms.xlsx"'
+            response = BytesIO()
+            writer = pd.ExcelWriter(response, engine="xlsxwriter")
+            writer.book.add_format({"bottom": 1, "left": 1, "right": 1, "top": 1})
+            data.to_excel(
+                writer,
+                sheet_name="Alarms",
+                header=[self.HEADER_ROW.get(cc, cc) for cc in out_columns],
+                index="alarm_id" in columns,
+                columns=out_columns,
+            )
+            #
+            worksheet = writer.sheets["Alarms"]
+            (max_row, max_col) = data.shape
+            worksheet.autofilter(0, 0, max_row, len(out_columns))
+            worksheet.freeze_panes(1, 0)
+            if enable_autowidth:
+                for i, width in enumerate(get_col_widths(data)):
+                    worksheet.set_column(i, i, width)
+            #
+            writer.close()
+            response.seek(0)
+            response = HttpResponse(response, content_type="application/vnd.ms-excel")
+            response["Content-Disposition"] = f'attachment; filename="{filename}.xlsx"'
             response.close()
             return response
