38a0ea76ce5949122a2f31a089dfe9933870a952
Andrey Vertiprahov <aversanta@gmail.com>
2022-04-24 13:50:41 +0500

    Fix typo.

--- core/script/cli/cli.py
+++ core/script/cli/cli.py
@@ -262,14 +262,12 @@ class CLI(BaseCLI):
                         self.profile.send_on_syntax_error, self.command, error_text
                     )
                 self.error = self.script.CLISyntaxError(error_text)
-                buffer = b""
             # Then check for operation error
             if (
                 self.profile.rx_pattern_operation_error_str
                 and self.profile.rx_pattern_operation_error.search(buffer)
             ):
                 self.error = self.script.CLIOperationError(buffer)
-                buffer = b""
             # Parse all possible objects
             while buffer and not self.error:
                 pr = parser(smart_text(buffer, errors="replace"))
