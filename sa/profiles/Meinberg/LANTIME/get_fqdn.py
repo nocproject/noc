5429d4cf73d17f8cf2331dc8ac4652cb8430b7dd
Andrey Vertiprahov <aversanta@gmail.com>
2022-04-26 18:18:01 +0000

    Meinberg.LANTIME. Cleanup black.

--- sa/profiles/Meinberg/LANTIME/get_fqdn.py
+++ sa/profiles/Meinberg/LANTIME/get_fqdn.py
@@ -1,31 +1,20 @@
-# -*- coding: utf-8 -*-
 # ---------------------------------------------------------------------
 # Meinberg.LANTIME.get_fqdn
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2018 The NOC Project
+# Copyright (C) 2007-2022 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
 # NOC modules
-from noc.core.script.base import BaseScript
+from noc.sa.profiles.Generic.get_fqdn import Script as BaseScript
 from noc.sa.interfaces.igetfqdn import IGetFQDN
 
 
 class Script(BaseScript):
     name = "Meinberg.LANTIME.get_fqdn"
     interface = IGetFQDN
+    always_prefer = "S"
 
-    def execute(self):
-        if self.has_snmp():
-            # and self.access_profile.snmp_ro:
-            try:
-                # sysName.0
-                v = self.snmp.get("1.3.6.1.2.1.1.5.0", cached=True)
-                if v:
-                    return v
-            except self.snmp.TimeOutError:
-                pass
+    def execute_cli(self, **kwargs):
         v = self.cli("uname -n").strip()
-        fqdn = v
-
-        return fqdn
+        return v
---
+++ sa/profiles/Meinberg/LANTIME/profile.py
@@ -0,0 +1,34 @@
+# ---------------------------------------------------------------------
+# Vendor: MEINBERG
+# OS:     LANTIME
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2022 The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+
+# NOC modules
+from noc.core.profile.base import BaseProfile
+
+
+class Profile(BaseProfile):
+    name = "Meinberg.LANTIME"
+
+    # supported_schemes = [NOCProfile.TELNET, NOCProfile.SSH]
+    pattern_username = rb"^((?!Last)\S+ login|[Ll]ogin):"
+    pattern_password = rb"^[Pp]assword:"
+
+    # use default BASH promt PS1='[\u@\h \W]\$ '
+    # pattern_prompt = r"^\[\S+@\S+\s\S+](#|\$)\s"
+    pattern_prompt = rb"\[\S+@\S+\s\S+](#|\$)\s"
+    pattern_syntax_error = (
+        rb"^(bash: \S+: command not found...\r\n|-\w+: \w+: not found|"
+        rb"-\w+: \w+: No such file or directory|\w+: \w+: command not found|"
+        rb"\w+: \w+: \w+: No such file or directory)"
+    )
+    # pattern_more = [
+    #    (r"Install package.*\[N/y\]\s$", "\n"),
+    #    (r"Is this ok \[y/N\]: ", "y\n")
+    # ]
+    command_disable_pager = "LANG=en_US.UTF-8 ; PATH=$PATH:/sbin:/usr/sbin ; PROMPT_COMMAND=''"
+    command_super = "sudo bash"
+    command_exit = "exit"
