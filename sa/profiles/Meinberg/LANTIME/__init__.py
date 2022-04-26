652cacb008ae7b44260862b7b5da84e17ff7dfd9
Andrey Vertiprahov <aversanta@gmail.com>
2022-04-26 22:45:29 +0500

    Meinberg.LANTIME. Cleanup black.

--- sa/profiles/Meinberg/LANTIME/__init__.py
+++ sa/profiles/Meinberg/LANTIME/__init__.py
@@ -21,16 +21,16 @@ class Profile(BaseProfile):
     # use default BASH promt PS1='[\u@\h \W]\$ '
     # pattern_prompt = r"^\[\S+@\S+\s\S+](#|\$)\s"
     pattern_prompt = r"\[\S+@\S+\s\S+](#|\$)\s"
-    pattern_syntax_error = \
-        r"^(bash: \S+: command not found...\r\n|-\w+: \w+: not found|" \
-        r"-\w+: \w+: No such file or directory|\w+: \w+: command not found|" \
+    pattern_syntax_error = (
+        r"^(bash: \S+: command not found...\r\n|-\w+: \w+: not found|"
+        r"-\w+: \w+: No such file or directory|\w+: \w+: command not found|"
         r"\w+: \w+: \w+: No such file or directory)"
+    )
     # pattern_more = [
     #    (r"Install package.*\[N/y\]\s$", "\n"),
     #    (r"Is this ok \[y/N\]: ", "y\n")
     # ]
-    command_disable_pager = \
-        "LANG=en_US.UTF-8 ; PATH=$PATH:/sbin:/usr/sbin ; PROMPT_COMMAND=''"
+    command_disable_pager = "LANG=en_US.UTF-8 ; PATH=$PATH:/sbin:/usr/sbin ; PROMPT_COMMAND=''"
     command_super = "sudo bash"
     command_exit = "exit"
     command_more = "\n"
