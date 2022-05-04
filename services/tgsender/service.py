e1f3349afdec40309e037098d0a7210d05330b84
sysfaray <sysfar@gmail.com>
2022-05-04 18:41:59 +0300

    TGSender fix markdown

--- services/tgsender/service.py
+++ services/tgsender/service.py
@@ -2,7 +2,7 @@
 # ----------------------------------------------------------------------
 # tgsender service
 # ----------------------------------------------------------------------
-# Copyright (C) 2007-2021 The NOC Project
+# Copyright (C) 2007-2022 The NOC Project
 # See LICENSE for details
 # ----------------------------------------------------------------------
 
@@ -59,7 +59,7 @@ class TgSenderService(FastAPIService):
     @staticmethod
     def escape_markdown(text):
         """Helper function to escape telegram markup symbols"""
-        escape_chars = r"\*_`"
+        escape_chars = r"\*_`["
         return re.sub(r"([%s])" % escape_chars, r"\\\1", text)
 
     def send_tb(self, topic: str, data: str) -> None:
