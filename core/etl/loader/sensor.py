f584233008b4e2cda6b54fa7a35179b9540b3ee9
Andrey Vertiprahov <aversanta@gmail.com>
2022-05-25 12:26:08 +0000

    Merge branch 'noc-1515' into 'master'
    noc/noc#1515 Add Sensor ETL model.
    
    See merge request noc/noc!5371
    
    (cherry picked from commit 1ae1b6cb6cde46bfd21dc5075eb0dd248e497e59)
    
    ee285b76 Add Sensor ETL model.
    60b69dc3 Set workflow_event_model to Sensor.
    03da772c Add remote_system, remote_id to Sensor.
    5ccb67be Fix typo.
    21029c9b Cleanup MeasurementUnits on sensor loader.

---
+++ core/etl/loader/sensor.py
@@ -0,0 +1,43 @@
+# ----------------------------------------------------------------------
+# Sensor loader
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2020 The NOC Project
+# See LICENSE for details
+# ----------------------------------------------------------------------
+
+# NOC modules
+from .base import BaseLoader
+from ..models.sensor import Sensor
+from noc.inv.models.object import Object
+from noc.inv.models.sensor import Sensor as SensorModel
+from noc.pm.models.measurementunits import MeasurementUnits
+
+
+class SensorLoader(BaseLoader):
+    """
+    Sensor loader
+    """
+
+    name = "sensor"
+    model = SensorModel
+    data_model = Sensor
+
+    discard_deferred = True
+    workflow_event_model = True
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.clean_map["units"] = lambda x: MeasurementUnits.get_by_name(x) if x else None
+
+    def find_object(self, v):
+        """
+        Find object by remote system/remote id
+        :param v:
+        :return:
+        """
+        r = super().find_object(v)
+        if not r:
+            oo = Object.get_managed(v["managed_object"])
+            find_query = {"local_id": v["local_id"], "object": oo}
+            r = self.model.objects.filter(**find_query).first()
+        return r
