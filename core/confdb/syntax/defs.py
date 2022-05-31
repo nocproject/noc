5376f030c253e9c313a953466d005405fa9bacd8
Dmitry Volodin <dv@nocproject.org>
2022-05-31 17:47:52 +0200

    Type annotations for ConfDB DEF

--- core/confdb/syntax/defs.py
+++ core/confdb/syntax/defs.py
@@ -1,18 +1,46 @@
 # ----------------------------------------------------------------------
 # Syntax Definitions
 # ----------------------------------------------------------------------
-# Copyright (C) 2007-2019 The NOC Project
+# Copyright (C) 2007-2022 The NOC Project
 # See LICENSE for details
 # ----------------------------------------------------------------------
 
 # Python modules
-from collections import namedtuple
+from dataclasses import dataclass
+from typing import Union, Optional, List
+from xmlrpc.client import boolean
+
+# NOC modules
+from .patterns import BasePattern
 
 
-SyntaxDef = namedtuple(
-    "SyntaxDef", ["token", "children", "required", "name", "multi", "default", "gen"]
-)
+@dataclass
+class SyntaxDef(object):
+    __slots__ = ("token", "children", "required", "name", "multi", "default", "gen")
+    token: Union[str, BasePattern]
+    children: Optional[List["SyntaxDef"]]
+    required: boolean
+    name: Optional[str]
+    multi: boolean
+    default: Optional[str]
+    gen: Optional[str]
 
 
-def DEF(token, children=None, required=False, multi=False, name=None, default=None, gen=None):
-    return SyntaxDef(token, children, required, name, multi, default, gen)
+def DEF(
+    token: Union[str, BasePattern],
+    children: Optional[List[SyntaxDef]] = None,
+    required: boolean = False,
+    multi: boolean = False,
+    name: Optional[str] = None,
+    default: Optional[str] = None,
+    gen: Optional[str] = None,
+) -> SyntaxDef:
+    return SyntaxDef(
+        token=token,
+        children=children,
+        required=required,
+        name=name,
+        multi=multi,
+        default=default,
+        gen=gen,
+    )
