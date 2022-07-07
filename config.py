c6e662da84cab6cde99cdd53ded4d98204dfc602
Andrey Vertiprahov <aversanta@gmail.com>
2022-07-07 13:43:08 +0500

    Add message type.

--- config.py
+++ config.py
@@ -469,7 +469,6 @@ class Config(BaseConfig):
         enable_metric_scopes = ListParameter(item=StringParameter(), default=[])
         #
         enable_snmptrap = BooleanParameter(default=False)
-        enable_syslog = BooleanParameter(default=False)
 
     class mongo(ConfigSection):
         addresses = ServiceParameter(service="mongo", wait=True)
