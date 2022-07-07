8f104eb24d2fbbb0bef2983a33e6fee0eab3163f
Andrey Vertiprahov <aversanta@gmail.com>
2022-07-07 13:39:00 +0500

    Add send syslog message to mx.

--- services/syslogcollector/syslogserver.py
+++ services/syslogcollector/syslogserver.py
@@ -51,4 +51,6 @@ class SyslogServer(UDPServer):
         # Get timestamp
         ts = int(time.time())
         #
-        self.service.register_message(cfg, ts, data, facility=priority >> 3, severity=priority & 7)
+        self.service.register_message(
+            cfg, ts, data, facility=priority >> 3, severity=priority & 7, source_address=address[0]
+        )
