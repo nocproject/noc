b830e9ca0d53ab8a7daf39b658a99b312a151a53
Michael R <rmiclee@gmail.com>
2022-07-08 12:29:20 +0300

    Make creating datagram connection through ready binded sockets

--- core/ioloop/udpserver.py
+++ core/ioloop/udpserver.py
@@ -7,8 +7,12 @@
 
 # Python modules
 import asyncio
+import errno
+import os
+import platform
 import socket
-from typing import Iterable, Tuple, Any
+import sys
+from typing import Iterable, Tuple, Optional, Any
 
 
 class UDPServerProtocol:
@@ -48,7 +52,8 @@ class UDPServer(object):
             yield addr, int(port)
 
     async def listen(self, port: int, address: str = "") -> None:
-        await self.bind_udp_sockets(port, address=address)
+        sockets = self.bind_udp_sockets(port, address=address)
+        await self.create_endpoints(sockets)
 
     def start(self):
         pass
@@ -58,18 +63,141 @@ class UDPServer(object):
         To be overriden
         """
 
-    async def bind_udp_sockets(
+    async def create_endpoints(self, sockets):
+        loop = asyncio.get_running_loop()
+        for sock in sockets:
+            print("*** sock", sock, type(sock))
+            transport, protocol = await loop.create_datagram_endpoint(
+                lambda: UDPServerProtocol(self), sock=sock
+            )
+
+    def bind_udp_sockets(
         self, port, address: str = None, family: int = socket.AF_UNSPEC, flags: Any = None
     ):
+        """Creates listening sockets bound to the given port and address.
+
+        Returns a list of socket objects (multiple sockets are returned if
+        the given address maps to multiple IP addresses, which is most common
+        for mixed IPv4 and IPv6 use).
+
+        Address may be either an IP address or hostname.  If it's a hostname,
+        the server will listen on all IP addresses associated with the
+        name.  Address may be an empty string or None to listen on all
+        available interfaces.  Family may be set to either `socket.AF_INET`
+        or `socket.AF_INET6` to restrict to IPv4 or IPv6 addresses, otherwise
+        both will be used if available.
+
+        The ``backlog`` argument has the same meaning as for
+        `socket.listen() <socket.socket.listen>`.
+
+        ``flags`` is a bitmask of AI_* flags to `~socket.getaddrinfo`, like
+        ``socket.AI_PASSIVE | socket.AI_NUMERICHOST``.
+        """
+        sockets = []
         if address == "":
             address = None
+        if not socket.has_ipv6 and family == socket.AF_UNSPEC:
+            # Python can be compiled with --disable-ipv6, which causes
+            # operations on AF_INET6 sockets to fail, but does not
+            # automatically exclude those results from getaddrinfo
+            # results.
+            # http://bugs.python.org/issue16208
+            family = socket.AF_INET
         if flags is None:
             flags = socket.AI_PASSIVE
-
-        loop = asyncio.get_running_loop()
+        bound_port = None
         for res in set(socket.getaddrinfo(address, port, family, socket.SOCK_DGRAM, 0, flags)):
-            # print("*** res", res, type(res))
             af, socktype, proto, canonname, sockaddr = res
-            transport, protocol = await loop.create_datagram_endpoint(
-                lambda: UDPServerProtocol(self), local_addr=(sockaddr[0], sockaddr[1])
-            )
+            if (
+                platform.system() == "Darwin"
+                and address == "localhost"
+                and af == socket.AF_INET6
+                and sockaddr[3] != 0
+            ):
+                # Mac OS X includes a link-local address fe80::1%lo0 in the
+                # getaddrinfo results for 'localhost'.  However, the firewall
+                # doesn't understand that this is a local address and will
+                # prompt for access (often repeatedly, due to an apparent
+                # bug in its ability to remember granting access to an
+                # application). Skip these addresses.
+                continue
+            try:
+                sock = socket.socket(af, socktype, proto)
+            except OSError as e:
+                if e.args[0] == errno.EAFNOSUPPORT:
+                    continue
+                raise
+            if os.name != "nt":
+                sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            if af == socket.AF_INET6:
+                # On linux, ipv6 sockets accept ipv4 too by default,
+                # but this makes it impossible to bind to both
+                # 0.0.0.0 in ipv4 and :: in ipv6.  On other systems,
+                # separate sockets *must* be used to listen for both ipv4
+                # and ipv6.  For consistency, always disable ipv4 on our
+                # ipv6 sockets and use a separate ipv4 socket when needed.
+                #
+                # Python 2.x on windows doesn't have IPPROTO_IPV6.
+                if hasattr(socket, "IPPROTO_IPV6"):
+                    sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 1)
+
+            # automatic port allocation with port=None
+            # should bind on the same port on IPv4 and IPv6
+            host, requested_port = sockaddr[:2]
+            if requested_port == 0 and bound_port is not None:
+                sockaddr = tuple([host, bound_port] + list(sockaddr[2:]))
+
+            sock.setblocking(0)
+            self.setup_socket(sock)
+            sock.bind(sockaddr)
+            bound_port = sock.getsockname()[1]
+            sockets.append(sock)
+        return sockets
+
+    def enable_reuseport(self) -> bool:
+        """
+        Override if SO_REUSEPORT should be set
+        :return:
+        """
+        return False
+
+    def enable_freebind(self) -> bool:
+        """
+        Override if IP_FREEBIND should be set
+        :return:
+        """
+        return True
+
+    @property
+    def has_reuseport(self) -> bool:
+        return hasattr(socket, "SO_REUSEPORT")
+
+    @property
+    def has_frebind(self) -> bool:
+        return self.get_ip_freebind() is not None
+
+    def setup_socket(self, sock: "socket"):
+        """
+        Called after socket created but before .bind().
+        Can be overriden to adjust socket options in superclasses
+        :param sock: socket instance
+        :return: None
+        """
+        # Set SO_REUSEPORT option
+        if self.has_reuseport and self.enable_reuseport():
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        # Set IP_FREEBIND option
+        if self.has_frebind and self.enable_freebind():
+            sock.setsockopt(socket.SOL_IP, self.get_ip_freebind(), 1)
+
+    def get_ip_freebind(self) -> Optional[int]:
+        """
+        Many python distributions does not include IP_FREEBIND to socket module
+        :return: IP_FREEBIND value or None
+        """
+        if hasattr(socket, "IP_FREEBIND"):
+            # Valid distribution
+            return socket.IP_FREEBIND
+        if sys.platform == "linux2":
+            return 15
+        return None
