2f30e275f3ead65fbbb98e20a070bb23f5e626c1
Andrey Vertiprahov <aversanta@gmail.com>
2022-07-17 00:18:54 +0500

    Huawei.VRP.get_portchannel. Add cleanup interface name from output.

--- sa/profiles/Huawei/VRP/get_portchannel.py
+++ sa/profiles/Huawei/VRP/get_portchannel.py
@@ -1,11 +1,14 @@
 # ---------------------------------------------------------------------
 # Huawei.VRP.get_portchannel
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2014 The NOC Project
+# Copyright (C) 2007-2022 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
+# Python modules
 import re
+
+# NOC modules
 from noc.sa.profiles.Generic.get_portchannel import Script as BaseScript
 from noc.sa.interfaces.igetportchannel import IGetPortchannel
 
@@ -23,6 +26,19 @@ class Script(BaseScript):
     )
 
     def execute_cli(self):
+        """
+            try:
+                trunk = self.cli("display link-aggregation summary", cached=True)
+            except self.CLISyntaxError:
+                return []
+            Need more examples
+
+            version 5.2 produce like this:
+            No link-aggregation group defined at present.
+            version 5.3 produce like this:
+            Error: No valid trunk in the system.
+        :return:
+        """
         if self.is_kernel_3:
             self.logger.info("Huawei 3.XX not portchannel command")
             return []
@@ -30,29 +46,21 @@ class Script(BaseScript):
         try:
             trunk = self.cli("display eth-trunk", cached=True)
         except self.CLISyntaxError:
-            try:
-                trunk = self.cli("display link-aggregation summary", cached=True)
-            except self.CLISyntaxError:
-                return []
-            """
-            Need more examples
-
-            version 5.2 produce like this:
-            No link-aggregation group defined at present.
-            version 5.3 produce like this:
-            Error: No valid trunk in the system.
-            """
             return []
         for match in self.rx_chan_line_vrp5.finditer(trunk):
             r += [
                 {
                     "interface": match.group("interface"),
-                    "members": [
-                        l.split(" ", 1)[0] for l in match.group("members").lstrip("\n").splitlines()
-                    ],
+                    "members": [],
                     "type": {"normal": "S", "static": "L", "lacp": "L", "dynamic": "L"}[
                         match.group("mode").lower()
                     ],
                 }
             ]
+            for ll in match.group("members").lstrip("\n").splitlines():
+                iface = ll.split(" ", 1)[0]
+                if iface.endswith(")"):
+                    # GigabitEthernet0/3/0(hr)
+                    iface = iface.split("(")[0]
+                r[-1]["members"] += [iface]
         return r
