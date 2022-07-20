6fec1b118a16cc9f29a6b33d61d4d58e38e8ae10
Andrey Vertiprahov <aversanta@gmail.com>
2022-07-20 11:05:40 +0000

    noc/noc#1851 Add cfgmxroute datastream for MX Services.

---
+++ services/mx/datastream.py
@@ -0,0 +1,20 @@
+# ----------------------------------------------------------------------
+# MX DataStream client
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2022 The NOC Project
+# See LICENSE for details
+# ----------------------------------------------------------------------
+
+# NOC modules
+from noc.core.datastream.client import DataStreamClient
+
+
+class RouteDataStreamClient(DataStreamClient):
+    async def on_change(self, data):
+        await self.service.update_route(data)
+
+    async def on_delete(self, data):
+        await self.service.delete_route(data["id"])
+
+    async def on_ready(self):
+        await self.service.on_ready()
