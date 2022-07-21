908abfb2ccf8c8cca1ee1bc1e86a93147557affa
bee26 <bee26@ya.ru>
2022-07-21 12:10:08 +0000

    model fix in get_version Huawei.MA5300

--- sa/profiles/Huawei/MA5300/get_version.py
+++ sa/profiles/Huawei/MA5300/get_version.py
@@ -2,7 +2,7 @@
 # Huawei.MA5300.get_version
 # sergey.sadovnikov@gmail.com
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2018 The NOC Project
+# Copyright (C) 2007-2022 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
@@ -26,11 +26,13 @@ class Script(BaseScript):
 
     def execute_snmp(self, **kwargs):
         # slot number
-        slots_count = self.snmp.get(mib["HUAWEI-DEVICE-MIB::hwSlots"])
+        slots_count = self.snmp.get(mib["HUAWEI-DEVICE-MIB::hwSlots", 0])
         version = self.snmp.get(mib["HUAWEI-DEVICE-MIB::hwSysVersion", 0])
         platform = self.snmp.get(mib["HUAWEI-DEVICE-MIB::hwFrameDesc", 0])
-        if platform == "MA5300" and slots_count == 8:
+        if slots_count == 8:
             platform = "MA5303"
+        elif slots_count == 16:
+            platform = "MA5300"
         return {"vendor": "Huawei", "platform": platform, "version": version}
 
     def execute_cli(self, **kwargs):
