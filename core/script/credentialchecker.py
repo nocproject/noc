39bbd4573a4ecebfbe5f967b0705d8722e9e29a6
Andrey Vertiprahov <aversanta@gmail.com>
2022-07-24 10:08:04 +0500

    Add Protocol enum.

--- core/script/credentialchecker.py
+++ core/script/credentialchecker.py
@@ -7,7 +7,9 @@
 
 # Python modules
 import logging
-from typing import Optional, Dict, List
+import enum
+from dataclasses import dataclass
+from typing import Optional, Dict, List, Tuple
 
 # Third-party modules
 import cachetools
@@ -17,10 +19,8 @@ from noc.core.log import PrefixLoggerAdapter
 from noc.core.service.client import open_sync_rpc
 from noc.core.service.error import RPCError
 from noc.core.text import safe_shadow
-from noc.core.snmp.version import SNMP_v1, SNMP_v2c
 from noc.sa.models.profile import Profile
 from noc.sa.models.credentialcheckrule import CredentialCheckRule
-from .scheme import PROTOCOLS, SSH, TELNET
 from noc.core.mib import mib
 
 CHECK_OIDS = [
@@ -28,6 +28,44 @@ CHECK_OIDS = [
     mib["SNMPv2-MIB::sysUpTime.0"],
     mib["SNMPv2-MIB::sysDescr.0"],
 ]
+
+
+@dataclass(frozen=True)
+class ProtoConfig(object):
+    alias: str
+    snmp_version: Optional[int] = None
+    is_http: bool = False
+    is_cli: bool = False
+
+
+CONFIGS = {
+    1: ProtoConfig("telnet", is_cli=True),
+    2: ProtoConfig("ssh", is_cli=True),
+    3: ProtoConfig("http", is_http=True),
+    4: ProtoConfig("https", is_http=True),
+    5: ProtoConfig("beef", is_cli=True),
+    6: ProtoConfig("snmp_v1", snmp_version=0),
+    7: ProtoConfig("snmp_v2c", snmp_version=1),
+    8: ProtoConfig("snmp_v3", snmp_version=3),
+}
+
+
+class Protocol(enum.Enum):
+    @property
+    def config(self):
+        return CONFIGS[self.value]
+
+    TELNET = 1
+    SSH = 2
+    HTTP = 3
+    HTTPS = 4
+    BEEF = 5
+    SNMPv1 = 6
+    SNMPv2c = 7
+    SNMPv3 = 8
+
+
+SUGGEST_PROTOCOLS = [1, 2, 7, 6]
 
 
 class CredentialChecker(object):
@@ -42,7 +80,7 @@ class CredentialChecker(object):
         logger: Optional[logging.root] = None,
         profile: Optional[str] = None,
         calling_service: str = "credentialchecker",
-        protocols: Optional[List[str]] = None,
+        protocols: Optional[List[int]] = None,
     ):
         self.address = address
         self.pool = pool
@@ -53,11 +91,27 @@ class CredentialChecker(object):
         self.calling_service = calling_service
         self.profile = profile
         self.profile: "Profile" = Profile.get_by_name(profile) if profile else None
-        self.protocols: Optional[List[str]] = protocols
+        self.protocols: Optional[List[Protocol]] = [
+            Protocol(p_num) for p_num in SUGGEST_PROTOCOLS if not protocols or p_num in protocols
+        ]
         self.ignoring_cli = False
         if self.profile is None or self.profile.is_generic:
             self.logger.error("CLI Access for Generic profile is not supported. Ignoring")
             self.ignoring_cli = True
+
+    def iter_snmp(self) -> Tuple[str, str]:
+        """
+        Iter SNMP Credential
+        :return: snmp_ro, snmp_rw
+        """
+        yield
+
+    def iter_cli(self) -> Tuple[str, str, str]:
+        """
+        Iter CLI Credential
+        :return: user, password, enable_password
+        """
+        yield
 
     def get_snmp_credentials(self):
         """
@@ -89,7 +143,7 @@ class CredentialChecker(object):
             self.logger.debug("RPC Error: %s", e)
             return False
 
-    def check_login(self, user: str, password: str, super_password: str):
+    def check_login(self, user: str, password: str, super_password: str, protocol: Protocol):
         self.logger.debug("Checking %s/%s/%s", user, password, super_password)
         self.logger.info(
             "Checking %s/%s/%s",
@@ -103,7 +157,7 @@ class CredentialChecker(object):
             ).script(
                 f"{self.profile}.login",
                 {
-                    "cli_protocol": "ssh" if self.object.scheme == SSH else "telnet",
+                    "cli_protocol": protocol.config.alias,
                     "address": self.address,
                     "user": user,
                     "password": password,
