ba909a987255ea2ef8e37747f9f27f1fce28a6be
Andrey Vertiprahov <aversanta@gmail.com>
2022-07-25 10:27:23 +0000

    noc/noc#1857 Add CredentialCheckRule model.

---
+++ services/web/apps/sa/credentialcheckrule/views.py
@@ -0,0 +1,22 @@
+# ---------------------------------------------------------------------
+# sa.credentialcheckrule application
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2022 The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+
+# NOC modules
+from noc.lib.app.extdocapplication import ExtDocApplication
+from noc.sa.models.credentialcheckrule import CredentialCheckRule
+from noc.core.translation import ugettext as _
+
+
+class CredentialCheckRuleApplication(ExtDocApplication):
+    """
+    CredentialCheckRule application
+    """
+
+    title = _("Credential Check Rule")
+    menu = [_("Setup"), _("Credential Check Rules")]
+    model = CredentialCheckRule
+    query_fields = ["name__icontains", "description__icontains"]
