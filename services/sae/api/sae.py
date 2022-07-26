6770bb7844163578baddc286eb99aa47dc4546f6
Andrey Vertiprahov <aversanta@gmail.com>
2022-07-26 17:15:20 +0000

    Merge branch 'fix-avs-sae-typo' into 'master'
    Fix typo on SAE.
    
    See merge request noc/noc!6515
    
    (cherry picked from commit 20df5c42f77c6846af75bd6b73968b162d1f1ad7)
    
    e540bdde Fix typo on SAE.

--- services/sae/api/sae.py
+++ services/sae/api/sae.py
@@ -193,7 +193,7 @@ class SAEAPI(API):
         if caps:
             for c in caps:
                 cc = Capability.get_by_id(c["capability"])
-                if cc and not cc.get("scope"):
+                if cc and not c.get("scope"):
                     capabilities[cc.name] = c.get("value")
         if auth_profile_id:
             user = ap_user
