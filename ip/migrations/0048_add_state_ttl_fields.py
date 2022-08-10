ce104ab28dcc7a41cd57e7e7342e1c8464d028f6
Michael R <rmiclee@gmail.com>
2022-08-10 17:38:32 +0300

    Modify migration

--- ip/migrations/0048_add_state_ttl_fields.py
+++ ip/migrations/0048_add_state_ttl_fields.py
@@ -13,66 +13,21 @@ from noc.core.migration.base import BaseMigration
 
 
 class Migration(BaseMigration):
+    def add_column(self, table: str, column: str, column_title: str):
+        self.db.add_column(
+            table,
+            column,
+            models.DateTimeField(column_title, blank=True, null=True),
+        )
+
     def migrate(self):
-        self.db.add_column(
-            "ip_address",
-            "state_changed",
-            models.DateTimeField("State Changed", blank=True, null=True),
-        )
-        self.db.add_column(
-            "ip_address",
-            "expired",
-            models.DateTimeField("Expired", blank=True, null=True),
-        )
-        self.db.add_column(
-            "ip_address",
-            "last_seen",
-            models.DateTimeField("Last Seen", blank=True, null=True),
-        )
-        self.db.add_column(
-            "ip_address",
-            "first_discovered",
-            models.DateTimeField("First Discovered", blank=True, null=True),
-        )
-
-        self.db.add_column(
-            "ip_prefix",
-            "state_changed",
-            models.DateTimeField("State Changed", blank=True, null=True),
-        )
-        self.db.add_column(
-            "ip_prefix",
-            "expired",
-            models.DateTimeField("Expired", blank=True, null=True),
-        )
-        self.db.add_column(
-            "ip_prefix",
-            "last_seen",
-            models.DateTimeField("Last Seen", blank=True, null=True),
-        )
-        self.db.add_column(
-            "ip_prefix",
-            "first_discovered",
-            models.DateTimeField("First Discovered", blank=True, null=True),
-        )
-
-        self.db.add_column(
-            "ip_vrf",
-            "state_changed",
-            models.DateTimeField("State Changed", blank=True, null=True),
-        )
-        self.db.add_column(
-            "ip_vrf",
-            "expired",
-            models.DateTimeField("Expired", blank=True, null=True),
-        )
-        self.db.add_column(
-            "ip_vrf",
-            "last_seen",
-            models.DateTimeField("Last Seen", blank=True, null=True),
-        )
-        self.db.add_column(
-            "ip_vrf",
-            "first_discovered",
-            models.DateTimeField("First Discovered", blank=True, null=True),
-        )
+        tables = ("ip_address", "ip_prefix", "ip_vrf")
+        columns = {
+            "state_changed": "State Changed",
+            "expired": "Expired",
+            "last_seen": "Last Seen",
+            "first_discovered": "First Discovered",
+        }
+        for table in tables:
+            for column, column_title in columns.items():
+                self.add_column(table, column, column_title)
