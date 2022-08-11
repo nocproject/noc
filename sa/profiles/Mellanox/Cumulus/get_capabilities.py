deea97194d154e7cfd6b5ea35d55a3fec0c9cf8e
Dmitry Luhtionov <dmitryluhtionov@gmail.com>
2022-08-11 12:54:13 +0300

    Fix

--- sa/profiles/Mellanox/Cumulus/get_capabilities.py
+++ sa/profiles/Mellanox/Cumulus/get_capabilities.py
@@ -1,5 +1,5 @@
 # ---------------------------------------------------------------------
-# Mellanox.Onyx.get_capabilities
+# Mellanox.Cumulus.get_capabilities
 # ---------------------------------------------------------------------
 # Copyright (C) 2007-2022 The NOC Project
 # See LICENSE for details
@@ -11,12 +11,11 @@ from noc.sa.profiles.Generic.get_capabilities import false_on_cli_error
 
 
 class Script(BaseScript):
-    name = "Mellanox.Onyx.get_capabilities"
+    name = "Mellanox.Cumulus.get_capabilities"
 
     @false_on_cli_error
     def has_lldp_cli(self):
         """
         Check box has LLDP enabled
         """
-        c = self.cli("show lldp local", cached=True)
-        return "LLDP: enabled" in c
+        return True
