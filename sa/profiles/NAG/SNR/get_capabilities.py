6b888f0a4cae2629d27c714d62b6c55f94d11a3f
Dmitry Lukhtionov <dmitryluhtionov@gmail.com>
2022-08-16 08:33:43 +0000

    Update NAG.SNR profile

--- sa/profiles/NAG/SNR/get_capabilities.py
+++ sa/profiles/NAG/SNR/get_capabilities.py
@@ -1,7 +1,7 @@
 # ---------------------------------------------------------------------
 # NAG.SNR.get_capabilities
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2019 The NOC Project
+# Copyright (C) 2007-2022 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
@@ -10,7 +10,6 @@ import re
 
 # NOC modules
 from noc.sa.profiles.Generic.get_capabilities import Script as BaseScript
-from noc.sa.profiles.Generic.get_capabilities import false_on_cli_error
 
 
 class Script(BaseScript):
@@ -19,23 +18,36 @@ class Script(BaseScript):
     rx_lldp_en = re.compile(r"LLDP has been enabled globally?")
     rx_stack = re.compile(r"-+member :(?P<id>\d+)-+")
 
-    @false_on_cli_error
-    def has_lldp_cli(self):
+    def get_lldp_cli(self):
         """
         Check box has lldp enabled on SNR
         """
-        cmd = self.cli("show lldp", ignore_errors=True)
-        return self.rx_lldp_en.search(cmd) is not None
+        try:
+            cmd = self.cli("show lldp")
+        except self.CLISyntaxError:
+            # FoxGate CLI
+            cmd = self.cli("show lldp interface")
+            return "System LLDP: enable" in cmd, True
+        return self.rx_lldp_en.search(cmd) is not None, False
+
+    def get_stack_members(self):
+        s = []
+        try:
+            cmd = self.cli("show slot")
+        except self.CLISyntaxError:
+            return
+        for match in self.rx_stack.finditer(cmd):
+            i = match.group("id")
+            s += [i]
+        return s
 
     def execute_platform_cli(self, caps):
-        try:
-            s = []
-            cmd = self.cli("show slot")
-            for match in self.rx_stack.finditer(cmd):
-                i = match.group("id")
-                s += [i]
-            if s:
-                caps["Stack | Members"] = len(s) if len(s) != 1 else 0
-                caps["Stack | Member Ids"] = " | ".join(s)
-        except Exception:
-            pass
+        members = self.get_stack_members()
+        if members:
+            caps["Stack | Members"] = len(members) if len(members) != 1 else 0
+            caps["Stack | Member Ids"] = " | ".join(members)
+        has_lldp, foxgate_cli = self.get_lldp_cli()
+        if has_lldp:
+            caps["Network | LLDP"] = True
+        if foxgate_cli:
+            caps["NAG | SNR | CLI | Old"] = True
--- sa/profiles/NAG/SNR/get_version.py
+++ sa/profiles/NAG/SNR/get_version.py
@@ -1,7 +1,7 @@
 # ---------------------------------------------------------------------
 # NAG.SNR.get_version
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2020 The NOC Project
+# Copyright (C) 2007-2022 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
@@ -20,13 +20,13 @@ class Script(BaseScript):
     cache = True
 
     rx_ver = re.compile(
-        r"^\s+(?P<platform>\S+) Device, Compiled on.*\n"
+        r"^\s+(?:(?P<vendor>FoxGate) )?(?P<platform>\S+) Device, Compiled on.*\n"
         r"(^\s+sysLocation.*\n)?"
         r"(^\s+CPU Mac \S+\s*\n)?"
         r"(^\s+Vlan MAC \S+\s*\n)?"
-        r"^\s+SoftWare(?: Package)? Version (?P<version>\S+)\s*\n"
+        r"^\s+Soft[Ww]are(?: Package)? Version (?P<version>\S+)\s*\n"
         r"^\s+BootRom Version (?P<bootprom>\S+)\s*\n"
-        r"^\s+HardWare Version (?P<hardware>\S+)\s*\n"
+        r"^\s+Hard[Ww]are Version (?P<hardware>\S+)\s*\n"
         r"^\s+CPLD Version.*\n"
         r"^\s+(?:Serial No.:|Device serial number)\s*(?P<serial>\S+)\s*\n",
         re.MULTILINE,
@@ -39,12 +39,14 @@ class Script(BaseScript):
         r"^\s+(?:Serial No.:|Device serial number)\s*(?P<serial>\S+)\s*\n",
         re.MULTILINE,
     )
+    rx_ver_snmp2 = re.compile(r"^(?P<vendor>FoxGate) (?P<platform>\S+)$", re.MULTILINE)
 
     def execute_snmp(self):
-        match = self.rx_ver_snmp.search(self.snmp.get(mib["SNMPv2-MIB::sysDescr.0"], cached=True))
+        v = self.snmp.get(mib["SNMPv2-MIB::sysDescr", 0], cached=True)
+        match = self.rx_ver_snmp.search(v)
         if match:
             return {
-                "vendor": "NAG",
+                "vendor": "NAG",  # Need more examples for other vendors
                 "platform": match.group("platform"),
                 "version": match.group("version"),
                 "attributes": {
@@ -53,6 +55,15 @@ class Script(BaseScript):
                     "Serial Number": match.group("serial"),
                 },
             }
+        else:
+            match = self.rx_ver_snmp.search(v)
+            if match:
+                # Device do not support .1.3.6.1.2.1.47.x SNMP table
+                return {
+                    "vendor": "FoxGate",
+                    "platform": match.group("platform"),
+                    "version": "unknown",  # I do not know right OID
+                }
         vendor = self.snmp.get("1.3.6.1.2.1.47.1.1.1.1.12.1", cached=True)
         platform = self.snmp.get(mib["SNMPv2-MIB::sysDescr.0"], cached=True)
         platform = platform.split(" ")[0]
@@ -72,8 +83,11 @@ class Script(BaseScript):
         match = self.rx_ver.search(v)
         if not match:
             match = self.rx_ver_snmp.search(v)
+        vendor = "NAG"
+        if match.group("vendor"):
+            vendor = "FoxGate"
         return {
-            "vendor": "NAG",
+            "vendor": vendor,
             "platform": match.group("platform"),
             "version": match.group("version"),
             "attributes": {
