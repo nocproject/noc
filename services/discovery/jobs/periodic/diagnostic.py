ef34bc12a3e843ba05d22eff7d79f8d70c2bbfec
Andrey Vertiprahov <aversanta@gmail.com>
2022-09-30 16:35:58 +0500

    Disable CLI Diagnostic when HTTP scheme set.

--- services/discovery/jobs/periodic/diagnostic.py
+++ services/discovery/jobs/periodic/diagnostic.py
@@ -125,10 +125,7 @@ class DiagnosticCheck(DiscoveryCheck):
         if bulk:
             self.logger.info("Diagnostic changed: %s", ", ".join(di.diagnostic for di in bulk))
             self.object.save_diagnostics(self.object.id, bulk)
-            if (self.is_box and self.object.can_create_box_alarms()) or (
-                self.is_periodic and self.object.can_create_periodic_alarms()
-            ):
-                self.object.sync_diagnostic_alarm([d.diagnostic for d in bulk])
+            self.object.sync_diagnostic_alarm([d.diagnostic for d in bulk])
             for di in bulk:
                 self.object.register_diagnostic_change(
                     di.diagnostic,
