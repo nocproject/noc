d6dba980a1d1f4b7ad07e2c9618c4660624c7747
Andrey Vertiprahov <aversanta@gmail.com>
2022-10-01 10:42:02 +0500

    Fix flake.

--- sa/profiles/EdgeCore/ES/get_metrics.py
+++ sa/profiles/EdgeCore/ES/get_metrics.py
@@ -1,12 +1,12 @@
 # ---------------------------------------------------------------------
 # EdgeCore.ES.get_metrics
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2019 The NOC Project
+# Copyright (C) 2007-2022 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
 # NOC modules
-from noc.sa.profiles.Generic.get_metrics import Script as GetMetricsScript, metrics
+from noc.sa.profiles.Generic.get_metrics import Script as GetMetricsScript
 
 
 class Script(GetMetricsScript):
