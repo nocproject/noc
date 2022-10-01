0786c7db15983df36f2f32d3de919e1aad56e264
Andrey Vertiprahov <aversanta@gmail.com>
2022-10-01 11:10:45 +0500

    Fix typo.

--- core/cdag/node/alarm.py
+++ core/cdag/node/alarm.py
@@ -161,9 +161,12 @@ class AlarmNode(BaseCDAGNode):
         if self.config.dry_run or not self.config.pool:
             return
         svc = get_service()
-        svc.publish(
-            orjson.dumps(msg), stream=f"dispose.{self.config.pool}", partition=self.config.partition
-        )
+        stream, partition = f"dispose.{self.config.pool}", self.config.partition
+        slots = svc.get_slot_limits(f"correlator-{self.config.pool}")
+        _, *sid = self.config.managed_object.split(":", 1)
+        if slots and sid:
+            partition = int(sid[0]) % int(slots)
+        svc.publish(orjson.dumps(msg), stream=stream, partition=partition)
 
     def is_active(self) -> bool:
         return self.state.active
