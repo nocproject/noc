7d7c8adbef3071df7c5b3990fe829d8a01d892d1
smile <sysfar@gmail.com>
2022-10-07 14:07:37 +0300

    Add Linux Profile

---
+++ sa/profiles/Linux/Linux/get_config.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# Linux.Linux.get_config
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2022 The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+
+# Python modules
+
+# NOC modules
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetconfig import IGetConfig
+
+
+class Script(BaseScript):
+    name = "Linux.Linux.get_config"
+    interface = IGetConfig
+
+    always_prefer = "С"
+
+    def execute(self, **kwargs):
+        config = ""
+        sstring = "-----BEGIN CONFIG BLOCK-----"
+        estring = "-----END CONFIG BLOCK-----"
+        clicommands = [
+            "rpm -qa | sort",
+            "cat /etc/fstab",
+            'systemctl --all --no-pager | grep -v "session-.*Session"',
+        ]
+        for command in clicommands:
+            config = config + (sstring + "\n" + self.cli(command) + estring + "\n")
+        return config
---
+++ sa/profiles/Linux/Linux/get_version.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------
+# Linux.Linux.get_version
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2022 The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+
+# Python modules
+import re
+
+# NOC modules
+from noc.core.script.base import BaseScript
+from noc.sa.interfaces.igetversion import IGetVersion
+from noc.core.text import parse_kv
+
+
+class Script(BaseScript):
+    name = "Linux.Linux.get_version"
+    cache = True
+    interface = IGetVersion
+
+    ver_map = {
+        "name": "distr",
+        "pretty_name": "full_name",
+        "version": "version",
+        "version_id": "version_id",
+        "version_codename": "codename"
+    }
+
+    def execute_cli(self):
+        version = None
+        codename = ""
+        distr = ""
+
+        r = self.cli("cat /etc/os-release", cached=True)
+        if "No such file or directory" not in r:
+            res = parse_kv(self.ver_map, r, sep="=")
+            distr = res["distr"].replace("\"", "")
+            version = res["version"].replace("\"", "")
+            name = res["full_name"].replace("\"", "")
+            if "codename" in res:
+                codename = res["codename"].replace("\"", "")
+        bit = self.cli("uname -m").strip()
+        kernel = self.cli("uname -r").strip()
+
+        return {
+            "vendor": distr,
+            "platform": name,
+            "version": version,
+            "attributes": {
+                "codename": codename,
+                "architecture": bit,
+                "kernel": kernel.strip(),
+            },
+        }
