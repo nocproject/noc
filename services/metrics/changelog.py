b8fcbcd5d8b5f1b35ebf6e67391b3b025e2379ba
Andrey Vertiprahov <aversanta@gmail.com>
2022-10-18 10:13:07 +0500

    Fix changelog reorder when compact.

--- services/metrics/changelog.py
+++ services/metrics/changelog.py
@@ -155,16 +155,16 @@ class ChangeLog(object):
             if not state:
                 self.logger.info("Nothing to compact")
                 return
-        self.logger.info("Compacting %d log items (%d bytes)", n, prev_size)
-        # Split to chunks when necessary
-        bulk = []
-        for c_data in self.iter_state_bulks(state):
-            t_mark += datetime.timedelta(seconds=1)
-            # For more one slots raise condition with same t_mark
-            # For that create random ObjectId
-            bulk.append(InsertOne({"_id": ObjectId(), "slot": self.slot, "data": c_data}))
-            nn += 1
-            next_size += len(c_data)
+            self.logger.info("Compacting %d log items (%d bytes)", n, prev_size)
+            # Split to chunks when necessary
+            bulk = []
+            for c_data in self.iter_state_bulks(state):
+                # t_mark += datetime.timedelta(seconds=1)
+                # For more one slots raise condition with same t_mark
+                # For that create random ObjectId
+                bulk.append(InsertOne({"_id": ObjectId(), "slot": self.slot, "data": c_data}))
+                nn += 1
+                next_size += len(c_data)
         bulk.append(DeleteMany({"_id": {"$lte": t_mark_id}, "slot": self.slot}))
         await coll.bulk_write(bulk, ordered=True)
         self.logger.info(
