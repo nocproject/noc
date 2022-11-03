16984aaffe0d04f02c6f5c702fcdf5078c0df405
Michael R <rmiclee@gmail.com>
2022-11-03 04:52:54 +0000

    Refactor lib/template module

--- sa/models/objectnotification.py
+++ sa/models/objectnotification.py
@@ -1,20 +1,85 @@
 # ----------------------------------------------------------------------
 # ObjectNotification
 # ----------------------------------------------------------------------
-# Copyright (C) 2007-2020 The NOC Project
+# Copyright (C) 2007-2022 The NOC Project
 # See LICENSE for details
 # ----------------------------------------------------------------------
+
+# Python modules
+import os
 
 # Third-party modules
 from noc.core.translation import ugettext as _
 from django.db import models
+from django.template import Template, Context
 
 # NOC modules
 from noc.core.model.base import NOCModel
 from noc.core.model.fields import DocumentReferenceField
 from noc.main.models.notificationgroup import NotificationGroup
 from noc.inv.models.resourcegroup import ResourceGroup
-from noc.lib.template import render_message
+
+_tpl_cache = {}  # name -> template instance
+
+
+def render_template(name, context=None):
+    """
+    Render template
+    :param name:
+    :param context:
+    :return:
+    """
+
+    def get_path(name):
+        local_path = os.path.join("local", "templates", name)
+        if os.path.exists(local_path):
+            return local_path
+        default_path = os.path.join("templates", name)
+        if os.path.exists(default_path):
+            return default_path
+        return None
+
+    # Get cached template
+    if name not in _tpl_cache:
+        path = get_path(name)
+        if path:
+            with open(path) as f:
+                _tpl_cache[name] = Template(f.read())
+        else:
+            _tpl_cache[name] = None
+    #
+    tpl = _tpl_cache[name]
+    if tpl:
+        return tpl.render(Context(context or {}))
+    else:
+        return None
+
+
+def render_message(name, context=None):
+    """
+    Render template. Treat first Subject: line as a subject.
+    Returns subject, body tuple
+    :param name:
+    :param context:
+    :return: subject, body tuple
+    """
+
+    def strip_leading_newlines(lines):
+        lc = lines[:]
+        while lc and not lc[0].strip():
+            lc.pop(0)
+        return lc
+
+    msg = render_template(name, context)
+    if not msg:
+        return None, None  # No template
+    ln = strip_leading_newlines(msg.splitlines())
+    if ln and ln[0].startswith("Subject"):
+        subject = ln.pop(0)[8:].strip()
+        body = "\n".join(strip_leading_newlines(ln[1:]))
+        return subject, body
+    else:
+        return None, "\n".join(ln)
 
 
 class ObjectNotification(NOCModel):
