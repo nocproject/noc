d9422882493403d34a9c58a4ff4828f1c0dbe3f5
Andrey Vertiprahov <aversanta@gmail.com>
2022-11-11 09:17:50 +0500

    Fix migration.

--- inv/migrations/0030_mapsettins_add_generator_type.py
+++ inv/migrations/0030_mapsettins_add_generator_type.py
@@ -11,6 +11,7 @@ from noc.core.migration.base import BaseMigration
 
 class Migration(BaseMigration):
     def migrate(self):
+        self.mongo_db["noc.mapsettings"].drop_index("segment_1")
         self.mongo_db["noc.mapsettings"].aggregate(
             [
                 {"$addFields": {"gen_id": "$segment", "gen_type": "segment"}},
