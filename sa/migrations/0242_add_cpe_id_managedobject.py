ca7d5b581df1c2e9034670ab41a4cfb2ad09491d
Andrey Vertiprahov <aversanta@gmail.com>
2023-02-17 16:07:29 +0500

    Fix black.

--- sa/migrations/0242_add_cpe_id_managedobject.py
+++ sa/migrations/0242_add_cpe_id_managedobject.py
@@ -12,7 +12,6 @@ from noc.core.model.fields import DocumentReferenceField
 
 class Migration(BaseMigration):
     def migrate(self):
-
         self.db.add_column(
             "sa_managedobject",
             "cpe_id",
