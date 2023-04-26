5b9660196fcaae8d3aa95e7e479065cebe2858df
Andrey Vertiprahov <aversanta@gmail.com>
2023-04-26 09:26:44 +0500

    Fix black.

--- main/models/metricstream.py
+++ main/models/metricstream.py
@@ -76,10 +76,7 @@ class MetricStream(Document):
             ]
         for f in self.fields:
             if f.expose_condition:
-                r += [
-                    f'    if not input.get("{f.metric_type.field_name}")'
-                    f"        return None"
-                ]
+                r += [f'    if not input.get("{f.metric_type.field_name}")', f"        return None"]
             if not f.expose_mx:
                 continue
             r += [
