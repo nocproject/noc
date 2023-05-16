1e3f399a34677d610e02365b7ee7c1670619aef5
Andrey Vertiprahov <aversanta@gmail.com>
2023-05-16 11:30:01 +0000

    Merge branch 'fix-avs-clenup-bad-sa-status-migration' into 'master'
    Cleanup object_status duplicate records.
    
    See merge request noc/noc!7301
    
    (cherry picked from commit ec5170e682333b24329184b5af6487e10e98df4b)
    
    d52f1ff0 Cleanup object_status duplicate records.

--- sa/migrations/0243_cleanup_objectstatus_wo_object.py
+++ sa/migrations/0243_cleanup_objectstatus_wo_object.py
@@ -1,9 +1,12 @@
 # ----------------------------------------------------------------------
 # Cleanup bad object_status documents
 # ----------------------------------------------------------------------
-# Copyright (C) 2007-2022 The NOC Project
+# Copyright (C) 2007-2023 The NOC Project
 # See LICENSE for details
 # ----------------------------------------------------------------------
+
+# Third-party modules
+from pymongo import DeleteMany, InsertOne
 
 # NOC modules
 from noc.core.migration.base import BaseMigration
@@ -11,4 +14,26 @@ from noc.core.migration.base import BaseMigration
 
 class Migration(BaseMigration):
     def migrate(self):
-        self.mongo_db["noc.cache.object_status"].delete_many({"object": {"$exists": False}})
+        coll = self.mongo_db["noc.cache.object_status"]
+        coll.delete_many({"object": {"$exists": False}})
+        bulk = []
+        for row in coll.aggregate(
+            [
+                {"$group": {"_id": "$object", "count": {"$sum": 1}}},
+                {"$match": {"count": {"$gte": 2}}},
+            ]
+        ):
+            bulk += [DeleteMany({"object": row["_id"]})]
+            r = coll.find_one({"object": row["_id"]})
+            if r:
+                bulk += [
+                    InsertOne(
+                        {
+                            "object": r["object"],
+                            "statue": r.get("status", True),
+                            "last": r.get("last"),
+                        }
+                    ),
+                ]
+        if bulk:
+            coll.bulk_write(bulk, ordered=True)
