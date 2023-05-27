d407a94d69406c35065a2960a5334e4cc6f9e976
Andrey Vertiprahov <aversanta@gmail.com>
2023-05-27 22:29:15 +0500

    Fix typo.

--- sa/migrations/0250_fix_object_status_migration_typo.py
+++ sa/migrations/0250_fix_object_status_migration_typo.py
@@ -12,5 +12,5 @@ from noc.core.migration.base import BaseMigration
 class Migration(BaseMigration):
     def migrate(self):
         coll = self.mongo_db["noc.cache.object_status"]
-        coll.update_many({"statue": {"$exists": True}}, {"$set": {"status": "$statue"}})
+        coll.update_many({"statue": {"$exists": True}}, {"$set": {"status": True}})
         coll.update_many({"statue": {"$exists": True}}, {"$unset": {"statue": 1}})
