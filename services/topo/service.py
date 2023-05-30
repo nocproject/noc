c61a282796a2c5e20a26663251f8ae00e73bd8ce
Andrey Vertiprahov <aversanta@gmail.com>
2023-05-30 18:11:04 +0500

    Fix black.

--- services/topo/service.py
+++ services/topo/service.py
@@ -84,11 +84,15 @@ class TopoService(FastAPIService):
                         up_links = []
                         for obj_id in affected:
                             try:
-                                up_links.append(ObjectUplinks(
-                                    object_id=obj_id,
-                                    uplinks=list(sorted(self.topo.get_uplinks(obj_id))),
-                                    rca_neighbors=list(sorted(self.topo.get_rca_neighbors(obj_id))),
-                                ))
+                                up_links.append(
+                                    ObjectUplinks(
+                                        object_id=obj_id,
+                                        uplinks=list(sorted(self.topo.get_uplinks(obj_id))),
+                                        rca_neighbors=list(
+                                            sorted(self.topo.get_rca_neighbors(obj_id))
+                                        ),
+                                    )
+                                )
                             except KeyError:
                                 self.logger.warning("Deleted object with id: %s", obj_id)
                         ManagedObject.update_uplinks(up_links)
