46756ee8db312212a2039042f6dbebc2a1009a58
uncol <d.a.zakharov@gmail.com>
2023-08-07 22:17:56 +0300

    mo list custom fileds added

--- ui/web/sa/managedobject/form/View.js
+++ ui/web/sa/managedobject/form/View.js
@@ -1654,4 +1654,27 @@ Ext.define('NOC.sa.managedobject.form.View', {
             }
         ]
     }],
+    initComponent: function() {
+        // add custom fields
+        var cust_fields = this.up('[itemId=sa-managedobject]').noc.cust_form_fields;
+        this.callParent();
+        console.log(cust_fields);
+        if(!Ext.isEmpty(cust_fields)) {
+            this.down('form').add({
+                xtype: "fieldset",
+                anchor: "100%",
+                minHeight: 130,
+                title: __("Custom Fields"),
+                collapsible: true,
+                collapsed: false,
+                items: cust_fields.map(function(field) {
+                    if(field.xtype === 'datefield') {
+                        field.altFormats = "Y-m-dTH:i:s";
+                        field.format = "Y-m-d H:i:s";
+                    }
+                    return field;
+                })
+            })
+        }
+    },
 });
