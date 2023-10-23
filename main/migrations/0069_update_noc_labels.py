733cccbfacdaf41dfed3a718e7b113b556476235
VetrovDmitry007 <13im@mail.ru>
2023-10-23 11:41:50 +0300

    Update using bulk

--- main/migrations/0069_update_noc_labels.py
+++ main/migrations/0069_update_noc_labels.py
@@ -6,13 +6,22 @@
 # ----------------------------------------------------------------------
 
 # NOC modules
+from pymongo import UpdateOne
 from noc.core.migration.base import BaseMigration
 import uuid
+
+MONGO_CHUNK = 500
 
 
 class Migration(BaseMigration):
     def migrate(self):
         coll = self.mongo_db["labels"]
-        for p in coll.find({}):
+        updates = []
+        for d in coll.find({"uuid": {"$exists": False}}, {"_id": 1}):
             u = uuid.uuid4()
-            self.mongo_db["labels"].update_one({"name": p["name"]}, {"$set": {"uuid": u}})
+            updates += [UpdateOne({"_id": d["_id"]}, {"$set": {"uuid": u}})]
+            if len(updates) >= MONGO_CHUNK:
+                coll.bulk_write(updates)
+                updates = []
+        if updates:
+            coll.bulk_write(updates)
