d1cf303c32116c82965f3ca17882916fddd56de3
Andrey Vertiprahov <aversanta@gmail.com>
2023-11-01 08:56:41 +0500

    Fix api_save_data.

--- services/web/apps/inv/inv/plugins/cfgdata.py
+++ services/web/apps/inv/inv/plugins/cfgdata.py
@@ -6,12 +6,12 @@
 # ---------------------------------------------------------------------
 
 # Python modules
-from typing import Dict, List, Optional, Any
+from typing import Dict, List, Any
 
 # NOC modules
 from noc.inv.models.object import Object
 from noc.cm.models.configurationparam import ConfigurationParam
-from noc.sa.interfaces.base import StringParameter
+from noc.sa.interfaces.base import StringParameter, StringListParameter, DictListParameter
 from .base import InvPlugin
 
 
@@ -26,11 +26,13 @@ class CfgDataPlugin(InvPlugin):
             self.api_save_data,
             url="^(?P<id>[0-9a-f]{24})/plugin/cfgdata/$",
             method=["PUT"],
-            validate={
-                "param": StringParameter(),
-                "scope": StringParameter(),
-                "value": StringParameter(default="", required=False),
-            },
+            validate=DictListParameter(
+                attrs={
+                    "param": StringParameter(),
+                    "scopes": StringListParameter(required=False),
+                    "value": StringParameter(default="", required=False),
+                },
+            ),
         )
         self.add_view(
             "api_plugin_%s_schema" % self.name,
@@ -61,17 +63,23 @@ class CfgDataPlugin(InvPlugin):
             ]
         return {"id": str(o.id), "name": o.name, "model": o.model.name, "data": data}
 
-    def api_save_data(self, request, oid, data: List[Dict[str, Any]]):
-        o = self.app.get_object_or_404(Object, id=oid)
+    def api_save_data(self, request, id, **kwargs):
+        o = self.app.get_object_or_404(Object, id=id)
+        data: List[Dict[str, Any]] = self.app.deserialize(request.body)
         for d in data:
-            param = self.app.get_object_or_404(ConfigurationParam, id=d["param"])
-            for s in d["scopes"]:
-                o.set_cfg_data(param, s, d["value"])
+            p = self.app.get_object_or_404(ConfigurationParam, id=d["param"])
+            for s in d["scopes"] or [""]:
+                o.set_cfg_data(p.code, d["value"], scope=s)
+        try:
+            o.save()
+        except Exception as e:
+            return {"status": False, "message": str(e), "traceback": str(e)}
+        return {"status": True}
 
-    def api_get_schema(self, request, oid, param=None, scope: Optional[str] = None):
-        """
-        Getting Param Schema
-        """
-        o = self.app.get_object_or_404(Object, id=oid)
-        param = self.app.get_object_or_404(ConfigurationParam, id=param)
-        return param.get_schema(o)
+    # def api_get_schema(self, request, id, param=None, scope: Optional[str] = None):
+    #     """
+    #     Getting Param Schema
+    #     """
+    #     o = self.app.get_object_or_404(Object, id=id)
+    #     param = self.app.get_object_or_404(ConfigurationParam, id=param)
+    #     return param.get_schema(o)
