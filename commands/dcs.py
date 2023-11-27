4558e72a50c5087fb9ed37df60cb09ed93e4aecf
Dmitry Volodin <dv@nocproject.org>
2023-11-27 13:14:11 +0100

    Renamed to dcs

--- commands/dcs.py
+++ commands/dcs.py
@@ -17,12 +17,17 @@ from noc.core.service.loader import get_dcs
 
 class Command(BaseCommand):
     def add_arguments(self, parser: argparse.ArgumentParser) -> None:
-        print("!")
-        parser.add_argument(
+        subparsers = parser.add_subparsers(dest="cmd", required=True)
+        set_slots = subparsers.add_parser("set-slots")
+        set_slots.add_argument(
             "-f", "--file", nargs=argparse.REMAINDER, help="Read arguments from file"
         )
 
     def handle(self, *args, **options):
+        cmd = options["cmd"]
+        return getattr(self, f'handle_{cmd.replace("-", "_")}')(*args, **options)
+
+    def handle_set_slots(self, *args, **options):
         async def inner() -> None:
             for k, v in sorted(limits):
                 await dcs.set_slot_limit(k, v)
