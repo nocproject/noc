47ab39e71233e0f844d6f2d1a7f3900fdea9bebb
Dmitry Volodin <dvolodin7@gmail.com>
2023-11-27 13:32:32 +0000

    ./noc dcs set-slots command

--- scripts/deploy/migrate.sh
+++ scripts/deploy/migrate.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 # Perform all migrations
 set -e
+if [ -n "$NOC_MIGRATE_SLOTS_PATH" ]; then
+    if [ -d "$NOC_MIGRATE_SLOTS_PATH" ]; then
+        ./noc dcs set-slots -f $NOC_MIGRATE_SLOTS_PATH/*
+    else 
+        ./noc dcs set-slots -f $NOC_MIGRATE_SLOTS_PATH
+    fi
+fi
 ./noc migrate
 ./noc ensure-indexes
 ./noc migrate-liftbridge
@@ -9,4 +16,3 @@ set -e
 ./noc migrate-ch
 ./noc sync-perm
 ./noc sync-mibs
-
