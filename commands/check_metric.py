b871d88cb3b236b6f53f351e4aed167b3895e1d4
alakiza <off.lakiza@yandex.ru>
2023-11-29 04:04:29 +0300

    code cleanup

--- commands/check_metric.py
+++ commands/check_metric.py
@@ -7,8 +7,6 @@
 
 # Python modules
 import os
-import argparse
-import re
 
 # Third-party modules
 import orjson
@@ -18,16 +16,10 @@ from noc.core.management.base import BaseCommand
 from noc.core.script.loader import loader as script_loader
 from noc.core.profile.loader import loader as profile_loader
 
-from noc.sa.profiles.Generic.get_metrics import MetricConfig
-
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        # parser.add_argument("script", nargs=1, help="Script name")
         parser.add_argument("--profile", help="Profile list in JSON format")
         parser.add_argument("--metric", help="Metric list in JSON format")
-        # parser.add_argument(
-        #     "arguments", nargs=argparse.REMAINDER, help="Arguments passed to script"
-        # )
 
     def parse_json(self, j):
         try:
@@ -65,36 +57,16 @@ class Command(BaseCommand):
         self,
         profile,
         metric
-        # arguments,
-        # *args,
-        # **options,
     ):
-        # self.stdout.write(f"{'='*16}\n")
-        # self.stdout.write(f"{profile}\n")
-        # self.stdout.write(f"{'-'*16}\n")
-        # self.stdout.write(f"{metric}\n")
-        # self.stdout.write(f"{'='*16}\n")
-
         if profile:
             profiles = self.parse_json(profile)
         else:
-            # self.stdout.write(f"{'='*16}\n")
-            # for x in profile_loader.iter_profiles():
-            #     self.stdout.write(f"{x}\n")
             profiles = [x for x in profile_loader.iter_profiles()]
-            # self.stdout.write(f"{'='*16}\n")
 
         if metric:
             metrics = self.parse_json(metric)
         else:
             metrics = []
-        # object_metrics = [MetricConfig(**m) for m in metrics]
-
-        # self.stdout.write(f"{'='*16}\n")
-        # self.stdout.write(f"{profiles}\n")
-        # self.stdout.write(f"{'-'*16}\n")
-        # self.stdout.write(f"{metrics}\n")
-        # self.stdout.write(f"{'='*16}\n")
 
         metric_list = []
         profile_list = []
@@ -121,8 +93,6 @@ class Command(BaseCommand):
             except:
                 continue
 
-            # supported_metrics = []
-            # unsupported_metrics = []
             if metrics:
                 for m in metrics:
                     if m not in metric_list:
@@ -136,102 +106,10 @@ class Command(BaseCommand):
                         metric_list.append(m)
                     p_item.get("metrics")[m] = self.get_metric_source(metric_func_list)
 
-            # for m in metrics:
-            #     if m not in metric_list:
-            #         metric_list.append(m)
-                
-            #     if m in scr._mt_map:
-            #         # supported_metrics.append(m)
-                    
-            #     # else:
-            #     #     unsupported_metrics.append(m)
-
             profile_list.append(p_item)
-        
-        # self.stdout.write(f"{metric_set}\n")
-        # self.stdout.write(f"{profile_list}\n")
 
         metric_list.sort()
         self.print_csv(profile_list, metric_list)
-
-            # self.stdout.write(f"supported:   {supported_metrics}\n")
-            # self.stdout.write(f"unsupported: {unsupported_metrics}\n")
-
-
-        # args = self.get_script_args(arguments)
-        # # Load script
-        # self.stdout.write(f"{script}")
-        # script = script[0]
-        # script_class = loader.get_script(script)
-        # if not script_class:
-        #     self.die("Failed to load script %s" % script_class)
-
-        # metrics = args.get("metrics")
-        # if not metrics:
-        #     self.die("args must have metrics")
-
-        # object_metrics = [MetricConfig(**m) for m in metrics]
-
-        # service = ServiceStub(pool="")
-        # scr = script_class(
-        #     service=service,
-        #     credentials={},
-        #     capabilities=[],
-        #     args=args,
-        #     version={},
-        #     timeout=3600,
-        #     name=script,
-        # )
-
-        # supported_metrics = []
-        # unsupported_metrics = []
-        # for m in object_metrics:
-        #     if m.metric in scr._mt_map:
-        #         supported_metrics.append(m.metric)
-        #     else:
-        #         unsupported_metrics.append(m.metric)
-
-        # self.stdout.write(f"supported:   {supported_metrics}\n")
-        # self.stdout.write(f"unsupported: {unsupported_metrics}\n")
-
-    rx_arg = re.compile(r"^(?P<name>[a-zA-Z][a-zA-Z0-9_]*)(?P<op>:?=@?)(?P<value>.*)$")
-
-    def get_script_args(self, arguments):
-        """
-        Parse arguments and return script's
-        """
-
-        def read_file(path):
-            if not os.path.exists(path):
-                self.die("Cannot open file '%s'" % path)
-            with open(path) as f:
-                return f.read()
-
-        def parse_json(j):
-            try:
-                return orjson.loads(j)
-            except ValueError as e:
-                self.die("Failed to parse JSON: %s" % e)
-
-        args = {}
-        for a in arguments:
-            match = self.rx_arg.match(a)
-            if not match:
-                self.die("Malformed parameter: '%s'" % a)
-            name, op, value = match.groups()
-            if op == "=":
-                # Set parameter
-                args[name] = value
-            elif op == "=@":
-                # Read from file
-                args[name] = read_file(value)
-            elif op == ":=":
-                # Set to JSON value
-                args[name] = parse_json(value)
-            elif op == ":=@":
-                # Set to JSON value from a file
-                args[name] = parse_json(read_file(value))
-        return args
 
 class ServiceStub(object):
     class ServiceConfig(object):
