f27abdcbde7aff87077967ba6a1221abf925b10f
Dmitry Lukhtionov <dmitryluhtionov@gmail.com>
2023-11-29 13:57:58 +0000

    Merge branch 'fix_get_interfaces_Alcatel.7302' into 'master'
    fix get_interfaces Alcatel.7302
    
    See merge request noc/noc!7729
    
    (cherry picked from commit fe49dba682a92f8c7e6960ab9dd9f8edd9378944)
    
    a21377b0 fix get_interfaces Alcatel.7302
    b4953cf9 Update get_interfaces.py
    e305dc75 Update file profile.py
    c5dc5ddc Update profile.py
    337a1e27 Update profile.py

--- sa/profiles/Alcatel/7302/profile.py
+++ sa/profiles/Alcatel/7302/profile.py
@@ -8,6 +8,7 @@
 # ----------------------------------------------------------------------
 
 # Python modules
+import re
 from typing import Tuple
 
 # NOC modules
@@ -22,6 +23,8 @@ class Profile(BaseProfile):
     pattern_operation_error = rb"Internal processing error"
     command_save_config = "admin software-mngt shub database save"
     command_exit = "logout"
+    # exclude rotating dash
+    rogue_chars = [re.compile(rb"[\/\|\\\- ]\\x1b\[1D"), b"\r"]
 
     matchers = {
         "is_telnet_problem": {"version": {"$regex": r"32\.\d+$"}},
