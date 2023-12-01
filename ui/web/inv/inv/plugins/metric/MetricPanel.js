0abcd0681c91d785e8c44c0c6e8fe29e3fefd3d4
uncol <d.a.zakharov@gmail.com>
2023-12-01 14:57:57 +0300

    save

--- ui/web/inv/inv/plugins/metric/MetricPanel.js
+++ ui/web/inv/inv/plugins/metric/MetricPanel.js
@@ -53,6 +53,41 @@ Ext.define("NOC.inv.inv.plugins.metric.MetricPanel", {
             {
               text: __("Value"),
               dataIndex: "value",
+              renderer: function (value) {
+                return Ext.String.format(
+                  '<div style="height: 100%; display: flex; align-items: center;"><svg max-width="100%" m-height="100%" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="{0}" /></svg></div>',
+                  value
+                );
+              },
+              // renderer: function (
+              // value,
+              // metaData,
+              // record,
+              // rowIndex,
+              // colIndex,
+              // store,
+              // view
+              // ) {
+              // const column = this.getColumns()[colIndex],
+              //   row = this.getRows()[rowIndex],
+              //   height = row.getHeight(),
+              //   width = column.getWidth();
+              // console.log(height, width);
+              // return (
+              // '<div style="width: 100%; height: 100%;display: flex; align-items: center; justify-content: center;">' +
+              // '<svg style="width: 100%' +
+              // width +
+              // "; height: 100%" +
+              // height +
+              // '<svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">' +
+              // '<text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="#000">' +
+              // value +
+              // "</text>" +
+              // "</svg>" +
+              // "</div>"
+              // );
+              // },
+              // renderer: this.generateSVGCode,
             },
             {
               text: __("Units"),
@@ -90,13 +125,6 @@ Ext.define("NOC.inv.inv.plugins.metric.MetricPanel", {
     var me = this;
     me.currentId = data.id;
     me.store.loadData(data);
-    me.down("[name=scope__label]").setStore({
-      autoLoad: true,
-      proxy: {
-        type: "rest",
-        url: "/inv/inv/" + data.id + "/plugin/metric/",
-      },
-    });
   },
   save: function () {
     var me = this.up("[itemId=metricPanel]");
@@ -138,4 +166,106 @@ Ext.define("NOC.inv.inv.plugins.metric.MetricPanel", {
   expandAll: function () {
     this.up("[itemId=metricPanel]").groupingFeature.expandAll();
   },
+  generateSVGCode: function (value) {
+    var svgCode = '<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">';
+
+    // Размеры и позиции элементов
+    var widthPercent = 100;
+    var heightPercent = 100;
+    var scaleHeightPercent = 10;
+    var limitLineHeightPercent = 20;
+
+    // Цвета
+    var criticalColor = "#FF0000";
+    var warningColor = "#FFFF00";
+    var normalColor = "#00FF00";
+    var scaleColor = "#000000";
+    var limitLineColor = "#000000";
+
+    // Вычисление позиции пределов
+    var criticalLimitPercent = 30;
+    var warningLimitPercent = 70;
+
+    // Отображение шкалы
+    // svgCode +=
+    //   '<rect x="0" y="' +
+    //   (100 - scaleHeightPercent) +
+    //   '%" width="' +
+    //   widthPercent +
+    //   '%" height="' +
+    //   scaleHeightPercent +
+    //   '%" fill="' +
+    //   scaleColor +
+    //   '"></rect>';
+
+    // Отображение пределов
+    // svgCode +=
+    //   '<line x1="' +
+    //   criticalLimitPercent +
+    //   '%" y1="0" x2="' +
+    //   criticalLimitPercent +
+    //   '%" y2="' +
+    //   limitLineHeightPercent +
+    //   '%" stroke="' +
+    //   limitLineColor +
+    //   '"></line>';
+    // svgCode +=
+    //   '<line x1="' +
+    //   warningLimitPercent +
+    //   '%" y1="0" x2="' +
+    //   warningLimitPercent +
+    //   '%" y2="' +
+    //   limitLineHeightPercent +
+    //   '%" stroke="' +
+    //   limitLineColor +
+    //   '"></line>';
+
+    // Отображение цветовых зон
+    svgCode +=
+      '<rect x="0" y="0" width="' +
+      criticalLimitPercent +
+      '%" height="' +
+      scaleHeightPercent +
+      '%" fill="' +
+      criticalColor +
+      '"></rect>';
+    svgCode +=
+      '<rect x="' +
+      criticalLimitPercent +
+      '%" y="0" width="' +
+      (warningLimitPercent - criticalLimitPercent) +
+      '%" height="' +
+      scaleHeightPercent +
+      '%" fill="' +
+      warningColor +
+      '"></rect>';
+    svgCode +=
+      '<rect x="' +
+      warningLimitPercent +
+      '%" y="0" width="' +
+      (100 - warningLimitPercent) +
+      '%" height="' +
+      scaleHeightPercent +
+      '%" fill="' +
+      normalColor +
+      '"></rect>';
+
+    // Отображение значения параметра
+    svgCode +=
+      '<text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="' +
+      scaleColor +
+      '">' +
+      value +
+      "</text>";
+
+    svgCode += "</svg>";
+
+    return svgCode;
+  },
 });
+//<svg
+//  style="width: 100%; height: 100%;"
+//  preserveAspectRatio="none"
+//  xmlns="http://www.w3.org/2000/svg">
+//  <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="#000">28</text>
+//</svg>
