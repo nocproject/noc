76c48b7b9177f087c99f921f8de5b89401869ea2
Andrey Vertiprahov <aversanta@gmail.com>
2023-12-01 08:42:43 +0500

    Fix get_lacp_neighbors

--- sa/profiles/HP/Aruba/get_lacp_neighbors.py
+++ sa/profiles/HP/Aruba/get_lacp_neighbors.py
@@ -1,13 +1,12 @@
 # ---------------------------------------------------------------------
 # HP.Aruba.get_lacp_neighbors
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2011 The NOC Project
+# Copyright (C) 2007-2023 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
 # Python modules
 import re
-from collections import defaultdict
 
 # NOC modules
 from noc.core.script.base import BaseScript
@@ -22,7 +21,7 @@ class Script(BaseScript):
     rx_actor_detail = re.compile(r"Actor details of all interfaces:")
     rx_partner_detail = re.compile(r"Partner details of all interfaces:")
     rx_row = re.compile(
-        r"(?P<lport>\S+)\s+(?P<lag>lag\d+)\S*\s+(?P<port_id>\d+)\s+(?P<port_pri>\d+)"
+        r"^(?P<lport>\S+)\s+(?P<lag>lag\d+)\S*\s+(?P<port_id>\d+)\s+(?P<port_pri>\d+)"
         r"\s+(?P<state>\S+)\s+(?P<system_id>\S+)\s+(?P<pri>\d+)\s+(?P<key>\d+).*",
         re.MULTILINE,
     )
@@ -33,13 +32,19 @@ class Script(BaseScript):
         v = self.cli("show lacp interfaces")
         a_match = self.rx_actor_detail.search(v)
         p_match = self.rx_partner_detail.search(v)
+        if not p_match:
+            # Neighbors not found
+            return []
         p_map = {}
-        m = defaultdict(list)
+        m = {}
         for lport, lag, port_id, _, _, system_id, _, key in self.rx_row.findall(
             v[a_match.end() : p_match.start()]
         ):
             p_map[lport] = {"local_port_id": port_id, "system": system_id, "key": key}
         for lport, lag, port_id, _, _, system_id, _, key in self.rx_row.findall(v[p_match.end() :]):
+            if system_id == "02:01:00:00:01:00":
+                # Neighbors not found
+                continue
             if lag not in m:
                 m[lag] = {"lag_id": lag[3:], "interface": lag, "system_id": sid, "bundle": []}
             m[lag]["bundle"] += [
