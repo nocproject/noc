93b0c534bc6c8abc5d930c058e4016d181ceebb6
Michael R <rmiclee@gmail.com>
2023-12-12 12:53:52 +0300

    Fix loader (for etl check)

--- core/etl/loader/base.py
+++ core/etl/loader/base.py
@@ -781,7 +781,7 @@ class BaseLoader(object):
             if not ls:
                 ls = line.get_current_state()
             ms = self.iter_jsonl(ls, data_model=line.data_model)
-            m_data[self.data_model.model_fields[f].name] = set(row.id for row in ms)
+            m_data[f] = set(row.id for row in ms)
         # Process data
         n_errors = 0
         for row in new_state:
