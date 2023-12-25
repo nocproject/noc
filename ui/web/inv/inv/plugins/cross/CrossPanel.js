419f3ad0737e1bb6a0f7712554e6e80874f221a9
uncol <d.a.zakharov@gmail.com>
2023-12-25 22:26:06 +0300

    add diag to cross plugin

--- ui/web/inv/inv/plugins/cross/CrossPanel.js
+++ ui/web/inv/inv/plugins/cross/CrossPanel.js
@@ -1,7 +1,7 @@
 //---------------------------------------------------------------------
 // inv.inv Cross Panel
 //---------------------------------------------------------------------
-// Copyright (C) 2007-2020 The NOC Project
+// Copyright (C) 2007-2023 The NOC Project
 // See LICENSE for details
 //---------------------------------------------------------------------
 console.debug("Defining NOC.inv.inv.plugins.cross.CrossPanel");
@@ -11,28 +11,40 @@ Ext.define("NOC.inv.inv.plugins.cross.CrossPanel", {
     title: __("Cross"),
     closable: false,
     layout: "fit",
-
-    initComponent: function () {
+    items: [
+        {
+            xtype: "panel",
+            flex: 1,
+            itemId: "diagram",
+            border: false,
+            scrollable: "y",
+        }
+    ],
+    initComponent: function() {
         var me = this;
         me.callParent();
     },
     //
-    preview: function (data) {
+    preview: function(data) {
         var me = this;
         me.currentId = data.id;
-        me.store.loadData(data.data);
+        NOC.drawDiagram(NOC.generateDiagram({
+            cross: data.data,
+            connections: [...Ext.Array.map(data.data, function(el) {return {name: el.input}}),
+            ...Ext.Array.map(data.data, function(el) {return {name: el.output}})]
+        }), this.down("[itemId=diagram]"));
     },
     //
-    onReload: function () {
+    onReload: function() {
         var me = this;
         Ext.Ajax.request({
             url: "/inv/inv/" + me.currentId + "/plugin/cross/",
             method: "GET",
             scope: me,
-            success: function (response) {
+            success: function(response) {
                 me.preview(Ext.decode(response.responseText));
             },
-            failure: function () {
+            failure: function() {
                 NOC.error(__("Failed to get data"));
             }
         });
