a88b6b97f7803c15ad566694f3aa5c616d63f6c9
Andrey Vertiprakhov <aversanta@gmail.com>
2024-01-06 16:43:27 +0500

    Revert get_interfaces change.

--- sa/profiles/Generic/get_interfaces.py
+++ sa/profiles/Generic/get_interfaces.py
@@ -130,7 +130,7 @@ class Script(BaseScript):
             ]
         ):
             if att_pc and att_pc != int(ifindex):
-                if int(sel_pc) > 0:
+                if sel_pc > 0:
                     r[int(ifindex)] = int(att_pc)
         return r
 
