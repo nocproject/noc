fc75b522d3b4b262169e07292bbaa58f6b4f309f
alakiza <off.lakiza@yandex.ru>
2024-01-29 03:16:13 +0300

    add tokenizer and normalizer to TPLink.T2600G

---
+++ sa/profiles/TPLink/T2600G/confdb/normalizer.py
@@ -0,0 +1,13 @@
+# ----------------------------------------------------------------------
+# TPLink.T2600G config normalizer
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2024 The NOC Project
+# See LICENSE for details
+# ----------------------------------------------------------------------
+
+# NOC modules
+from noc.core.confdb.normalizer.base import BaseNormalizer
+
+
+class TPLinkxT2600GNormalizer(BaseNormalizer):
+    pass
