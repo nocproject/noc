c8a21b4895de34d0dace5753139c5f88006e4908
Alexander Lakiza <off.lakiza@yandex.ru>
2024-02-05 07:38:18 +0000

    Add confdb normalizer to DLink.DxS

--- sa/profiles/DLink/DxS/profile.py
+++ sa/profiles/DLink/DxS/profile.py
@@ -46,8 +46,10 @@ class Profile(BaseProfile):
     snmp_ifstatus_get_chunk = 30
     snmp_ifstatus_get_timeout = 3
 
+    config_normalizer = "DLinkDxSNormalizer"
+
     config_tokenizer = "line"
-    config_tokenizer_settings = {"line_comment": "#"}
+    config_tokenizer_settings = {"line_comment": "#", "string_quote": '"'}
 
     snmp_display_hints = {mib["LLDP-MIB::lldpLocPortId"]: render_mac}
 
