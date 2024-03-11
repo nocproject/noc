dbbf722dfca162894e68137d769b4af061186ae9
alakiza <off.lakiza@yandex.ru>
2024-03-11 17:11:00 +0300

    removed unnecessary ifindex getting

--- sa/profiles/CData/xPON/get_interfaces.py
+++ sa/profiles/CData/xPON/get_interfaces.py
@@ -101,13 +101,11 @@ class Script(BaseScript):
                         }
                     ]
             v = self.cli("show lacp system port")
-            ifindexes = {y: x for y, x in self.scripts.get_ifindexes().items()}
             for match in self.rx_port.finditer(v):
                 ifname = match.group("ifname")
                 iface = {
                     "name": ifname,
                     "type": "physical",
-                    "snmp_ifindex": ifindexes[ifname],
                     "admin_status": True,
                     "oper_status": match.group("oper_status").lower() == "1",
                     "enabled_protocols": [],
