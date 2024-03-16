45b7964439ebc39b796a2d4730c3ed88b5cff51f
Andrey Vertiprahov <aversanta@gmail.com>
2024-03-16 14:59:59 +0500

    Always invalidate ManagedObject cache when change TTSystem settings.

--- fm/models/ttsystem.py
+++ fm/models/ttsystem.py
@@ -98,23 +98,17 @@ class TTSystem(Document):
         from noc.core.cache.base import cache
         from noc.sa.models.managedobject import ManagedObject, MANAGEDOBJECT_CACHE_VERSION
 
-        invalidate_mo_cache = (
-            not hasattr(self, "_changed_fields")
-            or "alarm_consequence_policy" in self._changed_fields
-        )
-
         # After save changed_fields will be empty
         super().save(*args, **kwargs)
 
         # Invalidate ManagedObject cache
-        if invalidate_mo_cache:
-            deleted_cache_keys = [
-                "managedobject-id-%s" % mo_id
-                for mo_id in ManagedObject.objects.filter(tt_system=self.id).values_list(
-                    "id", flat=True
-                )
-            ]
-            cache.delete_many(deleted_cache_keys, version=MANAGEDOBJECT_CACHE_VERSION)
+        deleted_cache_keys = [
+            "managedobject-id-%s" % mo_id
+            for mo_id in ManagedObject.objects.filter(tt_system=self.id).values_list(
+                "id", flat=True
+            )
+        ]
+        cache.delete_many(deleted_cache_keys, version=MANAGEDOBJECT_CACHE_VERSION)
 
     def get_system(self) -> BaseTTSystem:
         """
