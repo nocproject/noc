fbaac37f2bd9aac8086ee25b00f0c60a40a8af52
alakiza <off.lakiza@yandex.ru>
2024-03-16 22:51:28 +0300

    fixed oper_status mapping for BDCOM.xPON.get_spe_status and cpe local_id recognition for BDCOM P3616 series

--- sa/profiles/BDCOM/xPON/get_cpe_status.py
+++ sa/profiles/BDCOM/xPON/get_cpe_status.py
@@ -1,7 +1,7 @@
 # ---------------------------------------------------------------------
-# BDCOM.xPON.get_cpe
+# BDCOM.xPON.get_cpe_status
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2022 The NOC Project
+# Copyright (C) 2007-2024 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
@@ -21,11 +21,11 @@ class Script(BaseScript):
     splitter = re.compile(r"\s*-+\n")
 
     status_map = {
-        "auto-configured": "active",
-        "auto-configuring": "active",
-        "authenticated": "active",
-        "lost": "inactive",
-        "deregistered": "inactive",
+        "auto-configured": True,
+        "auto-configuring": True,
+        "authenticated": True,
+        "lost": False,
+        "deregistered": False,
     }
 
     def execute_cli(self, **kwargs):
@@ -36,11 +36,25 @@ class Script(BaseScript):
             for p in parts[1:]:
                 for onu in p.split("\n"):
                     line = onu.split()
-                    r.append(
-                        {
-                            "oper_status": self.status_map[line[6]],
-                            "local_id": line[0],  # Use int command show ap inventory NAME
-                            "global_id": line[3],
-                        }
-                    )
+                    if len(line) >= 8:
+                        r.append(
+                            {
+                                "oper_status": self.status_map[line[6]],
+                                "local_id": line[0],  # Use int command show ap inventory NAME
+                                "global_id": line[3],
+                            }
+                        )
+                    else:
+                        # Sometimes first fields overlaps
+                        # IntfName   VendorID  ModelID    MAC Address    Description                     BindType  Status          Dereg Reason
+                        # ---------- --------- ---------- -------------- ------------------------------- --------- --------------- -----------------
+                        # EPON0/13:9 VSOL      D401       006d.61d4.6bf8 N/A                             static    auto-configured N/A
+                        # EPON0/13:10xPON      101Z       e0e8.e61f.0759 N/A                             static    auto-configured N/A
+                        r.append(
+                            {
+                                "oper_status": self.status_map[line[5]],
+                                "local_id": line[0][0:11],
+                                "global_id": line[2],
+                            }
+                        )
         return r
