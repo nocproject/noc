615fc561e3ad0c80cf278c0442bb0bdeea89410a
alakiza <off.lakiza@yandex.ru>
2024-03-19 22:30:07 +0300

    regexp rewrite

--- sa/profiles/Huawei/MA5600T/get_interfaces.py
+++ sa/profiles/Huawei/MA5600T/get_interfaces.py
@@ -52,7 +52,7 @@ class Script(BaseScript):
     rx_tagged = re.compile(r"(?P<tagged>\d+)", re.MULTILINE)
     rx_ether = re.compile(
         r"^\s*(?P<port>\d+)\s+(?:10)?[GF]E(?:-Optic|-Elec)?\s+"
-        r"(\S+\s+)?(\d+\s+)?(\S+\s+)?\S+\s+\S+\s+\S+\s+"
+        r"(\S+\s+)?(\d+\s+)?(\S+\s+){3,4}"
         r"(?P<admin_status>\S+)\s+(?P<oper_status>\S+)\s*\n",
         re.MULTILINE,
     )
