ac6fd7d20a3de3ed579dadeba21377c71f2c213e
Dmitry Volodin <dv@nocproject.org>
2024-03-29 14:08:35 +0100

    Remove ChannelEndpoint

--- inv/models/endpoint.py
+++ inv/models/endpoint.py
@@ -12,12 +12,11 @@ from mongoengine.fields import StringField, ListField
 # NOC modules
 from noc.core.mongo.fields import PlainReferenceField
 from noc.main.models.label import Label
-from noc.core.model.decorator import on_delete_check
 from .techdomain import TechDomain
+from .channel import Channel
 
 
 @Label.model
-@on_delete_check(check=[("inv.ChannelEndpoint", "endpoint")])
 class Endpoint(Document):
     """
     Enpoint.
@@ -38,25 +37,23 @@ class Endpoint(Document):
         resource_id: Id within database model. Empty for promise.
         slot: Sub-identified within resource.
         effective_labels: List of effective labels.
-        deadline: Only for promise endpoints, expected terms
-            of the implementation.
-        channels: List of connected channels
-            channel: Id of the channel.
-            discriminators: List of channel's discriminators.
+        channels: Connected channel
     """
 
     meta = {
         "collection": "endpoints",
         "strict": False,
         "auto_create_index": False,
-        "indexes": [("model", "resource_id"), "effective_labels"],
+        "indexes": [("model", "resource_id", "slot"), "channel", "effective_labels"],
     }
     name = StringField(required=True)
     description = StringField()
+    channel = PlainReferenceField(Channel, required=True)
     tech_domain = PlainReferenceField(TechDomain, required=True)
     model = StringField(required=True)
     resource_id = StringField()
     slot = StringField(required=False)
+    discriminator = StringField(required=False)
     labels = ListField(StringField())
     effective_labels = ListField(StringField())
 
