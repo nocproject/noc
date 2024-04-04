4760388c888b1fad48715de09e7486bc47b501b8
Andrey Vertiprahov <aversanta@gmail.com>
2024-04-04 23:02:21 +0500

    Add UI Json field.

---
+++ main/handlers/emptyiterhandler.py
@@ -0,0 +1,11 @@
+# ---------------------------------------------------------------------
+# Empty Iter Handler
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2024, The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+
+
+def handler(*args, **kwargs):
+    print("Start handler with arguments", args, kwargs)
+    yield args[0]
