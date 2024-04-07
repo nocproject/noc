e7144198eadd5c3e1b97ac3a095dbee626f154c5
Michael R <rmiclee@gmail.com>
2024-04-07 22:46:25 +0300

    Run tests

--- urls.py
+++ urls.py
@@ -24,6 +24,7 @@ site.autodiscover()
 urlpatterns = site.urls + [path("jsi18n/", JavaScriptCatalog.as_view(), name="javascript-catalog")]
 
 
+#
 def handler500(request):
     error_report()
     return HttpResponseServerError("Internal Server Error")
