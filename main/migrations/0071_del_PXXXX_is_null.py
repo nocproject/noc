b69790ffe7496254a0432fc9d5b4351fea3c77a4
Andrey Vertiprahov <aversanta@gmail.com>
2024-04-14 00:01:04 +0500

    Fix remove Pool p0001 migration.

--- main/migrations/0071_del_PXXXX_is_null.py
+++ main/migrations/0071_del_PXXXX_is_null.py
@@ -12,11 +12,11 @@ import re
 class Migration(BaseMigration):
     def migrate(self):
         coll = self.mongo_db["noc.pools"]
-        pattern = re.compile(r"^D\d{4}$")
+        pattern = re.compile(r"^P\d{4}$")
 
         for pool in coll.find({}):
             rows_obj = self.db.execute(
                 "select name from sa_managedobject where pool = %s", [str(pool["_id"])]
             )
             if not rows_obj and pattern.match(pool["name"]):
-                coll.deleteOne({"_id": pool["_id"]})
+                coll.delete_one({"_id": pool["_id"]})
