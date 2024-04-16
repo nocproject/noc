47fbfe10fbf81dd834a6378ccba4a31e3edb75df
Dmitry Volodin <dv@nocproject.org>
2024-04-16 11:29:18 +0200

    Facade skeleton

--- ui/web/inv/inv/plugins/facade/FacadePanel.js
+++ ui/web/inv/inv/plugins/facade/FacadePanel.js
@@ -7,25 +7,26 @@
 console.debug("Defining NOC.inv.inv.plugins.facade.FacadePanel");
 
 Ext.define("NOC.inv.inv.plugins.facade.FacadePanel", {
-    extend: "Ext.panel.Panel",
-    requires: [
-    ],
-    title: __("Facade"),
-    closable: false,
-    scrollable: true,
+  extend: "Ext.panel.Panel",
+  requires: [],
+  title: __("Facade"),
+  closable: false,
+  scrollable: true,
 
-    initComponent: function () {
-        var me = this;
+  initComponent: function () {
+    var me = this;
 
-        // Grids
-        Ext.apply(me, {
-            items: [
-            ]
-        });
-        me.callParent();
-    },
-    //
-    preview: function (data) {
-        var me = this;
-    }
+    me.facadeViewPanel = Ext.create("Ext.container.Container", {
+      scrollable: true,
+      region: "center",
+    });
+    Ext.apply(me, {
+      items: [me.facadeViewPanel],
+    });
+    me.callParent();
+  },
+  //
+  preview: function (data) {
+    var me = this;
+  },
 });
