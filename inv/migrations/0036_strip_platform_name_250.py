e0fa5f86e12175b035715381f243760aaf76573e
Andrey Vertiprahov <aversanta@gmail.com>
2024-04-17 12:02:56 +0000

    #2458 Add MAX_PLATFORM_LENGTH check to version discovery.

---
+++ inv/migrations/0036_strip_platform_name_250.py
@@ -0,0 +1,29 @@
+# ----------------------------------------------------------------------
+# Strict Platform name to 200
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2024 The NOC Project
+# See LICENSE for details
+# ----------------------------------------------------------------------
+
+# Third-party modules
+from pymongo import UpdateOne
+
+# NOC modules
+from noc.core.migration.base import BaseMigration
+
+MAX_PLATFORM_LENGTH = 200
+
+
+class Migration(BaseMigration):
+    def migrate(self):
+        platform_coll = self.mongo_db["noc.platforms"]
+        bulk = []
+        for p in platform_coll.find({}, {"name": 1}):
+            if len(p["name"]) > MAX_PLATFORM_LENGTH:
+                bulk += [
+                    UpdateOne(
+                        {"_id": p["_id"]}, {"$set": {"name": p["name"][:MAX_PLATFORM_LENGTH]}}
+                    )
+                ]
+        if bulk:
+            platform_coll.bulk_write(bulk)
