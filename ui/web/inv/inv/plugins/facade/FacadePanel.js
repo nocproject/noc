a4553dc9800fcce611da9c34970319f579543623
uncol <d.a.zakharov@gmail.com>
2024-04-19 12:49:34 +0300

    front/rear switcher added

--- ui/web/inv/inv/plugins/facade/FacadePanel.js
+++ ui/web/inv/inv/plugins/facade/FacadePanel.js
@@ -12,13 +12,12 @@ Ext.define("NOC.inv.inv.plugins.facade.FacadePanel", {
   title: __("Facade"),
   closable: false,
   scrollable: true,
-
-  initComponent: function () {
+  //
+  initComponent: function(){
     var me = this;
-
     me.facadeViewPanel = Ext.create("Ext.container.Container", {
       scrollable: true,
-      region: "center",
+      layout: "fit",
     });
     Ext.apply(me, {
       items: [me.facadeViewPanel],
@@ -26,21 +25,63 @@ Ext.define("NOC.inv.inv.plugins.facade.FacadePanel", {
     me.callParent();
   },
   //
-  preview: function (data) {
-    var me = this;
-    Ext.each(data.views, function(view) {
-      me.facadeViewPanel.add([
-        {
-          xtype: "component",
-          html: Ext.String.format("<h2 style='padding-left: 10px;'>{0}</h2>", view.name),
-        },
-        {
-          xtype: "image",
-          src: view.src,
-          title: view.name,
-          padding: 5,
-        }
-      ]);
-    });
+  preview: function(data){
+    var me = this,
+      viewQty = data.views.length;
+    me.facadeViewPanel.removeAll();
+    switch(viewQty){
+      case 1:
+        me.facadeViewPanel.add(me.makeView(data.views[0]));
+        break;
+      default:
+        me.facadeViewPanel.add([
+          {
+            xtype: "container",
+            padding: 10,
+            items: [{
+              xtype: "radiogroup",
+              fieldLabel: __("Choose view"),
+              vertical: false,
+              items: Ext.Array.map(data.views, function(view, index){
+                return{
+                  boxLabel: view.name,
+                  name: "chooseView",
+                  margin: "0 10 0 0",
+                  inputValue: index,
+                }
+              }),
+              listeners: {scope: me, change: me.onChooseView},
+            }],
+          },
+          {
+            xtype: "container",
+            itemId: "viewPanel",
+            layout: "card",
+            activeItem: 0,
+            items: Ext.Array.map(data.views, function(view){ return{xtype: "container", layout: "fit", items: me.makeView(view)} }), 
+          },
+        ]);
+        me.facadeViewPanel.down("radiogroup").setValue({chooseView: 0});
+        break;
+    }
+  },
+  //
+  onChooseView: function(field){
+    this.down("[itemId=viewPanel]").setActiveItem(field.getValue().chooseView);
+  },
+  //
+  makeView: function(view){
+    return[
+      {
+        xtype: "component",
+        html: Ext.String.format("<h2 style='padding: 10px 0 0 10px;'>{0}</h2>", view.name),
+      },
+      {
+        xtype: "image",
+        src: view.src,
+        title: view.name,
+        padding: 5,
+      },
+    ]
   },
 });
