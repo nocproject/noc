74869a06383760c12d1f28e183e0737948f6be4f
uncol <d.a.zakharov@gmail.com>
2024-04-25 14:00:57 +0300

    add more space

--- ui/web/main/desktop/About.js
+++ ui/web/main/desktop/About.js
@@ -7,41 +7,41 @@
 console.debug("Defining NOC.main.desktop.About");
 
 Ext.define("NOC.main.desktop.About", {
-    extend: "Ext.Window",
-    title: __("About system"),
-    layout: "fit",
-    autoShow: true,
-    resizable: false,
-    closable: true,
-    modal: true,
-    version: null,
-    installation: null,
-    width: 600,
-    height: 132,
-    app: null,
-    aboutCfg: null,
+  extend: "Ext.Window",
+  title: __("About system"),
+  layout: "fit",
+  autoShow: true,
+  resizable: false,
+  closable: true,
+  modal: true,
+  version: null,
+  installation: null,
+  width: 600,
+  app: null,
+  aboutCfg: null,
 
-    initComponent: function () {
-        var me = this;
-        Ext.apply(me, {
-            items: [
-                {
-                    xtype: "container",
-                    html: new Ext.XTemplate('<div class="noc-tp">\n' +
-                        '    <img src="{logo_url}" style="width: 100px; height: 100px; padding: 8px; float: left"></img>\n' +
-                        '    <div style="font-size: 16pt; font-weight: bold">{brand} {version}</div>\n' +
-                        '    <div style="font-size: 12pt; font-style: italic">{installation}</div>\n' +
-                        '    <tpl if="system_id">\n' +
-                        '        <div style="font-size: 12pt">System ID: {system_id}</div>\n' +
-                        '    <tpl elseif=\'brand === "NOC"\'>\n' +
-                        '        <div style="font-size: 12pt">Unregistred system</div>\n' +
-                        '    </tpl>\n' +
-                        '    <div style="">Copyright &copy; {copyright}</div>\n' +
-                        '    <a href=\'http://nocproject.org/\' target=\'_\'>nocproject.org</a>\n' + '</div>'
-                    ).apply(me.aboutCfg)
-                }
-            ]
-        });
-        me.callParent();
-    }
+  initComponent: function(){
+    var me = this;
+    Ext.apply(me, {
+      items: [
+        {
+          xtype: "container",
+          margin: 40,
+          html: new Ext.XTemplate('<div style="padding: 40px;">\n' +
+                        '<img src="{logo_url}" style="width: 100px; height: 100px; padding: 8px; float: left"></img>\n' +
+                        '<div style="font-size: 16pt; font-weight: bold">{brand} {version}</div>\n' +
+                        '<div style="font-size: 12pt; font-style: italic">{installation}</div>\n' +
+                        ' <tpl if="system_id">\n' +
+                        '  <div style="font-size: 12pt">System ID: {system_id}</div>\n' +
+                        ' <tpl elseif=\'brand === "NOC"\'>\n' +
+                        '  <div style="font-size: 12pt">Unregistred system</div>\n' +
+                        '</tpl>\n' +
+                        '<div style="">Copyright &copy; {copyright}</div>\n' +
+                        '<a href=\'http://nocproject.org/\' target=\'_\'>nocproject.org</a>\n' + '</div>',
+          ).apply(me.aboutCfg),
+        },
+      ],
+    });
+    me.callParent();
+  },
 });
