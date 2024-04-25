f9118a75ba14cb880b14bf501afae322aa0be066
uncol <d.a.zakharov@gmail.com>
2024-04-25 11:30:56 +0000

    Change background color of the navigation tab bar

--- ui/web/main/desktop/NavPanel.js
+++ ui/web/main/desktop/NavPanel.js
@@ -7,45 +7,49 @@
 console.debug("Defining NOC.main.desktop.NavPanel");
 
 Ext.define("NOC.main.desktop.NavPanel", {
-    extend: "Ext.tree.Panel",
-    region: "west",
-    width: 200,
-    //collapsible: true,
-    animCollapse: true,
-    collapseFirst: false,
-    split: true,
-    layout: "fit",
-    app: null,
-    useArrows: true,
-    rootVisible: false,
-    singleExpand: true,
-    lines: false,
-    hideHeaders: true,
-    title: __("Navigation"),
-    glyph: NOC.glyph.globe,
-    store: null,
-    //hidden: true,
+  extend: "Ext.tree.Panel",
+  region: "west",
+  width: 200,
+  //collapsible: true,
+  animCollapse: true,
+  collapseFirst: false,
+  split: true,
+  layout: "fit",
+  app: null,
+  useArrows: true,
+  rootVisible: false,
+  singleExpand: true,
+  lines: false,
+  hideHeaders: true,
+  title: __("Navigation"),
+  glyph: NOC.glyph.globe,
+  store: null,
+  header: {
+    cls: "noc-navigation-header",
+  },
+  //hidden: true,
 
-    initComponent: function() {
-        var me = this;
-        Ext.apply(me, {
-            tools: [{
-                type: "up",
-                tooltip: __("Switch to breadcrumb view"),
-                listeners: {
-                    scope: me,
-                    click: function() {me.app.toggleNav();}
-                }
-            }],
-            listeners: {
-                scope: me,
-                itemclick: me.onItemClick
-            }
-        });
-        me.callParent();
-    },
-    onItemClick: function(view, record, item, index, event, opts) {
-        var me = this;
-        me.app.launchRecord(record, !event.shiftKey);
-    }
+  initComponent: function(){
+    var me = this;
+    Ext.apply(me, {
+      tools: [{
+        type: "up",
+        tooltip: __("Switch to breadcrumb view"),
+        cls: "noc-navigation-tool", 
+        listeners: {
+          scope: me,
+          click: function(){me.app.toggleNav();},
+        },
+      }],
+      listeners: {
+        scope: me,
+        itemclick: me.onItemClick,
+      },
+    });
+    me.callParent();
+  },
+  onItemClick: function(view, record, item, index, event){
+    var me = this;
+    me.app.launchRecord(record, !event.shiftKey);
+  },
 });
