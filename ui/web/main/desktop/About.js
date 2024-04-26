8f687a930693cb5e142e3666c557cc20634e2f93
Dmitry Volodin <dv@nocproject.org>
2024-04-26 10:41:22 +0200

    Fix about layout

--- ui/web/main/desktop/About.js
+++ ui/web/main/desktop/About.js
@@ -8,36 +8,39 @@ console.debug("Defining NOC.main.desktop.About");
 
 Ext.define("NOC.main.desktop.About", {
   extend: "Ext.Window",
-  title: __("About system"),
+  title: __("About System"),
   layout: "fit",
   autoShow: true,
   resizable: false,
   closable: true,
   modal: true,
-  version: null,
-  installation: null,
-  width: 600,
+  width: 380,
+  height: 190,
   app: null,
   aboutCfg: null,
 
-  initComponent: function(){
+  initComponent: function () {
     var me = this;
     Ext.apply(me, {
       items: [
         {
           xtype: "container",
           margin: 40,
-          html: new Ext.XTemplate('<div style="padding: 40px;">\n' +
-                        '<img src="{logo_url}" style="width: 100px; height: 100px; padding: 8px; float: left"></img>\n' +
-                        '<div style="font-size: 16pt; font-weight: bold">{brand} {version}</div>\n' +
-                        '<div style="font-size: 12pt; font-style: italic">{installation}</div>\n' +
-                        ' <tpl if="system_id">\n' +
-                        '  <div style="font-size: 12pt">System ID: {system_id}</div>\n' +
-                        ' <tpl elseif=\'brand === "NOC"\'>\n' +
-                        '  <div style="font-size: 12pt">Unregistred system</div>\n' +
-                        '</tpl>\n' +
-                        '<div style="">Copyright &copy; {copyright}</div>\n' +
-                        '<a href=\'http://getnoc.com/\' target=\'_\'>getnoc.com</a>\n' + '</div>',
+          html: new Ext.XTemplate(
+            '<div style="display: flex; flex-direction: row; padding: 20">\n' +
+              '<img src="{logo_url}" style="width: 100px; height: 100px">\n' +
+              '<div style="padding-left: 10">\n' +
+              '<div style="font-size: 16pt; font-weight: bold">{brand} {version}</div>\n' +
+              '<div style="font-size: 12pt; font-style: italic">{installation}</div>\n' +
+              ' <tpl if="system_id">\n' +
+              '  <div style="font-size: 12pt">System ID: {system_id}</div>\n' +
+              " <tpl elseif='brand === \"NOC\"'>\n" +
+              '  <div style="font-size: 12pt">Unregistred system</div>\n' +
+              "</tpl>\n" +
+              '<div style="">Copyright &copy; {copyright}</div>\n' +
+              "<a href='http://getnoc.com/' target='_'>getnoc.com</a>\n" +
+              "</div>" +
+              "</div>",
           ).apply(me.aboutCfg),
         },
       ],
