3ea3f0bdb483cbb7cf650934a09c6bfc9b78c370
Dmitry Volodin <dv@nocproject.org>
2024-04-28 09:15:18 +0200

    fix layout

--- ui/web/main/desktop/About.js
+++ ui/web/main/desktop/About.js
@@ -1,7 +1,7 @@
 //---------------------------------------------------------------------
 // Login window
 //---------------------------------------------------------------------
-// Copyright (C) 2007-2011 The NOC Project
+// Copyright (C) 2007-2024 The NOC Project
 // See LICENSE for details
 //---------------------------------------------------------------------
 console.debug("Defining NOC.main.desktop.About");
@@ -14,10 +14,10 @@ Ext.define("NOC.main.desktop.About", {
   resizable: false,
   closable: true,
   modal: true,
-  width: 380,
-  height: 190,
   app: null,
   aboutCfg: null,
+  width: 380,
+  height: 190,
 
   initComponent: function () {
     var me = this;
@@ -25,19 +25,20 @@ Ext.define("NOC.main.desktop.About", {
       items: [
         {
           xtype: "container",
-          margin: 40,
+          width: 380,
+          height: 190,
           html: new Ext.XTemplate(
             '<div style="display: flex; flex-direction: row; padding: 20">\n' +
               '<img src="{logo_url}" style="width: 100px; height: 100px">\n' +
-              '<div style="padding-left: 10">\n' +
-              '<div style="font-size: 16pt; font-weight: bold">{brand} {version}</div>\n' +
-              '<div style="font-size: 12pt; font-style: italic">{installation}</div>\n' +
+              '<div style="padding-left: 10; display: flex; flex-direction: column; font-size: 16px">\n' +
+              '<div style="font-size: 24px; font-weight: bold">{brand} {version}</div>\n' +
+              '<div style="font-style: italic">{installation}</div>\n' +
               ' <tpl if="system_id">\n' +
-              '  <div style="font-size: 12pt">System ID: {system_id}</div>\n' +
+              "  <div>System ID: {system_id}</div>\n" +
               " <tpl elseif='brand === \"NOC\"'>\n" +
-              '  <div style="font-size: 12pt">Unregistred system</div>\n' +
+              "  <div>System ID: Unregistred</div>\n" +
               "</tpl>\n" +
-              '<div style="">Copyright &copy; {copyright}</div>\n' +
+              "<div>Copyright &copy; {copyright}</div>\n" +
               "<a href='http://getnoc.com/' target='_'>getnoc.com</a>\n" +
               "</div>" +
               "</div>",
