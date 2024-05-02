4523560cb7d8044cf251c45dbd4ab3ef76622900
uncol <d.a.zakharov@gmail.com>
2024-05-02 15:03:52 +0300

    save

--- ui/web/inv/inv/plugins/facade/FacadePanel.js
+++ ui/web/inv/inv/plugins/facade/FacadePanel.js
@@ -15,7 +15,6 @@ Ext.define("NOC.inv.inv.plugins.facade.FacadePanel", {
   //
   initComponent: function(){
     var me = this;
-
     me.reloadButton = Ext.create("Ext.button.Button", {
       glyph: NOC.glyph.refresh,
       scope: me,
@@ -44,6 +43,28 @@ Ext.define("NOC.inv.inv.plugins.facade.FacadePanel", {
       },
     });
 
+    me.zoomButton = Ext.create("Ext.form.ComboBox", {
+      store: [
+        [0.25, "25%"],
+        [0.5, "50%"],
+        [0.75, "75%"],
+        [1.0, "100%"],
+        [1.25, "125%"],
+        [1.5, "150%"],
+        [2.0, "200%"],
+        [3.0, "300%"],
+        [4.0, "400%"],
+      ],
+      width: 100,
+      value: 1.0,
+      valueField: "zoom",
+      displayField: "label",
+      listeners: {
+        scope: me,
+        select: me.onZoom,
+      },
+    });
+
     me.segmentedButton = Ext.create("Ext.button.Segmented", {
       items: [me.sideFrontButton, me.sideRearButton],
     });
@@ -53,18 +74,20 @@ Ext.define("NOC.inv.inv.plugins.facade.FacadePanel", {
       layout: "card",
       activeItem: 0,
     });
+
     me.facadeViewPanel = Ext.create("Ext.container.Container", {
       scrollable: true,
       layout: "fit",
       items: [me.viewCard],
     });
+
     Ext.apply(me, {
       items: [me.facadeViewPanel],
       dockedItems: [
         {
           xtype: "toolbar",
           dock: "top",
-          items: [me.reloadButton, "-", me.segmentedButton],
+          items: [me.reloadButton, "-", me.segmentedButton, me.zoomButton],
         },
       ],
     });
@@ -78,7 +101,7 @@ Ext.define("NOC.inv.inv.plugins.facade.FacadePanel", {
     me.viewCard.removeAll();
     me.viewCard.add(
       Ext.Array.map(data.views, function(view){
-        return{
+        return {
           xtype: "container",
           layout: "fit",
           items: [
@@ -97,6 +120,7 @@ Ext.define("NOC.inv.inv.plugins.facade.FacadePanel", {
     // Press front button
     me.sideFrontButton.setPressed(true);
   },
+  //
   onReload: function(){
     var me = this;
     Ext.Ajax.request({
@@ -110,5 +134,9 @@ Ext.define("NOC.inv.inv.plugins.facade.FacadePanel", {
         NOC.error(__("Failed to get data"));
       },
     });
+  },
+  //
+  onZoom: function(combo){
+    console.log("Zoom to", combo.getValue());
   },
 });
