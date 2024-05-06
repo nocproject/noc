922ddbe7e5dfd0c1ee5a417eb8e805c33187c699
Andrey Vertiprahov <aversanta@gmail.com>
2024-05-06 13:42:49 +0000

    release-23.1:Backport!7993

--- sa/profiles/Eltex/WOPLR/get_version.py
+++ sa/profiles/Eltex/WOPLR/get_version.py
@@ -1,9 +1,12 @@
 # ---------------------------------------------------------------------
 # Eltex.WOPLR.get_version
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2019 The NOC Project
+# Copyright (C) 2007-2024 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
+
+# Python modules
+import re
 
 # NOC modules
 from noc.core.script.base import BaseScript
@@ -15,18 +18,25 @@ class Script(BaseScript):
     cache = True
     interface = IGetVersion
 
+    rx_platform = re.compile(r"^\s*hw-platform\s*(\||:)\s*(?P<platform>.+)", re.MULTILINE)
+    rx_version = re.compile(r"^\s*software-version\s*(\||:)\s*(?P<version>\S+)", re.MULTILINE)
+    rx_sn = re.compile(r"^\s*factory-serial-number\s*(\||:)\s*(?P<sn>\S+)", re.MULTILINE)
+    rx_hwversion = re.compile(r"^\s*hw-revision\s*(\||:)\s*(?P<hwversion>\S+)", re.MULTILINE)
+
     def execute_cli(self, **kwargs):
         c = self.cli("monitoring information", cached=True)
-        for line in c.splitlines():
-            r = line.split(":", 1)
-            if r[0].strip() == "software-version":
-                version = r[1].strip()
-            if r[0].strip() == "hw-platform":
-                platform = r[1].strip()
-            if r[0].strip() == "factory-serial-number":
-                sn = r[1].strip()
-            if r[0].strip() == "hw-revision":
-                hwversion = r[1].strip()
+
+        match = self.rx_platform.search(c)
+        platform = match.group("platform")
+
+        match = self.rx_version.search(c)
+        version = match.group("version")
+
+        match = self.rx_sn.search(c)
+        sn = match.group("sn")
+
+        match = self.rx_hwversion.search(c)
+        hwversion = match.group("hwversion")
         return {
             "vendor": "Eltex",
             "platform": platform,
