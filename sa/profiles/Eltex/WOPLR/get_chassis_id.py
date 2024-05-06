09696363cc6b90d439e55a72d0003ab9be3df384
Aleksandr Tratsevski <mranderson@internet.ru>
2024-05-06 13:19:28 +0000

    Fix get_version, get_chassis_id and add scripts for Eltex.WOPLR

--- sa/profiles/Eltex/WOPLR/get_chassis_id.py
+++ sa/profiles/Eltex/WOPLR/get_chassis_id.py
@@ -1,9 +1,12 @@
 # ---------------------------------------------------------------------
 # Eltex.WOPLR.get_chassis_id
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2019 The NOC Project
+# Copyright (C) 2007-2024 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
+
+# Python modules
+import re
 
 # NOC modules
 from noc.sa.profiles.Generic.get_chassis_id import Script as BaseScript
@@ -14,10 +17,14 @@ class Script(BaseScript):
     name = "Eltex.WOPLR.get_chassis_id"
     interface = IGetChassisID
 
+    rx_mac_wan = re.compile(r"^\s*factory-wan-mac\s*(\||:)\s*(?P<wan_mac>\S+)", re.MULTILINE)
+    rx_mac_lan = re.compile(r"^\s*factory-lan-mac\s*(\||:)\s*(?P<lan_mac>\S+)", re.MULTILINE)
+
     def execute_cli(self, **kwargs):
         d = self.cli("monitoring information", cached=True)
-        for line in d.splitlines():
-            r = line.split(":", 1)
-            if r[0].strip() == "factory-wan-mac":
-                mac = r[1].strip()
-        return [{"first_chassis_mac": mac, "last_chassis_mac": mac}]
+        match = self.rx_mac_wan.search(d)
+        wan_mac = match.group("wan_mac")
+
+        match = self.rx_mac_lan.search(d)
+        lan_mac = match.group("lan_mac")
+        return [{"first_chassis_mac": wan_mac, "last_chassis_mac": lan_mac}]
