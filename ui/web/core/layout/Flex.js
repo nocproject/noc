f4436d1080741835a52da9db4defd831a93870df
uncol <d.a.zakharov@gmail.com>
2024-05-14 23:33:21 +0300

    panels added

---
+++ ui/web/core/layout/Flex.js
@@ -0,0 +1,55 @@
+//---------------------------------------------------------------------
+// Flex Layout
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2024 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+
+console.debug("Defining NOC.core.layout.Flex");
+
+Ext.define("NOC.core.layout.Flex", {
+  extend: "Ext.layout.container.Container",
+  alias: "layout.flex",
+  type: "flex",
+
+  config: {
+    display: "flex",
+    direction: "row",
+    wrap: "wrap",
+    justifyContent: "flex-start",
+    alignItems: "stretch",
+  },
+
+  autoSize: Ext.emptyFn,
+
+  beginLayout: function(){
+    var me = this,
+      target = me.getRenderTarget();
+
+    me.callParent(arguments);
+
+    target.setStyle({
+      flexDirection: me.direction || "row",
+      flexWrap: me.wrap || "wrap",
+      justifyContent: me.justifyContent || "flex-start",
+      alignItems: me.alignItems || "stretch",
+    });
+  },
+
+  beginLayoutCycle: function(){
+    var me = this,
+      items = me.getLayoutItems(),
+      len = items.length,
+      i, item;
+
+    me.callParent(arguments);
+    for(i = 0; i < len; i++){
+      item = items[i];
+      item.el.setStyle({
+        flex: item.flex || "1 1 auto",
+        margin: item.margin || "10px",
+        minWidth: item.minWidth || "auto",
+      });
+    }
+  },
+});
--- ui/web/main/home/Application.js
+++ ui/web/main/home/Application.js
@@ -7,7 +7,116 @@
 console.debug("Defining NOC.main.home.Application");
 
 Ext.define("NOC.main.home.Application", {
-    extend: "NOC.core.Application",
-    //requires: [],
-    items: []
+  extend: "NOC.core.Application",
+  requires: [],
+  items: [
+    {
+      xtype: "container",
+      layout: {
+        type: "table",
+        columns: 3,
+        tdAttrs: {
+          style: {
+            "vertical-align": "top",
+          },
+        },
+      },
+      scrollable: true,
+      smallHeight: 90,
+      normalHeight: 200,
+      extraHeight: 310,
+      doubleHeight: 420,
+      cellWidth: 200,
+      cellMargin: 10,
+      items: [],
+      listeners: {
+        render: function(panel){
+          panel.mask(__("Loading..."));
+          Ext.Ajax.request({
+            method: "GET",
+            url: "/main/home/dashboard/",
+            scope: this,
+            success: function(response){
+              var data = Ext.decode(response.responseText);
+              if(Object.prototype.hasOwnProperty.call(data, "widgets") && !Ext.isEmpty(data.widgets)){
+                var width = panel.getWidth(),
+                  widgets = data.widgets,
+                  columns = Math.floor(width / this.cellWidth);
+                if((columns * (this.cellWidth + this.cellMargin * 2)) > width){
+                  columns -= 1;
+                }
+                panel.layout.columns = columns;
+                Ext.each(widgets, function(widget){
+                  var items;
+                  switch(widget.type){
+                    case "favorites":
+                      var html = "<ul style='list-style-type: none;margin: 0;padding-left: 5px'>";
+                      Ext.each(widget.data.items, function(item){
+                        html += "<li><a href='" + item.link + "'>" + item.title
+                              + "</a></li><ul style='list-style-type: none;margin: 0;padding-left: 5px'>";
+                        Ext.each(item.items, function(subitem){
+                          html += "<li><a href='" + subitem.link + "'>" + subitem.text + "</a></li>";
+                        });
+                        html += "</ul>";
+                      });
+                      items = [
+                        {
+                          xtype: "container",
+                          margin: 5,
+                          html: html + "</ul>", 
+                        },
+                      ];    
+                      break;
+                    case "summary":
+                      var rows = Ext.Array.map(widget.items, function(item){
+                        var value = item.value;
+                        if(Object.prototype.hasOwnProperty.call(item, "link")){
+                          value = "<a href='" + item.link + "'>" + item.value + "</a>";
+                        }
+                        return "<tr><td>" + item.text + "</td><td style='padding-left: 5px;'>" + value + "</td></tr>";
+                      });
+                      items = [
+                        {
+                          xtype: "container",
+                          margin: 5,
+                          html: "<table>" + rows.join("") + "</table>",
+                        },
+                      ];    
+                      break;
+                    case "text":
+                      items = [
+                        {
+                          xtype: "container",
+                          style: {
+                            textAlign: "center",
+                          },
+                          html: widget.data.text,
+                        },
+                      ];
+                      break;
+                  }
+                  panel.add(
+                    Ext.create("Ext.panel.Panel", {
+                      title: widget.title,
+                      border: true,
+                      width: this.cellWidth,
+                      margin: this.cellMargin,
+                      height: this[widget.height + "Height"] || this.normalHeight,
+                      scrollable: true,
+                      items: items,
+                    }),
+                  );
+                }, this);
+              }
+              panel.unmask();
+            },
+            failure: function(){
+              panel.unmask();
+              NOC.error(__("Failed to get home data"));
+            },
+          });
+        },
+      },
+    },
+  ],
 });
