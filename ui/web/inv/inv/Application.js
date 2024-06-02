c45e3127599ceb448df6d6590b15d26e443c01e6
Andrey Vertiprahov <aversanta@gmail.com>
2024-06-02 13:41:01 +0000

    release-24.1:Backport!8102

--- ui/web/inv/inv/Application.js
+++ ui/web/inv/inv/Application.js
@@ -150,7 +150,7 @@ Ext.define("NOC.inv.inv.Application", {
       app: me,
       listeners: {
         scope: me,
-        reloadInvNav: me.onReloadNav, 
+        reloadInvNav: me.onReloadNav,
       },
     });
     //
