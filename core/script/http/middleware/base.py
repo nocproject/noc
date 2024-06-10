347e52a5917d76306fb7dfdb56cb18b9e9fbf571
Andrey Vertiprahov <aversanta@gmail.com>
2024-06-10 05:48:52 +0000

    Fix encode HTTP headers value when set script.

--- core/script/http/middleware/base.py
+++ core/script/http/middleware/base.py
@@ -1,9 +1,12 @@
 # ----------------------------------------------------------------------
 # BaseMiddleware class
 # ----------------------------------------------------------------------
-# Copyright (C) 2007-2019 The NOC Project
+# Copyright (C) 2007-2024 The NOC Project
 # See LICENSE for details
 # ----------------------------------------------------------------------
+
+# Python modules
+from typing import Dict, Any
 
 
 class BaseMiddleware(object):
@@ -12,14 +15,14 @@ class BaseMiddleware(object):
     def __init__(self, http):
         self.http = http
 
-    def process_request(self, url, body, headers):
+    def process_request(self, url: str, body: Any, headers: Dict[str, bytes]):
         return url, body, headers
 
-    def process_get(self, url, body, headers):
+    def process_get(self, url: str, body: Any, headers: Dict[str, bytes]):
         return self.process_request(url, body, headers)
 
-    def process_post(self, url, body, headers):
+    def process_post(self, url: str, body: Any, headers: Dict[str, bytes]):
         return self.process_request(url, body, headers)
 
-    def process_put(self, url, body, headers):
+    def process_put(self, url: str, body: Any, headers: Dict[str, bytes]):
         return self.process_request(url, body, headers)
