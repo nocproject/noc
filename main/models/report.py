03f81c0716e18c821c63fd98f41bea4bb1687322
Michael R <rmiclee@gmail.com>
2024-07-15 08:37:32 +0000

    Merge branch 'issue-2516-fix-column-titles' into 'master'
    #2516 Fix column titles and localization for joined datasources in csv-output-reports
    
    See merge request noc/noc!8231
    
    (cherry picked from commit 3cd2c000ce19b230327a75d885c28b87a617860b)
    
    b487002c Fix column titles for joined datasources in csv-output-reports
    c8403f5a Fix py-lint
    396e80d5 Refactor
    
    Co-authored-by: Andrey Vertiprahov <aversanta@gmail.com>

--- main/models/report.py
+++ main/models/report.py
@@ -295,8 +295,9 @@ class Report(Document):
         for bf in self.bands_format:
             columns = []
             for cf in bf.column_format:
+                *_, cf_name = cf["name"].rsplit(".", 1)
                 cf["title"] = (
-                    self.get_localization(f"columns.{cf['name']}", lang=pref_lang) or cf["title"]
+                    self.get_localization(f"columns.{cf_name}", lang=pref_lang) or cf["title"]
                 )
                 columns += [cf]
             b_format_cfg[bf.name] = BandFormatCfg(
