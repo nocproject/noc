72d4a2f763ad0b4a0e6280a3f79a9aeea26a169f
Andrey Vertiprahov <aversanta@gmail.com>
2024-08-09 18:09:27 +0500

    Fix CLI diagnostic check.

--- core/script/diagnostic.py
+++ core/script/diagnostic.py
@@ -85,98 +85,41 @@ class CLISuggestsDiagnostic:
         logger=None,
         address: Optional[str] = None,
         profile: Optional[str] = None,
-        pool: Optional[str] = None,
         cred: Optional[Union[SNMPCredential, SNMPv3Credential]] = None,
+        **kwargs,
     ):
         self.config = cfg
         self.labels = set(labels or [])
         self.logger = logger or logging.getLogger("clisuggestsdiagnostic")
         self.address = address
-        self.cred = cred
         self.profile = profile
-        self.pool = pool
+        self.cred = cred
 
     def iter_checks(self) -> Iterable[Tuple[Check, ...]]:
-        yield []
+        r = []
+        for c in self.config.checks:
+            r.append(
+                Check(
+                    name=c.name,
+                    address=c.address,
+                    port=c.port,
+                    args={"arg0": self.profile},
+                    credential=c.credential,
+                )
+            )
+        yield r
 
     def get_result(
         self, checks: List[CheckResult]
     ) -> Optional[Tuple[Optional[bool], Optional[str], Optional[Dict[str, Any]]]]:
         """Getting Diagnostic result: State and reason"""
-        if not self.profile or self.profile == GENERIC_PROFILE:
-            return
-        for c in self.config.checks:
-            if not isinstance(c.credential, CLICredential):
+        error = ""
+        print("Get Result", checks)
+        for c in checks:
+            if c.skipped:
                 continue
-            if c.name == "TELNET":
-                proto = Protocol(1)
-                port = 21
-            else:
-                proto = Protocol(2)
-                port = 22
-            r, error = self.check_login(
-                self.address,
-                port,
-                c.credential.user,
-                c.credential.password,
-                c.credential.super_password,
-                protocol=proto,
-                raise_privilege=c.credential.raise_privilege,
-            )
-            if r:
-                return True, None, None
+            elif c.error and c.error.message:
+                error = c.error.message
+            if c.status:
+                return True, None, {}
         return False, error, None
-
-    def check_login(
-        self,
-        address: str,
-        port: int,
-        user: str,
-        password: str,
-        super_password: str,
-        protocol: Protocol,
-        raise_privilege: bool = True,
-    ) -> Tuple[bool, str]:
-        """
-        Check user, password for cli proto
-        Args:
-            address:
-            port:
-            user:
-            password:
-            super_password:
-            protocol:
-            raise_privilege:
-        """
-        self.logger.debug("Checking %s: %s/%s/%s", protocol, user, password, super_password)
-        self.logger.info(
-            "Checking %s: %s/%s/%s",
-            protocol,
-            safe_shadow(user),
-            safe_shadow(password),
-            safe_shadow(super_password),
-        )
-        try:
-            r = open_sync_rpc(
-                "activator",
-                pool=self.pool,
-                calling_service="clisuggestsdiagnostic",  # calling_service=self.calling_service
-            ).script(
-                f"{self.profile}.login",
-                {
-                    "cli_protocol": protocol.config.alias,
-                    "cli_port": port,
-                    "address": address,
-                    "user": user,
-                    "password": password,
-                    "super_password": super_password,
-                    "path": None,
-                    "raise_privileges": raise_privilege,
-                    "access_preference": "C",
-                },
-            )
-            self.logger.info("Result: %s, %s", r, r["message"])
-            return bool(r["result"]), r["message"]  # bool(False) == bool(None)
-        except RPCError as e:
-            self.logger.debug("RPC Error: %s", e)
-            return False, ""
