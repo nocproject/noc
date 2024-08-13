2eeeead9334ab54dc71a4e66b63aa96e391476be
Dmitry Volodin <dv@nocproject.org>
2024-08-13 13:58:03 +0200

    Set classes and input discriminators

--- services/web/apps/inv/inv/plugins/crossing.py
+++ services/web/apps/inv/inv/plugins/crossing.py
@@ -7,6 +7,7 @@
 
 # Python modules
 from typing import Dict, Any, Set
+from collections import defaultdict
 
 # NOC modules
 from .base import InvPlugin
@@ -22,14 +23,20 @@ class CrossingPlugin(InvPlugin):
         crossings = list(o.iter_effective_crossing())
         data = [
             {
+                "id": f"c_{n}",
                 "input": c.input,
                 "input_discriminator": c.input_discriminator,
                 "output": c.output,
                 "output_discriminator": c.output_discriminator,
                 "gain_db": c.gain_db,
             }
-            for c in crossings
+            for n, c in enumerate(crossings)
         ]
+        #
+        n_input_discriminators = defaultdict(int)
+        for c in crossings:
+            if c.input_discriminator:
+                n_input_discriminators[c.input] += 1
         # Build list
         inputs: Set[str] = set()
         outputs: Set[str] = set()
@@ -66,14 +73,36 @@ class CrossingPlugin(InvPlugin):
         if mixed:
             viz["nodes"].append(self._render_node("m", mixed))
         # Add edges
-        for c in crossings:
-            viz["edges"].append(
-                {
-                    "head": "o" if c.output in outputs else "m",
-                    "tail": "i" if c.input in inputs else "m",
-                    "attributes": {"headport": c.output, "tailport": c.input},
-                }
-            )
+        for n, c in enumerate(crossings):
+            edge = {
+                "head": "o" if c.output in outputs else "m",
+                "tail": "i" if c.input in inputs else "m",
+                "attributes": {
+                    "headport": c.output,
+                    "tailport": c.input,
+                    "class": "selectable",
+                    "id": f"c_{n}",
+                },
+            }
+            label_parts = []
+            if c.input_discriminator and n_input_discriminators[c.input] == 1:
+                # Single discriminator, generate virtual edge
+                viz["nodes"].append({"name": f"vi_{c.input}", "attributes": {"shape": "point"}})
+                viz["edges"].append(
+                    {
+                        "head": "i" if c.input in inputs else "m",
+                        "tail": f"vi_{c.input}",
+                        "attributes": {"headport": c.input, "label": c.input_discriminator},
+                    }
+                )
+            elif c.input_discriminator:
+                # Multiple discriminators
+                label_parts.append(c.input_discriminator)
+            if c.output_discriminator:
+                label_parts.append(c.output_discriminator)
+            if label_parts:
+                edge["attributes"]["label"] = " ".join(label_parts)
+            viz["edges"].append(edge)
         return {"id": str(o.id), "data": data, "viz": viz}
 
     def _render_node(self, name: str, items: Set[str]) -> Dict[str, Any]:
