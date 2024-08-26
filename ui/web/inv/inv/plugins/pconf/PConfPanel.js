5cf38cb7228eb5d320e52deb60dbcbf1e89928fb
uncol <d.a.zakharov@gmail.com>
2024-08-26 20:18:45 +0300

    edit added

--- ui/web/inv/inv/plugins/pconf/PConfPanel.js
+++ ui/web/inv/inv/plugins/pconf/PConfPanel.js
@@ -14,55 +14,125 @@ Ext.define("NOC.inv.inv.plugins.pconf.PConfPanel", {
   title: __("Config"),
   closable: false,
   layout: "fit",
-
-  initComponent: function(){
-    var me = this;
-
-    // Data Store
-    me.store = Ext.create("Ext.data.Store", {
-      model: "NOC.inv.inv.plugins.pconf.PConfModel",
-    });
-    // Grids
-    Ext.apply(me, {
-      items: [
+  tbar: [
+    {
+      glyph: NOC.glyph.refresh,
+      text: __("Reload"),
+      tooltip: __("Reload"),
+      handler: function(){
+        console.log("Reload not implemented");
+      },
+    },
+    "|",
+    {
+      xtype: "textfield",
+      emptyText: __("Search..."),
+      width: 400,
+      triggers: {
+        clear: {
+          cls: "x-form-clear-trigger",
+          hidden: true,
+          handler: function(field){
+            field.setValue("");
+            var grid = field.up("panel").down("gridpanel"),
+              store = grid.getStore();
+            store.clearFilter();
+            field.getTrigger("clear").hide();
+          },
+        },
+      },
+      listeners: {
+        change: function(field, newValue){
+          var grid = field.up("panel").down("gridpanel"),
+            store = grid.getStore();
+          
+          store.clearFilter();
+          store.filterBy(function(record){
+            return record.get("name").toLowerCase().includes(newValue.toLowerCase());
+          });
+          field.getTrigger("clear")[newValue.length > 0 ? "show" : "hide"]();
+        },
+      },
+    },
+  ],
+  items: [
+    {
+      xtype: "gridpanel",
+      border: false,
+      autoScroll: true,
+      stateful: true,
+      store: {
+        model: "NOC.inv.inv.plugins.pconf.PConfModel",
+      },
+      columns: [
         {
-          xtype: "gridpanel",
-          border: false,
-          autoScroll: true,
-          stateful: true,
-          store: me.store,
-          columns: [
-            {
-              text: __("Name"),
-              dataIndex: "name",
-              width: 150,
-            },
-            {
-              text: __("Value"),
-              dataIndex: "value",
-              width: 150,
-            },
-            {
-              text: __("Units"),
-              dataIndex: "units",
-              width: 50,
-            },
-            {
-              text: __("Description"),
-              dataIndex: "description",
-              flex: 1,
-            },
-          ],
+          text: __("Name"),
+          dataIndex: "name",
+          width: 150,
+        },
+        {
+          text: __("Value"),
+          dataIndex: "value",
+          editor: "textfield",
+          width: 150,
+          renderer: function(value, metaData, record){
+            if(record.get("type") === "enum"){
+              var options = record.get("options") || [];
+              var option = options.find(opt => opt.id === value);
+              return option ? option.label : value;
+            }
+            return value;
+          },
+        },
+        {
+          text: __("Units"),
+          dataIndex: "units",
+          width: 50,
+        },
+        {
+          text: __("Description"),
+          dataIndex: "description",
+          flex: 1,
         },
       ],
-    });
-    me.callParent();
-  },
-  //
+      plugins: [
+        Ext.create("Ext.grid.plugin.CellEditing", {
+          clicksToEdit: 1,
+          listeners: {
+            beforeedit: function(editor, context){
+              var record = context.record,
+                column = context.column;
+                    
+              if(column.dataIndex === "value"){
+                if(record.get("type") === "enum"){
+                  column.setEditor({
+                    xtype: "combobox",
+                    store: {
+                      fields: ["id", "label"],
+                      data: record.get("options") || [],
+                    },
+                    valueField: "id",
+                    displayField: "label",
+                    queryMode: "local",
+                  });
+                } else{
+                  column.setEditor({
+                    xtype: "textfield",
+                  });
+                }
+              }
+            },
+          },
+        }),
+      ],
+    },
+  ],
+
   preview: function(data){
     var me = this;
     me.currentId = data.id;
     console.log(">>>", data);
-    me.store.loadData(data.conf);
+    console.log(">>>", data.conf.filter(e=>e.type === "enum"));
+    me.down("gridpanel").store.loadData(data.conf);
   },
 });
