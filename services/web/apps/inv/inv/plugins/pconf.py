7e116ed87b5e810244e176cc8a58d7ff9b2b6853
Dmitry Volodin <dv@nocproject.org>
2024-08-27 15:35:20 +0200

    set api stub

--- services/web/apps/inv/inv/plugins/pconf.py
+++ services/web/apps/inv/inv/plugins/pconf.py
@@ -9,6 +9,8 @@
 import orjson
 
 # NOC modules
+from noc.inv.models.object import Object
+from noc.sa.interfaces.base import StringParameter
 from .base import InvPlugin
 
 
@@ -17,6 +19,16 @@ class PConfPlugin(InvPlugin):
     js = "NOC.inv.inv.plugins.pconf.PConfPanel"
 
     PATH = "var/adm200/AGG-200.json"
+
+    def init_plugin(self):
+        super().init_plugin()
+        self.add_view(
+            f"api_plugin_{self.name}_set",
+            self.api_set,
+            url=f"^(?P<id>[0-9a-f]{{24}})/plugin/{self.name}/set/$",
+            method=["POST"],
+            validate={"name": StringParameter(), "value": StringParameter()},
+        )
 
     def get_data(self, request, o):
         # @todo: Temporary
@@ -58,3 +70,10 @@ class PConfPlugin(InvPlugin):
                     c["options"] = [{"id": x["val"], "label": x["dsc"]} for x in options]
                 conf.append(c)
         return {"id": str(o.id), "conf": conf}
+
+    def api_set(self, request, id:str,,name:str,value:str):
+        o = self.app.get_object_or_404(Object, id=id)
+        print(f">>> name={name}, value={value}")
+        return {
+            "status": True
+        }
