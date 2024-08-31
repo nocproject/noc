ed38c488bf57e58e599f12029dce66fd40e2296c
Andrey Vertiprahov <aversanta@gmail.com>
2024-08-31 19:29:17 +0500

    Fix typo.

--- sa/migrations/0259_managedobject_effective_groups_add_gin_index.py
+++ sa/migrations/0259_managedobject_effective_groups_add_gin_index.py
@@ -1,7 +1,7 @@
 # ----------------------------------------------------------------------
 # Create managedobject_effective_service_groups index.
 # ----------------------------------------------------------------------
-# Copyright (C) 2007-2023 The NOC Project
+# Copyright (C) 2007-2024 The NOC Project
 # See LICENSE for details
 # ----------------------------------------------------------------------
 
@@ -13,15 +13,15 @@ class Migration(BaseMigration):
     def migrate(self):
         self.db.execute(
             """
-            CREATE INDEX x_sa_managedobject_effective_service_groups_gin
+            CREATE INDEX CONCURRENTLY x_sa_managedobject_effective_service_groups_gin
             ON "sa_managedobject" USING GIN("effective_service_groups")
         """
         )
         self.db.execute(
             """
-            CREATE INDEX x_sa_managedobject_effective_client_groups_gin
+            CREATE INDEX CONCURRENTLY x_sa_managedobject_effective_client_groups_gin
             ON "sa_managedobject" USING GIN("effective_client_groups")
         """
         )
-        self.db.execute("DROP INDEX sa_managedobject_static_client_groups")
-        self.db.execute("DROP INDEX sa_managedobject_static_service_groups")
+        self.db.execute("DROP CONCURRENTLY  INDEX IF EXISTS sa_managedobject_static_client_groups")
+        self.db.execute("DROP CONCURRENTLY  INDEX IF EXISTS sa_managedobject_static_service_groups")
