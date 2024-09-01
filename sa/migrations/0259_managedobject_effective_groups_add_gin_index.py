787015e5e4784e315e78b2b074c2bc5f55f61499
Andrey Vertiprahov <aversanta@gmail.com>
2024-09-01 09:18:52 +0000

    Speedup sync_model_groups query.

---
+++ sa/migrations/0259_managedobject_effective_groups_add_gin_index.py
@@ -0,0 +1,27 @@
+# ----------------------------------------------------------------------
+# Create managedobject_effective_service_groups index.
+# ----------------------------------------------------------------------
+# Copyright (C) 2007-2024 The NOC Project
+# See LICENSE for details
+# ----------------------------------------------------------------------
+
+# NOC modules
+from noc.core.migration.base import BaseMigration
+
+
+class Migration(BaseMigration):
+    def migrate(self):
+        self.db.execute(
+            """
+            CREATE INDEX CONCURRENTLY x_sa_managedobject_effective_service_groups_gin
+            ON "sa_managedobject" USING GIN("effective_service_groups")
+        """
+        )
+        self.db.execute(
+            """
+            CREATE INDEX CONCURRENTLY x_sa_managedobject_effective_client_groups_gin
+            ON "sa_managedobject" USING GIN("effective_client_groups")
+        """
+        )
+        self.db.execute("DROP INDEX CONCURRENTLY IF EXISTS sa_managedobject_static_client_groups")
+        self.db.execute("DROP INDEX CONCURRENTLY IF EXISTS sa_managedobject_static_service_groups")
