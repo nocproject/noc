e9f40f3d63bb830fb1cfd8434269cb395061c43c
Andrey Vertiprahov <aversanta@gmail.com>
2024-09-19 10:09:59 +0000

    Add check method to script SA interface.

--- sa/interfaces/iping.py
+++ sa/interfaces/iping.py
@@ -16,6 +16,9 @@ from .base import (
 
 
 class IPing(BaseInterface):
+    check = "remoteping"
+    check_script = "ping"
+
     address = IPParameter()
     count = IntParameter(required=False)
     source_address = IPParameter(required=False)
@@ -31,3 +34,22 @@ class IPing(BaseInterface):
             "max": FloatParameter(required=False),
         }
     )
+
+    def get_check_params(self, check):
+        return {"address": check["address"]}
+
+    def clean_check_result(self, check, result):
+        return {
+            "status": bool(result["success"]),
+            "address": check["address"],
+            "metrics": [
+                {
+                    "metric_type": "Check | Result",
+                    "value": result["success"],
+                    "labels": [
+                        f"noc::check::name::{self.check}",
+                        f"noc::check::arg0::{check['address']}",
+                    ],
+                }
+            ],
+        }
