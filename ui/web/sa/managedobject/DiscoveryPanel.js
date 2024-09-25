b33cf15639a8e3cd923196cc7727f258ec4aad17
uncol <d.a.zakharov@gmail.com>
2024-09-25 12:32:44 +0300

    clipboard button added

--- ui/web/sa/managedobject/DiscoveryPanel.js
+++ ui/web/sa/managedobject/DiscoveryPanel.js
@@ -7,314 +7,328 @@
 console.debug("Defining NOC.sa.managedobject.DiscoveryPanel");
 
 Ext.define("NOC.sa.managedobject.DiscoveryPanel", {
-    extend: "Ext.panel.Panel",
-    alias: "widget.sa.discovery",
-    app: null,
-    layout: "fit",
-    autoScroll: true,
+  extend: "Ext.panel.Panel",
+  alias: "widget.sa.discovery",
+  app: null,
+  layout: "fit",
+  autoScroll: true,
 
-    initComponent: function() {
-        var me = this;
+  initComponent: function(){
+    var me = this;
 
-        me.closeButton = Ext.create("Ext.button.Button", {
-            text: __("Close"),
-            glyph: NOC.glyph.arrow_left,
-            scope: me,
-            handler: me.onClose
-        });
+    me.closeButton = Ext.create("Ext.button.Button", {
+      text: __("Close"),
+      glyph: NOC.glyph.arrow_left,
+      scope: me,
+      handler: me.onClose,
+    });
 
-        me.refreshButton = Ext.create("Ext.button.Button", {
-            text: __("Refresh"),
-            glyph: NOC.glyph.refresh,
-            scope: me,
-            handler: me.onRefresh
-        });
+    me.refreshButton = Ext.create("Ext.button.Button", {
+      text: __("Refresh"),
+      glyph: NOC.glyph.refresh,
+      scope: me,
+      handler: me.onRefresh,
+    });
 
-        me.runSelectedButton = Ext.create("Ext.button.Button", {
-            text: __("Run"),
-            glyph: NOC.glyph.play,
-            scope: me,
-            disabled: true,
-            handler: me.onRunSelected
-        });
+    me.runSelectedButton = Ext.create("Ext.button.Button", {
+      text: __("Run"),
+      glyph: NOC.glyph.play,
+      scope: me,
+      disabled: true,
+      handler: me.onRunSelected,
+    });
 
-        me.stopSelectedButton = Ext.create("Ext.button.Button", {
-            text: __("Disable"),
-            glyph: NOC.glyph.minus_circle,
-            scope: me,
-            disabled: true,
-            handler: me.onStopSelected
-        });
+    me.stopSelectedButton = Ext.create("Ext.button.Button", {
+      text: __("Disable"),
+      glyph: NOC.glyph.minus_circle,
+      scope: me,
+      disabled: true,
+      handler: me.onStopSelected,
+    });
 
-        me.store = Ext.create("Ext.data.Store", {
-            fields: [
-                {
-                    name: "name",
-                    type: "string"
-                },
-                {
-                    name: "enable_profile",
-                    type: "boolean"
-                },
-                {
-                    name: "status",
-                    type: "string"
-                },
-                {
-                    name: "last_run",
-                    type: "date"
-                },
-                {
-                    name: "last_status",
-                    type: "string"
-                },
-                {
-                    name: "next_run",
-                    type: "date"
-                },
-                {
-                    name: "jcls",
-                    type: "string"
-                }
-            ]
-        });
+    me.clipboardButton = Ext.create("Ext.button.Button", {
+      text: __("Clipboard"),
+      glyph: NOC.glyph.copy,
+      scope: me,
+      handler: function(){
+        NOC.toClipboard(me.logText);
+      },
+    });
 
-        me.grid = Ext.create("Ext.grid.Panel", {
-            width: 550,
-            store: me.store,
-            stateful: true,
-            stateId: "sa.managedobject-discovery",
-            region: "west",
-            split: true,
-            columns: [
-                {
-                    text: __("Job"),
-                    dataIndex: "name",
-                    width: 120
-                },
-                {
-                    text: __("Profile"),
-                    dataIndex: "enable_profile",
-                    renderer: NOC.render.Bool,
-                    width: 40
-                },
-                {
-                    text: __("Status"),
-                    dataIndex: "status",
-                    width: 40,
-                    renderer: NOC.render.Choices({
-                        W: "Wait",
-                        R: "Run",
-                        S: "Stop",
-                        F: "Fail",
-                        D: "Disabled",
-                        s: "Suspend",
-                        true: "OK",
-                        false: "Fail"
-                    })
-                },
-                {
-                    text: __("Last Run"),
-                    dataIndex: "last_run",
-                    width: 120,
-                    renderer: NOC.render.DateTime
-                },
-                {
-                    text: __("Last Status"),
-                    dataIndex: "last_status",
-                    width: 40,
-                    renderer: NOC.render.Choices({
-                        W: "Wait",
-                        R: "Run",
-                        S: "Stop",
-                        F: "Fail"
-                    })
-                },
-                {
-                    text: __("Next Run"),
-                    dataIndex: "next_run",
-                    width: 120,
-                    renderer: NOC.render.DateTime
-                }
-            ],
-            dockedItems: [
-                {
-                    xtype: "toolbar",
-                    dock: "top",
-                    items: [
-                        me.closeButton,
-                        me.refreshButton,
-                        "-",
-                        me.runSelectedButton,
-                        me.stopSelectedButton
-                    ]
-                }
-            ],
-            viewConfig: {
-                getRowClass: Ext.bind(me.getRowClass, me)
-            },
-            selModel: Ext.create("Ext.selection.RowModel", {
-                mode: "MULTI"
-            }),
-            listeners: {
-                scope: me,
-                selectionchange: me.onGridSelection
-            }
-        });
+    me.store = Ext.create("Ext.data.Store", {
+      fields: [
+        {
+          name: "name",
+          type: "string",
+        },
+        {
+          name: "enable_profile",
+          type: "boolean",
+        },
+        {
+          name: "status",
+          type: "string",
+        },
+        {
+          name: "last_run",
+          type: "date",
+        },
+        {
+          name: "last_status",
+          type: "string",
+        },
+        {
+          name: "next_run",
+          type: "date",
+        },
+        {
+          name: "jcls",
+          type: "string",
+        },
+      ],
+    });
 
-        me.logPanel = Ext.create("Ext.panel.Panel", {
-            layout: "fit",
-            region: "center",
-            autoScroll: true,
-            flex: 1,
-            items: [{
-                xtype: "container",
-                autoScroll: true,
-                padding: 4
-            }]
-        });
+    me.grid = Ext.create("Ext.grid.Panel", {
+      width: 550,
+      store: me.store,
+      stateful: true,
+      stateId: "sa.managedobject-discovery",
+      region: "west",
+      split: true,
+      columns: [
+        {
+          text: __("Job"),
+          dataIndex: "name",
+          width: 120,
+        },
+        {
+          text: __("Profile"),
+          dataIndex: "enable_profile",
+          renderer: NOC.render.Bool,
+          width: 40,
+        },
+        {
+          text: __("Status"),
+          dataIndex: "status",
+          width: 40,
+          renderer: NOC.render.Choices({
+            W: "Wait",
+            R: "Run",
+            S: "Stop",
+            F: "Fail",
+            D: "Disabled",
+            s: "Suspend",
+            true: "OK",
+            false: "Fail",
+          }),
+        },
+        {
+          text: __("Last Run"),
+          dataIndex: "last_run",
+          width: 120,
+          renderer: NOC.render.DateTime,
+        },
+        {
+          text: __("Last Status"),
+          dataIndex: "last_status",
+          width: 40,
+          renderer: NOC.render.Choices({
+            W: "Wait",
+            R: "Run",
+            S: "Stop",
+            F: "Fail",
+          }),
+        },
+        {
+          text: __("Next Run"),
+          dataIndex: "next_run",
+          width: 120,
+          renderer: NOC.render.DateTime,
+        },
+      ],
+      dockedItems: [
+        {
+          xtype: "toolbar",
+          dock: "top",
+          items: [
+            me.closeButton,
+            me.refreshButton,
+            "-",
+            me.runSelectedButton,
+            me.stopSelectedButton,
+            "-",
+            me.clipboardButton,
+          ],
+        },
+      ],
+      viewConfig: {
+        getRowClass: Ext.bind(me.getRowClass, me),
+      },
+      selModel: Ext.create("Ext.selection.RowModel", {
+        mode: "MULTI",
+      }),
+      listeners: {
+        scope: me,
+        selectionchange: me.onGridSelection,
+      },
+    });
 
-        Ext.apply(me, {
-            items: [
-                {
-                    xtype: "container",
-                    layout: "border",
-                    items: [
-                        me.grid,
-                        me.logPanel
-                    ]
-                }
-            ]
-        });
-        me.callParent();
-    },
-    //
-    preview: function(record) {
-        var me = this;
+    me.logPanel = Ext.create("Ext.panel.Panel", {
+      layout: "fit",
+      region: "center",
+      autoScroll: true,
+      flex: 1,
+      items: [{
+        xtype: "container",
+        autoScroll: true,
+        padding: 4,
+      }],
+    });
 
-        me.logPanel.items.first().update('');
-        me.currentRecord = record;
-        me.setTitle(record.get("name") + " discovery");
-        if(me.historyHashPrefix) {
-            me.app.setHistoryHash(
-                me.currentRecord.get("id"),
-                me.historyHashPrefix
-            );
-        }
-        Ext.Ajax.request({
-            url: "/sa/managedobject/" + record.get("id") + "/discovery/",
-            method: "GET",
-            scope: me,
-            success: function(response) {
-                var data = Ext.decode(response.responseText);
-                me.store.loadData(data);
-            },
-            failure: function() {
-                NOC.error(__("Failed to load data"));
-            }
-        });
-    },
-    //
-    onClose: function() {
-        var me = this;
-        me.app.showForm();
-    },
-    //
-    getRowClass: function(record) {
-        var me = this;
-        if(!record.get("enable_profile")) {
-            return "noc-inactive";
-        } else if(record.get("last_status") === "F") {
-            return "noc-fail";
-        }
-    },
-    //
-    onRefresh: function() {
-        var me = this;
-        me.preview(me.currentRecord);
-    },
-    //
-    onRunSelected: function() {
-        var me = this,
-            records = me.grid.getSelectionModel().getSelection(),
-            names = records.map(function(r) {
-                return r.get("name");
-            });
-        Ext.Ajax.request({
-            url: "/sa/managedobject/" + me.currentRecord.get("id") + "/discovery/run/",
-            method: "POST",
-            scope: me,
-            jsonData: {
-                "names": names
-            },
-            success: function() {
-                NOC.msg.started(__("Job scheduled to start"));
-                me.onRefresh();
-            },
-            failure: function() {
-                NOC.error(__("Failed to run tasks"));
-            }
-        });
-    },
-    //
-    onStopSelected: function() {
-        var me = this,
-            records = me.grid.getSelectionModel().getSelection(),
-            names = records.map(function(r) {
-                return r.get("name");
-            });
-        Ext.Ajax.request({
-            url: "/sa/managedobject/" + me.currentRecord.get("id") + "/discovery/stop/",
-            method: "POST",
-            scope: me,
-            jsonData: {
-                "names": names
-            },
-            success: function() {
-                me.onRefresh();
-            },
-            failure: function() {
-                NOC.error(__("Failed to disable tasks"));
-            }
-        });
-    },
-    //
-    onGridSelection: function(selection, records) {
-        var me = this,
-            canRun, canStop;
-        canRun = records.filter(function(v) {
-            var status = v.get("status");
-            return v.get("enable_profile") === true && (status === "W" || status === "S" || status === "" || status === "D");
-        }).length > 0;
-        canStop = records.filter(function(v) {
-            var status = v.get("status");
-            return v.get("enable_profile") === true && (status === "W" || status === "");
-        }).length > 0;
-        me.runSelectedButton.setDisabled(!canRun);
-        me.stopSelectedButton.setDisabled(!canStop);
-        if(records.length === 1) {
-            me.showLog(records[0].get("jcls"));
-        }
-    },
-    //
-    setLog: function(text) {
-        var me = this;
-        me.logPanel.items.first().update("<pre>" + Ext.util.Format.htmlEncode(text) + "<pre>");
-    },
-    //
-    showLog: function(jcls) {
-        var me = this,
-            url = "/sa/managedobject/" + me.currentRecord.get("id") + "/job_log/" + jcls + "/";
-        Ext.Ajax.request({
-            url: url,
-            method: "GET",
-            scope: me,
-            success: function(response) {
-                me.setLog(response.responseText);
-            },
-            failure: function() {
-                me.setLog("Failed to get job log");
-            }
-        });
+    Ext.apply(me, {
+      items: [
+        {
+          xtype: "container",
+          layout: "border",
+          items: [
+            me.grid,
+            me.logPanel,
+          ],
+        },
+      ],
+    });
+    me.callParent();
+  },
+  //
+  preview: function(record){
+    var me = this;
+
+    me.logPanel.items.first().update('');
+    me.currentRecord = record;
+    me.setTitle(record.get("name") + " discovery");
+    if(me.historyHashPrefix){
+      me.app.setHistoryHash(
+        me.currentRecord.get("id"),
+        me.historyHashPrefix,
+      );
     }
+    Ext.Ajax.request({
+      url: "/sa/managedobject/" + record.get("id") + "/discovery/",
+      method: "GET",
+      scope: me,
+      success: function(response){
+        var data = Ext.decode(response.responseText);
+        me.store.loadData(data);
+      },
+      failure: function(){
+        NOC.error(__("Failed to load data"));
+      },
+    });
+  },
+  //
+  onClose: function(){
+    var me = this;
+    me.app.showForm();
+  },
+  //
+  getRowClass: function(record){
+    var me = this;
+    if(!record.get("enable_profile")){
+      return "noc-inactive";
+    } else if(record.get("last_status") === "F"){
+      return "noc-fail";
+    }
+  },
+  //
+  onRefresh: function(){
+    var me = this;
+    me.preview(me.currentRecord);
+  },
+  //
+  onRunSelected: function(){
+    var me = this,
+      records = me.grid.getSelectionModel().getSelection(),
+      names = records.map(function(r){
+        return r.get("name");
+      });
+    Ext.Ajax.request({
+      url: "/sa/managedobject/" + me.currentRecord.get("id") + "/discovery/run/",
+      method: "POST",
+      scope: me,
+      jsonData: {
+        "names": names,
+      },
+      success: function(){
+        NOC.msg.started(__("Job scheduled to start"));
+        me.onRefresh();
+      },
+      failure: function(){
+        NOC.error(__("Failed to run tasks"));
+      },
+    });
+  },
+  //
+  onStopSelected: function(){
+    var me = this,
+      records = me.grid.getSelectionModel().getSelection(),
+      names = records.map(function(r){
+        return r.get("name");
+      });
+    Ext.Ajax.request({
+      url: "/sa/managedobject/" + me.currentRecord.get("id") + "/discovery/stop/",
+      method: "POST",
+      scope: me,
+      jsonData: {
+        "names": names,
+      },
+      success: function(){
+        me.onRefresh();
+      },
+      failure: function(){
+        NOC.error(__("Failed to disable tasks"));
+      },
+    });
+  },
+  //
+  onGridSelection: function(selection, records){
+    var me = this,
+      canRun, canStop;
+    canRun = records.filter(function(v){
+      var status = v.get("status");
+      return v.get("enable_profile") === true && (status === "W" || status === "S" || status === "" || status === "D");
+    }).length > 0;
+    canStop = records.filter(function(v){
+      var status = v.get("status");
+      return v.get("enable_profile") === true && (status === "W" || status === "");
+    }).length > 0;
+    me.runSelectedButton.setDisabled(!canRun);
+    me.stopSelectedButton.setDisabled(!canStop);
+    if(records.length === 1){
+      me.showLog(records[0].get("jcls"));
+    }
+  },
+  //
+  setLog: function(text){
+    var me = this;
+    me.logPanel.items.first().update("<pre>" + Ext.util.Format.htmlEncode(text) + "<pre>");
+  },
+  //
+  showLog: function(jcls){
+    var me = this,
+      url = "/sa/managedobject/" + me.currentRecord.get("id") + "/job_log/" + jcls + "/";
+    Ext.Ajax.request({
+      url: url,
+      method: "GET",
+      scope: me,
+      success: function(response){
+        me.setLog(response.responseText);
+        me.logText = response.responseText;
+      },
+      failure: function(){
+        var text = "Failed to get job log";
+        me.setLog(text);
+        me.logText = text;
+      },
+    });
+  },
 });
--- ui/web/sa/managedobject/InteractionsPanel.js
+++ ui/web/sa/managedobject/InteractionsPanel.js
@@ -7,159 +7,180 @@
 console.debug("Defining NOC.sa.managedobject.InteractionsPanel");
 
 Ext.define("NOC.sa.managedobject.InteractionsPanel", {
-    extend: "Ext.panel.Panel",
-    alias: "widget.sa.interactions",
-    app: null,
-    layout: "fit",
-    autoScroll: true,
+  extend: "Ext.panel.Panel",
+  alias: "widget.sa.interactions",
+  app: null,
+  layout: "fit",
+  autoScroll: true,
 
-    initComponent: function() {
-        var me = this;
+  initComponent: function(){
+    var me = this;
 
-        me.closeButton = Ext.create("Ext.button.Button", {
-            text: __("Close"),
-            glyph: NOC.glyph.arrow_left,
-            scope: me,
-            handler: me.onClose
-        });
+    me.closeButton = Ext.create("Ext.button.Button", {
+      text: __("Close"),
+      glyph: NOC.glyph.arrow_left,
+      scope: me,
+      handler: me.onClose,
+    });
 
-        me.refreshButton = Ext.create("Ext.button.Button", {
-            text: __("Refresh"),
-            glyph: NOC.glyph.refresh,
-            scope: me,
-            handler: me.onRefresh
-        });
+    me.refreshButton = Ext.create("Ext.button.Button", {
+      text: __("Refresh"),
+      glyph: NOC.glyph.refresh,
+      scope: me,
+      handler: me.onRefresh,
+    });
 
-        me.store = Ext.create("Ext.data.Store", {
-            fields: [
-                {
-                    name: "ts",
-                    type: "date"
-                },
-                {
-                    name: "op",
-                    type: "integer"
-                },
-                {
-                    name: "user",
-                    type: "string"
-                },
-                {
-                    name: "text",
-                    type: "string"
-                }
-            ]
-        });
+    me.clipboardButton = Ext.create("Ext.button.Button", {
+      text: __("Clipboard"),
+      glyph: NOC.glyph.copy,
+      scope: me,
+      handler: me.onClipboard,
+    });
 
-        me.grid = Ext.create("Ext.grid.Panel", {
-            store: me.store,
-            stateful: true,
-            stateId: "sa.managedobject-interactions",
-            columns: [
-                {
-                    text: __("Date"),
-                    dataIndex: "ts",
-                    width: 120,
-                    renderer: NOC.render.DateTime
-                },
-                {
-                    text: __("Operation"),
-                    dataIndex: "op",
-                    width: 100,
-                    renderer: NOC.render.Choices({
-                        0: "Command",
-                        1: "Login",
-                        2: "Logout",
-                        3: "Reboot",
-                        4: "Started",
-                        5: "Halted",
-                        6: "Config"
-                    })
-                },
-                {
-                    text: __("User"),
-                    dataIndex: "user",
-                    width: 100
-                },
-                {
-                    text: __("Text"),
-                    dataIndex: "text",
-                    flex: 1
-                }
-            ],
-            dockedItems: [
-                {
-                    xtype: "toolbar",
-                    dock: "top",
-                    items: [
-                        me.closeButton,
-                        me.refreshButton,
-                    ]
-                }
-            ],
-            viewConfig: {
-                getRowClass: Ext.bind(me.getRowClass, me)
-            }
-        });
+    me.store = Ext.create("Ext.data.Store", {
+      fields: [
+        {
+          name: "ts",
+          type: "date",
+        },
+        {
+          name: "op",
+          type: "integer",
+        },
+        {
+          name: "user",
+          type: "string",
+        },
+        {
+          name: "text",
+          type: "string",
+        },
+      ],
+    });
 
-        Ext.apply(me, {
-            items: [
-                me.grid
-            ]
-        });
-        me.callParent();
-    },
-    //
-    preview: function(record) {
-        var me = this;
-        me.currentRecord = record;
-        me.setTitle(record.get("name") + " commands");
-        if(me.historyHashPrefix) {
-            me.app.setHistoryHash(
-                me.currentRecord.get("id"),
-                me.historyHashPrefix
-            );
-        }
-        Ext.Ajax.request({
-            url: "/sa/managedobject/" + record.get("id") + "/interactions/",
-            method: "GET",
-            scope: me,
-            success: function(response) {
-                var data = Ext.decode(response.responseText);
-                me.store.loadData(data);
-            },
-            failure: function() {
-                NOC.error(__("Failed to load data"));
-            }
-        });
-    },
-    //
-    onClose: function() {
-        var me = this;
-        me.app.showForm();
-    },
-    //
-    getRowClass: function(record) {
-        var me = this;
-        switch(record.get("op")) {
-            case 0:
-                return "noc-int-command";
-            case 1:
-                return "noc-int-login";
-            case 2:
-                return "noc-int-logout";
-            case 3:
-            case 5:
-                return "noc-int-reboot";
-            case 4:
-                return "noc-int-started";
-            case 6:
-                return "noc-int-config";
-        }
-    },
-    //
-    onRefresh: function() {
-        var me = this;
-        me.preview(me.currentRecord);
+    me.grid = Ext.create("Ext.grid.Panel", {
+      store: me.store,
+      stateful: true,
+      stateId: "sa.managedobject-interactions",
+      columns: [
+        {
+          text: __("Date"),
+          dataIndex: "ts",
+          width: 120,
+          renderer: NOC.render.DateTime,
+        },
+        {
+          text: __("Operation"),
+          dataIndex: "op",
+          width: 100,
+          renderer: NOC.render.Choices({
+            0: "Command",
+            1: "Login",
+            2: "Logout",
+            3: "Reboot",
+            4: "Started",
+            5: "Halted",
+            6: "Config",
+          }),
+        },
+        {
+          text: __("User"),
+          dataIndex: "user",
+          width: 100,
+        },
+        {
+          text: __("Text"),
+          dataIndex: "text",
+          flex: 1,
+        },
+      ],
+      dockedItems: [
+        {
+          xtype: "toolbar",
+          dock: "top",
+          items: [
+            me.closeButton,
+            me.refreshButton,
+            me.clipboardButton,
+          ],
+        },
+      ],
+      viewConfig: {
+        getRowClass: Ext.bind(me.getRowClass, me),
+      },
+    });
+
+    Ext.apply(me, {
+      items: [
+        me.grid,
+      ],
+    });
+    me.callParent();
+  },
+  //
+  preview: function(record){
+    var me = this;
+    me.currentRecord = record;
+    me.setTitle(record.get("name") + " commands");
+    if(me.historyHashPrefix){
+      me.app.setHistoryHash(
+        me.currentRecord.get("id"),
+        me.historyHashPrefix,
+      );
     }
+    Ext.Ajax.request({
+      url: "/sa/managedobject/" + record.get("id") + "/interactions/",
+      method: "GET",
+      scope: me,
+      success: function(response){
+        var data = Ext.decode(response.responseText);
+        me.store.loadData(data);
+      },
+      failure: function(){
+        NOC.error(__("Failed to load data"));
+      },
+    });
+  },
+  //
+  onClose: function(){
+    var me = this;
+    me.app.showForm();
+  },
+  //
+  getRowClass: function(record){
+    var me = this;
+    switch(record.get("op")){
+      case 0:
+        return "noc-int-command";
+      case 1:
+        return "noc-int-login";
+      case 2:
+        return "noc-int-logout";
+      case 3:
+      case 5:
+        return "noc-int-reboot";
+      case 4:
+        return "noc-int-started";
+      case 6:
+        return "noc-int-config";
+    }
+  },
+  //
+  onRefresh: function(){
+    var me = this;
+    me.preview(me.currentRecord);
+  },
+  //
+  onClipboard: function(){
+    var me = this;
+    var text = "";
+    me.store.each(function(record){
+      text += record.get("ts") + "," + record.get("user") + "," + record.get("text") + "\n";
+    });
+    if(!Ext.isEmpty(text)){
+      NOC.toClipboard(text);
+    }{
+      NOC.error(__("Nothing to copy"));
+    }
+  },
 });
