bdf73cb06048a48dde4aa7eed277fcb949be3b27
Dmitry Volodin <dv@nocproject.org>
2024-10-01 15:33:27 +0200

    Optical and OSC provisioning

--- sa/profiles/IRE-Polus/Horizon/controller/base.py
+++ sa/profiles/IRE-Polus/Horizon/controller/base.py
@@ -8,12 +8,24 @@
 # Python modules
 from dataclasses import dataclass
 from typing import Iterable
+from uuid import UUID
 
 # NOC modules
 from noc.core.runner.models.jobreq import JobRequest, InputMapping, KVInputMapping
 from noc.inv.models.object import Object
 from noc.inv.models.endpoint import Endpoint
 from noc.core.runner.job import JobRequest
+
+# Cards
+ADM_200 = UUID("1187f420-fa75-4701-9e7a-816f5923203b")
+OADM_4_V_4 = UUID("6aaed88a-4d69-4d91-ab45-3be075a60188")
+OADM_16_V_16 = UUID("c48af1f4-570e-4b1b-936d-24e5bb966064")
+OM_40_V_C = UUID("084017ac-d5f5-465d-8e67-1361b74b4a2e")
+OM_40_V_H = UUID("4a0ec15c-41c7-4a92-84ab-1654b1fb4c04")
+OD_40_C = UUID("3eb083ad-031f-4d86-bb3a-f52d57168a8c")
+OD_40_H = UUID("cd60fe64-67da-4570-bac8-8a3dd4f5b23d")
+OM_96_H = UUID("4ef97220-12fc-4df7-9be3-98a79a080ed5")
+OD_96_H = UUID("1777a6a3-7546-48c8-bec2-c9693d207493")
 
 
 @dataclass
@@ -139,7 +151,7 @@ class ChannelMixin(HorizonMixin):
             environment={"managed_object": f"{mo.id}" if mo else "0", "card": str(card)},
         )
 
-    def setup(self, ep: Endpoint) -> JobRequest | None:
+    def setup(self, ep: Endpoint, **kwargs) -> JobRequest | None:
         self.logger.info("Endpoint setup: %s", ep)
         # Get object
         obj = self.get_object(ep.resource)
@@ -149,7 +161,7 @@ class ChannelMixin(HorizonMixin):
         # Get port
         _, _, port = ep.resource.split(":", 2)
         # Get jobs
-        jobs = list(self.iter_set_requests(self.iter_setup(obj, port)))
+        jobs = list(self.iter_set_requests(self.iter_setup(obj, port, **kwargs)))
         if not jobs:
             self.logger.info("Nothing to setup, skipping")
             return None
@@ -183,24 +195,43 @@ class ChannelMixin(HorizonMixin):
             jobs=jobs,
         )
 
-    def iter_setup(self, obj: Object, port: str) -> Iterable[SetValue]:
+    def iter_setup(self, obj: Object, port: str, **kwargs) -> Iterable[SetValue]:
         """
         Endpoint setup sequence.
         """
-        h = self.HANDLERS.get(obj.model.uuid)
+        h = self._SETUP_HANDLERS.get(obj.model.uuid)
         if h is None:
             self.logger.info("Model %s is not supported", obj.model.get_short_label())
             return
-        yield from h[0](self, port)
+        yield from h(self, port, **kwargs)
 
     def iter_cleanup(self, obj: Object, port: str) -> Iterable[SetValue]:
         """
         Endpoint setup sequence.
         """
-        h = self.HANDLERS.get(obj.model.uuid)
+        h = self._CLEANUP_HANDLERS.get(obj.model.uuid)
         if h is None:
             self.logger.info("Model %s is not supported", obj.model.get_short_label())
             return
-        yield from h[1](self, port)
+        yield from h(self, port)
 
-    HANDLERS = {}
+    @classmethod
+    def setup_for(cls, *args: UUID):
+        def inner(f):
+            for uuid in args:
+                cls._SETUP_HANDLERS[uuid] = f
+            return f
+
+        return inner
+
+    @classmethod
+    def cleanup_for(cls, *args: UUID):
+        def inner(f):
+            for uuid in args:
+                cls._CLEANUP_HANDLERS[uuid] = f
+            return f
+
+        return inner
+
+    _SETUP_HANDLERS = {}
+    _CLEANUP_HANDLERS = {}
--- sa/profiles/IRE-Polus/Horizon/controller/otn_osc.py
+++ sa/profiles/IRE-Polus/Horizon/controller/otn_osc.py
@@ -5,10 +5,39 @@
 # See LICENSE for details
 # ----------------------------------------------------------------------
 
+# Python modules
+from typing import Iterable
+
 # NOC modules
 from noc.core.techdomain.profile.otn_osc import BaseOSCProfileController
-from .base import ChannelMixin
+from .base import ChannelMixin, SetValue, OADM_4_V_4, OADM_16_V_16, OM_40_V_C, OM_40_V_H, OM_96_H
+
+OSC_SET_STATE_MODELS = (OADM_4_V_4, OADM_16_V_16, OM_40_V_C, OM_40_V_H, OM_96_H)
 
 
-class Controller(BaseOSCProfileController, ChannelMixin):
+class Controller(ChannelMixin, BaseOSCProfileController):
     label = "OCS"
+
+    @ChannelMixin.setup_for(*OSC_SET_STATE_MODELS)
+    def iter_osc_setup(self, name: str) -> Iterable[SetValue]:
+        """
+        OSC initialization.
+
+        Args:
+            name: Port name.
+        """
+        # Bring port up
+        yield SetValue(name="OSCSetState", value="2", description="Bring OSC up. Set state to IS.")
+
+    @ChannelMixin.cleanup_for(*OSC_SET_STATE_MODELS)
+    def iter_osc_cleanup(self, name: str) -> Iterable[SetValue]:
+        """
+        OSC cleanup.
+
+        Args:
+            name: Port name.
+        """
+        # Bring port up
+        yield SetValue(
+            name="OSCSetState", value="1", description="Bring OSC down. Set state to MT."
+        )
