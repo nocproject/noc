e47e3719843709e7cf88e411a489252ce6548314
Andrey Vertiprahov <aversanta@gmail.com>
2024-10-26 21:24:38 +0500

    Fix formatter for ObjectSummary Report.

--- tests/reporter/test_report.py
+++ tests/reporter/test_report.py
@@ -1,7 +1,7 @@
 # ----------------------------------------------------------------------
 # Reporter testing
 # ----------------------------------------------------------------------
-# Copyright (C) 2007-2022 The NOC Project
+# Copyright (C) 2007-2024 The NOC Project
 # See LICENSE for details
 # ----------------------------------------------------------------------
 
@@ -16,14 +16,21 @@ from noc.core.reporter.types import ReportConfig, RunParams, OutputType
 from noc.core.mongo.connection import connect
 
 
-@pytest.mark.parametrize("report", ["report_simple_csv", "report_id_cache_poison"])
-def test_report(report):
+@pytest.mark.parametrize(
+    "report,params",
+    [
+        ("report_simple_csv", {}),
+        ("report_id_cache_poison", {}),
+        ("report_object_summary_profile", {"report_type": "profile"}),
+    ],
+)
+def test_report(report, params):
     path = os.path.realpath(os.path.dirname(__file__))
     with open(os.path.join(path, f"{report}.yml"), "rb") as f:
         cfg = yaml.safe_load(f)
     # r = yaml.safe_load(report_config)
     report_engine = ReportEngine(report_print_error=True)
-    rp = RunParams(report=ReportConfig(**cfg), output_type=OutputType.CSV)
+    rp = RunParams(report=ReportConfig(**cfg), output_type=OutputType.CSV, params=params)
     connect()
     out_doc = report_engine.run_report(r_params=rp)
     re_out = out_doc.content.decode("utf8")
