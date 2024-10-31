1a3912452de2cb94eaef347f68600a19e30e4e8c
uncol <d.a.zakharov@gmail.com>
2024-10-31 12:41:52 +0300

    MapLayersCreator refactored

--- ui/web/core/MapLayersCreator.js
+++ ui/web/core/MapLayersCreator.js
@@ -8,11 +8,11 @@ console.debug("Defining NOC.core.MapLayersCreator");
 
 Ext.define("NOC.core.MapLayersCreator", {
   defaultOptions: {
-  //options - layersControl 
+  //options - layersControl
   //          translator
   //          default_layer = [
-  //              osm, 
-  //              google_roadmap, google_hybrid, google_sat, google_terrain, 
+  //              osm,
+  //              google_roadmap, google_hybrid, google_sat, google_terrain,
   //              tile1, tile2, tile3,
   //              yandex_roadmap, yandex_hybrid, yandex_sat
   //          ]
@@ -25,10 +25,12 @@ Ext.define("NOC.core.MapLayersCreator", {
     translator: function(s){
       return s;
     },
-    layersControl: {},
     default_layer: "",
+    layersControl: {},
+    allowed_layers: {
+      enable_osm: true,
+    },
     yandex_supported: false,
-    allowed_layers: {enable_osm: true},
   },
   //
   validLayerValues: [
@@ -76,117 +78,118 @@ Ext.define("NOC.core.MapLayersCreator", {
     return true;
   },
   //
-  run: function(scope){
-    var translator = this.getTranslator(),
-      optionsLayersControl = this.getLayersControl(),
-      optionsDefaultLayer = this.getDefaultLayer(),
-      yandex_supported = this.isYandexSupported(),
-      optionsAllowedLayers = this.getAllowedLayers(),
-      default_layer_name = "osm",
-      tile_cfg = function(options){
-        if(options && Array.isArray(options.subdomains) && options.subdomains.length > 0){
-          return {subdomains: options.subdomains};
-        } else{
-          return {};
-        }        
-      };
-
-    if(!this.validate_def_layer(optionsDefaultLayer)){
-      console.warn("Default layer name is not valid", optionsDefaultLayer, "Using osm");
-    } else{
-      default_layer_name = optionsDefaultLayer;
-    }
-
-    if(!this.validate_allowed_layers(optionsAllowedLayers)){
-      console.warn("Allowed layers is not valid", optionsAllowedLayers);
-    }
-
-    var baseLayers = {
-      osm: optionsAllowedLayers.enable_osm
-        ? [
-          L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {}),
-          translator("OpenStreetMap"),
-        ]
-        : undefined,
-      google_roadmap: optionsAllowedLayers.enable_google_roadmap
-        ? [
-          L.tileLayer("https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}", {
-            subdomains: ["mt0", "mt1", "mt2", "mt3"],
-          }),
-          translator("Google Roadmap"),
-        ]
-        : undefined,
-      google_hybrid: optionsAllowedLayers.enable_google_hybrid
-        ? [
-          L.tileLayer("https://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}", {
-            subdomains: ["mt0", "mt1", "mt2", "mt3"],
-          }),
-          translator("Google Hybrid"),
-        ]
-        : undefined,
-      google_sat: optionsAllowedLayers.enable_google_sat
-        ? [
-          L.tileLayer("https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}", {
-            subdomains: ["mt0", "mt1", "mt2", "mt3"],
-          }),
-          translator("Google Satellite"),
-        ]
-        : undefined,
-      google_terrain: optionsAllowedLayers.enable_google_terrain
-        ? [
-          L.tileLayer("https://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}", {
-            subdomains: ["mt0", "mt1", "mt2", "mt3"],
-          }),
-          translator("Google Terrain"),
-        ]
-        : undefined,
-      tile1: (optionsAllowedLayers.enable_tile1 ? [
-        L.tileLayer(NOC.settings.gis.custom.tile1.url, tile_cfg(NOC.settings.gis.custom.tile1)), NOC.settings.gis.custom.tile1.name,
-      ] : undefined),
-      tile2: (optionsAllowedLayers.enable_tile2 ? [
-        L.tileLayer(NOC.settings.gis.custom.tile2.url, tile_cfg(NOC.settings.gis.custom.tile2)), NOC.settings.gis.custom.tile2.name,
-      ] : undefined),
-      tile3: (optionsAllowedLayers.enable_tile3 ? [
-        L.tileLayer(NOC.settings.gis.custom.tile3.url, tile_cfg(NOC.settings.gis.custom.tile3)), NOC.settings.gis.custom.tile3.name,
-      ] : undefined),
-      yandex_roadmap:
-        optionsAllowedLayers.enable_yandex_roadmap && yandex_supported
-          ? [L.yandex("yandex#map"), translator("Yandex Roadmap")]
-          : undefined,
-      yandex_hybrid:
-        optionsAllowedLayers.enable_yandex_hybrid && yandex_supported
-          ? [L.yandex("yandex#hybrid"), translator("Yandex Hybrid")]
-          : undefined,
-      yandex_sat:
-        optionsAllowedLayers.enable_yandex_sat && yandex_supported
-          ? [L.yandex("yandex#satellite"), translator("Yandex Satellite")]
-          : undefined,
+  createTileLayer: function(url, options = {}){
+    return L.tileLayer(url, options);
+  },
+  //
+  createGoogleLayer: function(type){
+    const types = {
+      roadmap: "m",
+      hybrid: "s,h",
+      satellite: "s",
+      terrain: "p",
+    };
+    return this.createTileLayer(
+      `https://{s}.google.com/vt/lyrs=${types[type]}&x={x}&y={y}&z={z}`,
+      {subdomains: ["mt0", "mt1", "mt2", "mt3"]},
+    );
+  },
+  //
+  createCustomTileLayer: function(config){
+    if(config && !config.url) return undefined;
+    const options = config.subdomains && Array.isArray(config.subdomains) && config.subdomains.length ? {subdomains: config.subdomains} : {};
+    return [
+      this.createTileLayer(config.url, options),
+      config.name,
+    ];
+  },
+  //
+  createYandexLayer: function(type){
+    const types = {
+      roadmap: "map",
+      hybrid: "hybrid",
+      satellite: "satellite",
     };
 
-    var default_layer = baseLayers[default_layer_name];
-    if(default_layer){
-      default_layer = default_layer[0];
-    } else{
-      console.warn("Layer not found", default_layer_name, "Using osm");
-      default_layer = baseLayers.osm[0];
-    }
+    return [
+      L.yandex(`yandex#${types[type]}`),
+      this.getTranslator()(`Yandex ${type.charAt(0).toUpperCase() + type.slice(1)}`),
+    ];
+  },
+  //
+  createBaseLayers: function(translator, isYandexSupported, options){
+    return {
+      osm: options.enable_osm && [
+        this.createTileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"),
+        translator("OpenStreetMap") ,
+      ] || undefined,
 
-    scope.map.addLayer(default_layer);
+      google_roadmap: options.enable_google_roadmap
+            && [ this.createGoogleLayer("roadmap"), translator("Google Roadmap") ] || undefined,
 
-    // Generate object
-    // {
-    //     "<someName1>": layer1,
-    //     "<someName2>": layer2
-    // }
-    var baseLayersToAdd = {};
-    Object.keys(baseLayers).forEach(function(key){
-      if(baseLayers[key]){
-        baseLayersToAdd[baseLayers[key][1]] = baseLayers[key][0];
+      google_hybrid: options.enable_google_hybrid
+            && [ this.createGoogleLayer("hybrid"), translator("Google Hybrid") ] || undefined,
+
+      google_sat: options.enable_google_sat
+            && [ this.createGoogleLayer("satellite"), translator("Google Satellite") ] || undefined,
+
+      google_terrain: options.enable_google_terrain
+            && [ this.createGoogleLayer("terrain"), translator("Google Terrain") ] || undefined,
+
+      tile1: options.enable_tile1 &&
+              this.createCustomTileLayer(NOC.settings.gis.custom.tile1) || undefined,
+
+      tile2: options.enable_tile2 &&
+              this.createCustomTileLayer(NOC.settings.gis.custom.tile2) || undefined,
+
+      tile3: options.enable_tile3 &&
+              this.createCustomTileLayer(NOC.settings.gis.custom.tile3) || undefined,
+
+      yandex_roadmap: options.enable_yandex_roadmap
+            && isYandexSupported && this.createYandexLayer("roadmap") || undefined,
+      yandex_hybrid: options.enable_yandex_hybrid
+            && isYandexSupported && this.createYandexLayer("hybrid") || undefined,
+      yandex_sat: options.enable_yandex_sat
+            && isYandexSupported && this.createYandexLayer("satellite") || undefined,
+    };
+  },
+  //
+  createLayersControl: function(baseLayers){
+    const layersMap = {};
+    Object.values(baseLayers).forEach(layer => {
+      if(layer){
+        layersMap[layer[1]] = layer[0];
       }
     });
+    return layersMap;
+  },
+  //
+  getDefaultLayerInstance: function(baseLayers, defaultLayerName){
+    const layer = baseLayers[defaultLayerName];
+    if(layer){
+      return layer[0];
+    }
+    console.warn("Layer not found", defaultLayerName, "Using osm");
+    return baseLayers.osm[0];
+  },
+  //
+  run: function(scope){
+    var translator = this.getTranslator(),
+      layersControl = this.getLayersControl(),
+      defaultLayer = this.getDefaultLayer(),
+      yandex_supported = this.isYandexSupported(),
+      allowedLayers = this.getAllowedLayers();
+    
+    if(!this.validate_allowed_layers(allowedLayers)){
+      console.warn("Allowed layers is not valid", allowedLayers);
+    }
 
-    scope.layersControl = L.control.layers(baseLayersToAdd, {}, optionsLayersControl);
-
+    var baseLayers = this.createBaseLayers(translator, yandex_supported, allowedLayers),
+      defaultLayerInstance = this.getDefaultLayerInstance(baseLayers, defaultLayer),
+      baseLayersToAdd = this.createLayersControl(baseLayers);
+        
+    scope.map.addLayer(defaultLayerInstance);
+    scope.layersControl = L.control.layers(baseLayersToAdd, {}, layersControl);
     scope.layersControl.addTo(scope.map);
   },
 });
