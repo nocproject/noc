1769c95e7c63ca8a80c771d60ac1e9cd990da0cf
uncol <d.a.zakharov@gmail.com>
2024-11-08 07:30:10 +0300

    path fixed

--- ui/web/gis/tms/Application.js
+++ ui/web/gis/tms/Application.js
@@ -4,9 +4,9 @@
 // Copyright (C) 2007-2012 The NOC Project
 // See LICENSE for details
 //---------------------------------------------------------------------
-console.debug("Defining NOC.gis.tile.Application");
+console.debug("Defining NOC.gis.tms.Application");
 
-Ext.define("NOC.gis.tile.Application", {
-    extend: "NOC.core.Application",
-    items: []
+Ext.define("NOC.gis.tms.Application", {
+  extend: "NOC.core.Application",
+  items: [],
 });
