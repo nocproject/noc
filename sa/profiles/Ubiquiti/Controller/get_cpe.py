790ca81b502d02cfbeae750ad9890120547e6082
Andrey Vertiprahov <aversanta@gmail.com>
2024-12-01 13:23:10 +0500

    Add Site param.

--- sa/profiles/Ubiquiti/Controller/get_cpe.py
+++ sa/profiles/Ubiquiti/Controller/get_cpe.py
@@ -5,6 +5,9 @@
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
+# Third-party modules
+import orjson
+
 # NOC modules
 from noc.core.script.base import BaseScript
 from noc.sa.interfaces.igetcpe import IGetCPE
@@ -24,27 +27,42 @@ class Script(BaseScript):
         3: "provisioning",  # downloading
     }
 
+    def get_sites(self):
+        v = self.http.post(
+            "/v2/api/sites/overview/",
+            orjson.dumps({}),
+            headers={"Content-Type": b"application/json"},
+            json=True,
+            cached=True,
+        )
+        if not v["data"]:
+            return ["default"]
+        return [x["name"] for x in v["data"]]
+
     def execute(self, **kwargs):
         r = []
-        v = self.http.get("/v2/api/site/default/device?separateUnmanaged=true", json=True)
-        for d in v["network_devices"]:
-            if d["device_type"] != "MANAGED" or not d["is_access_point"]:
-                continue
-            r.append(
-                {
-                    "vendor": "Ubiquiti",
-                    "model": d["model"],
-                    "version": d["version"],
-                    "mac": d["mac"],
-                    "status": "active" if d["state"] else "inactive",
-                    "id": d["_id"],  # Use int command show ap inventory NAME
-                    "global_id": d["mac"],
-                    "type": "ap",
-                    "name": d["name"],
-                    "ip": d["ip"],
-                    # "serial": ap_serial,
-                    "description": "",
-                    # "location": ap_location,
-                }
-            )
+        sites = self.get_sites()
+        for s in sites:
+            v = self.http.get(f"/v2/api/site/{s}/device?separateUnmanaged=true", json=True)
+            for d in v["network_devices"]:
+                if d["device_type"] != "MANAGED" or not d["is_access_point"]:
+                    continue
+                r.append(
+                    {
+                        "vendor": "Ubiquiti",
+                        "model": d["model"],
+                        "version": d["version"],
+                        "mac": d["mac"],
+                        "status": "active" if d["state"] else "inactive",
+                        "id": d["_id"],  # Use int command show ap inventory NAME
+                        "global_id": d["mac"],
+                        "type": "ap",
+                        "name": d["name"],
+                        "ip": d["ip"],
+                        # "serial": ap_serial,
+                        "description": "",
+                        "site": s,
+                        # "location": ap_location,
+                    }
+                )
         return r
