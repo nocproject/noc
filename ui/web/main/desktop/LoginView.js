c072b47e52eccfc09aaa612ada031854422b76cd
uncol <d.a.zakharov@gmail.com>
2024-12-26 10:45:57 +0000

    NOC move login

---
+++ ui/web/main/desktop/LoginView.js
@@ -0,0 +1,160 @@
+//---------------------------------------------------------------------
+// main.desktop.Login 
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2024 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.main.desktop.LoginView");
+
+Ext.define("NOC.main.desktop.LoginView", {
+  extend: "Ext.window.Window",
+  xtype: "login",
+  autoShow: true,
+  closable: false,
+  resizable: false,
+  title: __("NOC Login"),
+  layout: "fit",
+  modal: true,
+  defaultListenerScope: true,
+  defaultFocus: "user",
+  defaultButton: "okButton",
+  referenceHolder: true,
+  width: 500,
+  height: 310,
+  viewModel: "default",
+  requires: [
+    "NOC.main.desktop.ChangePassword",
+  ],
+  items: {
+    xtype: "form",
+    reference: "loginForm",
+    fixed: true,
+    border: false,
+    bodyStyle: {
+      background: "#e0e0e0",
+      padding: "20px",
+    },
+    autoEl: {
+      tag: "form",
+    },
+    defaults: {
+      anchor: "100%",
+      allowBlank: false,
+      enableKeyEvents: true,
+      xtype: "textfield",
+    },
+    layout: {
+      type: "vbox",
+      align: "stretch",
+      pack: "center",
+    },
+    items: [
+      {
+        xtype: "displayfield",
+        value: __("Type username and password:"),
+        hideLabel: true,
+      },
+      {
+        itemId: "user",
+        name: "user",
+        bind: "{user}",
+        fieldLabel: __("User"),
+        blankText: __("User name cannot be empty"),
+      },
+      {
+        itemId: "password",
+        name: "password",
+        bind: "{password}",
+        fieldLabel: __("Password"),
+        blankText: __("Password cannot be empty"),
+        inputType: "password",
+      },
+    ],
+    buttons: [
+      {
+        reference: "okButton",
+        formBind: true,
+        handler: "onLoginClick",
+        text: __("Login"),
+        glyph: "xf090@FontAwesome",
+        listeners: {
+          beforerender: function(){
+            Ext.apply(this.autoEl, {type: "submit"});
+          },
+        },
+      },
+      {
+        itemId: "resetBtn",
+        text: __("Reset"),
+        glyph: "xf00d@FontAwesome",
+        handler: function(){
+          this.up("form").reset();
+        },
+      },
+    ],
+  },
+  initComponent: function(){
+    var param = Ext.urlDecode(location.search);
+    if("msg" in param){ // show message when timeout
+      this.items.items[0].value = param.msg + "<br/>" + this.items.items[0].value;
+    }
+    this.callParent();
+  },
+  onLoginClick: function(){
+    var data = this.getViewModel().getData(),
+      params = Ext.encode({
+        user: data.user,
+        password: data.password,
+      });
+    if(params !== undefined){
+      this.lookup("loginForm").setDisabled(true);
+      Ext.Ajax.request({
+        url: "/api/login/login",
+        params: params,
+        method: "POST",
+        scope: this,
+        success: Ext.Function.pass(this.onLoginSuccess, this.onLoginFailure),
+        failure: this.onLoginFailure,
+        callback: "windowClose", 
+        defaultPostHeader: "application/json",
+      });
+    }
+  },
+  windowClose: function(){
+    var form = this.lookupReference("loginForm");
+    if(!Ext.isEmpty(form)){
+      form.setDisabled(false);
+    }
+  },
+  onLoginFailure: function(){
+    NOC.error(__("Failed to log in"));
+  },
+  onLoginSuccess: function(failureFunc, response){
+    var result = Ext.decode(response.responseText);
+    if(result.status === true){
+      if("must_change" in result && result.must_change === true){
+        this.close();
+        Ext.create("NOC.main.desktop.ChangePassword", {
+          listeners: {
+            scope: this,
+            close: this.applicationOpen, 
+          },
+        });
+      } else{
+        this.applicationOpen();
+      }
+    } else{
+      failureFunc();
+    }
+  },
+  applicationOpen: function(){
+    var param = Ext.urlDecode(location.search);
+    if("uri" in param){
+      if(location.hash){ // web app
+        location = "/" + location.hash;
+      } else{ // cards
+        location = param.uri;
+      }
+    }
+  },
+});
--- ui/web/main/desktop/app.js
+++ ui/web/main/desktop/app.js
@@ -1,29 +1,183 @@
 //---------------------------------------------------------------------
 // Application UI
 //---------------------------------------------------------------------
-// Copyright (C) 2007-2016 The NOC Project
+// Copyright (C) 2007-2024d The NOC Project
 // See LICENSE for details
 //---------------------------------------------------------------------
 console.debug("Defining NOC application");
 
 Ext.application({
-    name: "NOC",
-    paths: {
-        "NOC": "/ui/web",
-        "Ext.ux": "/ui/web/ux"
-    },
+  name: "NOC",
+  paths: {
+    "NOC": "/ui/web",
+    "Ext.ux": "/ui/web/ux",
+  },
 
-    requires: [
-        "NOC.main.desktop.Application"
-    ],
+  requires: [
+    "NOC.main.desktop.Application",
+    "NOC.main.desktop.LoginView",
+  ],
 
-    launch: function () {
-        var me = this;
-        Ext.setGlyphFontFamily("FontAwesome");
-        console.log("Initializing history API");
-        Ext.History.init();
-        console.log("NOC application starting");
-        // Create viewport
-        me.app = Ext.create("NOC.main.desktop.Application");
-    }
+  launch: function(){
+    Ext.Ajax.request({
+      url: "/api/login/is_logged/",
+      method: "GET",
+      scope: this,
+      success: function(response){
+        if(Ext.decode(response.responseText)){
+          this.openApplication();
+          return;
+        }
+
+        var hash = location.hash,
+          newUrl = location.protocol + "//" + 
+                      location.host + 
+            "?uri=/" + hash;
+
+        history.replaceState({}, document.title, newUrl);
+        this.openLogin();
+      },
+      failure: function(response){
+        console.error("Request failed:", response.status);
+      },
+    });
+  },
+  openApplication: function(){
+    Ext.setGlyphFontFamily("FontAwesome");
+    console.log("Initializing history API");
+    Ext.History.init();
+    console.log("NOC application starting");
+    this.settings();
+  },
+  openLogin: function(){
+    console.log("NOC login starting");
+    Ext.create("NOC.main.desktop.LoginView", {
+      listeners: {
+        scope: this,
+        afterRender: this.hideSplashScreen,
+      },
+    });
+    console.log("NOC login started");
+  },
+  settings: function(){
+    Ext.Ajax.request({
+      url: "/main/desktop/settings/",
+      method: "GET",
+      scope: this,
+      success: function(response){
+        var setup = Ext.decode(response.responseText);
+        console.log("!!!");
+        console.log("!!! Running NOC desktop");
+        console.log("!!!");
+        // Initialize loader
+        Ext.BLANK_IMAGE_URL = "/ui/web/img/s.gif";
+        NOC.settings = {
+          systemId: setup.system_uuid ? setup.system_uuid : null,
+          brand: setup.brand,
+          installation_name: setup.installation_name,
+          preview_theme: setup.preview_theme,
+          language: setup.language,
+          logo_url: setup.logo_url,
+          logo_width: setup.logo_width,
+          logo_height: setup.logo_height,
+          branding_color: setup.branding_color,
+          branding_background_color: setup.branding_background_color,
+          enable_search: setup.enable_search,
+          gitlab_url: setup.gitlab_url,
+          collections: {
+            allow_sharing: setup.collections.allow_sharing,
+            allow_overwrite: setup.collections.allow_overwrite,
+            project_id: setup.collections.project_id,
+          },
+          gis: {
+            base: {
+              enable_blank: setup.gis.base.enable_blank,
+              "enable_osm": setup.gis.base.enable_osm,
+              "enable_google_roadmap": setup.gis.base.enable_google_roadmap,
+              "enable_google_hybrid": setup.gis.base.enable_google_hybrid,
+              "enable_google_sat": setup.gis.base.enable_google_sat,
+              "enable_google_terrain": setup.gis.base.enable_google_terrain,
+              "enable_tile1": setup.gis.base.enable_tile1,
+              "enable_tile2": setup.gis.base.enable_tile2,
+              "enable_tile3": setup.gis.base.enable_tile3,
+              "enable_yandex_roadmap": setup.gis.base.enable_yandex_roadmap,
+              "enable_yandex_hybrid": setup.gis.base.enable_yandex_hybrid,
+              "enable_yandex_sat": setup.gis.base.enable_yandex_sat,
+            },
+            custom: {
+              tile1: {
+                name: setup.gis.custom.tile1.name,
+                url: setup.gis.custom.tile1.url,
+                subdomains: setup.gis.custom.tile1.subdomains,
+              },
+              tile2: {
+                name: setup.gis.custom.tile2.name,
+                url: setup.gis.custom.tile2.url,
+                subdomains: setup.gis.custom.tile2.subdomains,
+              },
+              tile3: {
+                name: setup.gis.custom.tile3.name,
+                url: setup.gis.custom.tile3.url,
+                subdomains: setup.gis.custom.tile3.subdomains,
+              },
+            },
+            "yandex_supported": setup.gis.yandex_supported,
+            default_layer: setup.gis.default_layer,
+          },
+          traceExtJSEvents: setup.traceExtJSEvents,
+          helpUrl: setup.helpUrl,
+          helpBranch: setup.helpBranch,
+          helpLanguage: setup.helpLanguage,
+          timezone: setup.timezone,
+          enable_remote_system_last_extract_info: setup.enable_remote_system_last_extract_info,
+          enableHelp: setup.helpUrl && setup.helpUrl !== "",
+          has_geocoder: setup.has_geocoder,
+        };
+        NOC.templates = {};
+        // Change title
+        document.title = setup.brand + "|" + setup.installation_name;
+        // Add favicon
+        if(setup.favicon_mime){
+          var link = document.createElement("link");
+          link.rel = "icon";
+          link.type = setup.favicon_mime;
+          link.href = setup.favicon_url;
+          document.head.appendChild(link);
+        }
+        Ext.Loader.loadScript({
+          url: "/ui/web/js/override.js",
+          onLoad: function(){
+            // Create viewport after overrides loaded
+            this.app = Ext.create("NOC.main.desktop.Application", {
+              listeners: {
+                scope: this,
+                applicationReady: this.hideSplashScreen,
+              },
+            });
+          },
+          onError: function(){
+            NOC.error(__("Failed to load override.js"));
+          },
+          scope: this,
+        });
+      },
+      failure: function(response){
+        console.error("Request failed:", response.status);
+      },
+    });
+  },
+  hideSplashScreen: function(){
+    var mask = Ext.get("noc-loading-mask"),
+      parent = Ext.get("noc-loading");
+    mask.fadeOut({
+      callback: function(){
+        mask.destroy();
+      },
+    });
+    parent.fadeOut({
+      callback: function(){
+        parent.destroy();
+      },
+    });
+  },
 });
