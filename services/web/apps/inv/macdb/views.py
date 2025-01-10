1822ef4c41e6486dacc70a8ff589a8c778d7c737
Andrey Vertiprahov <aversanta@gmail.com>
2025-01-10 07:58:44 +0500

    Add filter to MACDB UI.

--- services/web/apps/inv/macdb/views.py
+++ services/web/apps/inv/macdb/views.py
@@ -1,32 +1,26 @@
 # ---------------------------------------------------------------------
 # inv.macdb application
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2020 The NOC Project
+# Copyright (C) 2007-2024 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
 # Python modules
-import threading
 import re
+from typing import Optional
 
 # Third-party modules
-from django.db.models import Q as d_Q
+import orjson
 
 # NOC modules
 from noc.services.web.base.extdocapplication import ExtApplication, view
 from noc.sa.models.managedobject import ManagedObject
-from noc.inv.models.macdb import MACDB
 from noc.sa.interfaces.base import MACAddressParameter
 from noc.core.mac import MAC
-from noc.inv.models.maclog import MACLog
-from noc.bi.models.mac import MAC as MACDBC
-from noc.inv.models.interface import Interface
+from noc.bi.models.macdb import MACDB
+from noc.inv.models.interfaceprofile import InterfaceProfile
+from noc.core.validators import is_mac
 from noc.core.translation import ugettext as _
-
-# @todo: REST proxy for backend buffered output(paging support in history)
-# @todo: search in field Managed Object/Port/Description
-
-tags_lock = threading.RLock()
 
 
 class MACApplication(ExtApplication):
@@ -36,44 +30,70 @@ class MACApplication(ExtApplication):
 
     title = _("MacDB")
     menu = _("Mac DB")
-    model = MACDB
-    bi_c = ""
-    macdb = MACDBC()
+
     mac_search_re = re.compile(r"([\dABCDEF][\dABCDEF]:){2,}", re.IGNORECASE)
     mac_search_re_inv = re.compile(r"(:[\dABCDEF][\dABCDEF]){2,}", re.IGNORECASE)
 
-    @staticmethod
-    def field_description(o, iname):
-        # @todo cache
-        iface = Interface.objects.filter(managed_object=o, name=iname)
-        if not iface:
-            iface = ":%s" % iname
+    def parse_result(self):
+        """"""
+
+    def macdb_query(
+        self,
+        mac_query: str,
+        managed_object: Optional[int] = None,
+        segment: Optional[str] = None,
+        interface_profile: Optional[str] = None,
+        is_uni: Optional[bool] = None,
+        offset: Optional[int] = None,
+        limit: Optional[int] = None,
+    ):
+        from noc.core.clickhouse.connect import connection
+
+        limit = limit or 50
+
+        sql = [
+            "SELECT last_seen, managed_object, interface, mac, vlan FROM %s" % MACDB._get_db_table(),
+        ]
+        filter_x = []
+        if mac_query:
+            filter_x.append(mac_query)
+        if managed_object:
+            mo = ManagedObject.get_by_id(managed_object)
+            filter_x.append(f"managed_object = {mo.bi_id}")
+        if segment:
+            filter_x.append(f"segment = {segment}")
+        if interface_profile:
+            p = InterfaceProfile.get_by_id(interface_profile)
+            filter_x.append(f"interface_profile = {p.bi_id}")
+        if filter_x:
+            sql += ["WHERE %s" % " AND ".join(filter_x)]
+        sql += ["ORDER BY mac"]
+        if limit and offset:
+            sql += [f"LIMIT {offset}, {limit}"]
         else:
-            iface = iface[0]
-        return iface
-
-    def api_macdb(self, query, limit=0, offset=0):
-        current = []
-        m = self.macdb.mac_filter(query, limit=int(limit), offset=int(offset))
-
-        for p in m:
-            # mo = self.managedobject_name_to_id(int(p["managed_object"]))
-            mo = ManagedObject.objects.filter(bi_id=int(p["managed_object"]))
+            sql += [f"LIMIT {limit}"]
+        sql += ["FORMAT JSON"]
+        sql = " ".join(sql)
+        print("Run Query", sql)
+        ch = connection()
+        r = ch.execute(sql, return_raw=True)
+        r = orjson.loads(r)
+        out = []
+        for d in r["data"]:
+            mo = ManagedObject.get_by_bi_id(d["managed_object"])
             if not mo:
-                self.logger.warning("Managed object does not exists: %s" % p["managed_object"])
                 continue
-            mo = mo[0]
-            iface = self.field_description(mo, p["interface"])
-            current += [
+            iface = mo.get_interface(d["interface"])
+            out.append(
                 {
-                    "last_changed": p["timestamp"],
-                    "mac": str(MAC(int(p["mac"]))),
+                    "last_changed": d["last_seen"],
+                    "mac": str(MAC(int(d["mac"]))),
                     "l2_domain": str(mo.l2_domain),
                     "l2_domain__label": getattr(mo.l2_domain, "name", ""),
-                    "vlan": p["vlan"],
+                    "vlan": d["vlan"],
                     "managed_object": str(mo),
                     "managed_object__label": str(mo),
-                    "interface": str(iface),
+                    "interface": str(d["interface"]),
                     "interface__label": str(iface),
                     "description": getattr(iface, "description", ""),
                     "pool": str(mo.pool),
@@ -81,131 +101,37 @@ class MACApplication(ExtApplication):
                     "object_profile": str(mo.object_profile),
                     "object_profile__label": getattr(mo.object_profile, "name", ""),
                 }
-            ]
-        total = len(current)
-        if total == int(limit):
-            total = int(offset) + int(limit) * 2
-        else:
-            total = int(offset) + len(current)
-        return {"total": total, "success": True, "data": current}
+            )
+        return out, r["rows_before_limit_at_least"]
 
     @view(method=["GET", "POST"], url="^$", access="read", api=True)
     def api_list(self, request):
-        q = {str(k): v[0] if len(v) == 1 else v for k, v in request.GET.lists()}
-        # find mac request select max(ts), managed_object, interface, vlan from mac
-        # where like(MACNumToString(mac), 'A0:AB:1B%') group by managed_object, interface, vlan;
+        q = self.parse_request_query(request)
+        print(q)
         query = q.get("__query")
         start = q.get("__start")
         limit = q.get("__limit")
-        # page = q.get("__page")
-        out = []
         if not query:
-            return self.response(out, status=self.OK)
-        try:
-            mac = int(MAC(MACAddressParameter(accept_bin=False).clean(query)))
-            out = self.api_macdb({"mac": mac}, limit=limit, offset=start)
-        except ValueError:
-            if self.mac_search_re.match(query):
-                out = self.api_macdb(
-                    {"mac__like": "%s%%" % str(query.upper())}, limit=limit, offset=start
-                )
-            elif self.mac_search_re_inv.match(query):
-                out = self.api_macdb(
-                    {"mac__like": "%%%s" % str(query.upper())}, limit=limit, offset=start
-                )
-            else:
-                # Try MO search
-                # @todo ManagedObject search
-                self.logger.debug("MACDB ManagedObject search")
-                mo_q = ManagedObject.get_search_Q(query)
-                if not mo_q:
-                    mo_q = d_Q(name__contains=query)
-                mos = [mo.bi_id for mo in ManagedObject.objects.filter(mo_q)[:2]]
-                if mos:
-                    out = self.api_macdb({"managed_object__in": mos}, limit=limit, offset=start)
-        # out = self.api_get_maclog(request, mac)
-        return self.response(out, status=self.OK)
+            mac_query = None
+        elif is_mac(query):
+            mac_query = f"mac = {int(MAC(MACAddressParameter(accept_bin=False).clean(query)))}"
+        elif self.mac_search_re.match(query):
+            mac_query = f"MACNumToString(mac) like {query}"
+        elif self.mac_search_re_inv.match(query):
+            mac_query = f"MACNumToString(mac) like {query}"
+        else:
+            raise ValueError("Unknown query string")
+        out, total = self.macdb_query(
+            mac_query=mac_query,
+            managed_object=q.get("managed_object"),
+            interface_profile=q.get("interface_profile"),
+            segment=q.get("segment"),
+            offset=start, limit=limit,
+        )
+        return self.response({"total": total, "success": True, "data": out}, status=self.OK)
 
     @view(url="^(?P<mac>[0-9A-F:]+)/$", method=["GET"], access="view", api=True)
     def api_get_maclog(self, request, mac):
-        """
-        GET maclog
-        :param mac:
-        :return:
-        """
-        current = []
-        mc = self.macdb
-        mac = MAC(mac)
-        m = mc.get_mac_history(int(mac))
-        # m = MACDB.objects.filter(mac=mac).order_by("-timestamp")
-        for p in m:
-            # mo = self.managedobject_name_to_id(int(p["managed_object"]))
-            # mo = self.bi_c[p["managed_object"]]
-            mo = ManagedObject.objects.filter(bi_id=int(p["managed_object"]))
-            if not mo:
-                self.logger.warning("Managed object does not exists: %s" % p["managed_object"])
-                continue
-            mo = mo[0]
-            iface = Interface.objects.filter(managed_object=mo, name=p["interface"])
-            if not iface:
-                iface = ":%s" % p["interface"]
-            else:
-                iface = iface[0]
-            current += [
-                {
-                    "last_changed": p["timestamp"],
-                    "mac": str(MAC(int(p["mac"]))),
-                    "l2_domain": str(mo.l2_domain),
-                    "l2_domain__label": mo.l2_domain.name,
-                    "vlan": p["vlan"],
-                    "managed_object": str(mo),
-                    "managed_object__label": str(mo),
-                    "interface": str(iface),
-                    "interface__label": str(iface),
-                    "description": getattr(iface, "description", ""),
-                    "pool": str(mo.pool),
-                    "pool__label": mo.pool.name,
-                    "object_profile": str(mo.object_profile),
-                    "object_profile__label": mo.object_profile.name,
-                }
-            ]
-
-        history = []
-        id_cache = {}
-        d_cache = {}
-
-        for i in MACLog.objects.filter(mac=mac).order_by("-timestamp"):
-            id = id_cache.get(i.managed_object_name)
-            if id is None:
-                for p in ManagedObject.objects.filter(name=i.managed_object_name):
-                    id = p.id
-                    id_cache[i.managed_object_name] = p.id
-            c = d_cache.get((id, i.interface_name))
-            if c is None:
-                for d in Interface.objects.filter(managed_object=id, name=i.interface_name):
-                    if d:
-                        if d.description:
-                            c = d.description
-                            d_cache[id, i.interface_name] = d.description
-                        else:
-                            c = ""
-                            d_cache[id, i.interface_name] = ""
-
-                    else:
-                        c = ""
-                        d_cache[id, i.interface_name] = ""
-
-            history += [
-                {
-                    "timestamp": str(i.timestamp),
-                    "mac": i.mac,
-                    "l2_domain": "",
-                    "vlan": i.vlan,
-                    "managed_object_name": str(i.managed_object_name),
-                    "interface_name": str(i.interface_name),
-                    "description": c,
-                    "pool": str(i.pool_name),
-                    "object_profile": str(i.object_profile_name),
-                }
-            ]
-        return current
+        """GET maclog"""
+        out = []
+        return self.response(out, status=self.OK)
