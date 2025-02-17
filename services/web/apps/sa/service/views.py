b0f80209fc57187cfcd4f220713af9da46a40e2b
Andrey Vertiprahov <aversanta@gmail.com>
2025-02-17 16:11:53 +0500

    Fixed on backend.

--- services/web/apps/sa/service/views.py
+++ services/web/apps/sa/service/views.py
@@ -6,8 +6,7 @@
 # ----------------------------------------------------------------------
 
 # Python modules
-from collections import defaultdict
-from typing import Optional, List, Dict
+from typing import Optional, List, Dict, Any
 
 # Third-party modules
 from mongoengine.queryset import Q
@@ -20,7 +19,9 @@ from noc.sa.interfaces.base import (
     ModelParameter,
     DictListParameter,
     StringListParameter,
+    IntParameter,
     IPv4Parameter,
+    DictParameter,
 )
 from noc.sa.models.service import Service
 from noc.sa.models.serviceinstance import ServiceInstance
@@ -32,6 +33,7 @@ from noc.core.translation import ugettext as _
 from noc.core.validators import is_objectid, is_ipv4, is_mac
 from noc.core.models.serviceinstanceconfig import InstanceType
 from noc.core.models.inputsources import InputSource
+from noc.core.resource import from_resource
 from noc.core.comp import smart_text
 
 
@@ -63,24 +65,15 @@ class ServiceApplication(ExtDocApplication):
     def field_label(self, o):
         return o.label
 
-    def field_instance_count(self, o):
-        return ServiceInstance.objects.filter(service=o).count()
-
-    def bulk_field_instances(self, data):
-        """
-        Apply interface_count fields
-        :param data:
-        :return:
-        """
+    def bulk_field_instance_count(self, data):
         svc_ids = [x["id"] for x in data]
         if not svc_ids:
             return data
-        instances = defaultdict(list)
-        for si in ServiceInstance.objects.filter(service__in=svc_ids):
-            instances[str(si.service.id)].append(self.service_instance_to_dict(si))
+        instances = ServiceInstance.objects.item_frequencies("service")
+        instances = {str(k): v for k, v in instances.items()}
         # Apply service instance
         for x in data:
-            x["instances"] = instances.get(x["id"]) or []
+            x["instance_count"] = instances.get(x["id"]) or 0
         return data
 
     def get_Q(self, request, query):
@@ -153,40 +146,43 @@ class ServiceApplication(ExtDocApplication):
             ]
         }
 
-    @view("^(?P<sid>[0-9a-f]{24})/instance/", access="read", api=True)
-    def api_get_instance(self, request, sid: str):
-        o = self.get_object_or_404(Service, id=sid)
-        r = []
-        for si in ServiceInstance.objects.filter(service=o):
-            r.append(
+    def instance_to_dict_si(self, o: ServiceInstance) -> Dict[str, Any]:
+        r = {
+            "id": str(o.id),
+            "sources": [
+                {"discovery": "D", "etl": "E", "manual": "M"}[ss.value] for ss in o.sources
+            ],
+            "type": o.type,
+            "fqdn": o.fqdn,
+            "port": o.port,
+            "managed_object": None,
+            "addresses": [],
+            "name": o.name,
+            "resources": [],
+            "allow_update": o.type != InstanceType.OTHER,
+        }
+        if o.managed_object:
+            r |= {
+                "managed_object": o.managed_object.id,
+                "managed_object__label": o.managed_object.name,
+            }
+        for a in o.addresses:
+            if a.pool:
+                r["addresses"] += [
+                    {"address": a.address, "pool": str(a.pool.id), "pool__label": a.pool.name}
+                ]
+            else:
+                r["addresses"] += [{"address": a.address, "pool": None}]
+        for rr in o.resources:
+            x, _ = from_resource(rr)
+            r["resources"] += [
                 {
-                    "sources": [
-                        {"discovery": "D", "etl": "E", "manual": "M"}[ss.value] for ss in si.sources
-                    ],
-                    "type": si.type,
-                    "fqdn": si.fqdn,
-                    "port": si.port,
-                    "managed_object": None,
-                    "addresses": [],
-                    "name": si.name,
-                    "resources": [],
-                    "allow_update": True,
+                    "resource": rr,
+                    "resource__label": str(x),
+                    "managed_object": x.managed_object.id,
+                    "managed_object__label": str(x.managed_object.name),
                 }
-            )
-            if si.managed_object:
-                r[-1] |= {
-                    "managed_object": si.managed_object.id,
-                    "managed_object__label": si.managed_object.name,
-                }
-            for a in si.addresses:
-                if a.pool:
-                    r[-1]["addresses"] += [
-                        {"address": a.address, "pool": str(a.pool.id), "pool__label": a.pool.name}
-                    ]
-                else:
-                    r[-1]["addresses"] += [{"address": a.address, "pool": None}]
-            for rr in si.resources:
-                r[-1]["resources"] += [{"resource": rr, "resource_label": "Name1"}]
+            ]
         return r
 
     @view(r"^(?P<sid>[0-9a-f]{24})/resource/(?P<r_type>\S+)/", access="read", api=True)
@@ -213,8 +209,47 @@ class ServiceApplication(ExtDocApplication):
             )
         return r
 
+    @view("^(?P<sid>[0-9a-f]{24})/instance/$", access="read", api=True)
+    def api_get_instance(self, request, sid: str):
+        o = self.get_object_or_404(Service, id=sid)
+        r = []
+        for si in ServiceInstance.objects.filter(service=o):
+            r.append(self.instance_to_dict_si(si))
+        return r
+
     @view(
-        r"^(?P<sid>[0-9a-f]{24})/register_instance/(?P<i_type>\S+)/",
+        "^(?P<sid>[0-9a-f]{24})/instance/(?P<iid>[0-9a-f]{24})/$",
+        method=["PUT"],
+        access="update",
+        validate={
+            "name": UnicodeParameter(required=False),
+            "fqdn": UnicodeParameter(required=False),
+            "port": IntParameter(required=False, min_value=0, max_value=65536),
+        },
+        api=True,
+    )
+    def api_update_instance(
+        self,
+        request,
+        sid: str,
+        iid: str,
+        name: Optional[str] = None,
+        fqdn: Optional[str] = None,
+        port: Optional[int] = None,
+    ):
+        si = self.get_object_or_404(ServiceInstance, id=iid)
+        if si.name != name:
+            si.name = name
+        if si.fqdn != fqdn:
+            si.fqdn = fqdn
+        if si.port != port:
+            si.port = port
+        si.save()
+        return {"success": True, "data": self.instance_to_dict_si(si)}
+
+    # New Instance working
+    @view(
+        r"^(?P<sid>[0-9a-f]{24})/register_instance/(?P<i_type>\S+)/$",
         method=["POST"],
         access="register_instance",
         validate={
@@ -235,25 +270,45 @@ class ServiceApplication(ExtDocApplication):
         try:
             i_type = InstanceType(i_type)
         except ValueError:
-            return {"success": True, "detail": f"Not supported type: {i_type}"}
-        o.register_instance(i_type, name=name, fqdn=fqdn)
-        return {"success": True}
+            return {"success": True, "message": f"Not supported type: {i_type}"}
+        si = o.register_instance(i_type, name=name, fqdn=fqdn)
+        return {"success": True, "data": self.instance_to_dict_si(si)}
 
     @view(
-        r"^(?P<sid>[0-9a-f]{24})/instance/(?P<iid>[0-9a-f]{24})/bind/",
+        r"^(?P<sid>[0-9a-f]{24})/unregister_instance/(?P<iid>[0-9a-f]{24})/$",
+        method=["POST"],
+        access="unregister_instance",
+        api=True,
+    )
+    def api_unregister_instance(
+        self,
+        request,
+        sid: str,
+        iid: str,
+    ):
+        o = self.get_object_or_404(Service, id=sid)
+        si = self.get_object_or_404(ServiceInstance, id=iid)
+        o.deregister_instance(si.type, name=si.name)
+        return {"success": True}
+
+    # Resource Working
+    @view(
+        r"^(?P<sid>[0-9a-f]{24})/instance/(?P<iid>[0-9a-f]{24})/bind/$",
         method=["PUT"],
         access="update",
-        validate={
-            "managed_object": ModelParameter(model=ManagedObject, required=False),
-            "addresses": DictListParameter(
-                required=False,
-                attrs={
-                    "address": IPv4Parameter(required=True),
-                    "pool": UnicodeParameter(required=False),
-                },
-            ),
-            "resources": StringListParameter(required=False),
-        },
+        validate=DictParameter(
+            attrs={
+                "managed_object": ModelParameter(model=ManagedObject, required=False),
+                "addresses": DictListParameter(
+                    required=False,
+                    attrs={
+                        "address": IPv4Parameter(required=True),
+                        "pool": UnicodeParameter(required=False),
+                    },
+                ),
+                "resources": StringListParameter(required=False),
+            },
+        ),
         api=True,
     )
     def api_instance_bind(
@@ -268,11 +323,16 @@ class ServiceApplication(ExtDocApplication):
         si = self.get_object_or_404(ServiceInstance, id=iid)
         if addresses:
             si.register_endpoint(InputSource.MANUAL, addresses=[a["address"] for a in addresses])
+            si.save()
         if managed_object:
             si.refresh_managed_object(managed_object, source=InputSource.MANUAL)
         if resources:
-            si.update_resources(resources, source=InputSource.MANUAL)
-        return {"success": True}
+            r = []
+            for x in resources:
+                x, _ = from_resource(x)
+                r.append(x)
+            si.update_resources(r, source=InputSource.MANUAL)
+        return {"success": True, "data": self.instance_to_dict_si(si)}
 
     @view(
         r"^(?P<sid>[0-9a-f]{24})/instance/(?P<iid>[0-9a-f]{24})/unbind/(?P<r_type>\S+)/",
@@ -290,8 +350,10 @@ class ServiceApplication(ExtDocApplication):
         # Check Permission
         si = self.get_object_or_404(ServiceInstance, id=iid)
         if r_type == "managed_object":
-            si.refresh_managed_object()
+            si.reset_object()
         elif r_type == "addresses":
             si.deregister_endpoint(InputSource.MANUAL)
+            si.save()
         elif r_type == "resources":
             si.update_resources([], InputSource.MANUAL)
+        return {"success": True, "data": self.instance_to_dict_si(si)}
