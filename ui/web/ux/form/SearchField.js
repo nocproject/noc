e9c5a6827ac038608b3582cf84b1f18c6b8f4316
uncol <d.a.zakharov@gmail.com>
2025-02-18 15:41:58 +0000

    inv.channel search added

--- ui/web/ux/form/SearchField.js
+++ ui/web/ux/form/SearchField.js
@@ -1,57 +1,57 @@
-Ext.define('Ext.ux.form.SearchField', {
-    extend: 'Ext.form.field.Text',
+Ext.define("Ext.ux.form.SearchField", {
+  extend: "Ext.form.field.Text",
 
-    alias: 'widget.searchfield',
+  alias: "widget.searchfield",
 
-    scope: null,
-    handler: null,
-    emptytext: __("Search..."),
-    // Auto-select
-    typeAhead: false,
-    typeAheadDelay: 200,
-    // Minimum characters to search
-    // when typeAhead == true
-    minChars: 2,
-    // Require explicit submit to search
-    explicitSubmit: false,
+  scope: null,
+  handler: null,
+  emptyText: __("Search..."),
+  // Auto-select
+  typeAhead: false,
+  typeAheadDelay: 200,
+  // Minimum characters to search
+  // when typeAhead == true
+  minChars: 2,
+  // Require explicit submit to search
+  explicitSubmit: false,
 
-    initComponent: function() {
-        var me = this;
-        me.lastChars = 0;
-        me.callParent();
-        me.on("specialkey", me.onSpecialKey, me);
-        if(me.typeAhead) {
-            me.on("change", me.onChange, me, {
-                buffer: me.typeAheadDelay
-            });
-        }
-    },
-    //
-    onSpecialKey: function(field, event) {
-        var me = this,
-            key = event.getKey();
-        if(key == event.ENTER) {
-            me.onSearch();
-        } else if (key == event.ESC) {
-            field.reset();
-        }
-    },
-    //
-    onChange: function() {
-        var me = this,
-            query = me.getValue(),
-            ql = query.length;
-        if(!ql || ql < me.lastChars || ql >= me.minChars) {
-            if(!me.explicitSubmit) {
-                me.onSearch();
-            }
-        }
-        me.lastChars = ql;
-    },
-    //
-    onSearch: function() {
-        var me = this,
-            query = me.getValue();
-        Ext.callback(me.handler, me.scope || this, [query]);
+  initComponent: function(){
+    var me = this;
+    me.lastChars = 0;
+    me.callParent();
+    me.on("specialkey", me.onSpecialKey, me);
+    if(me.typeAhead){
+      me.on("change", me.onChange, me, {
+        buffer: me.typeAheadDelay,
+      });
     }
+  },
+  //
+  onSpecialKey: function(field, event){
+    var me = this,
+      key = event.getKey();
+    if(key == event.ENTER){
+      me.onSearch();
+    } else if(key == event.ESC){
+      field.reset();
+    }
+  },
+  //
+  onChange: function(){
+    var me = this,
+      query = me.getValue(),
+      ql = query.length;
+    if(!ql || ql < me.lastChars || ql >= me.minChars){
+      if(!me.explicitSubmit){
+        me.onSearch();
+      }
+    }
+    me.lastChars = ql;
+  },
+  //
+  onSearch: function(){
+    var me = this,
+      query = me.getValue();
+    Ext.callback(me.handler, me.scope || this, [query]);
+  },
 });
