ad434a8e935acc527b98ae646e9bf3f060de8c80
Andrey Vertiprahov <aversanta@gmail.com>
2025-02-22 15:14:41 +0500

    Fix redpanda client.

--- core/msgstream/redpanda.py
+++ core/msgstream/redpanda.py
@@ -47,7 +47,7 @@ class RedPandaClient(object):
     SUBSCRIBE_BULK = True
 
     def __init__(self):
-        self.bootstrap = run_sync(self.resolve_broker)
+        self.bootstrap = None
         self.producer: Optional[AIOKafkaProducer] = None
         self.consumer: Optional[AIOKafkaConsumer] = None
         self.client: Optional[AIOKafkaClient] = None
@@ -57,13 +57,19 @@ class RedPandaClient(object):
         kafka_logger = logging.getLogger("kafka")
         kafka_logger.setLevel(logging.WARN)
 
-    async def resolve_broker(self) -> str:
+    @classmethod
+    async def resolve_broker(cls) -> str:
         # Getting addresses from config directly will block the loop on resolve() method.
         # So get parameter via .find_parameter() and resolve explicitly.
         addresses = await config.find_parameter("redpanda.addresses").async_get()
         # Use random broker from seed
         svc = random.choice(addresses)
         return f"{svc.host}:{svc.port}"
+
+    def get_bootstrap(self) -> str:
+        if not self.bootstrap:
+            self.bootstrap = run_sync(self.resolve_broker)
+        return self.bootstrap
 
     async def __aenter__(self) -> "RedPandaClient":
         return self
@@ -152,11 +158,11 @@ class RedPandaClient(object):
         """
         if self.producer:
             return self.producer
-        # bootstrap = [x.strip() for x in config.kafkasender.bootstrap_servers.split(",")]
-        logger.info("Connecting to producer using bootstrap services %s", self.bootstrap)
+        bootstrap = self.get_bootstrap()
+        logger.info("Connecting to producer using bootstrap services %s", bootstrap)
         self.producer = AIOKafkaProducer(
             loop=self.loop,
-            bootstrap_servers=self.bootstrap,
+            bootstrap_servers=bootstrap,
             acks=1,
             max_batch_size=config.redpanda.max_batch_size,
             sasl_mechanism=config.redpanda.sasl_mechanism,
@@ -181,7 +187,7 @@ class RedPandaClient(object):
             return self.consumer
         self.consumer = AIOKafkaConsumer(
             loop=self.loop,
-            bootstrap_servers=self.bootstrap,
+            bootstrap_servers=self.get_bootstrap(),
             client_id=CLIENT_ID,
             enable_auto_commit=False,
             group_id=group_id,
@@ -203,20 +209,20 @@ class RedPandaClient(object):
         """
         if not self.client:
             self.client = AIOKafkaClient(
-                bootstrap_servers=self.bootstrap, client_id=CLIENT_ID
+                bootstrap_servers=self.get_bootstrap(), client_id=CLIENT_ID
             )  # config.client_id
         return self.client
 
-    def get_kafka_admin_client(self) -> AIOKafkaAdminClient:
+    async def get_kafka_admin_client(self) -> AIOKafkaAdminClient:
         """
         Return Kafka Admin Client
         :return:
         """
         if not self.admin_client:
             self.admin_client = AIOKafkaAdminClient(
-                bootstrap_servers=self.bootstrap, client_id=CLIENT_ID
+                bootstrap_servers=self.get_bootstrap(), client_id=CLIENT_ID
             )  # config.client_id
-            self.admin_client.start()
+            await self.admin_client.start()
         return self.admin_client
 
     @staticmethod
@@ -260,7 +266,7 @@ class RedPandaClient(object):
         :param replication_factor:
         :return:
         """
-        admin_client = self.get_kafka_admin_client()
+        admin_client = await self.get_kafka_admin_client()
         await admin_client.create_topics(
             new_topics=[
                 NewTopic(
@@ -279,7 +285,7 @@ class RedPandaClient(object):
         :param name: Topic name
         :return:
         """
-        admin_client = self.get_kafka_admin_client()
+        admin_client = await self.get_kafka_admin_client()
         try:
             await admin_client.delete_topics([name])
         except UnknownTopicOrPartitionError:
