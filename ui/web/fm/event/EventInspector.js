f914223e3a739083a2c9bff8b4a139cbe775ce1b
uncol <d.a.zakharov@gmail.com>
2025-03-01 16:04:48 +0300

    vars tables added

--- ui/web/fm/event/EventInspector.js
+++ ui/web/fm/event/EventInspector.js
@@ -9,7 +9,6 @@ console.debug("Defining NOC.fm.event.EventInspector");
 Ext.define("NOC.fm.event.EventInspector", {
   extend: "Ext.form.Panel",
   title: __("Event Inspector"),
-  layout: "form",
   scrollable: true,
   bodyPadding: 4,
   defaultListenerScope: true,
@@ -24,6 +23,22 @@ Ext.define("NOC.fm.event.EventInspector", {
       target: null,
       object: null,
       segment: null,
+      vars: null,
+    },
+    stores: {
+      varsStore: {
+        fields: ["name", "value", "type"],
+        groupField: "type",
+        sorters: ["type", "name"],
+        // data: [
+        // {type: "Var", name: "var1", value: "value1"},
+        // {type: "Var", name: "var2", value: "value2"},
+        // {type: "Var", name: "var3", value: "value3"},
+        // {type: "Raw", name: "var3", value: "value3"},
+        // {type: "Raw", name: "var1", value: "value1"},
+        // {type: "Raw", name: "var2", value: "value2"},
+        // ],
+      },
     },
   },
   items: [
@@ -72,8 +87,40 @@ Ext.define("NOC.fm.event.EventInspector", {
       fieldLabel: __("Segment"),
       bind: "{segment}",
     },
+    {
+      xtype: "gridpanel",
+      hideHeaders: true,
+      forceFit: true,
+      columnWidth: 1,
+      features: [{
+        ftype: "grouping",
+        groupHeaderTpl: "{name}",
+        collapsible: true,
+        enableGroupingMenu: false,
+      }],
+      viewConfig: {
+        stripeRows: true,
+        enableTextSelection: true,
+      },
+      bind: {
+        store: "{varsStore}",
+      },
+      columns: {
+        defaults: {
+          flex: 1,
+          cellWrap: true,
+          tdCls: "noc-white-space",
+          renderer: function(value){
+            return Ext.String.htmlEncode(value);
+          },
+        },
+        items: [
+          {dataIndex: "name"},
+          {dataIndex: "value"},
+        ],
+      },
+    },
   ],
-  
   dockedItems: [
     {
       xtype: "toolbar",
@@ -129,7 +176,10 @@ Ext.define("NOC.fm.event.EventInspector", {
     var object = record.get("object") || {},
       objectStr = Ext.Object.isEmpty(object) ? "-" : Ext.String.format("{0}({1})", object.name || "", object.address || ""),
       remoteStr = Ext.isEmpty(record.get("remote_system")) ? "-" : Ext.String.format("{0}:{1}", record.get("remote_system"), record.get("remote_id")),
-      labels = Ext.isEmpty(record.get("labels")) ? "-" : NOC.render.LabelField(record.get("labels"));
+      labels = Ext.isEmpty(record.get("labels")) ? "-" : NOC.render.LabelField(record.get("labels")),
+      varsData = [...this.makeVarsData(__("Raw vars"), record.get("raw_vars")),
+                  ...this.makeVarsData(__("Vars"), record.get("vars"))];
+    
     this.getViewModel().setData({
       id: record.get("id") || "-",
       message: record.get("message") || "-",
@@ -141,5 +191,21 @@ Ext.define("NOC.fm.event.EventInspector", {
       object: objectStr,
       segment: record.get("segment") || "",
     });
+    this.getViewModel().getStore("varsStore").loadData(varsData);
+  },
+  //
+  makeVarsData: function(type, object){
+    if(Ext.Object.isEmpty(object)){
+      return [];
+    }
+    var data = [];
+    Ext.Object.each(object, function(key, value){
+      data.push({
+        type: type,
+        name: key,
+        value: Ext.isObject(value) ? Ext.JSON.encode(value) : value,
+      });
+    });
+    return data;
   },
 });
