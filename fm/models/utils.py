bd7a8d92f2ec0f913357a667d395700a15f80a8a
Alexander Lakiza <off.lakiza@yandex.ru>
2025-03-01 05:48:33 +0000

    release-23.1 Event query optimization

--- fm/models/utils.py
+++ fm/models/utils.py
@@ -1,11 +1,12 @@
 # ---------------------------------------------------------------------
 # FM models utils
 # ---------------------------------------------------------------------
-# Copyright (C) 2007-2023 The NOC Project
+# Copyright (C) 2007-2024 The NOC Project
 # See LICENSE for details
 # ---------------------------------------------------------------------
 
 # Python modules
+import datetime
 from typing import Union
 
 # Third-party modules
@@ -13,34 +14,71 @@ from bson import ObjectId
 import orjson
 
 # NOC modules
+from noc.config import config
 from noc.core.clickhouse.connect import connection
+from noc.core.clickhouse.fields import DateField, DateTimeField
+
+MAX_DISPOSE_DELAY = datetime.timedelta(hours=1)
 
 
 def get_event(event_id):
+    event_id = str(ObjectId(event_id))
+    # Determine possible date
+    oid = ObjectId(event_id)
+    ts: datetime.datetime = oid.generation_time.astimezone(config.timezone)
+    from_ts = ts - MAX_DISPOSE_DELAY
+    to_ts = ts + MAX_DISPOSE_DELAY
     """
     Get event by event_id
     """
-    sql = f"""select
-        e.event_id as id,
-        e.ts as timestamp,
-        e.event_class as event_class_bi_id,
-        e.managed_object as managed_object_bi_id,
-        e.start_ts as start_timestamp,
-        e.source, e.raw_vars, e.resolved_vars, e.vars,
-        d.alarms as alarms
-        from events e
-        LEFT OUTER JOIN (
-            SELECT event_id, groupArray(alarm_id) as alarms
-            FROM disposelog
-            WHERE event_id='{event_id}' AND alarm_id != ''
-            GROUP BY event_id
-        ) as d
-        ON e.event_id == d.event_id
-        where event_id='{event_id}'
-        format JSON
+    sql = """
+        SELECT
+            e.event_id as id,
+            e.ts as timestamp,
+            e.event_class as event_class_bi_id,
+            e.managed_object as managed_object_bi_id,
+            e.start_ts as start_timestamp,
+            e.source, e.raw_vars, e.resolved_vars, e.vars,
+            d.alarms as alarms
+        FROM events e
+            LEFT OUTER JOIN (
+                SELECT event_id, groupArray(alarm_id) as alarms
+                FROM disposelog
+                WHERE
+                    event_id = %s
+                    AND alarm_id != ''
+                    AND ts BETWEEN %s AND %s
+                    AND date BETWEEN %s AND %s
+                GROUP BY event_id
+            ) as d
+            ON e.event_id == d.event_id
+        WHERE
+            event_id = %s
+            AND ts BETWEEN %s AND %s
+            AND date BETWEEN %s AND %s
+        FORMAT JSON
     """
     cursor = connection()
-    res = orjson.loads(cursor.execute(sql, return_raw=True))
+    df = DateField()
+    dtf = DateTimeField()
+    res = orjson.loads(
+        cursor.execute(
+            sql,
+            args=[
+                event_id,
+                dtf.to_json(from_ts),
+                dtf.to_json(to_ts),
+                df.to_json(from_ts),
+                df.to_json(to_ts),
+                event_id,
+                dtf.to_json(from_ts),
+                dtf.to_json(to_ts),
+                df.to_json(from_ts),
+                df.to_json(to_ts),
+            ],
+            return_raw=True,
+        )
+    )
     if res:
         return ActiveEvent.create_from_dict(res["data"][0])
     return None
