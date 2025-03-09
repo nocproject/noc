b4d522a78cda30024796601c56eec806fc713d9c
Andrey Vertiprahov <aversanta@gmail.com>
2025-03-09 08:23:29 +0500

    Add web backend.

---
+++ ui/web/fm/eventcategory/LookupField.js
@@ -0,0 +1,13 @@
+//---------------------------------------------------------------------
+// NOC.fm.eventcategory.Lookup
+//---------------------------------------------------------------------
+// Copyright (C) 2007-2025 The NOC Project
+// See LICENSE for details
+//---------------------------------------------------------------------
+console.debug("Defining NOC.fm.eventcategory.LookupField");
+
+Ext.define("NOC.fm.eventcategory.LookupField", {
+    extend: "NOC.core.ComboBox",
+    alias: "widget.fm.eventcategory.LookupField",
+    uiStyle: "medium-combo"
+});
