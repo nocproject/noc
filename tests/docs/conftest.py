8ed4db6e3f8dd40cc85b9b70451432dc23567345
Michael R <rmiclee@gmail.com>
2025-03-28 13:27:38 +0300

    Update tests for work with SUMMARY

--- tests/docs/conftest.py
+++ tests/docs/conftest.py
@@ -1,22 +1,45 @@
 # ----------------------------------------------------------------------
 # docs test fixtures
 # ----------------------------------------------------------------------
-# Copyright (C) 2007-2020 The NOC Project
+# Copyright (C) 2007-2025 The NOC Project
 # See LICENSE for details
 # ----------------------------------------------------------------------
+
+# Python modules
+import os
+import re
+from typing import List, Tuple
 
 # Third-party modules
 import pytest
 import yaml
 
+DOCS_DIR = "docs"
+SUMMARY_FILENAME = "SUMMARY.md"
+
 
 class ToC(object):
     def __init__(self, path):
-        with open(path) as f:
+        with open(path, encoding="utf-8") as f:
             data = yaml.safe_load(f.read().replace("!!python/name:", ""))
         self.items = {}
         for kv in data["nav"]:
             self.add_item([], kv)
+
+    def get_summary(self, path: str) -> List[Tuple]:
+        if path[-1] == "/":
+            path = path[:-1]
+        pp = os.path.join(DOCS_DIR, path, SUMMARY_FILENAME)
+        if os.path.exists(pp):
+            result = []
+            with open(pp, "r", encoding="utf-8") as f:
+                data = f.read().splitlines()
+            for line in data:
+                if line.strip():
+                    key, value = re.search(r"\[(.*)].*\((.*)\)", line).group(1, 2)
+                    result += [(key, value)]
+            return result
+        return []
 
     def add_item(self, path, kv):
         if isinstance(kv, str):
@@ -30,6 +53,11 @@ class ToC(object):
                 self.add_item(path + [k], lkv)
         else:
             self.items[tuple(path + [k])] = v
+            s_path = os.path.join(DOCS_DIR, v)
+            if os.path.isdir(s_path):
+                summary = self.get_summary(v)
+                for sk, sv in summary:
+                    self.add_item(path + [k], {sk: v + sv})
 
     def __contains__(self, item):
         return tuple(item) in self.items
