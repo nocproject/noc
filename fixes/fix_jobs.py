16f0bd1e695e18d8e5f8b6138944ac553181d4d1
alakiza <off.lakiza@yandex.ru>
2025-04-08 01:33:10 +0300

    refactor

--- fixes/fix_jobs.py
+++ fixes/fix_jobs.py
@@ -14,30 +14,25 @@ from noc.main.models.pool import Pool
 
 
 def fix():
-    # <State: ManagedObject Default: Managed>
-    managed_state = State.objects.filter(id="641b35e6fa01fd032a1f61f1").first()
-
+    print(f"Scanning ...")
     for p in Pool.objects.all():
-        print(f"Processing pool {p.name}")
+        print(f"\npool [{p.name}] ...")
         sched = Scheduler("discovery", pool=p.name)
 
         keys = []
         for job in sched.get_collection().aggregate(
             [{"$match": {Job.ATTR_STATUS: Job.S_SUSPEND}}, {"$group": {"_id": f"${Job.ATTR_KEY}"}}]
         ):
-            # print("Job", job)
             mo_id = job.get("_id")
-            if mo_id:
-                mo = ManagedObject.get_by_id(mo_id)
-                if not (
-                    mo.get_status() and mo.object_profile.enable_ping and mo.state == managed_state
-                ):
-                    continue
-                keys.append(mo_id)
+            if not mo_id:
+                continue
 
-        print(f"pool {p.name} has {len(keys)} bad objects")
-        for x in keys[0:5]:
-            print(x)
+            mo = ManagedObject.get_by_id(mo_id)
+            if not (mo.get_status() and mo.object_profile.enable_ping and mo.is_managed):
+                continue
+            keys.append(mo_id)
+
+        print(f"pool [{p.name}] has [{len(keys)}] online objects with suspended jobs")
         if keys:
-            print("Fix suspended state for jobs")
+            print("Fix suspended state for jobs ...")
             sched.suspend_keys(keys=keys, suspend=False)
