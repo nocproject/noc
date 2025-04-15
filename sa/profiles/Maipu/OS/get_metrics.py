3b6361f1b297c4e3c4829811a6ba754911676a39
alakiza <off.lakiza@yandex.ru>
2025-04-15 11:50:13 +0300

    Add initial support for Maipu

---
+++ sa/profiles/Maipu/OS/get_metrics.py
@@ -0,0 +1,13 @@
+# ---------------------------------------------------------------------
+# Maipu.OS.get_metrics
+# ---------------------------------------------------------------------
+# Copyright (C) 2007-2025 The NOC Project
+# See LICENSE for details
+# ---------------------------------------------------------------------
+
+# NOC modules
+from noc.sa.profiles.Generic.get_metrics import Script as BaseScript
+
+
+class Script(BaseScript):
+    name = "Maipu.OS.get_metrics"
