4e4a2cf6709405fb558b6337ec98134b3c31e7bc
uncol <d.a.zakharov@gmail.com>
2025-05-20 10:14:15 +0300

    main navigation, sticky

--- ui/web/main/desktop/NavPanel.js
+++ ui/web/main/desktop/NavPanel.js
@@ -10,7 +10,6 @@ Ext.define("NOC.main.desktop.NavPanel", {
   extend: "Ext.tree.Panel",
   region: "west",
   width: 200,
-  //collapsible: true,
   animCollapse: true,
   collapseFirst: false,
   split: true,
@@ -18,7 +17,7 @@ Ext.define("NOC.main.desktop.NavPanel", {
   app: null,
   useArrows: true,
   rootVisible: false,
-  singleExpand: true,
+  singleExpand: false,
   lines: false,
   hideHeaders: true,
   title: __("Navigation"),
@@ -27,8 +26,23 @@ Ext.define("NOC.main.desktop.NavPanel", {
   header: {
     cls: "noc-navigation-header",
   },
-  //hidden: true,
-
+  rowHeight: 36, // for noc theme
+  stickyNode: "nav-sticky-node",
+  maxStickyLevel: 1,
+  bufferedRenderer: false,
+  //
+  viewConfig: {
+    getRowClass: function(record){
+      var me = this.up("treepanel"),
+        depth = parseInt(record.get("depth"), 10),
+        isNewLevel = depth > me.maxStickyLevel;
+      if(isNewLevel){
+        me.createStickyCss(depth);
+      }
+      return `${me.stickyNode}-${depth}`;
+    },
+  },
+  //
   initComponent: function(){
     var me = this;
     Ext.apply(me, {
@@ -44,12 +58,52 @@ Ext.define("NOC.main.desktop.NavPanel", {
       listeners: {
         scope: me,
         itemclick: me.onItemClick,
+        viewready: me._onViewReady,
+        afterrender: function(){
+          me.store.on("datachanged", me._calculateRowHeight, me);
+          me.store.on("rootchange", me._calculateRowHeight, me);
+        },
       },
     });
     me.callParent();
   },
+  //
   onItemClick: function(view, record, item, index, event){
     var me = this;
     me.app.launchRecord(record, !event.shiftKey);
+  },
+  //
+  _onViewReady: function(treePanel){
+    var uniqueId = "nav-panel-" + Ext.id();
+    
+    treePanel.addCls(uniqueId);
+    Ext.util.CSS.createStyleSheet(
+      `.${uniqueId} .x-grid-item-container { overflow: unset !important; }`,
+    );
+  },
+  _calculateRowHeight: function(){
+    var nodes,
+      view = this.getView();
+    Ext.defer(function(){
+      nodes = view.getNodes(0, 0);
+      if(Ext.isDefined(nodes.length) && nodes.length > 0){
+        this.rowHeight = Ext.fly(nodes[0]).getHeight();
+        if(this.maxStickyLevel > 0){
+          for(var i = 1; i <= this.maxStickyLevel; i++){
+            this.createStickyCss(i);
+          }
+        } else{
+          this.createStickyCss(1);
+        }
+      } else{
+        Ext.defer(this._calculateRowHeight, 100, this);
+      }
+    }, 100, this);
+  },
+  //
+  createStickyCss: function(level){
+    var css = `table.x-grid-item:has(tr.${this.stickyNode}-${level}) { position: sticky; top: ${(level - 1) * this.rowHeight}px; z-index: ${this.zIndex}; }`;
+    Ext.util.CSS.createStyleSheet(css);
+    this.maxStickyLevel = level;
   },
 });
