23df3e6332a11f047a97f0b6763e501a9ef3ab52
Dmitry Volodin <dv@nocproject.org>
2025-05-30 16:17:24 +0200

    Remove ./noc dcs

--- scripts/deploy/migrate.sh
+++ scripts/deploy/migrate.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 # Perform all migrations
 set -e
-if [ -n "$NOC_MIGRATE_SLOTS_PATH" ]; then
-    if [ -d "$NOC_MIGRATE_SLOTS_PATH" ]; then
-        ./noc dcs set-slots -f $NOC_MIGRATE_SLOTS_PATH/*
-    else 
-        ./noc dcs set-slots -f $NOC_MIGRATE_SLOTS_PATH
-    fi
-fi
 ./noc migrate-slots
 ./noc migrate
 ./noc ensure-indexes
