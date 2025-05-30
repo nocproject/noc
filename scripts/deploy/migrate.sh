45b6ec7f0cb40054234204e4d2895cf2bc202d8c
Dmitry Volodin <dv@nocproject.org>
2025-05-30 16:15:30 +0200

    NEP-0102: NOC Deploy Configuration Protocol

--- scripts/deploy/migrate.sh
+++ scripts/deploy/migrate.sh
@@ -8,8 +8,10 @@ if [ -n "$NOC_MIGRATE_SLOTS_PATH" ]; then
         ./noc dcs set-slots -f $NOC_MIGRATE_SLOTS_PATH
     fi
 fi
+./noc migrate-slots
 ./noc migrate
 ./noc ensure-indexes
+./noc migrate-pools
 ./noc migrate-liftbridge
 ./noc collection sync
 ./noc migrate-liftbridge
