b129cbbeac19b7daf10d114ee8e08a0b9e8d8951
Andrey Vertiprakhov <aversanta@gmail.com>
2025-06-04 17:09:25 +0500

    Job methods.

--- services/escalator/job.py
+++ services/escalator/job.py
@@ -4,7 +4,7 @@
 # Copyright (C) 2007-2023, The NOC Project
 # See LICENSE for details
 # ----------------------------------------------------------------------
-
+import datetime
 # Python modules
 import logging
 import enum
@@ -38,6 +38,7 @@ from noc.fm.models.activealarm import ActiveAlarm
 from noc.fm.models.ttsystem import TTSystem
 from noc.fm.models.utils import get_alarm
 
+
 class ActionStatus(enum.Enum):
     """
     Job status.
@@ -50,7 +51,6 @@ class ActionStatus(enum.Enum):
         * 'e' - End Wait
     """
 
-    WAITING = "w"
     NEW = "n"
     SUCCESS = "s"
     FAILED = "f"
@@ -58,6 +58,7 @@ class ActionStatus(enum.Enum):
     CANCELLED = "c"
     SKIP = "s"
     WAIT_END = "we"
+    STOP = "stop/break"
 
 
 class JobStatus(enum.Enum):
@@ -77,6 +78,7 @@ class JobStatus(enum.Enum):
     """
 
     PENDING = "p"
+
 
 class ItemStatus(enum.Enum):
     """
@@ -111,15 +113,36 @@ class ActionResult(object):
     ctx: Optional[Dict[str, str]] = None
 
 
-class Action(object):
-    action: str
+class ActionLog(object):
+    """Action Part of log with Run"""
 
     def __init__(
         self,
+        action: EscalationMember,
         key,
+        # Time ?
+        delay: int = 0,
+        timestamp: Optional[datetime.datetime] = None,
+        status: ActionStatus = ActionStatus.NEW,
+        error: Optional[str] = None,
+        # Stop processed after action
+        stop_processing: bool = False,
+        # Source Task
+        user: Optional[User] = None,
+        tt_system: Optional[TTSystem] = None,
+        document_id: Optional[str] = None,
         **kwargs,
     ):
-        ...
+        self.action = action
+        self.key = key
+        self.timestamp = timestamp
+        self.status = status
+        self.error = error
+        self.ctx = kwargs
+        self.document_id = document_id
+
+    def set_status(self, result: ActionResult):
+        """Update Action Log"""
 
 
 class AlarmAutomationJob(object):
@@ -130,19 +153,40 @@ class AlarmAutomationJob(object):
     def __init__(
         self,
         name: str,
-        delay: int,
         status: JobStatus,
         items: List[Item],
         # alarms_ids
+        actions: List[ActionLog],
         ctx_id: Optional[int] = None,
         telemetry_sample: Optional[int] = None,
         id: Optional[str] = None,
         allow_fail: bool = False,
         logger: Optional[Any] = None,
     ):
-        ...
+        self.name = name
+        self.status = status
+        self.items = items
+        self.actions = actions
+        self.ctx_id = ctx_id
+        self.telemetry_sample = telemetry_sample
+        self.allow_fail = allow_fail
+        self.logger = logger
+
+    def run(self):
+        """Run Job"""
 
     def run_action(
         self, action: TTAction, key: str, **ctx: Dict[str, str],
     ) -> ActionResult:
         """Execute action"""
+
+    def get_state(self) -> Dict[str, Any]:
+        """Return Job State"""
+
+    @classmethod
+    def from_request(cls, req: EscalationRequest):
+        """Build Job from Request"""
+
+    @classmethod
+    def from_state(cls, state: Dict[str, Any]) -> "Job":
+        """Restore Job from state"""
