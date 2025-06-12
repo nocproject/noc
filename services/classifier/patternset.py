57d9138318027d8f315c8741959e2d2fd37aa552
Andrey Vertiprahov <aversanta@gmail.com>
2025-06-12 12:35:24 +0000

    Add FM IgnorePattern to datastream.

--- services/classifier/patternset.py
+++ services/classifier/patternset.py
@@ -6,9 +6,10 @@
 # ----------------------------------------------------------------------
 
 # Python modules
-from collections import defaultdict
 import logging
 import re
+from collections import defaultdict
+from typing import Dict, List, Tuple
 
 # NOC modules
 from noc.core.validators import is_oid
@@ -22,7 +23,10 @@ logger = logging.getLogger(__name__)
 
 class PatternSet(object):
     def __init__(self):
-        self.i_patterns = {}  # (profile, chain) -> [rule, ..., rule]
+        self.i_patterns: Dict[str, List[Tuple[str, str]]] = (
+            {}
+        )  # (profile, chain) -> [rule, ..., rule]
+        self.add_patterns = 0
 
     def load(self):
         """
@@ -38,17 +42,52 @@ class PatternSet(object):
             except re.error as e:
                 logger.error("Invalid ignore pattern '%s' (%s)" % (p.pattern, e))
                 continue
-            i_patterns[p.source] += [p.pattern]
+            i_patterns[p.source.value] += [str(p.id), p.pattern]
             n += 1
         self.i_patterns = i_patterns
         logger.info("%d Ignore patterns are loaded in the %d chains", n, len(self.i_patterns))
+
+    def update_pattern(self, rid: str, data):
+        """Update rule from lookup"""
+        source = data["sources"][0]
+        r = []
+        try:
+            re.compile(data["message_rx"])
+        except re.error as e:
+            logger.error("Invalid ignore pattern '%s' (%s)" % (data["message_rx"], e))
+            return
+        update = False
+        for p in self.i_patterns.get(source) or []:
+            if p[0] == rid:
+                r.append((rid, data["message_rx"]))
+                update |= True
+                logger.info("[%s] Update Ignore Pattern: %s", rid, data["message_rx"])
+                continue
+            r.append(p)
+        if not update:
+            r.append((rid, data["message_rx"]))
+            logger.info("[%s] New Ignore Pattern: %s", rid, data["message_rx"])
+            self.add_patterns += 1
+        self.i_patterns[source] = r
+
+    def delete_pattern(self, rid: str):
+        """Remove rule from lookup"""
+        r = []
+        for source in self.i_patterns:
+            for p in self.i_patterns[source]:
+                if p[0] == rid:
+                    logger.info("[%s] Delete Ignore Pattern", rid)
+                    continue
+                r.append(p)
+            if len(self.i_patterns[source]) != len(r):
+                self.i_patterns[source] = r
 
     def find_ignore_masks(self, source):
         if self.i_patterns:
             mask = self.i_patterns.get(source)
             if mask:
                 logging.debug("Setting ignore patterns for %s: %s", source, mask)
-                return [re.compile(x) for x in mask]
+                return [re.compile(x) for _, x in mask]
         return []
 
     def find_ignore_rule(self, event: Event):
