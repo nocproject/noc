0c636593d85b2d72a0fe2f0eb890af695af17bc6
Dmitry Volodin <dv@nocproject.org>
2025-06-12 08:26:41 +0200

    Code refactoring

--- commands/tech-report.py
+++ commands/tech-report.py
@@ -18,6 +18,10 @@ from pathlib import Path
 import re
 import subprocess
 import sys
+from dataclasses import dataclass
+from enum import IntEnum, auto
+from collections import defaultdict
+from typing import Dict, DefaultDict
 
 # NOC modules
 from noc.config import config
@@ -33,9 +37,64 @@ FORE_BLUE = f"{CSI}{str(34)}m"
 STYLE_RESET_ALL = f"{CSI}{str(0)}m"
 
 
+class LibStatus(IntEnum):
+    """
+    Library status.
+
+    Attributes:
+        OK: Library is ok.
+        MISMATCH: Version mismatch.
+        MISSED: Library is mandatory, but missed.
+        SKIP: Library is optional and skipped.
+    """
+
+    OK = auto()
+    MISMATCH = auto()
+    MISSED = auto()
+    SKIPPED = auto()
+
+
+@dataclass
+class Library(object):
+    original_name: str
+    req_version: str
+    inst_version: str = ""
+    node: bool = False
+
+    @property
+    def status(self) -> LibStatus:
+        if self.inst_version:
+            if self.req_version == self.inst_version:
+                return LibStatus.OK
+            return LibStatus.MISMATCH
+        if self.node:
+            return LibStatus.MISSED
+        return LibStatus.SKIPPED
+
+    def get_flag(self, is_ansi: bool = False) -> str:
+        if is_ansi:
+            return _ANSI_FLAGS[self.status]
+        return _UNICODE_FLAGS[self.status]
+
+
+_ANSI_FLAGS = {
+    LibStatus.OK: "v",
+    LibStatus.MISMATCH: "x",
+    LibStatus.MISSED: "?",
+    LibStatus.SKIPPED: "-",
+}
+
+_UNICODE_FLAGS = {
+    LibStatus.OK: f"{FORE_GREEN}\u2705{STYLE_RESET_ALL}",
+    LibStatus.MISMATCH: f"{FORE_RED}\u274c{STYLE_RESET_ALL}",
+    LibStatus.MISSED: f"{FORE_RED}\uff1f{STYLE_RESET_ALL}",
+    LibStatus.SKIPPED: f"{FORE_BLUE}\u2796{STYLE_RESET_ALL}",
+}
+
+
 class Command(BaseCommand):
     help = "Display system and dependencies information"
-    flag_ok, flag_error, flag_missing = "v ", "x ", "- "
+    is_ansi = False
 
     def add_arguments(self, parser):
         subparsers = parser.add_subparsers(dest="cmd")
@@ -45,14 +104,9 @@ class Command(BaseCommand):
         subparsers.add_parser("system", help="Display system information")
         subparsers.add_parser("dependencies", help="Display dependencies")
 
-    def set_unicode_symbols(self):
-        self.flag_ok = f"{FORE_GREEN}\u2705{STYLE_RESET_ALL}"
-        self.flag_error = f"{FORE_RED}\u274C{STYLE_RESET_ALL}"
-        self.flag_missing = f"{FORE_BLUE}\u2796{STYLE_RESET_ALL}"
-
     def handle(self, ansi_symbols, *args, **options):
-        if not ansi_symbols:
-            self.set_unicode_symbols()
+        if ansi_symbols:
+            self.is_ansi = True
         cmd = options.pop("cmd")
         cmd = cmd or "total"
         return getattr(self, f'handle_{cmd.replace("-", "_")}')(*args, **options)
@@ -61,7 +115,15 @@ class Command(BaseCommand):
         self.handle_system(*args, **options)
         self.handle_dependencies(*args, **options)
 
+    @property
+    def flags(self) -> Dict[LibStatus, str]:
+        if self.is_ansi:
+            return _ANSI_FLAGS
+        return _UNICODE_FLAGS
+
     def handle_system(self, *args, **options):
+        OK = self.flags[LibStatus.OK]
+        SKIPPED = self.flags[LibStatus.SKIPPED]
         self.print("System information")
         self.print("==================")
         self.print(f"NOC version       : {version.version}")
@@ -69,11 +131,7 @@ class Command(BaseCommand):
         self.print(f"Installation name : {config.installation_name}")
         custom_path = config.path.custom_path
         if custom_path:
-            cp_exists = (
-                f"exists {self.flag_ok}"
-                if os.path.exists(custom_path)
-                else f"NOT exists {self.flag_error}"
-            )
+            cp_exists = f"exists {OK}" if os.path.exists(custom_path) else f"NOT exists {SKIPPED}"
             self.print(f"Custom path       : {config.path.custom_path} ({cp_exists})")
             custom_revision = "-"
             if os.path.exists(custom_path):
@@ -95,16 +153,23 @@ class Command(BaseCommand):
     rx = re.compile(r"^(?P<lib_name>[a-zA-Z][-a-zA-Z0-9_]*)\[*.*]*==(?P<version>[a-zA-Z0-9.]+).*$")
 
     def handle_dependencies(self, *args, **options):
+        def lib_key(s: str) -> str:
+            return s.lower().replace("-", "_")
+
+        def n_status(s: LibStatus) -> str:
+            n = summary[s]
+            if n:
+                return f": {n}"
+            return ""
+
         self.print("Dependencies")
         self.print("============")
         # Get required versions
         root_path = Path(self.REQUIREMENTS_PATH)
-        libraries: dict[str, dict[str, str]] = {}
-        count = 0
+        libraries: dict[str, Library] = {}
         for fp in root_path.glob("*.txt"):
             if not fp.is_file:
                 continue
-            f_node = fp.name == "node.txt"
             with open(fp, "r") as f:
                 btext = f.read()
                 lines = btext.split("\n")
@@ -112,55 +177,41 @@ class Command(BaseCommand):
                 match = self.rx.match(line)
                 if match:
                     lib_name, req_version = match.groups()
-                    libraries[lib_name.lower()] = {
-                        "original_name": lib_name,
-                        "req_version": req_version,
-                        "inst_version": "",
-                        "node": f_node,
-                    }
-            count += 1
+                    libraries[lib_key(lib_name)] = Library(
+                        original_name=lib_name, req_version=req_version, node=fp.name == "node.txt"
+                    )
         # Get installed versions
         for distribution in metadata.distributions():
-            lib_name = distribution.metadata["Name"].lower()
+            lib_name = lib_key(distribution.metadata["Name"])
             if lib_name in libraries:
-                libraries[lib_name]["inst_version"] = distribution.version
-        # Sorting
-        libraries: list[tuple[str, dict[str, str]]] = sorted(
-            libraries.items(), key=lambda item: item[1]["original_name"]
-        )
+                libraries[lib_name].inst_version = distribution.version
         # Display information
-        col_lib_name, col_required, col_installed, col_node = 35, 25, 25, 25
+        col_lib_name, col_required, col_installed = 25, 25, 25
+        summary: DefaultDict[LibStatus, int] = defaultdict(int)
         self.print(
+            "   | "
             f"{'Library':{col_lib_name}} | {'Required':{col_required}} | "
-            f"{'Installed':{col_installed}} | {'Node/necessary':{col_node}}"
+            f"{'Installed':{col_installed}}"
         )
-        self.print(
-            f"{'-'*col_lib_name}-|-{'-'*col_required}-|-{'-'*col_installed}-|-{'-'*col_node}"
-        )
-        for lib_name, lib_data in libraries:
-            if not lib_data["inst_version"]:
-                flag = self.flag_missing
-            else:
-                flag = (
-                    self.flag_ok
-                    if lib_data["req_version"] == lib_data["inst_version"]
-                    else self.flag_error
-                )
-            flag_additional = (
-                self.flag_error if not lib_data["inst_version"] and lib_data["node"] else "  "
-            )
-            p_node = "YES" if lib_data["node"] else "-"
+        self.print(f"---|-{'-'*col_lib_name}-|-{'-'*col_required}-|-{'-'*col_installed}")
+        for lib_name, lib_data in sorted(libraries.items(), key=lambda item: item[1].original_name):
+            flag = lib_data.get_flag(self.is_ansi)
             self.print(
-                f"{flag}{flag_additional} {lib_data['original_name']:{col_lib_name - 5}} | "
-                f"{lib_data['req_version']:{col_required}} | "
-                f"{lib_data['inst_version']:{col_installed}} | {p_node:{col_node}}"
+                f"{flag} | {lib_data.original_name:{col_lib_name }} | "
+                f"{lib_data.req_version:{col_required}} | "
+                f"{lib_data.inst_version:{col_installed}}"
             )
+            summary[lib_data.status] += 1
         self.print("")
         self.print("Legend:")
-        self.print(f"{self.flag_ok}{'  '} Library version is correct")
-        self.print(f"{self.flag_error}{'  '} Library version is incorrect")
-        self.print(f"{self.flag_missing}{'  '} Library is missing")
-        self.print(f"{self.flag_missing}{self.flag_error} Library is necessary, but missing")
+        self.print(f"{self.flags[LibStatus.OK]} Library version is correct")
+        self.print(
+            f"{self.flags[LibStatus.MISMATCH]} Library version mismatch{n_status(LibStatus.MISMATCH)}"
+        )
+        self.print(
+            f"{self.flags[LibStatus.MISSED]} Library is mandatory, but missing{n_status(LibStatus.MISSED)}"
+        )
+        self.print(f"{self.flags[LibStatus.SKIPPED]} Library is optional and missing")
         self.print("")
 
 
