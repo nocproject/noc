32c25262f961fef6f44d7e0a46409faed6f83041
Andrey Vertiprahov <aversanta@gmail.com>
2025-06-12 05:26:54 +0000

    VMWare.vHost.get_capabilities. Fix Generic import.

--- sa/profiles/VMWare/vHost/get_capabilities.py
+++ sa/profiles/VMWare/vHost/get_capabilities.py
@@ -6,10 +6,11 @@
 # ---------------------------------------------------------------------
 
 # NOC modules
+from noc.sa.profiles.Generic.get_capabilities import Script as BaseScript
 from ..vim import VIMScript
 
 
-class Script(VIMScript):
+class Script(BaseScript, VIMScript):
     name = "VMWare.vHost.get_capabilities"
 
     def has_lldp_cli(self):
