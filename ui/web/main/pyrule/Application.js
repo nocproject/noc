e55553ddf9343508480fbf3cc9b9a07dc55f54fe
uncol <d.a.zakharov@gmail.com>
2025-07-16 09:52:13 +0300

    pyrule replace cmtext with codeviewer for source field

--- ui/web/main/pyrule/Application.js
+++ ui/web/main/pyrule/Application.js
@@ -7,55 +7,56 @@
 console.debug("Defining NOC.main.pyrule.Application");
 
 Ext.define("NOC.main.pyrule.Application", {
-    extend: "NOC.core.ModelApplication",
-    requires: [
-        // "NOC.core.CMText",
-        "NOC.main.pyrule.Model"
-    ],
-    model: "NOC.main.pyrule.Model",
-    formLayout: {
-        type: "vbox",
-        align: "stretch"
-    },
-    search: true,
-    initComponent: function () {
-        var me = this;
-        Ext.apply(me, {
-            columns: [
-                {
-                    text: __("Name"),
-                    dataIndex: "name",
-                    width: 200
-                },
-                {
-                    text: __("Full Name"),
-                    dataIndex: "full_name",
-                    flex: 1
-                }
-            ],
-            fields: [
-                {
-                    name: "name",
-                    xtype: "textfield",
-                    fieldLabel: __("Name"),
-                    allowBlank: false,
-                    uiStyle: "medium"
-                },
-                {
-                    name: "description",
-                    xtype: "textarea",
-                    fieldLabel: __("Description")
-                },
-                {
-                    name: "source",
-                    xtype: "cmtext",
-                    fieldLabel: __("Source"),
-                    allowBlank: true,
-                    flex: 1,
-                    mode: "python"
-                }
-            ]
-        });
-        me.callParent();
-    }
+  extend: "NOC.core.ModelApplication",
+  requires: [
+    "NOC.core.CodeViewer",
+    "NOC.main.pyrule.Model",
+  ],
+  model: "NOC.main.pyrule.Model",
+  formLayout: {
+    type: "vbox",
+    align: "stretch",
+  },
+  search: true,
+  initComponent: function(){
+    var me = this;
+    Ext.apply(me, {
+      columns: [
+        {
+          text: __("Name"),
+          dataIndex: "name",
+          width: 200,
+        },
+        {
+          text: __("Full Name"),
+          dataIndex: "full_name",
+          flex: 1,
+        },
+      ],
+      fields: [
+        {
+          name: "name",
+          xtype: "textfield",
+          fieldLabel: __("Name"),
+          allowBlank: false,
+          uiStyle: "medium",
+        },
+        {
+          name: "description",
+          xtype: "textarea",
+          fieldLabel: __("Description"),
+        },
+        {
+          name: "source",
+          xtype: "codeviewer",
+          fieldLabel: __("Source"),
+          allowBlank: true,
+          flex: 1,
+          language: "python",
+          theme: "vs-dark",
+        },
+      ],
+    });
+    me.callParent();
+  },
 });
