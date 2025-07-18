234048f0d3d8e6a7f61768a8b8b5bd3b93e7ebbb
Andrey Vertiprahov <aversanta@gmail.com>
2025-07-18 12:05:53 +0000

    release-25.1:Backport!9222

--- ui/web/aaa/group/Permission.js
+++ ui/web/aaa/group/Permission.js
@@ -7,83 +7,88 @@
 console.debug("Defining NOC.aaa.group.Permission");
 
 Ext.define("NOC.aaa.group.Permission", {
-    extend: "Ext.panel.Panel",
-    mixins: [
-        "Ext.form.field.Base"
-    ],
-    requires: [
-        "NOC.aaa.group.ApplicationPermission"
-    ],
-    alias: "widget.noc.group.permission",
-    defaultListenerScope: true,
-    setValue: function(value) {
-        var data = {};
-        if(value) {
-            data = this.unpack(value);
-            this.removeAll(true);
-            Ext.Object.each(data, function(name, module) {
-                var me = this;
-                me.add({
-                    xtype: "noc.group.applicationperm",
-                    title: module.name + " (" + name + ")",
-                    data: module.apps,
-                    listeners: {
-                        afterrender: function(panel) {
-                            panel.header.el.on("click", Ext.pass(panel.togglePanel, panel));
-                        }
-                    }
-                });
-            }, this);
-        }
-    },
-    getValue: function() {
-        var data = [];
-        Ext.each(this.getRefItems(), function(panel) {
-            Ext.Object.each(panel.getData(), function(name, title) {
-                Ext.each(title.perms, function(p) {
-                    if(p.status) {
-                        data.push(p.id.replace(/-/g, ":"));
-                    }
-                });
-            });
+  extend: "Ext.panel.Panel",
+  mixins: [
+    "Ext.form.field.Base",
+  ],
+  requires: [
+    "NOC.aaa.group.ApplicationPermission",
+  ],
+  alias: "widget.noc.group.permission",
+  defaultListenerScope: true,
+  setValue: function(value){
+    if(!Ext.isEmpty(value)){
+      let data = this.unpack(value);
+      this.removeAll(true);
+      Ext.Object.each(data, function(name, module){
+        var me = this;
+        me.add({
+          xtype: "noc.group.applicationperm",
+          title: module.name + " (" + name + ")",
+          data: module.apps,
+          listeners: {
+            afterrender: function(panel){
+              panel.header.el.on("click", Ext.pass(panel.togglePanel, panel));
+            },
+          },
         });
-        return data;
-    },
-    resetAllPermission: function() {
-        Ext.each(this.getRefItems(), function(panel) {
-            panel.resetPermission();
-        });
-    },
-    unpack: function(value) {
-        var data = {};
-        Ext.each(value, function(val) {
-            var perms = val.name.split(":"),
-                module = perms[0],
-                app = perms[1],
-                permission = perms[2],
-                makeId = function(name) {
-                    return name.replace(/:/g, "-");
-                };
-            if(data.hasOwnProperty(module)) {
-                if(data[module].hasOwnProperty("apps") && data[module]["apps"].hasOwnProperty(app)) {
-                    data[module]["apps"][app]["perms"].push({
-                        id: makeId(val.name),
-                        name: permission,
-                        status: val.status
-                    });
-                } else {
-                    // add application
-                    data[module]["apps"][app] = {title: val.title};
-                    data[module]["apps"][app]["perms"] = [{id: makeId(val.name), name: permission, status: val.status}];
-                }
-            } else {
-                // add module
-                data[module] = {name: val.module};
-                data[module]["apps"] = {};
-                data[module]["apps"][app] = {title: val.title};
-                data[module]["apps"][app]["perms"] = [{id: makeId(val.name), name: permission, status: val.status}];
-            }
-        });
-        return data;
+      }, this);
     }
+  },
+  getValue: function(){
+    var data = [];
+    Ext.each(this.getRefItems(), function(panel){
+      Ext.Object.each(panel.getData(), function(name, title){
+        Ext.each(title.perms, function(p){
+          if(p.status){
+            data.push(p.id.replace(/-/g, ":"));
+          }
+        });
+      });
+    });
+    return data;
+  },
+  resetAllPermission: function(){
+    Ext.each(this.getRefItems(), function(panel){
+      panel.resetPermission();
+    });
+  },
+  isPacked: function(value){
+    return typeof value === "object" && "name" in value;
+  },
+  unpack: function(value){
+    var data = {};
+    if(!this.isPacked(value[0])){
+      return value;
+    }
+    Ext.each(value, function(val){
+      var perms = val.name.split(":"),
+        module = perms[0],
+        app = perms[1],
+        permission = perms[2],
+        makeId = function(name){
+          return name.replace(/:/g, "-");
+        };
+      if(module in data){
+        if("apps" in data[module] && app in data[module]["apps"]){
+          data[module]["apps"][app]["perms"].push({
+            id: makeId(val.name),
+            name: permission,
+            status: val.status,
+          });
+        } else{
+          // add application
+          data[module]["apps"][app] = {title: val.title};
+          data[module]["apps"][app]["perms"] = [{id: makeId(val.name), name: permission, status: val.status}];
+        }
+      } else{
+        // add module
+        data[module] = {name: val.module};
+        data[module]["apps"] = {};
+        data[module]["apps"][app] = {title: val.title};
+        data[module]["apps"][app]["perms"] = [{id: makeId(val.name), name: permission, status: val.status}];
+      }
+    });
+    return data;
+  },
 });
