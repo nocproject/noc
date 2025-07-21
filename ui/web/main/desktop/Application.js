6c9856ff8a93428a88ecc0938ef05f24450acd38
uncol <d.a.zakharov@gmail.com>
2025-07-21 14:32:54 +0300

    Validate message type in onLogout method to ensure it's a string before processing.

--- ui/web/main/desktop/Application.js
+++ ui/web/main/desktop/Application.js
@@ -334,7 +334,7 @@ Ext.define("NOC.main.desktop.Application", {
   // Start logout sequence
   onLogout: function(msg){
     const url = "/api/login/logout/";
-    if(!Ext.isEmpty(msg)){
+    if(!Ext.isEmpty(msg) && typeof msg === "string"){
       window.removeEventListener("beforeunload", this.boundOnUnload);
       localStorage.setItem("NOC.restartReason", msg);
     }
