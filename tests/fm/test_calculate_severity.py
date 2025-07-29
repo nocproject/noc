5cbe722ec997ad24325fc7e9594a938728c69c3d
Andrey Vertiprahov <aversanta@gmail.com>
2025-07-29 12:02:40 +0500

    Fix tests.

--- tests/fm/test_calculate_severity.py
+++ tests/fm/test_calculate_severity.py
@@ -1,12 +1,9 @@
 # ----------------------------------------------------------------------
-# Test Alarm Watchers
+# Test calculate alarm severity
 # ----------------------------------------------------------------------
 # Copyright (C) 2007-2025 The NOC Project
 # See LICENSE for details
 # ----------------------------------------------------------------------
-
-# Python modules
-import datetime
 
 # Third-party modules
 import pytest
@@ -29,6 +26,7 @@ def user():
 
 def test_base_severity(alarm):
     """
+    Change based severity
     """
     severity = alarm.get_effective_severity()
     assert severity == 1000
@@ -38,15 +36,25 @@ def test_base_severity(alarm):
 
 def test_limit_severity_severity(alarm):
     """
+    Test Limit min/max severity
+    Set User Severity
     """
     alarm.add_watch(Effect.SEVERITY, key="", max_severity=500)
     severity = alarm.get_effective_severity()
     assert severity == 500
+    alarm.stop_watch(Effect.SEVERITY, key="")
     alarm.add_watch(Effect.SEVERITY, key="", min_severity=1500)
     severity = alarm.get_effective_severity()
     assert severity == 1500
 
 
-# Policy
-# Min/Max severity
-# User change Based Severity
+def test_severity_policies(alarm):
+    """
+    Test severity policies
+    """
+    # By summary
+    # severity = alarm.get_effective_severity(policy="AB", summary={})
+    # By Token
+    alarm.effective_labels = ["noc::severity::warning"]
+    severity = alarm.get_effective_severity(policy="ST")
+    assert severity == 2000
