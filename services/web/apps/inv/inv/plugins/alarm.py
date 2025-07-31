0302ddba22a7840104ce209036ccf4063733c614
Dmitry Volodin <dv@nocproject.org>
2025-07-31 16:32:26 +0200

    alarm plugin

--- services/web/apps/inv/inv/plugins/alarm.py
+++ services/web/apps/inv/inv/plugins/alarm.py
@@ -6,7 +6,8 @@
 # ---------------------------------------------------------------------
 
 # Python modules
-from typing import Dict
+from typing import Dict, DefaultDict, List, Any
+from collections import defaultdict
 
 # Third-party modules
 from bson import ObjectId
@@ -23,10 +24,31 @@ class AlarmPlugin(InvPlugin):
     js = "NOC.inv.inv.plugins.alarm.AlarmPanel"
     required_feature = Feature.FGALARMS
 
+    ROOT_ALARM_CLS = "fa fa-exclamation-triangle"
+    ALARM_CLS = "fa fa-exclamation"
+
     def init_plugin(self):
         super().init_plugin()
 
     def get_data(self, request, obj: Object):
+        def get_node(a: ObjectId) -> Dict[str, Any]:
+            alarm = alarms[a]
+            children_alarms = children[a]
+            r: Dict[str, Any] = {
+                "id": str(a),
+                "title": alarm.subject,
+                "alarm_class": alarm.alarm_class.name,
+                "iconCls": self.ROOT_ALARM_CLS if children_alarms else self.ALARM_CLS,
+            }
+            if children_alarms:
+                r["expanded"] = (True,)
+                r["children"] = [get_node(c) for c in children_alarms]
+            else:
+                r["leaf"] = True
+            return r
+
+        # @todo: Show external root alarms
+        # @todo: Relative path
         current = obj.as_resource()
         # Get all alarms
         alarms: Dict[ObjectId, ActiveAlarm] = {
@@ -35,6 +57,14 @@ class AlarmPlugin(InvPlugin):
                 resource_path__elemMatch={"code": PathCode.OBJECT.value, "path": current}
             )
         }
-        print("@" * 72)
-        print(">>>", alarms)
-        return {}
+        # parent -> list of children
+        children: DefaultDict[ObjectId, List[ObjectId]] = defaultdict(list)
+        # top-level
+        top: List[ObjectId] = []
+        for a in alarms.values():
+            if a.parent and a.parent.id in alarms:
+                children[a.parent.id].append(a.id)
+            else:
+                top.append(a.id)
+        r = [get_node(x) for x in top]
+        return {"expanded": True, "children": r}
--- ui/web/inv/inv/plugins/alarm/AlarmPanel.js
+++ ui/web/inv/inv/plugins/alarm/AlarmPanel.js
@@ -8,15 +8,58 @@ console.debug("Defining NOC.inv.inv.plugins.alarm.AlarmPanel");
 
 Ext.define("NOC.inv.inv.plugins.alarm.AlarmPanel", {
   extend: "Ext.panel.Panel",
+  requires: [
+    "NOC.inv.inv.plugins.alarm.AlarmModel",
+  ],
+
   title: __("Alarms"),
   closable: false,
   layout: "fit",
   //
   initComponent: function(){
-    this.callParent();
+    var me = this;
+
+    me.defaultRoot = {
+      text: __("."),
+      children: [],
+    };
+
+    me.store = Ext.create("Ext.data.TreeStore", {
+      model: "NOC.inv.inv.plugins.alarm.AlarmModel",
+      root: me.defaultRoot,
+    });
+
+    me.alarmPanel = Ext.create("Ext.tree.Panel", {
+      store: me.store,
+      rootVisible: false,
+      useArrows: true,
+      stateful: true,
+      stateId: "inv.inv-alarm-alarm",
+      columns: [
+        {
+          xtype: "treecolumn",
+          dataIndex: "title",
+          text: __("Title"),
+          width: 300,
+        },
+        {
+          text: __("Alarm Class"),
+          dataIndex: "alarm_class",
+          width: 200,
+        },
+      ],
+    });
+    //
+    Ext.apply(me, {
+      items: [
+        me.alarmPanel,
+      ],
+    });
+    me.callParent();
   },
   //
   preview: function(data, objectId){
-    
+    var me = this;
+    me.store.setRootNode(data || me.defaultRoot)
   },
 });
