840666b49dbbb91f2c7398ec4f59818de4cef86a
Andrey Vertiprahov <aversanta@gmail.com>
2025-08-05 09:37:16 +0500

    Fix set_caps function for detect change value.

--- tests/models/test_capabilities.py
+++ tests/models/test_capabilities.py
@@ -16,8 +16,8 @@ from noc.core.caps.decorator import capabilities
 class MockManagedObject(object):
     name = "mock"
 
-    def __init__(self, caps):
-        self.caps = caps
+    def __init__(self, caps=None):
+        self.caps = caps or []
 
     def save_caps(self, caps, **kwargs):
         self.caps = [
@@ -29,6 +29,18 @@ class MockManagedObject(object):
             }
             for c in caps
         ]
+
+
+def get_object_caps_mock() -> "MockManagedObject":
+    """Prepare ManagedObject for mock"""
+    # Send error/send success
+    mo = MockManagedObject()
+    return mo
+
+
+@pytest.fixture(scope="function")
+def object_caps():
+    return get_object_caps_mock()
 
 
 @pytest.mark.parametrize(
@@ -50,12 +62,24 @@ class MockManagedObject(object):
         ),
     ],
 )
-def test_update_caps(caps, update_caps, expected):
-    mock = MockManagedObject([])
+def test_update_caps(caps, update_caps, expected, object_caps):
     for c in caps:
-        mock.set_caps(**c)
-    mock.update_caps(update_caps, source="manual")
-    assert mock.get_caps() == expected
+        object_caps.set_caps(**c)
+    object_caps.update_caps(update_caps, source="manual")
+    assert object_caps.get_caps() == expected
+
+
+def test_set_caps(object_caps):
+    for _ in range(1, 4):
+        object_caps.set_caps("DB | Interfaces", 1)
+    assert len(object_caps.caps) == 1
+    for _ in range(1, 4):
+        object_caps.set_caps("CPE | Vendor", "Test1")
+    assert len(object_caps.caps) == 2
+    object_caps.set_caps("CPE | Vendor", "Test1", scope="New")
+    for _ in range(1, 4):
+        object_caps.set_caps("CPE | Vendor", "Test1", scope=None)
+    assert len(object_caps.caps) == 3
 
 
 @pytest.mark.parametrize(
@@ -85,10 +109,9 @@ def test_update_caps(caps, update_caps, expected):
         ),
     ],
 )
-def test_get_scope_caps(caps, update_caps, scope, expected_scope, expected):
-    mock = MockManagedObject([])
+def test_get_scope_caps(caps, update_caps, scope, expected_scope, expected, object_caps):
     for c in caps:
-        mock.set_caps(**c)
-    mock.update_caps(update_caps, source="manual", scope=scope)
-    assert mock.get_caps(scope=scope) == expected_scope
-    assert mock.get_caps() == expected
+        object_caps.set_caps(**c)
+    object_caps.update_caps(update_caps, source="manual", scope=scope)
+    assert object_caps.get_caps(scope=scope) == expected_scope
+    assert object_caps.get_caps() == expected
