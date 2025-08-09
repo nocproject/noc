9aba6f0a4cf6d6bc9c84b9489ac2e43e64a38aef
Andrey Vertiprahov <aversanta@gmail.com>
2025-08-09 02:04:01 +0000

    Add group_type field to ActiveAlarm.

--- fm/models/alarmjob.py
+++ fm/models/alarmjob.py
@@ -25,7 +25,7 @@ from mongoengine.fields import (
 )
 
 # NOC models
-from noc.core.fm.enum import AlarmAction, ActionStatus, GroupPolicy, ItemStatus
+from noc.core.fm.enum import AlarmAction, ActionStatus, ItemStatus
 from noc.sa.models.servicesummary import SummaryItem, ObjectSummaryItem
 
 
@@ -102,7 +102,7 @@ class ActionLog(EmbeddedDocument):
     stop_processing: bool = BooleanField(default=False)
     allow_fail: bool = BooleanField(default=False)
     repeat_num: int = IntField(default=0)
-    policy: GroupPolicy = EnumField(GroupPolicy, default=GroupPolicy.ROOT)
+    # policy: GroupPolicy = EnumField(GroupPolicy, default=GroupPolicy.ROOT)
     # Approve flag (is user Approved Received Message)
     # Notification adapter for sender
     # User Actions
