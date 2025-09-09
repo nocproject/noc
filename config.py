256e45958295e081bb3e78fdd9420f1004b18d65
Michael R <rmiclee@gmail.com>
2025-09-09 07:26:21 +0000

    Fix uuidRepresentation DeprecationWarning

--- config.py
+++ config.py
@@ -1105,6 +1105,7 @@ class Config(BaseConfig):
                 from noc.core.mongo.monitor import MongoCommandSpan
 
                 self._mongo_connection_args["event_listeners"] = [MongoCommandSpan()]
+            self._mongo_connection_args["uuidRepresentation"] = "standard"
         return self._mongo_connection_args
 
     def setup_logging(self, loglevel=None):
