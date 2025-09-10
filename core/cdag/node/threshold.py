5bdba3aaa0663864e57f110c098bb8ed950d4e4a
Michael R <rmiclee@gmail.com>
2025-09-10 10:44:45 +0300

    Revert TresholdNode clean_state to inherited

--- core/cdag/node/threshold.py
+++ core/cdag/node/threshold.py
@@ -220,10 +220,3 @@ class ThresholdNode(BaseCDAGNode):
 
     def __del__(self):
         self.reset_state()
-
-    def clean_state(self, state: Optional[Dict[str, Any]]) -> Optional[BaseModel]:
-        if not hasattr(self, "state_cls"):
-            return None
-        state = state or {}
-        c_state = self.state_cls(**state)
-        return c_state
