72d2c2588116968665885b02ca48342b8c8bddeb
Andrey Vertiprahov <aversanta@gmail.com>
2025-09-12 10:58:18 +0000

    Merge branch 'patch-avs-changed-caps' into 'master'
    Add changed caps when register Changes.
    
    See merge request noc/noc!9468
    
    (cherry picked from commit 5a56768d19468ac48e7a4bd287bfacd7fe2baa5f)
    
    b4b3c7bb Add changed caps when register Changes.
    c5980c2c Fix applied caps changes.
    08a07f3f Add on_change to vlan.
    baf6c743 Remove from_state param.
    00ebb688 Add from dict to change model.
    404daceb Remove unused imports.
    
    Co-authored-by: Andrey Vertiprahov <aversanta@gmail.com>

--- vc/models/vlanprofile.py
+++ vc/models/vlanprofile.py
@@ -25,6 +25,7 @@ from noc.core.mongo.fields import PlainReferenceField, ForeignKeyField
 from noc.core.bi.decorator import bi_sync
 from noc.core.model.decorator import on_delete_check, on_save
 from noc.core.vlanroles import VLANRole
+from noc.core.change.decorator import change
 
 id_lock = Lock()
 
@@ -35,6 +36,7 @@ VLAN_ROLE_LABEL_SCOPE = "vlanrole"
 @Label.model
 @on_save
 @bi_sync
+@change
 @on_delete_check(
     check=[
         ("vc.VLAN", "profile"),
