e0a42486e5c2227c92f989f7b0948526e2819dd3
Andrey Vertiprahov <aversanta@gmail.com>
2025-09-12 11:15:54 +0000

    release-25.1:Backport!9468

--- core/wf/decorator.py
+++ core/wf/decorator.py
@@ -57,16 +57,25 @@ def document_set_state(
     * Perform database update
     * Invalidate caches
     * Call State on_enter_handlers
-    :param self:
-    :param state:
-    :param state_changed:
-    :param bulk:
-    :param create: Set if assign default state
-    :return:
+    Args:
+        self:
+        state:
+        state_changed:
+        bulk:
+        create: Set if assign default state
     """
     # Direct update arguments
     set_op = {"state": state.id}
-    cf = ChangeField(field="state", old=str(self.state.id) if self.state else None, new=str(state))
+    if self.state:
+        cf = ChangeField(
+            field="state",
+            old=str(self.state.id),
+            old_label=str(self.state),
+            new=str(state.id),
+            new_label=str(self.state),
+        )
+    else:
+        cf = ChangeField(field="state", old=None, new=str(state.id), new_label=str(self.state))
     prev_labels = self.state.labels if self.state else []
     # Set state field
     self.state = state
@@ -159,17 +168,26 @@ def model_set_state(self, state, state_changed: datetime.datetime = None, bulk=N
     * Perform database update
     * Invalidate caches
     * Call State on_enter_handlers
-    :param self:
-    :param state:
-    :param state_changed:
-    :param bulk:
-    :param create: Set if assign default state
-    :return:
+    Args:
+        self:
+        state:
+        state_changed:
+        bulk:
+        create: Set if assign default state
     """
     # Direct update arguments
     logger.debug("[%s] Set state: %s", self.name, state)
     set_op = {"state": str(state.id)}
-    cf = ChangeField(field="state", old=str(self.state.id) if self.state else None, new=str(state))
+    if self.state:
+        cf = ChangeField(
+            field="state",
+            old=str(self.state.id),
+            old_label=str(self.state),
+            new=str(state.id),
+            new_label=str(self.state),
+        )
+    else:
+        cf = ChangeField(field="state", old=None, new=str(state.id), new_label=str(self.state))
     prev_labels = self.state.labels if self.state else []
     # Set state field
     self.state = state
