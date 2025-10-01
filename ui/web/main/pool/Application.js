da220504e7ab957e7bb60b0da69929457f1777f9
uncol <d.a.zakharov@gmail.com>
2025-10-01 12:12:41 +0300

    Overly permissive regular expression range - fixed

--- ui/web/main/pool/Application.js
+++ ui/web/main/pool/Application.js
@@ -7,47 +7,47 @@
 console.debug("Defining NOC.main.pool.Application");
 
 Ext.define("NOC.main.pool.Application", {
-    extend: "NOC.core.ModelApplication",
-    requires: [
-        "NOC.main.pool.Model"
-    ],
-    model: "NOC.main.pool.Model",
-    search: true,
-    helpId: "reference-pool",
+  extend: "NOC.core.ModelApplication",
+  requires: [
+    "NOC.main.pool.Model",
+  ],
+  model: "NOC.main.pool.Model",
+  search: true,
+  helpId: "reference-pool",
 
-    initComponent: function() {
-        var me = this;
-        Ext.apply(me, {
-            columns: [
-                {
-                    text: __("Name"),
-                    dataIndex: "name",
-                    width: 100
-                },
-                {
-                    text: __("Description"),
-                    dataIndex: "description",
-                    flex: 1
-                }
-            ],
+  initComponent: function(){
+    var me = this;
+    Ext.apply(me, {
+      columns: [
+        {
+          text: __("Name"),
+          dataIndex: "name",
+          width: 100,
+        },
+        {
+          text: __("Description"),
+          dataIndex: "description",
+          flex: 1,
+        },
+      ],
 
-            fields: [
-                {
-                    name: "name",
-                    xtype: "textfield",
-                    fieldLabel: __("Name"),
-                    regex: /^[0-9a-zA-z]{1,16}$/,
-                    allowBlank: false,
-                    uiStyle: "medium"
-                },
-                {
-                    name: "description",
-                    xtype: "textarea",
-                    fieldLabel: __("Description"),
-                    allowBlank: true
-                }
-            ]
-        });
-        me.callParent();
-    }
+      fields: [
+        {
+          name: "name",
+          xtype: "textfield",
+          fieldLabel: __("Name"),
+          regex: /^[0-9a-zA-Z]{1,16}$/,
+          allowBlank: false,
+          uiStyle: "medium",
+        },
+        {
+          name: "description",
+          xtype: "textarea",
+          fieldLabel: __("Description"),
+          allowBlank: true,
+        },
+      ],
+    });
+    me.callParent();
+  },
 });
