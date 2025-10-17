d714660aff3e8aa657cdcbb201964aea1e0a6387
Andrey Vertiprahov <aversanta@gmail.com>
2025-10-17 05:24:46 +0000

    Add database source to migrations.

--- sa/migrations/0265_migrate_caps_source_to_discovery.py
+++ sa/migrations/0265_migrate_caps_source_to_discovery.py
@@ -14,9 +14,16 @@ class Migration(BaseMigration):
         self.db.execute(
             """
             UPDATE sa_managedobject
-            SET caps = (select jsonb_agg(case
-            WHEN element @> '{"source": "caps"}' THEN element||'{"source": "discovery"}' ELSE element END)
+            SET caps = (select jsonb_agg(CASE
+            WHEN element @> '{"source": "caps"}' THEN element||'{"source": "discovery"}'
+            WHEN element @> '{"source": "attributes"}' THEN element||'{"source": "database"}'
+            WHEN element @> '{"source": "interface"}' THEN element||'{"source": "database"}'
+            WHEN element @> '{"source": "sla"}' THEN element||'{"source": "database"}'
+            WHEN element @> '{"source": "cpe"}' THEN element||'{"source": "database"}'
+            WHEN element @> '{"source": "asset"}' THEN element||'{"source": "database"}'
+            ELSE element END)
             FROM jsonb_array_elements(caps) as x(element))
-            WHERE caps @> '[{"source": "caps"}]'
+            WHERE caps @> '[{"source": "caps"}]' OR caps @> '[{"source": "interface"}]'
+                  OR caps @> '[{"source": "asset"}]' OR caps @> '[{"source": "attributes"}]'
         """
         )
