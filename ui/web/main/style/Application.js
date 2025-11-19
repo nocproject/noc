0f371e456aa02dcd0ff7cac2c3d7e78b33eb8071
uncol <d.a.zakharov@gmail.com>
2025-11-19 14:51:29 +0300

    refactor: enhance ColorField to use HTML5 color input and improve color handling (#98)

--- ui/web/main/style/Application.js
+++ ui/web/main/style/Application.js
@@ -51,37 +51,51 @@ Ext.define("NOC.main.style.Application", {
       allowBlank: true,
     },
     {
-      name: "font_color",
-      xtype: "colorfield",
-      uiStyle: "medium",
-      fieldLabel: __("Font Color"),
-      allowBlank: false,
-    },
+      xtype: "fieldset",
+      title: __("Colors"),
+      layout: "hbox",
+      defaults: {
+        xtype: "colorfield",
+        labelAlign: "top",
+        margin: "0 8 0 0",
+        width: 135,
+        allowBlank: false,
+      },
+      items: [
+        {
+          name: "font_color",
+          fieldLabel: __("Font"),
+        },
+        {
+          name: "background_color",
+          fieldLabel: __("Background"),
+        },
+      ]},
     {
-      name: "background_color",
-      xtype: "colorfield",
-      uiStyle: "medium",
-      fieldLabel: __("Background Color"),
-      allowBlank: false,
-    },
-    {
-      name: "bold",
-      xtype: "checkboxfield",
-      boxLabel: __("Bold"),
-      allowBlank: false,
-    },
-    {
-      name: "italic",
-      xtype: "checkboxfield",
-      boxLabel: __("Italic"),
-      allowBlank: false,
-    },
-    {
-      name: "underlined",
-      xtype: "checkboxfield",
-      boxLabel: __("Underlined"),
-      allowBlank: false,
-    },
+      xtype: "fieldset",
+      title: __("Style"),
+      layout: "hbox",
+      defaults: {
+        margin: "0 8 0 0",
+        allowBlank: false,
+      },
+      items: [
+        {
+          name: "bold",
+          xtype: "checkboxfield",
+          boxLabel: __("Bold"),
+        },
+        {
+          name: "italic",
+          xtype: "checkboxfield",
+          boxLabel: __("Italic"),
+        },
+        {
+          name: "underlined",
+          xtype: "checkboxfield",
+          boxLabel: __("Underlined"),
+        },
+      ]},
   ],
   filters: [
     {
