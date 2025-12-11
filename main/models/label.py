eda32872ab72e3d870586864fad9bc99e6ba9a20
Andrey Vertiprahov <aversanta@gmail.com>
2025-12-11 03:54:43 +0000

    release-25.1:Backport!9821

--- main/models/label.py
+++ main/models/label.py
@@ -112,6 +112,10 @@ class PrefixFilterItem(EmbeddedDocument):
 @on_delete
 @on_delete_check(
     check=[
+        ("sa.CapsProfile", "caps__set_label"),
+    ],
+    check_labels=[
+        ("wf.State", "labels"),
         ("fm.AlarmRule", "match__labels"),
         ("fm.AlarmRule", "match__exclude_labels"),
         ("pm.MetricRule", "match__labels"),
@@ -120,7 +124,6 @@ class PrefixFilterItem(EmbeddedDocument):
         ("main.MessageRoute", "match__exclude_labels"),
         ("sa.ObjectDiagnosticConfig", "match__labels"),
         ("sa.ObjectDiagnosticConfig", "match__exclude_labels"),
-        ("sa.CapsProfile", "caps__set_label"),
         ("sa.CredentialCheckRule", "match__labels"),
         ("sa.CredentialCheckRule", "match__exclude_labels"),
         ("sa.ReactionRule", "conditions__labels"),
@@ -129,7 +132,6 @@ class PrefixFilterItem(EmbeddedDocument):
         ("inv.ResourceGroup", "dynamic_service_labels__labels"),
         ("inv.ResourceGroup", "dynamic_client_labels__labels"),
         ("sa.ManagedObjectProfile", "match_rules__labels"),
-        ("wf.State", "labels"),
     ],
     clean=[
         ("sa.ManagedObject", "labels"),
