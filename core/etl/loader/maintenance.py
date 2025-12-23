b1031d84c9d3aec7f673001ccebc9f7b22ae5324
Andrey Vertiprahov <aversanta@gmail.com>
2025-12-23 11:08:46 +0500

    Add get_by methods to MaintenanceType.

--- core/etl/loader/maintenance.py
+++ core/etl/loader/maintenance.py
@@ -5,6 +5,9 @@
 # See LICENSE for details
 # ----------------------------------------------------------------------
 
+# Python modules
+from typing import Dict, Any
+
 # NOC modules
 from .base import BaseLoader
 from ..models.maintenance import Maintenance
@@ -22,3 +25,12 @@ class MaintenanceLoader(BaseLoader):
     model = MaintenanceModel
     data_model = Maintenance
     model_mappings = {"type": MaintenanceType}
+
+    post_save_fields = {"objects"}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.clean_map["type"] = MaintenanceType.get_by_name
+
+    def post_save(self, o: MaintenanceModel, fields: Dict[str, Any]):
+        """Processed maintenance object"""
--- core/etl/models/maintenance.py
+++ core/etl/models/maintenance.py
@@ -7,19 +7,36 @@
 
 # Python modules
 import datetime
-from typing import Optional, List
+from typing import Optional, List, Union, Literal
+
+# Third-party modules
+from pydantic import BaseModel as _BaseModel
 
 # NOC modules
 from .base import BaseModel
 
 
+class MaintenanceObject(_BaseModel):
+    type: Literal["managed_object"]
+    remote_id: str
+    name: Optional[str] = None
+    # As Resource ?
+
+
+class MaintenanceService(_BaseModel):
+    type: Literal["service"]
+    remote_id: str
+    name: Optional[str] = None
+
+
 class Maintenance(BaseModel):
     id: str
     subject: str
+    type: str
     start: datetime.datetime
     stop: datetime.datetime
     contacts: List[str]
-    type: Optional[str] =None
+    objects: List[Union[MaintenanceObject, MaintenanceService]]
     description: Optional[str] = None
     # Deadline
     suppress_alarms: bool = True
