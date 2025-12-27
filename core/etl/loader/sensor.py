f0c6f3100fd030422f7c009d50f7c6147b716e42
Andrey Vertiprahov <aversanta@gmail.com>
2025-12-27 09:33:28 +0000

    Merge branch 'patch-avs-add-sensor-map-to-portmap-discovery' into 'master'
    Add NRI Sensor map to nri_port discovery.
    
    See merge request noc/noc!9866
    
    (cherry picked from commit c72e8aa7d20bd3dbd3d7b23f7f820b8e0636e4d5)
    
    c2a61c06 Add NRI Sensor map to nri_port discovery.
    df40b72c Remove unused imports.
    
    Co-authored-by: Andrey Vertiprahov <aversanta@gmail.com>

--- core/etl/loader/sensor.py
+++ core/etl/loader/sensor.py
@@ -8,7 +8,6 @@
 # NOC modules
 from .base import BaseLoader
 from ..models.sensor import Sensor
-from noc.inv.models.object import Object
 from noc.inv.models.sensor import Sensor as SensorModel
 from noc.pm.models.measurementunits import MeasurementUnits
 
@@ -28,17 +27,3 @@ class SensorLoader(BaseLoader):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.clean_map["units"] = lambda x: MeasurementUnits.get_by_name(x) if x else None
-
-    def find_object(self, v, mappings=None, **kwargs):
-        """
-        Find object by remote system/remote id
-        Args:
-            v:
-            mappings
-        """
-        r = super().find_object(v)
-        if not r:
-            oo = Object.get_managed(v["managed_object"])
-            find_query = {"local_id": v["local_id"], "object": oo}
-            r = self.model.objects.filter(**find_query).first()
-        return r
