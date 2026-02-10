7288b188f7b684d07c4e61bc12510048a3433bb7
Andrey Vertiprahov <aversanta@gmail.com>
2026-02-10 08:46:54 +0000

    Merge branch 'fix-avs-event-deduplicate-vars' into 'master'
    Fix deduplication filter for event.
    
    See merge request noc/noc!9936
    
    (cherry picked from commit 6ce0e2b79c8284991cf2f1d5e29e597c232d54b7)
    
    d5465371 Fix deduplication filter for event.
    
    Co-authored-by: Andrey Vertiprahov <aversanta@gmail.com>

--- services/classifier/evfilter/dedup.py
+++ services/classifier/evfilter/dedup.py
@@ -20,7 +20,9 @@ class DedupFilter(BaseEvFilter):
     @staticmethod
     def event_hash(event: Event, event_config: EventConfig, e_vars) -> int:
         var_hash = dict_hash_int(e_vars) if e_vars else 0
-        return hash_int(f"{event.target.id}:{event_config.id}:{var_hash}")
+        return hash_int(
+            f"{event.target.id}:{event_config.id}:{var_hash}:{event.message or ''}:{event.type.severity or 0}"
+        )
 
     @staticmethod
     def get_window(event_config: EventConfig) -> int:
