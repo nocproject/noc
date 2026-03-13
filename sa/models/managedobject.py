1921b820db70618d0173e0d54b293635e61bfffe
Andrey Vertiprahov <aversanta@gmail.com>
2026-03-13 06:18:49 +0000

    Merge branch 'fix-avs-empty-lastts-on-mo-statuses' into 'master'
    Fix error when save empty last_ts on ManagedObject status
    
    See merge request noc/noc!9956
    
    (cherry picked from commit fd3a80968ffe0f0276e510c4f8de5d0924df20c8)
    
    9338e9de Fix error when save empty last_ts on ManagedObject status
    ebfdead4 Fix update query.
    
    Co-authored-by: Andrey Vertiprahov <aversanta@gmail.com>

--- sa/models/managedobject.py
+++ sa/models/managedobject.py
@@ -51,8 +51,8 @@ from pymongo import ASCENDING
 # NOC modules
 from noc.core.model.base import NOCModel
 from noc.config import config
-from noc.core.diagnostic.types import DiagnosticValue, DiagnosticState, DiagnosticConfig
-from noc.core.diagnostic.decorator import diagnostic, DEFER_CHANGE_STATE
+from noc.core.diagnostic.types import DiagnosticValue, DiagnosticConfig
+from noc.core.diagnostic.decorator import diagnostic
 from noc.core.diagnostic.hub import (
     DIAGNOCSTIC_LABEL_SCOPE,
     SA_DIAG,
@@ -122,7 +122,7 @@ from noc.core.model.decorator import (
 )
 from noc.inv.models.object import Object
 from noc.inv.models.resourcegroup import ResourceGroup
-from noc.core.defer import call_later, defer
+from noc.core.defer import call_later
 from noc.core.cache.decorator import cachedmethod
 from noc.core.cache.base import cache
 from noc.core.script.caller import SessionContext, ScriptCaller
@@ -3349,9 +3349,9 @@ class ManagedObjectStatus(NOCModel):
     ):
         """
         Update statuses bulk
-        :param statuses:
-        :param update_jobs:
-        :return:
+        Args:
+            statuses: Statuses bulk
+            update_jobs: Suspend ManagedObject jobs
         """
         from django.db import connection as pg_connection
         from collections import defaultdict
@@ -3363,22 +3363,21 @@ class ManagedObjectStatus(NOCModel):
         bulk = {}
         outages: List[Tuple[int, datetime.datetime, datetime.datetime]] = []
         # Getting current status
+        # Getting current status
         cs = {}
         with pg_connection.cursor() as cursor:
             cursor.execute(
                 """
-                SELECT id, os.status, os.last, mo.pool, diagnostics -> 'FIRST_AVAIL' ->> 'state' as fa_state
+                SELECT id, os.status, os.last, mo.pool
                 FROM sa_managedobject AS mo
                 LEFT JOIN sa_objectstatus AS os ON mo.id = os.managed_object_id
                 WHERE id = ANY(%s::INT[])
                 """,
                 [[x[0] for x in statuses]],
             )
-            for o, status, last, pool, d_state in cursor:
+            for o, status, last, pool in cursor:
                 pool = Pool.get_by_id(pool)
                 cs[o] = {"status": status, "last": last, "pool": pool.name}
-                if d_state and DiagnosticState(d_state) == DiagnosticState.unknown:
-                    cs[o]["d_avail_state"] = None
         # Processed new statuses
         suspended_jobs = defaultdict(list)  # Pool - ids
         for oid, status, ts in statuses:
@@ -3386,7 +3385,14 @@ class ManagedObjectStatus(NOCModel):
                 logger.error("Unknown object id: %s", oid)
                 continue
             ts = (ts or now).replace(microsecond=0, tzinfo=None)
-            if cs[oid]["status"] is None or (cs[oid]["status"] != status and cs[oid]["last"] <= ts):
+            if cs[oid]["status"] is None or cs[oid]["status"] != status:
+                if not cs[oid]["last"]:
+                    # Unknown state, from migration?
+                    pass
+                elif cs[oid]["last"] > ts:
+                    # Oops, out-of-order update
+                    # Restore correct state
+                    continue
                 bulk[oid] = (oid, status, ts)  # Only last status
                 if update_jobs:
                     suspended_jobs[(cs[oid]["pool"], status)].append(oid)
@@ -3394,21 +3400,10 @@ class ManagedObjectStatus(NOCModel):
                 if cs[oid]["last"] and status:
                     outages.append((oid, cs[oid]["last"], ts))
                 cs[oid].update({"status": status, "last": ts})
-            elif cs[oid]["last"] > ts:
-                # Oops, out-of-order update
-                # Restore correct state
-                pass
-            if status and "d_avail_state" in cs[oid]:
-                # fire event
-                defer(
-                    DEFER_CHANGE_STATE,
-                    diagnostic=FIRST_AVAIL,
-                    state=DiagnosticState.enabled.value,
-                    oid=oid,
-                    key=oid,
-                )
         if not bulk:
             return
+        # Add/Remove suspended Jobs
+        # Ensure First Avail - check job
         # Save statuses to db
         with pg_connection.cursor() as cursor:
             execute_values(
@@ -3416,7 +3411,7 @@ class ManagedObjectStatus(NOCModel):
                 """
                 INSERT INTO sa_objectstatus as os (managed_object_id, status, last) VALUES %s
                 ON CONFLICT (managed_object_id) DO UPDATE SET status = EXCLUDED.status, last = EXCLUDED.last
-                WHERE os.status != EXCLUDED.status and os.last < EXCLUDED.last
+                WHERE os.status != EXCLUDED.status and (os.last < EXCLUDED.last or os.last is Null)
                 """,
                 list(bulk.values()),
                 page_size=500,
